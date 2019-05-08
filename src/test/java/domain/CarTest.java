a61897a4766fae5a620fce049eeac39cac63e8de
heebong <heebg2@naver.com>
2019-05-08 17:58:39 +0900

    Car 객체에 대한 단위테스트 작성 - 객체 이름이 6글자 이상 or 0글자일 경우 예외 발생 - 객체 이름이 생성시 만든 이름과 동일 - 랜덤 함수가 0~3일 경우 포지션 고정 - 랜덤 함수가 4~9일 경우만 포지션 증가

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,48 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("hello");
+    }
+
+    @Test
+    void 자동차_객체_생성() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car("123456");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car("");
+        });
+
+        assertThat(car.getName()).isEqualTo("hello");
+    }
+
+    @Test
+    void 자동차_포지션_증가일_경우() {
+        int init = car.getPosition();
+        for (int i = Const.GO_RAN_NUM; i < Const.RANGE_RAN_NUM; i++) {
+            car.increasePositionOrNot(i);
+            assertThat(car.getPosition()).isEqualTo(++init);
+        }
+
+    }
+
+    @Test
+    void 자동차_포지션_변화_없음() {
+        int init = car.getPosition();
+        for (int i = Const.ZERO; i < Const.GO_RAN_NUM; i++) {
+            car.increasePositionOrNot(i);
+            assertThat(car.getPosition()).isEqualTo(init);
+        }
+    }
+}
