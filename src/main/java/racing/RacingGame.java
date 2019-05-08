fd64611cd6709a5366b01b39863161abc6e6876e
mrkwon <erased11@gmail.com>
2019-05-08 16:38:31 +0900

    [수정] getWinnerList 접근제어자 수정

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -33,7 +33,7 @@ public class RacingGame {
         System.out.println(result + "가 최종 우승했습니다.");
     }
 
-    private List<String> getWinnerList() {
+    public List<String> getWinnerList() {
             List<String> result = new ArrayList<>();
             Integer maxStatus  = Collections.max(carList).getStatus();
             for (Car car : carList) {
