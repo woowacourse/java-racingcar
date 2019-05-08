fb41b213b231931577370250586d67f53c50fe4c
men7627 <men1219@hanmail.net>
2019-05-08 17:22:51 +0900

    Update checkExpression(), inputExpression() for Exceoption

--- src/main/java/cal/InputView.java
+++ src/main/java/cal/InputView.java
@@ -7,11 +7,19 @@ public class InputView {
 	private static final String expressionPattern = "^(-?[0-9]+)(\\s(\\+|-|\\*|/)\\s(-?[0-9]+))+$";
 
 	public static String inputExpression() {
-		System.out.println("식을 입력하세요 : ");
-		return scanner.nextLine();
+		try {
+			System.out.println("식을 입력하세요 : ");
+			return checkExpression(scanner.nextLine());
+		} catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+			return inputExpression();
+		}
 	}
 
-	static boolean checkExpression(String expression) {
-		return expression.matches(expressionPattern);
+	static String checkExpression(String expression) {
+		if(!expression.matches(expressionPattern)){
+			throw new IllegalArgumentException("잘못 된 식입니다.");
+		}
+		return expression;
 	}
 }
