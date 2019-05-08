d3e171ec43642ff474a930addcfcdfbb46e6793b
stopsilver13 <stopsilver13>
2019-05-08 15:19:36 +0900

    validate car name length

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -4,7 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringUtils {
+    private static int CAR_NAME_MAX = 5;
+
     public static List<String> parseStringByComma(String string) {
         return Arrays.asList(string.split(","));
+    }
+
+    public static boolean isCarNameLength(String strings) {
+        if (strings.length() > CAR_NAME_MAX) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/util/StringUtilsTest.java
+++ src/test/java/util/StringUtilsTest.java
@@ -1,5 +1,6 @@
 package util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,5 +13,11 @@ public class StringUtilsTest {
     void parseStringByComma() {
         List<String> answer = Arrays.asList("a", "b", "c");
         assertThat(StringUtils.parseStringByComma("a,b,c")).isEqualTo(answer);
+    }
+
+    @Test
+    void isCarNameLength() {
+        assertThat(StringUtils.isCarNameLength("abc")).isTrue();
+        assertThat(StringUtils.isCarNameLength("abcdef")).isFalse();
     }
 }
