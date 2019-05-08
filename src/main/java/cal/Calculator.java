084371bfd1faab7eda3238af6681015136b5dbc1
men7627 <men1219@hanmail.net>
2019-05-08 14:59:39 +0900

    Add splitExpression() to Calculator

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -4,5 +4,14 @@ public class Calculator {
 	private int value;
 
 	public int execute(String expression) {
+		String[] splitedExpression = splitExpression(expression);
+
+		for (int i = 1; i < splitedExpression.length; i+=2) {
+
+		}
+	}
+
+	private String[] splitExpression(String expression) {
+		return expression.split(" ");
 	}
 }
