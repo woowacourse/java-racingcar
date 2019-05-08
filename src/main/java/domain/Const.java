5987ba3ff04e8e3fe975f8beba135c7f38686c38
heebong <heebg2@naver.com>
2019-05-08 17:19:26 +0900

    상수 관리 Const class  추가

---
+++ src/main/java/domain/Const.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Const {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_ROUND_CNT = "시도할 회수는 몇회인가요?";
+    public static final String EX_NAME_LEN = "이름은 1글자 이상 5글자 이하로 입력해주세요.";
+
+    public static final int MIN_NAME_LEN = 1;
+    public static final int MAX_NAME_LEN = 5;
+    public static final int GO_RAN_NUM = 4;
+    public static final int RANGE_RAN_NUM = 10;
+}
