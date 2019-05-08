6d82a39bbc9a333bb614ebc89cde2ec69e371b4d
deocksoo <dusol92@snu.ac.kr>
2019-05-08 14:47:07 +0900

    계산기 사칙연산 기능을 추가합니다

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,22 +1,58 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
-    int add(int a, int b) {
-        return a + b;
-    }
-
-    int sub(int a, int b) {
-        return a - b;
-    }
-
-    int mul(int a, int b) {
-        return a * b;
-    }
-
-    int div(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division By Zero");
+    List<String> getSplit(String inputString) {
+        List<String> row = Arrays.stream(inputString.split(" ")).collect(Collectors.toList());
+        
+        if (row.size() == 0) {
+            throw new IllegalArgumentException("데이터가 잘못되었습니다!");
         }
-        return a / b;
+        return row;
+    }
+
+    int calculate(String inputString) {
+        int result = 0;
+        String operator = null;
+        List<String> calculationFormula = getSplit(inputString);
+
+        for (String numberOrOperator : calculationFormula) {
+            if (isOperator(numberOrOperator)) {
+                operator = numberOrOperator;
+                continue;
+            }
+
+            if (operator == null) {
+                result += Integer.valueOf(numberOrOperator);
+                continue;
+            }
+
+            result = operate(result, operator, Integer.valueOf(numberOrOperator));
+        }
+        return result;
+    }
+
+    boolean isOperator(String operator) {
+        return (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"));
+    }
+
+    int operate(int previousResult, String operator, int number) {
+        if (operator.equals("+")) {
+            return previousResult + number;
+        }
+        if (operator.equals("-")) {
+            return previousResult - number;
+        }
+        if (operator.equals("*")) {
+            return previousResult * number;
+        }
+        if (operator.equals("/")) {
+            return previousResult / number;
+        }
+        return previousResult;
     }
 }
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    void 유효하지_않은_값() {
+        /* Given : */
+        /* When: */
+        /* Then: */
+    }
+
+    @Test
+    void 더하기() {
+        assertThat(calculator.calculate("2 + 4")).isEqualTo(6);
+    }
+
+    @Test
+    void 빼기() {
+        assertThat(calculator.calculate("2 - 4")).isEqualTo(-2);
+    }
+
+    @Test
+    void 곱하기() {
+        assertThat(calculator.calculate("2 * 4")).isEqualTo(8);
+    }
+
+    @Test
+    void 나누기() {
+        assertThat(calculator.calculate("2 / 4")).isEqualTo(0);
+    }
+
+    @Test
+    void 더하고_빼고_곱하고_나누고() {
+        assertThat(calculator.calculate("2 + 4 * 3 / 2 - 7")).isEqualTo(2);
+    }
+}
