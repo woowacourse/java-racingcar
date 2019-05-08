5da69fb9341ae2848861eaac25c15be6341db3da
mrkwon <erased11@gmail.com>
2019-05-08 15:46:57 +0900

    [유닛테스트] Car Class 의 goOrStop 메서드 테스트

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,12 +1,25 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    Car car;
+    @BeforeEach
+    void setUp() {
+        car = new Car("Lamborghini");
+    }
 
     @Test
-    void goOrStop() {
+    void 전진잘하나요() {
+        assertThat(car.goOrStop(5)).isEqualTo(true);
+    }
+
+    @Test
+    void 후진잘하나요() {
+        assertThat(car.goOrStop(3)).isEqualTo(false);
     }
 }
