d2f681d75780aecf588f5d2a6a2eb170cac82ee3
Seongmo Jeon <oeeen3@gmail.com>
2019-05-08 14:31:41 +0900

    feat: 전체 계산 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -33,4 +33,12 @@ public class Calculator {
 
         throw new IllegalArgumentException("올바른 연산자가 아닙니다.");
     }
+
+    int calculateAll(String[] inputs) throws Exception {
+        int result = Integer.parseInt(inputs[0]);
+        for (int i = 1; i < inputs.length; i+=2) {
+            result = calculate(result, inputs[i], Integer.parseInt(inputs[i + 1]));
+        }
+        return result;
+    }
 }
