359ee336f142e05d739c0f95c964cd9024cae0b0
men7627 <men1219@hanmail.net>
2019-05-08 15:04:30 +0900

    Add parseInt() to Calculator

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -4,14 +4,24 @@ public class Calculator {
 	private int value;
 
 	public int execute(String expression) {
-		String[] splitedExpression = splitExpression(expression);
+		String[] parsedExpression = splitExpression(expression);
 
-		for (int i = 1; i < splitedExpression.length; i+=2) {
-
+		value = parseInt(parsedExpression[0]);
+		for (int i = 1; i < parsedExpression.length; i+=2) {
+			int number = parseInt(parsedExpression[i]);
+			String operator = parsedExpression[i+1];
 		}
+
+		return value;
+	}
+
+	private int parseInt(String str) {
+		return Integer.parseInt(str);
 	}
 
 	private String[] splitExpression(String expression) {
 		return expression.split(" ");
 	}
+
+	
 }
