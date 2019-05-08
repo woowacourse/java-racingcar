a707a5c03f86b53d8948f05550b6b208565f78de
DaejunBae <dpudpu11@gmail.com>
2019-05-08 14:38:51 +0900

    feat: Create Calculator Class
    스태틱 클래스인 Calculator를 생성.
    파라미터는 모두 double이며 return 역시 double.
    Methods:
    plus
    minus
    multiply
    divide

---
+++ src/main/java/cal/Calculator.java
@@ -0,0 +1,19 @@
+package cal;
+
+public class Calculator {
+    public static double plus(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double minus(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
