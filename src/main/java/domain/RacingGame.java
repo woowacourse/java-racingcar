93fe96847b9dfe7296c8f28f0509a1f49d3b1ae9
강연욱 <fiasboc23@gmail.com>
2019-05-08 15:17:26 +0900

    시도 횟수 입력
    예외처리 미구현

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class RacingGame {
     public static Scanner sc = new Scanner(System.in);
     private ArrayList<String> carNames;
+    private int gameRound;
 
     private String input(){
         return sc.nextLine();
@@ -15,5 +16,11 @@ public class RacingGame {
     private void inputNames() {
         String names = input();
         carNames = new ArrayList<>(Arrays.asList(names.split(",")));
+    }
+
+    private void inputRound(){
+        String round = input();
+        //예외
+        gameRound = Integer.parseInt(round);
     }
 }
