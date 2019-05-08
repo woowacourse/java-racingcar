485d2e1d5861b9816005d760b37b47d4c3585b6b
stopsilver13 <stopsilver13>
2019-05-08 14:33:39 +0900

    input car names from user

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요 (이름은 쉼표를(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+}
