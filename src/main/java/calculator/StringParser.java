c61a8fbbfb55b5cfaa01e629835932f00e953556
Seongmo Jeon <oeeen3@gmail.com>
2019-05-08 12:23:03 +0900

    feat: 문자열 입력 기능 추가

---
+++ src/main/java/calculator/StringParser.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class StringParser {
+    String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+}
