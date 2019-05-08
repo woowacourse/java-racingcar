dc7b048f36a91600c885aafd3acbb3fe7ad2da47
Kwanwoo Kang <kkw200@naver.com>
2019-05-08 18:06:36 +0900

    랜덤 넘버 생성, 자동차 객체, 자동차 이동

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,4 +1,9 @@
 package racingcar;
 
 public class RacingCar {
+    public static void main(String[] args) {
+        Car c = new Car("ss");
+        c.move(new Car.RandomMovement());
+    }
+
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+import org.junit.jupiter.api.Test;
+
 public class RacingCarTest {
+
+
+    @Test
+    void moveTest() {
+        Car car = new Car("testCar");
+        car.move(new ForceMove());
+    }
+
+
+}
+
+// 코드 포함?
+class ForceMove implements Car.MovableStrategy {
+    public boolean isMove() {
+        return true;
+    }
 }
