4296ab33389e89cb09ef50d06d192849588b7351
men7627 <men1219@hanmail.net>
2019-05-08 16:30:49 +0900

    Add checkExpression() to InputView

--- src/main/java/cal/InputView.java
+++ src/main/java/cal/InputView.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
+	private static final String expressionPattern = "^(-?[0-9]+)(\\s(\\+|-|\\*|/)\\s(-?[0-9]+))+$";
 
 	public static String inputExpression() {
 		System.out.println("식을 입력하세요 : ");
 		return scanner.nextLine();
+	}
+
+	public static boolean checkExpression(String expression) {
+		return expression.matches(expressionPattern);
 	}
 }
