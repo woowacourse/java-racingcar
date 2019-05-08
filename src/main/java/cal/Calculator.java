23db110df4f88ab0e2d9f3abf4187f9aecf8a25a
men7627 <men1219@hanmail.net>
2019-05-08 15:15:28 +0900

    Add calculate() to Calculator

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -1,15 +1,20 @@
 package cal;
 
 public class Calculator {
+	private static String PLUS = "+";
+	private static String MINUS = "-";
+	private static String MULTIPLY = "*";
+	private static String DEVIDE = "/";
 	private int value;
 
 	public int execute(String expression) {
 		String[] parsedExpression = splitExpression(expression);
 
 		value = parseInt(parsedExpression[0]);
-		for (int i = 1; i < parsedExpression.length; i+=2) {
-			int number = parseInt(parsedExpression[i]);
-			String operator = parsedExpression[i+1];
+		for (int i = 1; i < parsedExpression.length; i += 2) {
+			String operator = parsedExpression[i];
+			int number = parseInt(parsedExpression[i + 1]);
+
 		}
 
 		return value;
@@ -23,5 +28,37 @@ public class Calculator {
 		return expression.split(" ");
 	}
 
-	
+	private void calculate(String operator, int number) {
+		if (operator.equals(PLUS)) {
+			add(number);
+		}
+
+		if (operator.equals(MINUS)) {
+			minus(number);
+		}
+
+		if (operator.equals(MULTIPLY)) {
+			multiply(number);
+		}
+
+		if (operator.equals(DEVIDE)) {
+			devide(number);
+		}
+	}
+
+	private void add(int number) {
+		value += number;
+	}
+
+	private void minus(int number) {
+		value -= number;
+	}
+
+	private void multiply(int number) {
+		value *= number;
+	}
+
+	private void devide(int number) {
+		value /= number;
+	}
 }
