727ba6edefd240feef1fba35fbf06b5df851e213
Ko eunsuk <eunsukko.128@gmail.com>
2019-05-09 14:14:58 +0900

    InputView 구현
    - 숫자받는 것 구현
    - 차이름들을 받는다

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
 import domain.Car;
 import domain.RacingGame;
+import view.InputView;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args) {
         // 입력 받기
-        ArrayList<Car> cars = new ArrayList();
+        String [] names = InputView.readNames();
+        ArrayList<Car> cars;
 
-        int nTrials = 10;
+        int nTrials = InputView.readNumber();
 
         RacingGame racingGame = new RacingGame();
         racingGame.play();
