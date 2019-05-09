c37453ba069fe1e0cdbe6ab063cba0fb6192161c
men7627 <men1219@hanmail.net>
2019-05-09 14:44:58 +0900

    Update RacingGame

--- src/main/java/model/RacingGame.java
+++ src/main/java/model/RacingGame.java
@@ -5,22 +5,12 @@ import java.util.List;
 
 public class RacingGame {
     List<Car> cars;
-    List<String> winners;
-    int lapCount;
 
-    RacingGame(int lapCount) {
-        cars = new ArrayList<>();
-        winners = new ArrayList<>();
-        this.lapCount = lapCount;
+    RacingGame(List<Car> cars) {
+        this.cars = cars;
     }
 
     void run() {
-        for (int i = 0; i < lapCount; i++) {
-            runLap();
-        }
-    }
-
-    private void runLap() {
         Racing racing = new Racing();
         for (Car car : cars) {
             racing.move(car);
