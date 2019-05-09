c00f3b18e3b244e573650f1376dc59613fc3a39a
kmdngyu <kmdngyu@gmail.com>
2019-05-09 15:59:13 +0900

    test: 중복된 이름

--- src/test/java/racing/domain/RaceTest.java
+++ src/test/java/racing/domain/RaceTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class RaceTest {
@@ -17,4 +18,13 @@ public class RaceTest {
         race = new Race();
     }
 
+    @Test
+    void 중복확인테스트() {
+        List<String> names = Arrays.asList(new String [] {"a","b","b"});
+        assertThrows(IllegalArgumentException.class, () -> {
+            race.checkDuplicated(names);
+        });
+        names.set(2,"c");
+        assertThat(race.checkDuplicated(names)).isEqualTo(true);
+    }
 }
