be7176f3a029c916c5ddc055b2b7bd4ed3df9c58
kangmin <kangmin789@naver.com>
2019-05-09 10:55:06 +0900

    [추가] 입력받은 자동차 이름을 분리하는 splitCar 메소드  추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,4 +1,24 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+
+    List<Car> cars = new ArrayList<Car>();
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+
+        String names = inputView.InputCarNames();
+        addCar(names);
+    }
+
+    private static void addCar(String names) {
+        splitCar(names);
+    }
+
+    private static String[] splitCar(String names) {
+        return names.split(",");
+    }
 }
