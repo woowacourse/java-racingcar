2c3febb958fa0d176a6c086e5efd4373b86db606
Ko eunsuk <eunsukko.128@gmail.com>
2019-05-09 14:25:41 +0900

    Car 객체 생성
    이름과 포지션을 가진 차 객체 생성

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,4 +1,19 @@
 package domain;
 
 public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public Car(String name){
+        this(name,0);
+    }
+
+    public void move() {
+        position++;
+    }
 }
