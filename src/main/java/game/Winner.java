3cbee4eeb4ddfeae13fe05b41e3618931bb6e6f5
Soojin Roh <sooreal91@gmail.com>
2019-05-09 11:32:05 +0900

    feature : Winner클래스 생성

---
+++ src/main/java/game/Winner.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.List;
+
+public class Winner {
+    private List<Car> cars;
+    private int maxInstance;
+
+    public Winner(List<Car> cars, int maxInstance) {
+        this.cars = cars;
+        this. maxInstance = maxInstance;
+    }
+}
