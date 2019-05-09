be570d8532f2674a8d804cdcf4466fd7fad955ba
kangmin <kangmin789@naver.com>
2019-05-09 11:18:55 +0900

    [추가,수정] 자동차 이름 글자수 예외처리기능 addCar 메소드 추가, README 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,9 +3,9 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-    public String InputCarNames(){
+    public static String InputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
     }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
-
-    List<Car> cars = new ArrayList<Car>();
+    final static int MAX_CAR_LENGTH = 5;
+    //List<Car> cars = new ArrayList<Car>();
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-
-        String names = inputView.InputCarNames();
-        addCar(names);
+        addCar();
     }
 
-    private static void addCar(String names) {
-        splitCar(names);
+    private static void addCar() {
+        String string = InputView.InputCarNames();
+
+        String[] names = splitCar(string);
+        for (String name: names) {
+            if (name.length() > MAX_CAR_LENGTH ) {
+                System.out.println(name);
+                System.out.println("글자 수는 5글자 이내여야 합니다.\n 다시 입력해 주세요.");
+                addCar();
+                break;
+            }
+        }
     }
 
     private static String[] splitCar(String names) {
