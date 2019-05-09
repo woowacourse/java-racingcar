fccfa3c9047ef3b1063604317d2103dff8792f51
starkim06 <starkim06@naver.com>
2019-05-09 14:41:02 +0900

    [자동차 경주 게임]- RacingCarManager 클래스 수정 및 자동차 전진 기능

--- src/main/java/racingcar/controller/RacingCarManager.java
+++ src/main/java/racingcar/controller/RacingCarManager.java
@@ -10,51 +10,31 @@ import java.util.List;
 public class RacingCarManager {
     private List<Car> cars;
     private int gameCount;
+    private InputManager inputManager;
 
-    RacingCarManager() {
-        racingGameStart();
-    }
-
-    private void racingGameStart() {
-        init();
-    }
-
-    private void init() {
+    public RacingCarManager() {
         cars = new ArrayList<>();
         gameCount = 0;
+        inputManager = new InputManager();
     }
 
-    private void getCarName() {
-        String[] carNames = InputView.getRacingCarInput().split(",");
-        for (String carName : carNames) {
-            try {
-                checkNameValid(carName);
-            } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException();
-            }
+    public void racingGameStart() {
+        getGameInformation();
+        moveRacingCar();
+    }
+
+    private void getGameInformation() {
+        cars = inputManager.getRacingCar();
+        gameCount = inputManager.getGameCount();
+    }
+
+    private void moveRacingCar() {
+        for (Car car : cars) {
+            car.move(getRandomNumber());
         }
     }
 
-    private void checkNameValid(String carName) {
-        if (carName.length() < 1 || carName.length() > 5) {
-            throw new IllegalArgumentException("이름을 1 글자이상 5 글자이하로 해주세요 ");
-        }
+    private int getRandomNumber() {
+        return (int) Math.random() * 10;
     }
-
-    private void getGameCount() {
-        try {
-            gameCount = InputView.getRacingCount();
-            checkGameCountValid(gameCount);
-        } catch (InputMismatchException e) {
-            getGameCount();
-        }
-    }
-
-    private void checkGameCountValid(int gameCount) {
-        if (gameCount < 0) {
-            throw new InputMismatchException();
-        }
-    }
-
-
 }
