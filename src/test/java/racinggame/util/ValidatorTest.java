0dc7d4c786b49b23f20286f1e1d333c4c80eda11
huisam <huisam@naver.com>
2019-05-09 15:33:14 +0900

    [TEST] Validator 테스트 코드 구현

---
+++ src/test/java/racinggame/util/ValidatorTest.java
@@ -0,0 +1,39 @@
+package racinggame.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ValidatorTest {
+
+    @Test
+    void 이름을_유효성검사에서_예외를_제대로_던지는지_테스트() {
+        /* Given */
+        final List<String> duplicatedCarNames = Arrays.asList("crong", "crong", "gogo");
+        final List<String> lengthExceededCarNames = Arrays.asList("abcdef", "gogo");
+        /* Then */
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.checkCarNames(duplicatedCarNames);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.checkCarNames(lengthExceededCarNames);
+        });
+    }
+
+    @Test
+    void 시도횟수_유효성검사에서_예외를_제대로_던지는지_테스트() {
+        /* Given */
+        final int negativeNumber = -1;
+        final int zero = 0;
+        /* Then */
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.checkPositiveNumber(negativeNumber);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.checkPositiveNumber(zero);
+        });
+    }
+}
