ce3c6f356215b289b4aa27296b4feecc76b055d4
kmdngyu <kmdngyu@gmail.com>
2019-05-09 15:34:28 +0900

    add: 예외 처리 추가

--- src/main/java/racing/domain/Race.java
+++ src/main/java/racing/domain/Race.java
@@ -5,20 +5,34 @@ import racing.view.InputView;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Race {
     private List<Car> cars;
     private int numTrials;
 
     public void createCars() {
-        List<String> carNames = InputView.requestNames();
         cars = new ArrayList<>();
-        Iterator<String> it = carNames.iterator();
+        Iterator<String> it = inputCarNames().iterator();
         while (it.hasNext()) {
             cars.add(new Car(it.next()));
         }
     }
-
+    public List<String> inputCarNames(){
+        try{
+            List<String> carNames = InputView.requestNames();
+            checkDuplicated(carNames);
+            return carNames;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return inputCarNames();
+        }
+    }
+    public boolean checkDuplicated(List<String> names){
+        List<String> namesSet = names.stream().distinct().collect(Collectors.toList());
+        if(names.size()!=namesSet.size()) throw new IllegalArgumentException("중복된 이름이 존재하면 안됩니다.");
+        return true;
+    }
     public void inputNumTrials(){
         numTrials = InputView.requestNumTrials();
     }
