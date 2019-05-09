8ed5a8f70fe54bd38bd55cd8831f1bdc10718cb1
mrkwon <erased11@gmail.com>
2019-05-09 12:05:23 +0900

    [단위테스트] 우승자확인,입력 Split 확인

--- src/test/java/racing/controller/ControllerTest.java
+++ src/test/java/racing/controller/ControllerTest.java
@@ -2,15 +2,37 @@ package racing.controller;
 
 import org.junit.jupiter.api.Test;
 
+import racing.domain.Car;
+import racing.domain.CarList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
     @Test
-    void splitNames() {
+    void 입력의이름이잘나눠지나요1() {
+        String[] expectation = {"kwon", "kim", "Luffy", "Buddy"};
+        assertThat(Controller.splitNames("kwon,kim,Luffy,Buddy")).isEqualTo(expectation);
     }
 
     @Test
-    void getWinners() {
+    void 입력의이름이잘나눠지나요2() {
+        String[] expectation = {"kwon"};
+        assertThat(Controller.splitNames("kwon,kim,Luffy,Buddy")).isNotEqualTo(expectation);
+    }
+
+    @Test
+    void 입력의이름이잘나눠지나요3() {
+        String[] expectation = {"kwon", "kim", "Luffy", "Buddy"};
+        assertThat(Controller.splitNames("Buddy")).isNotEqualTo(expectation);
+    }
+
+    @Test
+    void 카리스트에서우승자를잘뽑아내나요1() {
+        String[] expectation = {"kwon", "kim", "Luffy", "Buddy"};
+        CarList carList = new CarList();
+        carList.addCars(expectation);
+        assertThat(Controller.getWinners(carList)).isEqualTo("kwon, kim, Luffy, Buddy");
     }
 }
