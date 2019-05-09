9005728626ecea902e6173a0ccb474c50db08e64
men7627 <men1219@hanmail.net>
2019-05-09 11:38:45 +0900

    Update property, constructor to Car

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Car {
+    private final String name;
+    private int position;
+
+    Car(String name) {
+        this.name = name;
+    }
+
+    Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
