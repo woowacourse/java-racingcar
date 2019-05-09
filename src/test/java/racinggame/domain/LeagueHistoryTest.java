e9c0850341e09fd33d043c1de3019aef74995d36
huisam <huisam@naver.com>
2019-05-09 11:39:37 +0900

    [TEST] LeagueHistory findWinners 테스트 코드 추가

--- src/test/java/racinggame/domain/LeagueHistoryTest.java
+++ src/test/java/racinggame/domain/LeagueHistoryTest.java
@@ -4,26 +4,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LeagueHistoryTest {
     private LeagueHistory leagueHistory;
+    private List<Car> cars;
 
     @BeforeEach
     void setUp() {
         leagueHistory = new LeagueHistory();
+        cars = Arrays.asList(new Car("pobi"),
+                new Car("crong"), new Car("gogo"));
     }
 
     @Test
     void 기록을_제대로_하는지_테스트() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("pobi"));
-        cars.add(new Car("crong"));
-        cars.add(new Car("gogo"));
         TrialHistory trialHistory = new TrialHistory(cars);
         leagueHistory.record(1, trialHistory);
         assertThat(leagueHistory.getHistoryOf(1)).isEqualTo(trialHistory);
+    }
+
+    @Test
+    void 우승자를_제대로_선출하는지_테스트() {
+        /* When */
+        leagueHistory.record(1, new TrialHistory(cars));
+        List<String> winners = leagueHistory.findWinners(1);
+        /* Then */
+        assertThat(winners.contains("pobi")).isTrue();
+        assertThat(winners.contains("crong")).isTrue();
+        assertThat(winners.contains("gogo")).isTrue();
     }
 }
