66b7b94652043014433f427f6f0a09cc29a00001
men7627 <men1219@hanmail.net>
2019-05-09 11:55:10 +0900

    Add property, run(), runLap() to RacingGame

--- src/main/java/model/RacingGame.java
+++ src/main/java/model/RacingGame.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    List<Car> cars;
+    List<String> winners;
+    int lapCount;
+
+    RacingGame(int lapCount) {
+        cars = new ArrayList<>();
+        winners = new ArrayList<>();
+        this.lapCount = lapCount;
+    }
+
+    void run() {
+        for (int i = 0; i < lapCount; i++) {
+            runLap();
+        }
+    }
+
+    private void runLap() {
+        Racing racing = new Racing();
+        for (Car car : cars) {
+            racing.move(car);
+        }
+    }
 }
