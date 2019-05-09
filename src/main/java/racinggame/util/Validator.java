87481a176f7f99d8fa526186a726e0c59d8dc993
huisam <huisam@naver.com>
2019-05-09 15:34:22 +0900

    [ADD] 시도횟수에 대한 예외처리 기능 추가

--- src/main/java/racinggame/util/Validator.java
+++ src/main/java/racinggame/util/Validator.java
@@ -6,13 +6,15 @@ import java.util.Set;
 
 public class Validator {
     public static final int NAME_EMPTY = 0;
-    public static final int NAME_LENTH_BOUND = 5;
+    public static final int NAME_LENGTH_BOUND = 5;
+    public static final int TRIAL_MIN = 1;
+
     public static void checkCarNames(List<String> carNames) {
         for (String carName : carNames) {
             if (carName.length() == NAME_EMPTY) {
                 throw new IllegalArgumentException("공백은 입력할 수 없습니다");
             }
-            if (carName.length() > NAME_LENTH_BOUND) {
+            if (carName.length() > NAME_LENGTH_BOUND) {
                 throw new IllegalArgumentException("이름 길이 제한은 5자 이하입니다");
             }
         }
@@ -25,5 +27,11 @@ public class Validator {
         Set<String> nameSet = new HashSet<>(carNames);
 
         return nameSet.size() != carNames.size();
+    }
+
+    public static void checkPositiveNumber(int trial) {
+        if (trial < TRIAL_MIN) {
+            throw new IllegalArgumentException("시도횟수는 양의 정수여야 합니다.");
+        }
     }
 }
