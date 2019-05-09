f48516024e08a2f8ece9b3b7fbad15ce4b02ee34
deocksoo <dusol92@snu.ac.kr>
2019-05-09 14:06:51 +0900

    [ADD] 최종 우승자를 출력해주는 기능을 구현합니다

--- src/main/java/racinggame/util/OutputView.java
+++ src/main/java/racinggame/util/OutputView.java
@@ -5,8 +5,10 @@ import java.util.List;
 public class OutputView {
 
     public static void showWinners(List<String> winners) {
+        String winnerMessage = String.join(", ", winners.toArray(new String[winners.size()]));
 
-}
+        System.out.println(winnerMessage + "가 최종 우승했습니다.");
+    }
 
     public static void showRaceResult(String carName, int carPosition) {
         StringBuilder carNameAndPositionView = new StringBuilder(carName);
