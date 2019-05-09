169c9d7cd8ab7e2e0ee0de8e8be4810de6bf8fa8
starkim06 <starkim06@naver.com>
2019-05-09 14:08:01 +0900

    [자동차 경주 게임]- 시도할 횟수 입력

--- src/main/java/racingcar/controller/RacingCarManager.java
+++ src/main/java/racingcar/controller/RacingCarManager.java
@@ -4,9 +4,25 @@ import racingcar.model.Car;
 import racingcar.view.InputView;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class RacingCarManager {
+    private List<Car> cars;
+    private int gameCount;
+
+    RacingCarManager() {
+        racingGameStart();
+    }
+
+    private void racingGameStart() {
+        init();
+    }
+
+    private void init() {
+        cars = new ArrayList<>();
+        gameCount = 0;
+    }
 
     private void getCarName() {
         String[] carNames = InputView.getRacingCarInput().split(",");
@@ -24,4 +40,21 @@ public class RacingCarManager {
             throw new IllegalArgumentException("이름을 1 글자이상 5 글자이하로 해주세요 ");
         }
     }
+
+    private void getGameCount() {
+        try {
+            gameCount = InputView.getRacingCount();
+            checkGameCountValid(gameCount);
+        } catch (InputMismatchException e) {
+            getGameCount();
+        }
+    }
+
+    private void checkGameCountValid(int gameCount) {
+        if (gameCount < 0) {
+            throw new InputMismatchException();
+        }
+    }
+
+
 }
