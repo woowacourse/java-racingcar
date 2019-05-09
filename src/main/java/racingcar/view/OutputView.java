34ff1cb7796f581cbbb58a29c1a2e4d85e1db13b
Seongmo Jeon <oeeen3@gmail.com>
2019-05-09 16:13:26 +0900

    feat: 컨트롤러로 부터 받은 자동차 정보 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+
 public class OutputView {
+    public static void outputCarPosition(ArrayList<Car> carList) {
+        for (Car car : carList) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
 }
