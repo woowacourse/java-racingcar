9e0e3343a18c72950dd9d7cccf257d36edcf0a54
kmdngyu <kmdngyu@gmail.com>
2019-05-09 16:07:28 +0900

    add: 횟수 입력 / 객체내 예외 처리 / 자연수 여부

--- src/main/java/racing/domain/Race.java
+++ src/main/java/racing/domain/Race.java
@@ -18,6 +18,7 @@ public class Race {
             cars.add(new Car(it.next()));
         }
     }
+
     public List<String> inputCarNames(){
         try{
             List<String> carNames = InputView.requestNames();
@@ -28,13 +29,28 @@ public class Race {
             return inputCarNames();
         }
     }
+
     public boolean checkDuplicated(List<String> names){
         List<String> namesSet = names.stream().distinct().collect(Collectors.toList());
         if(names.size()!=namesSet.size()) throw new IllegalArgumentException("중복된 이름이 존재하면 안됩니다.");
         return true;
     }
+
     public void inputNumTrials(){
-        numTrials = InputView.requestNumTrials();
+        try {
+            numTrials = InputView.requestNumTrials();
+            checkValidNumTrials(numTrials);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            inputNumTrials();
+        }
+    }
+
+    public boolean checkValidNumTrials(int numTrials) {
+        if (0 >= numTrials) {
+            throw new IllegalArgumentException("시도 횟수는 자연수만 입력 가능합니다.");
+        }
+        return true;
     }
 
 
