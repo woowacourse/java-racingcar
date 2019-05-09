4f49cfba8f30607d9b4e628abc809d99903f0bd0
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-05-09 10:17:04 +0900

    테스트코드 수정.

--- src/main/java/racingcar/CarGameLauncher.java
+++ src/main/java/racingcar/CarGameLauncher.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.*;
+
 public class CarGameLauncher {
     public static void main(String[] args) {
+        List<String> nameList = new ArrayList<>(Arrays.asList("asddfdfd", "asd"));
+        boolean test = CarException.isOverLimit(nameList);
+        System.out.println(test);
 
     }
 }
--- src/test/java/racingcar/CarExceptionTest.java
+++ src/test/java/racingcar/CarExceptionTest.java
@@ -4,30 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarExceptionTest {
     @Test
-    void checkLimit(){
+    void checkLimitTest(){
         String name = "asdas";
         assertThat(CarException.checkLimit(name)).isEqualTo(false);
     }
 
     @Test
-    void isOverLimit(){
-        List<String> nameList = Arrays.asList("asd", "asd");
+    void isOverLimitTest(){
+        List<String> nameList = new ArrayList<>(Arrays.asList("asddfdfd", "asd"));
         assertThat(CarException.isOverLimit(nameList)).isEqualTo(true);
     }
 
     @Test
-    void isWhiteSpaceOnly(){
+    void isWhiteSpaceOnlyTest(){
         List<String> nameList = Arrays.asList("asd", "", "asd");
         assertThat(CarException.isWhiteSpaceOnly(nameList)).isEqualTo(true);
     }
 
     @Test
-    void isDuplicate() {
+    void isDuplicateTest() {
         List<String> nameList = Arrays.asList("asd", "ddd", "asd");
         assertThat(CarException.isDuplicate(nameList)).isEqualTo(true);
     }
