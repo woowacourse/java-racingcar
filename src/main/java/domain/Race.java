39084b302ebdf20f41ebe2b9ad13a38b480b0d0c
heebong <heebg2@naver.com>
2019-05-09 17:28:44 +0900

    Race 객체에서 우승자 가지고오는 로직 변경 - maxposition 구하는 위치 변경(레이스를 한번 할 때 설정)

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -7,39 +7,31 @@ import java.util.List;
 
 public class Race {
     private List<Car> raceCars;
+    private int maxPosition = Const.ZERO;
 
     public Race(List<Car> raceCars) {
         this.raceCars = raceCars;
     }
 
-    public void MoveAllCarOneTime(){
-        for(Car car : raceCars){
+    public void moveAllCarOneTime() {
+        for (Car car : raceCars) {
             car.increasePositionOrNot(Util.getRandomNumber());
+            if (car.checkGreaterThanMaxPos(maxPosition)) {
+                ++this.maxPosition;
+            }
         }
     }
 
-    public List<Car> getRaceCars(){
+    public List<Car> getRaceCars() {
         return raceCars;
     }
 
     public String getRaceWinners() {
-        int maxPosition = findMaxPosition();
         List<String> winnersList = new ArrayList<>();
-        for(Car car : raceCars){
-            if(car.getPosition() == maxPosition){
-                winnersList.add(car.getName());
-            }
-        }
-        return String.join(",",winnersList);
-    }
 
-    private int findMaxPosition() {
-        int max = 0;
-        for(Car car : raceCars){
-            if(max < car.getPosition()){
-                max = car.getPosition();
-            }
-        }
-        return max;
+        raceCars.stream().filter(x -> x.isMaxPosition(maxPosition))
+                .forEach(x -> winnersList.add(x.getName()));
+
+        return String.join(",", winnersList);
     }
 }
