7b9f5a00776246fc46394e3da7a9f79a39efe7c2
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-09 11:31:18 +0900

    <자동차 경주 게임> 0~9까지 랜덤값을 구한다.

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Racing {
+    private static final Random random = new Random();
+
+    private void moveOrNot() {
+        random.nextInt(10);
+    }
+}
