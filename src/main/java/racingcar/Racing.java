7a53d226d405e4c63223de4255959acc24559f60
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-09 12:10:44 +0900

    <자동차 경주 게임> 사용자가 입력한 수만큼 자동차 객체 생성.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,10 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Racing {
     private static final Random random = new Random();
+    private List<Car> cars = new ArrayList<>();
+
+    public void run() {
+        InputView.inputCarNames().forEach(x -> cars.add(new Car(x)));
+        for (int i = 0; i < InputView.inputNumberOfTimes(); ++i) {
+
+        }
+    }
 
     private void move(Car car) {
         car.moveOrNot(random.nextInt(10));
