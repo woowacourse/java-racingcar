e8fac4bbfaea3e13c8bf61397b74e57a3e183895
jinwook <jin942002@naver.com>
2019-05-09 10:28:06 +0900

    CaculatorTest 속 함수들 중 일부를 InputHandlerTest 파일에 삽입.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -49,17 +49,6 @@ public class CalculatorTest {
 
 
 
-    @Test
-    void 숫자연산자혼합인지확인() {
-        String[] array = "3 + 2 * 4".split(" ");
-        assertThat(InputHandler.canCalculate(array)).isEqualTo(true);
-    }
-
-    @Test
-    void 연산자로끝나는식() {
-        String[] array = "3 + 2 *".split(" ");
-        assertThat(InputHandler.canCalculate(array)).isEqualTo(false);
-    }
 
 
 }
---
+++ src/test/java/calculator/InputHandlerTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class InputHandlerTest {
+
+    @Test
+    void 숫자연산자혼합인지확인() {
+        String[] array = "3 + 2 * 4".split(" ");
+        assertThat(InputHandler.canCalculate(array)).isEqualTo(true);
+    }
+
+    @Test
+    void 연산자로끝나는식() {
+        String[] array = "3 + 2 *".split(" ");
+        assertThat(InputHandler.canCalculate(array)).isEqualTo(false);
+    }
+}
