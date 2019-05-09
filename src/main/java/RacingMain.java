28f6d6a6e05f29cfcd3c46057b9abb11ade3c895
김성훈 <seonghun127@gmail.com>
2019-05-09 16:09:55 +0900

    main 메소드 안 인스턴스 필드 객체의 접근제어자를 변경 : private

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -2,7 +2,7 @@ import controller.RacingController;
 import domain.Race;
 
 public class RacingMain {
-    static RacingController racingController = new RacingController();
+    private static RacingController racingController = new RacingController();
 
     public static void main(String[] args) {
         Race race = racingController.moveAllCar(racingController.setCarName(), racingController.setRoundCount());
