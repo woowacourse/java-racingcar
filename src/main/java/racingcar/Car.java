d3ccf62f4fef6c8ef995446b6bbb66a071d9c27a
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-09 14:46:07 +0900

    <자동차 경주 게임> 가장 멀리 이동한 자동차 계산

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
     private static final int CAR_MOVE_CONDITION = 4;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final Random random = new Random();
 
     private final String name;
     private int position = 0;
@@ -10,9 +14,21 @@ public class Car {
         this.name = name;
     }
 
-    public void moveOrNot(int randomNumber) {
-        if (randomNumber >= CAR_MOVE_CONDITION) {
+    public void moveOrNot() {
+        if (random.nextInt(MAX_RANDOM_NUMBER + 1) >= CAR_MOVE_CONDITION) {
             ++position;
         }
+    }
+
+    public boolean isMaxPosition(int maxDistance) {
+        return (position == maxDistance);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,24 +2,19 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Racing {
-    private static final Random random = new Random();
     private List<Car> cars = new ArrayList<>();
 
     public void run() {
         InputView.inputCarNames().forEach(x -> cars.add(new Car(x)));
         int numberOfTimes = InputView.inputNumberOfTimes();
+
         System.out.println("실행 결과");
         for (int i = 0; i < numberOfTimes; ++i) {
-            cars.forEach(car -> move(car));
+            cars.forEach(car -> car.moveOrNot());
             cars.forEach(car -> OutputView.printCarDistance(car));
             System.out.println();
         }
-    }
-
-    private void move(Car car) {
-        car.moveOrNot(random.nextInt(10));
     }
 }
