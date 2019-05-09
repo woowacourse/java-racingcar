9ea416ecef00e13ba8c7f0feb046658234403690
men7627 <men1219@hanmail.net>
2019-05-09 11:46:16 +0900

    Add property, move() to Racing

--- src/main/java/model/Racing.java
+++ src/main/java/model/Racing.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.Random;
+
 public class Racing {
+    static final int SEED = 11;
+    static final int BOUND = 4;
+
+    void move(Car car) {
+        Random random = new Random(SEED);        
+        int position = car.getPosition();
+
+        if (random.nextInt() >= BOUND) {
+            car.setPosition(++position);
+        }
+    }
 }
