a7245875538848b00d63bd87319fce156ca4b51f
conatuseus <conatuseus@gmail.com>
2019-05-09 18:07:27 +0900

    [FEATURE] maxPosition에 대한 Test Code 작성
    Squashed commit of the following:
    
    commit 911e3331f92506e51632bf54d772f8d997cff63c
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Thu May 9 18:06:30 2019 +0900
    
        Add Test Code

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,14 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    Car car;
+
+    @BeforeEach
+    void setup(){
+        car=new Car("tmp");
+    }
+
     @Test
     void 자동차_이동_검사(){
-        Car car=new Car("tmp");
         assertThat(car.getPosition()).isEqualTo(0);     // 초기에 0인지 확인
         car.move(true);
         assertThat(car.getPosition()).isEqualTo(1);     // 1회 이동했을 때 Poistion 변경 확인
         car.move(false);
         assertThat(car.getPosition()).isEqualTo(1);     // 이동을 하지 않았을 때 Position이 변경되지 않음을 확인
+    }
+
+    @Test
+    void maxPosition_비교_검사(){
+        car.move(true);
+        car.move(true);
+        car.move(true);
+        assertThat(car.getMaxPosition(2)).isEqualTo(3);
+        assertThat(car.getMaxPosition(3)).isEqualTo(3);
+        assertThat(car.getMaxPosition(4)).isEqualTo(4);
     }
 
 }
