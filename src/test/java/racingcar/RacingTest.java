7d42f23f4dc02b681528e99eb8accd70ca59614d
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-09 11:55:30 +0900

    <자동차 경주 게임> [단위 테스트] 랜덤 숫자가 4이상일 때 정상적으로 움직이는 지 테스트

---
+++ src/test/java/racingcar/RacingTest.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingTest {
+
+    @Test
+    void 랜덤숫자가_4이상일때만_움직인다() {
+        Car car = new Car("Test");
+        int beforePosition = car.getPosition();
+
+        car.moveOrNot(3);
+        assertTrue(beforePosition == car.getPosition());
+
+        car.moveOrNot(4);
+        assertTrue((beforePosition + 1) == car.getPosition());
+    }
+}
