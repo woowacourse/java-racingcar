b3cd1f0a6e34d7a4d1e5ec96692ef5adc6517b0a
deocksoo <dusol92@snu.ac.kr>
2019-05-09 10:32:33 +0900

    [TEST] CarTest 클래스의 makeCarHistory 테스트 코드를 구현

---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,31 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("testCar");
+    }
+
+    @Test
+    void 차가_자기_히스토리를_제대로_만드는지_테스트() {
+       /* Given : */
+        HashMap<String, Integer> carHistory;
+
+        /* When: */
+        carHistory = car.makeCarHisory();
+
+        /* Then: */
+        assertThat(carHistory.containsKey("testCar")).isTrue();
+        assertThat(carHistory.get("testCar")).isEqualTo(0);
+    }
+}
