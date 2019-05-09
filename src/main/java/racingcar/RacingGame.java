95b0d648ff3c58f96898df9c0ebdb61fc3d5ce46
kangmin <kangmin789@naver.com>
2019-05-09 11:33:25 +0900

    [수정] addCar 메소드에 글자 수가 1미만일 때의 예외처리 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
+    final static int MIN_CAR_LENGTH = 1;
     final static int MAX_CAR_LENGTH = 5;
     //List<Car> cars = new ArrayList<Car>();
 
@@ -13,11 +14,15 @@ public class RacingGame {
 
     private static void addCar() {
         String string = InputView.InputCarNames();
-
         String[] names = splitCar(string);
+
+        if(names.length < MIN_CAR_LENGTH){
+            System.out.println("글자 수는 1글자 이상이여야 합니다..\n 다시 입력해 주세요.");
+            addCar();
+        }
+
         for (String name: names) {
-            if (name.length() > MAX_CAR_LENGTH ) {
-                System.out.println(name);
+            if (name.length() > MAX_CAR_LENGTH  ) {
                 System.out.println("글자 수는 5글자 이내여야 합니다.\n 다시 입력해 주세요.");
                 addCar();
                 break;
@@ -26,6 +31,7 @@ public class RacingGame {
     }
 
     private static String[] splitCar(String names) {
+        names.replaceAll(" ", "");
         return names.split(",");
     }
 }
