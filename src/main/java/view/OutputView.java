75f30c26045eab8dd2a84c03370764b29a605f68
김성훈 <seonghun127@gmail.com>
2019-05-09 15:53:00 +0900

    commit

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,18 +6,13 @@ import domain.Const;
 import java.util.List;
 
 public class OutputView {
-<<<<<<< HEAD:src/main/java/view/OutputView.java
     public static void outputGameResultTile(){
         System.out.println(Const.OUTPUT_GAME_RESULT);
     }
 
     public static void outputGameResult(List<Car> carList){
-        for(Car car : carList){
-=======
-    public static void outputGameResult(List<Car> cars) {
         System.out.println(Const.OUTPUT_GAME_RESULT);
-        for (Car car : cars) {
->>>>>>> seonghun127:src/main/java/domain/OutputView.java
+        for (Car car : carList) {
             System.out.println(car.getName() + " : " + outputCarPosition(car.getPosition()));
         }
         System.out.println();
@@ -31,11 +26,7 @@ public class OutputView {
         return result;
     }
 
-<<<<<<< HEAD:src/main/java/view/OutputView.java
     public static void outputWinners(String winners){
-=======
-    public static void outputWinner(String winners) {
->>>>>>> seonghun127:src/main/java/domain/OutputView.java
         System.out.println(winners + Const.OUTPUT_WINNER);
     }
 }
