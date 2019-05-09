374098448043eb30b7f82b4707282250b3d797ad
heebong <heebg2@naver.com>
2019-05-09 13:32:02 +0900

    Util에 중복된 이름 체크 함수 추가

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -2,10 +2,22 @@ package util;
 
 import domain.Const;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Util {
     public static int getRandomNumber(){
         return new Random().nextInt(Const.RANGE_RAN_NUM);
+    }
+
+    public static void checkDuplicatedStringList(List<String> strList){
+        List<String> tempList = new ArrayList<>();
+        for(String str : strList){
+            if(tempList.contains(str)){
+                throw new IllegalArgumentException("중복된 이름이 있습니다.");
+            }
+            tempList.add(str);
+        }
     }
 }
