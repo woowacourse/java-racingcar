f40a805b98174f9f1fc65062f201180287179175
Jo Jinsik <kunggom@gmail.com>
2019-05-09 15:44:54 +0900

    CarTest 작성.

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    Car testCar;
+    static String name = "asdf";
+
+    @BeforeEach
+    void setup() {
+        testCar = new Car(name);
+    }
+
+    @Test
+    void 이름() {
+        assertThat(testCar.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void 위치() {
+        assertThat(testCar.getPosition()).isEqualTo(0);
+
+        for (int i = 0; i < 10; i++) {
+            testCar.moveForward();
+            assertThat(testCar.getPosition()).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    void 랜덤포워드() {
+        assertThat(testCar.getPosition()).isEqualTo(0);
+        testCar.randomForward(false);
+        assertThat(testCar.getPosition()).isEqualTo(0);
+        testCar.randomForward(true);
+        assertThat(testCar.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 투스트링() {
+        assertThat(testCar.toString()).isEqualTo("asdf : ");
+        for (int i = 0; i < 3; i++) {
+            testCar.moveForward();
+        }
+        assertThat(testCar.toString()).isEqualTo("asdf : ---");
+    }
+}
