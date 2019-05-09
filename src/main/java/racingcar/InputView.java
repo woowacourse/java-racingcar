24d74a5275d8460cb169efb68877dd7e9f9ff1ca
kangmin <kangmin789@naver.com>
2019-05-09 10:44:42 +0900

    [추가] 자동차의 이름을 입력받는 InputCarNames 메소드 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     Scanner scanner = new Scanner(System.in);
+    
     public String InputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
