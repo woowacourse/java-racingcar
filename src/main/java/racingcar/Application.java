7d4d4f931ead59ab8e26eeea55cfc7ccdc6e7281
Seongmo Jeon <oeeen3@gmail.com>
2019-05-09 16:51:27 +0900

    feat: Racingcar application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+
 public class Application {
     public static void main(String[] args) {
+        RacingGameController racingGameController = new RacingGameController();
 
+        try {
+            String carNames = racingGameController.requestCarList();
+            String[] validCarNames = racingGameController.makeValidCarNames(carNames);
+            racingGameController.generateCarList(validCarNames);
+
+            int tryCount = racingGameController.requestTryCount();
+            racingGameController.makeValidTryCount(tryCount);
+
+            racingGameController.startRacingGame();
+            racingGameController.findWinners();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
