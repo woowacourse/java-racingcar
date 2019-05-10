0bc019e4547f8d9b9499f22854f67152cc0c7724
deocksoo <dusol92@snu.ac.kr>
2019-05-10 18:29:08 +0900

    [REFACTOR] Validator의 중복된 duplication check 메서드를 제거합니다

--- src/main/java/racinggame/util/Validator.java
+++ src/main/java/racinggame/util/Validator.java
@@ -8,15 +8,11 @@ public class Validator {
     public static final int TRIAL_MIN = 1;
 
     public static void checkDuplicateNames(List<String> carNames) {
-       if (checkNameDuplication(carNames)) {
-            throw new IllegalArgumentException("중복되는 이름은 입력할 수 없습니다.");
-        }
-    }
-
-    public static boolean checkNameDuplication(List<String> carNames) {
         Set<String> nameSet = new HashSet<>(carNames);
 
-        return nameSet.size() != carNames.size();
+        if (nameSet.size() != carNames.size()) {
+            throw new IllegalArgumentException("중복되는 이름은 입력할 수 없습니다.");
+        }
     }
 
     public static void checkPositiveNumber(int trial) {
