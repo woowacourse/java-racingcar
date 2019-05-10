6ee1407a7e5a3f20fee600f780ec87abe775dfa9
yejilee <yjll1019@naver.com>
2019-05-10 16:14:53 +0900

    <자동차 경주 게임> 공백라인 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,10 +13,12 @@ public class InputView {
         System.out.println("경주할 자동차를 입력하세요. (이름은 쉼표(,) 기준으로 구분");
         String carNames;
         boolean result;
+
         do {
             carNames = scanner.nextLine();
             result = InputValidator.validateAll(carNames);
         } while (!result);
+
         return Arrays.asList(carNames.split(","));
     }
 
@@ -24,10 +26,12 @@ public class InputView {
         System.out.println("시도할 회수는 몇회인가요?");
         String numberOfTimes;
         boolean result;
+
         do {
             numberOfTimes = scanner.nextLine();
             result = InputValidator.validateNaturalNumber(numberOfTimes);
         } while (!result);
+
         return Integer.parseInt(numberOfTimes);
     }
 }
