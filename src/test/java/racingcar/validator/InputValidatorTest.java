87089a452797601a4dcb5a76ab297b98c2cabd3b
yejilee <yjll1019@naver.com>
2019-05-10 16:07:38 +0900

    <자동차 경주 게임>[단위 테스트] 테스트 메소드 변경

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -9,24 +9,24 @@ public class InputValidatorTest {
 
     @Test
     public void 자동차_이름이_6글자_이상인_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abcde,abcdef"));
+        assertFalse(InputValidator.validateAll("abc,abcd,abcde,abcdef"));
     }
 
     @Test
     public void 자동차_이름에_중복된_쉼표가_있는_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abcde,,abcde"));
+        assertFalse(InputValidator.validateAll("abc,abcd,abcde,,abcde"));
     }
 
     @Test
     public void 자동차_이름에_알파벳이_아닌_문자가_포함된_경우() {
-        assertFalse(InputValidator.validateForm(" "));
-        assertFalse(InputValidator.validateForm(""));
-        assertFalse(InputValidator.validateForm("abc#,abcd,abcde,abcd1"));
+        assertFalse(InputValidator.validateAll(" "));
+        assertFalse(InputValidator.validateAll(""));
+        assertFalse(InputValidator.validateAll("abc#,abcd,abcde,abcd1"));
     }
 
     @Test
     public void 자동차_이름에_중복된_이름이_있는_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abc,abcde"));
+        assertFalse(InputValidator.validateAll("abc,abcd,abc,abcde"));
     }
 
     @Test
