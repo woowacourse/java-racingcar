fca2b2800107c234a7e2f803bcd54c6b70f4a7a0
김성훈 <seonghun127@gmail.com>
2019-05-10 14:45:00 +0900

    equals(), hashCode() 메소드 추가, position값 비교를 위해 메소드명 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,15 @@
 package domain;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
 public class Car {
     private final String name;
     private int position = 0;
 
-    public Car(String name) {
-        if (Const.MIN_NAME_LEN > name.length() || Const.MAX_NAME_LEN < name.length()) {
+    public Car(final String name) {
+        if (StringUtils.isBlank(name) || name.length() > Const.MAX_NAME_LEN) {
             throw new IllegalArgumentException(Const.EX_NAME_LEN);
         }
         this.name = name;
@@ -19,17 +23,31 @@ public class Car {
         return this.position;
     }
 
-    public void increasePositionOrNot(int random) {
-        if (Const.GO_RAN_NUM <= random) {
+    public void increasePositionOrNot(int randomNumber) {
+        if (Const.GO_RAN_NUM <= randomNumber) {
             this.position++;
         }
     }
 
-    public boolean checkGreaterThanMaxPos(int maxPosition) {
+    public boolean isGreaterThanMaxPosition(int maxPosition) {
         return this.position > maxPosition;
     }
 
-    public boolean isMaxPosition(int maxPosition) {
-        return this.position == maxPosition;
+    public boolean matchPosition(int position) {
+        return this.position == position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position &&
+                Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
