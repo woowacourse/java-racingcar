c26994e8c13a3d0a4cca09860c92f9b5d7fb555f
Kwanwoo Kang <kkw200@naver.com>
2019-05-10 21:17:53 +0900

    <REFACTOR> Studied Java enum and refactored.

--- src/main/java/calc/Operator.java
+++ src/main/java/calc/Operator.java
@@ -1,25 +1,31 @@
 package calc;
 
+import java.util.NoSuchElementException;
 import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 public enum Operator {
-    ADD((lhs, rhs) -> lhs + rhs),
-    SUBTRACT((lhs, rhs) -> lhs - rhs),
-    MULTIPLY((lhs, rhs) -> lhs * rhs),
-    DIVIDE((lhs, rhs) -> lhs / rhs);
+    ADD("+", (lhs, rhs) -> lhs + rhs),
+    SUBTRACT("-", (lhs, rhs) -> lhs - rhs),
+    MULTIPLY("*", (lhs, rhs) -> lhs * rhs),
+    DIVIDE("/", (lhs, rhs) -> lhs / rhs);
 
+    private String symbol;
     private BiFunction<Double, Double, Double> operation;
 
     static public Operator getOperation(String symbol) {
-        final Operator conversionTable[] = { ADD, SUBTRACT, MULTIPLY, DIVIDE };
         try {
-            return conversionTable["+-*/".indexOf(symbol)];
-        } catch (IndexOutOfBoundsException e) {
+            return Stream.of(Operator.values())
+                    .filter(x -> x.symbol == symbol)
+                    .findFirst()
+                    .get();
+        } catch (NoSuchElementException e) {
             throw new IllegalArgumentException();
         }
     }
 
-    Operator(BiFunction<Double, Double, Double> operation) {
+    Operator(String symbol, BiFunction<Double, Double, Double> operation) {
+        this.symbol = symbol;
         this.operation = operation;
     }
 
