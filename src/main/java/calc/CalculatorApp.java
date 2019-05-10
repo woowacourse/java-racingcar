65038c2046bf68e3da5cce9dafe68f7ec0c24096
Kwanwoo Kang <kkw200@naver.com>
2019-05-10 22:46:47 +0900

    <REFACTOR> imma sleep

--- src/main/java/calc/CalculatorApp.java
+++ src/main/java/calc/CalculatorApp.java
@@ -6,7 +6,8 @@ public class CalculatorApp {
         Scanner input = new Scanner(System.in);
         System.out.println("수식을 입력하여 주십시오.");
         try {
-            System.out.println("감사합니다.\n결과값은 " + new Calculator(input.nextLine()) + "입니다.");
+            System.out.println("감사합니다.\n결과값은 "
+                + new Calculator(input.nextLine()) + "입니다.");
         } catch (IllegalArgumentException e) {
             System.out.println("잘못된 입력입니다.");
         }
--- src/main/java/racingcar/view/Hangul.java
+++ src/main/java/racingcar/view/Hangul.java
@@ -1,18 +1,19 @@
 package racingcar.view;
 
 public class Hangul {
+    static final int UNICODE_START_OF_HANGUL = 0xAC00;
+    static final int CYCLE = 28;
+    static final char[] CONVERSION_TABLE = ("영일이삼사오육칠팔구:;<=>?@"
+            + "이비씨디이프쥐치이이이엘엠엔오피큐알쓰티유이유스이지"
+            + "[원]^_`이비씨디이프쥐치이이이엘엠엔오피큐알쓰티유이유스이지"
+    ).toCharArray();
+
     /*
     앞 글자에 따라 뒤에 붙을 조사 이, 가 구분
     유니코드는 가, 각, 갂, 갃, 간, ..., 순으로 받침 없는 글자 + 받침 있는 글자 27가지가 있다
     영어나 기호는 마지막 발음을 단순 치환
     */
     public static String differentiateIGa(String sentence) {
-        final int UNICODE_START_OF_HANGUL = 0xAC00;
-        final int CYCLE = 28;
-        final char[] CONVERSION_TABLE = ("영일이삼사오육칠팔구:;<=>?@"
-                + "이비씨디이프쥐치이이이엘엠엔오피큐알쓰티유이유스이지"
-                + "[원]^_`이비씨디이프쥐치이이이엘엠엔오피큐알쓰티유이유스이지"
-        ).toCharArray();
         char letter = sentence.charAt(sentence.length() - 1);
         if ("0".charAt(0) <= letter && letter <= "Z".charAt(0)) {
             letter = CONVERSION_TABLE[letter - 48];
