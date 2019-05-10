40451598f609fdfea4ec4f791ef69493b3e02330
Seongmo Jeon <oeeen3@gmail.com>
2019-05-10 16:35:44 +0900

    test: RacingGame 객체 테스트

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,4 +1,50 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RacingGameTest {
+    RacingGame racingGame;
+
+    @BeforeEach
+    void setUp() {
+        racingGame = new RacingGame();
+    }
+
+    @Test
+    void 앞에_연속된_콤마가_입력된_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racingGame.generateCarList(",,,pobi");
+        });
+    }
+
+    @Test
+    void 중간에_연속된_콤마가_입력된_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racingGame.generateCarList("po,,,bi");
+        });
+    }
+
+    @Test
+    void 뒤에_연속된_콤마가_입력된_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racingGame.generateCarList("pobi,,,");
+        });
+    }
+
+    @Test
+    void 공백이_입력된_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racingGame.generateCarList("pobi,   ,brown");
+        });
+    }
+
+    @Test
+    void 시도_횟수가_최소값_미만인_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racingGame.configureTryCount(-1);
+        });
+    }
 }
