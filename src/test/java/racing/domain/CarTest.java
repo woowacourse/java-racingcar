d5917d7716d70849d0c7c319e0fab2908e042c37
mrkwon <erased11@gmail.com>
2019-05-11 02:30:29 +0900

    [수정] 단위테스트 수정

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -2,19 +2,32 @@ package racing.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
     @Test
-    void getName() {
+    void 이름예외처리아무것도입력되지않았을때() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("");
+        });
     }
 
     @Test
-    void getPosition() {
+    void 이름예외처리5글자초과시() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("abcdef");
+        });
     }
 
     @Test
-    void getStringPosition() {
+    void 포지션게터메서드() {
+        assertThat(new Car("aaa").getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 포지션을스트링으로겟하면맞나요() {
+        assertThat(new Car("aaa", 4).getStringPosition()).isEqualTo("----");
     }
 }
