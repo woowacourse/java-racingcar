34d96f458ea55c54d4a762560c3ac55103d69534
men7627 <men1219@hanmail.net>
2019-05-10 16:54:45 +0900

    Refactor(InputView): add Cars, LapCount

---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static final String REGEX_CAR_NAMES_FORMAT = "^([^,]+)(,[^,]+)*$";
+    private static final String REGEX_CAR_NAMES_SPLIT = ",";
+    List<Car> cars;
+
+    public Cars(String carNames) {
+        checkCarNamesFormat(carNames);
+        this.cars = parseCars(carNames);
+    }
+
+    private static void checkCarNamesFormat(String carNames) {
+        if (!carNames.matches(REGEX_CAR_NAMES_FORMAT)) {
+            throw new IllegalArgumentException("자동차 이름 형식에 맞지 않습니다.");
+        }
+    }
+
+    private List<Car> parseCars(String carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames.split(REGEX_CAR_NAMES_SPLIT)) {
+            Car car = new Car(new CarName(carName));
+            checkCarNamesDuplication(car);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    private void checkCarNamesDuplication(Car car) {
+        if (cars.contains(car)) {
+            throw new IllegalArgumentException("중복되는 자동차 이름이 존재합니다.");
+        }
+    }
+}
---
+++ src/main/java/model/LapCount.java
@@ -0,0 +1,22 @@
+package model;
+
+public class LapCount {
+    static final String REGEX_IS_NATURAL_NUMBER = "^([1-9])([0-9])*$";
+    private int lapCount;
+
+    LapCount(String lapCount) {
+        this.lapCount = parseNaturalNumber(lapCount);
+    }
+
+    private int parseNaturalNumber(String lapCount) {
+        if (lapCount.matches(REGEX_IS_NATURAL_NUMBER)) {
+            throw new IllegalArgumentException("시도 횟수는 자연수입니다.");
+        }
+
+        try {
+            return Integer.parseInt(lapCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수가 정수 범위롤 초과힙니다.");
+        }
+    }
+}
