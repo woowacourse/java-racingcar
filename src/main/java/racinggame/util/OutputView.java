9c537515ad8305c3eb8a441d1f64645cb425a004
deocksoo <dusol92@snu.ac.kr>
2019-05-10 18:25:02 +0900

    [REFACTOR] OutputView의 메서드 순서를 변경합니다

--- src/main/java/racinggame/util/OutputView.java
+++ src/main/java/racinggame/util/OutputView.java
@@ -6,6 +6,9 @@ import racinggame.domain.TrialHistory;
 import java.util.List;
 
 public class OutputView {
+    public static void showErrorMessage(String message) {
+        System.err.println(message);
+    }
 
     public static void showHistory(LeagueHistory leagueHistory) {
         System.out.println("실행 결과");
@@ -18,9 +21,5 @@ public class OutputView {
         String winnerMessage = String.join(", ", winners.toArray(new String[winners.size()]));
 
         System.out.println(winnerMessage + "가 최종 우승했습니다.");
-    }
-
-    public static void showErrorMessage(String message) {
-        System.err.println(message);
     }
 }
