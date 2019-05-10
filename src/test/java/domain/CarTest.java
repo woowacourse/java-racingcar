fc4c49701b45b32bb9b03452a8b30f615c73e9d9
김성훈 <seonghun127@gmail.com>
2019-05-10 14:46:45 +0900

    객체 생성 중복, name에 대한 예외, 숫자값에 따른 position 변화 테스트 코드 작성

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    Car car;
+    private Car car;
 
     @BeforeEach
     void setUp() {
@@ -17,34 +17,33 @@ class CarTest {
 
     @Test
     void 자동차_객체_생성() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Car car = new Car("123456");
-        });
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            Car car = new Car("");
-        });
-
-        assertThat(car.getName()).isEqualTo("hello");
+        assertThat(car).isEqualTo(new Car("hello"));
     }
 
     @Test
-    void 자동차_포지션_증가일_경우() {
-        int init = car.getPosition();
-        for (int i = Const.GO_RAN_NUM; i < Const.RANGE_RAN_NUM; i++) {
-            car.increasePositionOrNot(i);
-            assertThat(car.getPosition()).isEqualTo(++init);
-        }
+    void 자동차_객체_생성_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            car = new Car("123456");            // 5글자 초과 이름 입력 시 예외
+        });
 
+        assertThrows(IllegalArgumentException.class, () -> {
+            car = new Car("");                  // 길이가 0인 name을 입력 시 예외
+
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            car = new Car(null);                // null에 대한 예외
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            car = new Car(" ");                 // 공백 입력 시 에외
+        });
     }
 
     @Test
-    void 자동차_포지션_변화_없음() {
-        int init = car.getPosition();
-        for (int i = Const.ZERO; i < Const.GO_RAN_NUM; i++) {
-            car.increasePositionOrNot(i);
-            assertThat(car.getPosition()).isEqualTo(init);
-        }
+    void 자동차_포지션_변화() {
+        car.increasePositionOrNot(3);
+        assertThat(car.matchPosition(0)).isTrue();
     }
 
     @AfterEach
