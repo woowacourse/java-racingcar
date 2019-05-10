4f859ea29b69cb4b4d96bbb13e2f06fd301d19de
김성훈 <seonghun127@gmail.com>
2019-05-10 17:22:46 +0900

    우승 자동차를 구하기 위한 Winners 클래스 추가

--- src/main/java/domain/Winners.java
+++ src/main/java/domain/Winners.java
@@ -1,4 +1,32 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Winners {
+    private List<Car> carsFinishedRace;
+
+    public Winners(List<Car> carsFinishedRace) {
+        this.carsFinishedRace = carsFinishedRace;
+    }
+
+    public int getMaxPosition() {
+        int maxPosition = 0;
+
+        for(Car car : carsFinishedRace) {
+            if(car.isGreaterThanMaxPosition(maxPosition)){
+                maxPosition += car.getPositionGap(maxPosition);
+            }
+        }
+        return maxPosition;
+    }
+
+    public String getRaceWinners(int maxPosition) {
+        List<String> winnersList = new ArrayList<>();
+
+        carsFinishedRace.stream().filter(x -> x.matchPosition(maxPosition))
+                .forEach(x -> winnersList.add(x.getName()));
+
+        return String.join(",", winnersList);
+    }
 }
