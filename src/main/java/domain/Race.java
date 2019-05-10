64747f501403b33b71786143a4bf0e75b59a015a
김성훈 <seonghun127@gmail.com>
2019-05-10 16:24:58 +0900

    라운드 회수만큼 레이스 진행을 시키는 메소드 추가

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -6,32 +6,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
-    private List<Car> raceCars;
+    private List<Car> carsOnRace;
     private int roundCount;
     private int maxPosition = Const.ZERO;
 
-    public Race(List<Car> raceCars, int roundCount) {
-        this.raceCars = raceCars;
+    public Race(List<Car> carsOnRace, int roundCount) {
+        this.carsOnRace = carsOnRace;
         this.roundCount = roundCount;
     }
 
-    public void moveAllCarOneTime() {
-        for (Car car : raceCars) {
+    public String moveAllCarsByRoundCount() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < roundCount; i++) {
+            stringBuilder.append(moveAllCarsByOneTime());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private String moveAllCarsByOneTime() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : carsOnRace) {
             car.increasePositionOrNot(Util.getRandomNumber());
+            stringBuilder.append(car.toString());
             if (car.isGreaterThanMaxPosition(maxPosition)) {
                 ++this.maxPosition;
             }
         }
+        return stringBuilder.toString();
     }
 
     public List<Car> getRaceCars() {
-        return raceCars;
+        return carsOnRace;
     }
 
     public String getRaceWinners() {
         List<String> winnersList = new ArrayList<>();
 
-        raceCars.stream().filter(x -> x.matchPosition(maxPosition))
+        carsOnRace.stream().filter(x -> x.matchPosition(maxPosition))
                 .forEach(x -> winnersList.add(x.getName()));
 
         return String.join(",", winnersList);
