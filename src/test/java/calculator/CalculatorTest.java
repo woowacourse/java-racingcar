1bd022f190a0518345531f084f154b5ad922f58b
yejilee <yjll1019@naver.com>
2019-05-10 13:46:45 +0900

    <자동차 경주 게임> 테스트 코드 import 수정

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,31 +9,31 @@ public class CalculatorTest {
     Calculator calculator;
 
     @Test
-    void 덧셈() {
+    public void 덧셈() {
         calculator = new Calculator(new String[]{"2", "+", "3.5"});
         assertThat(calculator.add(2, 3.5)).isEqualTo(5.5);
     }
 
     @Test
-    void 뺄셈() {
+    public void 뺄셈() {
         calculator = new Calculator(new String[]{"2", "-", "3.5"});
         assertThat(calculator.subtract(2, 3.5)).isEqualTo(-1.5);
     }
 
     @Test
-    void 곱셈() {
+    public void 곱셈() {
         calculator = new Calculator(new String[]{"2.0", "*", "3.0"});
         assertThat(calculator.multiply(2.0, 3.0)).isEqualTo(6.0);
     }
 
     @Test
-    void 나눗셈() {
+    public void 나눗셈() {
         calculator = new Calculator(new String[]{"2.0", "/", "3.0"});
         assertThat(calculator.divide(2.0, 4.0)).isEqualTo(0.5);
     }
 
     @Test
-    void 정상적인_입력() {
+    public void 정상적인_입력() {
         String testInput = "2 + 3 * 4 / 2";
         Calculator cal = new Calculator(testInput.split(" "));
         assertThat(cal.calculate()).isEqualTo(10);
--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,13 +1,14 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class RacingTest {
+import static org.junit.Assert.assertTrue;
+
+public class RacingTest {
 
     @Test
-    void 랜덤숫자가_4이상일때만_움직인다() {
+    public void 랜덤숫자가_4이상일때만_움직인다() {
         Car car = new Car("Test");
         int beforePosition = car.getPosition();
 
