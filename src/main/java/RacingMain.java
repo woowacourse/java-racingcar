2f3f6821f1df7cb3e4c87f876cdfff6ae055e5c2
김성훈 <seonghun127@gmail.com>
2019-05-10 16:06:49 +0900

    메소드 명 변경에 따른 사용 메소드 명 변경

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -3,7 +3,7 @@ import domain.Race;
 
 public class RacingMain {
     public static void main(String[] args) {
-        Race race = RacingController.moveAllCar(RacingController.setCarName(), RacingController.setRoundCount());
+        Race race = RacingController.moveAllCar(RacingController.setCarsOnRace(), RacingController.setRoundCount());
         RacingController.printWinners(race);
     }
 }
