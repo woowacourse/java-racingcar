80da9e202a67632c41b7f116c34ab04570e291cb
yk1028 <wongeunsong94@gmail.com>
2019-05-12 00:30:14 +0900

    test: CarNameSpliter 클래스 테스트

--- src/test/java/racingcar/domain/CarNameSpliterTest.java
+++ src/test/java/racingcar/domain/CarNameSpliterTest.java
@@ -4,26 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class RacingGameTest {
-
+public class CarNameSpliterTest {
     @Test
-    void 공백이_입력된_경우() {
+    void 앞에_연속된_콤마가_입력된_경우() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new RacingGame(CarNameSpliter.splitCarNames("pobi,   ,brown"), 1);
+            CarNameSpliter.splitCarNames(",,,pobi");
         });
     }
 
     @Test
-    void 자동차들이_없는_경우() {
+    void 중간에_연속된_콤마가_입력된_경우() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new RacingGame(CarNameSpliter.splitCarNames(""), 1);
+            CarNameSpliter.splitCarNames("po,,,bi");
         });
     }
 
     @Test
-    void 시도_횟수가_최소값_미만인_경우() {
+    void 뒤에_연속된_콤마가_입력된_경우() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new RacingGame(CarNameSpliter.splitCarNames("pobi,cu,brown"), -1);
+            CarNameSpliter.splitCarNames("pobi,,,");
         });
     }
 }
