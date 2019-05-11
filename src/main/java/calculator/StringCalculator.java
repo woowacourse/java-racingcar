86f2fc55e879f6f091e3aed09af92c15b78db494
deocksoo <dusol92@snu.ac.kr>
2019-05-11 22:58:18 +0900

    [REFACOTR] StringCalculator를 리펙토링합니다
    1. 일부 변수명을 변경하였습니다.
    2. calculateAll 메서드에서 반복문 순회 전에 첫번째 숫자를 꺼내도록 하여
    반복문 밖에서 operator가 없을 때의 초기화를 수행하도록 하였습니다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,35 +5,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
-    List<String> getSplit(String inputString) {
-        List<String> row = Arrays.stream(inputString.split(" "))
-                .collect(Collectors.toList());
-
-        if (row.isEmpty()) {
-            throw new IllegalArgumentException("데이터가 잘못되었습니다!");
-        }
-        return row;
-    }
+    private static final int FIRST_ELEMENT_IDX = 0;
 
     int calculateAll(String inputString) {
+        List<String> elementsOfFormula = getSplit(inputString);
+        Operator operator = null;
         int result = 0;
-        Operator operation = null;
-        List<String> calculationFormula = getSplit(inputString);
 
-        for (String numberOrOperator : calculationFormula) {
-
+        result += Integer.valueOf(elementsOfFormula.remove(FIRST_ELEMENT_IDX));
+        for (String numberOrOperator : elementsOfFormula) {
             if (Operator.isOperator(numberOrOperator)) {
-                operation = Operator.valueof(numberOrOperator);
+                operator = Operator.valueof(numberOrOperator);
                 continue;
             }
 
-            if (operation == null) {
-                result += Integer.valueOf(numberOrOperator);
-                continue;
-            }
-
-            result = operation.applyAsInt(result, Integer.valueOf(numberOrOperator));
+           result = operator.applyAsInt(result, Integer.valueOf(numberOrOperator));
         }
         return result;
+    }
+
+    List<String> getSplit(String inputString) {
+        List<String> elementsOfFormula = Arrays.stream(inputString.split(" "))
+                .collect(Collectors.toList());
+
+        if (elementsOfFormula.isEmpty()) {
+            throw new IllegalArgumentException("데이터가 잘못되었습니다!");
+        }
+        return elementsOfFormula;
     }
 }
