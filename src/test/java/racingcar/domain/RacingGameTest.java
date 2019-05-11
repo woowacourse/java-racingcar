948ddbe51a8090c466564e2809642cd7bd392932
yk1028 <wongeunsong94@gmail.com>
2019-05-11 16:42:43 +0900

    test: 자동차들이 없는 경우 테스트

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -34,6 +34,13 @@ public class RacingGameTest {
     }
 
     @Test
+    void 자동차들이_없는_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new RacingGame(CarNameSpliter.splitCarNames(""),1);
+        });
+    }
+
+    @Test
     void 시도_횟수가_최소값_미만인_경우() {
         assertThrows(IllegalArgumentException.class, () -> {
             new RacingGame(CarNameSpliter.splitCarNames("pobi,cu,brown"),-1);
