39bc79c977c1db52f716b37c87dcf7a8b29ad07f
unknown <ddu0422@naver.com>
2019-05-11 16:24:29 +0900

    피드백 : UserInput 클래스 Scanner 변수명 축약어 지양

--- src/main/java/game/input/UserInput.java
+++ src/main/java/game/input/UserInput.java
@@ -4,17 +4,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInput {
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분");
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
     public static int inputGameCount() {
         try {
             System.out.println("시도할 회수는 몇 회인가요?");
-            return sc.nextInt();
+            return scanner.nextInt();
         } catch (InputMismatchException e) {
             return inputGameCount();
         }
