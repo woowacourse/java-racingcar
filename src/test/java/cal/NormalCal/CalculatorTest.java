74ddaa07ed6ea9662c3f26cfb283e7096def4f6f
mrkwon <erased11@gmail.com>
2019-05-12 16:25:15 +0900

    [수정] 문자열 계산기 디렉터리 수정

--- src/test/java/cal/NormalCal/CalculatorTest.java
+++ src/test/java/cal/NormalCal/CalculatorTest.java
@@ -1,4 +1,4 @@
-package cal;
+package cal.NormalCal;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/cal/NormalCal/StrCalculatorTest.java
+++ src/test/java/cal/NormalCal/StrCalculatorTest.java
@@ -1,4 +1,4 @@
-package cal;
+package cal.NormalCal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
