d12cd653a080c4da22ee00196ae4afb378308b9d
school0bhy <gusud96@naver.com>
2019-05-12 20:28:35 +0900

    test : Race 테스트 메소드 추가
    Race 클래스의 startRound 메소드에 대한 테스트 코드 추가

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -10,26 +10,46 @@ import java.util.Arrays;
 class RaceTest {
     @Test
     void createTest() {
-        assertThat(new Race(Arrays.asList("A", "B", "C")).equals(new Race(Arrays.asList("A", "B", "C")))).isTrue();
+        assertThat(new Race(Arrays.asList("A", "B", "C")).
+                equals(new Race(Arrays.asList("A", "B", "C")))).isTrue();
     }
 
     @Test
     void invalidCreateTest1() {
-        assertThat(new Race(Arrays.asList("A", "B", "C")).equals(new Race(Arrays.asList("D", "E", "F")))).isFalse();
+        assertThat(new Race(Arrays.asList("A", "B", "C")).
+                equals(new Race(Arrays.asList("D", "E", "F")))).isFalse();
     }
 
     @Test
     void invalidCreateTest2() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Race race = new Race(Arrays.asList("A"));
+            new Race(Arrays.asList("A"));
         });
     }
 
     @Test
     void invalidCreateTest3() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Race race = new Race(Arrays.asList("A", "A"));
+            new Race(Arrays.asList("A", "A"));
         });
+    }
+
+    @Test
+    void startRoundTest1() {
+        Race race = new Race(Arrays.asList("A", "B"), new ForceMove());
+        RoundResult roundResult = new RoundResult();
+        roundResult.addParticipant(new Car("A", 1), 1);
+        roundResult.addParticipant(new Car("B", 1), 1);
+        assertThat(race.startRound().equals(roundResult)).isTrue();
+    }
+
+    @Test
+    void startRoundTest2() {
+        Race race = new Race(Arrays.asList("A", "B"), new ForceMove());
+        RoundResult roundResult = new RoundResult();
+        roundResult.addParticipant(new Car("A", 3), 3);
+        roundResult.addParticipant(new Car("B", 3), 3);
+        assertThat(race.startRound().equals(roundResult)).isFalse();
     }
 
     @Test
