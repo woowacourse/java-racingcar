5e001939fe4d76e4edfcb67b14043f5e79c2657c
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-13 00:19:53 +0900

    [MOD] if문 제거 (enum 적용)

--- src/test/java/stringcalc/StringCalculatorTest.java
+++ src/test/java/stringcalc/StringCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
-    StringCalculator calc;
+    private StringCalculator calc;
 
     @BeforeEach
     void setUp() {
