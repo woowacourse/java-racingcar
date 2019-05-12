01373d1773e37bba84124d5a889cb20b600f8eb9
Jinwoo Shin <bluesky0255@gmail.com>
2019-05-13 00:00:30 +0900

    Refactor to apply feedback(String calculator)
    * Added operator's symbol field to `IntOperator`.

--- src/main/java/com/woowacourse/stringcalculator/IntOperator.java
+++ src/main/java/com/woowacourse/stringcalculator/IntOperator.java
@@ -3,28 +3,30 @@ package com.woowacourse.stringcalculator;
 import java.util.function.BinaryOperator;
 
 public enum IntOperator {
-    PLUS((i1, i2) -> i1 + i2),
-    MINUS((i1, i2) -> i1 - i2),
-    MULTIPLY((i1, i2) -> i1 * i2),
-    DIVIDE((i1, i2) -> i1 / i2);
+    PLUS('+', (i1, i2) -> i1 + i2),
+    MINUS('-', (i1, i2) -> i1 - i2),
+    MULTIPLY('*', (i1, i2) -> i1 * i2),
+    DIVIDE('/', (i1, i2) -> i1 / i2);
 
+    private char operatorSymbol;
     private BinaryOperator<Integer> operation;
 
-    IntOperator(BinaryOperator<Integer> operation) {
+    IntOperator(char symbol, BinaryOperator<Integer> operation) {
+        this.operatorSymbol = symbol;
         this.operation = operation;
     }
 
     public static IntOperator valueOf(char operatorChar) {
-        if (operatorChar == '+') {
+        if (operatorChar == PLUS.operatorSymbol) {
             return PLUS;
         }
-        if (operatorChar == '-') {
+        if (operatorChar == MINUS.operatorSymbol) {
             return MINUS;
         }
-        if (operatorChar == '*') {
+        if (operatorChar == MULTIPLY.operatorSymbol) {
             return MULTIPLY;
         }
-        if (operatorChar == '/') {
+        if (operatorChar == DIVIDE.operatorSymbol) {
             return DIVIDE;
         }
         throw new IllegalArgumentException("Invalid operator character: " + operatorChar);
