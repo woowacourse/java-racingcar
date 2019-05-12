09b186a3a395489d4a7a02b0916cd97ff6ef60b5
Ko eunsuk <eunsukko.128@gmail.com>
2019-05-13 06:45:19 +0900

    MoveStrategy 의 테스트 수정
    좀더 명확한 이름으로 변경

--- src/main/java/domain/MoveStrategy.java
+++ src/main/java/domain/MoveStrategy.java
@@ -3,7 +3,7 @@ package domain;
 import java.util.Random;
 
 public class MoveStrategy {
-    public static final int MAX_NUM = 10;
+    public static final int MAX_NUM = 10; // [0, MAX_NUM)
     public static final int MOVE_START = 4;
 
     private final Random random;
--- src/test/java/domain/MoveStrategyTest.java
+++ src/test/java/domain/MoveStrategyTest.java
@@ -14,15 +14,12 @@ class MoveStrategyTest {
 
     private MoveStrategy moveStrategy;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
+    // 외부에서 랜덤을 신경 쓸 필요가 없는 대신
+    // 테스트코드에서 내부에 있는 세부정보 (랜덤값 생성)을 신경쓰게되는 건가?
     @Test
-    void isMove_4이상일때_참인지() {
+    void isMove_참인랜덤값생성() {
         // Arrange
-        IntStream validRandValues = IntStream.of(4, 5, 6, 7, 8, 9);
+        IntStream validRandValues = IntStream.range(MoveStrategy.MOVE_START, MoveStrategy.MAX_NUM);
 
         validRandValues.forEach((v) -> {
             // Arrange
@@ -39,9 +36,9 @@ class MoveStrategyTest {
     }
 
     @Test
-    void isMove_3이하일때_거짓인지() {
+    void isMove_거짓인랜덤값생성() {
         // Arrange
-        IntStream validRandValues = IntStream.of(0, 1, 2, 3);
+        IntStream validRandValues = IntStream.of(0, MoveStrategy.MOVE_START);
 
         validRandValues.forEach((v) -> {
             // Arrange
