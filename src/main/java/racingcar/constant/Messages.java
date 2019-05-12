282adadf9086b727118d4b61dd19a3f2a6b4ddcd
yk1028 <wongeunsong94@gmail.com>
2019-05-12 17:25:24 +0900

    refactor: Messages의 static 변수들을 InputView 내부로 이동

--- src/main/java/racingcar/constant/Messages.java
+++
@@ -1,6 +0,0 @@
-package racingcar.constant;
-
-public class Messages {
-    public static final String INPUT_CAR_LIST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
-}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,14 @@
 package racingcar.view;
 
-import racingcar.constant.Messages;
-
 import java.util.Scanner;
 
 public class InputView {
-    public static final Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String INPUT_CAR_LIST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public static String inputCarList() {
-        System.out.println(Messages.INPUT_CAR_LIST);
+        System.out.println(INPUT_CAR_LIST);
         try {
             return SCANNER.nextLine();
         } catch (Exception e) {
@@ -18,7 +18,7 @@ public class InputView {
     }
 
     public static int inputTryCount() {
-        System.out.println(Messages.INPUT_TRY_COUNT);
+        System.out.println(INPUT_TRY_COUNT);
         try {
             return SCANNER.nextInt();
         } catch (Exception e) {
