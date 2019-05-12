31ed1a3a0f130adfb22f1a253c242cd9f1741953
heebong <heebg2@naver.com>
2019-05-12 10:15:06 +0900

    [변경] calculator Test

--- src/test/java/calculator/calcinterface/CalculatorLineTest.java
+++ src/test/java/calculator/calcinterface/CalculatorLineTest.java
@@ -9,65 +9,51 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorLineTest {
     private CalculatorLine calculatorLine;
-    private UserInterfaceView userInterfaceView;
 
     @BeforeEach
     void setUp() {
         calculatorLine = new CalculatorLine();
-        userInterfaceView = new UserInterfaceView();
     }
 
     @Test
     void 덧셈() {
         assertThat(5).isEqualTo(
-                calculatorLine.calculateFormula(
-                        userInterfaceView.splitFormula("2 + 3")
-                )
+                calculatorLine.calculateFormula("2 + 3")
         );
     }
 
     @Test
     void 뺄셈() {
         assertThat(2).isEqualTo(
-                calculatorLine.calculateFormula(
-                        userInterfaceView.splitFormula("5 - 3")
-                )
+                calculatorLine.calculateFormula("5 - 3")
         );
     }
 
     @Test
     void 곱셈() {
         assertThat(10).isEqualTo(
-                calculatorLine.calculateFormula(
-                        userInterfaceView.splitFormula("5 * 2")
-                )
+                calculatorLine.calculateFormula("5 * 2")
         );
     }
 
     @Test
     void 나눗셈() {
         assertThat(2).isEqualTo(
-                calculatorLine.calculateFormula(
-                        userInterfaceView.splitFormula("10 / 5")
-                )
+                calculatorLine.calculateFormula("10 / 5")
         );
     }
 
     @Test
     void 혼합_사칙연산() {
         assertThat(15).isEqualTo(
-                calculatorLine.calculateFormula(
-                        userInterfaceView.splitFormula("2 + 3 - 2 * 10 / 2")
-                )
+                calculatorLine.calculateFormula("2 + 3 - 2 * 10 / 2")
         );
     }
 
     @Test
     void 나눗셈_예외처리() {
         assertThrows(ArithmeticException.class, () -> {
-           calculatorLine.calculateFormula(
-                   userInterfaceView.splitFormula("10 / 0")
-           );
+           calculatorLine.calculateFormula("10 / 0");
         });
 
     }
@@ -75,6 +61,5 @@ class CalculatorLineTest {
     @AfterEach
     void tearDown() {
         calculatorLine = null;
-        userInterfaceView = null;
     }
 }
