7388bd2ce638354201533ca4435f116c85d79313
yejilee <yjll1019@naver.com>
2019-05-12 21:57:19 +0900

    <문자열 계산기> 정규식 상수화, 중복 코드 제거.

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -3,66 +3,54 @@ package calculator.validator;
 import java.util.Arrays;
 
 public class Validator {
-    private String value;
-    private String[] values;
+	private static final String REGULAR_EXPRESSION_OF_OPERATOR = "[+|\\-|*|/]";
+	private static final String REGULAR_EXPRESSION_OF_NUMBER = "[1-9][0-9]*";
 
-    public Validator(String value) {
-        this.value = value;
-        this.values = value.split(" ");
-    }
+	public static boolean validateAll(String value) {
+		String[] values = value.split(" ");
 
-    public boolean validateAll() {
-        if (!validateLength()) {
-            System.out.println("계산식을 잘못 입력하였습니다. 다시 입력해주세요!");
-            return false;
-        }
-        if (!validateForm()) {
-            System.out.println("숫자와 연산자 외의 다른 문자를 입력하였습니다! 다시 입력해주세요!");
-            return false;
-        }
-        if (!dividedZero()) {
-            System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요!");
-            return false;
-        }
-        if(!validateOrderOfNumbers() || !validateOrderOfOperator()) {
-            System.out.println("계산식의 순서가 올바르지 않습니다. 다시 입력해주세요!");
-            return false;
-        }
-        return true;
-    }
+		if (!validateLength(values)) {
+			System.out.println("계산식을 잘못 입력하였습니다. 다시 입력해주세요!");
+			return false;
+		}
+		if (!validateForm(values)) {
+			System.out.println("숫자와 연산자 외의 다른 문자를 입력하였습니다! 다시 입력해주세요!");
+			return false;
+		}
+		if (!dividedZero(value)) {
+			System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요!");
+			return false;
+		}
+		if (!validateOrder(values)) {
+			System.out.println("계산식의 순서가 올바르지 않습니다. 다시 입력해주세요!");
+			return false;
+		}
+		return true;
+	}
 
-    private boolean validateLength() {
-        return (values.length < 3) ? false : true;
-    }
+	private static boolean validateLength(String[] values) {
+		return (values.length < 3) ? false : true;
+	}
 
-    private boolean validateForm() {
-        return !(Arrays.stream(values).anyMatch(x -> !isNumeric(x) && !(x.matches("[+|\\-|*|/]"))));
-    }
+	private static boolean validateForm(String[] values) {
+		return !(Arrays.stream(values).anyMatch(x -> !(x.matches(REGULAR_EXPRESSION_OF_NUMBER)) && !(x.matches(REGULAR_EXPRESSION_OF_OPERATOR))));
+	}
 
-    private boolean dividedZero() {
-        return !(value.contains("/ 0"));
-    }
+	private static boolean dividedZero(String value) {
+		return !(value.contains("/ 0"));
+	}
 
-    private boolean validateOrderOfNumbers() {
-        int wrongCount = 0;
-        for (int i = 0; i < values.length; i += 2)
-            wrongCount += (isNumeric(values[i])) ? 0 : 1;
-        return (wrongCount > 0) ? false : true;
-    }
+	private static boolean validateOrder(String[] values) {
+		int wrongCount = 0;
 
-    private boolean validateOrderOfOperator() {
-        int wrongCount = 0;
-        for (int i = 1; i < values.length; i += 2)
-            wrongCount += (values[i].matches("[+|\\-|*|/]")) ? 0 : 1;
-        return (wrongCount > 0) ? false : true;
-    }
+		for (int i = 0; i < values.length; ++i) {
+			if (i % 2 == 0) {
+				wrongCount += (values[i].matches(REGULAR_EXPRESSION_OF_NUMBER)) ? 0 : 1;
+			} else {
+				wrongCount += (values[i].matches(REGULAR_EXPRESSION_OF_OPERATOR)) ? 0 : 1;
+			}
+		}
+		return (wrongCount > 0) ? false : true;
+	}
 
-    private boolean isNumeric(String input) {
-        try {
-            Double.parseDouble(input);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 }
--- src/test/java/calculator/validator/ValidatorTest.java
+++ src/test/java/calculator/validator/ValidatorTest.java
@@ -3,31 +3,37 @@ package calculator.validator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ValidatorTest {
 
     @Test
     public void 숫자와_연산자가_아닌_경우() {
-        assertFalse(new Validator("a + 1").validateAll());
+        assertFalse(Validator.validateAll("a + 1"));
     }
 
     @Test
     public void 길이가_3미만인_경우() {
-        assertFalse(new Validator("1 +").validateAll());
+        assertFalse(Validator.validateAll("1 +"));
     }
 
     @Test
     public void 분모가_0인_경우() {
-        assertFalse(new Validator("1 / 0").validateAll());
+        assertFalse(Validator.validateAll("1 / 0"));
     }
 
     @Test
     public void 숫자_순서가_잘못된_경우() {
-        assertFalse(new Validator("1 + +").validateAll());
+        assertFalse(Validator.validateAll("1 + +"));
     }
 
     @Test
     public void 연산자_순서가_잘못된_경우() {
-        assertFalse(new Validator("1 1 +").validateAll());
+        assertFalse(Validator.validateAll("1 1 +"));
+    }
+
+    @Test
+    public void 정상적인_입력() {
+        assertTrue(Validator.validateAll("1 + 1000"));
     }
 }
