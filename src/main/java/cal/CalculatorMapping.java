b05c3732ea16174e1c010acfe4ef3edb8d51a8cf
DaejunBae <dpudpu11@gmail.com>
2019-05-12 11:49:42 +0900

    refactor: TextCalculator.calculatorMap을 CalculatorMapping.class로 추출

---
+++ src/main/java/cal/CalculatorMapping.java
@@ -0,0 +1,19 @@
+package cal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculatorMapping {
+    private static Map<String, Calculator> calculators = new HashMap<>();
+
+    static{
+        calculators.put("+", new PlusCalculator());
+        calculators.put("-", new MinusCalculator());
+        calculators.put("*", new MultiplyCalculator());
+        calculators.put("/", new DivideCalculator());
+    }
+
+    public static Calculator getCalculator(String operator){
+        return calculators.get(operator);
+    }
+}
--- src/main/java/cal/TextCalculator.java
+++ src/main/java/cal/TextCalculator.java
@@ -1,34 +1,22 @@
 package cal;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TextCalculator {
-    private double result;
-    private Map<String, Calculator> calculatorMap;
-
-    public TextCalculator() {
-        calculatorMap = new HashMap<>();
-        calculatorMap.put("+", new PlusCalculator());
-        calculatorMap.put("-", new MinusCalculator());
-        calculatorMap.put("*", new MultiplyCalculator());
-        calculatorMap.put("/", new DivideCalculator());
-    }
-
     public double calculate(String inputText) {
         String tokens[] = inputText.trim().split(" ");
+        double result = toDouble(tokens[0]);
 
-        result = toDouble(tokens[0]);
         for (int i = 1; i < tokens.length; i += 2) {
-            detectOperator(tokens, i);
+            String operator = tokens[i];
+            double number = toDouble(tokens[i + 1]);
+            result = calculate(result, operator, number);
         }
-
         return result;
     }
 
-    private void detectOperator(String[] tokens, int i) {
-        result = calculatorMap.get(tokens[i]).calculate(result, Double.parseDouble(tokens[i + 1]));
+    private double calculate(double result, String operator, double number) {
+        return CalculatorMapping.getCalculator(operator).calculate(result, number);
     }
+
 
     private double toDouble(String value) {
         try {
