ce8836b1ca350214158c560a80b5137ef6c99c48
andole87 <andole87@gmail.com>
2019-05-12 16:21:05 +0900

    refactor: 변수명 변경 Main.main().rg
    Main.main.rg -> Main.main.racingGame

--- src/main/java/racing/Main.java
+++ src/main/java/racing/Main.java
@@ -2,7 +2,7 @@ package racing;
 
 public class Main {
     public static void main(String[] args) {
-        RacingGame rg = new RacingGame();
-        rg.run();
+        RacingGame racingGame = new RacingGame();
+        racingGame.run();
     }
 }
