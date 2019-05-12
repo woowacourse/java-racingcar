d2af8cfd870b57e9e8cb0ed4a1a6ffc25bcd0fac
andole87 <andole87@gmail.com>
2019-05-12 18:58:10 +0900

    style: 연산자 사이 공백 삽입
    cal 패키지 내부 클래스들의 연산자 사이 공백 삽입.

--- src/main/java/cal/MinusCalculator.java
+++ src/main/java/cal/MinusCalculator.java
@@ -3,6 +3,6 @@ package cal;
 public class MinusCalculator implements Calculator {
     @Override
     public double calculate(double num1, double num2) {
-        return num1-num2;
+        return num1 - num2;
     }
 }
--- src/main/java/cal/PlusCalculator.java
+++ src/main/java/cal/PlusCalculator.java
@@ -3,6 +3,6 @@ package cal;
 public class PlusCalculator implements Calculator {
     @Override
     public double calculate(double num1, double num2) {
-        return num1+num2;
+        return num1 + num2;
     }
 }
