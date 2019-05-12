05c07a4f1bee6c36de4200ab32c65d99847ec05e
Seongmo Jeon <oeeen3@gmail.com>
2019-05-12 11:56:44 +0900

    test: 우승자가 여러명인 경우 테스트 추가

--- src/test/java/racingcar/domain/WinningCarsFinderTest.java
+++ src/test/java/racingcar/domain/WinningCarsFinderTest.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class WinningCarsFinderTest {
+    WinningCarsFinder winningCarsFinder;
+
     @Test
     void 우승자_찾기() {
         ArrayList<Car> cars = new ArrayList<>();
@@ -14,6 +16,19 @@ public class WinningCarsFinderTest {
         cars.add(new Car("brown", 4));
         cars.add(new Car("jun", 3));
         cars.add(new Car("jason", 2));
-        assertThat(WinningCarsFinder.findWinningCars(cars).get(0)).isEqualTo(new Car("pobi", 5));
+        winningCarsFinder = new WinningCarsFinder(cars);
+        assertThat(winningCarsFinder.getWinningCars().get(0)).isEqualTo(new Car("pobi", 5));
+    }
+
+    @Test
+    void 우승자가_여러명인_경우() {
+        ArrayList<Car> cars = new ArrayList<>();
+        cars.add(new Car("pobi", 5));
+        cars.add(new Car("brown", 5));
+        cars.add(new Car("jun", 3));
+        cars.add(new Car("jason", 2));
+        winningCarsFinder = new WinningCarsFinder(cars);
+        assertThat(winningCarsFinder.getWinningCars().get(0)).isEqualTo(new Car("pobi", 5));
+        assertThat(winningCarsFinder.getWinningCars().get(1)).isEqualTo(new Car("brown", 5));
     }
 }
