11c867ec698718b137d55213a191eea958f85953
yk1028 <wongeunsong94@gmail.com>
2019-05-13 14:34:23 +0900

    refactor: Rule을 인터페이스로 변경

--- src/main/java/racingcar/domain/Rule/MoveRule.java
+++ src/main/java/racingcar/domain/Rule/MoveRule.java
@@ -2,18 +2,18 @@ package racingcar.domain.Rule;
 
 import racingcar.domain.RandomNumberGenerator;
 
-public class MoveRule extends Rule {
+public class MoveRule implements Rule {
     private static final int RANDOM_NUMBER_BOUND = 9;
     private static final int RANDOM_NUMBER_LOWER_BOUND = 1;
     private static final int MOVEMENT_CRITERIA = 4;
 
-    public MoveRule() {
-        this(RandomNumberGenerator.generateRandomNumber(RANDOM_NUMBER_BOUND, RANDOM_NUMBER_LOWER_BOUND));
+    public boolean canMove() {
+        int randomNumber = RandomNumberGenerator.generateRandomNumber(RANDOM_NUMBER_BOUND, RANDOM_NUMBER_LOWER_BOUND);
+
+        return canMove(randomNumber);
     }
 
-    public MoveRule(int number) {
-        if (number >= MOVEMENT_CRITERIA) {
-            super.canMove = true;
-        }
+    public boolean canMove(int number) {
+        return number >= MOVEMENT_CRITERIA;
     }
 }
--- src/main/java/racingcar/domain/Rule/Rule.java
+++ src/main/java/racingcar/domain/Rule/Rule.java
@@ -1,9 +1,5 @@
 package racingcar.domain.Rule;
 
-public class Rule {
-    protected boolean canMove = false;
-
-    public boolean canMove() {
-        return canMove;
-    }
+public interface Rule {
+    boolean canMove();
 }
