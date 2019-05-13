9d0b843714da23ad5528afea1303787f8a1f1151
hyperpace <cnk9109@gmail.com>
2019-05-13 16:19:47 +0900

    feat(Racing): Change random object location, make LapRecord to Racing

--- src/main/java/model/Racing.java
+++ src/main/java/model/Racing.java
@@ -4,15 +4,28 @@ import java.util.Random;
 
 public class Racing {
     private static final int RANDOM_BOUND = 10;
+    private static Random random = new Random();
     private Cars cars;
+    private LapCount lapCount;
+    private LapRecord lapRecord;
 
-    public Racing (Cars cars) {
+    public Racing(Cars cars, LapCount lapCount) {
         this.cars = cars;
+        this.lapCount = lapCount;
+        this.lapRecord = new LapRecord();
     }
 
-    public void run() {
+    public LapRecord run() {
+        for (int lapNo = 1; lapNo <= lapCount.getLapCount(); lapNo++) {
+            runLap();
+            lapRecord.add(cars.copy());
+        }
+
+        return lapRecord;
+    }
+
+    private void runLap() {
         for (Car car : cars.getCars()) {
-            Random random = new Random();
             car.move(random.nextInt(RANDOM_BOUND));
         }
     }
