2b7c3346af6db5b2cb52ea19d884905e5d303900
pkch93 <star_final@naver.com>
2019-05-13 17:25:03 +0900

    refactor Round
    - round 끝날때 string값으로 상태를 받은 것을 Car 객체를 담도록 변경

--- src/main/java/racingcargame/model/Round.java
+++ src/main/java/racingcargame/model/Round.java
@@ -1,42 +1,46 @@
 package racingcargame.model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Round {
     private static final int MAX_BOUND = 10;
 
-    private final List<String> roundResult;
+    private final List<Car> roundResult;
 
     Round(List<Car> cars){
         this.roundResult = round(cars);
     }
 
-    private List<String> round(List<Car> cars) {
+    private List<Car> round(List<Car> cars) {
         cars.forEach(car -> car.move(getRandomNo()));
-        return cars.stream()
-                .map(car -> car.getName() + " : " + positionResult(car.getPosition()))
-                .collect(Collectors.toList());
+        List<Car> roundResult = new ArrayList<>();
+        for (Car car : cars) {
+            roundResult.add(new Car(car.getName(), car.getPosition()));
+        }
+        return roundResult;
     }
 
     private int getRandomNo() {
         return (int) (Math.random() * MAX_BOUND);
     }
 
-    private String positionResult(int position){
+    @Override
+    public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < position; i++){
-            stringBuilder.append("-");
+        for (Car result : roundResult){
+            stringBuilder.append(result.getName())
+                    .append(" : ")
+                    .append(positionResult(result.getPosition()))
+                    .append("\n");
         }
         return stringBuilder.toString();
     }
 
-    @Override
-    public String toString() {
+    private String positionResult(int position){
         StringBuilder stringBuilder = new StringBuilder();
-        for (String result : roundResult){
-            stringBuilder.append(result)
-                    .append("\n");
+        for (int i = 0; i < position; i++){
+            stringBuilder.append("-");
         }
         return stringBuilder.toString();
     }
