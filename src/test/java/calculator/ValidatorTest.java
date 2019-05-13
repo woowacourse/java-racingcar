34deb215899c9952116cfd61edaa631d91e3a6bc
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-13 21:08:53 +0900

    <문자열 계산기> 입력 오류 테스트 수정

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -2,32 +2,37 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ValidatorTest {
 
     @Test
     void 숫자와_연산자가_아닌_경우() {
-        assertFalse(new Validator("a + 1").validateForm());
+        String value = "a + 1";
+        assertFalse(new Validator(value, value.split(" ")).validateForm());
     }
 
     @Test
     void 길이가_3미만인_경우() {
-        assertFalse(new Validator("1 +").validateLength());
+        String value = "1 +";
+        assertFalse(new Validator(value, value.split(" ")).validateLength());
     }
 
     @Test
     void 분모가_0인_경우() {
-        assertFalse(new Validator("1 / 0").dividedZero());
+        String value = "1 / 0";
+        assertFalse(new Validator(value, value.split(" ")).dividedZero());
     }
 
     @Test
     void 숫자_순서가_잘못된_경우() {
-        assertFalse(new Validator("1 + +").validateOrderOfNumbers());
+        String value = "1 + +";
+        assertFalse(new Validator(value, value.split(" ")).validateOrderOfNumbers());
     }
 
     @Test
     void 연산자_순서가_잘못된_경우() {
-        assertFalse(new Validator("1 1 +").validateOrderOfOperator());
+        String value = "1 1 +";
+        assertFalse(new Validator(value, value.split(" ")).validateOrderOfOperator());
     }
 }
