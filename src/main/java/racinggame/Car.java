4a82f1bd4c7c95f162f1f707b08d0c82aea238a3
Soojin Roh <sooreal91@gmail.com>
2019-05-13 12:55:22 +0900

    fix : Car클래스 생성자 변경 및 테스트 코드 추가

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -3,10 +3,16 @@ package racinggame;
 public class Car {
     private static final int MOVE_POINT = 4;
     private final String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
+    }
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
     }
 
     public void move(int randomValue) {
--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
     Car car;
+    Car car2;
 
     @BeforeEach
     void setUp() {
         car = new Car("bongjin");
+        car2 = new Car("sloth", 3);
     }
 
     @Test
@@ -37,9 +39,21 @@ public class CarTest {
     }
 
     @Test
+    void 최대_이동_거리_도달_확인_생성자_이용() {
+        boolean result = car2.isMaxInstance(3);
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
     void 자동차_이동_거리_출력_확인() {
         car.move(4);
         String result = car.toString();
         assertThat(result).isEqualTo("bongjin : -");
+    }
+
+    @Test
+    void 자동차_이동_거리_출력_확인_생성자_이용() {
+        String result = car2.toString();
+        assertThat(result).isEqualTo("sloth : ---");
     }
 }
