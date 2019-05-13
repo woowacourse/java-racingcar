77542a12cd7e570b43ff0383aeec47d921102380
deocksoo <dusol92@snu.ac.kr>
2019-05-13 12:49:06 +0900

    [REFACTOR] CarRacingApplication으로부터 league 클래스를 분리합니다

--- src/main/java/racinggame/CarRacingApplication.java
+++ src/main/java/racinggame/CarRacingApplication.java
@@ -3,26 +3,23 @@ package racinggame;
 
 import racinggame.domain.Car;
 import racinggame.domain.LeagueHistory;
-import racinggame.domain.TrialHistory;
 import racinggame.util.InputView;
 import racinggame.util.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class CarRacingApplication {
-    private static final int RANDOM_BOUND = 10;
-
     public static void main(String[] args) {
         CarRacingApplication application = new CarRacingApplication();
         List<Car> cars = application.getCars();
         int totalTrial = application.getTotalTrial();
-        LeagueHistory leagueHistory = new LeagueHistory();
 
-        application.startLeague(leagueHistory, cars, totalTrial);
-        OutputView.showHistory(leagueHistory);
-        OutputView.showWinners(leagueHistory.findWinnersOf(totalTrial));
+        League woowahanLeague = new League(totalTrial);
+        LeagueHistory woowahanLeagueHistory = woowahanLeague.startRaceOf(cars);
+
+        OutputView.showHistoryOf(woowahanLeagueHistory);
+        OutputView.showWinnersOf(woowahanLeagueHistory, totalTrial);
     }
 
     public List<Car> getCars() {
@@ -51,24 +48,5 @@ public class CarRacingApplication {
             OutputView.showErrorMessage(e.getMessage());
             return getTotalTrial();
         }
-    }
-
-    public void startLeague(LeagueHistory leagueHistory, List<Car> cars, int totalTrial) {
-        for (int trial = 1; trial <= totalTrial; trial++) {
-            randomMove(cars);
-            leagueHistory.record(trial, new TrialHistory(cars));
-        }
-    }
-
-    private void randomMove(List<Car> cars) {
-        for (Car car : cars) {
-            car.move(generateRandomNumber());
-        }
-    }
-
-    private int generateRandomNumber() {
-        Random random = new Random();
-
-        return random.nextInt(RANDOM_BOUND);
     }
 }
--- src/main/java/racinggame/util/OutputView.java
+++ src/main/java/racinggame/util/OutputView.java
@@ -10,14 +10,15 @@ public class OutputView {
         System.err.println(message);
     }
 
-    public static void showHistory(LeagueHistory leagueHistory) {
+    public static void showHistoryOf(LeagueHistory leagueHistory) {
         System.out.println("실행 결과");
         for (TrialHistory trialHistory : leagueHistory.values()) {
             System.out.println(trialHistory);
         }
     }
 
-    public static void showWinners(List<String> winners) {
+    public static void showWinnersOf(LeagueHistory leagueHistory, int trial) {
+        List<String> winners = leagueHistory.findWinnersOf(trial);
         String winnerMessage = String.join(", ", winners.toArray(new String[winners.size()]));
 
         System.out.println(winnerMessage + "가 최종 우승했습니다.");
