99d86c914b529b386f5e5ea81d399d14762827f2
yejilee <yjll1019@naver.com>
2019-05-13 15:12:23 +0900

    <자동차 경주 게임> convention 맞추기

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,47 +4,47 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class InputValidator {
-    private static final int MAX_NAME_LENGTH = 5;
-    private static final String REGULAR_EXPRESSION_OF_ALPHABET = "[a-zA-Z]+";
-    private static final String REGULAR_EXPRESSION_OF_NUMBER = "[1-9][0-9]*";
+	private static final int MAX_NAME_LENGTH = 5;
+	private static final String REGULAR_EXPRESSION_OF_ALPHABET = "[a-zA-Z]+";
+	private static final String REGULAR_EXPRESSION_OF_NUMBER = "[1-9][0-9]*";
 
-    public static boolean validateAll(String carNames) {
-        if (carNames.contains(",,")) {
-            System.out.println("중복된 쉼표가 있습니다. 다시 입력해주세요!");
-            return false;
-        }
-        if (!isAlphabet(carNames)) {
-            System.out.println("알파벳 이외의 잘못된 문자가 포함되어 있습니다. 다시 입력해주세요!");
-            return false;
-        }
-        if (!validateLength(carNames)) {
-            System.out.println("자동차 이름이 5글자를 초과하였습니다. 다시 입력해주세요!");
-            return false;
-        }
-        if (!overlapNames(carNames)) {
-            System.out.println("중복된 이름이 존재합니다. 다시 입력해주세요!");
-            return false;
-        }
-        return true;
-    }
+	public static boolean validateAll(String carNames) {
+		if (carNames.contains(",,")) {
+			System.out.println("중복된 쉼표가 있습니다. 다시 입력해주세요!");
+			return false;
+		}
+		if (!isAlphabet(carNames)) {
+			System.out.println("알파벳 이외의 잘못된 문자가 포함되어 있습니다. 다시 입력해주세요!");
+			return false;
+		}
+		if (!validateLength(carNames)) {
+			System.out.println("자동차 이름이 5글자를 초과하였습니다. 다시 입력해주세요!");
+			return false;
+		}
+		if (!overlapNames(carNames)) {
+			System.out.println("중복된 이름이 존재합니다. 다시 입력해주세요!");
+			return false;
+		}
+		return true;
+	}
 
-    private static boolean isAlphabet(String carNames) {
-        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.matches(REGULAR_EXPRESSION_OF_ALPHABET));
-    }
+	private static boolean isAlphabet(String carNames) {
+		return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.matches(REGULAR_EXPRESSION_OF_ALPHABET));
+	}
 
-    private static boolean validateLength(String carNames) {
-        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.length() <= MAX_NAME_LENGTH);
-    }
+	private static boolean validateLength(String carNames) {
+		return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.length() <= MAX_NAME_LENGTH);
+	}
 
-    private static boolean overlapNames(String carNames) {
-        return ((carNames.split(",").length) == (new HashSet<>(Arrays.asList(carNames.split(","))).size()));
-    }
+	private static boolean overlapNames(String carNames) {
+		return ((carNames.split(",").length) == (new HashSet<>(Arrays.asList(carNames.split(","))).size()));
+	}
 
-    public static boolean validateNaturalNumber(String numberOfTimes) {
-        if (!numberOfTimes.matches(REGULAR_EXPRESSION_OF_NUMBER)) {
-            System.out.println("자연수가 아닙니다. 다시 입력해주세요!");
-            return false;
-        }
-        return true;
-    }
+	public static boolean validateNaturalNumber(String numberOfTimes) {
+		if (!numberOfTimes.matches(REGULAR_EXPRESSION_OF_NUMBER)) {
+			System.out.println("자연수가 아닙니다. 다시 입력해주세요!");
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,31 +7,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static List<String> inputCarNames() {
-        System.out.println("경주할 자동차를 입력하세요. (이름은 쉼표(,) 기준으로 구분");
-        String carNames;
-        boolean result;
+	public static List<String> inputCarNames() {
+		System.out.println("경주할 자동차를 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+		String carNames;
+		boolean result;
 
-        do {
-            carNames = scanner.nextLine();
-            result = InputValidator.validateAll(carNames);
-        } while (!result);
+		do {
+			carNames = scanner.nextLine();
+			result = InputValidator.validateAll(carNames);
+		}
+		while (!result);
 
-        return Arrays.asList(carNames.split(","));
-    }
+		return Arrays.asList(carNames.split(","));
+	}
 
-    public static int inputNumberOfTimes() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String numberOfTimes;
-        boolean result;
+	public static int inputNumberOfTimes() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		String numberOfTimes;
+		boolean result;
 
-        do {
-            numberOfTimes = scanner.nextLine();
-            result = InputValidator.validateNaturalNumber(numberOfTimes);
-        } while (!result);
+		do {
+			numberOfTimes = scanner.nextLine();
+			result = InputValidator.validateNaturalNumber(numberOfTimes);
+		}
+		while (!result);
 
-        return Integer.parseInt(numberOfTimes);
-    }
+		return Integer.parseInt(numberOfTimes);
+	}
 }
