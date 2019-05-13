f6bca73af5f35f4d01c49892307d5527d4d8e93b
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-13 14:30:06 +0900

    <문자열 계산기> enum을 활용하여 연산자 계산

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -11,40 +11,8 @@ public class Calculator {
         double result = Double.parseDouble(values[0]);
 
         for (int i = 1; i < values.length - 1; i += 2) {
-            result = operate(result, values[i], Double.parseDouble(values[i + 1]));
+            result = Operation.run(values[i], result, Double.parseDouble(values[i + 1]));
         }
         return result;
-    }
-
-    private double operate(double first, String operator, double second) {
-        if (operator.equals("+")) {
-            return add(first, second);
-        }
-        if (operator.equals("-")) {
-            return subtract(first, second);
-        }
-        if (operator.equals("*")) {
-            return multiply(first, second);
-        }
-        if (operator.equals("/")) {
-            return divide(first, second);
-        }
-        throw new IllegalArgumentException("잘못된 계산식입니다.");
-    }
-
-    double add(double a, double b) {
-        return a + b;
-    }
-
-    double subtract(double a, double b) {
-        return a - b;
-    }
-
-    double multiply(double a, double b) {
-        return a * b;
-    }
-
-    double divide(double a, double b) {
-        return a / b;
     }
 }
