01be89002a0fffe3bee979798fc4dbc2c9188ef5
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-13 14:30:44 +0900

    <문자열 계산기> enum을 활용한 연산자 계산 테스트

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -5,30 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
-    Calculator calculator;
 
     @Test
     void 덧셈() {
-        calculator = new Calculator(new String[]{"2", "+", "3.5"});
-        assertThat(calculator.add(2, 3.5)).isEqualTo(5.5);
+        assertThat(Operation.run("+", 2, 3.5)).isEqualTo(5.5);
     }
 
     @Test
     void 뺄셈() {
-        calculator = new Calculator(new String[]{"2", "-", "3.5"});
-        assertThat(calculator.subtract(2, 3.5)).isEqualTo(-1.5);
+        assertThat(Operation.run("-", 2, 3.5)).isEqualTo(-1.5);
     }
 
     @Test
     void 곱셈() {
-        calculator = new Calculator(new String[]{"2.0", "*", "3.0"});
-        assertThat(calculator.multiply(2.0, 3.0)).isEqualTo(6.0);
+        assertThat(Operation.run("*", 2, 3)).isEqualTo(6.0);
     }
 
     @Test
     void 나눗셈() {
-        calculator = new Calculator(new String[]{"2.0", "/", "3.0"});
-        assertThat(calculator.divide(2.0, 4.0)).isEqualTo(0.5);
+        assertThat(Operation.run("/", 2, 4)).isEqualTo(0.5);
     }
 
     @Test
