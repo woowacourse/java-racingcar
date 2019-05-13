468616c2a8fa3df355cf996095124b19791009d0
wiseoldster <wiseoldster@gmail.com>
2019-05-14 01:15:36 +0900

    Refactoring inputNTrial, inputCarName : Main -> InputView
    inputView에서 효용성 검사를 하고 넘길 수 있으면 그렇게 하는 것이 좋으나, 차 이름의 경우 차 생성 시 규칙에 따라 예외를 발생시키므로 inputView에서 Car Class로 직접 접근하지 않고 올바른 차의 입력을 확인 할 방법 생각 중.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import domain.Car;
 import domain.CarFactory;
 import domain.RacingGame;
-import domain.RacingTrial;
 import view.InputView;
 import view.OutputView;
 
@@ -10,35 +9,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Car> cars = inputCars();
-        int nTrials = inputNTrials();
+        int nTrials = InputView.inputNTrials();
 
         RacingGame.play(cars, nTrials);
     }
 
     private static ArrayList<Car> inputCars() {
+        // 차 객체의 생성자에서 에러를 던지는 방식을 유지하면서 MVC패턴을 만드는 방법을 고민 중인데 쉽게 답이 안나오네요.
         try {
-            OutputView.printMassage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n");
-            return CarFactory.newCars(InputView.readString());
+            return CarFactory.newCars(InputView.inputCarNames());
         } catch (IllegalArgumentException e) {
             OutputView.printMassage(e.getMessage());
             return inputCars();
         }
     }
 
-    private static int inputNTrials() {
-        int nTrials;
-        try {
-            OutputView.printMassage("시도할 회수는 몇회인가요?\n");
-            nTrials = InputView.readNumber();
-        } catch (IllegalArgumentException e) {
-            OutputView.printMassage("올바르지 않은 입력입니다. 숫자를 입력해주세요.\n");
-            return inputNTrials();
-        }
-
-        if (nTrials < 0) {
-            OutputView.printMassage("입력 값이 음수입니다. 다시 입력해주세요.");
-            return inputNTrials();
-        }
-        return nTrials;
-    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,52 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
+    private static final String INPUT_N_TRIAL_MSG = "시도할 회수는 몇 회인가요?";
+    private static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_MISMATCH_MSG = "올바르지 않은 입력입니다.";
+    private static final String NEGATIVE_VALUE_MSG = "입력값이 음수입니다.";
+    private static final String NEXT_LINE = "\n";
 
-    public static int readNumber() {
+    private static int readNumber() {
         return sc.nextInt();
     }
 
-    public static String readString() {
+    private static int readNumberWithMsg(String msg) {
+        OutputView.printMassage(msg);
+        return readNumber();
+    }
+
+    private static String readLine() {
         return sc.nextLine();
+    }
+
+    private static String readLineWithMsg(String msg) {
+        OutputView.printMassage(msg);
+        return readLine();
+    }
+
+    public static String inputCarNames() {
+        return readLineWithMsg(INPUT_CAR_NAME_MSG + NEXT_LINE);
+    }
+
+    public static int inputNTrials() {
+        try {
+            return validNTrial(readNumberWithMsg(INPUT_N_TRIAL_MSG + NEXT_LINE));
+        } catch (IllegalArgumentException e) {
+            OutputView.printMassage(e.getMessage());
+            return inputNTrials();
+        } catch (InputMismatchException ie) {
+            OutputView.printMassage(INPUT_MISMATCH_MSG + NEXT_LINE);
+            return inputNTrials();
+        }
+    }
+
+    private static int validNTrial(int nTrial) {
+        if (nTrial < 0) throw new IllegalArgumentException(NEGATIVE_VALUE_MSG + NEXT_LINE);
+        return nTrial;
     }
 }
