369a3cb50b6e60cc63834e80324199be8c7938f7
huisam <huisam@naver.com>
2019-05-13 15:03:48 +0900

    [TEST] CarRaceResult에 기록된 내용을 확인하는 테스트 코드 추가

--- src/test/java/racinggame/controller/CarRaceResultTest.java
+++ src/test/java/racinggame/controller/CarRaceResultTest.java
@@ -1,8 +1,10 @@
 package racinggame.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racinggame.domain.Car;
 import racinggame.domain.LeagueHistory;
+import racinggame.domain.TrialHistory;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,14 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRaceResultTest {
+    private CarRacing carRacing;
+
+    @BeforeEach
+    void setUp() {
+        List<Car> cars = Arrays.asList(new Car("pobi", 2), new Car("crong", 2));
+        carRacing = new CarRacing(cars, 1);
+    }
+
     @Test
     void 객체가_제대로_생성되는지_테스트() {
         /* Given */
@@ -22,14 +32,22 @@ class CarRaceResultTest {
 
     @Test
     void CarRaceResult가_제대로_승자를_가져오는지_테스트() {
-        /* Given */
-        List<Car> cars = Arrays.asList(new Car("pobi", 2), new Car("crong", 2));
-        CarRacing carRacing = new CarRacing(cars, 1);
-
         /* When */
         CarRaceResult raceResult = carRacing.raceStart(new CarAlwaysRun());
 
         /* Then */
         assertThat(raceResult.getRaceWinners()).isEqualTo(Arrays.asList("pobi", "crong"));
+    }
+
+    @Test
+    void CarRaceResult에_제대로_기록을_가져오는지_테스트() {
+        /* When */
+        CarRaceResult raceResult = carRacing.raceStart(new CarAlwaysRun());
+
+        /* Then */
+        assertThat(raceResult.getRaceResult().contains(new TrialHistory(Arrays.asList(
+                new Car("pobi", 3), new Car("crong", 3))))).isTrue();
+        assertThat(raceResult.getRaceResult().contains(new TrialHistory(Arrays.asList(
+                new Car("pobi", 4), new Car("crong", 4))))).isFalse();
     }
 }
