45bfccd01cc1582a709551ed10ed76b54361e696
hyperpace <cnk9109@gmail.com>
2019-05-13 15:12:42 +0900

    feat(RacingCarApplication): Change service request method

--- src/main/java/RacingCarApplication.java
+++ src/main/java/RacingCarApplication.java
@@ -1,26 +1,23 @@
-import model.LapRecord;
-import model.Winners;
+import model.GameBoard;
 import service.RacingGameService;
 import view.InputView;
 import view.OutputView;
 
 public class RacingCarApplication {
     public static void main(String[] args) {
-        RacingGameService racingGameService = racingGameServiceGeneration();
-        LapRecord lapRecord = racingGameService.run();
-        Winners winners = racingGameService.getWinners();
-        OutputView.printGameResult(lapRecord);
-        OutputView.printWinners(winners);
+        GameBoard gameBoard = requestGame();
+        OutputView.printGameResult(gameBoard.getLapRecord());
+        OutputView.printWinners(gameBoard.getWinners());
     }
 
-    private static RacingGameService racingGameServiceGeneration() {
+    private static GameBoard requestGame() {
         try {
             String carNames = InputView.inputCarsNames();
             String lapCount = InputView.inputLapCount();
-            return new RacingGameService(carNames, lapCount);
+            return RacingGameService.request(carNames, lapCount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return racingGameServiceGeneration();
+            return requestGame();
         }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,12 +6,12 @@ import model.Winners;
 public class OutputView {
     private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다.";
 
-    public static void printWinners(Winners winners) {
-        System.out.println(String.format(WINNER_MESSAGE, winners.toString()));
-    }
-
     public static void printGameResult(LapRecord lapRecord){
         System.out.println("실행 결과");
         System.out.print(lapRecord.toString());
+    }
+
+    public static void printWinners(Winners winners) {
+        System.out.println(String.format(WINNER_MESSAGE, winners.toString()));
     }
 }
