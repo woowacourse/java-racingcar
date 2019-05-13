b7976ce4f116eda926f4ab8591d2a5b14ae1c175
yejilee <yjll1019@naver.com>
2019-05-13 14:43:56 +0900

    <자동차 경주 게임> 정규식 상수화

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 
 public class InputValidator {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String REGULAR_EXPRESSION_OF_ALPHABET = "[a-zA-Z]+";
+    private static final String REGULAR_EXPRESSION_OF_NUMBER = "[1-9][0-9]*";
 
     public static boolean validateAll(String carNames) {
         if (carNames.contains(",,")) {
@@ -27,7 +29,7 @@ public class InputValidator {
     }
 
     private static boolean isAlphabet(String carNames) {
-        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.matches("[a-zA-Z]+"));
+        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.matches(REGULAR_EXPRESSION_OF_ALPHABET));
     }
 
     private static boolean validateLength(String carNames) {
@@ -39,7 +41,7 @@ public class InputValidator {
     }
 
     public static boolean validateNaturalNumber(String numberOfTimes) {
-        if (!numberOfTimes.matches("[1-9]|[1-9][0-9]+")) {
+        if (!numberOfTimes.matches(REGULAR_EXPRESSION_OF_NUMBER)) {
             System.out.println("자연수가 아닙니다. 다시 입력해주세요!");
             return false;
         }
