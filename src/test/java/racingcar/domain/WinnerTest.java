a933c9f58661c506761d98ebccd49ac2048f52f1
yejilee <yjll1019@naver.com>
2019-05-13 15:13:19 +0900

    <자동차 경주 게임> [단위 테스트] 우승자가 여러 명일때 추가

--- src/test/java/racingcar/domain/WinnerTest.java
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -1,19 +1,37 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 
 public class WinnerTest {
+	private List<Car> cars;
+
+	@Before
+	public void init() {
+		cars = Arrays.asList(new Car("a"), new Car("b"), new Car("c"));
+		cars.get(0).moveOrNot(4);
+		cars.get(1).moveOrNot(4);
+		cars.get(1).moveOrNot(4);
+		cars.get(2).moveOrNot(4);
+	}
+
 	@Test
-	public void 우승자를_찾는다() {
-		List<Car> cars = new ArrayList<>(Arrays.asList(new Car("a", 1), new Car("b", 2), new Car("c", 3)));
-		assertTrue(new Winner(cars, 3).getWinner().get(0).equals("c"));
+	public void 우승자가_한명인_경우() {
+		Winner winner = new Winner(cars, 2);
+		assertTrue(winner.getWinner().equals(Arrays.asList(cars.get(1).toString())));
+	}
+
+	@Test
+	public void 우승자가_여러명인_경우() {
+		cars.get(2).moveOrNot(4);
+		Winner winner = new Winner(cars, 2);
+		assertTrue(winner.getWinner().equals(Arrays.asList(cars.get(1).toString(), cars.get(2).toString())));
 	}
 
 }
