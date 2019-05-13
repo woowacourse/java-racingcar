f19da38d21077db7bf289593f1674407884b5eab
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-13 20:32:34 +0900

    <자동차 경주 게임> 단일 책임 원칙 준수

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
-    private List<String> winnerNames;
+    private List<Car> cars;
+    private Car carWithMaxDistance;
 
     public Winner(List<Car> cars, Car carWithMaxDistance) {
-        winnerNames = new ArrayList<>();
+        this.cars = cars;
+        this.carWithMaxDistance = carWithMaxDistance;
+    }
 
+    public List<String> getWinnerNames() {
+        List<String> winnerNames = new ArrayList<>();
         for (int i = 0; i < cars.size(); ++i) {
             if (cars.get(i).isMaxPosition(carWithMaxDistance)) {
                 winnerNames.add(cars.get(i).getName());
             }
         }
-    }
 
-    public List<String> getWinnerNames() {
         return winnerNames;
     }
 }
