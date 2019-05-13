48227d73b150f6f2f3608a5a7387b03da65b88f9
stopsilver13 <stopsilver13>
2019-05-13 15:34:33 +0900

    [Review] Delete test code-judgeMove

--- src/test/java/CarRacingRuleTest.java
+++ src/test/java/CarRacingRuleTest.java
@@ -1,21 +1,11 @@
-import domain.Car;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarRacingRuleTest {
-    Car car;
-
     @Test
     void getRandomNumber() {
         assertThat((0 <= CarRacingRule.getRandomNumber())
                 && (CarRacingRule.getRandomNumber() <= 9)).isTrue();
-    }
-
-    @Test
-    void judgeMove() {
-        car = new Car("car");
-        assertThat(CarRacingRule.judgeMove(car, 4)).isEqualTo("Go");
-        assertThat(CarRacingRule.judgeMove(car, 3)).isEqualTo("Stop");
     }
 }
