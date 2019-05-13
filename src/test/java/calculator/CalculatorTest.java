9f4f778c215a5c9e1ee51ab9b1d54516e356eda6
je941004 <je941004@naver.com>
2019-05-13 15:24:19 +0900

    Refactor modify variable value cal -> calculator

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,46 +7,46 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
-    StringCalculator cal;
+    StringCalculator calculator;
 
     @BeforeEach
     void setUp() {
-        cal = new StringCalculator();
+        calculator = new StringCalculator();
     }
 
     @Test
     void 덧셈() {
-        int result = cal.calculate("2 + 3");
+        int result = calculator.calculate("2 + 3");
         assertThat(result).isEqualTo(5);
     }
 
     @Test
     void 뺄셈() {
-        int result = cal.calculate("3 - 2");
+        int result = calculator.calculate("3 - 2");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void 곱셈() {
-        int result = cal.calculate("3 * 2");
+        int result = calculator.calculate("3 * 2");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void 나눗셈() {
-        int result = cal.calculate("4 / 2");
+        int result = calculator.calculate("4 / 2");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     void 복합_계산() {
-        assertThat(cal.calculate("5 * 2 + 2")).isEqualTo(12);
-        assertThat(cal.calculate("12 + 3 * 2")).isEqualTo(30);
-        assertThat(cal.calculate("100 / 25 + 5")).isEqualTo(9);
+        assertThat(calculator.calculate("5 * 2 + 2")).isEqualTo(12);
+        assertThat(calculator.calculate("12 + 3 * 2")).isEqualTo(30);
+        assertThat(calculator.calculate("100 / 25 + 5")).isEqualTo(9);
     }
 
     @AfterEach
     void tearDown() {
-        cal = null;
+        calculator = null;
     }
 }
