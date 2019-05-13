aca29e35e2daef527feeda2fbffaf74633d924ee
choihz <coiio@naver.com>
2019-05-13 13:58:00 +0900

    [mod] 테스트 케이스별로 테스트 메소드 분리

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,22 +7,45 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
     @Test
-    public void Car_객체_생성시_예외가_제대로_발생하는지_확인한다() {
-        String userInput1 = "pobi";
-        String userInput2 = "abcdefg";
-        String userInput3 = "        ";
-        String userInput4 = "";
-        String userInput5 = null;
-
-        assertThatCode(() -> {new Car(userInput1);}).doesNotThrowAnyException();
-        assertThatThrownBy(() -> {new Car(userInput2);}).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> {new Car(userInput3);}).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> {new Car(userInput4);}).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> {new Car(userInput5);}).isInstanceOf(IllegalArgumentException.class);
+    public void Car_객체_생성이_잘_되는지_확인한다() {
+        String correctName = "pobi";
+        assertThatCode(() -> {new Car(correctName);}).doesNotThrowAnyException();
     }
 
     @Test
-    public void moveTest() {
+    public void 이름이_길_때_예외가_제대로_발생하는지_확인한다() {
+        String wrongName = "abcdefg";
+        assertThatThrownBy(() -> {new Car(wrongName);}).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 공백만_있을_때_예외가_제대로_발생하는지_확인한다() {
+        String wrongName = "        ";
+        assertThatThrownBy(() -> {new Car(wrongName);}).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 문자열이_비었을_때_예외가_제대로_발생하는지_확인한다() {
+        String wrongName = "";
+        assertThatThrownBy(() -> {new Car(wrongName);}).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 이름이_null일_때_예외가_제대로_발생하는지_확인한다() {
+        String wrongName = null;
+        assertThatThrownBy(() -> {new Car(wrongName);}).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 이동해야_할_때_이동하는지_확인한다() {
+        Car car = new Car("pobi");
+
+        car.move(4);
+        assertThat(car.matchPosition(1)).isEqualTo(true);
+    }
+
+    @Test
+    public void 이동하지_않아야_할_때_이동하지_않는지_확인한다() {
         Car car = new Car("pobi");
 
         car.move(0);
@@ -30,9 +53,6 @@ public class CarTest {
 
         car.move(3);
         assertThat(car.matchPosition(0)).isEqualTo(true);
-
-        car.move(4);
-        assertThat(car.matchPosition(1)).isEqualTo(true);
     }
 
     @Test
