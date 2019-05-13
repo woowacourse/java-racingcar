8e1495ac9c0fd2fc1cdd60aa00e71b7e5d4a8969
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-13 10:17:06 +0900

    <자동차 경주 게임> 패턴 상수화
    * InputValidator.java: 정규식을 이용한 패턴 문자열을 상수화하였다.

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 
 public class InputValidator {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String ALPHABET_REGULAR_EXPRESSION = "[a-zA-Z]+";
+    private static final String COMMA = ",";
+    private static final String NUMBER_REGULAR_EXPRESSION = "[1-9]|[1-9][0-9]+";
 
     public static boolean validateForm(String carNames) {
         if (carNames.contains(",,")) {
@@ -27,19 +30,19 @@ public class InputValidator {
     }
 
     private static boolean isAlphabet(String carNames) {
-        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.matches("[a-zA-Z]+"));
+        return Arrays.stream(carNames.split(COMMA)).allMatch(carName -> carName.matches(ALPHABET_REGULAR_EXPRESSION));
     }
 
     private static boolean validateLength(String carNames) {
-        return Arrays.stream(carNames.split(",")).allMatch(carName -> carName.length() <= MAX_NAME_LENGTH);
+        return Arrays.stream(carNames.split(COMMA)).allMatch(carName -> carName.length() <= MAX_NAME_LENGTH);
     }
 
     private static boolean overlapNames(String carNames) {
-        return ((carNames.split(",").length) == (new HashSet<>(Arrays.asList(carNames.split(","))).size()));
+        return ((carNames.split(COMMA).length) == (new HashSet<>(Arrays.asList(carNames.split(COMMA))).size()));
     }
 
     public static boolean validateNaturalNumber(String numberOfTimes) {
-        if (!numberOfTimes.matches("[1-9]|[1-9][0-9]+")) {
+        if (!numberOfTimes.matches(NUMBER_REGULAR_EXPRESSION)) {
             System.out.println("자연수가 아닙니다. 다시 입력해주세요!");
             return false;
         }
