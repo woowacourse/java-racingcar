7e14b85dd8ae7d61d861b067fabe761a9159f7b1
wiseoldster <wiseoldster@gmail.com>
2019-05-13 15:56:24 +0900

    미사용 메소드 제거 : findMaxVal2

--- src/main/java/domain/MaxFinder.java
+++ src/main/java/domain/MaxFinder.java
@@ -6,15 +6,6 @@ import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class MaxFinder {
-    public static <T> ArrayList<T> findAllMax2(ArrayList<T> values, Comparator<T> comp) {
-        ArrayList<T> maxValues = new ArrayList<>();
-        T maxVal = values.get(0);
-        for (T value : values) {
-            maxVal = checkNewIsMax(comp, maxValues, maxVal, value);
-        }
-        return maxValues;
-    }
-
     private static <T> T checkNewIsMax(Comparator<T> comp, ArrayList<T> maxValues, T maxVal, T newValue) {
         if (comp.compare(newValue, maxVal) == 0) {
             maxValues.add(newValue);
