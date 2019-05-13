2074c307a010fb21144e223d75794839dec4a8e6
김성훈 <seonghun127@gmail.com>
2019-05-13 15:42:57 +0900

    코드 포맷팅

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -11,7 +11,6 @@ public class RacingController {
 
     public static void main(String[] args) {
         Race race = setRace();
-
         OutputView.outputGameResultTile();
         while (race.hasNextRound()) {
             printEachCarsOnRace(race.moveAllCarsByRoundCount());
