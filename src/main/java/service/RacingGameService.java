3f92651fd9cd7119af7835fdb6383a2402e34f86
hyperpace <cnk9109@gmail.com>
2019-05-13 15:11:14 +0900

    feat(RacingGameService): remove property and add request(), gameStart(), calculatorWinners()

--- src/main/java/service/RacingGameService.java
+++ src/main/java/service/RacingGameService.java
@@ -3,25 +3,28 @@ package service;
 import model.*;
 
 public class RacingGameService {
-    private Cars cars;
-    private LapCount lapCount;
+    public static GameBoard request(String carNames, String count) {
+        Cars cars = new Cars(carNames);
+        LapCount lapCount = new LapCount(count);
 
-    public RacingGameService(String carNames, String lapCount) {
-        this.cars = new Cars(carNames);
-        this.lapCount = new LapCount(lapCount);
+        LapRecord lapRecord = gameStart(cars, lapCount);
+        Winners winners = calculatorWinners(cars);
+
+        return new GameBoard(lapRecord, winners);
     }
 
-    public LapRecord run() {
+    private static LapRecord gameStart(Cars cars, LapCount lapCount) {
         LapRecord lapRecord = new LapRecord();
         Racing racing = new Racing(cars);
         for (int lapNo = 1; lapNo <= lapCount.getLapCount(); lapNo++) {
             racing.run();
             lapRecord.add(cars.copy());
         }
+
         return lapRecord;
     }
 
-    public Winners getWinners() {
+    private static Winners calculatorWinners(Cars cars) {
         GameResult gameResult = new GameResult();
         return gameResult.calculatorWinners(cars);
     }
