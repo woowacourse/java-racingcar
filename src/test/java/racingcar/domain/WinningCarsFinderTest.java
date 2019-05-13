13465edd70d39176ab3f2ed95473004cd540bd70
Seongmo Jeon <oeeen3@gmail.com>
2019-05-13 19:28:26 +0900

    refactor: 각 메소드 변수로 위치 변경,  isEqualTo에서 containsExactly로 수정

--- src/test/java/racingcar/domain/WinningCarsFinderTest.java
+++ src/test/java/racingcar/domain/WinningCarsFinderTest.java
@@ -8,24 +8,24 @@ import java.util.List;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class WinningCarsFinderTest {
-    WinningCarsFinder winningCarsFinder;
 
     @Test
     void 우승자_찾기() {
+        WinningCarsFinder winningCarsFinder;
         List<Car> cars = Arrays.asList(new Car("pobi", 5), new Car("brown", 4),
                 new Car("jun", 3), new Car("jason", 2));
 
         winningCarsFinder = new WinningCarsFinder(cars);
-        assertThat(winningCarsFinder.getWinningCars().get(0)).isEqualTo(new Car("pobi", 5));
+        assertThat(winningCarsFinder.getWinningCars()).containsExactly(new Car("pobi", 5));
     }
 
     @Test
     void 우승자가_여러명인_경우() {
+        WinningCarsFinder winningCarsFinder;
         List<Car> cars = Arrays.asList(new Car("pobi", 5), new Car("brown", 5),
                 new Car("jun", 3), new Car("jason", 2));
 
         winningCarsFinder = new WinningCarsFinder(cars);
-        assertThat(winningCarsFinder.getWinningCars().get(0)).isEqualTo(new Car("pobi", 5));
-        assertThat(winningCarsFinder.getWinningCars().get(1)).isEqualTo(new Car("brown", 5));
+        assertThat(winningCarsFinder.getWinningCars()).containsExactly(new Car("pobi", 5), new Car("brown", 5));
     }
 }
