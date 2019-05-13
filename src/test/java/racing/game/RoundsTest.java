943e5c27be1975d949f89995ad217eca66644d29
skygl <jupiter9501@gmal.com>
2019-05-13 21:30:33 +0900

    Rounds 클래스 테스트에서 Car의 위치와 이름을 인자로 받는 생성자를 이용하도록 변경

--- src/test/java/racing/game/RoundsTest.java
+++ src/test/java/racing/game/RoundsTest.java
@@ -14,9 +14,7 @@ class RoundsTest {
     void 챔피언이_하나일때_챔피언계산() {
         List<Car> cars;
         try {
-            cars = Arrays.asList(new Car[]{new Car("pobi"), new Car("jihoo"), new Car("jinwo")});
-            cars.get(0).move(4);
-            cars.get(0).move(4);
+            cars = Arrays.asList(new Car[]{new Car("pobi", 2), new Car("jihoo"), new Car("jinwo")});
             List<Car> winners = new ArrayList<>();
             winners.add(cars.get(0));
             assertThat(Rounds.calculateChampions(cars)).isEqualTo(winners);
@@ -28,9 +26,7 @@ class RoundsTest {
     @Test
     void 챔피언이_여러명일때_챔피언계산() throws Exception {
         List<Car> cars;
-        cars = Arrays.asList(new Car[]{new Car("pobi"), new Car("jihoo"), new Car("jinwo")});
-        cars.get(0).move(4);
-        cars.get(1).move(4);
+        cars = Arrays.asList(new Car[]{new Car("pobi", 1), new Car("jihoo", 1), new Car("jinwo")});
         List<Car> winners = new ArrayList<>();
         winners.add(cars.get(1));
         winners.add(cars.get(0));
