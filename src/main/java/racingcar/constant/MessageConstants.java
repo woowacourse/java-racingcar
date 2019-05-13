b85549551be72b7b6fa6dd2f49cf735f0b365b68
backlo <hjun4512@naver.com>
2019-05-13 13:57:57 +0900

    ADD : 출력 메세지 분리

---
+++ src/main/java/racingcar/constant/MessageConstants.java
@@ -0,0 +1,18 @@
+package racingcar.constant;
+
+public class MessageConstants {
+    public static final String RESULT = "실행결과";
+    public static final String RESULT_PRINT_HYPHEN_FORMAT = "%s : %s";
+    public static final String RESULT_PRINT_WINNER_FORMAT = "%s 가 최종 우승 했습니다.";
+
+    public static final String INPUT_CARNAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_ROUND = "시도할 회수는 몇회인가요?";
+
+    public static final String ERROR_EMPTY = "에러 : 아무것도 입력 안되었습니다.";
+    public static final String ERROR_OVERLENGTH = "에러 : 5글자 이하로만 써주세요.";
+    public static final String ERROR_PLAYER_NUMBER = "에러 : 플레이어를 2명이상 입력해주세요.";
+    public static final String ERROR_DUPLICATE_NAME = "에러 : 중복된 이름이 있습니다.";
+    public static final String ERROR_CONTAIN_EMPTY = "에러 : 중간에 공백이름이 있습니다.";
+    public static final String ERROR_MINUS_NUMBER = "에러 : 양의정수가 아닌 수가 입력되었습니다.";
+    public static final String ERROR_INPUT_CHAR = "에러 : 문자가 입력되었습니다.";
+}
