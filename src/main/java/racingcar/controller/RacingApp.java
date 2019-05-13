145cc4d06166ff0459d9a5f5109d35de364dc66c
school0bhy <gusud96@naver.com>
2019-05-14 03:05:02 +0900

    refactor : Race 객체 선언 부분 수정
    static 제거

--- src/main/java/racingcar/controller/RacingApp.java
+++ src/main/java/racingcar/controller/RacingApp.java
@@ -8,23 +8,18 @@ import racingcar.view.OutputView;
 import java.util.List;
 
 public class RacingApp {
-    private static Race race;
-
     public static void main(String[] args) {
-        while (!initRace()) ;
+        Race race = null;
+        while (race == null) {
+            try {
+                race = new Race(InputView.inputCarNames());
+            } catch (IllegalArgumentException e) {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
         List<RoundResult> raceResult = race.startRace(getNumOfRounds());
         OutputView.printRaceResult(raceResult);
         OutputView.printWinners(raceResult.get(raceResult.size() - 1).getWinners());
-    }
-
-    private static boolean initRace() {
-        try {
-            race = new Race(InputView.inputCarNames());
-            return true;
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다.");
-            return false;
-        }
     }
 
     private static int getNumOfRounds() {
