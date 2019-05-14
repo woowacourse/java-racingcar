235b0aff0679e684eb61c46f3bb24e679a3b25e7
Kwanwoo Kang <kkw200@naver.com>
2019-05-14 17:57:35 +0900

    <REFACTOR> Deleted legacy files.

--- src/main/java/racingcar/domain/RaceResult.java
+++ src/main/java/racingcar/domain/RaceResult.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class RaceResult {
     Map<String, Integer> cars;
@@ -24,9 +23,11 @@ public class RaceResult {
     public Map<String, Integer> getWinners() {
         int positionOfWinners = Collections.max(cars.values());
         return Collections.unmodifiableMap(
-                cars.entrySet().stream()
-                    .filter(x -> x.getValue() == positionOfWinners)
-                    .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()))
+            new LinkedHashMap<String, Integer>() {{
+                    cars.entrySet().stream()
+                        .filter(car -> car.getValue() == positionOfWinners)
+                        .forEach(car -> put(car.getKey(), car.getValue()));
+            }}
         );
     }
 }
