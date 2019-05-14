1352c946848807efd0bbaafc9bccd9aeafea5470
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-14 09:04:56 +0900

    <자동차 경주 게임> 예외 처리 테스트 assertThrows로 변경

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -8,34 +8,60 @@ class InputValidatorTest {
 
     @Test
     void 자동차_이름이_6글자_이상인_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abcde,abcdef"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("abc,abcd,abcde,abcdef");
+        });
     }
 
     @Test
     void 자동차_이름에_중복된_쉼표가_있는_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abcde,,abcde"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("abc,abcd,abcde,,abcde");
+        });
     }
 
     @Test
     void 자동차_이름에_알파벳이_아닌_문자가_포함된_경우() {
-        assertFalse(InputValidator.validateForm(" "));
-        assertFalse(InputValidator.validateForm(""));
-        assertFalse(InputValidator.validateForm("abc#,abcd,abcde,abcd1"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm(" ");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("abc#,abcd,abcde,abcd1");
+        });
     }
 
     @Test
     void 자동차_이름에_중복된_이름이_있는_경우() {
-        assertFalse(InputValidator.validateForm("abc,abcd,abc,,abcde"));
-        assertFalse(InputValidator.validateForm("abc,abcd,abc,abcde"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("abc,abcd,abc,,abcde");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateForm("abc,abcd,abc,abcde");
+        });
     }
 
     @Test
     void 시도_횟수가_자연수가_아닌_경우() {
-        assertFalse(InputValidator.validateNaturalNumber(""));
-        assertFalse(InputValidator.validateNaturalNumber(" "));
-        assertFalse(InputValidator.validateNaturalNumber("0"));
-        assertFalse(InputValidator.validateNaturalNumber("01"));
-        assertFalse(InputValidator.validateNaturalNumber("-1"));
-        assertFalse(InputValidator.validateNaturalNumber("sadfsf"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber("");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber(" ");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber("0");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber("01");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber("-1");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateNaturalNumber("sadfsf");
+        });
     }
 }
