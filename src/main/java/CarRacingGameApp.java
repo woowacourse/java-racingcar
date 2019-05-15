a232cf701301ab5ea952c440c424c485a827450e
stopsilver13 <stopsilver13>
2019-05-15 09:56:05 +0900

    [Review2] Change main Class name RunCarRacingGame to CarRacingGameApp

--- src/main/java/CarRacingGameApp.java
+++ src/main/java/CarRacingGameApp.java
@@ -4,7 +4,7 @@ import view.OutputView;
 
 import java.util.List;
 
-public class RunCarRacingGame {
+public class CarRacingGameApp {
     public static void main(String[] args) {
         List<String> names = InputView.inputCarNames();
         List<Car> cars = CarRacingGame.readyCarForRacing(names);
