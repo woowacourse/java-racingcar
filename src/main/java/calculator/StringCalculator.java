99bbf45ea5c31803b9b98c3456487d9341707886
Jo Jinsik <kunggom@gmail.com>
2019-05-15 16:01:26 +0900

    수정: StringCalculator 클래스에 enum을 사용.
    "이펙티브 자바 [3판]"(인사이트, 2018) p. 214~216의 코드를 가져와 사용함.
    덤으로 Optional과 같은 개념에 대해서도 알게 되었다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,30 +1,29 @@
 package calculator;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class StringCalculator {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String DELIMITER = " ";
     private static final String STRING_INPUT_MESSAGE
             = "사칙연산 공식을 입력해 주십시오 :";
+    private int result;
 
-    private Map<String, Operation> operationMap = new HashMap<>();
-
-    public StringCalculator() {
-        operationMap.put("+", new Add());
-        operationMap.put("-", new Subtract());
-        operationMap.put("*", new Multiply());
-        operationMap.put("/", new Divide());
+    public static void main(String[] args) {
+        StringCalculator calc = new StringCalculator();
+        System.out.println(calc.run());
     }
 
-    public void run(){
+    public int run() {
         String formula = inputFormula();
         String[] inputData = formula.split(DELIMITER);
-        int result = calculate(inputData);
-
-        System.out.println(result);
+        result = calculate(inputData);
+        return result;
     }
 
     private String inputFormula() {
@@ -35,16 +34,52 @@ public class StringCalculator {
     public int calculate(String[] formula) {
         int result = Integer.parseInt(formula[0]);
 
-        for(int i = 1, len = formula.length; i < len; i += 2){
+        for (int i = 1, len = formula.length; i < len; i += 2) {
             int value = Integer.parseInt(formula[i + 1]);
-            result = operationMap.get(formula[i]).calculate(result, value);
+            Operation op = Operation.fromString(formula[i]).orElseThrow();
+            result = op.apply(result, value);
         }
 
         return result;
     }
 
-    public static void main(String[] args) {
-        StringCalculator calc = new StringCalculator();
-        calc.run();
+    @Override
+    public String toString() {
+        return Integer.toString(this.result);
+    }
+
+    // "이펙티브 자바 [3판]"(인사이트, 2018) p. 214~216의 코드를 가져옴.
+    public enum Operation {
+        PLUS("+") {
+            public int apply(int x, int y) { return x + y; }
+        },
+        MINUS("-") {
+            public int apply(int x, int y) { return x - y; }
+        },
+        TIMES("*") {
+            public int apply(int x, int y) { return x * y; }
+        },
+        DIVIDE("/") {
+            public int apply(int x, int y) { return x / y; }
+        };
+
+        private static final Map<String, Operation> stringToEnum
+                = Stream.of(values()).collect(toMap(Object::toString, e -> e));
+        private final String symbol;
+
+        Operation(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public static Optional<Operation> fromString(String symbol) {
+            return Optional.ofNullable(stringToEnum.get(symbol));
+        }
+
+        public abstract int apply(int x, int y);
+
+        @Override
+        public String toString() {
+            return symbol;
+        }
     }
 }
