373e9482b77643a1828df5750e109f757cf911f5
Ko eunsuk <eunsukko.128@gmail.com>
2019-06-22 15:05:11 +0900

    MoveCondition 을 RandomMoveStrategy 내부로 이동

--- src/main/java/domain/RandomMoveStrategy.java
+++ src/main/java/domain/RandomMoveStrategy.java
@@ -9,8 +9,30 @@ public class RandomMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove() {
-        int num = RANDOM.nextInt(MoveCondition.NUM_TO + 1);
+        int num = RANDOM.nextInt(RandomMoveCondition.NUM_TO + 1);
 
-        return MoveCondition.canMove(num);
+        return RandomMoveCondition.canMove(num);
+    }
+}
+
+
+class RandomMoveCondition {
+    // [NUM_START, NUM_END]
+    public static final int NUM_FROM = 0;
+    public static final int NUM_MOVE_FROM = 4;
+    public static final int NUM_TO = 9;
+
+    public static boolean canMove(int num) {
+        checkNum(num);
+
+        return NUM_MOVE_FROM <= num;
+    }
+
+    private static void checkNum(int num) {
+        if (num < NUM_FROM || NUM_TO < num) {
+            throw new IndexOutOfBoundsException(
+                    String.format("num 은 [%d, %d] 범위 내의 정수이여야 합니다.", NUM_FROM, NUM_TO)
+            );
+        }
     }
 }
--- src/test/java/domain/RandomMoveStrategyTest.java
+++ src/test/java/domain/RandomMoveStrategyTest.java
@@ -5,27 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MoveConditionTest {
-
+class RandomMoveStrategyTest {
     @Test
     void canMove_NUM_FROM보다작은경우() {
         assertThrows(IndexOutOfBoundsException.class,
-                () -> MoveCondition.canMove(MoveCondition.NUM_FROM - 1));
+                () -> RandomMoveCondition.canMove(RandomMoveCondition.NUM_FROM - 1));
     }
 
     @Test
     void canMove_NUM_TO보다큰경우() {
         assertThrows(IndexOutOfBoundsException.class,
-                () -> MoveCondition.canMove(MoveCondition.NUM_TO + 1));
+                () -> RandomMoveCondition.canMove(RandomMoveCondition.NUM_TO + 1));
     }
 
     @Test
     void canMove_NUM_MOVE_FROM() {
-        assertThat(MoveCondition.canMove(MoveCondition.NUM_MOVE_FROM)).isTrue();
+        assertThat(RandomMoveCondition.canMove(RandomMoveCondition.NUM_MOVE_FROM)).isTrue();
     }
 
     @Test
     void canMove_움직이지않을경우() {
-        assertThat(MoveCondition.canMove(MoveCondition.NUM_MOVE_FROM -  1)).isFalse();
+        assertThat(RandomMoveCondition.canMove(RandomMoveCondition.NUM_MOVE_FROM -  1)).isFalse();
     }
+
 }
