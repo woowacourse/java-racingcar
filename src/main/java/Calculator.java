7394baf030e18b3979005a970c5aa4ec5e6aebc1
icyMojito <dev.icymojito@gmail.com>
2020-02-11 13:59:39 +0900

    [feat] add 콤마, 콜론  구분자 기능
    콤마와 콜론 모두 구분자로 지정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,14 +2,22 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public static final String DELIMITER = ",";
+    public static final String COMMA = ",";
+    public static final String COLUMN = ":";
+    public static final String OR_OPERATOR = "|";
 
     public static int calculate(String input) {
-        String[] splitedInput = input.split(DELIMITER);
-        int result = 0;
-        for (String character : splitedInput) {
-            result += Integer.parseInt(character);
-        }
+        String[] splitedInputs = split(input);
+
+        int result = Arrays.stream(splitedInputs)
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .get();
+
         return result;
+    }
+
+    public static String[] split(String input) {
+        return input.split(COMMA + OR_OPERATOR + COLUMN);
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,8 +4,15 @@ public class Test {
 
     @org.junit.jupiter.api.Test
     void 덧셈() {
-        String input = "1,2,3,4";
+        String input = "1,2:3,4";
         int result = Calculator.calculate(input);
         assertThat(result).isEqualTo(10);
+    }
+
+    @org.junit.jupiter.api.Test
+    void 문자열_나누기() {
+        String input = "55:341,8";
+        String[] splitedInput = Calculator.split(input);
+        assertThat(splitedInput).isEqualTo(new String[]{"55", "341", "8"});
     }
 }
