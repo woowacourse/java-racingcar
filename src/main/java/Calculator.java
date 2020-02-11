bed34b70ad40661fa1de704f367929acbf410e46
giantim <giantim132@gmail.com>
2020-02-11 12:04:45 +0900

    입력된 문자열을 ,로 분리하는 기능 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,8 @@ public class Calculator {
     public static int calculation(int firstOperand, int secondOperand) {
         return firstOperand + secondOperand;
     }
+
+    public static String[] splitString(String s) {
+        return s.split(",");
+    }
 }
--- src/test/java/AddCalculatorTest.java
+++ src/test/java/AddCalculatorTest.java
@@ -9,5 +9,12 @@ public class AddCalculatorTest {
         int result = Calculator.calculation(2, 3);
         Assertions.assertThat(result).isEqualTo(5);
     }
+    @Test
+    @DisplayName("문자열이 들어오면 쉼표로 구분하여 배열로 변환한다.")
+    void string_Split_Test() {
+        String[] strArrays = Calculator.splitString("1,2");
+        String[] expected = {"1","2"};
+        Assertions.assertThat(strArrays).isEqualTo(expected);
+    }
 
 }
