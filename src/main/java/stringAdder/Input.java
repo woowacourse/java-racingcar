dfe2f5d18e65884b774910614712007065ab3e6a
gunhee lee <dlrjsgml42@naver.com>
2020-02-11 17:57:45 +0900

    feat: 입력 처리를 위한 InputView, Input 클래스
    InputView 입력을 위한 화면 출력과 Scanner 입력 연산 구현
    Input 입력받은 문자열을 저장, 검증, split해서 리스트로 반환하는 기능 구현

---
+++ src/main/java/stringAdder/Input.java
@@ -0,0 +1,52 @@
+package stringAdder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Input {
+    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+
+    private String input;
+
+    public Input(){
+        this.input = InputView.inputValue();
+    }
+
+    public Input(String value){
+        this.input = value;
+    }
+
+    private boolean isInputEmpty(){
+        return input == null || input.isEmpty();
+
+    }
+
+    private String getDelimiter(){
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
+        if(m.find()){
+            input = m.group(2);
+            return m.group(1);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    public List<String> splitInput() {
+        try {
+            String delimiter = getDelimiter();
+            List<String> list = Arrays.asList(input.split(delimiter));
+
+            if(isInputEmpty() || list.isEmpty()){
+                return null;
+            }
+
+            return list;
+        } catch(NullPointerException e){
+            System.out.println("빈 문자열을 입력할 수 없습니다.");
+            return null;
+        }
+    }
+
+}
