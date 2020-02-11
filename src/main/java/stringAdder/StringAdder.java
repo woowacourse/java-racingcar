ef0d1e2b7407b46e8721adca6eaf1b030e4cd5fd
gunhee lee <dlrjsgml42@naver.com>
2020-02-11 17:59:33 +0900

    refactor: StringAdder 리팩토링
    StringAdder 구조 개선 및 Input/InputView 사용을 위하여 리팩토링 작업을 수행

--- src/main/java/stringAdder/StringAdder.java
+++ src/main/java/stringAdder/StringAdder.java
@@ -1,28 +1,18 @@
 package stringAdder;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAdder {
     public static int add(String value) throws RuntimeException {
-        if (value == null || value.isEmpty()) {
+        Input input = new Input(value);
+        int result = 0;
+        List<String> values = input.splitInput();
+
+        if(values == null){
             return 0;
         }
-        int result = 0;
-        String customDelimiter = (",|:");
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(value);
-        if (m.find()) {
-            customDelimiter = m.group(1);
-            if (m.group(2) == null || m.group(2).isEmpty()) {
-                return 0;
-            }
-            String[] tokens = m.group(2).split(customDelimiter);
-            for (String token : tokens) {
-                result += Integer.parseInt(token);
-            }
-            return result;
-        }
-        String[] values = value.split(customDelimiter);
         for (String s : values) {
             try {
                 int operand = Integer.parseInt(s);
