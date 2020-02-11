b48ac93b4c8a4dabcd12aad8c66ef5fd2bcd681f
hwangbo <taekwonod714@naver.com>
2020-02-11 16:52:35 +0900

    refactor: 프로덕션 코드 리팩토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,39 +4,60 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
+
+    public static final String REGEX = "//(.)\n(.*)";
+    public static final int SINGLE_NUMBER = 1;
+    public static final String NEGATIVE_SYMBOL = "-";
+    public static final String OR_SYMBOL = "|";
+
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
+        if (isNullOrEmpty(input)) {
             return 0;
         }
-        int result = 0;
-        String delimiter = ",|:";
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            delimiter += "|" + customDelimiter;
-            input = m.group(2);
-        }
-        String[] inputs = input.split(delimiter);
-
-        for(String value : inputs) {
-            if (value.contains("-")) {
-                throw new RuntimeException("음수는 포함할 수 없습니다.");
-            }
-        }
-        if (inputs.length == 1) {
+        String[] inputs = splitByDelimiter(input);
+        if (inputs.length == SINGLE_NUMBER) {
+            isContainNegative(inputs[0]);
             return Integer.parseInt(inputs[0]);
         }
+        return sumNumbers(inputs);
+    }
+
+    private static String[] splitByDelimiter(String input) {
+        String delimiter = ",|:";
+        Matcher m = Pattern.compile(REGEX).matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            delimiter += OR_SYMBOL + customDelimiter;
+            input = m.group(2);
+        }
+        return input.split(delimiter);
+    }
+
+    private static int sumNumbers(String[] inputs) {
+        int result = 0;
         for (String value : inputs) {
-            try {
-                result += Integer.parseInt(value);
-            } catch(IllegalArgumentException e){
-                throw new IllegalArgumentException("지정된 구분자가 아닙니다.");
-            }
+            isContainNegative(value);
+            int number = checkDelimiter(value);
+            result += number;
         }
         return result;
     }
 
-    void checkInteger(String a){
+    private static int checkDelimiter(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("지정된 구분자가 아닙니다.");
+        }
+    }
 
+    private static void isContainNegative(String value) {
+        if (value.contains(NEGATIVE_SYMBOL)) {
+            throw new RuntimeException("음수는 포함할 수 없습니다.");
+        }
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 }
