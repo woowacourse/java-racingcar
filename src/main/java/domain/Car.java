7aeddcc7518e76c9bbf6ed5752415aa5788c0cba
moonyoungCHAE <xpf_fl@naver.com>
2020-02-11 17:27:04 +0900

    feat / test: 자동차 전진 기능 구현 및 테스트

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,13 +3,23 @@ package domain;
 public class Car {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_MOVABLE_NUM = 4;
 
     private final String name;
+    private int position;
 
     public Car(String name) {
+    private void validateName(String name) {
         if (!(name.length() >= MIN_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH)) {
             throw new IllegalArgumentException("이름 길이는 0~5이여야 합니다.");
         }
-        this.name = name;
+    }
+
+    public boolean move(int randomNumber) {
+        if (randomNumber >= MIN_MOVABLE_NUM) {
+            position++;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,9 +1,11 @@
 package domain;
 
 import calculator.StringCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class CarTest {
     @Test
@@ -15,5 +17,17 @@ public class CarTest {
     void createCar_부적합한_이름길이() {
         assertThatThrownBy(() ->  new Car("우아한형제들"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isMove_전진_성공() {
+        Car car = new Car("타미");
+        assertThat(car.move(4)).isTrue();
+    }
+
+    @Test
+    void isMove_전진_실패() {
+        Car car = new Car("타미");
+        assertThat(car.move(0)).isFalse();
     }
 }
