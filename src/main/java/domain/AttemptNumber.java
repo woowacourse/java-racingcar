e2ed978ddae82b9a006570d6e959f4b7b6f33f99
moonyoungCHAE <xpf_fl@naver.com>
2020-02-11 15:42:18 +0900

    feat: 시도 횟수를 저장할 객체 구현

---
+++ src/main/java/domain/AttemptNumber.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class AttemptNumber {
+    private static final String NOT_INTEGER_MESSAGE = "정수가 아닙니다.";
+    private static final String NOT_POSITIVE_MESSAGE = "양수가 아닙니다.";
+    private static final int MIN_ATTEMPT_NUMBER = 1;
+
+    private final int number;
+
+    public AttemptNumber(String number) {
+        validateInteger(number);
+        validatePositive(number);
+        this.number = Integer.parseInt(number);
+    }
+
+    public void validateInteger(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
+        }
+    }
+
+    public void validatePositive(String number) {
+        if (Integer.parseInt(number) < MIN_ATTEMPT_NUMBER) {
+            throw new IllegalArgumentException(NOT_POSITIVE_MESSAGE);
+        }
+    }
+}
