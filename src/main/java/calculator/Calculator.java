e6c0000205ff708346dcf34ccf3bdaef36b28a27
K.S.KIM <nullable@kakao.com>
2020-02-11 16:47:49 +0900

    refactor : 수식 split과 연산 로직 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,29 +1,65 @@
 package calculator;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
- *
+ * 계산 클래스
  *
  * @version 1.0.0
  * @author K.S.KIM
+ * @author HyoChan
  * @since 2020/02/11
  */
 public class Calculator {
-	public static final String DELIMETER = ",";
-	public static final String CUSTOM_DELIMETER_PREFIX = "//";
-	public static final String CUSTOM_DELIMETER_POSTFIX = "\n";
+	public static final String DELIMITER = "[,:]";
+	public static final String CUSTOM_DELIMITER_PREFIX = "//";
+	public static final String CUSTOM_DELIMITER_POSTFIX = "\n";
+	public static final int DELIMITER_POSITION = 0;
+	public static final int EXPRESSION_POSITION = 1;
+	public static final String CUSTOM_DELIMITER_PATTERN = "^(\\/\\/.\\n)?\\d+(.\\d+)*\\b";
+	public static final String NUMBER_LIST_IS_EMPTY_MESSAGE = "숫자가 한 개 이상 주어져아 합니다.";
+	public static final String INVALID_INPUT_MESSAGE = "입력이 유효하지 않습니다.";
 
-	public static int calculate(String input) {
-		String delimeter = DELIMETER;
-		String[] splitInput = input.split(delimeter);
-		if (input.contains(CUSTOM_DELIMETER_POSTFIX)&&input.contains(CUSTOM_DELIMETER_PREFIX)){
-			String[] splitDelimiter = input.split(CUSTOM_DELIMETER_POSTFIX);
-			delimeter = String.valueOf(splitDelimiter[0].charAt(splitDelimiter[0].length() - 1));
-			splitInput = splitDelimiter[1].split(delimeter);
+	public static int calculate(final List<Integer> numbers) {
+		validateNumbers(numbers);
+		return numbers.stream()
+				.mapToInt(value -> value)
+				.sum();
+	}
+
+	private static void validateNumbers(final List<Integer> numbers) {
+		Objects.requireNonNull(numbers);
+		if (numbers.isEmpty()) {
+			throw new IllegalArgumentException(NUMBER_LIST_IS_EMPTY_MESSAGE);
 		}
-		int result = 0;
-		for (String operand: splitInput) {
-			result += Integer.parseInt(operand);
+	}
+
+	public static List<Integer> splitByDelimiter(final String input) {
+		validateInput(input);
+		if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+			String[] splitDelimiter = input.split(CUSTOM_DELIMITER_POSTFIX);
+			int delimiterIndex = splitDelimiter[DELIMITER_POSITION].length() - 1;
+			String delimiter = String.valueOf(splitDelimiter[DELIMITER_POSITION].charAt(delimiterIndex));
+			String expression = splitDelimiter[EXPRESSION_POSITION];
+			return splitByDelimiter(expression, delimiter);
 		}
-		return result;
+		return splitByDelimiter(input, DELIMITER);
+	}
+
+	private static void validateInput(String input) {
+		if (!Pattern.matches(CUSTOM_DELIMITER_PATTERN, input)) {
+			throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+		}
+	}
+
+	private static List<Integer> splitByDelimiter(final String input, final String delimiter)
+			throws NumberFormatException {
+		return Stream.of(input.split(delimiter))
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
 	}
 }
