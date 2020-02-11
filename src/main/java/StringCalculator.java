dec5bf3236677098e2f5e1a26b7126a236bd814f
junk-sound <wom2277@naver.com>
2020-02-11 17:03:15 +0900

    refac : Removed fixed return value and applied plus logic according to input sentence

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,9 @@
 public class StringCalculator {
     public static int add(String s) {
-        return 3;
+        String[] words = s.split(",");
+        int firstNumber = Integer.parseInt(words[0]);
+        int secondNumber = Integer.parseInt(words[1]);
+
+        return firstNumber + secondNumber;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
-    void add() {
-        int result = StringCalculator.add("1 + 2");
+    void 덧셈_쉼표_여러구분자() {
+        int result = StringCalculator.add("1,2");
         assertThat(result).isEqualTo(3);
+
+        result = StringCalculator.add("4,5");
+        assertThat(result).isEqualTo(9);
     }
 }
