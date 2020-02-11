4e128da51ace24170fba4c632f9e4a4ea08384b5
kouz <lastpuzly@gmail.com>
2020-02-11 17:03:10 +0900

    feat: 횟수 유효성 검사 구현
    입력값이 1이상인지 확인한다.

--- src/main/java/tdd/racingcar/Round.java
+++ src/main/java/tdd/racingcar/Round.java
@@ -1,20 +1,29 @@
 package tdd.racingcar;
 
 public class Round {
+	private static final int ONE = 1;
+
 	private int round;
 
-	public Round(String value) {
+	public Round(final String value) {
 		checkValidationFor(value);
 		round = Integer.parseInt(value);
 	}
 
-	private void checkValidationFor(String value) {
+	private void checkValidationFor(final String value) {
 		if (isNotNumber(value)) {
 			throw new IllegalArgumentException("횟수는 숫자이어야 합니다.");
 		}
+		if (isUnderOne(value)) {
+			throw new IllegalArgumentException("횟수는 1이상 이어야 합니다.");
+		}
 	}
 
-	private boolean isNotNumber(String value) {
+	private boolean isNotNumber(final String value) {
 		return value.chars().anyMatch(ch -> !Character.isDigit(ch));
+	}
+
+	private boolean isUnderOne(final String value) {
+		return Integer.parseInt(value) < ONE;
 	}
 }
--- src/test/java/tdd/racingcar/RoundTest.java
+++ src/test/java/tdd/racingcar/RoundTest.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.api.Test;
 
 public class RoundTest {
 	private static final String NOT_NUMBER_INPUT = "A";
+	private static final String ZERO_INPUT = "0";
 
 	@Test
 	void canValidateRoundForNotNumber() {
 		assertThatIllegalArgumentException().isThrownBy(() ->
 			new Round(NOT_NUMBER_INPUT))
 			.withMessage("횟수는 숫자이어야 합니다.");
+	}
+
+	@Test
+	void canValidateRoundForZero() {
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			new Round(ZERO_INPUT))
+			.withMessage("횟수는 1이상 이어야 합니다.");
 	}
 }
