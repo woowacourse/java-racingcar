2b2d9beb132395a83f11758d7c327885e6c508cd
kouz <lastpuzly@gmail.com>
2020-02-11 12:06:42 +0900

    test: plusTwoNumberSplitByComma 메소드 테스트 작성

---
+++ src/test/java/PlusCalculatorTest.java
@@ -0,0 +1,13 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PlusCalculatorTest {
+	@Test
+	void plusTwoNumberSplitByComma() {
+		final String value = "1,2";
+		final int actual = PlusCalculator.calculate(value);
+		final int expected = 3;
+		assertThat(actual).isEqualTo(expected);
+	}
+}
