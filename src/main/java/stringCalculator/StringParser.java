4b43258066df64aac467d34228154b2c484970b0
chws <rebeccachoosir@gmail.com>
2020-02-11 18:56:04 +0900

    refactor: StringParser에서 리턴 형식 변경
     - List<String> -> List<Integer>

--- src/main/java/stringCalculator/StringParser.java
+++ src/main/java/stringCalculator/StringParser.java
@@ -2,6 +2,7 @@ package stringCalculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringParser {
     public static final String DEFAULT_DELIMITER_REGEX = ",|:";
@@ -12,22 +13,38 @@ public class StringParser {
     private StringParser() {
     }
 
-    public static List<String> parse(String input) {
+    public static List<Integer> parse(String input) {
         if (hasSign(input)) {
             return parseWithCustomDelimiter(input);
         }
 
-        return Arrays.asList(input.split(DEFAULT_DELIMITER_REGEX));
-
+        return splitStringListToIntegerList(input.split(DEFAULT_DELIMITER_REGEX));
     }
 
-    private static List<String> parseWithCustomDelimiter(String input) {
+    private static List<Integer> parseWithCustomDelimiter(String input) {
         int startOfCustomDelimiter = input.indexOf(FRONT_SIGN) + TWO_STEP;
         int endOfCustomDelimiter = input.indexOf(BACK_SIGN);
         String customDelimiter = input.substring(startOfCustomDelimiter, endOfCustomDelimiter);
         String[] result = input.substring(endOfCustomDelimiter + TWO_STEP, input.length())
-                            .split(DEFAULT_DELIMITER_REGEX + "|" + customDelimiter);
-        return Arrays.asList(result);
+                .split(DEFAULT_DELIMITER_REGEX + "|" + customDelimiter);
+        return splitStringListToIntegerList(result);
+    }
+
+    private static List<Integer> splitStringListToIntegerList(String[] split) {
+        if (split.equals(new String[]{""})) {
+            return Arrays.asList(new Integer[]{0});
+        }
+        // 여기에 예외 처리
+        try {
+            return Arrays.asList(split)
+                    .stream()
+                    .map((t) -> Integer.parseInt(t))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.err.println(Arrays.asList(split));
+            System.err.println(e.getMessage());
+            throw e;
+        }
     }
 
     private static boolean hasSign(String input) {
--- src/test/java/stringCalculator/StringParserTest.java
+++ src/test/java/stringCalculator/StringParserTest.java
@@ -2,16 +2,11 @@ package stringCalculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringParserTest {
     @Test
@@ -21,13 +16,13 @@ public class StringParserTest {
         String input2 = "1,2";
         String input3 = "1,2:3";
 
-        List<String> expected1 = Arrays.asList(new String[]{""});
-        List<String> expected2 = Arrays.asList(new String[]{"1","2"});
-        List<String> expected3 = Arrays.asList(new String[]{"1","2","3"});
+        List<Integer> expected1 = Arrays.asList(new Integer[]{0});
+        List<Integer> expected2 = Arrays.asList(new Integer[]{1, 2});
+        List<Integer> expected3 = Arrays.asList(new Integer[]{1, 2, 3});
 
-        List<String> result1 = StringParser.parse(input1);
-        List<String> result2 = StringParser.parse(input2);
-        List<String> result3 = StringParser.parse(input3);
+        List<Integer> result1 = StringParser.parse(input1);
+        List<Integer> result2 = StringParser.parse(input2);
+        List<Integer> result3 = StringParser.parse(input3);
 
         parseOneCase(input1, expected1, result1);
         parseOneCase(input2, expected2, result2);
@@ -38,13 +33,13 @@ public class StringParserTest {
     @DisplayName("커스텀 구분자로 문자열 파싱")
     void parse_WithCustomDelimiter() {
         String input = "//;\\n1;2;3";
-        List<String> expected = Arrays.asList(new String[]{"1", "2", "3"});
-        List<String> result = StringParser.parse(input);
+        List<Integer> expected = Arrays.asList(new Integer[]{1, 2, 3});
+        List<Integer> result = StringParser.parse(input);
 
         parseOneCase(input, expected, result);
     }
 
-    private void parseOneCase(String input, List<String> expected, List<String> result) {
+    private void parseOneCase(String input, List<Integer> expected, List<Integer> result) {
         assertThat(expected.equals(result)).isEqualTo(true);
     }
 
