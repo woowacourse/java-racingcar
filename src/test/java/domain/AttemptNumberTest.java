8236fbac4097a7f206d92e1fdea4f43ac1631a42
moonyoungCHAE <xpf_fl@naver.com>
2020-02-11 17:29:58 +0900

    test: 시도 횟수 테스트 코드 부적절한 경우 추가

--- src/test/java/domain/AttemptNumberTest.java
+++ src/test/java/domain/AttemptNumberTest.java
@@ -1,6 +1,9 @@
 package domain;
 
+import calculator.StringCalculator;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AttemptNumberTest {
     @Test
@@ -8,5 +11,15 @@ public class AttemptNumberTest {
         new AttemptNumber("1");
     }
 
-  
+    @Test
+    void createAttemptNumber_부적합한_횟수_문자() {
+        assertThatThrownBy(() -> new AttemptNumber("타미"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void createAttemptNumber_부적합한_횟수_음수() {
+        assertThatThrownBy(() -> new AttemptNumber("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
