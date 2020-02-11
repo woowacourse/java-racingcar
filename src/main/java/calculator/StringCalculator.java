ba078def735af4189280ec35f07d6b86f6e5acf8
YebinK <yebin825@gmail.com>
2020-02-11 15:56:58 +0900

    [FEATURE] 입력값이 null 또는 빈문자열인 경우 0을 반환 기능 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public static int splitAndSum(String text) {
+        int result = 0;
+        if(text == null || text.isEmpty()){
+            return 0;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    @DisplayName("null 또는 빈문자열을 입력할 경우 0 반환 테스트")
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+}
