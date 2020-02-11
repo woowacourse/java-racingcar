fd253a676ae33ab9a34f213214cb5f518e5e43ca
Andy H. Jung <commaniakr@gmail.com>
2020-02-11 17:21:22 +0900

    feat: splitAndSum split, 다항 연산 구현.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,15 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
-
     public static int splitAndSum(String expression) {
-
+        if(expression == null || expression.isEmpty()) {
+            return 0;
+        }
+        final String delimeter = ",|:";
+        List<String> numbers = Arrays.asList(expression.split(delimeter));
+        return numbers.stream().mapToInt(Integer::parseInt).sum();
     }
 }
