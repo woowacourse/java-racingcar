d0dcf92e8b461730cc47df11a30dccff5a07c665
kimjunseok <jnunseok@gmail.com>
2020-02-11 14:51:06 +0900

    [Add] 문자열 덧셈 계산기

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,65 @@
+package calculator;/*
+ * Copyright (c) 2019 by 이름
+ * All rights reserved.
+ *
+ * 클래스명.java
+ * 클래스 설명
+ *
+ * @author      이름
+ * @version     1.0
+ * @date        dd mm yyyy
+ *
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    private final static String REGEX_NOT_NUMBER = "[^0-9]";
+    private final static String DELIMITER = ",|:";
+    private final static String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+
+    public static int splitAndSum(String text) {
+        if (isNullOrBlank(text)) {
+            return 0;
+        }
+        return add(split(text));
+    }
+
+    private static boolean isNullOrBlank(String text) {
+        return text == null || text.equals("");
+    }
+
+    private static void notNaturalNumberException(String text) {
+        if (text.contains("-") || text.matches(REGEX_NOT_NUMBER)) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static int add(String[] inputs) {
+        int result = 0;
+        for (String inputA : inputs) {
+            notNaturalNumberException(inputA);
+            result += Integer.parseInt(inputA);
+        }
+        return result;
+    }
+
+    private static String[] split(String text) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = validateNoHyphen(matcher.group(1));
+            text = matcher.group(2);
+            return text.split(customDelimiter);
+        }
+        return text.split(DELIMITER);
+    }
+
+    private static String validateNoHyphen(String delimiter) {
+        if (delimiter.equals("-")) {
+            throw new RuntimeException("구분자로 -를 사용하실 수 없습니다.");
+        }
+        return delimiter;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2019 by 이름
+ * All rights reserved.
+ *
+ * 클래스명.java
+ * 클래스 설명
+ *
+ * @author      이름
+ * @version     1.0
+ * @date        dd mm yyyy
+ *
+ */
+
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringCalculatorTest {
+
+    @Test
+    void 빈문자열_또는_null_테스트() {
+        String input1 = null;
+        assertThat(StringCalculator.splitAndSum(input1)).isEqualTo(0);
+        String input2 = "";
+        assertThat(StringCalculator.splitAndSum(input2)).isEqualTo(0);
+    }
+
+    @Test
+    void 숫자_하나_문자열() {
+        String input = "1";
+        assertThat(StringCalculator.splitAndSum(input)).isEqualTo(1);
+    }
+
+    @Test
+    void 숫자_두개를_컴마_구분자로_입력() {
+        String input = "1,2";
+        assertThat(StringCalculator.splitAndSum(input)).isEqualTo(3);
+    }
+
+    @Test
+    void 콜론_구분자() {
+        String input = "1,2:3";
+        assertThat(StringCalculator.splitAndSum(input)).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자() {
+        String input = "//;\n1;2;3";
+        assertThat(StringCalculator.splitAndSum(input)).isEqualTo(6);
+    }
+
+    @Test
+    void 음수면_예외발생() {
+        String input = "-1,2,3";
+        assertThatThrownBy(() -> StringCalculator.splitAndSum(input))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 숫자_아니면_예외발생() {
+        String input = "a,b,3";
+        assertThatThrownBy(() -> StringCalculator.splitAndSum(input))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 딜리미터가_하이픈이면_예외발생() {
+        String input = "//-\n1;2;3";
+        assertThatThrownBy(() -> StringCalculator.splitAndSum(input))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("-");
+    }
+}
