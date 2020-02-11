ea382eec0b37140bf4a35b7f85819eb802292e30
begaonnuri <turtlenam0928@gmail.com>
2020-02-11 15:31:58 +0900

    refactor. 자동차의 이름을 검증하는 메소드를 private으로 변경
     - 숫자값을 상수로 변경
     - indent 수정

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,16 +1,25 @@
 package domain;
 
 public class Car {
-    public Car() {
+    public static final int ZERO = 0;
+    public static final int NAME_UPPER_LIMIT = 5;
+    private String name;
+
+    public Car(String name) {
+        validateNull(name);
+        validateName(name);
+        this.name = name;
     }
 
-    public void validateName(String name) {
-        if (name.length() <= 0 || name.length() > 5) {
+    private void validateName(String name) {
+        if (name.length() <= ZERO || name.length() > NAME_UPPER_LIMIT) {
             throw new IllegalArgumentException("이름은 1자 이상 5자 이하로 해주세요.");
         }
     }
 
-    public void validateNull(Object o) {
-        if (o==null) throw new IllegalArgumentException("이름은 null값이 될 수 없습니다.");
+    private void validateNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null값이 될 수 없습니다.");
+        }
     }
 }
