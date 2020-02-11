4b2747b3554eaba939f0ca863d32c8f49ff0c0d4
icyMojito <dev.icymojito@gmail.com>
2020-02-11 13:38:35 +0900

    [feat] add 덧셈 기능

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Calculator {
+
+    public static final String DELIMITER = ",";
+
+    public static int calculate(String input) {
+        String[] splitedInput = input.split(DELIMITER);
+        int result = 0;
+        for (String character : splitedInput) {
+            result += Integer.parseInt(character);
+        }
+        return result;
+    }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Test {
+
+    @org.junit.jupiter.api.Test
+    void 덧셈() {
+        String input = "1,2,3,4";
+        int result = Calculator.calculate(input);
+        assertThat(result).isEqualTo(10);
+    }
 }
