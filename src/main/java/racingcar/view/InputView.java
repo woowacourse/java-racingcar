aa846d0c6e07fa218dbe9953b7fbdff5169605f8
kimjunseok <jnunseok@gmail.com>
2020-02-11 19:58:38 +0900

    [Add] 입력값 받는 클래스

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 by 티거, 제이미
+ * All rights reserved.
+ *
+ * @author      티거, 제이미
+ * @version     1.0
+ * @date        11 Feb 2020
+ *
+ */
+
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public String inputCarNames() {
+        return scanner.nextLine();
+    }
+
+    public int inputTryCount() throws Exception {
+        return scanner.nextInt();
+    }
+}
