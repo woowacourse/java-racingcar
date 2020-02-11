6fe32a203abf05962b5838ddba2b1dffb610930f
LTTTTTE <10100aaaaa@gmail.com>
2020-02-11 12:07:25 +0900

    test : 요구사항에 맞는 테스트 코드 작성

---
+++ src/test/java/hiTest.java
@@ -0,0 +1,17 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class hiTest {
+	@Test
+	public void add() {
+		int result = Calculator.calculate("1,2:3");
+		assertThat(result).isEqualTo(6);
+	}
+
+	@Test
+	public void customAdd() {
+		int result = Calculator.calculate("//;\n1;2;3");
+		assertThat(result).isEqualTo(6);
+	}
+}
