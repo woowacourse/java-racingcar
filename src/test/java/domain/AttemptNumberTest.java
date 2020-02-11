e6b57aca64a331b7b02e0ac3d7716f593efec90e
moonyoungCHAE <xpf_fl@naver.com>
2020-02-11 15:40:45 +0900

    test: 시도 횟수 테스트 코드 추가

---
+++ src/test/java/domain/AttemptNumberTest.java
@@ -0,0 +1,12 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+public class AttemptNumberTest {
+    @Test
+    void createAttemptNumber_적합한_횟수() {
+        new AttemptNumber("1");
+    }
+
+  
+}
