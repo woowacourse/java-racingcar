82f876f4beb9550dce9a3890ee618b631471ff8b
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-11 18:05:41 +0900

    feat: 숫자나 음수 이외의 값이 포함될 경우 예외처리 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,14 +3,18 @@ package calculator;
 import java.util.Arrays;
 
 public class Calculator {
-
 	public static final String COLON_DELIMITER = ":";
+	public static final int ERROR = -1;
 
 	public static int calculate(String input) {
-		String[] values = Expression.getFinalExpression(input)
-				.split(COLON_DELIMITER);
-
-		return sum(values);
+		try {
+			String[] values = Expression.getFinalExpression(input)
+					.split(COLON_DELIMITER);
+			return sum(values);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return ERROR;
+		}
 	}
 
 	private static int sum(String[] values) {
--- src/test/java/calculator/ExpressionTest.java
+++ src/test/java/calculator/ExpressionTest.java
@@ -1,7 +1,10 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class ExpressionTest {
@@ -14,5 +17,12 @@ public class ExpressionTest {
 		value = "1,4,3:5";
 		expected = Expression.getFinalExpression(value);
 		assertThat(expected).isEqualTo("1:4:3:5");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"-1:3:4", "1:3^4", "1,3:4"})
+	void 식검증(String expression) {
+		assertThatThrownBy(() -> Expression.validate(expression))
+				.isInstanceOf(RuntimeException.class);
 	}
 }
