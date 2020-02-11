ced5be4f8fbe141c0c50785d412de2a19ee69203
junk-sound <wom2277@naver.com>
2020-02-11 17:08:58 +0900

    feat : Add with colon(:)

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,10 @@
 public class StringCalculator {
     public static int add(String s) {
         String[] words = s.split(",");
+        if (words.length == 1) {
+            words = s.split(":");
+        }
+
         int firstNumber = Integer.parseInt(words[0]);
         int secondNumber = Integer.parseInt(words[1]);
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,11 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
-    void 덧셈_쉼표_여러구분자() {
+    void 덧셈_쉼표_두개의인자_여러입력확인() {
         int result = StringCalculator.add("1,2");
         assertThat(result).isEqualTo(3);
 
         result = StringCalculator.add("4,5");
+        assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    void 덧셈_콜론_두개의인자_여러입력확인() {
+        int result = StringCalculator.add("1:2");
+        assertThat(result).isEqualTo(3);
+
+        result = StringCalculator.add("4:5");
         assertThat(result).isEqualTo(9);
     }
 }
