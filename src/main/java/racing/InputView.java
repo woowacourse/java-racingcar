7cae7f406bfcab260dec7e903681bef62392b96f
LTTTTTE <10100aaaaa@gmail.com>
2020-02-11 14:27:24 +0900

    feat : 각 자동차 이름 5자 이하 체크 기능
    - checkMaxLength 추가
    - checkMaxLengthTest 추가

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-
 	public static List<String> splitAsComma(String value) {
 		return Arrays.asList(value.split(","));
+	}
+
+	public static boolean checkMaxLength(List<String> input) {
+		return input.stream().allMatch(x -> x.length() <= 5);
 	}
 }
--- src/test/java/racing/InputViewTest.java
+++ src/test/java/racing/InputViewTest.java
@@ -2,18 +2,36 @@ package racing;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class InputViewTest {
 	@Test
-	public void splitAsCommaTest(){
+	public void splitAsCommaTest() {
 		String value = "A,B,C";
 		List<String> result = InputView.splitAsComma(value);
 
 		assertThat(result).contains("A");
 		assertThat(result).contains("B");
 		assertThat(result).contains("C");
+	}
+
+	@ParameterizedTest
+	@MethodSource("generateInput")
+	public void checkMaxLengthTest(List<String> input, boolean expected) {
+		assertThat(InputView.checkMaxLength(input)).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> generateInput() {
+		return Stream.of(
+			Arguments.of(Arrays.asList("ABCDEF", "B", "C"), false),
+			Arguments.of(Arrays.asList("A", "B", "ABCDEF"), false),
+			Arguments.of(Arrays.asList("A", "B", "C"), true));
 	}
 }
