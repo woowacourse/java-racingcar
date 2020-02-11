467817e4ccaa6fe4ba0f7f5d03e4eca146e80b71
kouz <lastpuzly@gmail.com>
2020-02-11 12:19:58 +0900

    test: 2개 이상의 숫자에 대한 테스트 작성

--- src/test/java/PlusCalculatorTest.java
+++ src/test/java/PlusCalculatorTest.java
@@ -12,10 +12,36 @@ public class PlusCalculatorTest {
 	}
 
 	@Test
+	void plusNumbersSplitByComma() {
+		final String threeValues = "1,2,3";
+		final int actualForThreeValues = PlusCalculator.calculate(threeValues);
+		final int expectedForThreeValues = 6;
+		assertThat(actualForThreeValues).isEqualTo(expectedForThreeValues);
+
+		final String fourValues = "1,2,3,4";
+		final int actualForFourValues = PlusCalculator.calculate(fourValues);
+		final int expectedForFourValues = 10;
+		assertThat(actualForFourValues).isEqualTo(expectedForFourValues);
+	}
+
+	@Test
 	void plusTwoNumberSplitByColon() {
 		final String value = "1:2";
 		final int actual = PlusCalculator.calculate(value);
 		final int expected = 3;
 		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	void plusNumbersSplitByColon() {
+		final String threeValues = "1:2:3";
+		final int actualForThreeValues = PlusCalculator.calculate(threeValues);
+		final int expectedForThreeValues = 6;
+		assertThat(actualForThreeValues).isEqualTo(expectedForThreeValues);
+
+		final String fourValues = "1:2:3:4";
+		final int actualForFourValues = PlusCalculator.calculate(fourValues);
+		final int expectedForFourValues = 10;
+		assertThat(actualForFourValues).isEqualTo(expectedForFourValues);
 	}
 }
