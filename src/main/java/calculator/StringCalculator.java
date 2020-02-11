b25f6fd25849ec89a9d5370fc7a11ae178ddefc4
Andy H. Jung <commaniakr@gmail.com>
2020-02-11 19:15:56 +0900

    feat: splitAndSum 음수 검출 구현.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,14 +2,30 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public static int splitAndSum(String expression) {
-        if(expression == null || expression.isEmpty()) {
+        if (expression == null || expression.isEmpty()) {
             return 0;
         }
-        final String delimeter = ",|:";
-        List<String> numbers = Arrays.asList(expression.split(delimeter));
-        return numbers.stream().mapToInt(Integer::parseInt).sum();
+        List<String> numbers = splitNumbers(expression);
+        if (numbers.stream().anyMatch(x -> Integer.parseInt(x) < 0)) {
+            throw new RuntimeException("음수가 포함되어 있습니다.");
+        }
+            return numbers.stream()
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+    }
+
+    private static List<String> splitNumbers(String expression) {
+        String delimeter = ",|:";
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
+        if (m.find()) {
+            delimeter = m.group(1);
+            expression = m.group(2);
+        }
+        return Arrays.asList(expression.split(delimeter));
     }
 }
