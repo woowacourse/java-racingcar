55be28d0e433e3763c1968e27d45ce9ff5dd4685
icyMojito <dev.icymojito@gmail.com>
2020-02-11 14:46:19 +0900

    [feat] add 커스텀 구분자 지정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,19 +5,35 @@ public class Calculator {
     public static final String COMMA = ",";
     public static final String COLUMN = ":";
     public static final String OR_OPERATOR = "|";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "`n";
+    public static String delimiter = COMMA + OR_OPERATOR + COLUMN;
 
     public static int calculate(String input) {
         String[] splitedInputs = split(input);
-
         int result = Arrays.stream(splitedInputs)
                 .map(Integer::parseInt)
                 .reduce(Integer::sum)
                 .get();
-
         return result;
     }
 
     public static String[] split(String input) {
-        return input.split(COMMA + OR_OPERATOR + COLUMN);
+        if (checkCustomDelimiter(input)) {
+            delimiter = input.substring(2, 3);
+            input = removeCustomDelimiterWord(input);
+        }
+        return input.split(delimiter);
+    }
+
+    private static boolean checkCustomDelimiter(String input) {
+        String prefix = input.substring(0, 2);
+        String suffix = input.substring(3, 5);
+        return prefix.equals(CUSTOM_DELIMITER_PREFIX)
+                && suffix.equals(CUSTOM_DELIMITER_SUFFIX);
+    }
+
+    private static String removeCustomDelimiterWord(String input) {
+        return input.substring(5);
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -15,4 +15,15 @@ public class Test {
         String[] splitedInput = Calculator.split(input);
         assertThat(splitedInput).isEqualTo(new String[]{"55", "341", "8"});
     }
+
+    @org.junit.jupiter.api.Test
+    void 커스텀_구분자_지정() {
+        String noneCustomDelimiterInput = "12:3,4";
+        String[] splitedInput2 = Calculator.split(noneCustomDelimiterInput);
+        assertThat(splitedInput2).isEqualTo(new String[]{"12", "3", "4"});
+
+        String customDelimiterInput = "//_`n11_222_3333";
+        String[] splitedInput = Calculator.split(customDelimiterInput);
+        assertThat(splitedInput).isEqualTo(new String[]{"11", "222", "3333"});
+    }
 }
