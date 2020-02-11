4e64e8dbeac8065ccbdf4432e97c1b8a8aa7016c
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-11 16:11:27 +0900

    feat : Numbers 일급 콜렉션 및 검증 단계 추가

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -1,4 +1,25 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Numbers {
+	private List<Integer> numbers = new ArrayList<>();
+
+	public Numbers(String[] inputs) {
+		for (String input : inputs) {
+            validate(input);
+            numbers.add(Integer.parseInt(input));
+		}
+	}
+
+    private void validate(String input) {
+        if (Integer.parseInt(input) < 0) {
+            throw new RuntimeException();
+        }
+    }
+
+    public int sum() {
+		return numbers.stream().reduce(0, Integer::sum);
+	}
 }
