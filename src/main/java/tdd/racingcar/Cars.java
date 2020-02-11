d9ee5083f3c21e3355b9747f7a5094438dd1d8ab
kouz <lastpuzly@gmail.com>
2020-02-11 17:45:49 +0900

    feat: 최대 거리 구하기 기능 구현

---
+++ src/main/java/tdd/racingcar/Cars.java
@@ -0,0 +1,18 @@
+package tdd.racingcar;
+
+import java.util.List;
+
+public class Cars {
+	private final List<Car> cars;
+
+	public Cars(final List<Car> cars) {
+		this.cars = cars;
+	}
+
+	public int getMaxPosition() {
+		return cars.stream()
+			.mapToInt(Car::getPosition)
+			.max()
+			.orElseThrow(() -> new IllegalArgumentException("차가 존재하지 않습니다."));
+	}
+}
---
+++ src/test/java/tdd/racingcar/CarsTest.java
@@ -0,0 +1,40 @@
+package tdd.racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+	private List<Car> DifferentPositionCars;
+
+	@BeforeEach
+	void init() {
+		DifferentPositionCars = CarFactory.create(Arrays.asList("a", "b", "c", "d"));
+		DifferentPositionCars.get(0).move(4);
+		DifferentPositionCars.get(0).move(4);
+		DifferentPositionCars.get(1).move(4);
+		DifferentPositionCars.get(1).move(4);
+		DifferentPositionCars.get(2).move(4);
+	}
+
+	@Test
+	void getMaxPosition() {
+		Cars cars = new Cars(DifferentPositionCars);
+		final int expectedPosition = 2;
+		final int maxPosition = cars.getMaxPosition();
+		assertEquals(expectedPosition, maxPosition);
+	}
+
+	@Test
+	void getMaxPositionInEmptyCars() {
+		Cars cars = new Cars(new ArrayList<>());
+		assertThatIllegalArgumentException().isThrownBy(cars::getMaxPosition)
+			.withMessage("차가 존재하지 않습니다.");
+	}
+}
