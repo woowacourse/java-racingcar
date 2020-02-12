f6e033da80d8d69c8ea2fbb7bd076800c03379ad
LTTTTTE <10100aaaaa@gmail.com>
2020-02-12 16:22:11 +0900

    refactor : 의미 전달이 약한 값들의 상수화

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -6,13 +6,15 @@ import java.util.stream.Collectors;
 import racing.domain.Car;
 
 public class OutputView {
+	private static final String WINNER_NAME_DELIMITER = ",";
+
 	public static void printRoundPosition(List<Car> cars) {
 		cars.forEach(car -> System.out.println(car.toString()));
 		System.out.println();
 	}
 
 	public static void printWinner(List<Car> winners) {
-		String winnersName = winners.stream().map(Car::getName).collect(Collectors.joining(","));
+		String winnersName = winners.stream().map(Car::getName).collect(Collectors.joining(WINNER_NAME_DELIMITER));
 		System.out.print(winnersName + " 최종 우승했습니다.");
 	}
 }
