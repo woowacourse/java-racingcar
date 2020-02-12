778da585e695a3321044e0c472bffdbbb95b1946
chanin <pci2676@gmail.com>
2020-02-12 13:07:26 +0900

    feat : null 과 빈칸 입력을 처리할 InputStringDTO 객체 생성
     null, empty일 경우 0 반환
     아닌경우 자기 자신을 다시 반환한다.

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class Calculator {
     public static int calculate(String input) {
-        List<String> maybeNumbers = StringParserGroup.findByInput(input)
+        InputStringDTO inputStringDTO = new InputStringDTO(input);
+        List<String> maybeNumbers = StringParserGroup.findByInput(inputStringDTO.getInput())
                 .parse();
         PositiveNumbers positiveNumbers = new PositiveNumbers(maybeNumbers);
         return positiveNumbers.getSum();
