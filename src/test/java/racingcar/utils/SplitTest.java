4da3831b7b477b1e584445cbf798b467fcf11e0b
Sony <sonypark0204@gmail.com>
2020-02-12 11:11:04 +0900

    [feat] 자동차 이름을 쉼표(,) 기준으로 분리하는 로직 구현

---
+++ src/test/java/racingcar/utils/SplitTest.java
@@ -0,0 +1,20 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class SplitTest {
+	@Test
+	@DisplayName("자동차 이름을 쉼표(,) 기준으로 분리 테스트")
+	void splitNameTest() {
+		// given
+		String values = "pobi,crong,honux";
+		String[] expected = {"pobi", "crong", "honux"};
+		// when
+		String[] result = Split.splitInputName(values);
+		//then
+		Assertions.assertArrayEquals(expected,result);
+	}
+}
