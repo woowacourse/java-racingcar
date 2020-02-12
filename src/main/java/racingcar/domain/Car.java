241ec2502eab65032ca9ad55df88ec84213f529f
MOON JINJU <mjj920930@gmail.com>
2020-02-12 11:12:06 +0900

    feat: 자동차 이름이 5자이하인지 검증하는 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,21 @@
 package racingcar.domain;
 
 public class Car {
-    public static void validateName(String input) throws IllegalArgumentException {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    public static void validateEmptyName(String input) throws IllegalArgumentException {
         if (input.isEmpty()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void validateNameLength(String input) {
+        if (isOverLength(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isOverLength(String input) {
+        return input.length() > MAX_NAME_LENGTH;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,7 +9,15 @@ public class CarTest {
     void 자동차_이름이_빈_문자열인지_검증() {
         String input = "";
         assertThatThrownBy(() -> {
-            Car.validateName(input);
+            Car.validateEmptyName(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름이_5자초과인_경우_검증_실패() {
+        String input = "두강두강무늬무늬";
+        assertThatThrownBy(() -> {
+            Car.validateNameLength(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
