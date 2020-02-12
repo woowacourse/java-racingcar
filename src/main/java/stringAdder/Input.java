ad00e5a5f343fc59c2880cc10bfeae1d187df440
gunhee lee <dlrjsgml42@naver.com>
2020-02-12 11:15:48 +0900

    refactor: Input 클래스 리팩토링
    Input 클래스의 checkInputEmpty 분리하여 validateInput, validateList 메서드 생성
    Input 클래스의 changeInput 메서드 생성

--- src/main/java/stringAdder/Input.java
+++ src/main/java/stringAdder/Input.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Input {
     public static final String DEFAULT_DELIMITER = ",|:";
@@ -11,40 +12,58 @@ public class Input {
 
     private String input;
 
-    public Input() {
-        this.input = InputView.inputValue();
-    }
-
-    public Input(String value) {
+    public Input(String value) throws NullPointerException {
         this.input = value;
+        validateInput();
     }
 
-    private List<String> checkInputEmpty(List<String> list) {
-        if (input == null || input.isEmpty() || list.isEmpty()){
-            return null;
+    private void validateInput(){
+        if (input == null || input.isEmpty()){
+            throw new NullPointerException("식을 입력해 주세요.");
         }
-        return list;
+    }
+
+    private void validateList(List<Integer> list) {
+        if (list.isEmpty()){
+            throw new NullPointerException("식을 입력해 주세요.");
+        }
     }
 
     private String getDelimiter() {
         Matcher m = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
         if (m.find()) {
-            input = m.group(2);
+            changeInput(m.group(2));
             return m.group(1);
         }
         return DEFAULT_DELIMITER;
     }
 
-    public List<String> splitInput() {
-        try {
-            String delimiter = getDelimiter();
-            List<String> list = Arrays.asList(input.split(delimiter));
+    private void changeInput(String group) {
+        input = group;
+        validateInput();
+    }
 
-            list = checkInputEmpty(list);
-            return list;
-        } catch (NullPointerException e) {
-            return null;
+    private List<Integer> parseIntList(String delimiter){
+        try {
+            String[] stringArrayValues = input.split(delimiter);
+            List<String> stringListValues = Arrays.asList(stringArrayValues);
+            return stringListValues.stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch(NumberFormatException e){
+            throw new RuntimeException("숫자 이외의 값은 입력하실 수 없습니다.");
         }
     }
 
+    public List<Integer> splitInput() {
+        try {
+            String delimiter = getDelimiter();
+            List<Integer> list = parseIntList(delimiter);
+
+            validateList(list);
+            return list;
+        }catch (NullPointerException e){
+            throw new NullPointerException();
+        }
+    }
 }
