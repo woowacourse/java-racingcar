5dab0655c54c13ff3182deebaa9647e90aac000f
hwanghe159 <hwanghe159@gmail.com>
2020-02-12 11:35:09 +0900

    feat : 입력한 수의 리스트를 다루는 PositiveNumbers클래스 생성

---
+++ src/main/java/stringcalculator/PositiveNumbers.java
@@ -0,0 +1,44 @@
+package stringcalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PositiveNumbers {
+
+    public static final int NEGATIVE = -1;
+    public static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "%s는 숫자가 아닙니다.";
+    public static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "%d는 음수입니다.";
+    private List<Integer> numbers;
+
+    public PositiveNumbers(List<String> maybeNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String maybeNumber : maybeNumbers) {
+            Integer number = parseInt(maybeNumber);
+            numbers.add(number);
+        }
+        this.numbers = numbers;
+    }
+
+    private Integer parseInt(String maybeNumber) {
+        try {
+            return parseInt(Integer.parseInt(maybeNumber));
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(String.format(NUMBER_FORMAT_EXCEPTION_MESSAGE, maybeNumber));
+        }
+    }
+
+    private Integer parseInt(Integer maybePositive) {
+        if (maybePositive > NEGATIVE) {
+            return maybePositive;
+        }
+        throw new IllegalArgumentException(String.format(NEGATIVE_NUMBER_EXCEPTION_MESSAGE, maybePositive));
+    }
+
+    public Integer getSum() {
+        Integer sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
