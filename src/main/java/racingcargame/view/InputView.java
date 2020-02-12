b83f67c847f4aa585b90321fe7eab697bb289bc1
icyMojito <dev.icymojito@gmail.com>
2020-02-12 11:01:48 +0900

    [feat] 이름 입력

---
+++ src/main/java/racingcargame/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcargame.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final static Scanner SCANNER  = new Scanner(System.in);
+
+    public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return SCANNER.nextLine();
+    }
+
+    public static String inputTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return SCANNER.nextLine();
+    }
+}
---
+++ src/test/java/racingcargame/GameTest.java
@@ -0,0 +1,11 @@
+package racingcargame;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    void getInput() {
+
+    }
+}
