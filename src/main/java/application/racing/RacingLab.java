c60c5ef66986fc17c3c313c30a13d59f1db57715
giantim <giantim132@gmail.com>
2020-02-12 15:01:12 +0900

    refactor : 조건 검사를 메소드로 추출

--- src/main/java/application/racing/RacingLab.java
+++ src/main/java/application/racing/RacingLab.java
@@ -11,15 +11,19 @@ public class RacingLab {
     private void throwExceptionWhenCannotParsing(String input) {
         try {
             this.racingLab = Integer.parseInt(input);
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException("숫자를 입력하십시오.");
         }
     }
 
     private void throwExceptionWhenNegativeRacingLabNumber() {
-        if(this.racingLab < 0) {
+        if (isRacingLabUnderZero()) {
             throw new IllegalArgumentException("0 이상의 수를 입력하십시오.");
         }
+    }
+
+    private boolean isRacingLabUnderZero() {
+        return this.racingLab < 0;
     }
 
     public int getRacingLab() {
