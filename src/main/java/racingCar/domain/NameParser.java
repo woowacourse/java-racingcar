e3be0df242fe584b3f0540fb3744ee998dadaee5
chws <rebeccachoosir@gmail.com>
2020-02-12 16:07:22 +0900

    feat: 이름 파싱하는 프로덕션 코드 작성

--- src/main/java/racingCar/domain/NameParser.java
+++ src/main/java/racingCar/domain/NameParser.java
@@ -1,9 +1,11 @@
 package racingCar.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NameParser {
+    public static final String DELIMITER = ",";
     public static List<String> parse(String input) {
-        return null;
+        return Arrays.asList(input.split(DELIMITER));
     }
 }
