673f88756d60fd3cdf79b69eabfbfb350b059d73
gunhee lee <dlrjsgml42@naver.com>
2020-02-12 11:19:40 +0900

    refactor: StringAdder 클래스 리팩토링
    StringAdder 클래스의 add 메서드내 리스트 parameterized type Intger로 변경
    StringAdder 클래스 내의 validateValues 메서드 생성

--- src/main/java/stringAdder/StringAdder.java
+++ src/main/java/stringAdder/StringAdder.java
@@ -1,27 +1,24 @@
 package stringAdder;
 
+import java.util.Collections;
 import java.util.List;
 
 public class StringAdder {
     public static int add(String value) throws RuntimeException {
-        Input input = new Input(value);
-        int result = 0;
-        List<String> values = input.splitInput();
-
-        if(values == null){
+        try {
+            Input input = new Input(value);
+            List<Integer> values = input.splitInput();
+            validateValues(values);
+            return values.stream()
+                    .reduce(0, Integer::sum);
+        }catch (NullPointerException e){
             return 0;
         }
-        for (String s : values) {
-            try {
-                int operand = Integer.parseInt(s);
-                if (operand < 0) {
-                    throw new RuntimeException("음수는 입력하실 수 없습니다.");
-                }
-                result += operand;
-            }catch(NumberFormatException e){
-                throw new RuntimeException("숫자 이외의 값은 입력하실 수 없습니다.");
-            }
+    }
+
+    private static void validateValues(List<Integer> values){
+        if(Collections.min(values) < 0){
+            throw new RuntimeException("음수는 입력하실 수 없습니다.");
         }
-        return result;
     }
 }
