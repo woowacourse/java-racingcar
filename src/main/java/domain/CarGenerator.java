86cff05ef8fe08e639924ba1a36c36fa49d34fcb
moonyoungCHAE <xpf_fl@naver.com>
2020-02-12 15:03:26 +0900

    feat / test: 자동차 이름 입력을 통한 자동차 리스트 생성 기능 구현 및 테스트

---
+++ src/main/java/domain/CarGenerator.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarGenerator {
+    private static final String COMMA = ",";
+
+    public static List<Car> create(String carsName) {
+        String[] carNames = carsName.split(COMMA);
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+}
---
+++ src/test/java/domain/CarGeneratorTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CarGeneratorTest {
+    @Test
+    public void carGenerateTest_적절한_경우() {
+        String carNames = "타미,둔덩";
+        List<Car> cars = CarGenerator.create(carNames);
+        assertThat(cars.get(0).getName()).isEqualTo("타미");
+        assertThat(cars.get(1).getName()).isEqualTo("둔덩");
+    }
+}
