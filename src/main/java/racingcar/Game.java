4a3bbe042d59d5e03c0eb588bf4ca57579e44fb0
Andy H. Jung <commaniakr@gmail.com>
2020-02-12 18:55:52 +0900

    refactor: Game 내 메소드 분리 및 정리

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -29,20 +29,24 @@ public class Game {
     }
 
     public List<String> findWinner(LinkedHashMap<String, Integer> finalStatus) {
-        int maxValue = 0;
         List<String> winners = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : finalStatus.entrySet()) {
-            String name = entry.getKey();
-            Integer position = entry.getValue();
-            if (maxValue < position) {
-                maxValue = position;
-            }
-        }
+        int maxValue = findMaxPosition(finalStatus);
         for (String name : finalStatus.keySet()) {
             if (finalStatus.get(name).equals(maxValue)) {
                 winners.add(name);
             }
         }
         return winners;
+    }
+
+    private int findMaxPosition(LinkedHashMap<String, Integer> finalStatus) {
+        int maxValue = 0;
+        for (Map.Entry<String, Integer> entry : finalStatus.entrySet()) {
+            Integer position = entry.getValue();
+            if (maxValue < position) {
+                maxValue = position;
+            }
+        }
+        return maxValue;
     }
 }
