89b6fcba336c4259ab4e26ebd58888379e401bb5
giantim <giantim132@gmail.com>
2020-02-12 15:04:43 +0900

    refactor : DisplayName 추가 / 메소드 이름 수정 / ParameterizedTest 를 이용해서 테스트 케이스 추가

--- src/test/java/application/racing/RacingCarTest.java
+++ src/test/java/application/racing/RacingCarTest.java
@@ -1,48 +1,44 @@
 package application.racing;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarTest {
-    @Test
-    public void splitTest() {
-        String input = ",abc";
-        String[] expected = {"", "abc"};
-        Assertions.assertThat(input.split(",")).isEqualTo(expected);
-    }
-
+    @DisplayName("잘못된 문자열로 자동차를 생성할 때 예외 발생 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"", ",asd", "asdfrq,pobi", "pobi,pobi", "pobi,,crong", "pobi, ,crong", "  ,pobi"})
-    public void validateInputCarNameTest(String input) {
+    public void validateCarNameTest(String input) {
         Assertions.assertThatThrownBy(() -> {
             CarName carName = new CarName(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("잘못된 문자열을 게임 시도 횟수로 입력시 예외 발생 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"", "a", "-1", "a,sd,fd"})
-    public void validateInputRacingLab(String input) {
+    public void validateRacingLabTest(String input) {
         Assertions.assertThatThrownBy(() -> {
             RacingLab racingLab = new RacingLab(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    public void moveCarWhenNumberOverFour() {
+    @DisplayName("입력받은 숫자에 따라 자동차의 위치 이동 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    public void moveCarWhenNumberOverFourTest(String input, String expected) {
         Car car = new Car("test");
-        int overFourCase = 4;
-        int lessFourCase = 0;
-        car.moveCarWhenNumberOverFour(overFourCase);
-        Assertions.assertThat(car.getPosition()).isEqualTo(1);
-        car.moveCarWhenNumberOverFour(lessFourCase);
-        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+        car.moveCarWhenNumberOverFour(Integer.parseInt(input));
+        Assertions.assertThat(car.getPosition()).isEqualTo(Integer.parseInt(expected));
     }
 
+    @DisplayName("생성한 랜덤 숫자가 0 이상 9 이하인지 테스트")
     @Test
     public void generateRandomNumberTest() {
         Racing racing = new Racing();
@@ -50,8 +46,16 @@ public class RacingCarTest {
         Assertions.assertThat(randomNumber).isBetween(0, 9);
     }
 
+    @DisplayName("우승자를 판단하는 메소드 테스트")
     @Test
-    public void findWinner() {
+    public void findWinnerTest() {
+        List<Car> cars = setUpCars();
+        List<String> winners = setUpWinners();
+        Racing racing = new Racing();
+        Assertions.assertThat(racing.findWinner(cars)).isEqualTo(winners);
+    }
+
+    public List<Car> setUpCars() {
         List<Car> cars = new ArrayList<>();
         Car pobi = new Car("pobi");
         Car lavin = new Car("lavin");
@@ -64,10 +68,13 @@ public class RacingCarTest {
         cars.add(pobi);
         cars.add(lavin);
         cars.add(ramen);
-        Racing racing = new Racing();
+        return cars;
+    }
+
+    public List<String> setUpWinners() {
         List<String> winners = new ArrayList<>();
         winners.add("pobi");
         winners.add("lavin");
-        Assertions.assertThat(racing.findWinner(cars)).isEqualTo(winners);
+        return winners;
     }
 }
