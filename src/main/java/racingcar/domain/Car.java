f31853f70ff238ae092672d256b3d81f012f166e
taejinseok <stj930@gmail.com>
2020-02-12 14:58:55 +0900

    Feat : create car object with name

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+/*
+ * Copyright (c) 2020 by 또동페어
+ * All rights reserved.
+ *
+ * Car.java
+ * 자동차 클래스
+ *
+ * @author      또동페어
+ * @version     1.0
+ * @date        12 Feb 2020
+ *
+ */
+
+public class Car {
+	private final String name;
+
+	public Car(String name) {
+		this.name = name;
+	}
+}
---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+/*
+ * Copyright (c) 2020 by 또동페어
+ * All rights reserved.
+ *
+ * CarTest.java
+ * car 객체 테스트
+ *
+ * @author      또동페어
+ * @version     1.0
+ * @date        12 Feb 2020
+ *
+ */
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+	@DisplayName("이름을 받아 car 객체를 정상적으로 생성")
+	@ParameterizedTest
+	@ValueSource(strings = {"또링", "동글"})
+	void testCar(String name) {
+		Car car = new Car(name);
+		Assertions.assertThat(car).isNotNull();
+	}
+}
