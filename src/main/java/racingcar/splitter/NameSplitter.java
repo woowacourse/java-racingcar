035122752a07fa2c3bb8762fa645d6d4fbdf4875
soring <jnsorn@gmail.com>
2020-02-12 22:33:07 +0900

    Refactor : separate validate method

--- src/main/java/racingcar/splitter/NameSplitter.java
+++ src/main/java/racingcar/splitter/NameSplitter.java
@@ -29,18 +29,34 @@ public class NameSplitter {
     public static List<String> split(String nameValue) {
         List<String> names = Arrays.stream(nameValue.split(DELIMITER))
                 .collect(Collectors.toList());
+        validate(names);
+        return names;
+    }
+
+    private static void validate(List<String> names){
+        validateNumberOfName(names);
+        validateLengthOfName(names);
+        validateDuplicatedName(names);
+    }
+
+    private static void validateNumberOfName(List<String> names){
         if (names.size() < MINIMUM_NUMBER_OF_NAMES) {
             throw new IllegalArgumentException(Message.EXCEPTION_NOT_ENOUGH_CAR.getMessageText());
         }
+    }
+
+    private static void validateLengthOfName(List<String> names){
         for (String name : names) {
             if (name.length() < MINIMUM_LENGTH_OF_NAME || name.length() > MAXIMUM_LENGTH_OF_NAME) {
                 throw new IllegalArgumentException(Message.EXCEPTION_LENGTH_OF_NAME.getMessageText());
             }
         }
+    }
+
+    private static void validateDuplicatedName(List<String> names){
         Set<String> tmpNameSet = new HashSet<>(names);
         if (tmpNameSet.size() != names.size()) {
             throw new IllegalArgumentException(Message.EXCEPTION_DUPLICATED_NAME.getMessageText());
         }
-        return names;
     }
 }
