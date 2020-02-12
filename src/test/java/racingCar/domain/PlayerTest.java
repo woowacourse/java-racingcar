4810927b80939f7693f0c9e2ae6e521448312049
chws <rebeccachoosir@gmail.com>
2020-02-12 17:30:58 +0900

    test: 게임 한 라운드가 올바르게 작동하는지 확인하는 테스트 코드 작성

--- src/test/java/racingCar/domain/PlayerTest.java
+++ src/test/java/racingCar/domain/PlayerTest.java
@@ -1,7 +1,29 @@
 package racingCar.domain;
 
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlayerTest {
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("pobi");
+    }
+
+    @Test
+    void move() {
+        int current, result;
+        current = player.getPosition();
+        player.play();
+        result = player.getPosition();
+        List<Integer> expected = Arrays.asList(0,1);
+
+        Assertions.assertThat(expected.contains(result - current)).isTrue();
+    }
 }
