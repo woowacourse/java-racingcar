236281d9731a7e7f3ea8ec004f13ab8364ace576
KIMSIYOUNG <tldud2404@gmail.com>
2020-02-12 10:46:56 +0900

    feat : 횟수 입력 검증 로직 작성

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,14 +1,25 @@
 package racingcar.util;
 
 public class Validator {
-	public static boolean validate(String input) {
-		if (isInvalid(input)) {
+
+	public static final int MINIMUM_LAP = 1;
+
+	public static boolean validateNamesInput(String input) {
+		if (isInvalidNames(input)) {
 			return false;
 		}
 		return true;
 	}
 
-	private static boolean isInvalid(String input) {
+	private static boolean isInvalidNames(String input) {
 		return input == null || !input.contains(",");
+	}
+
+	public static boolean validateLapInput(String input) {
+		try {
+			return MINIMUM_LAP <= Integer.parseInt(input);
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -4,16 +4,29 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"withoutcomma", "", " "})
 	void invalidStringInput(String input) {
-		assertThat(Validator.validate(input)).isFalse();
+		assertThat(Validator.validateNamesInput(input)).isFalse();
 	}
+
 	@Test
-	void nullInput(){
-		assertThat(Validator.validate(null)).isFalse();
+	void nullInput() {
+		assertThat(Validator.validateNamesInput(null)).isFalse();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"3,true", "0,false", "-3,false,100,true", "asdf,false", ",false"})
+	void lap(String input, boolean expected) {
+		assertThat(Validator.validateLapInput(input)).isEqualTo(expected);
+	}
+
+	@Test
+	void lapNullInput() {
+		assertThat(Validator.validateLapInput(null)).isFalse();
 	}
 }
