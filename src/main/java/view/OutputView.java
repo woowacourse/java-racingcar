44ef3cd2212bf2707c2f8b729fa14d097d934d4e
YerinCho <forjustice9839@gmail.com>
2020-02-12 13:44:19 +0900

    [RacingCar] feat : OutputView
     포지션 만큼 '-' 를 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,10 @@
 package view;
 
+import model.Car;
+
 public class OutputView {
+    public static String visualizePosition(Car car) {
+        int position = car.getPosition();
+        return new String(new char[position]).replace("\0", "-");
+    }
 }
---
+++ src/test/java/view/OutputViewTest.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.GameManager;
+import model.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OutputViewTest {
+    @DisplayName("포지션 값만큼 '-' 출력")
+    @Test
+    void visualizePosition() {
+        Car car = new Car("a");
+        GameManager.moveOrStay(car, 6);
+        GameManager.moveOrStay(car, 6);
+
+        String result = OutputView.visualizePosition(car);
+        assertThat(result).isEqualTo("--");
+    }
+}
