c09d449a974a6349fe9ffdaa096880e7ec594c6e
hwangbo <taekwonod714@naver.com>
2020-02-12 14:39:32 +0900

    refactor: 차 이름이 다섯글자인지 확인하는 메서드를 예외처리로 변형

--- src/main/java/racingcar/Controller/InputValidation.java
+++ src/main/java/racingcar/Controller/InputValidation.java
@@ -2,16 +2,15 @@ package racingcar.Controller;
 
 public class InputValidation {
 
-    public static boolean isSmallerThanSix(String result) {
-        if (result.isEmpty() || result.length() > 5) {
-            return false;
-        }
-        return true;
-    }
-
     public static void checkNullOrEmptyInput(String result) {
         if (result == null || result.isEmpty()) {
             throw new NullPointerException("입력이 null이나 빈 문자열입니다.");
+        }
+    }
+
+    public static void checkSmallerThanSix(String result) {
+        if (result.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이가 6 이상입니다.");
         }
     }
 }
--- src/test/java/racingcarTest/RacingCarTest.java
+++ src/test/java/racingcarTest/RacingCarTest.java
@@ -7,8 +7,7 @@ import racingcar.View.InputView;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class RacingCarTest {
 
@@ -20,21 +19,12 @@ public class RacingCarTest {
     }
 
     @Test
-    @DisplayName("차 이름이 5글자 이내")
-    void inputControl_차_이름_글자수() {
-        String result = "Maserati";
-        assertFalse(InputValidation.isSmallerThanSix(result));
-        result = "Ford";
-        assertTrue(InputValidation.isSmallerThanSix(result));
-    }
-
-    @Test
     @DisplayName("null 또는 빈 문자열 입력 시")
     void inputControl_null_또는_빈문자열_입력() {
         String nullResult = null;
         String emptyResult = "";
 
-        assertThatThrownBy( () -> {
+        assertThatThrownBy(() -> {
             InputValidation.checkNullOrEmptyInput(nullResult);
         }).isInstanceOf(NullPointerException.class)
         .hasMessage("입력이 null이나 빈 문자열입니다.");
@@ -43,5 +33,15 @@ public class RacingCarTest {
             InputValidation.checkNullOrEmptyInput(emptyResult);
         }).isInstanceOf(NullPointerException.class)
         .hasMessage("입력이 null이나 빈 문자열입니다.");
+    }
+
+    @Test
+    @DisplayName("차 이름이 5글자 이내")
+    void inputControl_차_이름_글자수() {
+        assertThatThrownBy(()->{
+            String result = "Maserati";
+            InputValidation.checkSmallerThanSix(result);
+        }).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("자동차 이름의 길이가 6 이상입니다.");
     }
 }
