c71fdeb5c813086204f8efd92240bc454fff4732
lxxjn0 <ljy3510@gmail.com>
2020-02-12 11:25:10 +0900

    refactor : MovementStrategy 클래스 삭제

--- src/main/java/racingGame/utils/MovementStrategy.java
+++
@@ -1,9 +0,0 @@
-package racingGame.utils;
-
-public class MovementStrategy {
-	private static final int MOVEMENT_STRATEGY_BOUND = 4;
-
-	public static boolean isMovementStrategy(final int randomNumber) {
-		return randomNumber >= MOVEMENT_STRATEGY_BOUND;
-	}
-}
