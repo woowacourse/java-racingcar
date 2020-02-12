ff7d465786540ee86f310e3ea36e41bead1abcbc
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-02-12 15:09:36 +0900

    feat: 자동차가 두 대 이상인지 검증하는 기능 추가

--- src/main/java/utils/InputUtil.java
+++ src/main/java/utils/InputUtil.java
@@ -8,20 +8,16 @@ public class InputUtil {
 	public static final String SPACE = " ";
 	public static final String EMPTY = "";
 	public static final String COMMA = ",";
+	public static final int MINIMUM_NUMBER_OF_CARS = 2;
+	public static final int LIMIT = -1;
 
 	public static void readCars(String input) {
 		List<String> cars = Arrays.asList(input.replace(SPACE, EMPTY)
-				.split(COMMA));
+				.split(COMMA, LIMIT));
 
 		validateLengthOfCarName(cars);
 		validateDuplicateCarName(cars);
-	}
-
-	private static void validateDuplicateCarName(List<String> cars) {
-		if (!cars.stream()
-			.allMatch(new HashSet<>()::add)) {
-			throw new IllegalArgumentException("중복된 이름이 존재합니다.");
-		}
+		validateNumberOfCars(cars);
 	}
 
 	private static void validateLengthOfCarName(List<String> cars) {
@@ -33,5 +29,16 @@ public class InputUtil {
 				});
 	}
 
+	private static void validateDuplicateCarName(List<String> cars) {
+		if (!cars.stream()
+			.allMatch(new HashSet<>()::add)) {
+			throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+		}
+	}
 
+	private static void validateNumberOfCars(List<String> cars) {
+		if (cars.size() < MINIMUM_NUMBER_OF_CARS) {
+			throw new IllegalArgumentException("자동차는 최소 2대 이상이어야 합니다.");
+		}
+	}
 }
