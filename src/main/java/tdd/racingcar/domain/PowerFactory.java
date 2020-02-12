d582006069ec6d72741cd8e72922a34571f17dd0
kouz <lastpuzly@gmail.com>
2020-02-12 13:16:02 +0900

    feat: createRandomPower 메소드 구현

---
+++ src/main/java/tdd/racingcar/domain/PowerFactory.java
@@ -0,0 +1,13 @@
+package tdd.racingcar.domain;
+
+import java.util.Random;
+
+public class PowerFactory {
+	private static final Random random = new Random();
+	private static final int MAX_POWER_BOUND = 10;
+
+	public static Power createRandomPower() {
+		final int powerAmount = random.nextInt(MAX_POWER_BOUND);
+		return new Power(powerAmount);
+	}
+}
