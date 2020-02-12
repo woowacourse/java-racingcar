c724e1eba4cf53de659fbbf884987bc74d995dfb
chanin <pci2676@gmail.com>
2020-02-12 13:38:31 +0900

    refactor : InputDTO 테스트 수정 및 상수 추출
      , 상수로 변환
      테스트 인풋 변경

--- src/main/java/racing/InputDTO.java
+++ src/main/java/racing/InputDTO.java
@@ -6,10 +6,11 @@ import java.util.stream.Collectors;
 
 public class InputDTO {
 
+    private static final String DELIMITER = ",";
     private List<String> names;
 
     public InputDTO(String names) {
-        this.names = Arrays.stream(names.split(","))
+        this.names = Arrays.stream(names.split(DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -1,5 +1,6 @@
 package racing;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -7,9 +8,11 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
+
+    @DisplayName("입력받은 문자를 trim 후 쉼표기준으로 하나씩 자르기")
     @Test
     void splitTest() {
-        InputDTO input = new InputDTO("pobi,crong,honux");
+        InputDTO input = new InputDTO("pobi,crong,  honux  ");
         assertThat(input.getNames()).isEqualTo(Arrays.asList("pobi", "crong", "honux"));
     }
 }
