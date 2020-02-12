64c1f48e7bdfe292b1b46eeb12d48f00d13b8b49
gunhee lee <dlrjsgml42@naver.com>
2020-02-12 14:50:09 +0900

    feat: 레이싱 게임 두번째 테스트
    RacingGameTest 클래스에 문자열 split 검증 테스트 구현
    RacingGame 테스트에 맞게 기능 구현

--- src/main/java/racingGame/RacingGame.java
+++ src/main/java/racingGame/RacingGame.java
@@ -1,9 +1,17 @@
 package racingGame;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RacingGame {
     public static void validateInput(String name) {
         if(name == null || name.isEmpty() || name.length() > 5){
             throw new IllegalArgumentException("이름의 길이는 1자 이상, 5자 이하만 가능합니다.");
         }
+    }
+
+    public static List<String> splitInput(String value) {
+        String[] names = value.split(",");
+        return Arrays.asList(names);
     }
 }
--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,16 +1,26 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingGame.RacingGame;
+
+import java.util.List;
 
 public class RacingGameTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"pobipobi", "crongcrong", "honuxisgreat", ""})
-    void 한명입력시_이름길이_검증(String value){
+    void 한명_입력시_이름길이_검증(String value){
         Assertions.assertThatThrownBy(()->{
             RacingGame.validateInput(value);
         }).isInstanceOf(IllegalArgumentException.class).hasMessageMatching("이름의 길이는 1자 이상, 5자 이하만 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"pobi,crong,honux:pobi", "a,b,c:c"}, delimiter = ':')
+    void 여러명_입력시_split_검증(String value, String expected){
+        List<String> list = RacingGame.splitInput(value);
+        Assertions.assertThat(list).contains(expected);
     }
 }
