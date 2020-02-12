690b052aba9d51d7837f087a1d9e4c0102853db7
hwangbo <taekwonod714@naver.com>
2020-02-12 16:30:35 +0900

    feat:  콘솔 입력 메세지 구현

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -2,6 +2,17 @@ package racingcar.View;
 
 public class OutputView {
 
+    private static final String CAR_NAMES_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분";
+    private static final String TRIAL_TIME_INPUT_GUIDE_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public static void printInputCarNamesMessage(){
+        System.out.println(CAR_NAMES_INPUT_GUIDE_MESSAGE);
+    }
+
+    public static void printInputTrialTimeMessage(){
+        System.out.println(TRIAL_TIME_INPUT_GUIDE_MESSAGE);
+    }
+
     public static void printExceptionMessage(Exception e) {
         System.out.println(e.getMessage());
     }
