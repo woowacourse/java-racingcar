e85af551a248110cfdda670746f32d608b96be87
KimSeongGyu1 <KimSeongGyu1@github.com>
2020-02-12 14:05:31 +0900

    [feat] 자동차 객체 이동 메서드 구현
    - 랜덤으로 이동할지 말지 결정하는 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,18 +1,39 @@
 package racingcar.domain;
 
+import java.util.Random;
+
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int RANDOM_MAX = 10;
+    private static final int RANDOM_MIN = 0;
 
     private final String name;
+    private int distance;
 
     public Car(String name) {
         checkValidName(name);
         this.name = name;
+        this.distance = 0;
     }
 
     private void checkValidName(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
         }
+    }
+
+    public void move() {
+        if (canMove()) {
+            this.distance++;
+        }
+    }
+
+    private boolean canMove() {
+        int randomNumber = new Random().nextInt(RANDOM_MAX - RANDOM_MIN) + RANDOM_MIN;
+        if (randomNumber >= MOVE_THRESHOLD) {
+            return true;
+        }
+        return false;
     }
 }
