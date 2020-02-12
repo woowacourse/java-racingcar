e9e5e46a5822540de394c2fa484ee563224c0ba2
lxxjn0 <ljy3510@gmail.com>
2020-02-12 16:49:22 +0900

    refactor : StringCalculator 테스트 코드 리팩토링
    1. ParameterizedTest를 활용하여 테스트 코드 리팩토링.

--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -3,42 +3,51 @@ package stringCalculator;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class StringCalculatorTest {
 	@Test
 	void calculate_null_또는_빈문자() {
-		String value = null;
-		assertThat(StringCalculator.calculate(value)).isEqualTo(0);
+		final String nullValue = null;
 
-		value = "";
-		assertThat(StringCalculator.calculate(value)).isEqualTo(0);
+		assertThat(StringCalculator.calculate(nullValue)).isEqualTo(0);
+
+		final String emptyValue = "";
+
+		assertThat(StringCalculator.calculate(emptyValue)).isEqualTo(0);
 	}
 
-	@Test
-	void calculate_쉼표_콜론_구분자로_숫자합_계산() {
-		String value = "1,2";
-		assertThat(StringCalculator.calculate(value)).isEqualTo(3);
+	@ParameterizedTest
+	@CsvSource(value = {"1,2|3", "1:2|3", "1,2:3|6"}, delimiter = '|')
+	void calculate_쉼표_콜론_구분자로_숫자합_계산(final String value, final int expected) {
+		final int actual = StringCalculator.calculate(value);
 
-		value = "1:2";
-		assertThat(StringCalculator.calculate(value)).isEqualTo(3);
-
-		value = "1,2:3";
-		assertThat(StringCalculator.calculate(value)).isEqualTo(6);
+		assertThat(actual).isEqualTo(expected);
 	}
 
+	// NOTE : @CsvSource로 value들을 넘기려고 할 때 \ 문자를 어떻게 넘길 수 있을까?
 	@Test
 	void calculate_커스텀_구분자로_숫자합_계산() {
-		String value = "//;\n1;2;3";
-		assertThat(StringCalculator.calculate(value)).isEqualTo(6);
+		final String value = "//;\n1;2;3";
+
+		final int actual = StringCalculator.calculate(value);
+
+		final int expected = 6;
+
+		assertThat(actual).isEqualTo(expected);
 	}
 
+	// NOTE : 위의 @CsvSource와 같은 고민
 	@Test
 	void calculate_숫자_이외의_값() {
 		final String invalidValue = "1:2,a";
+
 		assertThatThrownBy(() -> StringCalculator.calculate(invalidValue))
 			.isInstanceOf(RuntimeException.class);
 
 		final String invalidValue2 = "//;\n1;2;a";
+
 		assertThatThrownBy(() -> StringCalculator.calculate(invalidValue2))
 			.isInstanceOf(RuntimeException.class);
 	}
@@ -46,10 +55,12 @@ public class StringCalculatorTest {
 	@Test
 	void calculate_음수() {
 		final String invalidValue = "1:2,-2";
+
 		assertThatThrownBy(() -> StringCalculator.calculate(invalidValue))
 			.isInstanceOf(RuntimeException.class);
 
 		final String invalidValue2 = "//;\n1;2;-2";
+
 		assertThatThrownBy(() -> StringCalculator.calculate(invalidValue2))
 			.isInstanceOf(RuntimeException.class);
 	}
