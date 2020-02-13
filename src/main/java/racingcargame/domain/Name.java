bcd3f04b1335ab8eed0d358bf38078171ee47c51
Tak Hyungmin <milkho11@naver.com>
2020-02-13 17:31:58 +0900

    [fix] 이름 에러 발생 초기화
    이름 입력 관련 에러가 발생하였을 때 이름을 담는 static한 Set을 초기화하지 않아
    중복으로 에러가 발생된 이후에도 같은 이름을 입력하면 에러가 발생

--- src/main/java/racingcargame/domain/Name.java
+++ src/main/java/racingcargame/domain/Name.java
@@ -4,12 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Name {
-    // TODO : 에러 발생시 set 초기화 과정 필요하다.
     private static final Set<String> CAR_NAMES = new HashSet<>();
 
     private static final int NAME_MAX_LENGTH = 5;
     private static final String DUPLICATE_NAME_ERROR_MESSAGE = "중복된 이름을 가지고 있습니다.";
-    private static final String LENGTH_LIMIT_EXCEED_ERROR_MESSAGE = "길이 최대치를 초과하는 이름을 가지고 있습니다.";
+    private static final String LENGTH_LIMIT_EXCEED_ERROR_MESSAGE = "RR길이 최대치를 초과하는 이름을 가지고 있습니다.";
     private static final String SPACE_ERROR_MESSAGE = "공백 이름을 가지고 있습니다.";
 
     private String name;
@@ -33,6 +32,7 @@ public class Name {
 
     private void isDuplicatedNames(String input) {
         if (CAR_NAMES.contains(input)) {
+            CAR_NAMES.clear();
             System.out.println(DUPLICATE_NAME_ERROR_MESSAGE);
             throw new RuntimeException();
         }
@@ -40,6 +40,7 @@ public class Name {
 
     private void isExceededMaxLength(String name) {
         if (name.length() > NAME_MAX_LENGTH) {
+            CAR_NAMES.clear();
             System.out.println(LENGTH_LIMIT_EXCEED_ERROR_MESSAGE);
             throw new RuntimeException();
         }
@@ -47,6 +48,7 @@ public class Name {
 
     private void checkBlankInput(String name) {
         if (name.isEmpty()) {
+            CAR_NAMES.clear();
             System.out.println(SPACE_ERROR_MESSAGE);
             throw new RuntimeException();
         }
