ae4778e41c8e0528c34771e69a585b27dc2cc029
hwangbo <taekwonod714@naver.com>
2020-02-13 11:32:38 +0900

    fix: 첫 출력 문구 오류 수정

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -3,7 +3,7 @@ package racingcar.View;
 public class OutputView {
 
     public static final String RESULT_MESSAGE = "실행 결과";
-    private static final String CAR_NAMES_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분";
+    private static final String CAR_NAMES_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)";
     private static final String TRIAL_TIME_INPUT_GUIDE_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public static void printInputCarNamesMessage(){
