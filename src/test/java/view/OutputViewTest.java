5cab0dc6899cd49dade7228e04648390a53a8a61
YerinCho <forjustice9839@gmail.com>
2020-02-13 14:13:40 +0900

    [RacingCar] add : OutputViewTest
     테스트 케이스 추가

--- src/test/java/view/OutputViewTest.java
+++ src/test/java/view/OutputViewTest.java
@@ -4,7 +4,8 @@ import controller.GameManager;
 import model.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +23,17 @@ public class OutputViewTest {
         cars.add(new Car("c"));
         GameManager.moveOrStay(cars.get(0), 9);
         GameManager.moveOrStay(cars.get(0), 9);
+        GameManager.moveOrStay(cars.get(1), 1);
         GameManager.moveOrStay(cars.get(1), 9);
-        GameManager.moveOrStay(cars.get(1), 9);
+        GameManager.moveOrStay(cars.get(2), 2);
+        GameManager.moveOrStay(cars.get(2), 3);
     }
 
     @DisplayName("포지션 값만큼 '-' 출력")
-    @Test
-    void visualizePosition() {
-        String result = OutputView.printPositionBar(cars.get(0));
-        assertThat(result).isEqualTo("--");
+    @ParameterizedTest
+    @CsvSource(value = {"0,'--'", "1,'-'", "2,''"})
+    void visualizePosition(int index, String s) {
+        String result = OutputView.printPositionBar(cars.get(index));
+        assertThat(result).isEqualTo(s);
     }
 }
