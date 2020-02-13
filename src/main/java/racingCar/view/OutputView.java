bef6120a6998eaf0e42e19a5c93993ba11e473f3
junk-sound <wom2277@naver.com>
2020-02-13 15:53:47 +0900

    refac: 게임 구동을 위한 프린트 문구 위치 변경

--- src/main/java/racingCar/view/OutputView.java
+++ src/main/java/racingCar/view/OutputView.java
@@ -5,19 +5,26 @@ import racingCar.domain.RacingCar;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class OutputView {
-    void printRacingCar(RacingCar racingCar) {
+    public static void printRacingCar(RacingCar racingCar) {
         System.out.print(racingCar);
     }
 
-    void printStartingComment() {
+    public static void printStartingComment() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n");
     }
 
-    void printStateOfRacingCars(RacingCar[] racingCars, int numberToMove) {
+    public static void printAskingSentenceOfTry() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    static void printGuideOfResult() {
         System.out.println("실행 결과");
+    }
+
+    static void printStateOfRacingCars(RacingCar[] racingCars, int numberToMove) {
+
 
         for (int i = 0; i < numberToMove; i++) {
             for (RacingCar racingCar : racingCars) {
@@ -28,12 +35,12 @@ public class OutputView {
         }
     }
 
-    void printResult(RacingCar[] racingCars) {
+    static void printResult(RacingCar[] racingCars) {
         List<String> namesOfWinners = Arrays.asList(racingCars).stream().map(RacingCar::toString).collect(Collectors.toList());
         System.out.println(String.join(",", namesOfWinners) + "가 최종 우승했습니다.");
     }
 
-    private void printRacingPosition(RacingCar racingCar) {
+    static private void printRacingPosition(RacingCar racingCar) {
         String position = RacingCarPositionTranslator.translate(racingCar.getPostion());
         System.out.println(" : " + position);
     }
