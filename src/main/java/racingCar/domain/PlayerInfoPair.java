8111826d60160f3cca4fadf7326c7a56733fea89
chws <rebeccachoosir@gmail.com>
2020-02-13 13:53:13 +0900

    refactor: 자주 쓰는 문자열을 상수로 변환

--- src/main/java/racingCar/domain/PlayerInfoPair.java
+++ src/main/java/racingCar/domain/PlayerInfoPair.java
@@ -3,6 +3,9 @@ package racingCar.domain;
 import java.util.stream.IntStream;
 
 public class PlayerInfoPair {
+    private static final String COLON_WRAPPED_WITH_SPACE = " : ";
+    private static final String HYPHEN = "-";
+
     private String name;
     private int position;
 
@@ -13,9 +16,9 @@ public class PlayerInfoPair {
 
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder(name);
-        stringBuilder.append(" : ");
+        stringBuilder.append(COLON_WRAPPED_WITH_SPACE);
         IntStream.range(0, position)
-                .forEach((t) -> stringBuilder.append("-"));
+                .forEach((t) -> stringBuilder.append(HYPHEN));
         return stringBuilder.toString();
     }
 }
