e5360a1798136641c0abfde9448ee64ea71bee0d
junk-sound <wom2277@naver.com>
2020-02-13 11:41:15 +0900

    feat : 사용자는 몇 번의 이동을 할 것인지 입력할 수 있어야 한다.

---
+++ src/main/java/racingCar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingCar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private Scanner scanner;
+
+    InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int inputNumbersToMove() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+}
