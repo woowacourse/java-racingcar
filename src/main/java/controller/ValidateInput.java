2a80846481d349b5ef80039c03fe83962c27f864
YerinCho <forjustice9839@gmail.com>
2020-02-13 11:13:49 +0900

    [RacingCar] feat : ValidateInput, ValidationView
     UI 로직 분리, 사용자 입력 오류 시 재입력 기능 추가

--- src/main/java/controller/ValidateInput.java
+++ src/main/java/controller/ValidateInput.java
@@ -1,38 +1,57 @@
 package controller;
 
+import view.InputView;
+import view.ValidationView;
+
 import java.util.regex.Pattern;
 
 public class ValidateInput {
 
     private static final String NUMBER_FORMAT = "^\\d+$";
     private static final int MAX_NAME_LENGTH = 5;
-    private static final String NAME_LENGTH_ERROR_MESSAGE = "이름의 길이는 1이상 5이하만 가능합니다";
-    private static final String NOT_INTEGER_ERROR_MESSAGE = "숫자가 아닌 문자를 입력하였습니다.";
-    private static final String MIN_ROUND_NUMBER_ERROR_MESSAGE = "1이상의 숫자만 입력해야 합니다.";
 
     public static String[] validateName(String[] names) {
         for (String name : names) {
             if (name.length() > MAX_NAME_LENGTH || name.isEmpty()) {
-                throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+                names = IllegalNameException();
+                break;
             }
         }
         return names;
+    }
+
+    public static int ValidateCount(String input) {
+        int integerInput = Integer.parseInt(input);
+        ;
+        if (isNotInteger(input)) {
+            return IllegalNotIntegerException();
+        }
+        if (isUnderZero(integerInput)) {
+            integerInput = IllegalNegativeNumberException();
+        }
+        return integerInput;
+    }
+
+    private static int IllegalNotIntegerException() {
+        ValidationView.printNotIntegerErrorMessage();
+        return InputView.inputRound();
+    }
+
+    private static int IllegalNegativeNumberException() {
+        ValidationView.printMinRoundNumberErrorMessage();
+        return InputView.inputRound();
+    }
+
+    private static String[] IllegalNameException() {
+        ValidationView.printNameErrorMessage();
+        return InputView.inputCarName();
+
     }
 
     public static boolean isUnderZero(int round) {
         return round <= GameManager.ZERO;
     }
 
-    public static int ValidateCount(String input) {
-        if (isNotInteger(input)) {
-            throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
-        }
-        int integerInput = Integer.parseInt(input);
-        if (isUnderZero(integerInput)) {
-            throw new IllegalArgumentException(MIN_ROUND_NUMBER_ERROR_MESSAGE);
-        }
-        return integerInput;
-    }
 
     private static boolean isNotInteger(String input) {
         Pattern pattern = Pattern.compile(NUMBER_FORMAT);
