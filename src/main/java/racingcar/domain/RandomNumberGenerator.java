b4feda232d31f1325c80b844c40c251f44c61151
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-02-13 16:24:26 +0900

    refactor : 내부 로직에 사용되는 메소드 private으로 변경

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class RandomNumberGenerator {
     private final Random random = new Random();
 
-    public int generateRandomNumber() {
+    private int generateRandomNumber() {
         return random.nextInt(10);
     }
 
