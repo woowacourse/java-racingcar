033c5bd5ca6faac7d47a4a2d84a7bb45dcd67aa0
hwookim <gusdn9754@gmail.com>
2020-02-13 14:42:03 +0900

    refactor : Cars.Cars 매개변수명 변경 input -> carNames

--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -9,27 +9,27 @@ import racing.view.OutputView;
 public class Cars {
 	private List<Car> cars;
 
-	public Cars(List<String> input) {
-		cars = input.stream()
+	public Cars(List<String> carNames) {
+		cars = carNames.stream()
 				.map(Car::new)
 				.collect(Collectors.toList());
+	}
+
+	public void playRound() {
+		cars.forEach(car -> car.goForward(car.randomGenerate()));
+	}
+
+	private int findWinnersPosition() {
+		return cars.stream()
+				.map(Car::getPosition)
+				.reduce(Integer::max)
+				.orElse(0);
 	}
 
 	public List<Car> findWinner() {
 		return cars.stream()
 			.filter(car -> car.isWinner(findWinnersPosition()))
 			.collect(Collectors.toList());
-	}
-
-	private int findWinnersPosition() {
-		return cars.stream()
-			.map(Car::getPosition)
-			.reduce(Integer::max)
-			.orElse(0);
-	}
-
-	public void playRound() {
-		cars.forEach(car -> car.goForward(car.randomGenerate()));
 	}
 
 	public List<Car> getCars() {
