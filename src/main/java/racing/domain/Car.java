3b63aa48717a61d15f292be6329798f0560f966c
hwookim <gusdn9754@gmail.com>
2020-02-13 15:40:19 +0900

    refactor : Car.randomGenerate -> Cars 이동

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,24 +1,14 @@
 package racing.domain;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Car {
 	private static final int FORWARD_PIVOT = 4;
-	private static final int RANDOM_MAX = 9;
-	private static final int RANDOM_MIN = 0;
+
 	private String name;
 	private int position;
 
 	public Car(String name) {
 		this.name = name;
 		this.position = 0;
-	}
-
-	public int randomGenerate() {
-		return ThreadLocalRandom.current()
-			.ints(RANDOM_MIN, RANDOM_MAX)
-			.findFirst()
-			.orElse(RANDOM_MIN);
 	}
 
 	public void goForward(int number) {
--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -2,11 +2,15 @@ package racing.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import racing.view.OutputView;
 
 public class Cars {
+	private static final int RANDOM_MAX = 9;
+	private static final int RANDOM_MIN = 0;
+
 	private List<Car> cars;
 
 	public Cars(List<String> carNames) {
@@ -15,8 +19,15 @@ public class Cars {
 				.collect(Collectors.toList());
 	}
 
+	public int randomGenerate() {
+		return ThreadLocalRandom.current()
+				.ints(RANDOM_MIN, RANDOM_MAX)
+				.findFirst()
+				.orElse(RANDOM_MIN);
+	}
+
 	public void playRound() {
-		cars.forEach(car -> car.goForward(car.randomGenerate()));
+		cars.forEach(car -> car.goForward(randomGenerate()));
 	}
 
 	private int findWinnersPosition() {
