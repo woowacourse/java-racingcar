00013210c4237ac75c3248181794e85530e2735b
moonyoungCHAE <xpf_fl@naver.com>
2020-02-13 16:56:46 +0900

    style: 에러 메세지 수정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -6,6 +6,7 @@ public class Name {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
     private static final String WRONG_NAME_LENGTH_MESSAGE = "이름 길이는 1~5 이여야 합니다.";
+    private static final String NOT_INPUT_NAME_MESSAGE = "이름을 입력하지 않았습니다.";
 
     private final String name;
 
@@ -17,7 +18,7 @@ public class Name {
 
     private void validateNameBlankOrNull(String name) {
         if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException(WRONG_NAME_LENGTH_MESSAGE);
+            throw new IllegalArgumentException(NOT_INPUT_NAME_MESSAGE);
         }
     }
 
