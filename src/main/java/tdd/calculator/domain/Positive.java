6143ec14c386af6c3d8a51df081798da1c4dacf9
lalize <printf@kakao.com>
2020-02-13 10:58:09 +0900

    refactor: Positive 클래스 리팩토링

--- src/main/java/tdd/calculator/domain/Positive.java
+++ src/main/java/tdd/calculator/domain/Positive.java
@@ -1,32 +1,32 @@
 package tdd.calculator.domain;
 
 public class Positive {
-	private static final int INDEX_OF_MINUS_SIGN = 0;
-	private static final char MINUS_SIGN = '-';
-
 	private final int positive;
 
 	public Positive(final String positive) {
-		validate(positive);
-		this.positive = Integer.parseInt(positive);
+		this(Integer.parseInt(positive));
 	}
 
-	private void validate(final String positive) {
-		if (isNotNumber(positive)) {
-			throw new RuntimeException("숫자가 아닌 문자를 입력할 수 없습니다.");
+	public Positive(final int positive) {
+		validate(positive);
+		this.positive = positive;
+	}
+
+	private void validate(final int positive) {
+		if (isZero(positive)) {
+			throw new IllegalArgumentException("0을 입력할 수 없습니다.");
 		}
 		if (isNegative(positive)) {
 			throw new RuntimeException("음수를 입력할 수 없습니다.");
 		}
 	}
 
-	private boolean isNegative(final String positive) {
-		return positive.charAt(INDEX_OF_MINUS_SIGN) == MINUS_SIGN;
+	private boolean isZero(final int positive) {
+		return positive == 0;
 	}
 
-	private boolean isNotNumber(final String positive) {
-		return positive.chars()
-				.anyMatch(ch -> !Character.isDigit(ch));
+	private boolean isNegative(final int positive) {
+		return positive < 0;
 	}
 
 	public int getValue() {
--- src/test/java/tdd/calculator/domain/PositiveTest.java
+++ src/test/java/tdd/calculator/domain/PositiveTest.java
@@ -1,12 +1,38 @@
 package tdd.calculator.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PositiveTest {
 	@Test
-	void getPositive() {
+	@DisplayName("Positive를 0으로 생성할 때 예외 발생 테스트")
+	void testZero() {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+			new Positive(0);
+		}).withMessage("0을 입력할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("Positive를 음수로 생성할 때 예외 발생 테스트")
+	void testNegative() {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+			new Positive(-1);
+		}).withMessage("음수를 입력할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("Positive를 숫자 이외의 값으로 생성할 때 예외 발생 테스트")
+	void testNotNumber() {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+			new Positive("a");
+		});
+	}
+
+	@Test
+	void getValue() {
 		final Positive one = new Positive("1");
 		final int expected = 1;
 		final int actual = one.getValue();
