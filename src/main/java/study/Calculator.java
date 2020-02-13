ee4a4d41c979fefa7787152221408d12b9a864f7
hwookim <gusdn9754@gmail.com>
2020-02-13 17:15:57 +0900

    refactor : Pattern의 비용 소모 문제를 해결하고자 String.contains 사용

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,68 +1,64 @@
 package study;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Calculator {
-	private static final String BLANK = "";
-	private static final String CUSTOM_REGEX = "//(.)\n";
-	private static final String DEFAULT_REGEX = ",:";
-	private static final RuntimeException RUNTIME_EXCEPTION = new RuntimeException("잘못 입력하셨습니다.");
-	private static final int CUSTOM_REGEX_LENGTH = 4;
+    private static final String BLANK = "";
+    private static final String CUSTOM_REGEX = "//";
+    private static final String DEFAULT_REGEX = ",:";
+    private static final RuntimeException RUNTIME_EXCEPTION = new RuntimeException("잘못 입력하셨습니다.");
+    private static final int CUSTOM_REGEX_LENGTH = 4;
 
-	public static int calculate(String input) {
-		String regex = checkCustomRegex(BLANK, input);
-		input = subStringInput(regex, input);
+    public static int calculate(String input) {
+        String regex = checkCustomRegex(BLANK, input);
+        input = subStringInput(regex, input);
 
-		return Stream.of(splitAsRegex(regex, input))
-			.filter(Calculator::isNumber)
-			.map(Integer::parseInt)
-			.filter(Calculator::isPositive)
-			.reduce(Integer::sum)
-			.orElseThrow(() -> RUNTIME_EXCEPTION);
-	}
+        return Stream.of(splitAsRegex(regex, input))
+                .filter(Calculator::isNumber)
+                .map(Integer::parseInt)
+                .filter(Calculator::isPositive)
+                .reduce(Integer::sum)
+                .orElseThrow(() -> RUNTIME_EXCEPTION);
+    }
 
-	private static String checkCustomRegex(String regex, String input) {
-		Pattern p = Pattern.compile(CUSTOM_REGEX);
-		Matcher matcher = p.matcher(input);
-		while (matcher.find()) {
-			regex = matcher.group(1);
-		}
+    private static String checkCustomRegex(String regex, String input) {
+        if (input.contains(CUSTOM_REGEX)) {
+			regex = input.substring(2,3);
+        }
 
-		return regex;
-	}
+        return regex;
+    }
 
-	private static String subStringInput(String regex, String input) {
-		if (regex.equals(BLANK)) {
-			return input;
-		}
+    private static String subStringInput(String regex, String input) {
+        if (regex.equals(BLANK)) {
+            return input;
+        }
 
-		return input.substring(CUSTOM_REGEX_LENGTH);
-	}
+        return input.substring(CUSTOM_REGEX_LENGTH);
+    }
 
-	private static String[] splitAsRegex(String regex, String input) {
-		return input.split("[" + regex + DEFAULT_REGEX + "]");
-	}
+    private static String[] splitAsRegex(String regex, String input) {
+        return input.split("[" + regex + DEFAULT_REGEX + "]");
+    }
 
-	private static boolean isNumber(String input) {
-		if (stringToCharacterStream(input).noneMatch(Character::isDigit)) {
-			throw RUNTIME_EXCEPTION;
-		}
+    private static boolean isNumber(String input) {
+        if (stringToCharacterStream(input).noneMatch(Character::isDigit)) {
+            throw RUNTIME_EXCEPTION;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private static Stream<Character> stringToCharacterStream(String input) {
-		return input.chars().mapToObj(number -> (char)number);
-	}
+    private static Stream<Character> stringToCharacterStream(String input) {
+        return input.chars().mapToObj(number -> (char) number);
+    }
 
-	private static boolean isPositive(int number) {
-		if (number < 0) {
-			throw RUNTIME_EXCEPTION;
-		}
+    private static boolean isPositive(int number) {
+        if (number < 0) {
+            throw RUNTIME_EXCEPTION;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
 }
