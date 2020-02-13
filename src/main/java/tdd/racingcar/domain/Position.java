0835b6748843d2c44496abc8b4779e8b6a0fb610
kouz <lastpuzly@gmail.com>
2020-02-13 11:25:48 +0900

    refactor: equals 메소드 구현

--- src/main/java/tdd/racingcar/domain/Position.java
+++ src/main/java/tdd/racingcar/domain/Position.java
@@ -1,5 +1,7 @@
 package tdd.racingcar.domain;
 
+import java.util.Objects;
+
 public class Position {
 	private static final int MIN_POSITION = 0;
 
@@ -30,5 +32,20 @@ public class Position {
 
 	public boolean isPosition(final int position) {
 		return this.position == position;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Position position1 = (Position)o;
+		return position == position1.position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(position);
 	}
 }
