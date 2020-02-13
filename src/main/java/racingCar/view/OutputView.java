3492d47bddfcf8c93c7901131f4573ca7a193833
junk-sound <wom2277@naver.com>
2020-02-13 12:33:31 +0900

    feat: 시작 문구를 출력한다.

--- src/main/java/racingCar/view/OutputView.java
+++ src/main/java/racingCar/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     void printRacingCar(RacingCar racingCar) {
         System.out.println(racingCar);
     }
+
+    void printStartingComment() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n");
+    }
 }
