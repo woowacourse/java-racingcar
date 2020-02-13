290a60cc8cecd611c1272d65393f31e00026b91d
kouz <lastpuzly@gmail.com>
2020-02-13 09:48:28 +0900

    fix: CarsUtils 분리로 인한 수정

--- src/test/java/tdd/racingcar/domain/CarsTest.java
+++ src/test/java/tdd/racingcar/domain/CarsTest.java
@@ -1,14 +1,14 @@
 package tdd.racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import tdd.racingcar.util.CarsUtils;
 
 public class CarsTest {
 	private List<Car> differentPositionCars;
@@ -26,15 +26,7 @@ public class CarsTest {
 	@Test
 	void getWinnersForEmptyCars() {
 		final Cars cars = new Cars(new ArrayList<>());
-		assertThatIllegalArgumentException().isThrownBy(cars::getWinners)
+		assertThatIllegalArgumentException().isThrownBy(() -> CarsUtils.getWinners(cars))
 			.withMessage("차가 존재하지 않습니다.");
-	}
-
-	@Test
-	void getWinners() {
-		final Cars cars = new Cars(differentPositionCars);
-		final List<Car> expected = Arrays.asList(differentPositionCars.get(0), differentPositionCars.get(1));
-		final List<Car> actual = cars.getWinners();
-		assertEquals(expected, actual);
 	}
 }
