9aea242fc2ea119303e0518e9649ef0280ba9f18
junk-sound <wom2277@naver.com>
2020-02-13 11:54:15 +0900

    refac: 생성한 숫자가 랜덤이 되도록 수정

--- src/main/java/racingCar/domain/RandomNumberCreator.java
+++ src/main/java/racingCar/domain/RandomNumberCreator.java
@@ -1,7 +1,12 @@
 package racingCar.domain;
 
+import java.util.Random;
+
 public class RandomNumberCreator {
     public static int create() {
-        return 0;
+        Random randomNumberGenerator = new Random();
+
+        return randomNumberGenerator.ints(0, 10)
+                .limit(1).findFirst().orElse(-1);
     }
 }
