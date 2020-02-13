e72cfca49f4cb22ac86dfcd39218a92994d1b83a
chws <rebeccachoosir@gmail.com>
2020-02-13 17:22:53 +0900

    feat: 우승자 출력 기능 구현

--- src/main/java/racingCar/view/OutputView.java
+++ src/main/java/racingCar/view/OutputView.java
@@ -1,18 +1,26 @@
 package racingCar.view;
 
+import racingCar.domain.GameManager;
 import racingCar.domain.Player;
 
 import java.util.List;
 
 public class OutputView {
     private static final String RESULT_HEAD = "실행 결과";
-    public static void printResult(List<Player> players) {
+
+    public static void printTitle() {
         System.out.println(RESULT_HEAD);
-        players.forEach(System.out::println);
     }
 
-    public static void printWinners(List<Player> winners) {
-        StringBuilder stringBuilder = new StringBuilder();
+    public static void printEachRound(String string) {
+        System.out.println(string);
+    }
 
+    public static void printWinners(String string) {
+        System.out.println("우승자는 " + string + " 입니다.");
+    }
+
+    public static void printRound(int round) {
+        System.out.println(round);
     }
 }
