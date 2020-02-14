ab0bfb93538fa55ea31785e244dd6d5cc5407515
Sujin <sujinemilycho@gmail.com>
2020-02-14 20:58:14 +0900

    refactor: RandomNumberGenerator의 클래스 변수 상수화

--- src/main/java/racingcar/domian/RandomNumberGenerator.java
+++ src/main/java/racingcar/domian/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
     private static final int RANDOM_LIMIT_NUMBER = 10;
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public static int generateRandom() {
         return random.nextInt(RANDOM_LIMIT_NUMBER);
