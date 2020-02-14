9ae0e56c5c640bed26a8ae77a8c268a2099a793b
Sujin <sujinemilycho@gmail.com>
2020-02-14 21:26:52 +0900

    refactor: Util성 RandomNumberGenerator 생성자 접근제한 추가

--- src/main/java/racingcar/domian/RandomNumberGenerator.java
+++ src/main/java/racingcar/domian/RandomNumberGenerator.java
@@ -6,6 +6,8 @@ public class RandomNumberGenerator {
     private static final int RANDOM_LIMIT_NUMBER = 10;
     private static final Random random = new Random();
 
+    private RandomNumberGenerator() {}
+
     public static int generateRandom() {
         return random.nextInt(RANDOM_LIMIT_NUMBER);
     }
