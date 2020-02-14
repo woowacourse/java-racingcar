0b841eaa58025530ec2d9c009a38c6a6a0c17e6e
Andy H. Jung <commaniakr@gmail.com>
2020-02-14 18:35:17 +0900

    refactor: Number의 테스트

--- src/test/java/calculator/domain/NumberTest.java
+++ src/test/java/calculator/domain/NumberTest.java
@@ -1,6 +1,7 @@
 package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,9 +13,24 @@ public class NumberTest {
     @DisplayName("숫자가 아닌 것을 입력했을 때")
     @ValueSource(strings = {"1a, 2c, 3), 4*, 5&, 6^"})
     public void notNumberIncludedTest(String actual) {
-
         assertThatThrownBy(() -> new Number(actual))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("숫자 이외의 값");
+    }
+
+    @Test
+    @DisplayName("음수를 입력했을 때")
+    public void negativeIntIncludedTest() {
+        assertThatThrownBy(() -> new Number("-1"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("음수");
+    }
+
+    @Test
+    @DisplayName("빈 값을 입력했을 때")
+    public void emptyValueIncludedTest() {
+        assertThatThrownBy(() -> new Number(""))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("빈 값");
     }
 }
