3ede82e2ad81455587c9c47c4e085ce779df3a65
YerinCho <forjustice9839@gmail.com>
2020-02-14 16:44:00 +0900

    [RacingCar] refactor : GameManagerTest 테스트 코드 InputViewTest 로 이동

--- src/test/java/controller/GameManagerTest.java
+++ src/test/java/controller/GameManagerTest.java
@@ -3,7 +3,6 @@ package controller;
 import model.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import view.InputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameManagerTest {
 
-    @DisplayName("이름을 입력받아 ',' 로 분리")
-    @Test
-    public void nameSplit() {
-        String input = "k3,k5,audi";
-        String[] names = InputView.nameSplit(input);
-        assertThat(names).containsExactly("k3", "k5", "audi");
-    }
+    GameManager gameManager = new GameManager();
 
     @DisplayName("입력받은 이름으로 car 객체 생성")
     @Test
