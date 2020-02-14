affd06f1933d1821f49edf024f613346e0df25fc
Andy H. Jung <commaniakr@gmail.com>
2020-02-14 15:58:09 +0900

    feat: 중복된 이름 입력 테스트

--- src/test/java/racingcar/domain/CarFactoryTest.java
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -10,9 +10,18 @@ public class CarFactoryTest {
     @Test
     @DisplayName("차를 한 대만 입력할 경우")
     void oneCarExceptionTest() {
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             CarFactory carFactory = new CarFactory("hiro");
         }).isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("1대만 있으면 경기를 진행할 수 없습니다");
+                .hasMessageContaining("1대만 있으면 경기를 진행할 수 없습니다");
+    }
+
+    @Test
+    @DisplayName("중복된 이름이 있을 경우")
+    void duplicateNameTest() {
+        assertThatThrownBy(() -> {
+            CarFactory carFactory = new CarFactory("hiro, hiro");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 이름");
     }
 }
