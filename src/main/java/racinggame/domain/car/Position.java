4d208dd9443e9c8e6b09d1f1817ddbbd02634368
gunhee lee <dlrjsgml42@naver.com>
2020-02-14 15:48:44 +0900

    feat: Position 클래스 두번째 테스트 구현
    PositionTest position의 음수 입력에 대한 테스트 완료
    Position 테스트에 맞춰 클래스 구현.

--- src/main/java/racinggame/domain/car/Position.java
+++ src/main/java/racinggame/domain/car/Position.java
@@ -3,7 +3,14 @@ package racinggame.domain.car;
 public class Position {
     private int position;
 
+    private void validatePosition(int position){
+        if(position < 0){
+            throw new IllegalArgumentException("위치값은 0 이상이어야 합니다.");
+        }
+    }
+
     public Position(int position) {
+        validatePosition(position);
         this.position = position;
     }
 
--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -5,11 +5,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 import racinggame.domain.car.Position;
 
 public class PositionTest {
+    Position position;
 
     @ParameterizedTest
     @ValueSource(strings = {"10", "5", "0"})
     void 위치값_생성_테스트(int value) {
-        Position position = new Position(value);
+        position = new Position(value);
         Assertions.assertThat(position.match(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-214"})
+    void 위치값_오류_테스트(int value) {
+        /*
+        String 값도 넣어 보려고 했는데, 파라미터가 int형으로 지정되어 있어 NumberFormatException 발생함.
+        이는 position이 아닌 position을 호출하는 상위클래스에서 체크해야할듯....
+        */
+        Assertions.assertThatThrownBy(()->{
+            position = new Position(value);
+        }).isInstanceOf(IllegalArgumentException.class)
+        .hasMessageMatching("위치값은 0 이상이어야 합니다.");
     }
 }
