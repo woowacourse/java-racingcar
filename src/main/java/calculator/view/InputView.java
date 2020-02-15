18e50a93eeb390a5e1b446012c03ebc557cdb51e
begaonnuri <turtlenam0928@gmail.com>
2020-02-16 00:57:08 +0900

    refactor. 입력받는 방식을 개행을 두번 입력하도록 변경
     - 구분자를 명확하게 입력받을 수 있도록 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -9,8 +9,8 @@ public class InputView {
 	}
 
 	public static String inputExpression() {
-		System.out.println("쉼표(,) 또는 콜론(:)으로 구분해 더할 숫자를 입력해주세요.");
+		System.out.println("쉼표(,) 또는 콜론(:)으로 구분해 더할 숫자를 입력하고 \\n을 두번 입력해주세요.");
 		System.out.println("구분자를 따로 지정하시려면 //와 \\n 사이에 구분자를 넣어주세요.(ex //;\\n1;2;3)");
-		return scanner.nextLine();
+		return scanner.useDelimiter("\n\n").next();
 	}
 }
