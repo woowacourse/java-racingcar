3b7d80ba505812a8a6cb7d3f591ee2c6d53691ee
gunhee lee <dlrjsgml42@naver.com>
2020-02-15 16:10:20 +0900

    refactor: Input 내의 repeat를 정수형으로 수정
    Input 기존에 String으로 저장되던 repeat 를 int형으로 변경하였음.
          또한 isLoopDone 메서드를 통해 repeat값을 getter 없이 검사하도록 함.
    RacingGame Input 변경에 맞춰 구조 리팩토링

--- src/main/java/racinggame/controller/Input.java
+++ src/main/java/racinggame/controller/Input.java
@@ -11,42 +11,15 @@ import java.util.List;
  * 또한, 입력되는 두 값(이름 목록, 횟수)을 감싸 변조를 방지하는 역할을 담당한다.
  */
 public class Input {
-    /**
-     * LENGTH_ERROR_MESSAGE 는 문자열 상수 값으로, 이름에 대한 예외처리 발생 시 전달된다.
-     */
+    private String names;
+    private int repeat;
+
     private static final String LENGTH_ERROR_MESSAGE = "이름의 길이는 1자 이상, 5자 이하만 가능합니다.";
-
-    /**
-     * NUMBER_FORMAT_ERROR_MESSAGE 는 문자열 상수 값으로, 횟수에 대한 NumberFormatException 발생 시 전달된다.
-     */
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "횟수는 정수만 가능합니다.";
-
-    /**
-     * NOT_POSITIVE_ERROR_MESSAGE 는 문자열 상수 값으로, 횟수가 음수 혹은 0으로 들어와 예외처리 발생 시 전달된다.
-     */
     private static final String NOT_POSITIVE_ERROR_MESSAGE = "횟수는 1이상의 정수만 가능합니다.";
-
-    /**
-     * SPLIT_DELIMITER 는 문자열 상수 값으로, 입력값을 split하기 위한 구분자로 사용된다.
-     */
     private static final String SPLIT_DELIMITER = ",";
 
-    /**
-     * names는 사용자가 입력하는 문자열 값으로, 쉼표로 구분되는 여러 개의 이름을 저장한다.
-     */
-    private String names;
 
-    /**
-     * repeat는 사용자가 입력하는 정수 값으로, 경주가 반복될 횟수를 뜻하는 0보다 큰 정수를 String 인스턴스로 저장한다.
-     * 예외처리 등을 위해 일단 String으로 저장하고, 필요 시 이를 parseInt한 값을 메서드를 통해 전달함으로써
-     * 원래 값에 대한 보호와 예외상황 방지를 하도록 설계되었다.
-     */
-    private String repeat;
-
-    /**
-     * 생성자 메서드는 파라미터가 없을 경우, InputView를 통해 이름과 횟수를 전달받는다.
-     * 이후 이 값을 오버로딩된 다른 생성자 메서드에 전달한다.
-     */
     public Input() {
         this(InputView.inputName(), InputView.inputRepeat());
     }
@@ -64,9 +37,7 @@ public class Input {
         this.names = inputName;
         validateNames();
 
-        this.repeat = inputRepeat;
-        validateRepeat();
-        validateRepeatPositive();
+        this.repeat = validateRepeat(inputRepeat);
     }
 
     /**
@@ -80,14 +51,8 @@ public class Input {
         return Arrays.asList(splitName);
     }
 
-    /**
-     * parseRepeatToInt 는 문자열로 필드에 저장되어 있는 횟수 정보를
-     * 정수형으로 변환하여 반환한다. Input 바깥에서 이 값에 접근할 때 호출된다.
-     *
-     * @return 변환된 정수형 횟수 값을 반환한다.
-     */
-    public int parseRepeatToInt() {
-        return Integer.parseInt(repeat);
+    public boolean isLoopDone(int repeat){
+        return repeat < this.repeat;
     }
 
     /**
@@ -106,7 +71,7 @@ public class Input {
      * validateNames 는 이름의 목록 데이터를 분석하여, 목록 자체가 비어있거나 null인 경우 예외를 발생시킨다.
      * 또한, 각각의 이름 데이터에 문제가 있는지 validateNameLength를 호출하여 검사한다.
      */
-    public void validateNames() {
+     private void validateNames() {
         List<String> list = splitInputByComma();
         if (list == null) {
             throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
@@ -118,9 +83,11 @@ public class Input {
      * validateRepeat 는 반복횟수 값에 대한 예외처리 사항을 확인하여 문제 시 예외를 발생시킨다.
      * Input의 필드에 있는 repeat 값이 정수값이 아니어서 parseInt가 불가한 경우가 이에 해당한다.
      */
-    private void validateRepeat() {
+    private int validateRepeat(String inputRepeat) {
         try {
-            Integer.parseInt(repeat);
+            int repeat = Integer.parseInt(inputRepeat);
+            validateRepeatPositive(repeat);
+            return repeat;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
         }
@@ -130,9 +97,8 @@ public class Input {
      * validateRepeatPositive 는 반복횟수 값에 대한 예외처리 사항을 확인하여 문제 시 예외를 발생시킨다.
      * 만약 반복 횟수가 0이나 음수일 경우, 이는 비정상적인 값으로 판단되어 메서드에서 예외를 발생시킨다.
      */
-    private void validateRepeatPositive() {
-        int castingRepeat = parseRepeatToInt();
-        if (castingRepeat <= 0) {
+    private void validateRepeatPositive(int inputRepeat) {
+        if (inputRepeat <= 0) {
             throw new IllegalArgumentException(NOT_POSITIVE_ERROR_MESSAGE);
         }
     }
--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -63,12 +63,11 @@ public class RacingGame {
     public static void play(Input input, Output output) {
         List<String> names = input.splitInputByComma();
         List<Car> cars = new ArrayList<>();
-        int repeat = input.parseRepeatToInt();
 
         output.initCarStatus(names);
         names.stream().forEach(x -> cars.add(new Car(x)));
         OutputView.printResultFormat();
-        moveCars(output, cars, repeat);
+        moveCars(input,output, cars);
         output.makeWinnerNames(cars);
     }
 
@@ -82,8 +81,10 @@ public class RacingGame {
      * @param cars   Car 객체를 저장하는 List 이다.
      * @param repeat 반복할 횟수를 저장하는 정수형 변수이다.
      */
-    private static void moveCars(Output output, List<Car> cars, int repeat) {
-        for (int i = 0; i < repeat; i++) {
+    private static void moveCars(Input input, Output output, List<Car> cars) {
+        int repeat = 0;
+        while(input.isLoopDone(repeat)){
+            repeat++;
             cars.stream().forEach(car -> {
                 car.accelerate(generateRandomNumber(), FORWARD_NUMBER);
                 car.passingLog(output);
