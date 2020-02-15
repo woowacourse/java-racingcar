a01c5d5772e8e4343bd3f6d49c7b91c2f4ce7af3
woonjangahn <woonjangahn@gmail.com>
2020-02-15 16:43:53 +0900

    refactor : Game 객체 컨트롤러로 이전

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import racingcar.domain.Game;
+import racingcar.controller.Game;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,5 +1,8 @@
-package racingcar.domain;
+package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.domain.Times;
+import racingcar.domain.Winners;
 import racingcar.io.InputView;
 import racingcar.io.OutputView;
 
@@ -8,7 +11,15 @@ public class Game {
         Cars cars = InputView.getCars();
         Times times = InputView.getTimes();
         OutputView.printResultTitle();
-        cars.move(times);
+        move(cars, times);
         OutputView.printWinners(new Winners(cars.getWinners()));
+    }
+
+    public void move(Cars cars, Times times) {
+        while (!times.isDone()) {
+            cars.move();
+            times.reduce();
+            OutputView.printCars(cars);
+        }
     }
 }
