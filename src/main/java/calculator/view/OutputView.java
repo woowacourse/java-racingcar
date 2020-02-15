971edffce705efc6d991c06fdf45e098fb5818f2
begaonnuri <turtlenam0928@gmail.com>
2020-02-15 17:38:38 +0900

    feat. 결과를 출력하는 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+}
