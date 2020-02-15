41382af21d2ac8a5f33489e688edce1c94771035
junk-sound <wom2277@naver.com>
2020-02-15 15:30:44 +0900

    refac: 피드백 반영 5 - 매번 함수를 호출할 때마다 Random 객체를 생성하는 대신, 인스턴스 변수로 옮김

--- src/main/java/racingCar/domain/RandomNumberCreator.java
+++ src/main/java/racingCar/domain/RandomNumberCreator.java
@@ -3,8 +3,8 @@ package racingCar.domain;
 import java.util.Random;
 
 public class RandomNumberCreator {
+    private static Random randomNumberGenerator = new Random();
     public static int create() {
-        Random randomNumberGenerator = new Random();
 
         return randomNumberGenerator.ints(0, 10)
                 .limit(1).findFirst().orElse(-1);
