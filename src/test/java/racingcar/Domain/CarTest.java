255fb14d46ab923fe7a8bf99d96b00b86c8399a7
toneyparky <qkrtmddhks95@gmail.com>
2020-02-16 07:48:31 +0900

    test: Car 생성자 테스트

--- src/test/java/racingcar/Domain/CarTest.java
+++ src/test/java/racingcar/Domain/CarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 /**
  * 클래스 이름 : CarTest.java
@@ -21,8 +22,19 @@ public class CarTest {
 	private static Car car;
 
 	@BeforeEach
-	static void setUp() {
+	void setUp() {
 		car = new Car("보스독");
+	}
+
+	@Test
+	@DisplayName("Car 생성자 테스트")
+	void Car_생성자_정상_케이스_테스트() {
+		//given
+		String inputCarName = "toney";
+		//when
+		Car car = new Car(inputCarName);
+		//then
+		assertThat(car).isInstanceOf(Car.class);
 	}
 
 	@Test
