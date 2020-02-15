f2942d014b9ff98751a7c83f8ffed663f9c5cc7d
lalize <printf@kakao.com>
2020-02-15 12:41:55 +0900

    refactor: Car.position final로 선언하도록 수정

--- src/main/java/tdd/racingcar/domain/Car.java
+++ src/main/java/tdd/racingcar/domain/Car.java
@@ -4,7 +4,7 @@ public class Car {
 	private static final int DEFAULT_POSITION = 0;
 
 	private final Name name;
-	private Position position;
+	private final Position position;
 
 	public Car(final String name) {
 		this(name, DEFAULT_POSITION);
