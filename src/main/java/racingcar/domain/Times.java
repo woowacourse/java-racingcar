8866b0c354757e1ce13738c63a71eee4549732f0
woonjangahn <woonjangahn@gmail.com>
2020-02-15 16:43:17 +0900

    refactor : Exception throw 삭제 및 IllegalArgumentException으로 변경

--- src/main/java/racingcar/domain/Times.java
+++ src/main/java/racingcar/domain/Times.java
@@ -4,10 +4,10 @@ public class Times {
     public static final int MINIMUM = 1;
     private int times;
 
-    public Times(String times) throws Exception {
+    public Times(String times) {
         int parsed = Integer.parseInt(times);
         if (parsed < MINIMUM) {
-            throw new Exception();
+            throw new IllegalArgumentException("올바르지 않은 시간 형식입니다.");
         }
         this.times = parsed;
     }
