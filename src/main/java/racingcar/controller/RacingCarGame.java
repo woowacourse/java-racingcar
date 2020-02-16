d6627e51aaa35a9e6963856faf07964abd1954ed
ParkDooWon <pass345@naver.com>
2020-02-16 18:36:37 +0900

    refactor : 자동차 이름과 시도할 회수 입력받는 부분을 InputView 클래스 안으로 이동

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -7,8 +7,8 @@ import racingcar.view.OutputView;
 
 public class RacingCarGame {
     public static void main(String[] args) {
-        Cars cars = inputCarNames();
-        Round round = inputRoundNumber();
+        Cars cars = InputView.inputCarNames();
+        Round round = InputView.inputRoundNumber();
 
         System.out.println("실행 결과");
         playRounds(cars, round);
@@ -25,28 +25,6 @@ public class RacingCarGame {
             result = cars.getResult();
             OutputView.printResult(result);
             System.out.println();
-        }
-    }
-
-    private static Cars inputCarNames() {
-        try {
-            System.out.println("경주할 자동차 이름을 입력해주세요.(이름은 쉼표(,)를 기준으로 구분합니다.)");
-            String inputForNames = InputView.inputValue();
-            return new Cars(inputForNames);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            return inputCarNames();
-        }
-    }
-
-    private static Round inputRoundNumber() {
-        try {
-            System.out.println("시도할 회수는 몇회인가요?");
-            String inputForRound = InputView.inputValue();
-            return new Round(inputForRound);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            return inputRoundNumber();
         }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import racingcar.domain.Round;
+import racingcar.domain.car.Cars;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -7,5 +10,27 @@ public class InputView {
 
     public static String inputValue() {
         return scanner.nextLine();
+    }
+
+    public static Cars inputCarNames() {
+        try {
+            System.out.println("경주할 자동차 이름을 입력해주세요.(이름은 쉼표(,)를 기준으로 구분합니다.)");
+            String inputForNames = inputValue();
+            return new Cars(inputForNames);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            return inputCarNames();
+        }
+    }
+
+    public static Round inputRoundNumber() {
+        try {
+            System.out.println("시도할 회수는 몇회인가요?");
+            String inputForRound = inputValue();
+            return new Round(inputForRound);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            return inputRoundNumber();
+        }
     }
 }
