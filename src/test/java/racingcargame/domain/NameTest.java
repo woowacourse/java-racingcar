573ef2945675529c5254f415b15004a65466ba5b
icyMojito <dev.icymojito@gmail.com>
2020-02-17 00:19:38 +0900

    [test] Name 클래스 테스트 수정
    Name 클래스에 필요한 검증 테스트를 추가

--- src/test/java/racingcargame/domain/NameTest.java
+++ src/test/java/racingcargame/domain/NameTest.java
@@ -1,41 +1,45 @@
 package racingcargame.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import racingcargame.exception.CarNameInputException;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class NameTest {
 
-    @Test
-    void 자동차_이름이_중복되었을_때() {
-        assertThatThrownBy(() -> {
-            new Name("pobi");
-            new Name("pobi");
-        }).isInstanceOf(RuntimeException.class);
-    }
+	@Test
+	void null값이_들어왔을때() {
+		assertThatThrownBy(() -> {
+			new Name(null);
+		}).isInstanceOf(CarNameInputException.class);
+	}
 
-    @Test
-    void 자동차_이름의_길이가_5를_초과할때() {
-        assertThatThrownBy(() -> {
-            new Name("123456789");
-        })
-                .isInstanceOf(RuntimeException.class);
-    }
+	@ParameterizedTest
+	@ValueSource(strings = {"abcdef", "1234567"})
+	void 자동차_이름의_길이가_5를_초과할때(String input) {
+		assertThatThrownBy(() -> {
+			new Name(input);
+		}).isInstanceOf(CarNameInputException.class);
+	}
 
-    @Test
-    void 쉼표만_입력되었을_때() {
-        assertThatThrownBy(() -> {
-            new Name("");
-        })
-                .isInstanceOf(RuntimeException.class);
-    }
+	@Test
+	void 자동차_이름이_공백일때() {
+		assertThatThrownBy(() -> {
+			new Name("");
+		}).isInstanceOf(CarNameInputException.class);
+	}
 
-    @Test
-    void 같은_이름인지_확인() {
-        Name name = new Name("brown");
-        boolean result = name.equals("brown");
-        assertThat(result).isTrue();
-    }
-
+	@ParameterizedTest
+	@CsvSource(value = {"brown:true", "broil:false"}, delimiter = ':')
+	void 같은_이름인지_확인(String input, boolean expected) {
+		Name name1 = new Name("brown");
+		Name name2 = new Name(input);
+		boolean result = name1.equals(name2);
+		assertThat(result).isEqualTo(expected);
+	}
 }
