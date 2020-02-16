ad21dc603672cecf6a421d3abda5d62c561967d2
yeonnseok <ljinsk3@gmail.com>
2020-02-16 16:22:39 +0900

    feat: 시도횟수 숫자가 아닐경우 예외처리

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String DELIMITER = ",";
 
     public static String[] inputCarNames(){
+        OutputView.printInputCarNamesMessage();
         String carNames = scanner.nextLine();
         if (carNames == null || carNames.isEmpty()){
             throw new NullPointerException("차 이름이 입력되지 않았습니다.");
@@ -21,8 +22,31 @@ public class InputView {
     }
 
     public static TrialTime inputTrialTime() {
-        int trialTime = scanner.nextInt();
-        return new TrialTime(trialTime);
+        OutputView.printInputTrialTimeMessage();
+        String trialTime = scanner.nextLine();
+        try {
+            checkTrialTimeNullOrEmpty(trialTime);
+            checkTrialTimeNotNumber(trialTime);
+            int trialTimeIntegerValue = Integer.parseInt(trialTime);
+            return new TrialTime(trialTimeIntegerValue);
+        } catch(Exception e){
+            OutputView.printExceptionMessage(e);
+        }
+        return inputTrialTime();
+    }
+
+    private static void checkTrialTimeNotNumber(String trialTime){
+        try {
+            int trialTimeIntegerValue = Integer.parseInt(trialTime);
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("시도횟수는 정수를 입력해주세요.");
+        }
+    }
+
+    private static void checkTrialTimeNullOrEmpty(String trialTime) {
+        if (trialTime == null || trialTime.isEmpty()){
+            throw new NullPointerException("시도 횟수가 입력되지 않았습니다.");
+        }
     }
 }
 
---
+++ src/main/java/racing/view/OutputView.java
@@ -0,0 +1,15 @@
+package racing.view;
+
+public class OutputView {
+    public static void printInputCarNamesMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세(이름은 쉼표(,)를 기준으로 구분");
+    }
+
+    public static void printInputTrialTimeMessage() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
+
+    public static void printExceptionMessage(Exception e) {
+        System.out.println(e.getMessage());
+    }
+}
