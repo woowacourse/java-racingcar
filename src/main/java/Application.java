cb738ba20a098d2c4b5dd17a35dfe02f8d2e3553
JunSeong <ebseud6135@naver.com>
2020-02-17 00:57:59 +0900

    refactor : Application 수정
    메인 메소드에서 게임관련 로직을 최소화

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,39 +1,27 @@
-import domain.*;
+import domain.GameManager;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
-    public static void main(String[] args) {
-        Cars cars = createCarsByInput();
-        AttemptNumber attemptNumber = createAttemptNumberByInput();
+	public static void main(String[] args) {
+		String carsName = InputView.inputCarNames();
+		String attemptNo = InputView.inputAttemptNumber();
 
-        GameManager gameManager = new GameManager(cars, attemptNumber);
-        gameManager.playGame();
+		GameManager gameManager = createGameManager(carsName, attemptNo);
+		while (!gameManager.isEnd()) {
+			OutputView.printRacingResults(gameManager.race());
+		}
+		OutputView.printWinners(gameManager.findWinners());
+	}
 
-        int maxPosition = cars.getMaxPosition();
-        Cars winners = new Cars(cars.findByPosition(maxPosition));
-        OutputView.printWinners(winners);
-    }
-
-    private static Cars createCarsByInput() {
-        while(true) {
-            try {
-                String carNames = InputView.inputCarNames();
-                return new Cars(CarGenerator.create(carNames));
-            } catch (IllegalArgumentException e) {
-                OutputView.printExceptionMsg(e.getMessage());
-            }
-        }
-    }
-
-    private static AttemptNumber createAttemptNumberByInput() {
-        while(true) {
-            try {
-                return new AttemptNumber(InputView.inputAttemptNumber());
-            } catch (IllegalArgumentException e) {
-                OutputView.printExceptionMsg(e.getMessage());
-            }
-        }
-    }
+	private static GameManager createGameManager(String carNames, String attemptNo) {
+		while (true) {
+			try {
+				return new GameManager(carNames, attemptNo);
+			} catch (IllegalArgumentException e) {
+				OutputView.printExceptionMsg(e.getMessage());
+			}
+		}
+	}
 
 }
