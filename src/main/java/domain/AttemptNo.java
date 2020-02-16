678f6e974495dfa80016b14989389f3a762f6840
JunSeong <ebseud6135@naver.com>
2020-02-17 00:49:15 +0900

    refactor / test 반복횟수 저장할 클래스 수정
    끝났는지 여부를 체크하는 기능과 반복횟수를 차감하는 기능 추가

--- src/main/java/domain/AttemptNo.java
+++ src/main/java/domain/AttemptNo.java
@@ -1,37 +1,41 @@
 package domain;
 
-public class AttemptNumber {
-    private static final String NOT_INTEGER_MESSAGE = "정수가 아닙니다.";
-    private static final String NOT_POSITIVE_MESSAGE = "양수가 아닙니다.";
-    private static final int MIN_ATTEMPT_NUMBER = 1;
+public class AttemptNo {
+	private static final String NOT_INTEGER_MESSAGE = "정수가 아닙니다.";
+	private static final String NOT_POSITIVE_MESSAGE = "양수가 아닙니다.";
+	private static final int MIN_ATTEMPT_NUMBER = 1;
 
-    private int number;
+	private int number;
 
-    public AttemptNumber(final String number) {
-        validateInteger(number);
-        validatePositive(number);
-        this.number = Integer.parseInt(number);
-    }
+	public AttemptNo(final String number) {
+		validateInteger(number);
+		validatePositive(number);
+		this.number = Integer.parseInt(number);
+	}
 
-    private void validateInteger(final String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
-        }
-    }
+	private void validateInteger(final String number) {
+		try {
+			Integer.parseInt(number);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
+		}
+	}
 
-    private void validatePositive(final String number) {
-        if (Integer.parseInt(number) < MIN_ATTEMPT_NUMBER) {
-            throw new IllegalArgumentException(NOT_POSITIVE_MESSAGE);
-        }
-    }
+	private void validatePositive(final String number) {
+		if (Integer.parseInt(number) < MIN_ATTEMPT_NUMBER) {
+			throw new IllegalArgumentException(NOT_POSITIVE_MESSAGE);
+		}
+	}
 
-    public boolean isEnd() {
-        if (this.number >= MIN_ATTEMPT_NUMBER) {
-            this.number--;
-            return true;
-        }
-        return false;
-    }
+	public boolean execute() {
+		if (!isEnd()) {
+			this.number--;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isEnd() {
+		return this.number < MIN_ATTEMPT_NUMBER;
+	}
 }
--- src/test/java/domain/AttemptNoTest.java
+++ src/test/java/domain/AttemptNoTest.java
@@ -1,36 +1,51 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+public class AttemptNoTest {
+	@Test
+	void createAttemptNumber_적합한_횟수() {
+		new AttemptNo("1");
+	}
 
-public class AttemptNumberTest {
-    @Test
-    void createAttemptNumber_적합한_횟수() {
-        new AttemptNumber("1");
-    }
+	@Test
+	void createAttemptNumber_부적합한_횟수_문자() {
+		assertThatThrownBy(() -> new AttemptNo("타미"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @Test
-    void createAttemptNumber_부적합한_횟수_문자() {
-        assertThatThrownBy(() -> new AttemptNumber("타미"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@Test
+	void createAttemptNumber_부적합한_횟수_음수() {
+		assertThatThrownBy(() -> new AttemptNo("-1"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @Test
-    void createAttemptNumber_부적합한_횟수_음수() {
-        assertThatThrownBy(() -> new AttemptNumber("-1"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@Test
+	void isEndTest_1이상() {
+		AttemptNo attemptNo = new AttemptNo("1");
+		assertThat(attemptNo.isEnd()).isFalse();
+	}
 
-    @Test
-    void isEndTest_성공() {
-        assertThat(new AttemptNumber("1").isEnd()).isTrue();
-    }
+	@Test
+	void isEndTest_1미만() {
+		AttemptNo attemptNo = new AttemptNo("1");
+		attemptNo.execute();        // No -> 0
+		assertThat(attemptNo.isEnd()).isTrue();
+	}
 
-    @Test
-    void isEndTest_실패() {
-        assertThat(new AttemptNumber("0").isEnd()).isFalse();
-    }
+	@Test
+	void executeTest_실행_가능할_때() {
+		AttemptNo attemptNo = new AttemptNo("1");
+		assertThat(attemptNo.execute()).isTrue();
+	}
 
+	@Test
+	void executeTest_실행_불가능할_때() {
+		AttemptNo attemptNo = new AttemptNo("1");
+		attemptNo.execute();		// No -> 0
+		assertThat(attemptNo.execute()).isFalse();
+	}
 }
