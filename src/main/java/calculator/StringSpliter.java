aca74c25f5e0268c5f830142e56d53d444f8bd9d
Tak Hyungmin <milkho11@naver.com>
2020-02-17 02:40:45 +0900

    [refact] Pattern  상수화

--- src/main/java/calculator/StringSpliter.java
+++ src/main/java/calculator/StringSpliter.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 public class StringSpliter {
     private static final String OR_OPERATOR = "|";
+    
     private static String delimiter = "," + OR_OPERATOR + ":";
     private static final String CUSTOM_DELIMITER_EXPRESSION = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_EXPRESSION);
 
     public static String[] split(String input) {
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_EXPRESSION).matcher(input);
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (m.find()) {
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter);
