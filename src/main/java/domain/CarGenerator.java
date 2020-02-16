7a381400d4cc8b8e2e951494dcdd6c05706b0c1e
JunSeong <ebseud6135@naver.com>
2020-02-17 00:50:18 +0900

    refactor : CarGenerator 수정
    입력받은 이름을 Cars로 바꿔주는 기능을 Stream을 사용하도록 변경

--- src/main/java/domain/CarGenerator.java
+++ src/main/java/domain/CarGenerator.java
@@ -1,26 +1,28 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarGenerator {
-    private static final String COMMA = ",";
-    private static final String NOT_INPUT_MESSAGE = "이름을 입력하지 않았습니다.";
+	private static final String COMMA = ",";
+	private static final int MIN_LENGTH = 1;
+	private static final String NOT_INPUT_MESSAGE = "이름을 입력하지 않았습니다.";
 
-    public static List<Car> create(String carsName) {
-        String[] carNames = carsName.split(COMMA);
-        validateLength(carNames);
+	public static List<Car> create(String carsName) {
+		String[] carNames = carsName.split(COMMA);
+		validateLength(carNames);
 
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        return cars;
-    }
+		List<Car> cars = new ArrayList<>();
+		Arrays.stream(carNames)
+			.forEach(name -> cars.add(new Car(name)));
 
-    private static void validateLength(String[] carNames) {
-        if (carNames.length == 0) {
-            throw new IllegalArgumentException(NOT_INPUT_MESSAGE);
-        }
-    }
+		return cars;
+	}
+
+	private static void validateLength(String[] carNames) {
+		if (carNames.length < MIN_LENGTH) {
+			throw new IllegalArgumentException(NOT_INPUT_MESSAGE);
+		}
+	}
 }
