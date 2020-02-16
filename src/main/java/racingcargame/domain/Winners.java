5b75b424578bb1d6dfc6364d3f604b1822d4947f
icyMojito <dev.icymojito@gmail.com>
2020-02-17 00:47:57 +0900

    [refactor] Winners 클래스
    필요없는 메서드 제거

--- src/main/java/racingcargame/domain/Winners.java
+++ src/main/java/racingcargame/domain/Winners.java
@@ -1,25 +1,19 @@
 package racingcargame.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Winners {
-    private final List<Car> winners = new ArrayList<>();
+	private final List<Car> winners = new ArrayList<>();
 
-    public Winners(List<Car> winnerCars) {
-        winners.addAll(winnerCars);
-    }
+	public Winners(List<Car> winnerCars) {
+		winners.addAll(winnerCars);
+	}
 
-    public boolean contain(String name) {
-        return winners.stream()
-                .anyMatch(car -> car.isSame(name));
-    }
-
-    public List<String> getNames() {
-        return winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.toList());
-    }
+	public List<String> getNames() {
+		return winners.stream()
+					  .map(Car::getName)
+					  .collect(Collectors.toList());
+	}
 }
