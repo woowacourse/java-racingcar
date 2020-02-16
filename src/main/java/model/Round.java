b999b2ff837a5a2865b0fb3a6430c8d61e483b02
YerinCho <forjustice9839@gmail.com>
2020-02-16 21:13:56 +0900

    [RacingCar] feat : Round
     생성자 추가

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -3,6 +3,12 @@ package model;
 public class Round {
 
     public static final String ROUND_INPUT_ERROR_MESSAGE = "라운드 입력이 잘못되었습니다.";
+    private final int round;
+
+    private Round(int round) {
+        validateRound(round);
+        this.round = round;
+    }
 
     static void validateRound(int round) {
         if (round <= 0) {
