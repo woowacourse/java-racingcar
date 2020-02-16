c3c64cb004e7a05f03111c11406473399c61790b
JunSeong <ebseud6135@naver.com>
2020-02-17 00:56:27 +0900

    refactor : GameManager 수정
    우승자를 찾는 로직을 GameManager에서 담당하도록 수정
    레이싱할 때 RacingResult를 리턴하도록 수정

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -1,40 +1,47 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-import view.OutputView;
-
 public class GameManager {
-    private static final int RANDOM_BOUND = 10;
+	private static final int RANDOM_BOUND = 10;
+	private static final String ATTEMPT_NO_EXCESS = "실행 횟수를 초과하였습니다.";
 
-    private final Random random;
-    private final Cars cars;
-    private final AttemptNumber attemptNumber;
+	private final Random random;
+	private final Cars cars;
+	private final AttemptNo attemptNo;
 
-    public GameManager(Cars cars, AttemptNumber attemptNumber) {
-        this.cars = cars;
-        this.attemptNumber = attemptNumber;
-        this.random = new Random();
-    }
+	public GameManager(String carNames, String attemptNumber) {
+		this.cars = new Cars(CarGenerator.create(carNames));
+		this.attemptNo = new AttemptNo(attemptNumber);
+		this.random = new Random();
+	}
 
-    public void playGame() {
-        int repeatCount = attemptNumber.getNumber();
-        for (int count = 0; count < repeatCount; count++) {
-            playOneRound();
-        }
-    }
+	public RacingResults race() {
+		if (attemptNo.execute()) {
+			List<RacingResult> results = new ArrayList<>();
+			int carsSize = cars.getSize();
+			for (int count = 0; count < carsSize; count++) {
+				RacingResult racingResult = cars.moveByIndex(count, new RandomNo(getRandomNumber()));
+				results.add(racingResult);
+			}
+			return new RacingResults(results);
+		}
+		throw new IllegalArgumentException(ATTEMPT_NO_EXCESS);
+	}
 
-    private void playOneRound() {
-        int carsSize = cars.getSize();
-        for (int count = 0; count < carsSize; count++) {
-            Car car = cars.getCar(count);
-            car.move(getRandomNumber());
-            OutputView.printCurrentPosition(car.getName(), car.getPosition());
-        }
-        OutputView.println();
-    }
+	public Cars findWinners() {
+		int maxPosition = cars.getMaxPosition();
+		List<Car> winners = cars.findByPosition(maxPosition);
+		return new Cars(winners);
+	}
 
-    private int getRandomNumber() {
-        return random.nextInt(RANDOM_BOUND);
-    }
+	private int getRandomNumber() {
+		return random.nextInt(RANDOM_BOUND);
+	}
+
+	public boolean isEnd() {
+		return attemptNo.isEnd();
+	}
 }
