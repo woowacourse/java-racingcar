9e929320f2aac730338d6b8019503d11bf82ba6b
Andy H. Jung <commaniakr@gmail.com>
2020-02-17 02:20:00 +0900

    refactor: 상수 MAX를 클래스 변수로 변경

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,8 +2,9 @@ package racingcar.util;
 
 import java.util.Random;
 
+
 public class RandomNumberGenerator implements NumberGenerator {
-    int MAX = 9;
+    private static final int MAX = 9;
     
     @Override
     public int generateNumber() {
