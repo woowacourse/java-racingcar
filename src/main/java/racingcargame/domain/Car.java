e14822f4a73c5027451a76588d5b227b1b554f80
icyMojito <dev.icymojito@gmail.com>
2020-02-16 23:41:12 +0900

    [refactor] Car 클래스
    - 생성자의 중복을 제거하여 기본 생성자만 관리하면 되도록 수정
    - 최대한 간단명료한 의미 전달을 위해 메서드명 수정

--- src/main/java/racingcargame/domain/Car.java
+++ src/main/java/racingcargame/domain/Car.java
@@ -1,47 +1,38 @@
 package racingcargame.domain;
 
 public class Car implements Comparable<Car> {
-    private final Name name;
-    private int position;
+	private final Name name;
+	private int position;
 
-    public Car(String name) {
-        this.name = new Name(name);
-        this.position = 0;
-    }
+	public Car(String name) {
+		this(name, 0);
+	}
 
-    public Car(String name, int position) {
-        this.name = new Name(name);
-        this.position = position;
-    }
+	public Car(String name, int position) {
+		this.name = new Name(name);
+		this.position = position;
+	}
 
-    public void go(Engine engine) {
-        if (engine.isHigherThanLimit()) {
-            position++;
-        }
-    }
+	public void go(Engine engine) {
+		if (engine.canMove()) {
+			position++;
+		}
+	}
 
-    public boolean isSameToPosition(int position) {
-        return this.position == position;
-    }
+	public boolean isSamePositionWith(Car car) {
+		return this.position == car.position;
+	}
 
-    public boolean isSameToPosition(Car car) {
-        return this.position == car.position;
-    }
+	public int getPosition() {
+		return position;
+	}
 
-    public int getPosition() {
-        return position;
-    }
+	public String getName() {
+		return name.getName();
+	}
 
-    public String getName() {
-        return name.getName();
-    }
-
-    @Override
-    public int compareTo(Car o) {
-        return Integer.compare(this.position, o.position);
-    }
-
-    public boolean isSame(String name) {
-        return this.name.equals(name);
-    }
+	@Override
+	public int compareTo(Car o) {
+		return Integer.compare(this.position, o.position);
+	}
 }
