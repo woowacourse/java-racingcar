467d15d0b2e2a277d791c611e974bfd4bf304755
JunSeong <ebseud6135@naver.com>
2020-02-17 00:57:33 +0900

    refactor : OutputView 수정
    RacingResult가 생김으로 인해 OutputView의 역할 축소

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,43 +1,20 @@
 package view;
 
-import domain.Car;
 import domain.Cars;
-
-import java.util.List;
-import java.util.ArrayList;
+import domain.RacingResults;
 
 public class OutputView {
-    private static final char MOVE_SIGN = '-';
 
-    public static void printCurrentPosition(String name, int position) {
-        String carState = String.format("%s : %s", name, makeMoveSigns(position));
-        System.out.println(carState);
-    }
+	public static void printRacingResults(RacingResults racingResults) {
+		System.out.println(racingResults.toString());
+		System.out.println();
+	}
 
-    private static String makeMoveSigns(int position) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < position; i++) {
-            builder.append(MOVE_SIGN);
-        }
-        return builder.toString();
-    }
+	public static void printWinners(Cars winners) {
+		System.out.println(winners.toString() + "(이)가 우승했습니다.");
+	}
 
-    public static void printWinners(Cars winners) {
-        int winnerSize = winners.getSize();
-        List<String> winnerNames = new ArrayList<>();
-        for (int count = 0; count < winnerSize; count++) {
-            Car winner = winners.getCar(count);
-            winnerNames.add(winner.getName());
-        }
-        String formattedName = String.join(",", winnerNames);
-        System.out.println(formattedName + "(이)가 우승했습니다.");
-    }
-
-    public static void println() {
-        System.out.println();
-    }
-
-    public static void printExceptionMsg(String msg) {
-        System.out.println(msg);
-    }
+	public static void printExceptionMsg(String msg) {
+		System.out.println(msg);
+	}
 }
