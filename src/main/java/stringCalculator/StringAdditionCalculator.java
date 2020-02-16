2347c4f7189c3216744ef64c831a6a7a39c7c3f1
kuenhwi <kueniyam@naver.com>
2020-02-16 14:43:23 +0900

    refactor: StringParser 클래스 리팩토링

--- src/main/java/stringCalculator/StringAdditionCalculator.java
+++ src/main/java/stringCalculator/StringAdditionCalculator.java
@@ -1,13 +1,11 @@
 package stringCalculator;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class StringAdditionCalculator {
     public static int sum(List<Integer> operands) {
         return operands.stream()
-                .mapToInt((t) -> t)
+                .mapToInt(Integer::intValue)
                 .sum();
     }
 }
--- src/main/java/stringCalculator/StringParser.java
+++ src/main/java/stringCalculator/StringParser.java
@@ -1,11 +1,12 @@
 package stringCalculator;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringParser {
-    public static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    public static final String DEFAULT_DELIMITER_REGEX = "[,:]";
     public static final String FRONT_SIGN = "//";
     public static final String BACK_SIGN = "\\n";
     public static final int TWO_STEP = 2;
@@ -14,44 +15,51 @@ public class StringParser {
     }
 
     public static List<Integer> parse(String input) {
-        String[] parsedArray;
-        List<String> parsedList;
-
         if (hasSign(input)) {
             return parseWithCustomDelimiter(input);
         }
 
-        parsedArray = input.split(DEFAULT_DELIMITER_REGEX);
-        parsedList = Arrays.asList(parsedArray);
+        return parseWithDefaultDelimiter(input);
+    }
 
-        return splitStringListToIntegerList(parsedList);
+    private static List<Integer> parseWithDefaultDelimiter(String input) {
+        String[] parsed = input.split(DEFAULT_DELIMITER_REGEX);
+
+        return convertParsedStringsToIntegers(Arrays.asList(parsed));
     }
 
     private static List<Integer> parseWithCustomDelimiter(String input) {
-        int startOfCustomDelimiter = input.indexOf(FRONT_SIGN) + TWO_STEP;
-        int endOfCustomDelimiter = input.indexOf(BACK_SIGN);
-        String customDelimiter = input.substring(startOfCustomDelimiter, endOfCustomDelimiter);
-        String query = input.substring(endOfCustomDelimiter + TWO_STEP);
-        String[] split = query.split(DEFAULT_DELIMITER_REGEX + "|" + customDelimiter);
-        List<String> result = Arrays.asList(split);
-        return splitStringListToIntegerList(result);
+        String query = getQuery(input);
+        String customDelimiter = getCustomDelimiter(input);
+
+        String[] parsed = query.split(DEFAULT_DELIMITER_REGEX + "|" + customDelimiter);
+
+        return convertParsedStringsToIntegers(Arrays.asList(parsed));
     }
 
-    private static List<Integer> splitStringListToIntegerList(List<String> split) {
-        if (split.equals(Arrays.asList(new String[]{""}))) {
-            return Arrays.asList(new Integer[]{0});
+    private static String getQuery(String input) {
+        int startOfQuery = input.indexOf(BACK_SIGN) + TWO_STEP;
+        return input.substring(startOfQuery);
+    }
+
+    private static String getCustomDelimiter(String input) {
+        int startOfCustomDelimiter = input.indexOf(FRONT_SIGN) + TWO_STEP;
+        int endOfCustomDelimiter = input.indexOf(BACK_SIGN);
+        return input.substring(startOfCustomDelimiter, endOfCustomDelimiter);
+    }
+
+    private static List<Integer> convertParsedStringsToIntegers(List<String> parsed) {
+        if (parsed.get(0).isEmpty()) {
+            return Collections.singletonList(0);
         }
-
-        split.stream().forEach((t) -> checkIsPositiveNumber(t));
-
-        return split.stream()
-                .map((t) -> Integer.parseInt(t)).collect(Collectors.toList());
+        parsed.forEach(StringParser::checkIsPositiveNumber);
+        return parsed.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 
     private static void checkIsPositiveNumber(String string) throws RuntimeException {
-        string.chars().forEach((t) -> {
-            checkIsDigit(t);
-        });
+        string.chars().forEach(StringParser::checkIsDigit);
     }
 
     private static void checkIsDigit(int t) throws RuntimeException {
