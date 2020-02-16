bc8aeeec550b3409d25dae55f220a1717bc04575
yeonnseok <ljinsk3@gmail.com>
2020-02-16 10:02:44 +0900

    feat: 시도횟수가 0이하일 경우 예외처리 발생
    OA

---
+++ src/main/java/racing/model/TrialTime.java
@@ -0,0 +1,17 @@
+package racing.model;
+
+public class TrialTime {
+
+    private int trialTime;
+
+    public TrialTime(int trialTime) {
+        checkTrialTimeNegativeOrZero(trialTime);
+        this.trialTime = trialTime;
+    }
+
+    private void checkTrialTimeNegativeOrZero(int trialTime) {
+        if (trialTime <= 0) {
+            throw new IllegalArgumentException("시도횟수는 음수가 입력될 수 없습니다.");
+        }
+    }
+}
