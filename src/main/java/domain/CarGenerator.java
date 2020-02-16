25d3b163a6bfe8b041a97f1fc8bb1a42f6287171
moonyoungCHAE <xpf_fl@naver.com>
2020-02-16 14:20:09 +0900

    refactor: for문 stream으로 수정

--- src/main/java/domain/CarGenerator.java
+++ src/main/java/domain/CarGenerator.java
@@ -1,20 +1,23 @@
 package domain;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CarGenerator {
     private static final String COMMA = ",";
     private static final String NOT_INPUT_MESSAGE = "이름을 입력하지 않았습니다.";
 
+
+    
     public static List<Car> create(String carsName) {
         String[] carNames = carsName.split(COMMA);
         validateLength(carNames);
 
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
+        List<Car> cars = Arrays.stream(carNames).map(o -> new Car(o)).collect(Collectors.toList());
         return cars;
     }
 
