83bea35bb3f9ffc0b304944a68bed97bd9eafeb2
yeonnseok <ljinsk3@gmail.com>
2020-02-16 14:53:39 +0900

    feat: 차이름 입력 받아서 lineUp 인스턴스 생성

--- src/main/java/racing/controller/RacingGame.java
+++ src/main/java/racing/controller/RacingGame.java
@@ -1,8 +1,19 @@
 package racing.controller;
 
+import racing.model.Car;
 import racing.model.CarLineUp;
+import racing.view.InputView;
 
 public class RacingGame {
+
+    CarLineUp lineUp = new CarLineUp();
+
+    private void enrollLineUp(){
+        for (String name : InputView.inputCarNames()){
+            checkNameDuplicate(lineUp, name);
+            lineUp.add(new Car(name));
+        }
+    }
 
     public void checkNameDuplicate(CarLineUp lineUp, String name) {
         if (lineUp.isContainAlready(name)){
---
+++ src/main/java/racing/view/InputView.java
@@ -0,0 +1,22 @@
+package racing.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final static Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+
+    public static String[] inputCarNames(){
+        String carNames = scanner.nextLine();
+        if (carNames == null || carNames.isEmpty()){
+            throw new NullPointerException("차 이름이 입력되지 않았습니다.");
+        }
+        return splitByDelimiter(carNames);
+    }
+
+    private static String[] splitByDelimiter(String carNames) {
+        return carNames.split(DELIMITER);
+    }
+}
+
+
