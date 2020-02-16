c1b4b4b2dc52357c05ed2079f6a94487d1f03e19
YerinCho <forjustice9839@gmail.com>
2020-02-17 03:00:31 +0900

    [RacingCar] refactor : InputView
     inputCarName 메소드의 리턴 자료형 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,11 +11,10 @@ public class InputView {
     private static final String NAME_INPUT_MESSAGE = "자동차의 이름을 입력하세요.(',' 기준으로 구분, 5글자 이하)";
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static String[] inputCarNames() {
+    public static String inputCarNames() {
         System.out.println(NAME_INPUT_MESSAGE);
         return SCANNER.nextLine()
-                .trim()
-                .split(",");
+                .trim();
     }
 
     public static String inputRound() {
