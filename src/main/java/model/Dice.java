1f8c731524f796c2398f889e5912e044c01f430e
YerinCho <forjustice9839@gmail.com>
2020-02-17 01:27:49 +0900

    [RacingCar] feat : Dice
     랜덤 수 생성

--- src/main/java/model/Dice.java
+++ src/main/java/model/Dice.java
@@ -3,9 +3,10 @@ package model;
 import java.util.Random;
 
 public class Dice {
+
+    private static final Random RANDOM = new Random();
     private static final int MAX_RANDOM_NUMBER = 9;
     private static final int ONE = 1;
-    private final static Random RANDOM = new Random();
 
     public static int makeRandomNumber() {
         return RANDOM.nextInt(MAX_RANDOM_NUMBER + ONE);
