ce3d64dbd713b9f1d30fbb18890120c30aba5b17
Tak Hyungmin <milkho11@naver.com>
2020-02-17 02:41:26 +0900

    [refact] 매직 넘버 제거

--- src/main/java/calculator/StringSpliter.java
+++ src/main/java/calculator/StringSpliter.java
@@ -5,15 +5,17 @@ import java.util.regex.Pattern;
 
 public class StringSpliter {
     private static final String OR_OPERATOR = "|";
-    
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+
     private static String delimiter = "," + OR_OPERATOR + ":";
     private static final String CUSTOM_DELIMITER_EXPRESSION = "//(.)\n(.*)";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_EXPRESSION);
 
+
     public static String[] split(String input) {
         Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (m.find()) {
-            String customDelimiter = m.group(1);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
             return m.group(2).split(customDelimiter);
         }
         return input.split(delimiter);
