b92bf4530b1cd3c271e90693acdf029558cf4752
Tak Hyungmin <milkho11@naver.com>
2020-02-17 02:43:02 +0900

    [refact] 생성자 중복 제거

--- src/main/java/racingcargame/domain/Car.java
+++ src/main/java/racingcargame/domain/Car.java
@@ -7,8 +7,7 @@ public class Car implements Comparable<Car> {
     private Position position;
 
     public Car(String name) {
-        this.name = new Name(name);
-        this.position = new Position(0);
+        this(name, 0);
     }
 
     public Car(String name, int position) {
