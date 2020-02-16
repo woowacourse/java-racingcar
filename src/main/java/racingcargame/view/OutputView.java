009e97de8f3ce30d8eb87226f8e25aa15268a09c
icyMojito <dev.icymojito@gmail.com>
2020-02-17 00:53:43 +0900

    [refactor] OutputView 클래스
    - 상태값 변화가 필요 없으므로 인스턴스를 생성하지 못하게 처리
    - 입력값이 잘못되었을 때 사용자에게 올바른 입력값을 받을 수 있도록 안내 메세지 출력 메서드 추가

--- src/main/java/racingcargame/view/OutputView.java
+++ src/main/java/racingcargame/view/OutputView.java
@@ -1,41 +1,68 @@
 package racingcargame.view;
 
-import racingcargame.domain.Car;
-import racingcargame.domain.RacingCars;
-import racingcargame.domain.Winners;
-
 import java.util.List;
 
+import racingcargame.domain.Car;
+import racingcargame.domain.Cars;
+import racingcargame.domain.Winners;
+
 public class OutputView {
-    private static final String DISTANCE = "-";
+	private static final String MOVE_DISTANCE = "-";
 
-    public static void printWinners(Winners winners) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.join(",", winners.getNames()));
-        sb.append("가 최종 우승했습니다.");
-        System.out.println(sb.toString());
-    }
+	private OutputView() { /* prevent creating OutputView instance */ }
 
-    public static String printPosition(Car car) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(car.getName() + " : ");
-        for (int i = 0; i < car.getPosition(); i++) {
-            sb.append(DISTANCE);
-        }
-        sb.append("\n");
-        return sb.toString();
-    }
+	public static void printNoCarErrorMessage() {
+		System.out.println("※자동차 이름이 입력되지 않았습니다. 다시 입력해 주세요.");
+	}
 
-    public static void printPosition(RacingCars cars) {
-        List<Car> racingCars = cars.getUnmodifiableCars();
-        StringBuilder sb = new StringBuilder();
-        for (Car car : racingCars) {
-            sb.append(OutputView.printPosition(car));
-        }
-        System.out.println(sb.toString());
-    }
+	public static void printOnlyOneCarErrorMessage() {
+		System.out.println("※자동차 이름이 한 개만 입력되었습니다. 경주이므로 최소 두 개 이상의 자동차 이름을 입력해 주세요. (ex) pobi, crong");
+	}
 
-    public static void printHead() {
-        System.out.println("실행 결과");
-    }
+	public static void printDuplicatedNameErrorMessage() {
+		System.out.println("※중복되는 자동차 이름이 존재합니다. 각각 고유한 자동차 이름을 입력해 주세요.");
+	}
+
+	public static void printTooLongNameErrorMessage() {
+		System.out.println("※허용된 길이(5자)를 초과하는 자동차 이름이 존재합니다. 자동차 이름은 모두 5자 이하로 입력해 주세요.");
+	}
+
+	public static void printBlankInputErrorMessage() {
+		System.out.println("※이름이 없는 자동차가 존재합니다. 쉼표(,)가 연속으로 입력(,,)되지 않았는지 확인 주세요.");
+	}
+
+	public static void printNotNumberFormatErrorMessage() {
+		System.out.println("※시도 회수는 숫자(정수)만 입력해 주세요.");
+	}
+
+	public static void printInvalidTryNumberErrorMessage() {
+		System.out.println("※시도 회수는 1 이상의 숫자만 입력해 주세요.");
+	}
+
+	public static void printHead() {
+		System.out.println("실행 결과");
+	}
+
+	private static void printPosition(Car car) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(car.getName())
+		  .append(" : ");
+		for (int i = 0; i < car.getPosition(); i++) {
+			sb.append(MOVE_DISTANCE);
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static void printPositions(Cars cars) {
+		List<Car> racingCars = cars.getUnmodifiableCars();
+		for (Car car : racingCars) {
+			OutputView.printPosition(car);
+		}
+		System.out.println();
+	}
+
+	public static void printWinners(Winners winners) {
+		String winnersMessage = String.join(",", winners.getNames()) + "이(가) 최종 우승했습니다.";
+		System.out.println(winnersMessage);
+	}
 }
