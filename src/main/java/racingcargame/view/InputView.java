65631406af18f2eb3383b389ec994f1248d45bdc
icyMojito <dev.icymojito@gmail.com>
2020-02-17 00:52:56 +0900

    [refactor] InputView 클래스
    - 상태값 변화가 필요 없으므로 인스턴스를 생성하지 못하게 처리
    - View이기 때문에 입력값 검증에 대한 책임을 해당 입력값에 관련된 도메인(TryNumber)으로 이동

--- src/main/java/racingcargame/view/InputView.java
+++ src/main/java/racingcargame/view/InputView.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final static Scanner SCANNER  = new Scanner(System.in);
+	private final static Scanner SCANNER = new Scanner(System.in);
 
-    public static String inputCarNames() {
-       System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return SCANNER.nextLine();
-    }
+	private InputView() { /* prevent creating InputView instance */ }
 
-    public static int inputTryNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        int input;
-        try {
-            input =  SCANNER.nextInt();
-        } catch (NumberFormatException e) {
-            System.out.println("숫자만 입력해 주세요.");
-            input = SCANNER.nextInt();
-        }
-        return input;
-    }
+	public static String inputCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		return SCANNER.nextLine();
+	}
+
+	public static String inputTryNumber() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		return SCANNER.nextLine();
+	}
 }
