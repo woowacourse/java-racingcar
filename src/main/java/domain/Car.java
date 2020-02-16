9c354c37cd2c9b22890a218171e6b49310992a0a
JunSeong <ebseud6135@naver.com>
2020-02-17 00:51:05 +0900

    refactor : Car클래스 수정
    RandomNo클래스를 만듦으로써 생기는 변경사항 수정

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,39 +1,37 @@
 package domain;
 
-import java.util.Objects;
-
 public class Car {
-    private static final int MIN_MOVABLE_NUM = 4;
+	private static final int START_POSITION_VALUE = 0;
 
-    private final Name name;
-    private int position;
+	private final Name name;
+	private int position;
 
-    public Car(String name) {
-        this(name, 0);
-    }
+	public Car(String name) {
+		this(name, START_POSITION_VALUE);
+	}
 
-    public Car(String name, int position) {
-        this.name = new Name(name);
-        this.position = position;
-    }
+	public Car(String name, int position) {
+		this.name = new Name(name);
+		this.position = position;
+	}
 
-    public boolean move(int randomNumber) {
-        if (randomNumber >= MIN_MOVABLE_NUM) {
-            position++;
-            return true;
-        }
-        return false;
-    }
+	public boolean move(RandomNo randomNo) {
+		if (randomNo.isMove()) {
+			position++;
+			return true;
+		}
+		return false;
+	}
 
-    public String getName() {
-        return name.getName();
-    }
+	public Name getName() {
+		return name;
+	}
 
-    public int getPosition() {
-        return position;
-    }
+	public int getPosition() {
+		return position;
+	}
 
-    public boolean isSamePosition(int position) {
-        return this.position == position;
-    }
+	public boolean isSamePosition(int position) {
+		return this.position == position;
+	}
 }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,22 +1,31 @@
 package domain;
 
-import calculator.StringCalculator;
-import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 public class CarTest {
-    @Test
-    void isMove_전진_성공() {
-        Car car = new Car("타미");
-        assertThat(car.move(4)).isTrue();
-    }
+	@Test
+	void isMove_전진_성공() {
+		Car car = new Car("타미");
+		assertThat(car.move(new RandomNo(4))).isTrue();
+	}
 
-    @Test
-    void isMove_전진_실패() {
-        Car car = new Car("타미");
-        assertThat(car.move(0)).isFalse();
-    }
+	@Test
+	void isMove_전진_실패() {
+		Car car = new Car("타미");
+		assertThat(car.move(new RandomNo(3))).isFalse();
+	}
+
+	@Test
+	void isSamePositionTest_일치할_때() {
+		Car car = new Car("타미", 1);
+		assertThat(car.isSamePosition(1)).isTrue();
+	}
+
+	@Test
+	void isSamePositionTest_불일치할_때() {
+		Car car = new Car("타미", 1);
+		assertThat(car.isSamePosition(0)).isFalse();
+	}
 }
