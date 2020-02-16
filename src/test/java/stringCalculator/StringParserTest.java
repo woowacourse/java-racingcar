777fa6b20b9803f72a04f9b2d52008613d3e94fb
kuenhwi <kueniyam@naver.com>
2020-02-16 15:29:54 +0900

    test: StringParserTest 테스트 개편 및 케이스 추가

--- src/test/java/stringCalculator/StringParserTest.java
+++ src/test/java/stringCalculator/StringParserTest.java
@@ -3,57 +3,61 @@ package stringCalculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringParserTest {
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"1,2e1,2", "1,2:3e1,2,3", "1,2:3:4e1,2,3,4"}, delimiter = 'e')
     @DisplayName("문자열 파싱")
-    void parse() {
-        String input1 = "";
-        String input2 = "1,2";
-        String input3 = "1,2:3";
-
-        List<Integer> expected1 = Arrays.asList(new Integer[]{0});
-        List<Integer> expected2 = Arrays.asList(new Integer[]{1, 2});
-        List<Integer> expected3 = Arrays.asList(new Integer[]{1, 2, 3});
-
-        List<Integer> result1 = StringParser.parse(input1);
-        List<Integer> result2 = StringParser.parse(input2);
-        List<Integer> result3 = StringParser.parse(input3);
-
-        parseOneCase(input1, expected1, result1);
-        parseOneCase(input2, expected2, result2);
-        parseOneCase(input3, expected3, result3);
-    }
-
-    @Test
-    @DisplayName("커스텀 구분자로 문자열 파싱")
-    void parse_WithCustomDelimiter() {
-        String input = "//;\\n1;2;3";
-        List<Integer> expected = Arrays.asList(new Integer[]{1, 2, 3});
+    void parse(String input, String expected) {
+        List<Integer> comparison = Arrays.stream(expected.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toUnmodifiableList());
         List<Integer> result = StringParser.parse(input);
 
-        parseOneCase(input, expected, result);
-    }
-
-    private void parseOneCase(String input, List<Integer> expected, List<Integer> result) {
-        assertThat(expected.equals(result)).isEqualTo(true);
+        Assertions.assertThat(result.size() == comparison.size())
+                .isTrue();
+        Assertions.assertThat(result.containsAll(comparison))
+                .isTrue();
     }
 
     @Test
-    @DisplayName("음수와 숫자가 아닌 경우 예외 처리")
-    void parse_ShouldGenerateExceptionWhenInputNegativeOrNonDigit() {
-        Assertions.assertThatThrownBy(() -> {
-            String input = "//;\\n1;a;3";
-            List<Integer> result = StringParser.parse(input);
-        }).isInstanceOf(RuntimeException.class);
+    void parse_WhenInputIsBlank() {
+        List<Integer> result = StringParser.parse("");
+        Assertions.assertThat(result.size() == 1)
+                .isTrue();
+        Assertions.assertThat(result.get(0) == 0)
+                .isTrue();
+    }
 
+    @ParameterizedTest
+    @CsvSource(value = {"//;\\n1;2;3e1,2,3", "//bb\\n1bb2bb3,4e1,2,3,4"}, delimiter = 'e')
+    @DisplayName("커스텀 구분자로 문자열 파싱")
+    void parse_WithCustomDelimiter(String input, String expected) {
+        List<Integer> comparison = Arrays.stream(expected.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toUnmodifiableList());
+        List<Integer> result = StringParser.parse(input);
+
+        Assertions.assertThat(comparison.size() == result.size())
+                .isTrue();
+        Assertions.assertThat(comparison.containsAll(result));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n1;a;3", "//;\\n1;-2;3"})
+    @DisplayName("음수와 숫자가 아닌 경우 예외 처리")
+    void parse_WhenInputNegativeOrNonDigit(String input) {
         Assertions.assertThatThrownBy(() -> {
-            String input = "//;\\n1;-2;3";
             List<Integer> result = StringParser.parse(input);
         }).isInstanceOf(RuntimeException.class);
     }
