e29caf03c4bd19caa59723c630eb10b969dbcde5
hwookim <gusdn9754@gmail.com>
2020-02-16 14:30:37 +0900

    refactor : 메소드 분리 - OutputView.printPositionViewer 추가

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -10,11 +10,14 @@ public class OutputView {
 	private static final String WINNER_NAME_DELIMITER = ",";
 	private static final String POSITION_VIEWER = "-";
 
+	private static String printPositionViewer(Car car) {
+		return IntStream.range(0, car.getPosition())
+				.mapToObj(x -> POSITION_VIEWER)
+				.collect(Collectors.joining());
+	}
+
 	static void printEachPosition(Car car) {
-		System.out.println(car.getName() + " : "
-				+ IntStream.range(0, car.getPosition())
-					.mapToObj(x -> POSITION_VIEWER)
-					.collect(Collectors.joining()));
+		System.out.println(car.getName() + " : " + printPositionViewer(car));
 	}
 
 	public static void printAllPosition(List<Car> cars) {
