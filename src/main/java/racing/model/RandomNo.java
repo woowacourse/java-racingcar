4e9464757cce06993b765707dc756de9da30dc70
yeonnseok <ljinsk3@gmail.com>
2020-02-16 19:12:44 +0900

    reafactor: RandomNo 상수 추출

--- src/main/java/racing/model/RandomNo.java
+++ src/main/java/racing/model/RandomNo.java
@@ -3,13 +3,16 @@ package racing.model;
 import java.util.Random;
 
 public class RandomNo {
+    public static final int UPPER_BOUND = 9;
+    public static final int LOWER_BOUND = 1;
+    public static final int CRITERIA_NUMBER = 4;
     private int randomNo;
 
     public RandomNo(){
-        randomNo = new Random().nextInt(9) + 1;
+        randomNo = new Random().nextInt(UPPER_BOUND) + LOWER_BOUND;
     }
 
     public boolean isMovable() {
-        return randomNo >= 4;
+        return randomNo >= CRITERIA_NUMBER;
     }
 }
