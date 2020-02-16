2591547e91dc0e7343cc091527e475dae895b26c
YerinCho <forjustice9839@gmail.com>
2020-02-16 23:01:54 +0900

    [RacingCar] feat : GameManager, GameManagerTest
     사용자 입력의 유효성 검사 추가

--- src/main/java/controller/GameManager.java
+++ src/main/java/controller/GameManager.java
@@ -1,72 +1,103 @@
 package controller;
 
-import model.Car;
-import model.Dice;
-import model.Winner;
-import view.InputView;
-import view.OutputView;
-
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class GameManager {
-    private List<Car> cars = new ArrayList<>();
-    private Winner winner = new Winner();
-    private int round;
-    private static final int MIN_MOVE_NUMBER = 4;
-    static final int ZERO = 0;
 
-    private static String[] inputCarName() {
-        return InputView.inputCarName();
+    public static final String NO_NAME_ERROR_MESSAGE = "이름 입력값이 없습니다.";
+    public static final String INVALID_NAME_ERROR_MESSAGE = "유효하지 않은 이름입니다.";
+    private static final String NOT_INTEGER_ERROR_MESSAGE = "숫자가 아니라 문자입니다.";
+
+    static void validateInput(String carNames, String round) {
+        checkCarNames(carNames);
+        checkRound(round);
     }
 
-    public void startGame() {
-        init();
-        run();
-        award();
+    private static void checkRound(String round) {
+        isInteger(round);
     }
 
-    public static void moveOrStay(Car car, int RandomNumber) {
-        if (RandomNumber >= MIN_MOVE_NUMBER) {
-            car.move();
+    private static void isInteger(String round) {
+        try {
+            Integer.parseInt(round);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
         }
     }
 
-    private void init() {
-        setCarList(inputCarName());
-        setRound();
+    private static void checkCarNames(String carNames) {
+        ArrayList<String> tempCarNames = new ArrayList<>(Arrays.asList(carNames.split(",")));
+        checkNameEmpty(tempCarNames);
     }
 
-    private void race(List<Car> cars) {
-        for (Car car : cars) {
-            moveOrStay(car, Dice.makeRandomNumber());
+    private static void checkNameEmpty(ArrayList<String> tempCarNames) {
+        if (tempCarNames.isEmpty()) {
+            throw new IllegalArgumentException(NO_NAME_ERROR_MESSAGE);
         }
+        tempCarNames.stream()
+                .filter(x -> x.isEmpty())
+                .forEach(x -> {
+                    throw new IllegalArgumentException(INVALID_NAME_ERROR_MESSAGE);
+                });
     }
+//    private List<Car> cars = new ArrayList<>();
+//    private Winner winner = new Winner();
+//    private int round;
+//    private static final int MIN_MOVE_NUMBER = 4;
+//    static final int ZERO = 0;
 
-    private void run() {
-        OutputView.printBeginResult();
-        for (int i = ZERO; i < round; i++) {
-            race(cars);
-            OutputView.printScore(cars);
-        }
-    }
-
-    private void award() {
-        winner.setWinner(cars);
-        OutputView.printWinners(winner.getWinners());
-    }
-
-    public List<Car> getCarList() {
-        return this.cars;
-    }
-
-    private void setRound() {
-        this.round = InputView.inputRound();
-    }
-
-    void setCarList(String[] carNames) {
-        for (String carName : carNames) {
-            this.cars.add(new Car(carName));
-        }
-    }
+//    private static String[] inputCarName() {
+//        return InputView.inputCarName();
+//    }
+//
+//    public void startGame() {
+//        init();
+//        run();
+//        award();
+//    }
+//
+//    public static void moveOrStay(Car car, int RandomNumber) {
+//        if (RandomNumber >= MIN_MOVE_NUMBER) {
+//            car.move();
+//        }
+//    }
+//
+//    private void init() {
+////        setCarList(inputCarName());
+////        setRound();
+//    }
+//
+//    private void race(List<Car> cars) {
+//        for (Car car : cars) {
+//            moveOrStay(car, Dice.makeRandomNumber());
+//        }
+//    }
+//
+//    private void run() {
+//        OutputView.printBeginResult();
+//        for (int i = ZERO; i < round; i++) {
+//            race(cars);
+//            OutputView.printScore(cars);
+//        }
+//    }
+//
+//    private void award() {
+//        winner.setWinner(cars);
+//        OutputView.printWinners(winner.getWinners());
+//    }
+//
+//    public List<Car> getCarList() {
+//        return this.cars;
+//    }
+//
+//    private void setRound() {
+//        this.round = InputView.inputRound();
+//    }
+//
+//    void setCarList(String[] carNames) {
+//        for (String carName : carNames) {
+//            this.cars.add(new Car(carName));
+//        }
+//    }
 }
--- src/test/java/controller/GameManagerTest.java
+++ src/test/java/controller/GameManagerTest.java
@@ -1,43 +1,48 @@
 package controller;
 
-import model.Car;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameManagerTest {
 
-    GameManager gameManager = new GameManager();
-
-    @DisplayName("입력받은 이름으로 car 객체 생성")
-    @Test
-    void setCarList() {
-        String[] input = new String[]{"k3", "k5"};
-        gameManager.setCarList(input);
-
-        List<Car> result = new ArrayList<>();
-        result.add(new Car("k3"));
-        result.add(new Car("k5"));
-
-        assertThat(result).isEqualTo(gameManager.getCarList());
+    @DisplayName("사용자 입력값의 유효성 검사")
+    @ParameterizedTest
+    @CsvSource(value = {"'car0,car1,,car2','2'", "'car0,car1,car2','()'", "' ','3"})
+    void validateInput(String carNames, String round) {
+        assertThatThrownBy(() -> GameManager.validateInput(carNames, round))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("4이상 숫자가 주어지면 전진")
-    @Test
-    void MoveOrStay() {
-        Car car = new Car("car");
-        GameManager.moveOrStay(car, 1);
-        assertThat(car.getPosition()).isEqualTo(0);
-
-        GameManager.moveOrStay(car, 4);
-        assertThat(car.getPosition()).isEqualTo(1);
-
-        GameManager.moveOrStay(car, 4);
-        GameManager.moveOrStay(car, 6);
-        assertThat(car.getPosition()).isEqualTo(3);
-    }
+//    GameManager gameManager = new GameManager();
+//
+//    @DisplayName("입력받은 이름으로 car 객체 생성")
+//    @Test
+//    void setCarList() {
+//        String[] input = new String[]{"k3", "k5"};
+//        gameManager.setCarList(input);
+//
+//        List<Car> result = new ArrayList<>();
+//        result.add(new Car("k3"));
+//        result.add(new Car("k5"));
+//
+//        assertThat(result).isEqualTo(gameManager.getCarList());
+//    }
+//
+//    @DisplayName("4이상 숫자가 주어지면 전진")
+//    @Test
+//    void MoveOrStay() {
+//        Car car = new Car("car");
+//        GameManager.moveOrStay(car, 1);
+//        assertThat(car.getPosition()).isEqualTo(0);
+//
+//        GameManager.moveOrStay(car, 4);
+//        assertThat(car.getPosition()).isEqualTo(1);
+//
+//        GameManager.moveOrStay(car, 4);
+//        GameManager.moveOrStay(car, 6);
+//        assertThat(car.getPosition()).isEqualTo(3);
+//    }
 }
