9b10a8233f95f969e655b77bbe97d61b55c8d2ed
kuenhwi <kueniyam@naver.com>
2020-02-16 15:29:08 +0900

    test: StringAdditionCalculatorTest 테스트 개편 및 케이스 추가

--- src/test/java/stringCalculator/StringAdditionCalculatorTest.java
+++ src/test/java/stringCalculator/StringAdditionCalculatorTest.java
@@ -3,16 +3,25 @@ package stringCalculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringAdditionCalculatorTest {
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"1,3e4", "1,2,3,4e10"}, delimiter = 'e')
     @DisplayName("더하기")
-    void add() {
-        List<Integer> operands = Arrays.asList(new Integer[]{1, 3});
+    void add(String inputs, int expected) {
+        List<Integer> operands = Arrays.stream(inputs.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toUnmodifiableList());
+
         int result = StringAdditionCalculator.sum(operands);
-        Assertions.assertThat(result).isEqualTo(4);
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
