8662836004310a016b76b746d8505de2667e4db3
YerinCho <forjustice9839@gmail.com>
2020-02-17 14:39:33 +0900

    [RacingCar] refactor : Winner

--- src/main/java/model/Winner.java
+++ src/main/java/model/Winner.java
@@ -27,23 +27,4 @@ public class Winner {
                 .filter(x -> x.position.isMaxPosition(getMaxPosition(cars)))
                 .forEach(x -> winners.add(x.getName()));
     }
-//    private static int maxPosition = 0;
-//    private List<String> winners = new ArrayList<>();
-//
-//    int getMaxPosition(List<Car> cars) {
-//        for (Car car : cars) {
-//            maxPosition = Math.max(maxPosition, car.getPosition());
-//        }
-//        return maxPosition;
-//    }
-//
-//    public void setWinner(List<Car> cars) {
-//        cars.stream()
-//                .filter(x -> x.isMaxPosition(getMaxPosition(cars)))
-//                .forEach(x -> winners.add(x.getName()));
-//    }
-//
-//    public List<String> getWinners() {
-//        return this.winners;
-//    }
 }
--- src/test/java/model/WinnerTest.java
+++ src/test/java/model/WinnerTest.java
@@ -40,36 +40,4 @@ public class WinnerTest {
         winner.setWinners(cars);
         assertThat(winner.getWinners()).isEqualTo(result);
     }
-
-//    private List<Car> cars = new ArrayList<>();
-//    private Winner winner = new Winner();
-//
-//    @BeforeEach
-//    void setUp() {
-//        cars.add(new Car("a"));
-//        cars.add(new Car("b"));
-//        cars.add(new Car("c"));
-//        GameManager.moveOrStay(cars.get(0), 9);
-//        GameManager.moveOrStay(cars.get(0), 9);
-//        GameManager.moveOrStay(cars.get(1), 9);
-//        GameManager.moveOrStay(cars.get(1), 9);
-//        GameManager.moveOrStay(cars.get(2), 9);
-//    }
-//
-//    @DisplayName("car객체들의 최대 포지션 구하기")
-//    @Test
-//    void getMaxPosition() {
-//        int maxPosition = winner.getMaxPosition(cars);
-//        assertThat(maxPosition).isEqualTo(2);
-//    }
-//
-//    @DisplayName("우승자 Car 객체 리스트 구하기")
-//    @Test
-//    void setWinner() {
-//        List<String> result = new ArrayList<>();
-//        result.add("a");
-//        result.add("b");
-//        winner.setWinner(cars);
-//        assertThat(winner.getWinners()).isEqualTo(result);
-//    }
 }
