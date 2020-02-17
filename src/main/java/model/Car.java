77607e347c9226e2924b4ce087eb35f51525bbfe
YerinCho <forjustice9839@gmail.com>
2020-02-17 14:36:53 +0900

    [RacingCar] refactor : Car
     메소드 이름 변경 (move->moveOrNot), 접근 제한자 변경, 게터추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,13 +1,13 @@
 package model;
 
+import java.util.Objects;
+
 public class Car {
 
     private static final String NAME_LENGTH_ERROR_MESSAGE = "이름의 길이는 1~5 글자여야 합니다.";
-    public static final int MAX_NAME_LENGTH = 5;
-    public static final int MIN_MOVE_DELIMITER = 4;
-    public static final int MOVE = 1;
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
-    private int position;
+    Position position = new Position();
 
     public Car(String name) {
         validateName(name);
@@ -21,63 +21,29 @@ public class Car {
         }
     }
 
-    public void move(int randomNumber) {
-        if (randomNumber >= MIN_MOVE_DELIMITER) {
-            this.position += MOVE;
-        }
+    public void moveOrNot(int RandomNumber) {
+        this.position.move(RandomNumber);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPosition() {
-        return this.position;
+        return this.position.getPosition();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
 
-//    private String name;
-//    private static final int MAX_NAME_LENGTH = 5;
-//    private static final int ONE_STEP = 1;
-//    private int position = 0;
-//
-//    public Car(String name) {
-//        this.name = name;
-//    }
-//
-//    public static void validateName(String name) {
-//        try {
-//            if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
-//                throw new IllegalArgumentException(ValidationView.NAME_LENGTH_ERROR_MESSAGE);
-//            }
-//        } catch (NullPointerException e) {
-//            ValidationView.printNullPointerException();
-//        }
-//    }
-//
-//
-//    public void move() {
-//        position += ONE_STEP;
-//    }
-//
-//    public boolean isMaxPosition(int MaxPosition) {
-//        return MaxPosition == position;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public int getPosition() {
-//        return position;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Car car = (Car) o;
-//        return Objects.equals(name, car.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name);
-//    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
+
