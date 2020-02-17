95634f1ec49df7af0b9c9857ad0d4a8e483fb605
kimjunseok <jnunseok@gmail.com>
2020-02-18 01:02:31 +0900

    [Refactor] Winner class stream 사용으로 코드 간결화

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -22,27 +22,18 @@ public class Winner {
 
     public static String getWinner(Cars cars) {
         count = 0;
-        for (Car car : cars.getCars()) {
-            putWinnerNames(car.getName(), car.getPosition());
-        }
+        int maxNo = getMaxPosition(cars);
+        findWinner(cars, maxNo);
         return String.join(COMMA_SPACE, winnerNames);
     }
 
-    private static void putWinnerNames(String carName, int position) {
-        setCountAndWinnerNames(position);
-        addCarName(carName, position);
+    private static int getMaxPosition(Cars cars) {
+        return cars.getCars().stream().mapToInt(Car::getPosition).max().getAsInt();
     }
 
-    private static void setCountAndWinnerNames(int position) {
-        if (count < position) {
-            count = position;
-            winnerNames.clear();
-        }
-    }
-
-    private static void addCarName(String carName, int position) {
-        if (count <= position) {
-            winnerNames.add(carName);
-        }
+    private static void findWinner(Cars cars, int maxNo) {
+        cars.getCars().stream()
+            .filter(t -> t.getPosition() == maxNo)
+            .forEach(t -> winnerNames.add(t.getName()));
     }
 }
