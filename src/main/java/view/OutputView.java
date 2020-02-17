13829772450598e5efa080579bf38a4e2f29b6c2
YerinCho <forjustice9839@gmail.com>
2020-02-17 14:41:03 +0900

    [RacingCar] feat : OutputViewTest
     우승자 출력, 게임 시작 메시지 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,13 +5,22 @@ import model.Car;
 import java.util.List;
 
 public class OutputView {
-
     private static final String COLON = " : ";
     private static final String BLANK = "\0";
     private static final String SCORE_BAR = "-";
     private static final String COMMA = ",";
     private static final String EXECUTION_RESULT_MESSAGE = " 가 우승했습니다.";
     private static final String BEGIN_RESULT_MESSAGE = "실행 결과";
+
+
+    public static void printGameStartMessage() {
+        System.out.println(BEGIN_RESULT_MESSAGE);
+    }
+
+    public static void printWinner(List<String> winners) {
+        String winner = String.join(COMMA, winners);
+        System.out.println(winner + EXECUTION_RESULT_MESSAGE);
+    }
 
     public static void printScore(List<Car> cars) {
         for (Car car : cars) {
@@ -29,12 +38,4 @@ public class OutputView {
         return new String(new char[position]).replace(BLANK, SCORE_BAR);
     }
 
-    public static void printWinners(List<String> winners) {
-        String winner = String.join(COMMA, winners);
-        System.out.println(winner + EXECUTION_RESULT_MESSAGE);
-    }
-
-    public static void printBeginResult() {
-        System.out.println(BEGIN_RESULT_MESSAGE);
-    }
 }
--- src/test/java/view/OutputViewTest.java
+++ src/test/java/view/OutputViewTest.java
@@ -1,26 +1,35 @@
 package view;
 
+import model.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OutputViewTest {
-//    private List<Car> cars = new ArrayList<>();
-//
-//    @BeforeEach
-//    void setUp() {
-//        cars.add(new Car("a"));
-//        cars.add(new Car("b"));
-//        cars.add(new Car("c"));
-//        GameManager.moveOrStay(cars.get(0), 9);
-//        GameManager.moveOrStay(cars.get(0), 9);
-//        GameManager.moveOrStay(cars.get(1), 1);
-//        GameManager.moveOrStay(cars.get(1), 9);
-//        GameManager.moveOrStay(cars.get(2), 2);
-//        GameManager.moveOrStay(cars.get(2), 3);
-//    }
-//
-//    @DisplayName("포지션 값만큼 '-' 출력")
-//    @ParameterizedTest
-//    @CsvSource(value = {"0,'--'", "1,'-'", "2,''"})
-//    void visualizePosition(int index, String s) {
-//        String result = OutputView.printPositionBar(cars.get(index));
-//        assertThat(result).isEqualTo(s);
-//    }
+    private List<Car> cars = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        cars.add(new Car("a"));
+        cars.add(new Car("b"));
+        cars.add(new Car("c"));
+        cars.get(0).moveOrNot(9);
+        cars.get(0).moveOrNot(9);
+        cars.get(1).moveOrNot(9);
+        cars.get(2).moveOrNot(3);
+    }
+
+    @DisplayName("포지션 값만큼 '-' 출력")
+    @ParameterizedTest
+    @CsvSource(value = {"0,'--'", "1,'-'", "2,''"})
+    void visualizePosition(int index, String s) {
+        String result = OutputView.printPositionBar(cars.get(index));
+        assertThat(result).isEqualTo(s);
+    }
 }
