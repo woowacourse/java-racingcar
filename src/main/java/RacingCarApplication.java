f9c67bba500e09a227e76720445f41e94b99a703
YerinCho <forjustice9839@gmail.com>
2020-02-17 14:44:03 +0900

    [RacingCar] feat : RacingCarApplication
     메인 구현, GameManager 로부터 기능 분리

--- src/main/java/RacingCarApplication.java
+++ src/main/java/RacingCarApplication.java
@@ -1,10 +1,18 @@
 import controller.GameManager;
+import view.InputView;
+import view.OutputView;
 
 public class RacingCarApplication {
 
 
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
-        gameManager.startGame();
+        GameManager gameManager = new GameManager(InputView.inputCarNames(), InputView.inputRound());
+
+        OutputView.printGameStartMessage();
+        while (!gameManager.isFinish()) {
+            gameManager.race();
+            OutputView.printScore(gameManager.getCars());
+        }
+        OutputView.printWinner(gameManager.whoIsWinner());
     }
 }
