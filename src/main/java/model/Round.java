683081a5f314828f32f866d85293832e11945712
YerinCho <forjustice9839@gmail.com>
2020-02-17 14:38:57 +0900

    [RacingCar] feat : Round
     매 라운드 별 체크하는 기능과, 마지막 라운드인지 확인하는 기능 추가

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -2,17 +2,27 @@ package model;
 
 public class Round {
 
-    public static final String ROUND_INPUT_ERROR_MESSAGE = "라운드 입력이 잘못되었습니다.";
-    private final int round;
+    private static final String ROUND_INPUT_ERROR_MESSAGE = "라운드 입력이 잘못되었습니다.";
+    private static final int MIN_ROUND_NUMBER = 0;
+    private int round;
 
-    private Round(int round) {
+    public Round(int round) {
         validateRound(round);
         this.round = round;
     }
 
     static void validateRound(int round) {
-        if (round <= 0) {
+        if (round <= MIN_ROUND_NUMBER) {
             throw new IllegalArgumentException(ROUND_INPUT_ERROR_MESSAGE);
         }
     }
+
+    public boolean isLastRound() {
+        return this.round == MIN_ROUND_NUMBER;
+    }
+
+    public void afterOneRound() {
+        this.round--;
+    }
+
 }
--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -1,9 +1,11 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RoundTest {
@@ -14,5 +16,13 @@ public class RoundTest {
     void validateRound(int round) {
         assertThatThrownBy(() -> Round.validateRound(round))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("마지막 라운드인지 확인")
+    void isFinish() {
+        Round round = new Round(1);
+        round.afterOneRound();
+        assertThat(round.isLastRound()).isTrue();
     }
 }
