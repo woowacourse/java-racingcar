2f08a83bcb385a08c65fabf49ecf6522f4664fbd
yeonnseok <ljinsk3@gmail.com>
2020-02-18 11:36:40 +0900

    feat: RandomNo 숫자직접 입력해서 생성하는 생성자 추가

--- src/test/java/racingTest/RandomNoTest.java
+++ src/test/java/racingTest/RandomNoTest.java
@@ -4,16 +4,14 @@ import org.junit.jupiter.api.Test;
 import racing.model.RandomNo;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 public class RandomNoTest {
 
     @Test
-    void 랜덤넘버_4이상일_경우_true_반환(){
-        RandomNo randomNo = mock(RandomNo.class);
-
-        when(randomNo.isMovable()).thenReturn(true);
-        assertThat(randomNo.isMovable()).isTrue();
+    void 랜덤_넘버_숫자_확인(){
+        RandomNo randomNo = new RandomNo(4);
+        int result = randomNo.getNumber();
+        assertThat(result).isEqualTo(4);
     }
 }
