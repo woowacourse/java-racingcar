4440e49f1b1a5088783da947633debf45a5679d5
icyMojito <dev.icymojito@gmail.com>
2020-02-19 01:12:38 +0900

    [refactor] Car 클래스의 상수 처리와 깊은 복사를 하는 메서드 추가

--- src/main/java/racingcargame/domain/Car.java
+++ src/main/java/racingcargame/domain/Car.java
@@ -1,38 +1,44 @@
 package racingcargame.domain;
 
 public class Car implements Comparable<Car> {
-	private final Name name;
-	private int position;
+    public static final int DEFAULT_POSITION = 0;
 
-	public Car(String name) {
-		this(name, 0);
-	}
+    private final Name name;
+    private int position;
 
-	public Car(String name, int position) {
-		this.name = new Name(name);
-		this.position = position;
-	}
+    public Car(String name) {
+        this(name, DEFAULT_POSITION);
+    }
 
-	public void go(Engine engine) {
-		if (engine.canMove()) {
-			position++;
-		}
-	}
+    public Car(String name, int position) {
+        this.name = new Name(name);
+        this.position = position;
+    }
 
-	public boolean isSamePositionWith(Car car) {
-		return this.position == car.position;
-	}
+    public void go(Engine engine) {
+        if (engine.canMove()) {
+            position++;
+        }
+    }
 
-	public int getPosition() {
-		return position;
-	}
+    public boolean isSamePositionWith(Car car) {
+        return this.position == car.position;
+    }
 
-	public String getName() {
-		return name.getName();
-	}
+    public int getPosition() {
+        return position;
+    }
 
-	@Override
-	public int compareTo(Car o) {
-		return Integer.compare(this.position, o.position);
-	}
+    public String getName() {
+        return name.getName();
+    }
+
+    public Car newClone() {
+        return new Car(getName(), position);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Integer.compare(this.position, o.position);
+    }
 }
