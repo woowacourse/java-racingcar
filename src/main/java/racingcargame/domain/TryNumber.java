8fd6e0466a4a5ec085234a25f5bdc45a72dad6ef
icyMojito <dev.icymojito@gmail.com>
2020-02-19 01:43:33 +0900

    [refactor] TryNumber 클래스에 상수화로 가독성을 높이고 역할을 구체화함
    외부에서 의도치 않은 데이터를 받아 비교하는 것이 아니라 TryNumber 클래스 내에서 필요한 기능을 구현

--- src/main/java/racingcargame/domain/TryNumber.java
+++ src/main/java/racingcargame/domain/TryNumber.java
@@ -1,38 +1,33 @@
 package racingcargame.domain;
 
 import racingcargame.exception.InvalidTryNumberException;
-import racingcargame.view.OutputView;
 
 public class TryNumber {
-	private static final int MIN_TRY_NUMBER = 1;
+    private static final int MIN_TRY_NUMBER = 1;
 
-	private final int number;
+    private int tryNumber;
 
-	public TryNumber(String inputNumber) {
-		int number = getNumber(inputNumber);
-		checkUnderMinTryNumber(number);
-		this.number = number;
-	}
+    public TryNumber(String inputNumber) {
+        int number = getNumber(inputNumber);
+        checkUnderMinTryNumber(number);
+        this.tryNumber = number;
+    }
 
-	private int getNumber(String inputNumber) {
-		int number;
-		try {
-			number = Integer.parseInt(inputNumber);
-		} catch (NumberFormatException e) {
-			OutputView.printNotNumberFormatErrorMessage();
-			throw new NumberFormatException("※시도 회수는 정수만 입력 가능");
-		}
-		return number;
-	}
+    private int getNumber(String inputNumber) {
+        try {
+            return Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("※시도 회수는 숫자(정수)만 입력해 주세요.");
+        }
+    }
 
-	private void checkUnderMinTryNumber(int number) {
-		if (number < MIN_TRY_NUMBER) {
-			OutputView.printInvalidTryNumberErrorMessage();
-			throw new InvalidTryNumberException("※시도 회수는 1 이상의 숫자만 입력 가능");
-		}
-	}
+    private void checkUnderMinTryNumber(int number) {
+        if (number < MIN_TRY_NUMBER) {
+            throw new InvalidTryNumberException("※시도 회수는 " + MIN_TRY_NUMBER + " 이상의 숫자만 입력해 주세요.");
+        }
+    }
 
-	public boolean isBiggerThan(int number) {
-		return number < this.number;
-	}
+    public boolean next() {
+        return tryNumber-- != 0;
+    }
 }
