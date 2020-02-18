5304cba4dbc6f3e22d7682ebff3adce905f0305b
icyMojito <dev.icymojito@gmail.com>
2020-02-19 01:38:30 +0900

    [refactor] Cars 클래스에 예외처리 추가수정과 깊은 복사를 하는 메서드 추가
    - 구분자만 입력할 경우(ex:,,,) 예외가 발생하도록 메서드 수정
    - 예외 발생 시 뷰에서 사용자에게 알맞은 안내 메세지를 출력하기 위해 예외 타입 선언

--- src/main/java/racingcargame/domain/Cars.java
+++ src/main/java/racingcargame/domain/Cars.java
@@ -1,85 +1,84 @@
 package racingcargame.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import racingcargame.exception.CarsNameInputException;
+import racingcargame.exception.CarsNameInputExceptionType;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
-import racingcargame.exception.CarsNameInputException;
-import racingcargame.view.OutputView;
-
 public class Cars {
-	public static final String CAR_NAMES_DELIMITER = ",";
-	public static final String BLANK = "";
+    public static final String CAR_NAMES_DELIMITER = ",";
+    public static final String BLANK = "";
 
-	private final List<Car> cars = new ArrayList<>();
+    private final List<Car> readOnlyCars;
 
-	public Cars(List<Car> cars) {
-		this.cars.addAll(cars);
-	}
+    public Cars(List<Car> cars) {
+        this.readOnlyCars = Collections.unmodifiableList(new ArrayList<>(cars));
+    }
 
-	public static Cars create(String inputCarNames) {
-		checkCarAmount(inputCarNames);
-		String[] separatedCarNames = inputCarNames.split(CAR_NAMES_DELIMITER);
-		checkDuplicatedNames(separatedCarNames);
-		List<Car> cars = createCars(separatedCarNames);
-		return new Cars(cars);
-	}
+    public static Cars create(String inputCarNames) {
+        checkCarAmount(inputCarNames);
+        String[] separatedCarNames = inputCarNames.split(CAR_NAMES_DELIMITER);
+        checkDuplicatedNames(separatedCarNames);
+        List<Car> cars = createCars(separatedCarNames);
+        return new Cars(cars);
+    }
 
-	private static void checkCarAmount(String inputCarNames) {
-		checkNoCars(inputCarNames);
-		checkOnlyOneCar(inputCarNames);
-	}
+    private static void checkCarAmount(String inputCarNames) {
+        checkNoCars(inputCarNames);
+        checkOnlyOneCar(inputCarNames);
+    }
 
-	private static void checkNoCars(String inputCarNames) {
-		if (BLANK.equals(inputCarNames)) {
-			OutputView.printNoCarErrorMessage();
-			throw new CarsNameInputException("※자동차 이름이 입력되지 않음");
-		}
-	}
+    private static void checkNoCars(String inputCarNames) {
+        String[] separatedCarNames = inputCarNames.split(CAR_NAMES_DELIMITER);
+        if (BLANK.equals(inputCarNames) || separatedCarNames.length == 0) {
+            throw new CarsNameInputException(CarsNameInputExceptionType.NO_CAR, "※매개변수가 blank거나 구분자만 입력됨");
+        }
+    }
 
-	private static void checkOnlyOneCar(String inputCarNames) {
-		if (!inputCarNames.contains(CAR_NAMES_DELIMITER)) {
-			OutputView.printOnlyOneCarErrorMessage();
-			throw new CarsNameInputException("※자동차 이름이 한 개만 입력됨");
-		}
-	}
+    private static void checkOnlyOneCar(String inputCarNames) {
+        if (!inputCarNames.contains(CAR_NAMES_DELIMITER)) {
+            throw new CarsNameInputException(CarsNameInputExceptionType.ONLY_ONE_CAR, "※구분자가 없는 입력값이 들어옴");
+        }
+    }
 
-	private static void checkDuplicatedNames(String[] separatedCarNames) {
-		Set<String> carNames = Arrays.stream(separatedCarNames)
-									 .map(String::trim)
-									 .collect(Collectors.toSet());
-		if (carNames.size() != separatedCarNames.length) {
-			OutputView.printDuplicatedNameErrorMessage();
-			throw new CarsNameInputException("※중복되는 자동차 이름이 존재");
-		}
-	}
+    private static void checkDuplicatedNames(String[] separatedCarNames) {
+        Set<String> carNames = Arrays.stream(separatedCarNames)
+                .map(String::trim)
+                .collect(Collectors.toSet());
+        if (carNames.size() != separatedCarNames.length) {
+            throw new CarsNameInputException(CarsNameInputExceptionType.DUPLICATED_NAMES, "※중복되는 자동차 이름이 존재");
+        }
+    }
 
-	private static List<Car> createCars(String[] separatedCarNames) {
-		List<Car> cars = new ArrayList<>();
-		for (String carName : separatedCarNames) {
-			cars.add(new Car(carName));
-		}
-		return cars;
-	}
+    private static List<Car> createCars(String[] separatedCarNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : separatedCarNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
 
-	public void move() {
-		cars.forEach(car -> car.go(Engine.create()));
-	}
+    public void move() {
+        readOnlyCars.forEach(car -> car.go(Engine.create()));
+    }
 
-	public List<Car> extractWinners() {
-		List<Car> cars = this.cars;
-		Collections.sort(cars);
-		Car topPositionCar = cars.get(cars.size() - 1);
-		List<Car> winners = cars.stream()
-								.filter(car -> car.isSamePositionWith(topPositionCar))
-								.collect(Collectors.toList());
-		return Collections.unmodifiableList(winners);
-	}
+    public List<Car> extractWinners() {
+        List<Car> cars = new ArrayList<>(readOnlyCars);
+        Collections.sort(cars);
+        Car topPositionCar = cars.get(cars.size() - 1);
+        List<Car> winners = cars.stream()
+                .filter(car -> car.isSamePositionWith(topPositionCar))
+                .collect(Collectors.toList());
+        return Collections.unmodifiableList(winners);
+    }
 
-	public List<Car> getUnmodifiableCars() {
-		return Collections.unmodifiableList(cars);
-	}
+    public List<Car> getDeepCopiedCars() {
+        List<Car> deepCopiedCars = new ArrayList<>();
+        for (Car car : readOnlyCars) {
+            Car cloneCar = car.newClone();
+            deepCopiedCars.add(cloneCar);
+        }
+        return deepCopiedCars;
+    }
 }
