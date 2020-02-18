09ddf10d4942b63ec2593510777cedb961a75bb7
icyMojito <dev.icymojito@gmail.com>
2020-02-19 01:39:17 +0900

    [refactor] Name 클래스에 예외 발생 시 뷰에서 사용자에게 알맞은 안내 메세지를 출력하기 위해 예외 타입 선언

--- src/main/java/racingcargame/domain/Name.java
+++ src/main/java/racingcargame/domain/Name.java
@@ -1,63 +1,62 @@
 package racingcargame.domain;
 
+import racingcargame.exception.CarNameInputException;
+import racingcargame.exception.CarNameInputExceptionType;
+
 import java.util.Objects;
 
-import racingcargame.exception.CarNameInputException;
-import racingcargame.view.OutputView;
-
 public class Name {
-	private static final int MAX_NAME_LENGTH = 5;
+    private static final int MAX_NAME_LENGTH = 5;
 
-	private String name;
+    private String name;
 
-	public Name(String name) {
-		validate(name);
-		this.name = name.trim();
-	}
+    public Name(String name) {
+        validate(name);
+        this.name = name.trim();
+    }
 
-	private void validate(String name) {
-		checkNullInput(name);
-		name = name.trim();
-		checkOverMaxNameLength(name);
-		checkBlankInput(name);
-	}
+    private void validate(String name) {
+        checkNullInput(name);
+        name = name.trim();
+        checkOverMaxNameLength(name);
+        checkBlankInput(name);
+    }
 
-	private void checkNullInput(String name) {
-		if (name == null) {
-			throw new CarNameInputException("※자동차 이름으로 Null값이 들어옴");
-		}
-	}
+    private void checkNullInput(String name) {
+        if (name == null) {
+            throw new CarNameInputException(CarNameInputExceptionType.NULL_INPUT, "※자동차 이름으로 Null값이 들어옴");
+        }
+    }
 
-	private void checkOverMaxNameLength(String name) {
-		if (MAX_NAME_LENGTH < name.length()) {
-			OutputView.printTooLongNameErrorMessage();
-			throw new CarNameInputException("※허용된 길이(5자)를 초과하는 자동차 이름이 존재");
-		}
-	}
+    private void checkOverMaxNameLength(String name) {
+        if (MAX_NAME_LENGTH < name.length()) {
+            throw new CarNameInputException(CarNameInputExceptionType.OVER_MAX_NAME_LENGTH, "※허용된 길이(5자)를 초과하는 자동차 " +
+                    "이름이 존재");
+        }
+    }
 
-	private void checkBlankInput(String name) {
-		if (name.isEmpty()) {
-			OutputView.printBlankInputErrorMessage();
-			throw new CarNameInputException("※이름이 없는 자동차가 존재");
-		}
-	}
+    private void checkBlankInput(String name) {
+        if (name.isEmpty()) {
+            throw new CarNameInputException(CarNameInputExceptionType.BLANK_NAME, "※이름이 없는 자동차가 존재");
+        }
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Name name1 = (Name)o;
-		return name.equals(name1.name);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Name name1 = (Name) o;
+        return name.equals(name1.name);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(name);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
