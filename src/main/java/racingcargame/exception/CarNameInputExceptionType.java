e8ec3ea96fb8d8f96bc277d2cf686b87582f67b8
icyMojito <dev.icymojito@gmail.com>
2020-02-19 01:47:44 +0900

    [feat] 뷰에서 사용자에게 출력할 적절한 메세지를 구분하기 위한 예외 타입 생성

---
+++ src/main/java/racingcargame/exception/CarNameInputExceptionType.java
@@ -0,0 +1,10 @@
+package racingcargame.exception;
+
+public enum CarNameInputExceptionType {
+    NULL_INPUT,
+    OVER_MAX_NAME_LENGTH,
+    BLANK_NAME;
+
+    CarNameInputExceptionType() {
+    }
+}
---
+++ src/main/java/racingcargame/exception/CarsNameInputExceptionType.java
@@ -0,0 +1,10 @@
+package racingcargame.exception;
+
+public enum CarsNameInputExceptionType {
+    NO_CAR,
+    ONLY_ONE_CAR,
+    DUPLICATED_NAMES;
+
+    CarsNameInputExceptionType() {
+    }
+}
