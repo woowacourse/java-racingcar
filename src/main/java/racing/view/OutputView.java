3fb5275c3bcaa941d5d8c4ecabf076dca4773f92
yeonnseok <ljinsk3@gmail.com>
2020-02-18 12:10:08 +0900

    refactor: comma 접근제한자 private으로 변경

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -15,7 +15,7 @@ public class OutputView {
     private static final String INPUT_TRIAL_TIME_GUIDE_MESSAGE = "시도할 횟수는 몇회인가요?";
     private static final String RESULT_HEADING = "실행 결과";
     private static final String WINNER_SUB_MESSAGE = "가 최종 우승했습니다.";
-    public static final String COMMA = ", ";
+    private static final String COMMA = ", ";
 
     public static void printInputCarNamesMessage() {
         System.out.println(INPUT_CAR_NAMES_GUIDE_MESSAGE);
