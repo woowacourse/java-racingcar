893949746d48e4b2c6753705240baa31f6769b01
ParkDooWon <pass345@naver.com>
2020-02-20 15:39:16 +0900

    refactor : 적절한 상수명으로 수정, 에러 메시지에 더 많은 정보를 담도록 수정, 메서드로 분리하여 어떤 작업을 하는지 의미 전달

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -3,7 +3,7 @@ package calculator.domain;
 public class Calculator {
     public static final String DEFAULT_DELIMITER = ",|:";
     public static final char STARTS_OF_CUSTOM_DELIMITER = '/';
-    public static final int ZERO = 0;
+    public static final int DELIMITER_INDEX = 0;
     public static final int EXPRESSION = 5;
     public static final int CUSTOM_DELIMITER_BEGIN_INDEX = 2;
     public static final int CUSTOM_DELIMITER_END_INDEX = 3;
@@ -11,14 +11,18 @@ public class Calculator {
     public static String[] splitString(String input) {
         String delimiter = DEFAULT_DELIMITER;
         if (isCustomDelimiter(input)) {
-            delimiter = input.substring(CUSTOM_DELIMITER_BEGIN_INDEX, CUSTOM_DELIMITER_END_INDEX);
+            delimiter = findCustomDelimiter(input);
             input = input.substring(EXPRESSION);
         }
         return input.split(delimiter);
     }
 
+    private static String findCustomDelimiter(String input) {
+        return input.substring(CUSTOM_DELIMITER_BEGIN_INDEX, CUSTOM_DELIMITER_END_INDEX);
+    }
+
     private static boolean isCustomDelimiter(String input) {
-        return input.charAt(ZERO) == STARTS_OF_CUSTOM_DELIMITER;
+        return input.charAt(DELIMITER_INDEX) == STARTS_OF_CUSTOM_DELIMITER;
     }
 
     public static int calculate(String[] numbers) throws NumberFormatException {
@@ -33,11 +37,11 @@ public class Calculator {
 
     private static void validatePositiveNumber(int parsedNumber) {
         if (isNegativeNumber(parsedNumber)) {
-            throw new IllegalArgumentException("음수의 숫자는 입력이 불가능합니다.");
+            throw new IllegalArgumentException(String.format("음수의 숫자는 입력이 불가능합니다. 입력값 : %d", parsedNumber));
         }
     }
 
     private static boolean isNegativeNumber(int parsedNumber) {
-        return parsedNumber < ZERO;
+        return parsedNumber < 0;
     }
 }
