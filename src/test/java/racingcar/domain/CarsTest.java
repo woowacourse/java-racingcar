625464fecaae38bfafd02c7ee6c6c8e330b887ce
aegis1920 <aegis1920@gmail.com>
2020-02-20 19:32:34 +0900

    refactor : 가장 먼 자동차의 거리(우승자 찾기)를 구하는 테스트 삭제

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -2,32 +2,11 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domian.car.Car;
 import racingcar.domian.Cars;
-import racingcar.domian.car.Position;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsTest {
-
-    @DisplayName("가장 멀리 이동한 자동차의 위치 찾기")
-    @Test
-    void 가장_먼_자동차의_거리() {
-        String inputNames = "user1,user2,user3,user4";
-        int maxPosition = 3;
-        Cars cars = new Cars(inputNames);
-        cars.getCars()
-                .add(new Car(new Position(maxPosition), "user5"));
-
-        assertThat(cars.getCars()
-                .stream()
-                .filter(car -> car.isWinner(maxPosition))
-                .findFirst()
-                .get()
-                .getName())
-                .isEqualTo("user5");
-    }
 
     @DisplayName("예외 케이스: 게임에 참여하는 자동차(유저 이름)가 5대를 초과하는 경우")
     @Test
