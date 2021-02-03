5f96cc9e5c7a23a2fc93fa3e1bda3e4a0300582c
Minjeong <mathmjseo@naver.com>
2021-02-03 17:11:20 +0900

    feat: 빈 문자열 또는 null을 입력할 경우 기능 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+    public static int splitAndSum(String string) {
+        if (string == null || string.isEmpty()) {
+            return 0;
+        }
+        return 1;
+    }
+}
