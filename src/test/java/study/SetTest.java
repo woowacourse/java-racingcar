c3e68334141e42ba0936d7cde1c41ccdb0c9cf96
Minjeong <mathmjseo@naver.com>
2021-02-03 16:53:19 +0900

    test: 결과값이 다른 경우에 대한 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,12 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -31,5 +33,11 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void isNumber_True(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void isNumber_LessThan3_True(int number, boolean expected) {
+        assertEquals(expected, numbers.contains(number));
     }
 }
