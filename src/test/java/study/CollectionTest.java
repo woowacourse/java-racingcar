12390667d5123092995f7113d262213c364e11de
lns13301 <gyuhanKang@gmail.com>
2021-02-03 12:56:46 +0900

    test: 학습테스트를 통한 JUnit 학습하기

---
+++ src/test/java/study/CollectionTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsValidate(String input, String expected) {
+        assertEquals(expected, String.valueOf(numbers.contains(Integer.parseInt(input))));
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,46 @@
-package study;public class StringTest {
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] values = "1,2".split(",");
+
+        assertThat(values[0]).isEqualTo("1");
+        assertThat(values[1]).isEqualTo("2");
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    public void substring() {
+        String value = "(1,2)".substring(1, 4);
+
+        assertThat(value).isEqualTo("1,2");
+    }
+
+    @DisplayName("특정 위치의 문자 찾기")
+    @Test
+    public void getChar() {
+        String value = "abc";
+
+        assertThatThrownBy(() -> {
+            value.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+
+    @DisplayName("특정 위치의 문자 찾기")
+    @Test
+    public void getChar2() {
+        String value = "abc";
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    value.charAt(4);
+                }).withMessageMatching("String index out of range: \\d+");
+    }
 }
