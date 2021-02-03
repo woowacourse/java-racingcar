177c43318331c6aabc774efc5f4853e61b6a3719
mungto <daum7766@naver.com>
2021-02-03 18:28:08 +0900

    refactor(StringCalculator): code refactoring

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,28 +6,47 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     private static final StringBuilder sb = new StringBuilder();
+    private static final int ZERO = 0;
+    private static final int FIRST = 1;
+    private static final int SECOND = 2;
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_PATTERN = "//(.)\\n(.*)";
+    private static final String MULTIPLE_SEPARATOR = "|";
 
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
-            return 0;
+            return ZERO;
         }
-        sb.append(",|:");
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        String[] numbers = init(text);
+        int sum = ZERO;
+        for (String number: numbers) {
+            sum += parseIntegerNumber(number);
+        }
+        return sum;
+    }
+
+    private static String[] init(String text) {
+        sb.append(DEFAULT_DELIMITER);
+        Matcher m = Pattern.compile(CUSTOM_PATTERN).matcher(text);
         if (m.find()) {
-            sb.append('|');
-            sb.append(m.group(1));
-            text = m.group(2);
+            sb.append(MULTIPLE_SEPARATOR);
+            sb.append(m.group(FIRST));
+            text = m.group(SECOND);
         }
         String[] numbers = text.split(sb.toString());
-        int sum = 0;
-        for (String number: numbers) {
-            int intNumber = Integer.parseInt(number);
-            if (intNumber < 0) {
-                throw new RuntimeException();
-            }
-            sum += intNumber;
+        sb.setLength(ZERO);
+        return numbers;
+    }
+
+    private static int parseIntegerNumber(String number) {
+        int intNumber = Integer.parseInt(number);
+        positiveNumberCheck(intNumber);
+        return intNumber;
+    }
+
+    private static void positiveNumberCheck(int intNumber) {
+        if (intNumber < ZERO) {
+            throw new RuntimeException();
         }
-        sb.setLength(0);
-        return sum;
     }
 }
