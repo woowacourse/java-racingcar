1ecb0867b18f52810d21297c742dac3e82ced2da
cjy17 <cjy1705@naver.com>
2021-02-03 17:59:19 +0900

    feat : StringTest 작성

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void split(){
+        String[] values = "1,2".split(",");
+        assertThat(values).containsExactly("1","2");
+    }
+
+    @Test
+    void name() {
+        String values = "(1,2)".substring(1,4);
+        assertThat(values).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항3")
+    void name3() {
+        assertThatThrownBy(() -> {
+            throw new IndexOutOfBoundsException(String.valueOf("abc".charAt(4)));
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("a");
+    }
+}
