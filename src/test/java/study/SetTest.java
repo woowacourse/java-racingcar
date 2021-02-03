12f42e89b964a2bad8d993b80ff7c65ef55cafc6
Gomding <backchida@gmail.com>
2021-02-03 17:52:04 +0900

    Set Collection에 대한 학습 테스트 - 요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     public void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Set의 contains 메소드에 {0}을 넣었을 떄 결과값이 {1}인지 테스트한다.")
+//    @ParameterizedTest(name = "Set의 contains 메소드 입력값에 따라 결과값이 다른 경우에 대한 테스트한다.")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    public void testFailContains(int num, boolean result) {
+        assertThat(numbers.contains(num)).isEqualTo(result);
     }
 }
