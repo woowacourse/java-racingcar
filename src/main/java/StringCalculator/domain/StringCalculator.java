1f0ed97b1d16229c5bf1a12834ae17fc14378526
DWL5 <ssop6403@gmail.com>
2021-02-03 18:46:30 +0900

    feat: \\와\n 사이에 커스텀 구분자를 체크 한다.

--- src/main/java/StringCalculator/domain/StringCalculator.java
+++ src/main/java/StringCalculator/domain/StringCalculator.java
@@ -1,12 +1,15 @@
 package stringcalculator.domain;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_FORMAT = "//(.)\n(.*)";
 
     public static int splitAndSum(String input) {
-        if(isNullOrBlank(input)) {
+        if (isNullOrBlank(input)) {
             return 0;
         }
 
@@ -14,15 +17,30 @@ public class StringCalculator {
             return Integer.parseInt(input);
         }
 
-        String[] inputs = input.split(DEFAULT_DELIMITER);
+        String delimiter = extractCustomDelimiterOrGetDefaultDelimiter(input);
+        input = extractElementString(input);
+        System.out.println(input);
 
+        String[] inputs = input.split(delimiter);
         return Arrays.stream(inputs)
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
 
+    private static String extractElementString(String input) {
+        return input.substring(input.indexOf("\n") + 1);
+    }
+
+    private static String extractCustomDelimiterOrGetDefaultDelimiter(String input) {
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_FORMAT).matcher(input);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
     private static boolean isNullOrBlank(String input) {
-        if(input == null || input.isEmpty()){
+        if (input == null || input.isEmpty()) {
             return true;
         }
 
