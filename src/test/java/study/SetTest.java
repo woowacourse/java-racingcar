de6613427c8a35ff2e874e87d349f28cb7b2f7e6
Yeonwoo Cho <yeonwoocho@Yeonwooui-MacBookPro.local>
2021-02-03 17:41:24 +0900

    test: 집합 원소 포함 확인 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,9 +24,18 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("집합 크기 확인")
     void size() {
         int expectSize = 3;
 
         assertThat(numbers.size()).isEqualTo(expectSize);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("집합 원소 포함 확인")
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
