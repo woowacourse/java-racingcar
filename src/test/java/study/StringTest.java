a71aa96f50284e98f2beadb5bfc67dbf4572a230
binghe819 <binghe819@naver.com>
2021-02-03 19:07:17 +0900

    feat: 문자열 덧셈 계산기 구현

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,34 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] values = "1,2".split(",");
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String value = "(1, 2)".substring(1, 5);
+        assertThat(value).isEqualTo("1, 2");
+    }
+
+    @Test
+    void charAt() {
+        String value = "abc";
+
+        assertThat(value.charAt(0)).isEqualTo('a');
+        assertThat(value.charAt(1)).isEqualTo('b');
+
+        assertThatThrownBy(()-> {
+            char ch = value.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 3");
+    }
+}
+
