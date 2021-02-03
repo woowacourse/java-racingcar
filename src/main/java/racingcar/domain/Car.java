41ad76a725b158a9c07d785a58bbb1d7bc408969
sakjung <ssangyu123@gmail.com>
2021-02-03 20:17:00 +0900

    feat: Car 객체 생성과 자동차 움직이는 기능 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+public class Car {
+    private int position;
+    private String name;
+
+    private final int INIT_POSITION = 0;
+    private final int MOVABLE_VALUE = 4;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = INIT_POSITION;
+    }
+
+    public void move(int number){
+        if(isMovable(number)) {
+            position++;
+        }
+    }
+
+    private boolean isMovable(int number){
+        return number >= MOVABLE_VALUE;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+}
