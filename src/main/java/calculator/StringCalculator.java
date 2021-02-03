ffca48822c483c8f5f56c1a92240e3a7f4187f6d
Wannte <T.wannte@gmail.com>
2021-02-03 17:29:34 +0900

    feat(StringCalculator): add function by custom delimiter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,18 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+    static final String DEFAULT_DELIMITER = "[,:]";
+    static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
 
     static int splitAndSum(String input) {
         if (checkIfNull(input) || checkIfEmpty(input)) {
             return 0;
         }
-        String[] tokens = input.split("[,:]");
-        return calculateTotalSum(tokens);
+        return calculateTotalSum(getTokens(input));
     }
 
     static private boolean checkIfNull(String input) {
@@ -24,5 +27,22 @@ public class StringCalculator {
         return Arrays.stream(tokens)
                      .mapToInt(Integer::parseInt)
                      .sum();
+    }
+
+    static private String[] getTokens(String input) {
+        Matcher matcher = getMatcher(input);
+        if (matcher.find()) {
+            return getTokensByCustomDelimiter(matcher);
+        }
+        return input.split(DEFAULT_DELIMITER);
+    }
+
+    static private Matcher getMatcher(String input) {
+        return Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
+    }
+
+    static private String[] getTokensByCustomDelimiter(Matcher m) {
+        String customDelimiter = m.group(1);
+        return m.group(2).split(customDelimiter);
     }
 }
