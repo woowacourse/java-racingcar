619728c422983ddb10cc11a77c41d8b430fe7723
Minjeong <mathmjseo@naver.com>
2021-02-03 16:51:12 +0900

    test: 특정 위치의 문자 가져오기 test 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -17,5 +19,15 @@ public class StringTest {
     void substring() {
         String value = "(1,2)".substring(1, 4);
         assertThat(value).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    public void charAt() {
+        String value = "abc";
+        assertThatThrownBy(() -> {
+            char c = value.charAt(5);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
     }
 }
