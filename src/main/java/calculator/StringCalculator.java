3b6bb331b356d8e54e6d74f44f31864425af0e91
Minjeong <mathmjseo@naver.com>
2021-02-03 17:36:15 +0900

    feat: 숫자 두개를 컴마 구분자로 입력할 경우 합을 반환하는 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,6 +10,26 @@ public class StringCalculator {
             return Integer.parseInt(string);
         }
 
-        return 1;
+        String[] numbers = string.split(",");
+        int result = 0;
+        if (numbers.length == 2) {
+            for (String number : numbers) {
+                if (!checkDigit(number)) {
+                    throw new RuntimeException();
+                }
+                result += Integer.parseInt(number);
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean checkDigit(String number) {
+        for (int i = 0; i < number.length(); i++) {
+            if (!Character.isDigit(number.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
