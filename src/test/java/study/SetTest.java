f6a4642e86855ec56f76b014065f3edcd07699c1
yoonsung.jung <ybook2012@gmail.com>
2021-02-03 17:26:36 +0900

    feat : Junit 학습 Set Collection 요구사항 3번

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,11 +1,12 @@
 package study;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,8 +29,16 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 5})
-    void contains(int i) {
+    @ValueSource(ints = {1, 2, 3})
+    void contains1(int i) {
         assertThat(numbers.contains(i)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "5:false"}, delimiter = ':')
+    void contains3(int i, boolean expected) {
+//        boolean isContain = numbers.contains(i);
+//        assertEquals(expected, isContain);
+        assertThat(expected).isEqualTo(numbers.contains(i));
     }
 }
