abba2cf77984566de6e3021ac48f4b43241ebd42
Eugine Park <pkeugine@gmail.com>
2021-02-03 16:47:44 +0900

    refactor(StringCalculator): follow requirements

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,20 +6,20 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     public static int splitAndSum(String input) {
-
-        if (input == null || input.equals("")) {
+        if (input == null || input.isEmpty()) {
             return 0;
         }
 
         Matcher m = Pattern.compile("//(.)\\n(.*)").matcher(input);
         if (m.find()) {
             String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-            loop(tokens);
-            return sum(tokens);
+            return calculate(m.group(2), customDelimiter);
         }
+        return calculate(input, ",|:");
+    }
 
-        String[] numbers = input.split(",|:");
+    public static int calculate(String input, String delimiter) {
+        String[] numbers = input.split(delimiter);
         loop(numbers);
         return sum(numbers);
     }
