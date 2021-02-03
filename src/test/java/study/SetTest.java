7ed88583fefc66730544729072e6dbba06ed77e7
Yeonwoo Cho <yeonwoocho@Yeonwooui-MacBookPro.local>
2021-02-03 17:37:51 +0900

    test: 집합 크기 확인 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,14 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -15,5 +20,12 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int expectSize = 3;
+
+        assertThat(numbers.size()).isEqualTo(expectSize);
     }
 }
