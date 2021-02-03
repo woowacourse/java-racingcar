11a745ba7d85338f578d8eb4299d4582a7574f92
Minjeong <mathmjseo@naver.com>
2021-02-03 17:43:28 +0900

    feat: 구분자를 컴마 이외에 콜론도 사용할 수 있도록 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,6 +1,10 @@
 package calculator;
 
 public class StringCalculator {
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+    private static final String VERTICAL_LINE = "|";
+
     public static int splitAndSum(String string) {
         if (string == null || string.isEmpty()) {
             return 0;
@@ -10,15 +14,13 @@ public class StringCalculator {
             return Integer.parseInt(string);
         }
 
-        String[] numbers = string.split(",");
+        String[] numbers = string.split(COMMA + VERTICAL_LINE + COLON);
         int result = 0;
-        if (numbers.length == 2) {
-            for (String number : numbers) {
-                if (!checkDigit(number)) {
-                    throw new RuntimeException();
-                }
-                result += Integer.parseInt(number);
+        for (String number : numbers) {
+            if (!checkDigit(number)) {
+                throw new RuntimeException();
             }
+            result += Integer.parseInt(number);
         }
 
         return result;
