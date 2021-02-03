b629d09dd1a13d4b58cb1268188d8ef3e4d44a6d
Yeonwoo Cho <yeonwoocho@Yeonwooui-MacBookPro.local>
2021-02-03 17:10:47 +0900

    test: 문자열의 올바르지 않은 위치 호출 시 예외 처리 구현

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,7 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 public class StringTest {
     @Test
@@ -43,10 +44,29 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열 특정 위치의 값 반환 테스트")
     void charAt() {
         String value = "abc";
         int testIndex = 2;
 
         assertThat(value.charAt(testIndex)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("문자열의 올바르지 않은 위치 호출 시 예외 처리")
+    void charAtException() {
+        String value = "abc";
+        int testIndex = 4;
+        final String expectMessage = "String index out of range: ";
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(()->{
+                    value.charAt(testIndex);
+                }).withMessageMatching(expectMessage + "\\d+");
+
+        assertThatThrownBy(()->{
+            value.charAt(testIndex);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining(expectMessage + testIndex);
     }
 }
