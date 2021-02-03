4bfdc7a8d9d0cee61740b3e283ad398b60879a72
DWL5 <ssop6403@gmail.com>
2021-02-03 20:09:37 +0900

    refactor: IllegalCustomDelimiterPositionExeption 상수 대문자로 변경

--- src/main/java/StringCalculator/exception/IllegalCustomDelimiterPositionException.java
+++ src/main/java/StringCalculator/exception/IllegalCustomDelimiterPositionException.java
@@ -1,9 +1,9 @@
 package stringcalculator.exception;
 
 public class IllegalCustomDelimiterPositionException extends RuntimeException {
-    private static final String message = "구분자의 위치가 올바르지 않습니다.";
+    private static final String MESSAGE = "구분자의 위치가 올바르지 않습니다.";
 
     public IllegalCustomDelimiterPositionException() {
-        super(message);
+        super(MESSAGE);
     }
 }
