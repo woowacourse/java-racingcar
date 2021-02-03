44a6baf103483bc1e29a6efe3f2364cd11533157
mungto <daum7766@naver.com>
2021-02-03 15:27:00 +0900

    feature(StringTest): create JUnit StringTest

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,41 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] values = "1,2".split(",");
+        // 순서에 상관없이 포함되어 있는지 확인
+        assertThat(values).contains("1", "2");
+        // 순서대로 값이 정확하게 포함되어 있는지 확인
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    void name() {
+        String value = "(1,2)".substring(1, 4);
+        assertThat(value).isEqualTo("1,2");
+    }
+
+    @Test
+    void getCharacter() {
+        String value = "abc";
+        assertThat(value.charAt(0)).isEqualTo('a');
+        assertThat(value.charAt(1)).isEqualTo('b');
+        assertThat(value.charAt(2)).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            value.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("String index out of range: 3");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                value.charAt(4);
+            }).withMessageMatching("String index out of range: 4");
+    }
+}
