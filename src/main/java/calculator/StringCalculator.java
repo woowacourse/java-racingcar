d400e0a26916c2f98840e8704ecf03c7a3fdba62
mungto <daum7766@naver.com>
2021-02-03 17:48:49 +0900

    feature(StringCalculator): Requirement 1 solution

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int splitAndSum(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+        return 1;
+    }
+}
