358fd64b7d9b7c260035f7ba51e19814497d832a
xrabcde <xrabcde@gmail.com>
2021-02-04 14:17:32 +0900

    feat(RacingCarView): 경주할 자동차 이름을 받아오기위한 문구출력 기능 구현

---
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class RacingCarView {
+    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
+
+    public static void carListInput() {
+        System.out.printf(INPUT_MESSAGE);
+    }
+}
