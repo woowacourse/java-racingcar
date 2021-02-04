6433435e4ad9afd318ed2d4dc80bac7fffd73e69
DWL5 <ssop6403@gmail.com>
2021-02-04 20:16:52 +0900

    feat: 자동차 이름 유효값 체크 기능 추가

---
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -0,0 +1,28 @@
+package racingcar.domain.car;
+
+import racingcar.exception.CarNameBlankOrEmptyException;
+import racingcar.exception.CarNameLengthException;
+
+public class CarName {
+    private static final int CAR_NAME_LENGTH_THRESHOLD = 5;
+
+    private String name;
+
+    public CarName(String name) {
+        validateCarNameLength(name);
+        validateCarNameBlankOrEmpty(name);
+        this.name = name;
+    }
+
+    private void validateCarNameLength(String name) {
+        if(name.length() > CAR_NAME_LENGTH_THRESHOLD) {
+            throw new CarNameLengthException();
+        }
+    }
+
+    private void validateCarNameBlankOrEmpty(String name) {
+        if (name.isEmpty() || name.isBlank()) {
+            throw new CarNameBlankOrEmptyException();
+        }
+    }
+}
