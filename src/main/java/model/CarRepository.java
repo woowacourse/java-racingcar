71d80e2e1e0a5d3f74ab6e5c15cb934a83fd3ad9
pika96 <52442949+pika96@users.noreply.github.com>
2021-02-04 19:34:55 +0900

    feat : CarRepository 클래스 추가

--- src/main/java/model/CarRepository.java
+++ src/main/java/model/CarRepository.java
@@ -1,4 +1,21 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import utils.RandomUtils;
+
 public class CarRepository {
+
+    private static final List<Car> cars = new ArrayList<>();
+
+    public static List<Car> cars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public static void moveCars() {
+        cars.stream()
+            .filter(car -> car.getRandomNumber() >= 4)
+            .forEach(car -> car.movePosition());
+    }
 }
