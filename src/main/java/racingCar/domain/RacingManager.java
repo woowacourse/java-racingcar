893a527dede0f6a84f7da47872101aa160427704
pjy1368 <basejin0810@naver.com>
2021-02-04 21:40:58 +0900

    feat(RacingManager): 레이스 게임을 실행하는 기능 구현

--- src/main/java/racingCar/domain/RacingManager.java
+++ src/main/java/racingCar/domain/RacingManager.java
@@ -1,2 +1,31 @@
-package racingCar.domain;public class RacingMaager {
+package racingCar.domain;
+
+public class RacingManager {
+
+  private Participants participants;
+  private int turn;
+  private GasStation gasStation;
+
+  public RacingManager(Participants participants, int turn, GasStation gasStation) {
+    this.participants = participants;
+    this.turn = turn;
+    this.gasStation = gasStation;
+  }
+
+  public RacingResult start() {
+    RacingResult racingResult = new RacingResult(participants);
+    for (int i = 0; i < turn; i++) {
+      race(racingResult);
+    }
+    return racingResult;
+  }
+
+  private void race(RacingResult racingResult) {
+    participants.getCars().forEach(car -> {
+      gasStation.fillInFuel(car);
+      car.run();
+    });
+    racingResult.appendLog();
+  }
+
 }
