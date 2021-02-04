effa33b82b53c021f3e7b60e4225447e93414184
yoonsung.jung <ybook2012@gmail.com>
2021-02-04 12:14:13 +0900

    feat : 숫자만 있는지 확인하는 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,21 @@ package calculator;
 
 import calculator.util.InputStrValidator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
     public static int splitAndSum(String inputStr) {
         if (InputStrValidator.isEmptyOrNull(inputStr)) {
             return 0;
         }
-
+        if (InputStrValidator.isOnlyOneNumber(inputStr)) {
+            return Integer.parseInt(inputStr);
+        }
         return -1;
     }
 }
--- src/main/java/calculator/util/InputStrValidator.java
+++ src/main/java/calculator/util/InputStrValidator.java
@@ -1,7 +1,15 @@
 package calculator.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputStrValidator {
     public static boolean isEmptyOrNull(String inputStr) {
         return ("".equals(inputStr) || inputStr == null);
+    }
+
+    public static boolean isOnlyOneNumber(String inputStr) {
+        String pattern = "^[0-9]*$";
+        return Pattern.matches(pattern, inputStr);
     }
 }
