374a925f38cc7431529ad5ed53b182209800af87
김주원 <kjw11077@naver.com>
2021-02-04 17:41:41 +0900

    feat(Car): 이름,거리 반환 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,24 @@
 package racingcar;
 
+import java.util.stream.IntStream;
+
 public class Car {
+
     private int position;
+    private String name;
 
     public Car() {
         this.position = 0;
+    }
+
+    public void drive(int randomValue) {
+        if (randomValue < 0 || randomValue > 9) {
+            throw new RuntimeException();
+        }
+
+        if (randomValue >= 4) {
+            accelerate();
+        }
     }
 
     public int getPosition() {
@@ -15,9 +29,16 @@ public class Car {
         position++;
     }
 
-    public void drive(int randomValue) {
-        if (randomValue >= 4) {
-            accelerate();
-        }
+    @Override
+    public String toString() {
+        String a = name + " : ";
+        return a + getPositionToString();
+    }
+
+    private String getPositionToString() {
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, position)
+                .forEach(i -> sb.append("-"));
+        return sb.toString();
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,14 +14,23 @@ class CarTest {
 
     @BeforeEach
     void setUp() {
-        car = new Car();
+        car = new Car("joy");
     }
 
     @ParameterizedTest
     @DisplayName("4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다.")
-    @CsvSource(value = {"3,0", "4,1"})
-    void drive(Integer input, Integer expected) {
+    @CsvSource(value = {"3,1", "4,2"})
+    void drive_validNumber(Integer input, Integer expected) {
         car.drive(input);
         assertThat(car.getPosition()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("drive에 범위를 벗어난 값을 줌")
+    @CsvSource(value = {"-1", "10"})
+    void drive_invalidNumber(Integer input) {
+        assertThatThrownBy(() -> {
+            car.drive(input);
+        }).isInstanceOf(RuntimeException.class);
     }
 }
