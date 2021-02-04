6abb2d38b7435c957972b31f7200bb52cd505447
DWL5 <ssop6403@gmail.com>
2021-02-04 21:03:30 +0900

    feat: 이동횟수가 음수로 들어올 경우 체크하는 기능 추가

--- src/main/java/racingcar/domain/MoveCountInput.java
+++ src/main/java/racingcar/domain/MoveCountInput.java
@@ -1,17 +1,33 @@
 package racingcar.domain;
 
+import racingcar.exception.CarMoveCountNegativeException;
 import racingcar.exception.CarMoveCountNonNumericException;
 
 public class MoveCountInput {
 
+    int moveCount;
+
     public MoveCountInput(String input) {
         validateMoveCountInputNumeric(input);
+        validateMoveCountPositive(input);
+
+        this.moveCount = Integer.parseInt(input);
     }
 
     private void validateMoveCountInputNumeric(String input) {
         if (!isNumeric(input)) {
             throw new CarMoveCountNonNumericException();
         }
+    }
+
+    private void validateMoveCountPositive(String input) {
+        if(Integer.parseInt(input) < 0) {
+            throw new CarMoveCountNegativeException();
+        }
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 
     private boolean isNumeric(String input) {
