0f697cabc6975360bd45344544da79d3780a8394
pjy1368 <basejin0810@naver.com>
2021-02-04 17:20:13 +0900

    feat(GasStation): 주유소 기능 구현

---
+++ src/main/java/racingCar/service/GasStation.java
@@ -0,0 +1,15 @@
+package racingCar.service;
+
+import java.util.function.Supplier;
+
+public class GasStation {
+  private Supplier<Integer> supplier;
+
+  public GasStation(Supplier<Integer> supplier) {
+    this.supplier = supplier;
+  }
+
+  public void fillInFuel(Car car) {
+    car.fillInFuel(supplier.get());
+  }
+}
