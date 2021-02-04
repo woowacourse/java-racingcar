005936a95071f8ed14d88b1cc60cf59d089eb0b2
xrabcde <xrabcde@gmail.com>
2021-02-04 17:25:39 +0900

    feat(RacingCarViewTest): 실행결과 출력 테스트코드 작성

--- src/test/java/racingcar/view/RacingCarViewTest.java
+++ src/test/java/racingcar/view/RacingCarViewTest.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -13,7 +14,8 @@ public class RacingCarViewTest {
 
     private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
     private static final String TURN_INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
-
+    private static final String SAMPLE_CAR_NAME = "sample";
+    private static final int CRITERIA = 4;
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
@@ -29,5 +31,13 @@ public class RacingCarViewTest {
     void turnNumberInput() {
         RacingCarView.turnNumberInput();
         assertEquals(TURN_INPUT_MESSAGE, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void displayPosition() {
+        Car sampleCar = new Car(SAMPLE_CAR_NAME);
+        sampleCar.oneTurn(CRITERIA);
+        RacingCarView.displayPosition(sampleCar);
+        assertEquals("sample : -", outputStreamCaptor.toString());
     }
 }
