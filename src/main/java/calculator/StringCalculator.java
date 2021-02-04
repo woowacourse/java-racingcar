f7f737cf8a856dc0569d7c5968346095a13524b4
y_woo :) <dusdn1702@naver.com>
2021-02-04 12:19:34 +0900

    feat: 쉼표 구분자로 분리한 후 합을 반환하는 코드 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,26 @@ package calculator;
 
 public class StringCalculator {
     private static final int ZERO = 0;
-    public static final int INIT_NUMBER = -1;
+    private static final int INIT_NUMBER = -1;
+    private static final String DELIMITER = ",";
+//    private static final String[] delimiters = {",", ":"};
+
+//    public enum delimiters {
+//        COMMA(","),
+//        COLON(":");
+//
+//        private final String delimiter;
+//        private delimiters(String delimiter) {
+//            this.delimiter = delimiter;
+//        }
+//
+//        public boolean isIncludeComma(String text){
+//            if (text.contains(COMMA)){
+//                return true;
+//            }
+//            return false;
+//        }
+//    }
 
     public static int splitAndSum(String text) {
         int result = INIT_NUMBER;
@@ -17,6 +36,20 @@ public class StringCalculator {
         if(isOnlyNumber(text)){
             result = Integer.parseInt(text);
             return result;
+        }
+        return splitByDelimiter(text, DELIMITER);
+        //return result;
+    }
+
+    private static int splitByDelimiter(String text, String delimiter) {
+        String[] numbers = text.split(delimiter);
+        return sumNumbers(numbers);
+    }
+
+    private static int sumNumbers(String[] numbers) {
+        int result = 0;
+        for (String number : numbers){
+            result += Integer.parseInt(number);
         }
         return result;
     }
