46b747feae5deabd7a2ebaedd96757cc96183402
Wannte <T.wannte@gmail.com>
2021-02-04 18:19:08 +0900

    feat(InputController): trial 유효성 검사 및 입력 받기

--- src/main/java/javaracingcar/controller/InputController.java
+++ src/main/java/javaracingcar/controller/InputController.java
@@ -2,6 +2,7 @@ package javaracingcar.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class InputController {
     public static List<String> splitInput(String input) {
@@ -12,5 +13,13 @@ public class InputController {
         List<String> names;
         names = Arrays.asList(input.split(","));
         return names;
+    }
+
+    public static int getTrialInput(String input) {
+        input = input.trim();
+        if (!Pattern.matches("^[1-9]+[0-9]*$", input)) {
+            throw new IllegalArgumentException("양의 정수가 아닙니다.");
+        }
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/javaracingcar/controller/InputControllerTest.java
+++ src/test/java/javaracingcar/controller/InputControllerTest.java
@@ -2,18 +2,15 @@ package javaracingcar.controller;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputControllerTest {
-
     @Test
-    void splitInput_null입력() {
+    void splitInput_null_입력() {
         assertThatThrownBy(() -> InputController.splitInput(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("null");
@@ -22,8 +19,25 @@ class InputControllerTest {
     @Test
     void splitInput_정상입력() {
         String input = ",,A,B,C";
-        List<String> names = Arrays.asList("","","A","B","C");
-        assertEquals(InputController.splitInput(input).size(), names.size());
+        List<String> names = Arrays.asList("", "", "A", "B", "C");
+        assertEquals(InputController.splitInput(input)
+                                    .size(), names.size());
         assertEquals(names, InputController.splitInput(input));
+    }
+
+    @Test
+    void getTrialInput_자연수가아닌경우() {
+        assertThatThrownBy(() -> InputController.getTrialInput("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("양의 정수");
+        assertThatThrownBy(() -> InputController.getTrialInput("-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("양의 정수");
+    }
+
+    @Test
+    void getTrialInput_정상입력인경우() {
+        assertEquals(InputController.getTrialInput("30"), 30);
+        assertEquals(InputController.getTrialInput("3"), 3);
     }
 }
