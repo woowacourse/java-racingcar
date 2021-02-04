001717474d73c8c1ec9ea4b3ee587782331b6cdb
sakjung <ssangyu123@gmail.com>
2021-02-04 17:51:27 +0900

    test: 스틱 만들어지는지 테스트 하는 메서드 csvsource형태로 바꿈

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,33 +1,18 @@
 package racingcar.view;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
-import racingcar.domain.RacingGame;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.dto.CarResponseDto;
-import racingcar.dto.CarsResponseDto;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class OutputViewTest {
     @DisplayName("스틱이 제대로 만들어 지는지 확인")
-    @Test
-    void makeStick_스틱_생성_제대로_되는지() {
-
-        CarsResponseDto carsResponseDto = new CarsResponseDto();
-        CarResponseDto car1 = new CarResponseDto("포비", 3);
-        CarResponseDto car2 = new CarResponseDto("웨지", 2);
-        CarResponseDto car3 = new CarResponseDto("삭정", 1);
-
-        List<CarResponseDto> dtoList = Arrays.asList(car1, car2, car3);
-        List<String> sticks = Arrays.asList("---","--","-");
-
-        for (int i = 0; i < dtoList.size(); i++) {
-            assertThat(OutputView.makeStick(dtoList.get(i))).isEqualTo(sticks.get(i));
-        }
+    @ParameterizedTest
+    @CsvSource({"포비,3,---", "웨지,2,--", "삭정,1,-"})
+    void makeStick_스틱_생성_제대로_되는지(String name, int position, String stick) {
+        CarResponseDto car = new CarResponseDto(name, position);
+        assertThat(OutputView.makeStick(car)).isEqualTo(stick);
     }
 }
