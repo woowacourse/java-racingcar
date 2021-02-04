9c604e083c388c4856f5fff18572acc0d85e7252
pjy1368 <basejin0810@naver.com>
2021-02-04 21:38:30 +0900

    feat(Winner): 우승자 일급 컬렉션 처리

--- src/main/java/racingCar/domain/Winner.java
+++ src/main/java/racingCar/domain/Winner.java
@@ -1,2 +1,20 @@
-package racingCar.domain;public class Winner {
+package racingCar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winner {
+
+  private static final String WINNER_NAME_DELIMITER = ", ";
+  private List<Car> cars;
+
+  public Winner(List<Car> cars) {
+    this.cars = cars;
+  }
+
+  public String getWinnerName() {
+    return cars.stream()
+        .map(Car::getName)
+        .collect(Collectors.joining(WINNER_NAME_DELIMITER));
+  }
 }
