0d04dfdf74f54f0d1e01c6b5dfb0f3e7643471d1
xrabcde <xrabcde@gmail.com>
2021-02-04 14:12:30 +0900

    feat(RacingCarViewTest): 경주할 자동차 이름을 받아오기 위한 문구출력 테스트코드 작성

---
+++ src/test/java/racingcar/view/RacingCarViewTest.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RacingCarViewTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void carListInput() {
+        RacingCarView.carListInput();
+        assertEquals(INPUT_MESSAGE, outputStreamCaptor.toString());
+    }
+}
