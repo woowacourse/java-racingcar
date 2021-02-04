d17c16ce351e9dbd24617ca880a019bda372338d
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-04 20:44:51 +0900

    feat: 유저 입력(자동차이름, 횟수) 및 예외처리 완료

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,15 @@
 package view;
 
 public class OutputView {
+    public static void printUserPromptCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public static void printUserPromptTrials() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printErrorRetrialMessage() {
+        System.out.println("다시 입력해주세요.");
+    }
 }
