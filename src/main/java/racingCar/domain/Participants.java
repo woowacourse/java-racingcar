d61429331be5415881ac4e25fd8cd1fb41b2d448
pjy1368 <basejin0810@naver.com>
2021-02-04 21:37:04 +0900

    feat(Participants): 참여자 일급 컬렉션 처리

--- src/main/java/racingCar/domain/Participants.java
+++ src/main/java/racingCar/domain/Participants.java
@@ -1,2 +1,25 @@
-package racingCar.domain;public class Participants {
+package racingCar.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Participants {
+
+  private List<Car> cars;
+
+  private Participants() {
+    cars = new ArrayList<>();
+  }
+
+  public Participants(String... names) {
+    this();
+    for (String name : names) {
+      cars.add(new Car(name));
+    }
+  }
+
+  public List<Car> getCars() {
+    return Collections.unmodifiableList(cars);
+  }
 }
