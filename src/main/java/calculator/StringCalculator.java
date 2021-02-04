8614d2a644f044941e40b0bc5d466196af44ee17
y_woo :) <dusdn1702@naver.com>
2021-02-04 19:09:11 +0900

    refactor: Number 객체 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,28 +10,16 @@ public class StringCalculator {
     private static Delimiters delimiters = new Delimiters();
 
     public static int splitAndSum(String text) {
-        if(isNull(text) || isEmpty(text)){
+        if(text == null || text.isEmpty()){
             return ZERO;
         }
+
         Matcher matchedPattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(text);
         if(matchedPattern.find()){
             delimiters.addCustomDelimiters(matchedPattern.group(1));
             text = matchedPattern.group(2);
         }
         return splitAndSumByDelimiters(text);
-    }
-
-    private static boolean isNull(String text) {
-        try{
-            isEmpty(text);
-        } catch (NullPointerException e){
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isEmpty(String text) {
-        return text.isEmpty();
     }
 
     private static int splitAndSumByDelimiters(String text) {
@@ -41,24 +29,10 @@ public class StringCalculator {
 
     private static int sumNumbers(String[] numbers) {
         int result = 0;
+
         for (String numberText : numbers){
-            result += getValidatedNumber(numberText);
+            result += new Number(numberText).getNumber();
         }
         return result;
-    }
-
-    private static int getValidatedNumber(String numberText){
-        try{
-            return getPositiveNumber(Integer.parseInt(numberText));
-        } catch(NumberFormatException e){
-            throw new RuntimeException();
-        }
-    }
-
-    private static int getPositiveNumber(int number) {
-        if(number < 0){
-            throw new RuntimeException();
-        }
-        return number;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -37,11 +40,21 @@ public class StringCalculatorTest {
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
+        int result2 = StringCalculator.splitAndSum("//%\n4%%1%");
+        assertThat(result2).isEqualTo(5);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/;\n1,2,3", "//\n1:2:33", "//;\n-1;10"})
+    @DisplayName("옳지 않은 custom 구분자 입력 검증")
+    public void splitAndSum_several_custom_구분자_exception(String text) throws Exception {
+        assertThatThrownBy(() -> StringCalculator.splitAndSum(text))
                 .isInstanceOf(RuntimeException.class);
     }
 }
