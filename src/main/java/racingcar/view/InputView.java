8b792bb00dafb85c0c77586fbeea425c2b77f644
sakjung <ssangyu123@gmail.com>
2021-02-04 13:22:52 +0900

    feat: inputview 구현 및 이름받기 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public String nextLine() {
+        return scanner.nextLine();
+    }
+}
