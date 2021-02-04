7d12efac75536fef48b8b3f14f634c19bf3f4529
Minjeong <mathmjseo@naver.com>
2021-02-04 10:54:33 +0900

    refactor: 빈 문자열 및 null 문자열 확인 기능 메서드로 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,26 +3,25 @@ package calculator;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
 public class StringCalculator {
     private static final String COMMA = ",";
     private static final String COLON = ":";
     private static final String VERTICAL_LINE = "|";
 
-    public static int splitAndSum(String string) {
-        if (string == null || string.isEmpty()) {
+    public static int splitAndSum(String text) {
+        if (checkEmptyOrNullString(text)) {
             return 0;
         }
 
-        if (string.length() == 1 && Character.isDigit(string.charAt(0))) {
-            return Integer.parseInt(string);
+        if (text.length() == 1 && Character.isDigit(text.charAt(0))) {
+            return Integer.parseInt(text);
         }
 
-        String[] numbers = string.split(COMMA + VERTICAL_LINE + COLON);
+        String[] numbers = text.split(COMMA + VERTICAL_LINE + COLON);
         int result = 0;
 
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(string);
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
         if (matcher.find()) {
             String delimiter = matcher.group(1);
             numbers = matcher.group(2).split(delimiter);
@@ -34,6 +33,13 @@ public class StringCalculator {
         result = sum(numbers);
 
         return result;
+    }
+
+    private static boolean checkEmptyOrNullString(String text) {
+        if (text == null || text.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 
     private static boolean checkAllDigit(String[] numbers) {
