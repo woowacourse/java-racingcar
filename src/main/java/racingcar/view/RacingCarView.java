8eb8b5267dc3de38b2a1aa7834b8834bef97bf7b
xrabcde <xrabcde@gmail.com>
2021-02-04 15:53:06 +0900

    feat(RacingCarView): 시도할 횟수를 받아오기 위한 문구출력 기능 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -2,8 +2,13 @@ package racingcar.view;
 
 public class RacingCarView {
     private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
+    private static final String TURN_INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
 
     public static void carListInput() {
         System.out.printf(INPUT_MESSAGE);
+    }
+
+    public static void turnNumberInput() {
+        System.out.printf(TURN_INPUT_MESSAGE);
     }
 }
