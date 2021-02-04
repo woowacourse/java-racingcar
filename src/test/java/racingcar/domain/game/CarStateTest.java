1c2c348732a26c66a921a2bdacedaa3967dd6a93
DWL5 <ssop6403@gmail.com>
2021-02-05 00:38:40 +0900

    test:CarStateTest 추가

---
+++ src/test/java/racingcar/domain/game/CarStateTest.java
@@ -0,0 +1,18 @@
+package racingcar.domain.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.CarName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarStateTest {
+    @Test
+    @DisplayName("자동차의 현재 상태를 출력")
+    public void toString_자동차의_현재_상태를_출력() {
+        assertEquals(CarState.withNameAndPosition(new CarName("포비"), 3).toString(), "포비 : ---");
+        assertEquals(CarState.withNameAndPosition(new CarName("포비"), 1).toString(), "포비 : -");
+        assertEquals(CarState.withNameAndPosition(new CarName("포비"), 5).toString(), "포비 : -----");
+        assertEquals(CarState.withNameAndPosition(new CarName("포비"), 0).toString(), "포비 : ");
+    }
+}
