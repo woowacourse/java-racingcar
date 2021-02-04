2f5126905978cd1d4e0d19e4d3dc59f950fe8353
DWL5 <ssop6403@gmail.com>
2021-02-04 21:02:35 +0900

    test: 이동횟수가 양의 정수가 아닐 경우 예외 테스트 케이스 추가

--- src/test/java/racingcar/domain/MoveCountInputTest.java
+++ src/test/java/racingcar/domain/MoveCountInputTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.CarMoveCountNegativeException;
 import racingcar.exception.CarMoveCountNonNumericException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
@@ -15,5 +16,14 @@ public class MoveCountInputTest {
         assertThatExceptionOfType(CarMoveCountNonNumericException.class)
                 .isThrownBy(() -> new MoveCountInput(input))
                 .withMessageContaining(new CarMoveCountNonNumericException().getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("이동횟수가_양의 정수가_아닐_경우_예외")
+    @ValueSource(strings = {"-1","-4", "-3"})
+    public void 이동횟수가_양의_정수가_아닐_경우_예외(String input) {
+        assertThatExceptionOfType(CarMoveCountNegativeException.class)
+                .isThrownBy(() -> new MoveCountInput(input))
+                .withMessageContaining(new CarMoveCountNegativeException().getMessage());
     }
 }
