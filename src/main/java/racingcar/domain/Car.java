611e98a875d293f7ecd7dcd67530257f004b904f
xrabcde <xrabcde@gmail.com>
2021-02-04 15:49:24 +0900

    feat(Car, Cars): Car, Cars 객체 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int position;
+
+    private static final int GO_CRITERIA = 4;
+    private static final int INITIAL_POSITION = 0;
+
+    public Car(String name) {
+        name = this.name;
+        position = INITIAL_POSITION;
+    }
+}
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static final List<Car> carList = new ArrayList<>();
+
+    public Cars(List<String> carNameList) {
+        carNameList.stream()
+        .map(carName -> carList.add(new Car(carName)));
+    }
+
+}
