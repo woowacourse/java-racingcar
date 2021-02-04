a9e28de11205a25cba0e64ee9786145bedadff1f
pika96 <52442949+pika96@users.noreply.github.com>
2021-02-04 19:34:44 +0900

    feat : Car 클래스 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,43 @@
 package model;
 
+import utils.RandomUtils;
+
 public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void movePosition() {
+        this.position++;
+    }
+
+    public int getRandomNumber() {
+        return RandomUtils.nextInt(0, 9);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+//    @Override
+//    public int compareTo(Car car) {
+//        if (this.getPosition() < car.getPosition()) {
+//            return -1;
+//        }
+//
+//        if (this.getPosition() > car.getPosition()) {
+//            return 1;
+//        }
+//
+//        return 0;
+//    }
 }
--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -1,3 +1,5 @@
+package utils;
+
 import java.util.Random;
 
 public class RandomUtils {
