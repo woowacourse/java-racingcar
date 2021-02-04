d73ea4f22f260842b98bed6b31947d104a228759
pjy1368 <basejin0810@naver.com>
2021-02-04 16:21:13 +0900

    feat(Car): 전진 기능 구현

--- src/main/java/racingCar/service/Car.java
+++ src/main/java/racingCar/service/Car.java
@@ -2,12 +2,22 @@ package racingCar.service;
 
 public class Car {
 
+  private static final int MIN_RUNNABLE_FUEL = 4;
   private final String name;
   private int position = 0;
+
 
   public Car(String name) {
     this.name = name;
   }
 
-  // 추가 기능 구현
+  public void run(int fuel) {
+    if (isRunnable(fuel)) {
+      this.position++;
+    }
+  }
+
+  private boolean isRunnable(int fuel) {
+    return false;
+  }
 }
