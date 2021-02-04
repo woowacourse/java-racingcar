814f186bc78cf2b6b6c4eef0bc61bdd83d48940d
sakjung <ssangyu123@gmail.com>
2021-02-04 14:19:35 +0900

    feat: 리더보드 출력 기능 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -22,6 +22,7 @@ public class RacingGameController {
         OutputView.output(EXECUTE_RESULT_MSG);
         for (int i = 0; i < round; i++) {
             racingGame.playRound();
+            OutputView.printLeaderBoard(racingGame.getCars());
         }
 
         String winners = racingGame.decideWinner();
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,20 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.Collections;
+import java.util.List;
+
 public class OutputView {
     public static void output(String message){
         System.out.println(message);
+    }
+
+    public static void printLeaderBoard(List<Car> cars) {
+        for (Car car : cars) {
+            String positionStick = String.join("", Collections.nCopies(car.getPosition(), "-"));
+            System.out.println(String.format("%s : %s", car.getName(), positionStick));
+        }
+        System.out.println();
     }
 }
