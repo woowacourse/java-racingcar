d6d696059142c8bd4370ec37a3dae7678b821fc7
김주원 <kjw11077@naver.com>
2021-02-04 18:15:24 +0900

    feat: 어플리케이션 로직 구성 - 컨트롤러

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import racingcar.RacingGameController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        RacingGameController racingGameController = new RacingGameController();
+        racingGameController.startRace();
+
+    }
+}
---
+++ src/main/java/racingcar/RacingGameController.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class RacingGameController {
+    private final InputView inputView;
+
+    public RacingGameController() {
+        inputView = new InputView();
+    }
+
+    public void startRace() {
+
+    }
+}
