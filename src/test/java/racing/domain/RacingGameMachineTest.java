81eb58ba704958f6fdd565102b1a4164e4304c04
TaeJeong Kim <xowjd41@naver.com>
2021-02-04 14:56:51 +0900

    feat: 시도 횟수가 양수가 아니면 에러가 발생한다.

--- src/test/java/racing/domain/RacingGameMachineTest.java
+++ src/test/java/racing/domain/RacingGameMachineTest.java
@@ -2,23 +2,42 @@ package racing.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 class RacingGameMachineTest {
 
     @Test
     void Machine_tryCounts가_줄어든다() {
-        int tryCount = 1;
-        RacingGameMachine racingGameMachine = new RacingGameMachine(tryCount);
+        int tryCounts = 1;
+        RacingGameMachine racingGameMachine = new RacingGameMachine(tryCounts);
         racingGameMachine.play();
+        assertThat(racingGameMachine.canPlay()).isFalse();
+    }
 
-        Assertions.assertThat(racingGameMachine.canPlay()).isFalse();
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void Machine_시도횟수가_양수가_아니면_에러가_발생한다(int tryCounts) {
+        assertThatThrownBy(() -> new RacingGameMachine(tryCounts))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private static class RacingGameMachine {
+        private static final int ZERO = 0;
+
         private int tryCounts;
 
         RacingGameMachine(int tryCounts) {
             this.tryCounts = tryCounts;
+            validateTryCounts();
+        }
+
+        private void validateTryCounts() {
+            if (tryCounts <= ZERO) {
+                throw new IllegalArgumentException();
+            }
         }
 
         public void play() {
@@ -26,7 +45,7 @@ class RacingGameMachineTest {
         }
 
         public boolean canPlay() {
-            return tryCounts > 0;
+            return tryCounts > ZERO;
         }
     }
 }
