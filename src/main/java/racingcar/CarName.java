154fa939fd0daf6b5e0b336a9fd1510f1fcc51b0
TaewanKimmmm <ktww88@gmail.com>
2021-02-04 16:46:35 +0900

    feat: 이름이 같은 자동차를 같은 객체 취급하기 위해 hashCode와 equals 오버라이딩

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class CarName {
     private String carName;
 
@@ -16,5 +18,18 @@ public class CarName {
         if (carName.length() < 1 || carName.length() > 5) {
             throw new IllegalArgumentException("1~5 사이의 값을 입력해주세요.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarName carName1 = (CarName) o;
+        return Objects.equals(carName, carName1.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName);
     }
 }
