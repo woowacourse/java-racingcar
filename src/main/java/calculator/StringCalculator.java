d3199ce8ffd3f400c64876f9082ba6a06c9eb4d1
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-04 14:27:14 +0900

    refactor: StringCalculator 함수 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,40 +1,62 @@
 package calculator;
 
+import java.lang.reflect.Method;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public static int splitAndSum(String text) throws RuntimeException {
-        if (text == null || text.isEmpty())
-            return 0;
-        int number;
+    private static int number;
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private static final String SPLIT_REGEX = ",|:";
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+    public static int splitAndSum(String text) throws RuntimeException {
+        if (checkNullOrEmpty(text)) {
+            return 0;
+        }
+        if ((number = parseByCustomDelimiter(text)) != -1) {
+            return number;
+        }
+        if ((number = parseBySplitRegex(text)) != -1) {
+            return number;
+        }
+        return parseSingleTextToInt(text);
+    }
+
+    private static int parseSingleTextToInt(String text) {
+        return validateNegative(text);
+    }
+
+    private static boolean checkNullOrEmpty(String text) {
+        return (text == null || text.isEmpty());
+    }
+
+    private static int parseByCustomDelimiter(String text){
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens= m.group(2).split(customDelimiter);
-            // 덧셈 구현
             return addElements(tokens);
         }
+        return -1;
+    }
 
-        String[] numbers = text.split(",|:");
+    private static int parseBySplitRegex(String text) {
+        String[] numbers = text.split(SPLIT_REGEX);
         if (numbers.length >= 2){
             return addElements(numbers);
         }
-
-        number = checkNegative(text);
-        return number;
+        return -1;
     }
 
     private static int addElements(String[] numbers) throws RuntimeException {
         int number = 0;
         for (int i = 0; i < numbers.length; i++){
-            number += checkNegative(numbers[i]);
+            number += validateNegative(numbers[i]);
         }
         return number;
     }
 
-    private static int checkNegative(String numberText) throws RuntimeException {
+    private static int validateNegative(String numberText) throws RuntimeException {
         int parsedNumber = Integer.parseInt(numberText);
         if (parsedNumber < 0) {
             throw new RuntimeException();
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,28 +17,41 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
-    @Test
-    public void splitAndSum_숫자하나() throws Exception {
-        int result = StringCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "12,12", "123,123"})
+    public void splitAndSum_숫자하나(String input, int expectedResult) throws Exception {
+        int result = StringCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringCalculator.splitAndSum("1,2");
-        assertThat(result).isEqualTo(3);
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3=6", "12,23=35", "1,2,3,4=10"}, delimiter = '=')
+    public void splitAndSum_쉼표구분자(String input, int expectedResult) throws Exception {
+        int result = StringCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringCalculator.splitAndSum("1,2:3");
-        assertThat(result).isEqualTo(6);
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3=6", "12:23=35", "1:2,3:4=10"}, delimiter = '=')
+    public void splitAndSum_쉼표_또는_콜론_구분자(String input, int expectedResult) throws Exception {
+        int result = StringCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
+//    @ParameterizedTest
+//    @CsvSource(value = {"//;\n1;2;3=6", "//!\n12!23=35", "//&\n1&2&3&4=10"}, delimiter = '=')
+//    public void splitAndSum_custom_구분자(String input, int expectedResult) throws Exception {
+//        String test = "//;\n1;2;3";
+//        System.out.println(input.equals(test));
+//        System.out.println(input);
+//        int result = StringCalculator.splitAndSum(input);
+//        assertThat(result).isEqualTo(expectedResult);
+//    }
+
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
+    public void splitAndSum_custom_구분자2() throws Exception {
+        int result = StringCalculator.splitAndSum("//!\n12!23");
+        assertThat(result).isEqualTo(35);
     }
 
     @Test
