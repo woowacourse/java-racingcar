f3a98645190698ac4ed320aaa5fae25bcabed48d
pjy1368 <basejin0810@naver.com>
2021-02-04 16:30:02 +0900

    feat(Car): position getter 추가

--- src/main/java/racingCar/service/Car.java
+++ src/main/java/racingCar/service/Car.java
@@ -2,10 +2,9 @@ package racingCar.service;
 
 public class Car {
 
-  private static final int MIN_RUNNABLE_FUEL = 4;
+  private final static int MIN_RUNNABLE_FUEL = 4;
   private final String name;
   private int position = 0;
-
 
   public Car(String name) {
     this.name = name;
@@ -17,7 +16,12 @@ public class Car {
     }
   }
 
-  private boolean isRunnable(int fuel) {
-    return false;
+  public int getPosition() {
+    return position;
   }
+
+  private boolean isRunnable(int fuel) {
+   return fuel >= MIN_RUNNABLE_FUEL;
+  }
+
 }
