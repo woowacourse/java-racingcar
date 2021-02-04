90c0e46f1bd2247f4643d76c2f44d92fcaf4fc7d
y_woo :) <dusdn1702@naver.com>
2021-02-04 11:51:49 +0900

    refactor: 빈 문자 또는 null 입력시 0 반환 코드 수정
    null 검사 메서드와 빈 문자 검사 메서드 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,20 +1,36 @@
 package calculator;
 
 public class StringCalculator {
-
     private static final int ZERO = 0;
+    public static final int INIT_NUMBER = -1;
 
     public static int splitAndSum(String text) {
-        if (text != null && !text.isEmpty()) {
-            return 1;
+        int result = INIT_NUMBER;
+        if(isNull(text)){
+            result = ZERO;
+            return result;
         }
-        return ZERO;
+        if(isEmpty(text)) {
+            result = ZERO;
+            return result;
+        }
+        return result;
     }
 
-//    private static boolean isNothing(String input) {
-//        if (input.isEmpty()) {
-//            return true;
-//        }
-//        return false;
-//    }
+    private static boolean isEmpty(String text) {
+        return text.isEmpty();
+    }
+
+    private static void checkForNull(String text) {
+        text.isEmpty();
+    }
+
+    private static boolean isNull(String text) {
+        try{
+            checkForNull(text);
+        } catch (NullPointerException e){
+            return true;
+        }
+        return false;
+    }
 }
