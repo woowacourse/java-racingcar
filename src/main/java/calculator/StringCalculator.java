732555111a11bc363ce50ce69187702af13761c7
y_woo :) <dusdn1702@naver.com>
2021-02-04 11:18:46 +0900

    feat: 빈 문자 또는 null 입력시 0 반환 코드 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class StringCalculator {
+
+    private static final int ZERO = 0;
+
+    public static int splitAndSum(String text) {
+        if (text != null && !text.isEmpty()) {
+            return 1;
+        }
+        return ZERO;
+    }
+
+//    private static boolean isNothing(String input) {
+//        if (input.isEmpty()) {
+//            return true;
+//        }
+//        return false;
+//    }
+}
