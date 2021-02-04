28ddb4497876bea47e7718877c70f93e822ffea4
lns13301 <gyuhanKang@gmail.com>
2021-02-04 17:58:12 +0900

    test: 경주 전진 테스트 추가

---
+++ src/test/java/racingcar/GoForwardOrStopTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GoForwardOrStopTest {
+
+    @DisplayName("숫자를 입력받아 전진 여부 결정")
+    @Test
+    void isGoForward() {
+        int goForwardNumber = 4;
+        int stopNumber = 3;
+
+        Assertions.assertThat(GoForwardOrStop.isGoForward(goForwardNumber)).isTrue();
+        Assertions.assertThat(GoForwardOrStop.isGoForward(stopNumber)).isFalse();
+    }
+}
