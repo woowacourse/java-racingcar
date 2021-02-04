fb7fce1593fb903ca3a4e5d0be651318a69e0bf7
seed <hsik0225@gmail.com>
2021-02-04 16:27:19 +0900

    feat(string-splitter):문자열에서 숫자만 자르는 기능 구현

---
+++ src/test/java/calculator/StringSplitter.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringSplitter {
+    
+    private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+    
+    private static final String DEFAULT_REGEX = ",|:";
+    
+    private static final String OR = "|";
+    
+    private String input;
+    
+    public StringSplitter(String input) {
+        this.input = input;
+    }
+    
+    public String[] getStringSplitter(){
+        final String delimiter = getDelimiter();
+        
+        return input.split(delimiter);
+    }
+    
+    public String getDelimiter() {
+        Matcher matcher = PATTERN.matcher(input);
+        
+        String regex = DEFAULT_REGEX;
+        
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            input = matcher.group(2);
+            regex = customDelimiter + OR + DEFAULT_REGEX;
+        }
+        
+        return regex;
+    }
+}
