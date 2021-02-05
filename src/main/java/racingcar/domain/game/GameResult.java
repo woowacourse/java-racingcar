24ca6710af471f176e0cee8d5445eb9660308578
DWL5 <ssop6403@gmail.com>
2021-02-05 09:32:17 +0900

    refactor: GameResult에서 CarstateBuffer도 리턴 하도록 수정

--- src/main/java/racingcar/domain/game/GameResult.java
+++ src/main/java/racingcar/domain/game/GameResult.java
@@ -1,6 +1,7 @@
 package racingcar.domain.game;
 
 import racingcar.domain.car.CarName;
+import racingcar.domain.car.CarStatesBuffer;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,20 +12,25 @@ public class GameResult {
     private static final String MESSAGE_GAME_RESULT = "가 최종 우승했습니다.";
 
     private List<CarName> winners;
+    private CarStatesBuffer carStatesBuffer;
 
-    private GameResult(List<CarName> winners) {
+    private GameResult(List<CarName> winners, CarStatesBuffer carStateBuffer) {
         this.winners = winners;
+        this.carStatesBuffer = carStateBuffer;
     }
 
-    public static GameResult valueOf(List<CarName> winners) {
-        return new GameResult(winners);
+    public static GameResult valueOf(List<CarName> winners, CarStatesBuffer carStatesBuffer) {
+        return new GameResult(winners, carStatesBuffer);
     }
 
-    @Override
-    public String toString() {
+    public String getWinnersString() {
         List<String> winnerNames = winners.stream().map(CarName::toString).collect(Collectors.toList());
         Collections.sort(winnerNames);
 
         return String.join(DELIMITER, winnerNames) + MESSAGE_GAME_RESULT;
+    }
+
+    public String getExecutionResultString() {
+        return carStatesBuffer.toString();
     }
 }
