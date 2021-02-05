d202f611bf92cfc048095bcea4e0966034ab0543
mungto <daum7766@naver.com>
2021-02-05 17:37:33 +0900

    feat(Cars): move funtion create

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,10 +1,15 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+import racingcar.view.OutputView;
 
 public class Cars {
     private List<Car> cars;
+    private static final int BOUND = 10;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
@@ -12,5 +17,13 @@ public class Cars {
 
     public List<Car> getCars() {
         return Collections.unmodifiableList(cars);
+    }
+
+    public void move(Random random) {
+        for(Car car : cars) {
+            car.move(random.nextInt(BOUND));
+            OutputView.printCarStateMessage(car.getName(), car.positionToString());
+        }
+        OutputView.printNextLine();
     }
 }
