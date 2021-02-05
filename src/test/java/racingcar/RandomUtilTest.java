089feca3d7f785e097fc1a1115be499c0a7b1616
TaewanKimmmm <ktww88@gmail.com>
2021-02-05 17:31:14 +0900

    랜덤 숫자 생성 테스트

---
+++ src/test/java/racingcar/RandomUtilTest.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomUtilTest {
+    @RepeatedTest(50)
+    void checkRandomNumber() {
+        int randomNumber = RandomUtil.nextInt(0,9);
+        assertThat(-1 < randomNumber && randomNumber < 10).isTrue();
+    }
+}
