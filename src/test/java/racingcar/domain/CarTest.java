3ca206d861c45f23facbf539be065823eab1a6d2
pjy1368 <basejin0810@naver.com>
2021-02-05 16:22:33 +0900

    refactor(CarTest): 메소드명 수정 및 파라미터 기반 테스트로 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,7 +3,11 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
@@ -14,25 +18,27 @@ class CarTest {
     this.car = new Car("Pobi");
   }
 
-  @Test
-  void run_success() {
-    car.fillInFuel(Car.MIN_RUNNABLE_FUEL);
+  @ParameterizedTest
+  @CsvSource(value = {Car.MIN_RUNNABLE_FUEL + ":1", Car.MIN_RUNNABLE_FUEL - 1 + ":0"}, delimiter = ':')
+  @DisplayName("최소 달릴 수 있는 연료를 차에 주입했을 때 1 만큼 전진을 하였는 지 확인")
+  void run(int fuel, int expected) {
+    car.fillInFuel(fuel);
     car.run();
-    assertThat(car.getPosition()).isEqualTo(1);
+    assertThat(car.getPosition()).isEqualTo(expected);
   }
 
   @Test
-  void run_failure() {
-    car.fillInFuel(Car.MIN_RUNNABLE_FUEL - 1);
-    car.run();
-    assertThat(car.getPosition()).isEqualTo(0);
-  }
-
-  @Test
-  void checkFuelWhenRun() {
+  void checkFuelAfterRun() {
     car.fillInFuel(Car.MIN_RUNNABLE_FUEL);
     car.run();
     assertThat(car.getFuel()).isEqualTo(0);
   }
 
+  @ParameterizedTest
+  @ValueSource(strings = {"", "spring"})
+  @DisplayName("이름 길이가 0 이하 또는 5 이상일 때 에러 확인")
+  void car_name_exception(String name) {
+    assertThatThrownBy(() -> new Car(name))
+        .isInstanceOf(IllegalStateException.class);
+  }
 }
