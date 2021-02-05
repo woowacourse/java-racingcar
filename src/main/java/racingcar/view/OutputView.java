a3e82595e670439a02cc04e81faba14f6827afd1
mungto <daum7766@naver.com>
2021-02-05 16:06:30 +0900

    feat(outputView): create printInputCarNamesMessage funtion

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static void printInputCarNamesMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+}
