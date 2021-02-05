f5daa9f6fa4d1b93f6b62bace4961643055276bb
pjy1368 <basejin0810@naver.com>
2021-02-05 15:54:18 +0900

    feat(Application): 레이싱카 최종 흐름 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,24 @@
 package racingcar;
 
-import java.util.Scanner;
+import racingcar.domain.GasStation;
+import racingcar.domain.Participants;
+import racingcar.domain.RacingManager;
+import racingcar.domain.RacingResult;
+import racingcar.utils.RandomUtils;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
 
   public static void main(String[] args) {
-    final Scanner scanner = new Scanner(System.in);
-    // TODO 구현 진행
+    Participants participants = InputView.getNames();
+    int turn = InputView.getTurn();
+    GasStation gasStation = new GasStation(() -> RandomUtils.nextInt(0, 9));
+
+    RacingManager racingManager = new RacingManager(participants, turn, gasStation);
+
+    RacingResult racingResult = racingManager.start();
+    OutputView.printProgressResult(racingResult.getLog());
+    OutputView.printWinner(racingResult.getWinner());
   }
 }
