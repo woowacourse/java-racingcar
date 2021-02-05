721ad05880f49ead5e7bb61182976e72b6866dcf
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-05 20:23:54 +0900

    test(RacingCarTest): add @DisplayName to all functions

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -19,18 +20,16 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"benz", "bmw", "suv"})
+    @DisplayName("이름이 5글자 이하일 때 Car 객체가 생성되는지 테스트")
     void carNameTest(String text) {
-        // 자동차 객체가 만들어질때 에러가 안나고, 이름이 잘 저장되는가?
-        // 5글자 이하만 넣었을때
         Car car = new Car(text);
         assertThat(car.getName()).isEqualTo(text);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"grandeur", "sportsCar", "sonata"})
+    @DisplayName("이름이 6글자 이상일 때 Car 객체 생성 시 에러 발생하는지 테스트")
     void carNameLengthErrorTest(String text) {
-        // 자동차 객체가 만들어질때 에러가 나는가?
-        // 5글자 이상을 넣었을때
         assertThatThrownBy(() -> {
             new Car(text);
         }).isInstanceOf(IllegalArgumentException.class);
@@ -38,6 +37,7 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @CsvSource(value = {"pobi,crong,honux:3", "mt:1", "oz,mt:2"}, delimiter = ':')
+    @DisplayName("자동차 여러 대를 만들 때 Cars 리스트에 정상적으로 추가가 되었는지 테스트")
     void carCreateLengthTest(String input, String expected) {
         List<Car> carList = new ArrayList<>();
         String[] carNames = input.split(",");
@@ -50,8 +50,8 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,honux", "mt,oz", "oz,mt"})
+    @DisplayName("자동차 여러 대를 만들 때 이름이 일치하는지 테스트")
     void multiCarNameEqualTest(String text) {
-        // 입력한 이름과 객체들의 이름이 같은지 테스트
         List<Car> carList = new ArrayList<>();
         String[] carNames = text.split(",");
         for (String carName : carNames) {
@@ -66,6 +66,7 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1, -2, -3})
+    @DisplayName("양의 정수가 아닐 때 AttemptNumber 객체 생성 시 에러 발생하는지 테스트")
     void attemptsNumberErrorTest(int number) {
         // 에러가 잘 출력되는지 테스트
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -76,6 +77,7 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 5, 8, 10})
+    @DisplayName("양의 정수일 때 AttemptNumber 객체 생성되는지 테스트")
     void attemptsNumberTest(int number) {
         // 에러가 출력이 안되고 잘 동작하는지 테스트
         AttemptNumber attemptNumber = new AttemptNumber(number);
@@ -84,8 +86,8 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @CsvSource(value = {"9:1", "3:0", "5:1"}, delimiter = ':')
+    @DisplayName("랜덤 값이 4이상 일때 자동차 위치가 변하는지 테스트")
     void randomNumberPositionTest(String input, String expected) {
-        // 랜덤한 숫자에 따른 자동차 위치변환 상태확인
         Car car = new Car("temp");
         car.move(Integer.parseInt(input));
         assertThat(car.getPosition()).isEqualTo(Integer.parseInt(expected));
@@ -93,8 +95,8 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @CsvSource(value = {"3:---", "2:--", "5:-----"}, delimiter = ':')
+    @DisplayName("위치값이 문자열로 잘 변환되는지 테스트")
     void carPositionToStringTest(String input, String expected) {
-        // 전진한 거리만큼의 문자열이 잘 반환되는가?
         Car car = new Car("temp");
         for (int i = 0; i < Integer.parseInt(input); i++) {
             car.move(4);
@@ -106,6 +108,7 @@ public class RacingCarTest {
     @CsvSource(value = {"mt,oz,pobi@0,2:mt, pobi", "pobi,crong,honux@0:pobi",
         "pobi,crong,honux@1:crong", "pobi,crong,honux@2:honux", "pobi,crong,honux@1,2:crong, honux",
         "pobi,crong,honux@0,1,2:pobi, crong, honux"}, delimiter = ':')
+    @DisplayName("원하는 자동차 이동 후 우승자 문자열이 잘 반환되는지 테스트")
     void winnerCarTest(String input, String expected) {
         String[] inputArray = input.split("@");
         String[] carNames = inputArray[0].split(",");
