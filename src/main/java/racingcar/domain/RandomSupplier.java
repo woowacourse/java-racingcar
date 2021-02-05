53ee5802cdae6c29488140ae772f7fb95d28c884
pjy1368 <basejin0810@naver.com>
2021-02-05 19:28:07 +0900

    refactor(RandomSupplier): 랜덤 유틸 대신 랜덤 클래스 사용

--- src/main/java/racingcar/domain/RandomSupplier.java
+++ src/main/java/racingcar/domain/RandomSupplier.java
@@ -1,14 +1,13 @@
 package racingcar.domain;
 
+import java.util.Random;
 import java.util.function.Supplier;
-import racingcar.utils.RandomUtils;
 
 public class RandomSupplier implements Supplier<Integer> {
-  private static final int START_RANDOM_BOUND = 0;
-  private static final int END_RANDOM_BOUND = 9;
+  private static final int END_RANDOM_BOUND = 10;
 
   @Override
   public Integer get() {
-    return RandomUtils.nextInt(START_RANDOM_BOUND, END_RANDOM_BOUND);
+    return new Random().nextInt(END_RANDOM_BOUND);
   }
 }
