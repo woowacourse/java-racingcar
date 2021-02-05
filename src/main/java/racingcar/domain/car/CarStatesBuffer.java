b082110ad618db7334b8c9852fcf40edf0ac5a0a
DWL5 <ssop6403@gmail.com>
2021-02-05 13:43:48 +0900

    fix: 실행 결과 후 한줄 띄기 안되는 현상 수정

--- src/main/java/racingcar/domain/car/CarStatesBuffer.java
+++ src/main/java/racingcar/domain/car/CarStatesBuffer.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarStatesBuffer {
-    private static final String DELIMITER = "\n\n";
+    private static final String DELIMITER = "\n";
     private static final String EXECUTION_RESULT = "실행 결과\n";
 
     private List<CarStates> carStatesLog;
@@ -18,19 +18,16 @@ public class CarStatesBuffer {
         carStatesLog.add(carStates);
     }
 
-    public void clear() {
-        carStatesLog.clear();
-    }
-
     @Override
     public String toString() {
         StringBuffer stringBuffer = new StringBuffer();
 
         stringBuffer.append(EXECUTION_RESULT);
-        stringBuffer.append(String.join(DELIMITER,
+        stringBuffer.append(String.join(DELIMITER.repeat(2),
                 carStatesLog.stream()
                         .map(CarStates::toString)
                         .collect(Collectors.toList())));
+        stringBuffer.append(DELIMITER);
 
         return stringBuffer.toString();
     }
