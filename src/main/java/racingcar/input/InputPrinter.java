019b41daaadbe75b2b2b66b7bd06fede3f407c37
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-06 02:07:22 +0900

    refactor: 경주 시도 횟수 입력 메시지 출력부분 분리

--- src/main/java/racingcar/input/InputPrinter.java
+++ src/main/java/racingcar/input/InputPrinter.java
@@ -4,4 +4,8 @@ public class InputPrinter {
     public static void printCarNamesInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
+
+    public static void printRacingTryTimeInputMessage() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
 }
