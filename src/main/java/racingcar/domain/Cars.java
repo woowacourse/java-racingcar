65c1abfa91b3ad0fb383e7cb8c54107f2be6a8b3
sor <bosl95@naver.com>
2021-02-05 14:00:11 +0900

    feat : Cars 클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private List<Car> carList;
+
+    public Cars(String names) {
+        carList = new ArrayList<>();
+        this.carList.addAll(Arrays.stream(names.split(","))
+                .map(name -> new Car(name))
+                .collect(Collectors.toList()));
+    }
+
+    public List<String> getCarNames() {
+        return carList.stream()
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+    }
+}
