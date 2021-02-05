1f4239fc496fb3c8592f4a591ebdbaf234b5a498
seed <hsik0225@gmail.com>
2021-02-06 04:23:47 +0900

    feat(view):InputView, OutputView 구현

--- src/main/java/car/OutputView.java
+++ src/main/java/car/OutputView.java
@@ -1,45 +1,37 @@
 package car;
 
-import java.util.*;
-
 public class OutputView {
+    
     private static final String MOVE_SYMBOL = "-";
+    
     private static final String NULL_CHARACTER = "\0";
+    
     private static final String FINAL_WINNERS = "가 최종 우승했습니다.";
+    
     private static final String DELIMITER = ",";
+    
     private static final String COLON = " : ";
-    private static final String ROUND_RESULT = "실행 결과";
     
-    public void printResult(List<Car> cars) {
+    private static final String ROUND_RESULT = "\n실행 결과";
+    
+    public void printResult(ScoreBoard scoreBoard) {
         System.out.println(ROUND_RESULT);
-        
-        for (Car car : cars) {
-            String movedDistance = repeat(car.getPosition());
-            System.out.println(car.getName() + COLON + movedDistance);
+
+        for (Score score : scoreBoard.getScores()) {
+            String movedDistance = repeatSymbol(score.getPosition());
+            System.out.println(score.getName() + COLON + movedDistance);
         }
-    
+
         System.out.println();
     }
     
-    private String repeat(int times) {
+    private String repeatSymbol(int times) {
         return new String(new char[times]).replace(NULL_CHARACTER, MOVE_SYMBOL);
     }
     
-    public void printWinners(List<Car> cars) {
-        System.out.println(findWinners(cars) + FINAL_WINNERS);
-    }
-    
-    private String findWinners(List<Car> cars) {
-        final Map<Integer, List<String>> scoreBoard = new HashMap<>();
+    public void printWinners(ScoreBoard scoreBoard) {
+        String winners = String.join(DELIMITER, scoreBoard.findWinners());
         
-        for (Car racingCar : cars) {
-            final List<String> names =
-                    scoreBoard.computeIfAbsent(racingCar.getPosition(), ArrayList::new);
-            names.add(racingCar.getName());
-        }
-        
-        final int maxPosition = Collections.max(scoreBoard.keySet());
-        
-        return String.join(DELIMITER, scoreBoard.get(maxPosition));
+        System.out.println(winners + FINAL_WINNERS);
     }
 }
