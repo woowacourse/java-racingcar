f247b974f04b5324a6592902e87f07f8dec8a1f8
y_woo :) <dusdn1702@naver.com>
2021-02-05 16:07:30 +0900

    횟수 입력받는 기능 구현
    예외처리 완료

--- src/main/java/racingcar/domain/Lap.java
+++ src/main/java/racingcar/domain/Lap.java
@@ -4,7 +4,23 @@ public class Lap {
     private final int lap;
 
     public Lap(String inputLap) {
-        this.lap = Integer.parseInt(inputLap);
+        int numberLap = validateNumberLap(inputLap);
+        this.lap = validatePositiveLap(numberLap);
+    }
+
+    private int validateNumberLap(String inputLap) {
+        try{
+            return Integer.parseInt(inputLap);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자가 아닌 입력입니다.");
+        }
+    }
+
+    private int validatePositiveLap(int numberLap) {
+        if(numberLap < 1){
+            throw new IllegalArgumentException("1 미만의 입력입니다.");
+        }
+        return numberLap;
     }
 
     public int getLap() {
