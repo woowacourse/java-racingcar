300614ef7ba22de153852165f0bd0f6afefc8dfc
DWL5 <ssop6403@gmail.com>
2021-02-05 09:34:02 +0900

    refacotr: 사용하지 않는 상수 제거
    MoveCondition에서 STOP 제거

--- src/main/java/racingcar/domain/rule/MoveCondition.java
+++ src/main/java/racingcar/domain/rule/MoveCondition.java
@@ -5,7 +5,6 @@ import racingcar.domain.util.RandomNumber;
 public class MoveCondition implements Condition {
     private final static int DEFAULT_BOUND = 9;
     private final static int MOVABLE = 4;
-    private final static int STOP = 3;
 
     @Override
     public boolean isMovable() {
