1717a2146e7334325eefe18031fa9e800f9f9a81
binghe819 <binghe819@naver.com>
2021-02-05 17:50:54 +0900

    test: 자동차 이름 입력 및 시도횟수 예외처리 테스트 추가

--- src/test/java/racingCar/utils/ValidateUtilsTest.java
+++ src/test/java/racingCar/utils/ValidateUtilsTest.java
@@ -1,16 +1,30 @@
 package racingCar.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class ValidateUtilsTest {
 
-    // 자동차 이름 -> 1자 이하 5자 이상, 공백
+    @DisplayName("경주하는 자동차는 2개 이상이어야 한다")
+    @Test
+    public void 자동차_갯수_2개_이상_테스트_negative() {
+        // given, when
+        String[] carNames = {"pobi"};
+
+        // then
+        assertThrows(RuntimeException.class, ()->{
+            ValidateUtils.validateCarNames(carNames);
+        });
+    }
 
     @Test
     public void 자동차_이름_5자_이상_테스트_negative() {
@@ -24,15 +38,6 @@ class ValidateUtilsTest {
     }
 
     @Test
-    public void 자동차_이름_5자_이하_테스트_positive() {
-        // given, when
-        String[] carNames = {"pobi", "hong", "gil", "mark"};
-
-        // then
-
-    }
-
-    @Test
     public void 자동차_이름_공백_테스트_negative() {
         // given, when
         String[] carNames = {"pobi","hong","gil","",null};
@@ -43,26 +48,14 @@ class ValidateUtilsTest {
         });
     }
 
-    @Test
-    public void 시도횟수_음수_negative() {
-        // given, when
-        String count = "-2";
-
-        // then
-        assertThrows(RuntimeException.class, ()->{
-            ValidateUtils.validateRacingRoundCount(count);
-        });
-    }
-
-    @Test
-    public void 시도횟수_숫자아님_negative() {
-        // give, when
-        String count = "d";
-
-        // then
-        assertThrows(RuntimeException.class, ()->{
-            ValidateUtils.validateRacingRoundCount(count);
-        });
+    @DisplayName("시도횟수는 지정된 조건에 맞는 수가 입력되었습니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-2", "d"})
+    @NullAndEmptySource
+    void invalidRoundInput(String invalidInput) {
+        assertThatThrownBy(()->{
+            ValidateUtils.validateRacingRoundCount(invalidInput);
+        }).isInstanceOf(RuntimeException.class);
     }
 
     @ParameterizedTest
