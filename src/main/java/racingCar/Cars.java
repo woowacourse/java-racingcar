78f3230f8f1e28e77322ecfd3a48dba3a5c49ba5
cjy17 <cjy1705@naver.com>
2021-02-05 16:59:17 +0900

    fix(Cars) : 우승자 리스트 반환 기능 생성

--- src/main/java/racingCar/Cars.java
+++ src/main/java/racingCar/Cars.java
@@ -2,11 +2,27 @@ package racingCar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     static List<Car> cars = new ArrayList<>();
 
     static void carAdd(Car car){
         cars.add(car);
+    }
+
+    public static List<String> findWinners(int max){
+        return cars.stream()
+                .filter(car -> car.isWinner(max))
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+    }
+
+    public static int getSize() {
+        return cars.size();
+    }
+
+    public static List<Car> getCars() {
+        return cars;
     }
 }
