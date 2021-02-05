3f78224c2d551e442c8850a884a5756e04b5d0e1
yoonsung.jung <ybook2012@gmail.com>
2021-02-06 04:50:32 +0900

    style : 구글 자바 컨벤션 적용

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
     @DisplayName("Car의 이름을 null 또는 빈문자로 주었을때")
     @Test
-    public void Car_이름_null_또는_빈문자() {
+    public void car_이름_null_또는_빈문자() {
         String carName = "";
         assertThatThrownBy(() -> {
             Car car = new Car(carName);
@@ -18,7 +18,7 @@ public class CarTest {
 
     @DisplayName("Car의 이름이 한글 혹은 영어로 이루어지지 않은 경우")
     @Test
-    public void Car_이름_한글_영어_이외() {
+    public void car_이름_한글_영어_이외() {
         String carName = "";
         assertThatThrownBy(() -> {
             Car car = new Car(carName);
@@ -27,7 +27,7 @@ public class CarTest {
 
     @DisplayName("Car의 이름의 길이가 5 초과인 경우")
     @Test
-    public void Car_이름_길이_5_초과() {
+    public void car_이름_길이_5_초과() {
         String carName = "pobbbbbbi";
         assertThatThrownBy(() -> {
             Car car = new Car(carName);
