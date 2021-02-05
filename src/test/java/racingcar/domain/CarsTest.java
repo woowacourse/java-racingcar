780467cb34bbb6ebc20ffaf985b08e71dd294d74
yoonsung.jung <ybook2012@gmail.com>
2021-02-06 04:48:13 +0900

    test : 포지션 값이 제일 큰 자동차를 찾는 테스트 코드 구현

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,18 @@ import java.util.List;
 public class CarsTest {
     @DisplayName("자동차 이름이 중복되었을 경우")
     @Test
-    void Cars_중복된_자동차_이름() {
+    void cars_중복된_자동차_이름() {
         List<String> carNames = Arrays.asList("pobi", "pobi", "pobi");
         assertThatThrownBy(() -> {
             Cars.setCars(carNames);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차들 중 최대 position")
+    @Test
+    void carsGetMaxPosition() {
+        List<String> carNames = Arrays.asList("pobi", "amaz");
+        Cars.setCars(carNames);
+        assertThat(Cars.getMaxPositionByCars()).isEqualTo(0);
     }
 }
