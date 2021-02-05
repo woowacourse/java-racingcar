f55d1e5b99d6ddeb9e4fa4b35d32d4d5deeb581c
mungto <daum7766@naver.com>
2021-02-05 16:09:53 +0900

    refactor(outputView): change from private to public

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,12 @@ package racingcar.view;
 
 public class OutputView {
 
-    private static void printInputCarNamesMessage() {
+    public static void printInputCarNamesMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
-    private static void printInputAttemptNumberMessage() {
+    public static void printInputAttemptNumberMessage() {
         System.out.println("시도할 횟수는 몇회인가요?");
     }
+
 }
