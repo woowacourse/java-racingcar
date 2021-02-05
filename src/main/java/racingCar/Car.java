e647c23c84dc1b84eb3d968d63ae0a9db7d05a81
cjy17 <cjy1705@naver.com>
2021-02-05 16:57:45 +0900

    fix(Car) : Car 이름 유효성 및 최대거리 반환 수정

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -5,11 +5,8 @@ public class Car {
     String name;
     int distance;
 
-    Car(String name){
-        if(!NameSpliter.inputChecker(name)){
-            //error
-        }
-        this.distance = 0;
+    public Car(String name){
+        validate(name);
         this.name = name;
     }
 
@@ -17,5 +14,26 @@ public class Car {
         if(number >= 4){
             distance++;
         }
+    }
+
+    public void validate(String name) {
+        if (name.length() <= MINIMUM_LENGTH || name.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException("길이가 1에서 5사이의 이름을 입력해주세요.");
+        }
+    }
+
+    public boolean isWinner(int max){
+        if (distance == max) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
