a52757a7af64ec158fc03bdac9567bda1dbe1cbc
mungto <daum7766@naver.com>
2021-02-05 16:51:17 +0900

    feat(CarController): create init and play function

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,41 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.AttemptNumber;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarController {
+
+    private Cars cars;
+    private AttemptNumber attemptNumber;
+
+    public CarController() {
+    }
+
+    private void init() {
+        List<Car> carList = new ArrayList<>();
+        OutputView.printInputCarNamesMessage();
+        String carNames = InputView.getCarNames();
+        String[] carNamesArray = carNames.split(",");
+        for (String carName : carNamesArray) {
+            carList.add(new Car(carName));
+        }
+        cars = new Cars(carList);
+        OutputView.printInputAttemptNumberMessage();
+        attemptNumber = new AttemptNumber(InputView.getAttemptNumber());
+    }
+
+    public void play() {
+        init();
+        OutputView.printNextLine();
+        OutputView.printExecutionResultMessage();
+        for (int i = 0; i < attemptNumber.getAttemptNumber(); i++) {
+            cars.move();
+        }
+        OutputView.printWinnerCarNamesMessage(cars.getWinners());
+    }
+}
