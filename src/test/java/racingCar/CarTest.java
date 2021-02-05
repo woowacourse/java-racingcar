1d85dc39b90f2b83c1209870cf5e2fa67aad9d2d
cjy17 <cjy1705@naver.com>
2021-02-05 17:00:43 +0900

    fix(carTest) : 패키지명 수정

--- src/test/java/racingCar/CarTest.java
+++ src/test/java/racingCar/CarTest.java
@@ -1,7 +1,5 @@
 package racingCar;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
