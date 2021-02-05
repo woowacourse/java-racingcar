2ea7006ae1dce9e493d76b2cb9cc04c78f8adf4d
sor <bosl95@naver.com>
2021-02-05 14:00:26 +0900

    feat : CarsTest 클래스 생성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    @ParameterizedTest
+    @ValueSource (strings = {"포비", "제이슨"})
+    public void 자동차_객체_생성_테스트(String name) {
+        Cars cars = new Cars("포비,제이슨");
+        assertThat(cars.getCarNames()).contains(name);
+    }
+}
