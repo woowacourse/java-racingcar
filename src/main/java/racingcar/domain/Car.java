08d290e966cc014967036fd4d581d580a5d03583
Gomding <backchida@gmail.com>
2021-02-05 17:32:21 +0900

    (refactor) : Car 인스턴스를 생성할 때 사용하는 생성자를 정적 팩토리 메소드로 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,9 +5,13 @@ import racingcar.util.CarValidator;
 public class Car {
     private final String name;
 
-    public Car(String name) {
+    private Car(String name) {
         CarValidator.validateCarNameLength(name);
         this.name = name;
+    }
+
+    public static Car of(String name) {
+        return new Car(name);
     }
 
     public String getName() {
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -11,7 +11,7 @@ public class CarTest {
 
     @Test
     public void 경주할_자동차_이름_입력_체크() {
-        Car car = new Car("bmw");
+        Car car = Car.of("bmw");
         assertThat(car.getName()).isEqualTo("bmw");
     }
 
@@ -19,14 +19,14 @@ public class CarTest {
     @ValueSource(strings = {"", "abcdef"})
     public void carName_1자리_이하이거나_5자리를_초과하면_RuntimeException이_발생(String name) {
         assertThatThrownBy(() -> {
-            Car car = new Car(name);
+            Car.of(name);
         }).isInstanceOf(RuntimeException.class);
     }
 
     @ParameterizedTest(name = "carName_1자리이상_5자리이하(carName = {arguments})")
     @ValueSource(strings = {"a", "abcde"})
     public void carName_1자리이상_5자리이하(String name) {
-        Car car = new Car(name);
+        Car car = Car.of(name);
         assertThat(car.getName()).isEqualTo(name);
     }
 }
