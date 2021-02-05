fd9d10ad6cf5c617030e57e0c11485f4d296aa52
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-05 22:32:03 +0900

    test: 1회 경주 테스트

--- src/test/java/racingcar/CarRepositoryTest.java
+++ src/test/java/racingcar/CarRepositoryTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,53 @@ import java.util.List;
 public class CarRepositoryTest {
 
     @AfterEach
-    void afterEach() {
+    void deleteAllCars() {
         CarRepository.deleteAll();
+    }
+
+    @DisplayName("1회 경주 - 모든 자동차들이 랜덤값에 따라 전진하거나 전진하지 않아야 한다.")
+    @Test
+    void allCarsGoForwardOrStop() {
+        int numberOfCars = 10;
+
+        List<Car> cars = createCars(numberOfCars);
+        CarRepository.saveAll(cars);
+
+        List<Integer> randomNumbers = getRandomNumbers(numberOfCars);
+
+        CarRepository.raceOneTime(randomNumbers);
+
+        for (int i = 0; i < numberOfCars; i++) {
+            assertGoForwardOrStop(cars.get(i), randomNumbers.get(i));
+        }
+    }
+
+    private List<Car> createCars(int numberOfCars) {
+        List<Car> createdCars = new ArrayList<>();
+        for (int i = 0; i < numberOfCars; i++) {
+            createdCars.add(new Car(String.valueOf(i)));
+        }
+        return createdCars;
+    }
+
+    private List<Integer> getRandomNumbers(int numberOfRandomNumbersToGenerate) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < numberOfRandomNumbersToGenerate; i++) {
+            randomNumbers.add(i);
+        }
+        return randomNumbers;
+    }
+
+    private void assertGoForwardOrStop(Car car, Integer randomNumber) {
+        int minNumberToGoForward = 4;
+        int positionAfterGoForward = 1;
+        int positionAfterStop = 0;
+
+        if (randomNumber >= minNumberToGoForward) {
+            assertThat(car.getPosition()).isEqualTo(positionAfterGoForward);
+            return;
+        }
+        assertThat(car.getPosition()).isEqualTo(positionAfterStop);
     }
 
     @DisplayName("우승자 결과 테스트 한 명")
@@ -28,8 +74,8 @@ public class CarRepositoryTest {
 
         List<Car> winners = CarRepository.getWinners();
 
-        Assertions.assertThat(winners.size()).isEqualTo(1);
-        Assertions.assertThat(winners.get(0)).isEqualTo(winner);
+        assertThat(winners.size()).isEqualTo(1);
+        assertThat(winners.get(0)).isEqualTo(winner);
     }
 
     @DisplayName("우승자 결과 테스트 여러명")
@@ -57,7 +103,7 @@ public class CarRepositoryTest {
 
         List<Car> actualWinners = CarRepository.getWinners();
 
-        Assertions.assertThat(actualWinners.size()).isEqualTo(3);
-        Assertions.assertThat(actualWinners.containsAll(expectedWinners)).isTrue();
+        assertThat(actualWinners.size()).isEqualTo(3);
+        assertThat(actualWinners.containsAll(expectedWinners)).isTrue();
     }
 }
---
+++ src/test/java/racingcar/RandomGeneratorUtilsForTest.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+
+public class RandomGeneratorUtilsForTest {
+    private static int currentNumber = 0;
+
+    private RandomGeneratorUtilsForTest() {
+    }
+
+    public static int generateRandomNumber(final int startInclusive, final int endInclusive) {
+    }
+}
