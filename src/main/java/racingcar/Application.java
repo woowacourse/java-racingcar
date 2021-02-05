26fbd7665944117c65b6aa3dc444fe3ba430476c
YeongBeom Heo <hybeom@gmail.com>
2021-02-05 21:56:16 +0900

    refactor : Application 여백 부

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         RacingCarController racingCarController = new RacingCarController(scanner);
+
         racingCarController.generateCars();
         racingCarController.receiveTurns();
         racingCarController.playGame();
