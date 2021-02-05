ed472a21e1e3b7e334db7f092df89836ddea1cd8
TaewanKimmmm <ktww88@gmail.com>
2021-02-05 17:56:06 +0900

    feat: 자동차의 현재 위치를 리턴하는 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,16 +2,20 @@ package racingcar;
 
 public class Car {
     private final CarName carName;
+    private static final int CONDITION = 4;
     private int position = 0;
 
     public Car(CarName carName) {
         this.carName = carName;
     }
 
-    public void moveForward() {
-        position += 1;
+    public void moveForwardByRandomNumber(int randomNumber) {
+        if (randomNumber >= CONDITION) {
+            position += 1;
+        }
     }
 
-
-
+    public int getPosition() {
+        return position;
+    }
 }
