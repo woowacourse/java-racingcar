5c1cb159fd5ae7f5099490e1347bc544b5f538f5
xrabcde <xrabcde@gmail.com>
2021-02-05 17:08:16 +0900

    feat(Cars): 최종 우승자를 계산하는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,21 +1,49 @@
 package racingcar.domain;
 
+import racingcar.utils.RandomNumberGenerator;
 import racingcar.view.RacingCarView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    private static final List<Car> carList = new ArrayList<>();
+    private List<Car> carList = new ArrayList<>();
 
     public Cars(List<String> carNameList) {
         carNameList.stream()
-        .map(carName -> carList.add(new Car(carName)));
+                .forEach(carName -> carList.add(new Car(carName)));
     }
 
     public void showCarsPosition() {
         carList.stream()
-            .forEach(car -> RacingCarView.displayPosition(car));
+                .forEach(car -> RacingCarView.displayPosition(car));
 
+    }
+
+    public void processOneTurn() {
+        carList.stream()
+                .forEach(car -> car.oneTurn(RandomNumberGenerator.turnNumber()));
+    }
+
+    public List<Car> getList() {
+        return this.carList;
+    }
+
+    public String findWinners() {
+        int maxPosition = findMaxPosition(this.carList);
+        List<String> winnerList = new ArrayList<>();
+        carList.stream()
+            .filter(carElement -> (carElement.getPosition() == maxPosition))
+            .map(carElement -> carElement.getName())
+            .forEach(carWinner->winnerList.add(carWinner));
+        return String.join(", ", winnerList);
+    }
+
+    private static int findMaxPosition(List<Car> carList) {
+        int maxPosition = 0;
+        for (Car car : carList) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
     }
 }
