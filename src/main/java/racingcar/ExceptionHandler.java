82000c3bbe9c8d8042d1cc35adf0b729a6d3df6d
Junroot <junroot0909@gmail.com>
2021-02-05 17:25:25 +0900

    feat(racingcar): 시도할 횟수 입력 테스트 케이스 추가

--- src/main/java/racingcar/ExceptionHandler.java
+++ src/main/java/racingcar/ExceptionHandler.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class ExceptionHandler {
     private static final String NAME_SPLIT_DELIMITER = ",";
+    private static final Integer MIN_TRIAL = 1;
+    private static final Integer MAX_TRIAL = Integer.MAX_VALUE;
 
     public List<Car> setCars(String input) {
         String[] carNames = input.split(NAME_SPLIT_DELIMITER, -1);
@@ -15,7 +17,19 @@ public class ExceptionHandler {
             .collect(Collectors.toList());
     }
 
-    public int setTrial(String input) {
-        return Integer.parseInt(input);
+    public Integer setTrial(String input) {
+        try {
+            Integer trial = Integer.parseInt(input);
+            return isValidTrialBound(trial);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력할 수 있습니다.");
+        }
+    }
+
+    private int isValidTrialBound(Integer trial) {
+        if (trial < MIN_TRIAL || trial > MAX_TRIAL) {
+            throw new IllegalArgumentException("1 이상의 정수만 입력할 수 있습니다.");
+        }
+        return trial;
     }
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,16 +2,28 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarTest {
+    ExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        exceptionHandler = new ExceptionHandler();
+    }
+
+
     @Test
     @DisplayName("입력한 이름대로 올바르게 Car 리스트가 만들어지는지 확인")
     void setCarsTest() {
-        ExceptionHandler exceptionHandler = new ExceptionHandler();
         List<Car> expected = new ArrayList<>();
         expected.add(new Car("루트"));
         expected.add(new Car("소롱"));
@@ -22,12 +34,25 @@ public class RacingCarTest {
     }
 
     @Test
+    @DisplayName("입력한 시도 회수가 정상적인 값인지 확인")
     void setTrialTest() {
-        ExceptionHandler exceptionHandler = new ExceptionHandler();
         int expected = 3;
-
         int actual = exceptionHandler.setTrial("3");
-
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "2,2", "50,50","2147483647,2147483647"}, delimiter = ',')
+    void setTrial_정상입력(String input, Integer expected) {
+        Integer actual = exceptionHandler.setTrial(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2147483648", "", "2-1", "abc", "894-"})
+    void setTrial_비정상입력(String input) {
+        assertThatThrownBy(() -> {
+            exceptionHandler.setTrial(input);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
