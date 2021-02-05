17d533128982f68bd65369306e73ea5b3e23b732
yoonsung.jung <ybook2012@gmail.com>
2021-02-06 06:15:13 +0900

    refactor(car-test) : 사용하지 않는 import 제거

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
