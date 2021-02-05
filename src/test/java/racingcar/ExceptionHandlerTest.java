9d6d300a2871f007dfa8d02c21627cecf100efec
Junroot <junroot0909@gmail.com>
2021-02-05 17:36:26 +0900

    style: 테스트 클래스 분리를 위한 이름 변경

--- src/test/java/racingcar/ExceptionHandlerTest.java
+++ src/test/java/racingcar/ExceptionHandlerTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class RacingCarTest {
+public class ExceptionHandlerTest {
     ExceptionHandler exceptionHandler;
 
     @BeforeEach
