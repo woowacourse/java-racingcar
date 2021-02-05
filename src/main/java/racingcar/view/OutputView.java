302825f7be6260c79b7d4dc85a2e21414444de59
mungto <daum7766@naver.com>
2021-02-05 16:08:10 +0900

    feat(outputView): create printInputAttemptNumberMessage function

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,4 +6,7 @@ public class OutputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
+    private static void printInputAttemptNumberMessage() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
 }
