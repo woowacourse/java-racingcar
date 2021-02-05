c866be3d8319eff2a67dd767b13cc68f6c494b7d
xrabcde <xrabcde@gmail.com>
2021-02-05 17:16:29 +0900

    feat(RacingCarViewTest): 최종 우승자 출력 테스트코드 작성

--- src/test/java/racingcar/view/RacingCarViewTest.java
+++ src/test/java/racingcar/view/RacingCarViewTest.java
@@ -10,12 +10,16 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingCarViewTest {
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
     private static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
     private static final String TURN_INPUT_MESSAGE = "시도할 회수는 몇회인가요?\n";
     private static final String SAMPLE_CAR_NAME = "sample";
+    private static final String SAMPLE_CAR_MESSAGE = "sample : -\n";
+    private static final String SAMPLE_WINNERS = "pobi, bada, whyb";
+    private static final String WIN_MESSAGE = "가 최종 우승했습니다.\n";
     private static final int CRITERIA = 4;
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
@@ -38,6 +42,13 @@ public class RacingCarViewTest {
         Car sampleCar = new Car(SAMPLE_CAR_NAME);
         sampleCar.oneTurn(CRITERIA);
         RacingCarView.displayPosition(sampleCar);
-        assertEquals("sample : -\n", outputStreamCaptor.toString());
+        assertEquals(SAMPLE_CAR_MESSAGE, outputStreamCaptor.toString());
+    }
+
+    @Test
+    void showWinner() {
+        String sampleWinners = SAMPLE_WINNERS;
+        RacingCarView.showWinner(sampleWinners);
+        assertEquals(SAMPLE_WINNERS+WIN_MESSAGE, outputStreamCaptor.toString());
     }
 }
