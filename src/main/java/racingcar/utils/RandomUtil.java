23aeba6f58a127d6ca93edce298ec3ba93d9808f
TaewanKimmmm <ktww88@gmail.com>
2021-02-05 17:39:03 +0900

    feat: 랜덤 함수 만드는 유틸리티 클래스 추가

---
+++ src/main/java/racingcar/utils/RandomUtil.java
@@ -0,0 +1,26 @@
+package racingcar.utils;
+
+import java.util.Random;
+
+public class RandomUtil {
+    private static final Random RANDOM = new Random();
+
+    private RandomUtil() {
+    }
+
+    public static int nextInt(final int startInclusive, final int endInclusive) {
+        if (startInclusive > endInclusive) {
+            throw new IllegalArgumentException();
+        }
+
+        if (startInclusive < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (startInclusive == endInclusive) {
+            return startInclusive;
+        }
+
+        return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
+    }
+}
