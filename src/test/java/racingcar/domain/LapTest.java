b91248973a820750249bbda0ddd892c70dd57e2d
y_woo :) <dusdn1702@naver.com>
2021-02-05 16:07:30 +0900

    feat: 횟수 입력받는 기능 구현
    예외처리 완료

--- src/test/java/racingcar/domain/LapTest.java
+++ src/test/java/racingcar/domain/LapTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LapTest {
     @Test
@@ -12,5 +12,17 @@ class LapTest {
     public void lap_create() {
         Lap lap = new Lap("10");
         assertThat(lap.getLap()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 값을 입력한 경우")
+    void lap_숫자가_아닌_경우_예외처리() {
+        assertThatThrownBy(()->new Lap("hi")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("1보다 작은 값을 입력한 경우")
+    void lap_1보다_작은_경우_예외처리() {
+        assertThatThrownBy(()->new Lap("0")).isInstanceOf(IllegalArgumentException.class);
     }
 }
