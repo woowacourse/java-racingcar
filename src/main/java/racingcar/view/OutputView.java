083ec2074328fed8fb4374ece3b0957bd40575f7
Kimun Kim <kkm97351@gmail.com>
2021-02-06 15:08:26 +0900

    feat: 최종 우승자 출력 메소드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,10 @@ package racingcar.view;
 import racingcar.domain.CarDTO;
 import racingcar.domain.CarsDTO;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * OutputView.java
  * 입력을 제외한 출력을 담당하는 View 클래스
@@ -12,10 +16,16 @@ import racingcar.domain.CarsDTO;
 public class OutputView {
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
     private static final String POSITION_FORMAT = "%s : %s";
+    private static final String WINNERS_LIST = "최종 우승자 : ";
+    private static final String EXECUTION_RESULT = "실행 결과";
     private static final char POSITION_CHARACTER = '-';
 
     public static void printErrorMessage(Exception e) {
         System.out.println(String.format(ERROR_MESSAGE_FORMAT, e.getMessage()));
+    }
+
+    public static void printResultMessage() {
+        System.out.println(EXECUTION_RESULT);
     }
 
     public static void printCarsPosition(CarsDTO carsDTO) {
@@ -30,5 +40,16 @@ public class OutputView {
         }
         System.out.print(String.format(POSITION_FORMAT, carDTO.getName(), positionStringBuffer));
         System.out.println();   // 개행
+    }
+
+    public static void printWinners(List<String> winners) {
+        Collections.sort(winners);
+        System.out.print(WINNERS_LIST);
+
+        Iterator<String> iterator = winners.iterator();
+        System.out.print(iterator.next());
+        while(iterator.hasNext()) {
+            System.out.print(", " + iterator.next());
+        }
     }
 }
