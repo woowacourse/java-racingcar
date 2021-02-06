40cba79e5e8ec9d5055185f6fe0b364d731a7117
cjy17 <cjy1705@naver.com>
2021-02-06 09:43:13 +0900

    fix(RandomUtils) : 매개변수 수정

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class RandomUtils {
     final static Random RANDOM = new Random();
 
-    public static int nextInt(int start, int end) {
-        if(start < 0 || start > end){
+    public static int nextInt(final int START, final int END) {
+        if(START < 0 || START > END){
             throw new RuntimeException("입력 값을 확인해주세요.");
         }
-        if(start == end){
-            return start;
+        if(START == END){
+            return START;
         }
-        return RANDOM.nextInt(end - start + 1) + start ;
+        return RANDOM.nextInt(END - START + 1) + START ;
     }
 }
