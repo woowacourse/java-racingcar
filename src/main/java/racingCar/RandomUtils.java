c056599048e788692342514a68518b28eed223d3
sjpark-dev <sjpark.dev@gmail.com>
2021-02-06 22:07:49 +0900

    fix(RandomUtils) : 접근제어자 수정

--- src/main/java/racingCar/RandomUtils.java
+++ src/main/java/racingCar/RandomUtils.java
@@ -3,15 +3,15 @@ package racingCar;
 import java.util.Random;
 
 public class RandomUtils {
-    final static Random RANDOM = new Random();
+    private final static Random RANDOM = new Random();
 
-    static int nextInt(int start, int end) {
-        if(start < 0 || start > end){
+    public static int nextInt(int start, int end) {
+        if (start < 0 || start > end) {
             throw new RuntimeException("입력 값을 확인해주세요.");
         }
-        if(start == end){
+        if (start == end) {
             return start;
         }
-        return RANDOM.nextInt(end - start + 1) + start ;
+        return RANDOM.nextInt(end - start + 1) + start;
     }
 }
