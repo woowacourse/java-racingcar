927b6e5568b021533e6e758113153a2093fadc41
daeun <leede0418@likelion.org>
2021-02-06 22:29:39 +0900

    refactor(car): 자동차가 조건을 만족하면 전진하는 기능 구현

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -4,16 +4,31 @@ import racingcar.constant.Digit;
 import racingcar.domain.Car;
 import racingcar.utils.RandomUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarService {
-    public void decideMovableCar(List<Car> cars) {
-        for (Car car : cars) {
+    public void decideMovableCar(List<Car> cars, List<Integer> randoms) {
+        if (randoms == null) {
+            randoms = getRandoms(cars);
+        }
+
+        for (int i = 0; i < cars.size(); i++) {
+            moveIfPossible(cars.get(i), randoms.get(i));
+        }
+    }
+
+    private List<Integer> getRandoms(List<Car> cars) {
+        List<Integer> randoms = new ArrayList();
+
+        for (int i = 0; i < cars.size(); i++) {
             int random = RandomUtils.nextInt(Digit.MINIMUM_RANDOM_VALUE.getDigit(),
                     Digit.MAXIMUM_RANDOM_VALUE.getDigit());
 
-            moveIfPossible(car, random);
+            randoms.add(random);
         }
+
+        return randoms;
     }
 
     public void moveIfPossible(Car car, int random) {
--- src/test/java/racingcar/service/CarServiceTest.java
+++ src/test/java/racingcar/service/CarServiceTest.java
@@ -7,6 +7,11 @@ import racingcar.constant.Digit;
 import racingcar.domain.Car;
 import racingcar.utils.RandomUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarServiceTest {
@@ -15,6 +20,34 @@ class CarServiceTest {
     @BeforeEach
     public void setUp() {
         carService = new CarService();
+    }
+
+    @DisplayName("조건을 만족하는 자동차는 전진 성공")
+    @Test
+    void decideMovableCar_자동차가_전진할지_멈출지_판단() {
+        int numberOfCars = 4;
+        int numberOfMovableCar = 2;
+        int movedPosition = 1;
+
+        List<Integer> randoms = Arrays.stream(new int[numberOfCars])
+                .boxed()
+                .collect(Collectors.toList());
+        List<Car> cars = new ArrayList();
+
+        for (int i = 0; i < numberOfMovableCar; i++) {
+            randoms.set(i, Digit.MOVEMENT_CRITERION.getDigit());
+        }
+        for (int i = 0; i < numberOfCars; i++) {
+            cars.add(new Car("pobi" + i));
+        }
+
+        carService.decideMovableCar(cars, randoms);
+
+        int movedCount = (int) cars.stream()
+                .filter(car -> car.getPosition() == movedPosition)
+                .count();
+
+        assertThat(movedCount).isEqualTo(numberOfMovableCar);
     }
 
     @DisplayName("자동차가 조건을 만족하면 전진 성공")
