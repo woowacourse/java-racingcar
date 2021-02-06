3c7fa95df990328b0b9a844bf7f1f87962740f34
jujubebat <bbwwpark@naver.com>
2021-02-06 23:46:35 +0900

    refactor: 테스트 메서드명 수정 - 가독성을 위해 공백 라인 추가

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,10 +8,12 @@ import static org.assertj.core.api.Assertions.*;
 public class CarsTest {
 
     @Test
-    public void Cars_getWinner_동작_테스트() {
+    @DisplayName("우승자 선정이 잘 되는지 테스트")
+    public void check_get_winners() {
         String carNames[] = {"test1", "test2", "test3"};
         int distance[] = {8, 1, 3};
         Cars cars = new Cars(carNames, distance);
+
         assertThat(cars.getWinners(8)).isEqualTo("test1");
     }
 
