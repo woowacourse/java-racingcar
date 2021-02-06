7ae4d97f076aecdb1fe67a9ca085ecfa211b29f5
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-06 22:25:05 +0900

    test(CarsTest): 생성자 예외 테스트 추가

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -5,6 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class CarsTest {
+    @Test
+    public void Cars_객체_생성자_빈_배열_매개변수_예외_테스트() {
+        String[] carNames = {};
+        assertThatThrownBy(() ->  new Cars(carNames))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[Error] 최소 하나의 자동차 이름을 입력하세요.");
+    }
+
+    @Test
+    public void Cars_객체_생성자_중복_예외_테스트() {
+        String[] carNames = {"test1", "test2", "test3", "test4", "test1"};
+        assertThatThrownBy(() ->  new Cars(carNames))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("[Error] 자동차 이름이 중복됩니다.");
+    }
 
     @Test
     public void Cars_getWinner_동작_테스트() {
@@ -13,6 +28,4 @@ public class CarsTest {
         Cars cars = new Cars(carNames, distance);
         assertThat(cars.getWinner(8)).isEqualTo("test1");
     }
-
-
 }
