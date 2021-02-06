79b993eeac30b942b26fe23740215c0488fe8fe6
sjpark-dev <sjpark.dev@gmail.com>
2021-02-06 22:30:31 +0900

    fix(SetTest) : 클래스 접근제어자 수정, 테스트 의도 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,8 +14,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SetTest {
-    private Set<Integer> numbers;
+class SetTest {
+    Set<Integer> numbers;
 
     @BeforeEach
     void setUp() {
@@ -28,20 +29,23 @@ public class SetTest {
     // Test Case 구현
 
     @Test
+    @DisplayName("size 학습 테스트")
     void setSize() {
         assertThat(3).isEqualTo(numbers.size());
     }
 
     @ParameterizedTest
-    @ValueSource(ints={1,2,3})
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains 학습 테스트")
     void setContains(int input) {
         assertTrue(numbers.contains(input));
     }
 
     @ParameterizedTest
-    @CsvSource(value={"1:true","2:true","3:true","4:false","5:false"},delimiter=':')
+    @DisplayName("contains 학습 테스트")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void setContains2(int input, boolean expected) {
         boolean actualValue = numbers.contains(input);
-        assertEquals(expected,actualValue);
+        assertEquals(expected, actualValue);
     }
 }
