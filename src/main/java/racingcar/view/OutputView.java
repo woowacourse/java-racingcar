d94217a38b2313e993baf4c1805258a6debe91ba
thisisyoungbin <thisisyoungbin@gmail.com>
2021-02-06 14:44:13 +0900

    feat: 우승자를 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,12 +3,15 @@ package racingcar.view;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
     private static final String RUN_RESULT_MESSAGE = "실행 결과";
     private static final String DELIMITER_COLON = " : ";
+    private static final String DELIMITER_COMMA = ", ";
     private static final String POSITION_HYPHEN = "-";
+    private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
 
     private OutputView() {
     }
@@ -35,5 +38,14 @@ public class OutputView {
         }
 
         return stringBuilder.toString();
+    }
+
+    public static void printWinners(List<Car> winners) {
+        List<String> carNames = new ArrayList<>();
+        for (Car winner : winners) {
+            carNames.add(winner.getName());
+        }
+        String winnerNames = String.join(DELIMITER_COMMA, carNames);
+        System.out.println(winnerNames + WINNER_MESSAGE);
     }
 }
