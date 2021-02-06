37dff84f75d37c4b8cd41aa9645260d215546474
Hyeon9mak <jinha3507@gmail.com>
2021-02-06 12:15:19 +0900

    test: fix String.split method success test
    fix test variable names
    add final keyword in test variables
    fix assertj method

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,26 +1,28 @@
 package study;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
-
 public class StringTest {
+
     @Test
-    void split1() {
-        String value = "1,2";
-        String separator = ",";
+    @DisplayName("String.split 성공 테스트")
+    void testSuccessSplitString() {
+        final String testString = "1,2";
+        final String testSaparator = ",";
+        final int firstIndex = 0;
+        final int secondIndex = 1;
+        final String firstExpectResult = "1";
+        final String secondExpectResult = "2";
 
-        int testIndexOne = 0;
-        int testIndexTwo = 1;
-
-        String expectResultOne = "1";
-        String expectResultTwo = "2";
-
-        assertThat(value.split(separator)[testIndexOne]).contains(expectResultOne);
-        assertThat(value.split(separator)[testIndexTwo]).contains(expectResultTwo);
-
-        assertThat(value.split(separator)).containsExactly(expectResultOne, expectResultTwo);
+        assertThat(testString.split(testSaparator)[firstIndex]).isEqualTo(firstExpectResult);
+        assertThat(testString.split(testSaparator)[secondIndex]).isEqualTo(secondExpectResult);
+        assertThat(testString.split(testSaparator))
+            .containsExactly(firstExpectResult, secondExpectResult);
     }
 
     @Test
@@ -40,7 +42,7 @@ public class StringTest {
     @Test
     void substring() {
         String value = "(1,2)";
-        String newValue = value.substring(1,4);
+        String newValue = value.substring(1, 4);
 
         assertThat(newValue).isEqualTo("1,2");
     }
@@ -62,13 +64,13 @@ public class StringTest {
         final String expectMessage = "String index out of range: ";
 
         assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
-                .isThrownBy(()->{
-                    value.charAt(testIndex);
-                }).withMessageMatching(expectMessage + "\\d+");
+            .isThrownBy(() -> {
+                value.charAt(testIndex);
+            }).withMessageMatching(expectMessage + "\\d+");
 
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             value.charAt(testIndex);
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining(expectMessage + testIndex);
+            .hasMessageContaining(expectMessage + testIndex);
     }
 }
