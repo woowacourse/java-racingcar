6173b33991046bdc7589b33729380785a63969c0
yoonsung.jung <ybook2012@gmail.com>
2021-02-06 19:01:10 +0900

    refactor(racing-car) : tryCount와 gameRule을 인스턴스 변수로 분리 및 메소드명 수정

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -9,37 +9,40 @@ import racingcar.view.RacingCarView;
 import java.util.List;
 
 public class RacingCar {
+    private static TryCount tryCount;
+    private static GameRule gameRule;
+
     public static void run() {
         racingCarSetting();
+        racing(tryCount, gameRule);
         pickWinners();
         RacingCarView.printFinalResult(Winners.getWinnersNames());
     }
 
     private static void racingCarSetting() {
         Cars.setCarsByName(getCarNames());
-        TryCount tryCount = new TryCount(InputView.getTryCountInput());
-        GameRule gameRule = new GameRule();
-        progressTryCount(tryCount, gameRule);
+        tryCount = new TryCount(InputView.getTryCountInput());
+        gameRule = new GameRule();
     }
 
     private static List<String> getCarNames() {
         return RacingCarUtils.splitInputString(InputView.getCarNameInput());
     }
 
-    private static void progressTryCount(TryCount tryCount, GameRule gameRule) {
+    private static void racing(final TryCount tryCount, final GameRule gameRule) {
         for (int i = 0; i < tryCount.getCount(); i++) {
-            progressRacing(gameRule);
+            changeCarsStatus(gameRule);
             RacingCarView.printProgressResult(Cars.getCars());
         }
     }
 
-    private static void progressRacing(GameRule gameRule) {
+    private static void changeCarsStatus(final GameRule gameRule) {
         for (Car car : Cars.getCars()) {
             moveMovableCar(car, gameRule);
         }
     }
 
-    private static void moveMovableCar(Car car, GameRule gameRule) {
+    private static void moveMovableCar(final Car car, final GameRule gameRule) {
         CarController carController = new CarController(car);
         RandomNumber randomNumber = new RandomNumber();
         if (gameRule.isMoveNumber(randomNumber.getNumber())) {
