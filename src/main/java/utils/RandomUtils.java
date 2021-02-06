855b90eadfcf5b7b9c51580bd811cc04a3408c0c
cjy17 <cjy1705@naver.com>
2021-02-06 09:40:01 +0900

    fix(RandomUtils) : 패키지 이동에 따른 접근제어자 수정

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomUtils {
     final static Random RANDOM = new Random();
 
-    static int nextInt(int start, int end) {
+    public static int nextInt(int start, int end) {
         if(start < 0 || start > end){
             throw new RuntimeException("입력 값을 확인해주세요.");
         }
