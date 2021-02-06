bd8ceed628ac857b0e6d330ba212d3c2741065d4
sjpark-dev <sjpark.dev@gmail.com>
2021-02-06 22:14:26 +0900

    fix(StringCalculatorTest) : 접근제어자 수정, 메소드 의도 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,13 +1,15 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class StringCalculatorTest {
+class StringCalculatorTest {
     @Test
-    public void splitAndSum_null_또는_빈문자() {
+    @DisplayName("null 또는 빈문자열이면 0이 되는지 테스트")
+    void splitAndSum_null_또는_빈문자() {
         int result = StringCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
@@ -16,37 +18,43 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    @DisplayName("숫자하나인 문자열이 숫자가 되는지 테스트")
+    void splitAndSum_숫자하나() {
         int result = StringCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    @DisplayName("콤마로 구분된 숫자가 더해지는지 테스트")
+    void splitAndSum_쉼표구분자() {
         int result = StringCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    @DisplayName("콤마와 콜론으로 구분된 숫자가 더해지는지 테스트")
+    void splitAndSum_쉼표_또는_콜론_구분자() {
         int result = StringCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
+    @DisplayName("커스텀 구분자가 작동하는지 테스트")
+    void splitAndSum_custom_구분자() {
         int result = StringCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_negative() throws Exception {
+    @DisplayName("음수가 들어가면 에러가 나는지 테스트")
+    void splitAndSum_negative() {
         assertThatThrownBy(() -> StringCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    public void splitAndSum_alphabet() throws Exception {
+    @DisplayName("알파벳이 들어가면 에러가 나는지 테스트")
+    void splitAndSum_alphabet() {
         assertThatThrownBy(() -> StringCalculator.splitAndSum("1,a,3"))
                 .isInstanceOf(RuntimeException.class);
     }
