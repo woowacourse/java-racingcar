c7e77ff7f42baf6f3616dd3db27ecedd1bb88ee8
Jinho <jh8579@gmail.com>
2021-02-06 15:52:22 +0900

    feat(racingcar) : remove name method and round method in InputUtils

--- src/main/java/racingcar/utils/InputUtils.java
+++ src/main/java/racingcar/utils/InputUtils.java
@@ -1,24 +1,22 @@
 package racingcar.utils;
 
-import racingcar.domain.TryCount;
 import java.util.Arrays;
 import java.util.Scanner;
+import racingcar.validator.InputValidator;
 
 public class InputUtils {
 
     private static final String nameSeparator = ",";
 
     public static String[] getNames(Scanner scanner) {
-        String names = scanner.next();
-        String[] nameStrings = splitNames(names);
-        InputValidator.checkValidNames(nameStrings);
-        InputValidator.isDuplicatedNames(nameStrings);
-        return nameStrings;
+        String names = scanner.nextLine();
+        return splitNames(names);
     }
 
-    public static int getTryCount(Scanner scanner) {
-        String tryCount = scanner.next();
-        return InputValidator.isValidTryCount(tryCount);
+    public static int getInteger(Scanner scanner){
+        String next = scanner.next();
+        InputValidator.isOnlyNumber(next);
+        return Integer.parseInt(next);
     }
 
     public static String[] splitNames(String line) {
