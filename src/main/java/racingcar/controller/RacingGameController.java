4720361c26032248bc1f72a23b4b9ef90017ea14
jujubebat <bbwwpark@naver.com>
2021-02-07 23:07:35 +0900

    refactor: 컨트롤러에 있던 최대거리 로직을 cars 내부로 이동시킴

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -7,19 +7,17 @@ import racingcar.view.OutputView;
 
 public class RacingGameController {
 
-    private int maxDistance;
-
     public void play(final Cars cars, final int attemptNumber) {
         OutputView.printPlayResultMessage();
         for (int i = 0; i < attemptNumber; i++) {
             totalCarMove(cars);
         }
-        OutputView.printWinners(cars.getWinners(maxDistance));
+        OutputView.printWinners(cars.getWinners());
     }
 
     public void totalCarMove(final Cars cars) {
         for (Car car : cars.getCars()) {
-            maxDistance = Math.max(car.move(RandomUtils.getRandomNumber()), maxDistance);
+            car.move(RandomUtils.getRandomNumber());
             OutputView.printCarInformation(car);
         }
         OutputView.printNewLine();
