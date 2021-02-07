fb886946bf8eb9fe4e85450dfdaf1249e5d91ac1
Gomding <backchida@gmail.com>
2021-02-07 18:02:57 +0900

    (feat) : 시도 횟수 입력받는 기능 추가

--- src/main/java/racingcar/controller/GameManagerController.java
+++ src/main/java/racingcar/controller/GameManagerController.java
@@ -2,17 +2,19 @@ package racingcar.controller;
 
 import racingcar.view.InputView;
 
-import java.util.Arrays;
-
 public class GameManagerController {
     public void run() {
         String[] carNames = getInputCarNames();
+        int roundCount = getInputRoundCount();
     }
 
     private String[] getInputCarNames() {
         String input = InputView.inputCarNames();
         String[] carNames = input.split(",");
-
         return carNames;
+    }
+
+    private int getInputRoundCount() {
+        return InputView.inputRoundCount();
     }
 }
