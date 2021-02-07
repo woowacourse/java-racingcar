4fa624414a4b0ef5609645e06cf74d7de56955d0
TaewanKimmmm <ktww88@gmail.com>
2021-02-07 14:52:04 +0900

    test: 자동차 전진 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,12 +2,14 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CarTest {
     @Test
     void moveForward() {
         Car car = new Car(new CarName("ser"));
         int previous = car.getPosition();
         car.moveForwardByRandomNumber(5);
-        assertEqual(car.getPosition(), previous + 1);
+        assertEquals(car.getPosition(), previous + 1);
     }
 }
