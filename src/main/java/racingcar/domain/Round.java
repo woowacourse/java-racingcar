c7fd8baa97d6488fb53b85a42d55fb1e547804f8
Gomding <backchida@gmail.com>
2021-02-07 20:09:53 +0900

    (feat) : 시도할 횟수는 1이상의 숫자여야 한다.

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -1,13 +1,21 @@
 package racingcar.domain;
 
 public class Round {
+    private static final int MIN_COUNT_SIZE = 1;
     private final int count;
 
     private Round(int count) {
+        validateRoundCount(count);
         this.count = count;
     }
 
     public static Round of(int count) {
         return new Round(count);
+    }
+
+    private void validateRoundCount(int count) {
+        if (count < MIN_COUNT_SIZE) {
+            throw new IllegalArgumentException("시도할 횟수는 1이상 이어야 합니다.");
+        }
     }
 }
--- src/test/java/racingcar/domain/RoundTest.java
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -1,4 +1,27 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class RoundTest {
+    // TODO - 질문
+//    @Test
+//    public void 시도할_횟수는_숫자여야한다() {
+//        assertThatThrownBy(() -> {
+//            Round.of("a");
+//        }).isInstanceOf(Exception.class);
+//    }
+
+    @ParameterizedTest(name = "시도할_횟수는_1이상의_숫자여야_한다")
+    @ValueSource(ints = {0, -1, -5})
+    public void roundCountRangeTest(int count) {
+        assertThatThrownBy(() -> {
+            Round.of(count);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
