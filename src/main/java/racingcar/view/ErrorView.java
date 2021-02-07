ea59d8e6b562aa495f65ae571d6db4ec10241376
sor <bosl95@naver.com>
2021-02-07 18:27:12 +0900

    refactor : 에러 출력 수정

--- src/main/java/racingcar/view/ErrorView.java
+++ src/main/java/racingcar/view/ErrorView.java
@@ -2,19 +2,19 @@ package racingcar.view;
 
 public class ErrorView {
     public static void invalidText() {
-        System.out.println("문자 외의 이름은 입력될 수 없습니다.");
+        System.out.println("문자 외의 이름은 입력될 수 없습니다.\n");
     }
 
     public static void invalidNameLength(int maxLength) {
-        System.out.println("길이는 1 이상 " + maxLength + " 이하이어야합니다.");
+        System.out.println("길이는 1 이상 " + maxLength + " 이하이어야합니다.\n");
     }
 
     public static void duplicateName() {
-        System.out.println("이름이 중복됩니다.");
+        System.out.println("이름이 중복됩니다.\n");
     }
 
     public static void notEnoughCar(int number) {
-        System.out.println("자동차가 " + number + "대 이상이어야 합니다.");
+        System.out.println("자동차가 " + number + "대 이상이어야 합니다.\n");
     }
 
     public static void invalidTime() {
