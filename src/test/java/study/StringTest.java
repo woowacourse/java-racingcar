0b7a3b243ef102f69b32f75219196dbce6c078db
Eugine Park <pkeugine@gmail.com>
2021-02-08 03:29:22 +0900

    chores(StringTest): remove unnecessary comments and imports

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,14 +2,12 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class StringTest {
+class StringTest {
 
     @Test
     void split() {
@@ -21,14 +19,9 @@ public class StringTest {
     @DisplayName("특정 인덱스 문자 테스트")
     void chatAt() {
         String values = "abcd";
-        assertEquals(values.charAt(0), 'a');
-        assertEquals(values.charAt(1), 'b');
-        assertEquals(values.charAt(2), 'c');
-
-//        assertThatThrownBy(() -> {
-//            values.charAt(4);
-//        }).isInstanceOf(IndexOutOfBoundsException.class)
-//                .hasMessageContaining("String index out of range: 4");
+        assertEquals('a', values.charAt(0));
+        assertEquals('b', values.charAt(1));
+        assertEquals('c', values.charAt(2));
 
         assertThatExceptionOfType(IndexOutOfBoundsException.class)
                 .isThrownBy(() -> {
