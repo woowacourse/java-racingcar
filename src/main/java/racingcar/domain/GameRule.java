a1f65b7d572174c292090ac1b63f431a3504568c
yoonsung.jung <ybook2012@gmail.com>
2021-02-07 22:52:12 +0900

    refactor(game-rule) : 생성자 호출을 못하게 수정

--- src/main/java/racingcar/domain/GameRule.java
+++ src/main/java/racingcar/domain/GameRule.java
@@ -3,6 +3,9 @@ package racingcar.domain;
 public class GameRule {
     private static final int MOVE_PIVOT = 4;
 
+    private GameRule() {
+    }
+
     public static boolean isMoveNumber(final int number) {
         return MOVE_PIVOT <= number;
     }
