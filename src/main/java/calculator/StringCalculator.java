ccc1fde90e8ac109ef6da328b2abc1d53ee6d4d9
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-07 20:31:07 +0900

    refactor: Matcher 변수명 구체화, Pattern을 static 멤버변수로 바꿔 한번 생성하고 재사용

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,11 +6,12 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final int EMPTY_RESULT = 0;
-    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final int FIRST_MATCH = 1;
     private static final int SECOND_MATCH = 2;
-    private static final String DEFAULT_DELIMITER_PATTERN = "[,:]";
+    private static final String DEFAULT_DELIMITERS = "[,:]";
     private static final int ALLOWED_MIN_NUMBER = 0;
+
 
     public static int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
@@ -20,12 +21,13 @@ public class StringCalculator {
     }
 
     private static int getSplitNumbersSum(String input) {
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(FIRST_MATCH);
-            return validateAndSumNumbers(m.group(SECOND_MATCH).split(customDelimiter));
+        Matcher customDelimiterPatternMatcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (customDelimiterPatternMatcher.find()) {
+            String customDelimiter = customDelimiterPatternMatcher.group(FIRST_MATCH);
+            return validateAndSumNumbers(
+                customDelimiterPatternMatcher.group(SECOND_MATCH).split(customDelimiter));
         }
-        return validateAndSumNumbers(input.split(DEFAULT_DELIMITER_PATTERN));
+        return validateAndSumNumbers(input.split(DEFAULT_DELIMITERS));
     }
 
     private static int validateAndSumNumbers(String[] values) {
