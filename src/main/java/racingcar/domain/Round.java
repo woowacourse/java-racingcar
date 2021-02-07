1e642409f112dc82e978293e5b0f3067dbb5d31f
Gomding <backchida@gmail.com>
2021-02-07 18:13:41 +0900

    (feat) : 시도 횟수 생성해주는 Round  클래스 생성

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -1,2 +1,13 @@
-package racingcar.domain;public class Round {
+package racingcar.domain;
+
+public class Round {
+    private final int count;
+
+    private Round(int count) {
+        this.count = count;
+    }
+
+    public static Round of(int count) {
+        return new Round(count);
+    }
 }
