c3065176fac45f0121caeb7325de1d6e9fc12c3e
sor <bosl95@naver.com>
2021-02-07 18:05:53 +0900

    refactor : 자동차 전진 기능 테스트 코드 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,17 +12,13 @@ class CarsTest {
         assertThat(cars.carList().size()).isEqualTo(2);
     }
 
-//    @Test
-//    void 자동차가_이동하는_함수_정상_작동_테스트() {
-//        RuleTest ruleTest = new RuleTest();
-//        Cars cars = new Cars(new String[]{"포비", "제이슨"}, ruleTest);
-//
-//        ruleTest.number = 1;  // 임의의 랜덤 함수가 1로 지정
-//        cars.startRace();
-//        assertThat(cars.getCarPositions()).containsExactly(0, 0);
-//
-//        ruleTest.number = 4;  // 임의의 랜덤 함수가 4로 지정
-//        cars.startRace();
-//        assertThat(cars.getCarPositions()).containsExactly(1, 1);
-//    }
+    @Test
+    void 임의의_자동차_외의_모든_자동차_전진_테스트() {
+        RuleTest ruleTest = new RuleTest(); // 첫번째 자동차만 멈춤
+        Cars cars = new Cars(new String[]{"포비", "제이슨"}, ruleTest); // 첫번째 자동차를 제외한 모든 자동차가 현재 최대 거리를 갖게 된다.
+
+        cars.startRace();
+        Car jason = cars.carList().get(1);
+        Assertions.assertTrue(jason.isMaxPosition(1));
+    }
 }
--- src/test/java/racingcar/domain/RuleTest.java
+++ src/test/java/racingcar/domain/RuleTest.java
@@ -3,11 +3,11 @@ package racingcar.domain;
 import org.junit.jupiter.api.Test;
 
 public class RuleTest implements RuleStrategy {
-    static int number;
+    static int number = 3;
 
     @Override
     public boolean execute() {
-        return number > MOVE_LIMIT;
+        return number++ > MOVE_LIMIT;
     }
 
     @Test
