2b5911f2de64c783fc564d0b9d0433f8c4811af7
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-07 22:27:55 +0900

    refactor: CarRepository에서 이와 관련되지 않은 메소드와 static 모두 제거

--- src/main/java/racingcar/domain/CarRepository.java
+++ src/main/java/racingcar/domain/CarRepository.java
@@ -4,62 +4,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import racingcar.output.printer.OutputPrinter;
-import racingcar.utils.GoForwardOrStopUtils;
-import racingcar.utils.randomnumber.RandomNumbersGeneratorUtils;
 
 public class CarRepository {
-    private static final List<Car> cars = new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
 
-    private CarRepository() {
-    }
-
-    public static void saveAll(List<Car> carsToSave) {
+    public void saveAll(List<Car> carsToSave) {
         cars.addAll(carsToSave);
     }
 
-    public static void deleteAll() {
+    public void deleteAll() {
         cars.clear();
     }
 
-    public static void runAllRaces(int racingTryTime) {
-        OutputPrinter.printStartMessageOfAllCarRacing();
-
-        for (int i = 0; i < racingTryTime; i++) {
-            List<Integer> randomNumbers
-                = RandomNumbersGeneratorUtils.generateRandomNumbersSizeOf(cars.size());
-            raceOneTime(randomNumbers);
-            printCurrentPositionStateOfAllCars();
-            OutputPrinter.printNewLine();
-        }
-    }
-
-    public static void raceOneTime(List<Integer> randomNumbers) {
-        for (int i = 0; i < cars.size(); i++) {
-            goForwardIfRandomNumberOfCarIsFourOrMore(cars.get(i), randomNumbers.get(i));
-        }
-    }
-
-    private static void goForwardIfRandomNumberOfCarIsFourOrMore(Car car,
-        int randomNumberOfCar) {
-        if (GoForwardOrStopUtils.isGoForward(randomNumberOfCar)) {
-            car.goForward();
-        }
-    }
-
-    private static void printCurrentPositionStateOfAllCars() {
-        cars.forEach(OutputPrinter::printCurrentPositionStateOfCar);
-    }
-
-    public static List<Car> getWinners() {
+    public List<Car> getWinners() {
         int maxPosition = getMaxPosition();
-
         return Collections.unmodifiableList(cars.stream()
             .filter(car -> car.getPosition() == maxPosition)
             .collect(Collectors.toList()));
     }
 
-    private static int getMaxPosition() {
+    private int getMaxPosition() {
         return cars.stream()
             .mapToInt(Car::getPosition)
             .max()
