e73bde88d4d42480037a941d406a7c53789965e9
sakjung <ssangyu123@gmail.com>
2021-02-08 18:31:02 +0900

    test: racingGame생성시 음수나 0인 정수가 라운드 횟수로 들어오면 에러를 던지는지 테스트하는 기능 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -3,40 +3,57 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.dto.CarsDto;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static racingcar.domain.RacingGame.UNAVAILABLE_NUMBER_OF_ROUNDS_MESSAGE;
 
 class RacingGameTest {
     private static final int NUMBER_OF_ROUNDS_FOR_TEST = 5;
 
-    private RacingGame racingGame;
-    private final List<String> carNamesForTest = Arrays.asList("똘이","멍이","순이");
-
-    @BeforeEach
-    void setUp(){
-        this.racingGame = new RacingGame(new Cars(carNamesForTest), NUMBER_OF_ROUNDS_FOR_TEST);
-    }
+    private final List<String> carNamesForTest = Arrays.asList("똘이", "멍이", "순이");
 
     @DisplayName("주어진 이름대로 자동차 생성되는지 테스트")
     @Test
     void racingGameConstructor_givenCarNames_createCarsByGivenCarNames() {
+        RacingGame racingGame = createDefaultRacingGame();
+
         CarsDto cars = new CarsDto(racingGame.getCars());
         for (int i = 0; i < carNamesForTest.size(); i++) {
             assertThat(cars.getCarsDto().get(i).getName()).isEqualTo(carNamesForTest.get(i));
         }
     }
 
+    @DisplayName("음수나 0 인 라운드 횟수가 입력됐을 떄 에러 반환 하는지")
+    @ParameterizedTest
+    @ValueSource(ints = {0,-3})
+    void racingGameConstructor_NonPositiveIntegerNumberOfRounds_ThrowException(int numberOfRounds) {
+        Cars cars = new Cars(carNamesForTest);
+
+        assertThatThrownBy(() -> new RacingGame(cars, numberOfRounds)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(UNAVAILABLE_NUMBER_OF_ROUNDS_MESSAGE);
+    }
+
     @DisplayName("라운드가 진행되는지 확인")
     @Test
-    void playRound_playOneRound_currentRoundShouldIncreaseByOne(){
+    void playRound_playOneRound_currentRoundShouldIncreaseByOne() {
+        RacingGame racingGame = createDefaultRacingGame();
+
         int beforeRound = racingGame.getCurrentRound();
         racingGame.playAnotherRound();
         int afterRound = racingGame.getCurrentRound();
 
         assertThat(beforeRound + 1).isEqualTo(afterRound);
+    }
+
+    private RacingGame createDefaultRacingGame() {
+        Cars cars = new Cars(carNamesForTest);
+        return new RacingGame(cars, NUMBER_OF_ROUNDS_FOR_TEST);
     }
 }
