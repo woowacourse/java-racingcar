83092c597b7979719f9172a11600697641666fcd
perenok <perenoke@gmail.com>
2021-02-08 15:28:32 +0900

    refactor(StringCalculator): change pattern to constant

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,16 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static final Pattern PATTERN = Pattern.compile("//(.)\\n(.*)");
 
     public static int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
         String delimiter = ",|:";
-        Matcher m = Pattern.compile("//(.)\\n(.*)").matcher(input);
-        if (m.find()) {
-            delimiter += "|" + m.group(1);
-            return calculate(m.group(2), delimiter);
+        Matcher matcher = PATTERN.matcher(input);
+        if (matcher.find()) {
+            delimiter += "|" + matcher.group(1);
+            return calculate(matcher.group(2), delimiter);
         }
         return calculate(input, delimiter);
     }
