fe77873ff520d6d369c01479ec4c748ccceb8f51
sjpark-dev <sjpark.dev@gmail.com>
2021-02-08 22:27:57 +0900

    fix(Car) : 상수값 변경

--- src/main/java/racingCar/domain/Car.java
+++ src/main/java/racingCar/domain/Car.java
@@ -2,7 +2,7 @@ package racingCar.domain;
 
 public class Car {
     private static final int THRESHOLD = 4;
-    private static final int MINIMUM_LENGTH = 0;
+    private static final int MINIMUM_LENGTH = 1;
     private static final int MAXIMUM_LENGTH = 5;
 
     private String name;
@@ -20,7 +20,7 @@ public class Car {
     }
 
     private void validate(String name) {
-        if (name.length() <= MINIMUM_LENGTH || name.length() > MAXIMUM_LENGTH) {
+        if (name.length() < MINIMUM_LENGTH || name.length() > MAXIMUM_LENGTH) {
             throw new IllegalArgumentException("길이가 1에서 5사이의 이름을 입력해주세요.");
         }
     }
