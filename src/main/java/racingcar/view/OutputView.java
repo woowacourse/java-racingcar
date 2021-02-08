61467a206bfb0ad8b6f7bec7c1f1d257ac312247
TaewanKimmmm <ktww88@gmail.com>
2021-02-08 20:35:03 +0900

    refactor: 문자열 하드 코딩 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class OutputView {
     private static final String CAR_NAME_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String LAP_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
     private OutputView() {
     }
@@ -27,11 +29,11 @@ public class OutputView {
     }
 
     public static void showWinners() {
-        System.out.println(String.join(", ", CarRepository.getWinners()) + "가 최종 우승했습니다.");
+        System.out.println(String.join(", ", CarRepository.getWinners()) + RESULT_MESSAGE);
     }
 
     public static void showResultMessage() {
         System.out.println();
-        System.out.println("실행 결과");
+        System.out.println(WINNER_MESSAGE);
     }
 }
