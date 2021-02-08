ccf62baa64e18c90889d19f46f5bf1c142d0a027
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-08 20:30:07 +0900

    style: 배열 선언 방식 통일

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -23,7 +23,7 @@ public class CarsTest {
 
     @Test
     public void Cars_getWinner_동작_테스트() {
-        String carNames[] = {"test1", "test2", "test3"};
+        String[] carNames = {"test1", "test2", "test3"};
         int distance[] = {8, 1, 3};
         Cars cars = new Cars(carNames, distance);
         assertThat(cars.getWinner(8)).isEqualTo("test1");
