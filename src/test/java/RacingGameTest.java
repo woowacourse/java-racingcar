6ea3b62af48fe93eee137e0bed0875c9f5bb765c
younghoonkwon <younghoon.kwon91@gmail.com>
2021-02-08 17:14:56 +0900

    feat: RacingGameTest splitInputTest

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class RacingGameTest {
+import controller.RacingGame;
+import model.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RacingGameTest {
+
+    @Test
+    @DisplayName("입력값을 쉼표 단위로 나눠서 리스트 객체로 반환한다.")
+    public void splitInputTest() throws Exception {
+        List<String> splittedInput= RacingGame.splitInput("Ryan,Pika");
+        List<String> expected = Arrays.asList("Ryan","Pika");
+        assertThat(splittedInput).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 시행 횟수는 1 이상의 숫자여야 한다.")
+    @ValueSource(strings = {" ", "abcdefg", "  "})
+    void createCarExceptionTest(String name){
+        assertThrows(IllegalArgumentException.class, ()-> new Car(name));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Car(name));
+        assertThat(e.getMessage()).isEqualTo(Car.CAR_NAME_INVALID);
+    }
 }
