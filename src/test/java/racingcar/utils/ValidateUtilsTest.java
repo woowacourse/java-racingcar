16bea4768f3e3fffcfc7f4fcab4b825b1df9d6b6
binghe819 <binghe819@naver.com>
2021-02-09 02:12:40 +0900

    refactor: 자동차 이름 및 시도 횟수 입력값 테스트 코드 리팩토링
    * 람다 중괄호 생략
    * 자동치 이름 공백과 null 테스트 분리
    * 테스트 메서드 이름 통일

--- src/test/java/racingcar/utils/ValidateUtilsTest.java
+++ src/test/java/racingcar/utils/ValidateUtilsTest.java
@@ -10,20 +10,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.InvalidCarNameException;
+import racingcar.exception.InvalidNumOfMoveException;
 
 
 class ValidateUtilsTest {
 
-    @DisplayName("경주하는 자동차는 2개 이상이어야 한다")
     @Test
     public void 자동차_갯수_2개_이상_테스트_negative() {
         // given, when
         String[] carNames = {"pobi"};
 
         // then
-        assertThrows(RuntimeException.class, () -> {
-            ValidateUtils.validateCarNames(carNames);
-        });
+        assertThatThrownBy(() -> ValidateUtils.validateCarNames(carNames))
+            .isInstanceOf(InvalidCarNameException.class);
     }
 
     @Test
@@ -32,37 +32,42 @@ class ValidateUtilsTest {
         String[] carNames = {"pobi", "hong", "gil", "mark", "honggildong"};
 
         // then
-        assertThrows(RuntimeException.class, () -> {
-            ValidateUtils.validateCarNames(carNames);
-        });
+        assertThatThrownBy(() -> ValidateUtils.validateCarNames(carNames))
+            .isInstanceOf(InvalidCarNameException.class);
     }
 
     @Test
     public void 자동차_이름_공백_테스트_negative() {
         // given, when
-        String[] carNames = {"pobi", "hong", "gil", "", null};
+        String[] carNames = {"pobi", "hong", "gil", ""};
 
         // then
-        assertThrows(RuntimeException.class, () -> {
-            ValidateUtils.validateCarNames(carNames);
-        });
+        assertThatThrownBy(() -> ValidateUtils.validateCarNames(carNames))
+            .isInstanceOf(InvalidCarNameException.class);
     }
 
-    @DisplayName("시도횟수는 지정된 조건에 맞는 수가 입력되었습니다.")
+    @Test
+    public void 자동차_이름_null_테스트_negative() {
+        // given, when
+        String[] carNames = {"pobi", "hong", "gil", null};
+
+        // then
+        assertThatThrownBy(() -> ValidateUtils.validateCarNames(carNames))
+            .isInstanceOf(NullPointerException.class);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"-2", "d"})
     @NullAndEmptySource
-    void invalidRoundInput(String invalidInput) {
-        assertThatThrownBy(() -> {
-            ValidateUtils.validateRacingRoundCount(invalidInput);
-        }).isInstanceOf(RuntimeException.class);
+    public void 시도횟수_음수_및_숫자아닌_입력값_테스트_negative(String invalidInput) {
+        assertThatThrownBy(() -> ValidateUtils.validateRacingRoundCount(invalidInput))
+            .isInstanceOf(InvalidNumOfMoveException.class);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:1", "2:2", "3:3", "4:4", "5:5"}, delimiter = ':')
-    public void 시도횟수_성공케이스(String input, String expected) {
+    public void 시도횟수_positive(String input, int expected) {
         assertThat(ValidateUtils.validateRacingRoundCount(input))
-            .isEqualTo(Integer.parseInt(expected));
+            .isEqualTo(expected);
     }
-
 }
