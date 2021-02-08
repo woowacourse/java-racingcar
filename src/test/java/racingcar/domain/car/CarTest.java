bae5ed696542094dc7bcd1f200d169cd753b64f9
Hyeon9mak <jinha3507@gmail.com>
2021-02-08 12:46:55 +0900

    test: add before each setUp method in CarTest

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -2,29 +2,44 @@ package racingcar.domain.car;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+    private String[] testNames;
+    private Car firstCar;
+    private Car secondCar;
+    private Car thirdCar;
+
+    @BeforeEach
+    void setUp() {
+        testNames = new String[]{"샐리", "현구막", "휴"};
+        firstCar = Car.enrollWithName(testNames[0]);
+        secondCar = Car.enrollWithName(testNames[1]);
+        thirdCar = Car.enrollWithName(testNames[2]);
+    }
 
     @Test
     @DisplayName("자동차 생성 확인")
     void car_create() {
-        Car car1 = Car.enrollWithName("샐리");
-        assertThat(car1.getName()).isEqualTo("샐리");
-        assertThat(car1.getPosition().length()).isEqualTo(0);
+        assertThat(firstCar.getName()).isEqualTo(testNames[0]);
+        assertThat(firstCar.getPosition().length()).isEqualTo(0);
+        assertThat(secondCar.getName()).isEqualTo(testNames[1]);
+        assertThat(secondCar.getPosition().length()).isEqualTo(0);
+        assertThat(thirdCar.getName()).isEqualTo(testNames[2]);
+        assertThat(thirdCar.getPosition().length()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("자동차 전진 구현")
     void car_move() {
-        Car car1 = Car.enrollWithName("샐리");
-        car1.fillUpGas(4);
-        car1.forward();
-        assertThat(car1.getPosition().length()).isEqualTo(1);
+        firstCar.fillUpGas(4);
+        firstCar.forward();
+        assertThat(firstCar.getPosition().length()).isEqualTo(1);
 
-        car1.fillUpGas(3);
-        car1.forward();
-        assertThat(car1.getPosition().length()).isEqualTo(1);
+        secondCar.fillUpGas(3);
+        secondCar.forward();
+        assertThat(secondCar.getPosition().length()).isEqualTo(0);
     }
 }
