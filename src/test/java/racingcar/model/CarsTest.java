51090dfe0a0bf2124a663ba16aa7d1085135e6d2
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-08 20:27:17 +0900

    refactor: CarsTest 검증 예외 종류 IllegalArgumentException으로 변경

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -9,7 +9,7 @@ public class CarsTest {
     public void Cars_객체_생성자_빈_배열_매개변수_예외_테스트() {
         String[] carNames = {};
         assertThatThrownBy(() ->  new Cars(carNames))
-                .isInstanceOf(RuntimeException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 최소 하나의 자동차 이름을 입력하세요.");
     }
 
@@ -17,7 +17,7 @@ public class CarsTest {
     public void Cars_객체_생성자_중복_예외_테스트() {
         String[] carNames = {"test1", "test2", "test3", "test4", "test1"};
         assertThatThrownBy(() ->  new Cars(carNames))
-                .isInstanceOf(RuntimeException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 자동차 이름이 중복됩니다.");
     }
 
