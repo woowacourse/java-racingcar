d5581a10255302cc491aaf564635cce17c9b23b1
perenok <perenoke@gmail.com>
2021-02-08 17:14:19 +0900

    refactor(CarTest): add @DisplayName() and modify testFormatSingleRoundInformation()
    change method name from testToString to testFormatSingleRoundInformation
    add @CsvSource() to test the number of cases

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,16 +1,15 @@
 package racingcar.domain.car;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
 
     @ParameterizedTest
-    @CsvSource(value={"java:True", "python:False"}, delimiter = ':')
+    @CsvSource(value = {"java:True", "python:False"}, delimiter = ':')
     @DisplayName("Car 인스턴스의 이름의 제한조건인 5자 이상인 경우 false 테스트")
     void checkMaxName(String input, boolean result) {
         Car testingCar = new Car(input);
@@ -18,7 +17,7 @@ class CarTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value={"0:False", "1:False", "3:False", "4:True", "9:True"}, delimiter = ':')
+    @CsvSource(value = {"0:False", "1:False", "3:False", "4:True", "9:True"}, delimiter = ':')
     @DisplayName("조건에 따른 Car 인스턴스의 전진 테스트")
     void tryToMoveForward(int randomInteger, boolean result) {
         Car testingCar = new Car("java");
@@ -27,19 +26,22 @@ class CarTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value={"0:False", "1:True"}, delimiter = ':')
+    @CsvSource(value = {"0:False", "1:True"}, delimiter = ':')
+    @DisplayName("maxPosition과 Car 객체의 position 비교 테스트")
     void isMaxPosition(int maxPosition, boolean result) {
         Car testingCar = new Car("java");
         testingCar.tryToMoveForward(5);
         assertEquals(testingCar.isMaxPosition(maxPosition), result);
     }
 
-    @Test
-    void testToString() {
+    @ParameterizedTest
+    @CsvSource(value = {"0:'java : '", "1:'java : -'", "2:'java : --'"}, delimiter = ':')
+    @DisplayName("format에 맞게 return값이 나오는지 테스트")
+    void testFormatSingleRoundInformation(int position, String result) {
         Car testingCar = new Car("java");
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < position; ++i) {
             testingCar.tryToMoveForward(5);
         }
-        assertEquals(testingCar.toString(), "java : ---");
+        assertEquals(testingCar.formatSingleRoundInformation(), result);
     }
 }
