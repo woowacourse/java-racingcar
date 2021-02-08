3d4bec15186db80813c6f74dab9a8335a5f1e086
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-08 21:15:25 +0900

    refactor: controller에서 singleMove의 결과를 호출하도록 변경

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,15 +1,25 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RacingGameController {
     public void play(final Cars carsInGame, final int trialNumber) {
         OutputView.printOutputMessage();
         for (int i = 0; i < trialNumber; i++) {
-            carsInGame.singleMove();
+            printSingleMoveResult(carsInGame.singleMove());
         }
         OutputView.printWinner(carsInGame.getWinner());
+    }
+
+    private void printSingleMoveResult(final List<Car> carsAfterSingleMove) {
+        for (Car car : carsAfterSingleMove) {
+            OutputView.printCarInformation(car.getName(), car.getDistance());
+        }
+        OutputView.printNewLine();
     }
 }
 
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,12 @@
 package racingcar.view;
 
-import racingcar.model.Car;
-
 public class OutputView {
     private static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
     private static final String ENTER_TRIAL_NUMBER = "시도할 회수는 몇회인가요?\n";
     private static final String OUTPUT_MESSAGE = "\n실행 결과";
     private static final String WINNER_MESSAGE = "가 최종 우승했습니다.\n";
+    private static final String DASH = "-";
+    private static final String STATUS_INDICATOR = " : ";
 
     public static void readCarName() {
         System.out.print(ENTER_CAR_NAME);
@@ -24,8 +24,12 @@ public class OutputView {
         System.out.println(OUTPUT_MESSAGE);
     }
 
-    public static void printCarInformation(final Car car) {
-        System.out.println(car);
+    public static void printCarInformation(final String carName, final int carDistance) {
+        StringBuilder carDistanceIndicator = new StringBuilder();
+        for (int i=0 ;i<carDistance; i++) {
+            carDistanceIndicator.append(DASH);
+        }
+        System.out.println(carName + STATUS_INDICATOR + carDistanceIndicator);
     }
 
     public static void printNewLine() {
