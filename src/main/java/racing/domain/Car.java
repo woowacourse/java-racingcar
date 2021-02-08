3effee4fc2453ccca50de3ca8eb2af6f612acf49
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-08 10:32:04 +0900

    refactor: Car 이름 유효성 검사 순서 변경 및 메시지 추가
    예외 발생시 메시지를 추가해준다.

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class Car {
     private static final Pattern PATTERN = Pattern.compile("[a-zA-Z]{1,5}");
+    private static final String INVALID_NAME_FORMAT = "자동차 이름은 빈 문자열이 아닌 5자 이하의 영어로 구성되어야 합니다.";
     private static final int MINIMUM_MOVE_NUMBER = 4;
     private static final int DEFAULT_POSITION = 0;
 
@@ -13,18 +14,18 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = DEFAULT_POSITION;
-        validateName();
     }
 
-    private void validateName() {
-        if (Objects.isNull(this.name)) {
-            throw new IllegalArgumentException();
+    private void validateName(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException(INVALID_NAME_FORMAT);
         }
-        Matcher matcher = PATTERN.matcher(this.name);
+        Matcher matcher = PATTERN.matcher(name);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_NAME_FORMAT);
         }
     }
 
