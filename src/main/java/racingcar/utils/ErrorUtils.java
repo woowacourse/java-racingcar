7fecba3a8dcec126663ae1117308ed30d43fc631
perenok <perenoke@gmail.com>
2021-02-08 16:33:54 +0900

    refactor(ErrorUtils): use System.lineSeparator() instead of \n

--- src/main/java/racingcar/utils/ErrorUtils.java
+++ src/main/java/racingcar/utils/ErrorUtils.java
@@ -2,7 +2,7 @@ package racingcar.utils;
 
 public class ErrorUtils {
 
-    private static final String FORMAT = "\n[ERROR] %s\n";
+    private static final String FORMAT = System.lineSeparator() + "[ERROR] %s" + System.lineSeparator();
 
     public static void printError(Exception e) {
         System.out.printf((FORMAT) + "%n", e.getMessage());
