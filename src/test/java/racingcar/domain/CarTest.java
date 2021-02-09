e792ba2cbe8643e6fefe44d40a1399dd53af4b7a
NewWisdom <wlgp2500@gmail.com>
2021-02-09 13:49:13 +0900

    test(car): 자동차 움직임 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,7 +2,10 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
@@ -31,5 +34,23 @@ public class CarTest {
         assertThatThrownBy(() -> {
             Car car = new Car(carName);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("주어진 값이 4 이하일 경우 움직이지 않음 확인")
+    void carMoveRandomNumber_3(int randomNumber) {
+        Car car = new Car("pobi");
+        car.moveForward(randomNumber);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 6, 9})
+    @DisplayName("주어진 값이 4 이상일 경우 움직임 확인")
+    void carMoveRandomNumber_4(int randomNumber) {
+        Car car = new Car("pobi");
+        car.moveForward(randomNumber);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 }
