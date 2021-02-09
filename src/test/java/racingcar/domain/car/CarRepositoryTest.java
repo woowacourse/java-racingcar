7eab4e5dfd41e4fa6bfad1139e971a43eefe3e29
perenok <perenoke@gmail.com>
2021-02-09 20:36:54 +0900

    test(CarRepositoryTest) : modify methods
    add set() for redundant initialization process

--- src/test/java/racingcar/domain/car/CarRepositoryTest.java
+++ src/test/java/racingcar/domain/car/CarRepositoryTest.java
@@ -1,29 +1,57 @@
 package racingcar.domain.car;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRepositoryTest {
 
-    @Test
-    void addCars() {
-        List<Car> testingCars = new ArrayList<>();
-        testingCars.add(new Car("java"));
-        testingCars.add(new Car("html"));
-        CarRepository.addCars(testingCars);
-        assertEquals(testingCars, CarRepository.cars());
+    private static final List<Car> testCars = new ArrayList<>();
+    private static final List<Car> expectWinners = new ArrayList<>();
+
+    @BeforeAll
+    static void set() {
+        Car car1 = new Car("java");
+        Car car2 = new Car("html");
+        Car car3 = new Car("c++");
+        car2.tryToMoveForward(9);
+        car3.tryToMoveForward(9);
+        testCars.add(car1);
+        testCars.add(car2);
+        testCars.add(car3);
+        expectWinners.add(car2);
+        expectWinners.add(car3);
     }
 
     @Test
+    @DisplayName("addCars 메소드를 이용하여 car들이 추가되는지 테스트")
+    void addCars() {
+        CarRepository.addCars(testCars);
+        assertThat(testCars.containsAll(CarRepository.cars()));
+    }
+
+    @Test
+    @DisplayName("우승자들이 나오는지 테스트")
     void collectWinners() {
-        List<Car> testingCars = new ArrayList<>();
-        testingCars.add(new Car("java"));
-        testingCars.add(new Car("html"));
-        CarRepository.addCars(testingCars);
-        assertThat(CarRepository.collectWinners()).isEqualTo("java, html");
+        CarRepository.addCars(testCars);
+        List<Car> actualWinners = CarRepository.collectWinners();
+        assertThat(actualWinners.containsAll(expectWinners));
+    }
+
+    @Test
+    @DisplayName("최대거리값이 제대로 나오는지 테스트")
+    void selectMaxPosition() {
+        CarRepository.addCars(testCars);
+        int expectMaxPosition = expectWinners.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        int actualMaxPosition = CarRepository.selectMaxPosition();
+        assertThat(actualMaxPosition).isEqualTo(expectMaxPosition);
     }
 }
