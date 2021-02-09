c710baa2a4ea2822e34747d603e9a095c0051f08
Naeun <knae11@naver.com>
2021-02-10 08:52:31 +0900

    style: code formatting

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -18,7 +18,7 @@ public class CarTest {
         car.move(5);
         // then(검증)
         assertThat(car.getPosition()).isEqualTo(previous + 1);
-        assertThat(car).isEqualTo(new Car("mini",new Position(1)));
+        assertThat(car).isEqualTo(new Car("mini", new Position(1)));
     }
 
 }
