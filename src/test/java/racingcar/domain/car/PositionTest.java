0d4e5b7bc29d84d74429dd22e03071f3eb78c61f
lns13301 <rkdrbgks22@ks.ac.kr>
2021-02-09 20:48:41 +0900

    test: Position Wrapping 추가

--- src/test/java/racingcar/domain/car/PositionTest.java
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -1,6 +1,6 @@
 package racingcar.domain.car;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
