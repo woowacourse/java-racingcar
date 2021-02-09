774b6d8ec0957c98f0241a4783420272084fb138
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-02-09 09:44:31 +0900

    refactor: Random final -> static final로 변경

--- src/main/java/racingcar/domain/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/numbergenerator/RandomNumberGenerator.java
@@ -3,13 +3,9 @@ package racingcar.domain.numbergenerator;
 import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
-    private final Random random;
+    private static final Random random = new Random();
     private final int MIN_RANDOM_NUMBER = 0;
     private final int MAX_RANDOM_NUMBER = 9;
-
-    public RandomNumberGenerator() {
-        random = new Random();
-    }
 
     public int generate() {
         return MIN_RANDOM_NUMBER + random.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1);
