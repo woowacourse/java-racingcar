cd99bb98aa4cdd168899831b8f19fa8630d596a2
perenok <perenoke@gmail.com>
2021-02-09 20:44:12 +0900

    test(GameTest) : add methods
    change @Test to @ParameterizedTest to get several test cases
    add joinWinnerNames test

--- src/test/java/racingcar/domain/game/GameTest.java
+++ src/test/java/racingcar/domain/game/GameTest.java
@@ -1,15 +1,37 @@
 package racingcar.domain.game;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.car.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
+
+    private Game testingGame = new Game(0);
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1:false", "2:1:true"}, delimiter = ':')
+    @DisplayName("최종 횟수에 count가 도달 했을때, 하지 못했을 때 테스트")
+    void notFinished(final int round, final int count, final boolean result) {
+        testingGame = new Game(round);
+        for (int i = 0; i < count; i++) {
+            testingGame.incrementCount();
+        }
+        assertEquals(testingGame.notFinished(), result);
+    }
+
     @Test
-    void notFinished() {
-        Game testingGame = new Game(1);
-        assertTrue(testingGame.notFinished());
-        testingGame.incrementCount();
-        assertFalse(testingGame.notFinished());
+    @DisplayName("형태에 맞게 우승자들의 이름이 나오는지 테스트")
+    void joinWinnerNames() {
+        List<Car> winners = new ArrayList<>();
+        winners.add(new Car("java"));
+        winners.add(new Car("html"));
+        assertEquals(testingGame.joinWinnerNames(winners), "java, html");
     }
 }
