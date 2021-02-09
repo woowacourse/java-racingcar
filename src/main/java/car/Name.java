0081e2fbcc9ad61c11df0d5534e4af6140749b81
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-09 18:15:59 +0900

    feat(name): 메소드 로직 분리 및 하드코딩 제거

--- src/main/java/car/Name.java
+++ src/main/java/car/Name.java
@@ -3,32 +3,39 @@ package car;
 import java.util.Objects;
 
 public class Name {
+
     private String name;
-    
+    final static int CAR_NAME_LIMIT = 5;
+
     public Name(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
-        if (!isValid()) {
-            throw new IllegalArgumentException();
-        }
-        
+        isValidName(name);
         return name;
     }
-    
-    private boolean isValid() {
-        return name.length() <= 5 && !name.isEmpty();
+
+    private void isValidName(String name) {
+
+        if (name.length() <= CAR_NAME_LIMIT && !name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
     }
-    
+
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Name name1 = (Name) o;
         return Objects.equals(name, name1.name);
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hash(name);
--- src/main/java/car/ValidCheck.java
+++ src/main/java/car/ValidCheck.java
@@ -4,42 +4,42 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class ValidCheck {
-    
-    
+
+    final static int CAR_NAME_LIMIT = 5;
+
     private ValidCheck() {
-    
+
     }
-    
+
+    private boolean isNotValid(String carName) {
+        return !(carName.length() <= CAR_NAME_LIMIT &&
+            !carName.isEmpty());
+    }
+
     public static void carNameValid(String carNamesInput) {
         if (isSpace(carNamesInput) || isNameLength(carNamesInput)) {
             throw new IllegalArgumentException();
         }
     }
-    
+
     public static void round(String roundInput) {
         final String pattern = "[1-9]+[0-9]*";
         if (!Pattern.matches(pattern, roundInput)) {
             throw new IllegalArgumentException();
         }
     }
-    
+
     private static boolean isSpace(String carNamesInput) {
         return carNamesInput.contains(" ");
     }
-    
+
     private static boolean isNameLength(String carNamesInput) {
         final String delimiter = ",";
         String[] carNames = carNamesInput.split(delimiter);
-        
+
         return Arrays.stream(carNames)
-                     .anyMatch(name -> name.length() > 5);
+            .anyMatch(name -> name.length() > 5);
     }
-    
-    
+
+
 }
-
-//자동차 이름 예외 체크
-//시도 횟수 예외 체크
-
-// 라운드 결과 출력
-// 우승자 출력
