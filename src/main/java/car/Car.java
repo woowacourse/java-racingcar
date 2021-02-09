546e0c85fa6ca0a1cf669e79c8f0e6bd4282ccd8
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-09 19:20:58 +0900

    feat(car): Car 객체 상수 위치 변경

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -3,52 +3,54 @@ package car;
 import java.util.Objects;
 
 public class Car {
-    private final Name carName;
-    
     private static final int CONDITION = 4;
-    
+    private final Name carName;
     private int position;
-    
+
     public Car(String carName) {
         this(new Name(carName));
     }
-    
+
     public Car(Name carName) {
         this(carName, 0);
     }
-    
+
     public Car(String carName, int position) {
         this(new Name(carName), position);
     }
-    
+
     public Car(Name carName, int position) {
         this.carName = carName;
         this.position = position;
     }
-    
+
     public void moveOrStop() {
         if (RandomUtil.getRandomValue() >= CONDITION) {
             this.position++;
         }
     }
-    
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Car car = (Car) o;
         return position == car.position && Objects.equals(carName, car.carName);
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hash(carName, position);
     }
-    
+
     public int getPosition() {
         return position;
     }
-    
+
     public String getName() {
         return carName.getName();
     }
