b84ffeb68217e3fd3f04a7f22d8b54455414cd18
Junroot <junroot0909@gmail.com>
2021-02-09 15:09:44 +0900

    refactor(RacingGameTest): 테스트 픽스처 생성

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.Car;
 import racingcar.domain.RacingGame;
 
@@ -13,15 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
 
-    private List<Car> cars;
-    private RacingGame racingGame;
-
-    @BeforeEach
-    void setUp() {
-        cars = new ArrayList<>();
-    }
-
-    private Car newCar(String name, int position) {
+    private static Car newCar(String name, int position) {
         final Car car = new Car(name);
         for (int i = 0; i < position; i++) {
             car.tryToMove(4);
@@ -29,46 +23,46 @@ public class RacingGameTest {
         return car;
     }
 
-    @Test
-    @DisplayName("우승자가 한명인 경우")
-    void findWinnersTest_한명() {
+    @ParameterizedTest
+    @DisplayName("우승자 판별 테스트")
+    @MethodSource("provideRacingGameForFindWinner")
+    void findWinnerTest(RacingGame racingGame, List<String> expected) {
+        final List<String> actual = racingGame.getWinners();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideRacingGameForFindWinner() {
+        return Stream.of(
+            Arguments.of(new RacingGame(provideCars_우승자_한명(), 0), Arrays.asList("루트")),
+            Arguments.of(new RacingGame(provideCars_우승자_두명(), 0), Arrays.asList("루트", "소롱")),
+            Arguments.of(new RacingGame(provideCars_모두_움직이지_않음(), 0), Arrays.asList("루트", "소롱", "포비"))
+        );
+    }
+
+    private static List<Car> provideCars_우승자_한명() {
+        List<Car> cars = new ArrayList<>();
         cars.add(newCar("루트", 5));
         cars.add(newCar("소롱", 3));
         cars.add(newCar("포비", 1));
-        racingGame = new RacingGame(cars, 0);
 
-        final List<String> expected = new ArrayList<>();
-        expected.add(cars.get(0).getName());
-
-        final List<String> actual = racingGame.getWinners();
-        assertThat(actual).isEqualTo(expected);
+        return cars;
     }
 
-    @Test
-    @DisplayName("우승자가 두명인 경우")
-    void findWinnersTest_두명_이상() {
+    private static List<Car> provideCars_우승자_두명() {
+        List<Car> cars = new ArrayList<>();
         cars.add(newCar("루트", 5));
         cars.add(newCar("소롱", 5));
         cars.add(newCar("포비", 1));
-        racingGame = new RacingGame(cars, 0);
-        final List<String> actual = racingGame.getWinners();
-        final List<String> expected = new ArrayList<>();
-        expected.add(cars.get(0).getName());
-        expected.add(cars.get(1).getName());
 
-        assertThat(actual).isEqualTo(expected);
+        return cars;
     }
 
-    @Test
-    @DisplayName("모두 움직이지 않은 경우")
-    void findWinnerTest_모두_움직이지_않음() {
+    private static List<Car> provideCars_모두_움직이지_않음() {
+        List<Car> cars = new ArrayList<>();
         cars.add(new Car("루트"));
         cars.add(new Car("소롱"));
         cars.add(new Car("포비"));
-        racingGame = new RacingGame(cars, 0);
 
-        List<String> expected = cars.stream().map(Car::getName).collect(Collectors.toList());
-        List<String> actual = racingGame.getWinners();
-        assertThat(actual).isEqualTo(expected);
+        return cars;
     }
 }
