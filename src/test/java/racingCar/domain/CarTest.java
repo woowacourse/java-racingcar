bee1dd1af006a2bb0d44281671a21444241e92bf
RinSabbit <celena1004@naver.com>
2021-02-10 19:27:20 +0900

    feat: Car 생성 테스트 기능 구현

--- src/test/java/racingCar/domain/CarTest.java
+++ src/test/java/racingCar/domain/CarTest.java
@@ -3,14 +3,21 @@ package racingCar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import racingCar.domain.Engine.FixedEngine;
+import racingCar.domain.car.Car;
+import racingCar.domain.car.Engine.FixedEngine;
 
 class CarTest {
 
     @Test
+    public void 생성(){
+        final Car car = new Car("jason",new FixedEngine(2));
+        assertThat(car).isEqualTo(new Car("jason",new FixedEngine(2)));
+    }
+
+    @Test
     public void 자동차_움직임_및_현재상태출력_테스트() {
         // given
-        Car car = new Car("pobi", new FixedEngine(9));
+        Car car = new Car("pobi",new FixedEngine(9));
 
         // when
         int numOfRacingRound = 5;
@@ -19,13 +26,13 @@ class CarTest {
         }
 
         // then
-        assertThat(car.toString()).isEqualTo("pobi : -----");
+        assertThat(car.getPosition()).isEqualTo(5);
     }
 
     @Test
     public void 자동차_움직임_및_현재상태_출력_negative() {
         // given
-        Car car = new Car("pobi", new FixedEngine(2));
+        Car car = new Car("pobi",new FixedEngine(2));
 
         // when
         int numOfRacingRound = 5;
@@ -34,7 +41,7 @@ class CarTest {
         }
 
         // then
-        assertThat(car.toString()).isEqualTo("pobi : ");
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
 }
