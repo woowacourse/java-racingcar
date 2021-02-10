8ebf60f149402bb6dac24b725c7764e13b817bec
Jinhong <56240505+xlffm3@users.noreply.github.com>
2021-02-10 15:16:05 +0900

    refactor: RandomUtils 삭제
    해당 기능을 사용하는 RandomMovingStrategy는 랜덤 숫자 추출을
    클래스 내부에서 진행한다.

--- src/main/java/racing/domain/RandomMovingStrategy.java
+++ src/main/java/racing/domain/RandomMovingStrategy.java
@@ -1,15 +1,15 @@
 package racing.domain;
 
-import racing.utils.RandomUtils;
+import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy {
-    private static final int START_NUMBER = 0;
-    private static final int END_NUMBER = 9;
+    private static final Random RANDOM = new Random();
+    private static final int RANDOM_NUMBER_BOUND = 10;
     private static final int MINIMUM_MOVE_NUMBER = 4;
 
     @Override
     public boolean isMovable() {
-        int randomNumber = RandomUtils.getRandomNumber(START_NUMBER, END_NUMBER);
+        int randomNumber = RANDOM.nextInt(RANDOM_NUMBER_BOUND);
         return randomNumber >= MINIMUM_MOVE_NUMBER;
     }
 }
