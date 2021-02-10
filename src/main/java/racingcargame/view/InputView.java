929c5a49db0265838c124e30fdc017b28838ce59
sjpark-dev <sjpark.dev@gmail.com>
2021-02-10 12:40:38 +0900

    refactor(InputView) : 메소드 접근제어자 수정, private 생성자 추가

--- src/main/java/racingcargame/view/InputView.java
+++ src/main/java/racingcargame/view/InputView.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    private InputView() {
+
+    }
+
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carNamesInput = scanner.nextLine();
@@ -12,14 +16,14 @@ public class InputView {
         return carNamesInput;
     }
 
-    public static void validateCarNamesInput(String carNamesInput) {
+    private static void validateCarNamesInput(String carNamesInput) {
         validateNullOrEmpty(carNamesInput);
         if (carNamesInput.charAt(0) == ',' || carNamesInput.charAt(carNamesInput.length() - 1) == ',') {
             throw new IllegalArgumentException("시작이나 끝에 콤마(,)가 올 수 없습니다.");
         }
     }
 
-    public static void validateNullOrEmpty(String input) {
+    private static void validateNullOrEmpty(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("입력값이 있어야 합니다.");
         }
@@ -33,7 +37,7 @@ public class InputView {
         return Integer.parseInt(raceCountInput);
     }
 
-    public static void validateRaceCountInput(String raceCountInput) {
+    private static void validateRaceCountInput(String raceCountInput) {
         validateNullOrEmpty(raceCountInput);
         if (!raceCountInput.chars().allMatch(Character::isDigit) || raceCountInput.charAt(0) == '0') {
             throw new IllegalArgumentException("1 이상의 숫자를 입력해주세요.");
