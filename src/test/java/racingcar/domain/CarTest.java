fd42174960fc9af6a59f3eded90c7c4b04767abe
Junseo <air.junseo@gmail.com>
2021-02-10 09:46:06 +0900

    refactor: 여러 값에 따른 전진 여부 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,9 +2,10 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     private Car car;
@@ -14,18 +15,20 @@ class CarTest {
         car = new Car(new Racer("pobi"));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("4이상의 값이 들어간 경우 전진하는지 확인")
-    public void checkMoveTrue() {
-        car.movePosition(4);
+    public void checkMoveTrue(int number) {
+        car.movePosition(number);
         Position position = car.getPosition();
         assertEquals(1, position.getPosition());
     }
 
-    @Test
-    @DisplayName("3이의 값이 들어간 경우 전진하지 않았는지 확인")
-    public void checkMoveFalse() {
-        car.movePosition(3);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("3이하의 값이 들어간 경우 전진하지 않았는지 확인")
+    public void checkMoveFalse(int number) {
+        car.movePosition(number);
         Position position = car.getPosition();
         assertEquals(0, position.getPosition());
     }
