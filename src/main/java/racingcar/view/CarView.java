d91fba2aad6d756d477f500f8864dad277fd5370
yoonsung.jung <ybook2012@gmail.com>
2021-02-10 16:49:48 +0900

    feat(car-view) : car의 상태를 출력하는 기능 추가

---
+++ src/main/java/racingcar/view/CarView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+public class CarView {
+
+    public void printCarStatus(final Car car) {
+        StringBuilder carStatus = new StringBuilder(car.getName()).append(" : ");
+        carStatus.append(getCarProgressBarStatus(car.getPosition()));
+        OutputView.printMessage(carStatus.toString());
+    }
+
+    private StringBuilder getCarProgressBarStatus(final int carPosition) {
+        StringBuilder progressBarStatus = new StringBuilder();
+        for (int i = 0; i < carPosition; ++i) {
+            progressBarStatus.append('-');
+        }
+        return progressBarStatus;
+    }
+}
