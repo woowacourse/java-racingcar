4b22b21947fb0f6faa406acd9c2c7827a7a856c8
TaewanKimmmm <ktww88@gmail.com>
2021-02-10 17:31:28 +0900

    test: DisplayName 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,15 +1,17 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarTest {
+    @DisplayName("자동차 전진 테스트")
     @Test
     void moveForward() {
         Car car = new Car(new CarName("ser"));
         int previous = car.getPosition();
-        car.moveForwardByRandomNumber(5);
+        car.moveForwardByNumber(5);
         assertEquals(car.getPosition(), previous + 1);
     }
 }
