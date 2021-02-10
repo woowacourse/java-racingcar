b01151264fcb1d76a9ef5c9181220cbc2a83bbe5
Wannte <T.wannte@gmail.com>
2021-02-10 17:32:57 +0900

    Test(Car): isAtPosition 테스트 추가

--- src/test/java/javaracingcar/domain/CarTest.java
+++ src/test/java/javaracingcar/domain/CarTest.java
@@ -1,22 +1,40 @@
 package javaracingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CarTest {
+    Car car;
 
-    @Test
+    @BeforeEach
+    void setup() {
+        car = new Car("Benz");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints={4,5,7,9})
     void move() {
-        Car car = new Car("Benz");
         car.move(4);
         assertEquals(car.getPosition(), 1);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints={0,1,2,3})
     void stop() {
-        Car car = new Car("Benz");
         car.move(3);
         assertEquals(car.getPosition(), 0);
+    }
+
+    @Test
+    void isAtPosition() {
+        assertTrue(car.isAtPosition(0));
+        car.move(4);
+        assertTrue(car.isAtPosition(1));
     }
 }
