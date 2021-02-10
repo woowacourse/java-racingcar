0f47fe426f1e0418a4526a5f27e8637452f0f2ac
Sehwan_Jang <whilter08@gmail.com>
2021-02-10 15:04:27 +0900

    feat : `수정 6` RaceTrials 테스트 코드 생성

--- src/test/java/racingcar/utils/InputValidationTest.java
+++ src/test/java/racingcar/utils/InputValidationTest.java
@@ -1,11 +1,6 @@
 package racingcar.utils;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.utils.exception.InvalidTimeException;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidationTest {
 
@@ -15,10 +10,5 @@ class InputValidationTest {
         InputValidation.validateCars(validInput);   // 정상 처리 시 예외가 발생하지 않음
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"a", "-1", "0"})
-    public void 시도_횟수가_자연수가_아닌_경우_예외처리(String input) {
-        assertThatThrownBy(() -> InputValidation.validateTrials(input))
-                .isInstanceOf(InvalidTimeException.class);
-    }
+
 }
