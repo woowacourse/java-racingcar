9b593ae51dd57540e6504713683951c5b6d456c7
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-10 20:36:23 +0900

    feat(application): 사용하지 않은 패키지 제거

--- src/main/java/car/Application.java
+++ src/main/java/car/Application.java
@@ -1,8 +1,7 @@
 package car;
 
-import java.util.Scanner;
-
 public class Application {
+
     public static void main(String[] args) {
         GameController game = new GameController();
         game.start();
