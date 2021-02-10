10cb9c40a119dcf67cc531867ba12a541d49a446
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-10 21:40:31 +0900

    feat(name): 자동차 이름 예외처리 로직 분리

--- src/main/java/car/Name.java
+++ src/main/java/car/Name.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 public class Name {
 
     private static final int CAR_NAME_LIMIT = 5;
+    private static final String NAME_SPACE_ERROR = "자동차 이름에 공백은 불가능합니다.";
+    private static final String NAME_LENGTH_ERROR = "자동차 이름이 너무 깁니다.";
     private String name;
 
     public Name(String name) {
@@ -12,13 +14,30 @@ public class Name {
     }
 
     public String getName() {
-        isValidName(name);
+        isCarName(name);
         return name;
     }
 
-    private void isValidName(String name) {
-        if (!(name.length() <= CAR_NAME_LIMIT && !name.isEmpty())) {
-            throw new IllegalArgumentException();
+    private void isCarName(String name) {
+        try {
+            nameLengthCheck();
+            nameEmptyCheck();
+        } catch (IllegalArgumentException error) {
+            OutputView outputView = new OutputView();
+            outputView.errorPrint();
+        }
+
+    }
+
+    private void nameEmptyCheck() {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(NAME_SPACE_ERROR);
+        }
+    }
+
+    private void nameLengthCheck() {
+        if (name.length() > CAR_NAME_LIMIT) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
         }
     }
 
