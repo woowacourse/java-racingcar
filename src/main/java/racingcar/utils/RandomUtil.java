89ccbaa6e7c0e611156303f129ec0a2597b66577
TaewanKimmmm <ktww88@gmail.com>
2021-02-10 14:19:27 +0900

    refactor: MAXIMUM_BOUND 수정 및 불필요한 코드 제거

--- src/main/java/racingcar/utils/RandomUtil.java
+++ src/main/java/racingcar/utils/RandomUtil.java
@@ -3,25 +3,13 @@ package racingcar.utils;
 import java.util.Random;
 
 public class RandomUtil {
-    private static final int MAXIMUM_BOUND = 10;
+    private static final int MAXIMUM_BOUND = 9;
     private static final Random RANDOM = new Random();
 
     private RandomUtil() {
     }
 
-    public static int nextInt(final int startInclusive, final int endInclusive) {
-        if (startInclusive > endInclusive) {
-            throw new IllegalArgumentException();
-        }
-
-        if (startInclusive < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        if (startInclusive == endInclusive) {
-            return startInclusive;
-        }
-
+    public static int generateRandomNumber() {
         return RANDOM.nextInt(MAXIMUM_BOUND);
     }
 }
