7cd5a080d4a1b06958e19b530009a9d770d10411
sori <bosl95@naver.com>
2021-02-10 18:34:28 +0900

    style : 변수 이름(carList)에서 자료형(List)를 사용하지 않도록 "cars"로 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -6,31 +6,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
-    private List<Car> carList;
+    private List<Car> cars;
     private int maxPosition = 0;
 
     public Cars(String[] names) {
-        carList = new ArrayList<>();
-        this.carList.addAll(Arrays.stream(names)
+        cars = new ArrayList<>();
+        this.cars.addAll(Arrays.stream(names)
                 .map(name -> new Car(name, new RandomNumberRule()))
                 .collect(Collectors.toList()));
     }
 
     public void startRace() {
-        for (Car car : carList) {
+        for (Car car : cars) {
             car.move();
             maxPosition = car.aboveMaxPosition(maxPosition);
         }
     }
 
     public List<String> getStates() {
-        return carList.stream()
+        return cars.stream()
                 .map(car -> car.toString())
                 .collect(Collectors.toList());
     }
 
     public List<String> showWinners() {
-        return carList.stream()
+        return cars.stream()
                 .filter(car -> car.isMaxPosition(maxPosition))
                 .map(car -> car.getName())
                 .collect(Collectors.toList());
