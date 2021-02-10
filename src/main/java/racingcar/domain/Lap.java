8a0a296d5fee0c534f0296378ee410bd278fca5f
Hyeon9mak <jinha3507@gmail.com>
2021-02-10 15:57:00 +0900

    refactor: add defensive-copy in racingcar.domain.Lap)

--- src/main/java/racingcar/domain/Lap.java
+++ src/main/java/racingcar/domain/Lap.java
@@ -2,43 +2,45 @@ package racingcar.domain;
 
 public class Lap {
 
-    private static final int INIT_LAP = 0;
+    private static final int FINISH = 0;
     private static final int MINIMUM_LAP = 1;
-    private static final int UNIT_OF_ONE_LAP = 1;
+    private static final int LAP_UNIT = 1;
 
-    private final int goalLap;
-    private int currentLap;
+    private final int value;
 
-    private Lap(final String inputLap) {
-        this.goalLap = validatePositiveNumber(inputLap);
-        this.currentLap = INIT_LAP;
+    private Lap(final String value) {
+        this(Integer.parseInt(value));
     }
 
-    public static Lap initGoalLap(final String inputLap) {
-        return new Lap(inputLap);
+    private Lap(final int value){
+        this.value = value;
     }
 
-    private int validatePositiveNumber(final String inputLap) {
+    public static Lap initGoalLap(final String goalLap) {
+        validatePositiveNumber(goalLap);
+        return new Lap(goalLap);
+    }
+
+    private static void validatePositiveNumber(final String lap) {
         try {
-            return validatePositive(Integer.parseInt(inputLap));
+            validatePositive(Integer.parseInt(lap));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닌 입력입니다.");
         }
     }
 
-    private int validatePositive(final int numberLap) {
+    private static void validatePositive(final int numberLap) {
         if (numberLap < MINIMUM_LAP) {
             throw new IllegalArgumentException("1 미만의 입력입니다.");
         }
-        return numberLap;
     }
 
-    public void passOneLap() {
-        currentLap += UNIT_OF_ONE_LAP;
+    public Lap passOneLap() {
+        return new Lap(value - LAP_UNIT);
     }
 
     public boolean isFinish() {
-        return currentLap == goalLap;
+        return value == FINISH;
     }
 
 }
---
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -0,0 +1,2 @@
+package racingcar.domain.car;public class PositionTest {
+}
