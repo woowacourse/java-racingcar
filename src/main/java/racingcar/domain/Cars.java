308c5583ce235f52a2954e709a9e4c88992e0f86
sor <bosl95@naver.com>
2021-02-10 20:45:48 +0900

    refactor : data 패키지 import

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import racingcar.domain.data.Car;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.data.Car;
 import racingcar.utils.exception.InvalidNameLengthException;
 
 import static org.assertj.core.api.Assertions.assertThat;
