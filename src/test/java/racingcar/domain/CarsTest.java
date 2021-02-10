b50f980b69a08658bb64078e4aac623702f8a4ea
jaeseongDev <qkrwotjd1445@naver.com>
2021-02-10 23:59:28 +0900

    (test) : Cars 클래스에서 입력받은 값에 따라서 자동차가 정상적으로 움직이는 지 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,8 +1,10 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,5 +32,40 @@ public class CarsTest {
         assertThatThrownBy(() -> {
             Cars.from(cars);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("정상적으로 입력받은 값에 따라 자동차가 움직이는 지 테스트")
+    public void tryToMove() {
+        // given
+        Cars cars = Cars.from(Arrays.asList(
+            Car.from("톰"),
+            Car.from("제리"),
+            Car.from("포비"),
+            Car.from("제이슨")
+        ));
+        List<Integer> values = new ArrayList<>();
+        values.add(2);
+        values.add(3);
+        values.add(4);
+        values.add(5);
+
+        List<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(0);
+        expectedPositions.add(0);
+        expectedPositions.add(1);
+        expectedPositions.add(1);
+
+        // when
+        cars.tryToMove(values);
+
+        // then
+        Result result = cars.getResult();
+        List<CarState> carStates = result.getCarStates();
+        for (int i = 0; i < carStates.size(); i++) {
+            int actualPosition = carStates.get(i).getPosition();
+            int expectedPosition = expectedPositions.get(i);
+            assertThat(actualPosition).isEqualTo(expectedPosition);
+        }
     }
 }
