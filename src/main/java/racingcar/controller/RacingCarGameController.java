8670e21af3567c2a85589b07800c84ff0b6dafeb
Hyeon9mak <jinha3507@gmail.com>
2021-02-10 16:05:04 +0900

    refactor: reduce race method in racingcar.controller.RacingCarGameController

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -3,38 +3,32 @@ package racingcar.controller;
 import racingcar.domain.Lap;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
-import racingcar.utils.RandomUtils;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingCarGameController {
 
-    private static final int MINIMUM_GAS = 0;
-    private static final int MAXIMUM_GAS = 9;
-
     public void playGame() {
         Cars cars = Cars.enrollCarsWithNames(InputView.inputCarNames());
         Lap lap = Lap.initGoalLap(InputView.inputLap());
-
         OutputView.printStartRace();
-        racing(cars, lap);
-        OutputView.printWinners(cars.getWinners());
+        OutputView.printWinners(racing(cars, lap).findWinners());
     }
 
-    private void racing(Cars cars, Lap lap) {
+    private Cars racing(Cars cars, Lap lap) {
         while (!lap.isFinish()) {
-            passOneLap(cars, lap);
+            cars = cars.passOneLap();
+            lap = lap.passOneLap();
+            printCarInfo(cars);
+            OutputView.printPassOneLap();
         }
+        return cars;
     }
 
-    private void passOneLap(Cars cars, Lap lap) {
+    private void printCarInfo(Cars cars) {
         for (Car car : cars.getCars()) {
-            car.fillUpGas(RandomUtils.nextPositiveInt(MINIMUM_GAS, MAXIMUM_GAS));
-            car.forward();
             OutputView.printCarInfo(car.getName(), car.getPosition());
         }
-        lap.passOneLap();
-        OutputView.printPassOneLap();
     }
 
 }
