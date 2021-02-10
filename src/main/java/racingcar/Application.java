a1e1728a80e6f5ec11363cd9e8c447ba7cf3bb8a
yoonsung.jung <ybook2012@gmail.com>
2021-02-10 17:22:41 +0900

    refactor : 클래스 이름 변경으로 인해 메소드 호출방식 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        RacingCar racingCar = new RacingCar();
-        racingCar.run();
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.run();
     }
 }
