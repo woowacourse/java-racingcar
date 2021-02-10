4bcc3de21e406ade5d5697829ca70f33cd3d87a4
TaewanKimmmm <ktww88@gmail.com>
2021-02-10 12:23:41 +0900

    test: 자동차 이름 문자열 분리 테스트

--- src/test/java/racingcar/SplitTest.java
+++ src/test/java/racingcar/SplitTest.java
@@ -1,15 +1,25 @@
 package racingcar;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.utils.SplitUtil;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("자동차 이름 문자열 분리 테스트")
 public class SplitTest {
 
+    @DisplayName("콤마를 포함하지 않는 문자열 테스트")
     @Test
-    void split() {
-        assertEquals(SplitUtil.splitCarNames("car,papi,  bet, pobi").size(), 4);
+    void nonCommaTest() {
+        assertEquals(SplitUtil.splitCarNames("asdfasdf").size(), 1);
+    }
+
+    @DisplayName("콤마 뒤에 공백을 포함하는 문자열 테스트")
+    @Test
+    void spaceTest() {
+        assertEquals(SplitUtil.splitCarNames("car, papi, bet, pobi"),
+                SplitUtil.splitCarNames("car,papi,bet,pobi"));
     }
 }
