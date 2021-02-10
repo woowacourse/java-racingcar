3a9429f769cd812af6fdbd20efd871bbfbd84322
Eugine Park <pkeugine@gmail.com>
2021-02-11 00:10:05 +0900

    feat: update MainController

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -1,62 +1,53 @@
 package racingcar.controller;
 
-import static racingcar.utils.ValidateUtils.validateTurnQuantity;
-import static racingcar.utils.ValidateUtils.validateUniqueCarNames;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import racingcar.domain.car.Car;
-import racingcar.domain.car.CarRepository;
+import racingcar.domain.car.CarNames;
+import racingcar.domain.car.Cars;
+import racingcar.domain.car.WinnerCars;
 import racingcar.domain.game.Game;
-import racingcar.utils.ErrorUtils;
-import racingcar.view.MainPage;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class MainController {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private Game game;
+    private Cars cars;
 
     public void run() {
-        userInputNames();
-        userInputTurnQuantity();
+        initializeGame();
+        play();
     }
 
-    private void userInputNames() {
+    private void initializeGame() {
+        registerCars();
+        registerRounds();
+    }
+
+    private void registerCars() {
         try {
-            MainPage.printMainPage();
-            String userInput = scanner.nextLine();
-            registerCars(userInput);
+            CarNames carNames = CarNames.create(InputView.inputNames());
+            cars = new Cars(carNames);
         } catch (IllegalArgumentException e) {
-            ErrorUtils.printError(e);
-            userInputNames();
+            OutputView.printError(e);
+            registerCars();
         }
     }
 
-    private void userInputTurnQuantity() {
+    private void registerRounds() {
         try {
-            MainPage.askTurnQuantity();
-            String userInput = scanner.nextLine();
-            initiateGameWithTurn(userInput);
+            game = new Game(InputView.inputRound());
         } catch (IllegalArgumentException e) {
-            ErrorUtils.printError(e);
-            userInputTurnQuantity();
+            OutputView.printError(e);
+            registerRounds();
         }
     }
 
-    private void registerCars(String names) {
-        List<String> carNames = Arrays.asList(names.split(","));
-        validateUniqueCarNames(carNames);
-
-        List<Car> cars = carNames.stream()
-                .map(Car::new)
-                .collect(Collectors.toList());
-        CarRepository.addCars(cars);
-    }
-
-    private void initiateGameWithTurn(String userInput) {
-        int turnQuantity = validateTurnQuantity(userInput);
-        GameController gameController = new GameController();
-        gameController.startGame(new Game(turnQuantity));
+    private void play() {
+        OutputView.printResultTitle();
+        while (game.notFinished()) {
+            cars.moveIndividuals();
+            game.play();
+            OutputView.printRoundResult(cars);
+        }
+        OutputView.printFinalResult(new WinnerCars(cars));
     }
 }
