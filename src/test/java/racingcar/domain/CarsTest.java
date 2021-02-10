8a91a32c3b9f01e7f40f4db54c7d9a086f53d140
jujubebat <bbwwpark@naver.com>
2021-02-10 16:24:34 +0900

    refactor: 테스트 픽스처 생성 로직 구현

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -4,27 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 
+    private List<Name> names;
+    private List<Position> positions;
+
+    @BeforeEach
+    public void setUp() {
+        names = Arrays.asList(new Name("test1"), new Name("test2"), new Name("test3"));
+        positions = Arrays.asList(new Position(7), new Position(11), new Position(4));
+    }
+
     @Test
     @DisplayName("우승자 선정이 잘 되는지 테스트")
     public void check_get_winners() {
-        List<Name> names = Arrays.asList(
-            new Name("test1"),
-            new Name("test2"),
-            new Name("test3")
-        );
-
-        List<Position> positions = Arrays.asList(
-            new Position(8),
-            new Position(1),
-            new Position(3)
-        );
         Cars cars = new Cars(names, positions);
-        assertThat(cars.getWinners().get(0).getName()).isEqualTo("test1");
+        assertThat(cars.getWinners().get(0).getName()).isEqualTo("test2");
     }
 
 }
