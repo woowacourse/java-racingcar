1e4eedc196c79941efd27188210ce93ceb6bf14e
TaewanKimmmm <ktww88@gmail.com>
2021-02-10 17:29:47 +0900

    test: DisplayName 추가 및 경계값 주변에서 테스트하도록 수정

--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,21 +1,24 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("자동차 이름 길이 테스트")
 public class CarNameTest {
-
+    @DisplayName("5글자를 초과하는 자동차 이름 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"sdlfelsdkg"})
+    @CsvSource(value = {"abcdef"})
     void checkNameLength(String value) {
         assertThatThrownBy(() -> {
             CarName carName = new CarName(value);
         }).isInstanceOf(RuntimeException.class);
     }
 
+    @DisplayName("빈 문자열 자동차 이름 테스트")
     @ParameterizedTest
     @EmptySource
     void checkNameLength_empty(String value) {
@@ -23,5 +26,4 @@ public class CarNameTest {
             CarName carName = new CarName(value);
         }).isInstanceOf(RuntimeException.class);
     }
-
 }
