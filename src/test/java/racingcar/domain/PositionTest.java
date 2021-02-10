c50ae97aeb44fe284e6a7997739abc2215876b0c
y_woo :) <dusdn1702@naver.com>
2021-02-10 16:49:50 +0900

    test: Position 테스트 코드 생성

---
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositionTest {
+    @Test
+    @DisplayName("position 생성 확인")
+    void position_create() {
+        Position position = new Position();
+        assertEquals(position.getPosition(), 0);
+    }
+
+    @Test
+    @DisplayName("position 이동 확인")
+    void position_move() {
+        Position position = new Position();
+        position = position.moveForward();
+        assertEquals(position.getPosition(), 1);
+    }
+
+    @Test
+    @DisplayName("position 같은 위치 확인")
+    void position_isSameThan() {
+        Position position = new Position();
+        assertTrue(position.isSameThan(0));
+    }
+
+    @Test
+    @DisplayName("더 큰 위치 숫자 반환")
+    void position_getBiggerPosition() {
+        Position position = new Position();
+        assertThat(position.findBiggerPosition(3)).isEqualTo(3);
+    }
+}
