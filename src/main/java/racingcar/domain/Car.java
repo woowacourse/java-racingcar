b7cdca918d74f0d5321f0bf06c572e39b4b94f13
Minjeong <mathmjseo@naver.com>
2021-02-10 11:28:13 +0900

    refactor: Car 내부 생성자 추가 및 move 메서드 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,28 +1,37 @@
 package racingcar.domain;
 
+import static racingcar.domain.Position.ZERO;
+
 public class Car {
     private static final String MARKER_BETWEEN_NAME_AND_POSITION = " : ";
     private static final String MARKER_OF_POSITION = "-";
     private static final int MOVING_BASELINE = 4;
     private static final int INITIAL_POSITION = 0;
 
-    private final Name name;
-    private int position;
+    private Name name;
+    private Position position;
 
-    public Car(Name name) {
+    public Car(final String name, final int position) {
+        this(new Name(name), new Position(position));
+    }
+
+    public Car(final Name name, final Position position) {
         this.name = name;
-        this.position = INITIAL_POSITION;
+        this.position = position;
+    }
+
+    public Car(final Name name) {
+        this(name, ZERO);
     }
 
     public String getName() {
         return this.name.getName();
     }
 
-    public int movePosition(int value) {
-        if (value >= MOVING_BASELINE) {
-            position++;
+    public void move(int number) {
+        if (number >= MOVING_BASELINE) {
+            position.move();
         }
-        return position;
     }
 
     public String getCurrentPosition() {
