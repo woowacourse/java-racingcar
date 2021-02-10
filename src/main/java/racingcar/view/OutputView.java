aa532bc5989c6797f5b176f9160751049b40c99e
Hyeon9mak <jinha3507@gmail.com>
2021-02-10 16:08:17 +0900

    refactor: add 'toString' in racingcar.view.OutputView

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String POSITION_UNIT = "-";
+    private static final String DELIMITER = ", ";
+
     public static void printStartRace() {
         System.out.println(System.lineSeparator() + "실행 결과");
     }
 
-    public static void printCarInfo(String name, String position) {
-        System.out.println(name + " : " + position);
+    public static void printCarInfo(String name, int position) {
+        System.out.println(name + " : " + toString(position));
+    }
+
+    private static String toString(int position) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            stringBuilder.append(POSITION_UNIT);
+        }
+        return stringBuilder.toString();
     }
 
     public static void printPassOneLap() {
@@ -17,7 +28,7 @@ public class OutputView {
     }
 
     public static void printWinners(List<String> winnersName) {
-        System.out.println(String.join(", ", winnersName) + "가 최종 우승했습니다.");
+        System.out.println(String.join(DELIMITER, winnersName) + "가 최종 우승했습니다.");
     }
 
 }
