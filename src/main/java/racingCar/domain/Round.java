e40996215f38d484eabad949b5ca5c286cc4624f
RinSabbit <celena1004@naver.com>
2021-02-10 23:46:52 +0900

    feat: Rount TC 기능 구현

--- src/main/java/racingCar/domain/Round.java
+++ src/main/java/racingCar/domain/Round.java
@@ -1,5 +1,6 @@
 package racingCar.domain;
 
+import java.util.Objects;
 import racingCar.exception.InvalidNumOfMoveException;
 
 public class Round {
@@ -20,5 +21,22 @@ public class Round {
 
     public boolean isEnd() {
         return round == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Round round1 = (Round) o;
+        return round == round1.round;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(round);
     }
 }
--- src/test/java/racingCar/utils/ValidateUtilsTest.java
+++ src/test/java/racingCar/utils/ValidateUtilsTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -28,19 +27,12 @@ class ValidateUtilsTest {
 
     @DisplayName("시도횟수의 형식이 올바르지 않은 경우")
     @ParameterizedTest
-    @ValueSource(strings = {"-2", "d"})
+    @ValueSource(strings = {"d"})
     @NullAndEmptySource
     void invalidRoundInput(String invalidInput) {
         assertThatThrownBy(() -> {
-            ValidateUtils.validateRacingRoundCount(invalidInput);
+            ValidateUtils.validateInteger(invalidInput);
         }).isInstanceOf(RuntimeException.class);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"1:1", "2:2", "3:3", "4:4", "5:5"}, delimiter = ':')
-    public void 시도횟수_성공케이스(String input, String expected) {
-        assertThat(ValidateUtils.validateRacingRoundCount(input))
-            .isEqualTo(Integer.parseInt(expected));
     }
 
 }
