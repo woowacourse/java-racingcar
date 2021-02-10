c5ca9bad14eb8db6fe8508124e7c6a587e26bad1
cjy17 <cjy1705@naver.com>
2021-02-10 11:06:30 +0900

    feat : 유효성 검사(1~5 사이의 길이 체크) 구현

--- src/main/java/racingCar/Name.java
+++ src/main/java/racingCar/Name.java
@@ -5,7 +5,14 @@ public class Name {
     private String name;
 
     public Name(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if(name.length() < 1 || name.length() > 5){
+            throw new IllegalArgumentException("길이가 1~5사이의 이름을 입력해주세요.");
+        }
     }
 
     @Override
--- src/test/java/racingCar/NameTest.java
+++ src/test/java/racingCar/NameTest.java
@@ -1,17 +1,31 @@
 package racingCar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameTest {
 
     @Test
-    public void name_length() {
+    public void name_equals() {
         Name name = new Name("aaa");
 
         assertThat(name).isEqualTo(new Name("aaa"));
+    }
+
+    @Test
+    @DisplayName("자동차 이름 유효성 검사")
+    public void name_length() {
+        assertThatThrownBy(()->{
+            new Name("abcdefg");
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()->{
+            new Name("");
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
