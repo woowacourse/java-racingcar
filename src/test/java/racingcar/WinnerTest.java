947a114ecb7304f4499210f904e7805f8bd332ba
TaewanKimmmm <ktww88@gmail.com>
2021-02-10 17:42:27 +0900

    style: 코드 정렬

--- src/test/java/racingcar/WinnerTest.java
+++ src/test/java/racingcar/WinnerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,7 +18,7 @@ public class WinnerTest {
         a.moveForwardByNumber(5);
         b.moveForwardByNumber(5);
 
-        Cars cars = new Cars(Arrays.asList(a,b,c));
+        Cars cars = new Cars(Arrays.asList(a, b, c));
         assertEquals(cars.getWinners().size(), 2);
     }
 }
