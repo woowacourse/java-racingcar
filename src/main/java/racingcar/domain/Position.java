b1d792960766c0e49b31b1f705180f1c69be6995
Minjeong <mathmjseo@naver.com>
2021-02-10 11:30:07 +0900

    feat: Position 객체 추가

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Position {
+    public static final Position ZERO = new Position(0);
+
+    private int value;
+
+    public Position(int value) {
+        this.value = value;
+    }
+
+    public void move() {
+        value++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
