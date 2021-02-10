020848651bad98c4e5f805549d777dfd054a786c
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-10 15:28:52 +0900

    test: 위치값이 음수일 때 에러 발생 테스트 코드 추가

--- src/test/java/racingcar/domain/AttemptNumberTest.java
+++ src/test/java/racingcar/domain/AttemptNumberTest.java
@@ -9,9 +9,9 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class AttemptNumberTest {
 
+    @DisplayName("양의 정수가 아닐 때 AttemptNumber 객체 생성 시 에러 발생하는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {0, -1, -2, -3})
-    @DisplayName("양의 정수가 아닐 때 AttemptNumber 객체 생성 시 에러 발생하는지 테스트")
     void attemptsNumberErrorTest(int number) {
         // 에러가 잘 출력되는지 테스트
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -20,9 +20,9 @@ public class AttemptNumberTest {
                 });
     }
 
+    @DisplayName("양의 정수일 때 AttemptNumber 객체 생성되는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 5, 8, 10})
-    @DisplayName("양의 정수일 때 AttemptNumber 객체 생성되는지 테스트")
     void attemptsNumberTest(int number) {
         // 에러가 출력이 안되고 잘 동작하는지 테스트
         AttemptNumber attemptNumber = new AttemptNumber(number);
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -11,42 +11,51 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
+    @DisplayName("이름이 5글자 이하일 때 Car 객체가 생성되는지 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"benz", "bmw", "suv"})
-    @DisplayName("이름이 5글자 이하일 때 Car 객체가 생성되는지 테스트")
     void carNameTest(String text) {
         Car car = new Car(text);
         assertThat(car.getName()).isEqualTo(text);
     }
 
+    @DisplayName("이름이 6글자 이상일 때 Car 객체 생성 시 에러 발생하는지 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"grandeur", "sportsCar", "sonata"})
-    @DisplayName("이름이 6글자 이상일 때 Car 객체 생성 시 에러 발생하는지 테스트")
     void carNameLengthErrorTest(String text) {
         assertThatThrownBy(() -> {
             new Car(text);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("위치값이 음수 일때 에러 발생하는지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
+    void negativeNumberPositionErrorTest(int number) {
+        assertThatThrownBy(() -> {
+            new Car("temp", number);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("랜덤 값이 4이상 일때 자동차 위치가 변하는지 테스트")
     @ParameterizedTest
     @CsvSource(value = {"9:1", "3:0", "4:1"}, delimiter = ':')
-    @DisplayName("랜덤 값이 4이상 일때 자동차 위치가 변하는지 테스트")
     void randomNumberPositionTest(int input, int expected) {
         Car car = new Car("temp");
         car.move(() -> input);
         assertThat(car.getPosition()).isEqualTo(expected);
     }
 
+    @DisplayName("위치값이 문자열로 잘 변환되는지 테스트")
     @ParameterizedTest
     @CsvSource(value = {"3:---", "2:--", "5:-----"}, delimiter = ':')
-    @DisplayName("위치값이 문자열로 잘 변환되는지 테스트")
     void carPositionToStringTest(int input, String expected) {
         Car car = new Car("temp", input);
         assertThat(OutputView.positionToHyphen(car)).isEqualTo(expected);
     }
 
-    @Test
     @DisplayName("범위에 맞는 랜덤 값이 생성하는지 테스트")
+    @Test
     void randomValueRangeTest() {
         RandomValueGenerator randomValueGenerator = new RandomValueGenerator();
         for (int i = 0; i < 10000; i++) {
