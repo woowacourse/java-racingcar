e8904fbe9542fa6eb81d074cc8d1ae7fc2ca443e
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-10 21:17:53 +0900

    feat(name): 접근제어자 재설정, 컨벤션에 맞도록 순서 재설정

--- src/main/java/car/Name.java
+++ src/main/java/car/Name.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class Name {
 
+    private static final int CAR_NAME_LIMIT = 5;
     private String name;
-    final static int CAR_NAME_LIMIT = 5;
 
     public Name(String name) {
         this.name = name;
