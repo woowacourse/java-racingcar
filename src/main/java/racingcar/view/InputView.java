eb7c21b27f16b03fcda11f4dd956d2d19d371f6d
sor <bosl95@naver.com>
2021-02-10 23:33:21 +0900

    refactor : InputView는 오직 입력만 받도록 리팩토링

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,21 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
-    private static String BLANK = " ";
-    private static String EMPTY = "";
-    private static String DELIMITER = ",";
 
     private InputView() {
     }
 
-    public static String[] inputCarName() {
-        String[] name = scanner.nextLine()
-                .replace(BLANK, EMPTY)
-                .split(DELIMITER);
-        return name;
-    }
-
-    public static String inputTime() {
+    public static String input() {
         return scanner.nextLine();
     }
 }
