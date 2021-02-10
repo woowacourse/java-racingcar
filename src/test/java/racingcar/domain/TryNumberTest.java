8f6a12c6075b76bf41734df53a9485a91329d281
jujubebat <bbwwpark@naver.com>
2021-02-10 16:25:45 +0900

    style: 불필요한 공백 제거

--- src/test/java/racingcar/domain/TryNumberTest.java
+++ src/test/java/racingcar/domain/TryNumberTest.java
@@ -12,7 +12,7 @@ public class TryNumberTest {
     @ValueSource(ints = {0, -1, -2})
     @DisplayName("시도 횟수가 0보다 작을 경우 예외처리 테스트")
     public void check_position_exception(int tryNumber) {
-        assertThatThrownBy(() ->  new TryNumber(tryNumber))
+        assertThatThrownBy(() -> new TryNumber(tryNumber))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("[Error] 시도할 횟수는 0보다 커야합니다.\n");
     }
