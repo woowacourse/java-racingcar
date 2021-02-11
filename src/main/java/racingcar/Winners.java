05d0abab8bb03ac4fe4bcbf4a6df2dd63ef54c08
Solji Choi <bssj9307@gmail.com>
2021-02-12 00:35:12 +0900

    refactor: WinnerFinder -> Winners 리네이밍 - 우승자를 찾을 때 반복문에서 filter 사용으로 변경

---
+++ src/main/java/racingcar/Winners.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winners {
+
+    private final List<Car> cars;
+    private final int maxPosition;
+    private List<String> winners = new ArrayList<>();
+
+    public Winners(List<Car> cars, int maxPosition) {
+        this.cars = cars;
+        this.maxPosition = maxPosition;
+        findWinner();
+    }
+
+    public List<String> getWinners() {
+        return winners;
+    }
+
+    private void findWinner() {
+        winners = cars.stream()
+                .filter(car -> isWinner(car))
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isWinner(Car car) {
+        return car.isPosition(maxPosition);
+    }
+}
