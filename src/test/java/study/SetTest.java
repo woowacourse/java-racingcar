0536a6c38ac71b5d7674130ec0d0f7f779d96f9e
Eugine Park <pkeugine@gmail.com>
2021-02-11 14:33:56 +0900

    refactor: add display names to SetTest and simplify method

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,21 +26,23 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("set 사이즈 출력")
     void size() {
         assertEquals(numbers.size(), 3);
     }
 
     @ParameterizedTest
+    @DisplayName("set 삽입 후 존재 확인")
     @ValueSource(ints = {1, 2, 3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
     }
 
     @ParameterizedTest
+    @DisplayName("set 삽입하지 않은 값 존재 유무 확인")
     @CsvSource(value = {"1:True", "2:True", "3:True", "4:False", "5:False"}, delimiter = ':')
-    void trueOrFalse(String input, String expected) {
-        boolean check = numbers.contains(Integer.parseInt(input));
-        boolean actual = Boolean.parseBoolean(expected);
-        assertEquals(check, actual);
+    void trueOrFalse(int input, boolean actual) {
+        boolean check = numbers.contains(input);
+        assertEquals(actual, check);
     }
 }
