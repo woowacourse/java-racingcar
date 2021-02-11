7f241111b07570e85c17c06deb0b9590343de2f6
DWL5 <ssop6403@gmail.com>
2021-02-11 18:45:01 +0900

    refactor: Message의 message final로 변경

--- src/main/java/racingcar/view/Messages.java
+++ src/main/java/racingcar/view/Messages.java
@@ -4,7 +4,7 @@ public enum Messages {
     REQUEST_CAR_NAME("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."),
     REQUEST_MOVE_COUNT("시도할 회수는 몇회인가요?");
 
-    private String message;
+    private final String message;
 
     Messages(String message) {
         this.message = message;
