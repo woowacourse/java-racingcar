602567b368a61d5fb248be27ed1fd3cb986197ae
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-11 18:03:09 +0900

    refactor: Car 인스턴스 변수 distance를 Distance 클래스로 포장

---
+++ src/main/java/racingcar/model/Distance.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public class Distance {
+    private int distance;
+
+    public Distance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+}
