0392689f3de4da36456a820950bb11bdd6fd0dd3
mungto <daum7766@naver.com>
2021-02-11 11:38:04 +0900

    refactor(OutputView): create private constuctor and change function name

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,11 +2,14 @@ package racingcar.view;
 
 public class OutputView {
 
+    private OutputView() {
+    }
+
     public static void printInputCarNamesMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
-    public static void printInputAttemptNumberMessage() {
+    public static void printInputTryNumberMessage() {
         System.out.println("시도할 횟수는 몇회인가요?");
     }
 
