886b46e7d8a34487b7b595389cf98668b5a9d564
DWL5 <ssop6403@gmail.com>
2021-02-11 19:06:12 +0900

    refacotr: 에러메시지 통일성을 위한 [ERROR] prefix 적용

--- src/main/java/racingcar/exception/CarNameDuplicateException.java
+++ src/main/java/racingcar/exception/CarNameDuplicateException.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public class CarNameDuplicateException extends RuntimeException {
-    private static String MESSAGE = "[ERROR]자동차 이름은 중복을 허용하지 않습니다.";
+    private static String MESSAGE = "[ERROR] 자동차 이름은 중복을 허용하지 않습니다.";
 
     public CarNameDuplicateException() {
         super(MESSAGE);
