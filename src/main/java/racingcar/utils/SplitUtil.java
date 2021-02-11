c9db0ef180aa5e0eacea1d247535a38bcc5a6f62
TaewanKimmmm <ktww88@gmail.com>
2021-02-11 15:51:02 +0900

    refactor: 하드 코딩 수정

--- src/main/java/racingcar/utils/SplitUtil.java
+++ src/main/java/racingcar/utils/SplitUtil.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SplitUtil {
+    private static final String SPACE = " ";
+    private static final String BLANK = "";
+    private static final String COMMA = ",";
 
     public static List<String> splitCarNames(String carNamesInput) {
-        return Arrays.asList(carNamesInput.replace(" ", "").split(","));
+        return Arrays.asList(carNamesInput.replace(SPACE, BLANK).split(COMMA));
     }
 }
