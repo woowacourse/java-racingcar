e7afa056652c7765df014f181ccc7222a848eba9
seunghyun <ggyool@naver.com>
2021-02-11 19:06:05 +0900

    feat(Name): Name 클래스 생성

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,61 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static racingcar.domain.Name.LENGTH_ERROR_MESSAGE;
+import static racingcar.domain.Name.MAXIMUM_LENGTH;
+import static racingcar.domain.Name.NULL_ERROR_MESSAGE;
+
+class NameTest {
+    @DisplayName("객체 생성 성공")
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "brown", "json", "중간곰"})
+    public void create_success(final String value) {
+        assertThatCode(() -> new Name(value))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("이름 길이가 길어 객체 생성 실패")
+    @Test
+    public void create_fail_long() {
+        final String longName = new String(new char[MAXIMUM_LENGTH + 1]);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name(longName))
+                .withMessageContaining(LENGTH_ERROR_MESSAGE);
+    }
+
+    @DisplayName("이름 길이가 짧아 객체 생성 실패")
+    @Test
+    public void create_fail_short() {
+        final String shortName = "";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name(shortName))
+                .withMessageContaining(LENGTH_ERROR_MESSAGE);
+    }
+
+    @DisplayName("이름의 값이 null이면 객체 생성 실패")
+    @Test
+    public void create_fail_null() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name(null))
+                .withMessageContaining(NULL_ERROR_MESSAGE);
+    }
+
+    @DisplayName("동등 비교")
+    @Test
+    public void equals() {
+        final String value = "중간곰";
+        final Name name = new Name(value);
+
+        assertThat(name)
+                .isEqualTo(new Name(value));
+    }
+}
