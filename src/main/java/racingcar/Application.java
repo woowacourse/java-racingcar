fd846a6b7de578a46e254049b20da848d825550c
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-11 14:31:30 +0900

    refactor: 게임 정보를 controller에서 받도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,14 @@
 package racingcar;
 
 import racingcar.controller.RacingGameController;
-import racingcar.model.Cars;
-import racingcar.view.InputView;
 
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Cars carsInGame = InputView.getUserCarInput(scanner);
-        int trialNumber = InputView.getUserTrialNumberInput(scanner);
+        final Scanner scanner = new Scanner(System.in);
 
         RacingGameController racingGame = new RacingGameController();
-        racingGame.play(carsInGame, trialNumber);
+        racingGame.play(scanner);
     }
 }
--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,25 +1,38 @@
+
 package racingcar.controller;
 
 import racingcar.model.Car;
 import racingcar.model.Cars;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class RacingGameController {
-    public void play(final Cars carsInGame, final int trialNumber) {
-        OutputView.printOutputMessage();
-        for (int i = 0; i < trialNumber; i++) {
-            printSingleTrialResult(carsInGame.singleTrial());
-        }
-        OutputView.printWinner(carsInGame.getWinner());
+    public void play(Scanner scanner) {
+        final Cars carsInGame = InputView.getUserCarInput(scanner);
+        final int trialNumber = InputView.getUserTrialNumberInput(scanner);
+
+        playEveryTrial(carsInGame, trialNumber);
+        announceWinner(carsInGame);
     }
 
-    private void printSingleTrialResult(final List<Car> carsAfterSingleTrial) {
+    private void playEveryTrial(final Cars carsInGame, final int trialNumber) {
+        OutputView.printOutputMessage();
+        for (int i = 0; i < trialNumber; i++) {
+            playSingleTrial(carsInGame.singleTrial());
+        }
+    }
+
+    private void playSingleTrial(final List<Car> carsAfterSingleTrial) {
         for (Car car : carsAfterSingleTrial) {
             OutputView.printCarInformation(car.getName(), car.getDistance());
         }
         OutputView.printNewLine();
     }
-}
 
+    private void announceWinner(Cars carsInGame) {
+        OutputView.printWinner(carsInGame.getWinner());
+    }
+}
