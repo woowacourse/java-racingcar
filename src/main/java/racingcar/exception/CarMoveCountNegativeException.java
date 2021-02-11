195df77377ef77a889c1774f617a201781d70a1c
DWL5 <ssop6403@gmail.com>
2021-02-11 19:06:12 +0900

    refacotr: 에러메시지 통일성을 위한 [ERROR] prefix 적용

--- src/main/java/racingcar/exception/CarMoveCountNegativeException.java
+++ src/main/java/racingcar/exception/CarMoveCountNegativeException.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public class CarMoveCountNegativeException extends RuntimeException {
-    private static final String MESSAGE = "이동횟수는 양의 정수로 입력되어야 합니다.";
+    private static final String MESSAGE = "[ERROR] 이동횟수는 양의 정수로 입력되어야 합니다.";
 
     public CarMoveCountNegativeException() {
         super(MESSAGE);
--- src/main/java/racingcar/exception/CarNameBlankOrEmptyException.java
+++ src/main/java/racingcar/exception/CarNameBlankOrEmptyException.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public class CarNameBlankOrEmptyException extends RuntimeException {
-    private static String MESSAGE = "[ERROR]자동차 이름으로는 빈값이 들어오면 안된다.";
+    private static String MESSAGE = "[ERROR] 자동차 이름으로는 빈값이 들어오면 안됩니다.";
 
     public CarNameBlankOrEmptyException() {
         super(MESSAGE);
