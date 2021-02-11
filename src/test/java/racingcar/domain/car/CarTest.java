43ab6af6f80a75883d0ab6ce59edffd590f11b6d
Minsung Son <sns2831@gmail.com>
2021-02-11 18:46:56 +0900

    fix(racingcar): 자동차의 움직임 메소드 누락

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -10,9 +10,11 @@ public class CarTest {
     @DisplayName("랜덤 값이 4 이상일 경우 전진하고 3 이하의 값이면 멈춘다.")
     public void move_랜덤_값이_4_이상일_경우_전진하고_3_이하의_값이면_멈춘다() {
         Car car = new Car("pobi", i -> 6);
+        car.move();
         assertTrue(car.isSamePosition(1));
 
         car = new Car("pobi", i -> 3);
+        car.move();
         assertTrue(car.isSamePosition(0));
     }
 }
