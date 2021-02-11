aaaf93f6621aaa1606d53811ecb70a1e9c4cf4ff
DWL5 <ssop6403@gmail.com>
2021-02-11 18:41:12 +0900

    style: CarNameBlankOrEmptyException 코딩 컨벤션 수정

--- src/main/java/racingcar/exception/CarNameBlankOrEmptyException.java
+++ src/main/java/racingcar/exception/CarNameBlankOrEmptyException.java
@@ -1,6 +1,6 @@
 package racingcar.exception;
 
-public class CarNameBlankOrEmptyException extends RuntimeException{
+public class CarNameBlankOrEmptyException extends RuntimeException {
     private static String MESSAGE = "[ERROR]자동차 이름으로는 빈값이 들어오면 안된다.";
 
     public CarNameBlankOrEmptyException() {
