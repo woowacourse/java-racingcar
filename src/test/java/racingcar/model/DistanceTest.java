0bbfc956c65a60c55cdc9f5e8c6d8ae468b0262f
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-02-11 21:40:38 +0900

    test(DistanceTest): Distance 클래스의 public 메서드 테스트

--- src/test/java/racingcar/model/DistanceTest.java
+++ src/test/java/racingcar/model/DistanceTest.java
@@ -1,13 +1,31 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class DistanceTest {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -100})
+    public void Name_생성자_테스트(int invalidDistance) {
+        assertThatThrownBy(() -> new Distance(invalidDistance))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error] 거리는 0 미만으로 초기화 할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5,10,15})
+    public void Name_getDistance_테스트(int testDistance) {
+        Distance distance = new Distance(testDistance);
+        assertThat(testDistance == distance.getDistance()).isTrue();
+    }
+
     @Test
     public void Distance_equals_테스트() {
         Distance test1 = new Distance(3);
