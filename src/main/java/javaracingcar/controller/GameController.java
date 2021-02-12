a2acbf9dd3fd6462ca613bc583394a369fe8bf73
yjksw <cairofaith@gmail.com>
2021-02-12 23:37:11 +0900

    refactor(GameController.java): run() 메소드 분리

--- src/main/java/javaracingcar/controller/GameController.java
+++ src/main/java/javaracingcar/controller/GameController.java
@@ -9,21 +9,17 @@ public class GameController {
     public static void startGame() {
         String carNames = InputView.receiveCarNamesFromUser();
         String trial = InputView.receiveTrialFromUser();
-        validateNotNull(carNames);
-        validateNotNull(trial);
-        GameController.run(carNames, trial.replaceAll(" ", ""));
-    }
-
-    private static void validateNotNull(String value) {
-        if (value == null) {
-            throw new IllegalArgumentException("null 이 입력되었습니다.");
-        }
+        run(carNames, trial.replaceAll(" ", ""));
     }
 
     private static void run(String carNames, String trial) {
-        Game game = Game.init(carNames, trial);
+        Game game = initializeGame(carNames, trial);
         race(game);
-        OutputView.printWinners(game.getCars().getWinners());
+        printWinner(game);
+    }
+
+    private static Game initializeGame(String carNames, String trial) {
+        return Game.init(carNames, trial);
     }
 
     private static void race(Game game) {
@@ -37,5 +33,9 @@ public class GameController {
     private static void playOneRound(Game game) {
         game.moveCars();
         OutputView.printRoundResult(game.getCars());
+    }
+
+    private static void printWinner(Game game) {
+        OutputView.printWinners(game.getCars().getWinners());
     }
 }
