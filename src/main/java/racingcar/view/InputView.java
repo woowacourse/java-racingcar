c0bd991b0f1b69e49671d0a8e1ccf2d4d4cb1bbc
DWL5 <ssop6403@gmail.com>
2021-02-12 20:08:27 +0900

    refactor: Messages 클래스 명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String inputFormUser(Messages messages) {
+    public static String inputFormUser(InputViewMessages messages) {
         System.out.println(messages.getMessage());
         String input = scanner.nextLine();
 
