5b9ffe736a4252c20e3388e687606d4bcd432b06
DWL5 <ssop6403@gmail.com>
2021-02-12 19:55:31 +0900

    feat: 특수 문자 관리하는 Mark class 추가

---
+++ src/main/java/racingcar/view/Marks.java
@@ -0,0 +1,7 @@
+package racingcar.view;
+
+public class Marks {
+    public static final String dash = "-";
+    public static final String comma = ",";
+    public static final String colon = ":";
+}
