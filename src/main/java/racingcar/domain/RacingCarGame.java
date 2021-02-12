6f0f0b3fb8de1728fb005897474a598f7150eec3
TaewanKimmmm <ktww88@gmail.com>
2021-02-13 04:17:07 +0900

    refactor: mvc 패턴으로 리팩토링

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -1,35 +1,38 @@
 package racingcar.domain;
 
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RacingCarGame {
-    public void start() {
-        try {
-            List<String> splittedCarNames = InputView.getCarNamesInput();
-            Cars cars = createRacingCars(splittedCarNames);
-            int lap = InputView.getLapInput();
-            race(cars, lap);
-            OutputView.showWinners(cars.getWinners());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    private final Cars cars;
+    private final Lap lap;
+
+    public RacingCarGame(List<String> carNames, Lap lap) {
+        this.cars = createRacingCars(carNames);
+        this.lap = lap;
+    }
+
+    public boolean isEnd() {
+        return lap.isZero();
+    }
+
+    public void race() {
+        cars.raceOneLap();
+        lap.decrement();
+    }
+
+    public Map<String, Integer> getResult() {
+        return cars.getStatus();
+    }
+
+    public List<String> getWinners() {
+        return cars.getWinners();
     }
 
     private Cars createRacingCars(List<String> carNames) {
         List<Car> value = new ArrayList<>();
         carNames.forEach(carName -> value.add(new Car(new CarName(carName))));
         return new Cars(value);
-    }
-
-    private void race(Cars cars, int lap) {
-        OutputView.showResultMessage();
-        for (int i = 0; i < lap; i++) {
-            cars.raceOneLap();
-            OutputView.showOneLapResult(cars.getStatus());
-        }
     }
 }
