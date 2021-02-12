dbbda9228699527020a88847ee85bd137266dcda
DWL5 <ssop6403@gmail.com>
2021-02-12 20:10:26 +0900

    feat: OutputView에 우승자, 자동차 상태 출력하는 함수 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,33 @@
 package racingcar.view;
 
+import racingcar.domain.car.CarName;
+import racingcar.domain.car.Cars;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static racingcar.view.Marks.comma;
+
 public class OutputView {
+    public static final String EXECUTION_RESULT = "실행 결과";
+    public static final String MESSAGE_GAME_RESULT = "가 최종 우승했습니다.";
+
     public static void print(String message) {
         System.out.println(message);
+    }
+
+    public static void printCarStates(Cars cars) {
+        cars.getCarList().stream().forEach(car -> {
+            System.out.println(car.toString());
+        });
+        System.out.println();
+    }
+
+    public static void printWinner(Cars cars) {
+        List<CarName> winners = cars.getWinnersName();
+        List<String> winnerNames = winners.stream().map(CarName::toString).collect(Collectors.toList());
+        Collections.sort(winnerNames);
+        System.out.println(String.join(comma, winnerNames) + MESSAGE_GAME_RESULT);
     }
 }
