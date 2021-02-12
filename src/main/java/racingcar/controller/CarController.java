49379dce734d2c7aad1940c209fe2ea326af7a75
mungto <daum7766@naver.com>
2021-02-13 02:01:43 +0900

    refactor(CarController): refactoring CarController
    Transfer of responsibility
    add function isEnd, getCars, getWinners

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -2,70 +2,62 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import racingcar.domain.Cars;
 import racingcar.domain.TryNumber;
 import racingcar.domain.car.Car;
-import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class CarController {
 
     private static final String Separator = ",";
     private static final String ERROR_MESSAGE = "자동차 이름은 없을 수 없습니다.";
+    private static final int ZERO = 0;
 
     private Cars cars;
     private TryNumber tryNumber;
 
-    public CarController() {
+    public CarController(final String values, final int tryNumber) {
+        getCarNames(values);
+        this.tryNumber = new TryNumber(tryNumber);
+        init();
     }
 
     private void init() {
-        getCarNames();
-        getAttemptNumber();
+        OutputView.printNextLine();
+        OutputView.printExecutionResultMessage();
     }
 
-    private void getCarNames() {
-        List<Car> carList = new ArrayList<>();
-        String carNames = InputView.getCarNames();
-        String[] carNamesArray = carNamesSplit(carNames);
+    private void getCarNames(final String values) {
+        final List<Car> carList = new ArrayList<>();
+        final String[] carNamesArray = carNamesSplit(values);
         for (String carName : carNamesArray) {
             carList.add(new Car(carName));
         }
         cars = new Cars(carList);
     }
 
-    private void getAttemptNumber() {
-        tryNumber = new TryNumber(InputView.getTryNumber());
-    }
-
-    private String[] carNamesSplit(String carNames) {
-        String[] carNamesArray = carNames.split(Separator);
-        if (carNamesArray.length == 0) {
+    private String[] carNamesSplit(final String carNames) {
+        final String[] carNamesArray = carNames.split(Separator);
+        if (carNamesArray.length == ZERO) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return carNamesArray;
     }
 
     public void play() {
-        init();
-        OutputView.printNextLine();
-        OutputView.printExecutionResultMessage();
-        Random random = new Random();
-        for (int i = 0; i < tryNumber.getTryNumber(); i++) {
-            cars.move(random);
-        }
-        List<String> winners = cars.getWinners();
-        OutputView.printWinnerCarNamesMessage(winnerListToString(winners));
+        cars.move();
+        tryNumber = tryNumber.decreaseNumber();
     }
 
-    public static String winnerListToString(List<String> winners) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(winners.get(0));
-        for (int i = 1; i < winners.size(); i++) {
-            stringBuilder.append(", ");
-            stringBuilder.append(winners.get(i));
-        }
-        return stringBuilder.toString();
+    public boolean isEnd() {
+        return tryNumber.isEnd();
+    }
+
+    public List<Car> getCars() {
+        return cars.getCars();
+    }
+
+    public List<Car> getWinners() {
+        return cars.getWinners();
     }
 }
