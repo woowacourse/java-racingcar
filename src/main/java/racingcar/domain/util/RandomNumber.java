83662676fbdd0092960c98441c067f985e99cd58
DWL5 <ssop6403@gmail.com>
2021-02-13 01:26:35 +0900

    refactor: 사용하지 않는 상수 제거

--- src/main/java/racingcar/domain/util/RandomNumber.java
+++ src/main/java/racingcar/domain/util/RandomNumber.java
@@ -5,8 +5,6 @@ import racingcar.exception.MoveConditionOutOfBoundException;
 import java.util.Random;
 
 public class RandomNumber {
-    private static final int MINIMUM = 0;
-
     private static final Random random = new Random();
 
     public static int generate(int bound) {
