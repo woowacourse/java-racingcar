48d0d094e2b19b8f334e9d6b4bf8095aeba7ed16
mungto <daum7766@naver.com>
2021-02-14 02:02:47 +0900

    feat(Car): equals, hashCode function add

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain.car;
 
+import java.util.Objects;
+
 public class Car {
 
     private static final int START_POSITION = 0;
@@ -30,5 +32,23 @@ public class Car {
 
     public boolean isEqualPosition(final int value) {
         return position.isEqualNumber(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects
+            .equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
