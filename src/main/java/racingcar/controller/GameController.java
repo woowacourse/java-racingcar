f39a7727d68279b0c16b92fd2258f393b220ae20
perenok <perenoke@gmail.com>
2021-02-13 13:57:45 +0900

    refactor(GameController) : add fillFuel and getMaxPosition
    CarRepository에 있던 generateRandomInteger를 GameController로 옮기고 이름은 fillFuel로 수정
    CarRepository에 있던 getMaxPosition을 GameController로 옮김
    상수들의 이름을 Inclusive 대신 ZERO_FUEL, FULL_FUEL로 바꿔 의미 부여

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,23 +3,42 @@ package racingcar.controller;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.CarRepository;
 import racingcar.domain.game.Game;
+import racingcar.utils.RandomUtils;
 import racingcar.view.GamePage;
 
+import java.util.List;
+
 public class GameController {
+
+    private static final int ZERO_FUEL = 0;
+    private static final int FULL_FUEL = 9;
 
     public void startGame(Game game) {
         GamePage.printResultPage();
         while (game.notFinished()) {
             game.incrementCount();
-            playSingleRound(game);
+            playSingleRound();
         }
-        GamePage.printFinalResult(game.joinWinnerNames(CarRepository.collectWinners()));
+        List<Car> winners = CarRepository.selectWinners(getMaxPosition());
+        GamePage.printFinalResult(winners);
     }
 
-    public void playSingleRound(Game game) {
+    public void playSingleRound() {
         for (Car car : CarRepository.cars()) {
-            car.tryToMoveForward(game.generateRandomInteger());
+            car.tryToMoveForward(fillFuel());
         }
         GamePage.printSingleRoundResult(CarRepository.cars());
+    }
+
+    public int fillFuel() {
+        return RandomUtils.nextInt(ZERO_FUEL, FULL_FUEL);
+    }
+
+
+    public int getMaxPosition() {
+        return CarRepository.cars().stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
     }
 }
