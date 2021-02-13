2a25a7997e97a492495a1f3dca2a2239593f3ef6
perenok <perenoke@gmail.com>
2021-02-13 13:44:40 +0900

    refactor(Game) : remove generateRandomInteger and joinWinnerNames

--- src/main/java/racingcar/domain/game/Game.java
+++ src/main/java/racingcar/domain/game/Game.java
@@ -1,15 +1,6 @@
 package racingcar.domain.game;
 
-import racingcar.domain.car.Car;
-import racingcar.utils.RandomUtils;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Game {
-
-    private static final int START_INCLUSIVE = 0;
-    private static final int END_INCLUSIVE = 9;
 
     private final int round;
     private int count = 0;
@@ -18,21 +9,11 @@ public class Game {
         this.round = round;
     }
 
-    public int generateRandomInteger() {
-        return RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE);
-    }
-
     public boolean notFinished() {
         return round != count;
     }
 
     public void incrementCount() {
         ++count;
-    }
-
-    public String joinWinnerNames(List<Car> winners) {
-        return winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
     }
 }
