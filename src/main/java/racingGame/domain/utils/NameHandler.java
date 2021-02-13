c24a850deb9d41afbb26eaa4f60d37a22da1496a
ecsimsw <ecsimsw@gmail.com>
2021-02-13 22:29:34 +0900

    refactor : 상수명은 대문자로, 언더바 구분

--- src/main/java/racingGame/domain/utils/NameHandler.java
+++ src/main/java/racingGame/domain/utils/NameHandler.java
@@ -7,13 +7,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class NameHandler {
-    private static final String nameSeparator = ",";
+    private static final String NAME_SEPARATOR = ",";
 
     private NameHandler() {
     }
 
     public static List<Name> splitNames(final String line) {
-        return Arrays.stream(line.split(nameSeparator))
+        return Arrays.stream(line.split(NAME_SEPARATOR))
                 .map(String::trim)
                 .map(name -> Name.create(name))
                 .collect(Collectors.toList());
