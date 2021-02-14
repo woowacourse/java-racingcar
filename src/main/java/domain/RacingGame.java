8534259c05ba00a2f4bcc12702ad9972951115c2
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-02-14 23:01:27 +0900

    feat(racing-game): 게임 진행에 사용할 move 알고리즘 domain 패키지로 이동

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -2,16 +2,26 @@ package domain;
 
 import car.Car;
 import java.util.List;
+import view.OutputView;
 
 public class RacingGame {
 
     private final List<Car> carNames;
     private final int round;
+    private OutputView message = new OutputView();
 
     public RacingGame(List<Car> carNames, int round) {
         this.carNames = carNames;
         this.round = round;
     }
 
+    public void moveCar(List<Car> carNames, int round) {
 
+        message.roundStart();
+        for (int i = 0; i < round; i++) {
+            carNames.forEach(Car::moveOrStop);
+            message.printResult(carNames);
+        }
+        printWinner(carNames);
+    }
 }
