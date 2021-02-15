9c11970d6de2028a93e49c2e0d16d77379a01e10
seunghyun <ggyool@naver.com>
2021-02-16 02:14:27 +0900

    refactor(racingcar): Application 실행 로직 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,29 @@
 package racingcar;
 
 import racingcar.controller.RacingCarController;
+import racingcar.domain.Cars;
+import racingcar.domain.Times;
+import racingcar.view.OutputView;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        final RacingCarController racingCarController = new RacingCarController(scanner);
+        final List<String> carNames = racingCarController.scanCarNames();
+        Times times = racingCarController.scanTimes();
+        Cars cars = Cars.createByName(carNames);
 
-        RacingCarController racingCarController = new RacingCarController(scanner);
-        racingCarController.run();
+        OutputView.printResultGuide();
+        while (times.isRemain()) {
+            times = times.reduce();
+            cars = cars.move();
+            OutputView.printResults(cars.toDtos());
+        }
+
+        cars = Cars.create(cars.getWinners());
+        OutputView.printWinnerNames(cars.toNameDtos());
     }
 }
