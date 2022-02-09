a316e95b8243aa114b2bd5dab6bc6a189031d592
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-09 20:31:55 +0900

    feat: 컴마(,)구분자로 분리한 뒤 합을 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,25 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 	public static int splitAndSum(String text) {
 		if (text == null || text.equals("")) {
 			return 0;
 		}
-		
-		int number = Integer.parseInt(text);
+		List<Integer> numbers = toInt(splitText(text));
+		return numbers.stream().mapToInt(i -> i).sum();
+	}
 
-		return number;
+	public static List<String> splitText(String text) {
+		return Arrays.asList(text.split(","));
+	}
+
+	public static List<Integer> toInt(List<String> list) {
+		return list.stream()
+			.map(a -> Integer.parseInt(a))
+			.collect(Collectors.toList());
 	}
 }
