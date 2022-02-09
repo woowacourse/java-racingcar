733cb661bb60a81607193cb2cd108df1328094d0
Ohzzi <clover7even@naver.com>
2022-02-09 17:23:41 +0900

    refactor: 빈 문자열 체크와 합 구하는 메소드 분리

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,12 +2,20 @@ package stringcalculator;
 
 public class StringCalculator {
 	public static int splitAndSum(String input) {
-		if (input == null || input.isBlank()) {
+		if (isEmpty(input)) {
 			return 0;
 		}
+		return getSum(input);
+	}
+
+	private static boolean isEmpty(String input) {
+		return input == null || input.isBlank();
+	}
+
+	private static int getSum(String input) {
 		String[] numbers = input.split(",");
 		int sum = 0;
-		for(String number: numbers) {
+		for (String number : numbers) {
 			sum += Integer.parseInt(number);
 		}
 		return sum;
