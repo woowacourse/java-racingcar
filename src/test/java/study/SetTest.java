092b27cee50d94c006d0672e6eac0e409a07d78d
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-09 20:55:03 +0900

    test(study): check contains method in Set class with ParameterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,6 +12,7 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
+
     private Set<Integer> numbers;
 
     @BeforeEach
@@ -27,5 +30,14 @@ public class SetTest {
         int sizeResult = numbers.size();
 
         assertThat(sizeResult).isEqualTo(3);
+    }
+
+    @DisplayName("contains 메서드는 Set에 특정 값이 존재하는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        boolean containsResult = numbers.contains(number);
+
+        assertThat(containsResult).isTrue();
     }
 }
