fb4a33aceec3468f4600694bcc45a21c2aa3247a
Donggyu Kim <a29661498@gmail.com>
2022-02-09 17:57:28 +0900

    test : SetCollection 요구사항 2
    집합 원소 존재유무 테스트

--- src/test/java/junitstudy/SetCollectionTest.java
+++ src/test/java/junitstudy/SetCollectionTest.java
@@ -1,9 +1,12 @@
 package junitstudy;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,5 +27,11 @@ public class SetCollectionTest {
     @Test
     public void 집합_크기_테스트() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    public void 집합_원소_존재_테스트(int number) throws Exception {
+        assertTrue(numbers.contains(number));
     }
 }
