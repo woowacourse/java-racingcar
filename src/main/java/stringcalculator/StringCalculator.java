38290f76d8e0b376eb7585d2044e1b35ace006f9
Ohzzi <clover7even@naver.com>
2022-02-09 20:19:25 +0900

    refactor: 하드코딩 제거 및 2개 이상의 역할을 하는 메소드 분리

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -5,10 +5,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+	private static final int DEFAULT_RETURN_NUMBER = 0;
+	private static final int GET_CUSTOM_DELIMITER = 1;
+	private static final int GET_NUMBERS = 2;
+
+	private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+	private static final String DEFAULT_DELIMITER = ",:";
+	private static final String LEFT_BRACKET = "[";
+	private static final String RIGHT_BRACKET = "]";
+
+	private static Matcher matcher;
+
 	public static int splitAndSum(String input) {
 		if (isEmpty(input)) {
-			return 0;
+			return DEFAULT_RETURN_NUMBER;
 		}
+		getMatcher(input);
 		return getSum(input);
 	}
 
@@ -17,7 +29,7 @@ public class StringCalculator {
 	}
 
 	private static int getSum(String input) {
-		return Arrays.stream(convertStringArrToIntArr(checkAndSplit(input)))
+		return Arrays.stream(convertStringArrToIntArr(split(input)))
 			.sum();
 	}
 
@@ -35,12 +47,22 @@ public class StringCalculator {
 		return converted;
 	}
 
-	private static String[] checkAndSplit(String input) {
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-		if (m.find()) {
-			String delimiter = "[" + m.group(1) + ",:]";
-			return m.group(2).split(delimiter);
+	private static String[] split(String input) {
+		if (matcher.find()) {
+			return splitByCustomDelimiter(matcher.group(GET_NUMBERS));
 		}
-		return input.split("[,:]");
+		return input.split(LEFT_BRACKET + DEFAULT_DELIMITER + RIGHT_BRACKET);
+	}
+
+	private static String[] splitByCustomDelimiter(String input) {
+		return input.split(getCustomDelimiter());
+	}
+
+	private static String getCustomDelimiter() {
+		return LEFT_BRACKET + matcher.group(GET_CUSTOM_DELIMITER) + DEFAULT_DELIMITER + RIGHT_BRACKET;
+	}
+
+	private static void getMatcher(String input) {
+		matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
 	}
 }
