ed94b91ce16f47027404e4dda06d0fdec3a91662
sure-why-not <pkse1213@gmail.com>
2022-02-09 20:22:59 +0900

    feat : “//”와 “\n” 문자 사이에 커스텀 구분자 지정 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,19 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
 	final static String DEFAULT_SEPARATOR = "[,:]";
+	final static String CUSTOM_PATTERN_REGEX = "//(.)\n(.*)";
 
 	public static int splitAndSum(String input) {
 		if (isBlank(input)) {
 			return 0;
 		}
+
 		return sum(split(input));
 	}
 
@@ -23,7 +27,28 @@ public class StringCalculator {
 	}
 
 	public static int[] split(String input) {
+		Matcher matcher = createMatcher(input);
+		if (matcher.find()) {
+			return splitByCustomSeparator(matcher);
+		}
+
+		return splitByDefaultSeparator(input);
+	}
+
+	public static Matcher createMatcher(String input) {
+		return Pattern.compile(CUSTOM_PATTERN_REGEX).matcher(input);
+	}
+
+	public static int[] splitByDefaultSeparator(String input) {
 		return Arrays.stream(input.split(DEFAULT_SEPARATOR))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+	}
+
+	public static int[] splitByCustomSeparator(Matcher matcher) {
+		String customDelimiter = matcher.group(1);
+		String[] tokens = matcher.group(2).split(customDelimiter);
+		return Arrays.stream(tokens)
 			.mapToInt(Integer::parseInt)
 			.toArray();
 	}
