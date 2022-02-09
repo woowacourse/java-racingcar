ef991b3a4fd7287cf4d5138d456de395dcee902f
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-09 20:42:51 +0900

    test(study): check size method in Set class

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메서드는 Set의 크기를 반환한다.")
+    @Test
+    void size() {
+        int sizeResult = numbers.size();
+
+        assertThat(sizeResult).isEqualTo(3);
+    }
+}
