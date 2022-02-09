a7b6a2cfa271bdc9fe791032817b34bc3b3f07db
her0807 <her0807@naver.com>
2022-02-09 22:16:06 +0900

    feat: 값이 Null or 빈값일 경우 예외처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,19 +1,20 @@
 package calculator;
 
 public class Calculator {
-	int add(int i, int j) {
+	public int splitAndSum(String text) {
+		if (!isValidValues(text)) {
+			return 0;
+		}
+		String[] numbers = text.split(",");
+
+		return add(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+	}
+
+	private boolean isValidValues(String text) {
+		return text != null && !text.trim().isEmpty();
+	}
+
+	private int add(int i, int j) {
 		return i + j;
-	}
-
-	int subtract(int i, int j) {
-		return i - j;
-	}
-
-	int multiply(int i, int j) {
-		return i * j;
-	}
-
-	int divide(int i, int j) {
-		return i / j;
 	}
 }
