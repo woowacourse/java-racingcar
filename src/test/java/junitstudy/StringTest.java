37f0320f19d442dafb28f6461f6ef1770d8145c0
Donggyu Kim <a29661498@gmail.com>
2022-02-09 17:42:50 +0900

    test: String 요구사항3
    String 인덱스 범위 테스트

--- src/test/java/junitstudy/StringTest.java
+++ src/test/java/junitstudy/StringTest.java
@@ -1,5 +1,6 @@
 package junitstudy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,15 +22,14 @@ public class StringTest {
     }
     
     @Test
-    public void 괄호_제거_테스트() throws Exception {
-        //given
-        String input = "(1,2)";
-        
-        //when
-        String result = input.substring(1, input.length()-1);
-        
-        //then
-        assertThat(result).isEqualTo("1,2");
+    @DisplayName("인덱스 범위 테스트")
+    public void 특정_위치_문자_테스트() throws Exception {
+        assertThatThrownBy(()-> {"abc".charAt(3);})
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+
     }
 
+    private String[] split(String input) {
+        return input.split(",");
+    }
 }
