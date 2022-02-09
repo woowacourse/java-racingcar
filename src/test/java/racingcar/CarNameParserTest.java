e0e767e42348a8c095074f93b9aefa67d28e7c19
wilgur513 <wilgur513@naver.com>
2022-02-09 19:03:39 +0900

    feat: throw execption by invalid length

--- src/test/java/racingcar/CarNameParserTest.java
+++ src/test/java/racingcar/CarNameParserTest.java
@@ -1,17 +1,24 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CarNameParserTest {
+
     @Test
-    public void parse_name_by_comma() {
+    public void 컴마로_자동차_이름_분리() {
         CarNameParser parser = new CarNameParser();
-        List<Car> expect = parser.parse("pobi,crong,honux");
-        assertThat(expect).containsExactly(Car.from("pobi"), Car.from("crong"), Car.from("honux"));
+        List<String> expect = parser.parse("pobi,crong,honux");
+        assertThat(expect).containsExactly("pobi", "crong", "honux");
     }
 
-
+    @Test
+    public void 이름이_6글자_이상인_경우_예외_발생() {
+        CarNameParser parser = new CarNameParser();
+        assertThatThrownBy(() -> parser.parse("pobi,crong,honuxx"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
