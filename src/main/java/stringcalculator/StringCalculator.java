3144381d5598ce920d99d3d36279f9b2fdd9ffed
Ohzzi <clover7even@naver.com>
2022-02-09 17:20:19 +0900

    feat: 쉼표 구분자로 분리하여 숫자의 합 반환

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
 		if (input == null || input.isBlank()) {
 			return 0;
 		}
-		return 0;
+		String[] numbers = input.split(",");
+		int sum = 0;
+		for(String number: numbers) {
+			sum += Integer.parseInt(number);
+		}
+		return sum;
 	}
 }
