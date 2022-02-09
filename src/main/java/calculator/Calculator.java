b95b5f932f579a1e420e0a861361dc5a695efbf2
OzRagwort <ozragwort@gmail.com>
2022-02-09 21:43:20 +0900

    feat(calculator): 문자열 배열을 숫자 배열로 바꾸는 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -11,4 +11,12 @@ public class Calculator {
   public static String[] split(String input) {
     return input.split("[,:]");
   }
+
+  public static int[] toInteger(String[] stringNumbers) {
+    int[] numbers = new int[stringNumbers.length];
+    for(int i=0; i < stringNumbers.length; i++){
+      numbers[i] = Integer.parseInt(stringNumbers[i]);
+    }
+    return numbers;
+  }
 }
