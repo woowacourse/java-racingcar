2e96eea0b0ce89207772536ec94e4fcc2c1b65ac
yxxnghwan <younghwan960@gmail.com>
2022-02-09 21:05:05 +0900

    feat(calculator): 양의 정수가 아닌 경우 예외처리 작성

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,9 +2,9 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
@@ -12,21 +12,36 @@ public class StringCalculator {
 		if (isNullOrEmpty(input)) {
 			return 0;
 		}
-		if(isNumeric(input)){
+		if(isPositiveInteger(input)){
 			return Integer.parseInt(input);
 		}
-		String[] numStrArray = split(input);
+		List<String> numberStringList = split(input);
+		// 문자나 음수 걸러내기
+		checkRightPositiveInteger(numberStringList);
+		List<Integer> numberArray = numberStringList.stream()
+			.mapToInt(Integer::parseInt)
+			.boxed()
+			.collect(Collectors.toList());
+
+	}
+
+	private static void checkRightPositiveInteger(List<String> numberStringList) {
+		numberStringList.forEach(numberString -> {
+			if (isPositiveInteger(numberString)) {
+				throw new RuntimeException("양의 정수를 입력 해주세요.");
+			}
+		});
 	}
 
 	private static boolean isNullOrEmpty(String input) {
 		return input.isBlank() || input == null;
 	}
 
-	private static boolean isNumeric(String input) {
+	private static boolean isPositiveInteger(String input) {
 		return input.matches("^(0|[1-9][0-9]*)$");
 	}
 
-	private static String[] split(String input) {
+	private static List<String> split(String input) {
 		List<String> delimiters = new ArrayList<>();
 		delimiters.add(",");
 		delimiters.add(":");
@@ -37,6 +52,6 @@ public class StringCalculator {
 			input = input.substring(input.indexOf("\n") + 1);
 		}
 		String delimiter = String.join("|", delimiters);
-		return input.split(delimiter);
+		return Arrays.asList(input.split(delimiter));
 	}
 }
