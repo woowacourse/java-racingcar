fe51cb556e4aeea995d4fb0135beeaf58b37ede3
Chan Kim <chankim@Chanui-MacBookPro.local>
2022-02-09 17:09:39 +0900

    feat : 문자열 계산기 구현

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,49 @@
+package Calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+    public static int splitAndSum(String input) {
+        if (isEmpty(input)) {
+            return 0;
+        }
+        return getSum(input);
+    }
+
+    private static int getSum(String input) {
+        int sum = 0;
+        for (String token : split(input)) {
+            int number = parse(token);
+            isNegative(number);
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static int parse(String token) {
+        if (isEmpty(token)) {
+            return 0;
+        }
+        return Integer.parseInt(token);
+    }
+
+    private static void isNegative(int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static boolean isEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static String[] split(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String group = m.group(1);
+            return m.group(2).split(group);
+        }
+        return input.split(",|:");
+    }
+}
