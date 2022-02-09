eed64dc41983208c949f60a7eaf3f8674715d1d8
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-09 17:45:55 +0900

    feat: 입력받은 문자열 null 또는  일 경우 0을 반환

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+	public static int splitAndSum(String text) {
+		if (text == null || text.equals("")) {
+			return 0;
+		}
+		return 0;
+	}
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+// 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
+public class StringCalculatorTest {
+	@Test
+	public void splitAndSum_null_또는_빈문자() throws Exception {
+		int result = StringCalculator.splitAndSum(null);
+		assertThat(result).isEqualTo(0);
+
+		result = StringCalculator.splitAndSum("");
+		assertThat(result).isEqualTo(0);
+	}
+}
