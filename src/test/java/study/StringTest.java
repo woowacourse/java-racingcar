728bdc89d4624fb34f342307566c73e9fffc58f0
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-09 21:15:27 +0900

    test: DisplayName 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,13 @@ package study;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
     @Test
+    @DisplayName("split 함수로 주어진 값을 구분할 수 있다.")
     void splitTestContains() {
         String input = "1,2";
         String[] result = input.split(",");
@@ -17,6 +19,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("split 함수로 구분자가 포함되지 않은 경우, 값을 그대로 반환한다.")
     void splitTestContainsOnly1() {
         String input = "1";
         String[] result = input.split(",");
@@ -25,6 +28,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("substring으로 특정 구간 값을 반환한다.")
     void substringTest() {
         String input = "(1,2)";
         String result = input.substring(1, 4);
@@ -33,6 +37,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열의 인덱스로 특정 문자 찾는다.")
     void stringCharAtTest() {
         String input = "abc";
         char charAtElement = input.charAt(0);
@@ -40,6 +45,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열의 길이보다 큰 숫자로 위치를 찾을 때 예외를 발생한다.")
     void stringCharAtExceptionTest() {
         String input = "abc";
         
