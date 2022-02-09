11d8556d2798d36777c73dcc9eeb142f2cdcccf3
sure-why-not <pkse1213@gmail.com>
2022-02-09 19:29:05 +0900

    feat : 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합 반환 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,28 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
 	public static int splitAndSum(String input) {
-		if (input == null || input.isEmpty()) {
+		if (isBlank(input)) {
 			return 0;
 		}
-		return Integer.parseInt(input);
+		return sum(split(input));
+	}
+
+	public static boolean isBlank(String input) {
+		return input == null || input.isEmpty();
+	}
+
+	public static int sum(int[] numbers) {
+		return Arrays.stream(numbers)
+			.sum();
+	}
+
+	public static int[] split(String input) {
+		return Arrays.stream(input.split(","))
+			.mapToInt(Integer::parseInt)
+			.toArray();
 	}
 }
