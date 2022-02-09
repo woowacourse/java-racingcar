fbd8f448ca5d03bf36c32cfa7809963ca9c24b7b
Donggyu Kim <a29661498@gmail.com>
2022-02-09 18:06:38 +0900

    test : SetCollection 요구사항3
    집합에 존재하는지 여부에 따라 결과값이 달라지도록 테스트

--- src/test/java/junitstudy/SetCollectionTest.java
+++ src/test/java/junitstudy/SetCollectionTest.java
@@ -1,15 +1,17 @@
 package junitstudy;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetCollectionTest {
 
@@ -33,5 +35,11 @@ public class SetCollectionTest {
     @ValueSource(ints = {1,2,3})
     public void 집합_원소_존재_테스트(int number) throws Exception {
         assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    public void 집합_원소_존재_테스트_2(int number, boolean expected) {
+        assertEquals(numbers.contains(number), expected);
     }
 }
