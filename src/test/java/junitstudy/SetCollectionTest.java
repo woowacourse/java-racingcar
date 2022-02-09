da9b59de451ec2b34d62f23ca4fcef5348fb34a4
Donggyu Kim <a29661498@gmail.com>
2022-02-09 17:53:03 +0900

    test : SetCollection 요구사항 1
    Set 크기 확인 테스트

---
+++ src/test/java/junitstudy/SetCollectionTest.java
@@ -0,0 +1,28 @@
+package junitstudy;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void 집합_크기_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
