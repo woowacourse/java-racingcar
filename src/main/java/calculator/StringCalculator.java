51542c2d1fea7d0dfc93be11a22ca5068a64aede
wilgur513 <wilgur513@naver.com>
2022-02-09 16:27:36 +0900

    refactor: 숫자로 변경하고 합을 구하는 메서드 추출

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,11 +6,21 @@ public class StringCalculator {
 		if (text == null || text.isEmpty()) {
 			return 0;
 		}
+		return sum(toInts(text.split(",")));
+	}
 
-		String[] values = text.split(",");
+	private int[] toInts(String[] values) {
+		int[] numberList = new int[values.length];
+		for (int i = 0; i < values.length; i++) {
+			numberList[i] = Integer.parseInt(values[i]);
+		}
+		return numberList;
+	}
+
+	private int sum(int[] values) {
 		int total = 0;
-		for (String value : values) {
-			total += Integer.parseInt(value);
+		for (int value : values) {
+			total += value;
 		}
 		return total;
 	}
