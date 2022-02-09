8bf4b587087da7ff49631ac249d01243d1080217
OzRagwort <ozragwort@gmail.com>
2022-02-09 21:21:33 +0900

    feat(calculator): null 값 또는 빈 문자열을 입력할 경우 0을 반환

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+  public static int sum(String input) {
+    if (input == null || input.equals("")) {
+      return 0;
+    }
+    return 0;
+  }
+}
+
