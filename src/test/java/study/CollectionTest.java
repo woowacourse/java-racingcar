112151ab5b8c2df5464a251ff2e51e00f8ea8fa9
yhh1056 <yhh1056@naver.com>
2022-02-09 17:27:49 +0900

    feat(study): JUnit 학습

---
+++ src/test/java/study/CollectionTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 메서드 테스트")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메서드 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메서드 테스트 - 실패 케이스 포함")
+    @CsvSource(value = {"0:false", "1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input, boolean expected) {
+        assertEquals(numbers.contains(input), expected);
+    }
+}
