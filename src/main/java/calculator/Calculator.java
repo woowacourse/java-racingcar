33369108bf2856b2dc69b668c31c805dc071ea23
her0807 <her0807@naver.com>
2022-02-09 22:28:19 +0900

    feat: 커스텀 구분자가 있을 경우 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 	public int splitAndSum(String text) {
 		if (!isValidValues(text)) {
 			return 0;
 		}
-		String[] numbers = text.split(",|:");
+		int[] numbers = stringToIntArray(getValues(text));
 
 		return add(numbers);
 	}
@@ -14,10 +18,45 @@ public class Calculator {
 		return text != null && !text.trim().isEmpty();
 	}
 
-	private int add(String[] numbers) {
-		if (numbers.length == 1) {
-			return Integer.parseInt(numbers[0]);
+	private int[] stringToIntArray(String[] strings) {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (String i : strings) {
+			numbers.add(convertValue(i));
 		}
-		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		return numbers.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	private int convertValue(String string) {
+		if (!isValidValues(string)) {
+			return 0;
+		}
+		if (!isNumber(string)) {
+			throw new IllegalArgumentException("숫자를 입력해주세요.");
+		}
+		return Integer.parseInt(string);
+	}
+
+	private boolean isNumber(String string) {
+		return string.matches("[+-]?\\d*(\\.\\d+)?");
+	}
+
+	private String[] getValues(String text) {
+		String delimiter = ",|:";
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			return m.group(2).split(String.format("%s|%s", delimiter, m.group(1)));
+		}
+		return text.split(delimiter);
+	}
+
+	private int add(int[] numbers) {
+		if (numbers.length == 1) {
+			return numbers[0];
+		}
+		int result = 0;
+		for (int i : numbers) {
+			result += i;
+		}
+		return result;
 	}
 }
