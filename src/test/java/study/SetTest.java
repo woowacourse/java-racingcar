c88c5c701289ec741b4570971e551ace2824f1d1
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-09 21:25:43 +0900

    test : set contains 기능 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,12 +1,15 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -25,5 +28,12 @@ public class SetTest {
     @DisplayName("size로 Set의 크기를 구할 수 있다.")
     void setSizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains로 원소가 포함되어 있는지 여부를 확인한다.")
+    void setContainsTest(int number) {
+        assertTrue(numbers.contains(number));
     }
 }
