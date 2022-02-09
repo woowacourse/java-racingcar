692ad1bae21747ab41cf293a743ce09ad466e4f6
sure-why-not <pkse1213@gmail.com>
2022-02-09 23:41:10 +0900

    feat : 시도횟수 입력값 검증 로직 구현

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -5,10 +5,13 @@ public class Validator {
 	private static final String EMPTY_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 1글자 이상이어야 합니다.";
 	private static final String INVALID_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 5글자 이하여야 합니다.";
 	private static final String CAR_COUNT_LIMIT_ERROR_MESSAGE = "자동차는 2대 이상이어야 합니다";
+	private static final String ATTEMPT_NUMBER_TYPE_ERROR_MESSAGE = "시도 횟수는 숫자여야 합니다.";
+	private static final String ATTEMPT_NUMBER_RANGE_ERROR_MESSAGE = "시도 횟수는 1회 이상이어야 합니다.";
 
 	private static final int NAME_LENGTH_LIMIT = 5;
 	private static final int CAR_COUNT_LIMIT = 2;
 	private static final String SEPARATOR_OF_CAR_NAME = ",";
+	private static final String INTEGER_REGEX = "-?\\d+";
 
 	public static void ofCarName(String name) {
 		if (name.isEmpty()) {
@@ -22,6 +25,23 @@ public class Validator {
 	public static void ofCarCount(String input) {
 		if (input.split(SEPARATOR_OF_CAR_NAME).length < CAR_COUNT_LIMIT) {
 			throw new IllegalArgumentException(CAR_COUNT_LIMIT_ERROR_MESSAGE);
+		}
+	}
+
+	public static void ofAttemptNumber(String input) {
+		ofAttemptNumberType(input);
+		ofAttemptNumberRange(input);
+	}
+
+	private static void ofAttemptNumberType(String input) {
+		if (!input.matches(INTEGER_REGEX)) {
+			throw new IllegalArgumentException(ATTEMPT_NUMBER_TYPE_ERROR_MESSAGE);
+		}
+	}
+
+	private static void ofAttemptNumberRange(String input) {
+		if (Integer.parseInt(input) < 1) {
+			throw new IllegalArgumentException(ATTEMPT_NUMBER_RANGE_ERROR_MESSAGE);
 		}
 	}
 }
--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -46,4 +46,29 @@ public class ValidatorTest {
 	public void 자동차_개수_정상(String input) {
 		Validator.ofCarCount(input);
 	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"아라", "abc"})
+	public void 시도횟수_타입_예외(String input) {
+		Assertions.assertThatThrownBy(() -> {
+				Validator.ofAttemptNumber(input);
+			}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("시도 횟수는 숫자여야 합니다.");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"-1", "0"})
+	public void 시도횟수_범위_예외(String input) {
+		Assertions.assertThatThrownBy(() -> {
+				Validator.ofAttemptNumber(input);
+			}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("시도 횟수는 1회 이상이어야 합니다.");
+	}
+
+
+	@ParameterizedTest
+	@ValueSource(strings = {"3", "344"})
+	public void 시도횟수_정상(String input) {
+		Validator.ofAttemptNumber(input);
+	}
 }
