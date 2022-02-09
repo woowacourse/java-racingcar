217eb9080861c0445c71f43da10aeaa2646403cf
woong7 <driedseas@gmail.com>
2022-02-09 17:02:14 +0900

    (feat): JUnit 연습

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    private static final String testString = "1,2";
+
+    @Test
+    @DisplayName("요구사항 1")
+    public void testSplit() {
+        String[] splitedString = testString.split(",");
+        assertThat(splitedString).contains("1", "2");
+        assertThat("1".split(",")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("요구사항 2")
+    public void subString() {
+        assertThat("(1,2)".replaceAll("[()]", "")).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항 3")
+    public void charAt() {
+
+        // 정상
+        assertThat("abc".charAt(1)).isEqualTo('b');
+
+        // 예외
+        assertThatThrownBy(()->{
+            //  발생된 예외
+            // index out of array
+            // 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분
+            "abc".charAt(5);
+//            Character.compare("abc".charAt(1),'a');
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+        //assertThatThrownBy("abc".charAt(1)).isEqualTo('b');
+    }
+}
