1123f925f0c9ea6c525c73c7a36f9cb7ff7bc202
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-09 21:53:27 +0900

    test: NullAndEmptySource로 테스트 중복 제거

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,14 +3,15 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class CalculatorTest {
 
-    @Test
+    @ParameterizedTest
+    @NullAndEmptySource
     @DisplayName("입력값이 공백 혹은 null인 경우 0을 반환한다.")
-    void sumNullAndBlankInput() {
-        assertThat(Calculator.sum(null)).isEqualTo(0);
-        assertThat(Calculator.sum("")).isEqualTo(0);
+    void sumNullAndBlankInput(String input) {
+        assertThat(Calculator.sum(input)).isEqualTo(0);
     }
 }
