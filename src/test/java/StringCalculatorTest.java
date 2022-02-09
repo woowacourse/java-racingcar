dcebb50c889517a97440df62c5e1009cb19d41d2
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-09 18:11:55 +0900

    test: 커스텀 구분자 테스트 수정

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -9,27 +9,28 @@ public class StringCalculatorTest {
 	public StringCalculator stringCalculator = new StringCalculator();
 
 	@Test
-	public void 쉼표를_기준으로_구분(){
-		String str="1,2";
+	public void 쉼표를_기준으로_구분() {
+		String str = "1,2";
 		String[] splitStringArray = stringCalculator.splitString(str);
 		assertThat(splitStringArray).contains("1", "2");
 	}
 
 	@Test
-	public void 콜론를_기준으로_구분(){
-		String str="1:2";
+	public void 콜론를_기준으로_구분() {
+		String str = "1:2";
 		String[] splitStringArray = stringCalculator.splitString(str);
 		assertThat(splitStringArray).contains("1", "2");
 	}
 
 	@Test
-	public void 커스텀구분자를_기준으로_구분(){
-		String str="1//+\n2+3+4";
+	public void 커스텀구분자를_기준으로_구분() {
+		String str = "//a\n2a3a4";
 		String[] splitStringArray = stringCalculator.splitString(str);
-		assertThat(splitStringArray).contains("1", "2");
+		assertThat(splitStringArray).contains("2", "3", "4");
 	}
+	//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=jaegoan&logNo=220704651370
 
-		// @Test
+	// @Test
 	// void 구분자_없() {
 	// 	int actual = StringCalculator.spiltAndSum("1");
 	// 	assertThat(actual).isEqualTo(1);
