38d568cf8fbe8f0425182817bf2f7d13a8e839f5
woong7 <driedseas@gmail.com>
2022-02-09 21:35:28 +0900

    (feat): 계산기 기능 완성

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # java-racingcar
 
-자동차 경주 미션 저장소
+문자열 계산기 TDD / 자동차 경주 미션 저장소
 
-## 우아한테크코스 코드리뷰
+## 문자열 덧셈 계산기를 통한 TDD/리팩터링 실습
+
+### 요구사항
+- 덧셈기능
+- 문자열 파싱
+- 커스텀 구분자 처리
+
+### 예외처리사항
+- 숫자이면서 음수인 경우
+- 숫자 이외의 값 (특수문자, 한글, 영어, 공백)
+- 
+ 
+# 우아한테크코스 코드리뷰
 
 - [온라인 코드 리뷰 과정](https://github.com/woowacourse/woowacourse-docs/blob/master/maincourse/README.md)
---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringCalculator {
+    static int sum;
+    // CUSTOM_DELIMITERSIGN
+    static final String CUSTOM_DELIMETER_SIGN = "//.*\\n.*";
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println(splitAndSum(input));
+    }
+
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        if (isNumber(input)) {
+            return Integer.parseInt(input);
+        }
+
+        String[] tokens = tokenizer(input);
+        return adder(tokens);
+    }
+
+    public static String[] tokenizer(String input) {
+        if (input.matches(CUSTOM_DELIMETER_SIGN)) {
+            String[] tempStr = input.split("\\n");
+            String customDelimiter = tempStr[0].substring(2);
+            String[] tokens = tempStr[1].split(customDelimiter);
+            return tokens;
+        }
+
+        String[] tokens = input.split("[,:]");
+        return tokens;
+    }
+
+    public static int adder(String[] tokens) {
+        if (Arrays.asList(tokens).stream().anyMatch(x -> Integer.parseInt(x) < 0)) {
+            throw new RuntimeException();
+        }
+        zeroFill();
+        Arrays.asList(tokens).forEach(x -> sum += Integer.parseInt(x));
+        return sum;
+    }
+
+    public static void zeroFill(){
+        sum=0;
+    }
+    public static boolean isNumber(String s) {
+        return s.matches("-?\\d+(\\.\\d+)?");
+    }
+}
