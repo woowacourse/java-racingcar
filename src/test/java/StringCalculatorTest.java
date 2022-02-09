26ac015963e65fe8c9a4c22eee2b09d8a73aefa1
brorae <jhr3239@gmail.com>
2022-02-09 17:21:40 +0900

    test(StringCalculatorTest) : Test Case 작성

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    @Test
+    void 콤마_더하기() {
+        int actual = StringCalculator.add("1,2");
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    void 콜론_더하기() {
+        int actual = StringCalculator.add("1:2:3");
+        assertThat(actual).isEqualTo(6);
+    }
+
+    @Test
+    void 혼합_더하기() {
+        int actual = StringCalculator.add("1,2:3");
+        assertThat(actual).isEqualTo(6);
+    }
+
+    @Test
+    void 구분자로_나누기() {
+        String radix = ",|:";
+        String[] splitString = StringCalculator.split("1, 2, 3", radix);
+        assertThat(splitString).contains("1", "2", "3");
+    }
+
+    @Test
+    void 정수형_배열로_바꾸기() {
+        String[] testData = {"1", "2", "3"};
+        int[] changed = StringCalculator.toIntArray(testData);
+        assertThat(changed).contains(1, 2, 3);
+    }
+
+    @Test
+    void 배열_더하기() {
+        int[] numberList = {1, 2, 3};
+        int sum = StringCalculator.sum(numberList);
+        assertThat(sum).isEqualTo(6);
+    }
+}
