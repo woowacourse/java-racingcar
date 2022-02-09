6b2c5e6c4788ac6ebb97f1d6de84ec1f329917da
cjlee38 <gptpem38@gmail.com>
2022-02-09 18:04:27 +0900

    test: Set 클래스 size 학습 테스트 작성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	void sizeSet() {
+		int size = numbers.size();
+		assertThat(size).isEqualTo(3);
+	}
+}
