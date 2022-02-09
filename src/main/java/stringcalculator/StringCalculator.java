55d1d22664a58f9e24615194967e450dc24ab12e
Ohzzi <clover7even@naver.com>
2022-02-09 17:12:23 +0900

    test: 테스트 코드 작성

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package stringcalculator;
+
+public class StringCalculator {
+	public static int splitAndSum(String input) {
+		if (input == null || input.isBlank()) {
+			return 0;
+		}
+		return 0;
+	}
+}
