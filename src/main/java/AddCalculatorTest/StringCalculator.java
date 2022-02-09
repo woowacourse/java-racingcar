d2d92241642b7ac8adf0ae48a9b5a647d4359e05
asebn1 <asebn1@khu.ac.kr>
2022-02-09 18:08:01 +0900

    feat: TDD 문자열 덧셈 계산기 추가

---
+++ src/main/java/AddCalculatorTest/StringCalculator.java
@@ -0,0 +1,41 @@
+package AddCalculatorTest;
+
+import java.util.Arrays;
+
+public class StringCalculator {
+    public static void main(String[] args) {
+    }
+
+    public static int splitAndSum(String s) {
+        if (s == null || s == "") {
+            return 0;
+        }
+
+        if (!(s.contains(",") || s.contains(":") || s.contains(";"))) {
+            return Integer.parseInt(s);
+        }
+
+        //
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '-' || c == ':' || c == ',' || c == ';' || (c >= '0' && c <= '9')) {
+                stringBuilder.append(c);
+            }
+        }
+
+        String[] split = stringBuilder.toString().split(",|:|;");
+
+        int sum = 0;
+        for (String number : split) {
+            if (number.contains("-")) {
+                throw new RuntimeException();
+            }
+            if (number.equals("")) {
+                continue;
+            }
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+}
