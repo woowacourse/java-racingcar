09ab1c37f8b03e3921bd1b3755402e30524b1d16
wilgur513 <wilgur513@naver.com>
2022-02-09 19:03:39 +0900

    feat: throw execption by invalid length

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -7,9 +7,25 @@ import java.util.List;
 
 public class CarNameParser {
 
-    public List<Car> parse(String names) {
-        return Arrays.stream(names.split(","))
-            .map(Car::from)
+    public List<String> parse(String names) {
+        validateCarNames(names);
+
+        return Arrays.stream(splitByComma(names))
             .collect(toList());
+    }
+
+    private void validateCarNames(String names) {
+        if (isInvalidLength(names)) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+        }
+    }
+
+    private boolean isInvalidLength(String names) {
+        return Arrays.stream(splitByComma(names))
+            .anyMatch(n -> n.length() > 5);
+    }
+
+    private String[] splitByComma(String names) {
+        return names.split(",");
     }
 }
