ad930a73c981e6a67afd5d29be370adc05e69fdf
sure-why-not <pkse1213@gmail.com>
2022-02-09 18:58:15 +0900

    feat : 빈 문자열 또는 null 값을 입력할 경우 0 반환 기능 구현

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void splitAndSum_null_또는_빈문자() throws Exception {
+		int result = StringCalculator.splitAndSum(null);
+		assertThat(result).isEqualTo(0);
+
+		result = StringCalculator.splitAndSum("");
+		assertThat(result).isEqualTo(0);
+	}
+}
