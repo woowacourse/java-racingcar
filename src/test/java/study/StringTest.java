1a6abf0ffb9e579465c37221732c37ead9d23527
cjlee38 <gptpem38@gmail.com>
2022-02-09 17:57:28 +0900

    test: String 클래스 charAt 학습 테스트 작성
    1. 문자열로부터 인덱스 값을 기반으로 문자 하나를 가져온다.
    2. 문자열의 인덱스 값이 범위를 벗어나는 경우 예외를 던진다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -23,5 +24,29 @@ public class StringTest {
 		String input = "(1,2)";
 		String result = input.substring(1, input.length() - 1);
 		assertThat(result).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("인덱스로 특정 위치의 문자를 가져온다.")
+	public void getCharacterFromStringByIndex_Success() {
+		// given
+		String input = "abc";
+		// when
+		char result = input.charAt(1);
+		// then
+		assertThat(result).isEqualTo('b');
+	}
+
+	@Test
+	@DisplayName("인덱스로 특정 위치의 문자를 가져올 때 범위를 벗어나면 안된다.")
+	public void getCharacterFromStringByIndexWithOutbound_Fail() {
+		// given
+		String input = "abc";
+		int index = 10;
+		// then
+		assertThatThrownBy(() -> input.charAt(index)).isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageContaining(String.format("String index out of range: %d", index));
+		assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> input.charAt(index))
+			.withMessageMatching("String index out of range: \\d+");
 	}
 }
