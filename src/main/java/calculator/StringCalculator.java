6919cdb690c50b467c0e5ae181f07c29ed82d7be
sun <syoun602@gmail.com>
2022-02-09 18:01:29 +0900

    feat: 쉼표 구분자로 문자열 파싱

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,9 @@
 package calculator;
 
 public class StringCalculator {
+    public static String[] splitString(String s) {
+        String[] values = s.split(",");
+
+        return values;
+    }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,5 +1,17 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringCalculatorTest {
+
+    @Test
+    void 콤마_구분자() {
+        String[] actual = StringCalculator.splitString("1,2");
+        String[] result = {"1", "2"};
+
+        assertThat(actual).isEqualTo(result);
+    }
 
 }
