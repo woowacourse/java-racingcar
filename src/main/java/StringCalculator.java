0bfa5703058170152f9df13e82387718c4f78690
brorae <jhr3239@gmail.com>
2022-02-09 17:48:27 +0900

    feat : 숫자를 더해주는 기능

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,23 @@
 public class StringCalculator {
     public static int calculate(String s) {
         String[] splitStrings = s.split(",|:");
-        return 0;
+        int[] splitNumbers = toIntArray(splitStrings);
+        return sumOfList(splitNumbers);
+    }
+
+    public static int[] toIntArray(String[] testData) {
+        int[] result = new int[testData.length];
+        for (int i = 0; i < testData.length; i++) {
+            result[i] = Integer.parseInt(testData[i]);
+        }
+        return result;
+    }
+
+    public static int sumOfList(int[] numberList) {
+        int total = 0;
+        for (int number : numberList) {
+            total += number;
+        }
+        return total;
     }
 }
