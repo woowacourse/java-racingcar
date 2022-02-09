76fb961b3f76372843b09b82d9ee3327cca959b6
sure-why-not <pkse1213@gmail.com>
2022-02-09 23:10:05 +0900

    feat : 자동차 갯수 검증 로직 구현

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -4,7 +4,11 @@ public class Validator {
 
 	private static final String EMPTY_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 1글자 이상이어야 합니다.";
 	private static final String INVALID_CAR_NAME_ERROR_MESSAGE = "자동차의 이름은 5글자 이하여야 합니다.";
+	private static final String CAR_COUNT_LIMIT_ERROR_MESSAGE = "자동차는 2대 이상이어야 합니다";
+
 	private static final int NAME_LENGTH_LIMIT = 5;
+	private static final int CAR_COUNT_LIMIT = 2;
+	private static final String SEPARATOR_OF_CAR_NAME = ",";
 
 	public static void ofCarName(String name) {
 		if (name.isEmpty()) {
@@ -12,6 +16,12 @@ public class Validator {
 		}
 		if (name.length() > NAME_LENGTH_LIMIT) {
 			throw new IllegalArgumentException(INVALID_CAR_NAME_ERROR_MESSAGE);
+		}
+	}
+
+	public static void ofCarCount(String input) {
+		if (input.split(SEPARATOR_OF_CAR_NAME).length < CAR_COUNT_LIMIT) {
+			throw new IllegalArgumentException(CAR_COUNT_LIMIT_ERROR_MESSAGE);
 		}
 	}
 }
--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -31,5 +30,20 @@ public class InputViewTest {
 	@ValueSource(strings = {"abcde", "abc"})
 	public void 자동차_이름_정상_입력(String name) {
 		Validator.ofCarName(name);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"abc", "awer"})
+	public void 자동차_개수_예외(String input) {
+		Assertions.assertThatThrownBy(() -> {
+				Validator.ofCarCount(input);
+			}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차는 2대 이상이어야 합니다");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"abc,a", "awer,b"})
+	public void 자동차_개수_정상(String input) {
+		Validator.ofCarCount(input);
 	}
 }
