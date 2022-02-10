31e90b9f7b7c9cf9583d315182492a262bc414e6
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-02-10 14:48:32 +0900

    refactor(StringCalculator): 메소드 분리

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -13,17 +13,27 @@ public class StringCalculator {
 	}
 
 	public String[] customDelimiter(String input) {
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+		Matcher m = getMatcher(input);
 		if (!m.find()) {
 			throw new RuntimeException();
 		}
 		String customDelimiter = m.group(1);
-		if (customDelimiter.equals("+") || customDelimiter.equals("*") || customDelimiter.equals("^")) {
-			StringBuffer prefix = new StringBuffer("\\");
-			StringBuffer buffer = prefix.append(customDelimiter);
-			return m.group(2).split(buffer.toString());
+		if (isSpecialDelimiter(customDelimiter)) {
+			return m.group(2).split(addCustomDelimiterPrefix(customDelimiter));
 		}
 		return m.group(2).split(customDelimiter);
+	}
+
+	private Matcher getMatcher(String input) {
+		return Pattern.compile("//(.)\n(.*)").matcher(input);
+	}
+
+	private String addCustomDelimiterPrefix(String customDelimiter) {
+		return "\\" + customDelimiter;
+	}
+
+	private boolean isSpecialDelimiter(String customDelimiter) {
+		return customDelimiter.equals("+") || customDelimiter.equals("*") || customDelimiter.equals("^");
 	}
 
 	public int splitAndSum(String input) {
@@ -31,7 +41,7 @@ public class StringCalculator {
 			return 0;
 		}
 
-		if (input.length() > 2 && input.substring(0, 2).equals("//")) {
+		if (isCustomInput(input)) {
 			String[] numbers = customDelimiter(input);
 			return this.sum(numbers);
 		}
@@ -39,30 +49,25 @@ public class StringCalculator {
 		return this.sum(numbers);
 	}
 
+	private boolean isCustomInput(String input) {
+		return input.length() > 2 && input.startsWith("//");
+	}
+
 	private boolean validNullOrEmpty(String input) {
 		return input == null || input.equals("");
 	}
 
 	public int sum(String[] numbers) {
-
 		return Arrays.stream(numbers).mapToInt(this::mapToInt).sum();
 	}
 
 	private int mapToInt(String number) {
-		if (validNumber(number)) {
-			return Integer.parseInt(number);
-		}
-		throw new RuntimeException("");
-	}
-
-
-	private boolean validNumber(String number) {
 		try {
 			int i = Integer.parseInt(number);
 			if (i < 0) {
 				throw new RuntimeException("음수의 값이 입력되었습니다. 양수를 입력하세요.");
 			}
-			return true;
+			return i;
 		} catch (NumberFormatException e) {
 			throw new RuntimeException("숫자 이외의 값이 입력되었습니다. 숫자를 입력하세요.");
 		}
