c8c0572de99a79a84d9294f5c9a95a71bd5f1dc4
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 19:23:06 +0900

    refactor: 출력 문구 상수 변경

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -8,6 +8,9 @@ import java.util.stream.IntStream;
 
 public class OutputView {
 
+    private static final String GAME_RESULT_TITLE = "실행 결과";
+    private static final String CAR_POSITION_INFO_MESSAGE = "%s : %s";
+    private static final String POSITION_BAR = "-";
     private static final String PRINT_WINNER_DELIMITER = ", ";
     private static final String PRINT_WINNER_MESSAGE = "가 최종 우승했습니다.";
 
@@ -15,7 +18,7 @@ public class OutputView {
     }
 
     public void printGameResultTitle() {
-        System.out.println("실행 결과");
+        System.out.println(GAME_RESULT_TITLE);
     }
 
     public void printCarsPosition(List<RacingCarDto> cars) {
@@ -23,12 +26,11 @@ public class OutputView {
     }
 
     private void printCarPosition(RacingCarDto car) {
-        System.out.print(car.getName() + " : " + getCarPosistionBar(car.getPosition()));
+        System.out.printf(CAR_POSITION_INFO_MESSAGE, car.getName(), getCarPosistionBar(car.getPosition()));
     }
 
-    private String getCarPosistionBar(int position) {
-        return IntStream.range(0, position)
-            .mapToObj(value -> "-")
+    private String getCarPosistionBar(int position) { return IntStream.range(0, position)
+            .mapToObj(value -> POSITION_BAR)
             .collect(Collectors.joining());
     }
 
