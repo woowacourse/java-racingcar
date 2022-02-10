49a2d7cc92c35582a5fb6e825c85fa16a2a45b02
brorae <jhr3239@gmail.com>
2022-02-10 15:20:33 +0900

    test : 랜덤 생성 기능 테스트 작성

---
+++ src/test/java/racingcar/RandomNumberGeneratorTest.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomNumberGeneratorTest {
+
+    @Test
+    public void 랜덤값_범위() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        int value = randomNumberGenerator.generate();
+        assertTrue(0 <= value && value <= 9);
+    }
+}
