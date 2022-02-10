33f05e441b55a30ba53521a668850599b95acfc3
yxxnghwan <younghwan960@gmail.com>
2022-02-10 13:54:01 +0900

    test(racing-car): 난수 생성함수 범위 테스트 작성

---
+++ src/test/java/racingcar/RandomIntegerGeneratorTest.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RandomIntegerGeneratorTest {
+
+	@Test
+	void 숫자_범위_테스트() {
+		Assertions.assertThat(RandomIntegerGenerator.random(3, 3)).isEqualTo(3);
+		Assertions.assertThat(RandomIntegerGenerator.random(0, 9)).isBetween(0, 9);
+	}
+}
