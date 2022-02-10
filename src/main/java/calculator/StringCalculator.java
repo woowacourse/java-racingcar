8adf17757e2509fb71056f47b7c561da62e67082
pup-paw <wldusdl0310@gmail.com>
2022-02-10 13:05:06 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환, 컴마 구분자로 입력할 경우 합 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,10 +6,26 @@ public class StringCalculator {
 			return 0;
 		}
 
+		return sum(splitByColon(a));
 	}
 
 	private static boolean checkNull(String a) {
 		return a == null || a.isEmpty();
+	}
+
+	private static String[] splitByColon(String a) {
+		return a.split(":");
+	}
+
+	private static int sum(String[] numbers) {
+		if (numbers.length == 0) {
+			return 0;
+		}
+		int result = 0;
+		for (String str : numbers) {
+			result += Integer.parseInt(str);
+		}
+		return result;
 	}
 
 }
