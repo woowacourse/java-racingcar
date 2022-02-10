5263b57fa28e9241810dc8aa53c62d40dba0bc7e
wilgur513 <wilgur513@naver.com>
2022-02-10 11:55:06 +0900

    refactor: extract TryCountException class

--- src/main/java/racingcar/parser/exception/TryCountBlankException.java
+++
@@ -1,9 +0,0 @@
-package racingcar.parser.exception;
-
-public class TryCountBlankException extends IllegalArgumentException{
-    private static final String MESSAGE = "시도횟수는 공백일 수 없습니다.";
-
-    public TryCountBlankException() {
-        super(MESSAGE);
-    }
-}
