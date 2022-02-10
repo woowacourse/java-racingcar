cdb6e314d09f349f7302144e130b352cf496d67c
yukong <kyb062666@gmail.com>
2022-02-10 14:10:30 +0900

    feat: 자동차 개수가 2개 미만인 경우 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 import static racingcar.view.Output.*;
 
+/**
+ * - [ ] [ 예외 ] : `공백` - *(입력받은 문자열의 앞 뒤를 제거해준다.)*
+ * - [ ] [ 예외 ] : 자동차가 `2대 미만`
+ * - [ ] [ 예외 ] : 이름이 `중복`
+ * - [ ] [ 예외 ] : `5글자 초과`
+ */
 public class Input {
     Scanner scan = new Scanner(System.in);
 
@@ -13,9 +19,14 @@ public class Input {
     public String carName() {
         try {
             Output.getCarName();
-            String names = scan.nextLine();
-            if (names.isBlank() ) {
+            String inputValue = scan.nextLine();
+            if (inputValue.isBlank()) {
                 throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            }
+
+            String[] names = inputValue.split(",");
+            if (names.length < 2) {
+                throw new IllegalArgumentException("자동차를 두 개 이상 입력해주세요.");
             }
         } catch (IllegalArgumentException e) {
             return carName();
