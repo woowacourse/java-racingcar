64b258d266f13cd4724c06696a7d463d02fea8bc
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-10 20:40:35 +0900

    feat(GameManager): split car names input by comma

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -5,6 +5,15 @@ import racingcar.view.InputView;
 public class GameManager {
 
     public void run() {
-        InputView.requestUserInput();
+        String[] carNames = requestAndSplitCarNames();
+    }
+
+    private String[] requestAndSplitCarNames() {
+        String userInput = InputView.requestUserInput();
+        return splitUserInputByComma(userInput);
+    }
+
+    private String[] splitUserInputByComma(String userInput) {
+        return userInput.split(",");
     }
 }
