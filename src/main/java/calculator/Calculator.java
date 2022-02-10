e51e7b574c2ed091c2c35bad65fc97e5468309ef
meatsby <meatsby@gmail.com>
2022-02-10 14:29:21 +0900

    feat: 숫자 배열 중 음수가 있는지 확인하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,9 +4,18 @@ public class Calculator {
 	public int sum(int[] nums) {
 		int total = 0;
 
+		checkNegative(nums);
 		for (int num : nums) {
 			total += num;
 		}
 		return total;
+	}
+
+	private void checkNegative(int[] nums) {
+		for (int num : nums) {
+			if (num < 0) {
+				throw new RuntimeException("음수 불가능");
+			}
+		}
 	}
 }
