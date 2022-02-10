a425938ce920381d329ab2426f7799cb4238f5d7
Donggyu Kim <a29661498@gmail.com>
2022-02-10 15:43:20 +0900

    refactor: StringSplitter 클래스를 통해서 문자열 분리하는 부분 추출
    StringSplitter 클래스를 별도로 두어서 문자열 분리하는 책임을 위임

---
+++ src/main/java/calculator/StringSplitter.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringSplitter {
+
+    public String[] parseString(String str){
+        if(str.contains(",|:")){
+            return defaultSplit(str);
+        }
+
+        return customSplit(str);
+    }
+
+    private String[] defaultSplit(String str) {
+        return str.split(",|:");
+    }
+
+    private String[] customSplit(String str) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
+        String customDelimiter = matcher.group(1);
+        String[] tokens = matcher.group(2).split(customDelimiter);
+
+        return tokens;
+    }
+
+}
