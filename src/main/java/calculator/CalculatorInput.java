6f291a3fb58952a116ba4813b86242666f99a0b9
yxxnghwan <younghwan960@gmail.com>
2022-02-10 20:35:39 +0900

    refactor(calculator): CalculatorInput 클래스 상수화 작업

--- src/main/java/calculator/CalculatorInput.java
+++ src/main/java/calculator/CalculatorInput.java
@@ -1,13 +1,15 @@
 package calculator;
 
 public class CalculatorInput {
-	private final String INPUT_MESSAGE = "입력: ";
+	private static final String CUSTOM_SEPARATOR_START_SIGNATURE = "//";
+	private static final String CUSTOM_SEPARATOR_END_SIGNATURE = "\n";
+	private static final String INPUT_MESSAGE = "입력: ";
 
 	public String userInput() {
 		System.out.print(INPUT_MESSAGE);
 		String stringInput = CalculatorMain.sc.nextLine();
-		if (stringInput.contains("//")) {
-			return stringInput + '\n' + CalculatorMain.sc.nextLine();
+		if (stringInput.contains(CUSTOM_SEPARATOR_START_SIGNATURE )) {
+			return stringInput + CUSTOM_SEPARATOR_END_SIGNATURE + CalculatorMain.sc.nextLine();
 		}
 		return stringInput;
 	}
