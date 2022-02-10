dcb214bb725e6441a790d8f0c9812ee225ccfbd0
skullKim <kyk990328@gmail.com>
2022-02-10 20:13:47 +0900

    test(InputValidationTest.java): 테스트 추가
    이름 데이터를 검증하는 기능에 대한 테스트 추가
    
    유효하지 않은 이름(6자 이상)이 왔을 때 예외를 던지는지 확인하는 테스트
    추가

--- src/test/java/racingcar/utils/InputValidationTest.java
+++ src/test/java/racingcar/utils/InputValidationTest.java
@@ -1,5 +1,6 @@
 package racingcar.utils;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -15,14 +16,24 @@ class InputValidationTest {
 	@Nested
 	@DisplayName("이름 문자열이 주어졌을 때 ")
 	class NameTest {
-		InputValidation validation = mock(InputValidation.class);
 
 		@DisplayName("옳바른 형식이면 통과한다.")
 		@ParameterizedTest(name = "{index} {displayName} names={0}")
 		@ValueSource(strings = {"pobi,crong,honux", "pobi"})
 		void checkRightName(final String names) {
+			InputValidation validation = mock(InputValidation.class);
 			validation.validateName(names);
 			verify(validation).validateName(names);
+		}
+
+		@DisplayName("5자리 이상의 문자열이 주어지면 exception이 발생한다.")
+		@ParameterizedTest(name = "{index} {displayName} names={0}")
+		@ValueSource(strings = {"pobi,crong,honuxxx", "pobixxxx"})
+		void checkWrongName(final String names) {
+			InputValidation validation = new InputValidation();
+			assertThatExceptionOfType(RuntimeException.class)
+				.isThrownBy(() -> validation.validateName(names))
+				.withMessageMatching("이름은 5자리 이하만 가능합니다.");
 		}
 	}
 
