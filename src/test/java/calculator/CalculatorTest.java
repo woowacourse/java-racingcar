16f3b8eb1c2da6eba414403c41243eddf13f0ed1
Donggyu Kim <a29661498@gmail.com>
2022-02-10 14:46:43 +0900

    test : 계산기 요구사항 6
    음수를 입력받은 경우 RuntimeException 발생 여부 확인

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
 
@@ -37,5 +38,11 @@ public class CalculatorTest {
     public void splitAndSum_custom_구분자() throws Exception {
         int result = Calculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> Calculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
