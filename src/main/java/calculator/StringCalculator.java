ded1a80632001b6cfce29b3bbfa6c9068e0fe50a
Gyuchool <balance0591@naver.com>
2022-02-10 23:23:18 +0900

    feat: 커스텀 구분자를 통한 덧셈 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,29 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public static int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
         int sum = 0;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] separatedValues = m.group(2).split(customDelimiter);
+            return getSum(separatedValues, sum);
+        }
+
         String[] separatedValues = input.split(",|:");
+        return getSum(separatedValues, sum);
+    }
+
+    private static int getSum(String[] separatedValues, int sum) {
         for (String separatedValue : separatedValues) {
             sum += Integer.parseInt(separatedValue);
         }
-        return Integer.parseInt(input);
+        return sum;
     }
 }
