5c7e0eaf809da01402758d83b25a72ab52e29fa6
jayjaehunchoi <wogns0108@naver.com>
2022-02-10 17:07:02 +0900

    feat : Output 클래스 생성

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+public class Output {
+
+    private static String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static String INPUT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static String RESULT_MESSAGE = "실행 결과";
+
+    public static void printCarNamesMessage() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+    }
+
+    public static void printNumberMessage() {
+        System.out.println(INPUT_NUMBER_MESSAGE);
+    }
+
+    public static void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
