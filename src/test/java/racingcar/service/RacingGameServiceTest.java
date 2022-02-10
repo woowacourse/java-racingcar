4bbe1de92aba69bcad1b580e532514bec7861ac8
wilgur513 <wilgur513@naver.com>
2022-02-10 15:42:55 +0900

    feat: find winners method

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -17,6 +17,7 @@ public class RacingGameServiceTest {
         static final int[] RANDOM_NUMBERS = {
             FORWARDING_NUMBER, STOPPING_NUMBER,
             FORWARDING_NUMBER, STOPPING_NUMBER,
+            STOPPING_NUMBER, FORWARDING_NUMBER,
             STOPPING_NUMBER, FORWARDING_NUMBER
         };
         int index = 0;
@@ -27,34 +28,58 @@ public class RacingGameServiceTest {
         }
     }
 
+    private static final String CAR_1_Name = "pobi";
+    private static final String CAR_2_Name = "jason";
+
     CarRepository carRepository;
     RacingGameService racingGameService;
+    Car car1;
+    Car car2;
 
     @BeforeEach
     public void setUp() {
         carRepository = new CarRepository();
-        carRepository.save(Car.from("pobi"));
-        carRepository.save(Car.from("jason"));
+        car1 = Car.from(CAR_1_Name);
+        car2 = Car.from(CAR_2_Name);
+        carRepository.save(car1);
+        carRepository.save(car2);
         racingGameService = new RacingGameService(carRepository, new MockRandomNumberGenerator());
     }
 
     @Test
     public void 한_턴의_게임진행() {
         racingGameService.proceedTurn();
-        assertThat(carRepository.findByName("pobi").getPosition()).isEqualTo(1);
-        assertThat(carRepository.findByName("jason").getPosition()).isEqualTo(0);
+        assertThat(carRepository.findByName(CAR_1_Name).getPosition()).isEqualTo(1);
+        assertThat(carRepository.findByName(CAR_2_Name).getPosition()).isEqualTo(0);
     }
 
     @Test
     public void 여러_턴의_게임진행() {
         racingGameService.proceedTurn();
-        assertThat(carRepository.findByName("pobi").getPosition()).isEqualTo(1);
-        assertThat(carRepository.findByName("jason").getPosition()).isEqualTo(0);
+        assertThat(carRepository.findByName(CAR_1_Name).getPosition()).isEqualTo(1);
+        assertThat(carRepository.findByName(CAR_2_Name).getPosition()).isEqualTo(0);
         racingGameService.proceedTurn();
-        assertThat(carRepository.findByName("pobi").getPosition()).isEqualTo(2);
-        assertThat(carRepository.findByName("jason").getPosition()).isEqualTo(0);
+        assertThat(carRepository.findByName(CAR_1_Name).getPosition()).isEqualTo(2);
+        assertThat(carRepository.findByName(CAR_2_Name).getPosition()).isEqualTo(0);
         racingGameService.proceedTurn();
-        assertThat(carRepository.findByName("pobi").getPosition()).isEqualTo(2);
-        assertThat(carRepository.findByName("jason").getPosition()).isEqualTo(1);
+        assertThat(carRepository.findByName(CAR_1_Name).getPosition()).isEqualTo(2);
+        assertThat(carRepository.findByName(CAR_2_Name).getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void 단독우승자_조회() {
+        racingGameService.proceedTurn();
+        racingGameService.proceedTurn();
+        racingGameService.proceedTurn();
+        assertThat(racingGameService.getWinners()).contains(car1);
+    }
+
+    @Test
+    public void 우승자_조회() {
+        racingGameService.proceedTurn();
+        racingGameService.proceedTurn();
+        racingGameService.proceedTurn();
+        racingGameService.proceedTurn();
+        assertThat(racingGameService.getWinners()).contains(car1, car2);
     }
 }
