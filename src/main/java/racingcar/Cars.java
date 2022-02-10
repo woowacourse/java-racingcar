f5f7374d0e53cc67b7be3521343c75fc03d9be25
meatsby <meatsby@gmail.com>
2022-02-10 15:12:02 +0900

    feat: 자동차 객체에 대한 일급 컬렉션 및 전체 이동 명령 기능 구현

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars = new ArrayList<>();
+
+    public Cars(String[] names) {
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+}
