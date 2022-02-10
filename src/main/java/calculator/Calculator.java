06ad059f8037380a9543d1daca789264d52b6afc
Donggyu Kim <a29661498@gmail.com>
2022-02-10 14:26:36 +0900

    feat : 계산기 요구사항2
    숫자 하나만 입력하는 경우 해당 숫자 반환 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,7 +2,11 @@ package calculator;
 
 public class Calculator {
 
-    public static int splitAndSum(Object o) {
-        return 0;
+    public static int splitAndSum(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+
+        return Integer.parseInt(str);
     }
 }
