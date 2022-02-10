1bc99932d500af5988e0e004b9543e45ccb3b2e6
EastHShin <xldk78@gmail.com>
2022-02-10 14:10:18 +0900

    feat: 자동차 이름의 길이를 검증하는 기능 구현

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -1,2 +1,19 @@
-package carracing;public class Car {
+package carracing;
+
+public class Car {
+	private String name;
+
+	public Car(String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("자동차 이름의 길이는 5를 초과할 수 없습니다.");
+		}
+		if (name.length() < 1) {
+			throw new IllegalArgumentException("자동차 이름의 길이는 최소 한글자 이상이어야 합니다.");
+		}
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
