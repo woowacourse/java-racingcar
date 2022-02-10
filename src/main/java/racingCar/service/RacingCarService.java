e5a8d62ea45517066481f0a333cd637480ab2978
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-11 00:46:10 +0900

    refactor: racingCar List 기능 중복

--- src/main/java/racingCar/service/RacingCarService.java
+++ src/main/java/racingCar/service/RacingCarService.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import racingCar.repository.RacingCars;
+import racingCar.model.Car;
 import racingCar.utlis.Convertor;
 import racingCar.validator.NameValidator;
 
 public class RacingCarService {
 
-	RacingCars racingCars = new RacingCars();
+	private List<Car> cars = new ArrayList<>();
+	private int count;
 
 	public void saveCars(String carsNames) throws Exception {
 		List<String> carNameList = new ArrayList<>(Arrays.asList(Convertor.separateNamesByDelimiter(carsNames)));
 		NameValidator.checkOneName(carNameList);
 		NameValidator.checkDuplicatedName(carNameList);
-		racingCars.save(carNameList);
+		save(carNameList);
+	}
+
+	public void save(List<String> carNameList) throws Exception {
+		for (String name : carNameList) {
+			cars.add(new Car(name));
+		}
+	}
+
+	public void saveCount(int count) {
+		this.count = count;
 	}
 }
