4b06bf6ac20c7380ec016fd5a92783f9a99be829
Juhyung990122 <harukawa99@naver.com>
2022-02-10 11:25:41 +0900

    feat: 피연산자 유효성 검사 기능 구현

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private static final String POSITIVE_NUMBER_REGEX = "^[1-9]";
+    private static final String POSITIVE_NUMBER_REGEX = "^[0-9]";
 
     public int calculate(final String expression) {
         String[] tokens = splitExpression(expression);
@@ -24,23 +24,21 @@ public class StringCalculator {
     }
 
     private int[] parseToInts(final String[] tokens) {
+        validate(tokens);
         return Arrays.stream(tokens)
                 .mapToInt(Integer::parseInt)
                 .toArray();
     }
 
+
     private void validate(final String[] tokens) {
-            if (hasContainsNotPositiveNumber(tokens)) {
-                throw new RuntimeException("피연산자에 양수정의 가 아닌 값이 있습니다.");
-            }
+        if (hasContainsNotPositiveNumber(tokens)) {
+            throw new RuntimeException("피연산자에 양수정의 가 아닌 값이 있습니다.");
         }
+    }
 
-        private boolean hasContainsNotPositiveNumber(final String[] tokens) {
-            return Arrays.stream(tokens)
-                    .anyMatch(token -> !token.matches(POSITIVE_NUMBER_REGEX));
-        }
-
-//    private int add(int firstNum, int secondNum) {
-//        return firstNum + secondNum;
-//    }
+    private boolean hasContainsNotPositiveNumber(final String[] tokens) {
+        return Arrays.stream(tokens)
+                .anyMatch(token -> !token.matches(POSITIVE_NUMBER_REGEX));
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,16 +2,29 @@
 import StringCalculator.StringCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
+    private final StringCalculator stringCalculator = new StringCalculator();
+
     @Test
     @DisplayName("정수 파라미터 두개를 더해서 반환한다.")
-    void add() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int actual = stringCalculator.calculate("//;\n1;2;3");
+    void calculate() {
+        String expression = "//;\n1;2;3";
+        int actual = stringCalculator.calculate(expression);
         int expected = 6;
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("피연산자의 위치에 0 혹은 양의 정수가 아닌 값이 들어오면 예외를 발생시킨다.")
+    @ValueSource(strings = {"//;\n-1;2;3","//;\na;2;3"})
+    void calculate_NotPositiveOrZeroOperand(String expression) {
+        assertThatThrownBy(() -> stringCalculator.calculate(expression))
+                        .isInstanceOf(RuntimeException.class);
     }
 }
