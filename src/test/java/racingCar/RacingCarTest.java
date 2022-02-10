0bf521a40a35fc5343ed28053707f5738fe1ac3e
RunaNam <nmj5012@naver.com>
2022-02-10 16:32:40 +0900

    test: fix racingCarTest

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -5,33 +5,35 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class RacingCarTest {
+	private final RacingCar racingCar = new RacingCar();
+
 	@Test
 	void 자동차_이름_정상_입력() {
-		String[] names = RacingCar.getCarName("a,bqwer,cdb");
+		String[] names = racingCar.getCarName("a,bqwer,cdb");
 		assertThat(names).isEqualTo(new String[] {"a", "bqwer", "cdb"});
 	}
 
 	@Test
 	void 자동차_이름_중복() {
-		String[] names = RacingCar.getCarName("a,a,c");
+		String[] names = racingCar.getCarName("a,a,c");
 		assertThat(names).isEqualTo(new String[] {"a", "a", "c"});
 	}
 
 	@Test
 	void 자동차_이름_앞뒤_공백() {
-		String[] names = RacingCar.getCarName("   abc, a ,cde  ");
+		String[] names = racingCar.getCarName("   abc, a ,cde  ");
 		assertThat(names).isEqualTo(new String[] {"abc", "a", "cde"});
 	}
 
 	@Test
 	void 자동차_이름_6자_에러() {
-		assertThatThrownBy(() -> RacingCar.getCarName("woowahan,techCourse"))
+		assertThatThrownBy(() -> racingCar.isRightLength("techCourse"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	void 자동차_이름_공백_에러() {
-		assertThatThrownBy(() -> RacingCar.getCarName(",,a"))
+		assertThatThrownBy(() -> racingCar.isRightLength(""))
 			.isInstanceOf(RuntimeException.class);
 	}
 
