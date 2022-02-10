bf30a0fd32131f6319bf41e07c4946100417e329
cjlee38 <cjlee38@github.com>
2022-02-10 21:34:35 +0900

    feat(InputView): 시도할 회수(라운드)를 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String ROUND_NUMBER_PROMPT = "시도할 회수는 몇회인가요?";
 
     private final Scanner sc;
 
@@ -15,6 +16,11 @@ public class InputView {
 
     public String inputCarNames() {
         System.out.println(CAR_NAME_PROMPT);
+        return sc.nextLine();
+    }
+
+    public String inputRoundNumber() {
+        System.out.println(ROUND_NUMBER_PROMPT);
         return sc.nextLine();
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -19,4 +19,15 @@ public class InputViewTest extends ConsoleTest {
         Assertions.assertThat(outputStream.toString()).contains("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         Assertions.assertThat(carName).isEqualTo("pobi");
     }
+
+    @Test
+    void inputRoundNumber() {
+        changeOutput();
+        changeInput("5");
+        inputView = ViewConfig.getInputView();
+        String inputNumber = inputView.inputRoundNumber();
+
+        Assertions.assertThat(outputStream.toString()).contains("시도할 회수는 몇회인가요?");
+        Assertions.assertThat(inputNumber).isEqualTo("5");
+    }
 }
