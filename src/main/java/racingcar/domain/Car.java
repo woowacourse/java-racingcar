2a5c44d2f7d82c1e48e6993091a25370aec3f6a1
yxxnghwan <younghwan960@gmail.com>
2022-02-10 15:31:35 +0900

    feat(racing-car): Car 초기 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+public class Car {
+	private static final int PROCEED_FlAG_NUMBER = 4;
+
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		this.name = name;
+		this.position = 0;
+	}
+
+	public void proceed(int randomNumber) {
+		if (randomNumber >= PROCEED_FlAG_NUMBER) {
+			this.position++;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+}
