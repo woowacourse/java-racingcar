aac522a5255fb19a1558cbf3bb7982efb67382f4
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-11 01:08:17 +0900

    test: 랜덤값 추출 기능 테스트

---
+++ src/test/java/racingCar/utlis/UtilTest.java
@@ -0,0 +1,13 @@
+package racingCar.utlis;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class UtilTest {
+
+	@Test
+	public void 랜덤값_테스트() {
+		assertThat(Util.getRandomInt()).isBetween(0,9);
+	}
+
+}
