7b458a283f5face2d13ee992ec4af36b87112e73
EastHShin <xldk78@gmail.com>
2022-02-10 14:35:45 +0900

    feat: 자동차 이름이 중복인지 검증하는 기능 구현

--- src/main/java/carracing/Cars.java
+++ src/main/java/carracing/Cars.java
@@ -1,2 +1,18 @@
-package carracing;public class Cars {
+package carracing;
+
+import java.util.List;
+
+public class Cars {
+	private List<Car> cars;
+
+	public Cars(List<Car> cars) {
+		boolean isDuplicated = cars.stream()
+			.map(Car::getName)
+			.distinct()
+			.count() != cars.size();
+		if (isDuplicated) {
+			throw new IllegalArgumentException("자동차 이름은 중복이 되면 안됩니다.");
+		}
+		this.cars = cars;
+	}
 }
