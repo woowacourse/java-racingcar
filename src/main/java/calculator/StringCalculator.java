6645644737ae2dade69661b4927cd6458b362883
hyeonic <dev.hyeonic@gmail.com>
2022-02-10 11:32:34 +0900

    feat: splitAndSum 콜론 구분자 기능 추가 및 테스트

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,7 @@ package calculator;
 public class StringCalculator {
 
     public static int splitAndSum(String text) {
-        String[] numbers = text.split(",");
+        String[] numbers = text.split(",|:");
         int total = 0;
         for (String number : numbers) {
             total += Integer.parseInt(number);
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -13,4 +13,18 @@ class StringCalculatorTest {
         int result = StringCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
+
+    @DisplayName("문자열이 주어지면 `콜론(:)` 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
+    @Test
+    public void splitAndSum_콜론구분자() {
+        int result = StringCalculator.splitAndSum("1:2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("문자열이 주어지면 `쉼표(,)`와 `콜론(:)` 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
+    @Test
+    public void splitAndSum_쉼표와콜론구분자() {
+        int result = StringCalculator.splitAndSum("1:2,3");
+        assertThat(result).isEqualTo(6);
+    }
 }
