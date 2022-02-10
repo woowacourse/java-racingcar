1589f4dcad59b273aa0763ef3fc56ad532fe85b1
yxxnghwan <younghwan960@gmail.com>
2022-02-10 14:25:24 +0900

    test(racing-car): RoundNumberValidatorTest 작성

---
+++ src/test/java/racingcar/validator/RoundNumberValidatorTest.java
@@ -0,0 +1,19 @@
+package racingcar.validator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RoundNumberValidatorTest {
+	@Test
+	void 자연수_체크(){
+		assertThatThrownBy(()->RoundNumberValidator.validate(0))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(()->RoundNumberValidator.validate(1.33))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(()->RoundNumberValidator.validate("abc"))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(()->RoundNumberValidator.validate(-23))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}
