7bcd1cabde9e15b543015b747e9cacd870fe89b1
RunaNam <nmj5012@naver.com>
2022-02-10 12:04:46 +0900

    test: add nullTest

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -23,4 +23,16 @@ public class StringCalculatorTest {
 		assertThat(result).isEqualTo(0);
 	}
 
+	@Test
+	public void nullTest() {
+		int result = StringCalculator.add(null);
+		assertThat(result).isEqualTo(0);
+	}
+
+	@Test
+	public void customDividerTest() {
+		int result = StringCalculator.add("//;/n1;2");
+		assertThat(result).isEqualTo(3);
+	}
+
 }
