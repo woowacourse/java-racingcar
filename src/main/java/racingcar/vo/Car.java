74e5c1a6d51efe8c915cc2d5ab06a3bdea25f223
2young <yh20studio@gmail.com>
2022-02-10 21:41:53 +0900

    feat(car): 자동차의 실행 결과를 반환하는 기능 추가

--- src/main/java/racingcar/vo/Car.java
+++ src/main/java/racingcar/vo/Car.java
@@ -22,4 +22,9 @@ public class Car {
       position.increase();
     }
   }
+
+  @Override
+  public String toString() {
+    return name.get() + " : " + position.toString();
+  }
 }
--- src/main/java/racingcar/vo/Position.java
+++ src/main/java/racingcar/vo/Position.java
@@ -15,4 +15,13 @@ public class Position {
   public int get() {
     return position;
   }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < position; i++) {
+      builder.append("-");
+    }
+    return builder.toString();
+  }
 }
