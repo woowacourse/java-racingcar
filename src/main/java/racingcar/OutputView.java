16ae82cb16ff43e0f5215178354e77fb1f4e6c79
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 18:44:56 +0900

    refactor: 출력메시지 상수화

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String PRINT_WINNER_DELIMITER = ", ";
+    private static final String PRINT_WINNER_MESSAGE = "가 최종 우승했습니다.";
+
     public OutputView() {
     }
 
     public void printWinnerNames(List<String> winnerNames) {
-        System.out.println(join(", ", winnerNames) + "가 최종 우승했습니다.");
+        System.out.println(join(PRINT_WINNER_DELIMITER, winnerNames) + PRINT_WINNER_MESSAGE);
     }
 }
