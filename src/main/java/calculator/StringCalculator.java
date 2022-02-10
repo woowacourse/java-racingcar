5b5547cd07ca4d8239d49f76808b1ac230bb0f51
BeomsooKim <kbsat@naver.com>
2022-02-10 11:43:04 +0900

    feat(calculator): implements null or empty argument for splitAndSum method

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int splitAndSum(String targetString) {
+        if (targetString == null || targetString.isBlank()) {
+            return 0;
+        }
+    }
+}
