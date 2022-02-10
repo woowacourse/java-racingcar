16283452d12528973165980ba583f0ffa540bfda
pup-paw <wldusdl0310@gmail.com>
2022-02-10 22:59:37 +0900

    feat: 차 이름이 5글자 이상일 경우 에러 처리

--- src/main/java/racingcar/util/Checker.java
+++ src/main/java/racingcar/util/Checker.java
@@ -1,5 +1,7 @@
 package racingcar.util;
 
+import java.util.Arrays;
+
 public class Checker {
 
 	private void isNull(String str) throws IllegalArgumentException {
@@ -11,6 +13,15 @@ public class Checker {
 	private void isBlank(String str) throws IllegalArgumentException {
 		if (str.equals("")) {
 			throw new IllegalArgumentException(StringConst.ERROR_BLANK.getValue());
+		}
+	}
+
+	private void isOverSize(String[] str) throws IllegalArgumentException {
+		int overSizeCount = (int)Arrays.stream(str)
+			.filter(eachStr -> eachStr.length() > IntegerConst.SIZE_BOUND.getValue())
+			.count();
+		if (overSizeCount > IntegerConst.ZERO.getValue()) {
+			throw new IllegalArgumentException(StringConst.ERROR_SIZE.getValue());
 		}
 	}
 
