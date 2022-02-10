ba3d1419b0ac00cb0918958a844c0ea88e8ea6fa
BeomsooKim <kbsat@naver.com>
2022-02-10 12:10:20 +0900

    test(calculator): add DisplayName for each test case

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,8 +3,19 @@ package calculator;
 public class StringCalculator {
 
     public static int splitAndSum(String targetString) {
-        if (targetString == null || targetString.isBlank()) {
+        if (targetString == null || targetString.isBlank()){
             return 0;
         }
+
+        int sum = 0;
+        String[] splitResult = targetString.split(",");
+        for (String s : splitResult) {
+            sum += toInt(s);
+        }
+        return sum;
+    }
+
+    private static int toInt(String string) {
+        return Integer.parseInt(string);
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,14 +1,21 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.ParameterizedTest.*;
 
 public class StringCalculatorTest {
 
+    private final String PARAMETERIZED_TEST_DISPLAY_FORMAT =
+            DISPLAY_NAME_PLACEHOLDER + " ["
+            + ARGUMENTS_PLACEHOLDER + "]";
+
+    @DisplayName("splitAndSum 메서드는 null 혹은 빈 문자열이 입력된 경우 0을 반환한다.")
     @Test
     void splitAndSum_nullOrEmpty() {
         int result = StringCalculator.splitAndSum(null);
@@ -18,15 +25,17 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndSum 메서드는 숫자 하나가 문자열로 입력된 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"1", "2", "5"})
     void splitAndSum_oneNumber(String input) {
-        int result = StringCaculator.splitAndSum(input);
+        int result = StringCalculator.splitAndSum(input);
 
         assertThat(result).isEqualTo(toInt(input));
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndSum 메서드는 복수의 숫자를 쉼표(,) 구분자로 입력할 경우 숫자들의 합을 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @CsvSource(value = {"1,2|3", "1,2,3|6"}, delimiter = '|')
     void splitAndSum_commaSeparator(String input, String resultString) {
         int result = StringCalculator.splitAndSum(input);
@@ -34,15 +43,17 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(toInt(resultString));
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"1:3|4", "2:3|5"}, delimiter = '|')
+    @DisplayName("splitAndSum 메서드는 복수의 숫자를 콜론(:) 구분자로 입력할 경우 숫자들의 합을 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
+    @CsvSource(value = {"1:3|4", "2:3:4|9"}, delimiter = '|')
     void splitAndSum_colonSeparator(String input, String resultString) {
         int result = StringCalculator.splitAndSum(input);
 
         assertThat(result).isEqualTo(toInt(resultString));
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndSum 메서드는 다양한 구분자가 함께 입력되더라도 예외가 발생하지 않는다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @CsvSource(value = {"1,2:3|6", "3:4,5|12"}, delimiter = '|')
     void splitAndSum_multipleSeparators(String input, String resultString) {
         int result = StringCalculator.splitAndSum(input);
