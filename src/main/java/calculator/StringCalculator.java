d82495dcc51e53e7478152154b21d2e2a7e8646e
RunaNam <nmj5012@naver.com>
2022-02-10 12:38:15 +0900

    feat: add check null and emptyString

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,9 +2,12 @@ package calculator;
 
 public class StringCalculator {
 	public static int add(String input) {
+		if (input == null || input.equals("")) {
+			return 0;
+		}
 		String[] numbers = input.split(",|:");
 		int result = 0;
-		for(String number: numbers) {
+		for (String number : numbers) {
 			result += Integer.parseInt(number);
 		}
 		return result;
