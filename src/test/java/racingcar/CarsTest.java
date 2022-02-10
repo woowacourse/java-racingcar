50202031762dac95be16095eb85f0e552f80d93a
jayjaehunchoi <wogns0108@naver.com>
2022-02-10 17:02:18 +0900

    test : Cars test 케이스 추가

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,32 +1,46 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class CarsTest extends MockRandomGenerator{
+public class CarsTest extends MockRandomGenerator {
 
 
     @Test
-    void 자동차_우승_성공() {
+    void 자동차_우승_1명_성공() {
+        Car jaeCar = new Car("jae");
         Car rickCar = new Car("rick");
 
         for (int i = 0; i < 3; i++) {
             randomNumberOverFour();
             rickCar.goForward();
         }
-
+        closeMockRandom();
         Cars cars = new Cars();
-        cars.addCar(new Car("jae"));
+        cars.addCar(jaeCar);
         cars.addCar(rickCar);
 
         assertThat(cars.getWinners()).isEqualTo("rick가 최종 우승 했습니다.");
+    }
 
+    @Test
+    void 자동차_우승_2명_성공() {
+        Car jaeCar = new Car("jae");
+        Car rickCar = new Car("rick");
+
+        for (int i = 0; i < 3; i++) {
+            randomNumberOverFour();
+            rickCar.goForward();
+
+            randomNumberOverFour();
+            jaeCar.goForward();
+        }
         closeMockRandom();
+        Cars cars = new Cars();
+        cars.addCar(jaeCar);
+        cars.addCar(rickCar);
+
+        assertThat(cars.getWinners()).isEqualTo("jae, rick가 최종 우승 했습니다.");
     }
 }
