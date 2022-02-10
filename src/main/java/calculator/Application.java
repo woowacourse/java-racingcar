15d9170f39b2b5c70039c2026af7095f4c80b2c3
jaejae-yoo <wotj102@gmail.com>
2022-02-10 11:01:16 +0900

    feat : Application 클래스에 공백입력 기능생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,40 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        runCalculator(askInput());
+    }
 
+    public static int runCalculator(String input) {
+        if (isNotValidateInput(input)) {
+            return 0;
+        }
+    }
+
+    private static boolean isNotValidateInput(String input) {
+        if (isNullInput(input) || isEmptyInput(input) || isBlankInput(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isBlankInput(String input) {
+        return input.isBlank();
+    }
+
+    private static boolean isEmptyInput(String input) {
+        return input.isEmpty();
+    }
+
+    private static boolean isNullInput(String input) {
+        return input == null;
+    }
+
+    private static String askInput () {
+        return scanner.nextLine();
     }
 }
