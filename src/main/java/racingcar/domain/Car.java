f107cfdb7828e85bbb9d40a8fe9fe7ad92bfbe0d
Juhyung990122 <harukawa99@naver.com>
2022-02-10 17:16:31 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,18 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int INITIAL_DISTANCE = 0;
     private final CarName carName;
+    private int distance;
 
     public Car(String carName) {
         this.carName = new CarName(carName);
+        distance = INITIAL_DISTANCE;
+    }
+
+    public void move(boolean movement) {
+        if (movement) {
+            distance++;
+        }
     }
 }
