7c5e0b5c4f14eaac31fb063b7d5a933382ed26a6
woong7 <driedseas@gmail.com>
2022-02-10 16:34:27 +0900

    feat: 자동차 위치 이동

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,25 @@
 package racingcar;
 
 public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveOrHold(Boolean isMove) {
+        if (isMove) {
+            this.position += 1;
+        }
+    }
+
+    public void moveOrHold() {
+
+    }
 }
