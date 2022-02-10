27bf3ad32b315d3544adcde9813f611c3d2bd08d
Chan Kim <chankim@Chanui-MacBookPro.local>
2022-02-10 14:26:18 +0900

    feat : 시도 횟수 입력 기능 추가

--- src/main/java/racingcar/utils/TrialNumberValidator.java
+++ src/main/java/racingcar/utils/TrialNumberValidator.java
@@ -1,2 +1,25 @@
-package racingcar.utils;public class TrialNumberValidator {
+package racingcar.utils;
+
+public class TrialNumberValidator {
+    private static final int MIN_BOUND = 1;
+
+    public static void isEmpty(String trialNumber) {
+        if (trialNumber.equals("")) {
+            throw new IllegalArgumentException("시도 횟수가 빈 문자열 입니다.");
+        }
+    }
+
+    public static void isNotPositive(int trialNumber) {
+        if (trialNumber < MIN_BOUND) {
+            throw new IllegalArgumentException("시도 횟수가 1 이상이 아닙니다.");
+        }
+    }
+
+    public static void isNotInteger(String trialNumber) {
+        try {
+            Integer.parseInt(trialNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수에는 정수만 입력할 수 있습니다.");
+        }
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,31 @@
 package racingcar.view;
 
 import racingcar.utils.NameGenerator;
+import racingcar.utils.TrialNumberGenerator;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final Scanner in = new Scanner(System.in);
+
     public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분).");
-        Scanner in = new Scanner(System.in);
         try {
             return NameGenerator.generateCarNames(in.nextLine());
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] " + e.getMessage());
             return getCarNames();
+        }
+    }
+
+    public static int getTrialNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        try {
+            return TrialNumberGenerator.generateTrialNumber(in.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getTrialNumber();
         }
     }
 }
