53b6a516fee08cb053e10eb2c55cd9036c42256f
yxxnghwan <younghwan960@gmail.com>
2022-02-10 14:30:46 +0900

    fix(racing-car): RoundNumberValidatorTest 파라미터 타입 수정

--- src/test/java/racingcar/validator/RoundNumberValidatorTest.java
+++ src/test/java/racingcar/validator/RoundNumberValidatorTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 public class RoundNumberValidatorTest {
 	@Test
 	void 자연수_체크(){
-		assertThatThrownBy(()->RoundNumberValidator.validate(0))
+		assertThatThrownBy(()->RoundNumberValidator.validate("0"))
 			.isInstanceOf(IllegalArgumentException.class);
-		assertThatThrownBy(()->RoundNumberValidator.validate(1.33))
+		assertThatThrownBy(()->RoundNumberValidator.validate("1.33"))
 			.isInstanceOf(IllegalArgumentException.class);
 		assertThatThrownBy(()->RoundNumberValidator.validate("abc"))
 			.isInstanceOf(IllegalArgumentException.class);
-		assertThatThrownBy(()->RoundNumberValidator.validate(-23))
+		assertThatThrownBy(()->RoundNumberValidator.validate("-23"))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
