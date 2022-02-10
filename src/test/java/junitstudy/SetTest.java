009cc2b283a822587f5253527105b79d93d5af17
summerlunaa <summerlunaa9@gmail.com>
2022-02-10 11:19:36 +0900

    test : containsFalseTest 구현

--- src/test/java/junitstudy/SetTest.java
+++ src/test/java/junitstudy/SetTest.java
@@ -3,6 +3,7 @@ package junitstudy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,5 +32,11 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void containsTest(int argument) {
         assertThat(numbers.contains(argument)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsFalseTest(int argument, boolean expected) {
+        assertThat(numbers.contains(argument)).isEqualTo(expected);
     }
 }
