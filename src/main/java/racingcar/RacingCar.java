c4dda42b5ea8b9a45510250ef3c958056b9fc4f1
2young <yh20studio@gmail.com>
2022-02-10 17:17:43 +0900

    refactor(racingCar): cars 일급 컬렉션 리팩토링

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -2,18 +2,23 @@ package racingcar;
 
 import java.util.Scanner;
 import racingcar.vo.Attempt;
+import racingcar.vo.Car;
+import racingcar.vo.CarName;
+import racingcar.vo.Cars;
 
 public class RacingCar {
+
   private Scanner scanner;
 
   public RacingCar(Scanner scanner) {
-    this.scanner =  scanner;
+    this.scanner = scanner;
   }
 
   public void run() {
-    String carNames = readNextLine();
-    String[] names = splitCarNames(carNames);
-    Attempt attempt = new Attempt(readNextLine());
+    String carNamesInput = readNextLine();
+    Cars cars = createCars(splitCarNames(carNamesInput));
+    String attemptInput = readNextLine();
+    Attempt attempt = new Attempt(attemptInput);
   }
 
   public String readNextLine() {
@@ -22,5 +27,17 @@ public class RacingCar {
 
   public String[] splitCarNames(String carNames) {
     return carNames.split(",");
+  }
+
+  private Cars createCars(String[] names) {
+    Cars cars = new Cars();
+    for (String name : names) {
+      cars.add(createCar(name));
+    }
+    return cars;
+  }
+
+  private Car createCar(String name) {
+    return new Car(new CarName(name));
   }
 }
