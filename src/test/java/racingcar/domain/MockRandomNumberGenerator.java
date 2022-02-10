6fd8083b35e7befeaa4593302e09e7d2797e61cf
wilgur513 <wilgur513@naver.com>
2022-02-10 17:49:17 +0900

    test: extract MockRandomNumberGenerator class

---
+++ src/test/java/racingcar/domain/MockRandomNumberGenerator.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+public class MockRandomNumberGenerator implements RandomNumberGenerator {
+
+    static final int FORWARDING_NUMBER = 4;
+    static final int STOPPING_NUMBER = 0;
+    static final int[] RANDOM_NUMBERS = {
+        FORWARDING_NUMBER, STOPPING_NUMBER,
+        FORWARDING_NUMBER, STOPPING_NUMBER,
+        STOPPING_NUMBER, FORWARDING_NUMBER,
+        STOPPING_NUMBER, FORWARDING_NUMBER
+    };
+    int index = 0;
+
+    @Override
+    public int getRandomNumber() {
+        return RANDOM_NUMBERS[index++];
+    }
+}
