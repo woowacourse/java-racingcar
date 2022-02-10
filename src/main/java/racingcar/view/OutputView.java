4aded2eaae9d35f38a456e8840b8fbd559a12fbc
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-10 22:27:07 +0900

    feat(view): implement OutputView static methods for game logic messages

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,10 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.StringJoiner;
+
 public class OutputView {
 
     public static void printCarNamesInputRequestMessage() {
@@ -8,6 +13,40 @@ public class OutputView {
 
     public static void printTotalRoundsInputRequestMessage() {
         print("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printRoundResultText() {
+        print("\n실행 결과");
+    }
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            printCarNameCurrentPosition(car);
+        }
+        print("");
+    }
+
+    public static void printCarNameCurrentPosition(Car car) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(car.getName()).append(" : ");
+
+        for (int i = 0; i < car.getPosition(); i++) {
+            sb.append("-");
+        }
+
+        print(sb.toString());
+    }
+
+    public static void printWinners(List<Car> winners) {
+        StringJoiner joiner = new StringJoiner(", ");
+
+        for (Car winner : winners) {
+            joiner.add(winner.getName());
+        }
+
+        String winnerNames = joiner.toString();
+        print(winnerNames + "가 최종 우승했습니다.");
     }
 
     private static void print(String string) {
