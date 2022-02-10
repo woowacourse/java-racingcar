5d599d1928a89523d2060d6731f44df4a8321734
jaejae-yoo <wotj102@gmail.com>
2022-02-10 22:41:30 +0900

    test: 시도 횟수 NULL 입력시 예외 테스트 함수 작성

--- src/test/java/racingcargame/utils/RaceCountInputValidatorTest.java
+++ src/test/java/racingcargame/utils/RaceCountInputValidatorTest.java
@@ -7,8 +7,16 @@ public class RaceCountInputValidatorTest {
     @Test
     void 경주_횟수_숫자가_아닌_입력_예외_테스트() {
         assertThatThrownBy(() -> {
-            RaceCountInputValidator.validateRaceCount("a");
+            RaceCountInputValidator.validateRaceCount("3a");
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[error] 시도 횟수는 숫자만 입력해주세요. ");
+                .hasMessageContaining("[error] 경주횟수는 0부터 9까지의 숫자만 입력해주세요.");
+    }
+
+    @Test
+    void 경주_횟수_NULL_입력_예외_테스트() {
+        assertThatThrownBy(() -> {
+            RaceCountInputValidator.validateRaceCount(null);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[error] 경주횟수를 입력해주세요.");
     }
 }
