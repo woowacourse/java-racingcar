153cce87a660decf8a9980554cb4cb323576049e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 14:02:04 +0900

    refactor: 최대 랜덤 값 상수화

--- src/main/java/racingcar/RandomGenerator.java
+++ src/main/java/racingcar/RandomGenerator.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class RandomGenerator {
 
     private static final Random random = new Random();
+    private static final int MAX_NUMBER = 10;
 
     public int generateRandomNumber() {
-        return random.nextInt(10);
+        return random.nextInt(MAX_NUMBER);
     }
 }
