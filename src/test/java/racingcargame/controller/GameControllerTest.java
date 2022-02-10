58eecd341ee6122cce7ea35bf25dbf095f544f72
jaejae-yoo <wotj102@gmail.com>
2022-02-10 17:51:08 +0900

    test: 자동차 이름 5자 초과 입력 예외 처리함수 작성

--- src/test/java/racingcargame/controller/GameControllerTest.java
+++ src/test/java/racingcargame/controller/GameControllerTest.java
@@ -23,4 +23,12 @@ public class GameControllerTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[error] 입력된 자동차 이름 중 이름이 없는 자동차가 있습니다.");
     }
+
+    @Test
+    void 자동차_이름_5자초과_입력_예외_테스트() {
+        assertThatThrownBy(() -> {
+            NameInputValidator.validateCarNames("p,kdrejkr,d");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[error] 자동차 이름을 5자 이하로 입력해주세요.");
+    }
 }
