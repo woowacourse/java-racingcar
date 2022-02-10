3681cfc04b5e7f4bf5be93b08ed601802d100595
wilgur513 <wilgur513@naver.com>
2022-02-10 11:11:27 +0900

    feat: throw exception by float try count

--- src/main/java/racingcar/NumberParser.java
+++ src/main/java/racingcar/NumberParser.java
@@ -3,6 +3,9 @@ package racingcar;
 public class NumberParser {
 
     public int parse(String input) {
+        if (input.matches("(-|\\+)?[0-9]+\\.[0-9]+")) {
+            throw new IllegalArgumentException("시도 횟수는 실수가 될 수 없습니다.");
+        }
         int tryCount = Integer.parseInt(input);
         if (tryCount < 0) {
             throw new IllegalArgumentException("시도 횟수는 음수가 될 수 없습니다.");
