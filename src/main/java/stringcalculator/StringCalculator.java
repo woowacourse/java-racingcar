346a175853dcf67f6aac0f8962057ab101ed104a
YJGwon <yj970125@gmail.com>
2022-02-10 11:55:31 +0900

    feat: 입력값이 null값 또는 빈 문자열인지 판별하여 0을 반환
    StringCalculator.java
       - splitAndSum() 추가
       - castStringToInts(String input): String 판별해서 int Array로 cast
       - calculate(int[] numbers): cast된 배열의 총합을 계산

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,4 +1,27 @@
 package stringcalculator;
 
 public class StringCalculator {
+    public static int splitAndSum(String input) {
+        return calculate(castStringToInts(input));
+    }
+
+    private static int[] castStringToInts(String input) {
+        if (isNullOrEmpty(input)) {
+            return new int[] {0};
+        }
+        return null;
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static int calculate(int[] numbers) {
+        //numbers를 계산
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
 }
