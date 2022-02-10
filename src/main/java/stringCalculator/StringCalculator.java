ac992e93ea91b8a7694f2dca78f910c12325531b
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-10 16:17:27 +0900

    feat: 각 숫자의 합 반환 기능 구현

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -4,15 +4,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-
 	public String[] splitString(String str) {
+		if (str == null || str.length() == 0)
+			return new String[] {"0"};
 		String regex = ",|:";
-		Matcher m= Pattern.compile("//(.)\n(.*)").matcher(str);
-		if (m.find()){
+		CheckException.checkSplitOk(str, regex);
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+		if (m.find()) {
 			regex += "|" + m.group(1);
+			CheckException.checkSplitOk(str, regex);
+			return m.group(2).split(regex);
 		}
-		return m.group(2).split(regex);
+		return str.split(regex);
 	}
-	//https://m.blog.naver.com/bb_/220863282423
-	//https://regexr.com/5mhou
+
+	public int sumString(String[] splitStringArray) {
+		int sumResult = 0;
+		CheckException.check(splitStringArray);
+		for (String s : splitStringArray) {
+			sumResult += Integer.parseInt(s);
+		}
+		return sumResult;
+	}
+	/*
+	^[regex]
+	((.)[regex]{2,}(.)*
+	$[regex]
+	**/
 }
