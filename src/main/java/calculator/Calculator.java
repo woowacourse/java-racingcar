c3b797b280b7902309a21afcb6ec5c6db108c2e4
Donggyu Kim <a29661498@gmail.com>
2022-02-10 14:43:17 +0900

    feat: 계산기 요구사항5
    커스텀 구분자(`//` 와 `\n` 사이)로 구분해서 합을 구하도록 splitAndSum 메소드 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     public static int splitAndSum(String str) {
@@ -9,10 +12,29 @@ public class Calculator {
             return 0;
         }
 
-        String[] split = str.split(",|:");
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
 
-        for (String s : split) {
-            sum += Integer.parseInt(s);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] tokens = matcher.group(2).split(customDelimiter);
+
+            sum = getSum (sum, tokens);
+
+            return sum;
+        }
+
+        String[] tokens = str.split(",|:");
+
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+
+        return sum;
+    }
+
+    private static int getSum(int sum, String[] tokens) {
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
         }
 
         return sum;
