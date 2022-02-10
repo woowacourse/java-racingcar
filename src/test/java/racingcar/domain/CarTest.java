52254f5b2fc39c123ea23775bc7d27b3034b662f
wilgur513 <wilgur513@naver.com>
2022-02-10 14:03:52 +0900

    test: add stopping car test

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,12 +7,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "자동차 전진 - 난수 : {0}")
     @ValueSource(ints = {4, 5, 6})
     public void 자동차_전진(int randomNumber) {
         Car car = Car.from("pobi");
         int prevPosition = car.getPosition();
         car.move(randomNumber);
         assertThat(car.getPosition()).isEqualTo(prevPosition + 1);
+    }
+
+    @ParameterizedTest(name = "자동차 정지 - 난수 : {0}")
+    @ValueSource(ints = {1, 2, 3, -1})
+    public void 자동차_정지(int randomNumber) {
+        Car car = Car.from("pobi");
+        int prevPosition = car.getPosition();
+        car.move(randomNumber);
+        assertThat(car.getPosition()).isEqualTo(prevPosition);
     }
 }
