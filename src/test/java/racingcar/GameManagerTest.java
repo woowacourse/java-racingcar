12802b705b8746576f8947c572da7e6320f785a3
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-10 21:47:32 +0900

    "test(GameManager): setup getValidateTotalRounds pass and exception cases"

--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -1,13 +1,15 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameManagerTest {
 
@@ -19,13 +21,13 @@ public class GameManagerTest {
 
         String[] splitResult = (String[]) privateMethod.invoke(gameManager, "a,bc,bacon");
 
-        assertThat(splitResult).containsExactly("a","bc","bacon");
+        assertThat(splitResult).containsExactly("a", "bc", "bacon");
     }
 
     @Test
     void initCars() throws Exception {
         Method privateMethod = getPrivateMethod("initCars", String[].class);
-        String[] carNames = new String[] {"a","bc","bacon"};
+        String[] carNames = new String[]{"a", "bc", "bacon"};
 
         privateMethod.invoke(gameManager, (Object) carNames);
 
@@ -36,10 +38,45 @@ public class GameManagerTest {
         }
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "10"})
+    void getValidateTotalRounds_passesOnPositiveIntString(String totalRoundsInput) throws Exception {
+        Method validateTotalRoundsMethod = getValidateTotalRoundsMethod();
+
+        assertThatNoException().isThrownBy(
+                () -> validateTotalRoundsMethod.invoke(gameManager, totalRoundsInput)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"한글", "eng", "$", "1.0"})
+    void getValidateTotalRounds_errorOnNonInteger(String totalRoundsInput) throws Exception {
+        Method validateTotalRoundsMethod = getValidateTotalRoundsMethod();
+
+        assertThatThrownBy(
+                () -> validateTotalRoundsMethod.invoke(gameManager, totalRoundsInput)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0"})
+    void getValidateTotalRounds_errorOnNonPositiveInteger(String totalRoundsInput) throws Exception {
+        Method validateTotalRoundsMethod = getValidateTotalRoundsMethod();
+
+        assertThatThrownBy(
+                () -> validateTotalRoundsMethod.invoke(gameManager, totalRoundsInput)
+        );
+    }
+
     private Method getPrivateMethod(String methodName, Class<?>... argsType) throws NoSuchMethodException {
         Method privateMethod = gameManager.getClass()
                 .getDeclaredMethod(methodName, argsType);
         privateMethod.setAccessible(true);
         return privateMethod;
+    }
+
+    private Method getValidateTotalRoundsMethod() throws NoSuchMethodException {
+        String methodName = "getValidateTotalRounds";
+        return getPrivateMethod(methodName, String.class);
     }
 }
