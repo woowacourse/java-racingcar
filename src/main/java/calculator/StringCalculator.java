d28c754d7a97578491793eb4a2b7c61f3b1a6420
Wishoon <heonga2@gmail.com>
2022-02-10 20:15:13 +0900

    refactor: 문자열 덧셈 계산기 리팩토링

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,31 +6,23 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final String DEFAULT_DELIMITERS = ",|:";
-    private static final String CUSTOM_DELIMITERS = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITERS = Pattern.compile("//(.)\n(.*)");
 
-    public static int splitAndSum(String value) {
+    public static int splitAndSum(final String value) {
         if (value == null || value.isEmpty()) {
             return 0;
         }
-
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITERS).matcher(value);
-
+        Matcher matcher = CUSTOM_DELIMITERS.matcher(value);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            String[] token = matcher.group(2).split(customDelimiter + "|" + DEFAULT_DELIMITERS);
-
-            return calculator(token);
+            return calculator(matcher.group(2), DEFAULT_DELIMITERS + "|" + CUSTOM_DELIMITERS);
         }
-        String[] values = value.split(DEFAULT_DELIMITERS);
-        return calculator(values);
+        return calculator(value, DEFAULT_DELIMITERS);
     }
 
-    private static int calculator(final String[] values) {
-        validateAllPositive(values);
-        int sum = Arrays.stream(values)
-            .mapToInt(Integer::parseInt)
-            .sum();
-        return sum;
+    private static int calculator(final String input, final String delimiters) {
+        final String[] numbers = input.split(delimiters);
+        validateAllPositive(numbers);
+        return addAll(numbers);
     }
 
     private static void validateAllPositive(final String[] numbers) {
@@ -43,5 +35,11 @@ public class StringCalculator {
         return Arrays.stream(numbers)
             .map(Integer::parseInt)
             .anyMatch(number -> number < 0);
+    }
+
+    private static int addAll(String[] numbers) {
+        return Arrays.stream(numbers)
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
