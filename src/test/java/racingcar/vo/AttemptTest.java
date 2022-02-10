d4c992d5df400b6ae8225bd2307df7438d49453c
2young <yh20studio@gmail.com>
2022-02-10 22:28:43 +0900

    test(cars): 시도할 회수만큼 경주하는 기능 테스트

--- src/test/java/racingcar/vo/AttemptTest.java
+++ src/test/java/racingcar/vo/AttemptTest.java
@@ -12,4 +12,27 @@ public class AttemptTest {
     Attempt attempt = new Attempt(input);
     assertThat(attempt.get()).isEqualTo(3);
   }
+
+  @Test
+  public void isLeft_test() throws Exception {
+    String inputZero = "0";
+    String inputOne = "1";
+    Attempt attemptZero = new Attempt(inputZero);
+    Attempt attemptOne = new Attempt(inputOne);
+    assertThat(attemptZero.isLeft()).isFalse();
+    assertThat(attemptOne.isLeft()).isTrue();
+  }
+
+  @Test
+  public void decrease_test() throws Exception {
+    String input = "3";
+    Attempt attempt = new Attempt(input);
+    assertThat(attempt.get()).isEqualTo(3);
+    attempt.decrease();
+    assertThat(attempt.get()).isEqualTo(2);
+    attempt.decrease();
+    assertThat(attempt.get()).isEqualTo(1);
+    attempt.decrease();
+    assertThat(attempt.get()).isEqualTo(0);
+  }
 }
--- src/test/java/racingcar/vo/CarsTest.java
+++ src/test/java/racingcar/vo/CarsTest.java
@@ -2,6 +2,8 @@ package racingcar.vo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
@@ -14,5 +16,24 @@ public class CarsTest {
     Cars cars = new Cars();
     cars.add(car);
     assertThat(cars.isSize(1)).isTrue();
+  }
+
+  @Test
+  public void repeatRace_test() throws Exception {
+    String name = "name1";
+    CarName carName = new CarName(name);
+    Car car = new Car(carName);
+    Cars cars = new Cars();
+    cars.add(car);
+    Attempt attempt = new Attempt("5");
+    String result = cars.repeatRace(attempt);
+    Matcher matcher = Pattern.compile(name).matcher(result);
+    int nameCount = 0;
+    while (matcher.find()) {
+      nameCount++;
+    }
+    assertThat(attempt.isLeft()).isFalse();
+    assertThat(result).contains("실행 결과", name);
+    assertThat(nameCount).isEqualTo(5);
   }
 }
