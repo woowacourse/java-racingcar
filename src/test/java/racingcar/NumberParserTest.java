c811a9337d22e8a7ddb18168d6c620b815adb1b3
wilgur513 <wilgur513@naver.com>
2022-02-10 11:11:27 +0900

    feat: throw exception by float try count

--- src/test/java/racingcar/NumberParserTest.java
+++ src/test/java/racingcar/NumberParserTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberParserTest {
 
@@ -23,6 +25,13 @@ public class NumberParserTest {
     @Test
     public void 음수_숫자_예외_발생() {
         assertThatThrownBy(() -> parser.parse("-1"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1.234", "-123.2345", "+12.674"})
+    public void 자연수가_아닐때_예외_발생(String input) {
+        assertThatThrownBy(() -> parser.parse(input))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
