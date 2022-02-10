bf07e99e62a9165294582ae6ff18d3d53470a920
Wishoon <heonga2@gmail.com>
2022-02-10 11:30:37 +0900

    test: 집합 원소 포함 확인 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -25,5 +27,12 @@ public class SetTest {
     @DisplayName("집합 크기 확인")
     public void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("집합 원소 포함 확인")
+    public void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
