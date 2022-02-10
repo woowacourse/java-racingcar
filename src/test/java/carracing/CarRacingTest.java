0f7aca58872d682e6386128bd28035dd49c52c17
EastHShin <xldk78@gmail.com>
2022-02-10 14:05:17 +0900

    test: 자동차 이름 길이 검증 테스트 코드 작성

--- src/test/java/carracing/CarRacingTest.java
+++ src/test/java/carracing/CarRacingTest.java
@@ -1,2 +1,27 @@
-package carracing;public class CarRaingTest {
+package carracing;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarRacingTest {
+
+	@Test
+	@DisplayName("자동차 이름의 길이가 5를 초과하는 경우 에러 발생")
+	void validate_자동차이름_길이() {
+		assertThatThrownBy(() -> {
+			new Car("pobbiii");
+		}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차 이름의 길이는 5를 초과할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("자동차 이름의 길이가 1미만인 경우 에러 발생")
+	void validate_자동차이름_길이기준_미달() {
+			assertThatThrownBy(() -> {
+				new Car("");
+			}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("자동차 이름의 길이는 최소 한글자 이상이어야 합니다.");
+	}
 }
