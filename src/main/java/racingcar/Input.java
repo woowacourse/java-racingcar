87402126dd0afab93c8c40f25414421046e3005b
jayjaehunchoi <wogns0108@naver.com>
2022-02-10 15:27:05 +0900

    feat : 횟수 입력 기능 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -6,14 +6,19 @@ import static racingcar.Validator.*;
 
 public class Input {
 
+    private static Scanner scanner = new Scanner(System.in);
     private static String SPLIT_DELIMITER = ",";
 
     public static String[] insertRightName() {
-        Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         validateBlank(input);
         String[] names = input.split(SPLIT_DELIMITER);
         validateEachCarName(names);
         return names;
+    }
+
+    public static int insertRightNumber() {
+        String input = scanner.next();
+        return toInt(input);
     }
 }
--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.Input.*;
+
 
 public class InputTest {
 
@@ -15,8 +18,30 @@ public class InputTest {
     @Test
     void 이름_입력_성공() {
         systemIn("ab,cd,ef");
-        String[] names = Input.insertRightName();
-        Assertions.assertThat(names).contains("ab", "cd", "ef");
+        String[] names = insertRightName();
+        assertThat(names).contains("ab", "cd", "ef");
     }
 
+    @Test
+    void 이름_입력_실패() {
+        systemIn("abbbbbb,cd,ef,g,h,i");
+        assertThatThrownBy(() -> insertRightName())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차는 5대 이하이어야 합니다.");
+    }
+
+    @Test
+    void 횟수_입력_성공() {
+        systemIn("5");
+        int count = insertRightNumber();
+        assertThat(count).isEqualTo(5);
+    }
+
+    @Test
+    void 횟수_입력_실패() {
+        systemIn("a");
+        assertThatThrownBy(() -> insertRightNumber())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 횟수는 숫자로 입력해야 합니다.");
+    }
 }
