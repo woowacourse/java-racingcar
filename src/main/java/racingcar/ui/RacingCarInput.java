d7ca5a7a6a7f82d5b3fdca9edb500f6db7fedf64
yxxnghwan <younghwan960@gmail.com>
2022-02-10 17:30:21 +0900

    feat(racing-car): 잘못된 값 입력 시 재입력받도록 수정

--- src/main/java/racingcar/ui/RacingCarInput.java
+++ src/main/java/racingcar/ui/RacingCarInput.java
@@ -2,19 +2,38 @@ package racingcar.ui;
 
 import static racingcar.RacingCarMain.*;
 
+import java.util.List;
+
+import racingcar.domain.Car;
+import racingcar.utils.CarsGenerator;
+import racingcar.validator.RoundNumberValidator;
+
 public class RacingCarInput {
 	private static final String CAR_NAME_INPUT_MESSAGE
 		= "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String ROUND_INPUT_MESSAGE
 		= "시도할 회수는 몇회인가요?";
 
-	public static String carNameInput() {
+	public static List<Car> carNameInput() {
 		System.out.println(CAR_NAME_INPUT_MESSAGE);
-		return sc.nextLine();
+		String userInput = sc.nextLine();
+		try {
+			return CarsGenerator.generateCars(userInput);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return carNameInput();
+		}
 	}
 
-	public static String roundInput() {
+	public static int roundInput() {
 		System.out.println(ROUND_INPUT_MESSAGE);
-		return sc.nextLine();
+		String roundNumberString = sc.nextLine();
+		try {
+			RoundNumberValidator.validate(roundNumberString);
+			return Integer.parseInt(roundNumberString);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return roundInput();
+		}
 	}
 }
