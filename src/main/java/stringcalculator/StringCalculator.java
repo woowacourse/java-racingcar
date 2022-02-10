6f05d548bee05730e7c13d13109d54580067595d
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-02-10 11:36:18 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,10 +1,27 @@
 package stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 	public StringCalculator() {
 	}
 
 	public String[] split(String input) {
 		return input.split(",|:");
+	}
+
+	public String[] customDelimiter(String input) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+		if (!m.find()) {
+			throw new RuntimeException();
+		}
+		String customDelimiter = m.group(1);
+		if (customDelimiter.equals("+") || customDelimiter.equals("*") || customDelimiter.equals("^")) {
+			StringBuffer prefix = new StringBuffer("\\");
+			StringBuffer buffer = prefix.append(customDelimiter);
+			return m.group(2).split(buffer.toString());
+		}
+		return m.group(2).split(customDelimiter);
 	}
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -2,7 +2,6 @@ package stringcalculator;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
@@ -18,5 +17,19 @@ public class StringCalculatorTest {
 	void 콜론_구분자() {
 		String[] expected = {"1", "2"};
 		assertThat(calculator.split("1:2")).isEqualTo(expected);
+	}
+
+	@Test
+	void 커스텀_구분자() {
+		// java.util.regex 패키지의 Matcher, Pattern import
+		String input = "//+\n1+2+3"; // 예약어인 +, *, ^ 는 그냥 쓸 경우 오류
+		String input2 = "//;\n1;2;3";
+
+		String[] actual = calculator.customDelimiter(input);
+		String[] actual2 = calculator.customDelimiter(input2);
+
+		String[] expected = {"1", "2", "3"};
+		assertThat(actual).isEqualTo(0);
+		assertThat(actual2).isEqualTo(expected);
 	}
 }
