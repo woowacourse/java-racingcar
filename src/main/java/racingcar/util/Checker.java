d57953766299f608222a61a988cccc57b902bc0c
pup-paw <wldusdl0310@gmail.com>
2022-02-10 22:58:52 +0900

    feat: 입력이 공백인 경우 에러 처리

--- src/main/java/racingcar/util/Checker.java
+++ src/main/java/racingcar/util/Checker.java
@@ -8,4 +8,10 @@ public class Checker {
 		}
 	}
 
+	private void isBlank(String str) throws IllegalArgumentException {
+		if (str.equals("")) {
+			throw new IllegalArgumentException(StringConst.ERROR_BLANK.getValue());
+		}
+	}
+
 }
