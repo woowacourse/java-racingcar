693a7f2f79ae1151ec198c364bdfbab8c13c800a
2young <yh20studio@gmail.com>
2022-02-10 14:12:55 +0900

    feat(racingCar): 자동차 이름을 입력받는 기능 추가

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class RacingCar {
+
+  public static String readCarNames() {
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextLine();
+  }
+}
