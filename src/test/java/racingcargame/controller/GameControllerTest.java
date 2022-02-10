5fb408a23244c22732df14f1cba3b318b614a058
jaejae-yoo <wotj102@gmail.com>
2022-02-10 17:48:48 +0900

    test: 자동차 이름 빈칸  입력 예외 처리함수 작성

--- src/test/java/racingcargame/controller/GameControllerTest.java
+++ src/test/java/racingcargame/controller/GameControllerTest.java
@@ -1,6 +1,8 @@
 package racingcargame.controller;
 
 import org.junit.jupiter.api.Test;
+import racingcargame.utils.NameInputValidator;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class GameControllerTest {
@@ -9,8 +11,16 @@ public class GameControllerTest {
     @Test
     void 자동차_이름_공백_입력_예외_테스트() {
         assertThatThrownBy(() -> {
-            inputController.checkCarNames("");
+            NameInputValidator.validateCarNames(null);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[error] 입력된 자동차 이름이 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름_빈칸_입력_예외_테스트() {
+        assertThatThrownBy(() -> {
+            NameInputValidator.validateCarNames("p, ,d");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[error] 입력된 자동차 이름 중 이름이 없는 자동차가 있습니다.");
     }
 }
