168e1cbfdc8db1e995d682ad929f2c96ec33bbdb
SeungCheol <ssc6839@gmail.com>
2022-02-10 16:42:23 +0900

    feat(racingcar): 시도 횟수 입력 UI 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public String inputCarNames() {
         printCarNameInputMessage();
@@ -13,5 +14,15 @@ public class InputView {
 
     private void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
+    }
+
+    public String inputTryCount() {
+        printTryCountInputMessage();
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private void printTryCountInputMessage() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
     }
 }
