e527947e82f7f58066cb36f01fd3aec7bd7c8c14
Donggyu Kim <a29661498@gmail.com>
2022-02-10 16:04:01 +0900

    refactor : 입력값 검증 부분 CalculatorValidation 클래스로 분리
    CalculatorValidation에서 입력값을 검증하도록 책임을 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,29 +8,20 @@ public class Calculator {
     private static StringSplitter stringSplitter = StringSplitter.getInstance();
 
     public static int splitAndSum(String str) {
-        if (str == null || str.length() == 0) {
+        if (isEmpty(str)) {
             return 0;
         }
 
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
+        String[] tokens = stringSplitter.parseString(str);
 
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            String[] tokens = matcher.group(2).split(customDelimiter);
+        isValidTokens(tokens);
 
-            sum = getSum (sum, tokens);
-
-            return sum;
-        }
-
-        String[] tokens = str.split(",|:");
-
-        sum = getSum (sum, tokens);
-
-        return sum;
+        return getSum(tokens);
     }
 
-    private static int getSum(int sum, String[] tokens) {
+    private static int getSum(String[] tokens) {
+        int sum = 0;
+
         for (String token : tokens) {
             sum += Integer.parseInt(token);
         }
