9abda81aadc2995d7af6b049b1d9fda1976ddee4
yhh1056 <yhh1056@naver.com>
2022-02-10 12:14:24 +0900

    feat(advance): 조건에 따른 자동차 전진 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,26 @@
 package racingcar;
 
 public class Car {
+    private static final int ADVANCE_STANDARD = 4;
+
     private final String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
     }
 
+    public void advance(int number) {
+        if (number >= ADVANCE_STANDARD) {
+            this.position++;
+        }
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
@@ -18,5 +20,21 @@ public class CarTest {
         // then
         assertThat(car.getName()).isEqualTo(testName);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:1", "3:0"}, delimiter = ':')
+    @DisplayName("자동차 전진 테스트")
+    void advanceTest(int number, int expected) {
+        // given
+        Car car = new Car("testCar");
+
+        // when
+        car.advance(number);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(expected);
+    }
+
+
 
 }
