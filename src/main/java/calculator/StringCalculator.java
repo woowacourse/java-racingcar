f0f9b04b9d917d382ce71c678fc3f5a7396b852f
Gyuchool <balance0591@naver.com>
2022-02-10 23:15:14 +0900

    feat: 쉼표 또는 콜론 구분자를 통한 덧셈 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        int sum = 0;
+        String[] separatedValues = input.split(",|:");
+        for (String separatedValue : separatedValues) {
+            sum += Integer.parseInt(separatedValue);
+        }
         return Integer.parseInt(input);
     }
 }
