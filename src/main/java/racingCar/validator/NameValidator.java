ac220ef69a81453d2a9e9ae02d2e3f40fdcd45b5
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-11 01:42:08 +0900

    refactor(Convertor): static 추가

--- src/main/java/racingCar/validator/NameValidator.java
+++ src/main/java/racingCar/validator/NameValidator.java
@@ -1,48 +1,43 @@
 package racingCar.validator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import racingCar.utlis.Convertor;
-
 public class NameValidator {
-	public final String ERROR = "ERROR";
+	public static final String ERROR = "ERROR";
 
-	public void checkNull(String inputString) throws Exception {
+	public static void checkNull(String inputString) throws Exception {
 		if (inputString == null || inputString.isEmpty())
 			throw new Exception(ERROR);
 	}
 
-	public void checkSpace(String name) throws Exception {
+	public static void checkSpace(String name) throws Exception {
 		if (name.trim().isEmpty()) {
 			throw new Exception(ERROR);
 		}
 	}
 
-	public void checkNameSize(String name) throws Exception {
+	public static void checkNameSize(String name) throws Exception {
 		if (name.length() > 5) {
 			throw new Exception(ERROR);
 		}
 	}
 
-	public void checkSpecialChar(String name) throws Exception {
+	public static void checkSpecialChar(String name) throws Exception {
 		if (!Pattern.matches("[가-힣\\w_]*", name)) {
 			throw new Exception(ERROR);
 		}
 	}
 
-	public void checkOneName(List<String> nameList) throws Exception {
+	public static void checkOneName(List<String> nameList) throws Exception {
 		if (nameList.size() <= 1) {
 			throw new Exception(ERROR);
 		}
 	}
 
-	public void checkDuplicatedName(String names) throws Exception {
-		List<String> nameList = new ArrayList<>(Arrays.asList(Convertor.separateNamesByDelimiter(names)));
+	public static void checkDuplicatedName(List<String> nameList) throws Exception {
 		Set<String> nameSet = new HashSet<>(nameList);
 		if (nameSet.size() != nameList.size()) {
 			throw new Exception(ERROR);
