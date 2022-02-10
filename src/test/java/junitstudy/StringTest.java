621a9ce10602c5af814adb0591a6c36df204f855
summerlunaa <summerlunaa9@gmail.com>
2022-02-10 10:54:27 +0900

    test : removeBracketTest 구현

--- src/test/java/junitstudy/StringTest.java
+++ src/test/java/junitstudy/StringTest.java
@@ -19,4 +19,12 @@ public class StringTest {
         assertThat(result1).containsExactly("1", "2");
         assertThat(result2).containsExactly("1");
     }
+
+    @Test
+    void removeBracketTest() {
+        String testString = "(1,2)";
+        String result = testString.substring(1, testString.length() - 1);
+
+        assertThat(result).isEqualTo("1,2");
+    }
 }
