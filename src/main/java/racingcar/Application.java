533fdbc83b89aef31adb8e35f978636c317bc496
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-10 16:13:05 +0900

    feat: 경주할 자동차들 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.controller.RacingCarGame;
+
+public class Application {
+	public static void main(String[] args) {
+		RacingCarGame racingCarGame = new RacingCarGame();
+		racingCarGame.getCarNames();
+	}
+}
--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,9 +1,23 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
 public class RacingCarGame {
+	private InputView inputView = new InputView();
+
+	public void getCarNames() {
+		String carNames = inputView.getCarNames();
+		try {
+			checkCarNames(carNames);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			getCarNames();
+		}
+	}
+
 	public void checkCarNames(String input) {
 		if (input == null || input.equals("")) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("[Error] 다시 이름을 입력하세요");
 		}
 	}
 }
