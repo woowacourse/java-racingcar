bbd56b5b3958d44d3ca62eb42a6f24bc49526bb8
YJGwon <yj970125@gmail.com>
2022-02-10 14:18:25 +0900

    feat: 커스텀 구분자가 존재하는 경우
    StringCaculator.java
       - separate(String input, String separator): 구분자로 문자열을 쪼개서 int array로 반환
       - getCustomSeparatorMatcher(String input): 커스텀 구분자 패턴 matcher 반환

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,13 +1,31 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
+
     public static int splitAndSum(String input) {
         return calculate(castStringToInts(input));
+    }
+
+    private static int calculate(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
     }
 
     private static int[] castStringToInts(String input) {
         if (isNullOrEmpty(input)) {
             return new int[] {0};
+        }
+        Matcher matcher = getCustomSeparatorMatcher(input);
+        if (matcher.find()) {
+            return separate(matcher.group(2), matcher.group(1));
         }
         return null;
     }
@@ -16,12 +34,14 @@ public class StringCalculator {
         return input == null || input.isEmpty();
     }
 
-    private static int calculate(int[] numbers) {
-        //numbers를 계산
-        int result = 0;
-        for (int number : numbers) {
-            result += number;
-        }
-        return result;
+    private static int[] separate(String input, String separator) {
+        return Arrays.stream(input.split(separator))
+            .mapToInt(string ->Integer.parseInt(string))
+            .toArray();
+    }
+
+    private static Matcher getCustomSeparatorMatcher(String input) {
+        Pattern pattern = Pattern.compile(CUSTOM_SEPARATOR_REGEX);
+        return pattern.matcher(input);
     }
 }
