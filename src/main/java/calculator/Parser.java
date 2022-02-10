df4edfa27a6bc482b66a16b7b858c6588ffa35dd
meatsby <meatsby@gmail.com>
2022-02-10 14:22:26 +0900

    feat: 커스텀 구분자 분리 기능 구현

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,11 +1,19 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
     String regex = ",|:";
 
     public String[] splitter(String input) {
         if (input == null || input.isEmpty()) {
             input = "0";
+        }
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            regex = m.group(1);
+            input = m.group(2);
         }
         return input.split(regex);
     }
