d01117a8eeb6782328dadf7314b2edf3bedfd300
YJGwon <yj970125@gmail.com>
2022-02-10 20:04:41 +0900

    feat: 우승자 판정
    Referee.java
       - getMaxPosition(Set<Car> cars): stream의 max()를 이용해서 최대 position값 도출
       - judgeWinner(Set<Car> cars): getMaxPosition()에서 도출한 값과 position이 같은 Car 객체 List 반환
    RefereeTest.java
       - 우승자_판정(): position값에 차이를 뒀을 때 우승자 판정 테스트
       - 우승자_여러명(): 우승자가 여러명일 때 테스트

--- src/main/java/racingcar/Referee.java
+++ src/main/java/racingcar/Referee.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Referee {
 	private static final int MOVE_CRITERIA = 4;
 
@@ -7,5 +12,18 @@ public class Referee {
 		if (randomValue >= MOVE_CRITERIA) {
 			car.move();
 		}
+	}
+
+	private static int getMaxPosition(Set<Car> cars) {
+		return cars.stream()
+			.mapToInt(Car::getPosition)
+			.max()
+			.getAsInt();
+	}
+
+	public static List<Car> judgeWinner(Set<Car> cars) {
+		return cars.stream()
+			.filter(car -> car.isPosition(getMaxPosition(cars)))
+			.collect(Collectors.toList());
 	}
 }
--- src/test/java/racingcar/RefereeTest.java
+++ src/test/java/racingcar/RefereeTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class RefereeTest {
@@ -10,5 +13,22 @@ public class RefereeTest {
 		Car car = new Car("kun");
 		Referee.judgeCarMove(car, 5);
 		assertThat(car.getPosition()).isEqualTo(1);
+	}
+
+	@Test
+	public void 우승자_판정() {
+		Set<Car> cars = new HashSet<>();
+		Car car1 = new Car("forky");
+		Car car2 = new Car("kun");
+		car1.move();
+		cars.add(car1);
+		cars.add(car2);
+		assertThat(Referee.judgeWinner(cars).get(0)).isEqualTo(car1);
+	}
+
+	@Test
+	public void 우승자_여러명() {
+		Set<Car> cars = CarFactory.of("forky,kun");
+		assertThat(Referee.judgeWinner(cars).size()).isEqualTo(2);
 	}
 }
