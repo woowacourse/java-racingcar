d80d404044ac90fd334784d773e31362c926c259
yoon <duddbs774@gmail.com>
2022-02-11 01:55:42 +0900

    test: 테스트 코드 리팩토링

--- src/test/java/racingcar/CarControllerTest.java
+++ src/test/java/racingcar/CarControllerTest.java
@@ -1,55 +1,43 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarControllerTest {
 
+    CarController carController;
+    Car car1;
+    Car car2;
+    List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        carController = new CarController();
+        car1 = new Car();
+        car2 = new Car();
+
+        cars = Arrays.asList(car1, car2);
+    }
+
     @Test
     void 우승자를_선출한다_우승자가_한명인경우() {
-        // given
-        CarController carController = new CarController();
-        Car car1 = new Car();
-        Car car2 = new Car();
-        List<Car> cars = new ArrayList<>();
-
-        cars.add(car1);
-        cars.add(car2);
-
-        // when
         car1.attemptToMove(4);
         car2.attemptToMove(3);
 
-        // then
         assertThat(carController.findWinners(cars)).containsExactly(car1);
     }
 
     @Test
     void 우승자를_선출한다_우승자가_두명인경우() {
-        // given
-        CarController carController = new CarController();
-        Car car1 = new Car();
-        Car car2 = new Car();
-
-        List<Car> cars = new ArrayList<>();
-
-        cars.add(car1);
-        cars.add(car2);
-
-        // when
         car1.attemptToMove(4);
         car2.attemptToMove(4);
 
-        // then
         assertThat(carController.findWinners(cars)).containsExactly(car1, car2);
     }
-
-
-
-
 }
