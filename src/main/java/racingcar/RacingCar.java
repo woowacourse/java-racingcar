9a31959741c76da7b488d538fba5662c11569489
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 11:59:04 +0900

    refactor: 이름 검증 메소드 분리

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -5,9 +5,13 @@ public class RacingCar {
     private String name;
 
     public RacingCar(String name) {
-        if(name.length() > 5) {
+        checkNameLength(name);
+        this.name = name;
+    }
+
+    private void checkNameLength(String name) {
+        if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
         }
-        this.name = name;
     }
 }
