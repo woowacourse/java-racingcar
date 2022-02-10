2a549349ca79cfb07e6915be65b7809996573066
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-11 01:07:08 +0900

    feat: 입력 받은 횟수만큼 라운드을 반복 기능 구현

--- src/main/java/racingCar/service/RacingCarService.java
+++ src/main/java/racingCar/service/RacingCarService.java
@@ -7,11 +7,25 @@ import java.util.List;
 import racingCar.model.Car;
 import racingCar.utlis.Convertor;
 import racingCar.validator.NameValidator;
+import racingCar.view.Output;
 
 public class RacingCarService {
 
 	private List<Car> cars = new ArrayList<>();
 	private int count;
+
+	public void runGame() {
+		for (int i = 0; i < count; i++) {
+			carsGo();
+		}
+		Output.printRoundResult(cars);
+	}
+
+	public void carsGo() {
+		for (Car car : cars) {
+			car.go();
+		}
+	}
 
 	public void saveCars(String carsNames) throws Exception {
 		List<String> carNameList = new ArrayList<>(Arrays.asList(Convertor.separateNamesByDelimiter(carsNames)));
