9b6195488fd61773f29d970799f114869401e034
yukong <kyb062666@gmail.com>
2022-02-10 13:51:49 +0900

    test: 자동차 이름이 중복될 경우 테스트 추가

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -5,23 +5,31 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- - [ ] [ 예외 ] : `공백` - *(입력받은 문자열의 앞 뒤를 제거해준다.)*
- - [ ] [ 예외 ] : 자동차가 `2대 미만`
- - [ ] [ 예외 ] : 이름이 `중복`
- - [ ] [ 예외 ] : `5글자 초과`
+ * - [ ] [ 예외 ] : `공백` - *(입력받은 문자열의 앞 뒤를 제거해준다.)*
+ * - [ ] [ 예외 ] : 자동차가 `2대 미만`
+ * - [ ] [ 예외 ] : 이름이 `중복`
+ * - [ ] [ 예외 ] : `5글자 초과`
  */
 class InputTest {
     @Test
     void 자동차_이름_공백_테스트() {
         String inputValue = "";
-        assertThatThrownBy(() -> {
-            Input.carName(inputValue);
-        }).isInstanceOf(IllegalArgumentException.class);
+        validateName(inputValue);
     }
 
     @Test
-    void 자동차_개수_테스트(){
+    void 자동차_개수_테스트() {
         String names = "pobi";
+        validateName(names);
+    }
+
+    @Test
+    void 자동차_이름_중복_테스트() {
+        String names = "pobi,pobi";
+        validateName(names);
+    }
+
+    void validateName(String names) {
         assertThatThrownBy(() -> {
             Input.carName(names);
         }).isInstanceOf(IllegalArgumentException.class);
