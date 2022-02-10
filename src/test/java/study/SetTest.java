375b72fb937a89a89475f436db66da72d70ebefa
Wishoon <heonga2@gmail.com>
2022-02-10 11:42:01 +0900

    test: 집합 원소 포함, 미포함 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -34,5 +36,12 @@ public class SetTest {
     @DisplayName("집합 원소 포함 확인")
     public void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    @DisplayName("집합 원소 포함, 미포함")
+    public void contains_And_notContains(int number, boolean expected) {
+        assertEquals(expected, numbers.contains(number));
     }
 }
