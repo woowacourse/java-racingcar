d70123ee34b8023b2d49ce25dc5b856c7940323c
sure-why-not <pkse1213@gmail.com>
2022-02-10 17:00:49 +0900

    refactor : Test Code 중복 코드 제거

--- src/test/java/racingcar/RacingServiceTest.java
+++ src/test/java/racingcar/RacingServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import racingcar.domain.Car;
@@ -14,18 +15,24 @@ import racingcar.util.Converter;
 
 public class RacingServiceTest {
 
+	private RacingService racingService;
+	private CarRepository carRepository;
+
+	@BeforeEach
+	public void init() {
+		racingService = new RacingService();
+		carRepository = new CarRepository();
+		String input = "pobi,joon";
+		racingService.registerCars(Converter.toCarList(input));
+	}
+
 	@AfterEach
-	public void beforeEach() {
-		CarRepository carRepository = new CarRepository();
+	public void afterEach() {
 		carRepository.clear();
 	}
 
 	@Test
 	public void 자동차_저장() {
-		String input = "pobi,joon";
-
-		RacingService racingService = new RacingService();
-		racingService.registerCars(Converter.toCarList(input));
 
 		CarRepository carRepository = new CarRepository();
 		int carCount = carRepository.count();
@@ -35,15 +42,10 @@ public class RacingServiceTest {
 
 	@Test
 	public void 자동차_경주() {
-		String input = "pobi,joon";
-
-		RacingService racingService = new RacingService();
-		racingService.registerCars(Converter.toCarList(input));
 
 		racingService.race(bound -> 5);
 		racingService.race(bound -> 5);
 
-		CarRepository carRepository = new CarRepository();
 		List<Car> cars = carRepository.findCars();
 
 		cars.forEach(car -> {
@@ -53,36 +55,27 @@ public class RacingServiceTest {
 
 	@Test
 	public void 우승자_한명_계산() {
-		Car car1 = Car.of("pobi");
+		Car car1 = Car.of("lala");
 		Car car2 = Car.of("good");
-		Car car3 = Car.of("jun");
+		Car car3 = Car.of("jason");
 
 		car1.move(5);
 		car1.move(5);
 		car1.move(5);
 		car2.move(5);
 
-		RacingService racingService = new RacingService();
 		racingService.registerCars(List.of(car1, car2, car3));
 
 		List<String> winnerNames = racingService.findWinnerNames();
 		assertThat(winnerNames.size()).isEqualTo(1);
-		assertThat(winnerNames.get(0)).isEqualTo("pobi");
+		assertThat(winnerNames.get(0)).isEqualTo("lala");
 	}
 
 	@Test
 	public void 우승자_여러명_계산() {
-		Car car1 = Car.of("pobi");
-		Car car2 = Car.of("good");
-		Car car3 = Car.of("jun");
-
-		RacingService racingService = new RacingService();
-		racingService.registerCars(List.of(car1, car2, car3));
-
 		List<String> winnerNames = racingService.findWinnerNames();
-		assertThat(winnerNames.size()).isEqualTo(3);
+		assertThat(winnerNames.size()).isEqualTo(2);
 		assertThat(winnerNames).containsSequence("pobi");
-		assertThat(winnerNames).containsSequence("good");
-		assertThat(winnerNames).containsSequence("jun");
+		assertThat(winnerNames).containsSequence("joon");
 	}
 }
