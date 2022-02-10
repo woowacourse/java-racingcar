81fbc9fe1618875848e2d404b1d7bbe99dd3dc27
hyeonic <dev.hyeonic@gmail.com>
2022-02-11 00:08:42 +0900

    refactor: Car 객체 리스트를 가진 일급 컬렉션 Cars 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,50 @@
+package racingcar.domain;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(String[] names, MovingPolicy movingPolicy) {
+        validateDuplicate(names);
+        this.cars = Arrays.stream(names)
+            .map(name -> new Car(name, movingPolicy))
+            .collect(toList());
+    }
+
+    private void validateDuplicate(String[] names) {
+        int originalSize = names.length;
+        Set<String> noneDuplicatedNames = Arrays.stream(names).collect(toSet());
+        if (originalSize != noneDuplicatedNames.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void move() {
+        cars.forEach(Car::move);
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+            .filter(car -> maxPosition == car.getPosition())
+            .map(Car::getName)
+            .collect(toList());
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+            .mapToInt(Car::getPosition)
+            .max()
+            .orElse(0);
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+}
--- src/main/java/racingcar/domain/RacingCarController.java
+++ src/main/java/racingcar/domain/RacingCarController.java
@@ -1,56 +1,27 @@
 package racingcar.domain;
 
-import static java.util.stream.Collectors.*;
-
-import java.util.Arrays;
-import java.util.List;
-
 import racingcar.view.InputView;
 import racingcar.view.OutPutView;
 
 public class RacingCarController {
 
     public void run() {
-        List<Car> cars = getCars();
+        Cars cars = new Cars(InputView.getCarNames(), new RandomMovingPolicy());
         race(cars, getCount());
 
-        OutPutView.printResult(getWinners(cars, getMaxPosition(cars)));
-    }
-
-    private List<Car> getCars() {
-        return Arrays.stream(InputView.getCarNames())
-            .map(carName -> new Car(carName, new RandomMovingPolicy()))
-            .collect(toList());
+        OutPutView.printResult(cars.getWinners());
     }
 
     private Count getCount() {
         return InputView.getCount();
     }
 
-    private void race(List<Car> cars, Count count) {
+    private void race(Cars cars, Count count) {
         OutPutView.printStatusMessage();
         while (count.isPositive()) {
             count.subtract();
-            moveCars(cars);
-            OutPutView.printStatus(cars);
+            cars.move();
+            OutPutView.printStatus(cars.getCars());
         }
-    }
-
-    private void moveCars(List<Car> cars) {
-        cars.forEach(Car::move);
-    }
-
-    private int getMaxPosition(List<Car> cars) {
-        return cars.stream()
-            .mapToInt(Car::getPosition)
-            .max()
-            .orElse(0);
-    }
-
-    private List<String> getWinners(List<Car> cars, int maxPosition) {
-        return cars.stream()
-            .filter(car -> maxPosition == car.getPosition())
-            .map(Car::getName)
-            .collect(toList());
     }
 }
