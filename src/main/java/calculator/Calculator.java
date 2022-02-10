29c8fae0420d65a83fc47289da2e9a69f4c4c643
Donggyu Kim <a29661498@gmail.com>
2022-02-10 14:30:12 +0900

    feat: 계산기 요구사항3
    숫자 2개인 경우 `,`로 구분하여 합하도록 splitAndSum 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,10 +3,18 @@ package calculator;
 public class Calculator {
 
     public static int splitAndSum(String str) {
+        int sum = 0;
+
         if (str == null || str.length() == 0) {
             return 0;
         }
 
-        return Integer.parseInt(str);
+        String[] split = str.split(",");
+
+        for (String s : split) {
+            sum += Integer.parseInt(s);
+        }
+
+        return sum;
     }
 }
