8b80ab06b961970eb8c5b892340859f25238c947
meatsby <meatsby@gmail.com>
2022-02-10 18:06:07 +0900

    test: 최종 우승자 테스트 구현

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -47,4 +47,18 @@ public class ApplicationTest {
         car.move(3);
         assertThat(car.getCurrentPosition()).isEqualTo("test : -");
     }
+
+    @Test
+    void 최종_우승자_테스트() {
+        Cars cars = new Cars(new String[] {"car1","car2","car3"});
+
+        cars.getCars().get(0).move(5);
+        cars.getCars().get(1).move(5);
+        cars.getCars().get(2).move(3);
+
+        assertThat(cars.getChampions())
+            .contains("car1")
+            .contains("car2")
+            .doesNotContain("car3");
+    }
 }
