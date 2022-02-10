74da12e8dc3d49208d741a8589a8bf9a95227567
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 19:54:05 +0900

    test: 중복 생성 부분 제거

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -14,16 +14,17 @@ import racingcar.controller.RacingCarDto;
 class OutputViewTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private OutputView outputView;
 
     @BeforeEach
     void setUp() {
         System.setOut(new PrintStream(outContent));
+        outputView = new OutputView();
     }
 
     @Test
     @DisplayName("psotion을 -로 출력한다.")
     void printCarsPosition() {
-        OutputView outputView = new OutputView();
         List<RacingCarDto> racingCarDtos = Arrays.asList(
             new RacingCarDto("pobi", 2),
             new RacingCarDto("crong", 3)
@@ -35,7 +36,6 @@ class OutputViewTest {
     @Test
     @DisplayName(", 를 구분자로 우승자를 출력한다.")
     void printWinnerNames() {
-        OutputView outputView = new OutputView();
         List<String> winnerNames = Arrays.asList("pobi", "crong", "honux");
         outputView.printWinnerNames(winnerNames);
         assertThat(outContent.toString()).isEqualTo("pobi, crong, honux가 최종 우승했습니다.\n");
