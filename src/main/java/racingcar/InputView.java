ded077c7bb58133f48573325a61eab9ef54b81a4
summerlunaa <summerlunaa9@gmail.com>
2022-02-10 21:42:24 +0900

    feat : 이동횟수를 입력받는 getMoveCount 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     public static final String REQUEST_INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_MOVE_COUNT = "시도할 회수는 몇회인가요?";
 
     public static String getCarNames(Scanner scanner) {
         try {
@@ -14,6 +15,18 @@ public class InputView {
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
             return getCarNames(scanner);
+        }
+    }
+
+    public static String getMoveCount(Scanner scanner) {
+        try {
+            System.out.println(REQUEST_MOVE_COUNT);
+            String inputValues = scanner.nextLine();
+            MoveCountValidator.validateMoveCount(inputValues);
+            return inputValues;
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getMoveCount(scanner);
         }
     }
 }
