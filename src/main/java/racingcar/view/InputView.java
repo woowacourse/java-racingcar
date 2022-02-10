6dbcc570504125c7d3ceb37adb5f13984596140c
sure-why-not <pkse1213@gmail.com>
2022-02-10 15:46:02 +0900

    refactor : View 메소드를 static 접근으로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,12 +13,12 @@ public class InputView {
 	private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
-	public List<Car> getCarNames() {
+	public static List<Car> getCarNames() {
 		System.out.println(CAR_NAMES_MESSAGE);
 		return Converter.toCarList(scanner.next());
 	}
 
-	public int getAttemptNumber() {
+	public static int getAttemptNumber() {
 		System.out.println(ATTEMPT_NUMBER_MESSAGE);
 		return Converter.toAttemptNumber(scanner.next());
 	}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,12 +9,12 @@ public class OutputView {
 	private static final String RESULT_MESSAGE ="실행결과";
 	private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
 
-	public void printResultMessage() {
+	public static void printResultMessage() {
 		System.out.println();
 		System.out.println(RESULT_MESSAGE);
 	}
 
-	public void printRacingInfo(List<Car> cars) {
+	public static void printRacingInfo(List<Car> cars) {
 		for (Car car : cars) {
 			System.out.print(car.getName() + " : ");
 			printPositionBar(car.getPosition());
@@ -22,7 +22,7 @@ public class OutputView {
 		System.out.println();
 	}
 
-	private void printPositionBar(int position) {
+	private static void printPositionBar(int position) {
 		for (int i = 0; i < position; i++) {
 			System.out.print("-");
 		}
