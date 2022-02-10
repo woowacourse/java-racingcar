c608b233c3620c623863901c44a8aa2c6aad377a
BeomsooKim <kbsat@naver.com>
2022-02-10 13:16:31 +0900

    feat(calculator): use custom separator if exists in splitAndSum argument

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,17 +1,32 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public static int splitAndSum(String targetString) {
-        if (targetString == null || targetString.isBlank()){
+        if (targetString == null || targetString.isBlank()) {
             return 0;
         }
 
         int sum = 0;
-        String[] splitResult = targetString.split("[,:]");
+        String separators = ",:";
+        String[] splitResult;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(targetString);
+
+        if (m.find()) {
+            String customSeparator = m.group(1);
+            separators += customSeparator;
+            targetString = m.group(2);
+        }
+
+        splitResult = targetString.split("[" + separators + "]");
+
         for (String s : splitResult) {
             sum += toInt(s);
         }
+
         return sum;
     }
 
