b27baf5786372d675d214e4852661f0dc2e3af7c
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-10 16:27:33 +0900

    feat: 자동차 이름 구분하는 기능 구현

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,23 +1,33 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import racingcar.view.InputView;
 
 public class RacingCarGame {
 	private InputView inputView = new InputView();
 
-	public void getCarNames() {
+	public List<String> getCarNames() {
 		String carNames = inputView.getCarNames();
+
 		try {
 			checkCarNames(carNames);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
-			getCarNames();
+			return getCarNames();
 		}
+
+		return splitCarNames(carNames);
 	}
 
 	public void checkCarNames(String input) {
 		if (input == null || input.equals("")) {
 			throw new IllegalArgumentException("[Error] 다시 이름을 입력하세요");
 		}
+	}
+
+	public List<String> splitCarNames(String carNames) {
+		return Arrays.asList(carNames.split(","));
 	}
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,17 +2,27 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import racingcar.controller.RacingCarGame;
 
 public class RacingCarTest {
+	RacingCarGame racingCarGame = new RacingCarGame();
+
 	@Test
 	public void 자동차_이름_공백() throws Exception {
-		RacingCarGame racingCarGame = new RacingCarGame();
 		assertThatThrownBy(() -> racingCarGame.checkCarNames(""))
 			.isInstanceOf(IllegalArgumentException.class);
 		assertThatThrownBy(() -> racingCarGame.checkCarNames(null))
 			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void 자동차_이름_구분() throws Exception {
+		List<String> result = racingCarGame.splitCarNames("배카라,아스피");
+		assertThat(result).isEqualTo(Arrays.asList("배카라", "아스피"));
 	}
 }
