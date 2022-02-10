579937fe3df8dfea0e5c528d155736066ff1fee1
jaejae-yoo <wotj102@gmail.com>
2022-02-10 17:57:33 +0900

    feat: 자동차 이름 5자 초과 입력 예외 처리 기능생성

--- src/main/java/racingcargame/utils/NameInputValidator.java
+++ src/main/java/racingcargame/utils/NameInputValidator.java
@@ -8,7 +8,7 @@ public class NameInputValidator {
     }
 
     public static String validateCarNames(String carNames) {
-        if (isNotBlankInCarNames(carNames) && isNotEmptyInCarNames(carNames)) {
+        if (isNotBlankInCarNames(carNames) && isNotEmptyInCarNames(carNames) && isOverCarNamesLimitSize(carNames)) {
             return carNames;
         }
         throw new IllegalArgumentException();
@@ -27,6 +27,16 @@ public class NameInputValidator {
                 .filter(String::isBlank).count();
         if (count > 0) {
             throw new IllegalArgumentException("[error] 입력된 자동차 이름 중 이름이 없는 자동차가 있습니다.");
+        }
+        return true;
+    }
+
+    private static boolean isOverCarNamesLimitSize(String carNames) {
+        String[] cars = carNames.split(",");
+        int count = (int) Arrays.stream(cars)
+                .filter(car -> car.length() > 5).count();
+        if (count > 0) {
+            throw new IllegalArgumentException("[error] 자동차 이름을 5자 이하로 입력해주세요.");
         }
         return true;
     }
