fc9e9b89329346a796ff08aa78757e6b5efc0fe5
SeungCheol <ssc6839@gmail.com>
2022-02-10 20:23:26 +0900

    test(racingcar): 자동차 전진, 멈춤 테스트

---
+++ src/test/java/model/CarTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import racingcar.model.Car;
+
+public class CarTest {
+    private static Car car;
+
+    @BeforeEach
+    void init() {
+        car = new Car("carA");
+    }
+
+    @Test
+    void 자동차_전진() {
+        car.moveForward(5);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_멈춤() {
+        car.moveForward(0);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
