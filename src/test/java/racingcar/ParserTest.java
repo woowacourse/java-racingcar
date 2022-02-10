733265bac7607a3d9279058536f11b7347aeaac0
woong7 <driedseas@gmail.com>
2022-02-10 22:50:25 +0900

    <docs>: 기능 목록 최신화

--- src/test/java/racingcar/ParserTest.java
+++ src/test/java/racingcar/ParserTest.java
@@ -17,38 +17,10 @@ public class ParserTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    /**
-     * 파싱 테스트 : 이상한 입력은 예외 처리
-     * 예) ,philz,panda
-     * ,philz,
-     * 1) 5자를 초과하는 이름에 대해 예외처리
-     * 2) 이동 횟수가 음수일 때 예외처리
-     * 3) 앞이나 뒤에 쉼표가 있는 경우 예외처리
-     * 4) 공백이 있는경우는 trim해주어 처리를한다
-     * 5) # 등의 특수문자가 있는 경우 예외처리한다
-     * 6) 허용하는 입력 : 숫자, 알파벳, 한글
-     * 기능테스트
-     * 1) 전진한다 : 위치가 1이다
-     * 2) 움직이지 않는다 : 위치가 0이다
-     * 3) 우승자 판단 - 주어진 입력값에 대해 올바른 우승자를 도출하는지
-     * 3명의 참가자 중 2명이 완주일 경우: 2명을 이름 순 출력
-     * 3명의 참가자 중 1명이 완주일 경우: 1명 출력
-     */
-
-    // 예외 입력: #, null, 앞뒤로 쉼표
     @ParameterizedTest
     @ValueSource(strings = {",", "#", "panda,", "#philz", ",phobi,"})
     @DisplayName("이름에 허용되지 않는 문자")
     public void Not_Available_Character(String input) {
-        /*assertThatThrownBy(()->{
-            input.contains("[;,]");
-        }).isInstanceOf(Exception.class);*/
-        //assertThat(input.contains("[;,]")).isTrue();
         assertThatThrownBy(
                 () -> StringParser.validateCarName(input)
         ).isInstanceOf(RuntimeException.class);
@@ -57,7 +29,6 @@ public class ParserTest {
     @Test
     @DisplayName("이름에 허용되지 않는 문자 : null")
     public void Not_Available_Null() {
-        // then
         assertThatThrownBy(
                 () -> StringParser.validateCarName(null)
         ).isInstanceOf(RuntimeException.class);
@@ -66,8 +37,6 @@ public class ParserTest {
     @Test
     @DisplayName("한 번 해보는 정상입력")
     public void Available_Character() {
-
-        //StringParser.validateCarName("Keesun");
         Assertions.assertDoesNotThrow(() -> {
             StringParser.validateCarName("polo");
         });
@@ -78,7 +47,6 @@ public class ParserTest {
     @DisplayName("자동차 전체 입력 : 정상")
     public void input_all_car_name(String input) {
         String[] inputNames = StringParser.readCarNameInputs(input);
-
         assertThat(inputNames).containsExactly("panda", "philz", "java");
     }
 
@@ -89,17 +57,4 @@ public class ParserTest {
             StringParser.readCarNameInputs("panda,philz,javajigi");
         });
     }
-/*
-    @Test
-    @DisplayName("자동차 이동")
-    public void move_car() {
-       // given
-        Car car = new Car("philz");
-
-       // when
-
-       // then
-
-    }
-*/
 }
