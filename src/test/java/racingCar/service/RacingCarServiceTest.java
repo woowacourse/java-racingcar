04fd23b787e5afe7366e4f709a54ae937ed0b18c
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-11 02:55:38 +0900

    test: 우승자 찾기 테스트 구현

--- src/test/java/racingCar/service/RacingCarServiceTest.java
+++ src/test/java/racingCar/service/RacingCarServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -32,4 +33,23 @@ class RacingCarServiceTest {
 		assertThat(racingCarService.findMaxPosition()).isEqualTo(expectMaxPosition);
 	}
 
+	@Test
+	public void 우승자_찾기_테스트() throws Exception {
+		int expectMaxPosition = 4;
+		Car car1 = new Car("juri");
+		Car car2 = new Car("hunch");
+		Car car3 = new Car("pobi");
+		for (int i = 0; i < 2; i++) {
+			car1.moveCar();
+		}
+		for (int i = 0; i < 1; i++) {
+			car2.moveCar();
+		}
+		for (int i = 0; i < expectMaxPosition; i++) {
+			car3.moveCar();
+		}
+		racingCarService.cars = new ArrayList<>(Arrays.asList(new Car[] {car1, car2, car3}));
+		ArrayList<String> result = new ArrayList<>(Arrays.asList(new String[] {"pobi"}));
+		assertThat(racingCarService.findWinner()).isEqualTo(result);
+	}
 }
