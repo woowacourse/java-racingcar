177445a7cc6615d96c4f0c997ac8c8c502bbcf07
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-10 23:27:53 +0900

    test(ValidatorUtils): setup validations for carNames input

--- src/test/java/racingcar/util/ValidatorUtilsTest.java
+++ src/test/java/racingcar/util/ValidatorUtilsTest.java
@@ -1,6 +1,7 @@
 package racingcar.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -26,5 +27,30 @@ public class ValidatorUtilsTest {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> validatePositiveInt(integer))
                 .withMessageMatching("양수를 입력해야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    void _validateNotBlank(String string) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> validateNotBlank(string))
+                .withMessageMatching("공백을 입력하면 안 됩니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456", "carrots"})
+    void _validateNotOverFiveCharacters(String string) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> validateNotOverFiveCharacters(string))
+                .withMessageMatching("5글자 이하의 이름을 입력해야 합니다.");
+    }
+
+    @Test
+    void _validateNoDuplicates() {
+        String[] carNames = new String[]{"aa", "aa", "b"};
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> validateNoDuplicates(carNames))
+                .withMessageMatching("중복된 이름을 입력하면 안 됩니다.");
     }
 }
