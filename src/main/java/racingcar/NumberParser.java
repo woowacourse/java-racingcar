1a85090295ea546ae3a6ff85d4e78692a228f802
wilgur513 <wilgur513@naver.com>
2022-02-10 10:56:02 +0900

    feat: throw exception by negative try count

--- src/main/java/racingcar/NumberParser.java
+++ src/main/java/racingcar/NumberParser.java
@@ -3,6 +3,10 @@ package racingcar;
 public class NumberParser {
 
     public int parse(String input) {
-        return Integer.parseInt(input);
+        int tryCount = Integer.parseInt(input);
+        if (tryCount < 0) {
+            throw new IllegalArgumentException("시도 횟수는 음수가 될 수 없습니다.");
+        }
+        return tryCount;
     }
 }
--- src/test/java/racingcar/NumberParserTest.java
+++ src/test/java/racingcar/NumberParserTest.java
@@ -1,13 +1,28 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NumberParserTest {
+
+    private NumberParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new NumberParser();
+    }
+
     @Test
     public void 문자열을_숫자로_변환() {
-        NumberParser parser = new NumberParser();
         assertThat(parser.parse("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void 음수_숫자_예외_발생() {
+        assertThatThrownBy(() -> parser.parse("-1"))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
