43bc941207a56e1dc74ac03e421d929be6a01342
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-10 20:54:30 +0900

    test(GameManager): setup initCars to generate car list

--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -1,8 +1,11 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +20,20 @@ public class GameManagerTest {
         String[] splitResult = (String[]) privateMethod.invoke(gameManager, "a,bc,bacon");
 
         assertThat(splitResult).containsExactly("a","bc","bacon");
+    }
+
+    @Test
+    void initCars() throws Exception {
+        Method privateMethod = getPrivateMethod("initCars", String[].class);
+        String[] carNames = new String[] {"a","bc","bacon"};
+
+        privateMethod.invoke(gameManager, (Object) carNames);
+
+        assertThat(gameManager.cars.size()).isEqualTo(3);
+        for (int idx = 0; idx < 3; idx++) {
+            Car car = gameManager.cars.get(idx);
+            assertThat(car.getName()).isEqualTo(carNames[idx]);
+        }
     }
 
     private Method getPrivateMethod(String methodName, Class<?>... argsType) throws NoSuchMethodException {
