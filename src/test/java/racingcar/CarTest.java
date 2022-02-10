d9291b5d426fd57158809c6ca7b0a569c354a749
summerlunaa <summerlunaa9@gmail.com>
2022-02-10 17:17:18 +0900

    test : Car 객체에 대한 테스트 작성

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+    @Test
+    @DisplayName("자동차 이름으로 클래스가 생성되는지 테스트")
+    void carGenerateTest() {
+        String name = "pobi";
+        Car car = new Car(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("자동차가 전진하는지 테스트")
+    void carMoveForwardTest() {
+        String name = "pobi";
+        Car car = new Car(name);
+        car.moveForward();
+        assertThat(car.isSamePosition(1)).isTrue();
+    }
+}
