42119db87a6cc58f8b930c4a1f7f268eae79bf39
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-10 15:32:40 +0900

    refactor: RacingGame stream 사용

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,26 +1,28 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
 
+    private static final String CAR_NAME_DELIMITER = ",";
     private final List<RacingCar> cars;
 
-    public RacingGame(String input) {
-        String[] carNames = input.split(",");
-        this.cars = new ArrayList<>();
-        for(String carName : carNames) {
-            RacingCar car = new RacingCar(carName);
-            this.cars.add(car);
-        }
+    public RacingGame(String carStringNames) {
+        this.cars = makeRacingCars(carStringNames.split(CAR_NAME_DELIMITER));
+    }
+
+    private List<RacingCar> makeRacingCars(String[] carNames) {
+        return Arrays.stream(carNames)
+            .map(RacingCar::new)
+            .collect(Collectors.toList());
     }
 
     public List<String> getCarNames() {
-        List<String> carNames = new ArrayList<>();
-        for(RacingCar car : cars) {
-            carNames.add(car.getName());
-        }
-        return carNames;
+        return cars.stream()
+            .map(RacingCar::getName)
+            .collect(Collectors.toList());
     }
 }
