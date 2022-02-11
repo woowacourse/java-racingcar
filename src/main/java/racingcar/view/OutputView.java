31cd2d495e931ba400d9ec49b9b722bda253e649
devHudi <devhudi@gmail.com>
2022-02-11 15:27:15 +0900

    feat: view 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import racingcar.model.car.Name;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public static void printInputNamesUi() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public static void printInputTryCountUi() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printRaceResultUi() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printRaceResultUi(List<Name> names) {
+        System.out.printf("%s가 최종 우승했습니다.", names.stream()
+                .map(Name::toString)
+                .collect(Collectors.joining(", ")));
+    }
+}
