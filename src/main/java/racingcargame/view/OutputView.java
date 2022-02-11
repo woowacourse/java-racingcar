1466eb6810e266785b7660e1166ee8a192e7e07b
koo <gudonghee2000@gmail.com>
2022-02-11 17:04:03 +0900

    refactor: OutputView클래스 함수 수정및 매직넘버 상수화

--- src/main/java/racingcargame/view/OutputView.java
+++ src/main/java/racingcargame/view/OutputView.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
-import racingcargame.model.CarVO;
+import java.util.stream.IntStream;
 
 public class OutputView {
 	private static final String CAR_NAMES_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String RACE_COUNT_INPUT_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
-	private static final String RACE_PROGRESS_GUIDE_MESSAGE = "실행 결과";
+	private static final String NOW_RACE_PROGRESS_GUIDE_MESSAGE = "실행 결과";
 	private static final String RACE_PROGRESS_OUTPUT_SEPARATOR = " : ";
 	private static final String GAME_WINNER_MESSAGE = "가 최종 우승했습니다.";
-	private static final String RACE_MOVE = "-";
+	private static final String CAR_MOVE = "-";
+	private static final String CAR_NAME_SEPARATOR = ", ";
 
 	private OutputView() {
 	}
@@ -27,22 +27,32 @@ public class OutputView {
 	}
 
 	public static void showRaceProgressGuideMessage() {
-		print(RACE_PROGRESS_GUIDE_MESSAGE);
+		print(NOW_RACE_PROGRESS_GUIDE_MESSAGE);
 	}
 
 	public static void showRaceProgress(HashMap<String, Integer> carsInformation) {
-		carsInformation.entrySet()
-			.forEach(carInformation -> print(
-				carInformation.getKey() + RACE_PROGRESS_OUTPUT_SEPARATOR + showMove(carInformation.getValue())));
+		carsInformation.forEach((carName, carPosition) -> {
+			showCarName(carName);
+			showCarPosition(carPosition);
+		});
 		newLine();
 	}
 
-	public static String showMove(int moveCount) {
-		StringBuilder move = new StringBuilder();
-		for (int count = 0; count < moveCount; count++) {
-			move.append(RACE_MOVE);
+	public static void showCarName(String carName) {
+		System.out.print(carName + RACE_PROGRESS_OUTPUT_SEPARATOR);
+	}
+
+	public static void showCarPosition(int carPosition) {
+		IntStream.range(0, carPosition).forEach(index -> System.out.println(CAR_MOVE));
+	}
+
+	public static void showGameWinner(List<String> winners) {
+		Queue<String> winner = new LinkedList<>(winners);
+
+		while (winner.size() > 1) {
+			System.out.print(winner.poll() + CAR_NAME_SEPARATOR);
 		}
-		return move.toString();
+		System.out.print(winner.poll() + GAME_WINNER_MESSAGE);
 	}
 
 	private static void print(String message) {
@@ -51,15 +61,5 @@ public class OutputView {
 
 	private static void newLine() {
 		System.out.println();
-	}
-
-	public static void showGameWinner(List<String> winners) {
-		Queue<String> winner = new LinkedList<>();
-		winners.forEach(wr -> winner.add(wr));
-
-		while (winner.size() > 1) {
-			System.out.print(winner.poll() + ", ");
-		}
-		System.out.print(winner.poll() + GAME_WINNER_MESSAGE);
 	}
 }
