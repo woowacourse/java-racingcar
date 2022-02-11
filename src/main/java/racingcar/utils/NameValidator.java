ea47981656f0fe6ef13b492cf449f364e289f428
Chan Kim <chankim@Chanui-MacBookPro.local>
2022-02-11 14:02:04 +0900

    refactor : racingcar 리팩토링 작업

--- src/main/java/racingcar/utils/NameValidator.java
+++ src/main/java/racingcar/utils/NameValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class NameValidator {
     private static final Pattern pattern = Pattern.compile("[a-zA-Z가-힣0-9]{1,5}(,[a-zA-Z가-힣0-9]{1,5})*");
     private static final String WRONG_INPUT_MESSAGE = "올바르지 않은 입력입니다.";
-    private static final String DUPLICATED_NAME_MESSAGE = "중복된 이름이 있습니다.";  
+    private static final String DUPLICATED_NAME_MESSAGE = "중복된 이름이 있습니다.";
 
     public static void isValidateNames(String carNames) {
         if (isInvalidPattern(carNames)) {
