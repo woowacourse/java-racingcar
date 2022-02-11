81e7b05a80838707addf7c693748724f99ccc186
hj-rich <ztzy1907@gmail.com>
2022-02-12 03:39:07 +0900

    refactor(test): 단건 테스트시 ParameterizedTest -> Test

--- src/test/java/racingcar/model/vo/NameTest.java
+++ src/test/java/racingcar/model/vo/NameTest.java
@@ -1,10 +1,11 @@
 package racingcar.model.vo;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import static org.assertj.core.api.Assertions.*;
 
 public class NameTest {
     @ParameterizedTest
@@ -14,12 +15,13 @@ public class NameTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = "hohoho")
+    @Test
     @DisplayName("이름이 5글자가 넘으면 예외 발생 테스트")
-    void nameLengthShouldBeLessThenSix(String actual) {
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Car(actual))
-            .withMessageMatching("이름은 \\d+글자가 넘을 수 없습니다.");
+    void nameLengthShouldBeLessThenSix() {
+        String overSizedName = "richard";
+
+        assertThatThrownBy(() -> new Car(overSizedName))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageMatching("이름은 \\d+글자가 넘을 수 없습니다.");
     }
 }
