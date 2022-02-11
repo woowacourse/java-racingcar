74304a8e697f9e1a309224b3bff583b44eef0b8c
pup-paw <wldusdl0310@gmail.com>
2022-02-11 14:01:07 +0900

    style: 오타 수정

--- src/main/java/racingcar/util/StringConst.java
+++ src/main/java/racingcar/util/StringConst.java
@@ -11,7 +11,7 @@ public enum StringConst {
 	RESULT_MESSAGE("실행 결과"),
 	ERROR_NULL("[ERROR] 입력값은 NULL일 수 없습니다."),
 	ERROR_BLANK("[ERROR] 입력값은 빈 입력일 수 없습니다."),
-	ERROR_SIZE("[ERROR] 입력값의 크기는 5초과일 수 없습니다."),
+	ERROR_SIZE("[ERROR] 입력값의 크기는 5 초과일 수 없습니다."),
 	ERROR_SPECIAL_CHAR("[ERROR] 입력값은 특수문자일 수 없습니다."),
 	ERROR_DUPLICATE("[ERROR] 입력값은 중복될 수 없습니다."),
 	ERROR_INT_NUM("[ERROR] 입력값은 문자일 수 없습니다."),
