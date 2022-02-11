0bedfd4613dd271587b2d7880ef4d5c60c6712d4
her0807 <her0807@naver.com>
2022-02-11 14:55:27 +0900

    refactor(Cars): 상수 추출과 자동차 경주, 우승자 추출 로직 테스트
    - hasNext() 에 가중된 책임을 generate() 메서드분리로 해결
    - 상수 추출
    - 랜덤 숫자 범위 test ,우승자 추출 test

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -8,41 +8,53 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class Cars {
-    private List<Car> cars;
+	private static final String MAX_FIND_ERROR = "max 값을 찾을 수 없습니다.";
+	private static final int DRIVE_FLAG = 3;
+	private static final int RANDOM_MULTIPLE = 100;
+	private static final int RANDOM_DIVISION = 10;
+	private List<Car> cars;
 
-    public Cars(String[] names) {
-        cars = new ArrayList<>();
-        for (String name : names) {
-            cars.add(createCar(name.trim()));
-        }
-    }
+	public Cars(String[] names) {
+		cars = new ArrayList<>();
+		for (String name : names) {
+			cars.add(createCar(name));
+		}
+	}
 
-    public void play() {
-        for (Car car : cars) {
-            car.drive();
-        }
-        Output.roundResult(cars);
-    }
+	public void play() {
+		for (Car car : cars) {
+			car.drive(hasNext());
+		}
+		Output.roundResult(cars);
+	}
 
-    private Car createCar(String name) {
-        return new Car(name);
-    }
+	private boolean hasNext() {
+		return generate() > DRIVE_FLAG;
+	}
 
-    public List<String> findWinners() {
-        Car maxPosition = findMaxPosition();
-        return findWinnerPosition(maxPosition);
-    }
+	private int generate() {
+		return (int)(Math.random() * RANDOM_MULTIPLE) % RANDOM_DIVISION;
+	}
 
-    private Car findMaxPosition() {
-        return cars.stream()
-                .max(Car::compareTo)
-                .orElseThrow(() -> new NoSuchElementException("max 값을 찾을 수 없습니다."));
-    }
+	private Car createCar(String name) {
+		return new Car(name.trim());
+	}
 
-    private List<String> findWinnerPosition(Car maxPositionCar) {
-        return cars.stream()
-                .filter(car -> car.isSamePosition(maxPositionCar))
-                .map(Car::getName)
-                .collect(Collectors.toList());
-    }
+	public List<String> findWinners() {
+		Car maxPositionCar = findMaxPositionCar();
+		return findSamePositionCar(maxPositionCar);
+	}
+
+	private Car findMaxPositionCar() {
+		return cars.stream()
+			.max(Car::compareTo)
+			.orElseThrow(() -> new NoSuchElementException(MAX_FIND_ERROR));
+	}
+
+	private List<String> findSamePositionCar(Car target) {
+		return cars.stream()
+			.filter(car -> car.isSamePosition(target))
+			.map(Car::getName)
+			.collect(Collectors.toList());
+	}
 }
