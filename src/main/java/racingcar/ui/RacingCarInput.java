225dcac835d893854621267b09f995cbec888f4b
yxxnghwan <younghwan960@gmail.com>
2022-02-11 16:33:06 +0900

    refactor(racing-car): RacingCarInput 상수선언 개행 변경

--- src/main/java/racingcar/ui/RacingCarInput.java
+++ src/main/java/racingcar/ui/RacingCarInput.java
@@ -9,10 +9,8 @@ import racingcar.utils.CarsGenerator;
 import racingcar.validator.RoundNumberValidator;
 
 public class RacingCarInput {
-	private static final String CAR_NAME_INPUT_MESSAGE
-		= "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-	private static final String ROUND_INPUT_MESSAGE
-		= "시도할 회수는 몇회인가요?";
+	private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+	private static final String ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
 	public static List<Car> carNameInput() {
 		String userInput = userCarNameInput();
