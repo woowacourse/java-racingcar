24b112f94b7df51ca020938a953e4690bcb4bb43
유영우 <fbduddn97@naver.com>
2022-02-11 21:11:28 +0900

    refactor: Car 에러메시지 작성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
 public class Car {
+    private static final String NAME_INPUT_ERROR = "이름은 공백, 빈 값, null일 수 없습니다.";
+    private static final String NAME_LENGTH_ERROR = "이름은 5자를 초과할 수 없습니다.";
+
     private static final int NAME_LIMITED_LENGTH = 5;
     private static final int POSITION_INIT = 0;
 
@@ -17,11 +20,11 @@ public class Car {
 
     private void validateName(String name) {
         if (name == null || name.isEmpty() || name.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NAME_INPUT_ERROR);
         }
 
         if (name.length() > NAME_LIMITED_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
         }
     }
 
