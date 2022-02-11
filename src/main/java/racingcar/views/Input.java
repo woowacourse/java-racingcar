7c0811ad077c2ffc63604f5613daab311ba0a795
skullKim <kyk990328@gmail.com>
2022-02-11 14:20:09 +0900

    refactor(src/main/java/racingcar): 게임에 필요한 메시지 통합
    게임 진행을 위한 입출력, 에러 안내 메시지를 하나의 클래스에서 관리하기
    위해 RacingGameMessage 생성. RacingGameMessage에서 모든 메시지를
    관리한다.

--- src/main/java/racingcar/views/Input.java
+++ src/main/java/racingcar/views/Input.java
@@ -6,11 +6,9 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import racingcar.utils.InputValidation;
+import racingcar.utils.RacingGameMessage;
 
 public class Input {
-
-	private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-	private static final String INPUT_REPEATS = "시도할 회수는 몇회인가요?";
 
 	final InputValidation inputValidation;
 	final Scanner scanner;
@@ -22,13 +20,13 @@ public class Input {
 
 	public List<String> inputValidNames() {
 		final String names = inputNames();
-		return Arrays.stream(names.split(","))
+		return Arrays.stream(names.split(RacingGameMessage.CAR_NAME_DISTRIBUTOR))
 			.collect(Collectors.toList());
 	}
 
 	private String inputNames() {
 		try {
-			System.out.println(INPUT_CAR_NAME);
+			System.out.println(RacingGameMessage.INPUT_CAR_NAME);
 			final String names = scanner.next();
 			inputValidation.validateName(names);
 			return names;
@@ -44,7 +42,7 @@ public class Input {
 
 	private String inputRepeats() {
 		try {
-			System.out.println(INPUT_REPEATS);
+			System.out.println(RacingGameMessage.INPUT_REPEATS);
 			final String repeats = scanner.next();
 			inputValidation.validateRepeats(repeats);
 			return repeats;
--- src/main/java/racingcar/views/Output.java
+++ src/main/java/racingcar/views/Output.java
@@ -3,33 +3,28 @@ package racingcar.views;
 import java.util.List;
 
 import racingcar.models.Car;
+import racingcar.utils.RacingGameMessage;
 
 public class Output {
 
-	private static final String TURN_RESULT = "\n실행결과";
-	private static final String CAR_POSITION = "-";
-	private static final String WINNER = "가 최종 우승했습니다.";
-	private static final String NAME_POSITION_SPLITTER = " : ";
-
 	public void printResultMessage() {
-		System.out.println(TURN_RESULT);
+		System.out.println(RacingGameMessage.TURN_RESULT);
 	}
 
 	public void printTurnResult(final List<Car> carList) {
 		carList.forEach(this::printCarPosition);
-		System.out.println();
+		System.out.print("\n");
 	}
 
 	private void printCarPosition(final Car car) {
-		System.out.print(car.getName() + NAME_POSITION_SPLITTER);
+		System.out.print(car.getName() + RacingGameMessage.NAME_POSITION_SPLITTER);
 		for (int i = 0; i < car.getPosition(); i++) {
-			System.out.print(CAR_POSITION);
+			System.out.print(RacingGameMessage.CAR_POSITION);
 		}
 		System.out.print("\n");
 	}
 
 	public void printWinner(final String winners) {
-		System.out.print(winners + WINNER);
+		System.out.print(winners + RacingGameMessage.WINNER);
 	}
-
 }
