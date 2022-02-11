1858e58ba47fb252d99d861124aa07e6d1760ae3
jaejae-yoo <wotj102@gmail.com>
2022-02-11 18:10:16 +0900

    refactor: 예외 문구 명확하게 변경

--- src/test/java/racingcargame/controller/GameControllerTest.java
+++ src/test/java/racingcargame/controller/GameControllerTest.java
@@ -12,23 +12,23 @@ public class GameControllerTest {
         assertThatThrownBy(() -> {
             NameInputValidator.validateCarNames(null);
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[error] 입력된 자동차 이름이 없습니다.");
+                .hasMessageContaining("[error] 경주할 자동차 이름을 입력해주세요.");
     }
 
     @Test
     void 자동차_이름_빈칸_입력_예외_테스트() {
         assertThatThrownBy(() -> {
-            NameInputValidator.validateCarNames("p, ,d");
+            NameInputValidator.validateCarNames("pii, ,doo");
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[error] 입력된 자동차 이름 중 이름이 없는 자동차가 있습니다.");
+                .hasMessageContaining("[error] 입력된 자동차 이름 중 이름이 입력되지 않는 자동차가 있습니다.");
     }
 
     @Test
     void 자동차_이름_5자초과_입력_예외_테스트() {
         assertThatThrownBy(() -> {
-            NameInputValidator.validateCarNames("p,kdrejkr,d");
+            NameInputValidator.validateCarNames("pii,kikiee,doo");
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[error] 자동차 이름을 5자 이하로 입력해주세요.");
+                .hasMessageContaining("[error] 자동차 이름을 1자이상, 5자 이하로 입력해주세요.");
     }
 
     @Test
