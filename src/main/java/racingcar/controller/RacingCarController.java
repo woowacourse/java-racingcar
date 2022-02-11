84a76064481a1230cbe7a4734ec49b59356224d6
YuJeong <yjfflq4872@gmail.com>
2022-02-11 15:31:11 +0900

    refactor: 입력 검증 로직 리팩토링

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,7 +1,11 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import racingcar.validator.CarNameValidator;
+import racingcar.validator.TrialCountValidator;
 import racingcar.domain.Cars;
-import racingcar.InputManager;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -9,8 +13,8 @@ public class RacingCarController {
 	public final Cars cars = new Cars();
 
 	public void playGame() {
-		cars.generateCars(InputManager.splitAndSaveCarNames(InputView.inputCarNames()));
-		int trialCount = InputManager.getTrial(InputView.inputTrials());
+		cars.generateCars(getCarNames(InputView.inputCarNames()));
+		int trialCount = getTrialCount(InputView.inputTrials());
 
 		OutputView.printResultMessage();
 		for (int i = 0; i < trialCount; i++) {
@@ -18,5 +22,21 @@ public class RacingCarController {
 		}
 
 		OutputView.printWinnerNames(cars.findWinners());
+	}
+
+	public List<String> getCarNames(String carNamesLine) {
+		CarNameValidator.checkCarNamesLine(carNamesLine);
+		return split(carNamesLine);
+	}
+
+	public List<String> split(String carNamesLine) {
+		List<String> carNames = Arrays.asList(carNamesLine.split(","));
+		CarNameValidator.checkCarNames(carNames);
+		return carNames;
+	}
+
+	public int getTrialCount(String line) {
+		TrialCountValidator.checkTrialCountLine(line);
+		return Integer.parseInt(line);
 	}
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,22 +3,24 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-
-	private static String RACING_RESULT_MESSAGE = "실행 결과";
+	private static final String NEW_LINE = "\n";
+	private static final String RACING_RESULT_MESSAGE = "실행 결과";
+	private static final String WINNER_NAME_DELIMITER = ", ";
+	private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
 
 	public static void printResultMessage() {
-		System.out.println(RACING_RESULT_MESSAGE);
+		System.out.println(NEW_LINE + RACING_RESULT_MESSAGE);
 	}
 
 	public static void printRacingRecords(List<String> racingRecords) {
-		for (int i = 0 ; i < racingRecords.size() ; i++) {
-			System.out.println(racingRecords.get(i));
+		for (String racingRecord : racingRecords) {
+			System.out.println(racingRecord);
 		}
 		System.out.println();
 	}
 
 	public static void printWinnerNames(List<String> winnerNames) {
-		String winners = String.join(", ", winnerNames);
-		System.out.println(winners + "가 최종 우승했습니다.");
+		String winners = String.join(WINNER_NAME_DELIMITER, winnerNames);
+		System.out.println(winners + WINNER_MESSAGE);
 	}
 }
