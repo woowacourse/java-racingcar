2c1c46b9a3668ef1a9469d2f766cd61c0547ae50
yoon <duddbs774@gmail.com>
2022-02-11 16:39:30 +0900

    refactor: 문자열 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,27 +6,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+
+    public static final String NAME_POSITION_SEPARATOR = ":";
+    public static final String BLANK = " ";
+    public static final String POSITION_UNIT = "-";
+    public static final String WINNERS_DELIMITER = ",";
+
     public static void printResult(List<Car> cars) {
         for (Car car : cars) {
-            String result = car.getName() + " : ";
+            String result = car.getName() + BLANK + NAME_POSITION_SEPARATOR + BLANK;
             result += makePositionString(car);
             System.out.println(result);
         }
+        newLine();
+    }
+
+    private static void newLine() {
         System.out.println();
     }
 
     static String makePositionString(Car car) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < car.getPosition(); i++) {
-            sb.append("-");
+            sb.append(POSITION_UNIT);
         }
         return sb.toString();
     }
 
     public static void printWinners(List<Car> winners) {
         String result = winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining());
+            .map(Car::getName)
+            .collect(Collectors.joining(WINNERS_DELIMITER + BLANK));
         System.out.println(result + "가 최종 우승했습니다.");
     }
 }
