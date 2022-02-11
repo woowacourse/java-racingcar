bdf563412a30e22f7d15fa1b6ea62d6ef12a679e
TaeHyeon Kim <kth990303@naver.com>
2022-02-11 17:46:02 +0900

    fix(calculator): 잘못된 입력일 경우 재입력받도록 수정

--- src/main/java/calculator/CalculatorMain.java
+++ src/main/java/calculator/CalculatorMain.java
@@ -1,10 +1,20 @@
 package calculator;
 
 public class CalculatorMain {
-	public static void main(String[] args) {
-		CalculatorInput calculatorInput = new CalculatorInput();
-		int result = StringCalculator.calculate(calculatorInput.userInput());
-		CalculatorOutput calculatorOutput = new CalculatorOutput(result);
-		calculatorOutput.OutputResult();
-	}
+    public static void main(String[] args) {
+        int result = run();
+
+        CalculatorOutput calculatorOutput = new CalculatorOutput(result);
+        calculatorOutput.OutputResult();
+    }
+
+    public static int run() {
+        CalculatorInput calculatorInput = new CalculatorInput();
+        try {
+            return StringCalculator.calculate(calculatorInput.userInput().trim());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return run();
+        }
+    }
 }
