7116134a057ff6e1aca1e2221caf286c314b2c01
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-11 15:08:33 +0900

    refactor: 오타 수정

--- src/main/java/stringcalculator/CheckException.java
+++ src/main/java/stringcalculator/CheckException.java
@@ -11,7 +11,7 @@ public class CheckException extends Throwable {
 	static final String SplitOk1_ERROR_MESSAGE = "1구분자가 올바르지 않은 위치에 있습니다!";
 	static final String SplitOk2_ERROR_MESSAGE = "2구분자가 올바르지 않은 위치에 있습니다!";
 	static final String SplitOk3_ERROR_MESSAGE = "3구분자가 올바르지 않은 위치에 있습니다!";
-	public static final String PRONT_REGEX = "(.)[";
+	public static final String FRONT_REGEX = "(.)[";
 	public static final String BACK_REGEX = "]{2,}(.)*";
 
 	public static void check(String[] array) {
@@ -42,7 +42,7 @@ public class CheckException extends Throwable {
 		if (regex.contains(Character.toString(string.charAt(string.length() - 1)))) {
 			throw new RuntimeException(SplitOk2_ERROR_MESSAGE);
 		}
-		Matcher mMid = Pattern.compile(PRONT_REGEX + regex + BACK_REGEX).matcher(string);
+		Matcher mMid = Pattern.compile(FRONT_REGEX + regex + BACK_REGEX).matcher(string);
 		if (mMid.matches()) {
 			throw new RuntimeException(SplitOk3_ERROR_MESSAGE);
 		}
