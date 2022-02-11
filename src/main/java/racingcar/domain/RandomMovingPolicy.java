0be5a286b62026bd2302f746fda5557ee6e357d8
유영우 <fbduddn97@naver.com>
2022-02-11 21:19:41 +0900

    refactor: RandomMovingPolicy 매직넘버 상수화

--- src/main/java/racingcar/domain/RandomMovingPolicy.java
+++ src/main/java/racingcar/domain/RandomMovingPolicy.java
@@ -3,8 +3,12 @@ package racingcar.domain;
 import racingcar.utils.Randoms;
 
 public class RandomMovingPolicy implements MovingPolicy {
+	private static final int START_INCLUSIVE = 0;
+	private static final int END_INCLUSIVE = 9;
+	private static final int CONDITION_FOR_MOVE = 4;
 
 	public boolean isMove() {
-		return Randoms.pickNumberInRange(0, 9) >= 4;
+		return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE)
+			>= CONDITION_FOR_MOVE;
 	}
 }
