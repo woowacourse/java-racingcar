19a735b484e28f0c275aba2f9a21adbf92a4aaee
jayjaehunchoi <wogns0108@naver.com>
2022-02-11 13:28:08 +0900

    feat : Scanner 를 사용할 수 있는 싱글턴 객체 생성

---
+++ src/main/java/racingcar/Console.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner;
+
+    private Console() {
+
+    }
+
+    private static Scanner getInstance() {
+        if(Objects.isNull(scanner) || isClosed()) {
+            scanner = new Scanner(System.in);
+        }
+        return scanner;
+    }
+
+    public static String readLine() {
+        return getInstance().nextLine();
+    }
+
+    private static boolean isClosed() {
+        try {
+            System.err.close();
+            Field sourceClosed = Scanner.class.getDeclaredField("sourceClosed");
+            sourceClosed.setAccessible(true);
+            return sourceClosed.getBoolean(scanner);
+        } catch (Exception e) {
+        }
+        return true;
+    }
+
+}
--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,18 +1,16 @@
 package racingcar;
 
-import java.util.Scanner;
 
 import static racingcar.Output.*;
 import static racingcar.Validator.*;
 
 public class Input {
 
-    private static Scanner scanner = new Scanner(System.in);
     private static String SPLIT_DELIMITER = ",";
 
     public static String[] insertRightName() {
         printCarNamesMessage();
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         validateBlank(input);
         String[] names = input.split(SPLIT_DELIMITER, -1);
         validateEachCarName(names);
@@ -21,7 +19,7 @@ public class Input {
 
     public static int insertRightNumber() {
         printNumberMessage();
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         return toInt(input);
     }
 }
