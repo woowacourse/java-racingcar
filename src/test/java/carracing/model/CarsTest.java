fdb0c134d626c69bfbb1f5ee8d9fd858945d700b
EastHShin <xldk78@gmail.com>
2022-02-11 14:12:56 +0900

    refactor(CarsTest): 반복되는 코드 메소드로 분리

--- src/test/java/carracing/model/CarsTest.java
+++ src/test/java/carracing/model/CarsTest.java
@@ -6,22 +6,34 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 
+	Car eastCar;
+	Car pobiCar;
+	Car peperCar;
+
+	@BeforeEach
+	void car_init() {
+		eastCar = new Car("east");
+		pobiCar = new Car("pobi");
+		peperCar = new Car("peper");
+	}
+
 	@Test
 	@DisplayName("자동차의 이름이 중복될 경우 에러 발생")
-	void validate_자동차이름_중복(){
+	void validate_자동차이름_중복() {
 		//given
 		//when
 		//then
-		assertThatThrownBy(()-> {
+		assertThatThrownBy(() -> {
 			Cars cars = new Cars(Arrays.asList(
-				new Car("pobi"),
-				new Car("east"),
-				new Car("peper"),
+				pobiCar,
+				eastCar,
+				peperCar,
 				new Car("east")
 			));
 		}).isInstanceOf(IllegalArgumentException.class)
@@ -32,15 +44,9 @@ public class CarsTest {
 	@DisplayName("position이 가장 높은 자동차가 우승")
 	void 최종우승자_결정_한명() {
 		//given
-		Car pobiCar = new Car("pobi");
-		Car eastCar = new Car("east");
-		Car peperCar = new Car("peper");
-		pobiCar.move(9);
-		eastCar.move(9);
-		eastCar.move(9);
-		peperCar.move(9);
-		peperCar.move(9);
-		peperCar.move(9);
+		move(pobiCar, 1);
+		move(eastCar, 2);
+		move(peperCar, 3);
 
 		Cars cars = new Cars(Arrays.asList(
 			pobiCar, eastCar, peperCar
@@ -54,18 +60,11 @@ public class CarsTest {
 
 	@Test
 	@DisplayName("position이 가장 높은 자동차가 두 대일 때 공동 우승자")
-	public void 최종우승자_결정_여러명() throws Exception{
+	public void 최종우승자_결정_여러명() throws Exception {
 		//given
-		Car pobiCar = new Car("pobi");
-		Car eastCar = new Car("east");
-		Car peperCar = new Car("peper");
-		pobiCar.move(9);
-		eastCar.move(9);
-		eastCar.move(9);
-		eastCar.move(9);
-		peperCar.move(9);
-		peperCar.move(9);
-		peperCar.move(9);
+		move(pobiCar, 1);
+		move(eastCar, 3);
+		move(peperCar, 3);
 
 		Cars cars = new Cars(Arrays.asList(
 			pobiCar, eastCar, peperCar
@@ -73,6 +72,12 @@ public class CarsTest {
 		// when
 		// then
 		List<String> winners = cars.getWinners();
-		assertThat(winners).contains("peper","east");
+		assertThat(winners).contains("peper", "east");
+	}
+	
+	void move(Car car, int cnt) {
+		while ((cnt--) > 0) {
+			car.move(9);
+		}
 	}
 }
