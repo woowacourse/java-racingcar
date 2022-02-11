d00c9d9de63de3f53b14afb5f0187bcd9349f0b4
jaejae-yoo <wotj102@gmail.com>
2022-02-11 18:03:42 +0900

    refactor: 불필요한 applicaion 객체 삭제

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
-    Application application = new Application();
 
     @Test
     void NULL_입력_예외_처리() {
@@ -15,25 +14,25 @@ public class ApplicationTest {
 
     @Test
     void 반칸_입력_예외_처리() {
-        int number = application.runCalculator("");
+        int number = Application.runCalculator("");
 
         assertThat(number).isEqualTo(0);
     }
 
     @Test
     void 공백_입력_예외_처리() {
-        int number = application.runCalculator(" ");
+        int number = Application.runCalculator(" ");
 
         assertThat(number).isEqualTo(0);
     }
 
     @Test
     void 구분자_기준_연산() {
-        int number = application.runCalculator("1,2");
+        int number = Application.runCalculator("1,2");
 
         assertThat(number).isEqualTo(3);
 
-        number = application.runCalculator("1:2");
+        number = Application.runCalculator("1:2");
 
         assertThat(number).isEqualTo(3);
     }
@@ -47,20 +46,20 @@ public class ApplicationTest {
 
     @Test
     void 커스컴_구분자_기준_연산() {
-        int number = application.runCalculator("//;\n12;2;3");
+        int number = Application.runCalculator("//;\n12;2;3");
 
         assertThat(number).isEqualTo(17);
     }
 
     @Test
     void 음수_입력_예외_처리() {
-        assertThatThrownBy(() -> application.runCalculator("-1,2,3"))
+        assertThatThrownBy(() -> Application.runCalculator("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void 문자_입력_예외_처리() {
-        assertThatThrownBy(() -> application.runCalculator("a,2,3"))
+        assertThatThrownBy(() -> Application.runCalculator("a,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
