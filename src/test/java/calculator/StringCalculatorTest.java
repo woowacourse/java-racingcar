c4d7f88ddbf7fa20f5494c13d78e8266ecf6cd0e
유영우 <fbduddn97@naver.com>
2022-02-11 11:11:31 +0900

    refactor: StringCalculatorTest 변수 final로 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -10,49 +10,49 @@ class StringCalculatorTest {
     @DisplayName("문자열이 주어지면 `쉼표(,)` 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
     public void splitAndSum_쉼표구분자() {
-        int result = StringCalculator.splitAndSum("1,2");
+        final int result = StringCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @DisplayName("문자열이 주어지면 `콜론(:)` 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
     public void splitAndSum_콜론구분자() {
-        int result = StringCalculator.splitAndSum("1:2");
+        final int result = StringCalculator.splitAndSum("1:2");
         assertThat(result).isEqualTo(3);
     }
 
     @DisplayName("문자열이 주어지면 `쉼표(,)`와 `콜론(:)` 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
     public void splitAndSum_쉼표와콜론구분자() {
-        int result = StringCalculator.splitAndSum("1:2,3");
+        final int result = StringCalculator.splitAndSum("1:2,3");
         assertThat(result).isEqualTo(6);
     }
 
     @DisplayName("기본 구분자인 `쉼표(,)`, `콜론(:)`외에 커스텀 구분자를 지정할 수 있다.")
     @Test
     public void splitAndSum_커스텀구분자() {
-        int result = StringCalculator.splitAndSum("//;\n1;2;3");
+        final int result = StringCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @DisplayName("빈 문자열인 경우 0을 반환한다.")
     @Test
     void splitAndSum_빈문자열() {
-        int result = StringCalculator.splitAndSum("");
+        final int result = StringCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("null인 경우 0을 반환한다.")
     @Test
     void splitAndSum_null() {
-        int result = StringCalculator.splitAndSum(null);
+        final int result = StringCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @Test
     void splitAndSum_숫자하나() {
-        int result = StringCalculator.splitAndSum("1");
+        final int result = StringCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
