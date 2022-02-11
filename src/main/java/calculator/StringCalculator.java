8110de6c616f9ecdb3ddfd0245c0ca0280da8dcd
ellie <ririhan217@gmail.com>
2022-02-11 16:52:03 +0900

    style(calculator): 자바 코드 컨벤션에 맞게 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,36 +5,36 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
-            return 0;
-        }
-        return sum(split(text));
-    }
+	public static int splitAndSum(String text) {
+		if (text == null || text.isEmpty()) {
+			return 0;
+		}
+		return sum(split(text));
+	}
 
-    private static String[] split(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
-        }
-        return text.split(",|:");
-    }
+	private static String[] split(String text) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+		return text.split(",|:");
+	}
 
-    private static int sum(String[] tokens) {
-        int result = 0;
-        int number;
-        for (String token : tokens) {
-            number = Integer.parseInt(token);
-            validateNegative(number);
-            result += number;
-        }
-        return result;
-    }
+	private static int sum(String[] tokens) {
+		int result = 0;
+		int number;
+		for (String token : tokens) {
+			number = Integer.parseInt(token);
+			validateNegative(number);
+			result += number;
+		}
+		return result;
+	}
 
-    private static void validateNegative(int number) {
-        if (number < 0) {
-            throw new RuntimeException();
-        }
-    }
+	private static void validateNegative(int number) {
+		if (number < 0) {
+			throw new RuntimeException();
+		}
+	}
 }
