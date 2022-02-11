bf8ba1ccbf2bfd5bf73cf36ff82c8c60002fa571
summerlunaa <summerlunaa9@gmail.com>
2022-02-11 15:18:54 +0900

    refactor : 스트림 적용

--- src/main/java/racingcar/Utils.java
+++ src/main/java/racingcar/Utils.java
@@ -1,13 +1,13 @@
 package racingcar;
 
+import java.util.Arrays;
+
 public class Utils {
     public static final String STRING_REGEX = ",";
 
     public static String[] splitAndTrim(String names) {
-        String[] carNames = names.split(STRING_REGEX);
-        for (int i = 0; i < carNames.length; i++) {
-            carNames[i] = carNames[i].trim();
-        }
-        return carNames;
+        String[] splitNames = names.split(STRING_REGEX);
+        Arrays.setAll(splitNames, i -> splitNames[i].trim());
+        return splitNames;
     }
 }
