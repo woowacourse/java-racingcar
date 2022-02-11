0cb700d0eca49d92dabfccb06572f19690df9133
유영우 <fbduddn97@naver.com>
2022-02-11 21:24:52 +0900

    refactor: RacingController.getCount() 의미없는 메서드 제거

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import racingcar.domain.Car;
-import racingcar.domain.Count;
 import racingcar.domain.Racing;
 import racingcar.domain.RandomMovingPolicy;
 import racingcar.view.InputView;
@@ -16,16 +15,12 @@ public class RacingCarController {
     private final Racing racing = new Racing();
 
     public void run() {
-        OutPutView.printResult(racing.race(getCars(), getCount()));
+        OutPutView.printResult(racing.race(getCars(), InputView.getCount()));
     }
 
     private List<Car> getCars() {
         return Arrays.stream(InputView.getCarNames())
             .map(carName -> new Car(carName, new RandomMovingPolicy()))
             .collect(toList());
-    }
-
-    private Count getCount() {
-        return InputView.getCount();
     }
 }
