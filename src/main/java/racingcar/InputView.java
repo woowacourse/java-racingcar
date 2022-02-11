24109dd77f1bc692a3d001db024c065b35dd8b97
Gyuchool <balance0591@naver.com>
2022-02-11 15:56:06 +0900

    feat: 자동차의 이름 입력받는 기능 구현 및 시도할 횟수를 입력받는 기능 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static String askCarName() {
+        System.out.println(QUESTION_CAR_NAME);
+        return sc.nextLine();
+    }
+
+
+    public static String askTryCount() {
+        System.out.println(QUESTION_TRY_COUNT);
+        return sc.nextLine();
+    }
+
+}
