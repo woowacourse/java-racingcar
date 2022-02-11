01b7a943d3ae69d6413882dac5b6d68cb5892a68
YJGwon <yj970125@gmail.com>
2022-02-11 15:14:32 +0900

    feat: 우승자 출력
    Game.java
       - showWinner(): Stream의Collectors.joining()을 사용해서 collect가 될 때원하는 형태의 문자열로 결합되게 함

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,15 +1,21 @@
 package racingcar;
 
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Game {
+
+	public static final String WINNER_NAME_DELIMITER = ", ";
+
 	public void start() {
 		Set<Car> cars = CarFactory.of(InputView.inputCarNames());
 		int count = InputView.inputGameCount();
 		OutputView.printGameResultTitle();
 		play(cars);
 		showResult(cars);
+		showWinner(Referee.judgeWinner(cars));
 	}
 
 	public Set<Car> play(Set<Car> cars) {
@@ -24,6 +30,12 @@ public class Game {
 			OutputView.printLineString(car.toString());
 		}
 		OutputView.printBlankLine();
+	}
+
+	public void showWinner(List<Car> winnerCars) {
+		String winnerNames = winnerCars.stream()
+			.map(Car::getName).collect(Collectors.joining(WINNER_NAME_DELIMITER));
+		OutputView.printWinner(winnerNames);
 	}
 
 	public int makeRandomValue() {
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class OutputView {
 	private static final String GAME_RESULT_TITLE = "\n실행 결과";
+	private static final String WINNER_FORMAT = "%s가 최종 우승했습니다.\n";
 
 	public static void printGameResultTitle() {
 		System.out.println(GAME_RESULT_TITLE);
@@ -13,5 +14,9 @@ public class OutputView {
 
 	public static void printBlankLine() {
 		System.out.println();
+	}
+
+	public static void printWinner(String winnerNames) {
+		System.out.printf(WINNER_FORMAT, winnerNames);
 	}
 }
