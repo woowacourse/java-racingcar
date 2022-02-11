1d9a36d3bb44739e6dbeab8d15128d7615674817
YuJeong <yjfflq4872@gmail.com>
2022-02-11 15:39:33 +0900

    refactor: 테스트 코드 수정

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -8,17 +8,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import racingcar.controller.RacingCarController;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.util.BoundedRandomNumberGenerator;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class RacingCarTest {
+	public RacingCarController racingCarController = new RacingCarController();
 	public BoundedRandomNumberGenerator randomNumberGenerator = new BoundedRandomNumberGenerator(9, 0);
 	public Cars cars = new Cars();
 
 	@Test
 	public void 자동차_이름_분리해서_가져오기_테스트() {
-		List<String> carNames = InputManager.splitAndSaveCarNames("이브,클레이,포비");
+		List<String> carNames = racingCarController.getCarNames("이브,클레이,포비");
 		assertThat(carNames.get(0)).isEqualTo("이브");
 		assertThat(carNames.get(1)).isEqualTo("클레이");
 		assertThat(carNames.get(2)).isEqualTo("포비");
@@ -26,21 +29,21 @@ public class RacingCarTest {
 
 	@Test
 	public void 빈_문자열_입력_테스트() {
-		assertThatThrownBy(() -> { InputManager.splitAndSaveCarNames("");})
+		assertThatThrownBy(() -> racingCarController.getCarNames(""))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("빈 문자열을 자동차 이름으로 입력할 수 없습니다.");
 	}
 
 	@Test
 	public void 자동차_이름_글자수_테스트() {
-		assertThatThrownBy(() -> { InputManager.splitAndSaveCarNames("이브,클레이클레이,포비"); })
+		assertThatThrownBy(() -> racingCarController.getCarNames("이브,클레이클레이,포비"))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("자동차 이름은 5자 이하여야 합니다.");
 	}
 
 	@Test
 	public void 자동차_이름이_존재하지_않는_경우_테스트() {
-		assertThatThrownBy(() -> { InputManager.splitAndSaveCarNames("이브,,포비");})
+		assertThatThrownBy(() -> racingCarController.getCarNames("이브,,포비"))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("모든 자동차 이름은 반드시 존재해야 합니다.");
 	}
@@ -48,27 +51,27 @@ public class RacingCarTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"이브, ,포비", "이브,   ,포비"})
 	public void 자동차_이름이_공백인_경우_테스트(String carNamesLine) {
-		assertThatThrownBy(() -> {  InputManager.splitAndSaveCarNames(carNamesLine); })
+		assertThatThrownBy(() -> racingCarController.getCarNames(carNamesLine))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("자동차 이름은 공백으로 설정할 수 없습니다.");
 	}
 
 	@Test
 	public void 자동차_이름_중복_테스트() {
-		assertThatThrownBy(() -> { InputManager.splitAndSaveCarNames("이브,이브,포비"); })
+		assertThatThrownBy(() -> racingCarController.getCarNames("이브,이브,포비"))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
 	}
 
 	@Test
 	public void 시도_횟수_입력_테스트() {
-		int gameCount = InputManager.getTrial("3");
+		int gameCount = racingCarController.getTrialCount("3");
 		assertThat(gameCount).isEqualTo(3);
 	}
 
 	@Test
 	public void 숫자가_아닌_시도_횟수_테스트() {
-		assertThatThrownBy(() -> { InputManager.getTrial("이브"); })
+		assertThatThrownBy(() -> racingCarController.getTrialCount("이브"))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("시도 횟수가 숫자가 아닙니다.");
 	}
@@ -76,7 +79,7 @@ public class RacingCarTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"-1", "0"})
 	public void 시도_횟수_음수_혹은_0_테스트(String trial) {
-		assertThatThrownBy(() -> { InputManager.getTrial(trial); })
+		assertThatThrownBy(() -> racingCarController.getTrialCount(trial))
 			.isInstanceOf(RuntimeException.class)
 			.hasMessageContaining("시도 횟수가 음수나 0이 될 수 없습니다.");
 	}
@@ -101,9 +104,9 @@ public class RacingCarTest {
 
 	@Test
 	public void 자동차_생성_테스트() {
-		List<String> carNames = InputManager.splitAndSaveCarNames("이브,클레이,포비");
+		List<String> carNames = racingCarController.getCarNames("이브,클레이,포비");
 		cars.generateCars(carNames);
-		assertThat(cars.size()).isEqualTo(3);
+		assertThat(cars.getSize()).isEqualTo(3);
 	}
 
 	@Test
