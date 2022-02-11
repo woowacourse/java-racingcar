3854bee73a042012b5a0650f5de9ce8dbe76035d
Donggyu Kim <a29661498@gmail.com>
2022-02-11 13:26:05 +0900

    test: 자동차 진행 테스트
    자동차 한 번 진행시 '전진' 혹은 '정지' 하는지 테스트

---
+++ src/test/java/racingcar/entity/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CarTest {
+
+    @Test
+    @DisplayName("자동차 진행시 전진 혹은 정지 하는지 확인")
+    public void 자동차_진행_테스트() throws Exception {
+        //given
+        Car car = new Car("woo");
+
+        //when
+        car.progress();
+
+        //then
+        assertThat(car.getPosition())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(1);
+    }
+}
