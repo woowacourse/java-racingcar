7c22b86401b53b4ee12cd79fab7fc2df5f3c19e0
Donggyu Kim <a29661498@gmail.com>
2022-02-11 13:46:33 +0900

    test: 시도횟수 검증 테스트
    시도횟수가 숫자가 아니면 예외가 제대로 발생하는지, 음수인 경우에도 제대로 예외가 발생하는지 테스트

--- src/test/java/racingcar/util/VerificationUtilTest.java
+++ src/test/java/racingcar/util/VerificationUtilTest.java
@@ -45,4 +45,28 @@ class VerificationUtilTest {
             validateCarName(name);
         });
     }
+
+    @Test
+    @DisplayName("시도 횟수가 정수인지 테스트")
+    public void 시도횟수_정수인지_테스트() throws Exception {
+        // given
+        String attempt = "숫자";
+
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validateAttempt(attempt);
+        });
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 음수인 경우 테스트")
+    public void 시도횟수_음수_테스트() throws Exception {
+        // given
+        String attempt = "-3";
+
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validateAttempt(attempt);
+        });
+    }
 }
