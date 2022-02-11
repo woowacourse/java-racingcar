b8d3474e6cac660c4f34a651701eecfdf205e3ab
pup-paw <wldusdl0310@gmail.com>
2022-02-11 13:48:10 +0900

    feat: 자동차 생성, 자동차들의 최대 위치 구하기, 자동차들의 정보 반환 기능

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,13 +1,20 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import racingcar.model.Car;
 import racingcar.model.CarList;
 import racingcar.util.IntegerConst;
+import racingcar.util.StringConst;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 	OutputViewController outputViewController = new OutputViewController();
 	CarListController carListController = new CarListController();
+	ArrayList<Car> carList;
+
 	public void runRace() {
 		OutputView.askCarName();
 		String[] carName = InputView.getCarNameInput();
@@ -26,5 +33,30 @@ public class GameController {
 			OutputView.displayCarPosition(outputViewController.mapCarList(carList));
 		}
 		OutputView.displayWinner(outputViewController.mapWinner(carList));
+	}
+
+	public ArrayList<Car> generateCarList(String[] names) {
+		this.carList = new ArrayList<>();
+		for (String name : names) {
+			carList.add(new Car(name));
+		}
+
+		return this.carList;
+	}
+
+	public int getMaxPosition(ArrayList<Car> carList) {
+		int max = 0;
+		for (Car car : carList) {
+			max = Math.max(max, car.getPosition());
+		}
+		return max;
+	}
+
+	public LinkedHashMap<String, String> getCarListInfo(ArrayList<Car> carList) {
+		LinkedHashMap<String, String> carListInfo = new LinkedHashMap<>();
+		for (Car car : carList) {
+			carListInfo.put(car.getName(), StringConst.POSITION_DELEIMIER.getValue().repeat(car.getPosition()));
+		}
+		return carListInfo;
 	}
 }
