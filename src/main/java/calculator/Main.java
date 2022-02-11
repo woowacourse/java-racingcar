3c31685cca0a4d625a144855ea36aa2f2b42d2a8
hamcheeseburger <yoo77hyeon@gmail.com>
2022-02-11 12:01:00 +0900

    refactor: calculator main 변수명 변경

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String CUSTOM_DELIMITER_1 = "//";
-    private static final String CUSTOM_DELIMITER_2 = "\n";
+    private static final String CUSTOM_DELIMITER_ANNOUNCER_1 = "//";
+    private static final String CUSTOM_DELIMITER_ANNOUNCER_2 = "\n";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        if(input.startsWith(CUSTOM_DELIMITER_1)) {
-            input += CUSTOM_DELIMITER_2 + scanner.nextLine();
+        if(input.startsWith(CUSTOM_DELIMITER_ANNOUNCER_1)) {
+            input += CUSTOM_DELIMITER_ANNOUNCER_2 + scanner.nextLine();
         }
         int result = Calculator.sum(input);
         System.out.println(result);
