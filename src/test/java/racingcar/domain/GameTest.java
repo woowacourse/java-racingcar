b4dc4d284d09e4a362246af5117a23fb0dd47016
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-11 17:15:14 +0900

    style(test): add displaynames for Game and ValidatorUtils test cases

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,6 +9,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_PLACEHOLDER;
+import static org.junit.jupiter.params.ParameterizedTest.DISPLAY_NAME_PLACEHOLDER;
 
 public class GameTest {
 
@@ -15,12 +18,17 @@ public class GameTest {
     CarRepository carRepository = new CarRepository();
     Game game;
 
+    final String PARAMETERIZED_TEST_DISPLAY_FORMAT =
+            DISPLAY_NAME_PLACEHOLDER + " ["
+                    + ARGUMENTS_PLACEHOLDER + "]";
+
     @BeforeEach
     void beforeEach(){
         carRepository.clear();
         game = new Game(new String[]{"pobi", "jeong", "roma"}, TOTAL_ROUNDS);
     }
 
+    @DisplayName("getWinners 메서드는 가장 position 값이 큰 car 객체들을 반환한다")
     @Test
     void getWinners(){
         for (int i = 0; i < TOTAL_ROUNDS; i++) {
@@ -35,7 +43,8 @@ public class GameTest {
         }
     }
 
-    @ParameterizedTest
+    @DisplayName("isOver 메서드는 totalRounds와 currentRounds의 일치 여부를 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @CsvSource(value = {"4,false", "5,true"}, delimiter = ',')
     void isOver(String roundNumString, String resultString) {
         int roundNum = Integer.parseInt(roundNumString);
--- src/test/java/racingcar/util/ValidatorUtilsTest.java
+++ src/test/java/racingcar/util/ValidatorUtilsTest.java
@@ -1,17 +1,30 @@
 package racingcar.util;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_PLACEHOLDER;
+import static org.junit.jupiter.params.ParameterizedTest.DISPLAY_NAME_PLACEHOLDER;
+import static racingcar.constants.OutputMessages.BLANK_NOT_ALLOWED_EXCEPTION;
+import static racingcar.constants.OutputMessages.DUPLICATE_NAME_EXCEPTION;
+import static racingcar.constants.OutputMessages.NOT_POSITIVE_INTEGER_EXCEPTION;
+import static racingcar.constants.OutputMessages.OVER_FIVE_CHARACTERS_EXCEPTION;
+import static racingcar.constants.SystemConstants.COMMA;
 import static racingcar.util.ValidatorUtils.splitAndValidateCarNames;
 import static racingcar.util.ValidatorUtils.validateAndParsePositiveInt;
 
 public class ValidatorUtilsTest {
 
-    @ParameterizedTest
+    final String PARAMETERIZED_TEST_DISPLAY_FORMAT =
+            DISPLAY_NAME_PLACEHOLDER + " ["
+                    + ARGUMENTS_PLACEHOLDER + "]";
+
+    @DisplayName("validateAndParsePositiveInt 메서드는 문자열을 양수로 변환하여 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"1", "2", "10"})
     void validateAndParsePositiveInt_returnParsedPositiveInteger(String string) {
         int parsedInt = validateAndParsePositiveInt(string);
@@ -19,50 +32,56 @@ public class ValidatorUtilsTest {
         assertThat(parsedInt).isPositive();
     }
 
-    @ParameterizedTest
+    @DisplayName("validateAndParsePositiveInt 메서드는 양수 이외의 문자열이 입력되었을 때 예외를 발생시킨다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"한글", "eng", "$", "1.0"})
     void validateAndParsePositiveInt_errorOnNonInteger(String string) {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> validateAndParsePositiveInt(string));
     }
 
-    @ParameterizedTest
+    @DisplayName("validateAndParsePositiveInt 메서드는 음수 혹은 0에 해당되는 문자열이 입력되었을 때 예외를 발생시킨다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"-1", "0"})
     void validateAndParsePositiveInt_errorOnNonPositiveInteger(String string) {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> validateAndParsePositiveInt(string))
-                .withMessageMatching("양수를 입력해야 합니다.");
+                .withMessageMatching(NOT_POSITIVE_INTEGER_EXCEPTION);
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndValidateCarNames 메서드는 문자열을 쉼표를 기준으로 구분하여 반환한다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"a,b,jeong", "#,$%^&*", "roma,b d", "1  2,c,d"})
     void splitAndValidateCarNames_returnStringArrayOnPass(String carNamesString) {
         String[] carNames = splitAndValidateCarNames(carNamesString);
-        assertThat(carNames).isEqualTo(carNamesString.split(","));
+        assertThat(carNames).isEqualTo(carNamesString.split(COMMA));
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndValidateCarNames 메서드는 공백 혹은 빈 문자열이 입력되었을 때 예외를 발생시킨다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"", ",a"," ,b", "   ,c,d"})
     void splitAndValidateCarNames_errorOnBlank(String carNamesString) {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> splitAndValidateCarNames(carNamesString))
-                .withMessageMatching("공백을 입력하면 안 됩니다.");
+                .withMessageMatching(BLANK_NOT_ALLOWED_EXCEPTION);
     }
 
-    @ParameterizedTest
+    @DisplayName("splitAndValidateCarNames 메서드는 5글자를 초과하는 문자열이 입력되었을 때 예외를 발생시킨다.")
+    @ParameterizedTest(name = PARAMETERIZED_TEST_DISPLAY_FORMAT)
     @ValueSource(strings = {"123456,a,b", "carrots,c,d"})
     void splitAndValidateCarNames_errorOnOverFiveCharacters(String carNamesString) {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> splitAndValidateCarNames(carNamesString))
-                .withMessageMatching("5글자 이하의 이름을 입력해야 합니다.");
+                .withMessageMatching(OVER_FIVE_CHARACTERS_EXCEPTION);
     }
 
+    @DisplayName("splitAndValidateCarNames 메서드는 중복된 이름이 입력되었을 때 예외를 발생시킨다.")
     @Test
     void splitAndValidateCarNames_errorOnDuplicates() {
         String carNamesString = "aa,aa,b";
 
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> splitAndValidateCarNames(carNamesString))
-                .withMessageMatching("중복된 이름을 입력하면 안 됩니다.");
+                .withMessageMatching(DUPLICATE_NAME_EXCEPTION);
     }
 }
