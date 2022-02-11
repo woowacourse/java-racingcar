f1591b9b1efcd06456412bdf0966b6a80bb4c90d
hyewoncc <hyewoncc@gmail.com>
2022-02-11 15:18:50 +0900

    refactor: 메소드 분리

--- src/main/java/addCalculatorTest/StringCalculator.java
+++ src/main/java/addCalculatorTest/StringCalculator.java
@@ -1,36 +1,65 @@
 package addCalculatorTest;
 
 public class StringCalculator {
+    private static final String DELIMITERS = "-:,;";
+
     public static int splitAndSum(String s) {
-        if (s == null || s == "") {
+        if (isNullOrBlank(s)) {
             return 0;
         }
 
-        if (!(s.contains(",") || s.contains(":") || s.contains(";"))) {
+        if (isOneLengthDigit(s)) {
             return Integer.parseInt(s);
         }
 
-        //
+        int sum = 0;
+        for (String number : removeWrongCharacters(s).split(",|:|;")) {
+            sum += extractInt(number);
+        }
+
+        return sum;
+    }
+
+    private static boolean isNullOrBlank(final String s) {
+        if (s == null || s == "") {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOneLengthDigit(final String s) {
+        if (s.length() == 1 && "0123456789".contains(s)){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isDelimOrDigit(final char c) {
+        if (DELIMITERS.contains(Character.toString(c)) || (c >= '0' && c <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String removeWrongCharacters(final String s) {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (char c : s.toCharArray()) {
-            if (c == '-' || c == ':' || c == ',' || c == ';' || (c >= '0' && c <= '9')) {
+            if (isDelimOrDigit(c)) {
                 stringBuilder.append(c);
             }
         }
 
-        String[] split = stringBuilder.toString().split(",|:|;");
+        return stringBuilder.toString();
+    }
 
-        int sum = 0;
-        for (String number : split) {
-            if (number.contains("-")) {
-                throw new RuntimeException();
-            }
-            if (number.equals("")) {
-                continue;
-            }
-            sum += Integer.parseInt(number);
+    private static int extractInt(final String numberString) {
+        if (numberString.contains("-")) {
+            throw new RuntimeException();
         }
-        return sum;
+        if (numberString.equals("")) {
+            return 0;
+        }
+        return Integer.parseInt(numberString);
     }
 }
