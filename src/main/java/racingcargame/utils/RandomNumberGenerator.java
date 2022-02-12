797798b765d8f11ff006b408ee05b91dceec820f
jaejae-yoo <wotj102@gmail.com>
2022-02-12 16:14:56 +0900

    refacor: 난수 생성 라이브러리 사용-> 난수 생성 로직 생성

--- src/main/java/racingcargame/utils/RandomNumberGenerator.java
+++ src/main/java/racingcargame/utils/RandomNumberGenerator.java
@@ -1,15 +1,54 @@
 package racingcargame.utils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class RandomNumberGenerator {
-    private static final int MAX_NUMBER = 10;
+    private static final Random random = new Random();
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int RANDOM_NUMBER_PICK = 0;
+    private static final int SHUFFLE_FOR_INDEX_FIRST = 0;
+    private static final int SHUFFLE_FOR_INDEX_SECOND = 1;
 
     private RandomNumberGenerator() {
     }
 
     public static int makeRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(MAX_NUMBER);
+        return shuffle();
+    }
+
+    public static int getRandomNumber(List<Integer> numbers) {
+        return numbers.get(RANDOM_NUMBER_PICK);
+    }
+
+    private static int shuffle() {
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        for (int i = 0; i < MAX_RANDOM_NUMBER; i++) {
+            int indexFirst = random.nextInt(MAX_RANDOM_NUMBER + 1);
+            int indexSecond = random.nextInt(MAX_RANDOM_NUMBER + 1);
+
+            List<Integer> indexList = makeDifferentIndexForShuffle(random, indexFirst, indexSecond, MAX_RANDOM_NUMBER);
+            indexFirst = indexList.get(SHUFFLE_FOR_INDEX_FIRST);
+            indexSecond = indexList.get(SHUFFLE_FOR_INDEX_SECOND);
+
+            swap(numbers, indexFirst, indexSecond);
+        }
+        return getRandomNumber(numbers);
+    }
+
+    public static List<Integer> swap(List<Integer> numbers, int indexFirst, int indexSecond) {
+        int tmp = numbers.get(indexFirst);
+        numbers.set(indexFirst, numbers.get(indexSecond));
+        numbers.set(indexSecond, tmp);
+        return numbers;
+    }
+
+    public static List<Integer> makeDifferentIndexForShuffle(Random random, int indexFirst, int indexSecond, int length) {
+        while (indexFirst == indexSecond) {
+            indexFirst = random.nextInt(length);
+            indexSecond = random.nextInt(length);
+        }
+        return Arrays.asList(indexFirst, indexSecond);
     }
 }
--- src/test/java/racingcargame/utils/RandomNumberGeneratorTest.java
+++ src/test/java/racingcargame/utils/RandomNumberGeneratorTest.java
@@ -2,17 +2,40 @@ package racingcargame.utils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RandomNumberGeneratorTest {
+    private static final List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:9", "0:3", "0:8", "0:4"}, delimiter = ':')
+    void 난수_생성을_위한_인덱스_swap_테스트(int input, int expected) {
+        int randomNumber = RandomNumberGenerator.getRandomNumber(RandomNumberGenerator.swap(numbers, input, expected));
+
+        assertEquals(expected, randomNumber);
+    }
 
     @Test
     void 난수_생성_범위_테스트() {
-        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         int actual = RandomNumberGenerator.makeRandomNumber();
 
         assertThat(numbers.contains(actual)).isTrue();
     }
+
+    @Test
+    void swap을_위한_서로_다른_인덱스_추출() {
+        Random random = new Random();
+        List<Integer> indexList = RandomNumberGenerator
+                .makeDifferentIndexForShuffle(random, 0, 0, 10);
+
+        assertThat(indexList.get(0)).isNotEqualTo(indexList.get(1));
+    }
+
 }
