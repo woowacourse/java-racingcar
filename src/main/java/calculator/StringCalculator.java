ebedbecaa6f0d6511543c1e77f9f629cf376e17d
SeungCheol <ssc6839@gmail.com>
2022-02-12 15:45:04 +0900

    refactor(calculator): 문자열 덧셈 계산기 리팩터링
    - 매직 넘버 -> 상수 변경
    - 변수 사용 범위에 맞게 선언 위치 변경
    - 하나의 테스트 메서드에서 하나의 테스트만 수행하도록 변경
    - 메소드명 일부 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,30 +5,39 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+    public static final int CUSTOM_DELIMITER_INDEX = 1;
+    public static final int TEXT_INDEX = 2;
+
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
+
         return sum(split(text));
     }
 
     private static String[] split(String text) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+
+            return m.group(TEXT_INDEX).split(customDelimiter);
         }
-        return text.split(",|:");
+
+        return text.split("[,:]");
     }
 
     private static int sum(String[] tokens) {
         int result = 0;
-        int number;
+
         for (String token : tokens) {
-            number = Integer.parseInt(token);
+            int number = Integer.parseInt(token);
+
             validateNegative(number);
             result += number;
         }
+
         return result;
     }
 
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,48 +5,53 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class StringCalculatorTest {
-    @Test
-    public void splitAndSum_null_또는_빈문자() throws Exception {
-        int result = StringCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
 
-        result = StringCalculator.splitAndSum("");
+    @Test
+    public void splitAndSum_null() {
+        int result = StringCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    public void splitAndSum_빈문자() {
+        int result = StringCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() {
         int result = StringCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    public void splitAndSum_쉼표구분자() {
         int result = StringCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
         int result = StringCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
+    public void splitAndSum_커스텀_구분자() {
         int result = StringCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_negative_음수() throws Exception {
+    public void splitAndSum_음수입력_예외처리() {
         assertThatThrownBy(() -> StringCalculator.splitAndSum("-1,2,3"))
             .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    public void splitAndSum_negative_문자() throws Exception {
+    public void splitAndSum_문자입력_예외처리() {
         assertThatThrownBy(() -> StringCalculator.splitAndSum("q,2,3"))
             .isInstanceOf(RuntimeException.class);
     }
