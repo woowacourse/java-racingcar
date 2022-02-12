a892cc0a5e05e83c28b8f2916bb9c2669a39f9b4
jaejae-yoo <wotj102@gmail.com>
2022-02-12 23:22:12 +0900

    refactor: 난수 값 범위 텍스트 픽처스 초기화

--- src/test/java/racingcargame/utils/RandomNumberGeneratorTest.java
+++ src/test/java/racingcargame/utils/RandomNumberGeneratorTest.java
@@ -7,13 +7,19 @@ import java.util.Random;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RandomNumberGeneratorTest {
-    private static final List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    private List<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"0:9", "0:3", "0:8", "0:4"}, delimiter = ':')
