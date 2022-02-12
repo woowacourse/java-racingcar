b242bd39252304099bd50774a858f37d34b0807c
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-12 22:42:57 +0900

    refactor: 상수화 하여 중복 로직 제거

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -6,15 +6,14 @@ public class Input {
 
 	static final String REQUEST_CARS_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	static final String REQUEST_COUNT = "시도할 회수는 몇회인가요?";
+	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String inputCarsName() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println(REQUEST_CARS_NAME);
 		return scanner.nextLine();
 	}
 
 	public static String inputCount() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println(REQUEST_COUNT);
 		return scanner.nextLine();
 	}
