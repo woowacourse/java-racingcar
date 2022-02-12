6445952f09af763ab8291c5a5cc85b34910f6929
yxxnghwan <younghwan960@gmail.com>
2022-02-13 02:39:56 +0900

    refactor(racing-car): RacingServiceTest 테스트케이스 준비 과정 분리
    @BeforeEach, @AfterEach 활용해서 각 테스트 케이스 시행마다 경주에 참여할 자동차들 세팅

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -4,53 +4,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
 public class RacingServiceTest {
     private static final int PROCEED_FlAG_NUMBER = 4;
+    private static final List<Car> cars = new ArrayList<>();
 
-    //todo: BeforeEach, AfterEach 활용해서 테스트 준비과정 줄이기
+    @BeforeEach
+    void beforeEach() {
+        cars.add(new Car("A"));
+        cars.add(new Car("B"));
+        cars.add(new Car("C"));
+        cars.add(new Car("D"));
+    }
+
+    @AfterEach
+    void afterEach() {
+        cars.clear();
+    }
+
 
     @Test
     @DisplayName("우승자 판단 - 단독")
     void checkSingleWinner() {
-        Car carA = new Car("A");
-        Car carB = new Car("B");
-        Car carC = new Car("C");
-        Car carD = new Car("D");
-
-        carA.proceed(PROCEED_FlAG_NUMBER);
-
-        List<Car> cars = new ArrayList<Car>();
-        cars.add(carA);
-        cars.add(carB);
-        cars.add(carC);
-        cars.add(carD);
-
+        cars.get(0).proceed(PROCEED_FlAG_NUMBER);
         RacingService racingService = new RacingService(cars);
-        assertThat(racingService.findWinners()).contains(carA);
+        assertThat(racingService.findWinners()).contains(cars.get(0));
     }
 
     @Test
     @DisplayName("우승자 판단 - 공동 우승")
     void checkCoWinner() {
-        Car carA = new Car("A");
-        Car carB = new Car("B");
-        Car carC = new Car("C");
-        Car carD = new Car("D");
-
-        carA.proceed(PROCEED_FlAG_NUMBER);
-        carB.proceed(PROCEED_FlAG_NUMBER);
-
-        List<Car> cars = new ArrayList<Car>();
-        cars.add(carA);
-        cars.add(carB);
-        cars.add(carC);
-        cars.add(carD);
-
+        cars.get(0).proceed(PROCEED_FlAG_NUMBER);
+        cars.get(1).proceed(PROCEED_FlAG_NUMBER);
         RacingService racingService = new RacingService(cars);
-        assertThat(racingService.findWinners()).contains(carA, carB);
+        assertThat(racingService.findWinners()).contains(cars.get(0), cars.get(1));
     }
 }
