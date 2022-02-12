06556fd89eacf3dc1084ba1a68665d08ac4c9242
skullKim <kyk990328@gmail.com>
2022-02-12 11:24:36 +0900

    refactor(InputValidation.java): Pattern객체 사용 방식 변경
    피드백을 수용해 성능 향상을 위해 Pattern을 컴파일 한 후 사용하게 수정
    
    BREAKING CHNAGE: Pattern으로 입력값과 정규식 일치 여부를 판단하는 코드
    수정
    as-is:
    public class InputValidation {
    
        ...
        private final static String NUMBER_PATTERN = "^[0-9]*$";
    
        ...
    
        public void validateRepeats(final String repeats) {
            if (!COMPILED_NUMBER_PATTERN.matcher(repeats).matches()) {
                ...
            }
        }
    }
    
    to-be:
    public class InputValidation {
    
        ...
        private final static String NUMBER_PATTERN = "^[0-9]*$";
        private final static Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
    
        ...
    
        public void validateRepeats(final String repeats) {
            if (!COMPILED_NUMBER_PATTERN.matcher(repeats).matches()) {
                ...
            }
        }
    }

--- src/main/java/racingcar/utils/InputValidation.java
+++ src/main/java/racingcar/utils/InputValidation.java
@@ -6,6 +6,7 @@ public class InputValidation {
 
 	private final static int MAX_NAME_LENGTH = 5;
 	private final static String NUMBER_PATTERN = "^[0-9]*$";
+	private final static Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
 
 	public void validateName(final String names) {
 		final String[] carNames = names.split(RacingGameMessage.CAR_NAME_DISTRIBUTOR);
@@ -21,7 +22,7 @@ public class InputValidation {
 	}
 
 	public void validateRepeats(final String repeats) {
-		if (!Pattern.matches(NUMBER_PATTERN, repeats)) {
+		if (!COMPILED_NUMBER_PATTERN.matcher(repeats).matches()) {
 			throw new RuntimeException(RacingGameMessage.REPEATS_ERROR);
 		}
 	}
