288a69592130d402ee8e9537bf3aafbcd35f2c16
jayjaehunchoi <wogns0108@naver.com>
2022-02-13 16:04:13 +0900

    test : StubNumberGenerator 수정 후 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,16 +1,34 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.utils.NumberGenerator;
 import racingcar.utils.StubNumberGenerator;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class CarsTest{
 
+    NumberGenerator numberGenerator;
+
+    @BeforeEach
+    void setUp() {
+        numberGenerator = new StubNumberGenerator();
+    }
+
+    @Test
+    void 자동차_우승_1명_성공() {
+        Cars cars = new Cars(new String[]{"jae","rick"});
+        cars.moveCars(numberGenerator);
+        assertThat(cars.getWinners()).isEqualTo("rick가 최종 우승 했습니다.");
+    }
+
     @Test
     void 자동차_우승_2명_성공() {
-        Cars cars = new Cars(new String[]{"jae","rick"});
-        cars.moveCars(new StubNumberGenerator());
+        Cars cars = new Cars(new String[]{"jae", "rick"});
+        for (int i = 0; i < 2; i++) {
+            cars.moveCars(numberGenerator);
+        }
         assertThat(cars.getWinners()).isEqualTo("jae, rick가 최종 우승 했습니다.");
     }
 
--- src/test/java/racingcar/utils/StubNumberGenerator.java
+++ src/test/java/racingcar/utils/StubNumberGenerator.java
@@ -2,8 +2,17 @@ package racingcar.utils;
 
 public class StubNumberGenerator implements NumberGenerator{
 
+    private static final int NOT_MOVABLE = 3;
+    private static final int MOVABLE = 4;
+
+    private int[] numbers = new int[]{NOT_MOVABLE, MOVABLE, MOVABLE, NOT_MOVABLE};
+    private int sequence = 0;
+
     @Override
     public int generate() {
-        return 4;
+        if (sequence == 4) {
+            sequence = 0;
+        }
+        return numbers[sequence++];
     }
 }
