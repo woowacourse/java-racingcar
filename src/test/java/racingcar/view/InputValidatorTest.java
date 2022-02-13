3c68e816dbb01d1d4ba0cd5c6115266376d3bebc
jochungbeom <cndqjacndqja@gmail.com>
2022-02-13 23:40:59 +0900

    refactor(InputValidator): validPositiveInput() 검증 로직 수정

--- src/test/java/racingcar/view/InputValidatorTest.java
+++ src/test/java/racingcar/view/InputValidatorTest.java
@@ -6,20 +6,29 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
-	@ParameterizedTest
-	@ValueSource(strings = {"0", "-1"})
-	void 시도_횟수_입력_검증(String input) {
-		Assertions.assertThatThrownBy(() -> {
-				InputValidator.validIterationNo(input);
-			}).isInstanceOf(RuntimeException.class)
-			.hasMessageContaining("양수");
-	}
+    @Test
+    void 시도_횟수_빈_입력_검증() {
+        Assertions.assertThatThrownBy(() -> {
+                    InputValidator.validIterationNo("");
+                }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("빈 값");
+    }
 
-	@Test
-	void 시도_횟수_빈_입력_검증() {
-		Assertions.assertThatThrownBy(() -> {
-				InputValidator.validIterationNo("");
-			}).isInstanceOf(RuntimeException.class)
-			.hasMessageContaining("빈 값");
-	}
+    @Test
+    void 시도_횟수_숫자_입력_검증() {
+        String input = "문자열";
+        Assertions.assertThatThrownBy(() -> {
+                    InputValidator.validIterationNo(input);
+                }).isInstanceOf(NumberFormatException.class)
+                .hasMessageContaining("입력 받은 값이 올바른 숫자 값이 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1"})
+    void 시도_횟수_양수_입력_검증(String input) {
+        Assertions.assertThatThrownBy(() -> {
+                    InputValidator.validIterationNo(input);
+                }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("양수");
+    }
 }
