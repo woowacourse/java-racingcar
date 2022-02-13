339e253129428c70bace651c636fa816d886d02f
Ohzzi <clover7even@naver.com>
2022-02-14 02:25:54 +0900

    feat: Position 객체에 equals(), hashCode() 오버라이드

--- src/main/java/racingcar/model/value/Position.java
+++ src/main/java/racingcar/model/value/Position.java
@@ -1,5 +1,7 @@
 package racingcar.model.value;
 
+import java.util.Objects;
+
 public class Position {
     private static final int INITIAL_POSITION = 0;
 
@@ -15,5 +17,22 @@ public class Position {
 
     public int toInt() {
         return this.position;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Position position1 = (Position) obj;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
