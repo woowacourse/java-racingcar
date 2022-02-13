786ad52a6d0ca546ddf095d3249e03d7ab7b0de1
Donggyu Kim <a29661498@gmail.com>
2022-02-13 14:17:31 +0900

    refactor: RandomUtil 의 매직넘버에 상수 적용
    랜덤한 값의 범위를 지정하는 10이라는 매직넘버에 대해서 상수로 표현

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -2,6 +2,7 @@ package racingcar.util;
 
 public class RandomUtil {
 
+    public static final int RANDOM_RANGE= 10;
     private static final int CONDITION_VALUE = 4;
 
     private RandomUtil() {
@@ -12,6 +13,6 @@ public class RandomUtil {
     }
 
     private static int getRandomNumber() {
-        return (int) Math.floor(Math.random()*10);
+        return (int) Math.floor(Math.random()*RANDOM_RANGE);
     }
 }
