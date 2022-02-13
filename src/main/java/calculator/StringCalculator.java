c342ec43dfae917f58f7ef03f70ff30e0e50e247
유영우 <fbduddn97@naver.com>
2022-02-13 15:34:38 +0900

    refactor: NaturalNumber 불필요한 class 제거

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,6 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static final String NOT_NUMBER_ERROR = "입력값은 숫자여야 합니다.";
+    private static final String NEGATIVE_NUMBER_ERROR = "입력값은 자연수여야 합니다.";
+
     private static final String NORMAL_DELIMITER = "[,:]";
     private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
@@ -29,15 +32,32 @@ public class StringCalculator {
         return text.split(NORMAL_DELIMITER);
     }
 
-    private static List<NaturalNumber> toInts(final String[] values) {
+    private static List<Integer> toInts(final String[] values) {
         return Arrays.stream(values)
-            .map(NaturalNumber::new)
+            .map(StringCalculator::toInt)
             .collect(toList());
     }
 
-    private static int sum(final List<NaturalNumber> numbers) {
+    private static int toInt(final String value) {
+        validateNumber(value);
+        return Integer.parseInt(value);
+    }
+
+    private static void validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(NOT_NUMBER_ERROR);
+        }
+
+        if (Integer.parseInt(number) < 0) {
+            throw new RuntimeException(NEGATIVE_NUMBER_ERROR);
+        }
+    }
+
+    private static int sum(final List<Integer> numbers) {
         return numbers.stream()
-            .mapToInt(NaturalNumber::get)
+            .mapToInt(number -> number)
             .sum();
     }
 }
