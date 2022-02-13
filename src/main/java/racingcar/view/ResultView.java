c88abc1c30bfc0c729644a7651478bc7ba06a05f
jochungbeom <cndqjacndqja@gmail.com>
2022-02-14 00:28:09 +0900

    refactor: CarDto toString() 제거

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,19 +1,21 @@
 package racingcar.view;
 
+import racingcar.model.CarDto;
+
 import java.util.List;
 import java.util.stream.Collectors;
-
-import racingcar.model.CarDto;
 
 public class ResultView {
 	private static final String BEFORE_GAME_RESULT_MESSAGE = "실행 결과";
 	private static final String JOIN_DELIMITER = ", ";
 	private static final String WINNERS_RESULT_SUFFIX = "가 최종 우승했습니다.";
+	private static final String SUFFIX_PRINT_RESULT_CAR_NAME = " : ";
+	private static final String CAR_POSITION_STATUS_EXPRESSION = "-";
 
 	public static void printGameResult(List<CarDto> cars) {
-		cars.stream()
-			.map(CarDto::toString)
-			.forEach(System.out::println);
+		for (CarDto car: cars) {
+			System.out.println(car.getName() + SUFFIX_PRINT_RESULT_CAR_NAME + CAR_POSITION_STATUS_EXPRESSION.repeat(car.getPosition()));
+		}
 		printEmptyLine();
 	}
 
