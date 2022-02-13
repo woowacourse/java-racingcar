f176a48b13cc6966159f1078d3e2764cfae87daf
Donggyu Kim <a29661498@gmail.com>
2022-02-13 16:03:01 +0900

    test: 보다 정확히 자동차의 전진 혹은 정지 테스트
    수정한 Car 클래스의 progress() 에 따라서 보다 정확하게 자동차의 전진 혹은 정지를 테스트하도록 수정하였습니다.

--- src/test/java/racingcar/entity/CarTest.java
+++ src/test/java/racingcar/entity/CarTest.java
@@ -8,36 +8,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SuppressWarnings("NonAsciiCharacters")
 class CarTest {
 
+    public static final int MOVE_CONDITION= 10;
+    public static final int STOP_CONDITION = 0;
+
     @Test
-    @DisplayName("자동차 진행시 전진 혹은 정지 하는지 확인")
+    @DisplayName("자동차에 기준값 보다 큰 값을 주면 전진 하는지 확인")
     public void 자동차_진행_테스트() {
         //given
         Car car = new Car("woo");
 
         //when
-        car.progress();
+        car.progress(MOVE_CONDITION);
 
         //then
-        assertThat(car.getPosition())
-                .isGreaterThanOrEqualTo(0)
-                .isLessThanOrEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("자동차 반복 진행시 정상 동작 확인")
+    @DisplayName("자동차에 기준값 보다 작은 값을 주면 정지 하는지 확인")
+    public void 자동차_정지_테스트() {
+        //given
+        Car car = new Car("woo");
+
+        //when
+        car.progress(STOP_CONDITION);
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차가 정해진 회수만큼 반복 진행시 정상적으로 동작하는지 확인")
     public void 자동차_반복_진행_테스트() {
         //given
         Car car = new Car("woo");
-        int count = 5;
+        int repeatCount = 5;
 
         //when
-        for (int i = 0; i < count; i++) {
-            car.progress();
+        for (int i = 0; i < repeatCount; i++) {
+            car.progress(MOVE_CONDITION);
         }
 
         //then
-        assertThat(car.getPosition())
-                .isGreaterThanOrEqualTo(0)
-                .isLessThanOrEqualTo(5);
+        assertThat(car.getPosition()).isEqualTo(repeatCount);
     }
 }
