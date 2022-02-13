c861cc4475a13845b58c4d88a5122f4eb4c469b1
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-13 23:02:46 +0900

    test(RandomUtils): add test cases on boundary values

--- src/test/java/racingcar/util/RandomUtilsTest.java
+++ src/test/java/racingcar/util/RandomUtilsTest.java
@@ -2,7 +2,6 @@ package racingcar.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.util.RandomUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +13,31 @@ public class RandomUtilsTest {
         for (int i = 0; i < 10000; i++) {
             int randomNum = RandomUtils.generateNumber();
             assertThat(randomNum).isBetween(0, 9);
+        }
+    }
+
+    @DisplayName("generateNumber 메서드는 0도 반환할 수 있다.")
+    @Test
+    void generateNumber_includeZero() {
+        boolean foundZero = false;
+
+        for (int i = 0; i < 10000; i++) {
+            int randomNum = RandomUtils.generateNumber();
+            if (randomNum == 0) {
+                foundZero = true;
+                break;
+            }
+        }
+
+        assertThat(foundZero).isTrue();
+    }
+
+    @DisplayName("generateNumber 메서드는 10을 반환하지 않는다.")
+    @Test
+    void generateNumber_excludeTen() {
+        for (int i = 0; i < 10000; i++) {
+            int randomNum = RandomUtils.generateNumber();
+            assertThat(randomNum).isNotEqualTo(10);
         }
     }
 }
