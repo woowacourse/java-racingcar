5f3967df228e6df2144bce7d7930c6d41b2fed52
Hongdonggeon <ghd700@naver.com>
2022-02-14 04:17:01 +0900

    refactor: Car 객체 생성해주는 팩토리 메서드 역할 분리
    CarFactory.java
       of(): 이전에는 문자열을 쪼개서 중복 검사를 해주고 Set으로 반환했다면, 증복검사는 Cars 객체에 위임하고 단지 문자열을 쪼개서 Car객체만 생성해주는 기능만 하도록함

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -1,25 +1,18 @@
 package racingcar;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CarFactory {
     private static final String DUPLICATED_NAME_ERROR = "[ERROR] 차 이름은 중복될 수 없습니다.";
 
-    public static Set<Car> of(String names) {
+    public static List<Car> of(String names) {
         String[] arrNames = names.split(",");
-        Set<Car> cars = Arrays.stream(arrNames)
+        return  Arrays.stream(arrNames)
                 .map(String::trim)
                 .map(Car::new)
-                .collect(Collectors.toSet());
-        validateDuplicatedCarName(arrNames, cars);
-        return cars;
-    }
-
-    private static void validateDuplicatedCarName(String[] arrNames, Set<Car> cars) {
-        if (arrNames.length != cars.size()) {
-            throw new IllegalArgumentException(DUPLICATED_NAME_ERROR);
-        }
+                .collect(Collectors.toList());
     }
 }
