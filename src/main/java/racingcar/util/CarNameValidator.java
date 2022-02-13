50f42f474ce8cb60679b15c6dd5c6667f271a9eb
meatsby <meatsby@gmail.com>
2022-02-13 16:49:35 +0900

    refactor: 에러 메세지 구체화

--- src/main/java/racingcar/util/CarNameValidator.java
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -6,19 +6,19 @@ public class CarNameValidator {
 
     public static void validate(String text) {
         if (text == null || text.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("참가자 명단은 공백일 수 없습니다.");
         }
     }
 
     public static void checkQuantity(String[] carNames) {
         if (carNames.length == EMPTY) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1대 이상의 자동차가 참여해야합니다.");
         }
     }
 
     public static void checkCarName(String carName) {
         if (carName == null || carName.isEmpty() || carName.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
         }
     }
 }
--- src/main/java/racingcar/util/RoundValidator.java
+++ src/main/java/racingcar/util/RoundValidator.java
@@ -10,13 +10,13 @@ public class RoundValidator {
 
     private static void checkDigit(String round) {
         if (!round.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("라운드 횟수는 숫자만 가능합니다.");
         }
     }
 
     private static void checkPositive(String round) {
         if (Integer.parseInt(round) < POSITIVE_INTEGER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("라운드 횟수는 자연수만 가능합니다.");
         }
     }
 }
