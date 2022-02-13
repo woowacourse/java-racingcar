6597b72262cd4ee4eb8588a65a1ed83183346aff
meatsby <meatsby@gmail.com>
2022-02-13 13:25:10 +0900

    refactor: StringCalculator 클래스로 통합 및 로직 개선
    - 변수명, 메서드명 수정
    - 매직넘버 상수화

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,23 +1,50 @@
 package calculator;
 
-public class StringCalculator {
-    private static final Parser parser = new Parser();
-    private static final Calculator calculator = new Calculator();
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
-        splitAndSum("1,2,3");
-    }
+public class StringCalculator {
+    public static final String ORIGINAL_DELIMITER = "[,:]";
+    public static final String CUSTOM_REGEX = "//(.)\n(.*)";
+    public static final int CUSTOM_DELIMITER_INDEX = 1;
+    public static final int CUSTOM_INPUT_INDEX = 2;
 
     public static int splitAndSum(String input) {
-        String[] values = parser.splitter(input);
-        parser.checkNumber(values);
-
-        int[] nums = new int[values.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(values[i]);
+        if (input == null || input.isEmpty()) {
+            return 0;
         }
 
-        return calculator.sum(nums);
+        return sum(split(input));
+    }
+
+    private static String[] split(String input) {
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX).matcher(input);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
+            String customInput = matcher.group(CUSTOM_INPUT_INDEX);
+
+            return customInput.split(customDelimiter);
+        }
+
+        return input.split(ORIGINAL_DELIMITER);
+    }
+
+    private static int sum(String[] values) {
+        int result = 0;
+
+        for (String value : values) {
+            int number = Integer.parseInt(value);
+            checkNegative(number);
+            result += number;
+        }
+
+        return result;
+    }
+
+    private static void checkNegative(int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
     }
 }
