32e190449b361618a6e95fde03bc2a30d159f82e
dongho <dongho108@naver.com>
2022-02-13 17:55:27 +0900

    refactor : 이름입력검증 오류메세지 수정

--- src/main/java/racingcar/utils/NameValidator.java
+++ src/main/java/racingcar/utils/NameValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class NameValidator {
     private static final Pattern pattern = Pattern.compile("[a-zA-Z가-힣0-9]{1,5}(,[a-zA-Z가-힣0-9]{1,5})*");
-    private static final String WRONG_INPUT_MESSAGE = "올바르지 않은 입력입니다.";
+    private static final String WRONG_INPUT_MESSAGE = "이름은 최소 한명이상이어야 하고, 이름의 길이는 1글자 이상 5글자 이하여야 합니다.";
     private static final String DUPLICATED_NAME_MESSAGE = "중복된 이름이 있습니다.";
 
     public static void isValidateNames(String carNames) {
