796d779b2d90bb9d5312cb7f0f00bf86901a5f33
BeomsooKim <kbsat@naver.com>
2022-02-14 00:37:02 +0900

    refactor(ResultOutputMessages): use lineSeperator for compatibility with other OS

--- src/main/java/racingcar/constants/output/ResultOutputMessages.java
+++ src/main/java/racingcar/constants/output/ResultOutputMessages.java
@@ -2,6 +2,6 @@ package racingcar.constants.output;
 
 public class ResultOutputMessages {
 
-    public static final String RESULT_ROUND_TEXT = "\n실행 결과";
+    public static final String RESULT_ROUND_TEXT = System.lineSeparator() + "실행 결과";
     public static final String RESULT_GAME_WINNER_TEXT = "가 최종 우승했습니다.";
 }
