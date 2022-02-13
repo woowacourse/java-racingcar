3ee921ed7346ac7bdfa51a082f3019be3d0004b3
is2js <tingstyle1@gmail.com>
2022-02-14 01:25:37 +0900

    feat: Car 생성자에 검증 로직 추가함

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,53 +1,58 @@
 package racingcar.domain;
 
 import java.util.Objects;
+import racingcar.util.Utils;
 
 public class Car implements Comparable<Car> {
-    private static final int MINIMUM_DRIVE_CONDITION = 4;
-    private static final int DEFAULT_POSITION_VALUE = 0;
+	private static final int MINIMUM_DRIVE_CONDITION = 4;
+	private static final int DEFAULT_POSITION_VALUE = 0;
 
-    private final String name;
-    private int position = DEFAULT_POSITION_VALUE;
+	private final String name;
+	private int position = DEFAULT_POSITION_VALUE;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+		Utils.validateCarsNameLength(this.name);
+		Utils.validateCarsNameBlank(this.name);
+	}
 
-    public void drive(int value) {
-        if (MINIMUM_DRIVE_CONDITION <= value) {
-            this.position++;
-        }
-    }
+	public void drive(int value) {
+		if (MINIMUM_DRIVE_CONDITION <= value) {
+			this.position++;
+		}
+	}
 
-    public boolean isSamePosition(Car car) {
-        return this.position == car.position;
-    }
+	public boolean isSamePosition(Car car) {
+		return this.position == car.position;
+	}
 
-    public int getPosition() {
-        return position;
-    }
+	public int getPosition() {
+		return position;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public int compareTo(Car car) {
-        return this.position - car.position;
-    }
+	@Override
+	public int compareTo(Car car) {
+		return this.position - car.position;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof Car))
-            return false;
-        Car car = (Car)o;
-        return position == car.position && Objects.equals(name, car.name);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Car)) {
+			return false;
+		}
+		Car car = (Car) o;
+		return position == car.position && Objects.equals(name, car.name);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, position);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, position);
+	}
 }
