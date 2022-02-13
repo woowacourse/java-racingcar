ac08d9f1ea2cc876bedebc6e3662b23493aa15a8
Ohzzi <clover7even@naver.com>
2022-02-13 15:07:47 +0900

    test: Winners의 테스트를 기존에 우승자가 나왔는지만 체크하던 테스트에서 단독 우승과 공동 우승을 체크하는 테스트로 변경

--- src/test/java/racingcar/model/car/WinnersTest.java
+++ src/test/java/racingcar/model/car/WinnersTest.java
@@ -5,20 +5,49 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.car.Car;
 import racingcar.model.car.Winners;
 
 @SuppressWarnings("NonAsciiCharacters")
 class WinnersTest {
+    String name1 = "오찌";
+    String name2 = "연로그";
+
+    private Car car1;
+    private Car car2;
+
+    private List<Car> cars;
+
+    @BeforeEach
+    public void 자동차_생성() {
+        cars = new ArrayList<>();
+
+        car1 = new Car(name1);
+        car2 = new Car(name2);
+
+        cars.add(car1);
+        cars.add(car2);
+    }
+
     @Test
-    void 비어있지_않음() {
+    public void 우승자_1명() {
         // given
-        String name1 = "오찌";
-        String name2 = "연로그";
-        List<Car> cars = new ArrayList<>();
-        Car car1 = new Car(name1);
-        Car car2 = new Car(name2);
+        car1.goOrStop(4);
+
+        // when
+        Winners winners = new Winners(cars);
+
+        // then
+        assertThat(winners.getNames()).containsOnly(name1);
+    }
+
+    @Test
+    public void 공동우승() {
+        // given
+        car1.goOrStop(4);
+        car2.goOrStop(4);
 
         cars.add(car1);
         cars.add(car2);
