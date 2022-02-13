34ce164b78781997a6b68856debc827b74a7bc33
yoon <duddbs774@gmail.com>
2022-02-13 13:20:43 +0900

    feat: 자동차 이름 객체 생성

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+import racingcar.utils.Validator;
+
+public class CarName {
+    private final String name;
+
+    public CarName(String name) {
+        Validator.checkName(name);
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (other == null || getClass() != other.getClass())
+            return false;
+        CarName carName = (CarName)other;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
