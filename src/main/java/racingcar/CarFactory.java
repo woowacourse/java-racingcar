e6fc95423c3f664d4006325c78b30940efb83cac
Hongdonggeon <ghd700@naver.com>
2022-02-14 00:52:53 +0900

    refactor: 스트림 한 단계 중간 연산 추가
    CarFactory.java
       -of(): 한번에 여러가지 작업을 하는 것보다 하나의 행위 위주로 한단계씩 작성하는 것이 좋다.

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -5,20 +5,21 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CarFactory {
-	private static final String DUPLICATED_NAME_ERROR = "[ERROR] 차 이름은 중복될 수 없습니다.";
+    private static final String DUPLICATED_NAME_ERROR = "[ERROR] 차 이름은 중복될 수 없습니다.";
 
-	public static Set<Car> of(String names) {
-		String[] arrNames = names.split(",");
-		Set<Car> cars = Arrays.stream(arrNames)
-			.map(name -> new Car(name.trim()))
-			.collect(Collectors.toSet());
-		validateDuplicatedCarName(arrNames, cars);
-		return cars;
-	}
+    public static Set<Car> of(String names) {
+        String[] arrNames = names.split(",");
+        Set<Car> cars = Arrays.stream(arrNames)
+                .map(String::trim)
+                .map(Car::new)
+                .collect(Collectors.toSet());
+        validateDuplicatedCarName(arrNames, cars);
+        return cars;
+    }
 
-	private static void validateDuplicatedCarName(String[] arrNames, Set<Car> cars) {
-		if (arrNames.length != cars.size()) {
-			throw new IllegalArgumentException(DUPLICATED_NAME_ERROR);
-		}
-	}
+    private static void validateDuplicatedCarName(String[] arrNames, Set<Car> cars) {
+        if (arrNames.length != cars.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NAME_ERROR);
+        }
+    }
 }
