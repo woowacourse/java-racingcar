1b1e50fee02c8b19f9017c8c288873865ea99699
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-14 00:39:42 +0900

    test: 게임 작동 테스트 추가

--- src/test/java/racingCar/service/RacingCarsServiceTest.java
+++ src/test/java/racingCar/service/RacingCarsServiceTest.java
@@ -2,6 +2,9 @@ package racingCar.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,10 +15,25 @@ import racingCar.model.Car;
 import racingCar.model.RacingCars;
 
 class RacingCarsServiceTest {
+	@Test
+	public void 게임_작동_테스트() {
+		//given
+		RacingCarsService.initiateCars("juri,hunch");
+		RacingCarsService.initiateCount(5);
+		OutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		//when
+		RacingCarsService.runGame();
+		String result = out.toString();
+
+		//then
+		assertThat(result)
+			.contains("","실행 결과","juri : -","hunch : -");
+	}
 
 	@Test
-	public void 우승자_찾기_테스트() throws Exception {
-
+	public void 우승자_찾기_테스트() {
 		//given
 		int expectMaxPosition = 4;
 		RacingCars cars = new RacingCars(new ArrayList<>(Arrays.asList("juri", "hunch")));
