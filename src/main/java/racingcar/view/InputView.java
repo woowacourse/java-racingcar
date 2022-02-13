483868d6c4573dc4430b187d152cb18b10be409a
hyunrrr <whgusrms96@gmail.com>
2022-02-13 22:33:00 +0900

    refactor: 축약된 변수명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,17 +7,17 @@ public class InputView {
     private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
-    private static final Scanner sc = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String askCarName() {
         System.out.println(QUESTION_CAR_NAME);
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
 
     public static String askTryCount() {
         System.out.println(QUESTION_TRY_COUNT);
-        return sc.nextLine();
+        return scanner.nextLine();
     }
 
 }
