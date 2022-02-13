19ebfd191544a17135d04c4f2bca19b9a0b95735
meatsby <meatsby@gmail.com>
2022-02-13 13:47:39 +0900

    refactor: random 값을 생성하는 Engine 클래스를 새로 구현 및 startEngine() 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,29 +1,27 @@
 package racingcar.domain;
 
-import java.util.Random;
-
 public class Car {
-    private static final int RANGE = 10;
     private static final int THRESHOLD = 4;
     private static final String DELIMITER = " : ";
     private static final String DISTANCE = "-";
 
     private final String name;
+    private final Engine engine;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+        this.engine = new Engine();
     }
 
-    public void move(int randomNumber) {
-        if (randomNumber >= THRESHOLD) {
+    public void move(int power) {
+        if (power >= THRESHOLD) {
             position++;
         }
     }
 
-    public int generateRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(RANGE);
+    public int startEngine() {
+        return engine.ignite();
     }
 
     public String getCurrentPosition() {
