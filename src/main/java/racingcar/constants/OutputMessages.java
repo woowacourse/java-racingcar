eeca580c868e0b720c1dc483bca3fc35d28015f9
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-13 18:41:19 +0900

    refactor(OutputView): implement printBlankLine method, private constants, replace builder with repeat

--- src/main/java/racingcar/constants/OutputMessages.java
+++ src/main/java/racingcar/constants/OutputMessages.java
@@ -8,11 +8,4 @@ public class OutputMessages {
 
     public static final String REQUEST_CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String REQUEST_TOTAL_ROUNDS_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-
-    public static final String ROUND_RESULT_TEXT = "\n실행 결과";
-    public static final String EMPTY_LINE = "";
-    public static final String CAR_INFO_DELIMITER = " : ";
-    public static final String DISTANCE_SYMBOL = "-";
-    public static final String WINNERS_JOINER_DELIMITER = ", ";
-    public static final String ANNOUNCE_GAME_WINNER_TEXT = "가 최종 우승했습니다.";
 }
