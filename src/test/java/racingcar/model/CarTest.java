124538e3babd7451322e3ff25851205e68bcec48
jochungbeom <cndqjacndqja@gmail.com>
2022-02-14 20:43:47 +0900

    refactor(CarTest): DisplayName() 추가 및 ParameterizedTest()를 통해 입력 케이스 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,49 +1,58 @@
 package racingcar.model;
 
-import static org.assertj.core.api.Assertions.*;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
 	@Test
-	void 자동차_초기위치() {
+	@DisplayName("자동차 생성 시, position 값을 0이 된다.")
+	void initCarPosition() {
 		Car car = new Car("소주캉");
 		assertThat(car.matchPosition(0)).isTrue();
 	}
 
-	@Test
-	void 이름_5글자_초과() {
+	@ParameterizedTest
+	@ValueSource(strings = {"소주캉범고래", "여섯글자이름"})
+	@DisplayName("자동차 이름이 5글자를 초과하면 에러를 발생시킨다. ")
+	void isOverNameLength(String input) {
 		assertThatThrownBy(() -> {
-			Car car = new Car("소주캉범고래");
-		}).isInstanceOf(RuntimeException.class);
+			Car car = new Car(input);
+		}).isInstanceOf(RuntimeException.class)
+				.hasMessageContaining("5글자를 초과했습니다.");
 	}
 
 	@Test
-	void 이름_빈문자열() {
+	@DisplayName("자동차 이름에 빈 문자열을 입력 시, 에러를 발생시킨다. ")
+	void validName() {
 		assertThatThrownBy(() -> {
-			Car car = new Car("");
-		}).isInstanceOf(RuntimeException.class);
+			String input = "";
+			Car car = new Car(input);
+		}).isInstanceOf(RuntimeException.class)
+				.hasMessageContaining("빈 문자열");
 	}
 
-	@Test
-	void 이름_null() {
-		assertThatThrownBy(() -> {
-			Car car = new Car(null);
-		}).isInstanceOf(RuntimeException.class);
-	}
-
-	@Test
-	void 자동차_이동() {
+	@ParameterizedTest
+	@CsvSource(value = {"5:1", "4:1", "3:0", "2:0"}, delimiter = ':')
+	@DisplayName("Car move()에 4 이상의 값을 입력 시, 자동차 position이 1 증가한다.")
+	void move(int number, int position) {
 		Car car = new Car("범고래");
-		car.move(4);
-		assertThat(car.matchPosition(1)).isTrue();
+		car.move(number);
+		assertThat(car.matchPosition(position)).isTrue();
 	}
 
-	@Test
-	void 값이_더_큰_포지션_반환() {
+	@ParameterizedTest
+	@CsvSource(value = {"5:1", "4:1", "3:0", "2:0"}, delimiter = ':')
+	@DisplayName("입력 받은 자동차의 position 값과 비교하여 더 큰 값을 반환한다.")
+	void getBiggerPosition(int number, int position) {
 		Car car = new Car("범고래");
-		car.move(4);
-		assertThat(car.getBiggerPosition(0)).isEqualTo(1);
+		car.move(number);
+		assertThat(car.getBiggerPosition(0)).isEqualTo(position);
 	}
 }
