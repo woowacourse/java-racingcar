6c87c0c687381979ec4ef3d9df4b36c53b72bd21
Hongdonggeon <ghd700@naver.com>
2022-02-14 10:57:36 +0900

    Test: Cars의 getCarByName()과 getMaxPosition() 테스트 코드 추가
    CarsTest.Car_집합에서_찾는_이름이_존재_X(): 찾는 이름이 존재하지 않은 경우

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -9,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsTest {
     private static final String DUPLICATED_NAME_ERROR = "[ERROR] 차 이름은 중복될 수 없습니다.";
+    private static final String NOT_FOUND_CAR_NAME_ERROR = "[ERROR] 해당하는 차 이름은 존재하지 않습니다.";
+
 
     @Test
     public void Car_2개일때_Cars_객체_생성_테스트() {
@@ -43,5 +45,23 @@ public class CarsTest {
         Cars cars = new Cars(CarFactory.of("kun,forky"));
         Car car = cars.getCarByName("forky");
         assertThat(car.getName()).isEqualTo("forky");
+    }
+
+    @Test
+    public void Car_집합에서_찾는_이름이_존재_X() {
+        Cars cars = new Cars(CarFactory.of("kun,forky"));
+        assertThatThrownBy(() -> cars.getCarByName("pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_FOUND_CAR_NAME_ERROR);
+    }
+
+    @Test
+    public void Car_집합에서_최대_position_값_찾기() {
+        Cars cars = new Cars(CarFactory.of("kun,forky"));
+        Car car = cars.getCarByName("kun");
+        for (int i = 0; i < 5; i++) {
+            car.move(true);
+        }
+        assertThat(cars.getMaxPosition()).isEqualTo(5);
     }
 }
