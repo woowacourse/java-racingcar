312c848748aafe28f04203c0987afd7cf3f8aef1
TaeHyeon Kim <kth990303@naver.com>
2022-02-14 11:47:19 +0900

    test(racing-car):테스트 코드 indent 리팩토링 및 컨벤션 통일

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -1,52 +1,49 @@
 package racingcar.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class RacingServiceTest {
-	private List<Car> cars;
-	private Car carA;
-	private Car carB;
-	private Car carC;
-	private Car carD;
-	private RacingService racingService;
+    private List<Car> cars;
+    private Car carA;
+    private Car carB;
+    private Car carC;
+    private Car carD;
+    private RacingService racingService;
 
-	@BeforeEach
-	void beforeEach(){
-		carA = new Car("A");
-		carB = new Car("B");
-		carC = new Car("C");
-		carD = new Car("D");
+    @BeforeEach
+    void beforeEach() {
+        carA = new Car("A");
+        carB = new Car("B");
+        carC = new Car("C");
+        carD = new Car("D");
 
-		cars = new ArrayList<>();
-		cars.add(carA);
-		cars.add(carB);
-		cars.add(carC);
-		cars.add(carD);
+        cars = new ArrayList<>();
+        cars.add(carA);
+        cars.add(carB);
+        cars.add(carC);
+        cars.add(carD);
 
-		racingService = new RacingService(cars);
-	}
+        racingService = new RacingService(cars);
+    }
 
-	@Test
-	void 우승자_판단() {
-		carA.proceed();
-		assertThat(racingService.findWinners()).contains(carA);
-	}
+    @Test
+    void 우승자_판단() {
+        carA.proceed();
+        assertThat(racingService.findWinners()).contains(carA);
+    }
 
-	@Test
-	void 중복_우승자_판단() {
-		carA.proceed();
-		carB.proceed();
-		assertThat(racingService.findWinners()).contains(carA, carB);
-	}
+    @Test
+    void 중복_우승자_판단() {
+        carA.proceed();
+        carB.proceed();
+        assertThat(racingService.findWinners()).contains(carA, carB);
+    }
 }
