35dc23f596faddd8e1789e0d00aeebdeaaf04322
ellie <ririhan217@gmail.com>
2022-02-14 22:39:54 +0900

    refactor(racingcar): 생성자 체인 기법을 사용하도록 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,31 +2,32 @@ package racingcar.model;
 
 public class Car {
 
-	private static final int MOVE_FORWARD_CONDITION = 4;
+    private static final int MOVE_FORWARD_CONDITION = 4;
 
-	private final String name;
-	private int position = 0;
+    private final String name;
+    private int position = 0;
 
-	public Car(final String name) {
-		this.name = name;
-	}
+    public Car(final String name) {
+        this.name = name;
+        position = 0;
+    }
 
-	public Car(final String name, final int position) {
-		this.name = name;
-		this.position = position;
-	}
+    public Car(final String name, final int position) {
+        this.name = name;
+        this.position = position;
+    }
 
-	public String getCarName() {
-		return name;
-	}
+    public String getCarName() {
+        return name;
+    }
 
-	public int getPosition() {
-		return position;
-	}
+    public int getPosition() {
+        return position;
+    }
 
-	public void moveForward(final int number) {
-		if (number >= MOVE_FORWARD_CONDITION) {
-			position++;
-		}
-	}
+    public void moveForward(final int number) {
+        if (number >= MOVE_FORWARD_CONDITION) {
+            position++;
+        }
+    }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -8,22 +8,22 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NonAsciiCharacters")
 public class CarTest {
 
-	private static Car car;
+    private static Car car;
 
-	@BeforeEach
-	void init() {
-		car = new Car("carA");
-	}
+    @BeforeEach
+    void init() {
+        car = new Car("carA");
+    }
 
-	@Test
-	void 자동차_전진() {
-		car.moveForward(5);
-		assertThat(car.getPosition()).isEqualTo(1);
-	}
+    @Test
+    void 자동차_전진() {
+        car.moveForward(5);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 
-	@Test
-	void 자동차_멈춤() {
-		car.moveForward(0);
-		assertThat(car.getPosition()).isEqualTo(0);
-	}
+    @Test
+    void 자동차_멈춤() {
+        car.moveForward(0);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
 }
