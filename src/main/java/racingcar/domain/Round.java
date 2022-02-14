e23cd08db8fa78a0225469b4bc813fe4b0a65fcb
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-15 04:53:22 +0900

    feat: 초기 round 생서 메소드 추가

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 public class Round {
 
+    private static final int FIRST_ROUND = 1;
+
     private final int round;
 
     public Round(final int round) {
@@ -15,6 +17,10 @@ public class Round {
         if (round <= 0) {
             throw new IllegalArgumentException("라운드는 0이하의 값이 들어올 수 없다.");
         }
+    }
+
+    public static Round init() {
+        return new Round(FIRST_ROUND);
     }
 
     @Override
