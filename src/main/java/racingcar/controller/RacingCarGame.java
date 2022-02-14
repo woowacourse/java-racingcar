6d3e9da6e9e7584bce7272e0809b4157ed58c761
shindong96 <2006peter@naver.com>
2022-02-14 22:06:43 +0900

    refactor: 불필요한 메서드나 상수 삭제

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -13,10 +13,6 @@ import racingcar.view.OutputView;
 
 public class RacingCarGame {
 	public static final String COMMA_DELIMITER = ",";
-	public static final String NULL_EMPTY_CAR_NAME_ERROR_MESSAGE = "[Error] 다시 이름을 입력하세요";
-	public static final int MAX_CAR_NAME_LENGTH = 5;
-	public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = String.format("[Error] %d자 이하의 이름을 입력하세요.",
-		MAX_CAR_NAME_LENGTH);
 	public static final String GAME_TURN_NUMBER_ERROR_MESSAGE = "[Error] 숫자를 입력하세요.";
 	public static final String RANDOM_NUMBER_REGEX = "[1-9]\\d*";
 
@@ -52,19 +48,6 @@ public class RacingCarGame {
 	public List<String> splitCarNames(String carNames) {
 		return Arrays.asList(carNames.split(COMMA_DELIMITER));
 	}
-
-	/*public void checkCarNamesBlank(String input) {
-		if (input == null || input.isEmpty()) {
-			throw new IllegalArgumentException(NULL_EMPTY_CAR_NAME_ERROR_MESSAGE);
-		}
-	}
-
-	public void checkCarNamesLength(List<String> carNames) {
-		if (carNames.stream()
-			.anyMatch(carName -> carName.length() > MAX_CAR_NAME_LENGTH)) {
-			throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
-		}
-	}*/
 
 	public void saveGameTurn() {
 		gameTurn = new GameTurn(getGameTurn());
