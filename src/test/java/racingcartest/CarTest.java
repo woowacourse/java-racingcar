146e64a8e9b848e0c394d16d9ab3a01e890f964d
bcc0830 <bcc0830@naver.com>
2022-02-14 21:28:19 +0900

    refactor(MainTest) : 이름, 횟수, 자동차, 컨트롤러 별 테스트 파일로 나눔

---
+++ src/test/java/racingcartest/CarTest.java
@@ -0,0 +1,24 @@
+package racingcartest;
+
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+
+    @Test
+    public void carMoveTest() {
+        Car car = new Car("pobi");
+        car.movePosition(5);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void carPositionEqualTest() {
+        Car car = new Car("pobi");
+        car.movePosition(5);
+        car.movePosition(6);
+        assertThat(car.isMaxPosition(2)).isEqualTo(true);
+    }
+}
