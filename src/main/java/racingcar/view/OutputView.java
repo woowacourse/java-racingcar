2f0154e49aa99226a9d4af18704ed20b95795171
tonic523 <diddmlvkf@gmail.com>
2022-02-14 11:14:33 +0900

    refactor: 입력을 받기 위한 출력 기능을 OutputView -> InputView 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,31 +4,23 @@ import racingcar.model.car.Cars;
 import racingcar.model.car.Names;
 
 public class OutputView {
-    public static void printInputNamesUi() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-    }
+	public static void printRaceResultUi() {
+		System.out.format("%n실행 결과%n");
+	}
 
-    public static void printInputTryCountUi() {
-        System.out.println("시도할 회수는 몇회인가요?");
-    }
+	public static void printCarsUi(Cars cars) {
+		System.out.printf(cars.result() + "%n");
+	}
 
-    public static void printRaceResultUi() {
-        System.out.format("%n실행 결과%n");
-    }
+	public static void printEnterUi() {
+		System.out.println();
+	}
 
-    public static void printCarsUi(Cars cars) {
-        System.out.printf(cars.result() + "%n");
-    }
+	public static void printWinnersUi(Names names) {
+		System.out.printf("%s가 최종 우승했습니다.", names);
+	}
 
-    public static void printEnterUi() {
-        System.out.println();
-    }
-
-    public static void printWinnersUi(Names names) {
-        System.out.printf("%s가 최종 우승했습니다.", names);
-    }
-
-    public static void printErrorUi(Exception exception) {
-        System.out.println(exception.getMessage());
-    }
+	public static void printErrorUi(Exception exception) {
+		System.out.println(exception.getMessage());
+	}
 }
