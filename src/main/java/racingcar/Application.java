a5111b926af73de52bd9ad10ba275067d67dae23
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-14 17:35:01 +0900

    refactor(Application): 메소드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,8 @@ package racingcar;
 import racingcar.controller.RacingCarsController;
 
 public class Application {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		RacingCarsController controller = new RacingCarsController();
-		controller.requestCarsName();
-		controller.requestCount();
-		controller.startGame();
-		controller.endGame();
+		controller.start();
 	}
 }
