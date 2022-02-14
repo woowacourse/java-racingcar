a26dda5bb74a5250ec144aeb96773902eb9703b7
yukong <kyb062666@gmail.com>
2022-02-14 17:13:06 +0900

    refactor(Calculator): 메소드 네이밍 수정, int 배열로 변환하는 로직 수정
    - splitAndSum() : validate, split, sum 의 단계로 나누어지도록 수정
    - stringToIntArray() : stream 을 사용하여 코드 리팩토링
    - sum() : 메소드의 역할을 명확하게 표시하도록 메소드 이름 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,75 +1,72 @@
 package calculator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-	public int splitAndSum(String text) {
-		if (!isValidValues(text)) {
-			return 0;
-		}
+	private static final int WRONG_VALUE_RESULT = 0;
+	private static final int CUSTOM_DELIMITER_PART = 1;
+	private static final int NUMBER_PART = 2;
 
-		int[] numbers = stringToIntArray(getValues(text));
-		checkNumbers(numbers);
-		return add(numbers);
+	public int splitAndSum(String text) {
+		if (isEmpty(text)) {
+			return WRONG_VALUE_RESULT;
+		}
+		return sum(getSplitNumbers(text));
 	}
 
-	private boolean isValidValues(String text) {
-		return text != null && !text.trim().isEmpty();
+	private boolean isEmpty(String text) {
+		return text == null || text.trim().isEmpty();
+	}
+
+	private int[] getSplitNumbers(String text) {
+		String delimiter = ",|:";
+
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			delimiter += "|" + m.group(CUSTOM_DELIMITER_PART);
+			text = m.group(NUMBER_PART);
+		}
+		return stringToIntArray(text.split(delimiter));
 	}
 
 	private int[] stringToIntArray(String[] strings) {
-		ArrayList<Integer> numbers = new ArrayList<>();
-		for (String i : strings) {
-			numbers.add(convertValue(i));
-		}
-		return numbers.stream().mapToInt(Integer::intValue).toArray();
+		return Arrays.stream(strings)
+				.filter(this::isValidNumber)
+				.map(Integer::parseInt)
+				.mapToInt(Integer::intValue).toArray();
 	}
 
-	private int convertValue(String string) {
-		if (!isValidValues(string)) {
-			return 0;
+	private boolean isValidNumber(String string) {
+		if (isEmpty(string)) {
+			return false;
 		}
 		if (!isNumber(string)) {
-			throw new IllegalArgumentException("숫자를 입력해주세요.");
+			throw new IllegalArgumentException("숫자를 입력해주세요!");
 		}
-		return Integer.parseInt(string);
+		return true;
 	}
 
 	private boolean isNumber(String string) {
-		return string.matches("[+-]?\\d*(\\.\\d+)?");
+		return string.matches("[+-]?\\d*?(\\.\\d+)?");
 	}
 
-	private String[] getValues(String text) {
-		String delimiter = ",|:";
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-		if (m.find()) {
-			return m.group(2).split(String.format("%s|%s", delimiter, m.group(1)));
-		}
-		return text.split(delimiter);
-	}
-
-	private void checkNumbers(int[] numbers) {
-		for (int i : numbers) {
-			checkNegativeNumber(i);
-		}
-	}
-
-	private void checkNegativeNumber(int number) {
-		if (number < 0) {
-			throw new RuntimeException("양수를 입력해주세요.");
-		}
-	}
-
-	private int add(int[] numbers) {
+	private int sum(int[] numbers) {
 		if (numbers.length == 1) {
 			return numbers[0];
 		}
 		int result = 0;
 		for (int i : numbers) {
+			checkNegative(i);
 			result += i;
 		}
 		return result;
+	}
+
+	private void checkNegative(int number) {
+		if (number < 0) {
+			throw new RuntimeException("자연수를 입력해주세요.");
+		}
 	}
 }
