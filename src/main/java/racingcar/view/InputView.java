77dfab1afa9cdc77a3b4f1dd870fd332d60d93d4
YuJeong <yjfflq4872@gmail.com>
2022-02-14 17:29:53 +0900

    refactor: InputView 내부 메소드 접근제한자 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
 	private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
 
-	public static String inputFromUser() {
+	private static String inputFromUser() {
 		return scanner.nextLine();
 	}
 
