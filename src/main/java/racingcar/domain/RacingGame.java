97c3093f1f62768e05537cb913ec08d864fa6945
is2js <tingstyle1@gmail.com>
2022-02-14 15:47:03 +0900

    refactor: 불필요한 생성자 제거함

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -3,39 +3,35 @@ package racingcar.domain;
 import java.util.List;
 
 public class RacingGame {
-    private final Cars cars;
-    private final NumberGeneratePolicy numberGeneratePolicy;
-    private Round round;
+	private final Cars cars;
+	private final NumberGeneratePolicy numberGeneratePolicy;
+	private Round round;
 
-    public RacingGame(Cars cars, Round round, NumberGeneratePolicy numberGeneratePolicy) {
-        this.cars = cars;
-        this.round = round;
-        this.numberGeneratePolicy = numberGeneratePolicy;
-    }
+	public RacingGame(Cars cars, Round round, NumberGeneratePolicy numberGeneratePolicy) {
+		this.cars = cars;
+		this.round = round;
+		this.numberGeneratePolicy = numberGeneratePolicy;
+	}
 
-    public RacingGame(List<String> carsName, int round, NumberGeneratePolicy numberGeneratePolicy) {
-        this(Cars.from(carsName), Round.from(round), numberGeneratePolicy);
-    }
+	public static RacingGame createRacingGame(List<String> carsName, String round,
+											  NumberGeneratePolicy numberGeneratePolicy) {
+		return new RacingGame(Cars.from(carsName), Round.from(round), numberGeneratePolicy);
+	}
 
-    public static RacingGame createRacingGame(List<String> carsName, int round,
-        NumberGeneratePolicy numberGeneratePolicy) {
-        return new RacingGame(carsName, round, numberGeneratePolicy);
-    }
+	public RacingRecord race() {
+		cars.driveAll(numberGeneratePolicy);
+		return new RacingRecord(cars.getDriveRecord());
+	}
 
-    public RacingRecord race() {
-        cars.driveAll(numberGeneratePolicy);
-        return new RacingRecord(cars.getDriveRecord());
-    }
+	public boolean hasNextGame() {
+		if (this.round.isValidRound()) {
+			this.round = round.toNextRound();
+			return true;
+		}
+		return false;
+	}
 
-    public boolean hasNextGame() {
-        if (this.round.isValidRound()) {
-            this.round = round.toNextRound();
-            return true;
-        }
-        return false;
-    }
-
-    public RacingResult getRacingResult() {
-        return new RacingResult(cars.getWinners());
-    }
+	public RacingResult getRacingResult() {
+		return new RacingResult(cars.getWinners());
+	}
 }
