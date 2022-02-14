f7e815ef1ab5f7bd410233c9ae4345f33fe864d4
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-15 01:28:45 +0900

    refactor: mvc 패턴으로 리팩터링
    controller를 Application 클래스로 변경, RacingCarGame을 도메인에 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,28 @@
 package racingcar;
 
-import racingcar.controller.RacingCarGame;
+import racingcar.model.RacingCarGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
-	public static void main(String[] args) {
-		RacingCarGame racingCarGame = new RacingCarGame();
-		racingCarGame.makeCars();
-		racingCarGame.saveGameTurn();
-		racingCarGame.moveCars();
-		racingCarGame.printWinners();
-	}
+    public static void main(String[] args) {
+        RacingCarGame racingCarGame = getRacingCarGame();
+        OutputView.printResultSentence();
+        while (racingCarGame.canRace()) {
+            racingCarGame.race();
+            OutputView.printResult(racingCarGame.getCarsPosition());
+        }
+        OutputView.printWinners(racingCarGame.getWinners());
+    }
+
+    private static RacingCarGame getRacingCarGame() {
+        try {
+            String carNames = InputView.getCarNames();
+            int gameTurn = InputView.getGameTurn();
+            return new RacingCarGame(carNames, gameTurn);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getRacingCarGame();
+        }
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,23 +1,16 @@
 package racingcar.view;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final String REQUIRE_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static final String COMMA_DELIMITER = ",";
     private static final String REQUIRE_GAME_TURN_MESSAGE = "시도할 회수는 몇회인가요?";
     public static final String NOT_NUMBER_ERROR = "[ERROR] 숫자를 입력하세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static List<String> getCarNames() {
+    public static String getCarNames() {
         System.out.println(REQUIRE_CAR_NAMES_MESSAGE);
-        return splitCarNames(scanner.nextLine());
-    }
-
-    private static List<String> splitCarNames(String carNames) {
-        return Arrays.asList(carNames.split(COMMA_DELIMITER));
+        return scanner.nextLine();
     }
 
     public static int getGameTurn() {
