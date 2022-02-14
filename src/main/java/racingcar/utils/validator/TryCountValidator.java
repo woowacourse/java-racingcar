9fbd8dd1fcb227545f44d016523d8e89d8a5a369
ellie <ririhan217@gmail.com>
2022-02-14 22:23:14 +0900

    refactor(racingcar): 시도 횟수 검증에서 패턴을 사용하지 않는 방식으로 변경

--- src/main/java/racingcar/utils/validator/TryCountValidator.java
+++ src/main/java/racingcar/utils/validator/TryCountValidator.java
@@ -2,15 +2,39 @@ package racingcar.utils.validator;
 
 public class TryCountValidator {
 
-	private static final String PATTERN = "^([1-9])([0-9])*$";
-	private static final String PATTERN_ERROR_MESSAGE = "[ERROR] 시도 회수는 양수만 가능합니다.";
+    private static final int ZERO = 0;
+    private static final String TRY_COUNT_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양수만 가능합니다.";
 
-	private TryCountValidator() {
-	}
+    private TryCountValidator() {
+    }
 
-	public static void validatePattern(final String inputValue) {
-		if (!inputValue.matches(PATTERN)) {
-			throw new IllegalArgumentException(PATTERN_ERROR_MESSAGE);
-		}
-	}
+    public static void validateTryCount(final String inputValue) {
+        validateNullAndBlank(inputValue);
+        validateNotNumber(inputValue);
+        validateZeroAndNegative(inputValue);
+    }
+
+    private static void validateNullAndBlank(final String inputValue) {
+        if ((inputValue == null) || (inputValue.isBlank())) {
+            throw new IllegalArgumentException(TRY_COUNT_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateNotNumber(final String inputValue) {
+        for (int i = 0; i < inputValue.length(); i++) {
+            isNumber(inputValue.charAt(i));
+        }
+    }
+
+    private static void isNumber(char target) {
+        if (!Character.isDigit(target)) {
+            throw new IllegalArgumentException(TRY_COUNT_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateZeroAndNegative(final String inputValue) {
+        if (Integer.parseInt(inputValue) <= ZERO) {
+            throw new IllegalArgumentException(TRY_COUNT_ERROR_MESSAGE);
+        }
+    }
 }
--- src/test/java/racingcar/validator/TryCountValidatorTest.java
+++ src/test/java/racingcar/validator/TryCountValidatorTest.java
@@ -9,30 +9,30 @@ import racingcar.utils.validator.TryCountValidator;
 @SuppressWarnings("NonAsciiCharacters")
 public class TryCountValidatorTest {
 
-	@Test
-	void 시도_회수_문자() {
-		String tryCount = "q";
-		assertThatThrownBy(() -> {
-			TryCountValidator.validatePattern(tryCount);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 시도 회수는 양수만 가능합니다.");
-	}
+    @Test
+    void 시도_회수_문자() {
+        String tryCount = "q";
+        assertThatThrownBy(() -> {
+            TryCountValidator.validateTryCount(tryCount);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 시도 횟수는 양수만 가능합니다.");
+    }
 
-	@Test
-	void 시도_횟수_음수() {
-		String tryCount = "-1";
-		assertThatThrownBy(() -> {
-			TryCountValidator.validatePattern(tryCount);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 시도 회수는 양수만 가능합니다.");
-	}
+    @Test
+    void 시도_횟수_음수() {
+        String tryCount = "-1";
+        assertThatThrownBy(() -> {
+            TryCountValidator.validateTryCount(tryCount);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 시도 횟수는 양수만 가능합니다.");
+    }
 
-	@Test
-	void 시도_횟수_공백() {
-		String tryCount = "";
-		assertThatThrownBy(() -> {
-			TryCountValidator.validatePattern(tryCount);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 시도 회수는 양수만 가능합니다.");
-	}
+    @Test
+    void 시도_횟수_공백() {
+        String tryCount = "";
+        assertThatThrownBy(() -> {
+            TryCountValidator.validateTryCount(tryCount);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 시도 횟수는 양수만 가능합니다.");
+    }
 }
