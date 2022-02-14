425cd3d7df0a48db888c35ec64ff2139fda05e64
koo <gudonghee2000@gmail.com>
2022-02-14 15:41:10 +0900

    refactor: 입력된 자동차 이름을 검사하는 함수들을 InputController와 Car클래스로 이동

--- src/main/java/racingcargame/controller/InputController.java
+++ src/main/java/racingcargame/controller/InputController.java
@@ -3,11 +3,11 @@ package racingcargame.controller;
 import java.util.List;
 import java.util.Scanner;
 
-import racingcargame.utils.NameInputValidator;
 import racingcargame.utils.RaceCountInputValidator;
 
 public class InputController {
 	private static final String SEPARATOR = ",";
+	private static final String NOTHING_INPUT_IN_CAR_NAMES_ERROR_MESSAGE = "[error] 입력된 자동차 이름이 없습니다.";
 
 	private static final InputController inputController = new InputController();
 	private static final Scanner scanner = new Scanner(System.in);
@@ -20,8 +20,15 @@ public class InputController {
 	}
 
 	public List<String> inputRacingCarNames() {
-		String racingCarNames = scanner.nextLine();
-		return List.of(NameInputValidator.validateCarNames(racingCarNames).split(SEPARATOR));
+		String racingCarNames = checkNothingInputInCarNames(scanner.nextLine());
+		return List.of((racingCarNames).split(SEPARATOR));
+	}
+
+	private static String checkNothingInputInCarNames(final String carNames) {
+		if (carNames == null) {
+			throw new IllegalArgumentException(NOTHING_INPUT_IN_CAR_NAMES_ERROR_MESSAGE);
+		}
+		return carNames;
 	}
 
 	public int inputRaceCount() {
--- src/main/java/racingcargame/model/Car.java
+++ src/main/java/racingcargame/model/Car.java
@@ -4,14 +4,41 @@ import racingcargame.utils.RandomNumberGenerator;
 
 public class Car {
 	private static final int MIN_MOVE_NUMBER = 4;
+	private static final int MAX_NAME_LENGTH_COUNT = 5;
+	private static final int MIN_NAME_LENGTH_COUNT = 1;
+	private static final String BLANK = " ";
+	private static final String HAS_BLANK_IN_NAMES_ERROR_MESSAGE = "[error] 입력한 자동차 이름에 공백이 있습니다.";
+	private static final String NO_VALIDATE_NAME_LENGTH_ERROR_MESSAGE = "[error] 자동차 이름을 5자 이하로 입력해주세요.";
 
 	private final String name;
 	private int position;
 	private int moveNumber;
 
 	public Car(final String name, final int position) {
+		checkValidateCarName(name);
 		this.name = name;
 		this.position = position;
+	}
+
+	public static String checkValidateCarName(final String name) {
+		if (!hasBlankInNames(name) && isValidNameLength(name)) {
+			return name;
+		}
+		throw new IllegalArgumentException();
+	}
+
+	private static boolean hasBlankInNames(final String name) {
+		if (name.contains(BLANK)) {
+			throw new IllegalArgumentException(HAS_BLANK_IN_NAMES_ERROR_MESSAGE);
+		}
+		return false;
+	}
+
+	private static boolean isValidNameLength(final String name) {
+		if (name.length() < MIN_NAME_LENGTH_COUNT || name.length() > MAX_NAME_LENGTH_COUNT) {
+			throw new IllegalArgumentException(NO_VALIDATE_NAME_LENGTH_ERROR_MESSAGE);
+		}
+		return true;
 	}
 
 	String getName() {
