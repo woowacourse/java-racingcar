744873c9ee92e476eeac8d7da2e35b7a5c0cfc8e
YuJeong <yjfflq4872@gmail.com>
2022-02-15 01:25:56 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,17 +8,13 @@ public class InputView {
 	private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String INPUT_TRIAL_MESSAGE = "시도할 회수는 몇회인가요?";
 
-	private static String inputFromUser() {
-		return scanner.nextLine();
-	}
-
 	public static String inputCarNames() {
 		System.out.println(INPUT_CAR_NAMES_MESSAGE);
-		return inputFromUser();
+		return scanner.nextLine();
 	}
 
 	public static String inputTrials() {
 		System.out.println(INPUT_TRIAL_MESSAGE);
-		return inputFromUser();
+		return scanner.nextLine();
 	}
 }
