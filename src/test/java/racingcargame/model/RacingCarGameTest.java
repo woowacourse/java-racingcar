0a06f6464cab4ec92d8b9b90dc93f35f061b9950
koo <gudonghee2000@gmail.com>
2022-02-14 15:19:18 +0900

    test: RacingCarGame클래스의 객체가 정상적으로 생성되는지 테스트하는 함수 생성

--- src/test/java/racingcargame/model/RacingCarGameTest.java
+++ src/test/java/racingcargame/model/RacingCarGameTest.java
@@ -1,13 +1,28 @@
 package racingcargame.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RacingCarGameTest {
 
+	@DisplayName("RacingCarGame객체가 정상적으로 생성되는지 테스트")
+	@Test
+	void createObject_RacingCarGame() {
+		List<String> carNames = List.of("토미", "제인", "데이빗");
+		int raceCount = 5;
+		RacingCarGame racingCarGame = new RacingCarGame(carNames, raceCount);
+
+		HashMap<String, Integer> carsPosition = racingCarGame.sendCurrentPositionOfRacingCars();
+		carsPosition.forEach((carName, carPosition) -> {
+			assertThat(carNames).contains(carName);
+			assertThat(carPosition).isEqualTo(0);
+		});
+
+		assertThat(racingCarGame.isOverRace()).isEqualTo(false);
+	}
 }
