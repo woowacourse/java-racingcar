b654cd001d04cbbff0fa993c85da5f4ea36d665e
Hyunta <mohai2618@gmail.com>
2022-02-14 09:58:26 +0900

    refactor : 자동차 이름을 분리하는 구분자 이름 수정

--- src/main/java/racingcar/utils/StringSeparator.java
+++ src/main/java/racingcar/utils/StringSeparator.java
@@ -3,10 +3,10 @@ package racingcar.utils;
 import java.util.Arrays;
 
 public class StringSeparator {
-    private static final String STRING_DELIMITER = ",";
+    private static final String CAR_NAMES_DELIMITER = ",";
 
     public static String[] splitAndTrim(String names) {
-        String[] splitNames = names.split(STRING_DELIMITER);
+        String[] splitNames = names.split(CAR_NAMES_DELIMITER);
         Arrays.setAll(splitNames, i -> splitNames[i].trim());
         return splitNames;
     }
