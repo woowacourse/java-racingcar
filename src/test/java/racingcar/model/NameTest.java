bc3c5d9e8ee0c12c95670e83b2d0056e93ff795c
yeon-06 <rnjstldus2@hs.ac.kr>
2022-02-14 18:46:51 +0900

    test: name 생성 테스트 케이스 추가

---
+++ src/test/java/racingcar/model/NameTest.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NameTest {
+    @Test
+    @DisplayName("Name 정상 생성")
+    public void success() {
+        // when
+        Name name = new Name("test");
+
+        // then
+        assertThat(name).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "1231456"})
+    @DisplayName("Name 생성 실패")
+    public void failed(String string) {
+        // then
+        assertThatThrownBy(() -> new Name(string)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
