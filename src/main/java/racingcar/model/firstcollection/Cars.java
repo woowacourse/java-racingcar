3fcaae6c977456ea6fe73284da1c5ed5aac8ae64
hj-rich <ztzy1907@gmail.com>
2022-02-14 11:20:06 +0900

    refactor(cars): Cars 생성 시 Null, Empty 에 대한 예외처리

--- src/main/java/racingcar/model/firstcollection/Cars.java
+++ src/main/java/racingcar/model/firstcollection/Cars.java
@@ -1,7 +1,9 @@
 package racingcar.model.firstcollection;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
@@ -12,14 +14,22 @@ public class Cars {
     private static final String MESSAGE_FOR_CAR_NAME_DUPLICATE = "이름은 중복될 수 없습니다.";
     private static final int RANGE_OF_ZERO_TO_NINE = 10;
     private static final String ERROR_MESSAGE_TO_GET_MAX_POSITION = "최대값을 구할 수 없습니다.";
+    private static final String ERROR_MESSAGE_FOR_EMPTY_CAR_LIST = "자동차 목록을 확인해주세요";
 
     private final List<Car> cars;
 
     public Cars(final List<Car> cars) {
+        validateNullOrEmpty(cars);
         List<Car> unmodifiableCars = List.copyOf(cars);
         validateDuplicate(unmodifiableCars);
 
         this.cars = unmodifiableCars;
+    }
+
+    private void validateNullOrEmpty(List<Car> cars) {
+        if (Objects.isNull(cars) || cars.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_EMPTY_CAR_LIST);
+        }
     }
 
     private static void validateDuplicate(final List<Car> cars) {
@@ -69,6 +79,6 @@ public class Cars {
     }
 
     public List<Car> getCars() {
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
 }
--- src/test/java/racingcar/model/firstcollection/CarsTest.java
+++ src/test/java/racingcar/model/firstcollection/CarsTest.java
@@ -1,5 +1,23 @@
 package racingcar.model.firstcollection;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import racingcar.model.vo.Car;
+
 public class CarsTest {
+	@ParameterizedTest(name = "new Cars({0})")
+	@NullAndEmptySource
+	@DisplayName("new Cars()에 Null, Empty 전달 시 IAE발생")
+	void validateNullOrEmpty(List<Car> cars) {
+		assertThatThrownBy(() -> new Cars(cars))
+					.isInstanceOf(IllegalArgumentException.class)
+					.hasMessageMatching("자동차 목록을 확인해주세요");
+	}
 
 }
