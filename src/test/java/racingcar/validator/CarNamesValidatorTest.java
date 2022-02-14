70ca298efd836799f7f10e6163b448b273b3eb80
ellie <ririhan217@gmail.com>
2022-02-14 22:16:10 +0900

    refactor(racingcar): 자동차 이름 입력값이 blank, null 일때도 처리하도록 수정

--- src/test/java/racingcar/validator/CarNamesValidatorTest.java
+++ src/test/java/racingcar/validator/CarNamesValidatorTest.java
@@ -9,39 +9,48 @@ import racingcar.utils.validator.CarNamesValidator;
 @SuppressWarnings("NonAsciiCharacters")
 public class CarNamesValidatorTest {
 
-	@Test
-	void 자동차_이름_5자_초과() {
-		String[] carNames = {"aa", "bb", "ccccccc"};
-		assertThatThrownBy(() -> {
-			CarNamesValidator.validateCarNames(carNames);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 자동차 이름은 5자 미만이어야 합니다.");
-	}
+    @Test
+    void 자동차_이름_empty() {
+        String[] carNames = {"aa", "", "cc"};
+        assertThatThrownBy(() -> {
+            CarNamesValidator.validateCarNames(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
+    }
 
-	@Test
-	void 자동차_이름_중복() {
-		String[] carNames = {"aa", "bb", "aa"};
-		assertThatThrownBy(() -> {
-			CarNamesValidator.validateCarNames(carNames);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 자동차 이름은 중복될 수 없습니다.");
-	}
+    @Test
+    void 자동차_이름_null() {
+        String[] carNames = {null};
+        assertThatThrownBy(() -> {
+            CarNamesValidator.validateCarNames(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
+    }
 
-	@Test
-	void 자동차_이름_공백() {
-		String[] carNames = {"aa", "", "cc"};
-		assertThatThrownBy(() -> {
-			CarNamesValidator.validateCarNames(carNames);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
-	}
+    @Test
+    void 자동차_이름_blank() {
+        String[] carNames = {"aa", "", "cc"};
+        assertThatThrownBy(() -> {
+            CarNamesValidator.validateCarNames(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
+    }
 
-	@Test
-	void 자동차_이름_입력X() {
-		String[] carNames = {""};
-		assertThatThrownBy(() -> {
-			CarNamesValidator.validateCarNames(carNames);
-		}).isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
-	}
+    @Test
+    void 자동차_이름_5자_초과() {
+        String[] carNames = {"aa", "bb", "ccccccc"};
+        assertThatThrownBy(() -> {
+            CarNamesValidator.validateCarNames(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 5자 미만이어야 합니다.");
+    }
+
+    @Test
+    void 자동차_이름_중복() {
+        String[] carNames = {"aa", "bb", "aa"};
+        assertThatThrownBy(() -> {
+            CarNamesValidator.validateCarNames(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+    }
 }
