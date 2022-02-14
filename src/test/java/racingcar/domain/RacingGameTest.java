d7fe857511c9cb488531caeba201396476ca1a3f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-02-15 01:50:43 +0900

    test: 단독우승자 1인 반환 테스트

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -51,5 +55,27 @@ public class RacingGameTest {
         assertThatExceptionOfType(UnsupportedOperationException.class)
             .isThrownBy(() -> racingGame.race())
             .withMessageMatching("종료된 게임은 더이상 실행할 수 없다.");
+    }
+
+    @Test
+    @DisplayName("단독 우승자가 있는 경우 우승자만 반환한다.")
+    void oneWinnerNames() {
+        // given
+        List<RacingCar> racingCars = Arrays.asList(
+            new RacingCar(new Name("pobi"), () -> true),
+            new RacingCar(new Name("crong"), () -> false),
+            new RacingCar(new Name("honux"), () -> false)
+        );
+        RacingGame racingGame = new RacingGame(new RacingCars(racingCars), 1);
+
+        // when
+        racingGame.race();
+        List<String> result = racingGame.winnerNames();
+
+        // then
+        assertAll(
+            () -> assertThat(result.size()).isEqualTo(1),
+            () -> assertThat(result).contains("pobi")
+        );
     }
 }
