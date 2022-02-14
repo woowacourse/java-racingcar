bffd194d41a6ccb6ebf5fe6040828737ab281c5a
양동주 <djwhy5510@gmail.com>
2022-02-14 22:37:40 +0900

    refactor: 문자열 계산기 메서드명 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -22,6 +22,21 @@ public class StringCalculator {
         return sumElements(numbers);
     }
 
+    private static boolean isNullOrEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String getDelimiter(String input) {
+        String delimiter = DEFAULT_DELIMITER;
+        if (Pattern.matches(REGEX, input)) {
+            delimiter = getCustomDelimiter(input);
+        }
+        return delimiter;
+    }
+
     private static String substringRegexPart(String input, String delimiter) {
         if (!delimiter.equals(DEFAULT_DELIMITER)) {
             input = input.substring(VALID_REGEX_PART_LENGTH);
@@ -31,23 +46,26 @@ public class StringCalculator {
 
     private static List<Integer> convertToNumberList(String[] array) {
         List<Integer> numbers = getNumbers(array);
-        elementsIsEmptyException(numbers);
+        checkElementsNotNull(numbers);
         return numbers;
     }
 
     private static int sumElements(List<Integer> numbers) {
         int sum = 0;
         for (Integer number : numbers) {
-            negativeNumberException(number);
+            checkPositiveNumber(number);
             sum += number;
         }
         return sum;
     }
 
-    private static void negativeNumberException(Integer number) {
-        if (number < 0) {
-            throw new RuntimeException("음수 값은 포함될 수 없습니다.");
-        }
+    private static String getCustomDelimiter(String input) {
+        checkDelimiterLength(input);
+
+        String delimiter = String.valueOf(input.charAt(CUSTOM_DELIMITER_INDEX));
+        checkDelimiterNotDigit(delimiter);
+
+        return delimiter;
     }
 
     private static List<Integer> getNumbers(String[] result) {
@@ -68,40 +86,28 @@ public class StringCalculator {
         return result;
     }
 
-    private static void elementsIsEmptyException(List<Integer> numbers) {
+    private static void checkElementsNotNull(List<Integer> numbers) {
         if (numbers.isEmpty()) {
             throw new RuntimeException("전달된 배열에 원소가 없습니다.");
         }
     }
 
-    private static boolean isNullOrEmpty(String input) {
-        if (input == null || input.isEmpty()) {
-            return true;
+    private static void checkPositiveNumber(Integer number) {
+        if (number < 0) {
+            throw new RuntimeException("음수 값은 포함될 수 없습니다.");
         }
-        return false;
     }
 
-    private static String getDelimiter(String input) {
-        String delimiter = DEFAULT_DELIMITER;
-        if (Pattern.matches(REGEX, input)) {
-            delimiter = getCustomDelimiter(input);
-        }
-        return delimiter;
-    }
-
-    private static String getCustomDelimiter(String input) {
-        delimiterSizeException(input);
-
-        String delimiter = String.valueOf(input.charAt(CUSTOM_DELIMITER_INDEX));
-        delimiterNumberException(delimiter);
-
-        return delimiter;
-    }
-
-    private static void delimiterSizeException(String input) {
+    private static void checkDelimiterLength(String input) {
         String regexPart = findRegexPart(input);
-        delimiterOverSizeException(regexPart);
-        delimiterNotFoundException(regexPart);
+        hasRegex(regexPart);
+        checkRegexLength(regexPart);
+    }
+
+    private static void checkDelimiterNotDigit(String delimiter) {
+        if (Character.isDigit(delimiter.charAt(0))) {
+            throw new RuntimeException("정수는 커스텀 구분자로 지정할 수 없습니다.");
+        }
     }
 
     private static String findRegexPart(String input) {
@@ -110,21 +116,15 @@ public class StringCalculator {
         return matcher.group();
     }
 
-    private static void delimiterNotFoundException(String regexPart) {
+    private static void hasRegex(String regexPart) {
         if (regexPart.length() < VALID_REGEX_PART_LENGTH) {
             throw new RuntimeException("커스텀 구분자가 입력되지 않았습니다.");
         }
     }
 
-    private static void delimiterOverSizeException(String regexPart) {
+    private static void checkRegexLength(String regexPart) {
         if (regexPart.length() > VALID_REGEX_PART_LENGTH) {
             throw new RuntimeException("커스텀 구분자는 한 글자여야 합니다.");
-        }
-    }
-
-    private static void delimiterNumberException(String delimiter) {
-        if (Character.isDigit(delimiter.charAt(0))) {
-            throw new RuntimeException("정수는 커스텀 구분자로 지정할 수 없습니다.");
         }
     }
 }
