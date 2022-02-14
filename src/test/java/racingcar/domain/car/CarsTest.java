9d64263465ccc6141d94fba0aa7e77da6f2b44f1
Kim Seunglae <obey1342@gmail.com>
2022-02-14 18:42:54 +0900

    test(CarsTest): 우승자 찾기 테스트 코드 작성

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -5,9 +5,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
+
+    private Car pobi;
+    private List<Car> sampleCarList;
+
+    @BeforeEach
+    void setUp() {
+        pobi = new Car(new Name("pobi"), new Position(3));
+        sampleCarList = List.of(pobi,
+            new Car(new Name("seung"), new Position(3)));
+    }
 
     @Test
     void createCarsBySplitComma() {
@@ -27,5 +39,23 @@ class CarsTest {
             new Cars(input);
         }).isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("자동차 이름에 ,만 사용할 수 없습니다.");
+    }
+
+    @DisplayName("우승자가 1명일 경우 정상적인지 확인")
+    @Test
+    void Should_SuccessToFindOneWinners() {
+        List<Car> sampleWinners = List.of(pobi);
+        List<Car> carList = List.of(pobi, new Car("seung"));
+        Cars cars = new Cars(carList);
+        assertThat(cars.findWinners()).isEqualTo(sampleWinners);
+    }
+
+    @DisplayName("우승자가 2명일 경우 정상적인지 확인")
+    @Test
+    void Should_SuccessToFindTwoWinners() {
+        List<Car> sampleWinners = sampleCarList;
+        List<Car> carList = sampleCarList;
+        Cars cars = new Cars(carList);
+        assertThat(cars.findWinners()).isEqualTo(sampleWinners);
     }
 }
