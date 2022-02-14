e127eb341f139b8e48820045965162c244c62116
wilgur513 <wilgur513@naver.com>
2022-02-14 16:32:47 +0900

    test: getPositionByName test

--- src/test/java/racingcar/domain/MidtermResultTest.java
+++ src/test/java/racingcar/domain/MidtermResultTest.java
@@ -9,6 +9,7 @@ import racingcar.domain.result.MidtermResult;
 
 public class MidtermResultTest {
 
+    private static final int FORWARDING_RANDOM_NUMBER = 4;
     private static final String CAR_1_NAME = "pobi";
     private static final String CAR_2_NAME = "crong";
     private static final String CAR_3_NAME = "honux";
@@ -22,12 +23,20 @@ public class MidtermResultTest {
         car1 = Car.createByName(CAR_1_NAME);
         car2 = Car.createByName(CAR_2_NAME);
         car3 = Car.createByName(CAR_3_NAME);
+
+        car1.move(FORWARDING_RANDOM_NUMBER);
+        car1.move(FORWARDING_RANDOM_NUMBER);
     }
 
     @Test
     public void 중간_결과_자동차_이름_리스트_반환() {
-        MidtermResult midtermResult = new MidtermResult(
-            Arrays.asList(car1, car2, car3));
+        MidtermResult midtermResult = new MidtermResult(Arrays.asList(car1, car2, car3));
         assertThat(midtermResult.getCarNames()).contains(CAR_1_NAME, CAR_2_NAME, CAR_3_NAME);
+    }
+
+    @Test
+    public void 중간_결과_자동차_위치_이름으로_조회() {
+        MidtermResult midtermResult = new MidtermResult(Arrays.asList(car1, car2, car3));
+        assertThat(midtermResult.getPositionByName(CAR_1_NAME)).isEqualTo(car1.getPosition());
     }
 }
