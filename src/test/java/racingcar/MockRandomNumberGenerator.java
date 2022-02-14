7ad53f42445bc67200ba25a44bf1da21afeee999
jojogreen91 <iawbg13@gmail.com>
2022-02-14 22:21:37 +0900

    refactor: define access modifier

--- src/test/java/racingcar/MockRandomNumberGenerator.java
+++ src/test/java/racingcar/MockRandomNumberGenerator.java
@@ -5,16 +5,15 @@ import racingcar.domain.random.RandomNumberGenerator;
 public class MockRandomNumberGenerator implements RandomNumberGenerator {
 
     private static final int FORWARD_BOUNDARY_NUMBER = 4;
-
-    static final int NUMBER_FOR_FORWARD = 4;
-    static final int NUMBER_FOR_STOP = 0;
-    static final int[] MOCK_RANDOM_NUMBERS = {
+    private static final int NUMBER_FOR_FORWARD = 4;
+    private static final int NUMBER_FOR_STOP = 0;
+    private static final int[] MOCK_RANDOM_NUMBERS = {
             NUMBER_FOR_FORWARD, NUMBER_FOR_STOP,
             NUMBER_FOR_FORWARD, NUMBER_FOR_STOP,
             NUMBER_FOR_STOP, NUMBER_FOR_FORWARD,
             NUMBER_FOR_STOP, NUMBER_FOR_FORWARD
     };
-    int index = 0;
+    private int index = 0;
 
     @Override
     public boolean isAvailableMove() {
