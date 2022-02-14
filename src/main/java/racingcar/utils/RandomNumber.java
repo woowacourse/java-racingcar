25405cc5be6dc4436ed439ca683b988bd45940a2
Seongwon97 <osw970325@naver.com>
2022-02-14 18:20:34 +0900

    refactor(Random.java): 불필요한 random의 생성 제거
    static 으로 random객체를 생성하여 불필요하게 반복적인 random객체 생성을 제거하였다.

--- src/main/java/racingcar/utils/RandomNumber.java
+++ src/main/java/racingcar/utils/RandomNumber.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class RandomNumber {
 
 	private final static int MAX_EXCLUSIVE = 9;
+	private static final Random random = new Random();
 
 	public int getRandomNumber() {
-		final Random random = new Random();
 		return random.nextInt(MAX_EXCLUSIVE) + 1;
 	}
 }
