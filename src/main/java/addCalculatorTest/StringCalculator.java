24d6de8498fd55dcb61c22f4a371be6bbb4c4fba
asebn1 <asebn1@khu.ac.kr>
2022-02-16 00:10:17 +0900

    refactor : AddCalulatorTest 에러 병합

--- src/main/java/addCalculatorTest/StringCalculator.java
+++ src/main/java/addCalculatorTest/StringCalculator.java
@@ -1,6 +1,11 @@
-package addCalculatorTest;
+package AddCalculatorTest;
+
+import java.util.Arrays;
 
 public class StringCalculator {
+    public static void main(String[] args) {
+    }
+
     public static int splitAndSum(String s) {
         if (s == null || s == "") {
             return 0;
