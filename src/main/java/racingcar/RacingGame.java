5557c8f132403c0feb0a91ead81a54b205f91d94
hyunrrr <whgusrms96@gmail.com>
2022-02-15 21:10:46 +0900

    refactor: 시도회수가 자연수인지 확인하는 기능 위치 이동
    입력이 숫자로 이루어졌는지는 view에서, 자연수인지는 RacingGame클래스에서 검사하도록 이동

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class RacingGame {
 
+    private static final String EXCEPTION_TRIAL_NUMBER = "[ERROR] 시도횟수는 1이상의 자연수여야 합니다.\n";
     private RacingCars racingCars;
     private int trialCount;
 
@@ -19,9 +20,15 @@ public class RacingGame {
     private void makeRacingCars() {
         List<String> carNameBucket = InputView.askCarName();
         trialCount = InputView.askTryCount();
+        checkTrialCountIsNaturalNumber();
         racingCars = new RacingCars(carNameBucket);
     }
 
+    private void checkTrialCountIsNaturalNumber() {
+        if (trialCount < 1) {
+            throw new IllegalArgumentException(EXCEPTION_TRIAL_NUMBER);
+        }
+    }
 
     public void playGame() {
         makeRacingCars();
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String CAR_NAME_DELIMITER = ",";
     private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String EXCEPTION_INPUT_MUST_NUMBER = "[ERROR] 입력은 숫자여야 합니다.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +23,20 @@ public class InputView {
 
     public static int askTryCount() {
         System.out.println(QUESTION_TRY_COUNT);
-        return Integer.parseInt(scanner.nextLine());
+        String userInput = scanner.nextLine();
+        checkStringIsNumber(userInput);
+        return Integer.parseInt(userInput);
     }
 
+    private static void checkStringIsNumber(String target) {
+        for (int i = 0; i < target.length(); ++i) {
+            checkCharIsNumber(target.charAt(i));
+        }
+    }
+
+    private static void checkCharIsNumber(char target) {
+        if (target < '0' || target > '9') {
+            throw new IllegalArgumentException(EXCEPTION_INPUT_MUST_NUMBER);
+        }
+    }
 }
