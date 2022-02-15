29aecd6e802d40a2c59720eb0e3ea29a193e50c4
sinb57 <sinb57@naver.com>
2022-02-15 12:38:06 +0900

    refactor: 예외메시지 상수 문자열의 접근제어자 변경

--- src/main/java/racingcar/exception/reader/message/ReaderExceptionMessage.java
+++ src/main/java/racingcar/exception/reader/message/ReaderExceptionMessage.java
@@ -2,6 +2,6 @@ package racingcar.exception.reader.message;
 
 public class ReaderExceptionMessage {
 
-    public static String READER_CANNOT_READ_EXCEPTION_MESSAGE = "입력을 받아올 수 없습니다.";
+    public static final String READER_CANNOT_READ_EXCEPTION_MESSAGE = "입력을 받아올 수 없습니다.";
 
 }
--- src/main/java/racingcar/exception/round/message/RoundExceptionMessage.java
+++ src/main/java/racingcar/exception/round/message/RoundExceptionMessage.java
@@ -2,7 +2,7 @@ package racingcar.exception.round.message;
 
 public class RoundExceptionMessage {
 
-    public static String ROUND_NOT_NUMERIC_EXCEPTION_MESSAGE = "이동 횟수는 숫자여야 합니다.";
-    public static String ROUND_NOT_POSITIVE_EXCEPTION_MESSAGE = "이동 횟수는 양수여야 합니다.";
+    public static final String ROUND_NOT_NUMERIC_EXCEPTION_MESSAGE = "이동 횟수는 숫자여야 합니다.";
+    public static final String ROUND_NOT_POSITIVE_EXCEPTION_MESSAGE = "이동 횟수는 양수여야 합니다.";
 
 }
