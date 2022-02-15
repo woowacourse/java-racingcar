5e0bc450b7de26e3a4c3d183ced3066fef6d2cd8
dongho <dongho108@naver.com>
2022-02-16 01:24:02 +0900

    refactor : 빈문자 검증 리팩토링

--- src/main/java/racingcar/utils/TrialNumberValidator.java
+++ src/main/java/racingcar/utils/TrialNumberValidator.java
@@ -2,7 +2,6 @@ package racingcar.utils;
 
 public class TrialNumberValidator {
     private static final int MIN_BOUND = 1;
-    private static final String EMPTY_STRING = "";
     private static final String EMPTY_INPUT_MESSAGE = "시도 횟수에 빈 문자열은 입력될 수 없습니다.";
     private static final String NOT_POSITIVE_NUMBER_MESSAGE = "시도 횟수가 1 이상이 아닙니다.";
     private static final String NOT_INTEGER_NUMBER_MESSAGE = "시도 횟수에는 정수만 입력할 수 있습니다.";
@@ -14,7 +13,7 @@ public class TrialNumberValidator {
     }
 
     private static void isEmpty(String trialNumber) {
-        if (trialNumber.equals(EMPTY_STRING)) {
+        if (trialNumber.isEmpty() || trialNumber.isBlank()) {
             throw new IllegalArgumentException(EMPTY_INPUT_MESSAGE);
         }
     }
