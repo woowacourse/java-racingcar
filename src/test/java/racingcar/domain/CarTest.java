054c5e47180cd43499fe5b63a7fe6f1224237452
yukong <kyb062666@gmail.com>
2022-02-15 22:12:02 +0900

    refactor(CarTest): Car의 drive 테스트 메소드 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,12 +3,11 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CarTest {
-	Car car = new Car("sudal");
 
 	@DisplayName("자동차 이름 공백 테스트")
 	@Test
@@ -37,24 +36,22 @@ class CarTest {
 	@DisplayName("랜덤 넘버가 4 이상일 경우 전진")
 	@Test
 	void car_drive_true() {
-		//given
-		int positionStatus = car.getPosition();
-		boolean directing = true;
-		//when
-		car.drive(directing);
-		//then
-		assertThat(car.getPosition()).isEqualTo(positionStatus + 1);
+		Car carPositionZero = new Car("zero"); // position: 0
+		Car carDriveTrue = new Car("drive");
+
+		carDriveTrue.drive(true);
+
+		assertFalse(carDriveTrue.isSamePosition(carPositionZero));
 	}
 
 	@DisplayName("랜덤 넘버가 3 이하일 경우 멈춤")
 	@Test
 	void car_drive_false() {
-		//given
-		int positionStatus = car.getPosition();
-		boolean directing = false;
-		//when
-		car.drive(directing);
-		//then
-		assertThat(car.getPosition()).isEqualTo(positionStatus);
+		Car carPositionZero = new Car("zero"); // position: 0
+		Car carDriveFalse = new Car("drive");
+
+		carDriveFalse.drive(false);
+
+		assertTrue(carDriveFalse.isSamePosition(carPositionZero));
 	}
 }
