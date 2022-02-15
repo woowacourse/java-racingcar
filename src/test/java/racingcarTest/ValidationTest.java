121eb950d5aebaca9bdcfdc68257be35828ac523
hyewoncc <hyewoncc@gmail.com>
2022-02-15 22:58:49 +0900

    style: 테스트 이름 수정

--- src/test/java/racingcarTest/ValidationTest.java
+++ src/test/java/racingcarTest/ValidationTest.java
@@ -1,37 +1,37 @@
 package racingcarTest;
 
 import org.junit.jupiter.api.Test;
-import racingcar.controller.Validation;
+import racingcar.util.Validation;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidationTest {
 
     @Test
-    void player_name_over_lengthLimit_throw_exception() {
+    void carNamesInput_overLengthLimit_throwException() {
         assertThatThrownBy(() -> Validation.carNameValidation("aaaaaaa,bbbb"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void player_name_blank_throw_exception() {
+    void carNamesInput_hasBlankName_throwException() {
         assertThatThrownBy(() -> Validation.carNameValidation(",,,bbbb"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void if_tryNum_is_string_throw_exception() {
+    void tryNumInput_isNotDigit_throwException() {
         assertThatThrownBy(() -> Validation.tryNumValidation("aaa"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void if_tryNum_is_minus_throw_exception() {
+    void tryNumInput_isNotNaturalNumber_throwException() {
         assertThatThrownBy(() -> Validation.tryNumValidation("-5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void validate_tryNum_is_integer() {
+    void tryNumInput_isRightDigit() {
         Validation.tryNumValidation("5");
     }
 }
