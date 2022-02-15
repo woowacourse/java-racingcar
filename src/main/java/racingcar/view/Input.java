450a462ad2359d5ee85e3e95984b1098bc5bf6ad
hyewoncc <hyewoncc@gmail.com>
2022-02-15 16:08:21 +0900

    style: 선언 순서 변경

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -3,9 +3,10 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class Input {
-    private final static Scanner sc = new Scanner(System.in);
     private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_MESSAGE = "시도할 회수는 몇회인가요?";
+
+    private final static Scanner sc = new Scanner(System.in);
 
     public static String inputCarNames() {
         System.out.println(INPUT_CAR_MESSAGE);
