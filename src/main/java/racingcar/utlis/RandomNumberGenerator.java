8819032d09d964ed0d703770e4efc941564eb96e
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-15 15:01:48 +0900

    refactor: 재사용 객체 상수화

--- src/main/java/racingcar/utlis/RandomNumberGenerator.java
+++ src/main/java/racingcar/utlis/RandomNumberGenerator.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class RandomNumberGenerator {
 
 	private static final int MAX_RANDOM_NUMBER = 9;
+	private static final Random random = new Random();
 
 	public static int getRandomInt() {
-		Random random = new Random();
 		return random.nextInt(MAX_RANDOM_NUMBER);
 	}
 
