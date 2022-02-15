4c8549af59b9689ab224aaed1c1c62a27eb6013a
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-15 17:46:11 +0900

    refactor(carDTO): car 데이터 저장 클래스 생성, get 리턴값으로 적용

--- src/test/java/racingCar/view/OutputTest.java
+++ src/test/java/racingCar/view/OutputTest.java
@@ -6,27 +6,73 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
-import org.junit.jupiter.api.DisplayName;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingCar.model.CarDTO;
 import racingCar.model.RacingCars;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class OutputTest {
+
     @Test
-    void 차_상태확인_잘되는지_검사() {
+    void 시작메세지_검사() {
+        //given
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        //when
+        Output.printStartMessage();
+
+        //then
+        assertThat(out.toString())
+                .isEqualTo("\n실행 결과\n");
+    }
+
+    @Test
+    void 실행결과출력_검사() {
         //given
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         RacingCars cars = new RacingCars(Arrays.asList("jiwoo", "juri"));
-        cars.get().get(0).move();
-        cars.get().get(0).move();
+        CarDTO jiwooCarDTO = cars.get().get(0);
+        jiwooCarDTO.position = 2;
 
         //when
-        Output.printRoundResult(cars.get());
+        Output.printRoundResult(Arrays.asList(jiwooCarDTO, cars.get().get(1)));
 
         //then
         assertThat(out.toString())
-                .contains("jiwoo : --\n")
-                .contains("juri : \n");
+                .isEqualTo("jiwoo : --\njuri : \n" + System.lineSeparator());
+    }
+
+    @Test
+    void 위너출력_검사_1명() {
+        //given
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        RacingCars cars = new RacingCars(Arrays.asList("jiwoo", "juri"));
+        CarDTO jiwooCarDTO = cars.get().get(0);
+
+        //when
+        Output.printWinner(List.of(jiwooCarDTO));
+
+        //then
+        assertThat(out.toString())
+                .isEqualTo("jiwoo가 최종 우승했습니다.\n");
+    }
+
+    @Test
+    void 위너출력_검사_2명() {
+        //given
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        RacingCars cars = new RacingCars(Arrays.asList("jiwoo", "juri"));
+
+        //when
+        Output.printWinner(cars.get());
+
+        //then
+        assertThat(out.toString())
+                .isEqualTo("jiwoo, juri가 최종 우승했습니다.\n");
     }
 }
