d81c38075eb73662ac5e318c71a28bdd7f3a0f6b
hyunrrr <whgusrms96@gmail.com>
2022-02-15 16:46:00 +0900

    refactor: 자동차와 관련된 예외 확인 기능 위치 이동

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -5,18 +5,60 @@ import racingcar.validator.Validator;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RacingCars {
 
     private static final int ANY_INDEX = 0;
+    private static final int MINIMUM_CAR_NUMBER = 2;
     private static final String EXCEPTION_NUMBER_OF_CAR = "[ERROR] 경기를 위한 자동차는 최소 2대이상이여야 합니다.";
     private List<RacingCar> racingCarBucket;
 
     public RacingCars(List<String> carNameBucket) {
+        validateCar(carNameBucket);
         racingCarBucket = new ArrayList<>();
         for (String name : carNameBucket) {
             racingCarBucket.add(new RacingCar(name));
+        }
+    }
+
+    private void validateCar(List<String> carNameBucket) {
+        checkNumberOfCar(carNameBucket);
+        checkDuplicatedCarNameExist(carNameBucket);
+    }
+
+    private void checkNumberOfCar(List<String> carNameBucket) {
+        if (carNameBucket.size() < MINIMUM_CAR_NUMBER) {
+            throw new IllegalArgumentException(EXCEPTION_NUMBER_OF_CAR);
+        }
+    }
+
+    private void checkDuplicatedCarNameExist(List<String> carNameBucket) {
+        for (String name : carNameBucket) {
+            checkNameExistMoreThanOne(name, carNameBucket);
+        }
+    }
+
+    private void checkNameExistMoreThanOne(String sourceName, List<String> carNameBucket) {
+        int numberOfName = 0;
+        for (String targetName : carNameBucket) {
+            numberOfName += isNameSame(sourceName, targetName);
+        }
+        checkNumberOfNameOverLimit(numberOfName);
+    }
+
+    private int isNameSame(String sourceName, String targetName) {
+        if (sourceName.equals(targetName)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private void checkNumberOfNameOverLimit(int number) {
+        if (number < MINIMUM_CAR_NUMBER) {
+            throw new IllegalArgumentException(EXCEPTION_NUMBER_OF_CAR);
         }
     }
 
