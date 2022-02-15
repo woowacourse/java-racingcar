7dd86dac8f349aa5c5d1687957cad7f7bcad0aba
Chris <bjuuuu98@gmail.com>
2022-02-15 15:12:08 +0900

    refactor: 예외메시지 상수화

--- src/main/java/racingcar/domain/car/vo/CarName.java
+++ src/main/java/racingcar/domain/car/vo/CarName.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class CarName {
     private static final String LONGER_THAN_FIVE_CAR_NAME = "자동차 이름은 최대 5자입니다.";
     private static final String BLANK_CAR_NAME = "공백으로만 이루어진 자동차 이름이 있습니다.";
+    private static final String CAR_NAME_NULL_EXCEPTION = "자동차 이름에 null이 들어왔습니다.";
     private static final int MAX_LENGTH_OF_CAR_NAME = 5;
 
     private final String name;
@@ -23,7 +24,7 @@ public class CarName {
 
     private void validateBlank(final String value) {
         if (value == null) {
-            throw new NullPointerException("자동차 이름에 null이 들어왔습니다.");
+            throw new NullPointerException(CAR_NAME_NULL_EXCEPTION);
         }
         if (value.isBlank()) {
             throw new IllegalArgumentException(BLANK_CAR_NAME);
