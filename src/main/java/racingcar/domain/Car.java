8fe382dcf8d493fd9bba42f160b4811f32365edc
yukong <kyb062666@gmail.com>
2022-02-15 22:12:34 +0900

    refactor(Car): public 과 private 메소드 위치 조정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -18,31 +18,6 @@ public class Car implements Comparable<Car> {
 		}
 	}
 
-	private void checkValid(String names) {
-		checkBlank(names);
-		checkNameLength(names);
-	}
-
-	private void checkBlank(String name) {
-		String text = "자동차 이름은 %s일 수 없습니다.";
-		if (name == null) {
-			throw new NullPointerException(String.format(text, "null"));
-		}
-		if (name.trim().equals("")) {
-			throw new IllegalArgumentException(String.format(text, "공백"));
-		}
-	}
-
-	private void checkNameLength(String name) {
-		if (!(name.trim().length() <= CAR_LENGTH_LIMIT)) {
-			throw new IllegalArgumentException("자동차의 이름은 " + CAR_LENGTH_LIMIT + "글자를 초과할 수 없습니다.");
-		}
-	}
-
-	private void move() {
-		position++;
-	}
-
 	public boolean isSamePosition(Car other) {
 		return this.position == other.position;
 	}
@@ -67,5 +42,30 @@ public class Car implements Comparable<Car> {
 	@Override
 	public int compareTo(Car car) {
 		return this.position - car.position;
+	}
+
+	private void checkValid(String names) {
+		checkBlank(names);
+		checkNameLength(names);
+	}
+
+	private void checkBlank(String name) {
+		String text = "자동차 이름은 %s일 수 없습니다.";
+		if (name == null) {
+			throw new NullPointerException(String.format(text, "null"));
+		}
+		if (name.trim().equals("")) {
+			throw new IllegalArgumentException(String.format(text, "공백"));
+		}
+	}
+
+	private void checkNameLength(String name) {
+		if (!(name.trim().length() <= CAR_LENGTH_LIMIT)) {
+			throw new IllegalArgumentException("자동차의 이름은 " + CAR_LENGTH_LIMIT + "글자를 초과할 수 없습니다.");
+		}
+	}
+
+	private void move() {
+		position++;
 	}
 }
