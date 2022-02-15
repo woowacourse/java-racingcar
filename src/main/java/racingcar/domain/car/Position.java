569beb9b6fc39547a07c178d5b27c379b93c0e1b
cjlee38 <cjlee38@github.com>
2022-02-15 17:16:00 +0900

    feat(Position): 자동차 위치 검증로직 추가

--- src/main/java/racingcar/domain/car/Position.java
+++ src/main/java/racingcar/domain/car/Position.java
@@ -3,17 +3,26 @@ package racingcar.domain.car;
 import java.util.Objects;
 
 import racingcar.domain.movement.MovementStrategy;
+import racingcar.exception.NumberTooSmallException;
 
 public class Position {
 
+    private static final int MIN_VALUE = 0;
     private int value;
 
     public Position() {
-        this(0);
+        this(MIN_VALUE);
     }
 
     public Position(int value) {
+        validateMinimum(value);
         this.value = value;
+    }
+
+    private void validateMinimum(int value) {
+        if (value < MIN_VALUE) {
+            throw new NumberTooSmallException("자동차의 위치는 0 미만이 될 수 없습니다.");
+        }
     }
 
     public boolean isSame(int num) {
