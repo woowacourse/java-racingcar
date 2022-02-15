1926203469218c3450e95451c917ee16b28f8ddd
leokim <kunga077@gmail.com>
2022-02-16 00:31:16 +0900

    refactor: BeforeEach 테스트로 중복되는 인스턴스 생성 제거

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,20 +2,24 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
+    private Car car;
+    @BeforeEach
+    public void setUp() {
+        car = new Car("Thor");
+    }
     @Test
     public void move() {
-        Car car = new Car("Thor");
         car.tryMove(5);
         assertThat(car.getCarPosition()).isEqualTo(1);
     }
 
     @Test
     public void doNotMove() {
-        Car car = new Car("Thor");
         car.tryMove(3);
         assertThat(car.getCarPosition()).isEqualTo(0);
     }
