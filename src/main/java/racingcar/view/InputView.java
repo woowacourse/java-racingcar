d2676faad33b039e391c66ff63db353a99f59917
hamcheeseburger <yoo77hyeon@gmail.com>
2022-02-15 13:36:30 +0900

    refactor: Scanner 상수 이름 컨벤션

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,17 +8,17 @@ public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner CONSOLE = new Scanner(System.in);
 
     public static String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        return scanner.nextLine();
+        return CONSOLE.nextLine();
     }
 
     public static int inputTryCount() {
         try {
             System.out.println(INPUT_TRY_COUNT_MESSAGE);
-            return scanner.nextInt();
+            return CONSOLE.nextInt();
         } catch (InputMismatchException e) {
             throw new NumberFormatException("시도횟수는 숫자이어야 합니다.");
         }
