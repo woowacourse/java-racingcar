caf1f87e9961c7c676c8195405081433e32ff5c5
yoon <duddbs774@gmail.com>
2022-02-15 09:17:56 +0900

    refactor: 사용하지 않는 기능 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,5 @@
 package racingcar.view;
 
-import racingcar.utils.Validator;
-
 import java.util.Scanner;
 
 public class InputView {
@@ -9,17 +7,7 @@ public class InputView {
 
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return getCarNames(scanner.nextLine());
-    }
-
-    private static String getCarNames(final String input) {
-        try {
-            Validator.checkBlankOrNull(input);
-            return input;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getCarNames();
-        }
+        return scanner.nextLine();
     }
 
     public static int getCount() {
