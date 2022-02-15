a7a53f9c5c96a37281607194e973544a1995de86
Hongdonggeon <ghd700@naver.com>
2022-02-15 17:55:48 +0900

    refactor: 사용하지 않은 상수 삭제

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CarFactory {
-    private static final String DUPLICATED_NAME_ERROR = "[ERROR] 차 이름은 중복될 수 없습니다.";
 
     public static List<Car> of(String names) {
         String[] arrNames = names.split(",");
