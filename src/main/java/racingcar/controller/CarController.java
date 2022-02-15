bccf36697c7a384810d160b77115c1be44f9ebaa
asebn1 <57438644+asebn1@users.noreply.github.com>
2022-02-15 22:21:14 +0900

    [1단계 - 자동차 경주 구현] 제로(공재호) 미션 제출합니다. (#268)
    * feat: TDD 문자열 덧셈 계산기 추가
    
    * docs: 구현 기능 목록 작성
    
    * feat: 사용자입력 받기
    
    * feat: 유저 입력 검증
    
    * feat: 유저 입력 검증 및 저장
    
    * test: ValidationTest TDD 검증 추가
    
    * feat: 자동차 이동 및 출력
    
    * feat: 최종 우승자 출력
    
    * style: formatting 수정
    
    * refactor: 변수 값 상수화 변경
    
    * refactor: 파일 끝 개행 추가
    
    * refactor: Num과 Number 구분해 사용한 것을 같도록 수정
    
    * refactor: 메서드 명 수정. 동사를 먼저
    
    * refactor: 변수명 수정
    
    * refactor: 테스트 명 수정 및 중복제거
    
    * refactor: 불필요한 import 제거

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,56 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.view.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarController {
+    private static final String PRINT_RESULT = "실행 결과";
+
+    public static void startRace(List<Car> cars, int tryNum) {
+        System.out.println(PRINT_RESULT);
+
+        for (int i = 0; i < tryNum; i++) {
+            moveCar(cars);
+            Output.printRace(cars);
+        }
+    }
+
+    public static void finalWinner(List<Car> cars) {
+        int maxPosition = findMaxPosition(cars);
+        List<String> winnerList = getWinnerList(cars, maxPosition);
+        Output.printWinner(winnerList);
+    }
+
+    private static void moveCar(List<Car> cars) {
+        for (Car car : cars) {
+            car.goForward();
+        }
+    }
+
+    private static List<String> getWinnerList(List<Car> cars, int maxPosition) {
+        List<String> winnerList = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winnerList.add(car.getName());
+            }
+        }
+
+        return winnerList;
+    }
+
+    private static int findMaxPosition(List<Car> cars) {
+        int maxNum = 0;
+
+        for (Car car : cars) {
+            if (car.getPosition() > maxNum) {
+                maxNum = car.getPosition();
+            }
+        }
+
+        return maxNum;
+    }
+}
---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class Output {
+    private static final String SPLIT_DELIM = ", ";
+    private static final String OUTPUT_WINNER_MESSAGE = "가 최종 우승했습니다.";
+
+    public static void printRace(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.toString());
+        }
+        System.out.println();
+    }
+
+    public static void printWinner(List<String> winnerNames) {
+        System.out.println(String.join(SPLIT_DELIM, winnerNames) + OUTPUT_WINNER_MESSAGE);
+        printLastLineBreak();
+    }
+
+    private static void printLastLineBreak() {
+        System.out.println();
+    }
+}
