0add70a2de8871c7a8714b5dafee3ebd367aa354
jurlring <78091011+jurlring@users.noreply.github.com>
2022-02-15 15:00:27 +0900

    test(RacingCarServiceTest): 공통 실행 메소드 분리

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import racingcar.model.Car;
@@ -15,6 +16,7 @@ class RacingCarServiceTest {
 	private RacingCarService racingCarService;
 	private final int expectMaxPosition = 4;
 
+	@BeforeEach
 	public void customRacingCars() {
 		Car car1 = new Car("juri", 2);
 		Car car2 = new Car("hunch", 1);
@@ -24,13 +26,11 @@ class RacingCarServiceTest {
 
 	@Test
 	public void 최대값_찾기_테스트() {
-		customRacingCars();
 		assertThat(racingCarService.findMaxPosition()).isEqualTo(expectMaxPosition);
 	}
 
 	@Test
 	public void 우승자_찾기_테스트() {
-		customRacingCars();
 		ArrayList<String> result = new ArrayList<>(Collections.singletonList("pobi"));
 		assertThat(racingCarService.findWinner()).isEqualTo(result);
 	}
