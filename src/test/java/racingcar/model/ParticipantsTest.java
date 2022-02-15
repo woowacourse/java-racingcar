06d9564690564993477a070e3a981f7e34196adb
leokim <kunga077@gmail.com>
2022-02-15 18:36:32 +0900

    refactor: 매 테스트시 인스턴스를 생성할 수 있는 setUp 메소드 추가

--- src/test/java/racingcar/model/ParticipantsTest.java
+++ src/test/java/racingcar/model/ParticipantsTest.java
@@ -2,17 +2,22 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ParticipantsTest {
 
+    private Participants participants;
+    @BeforeEach
+    public void setUp() {
+        this.participants = new Participants();
+        participants.participateInRacing(new Car("Eden"));
+        participants.participateInRacing(new Car("Thor"));
+    }
+
     @Test
     public void participateInRacing() {
-        Participants participants = new Participants();
-
-        participants.participateInRacing(new Car("Eden"));
-
-        assertThat(participants.participateSize()).isEqualTo(1);
+        assertThat(participants.participateSize()).isEqualTo(2);
     }
 
     @Test
