a3e68399b61ee87392f027023d2691266db4a3f6
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-15 12:46:30 +0900

    test(car): 자동차의 move() 메서드 테스트 추가

--- src/test/java/racingcar/vo/CarTest.java
+++ src/test/java/racingcar/vo/CarTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.numberGenerator.MovableNumberGenerator;
+import racingcar.numberGenerator.NumberGenerator;
+import racingcar.numberGenerator.UnMovableNumberGenerator;
 
 public class CarTest {
 
@@ -50,5 +53,29 @@ public class CarTest {
         Car car = new Car(carName, onePosition);
         Car testCar = new Car(testCarName, zeroPosition);
         assertThat(car.compareTo(testCar)).isGreaterThan(0);
+    }
+
+    @DisplayName("move() 움직임 테스트")
+    @Test
+    public void move_with_movableNumber_test() throws Exception {
+        String name = "name1";
+        CarName carName = new CarName(name);
+        Position zeroPosition = new Position(0);
+        Car car = new Car(carName, zeroPosition);
+        NumberGenerator numberGenerator = new MovableNumberGenerator();
+        Car movedCar = car.move(numberGenerator.generate());
+        assertThat(movedCar.compareTo(car)).isGreaterThan(0);
+    }
+
+    @DisplayName("move() 정지 테스트")
+    @Test
+    public void move_with_unMovableNumber_test() throws Exception {
+        String name = "name1";
+        CarName carName = new CarName(name);
+        Position zeroPosition = new Position(0);
+        Car car = new Car(carName, zeroPosition);
+        NumberGenerator numberGenerator = new UnMovableNumberGenerator();
+        Car movedCar = car.move(numberGenerator.generate());
+        assertThat(movedCar.compareTo(car)).isEqualTo(0);
     }
 }
