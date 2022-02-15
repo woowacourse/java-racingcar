7ed26305d176f54ae4e389146f69a6fd80aaa384
Chris <bjuuuu98@gmail.com>
2022-02-15 16:11:31 +0900

    test: Test 클래스 public 키워드 삭제

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class StringCalculatorTest {
+class StringCalculatorTest {
     private final StringCalculator stringCalculator = new StringCalculator();
 
     @ParameterizedTest
