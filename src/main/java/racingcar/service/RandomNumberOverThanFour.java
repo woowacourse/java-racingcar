f7e32473c7d88070c16a1a2ee71f6319965de433
Chris <bjuuuu98@gmail.com>
2022-02-15 17:10:56 +0900

    refactor: 전진 값 생성 로직 수정

--- src/main/java/racingcar/service/RandomNumberOverThanFour.java
+++ src/main/java/racingcar/service/RandomNumberOverThanFour.java
@@ -1,16 +1,18 @@
 package racingcar.service;
 
-import java.util.Random;
-
 public class RandomNumberOverThanFour implements Movement {
     private static final int NUMBER_POSSIBLE_TO_MOVE = 4;
     private static final int MAX_RANDOM_NUMBER_BOUNDARY = 10;
 
-    private final Random random = new Random();
+    private final NumberGenerator numberGenerator;
+
+    public RandomNumberOverThanFour(final NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
 
     public int move() {
-        final int randomNumber = random.nextInt(MAX_RANDOM_NUMBER_BOUNDARY);
-        if(randomNumber >= NUMBER_POSSIBLE_TO_MOVE) {
+        final int randomNumber = numberGenerator.generate(MAX_RANDOM_NUMBER_BOUNDARY);
+        if (randomNumber >= NUMBER_POSSIBLE_TO_MOVE) {
             return 1;
         }
         return 0;
