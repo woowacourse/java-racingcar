d249d043c3b24f71612b1486af84fab40ad9fbb0
summerlunaa <summerlunaa9@gmail.com>
2022-02-15 23:39:59 +0900

    chore: 주석 코드 삭제

--- src/test/java/racingcar/validator/MoveCountValidatorTest.java
+++ src/test/java/racingcar/validator/MoveCountValidatorTest.java
@@ -4,22 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import racingcar.validator.MoveCountValidator;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import static racingcar.validator.MoveCountValidator.*;
 
 class MoveCountValidatorTest {
-	// @ParameterizedTest
-	// @ValueSource(strings = {"a", " ", "가"})
-	// @DisplayName("입력값이 숫자가 아닐 경우")
-	// void notInteger(String moveCount) {
-	// 	assertThatThrownBy(() -> MoveCountValidator.validateMoveCount(moveCount))
-	// 		.isInstanceOf(IllegalArgumentException.class)
-	// 		.hasMessageContaining(ERROR_NOT_INTEGER);
-	// }
-
 	@ParameterizedTest
 	@ValueSource(ints = {-1, 0})
 	@DisplayName("입력값이 양수가 아닐 경우")
