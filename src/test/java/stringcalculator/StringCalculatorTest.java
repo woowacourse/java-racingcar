ddaeef54dae3f107157f7e6e15345d8bc85814af
awesomeo184 <awesomeo184@gmail.com>
2022-02-16 17:47:12 +0900

    chore: 불필요한 임포트문을 제거한다

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -2,7 +2,7 @@ package stringcalculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class StringCalculatorTest {
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,11 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 
