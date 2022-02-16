11945a799cf9e2939ee1226897899fda0dd607a1
her0807 <her0807@naver.com>
2022-02-16 19:33:43 +0900

    feat: 쉼표 또는 콜론 구분자로 분리 후 sum 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,19 @@
 package calculator;
 
 public class Calculator {
-
-	public int splitAndSum(final String text) {
+	public static int splitAndSum(final String text) {
 		if (text == null || text.isEmpty()) {
 			return 0;
 		}
-		return Integer.parseInt(text);
+		final String[] splitValues = text.split(",|:");
+		return sum(splitValues);
+	}
+
+	private static int sum(String[] values) {
+		int sum = 0;
+		for (String i : values) {
+			sum += Integer.parseInt(i);
+		}
+		return sum;
 	}
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,28 +6,38 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-	Calculator calculator = new Calculator();
-
 	//1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다
 	@DisplayName("빈문자열, null 테스트")
 	@Test
 	public void splitAndSum_null_또는_빈문자() throws Exception {
-		int result = calculator.splitAndSum(null);
+		int result = Calculator.splitAndSum(null);
 		assertThat(result).isEqualTo(0);
 
-		result = calculator.splitAndSum("");
+		result = Calculator.splitAndSum("");
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
 	public void splitAndSum_숫자하나() throws Exception {
-		int result = calculator.splitAndSum("1");
+		int result = Calculator.splitAndSum("1");
 		assertThat(result).isEqualTo(1);
 	}
 
 	@Test
 	public void splitAndSum_숫자하나_번외() throws Exception {
-		int result = calculator.splitAndSum("2");
+		int result = Calculator.splitAndSum("2");
 		assertThat(result).isEqualTo(2);
+	}
+
+	@Test
+	public void splitAndSum_쉼표구분자() throws Exception {
+		int result = Calculator.splitAndSum("1,2");
+		assertThat(result).isEqualTo(3);
+	}
+
+	@Test
+	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+		int result = Calculator.splitAndSum("1,2:3");
+		assertThat(result).isEqualTo(6);
 	}
 }
