960d5f0f330113f1036d16516857a8bc7fa0b455
RunaNam <nmj5012@naver.com>
2022-02-16 14:54:51 +0900

    refactor: change main function code

--- src/main/java/racingCar/Application.java
+++ src/main/java/racingCar/Application.java
@@ -5,7 +5,6 @@ import racingCar.controller.RacingCarController;
 public class Application {
 
 	public static void main(String[] args) {
-		RacingCarController racingCar = new RacingCarController();
-		racingCar.start();
+		new RacingCarController().start();
 	}
 }
