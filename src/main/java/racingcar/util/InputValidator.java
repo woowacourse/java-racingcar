6e366521563eac40d8db3186efa3b6eaba6b4dab
sun <syoun602@gmail.com>
2022-02-16 22:48:35 +0900

    refactor: delete unused method

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,20 +1,9 @@
 package racingcar.util;
 
-import java.util.List;
-
 public class InputValidator {
     public static void validateBlank(String carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("최소 1개 이상의 자동차가 입력되어야 합니다.");
-        }
-    }
-
-    public static void validateDuplicateName(List<String> carNames) {
-        long distinctNum = carNames.stream()
-                .distinct()
-                .count();
-        if (distinctNum != carNames.size()) {
-            throw new IllegalArgumentException("자동차의 이름에 중복이 포함되면 안됩니다.");
         }
     }
 }
