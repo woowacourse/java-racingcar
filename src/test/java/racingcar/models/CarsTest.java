aeb78cbc83483475650d35ccc839342b2749ddd6
Seongwon97 <osw970325@naver.com>
2022-02-17 01:57:25 +0900

    refactor(CarsTest.java): 불필요한 import제거

--- src/test/java/racingcar/models/CarsTest.java
+++ src/test/java/racingcar/models/CarsTest.java
@@ -1,6 +1,5 @@
 package racingcar.models;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
