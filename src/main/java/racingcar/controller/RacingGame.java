06c747c5390f8a74eecfdd1960c1b48c09b2691b
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-02-16 18:22:17 +0900

    refactor: RacingGame 클래스에서 불필요한 필드 제거

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -6,11 +6,9 @@ import java.util.Arrays;
 
 public class RacingGame {
     private final RaceController raceController;
-    private final String[] carNames;
     private final int tryCount;
 
     public RacingGame(String[] carNames, int tryCount) {
-        this.carNames = Arrays.copyOf(carNames, carNames.length);
         this.tryCount = tryCount;
         raceController = new RaceController();
         raceController.insertCarFromCarNames(carNames);
