9bb1b87b8b4b572f5652ea3c5c37e864a3d69c5e
her0807 <her0807@naver.com>
2022-02-16 18:49:34 +0900

    feat: 계산기 빈 문자열, null일 경우 0으로 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,75 +1,12 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Calculator {
+
 	public int splitAndSum(String text) {
-		if (!isValidValues(text)) {
+		if (text == null)
 			return 0;
-		}
-
-		int[] numbers = stringToIntArray(getValues(text));
-		checkNumbers(numbers);
-		return add(numbers);
-	}
-
-	private boolean isValidValues(String text) {
-		return text != null && !text.trim().isEmpty();
-	}
-
-	private int[] stringToIntArray(String[] strings) {
-		ArrayList<Integer> numbers = new ArrayList<>();
-		for (String i : strings) {
-			numbers.add(convertValue(i));
-		}
-		return numbers.stream().mapToInt(Integer::intValue).toArray();
-	}
-
-	private int convertValue(String string) {
-		if (!isValidValues(string)) {
+		if (text.isEmpty())
 			return 0;
-		}
-		if (!isNumber(string)) {
-			throw new IllegalArgumentException("숫자를 입력해주세요.");
-		}
-		return Integer.parseInt(string);
-	}
-
-	private boolean isNumber(String string) {
-		return string.matches("[+-]?\\d*(\\.\\d+)?");
-	}
-
-	private String[] getValues(String text) {
-		String delimiter = ",|:";
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-		if (m.find()) {
-			return m.group(2).split(String.format("%s|%s", delimiter, m.group(1)));
-		}
-		return text.split(delimiter);
-	}
-
-	private void checkNumbers(int[] numbers) {
-		for (int i : numbers) {
-			checkNegativeNumber(i);
-		}
-	}
-
-	private void checkNegativeNumber(int number) {
-		if (number < 0) {
-			throw new RuntimeException("양수를 입력해주세요.");
-		}
-	}
-
-	private int add(int[] numbers) {
-		if (numbers.length == 1) {
-			return numbers[0];
-		}
-		int result = 0;
-		for (int i : numbers) {
-			result += i;
-		}
-		return result;
+		return 1;
 	}
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 	Calculator calculator = new Calculator();
 
+	//1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다
 	@DisplayName("빈문자열, null 테스트")
 	@Test
 	public void splitAndSum_null_또는_빈문자() throws Exception {
@@ -16,54 +17,5 @@ public class CalculatorTest {
 
 		result = calculator.splitAndSum("");
 		assertThat(result).isEqualTo(0);
-	}
-
-	@DisplayName("숫자가 하나일 경우")
-	@Test
-	public void splitAndSum_숫자하나() throws Exception {
-		int result = calculator.splitAndSum("1");
-		assertThat(result).isEqualTo(1);
-	}
-
-	@DisplayName("쉼표 구분자 테스트")
-	@Test
-	public void splitAndSum_쉼표구분자() throws Exception {
-		int result = calculator.splitAndSum("1,2");
-		assertThat(result).isEqualTo(3);
-	}
-
-	@DisplayName("쉼표, 콜론 구분자 테스트")
-	@Test
-	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-		int result = calculator.splitAndSum("1,2:3");
-		assertThat(result).isEqualTo(6);
-	}
-
-	@DisplayName("중간에 공백 들어올 때 테스트")
-	@Test
-	public void splitAndSum_중간_공백_테스트() throws Exception {
-		int result = calculator.splitAndSum("1,:3");
-		assertThat(result).isEqualTo(4);
-	}
-
-	@DisplayName("사용자가 추가한 구분자 테스트")
-	@Test
-	public void splitAndSum_custom_구분자() throws Exception {
-		int result = calculator.splitAndSum("//;\n1;2;3");
-		assertThat(result).isEqualTo(6);
-	}
-
-	@DisplayName("음수일 경우 테스트")
-	@Test
-	public void splitAndSum_negative() throws Exception {
-		assertThatThrownBy(() -> calculator.splitAndSum("-1,2,3"))
-			.isInstanceOf(RuntimeException.class);
-	}
-
-	@DisplayName("값이 문자일 경우 테스트")
-	@Test
-	public void splitAndSum_string_value_test() throws Exception {
-		assertThatThrownBy(() -> calculator.splitAndSum("1,일곱,3"))
-			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
