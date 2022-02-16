5aa09674caa1df82d33063186d00c9a6dc8e99cd
양동주 <djwhy5510@gmail.com>
2022-02-16 18:08:04 +0900

    refactor: toString 대신 뷰 로직을 뷰가 하도록 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
 import racingcar.domain.WinnerNames;
 
 import java.util.List;
@@ -9,16 +10,25 @@ public class OutputView {
     private static final String RACING_RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_NAME_DELIMITER = ", ";
     private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
+    private static final String STATUS_DELIMITER = " : ";
+    private static final String POSITION_TO_STRING = "-";
 
     public static void printResultMessage() {
         System.out.println(NEW_LINE + RACING_RESULT_MESSAGE);
     }
 
-    public static void printRacingRecords(List<String> racingRecords) {
-        for (String racingRecord : racingRecords) {
-            System.out.println(racingRecord);
-        }
+    public static void printRacingRecords(List<Car> cars) {
+        cars.stream().forEach(car -> System.out.println(getCarRecord(car)));
         System.out.println();
+    }
+
+    private static String getCarRecord(Car car) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(car.getName()).append(STATUS_DELIMITER);
+        for (int i = 0; i < car.getPosition(); i++) {
+            stringBuilder.append(POSITION_TO_STRING);
+        }
+        return stringBuilder.toString();
     }
 
     public static void printWinnerNames(WinnerNames winnerNames) {
