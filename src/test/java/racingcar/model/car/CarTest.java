d369f3eae0ed3e876866851c16d62536aa5b8538
tonic523 <diddmlvkf@gmail.com>
2022-02-16 21:02:02 +0900

    refactor: 컨벤션 적용 및 불필요한 DisplayName 제거
    - 탭 -> 스페이스로 변경
    - DisplayName 제거

--- src/test/java/racingcar/model/car/CarTest.java
+++ src/test/java/racingcar/model/car/CarTest.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Car 클래스")
 public class CarTest {
-	@Nested
-	@DisplayName("move 메소드는")
-	class Move {
-		@Test
-		@DisplayName("1칸 이동할 수 있다.")
-		void it_increase_position() {
-			Car car = Car.of("car", 0);
-			car.move();
-			assertThat(car.toDto().getPosition()).isEqualTo(1);
-		}
-	}
+    @Nested
+    @DisplayName("move 메소드는")
+    class Move {
+        @Test
+        @DisplayName("1칸 이동할 수 있다.")
+        void it_increase_position() {
+            Car car = Car.of("car", 0);
+            car.move();
+            assertThat(car.toDto().getPosition()).isEqualTo(1);
+        }
+    }
 }
--- src/test/java/racingcar/model/car/PositionTest.java
+++ src/test/java/racingcar/model/car/PositionTest.java
@@ -10,50 +10,49 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("Position 클래스")
 public class PositionTest {
-	private Position position;
+    private Position position;
 
-	@BeforeEach
-	void setUp() {
-		position = Position.create();
-	}
+    @BeforeEach
+    void setUp() {
+        position = Position.create();
+    }
 
-	@Nested
-	@DisplayName("increase 메소드는")
-	class Increase {
-		@Test
-		@DisplayName("현재 위치에 1을 더한 Position을 리턴한다.")
-		void it_returns_Position_of_one_plus_value() {
-			assertThat(position.increase()).isEqualTo(Position.from(1));
-		}
-	}
+    @Nested
+    @DisplayName("increase 메소드는")
+    class Increase {
+        @Test
+        @DisplayName("현재 위치에 1을 더한 Position을 리턴한다.")
+        void it_returns_Position_of_one_plus_value() {
+            assertThat(position.increase()).isEqualTo(Position.from(1));
+        }
+    }
 
-	@Nested
-	@DisplayName("from 메소드는")
-	class From {
+    @Nested
+    @DisplayName("from 메소드는")
+    class From {
 
-		@Nested
-		@DisplayName("입력값이 음수가 아니라면")
-		class Context_with_not_negative_number {
-			@ParameterizedTest
-			@DisplayName("Position을 반환한다.")
-			@ValueSource(ints = {0, 1, 11})
-			void it_returns_Position(int input) {
-				assertDoesNotThrow(() -> Position.from(input));
-			}
-		}
+        @Nested
+        @DisplayName("입력값이 음수가 아니라면")
+        class Context_with_not_negative_number {
+            @ParameterizedTest
+            @DisplayName("Position을 반환한다.")
+            @ValueSource(ints = {0, 1, 11})
+            void it_returns_Position(int input) {
+                assertDoesNotThrow(() -> Position.from(input));
+            }
+        }
 
-		@Nested
-		@DisplayName("입력값이 음수라면")
-		class Context_with_negative_number {
-			@ParameterizedTest
-			@DisplayName("예외를 발생시킨다.")
-			@ValueSource(ints = {-1, -22})
-			void it_throw_exception(int input) {
-				assertThatThrownBy(() -> Position.from(input))
-					.isInstanceOf(IllegalArgumentException.class);
-			}
-		}
-	}
+        @Nested
+        @DisplayName("입력값이 음수라면")
+        class Context_with_negative_number {
+            @ParameterizedTest
+            @DisplayName("예외를 발생시킨다.")
+            @ValueSource(ints = {-1, -22})
+            void it_throw_exception(int input) {
+                assertThatThrownBy(() -> Position.from(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+    }
 }
