f330a48a6643b3b9f0dd0fcbdddab90fce38002f
Donggyu Kim <a29661498@gmail.com>
2022-02-16 16:54:26 +0900

    refactor: 자동차 이름 검증 Car 내부로 병합
    Cars 클래스와 통일성 있게 VerificationUtil 에 있던 자동차 이름 검증 코드를 Car 클래스 내부로 병합하였습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,11 @@
 package racingcar.domain;
 
-import static racingcar.util.VerificationUtil.validateCarName;
-
 public class Car {
 
     private static final int INITIAL_VALUE = 0;
     private static final int PROGRESS_CONDITION_VALUE = 4;
+    private static final int CAR_NAME_MIN_LENGTH = 1;
+    private static final int CAR_NAME_MAX_LENGTH = 5;
 
     private final String name;
     private int position = INITIAL_VALUE;
@@ -34,6 +34,31 @@ public class Car {
             return true;
         }
         return false;
+    }
+
+    public void validateCarName(String carName) {
+        if (carName == null) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 null 일 수 없습니다.");
+        }
+
+        validateCarNameLength(carName);
+        validateCarNameSpace(carName);
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() < CAR_NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1글자 이상이어야 합니다.");
+        }
+
+        if (carName.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5글자 이하이어야 합니다.");
+        }
+    }
+
+    private void validateCarNameSpace(String carName) {
+        if (carName.contains(" ")){
+            throw new IllegalArgumentException("[ERROR] 공백 없이 입력해주세요.");
+        }
     }
 }
 
