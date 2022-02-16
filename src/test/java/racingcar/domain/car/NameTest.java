58933308ed3e8991d4702d9c60f0d53451358219
Kim Seunglae <obey1342@gmail.com>
2022-02-16 13:12:18 +0900

    test(NameTest): 자동차 이름이 5자일 경우 정상적으로 생성되는 케이스 추가

--- src/test/java/racingcar/domain/car/NameTest.java
+++ src/test/java/racingcar/domain/car/NameTest.java
@@ -1,8 +1,8 @@
 package racingcar.domain.car;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,15 +32,23 @@ class NameTest {
     @DisplayName("Name이 정상적으로 생성")
     @Test
     void carNameWithRightFormat() {
-        Assertions.assertThat(new Name(SAMPLE_NAME)).isEqualTo(testName);
+        assertThat(new Name(SAMPLE_NAME)).isEqualTo(testName);
     }
 
-    @DisplayName("자동차 이름은 5글자를 초과할 경우 예외 발생")
+    @DisplayName("자동차 이름은 5자를 초과할 경우 예외 발생")
     @Test
     void carNameGreaterThanFive() {
         String input = "hellow";
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> new Name(input))
             .withMessageContaining("자동차 이름은 5글자 이하여야 합니다.");
+    }
+
+    @DisplayName("자동차 이름이 5자일 경우 정상적으로 생성")
+    @Test
+    void Should_SuccessToCar_When_NameIsFiveLength() {
+        String input = "seung";
+        Name name = new Name(input);
+        assertThat(name.getName()).isEqualTo(input);
     }
 }
