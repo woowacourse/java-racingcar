0d6784c73e39444af02aaed8b8e24ad917db3510
skullKim <kyk990328@gmail.com>
2022-02-16 22:06:24 +0900

    refactor(InputValidation.java): 클래스 필드 키워드 순서 변경
    컨벤션에 맞추기 위해 클래스 필드 키워드 순서 변경
    
    BREAKING CHANGE: final과 static 키워드 순서 변경
    as-is:
    public class InputValidation {
    
        private final static int MAX_NAME_LENGTH = 5;
        private final static String NUMBER_PATTERN = "^[0-9]*$";
        private final static Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
    
        ...
    }
    
    to-be:
    public class InputValidation {
    
        private static final int MAX_NAME_LENGTH = 5;
        private static final String NUMBER_PATTERN = "^[0-9]*$";
        private static final Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
    
        ...
    }

--- src/main/java/racingcar/utils/InputValidation.java
+++ src/main/java/racingcar/utils/InputValidation.java
@@ -4,9 +4,9 @@ import java.util.regex.Pattern;
 
 public class InputValidation {
 
-	private final static int MAX_NAME_LENGTH = 5;
-	private final static String NUMBER_PATTERN = "^[0-9]*$";
-	private final static Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
+	private static final int MAX_NAME_LENGTH = 5;
+	private static final String NUMBER_PATTERN = "^[0-9]*$";
+	private static final Pattern COMPILED_NUMBER_PATTERN = Pattern.compile(NUMBER_PATTERN);
 
 	public static void validateName(final String names) {
 		final String[] carNames = names.split(RacingGameMessage.CAR_NAME_DISTRIBUTOR);
