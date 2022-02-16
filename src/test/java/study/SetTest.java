bfd698db3c9f65fe11f773cfede9fad72df52623
her0807 <her0807@naver.com>
2022-02-16 20:34:23 +0900

    refactor: 불필요한 test 제거

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Set;
@@ -8,40 +7,33 @@ import java.util.HashSet;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
-    private Set<Integer> numbers;
+	private Set<Integer> numbers;
 
-    @BeforeEach
-    void setUp() {
-        numbers = new HashSet<>();
-        numbers.add(1);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-    }
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
 
-    @DisplayName("set 사이즈 확인 test")
-    @Test
-    void sizeValueTest() {
-        assertThat(numbers.size()).isEqualTo(3);
-    }
+	@DisplayName("해당 값이 포함되어 있는지 확인하는 테스트")
+	@ParameterizedTest
+	@ValueSource(strings = {"1", "2", "3"})
+	void contains(int input) {
+		assertTrue(numbers.contains(input));
+	}
 
-    @DisplayName("해당 값이 포함되어 있는지 확인하는 테스트")
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "2", "3"})
-    void contains(int input) {
-        assertTrue(numbers.contains(input));
-    }
-
-    @DisplayName("포함 여부를 확인하는 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void hasValue(int input, boolean expected) {
-        assertEquals(expected, numbers.contains(input));
-    }
+	@DisplayName("포함 여부를 확인하는 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void hasValue(int input, boolean expected) {
+		assertEquals(expected, numbers.contains(input));
+	}
 }
