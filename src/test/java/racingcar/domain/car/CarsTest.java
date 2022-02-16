90a6825b233d8b580f5fe1861e0332b180a92e9a
Kim Seunglae <obey1342@gmail.com>
2022-02-16 13:10:25 +0900

    test(CarsTest): 위치가 각각 다른 경우 우승자 찾는 테스트 코드 추가

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -24,7 +24,7 @@ class CarsTest {
     @Test
     void createCarsBySplitComma() {
         String input = "pobi,seung,char";
-        Cars cars = new Cars(input);
+        Cars cars = Cars.create(input);
         List<Car> carList = cars.getCarList();
         assertThat(carList.size()).isEqualTo(3);
         assertThat(carList.get(0)).isEqualTo(new Car("pobi"));
@@ -36,14 +36,14 @@ class CarsTest {
     void errorOnContinuousComma() {
         String input = ",,,";
         assertThatThrownBy(() -> {
-            new Cars(input);
+            Cars.create(input);
         }).isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("자동차 이름에 ,만 사용할 수 없습니다.");
     }
 
     @DisplayName("우승자가 1명일 경우 정상적인지 확인")
     @Test
-    void Should_SuccessToFindOneWinners() {
+    void Should_SuccessToFindOneWinner() {
         List<Car> sampleWinners = List.of(pobi);
         List<Car> carList = List.of(pobi, new Car("seung"));
         Cars cars = new Cars(carList);
@@ -55,6 +55,16 @@ class CarsTest {
     void Should_SuccessToFindTwoWinners() {
         List<Car> sampleWinners = sampleCarList;
         List<Car> carList = sampleCarList;
+        Cars cars = new Cars(carList);
+        assertThat(cars.findWinners()).isEqualTo(sampleWinners);
+    }
+
+    @DisplayName("위치가 각각 다른 경우 우승자 찾기 확인")
+    @Test
+    void Should_SuccessToFindWinners_When_DifferentPosition() {
+        Car winnerCar = new Car(new Name("char"), new Position(5));
+        List<Car> sampleWinners = List.of(winnerCar);
+        List<Car> carList = List.of(winnerCar, pobi);
         Cars cars = new Cars(carList);
         assertThat(cars.findWinners()).isEqualTo(sampleWinners);
     }
