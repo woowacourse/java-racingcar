3ab16349faf7e8d142123aa9a15b1c5bc43448c1
hamcheeseburger <yoo77hyeon@gmail.com>
2022-02-16 17:55:36 +0900

    refactor: RacingCar 자동차길이 validate 제거 및 테스트 코드 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,13 @@
 package racingcar.view;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-
-    private static final Scanner CONSOLE = new Scanner(System.in);
     private static final String CAR_NAME_DELIMITER = ",";
+    private static final Scanner CONSOLE = new Scanner(System.in);
 
     private InputView() {
     }
