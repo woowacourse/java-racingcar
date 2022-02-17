f1661315db757aa9457b2080e92df7640cdff577
koo <gudonghee2000@gmail.com>
2022-02-17 18:20:36 +0900

    refactor: OutputView클래스의 불필요한 라이브러리 제거, 상수명 변경

--- src/main/java/racingcargame/view/OutputView.java
+++ src/main/java/racingcargame/view/OutputView.java
@@ -1,22 +1,18 @@
 package racingcargame.view;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringJoiner;
 import java.util.stream.IntStream;
 import racingcargame.dto.CarDto;
 
 public class OutputView {
-    private static final String NOW_RACE_PROGRESS_GUIDE_MESSAGE = "실행 결과";
-    private static final String RACE_PROGRESS_OUTPUT_SEPARATOR = " : ";
+    private static final String RACE_RESULT_GUIDE_MESSAGE = "실행 결과";
+    private static final String RACING_CAR_NAME_SEPARATOR = " : ";
     private static final String GAME_WINNER_MESSAGE = "가 최종 우승했습니다.";
-    private static final String CAR_MOVE = "-";
-    private static final String CAR_NAME_SEPARATOR = ", ";
+    private static final String CAR_POSITION_SYMBOL = "-";
 
     public void showRaceResultMessage() {
-        System.out.println(NOW_RACE_PROGRESS_GUIDE_MESSAGE);
+        System.out.println(RACE_RESULT_GUIDE_MESSAGE);
     }
 
     public void showCurrentRaceStatus(List<CarDto> racingCarsInformation) {
@@ -28,18 +24,18 @@ public class OutputView {
     }
 
     private void showRacingCarName(String racingCarName) {
-        System.out.print(racingCarName + RACE_PROGRESS_OUTPUT_SEPARATOR);
+        System.out.print(racingCarName + RACING_CAR_NAME_SEPARATOR);
     }
 
     private void showRacingCarPosition(int racingCarPosition) {
-        IntStream.range(0, racingCarPosition).forEach(index -> System.out.print(CAR_MOVE));
+        IntStream.range(0, racingCarPosition).forEach(index -> System.out.print(CAR_POSITION_SYMBOL));
         newLine();
     }
 
     public void showWinners(List<CarDto> winners) {
         final StringJoiner winnerNames = new StringJoiner(", ");
         winners.forEach(winner -> winnerNames.add(winner.getName()));
-        System.out.println(winnerNames);
+        System.out.println(winnerNames + GAME_WINNER_MESSAGE);
     }
 
     private void newLine() {
