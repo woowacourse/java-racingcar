d677192e91da73e5a7e977178bb812a7e1e14359
bcc0830 <bcc0830@naver.com>
2022-02-17 11:23:43 +0900

    refactor : private 메소드 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,19 +9,19 @@ import racingcar.util.IntegerConst;
 import racingcar.util.StringConst;
 
 public class InputView {
-    private static void isNull(String stringInput) throws IllegalArgumentException {
+    private static void checkNull(String stringInput) throws IllegalArgumentException {
         if (stringInput == null) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_NULL.getValue());
         }
     }
 
-    private static void isBlank(String stringInput) throws IllegalArgumentException {
+    private static void checkBlank(String stringInput) throws IllegalArgumentException {
         if (stringInput.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_BLANK.getValue());
         }
     }
 
-    private static void isOverSize(String[] splitStringInput) throws IllegalArgumentException {
+    private static void checkOverSize(String[] splitStringInput) throws IllegalArgumentException {
         int overSizeCount = (int) Arrays.stream(splitStringInput)
             .filter(eachStringInput -> eachStringInput.length() > IntegerConst.SIZE_BOUND.getValue())
             .count();
@@ -30,14 +30,14 @@ public class InputView {
         }
     }
 
-    private static void isZeroCars(String[] splitStringInput) throws IllegalArgumentException {
+    private static void checkZeroCars(String[] splitStringInput) throws IllegalArgumentException {
         if (splitStringInput.length == IntegerConst.ZERO.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_NO_CAR.getValue());
         }
     }
 
-    private static void isSpecialChar(String[] splitStringInput) throws IllegalArgumentException {
-        int specialCount = (int)Arrays.stream(splitStringInput)
+    private static void checkSpecialChar(String[] splitStringInput) throws IllegalArgumentException {
+        int specialCount = (int) Arrays.stream(splitStringInput)
             .filter(eachStringInput -> !eachStringInput.matches(StringConst.REGEX_PATTERN.getValue()))
             .count();
         if (specialCount > IntegerConst.ZERO.getValue()) {
@@ -45,51 +45,51 @@ public class InputView {
         }
     }
 
-    private static void isDuplicate(String[] splitStringInput) throws IllegalArgumentException {
+    private static void checkDuplicate(String[] splitStringInput) throws IllegalArgumentException {
         HashSet<String> nameSet = new HashSet<>(Arrays.asList(splitStringInput));
         if (nameSet.size() != splitStringInput.length) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_DUPLICATE.getValue());
         }
     }
 
-    private static void isIntNum(String stringInput) throws IllegalArgumentException {
+    private static void checkIntNum(String stringInput) throws IllegalArgumentException {
         if (!stringInput.matches(StringConst.NUMBER_REGEX_PATTERN.getValue())) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_INT_NUM.getValue());
         }
     }
 
     private static void nullOrBlank(String stringInput) throws IllegalArgumentException {
-        isNull(stringInput);
-        isBlank(stringInput);
+        checkNull(stringInput);
+        checkBlank(stringInput);
     }
 
     public static void checkNameError(String nameInput) throws IllegalArgumentException {
         nullOrBlank(nameInput);
         String[] commaSeparateName = nameInput.split(StringConst.DELIMITER.getValue());
-        isZeroCars(commaSeparateName);
-        isSpecialChar(commaSeparateName);
-        isOverSize(commaSeparateName);
-        isDuplicate(commaSeparateName);
+        checkZeroCars(commaSeparateName);
+        checkSpecialChar(commaSeparateName);
+        checkOverSize(commaSeparateName);
+        checkDuplicate(commaSeparateName);
     }
 
     public static void checkTurnError(String turnInput) throws IllegalArgumentException {
         nullOrBlank(turnInput);
-        isIntNum(turnInput);
+        checkIntNum(turnInput);
     }
 
-	public static String[] getCarNameInput() throws IllegalArgumentException {
-		Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
-		checkNameError(name);
-		return name.split(StringConst.DELIMITER.getValue());
-	}
+    public static String[] getCarNameInput() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        checkNameError(name);
+        return name.split(StringConst.DELIMITER.getValue());
+    }
 
 
-	public static int getTurnInput() throws IllegalArgumentException {
-		Scanner scanner = new Scanner(System.in);
-		String turn = scanner.nextLine();
-		checkTurnError(turn);
-		return Integer.parseInt(turn);
-	}
+    public static int getTurnInput() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+        String turn = scanner.nextLine();
+        checkTurnError(turn);
+        return Integer.parseInt(turn);
+    }
 
 }
