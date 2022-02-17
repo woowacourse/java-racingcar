a297de18633cbe801e997457671ab6ce3781022f
koo <gudonghee2000@gmail.com>
2022-02-18 02:55:58 +0900

    refactor: Car클래스의 자동차 이름 검증 기능을 Name클래스로 이동

--- src/main/java/racingcargame/model/car/Car.java
+++ src/main/java/racingcargame/model/car/Car.java
@@ -4,11 +4,6 @@ import racingcargame.dto.CarDto;
 
 public class Car implements Comparable<Car> {
     private static final int MIN_MOVABLE_NUMBER = 4;
-    private static final int MAX_NAME_LENGTH_COUNT = 5;
-    private static final int MIN_NAME_LENGTH_COUNT = 1;
-    private static final String EMPTY_NAME = "";
-    private static final String EMPTY_NAME_ERROR_MESSAGE = "[error] 입력된 자동차 이름중 이름이 없는 자동차가 있습니다.";
-    private static final String NO_VALIDATE_NAME_LENGTH_ERROR_MESSAGE = "[error] 자동차 이름을 5자 이하로 입력해주세요.";
 
     private final Name name;
     private int position;
@@ -16,23 +11,6 @@ public class Car implements Comparable<Car> {
     public Car(final String name) {
         this.name = new Name(name);
         this.position = 0;
-    }
-
-    private void checkValidateName(final String name) {
-        checkNameExists(name);
-        checkValidNameLength(name);
-    }
-
-    private void checkNameExists(final String name) {
-        if (EMPTY_NAME.equals(name)) {
-            throw new IllegalArgumentException(EMPTY_NAME_ERROR_MESSAGE);
-        }
-    }
-
-    private void checkValidNameLength(final String name) {
-        if (name.length() < MIN_NAME_LENGTH_COUNT || name.length() > MAX_NAME_LENGTH_COUNT) {
-            throw new IllegalArgumentException(NO_VALIDATE_NAME_LENGTH_ERROR_MESSAGE);
-        }
     }
 
     public void moveCar(final int number) {
