ed4b3337cf1af54cda627fa33b672059db53492e
nailseong <nnis9857@gmail.com>
2022-02-17 19:33:59 +0900

    test(Cars) : 이름 중복, moveCarsForward 메서드의 테스트 케이스 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,8 +1,11 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
@@ -20,5 +23,25 @@ public class CarsTest {
                 () -> Cars.withNames(List.of("rick", "huni", "sudal", "ukong", "tomy", "pobi")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차는 2대 이상 5대 이하이어야 합니다.");
+    }
+
+    @DisplayName("자동차 이름 중복")
+    @Test
+    void carNamesDuplicated() {
+        assertThatThrownBy(() -> Cars.withNames(List.of("rick", "rick", "huni")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름입니다.");
+    }
+
+    @DisplayName("자동차들을 모두 이동시킴")
+    @Test
+    void carsMoveForward() {
+        Cars cars = Cars.withNames(List.of("rick", "huni", "pobi"));
+
+        cars.moveCarsForward(() -> true);
+
+        List<Integer> positions = cars.getCars().stream().map(Car::getPosition)
+                .collect(Collectors.toList());
+        assertThat(positions).containsExactly(1, 1, 1);
     }
 }
