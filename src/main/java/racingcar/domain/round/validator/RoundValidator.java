52d6743411e4025d41ab8c0ee796fdd69845ede7
sinb57 <sinb57@naver.com>
2022-02-18 02:44:39 +0900

    refactor: RoundCount 클래스, enum 클래스로 변경

--- src/main/java/racingcar/domain/round/validator/RoundValidator.java
+++ src/main/java/racingcar/domain/round/validator/RoundValidator.java
@@ -1,6 +1,6 @@
 package racingcar.domain.round.validator;
 
-import racingcar.domain.round.condition.RoundCountRange;
+import racingcar.domain.round.condition.RoundCount;
 import racingcar.exception.WrongArgumentException;
 import racingcar.exception.status.round.RoundCountExceptionStatus;
 
@@ -16,7 +16,7 @@ public class RoundValidator {
     }
 
     private void validateCountPositive(final int count) {
-        if (RoundCountRange.isTooSmall(count)) {
+        if (RoundCount.isTooSmall(count)) {
             throw new WrongArgumentException(RoundCountExceptionStatus.ROUND_IS_NOT_POSITIVE);
         }
     }
