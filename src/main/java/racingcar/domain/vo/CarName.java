4295cbf5a3ad2e1f537e2f1fd055bf3a49086aff
swcho <progress0407@gmail.com>
2022-02-17 12:39:57 +0900

    refactor(CarName): 정규표현식(Pattern.compile)을 한번 컴파일 하여 캐싱

--- src/main/java/racingcar/domain/vo/CarName.java
+++ src/main/java/racingcar/domain/vo/CarName.java
@@ -1,14 +1,17 @@
 package racingcar.domain.vo;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class CarName {
 
 	public static final int CAR_NAME_MAX_LENGTH = 5;
-	public static final String ALLOWED_CHARACTERS = ".*[^\\w\\dㄱ-ㅎ가-힣]+.*";
+	public static final String ALLOWED_CHARACTERS = "\\w\\dㄱ-ㅎ가-힣";
+	public static final String NOT_ALLOWED_REGEXP_STRING = ".*[^" + ALLOWED_CHARACTERS + "]+.*";
 	public static final String EXCEED_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 최대 5자입니다.";
 	public static final String NOT_ALLOWED_FORMAT_MESSAGE = "[ERROR] 올바르지 않은 입력 형식입니다.";
 	public static final String INVALID_CAR_NAME_SHOULD_BE_ONE_MORE_CHARACTER = "[ERROR] 자동차 이름은 최대 한 글자 이상입니다";
+	public static final Pattern COMPILED_CAR_NAME_PATTERN = Pattern.compile(NOT_ALLOWED_REGEXP_STRING);
 
 	private final String name;
 
@@ -37,9 +40,14 @@ public class CarName {
 	}
 
 	private static void hasCarNameWrongFormat(final String carName) {
-		if (carName.matches(ALLOWED_CHARACTERS)) {
+		// if (carName.matches(NOT_ALLOWED_REGEXP_STRING)) {
+		if (doesNotMatchCarName(carName)) {
 			throw new RuntimeException(NOT_ALLOWED_FORMAT_MESSAGE);
 		}
+	}
+
+	private static boolean doesNotMatchCarName(String carName) {
+		return COMPILED_CAR_NAME_PATTERN.matcher(carName).matches();
 	}
 
 	private static void isCarNameExceed(final String carName) {
