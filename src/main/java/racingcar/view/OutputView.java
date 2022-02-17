4bb7cf48130c309c9a5164bbbb70cb0113f49ed6
devHudi <devhudi@gmail.com>
2022-02-17 11:58:39 +0900

    fix: 공동 우승자 출력에 구분자가 제대로 출력되지 않는 버그 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final String NAME_DELIMITER = ",";
+    private static final String NAME_DELIMITER = ", ";
 
     public static void printInputNamesUi() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -27,9 +27,9 @@ public class OutputView {
     }
 
     public static void printRaceResultUi(List<Name> names) {
-        System.out.printf("%s가 최종 우승했습니다.", String.join(NAME_DELIMITER, names.stream()
+        System.out.printf("%s가 최종 우승했습니다.", names.stream()
                 .map(Name::toString)
-                .collect(Collectors.joining()))
+                .collect(Collectors.joining(NAME_DELIMITER))
         );
     }
 
