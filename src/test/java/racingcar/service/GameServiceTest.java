ba411cc897a355cb3fa6ecd06cbd6edb40222dc9
sinb57 <sinb57@naver.com>
2022-02-18 04:10:10 +0900

    test: GameService 클래스, 실행 횟수 초기화 기능 테스트

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -8,7 +8,10 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import racingcar.dto.CarStatusDto;
+import racingcar.dto.RoundDto;
 
 class GameServiceTest {
 
@@ -42,6 +45,15 @@ class GameServiceTest {
                 .collect(Collectors.toUnmodifiableList());
 
         carLocations.forEach(location -> assertThat(location).isEqualTo(CAR_INITIALIZED_LOCATION));
+    }
+
+    @DisplayName("실행 횟수 초기화 기능 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void initRoundTest(final int roundCount) {
+        gameService.initRound(roundCount);
+        final RoundDto roundDto = gameService.getCurrentRound();
+        assertThat(roundDto.getCount()).isEqualTo(roundCount);
     }
 
 }
