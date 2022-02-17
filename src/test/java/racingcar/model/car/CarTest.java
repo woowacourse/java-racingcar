0e6d8ecc75c5b82e3e1423c91ac5240edc38f751
tonic523 <diddmlvkf@gmail.com>
2022-02-17 22:03:05 +0900

    refactor: Car 테스트 코드 수정
    - 임의의 값을 주던 RacingCarMovableStrategy()에서 항상 true값이 주어지도록 수정

--- src/test/java/racingcar/model/car/CarTest.java
+++ src/test/java/racingcar/model/car/CarTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import racingcar.util.RacingCarMovableStrategy;
-
 public class CarTest {
     @Nested
     @DisplayName("move 메소드는")
@@ -16,7 +14,7 @@ public class CarTest {
         @DisplayName("1칸 이동할 수 있다.")
         void it_increase_position() {
             Car car = new Car(new Name("car1"));
-            car.move(new RacingCarMovableStrategy());
+            car.move(() -> true);
             assertThat(car.toDto().getPosition()).isEqualTo(1);
         }
     }
