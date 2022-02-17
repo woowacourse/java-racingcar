dd7a8b7d07a8f2335f9ab06cc767de748ae5dac1
hyunrrr <whgusrms96@gmail.com>
2022-02-17 12:16:32 +0900

    refactor: 매직넘버 상수로 포장

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class RacingGame {
 
+    private static final int MINIMUM_TRIAL_NUMBER = 1;
     private static final String EXCEPTION_TRIAL_NUMBER = "[ERROR] 시도횟수는 1이상의 자연수여야 합니다.\n";
     private RacingCars racingCars;
     private int trialCount;
@@ -24,7 +25,7 @@ public class RacingGame {
     }
 
     private void checkTrialCountIsNaturalNumber() {
-        if (trialCount < 1) {
+        if (trialCount < MINIMUM_TRIAL_NUMBER) {
             throw new IllegalArgumentException(EXCEPTION_TRIAL_NUMBER);
         }
     }
