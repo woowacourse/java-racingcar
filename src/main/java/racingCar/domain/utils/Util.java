17c1c968ef5e777179b096d4baa21ceb3964ac10
BETTERFUTURE4 <rerub0831@gmail.com>
2022-02-17 17:33:47 +0900

    refactor: random 인스턴스를 상수로 이동

--- src/main/java/racingCar/domain/utils/Util.java
+++ src/main/java/racingCar/domain/utils/Util.java
@@ -1,7 +1,5 @@
 package racingCar.domain.utils;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
 import racingCar.domain.exception.count.CountNotNumberException;
@@ -10,9 +8,10 @@ public class Util {
     private static final String NAME_REGEX = ",";
     private static final int RANDOM_MAX_BOUND_NUM = 10;
     private static final String NUM_REGEX = "[0-9]+";
+    private static final Random RANDOM = new Random();
 
     public static int getRandomInteger() {
-        return new Random().nextInt(RANDOM_MAX_BOUND_NUM);
+        return RANDOM.nextInt(RANDOM_MAX_BOUND_NUM);
     }
 
     public static String[] separate(String text) {
