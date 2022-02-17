df80963e10443506fae2f4dfd8605472f05b3089
koo <gudonghee2000@gmail.com>
2022-02-17 18:06:27 +0900

    refactor: RandomNumberGenerator 클래스의 static을 제거하고 숫자생성 함수 구조 변경

--- src/main/java/racingcargame/utils/RandomNumberGenerator.java
+++ src/main/java/racingcargame/utils/RandomNumberGenerator.java
@@ -1,24 +1,11 @@
 package racingcargame.utils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Random;
 
 public class RandomNumberGenerator {
-    private static final int MAX_NUMBER = 10;
-    private static final int MIN_NUMBER = 0;
-    private static final int SHUFFLED_INDEX = 0;
+    private final Random random = new Random();
 
-    private static final List<Integer> numbers = IntStream.range(MIN_NUMBER, MAX_NUMBER)
-            .boxed()
-            .collect(Collectors.toList());
-
-    private RandomNumberGenerator() {
-    }
-
-    public static int pickRandomNumber() {
-        Collections.shuffle(numbers);
-        return numbers.get(SHUFFLED_INDEX);
+    public int generateNumber(final int boundaryNumber) {
+        return random.nextInt(boundaryNumber);
     }
 }
