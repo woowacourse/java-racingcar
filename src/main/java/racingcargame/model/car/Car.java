14027b2e2501eff9a8e52e34efade660894b7a67
koo <gudonghee2000@gmail.com>
2022-02-18 02:57:45 +0900

    feat: 자동차 위치를 담는 Position클래스 생성

--- src/main/java/racingcargame/model/car/Car.java
+++ src/main/java/racingcargame/model/car/Car.java
@@ -6,11 +6,11 @@ public class Car implements Comparable<Car> {
     private static final int MIN_MOVABLE_NUMBER = 4;
 
     private final Name name;
-    private int position;
+    private Position position;
 
     public Car(final String name) {
         this.name = new Name(name);
-        this.position = 0;
+        this.position = new Position();
     }
 
     public void moveCar(final int number) {
---
+++ src/main/java/racingcargame/model/car/Position.java
@@ -0,0 +1,9 @@
+package racingcargame.model.car;
+
+public class Position {
+    private int position;
+
+    public Position() {
+        this.position = 0;
+    }
+}
