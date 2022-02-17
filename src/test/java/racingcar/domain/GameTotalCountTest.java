ec54d6e7ca4b7d6d42efe321baaa11db820ad03a
Donggyu Kim <a29661498@gmail.com>
2022-02-17 17:02:38 +0900

    fix: 누락된 @SuppressWarnings 추가
    누락된 @SuppressWarnings("NonAsciiCharacters") 어노테이션을 추가해주었습니다.

--- src/test/java/racingcar/domain/GameTotalCountTest.java
+++ src/test/java/racingcar/domain/GameTotalCountTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("NonAsciiCharacters")
 class GameTotalCountTest {
 
     @Test
