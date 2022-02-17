8af5cececcc968c9dbb6e3a9e0ba0022932da87f
yukong <kyb062666@gmail.com>
2022-02-17 18:48:14 +0900

    refactor(Car): 자동차 이름, 위치를 입력받는 생성자 추가
    - Cars 테스트에서 사용하기 위해 추가
    - 위치를 입력받는 과정이 추가됨에 따라 테스트 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -8,8 +8,15 @@ public class Car implements Comparable<Car> {
 	private int position = 0;
 
 	public Car(String name) {
-		checkValid(name);
+		checkValidName(name);
 		this.name = name;
+	}
+
+	public Car(String name, int position) {
+		checkValidName(name);
+		checkValidPosition(position);
+		this.name = name;
+		this.position = position;
 	}
 
 	public void drive(boolean directing) {
@@ -39,7 +46,7 @@ public class Car implements Comparable<Car> {
 		return this.position - car.position;
 	}
 
-	private void checkValid(String names) {
+	private void checkValidName(String names) {
 		checkBlank(names);
 		checkNameLength(names);
 	}
@@ -57,6 +64,12 @@ public class Car implements Comparable<Car> {
 	private void checkNameLength(String name) {
 		if (!(name.trim().length() <= CAR_LENGTH_LIMIT)) {
 			throw new IllegalArgumentException("자동차의 이름은 " + CAR_LENGTH_LIMIT + "글자를 초과할 수 없습니다.");
+		}
+	}
+
+	private void checkValidPosition(int position) {
+		if (position < 0) {
+			throw new IllegalArgumentException("자동차 위치는 자연수를 입력해주세요.");
 		}
 	}
 
