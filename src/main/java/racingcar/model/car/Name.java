da287fe95ff94ba2d6b65263c97fc5ddabfc63f0
tonic523 <diddmlvkf@gmail.com>
2022-02-17 22:03:53 +0900

    refactor: StringValidator의 메소드를 Name으로 이동

--- src/main/java/racingcar/model/car/Name.java
+++ src/main/java/racingcar/model/car/Name.java
@@ -2,19 +2,38 @@ package racingcar.model.car;
 
 import java.util.Objects;
 
-import racingcar.util.StringValidator;
-
 public class Name {
+    private static final String FORMAT_PATTERN = "[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]+";
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
 
     private final String name;
 
     public Name(String name) {
-        StringValidator.validateIsEmpty(name);
-        StringValidator.validateLength(name, MIN_LENGTH, MAX_LENGTH);
-        StringValidator.validateFormat(name);
+        validateIsEmpty(name);
+        validateLength(name, MIN_LENGTH, MAX_LENGTH);
+        validateFormat(name);
         this.name = name;
+    }
+
+    private static void validateIsEmpty(String text) {
+        if (text.isEmpty()) {
+            throw new IllegalArgumentException("문자열은 빈값이면 안됩니다.");
+        }
+    }
+
+    private static void validateLength(String text, int min, int max) {
+        if (text.length() < min || text.length() > max) {
+            throw new IllegalArgumentException(
+                String.format("문자열의 길이가 %d 이상 %d 이하가 아닙니다.", min, max)
+            );
+        }
+    }
+
+    private static void validateFormat(String text) {
+        if (!text.matches(FORMAT_PATTERN)) {
+            throw new IllegalArgumentException("올바르지 않은 형식의 문자열입니다.");
+        }
     }
 
     @Override
