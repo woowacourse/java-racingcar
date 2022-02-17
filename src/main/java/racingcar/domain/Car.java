42d69c427cbcf38edf10bee27dc640a6e196c300
summerlunaa <summerlunaa9@gmail.com>
2022-02-17 12:17:37 +0900

    refactor: 자동차의 위치 출력을 위한 문자열 구성 로직을 Car에서 OutputView로 이동

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,9 +3,6 @@ package racingcar.domain;
 import racingcar.validator.CarNameValidator;
 
 public class Car {
-	private static final String STRING_JOIN_DELIMITER = " : ";
-	private static final String MOVE_EXPRESSION = "-";
-
 	private final String name;
 	private int position = 0;
 
@@ -28,10 +25,5 @@ public class Car {
 
 	public boolean isSamePosition(int position) {
 		return this.position == position;
-	}
-
-	@Override
-	public String toString() {
-		return this.name + STRING_JOIN_DELIMITER + MOVE_EXPRESSION.repeat(Math.max(0, this.position));
 	}
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,12 +6,18 @@ import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class OutputView {
+	private static final String STRING_JOIN_DELIMITER = " : ";
+	private static final String MOVE_EXPRESSION = "-";
 	public static final String RESULT_MESSAGE = "실행 결과";
 	public static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
 	public static final String WINNER_JOIN_DELIMITER = ", ";
 
-	public static void printCarPosition(List<String> cars) {
-		cars.forEach(System.out::println);
+	public static void printCarPosition(List<Car> cars) {
+		cars.stream()
+			.map(car -> car.getName()
+				+ STRING_JOIN_DELIMITER
+				+ MOVE_EXPRESSION.repeat(Math.max(0, car.getPosition())))
+			.forEach(System.out::println);
 		System.out.println();
 	}
 
