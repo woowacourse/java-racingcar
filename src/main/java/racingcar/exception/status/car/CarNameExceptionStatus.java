28fc0dd7ca083d2be4f8c7264e2f4b4abc10e532
sinb57 <sinb57@naver.com>
2022-02-18 02:28:27 +0900

    refactor: NameLength 클래스, Enum 클래스로 수정

--- src/main/java/racingcar/exception/status/car/CarNameExceptionStatus.java
+++ src/main/java/racingcar/exception/status/car/CarNameExceptionStatus.java
@@ -9,7 +9,7 @@ public enum CarNameExceptionStatus implements ExceptionStatus {
     NAME_IS_EMPTY("자동차의 이름은 공백이 될 수 없습니다."),
     NAME_IS_NULL("자동차의 이름은 NULL이 될 수 없습니다."),
     NAME_IS_TOO_LONG(
-            String.format("자동차의 이름은 %d자 이하여야 합니다.", NameLength.ALLOWED_MAXIMUM_LENGTH));
+            String.format("자동차의 이름은 %d자 이하여야 합니다.", NameLength.ALLOWED_MAXIMUM_LENGTH.getLength()));
 
     private final String message;
 
