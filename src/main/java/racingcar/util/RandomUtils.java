154e848287696097fe0111bc60840b3736f29377
yeon-06 <rnjstldus2@hs.ac.kr>
2022-02-18 00:07:14 +0900

    refactor: utils 에서는 생성자 제한

--- src/main/java/racingcar/util/RandomUtils.java
+++ src/main/java/racingcar/util/RandomUtils.java
@@ -3,6 +3,9 @@ package racingcar.util;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
+    private RandomUtils() {
+    }
+
     public static int generateNumber(int min, int max) {
         if (min > max) {
             int temp = min;
