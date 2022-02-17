8e6d22638b51d2c718c0afc4720bdc37c6a15599
sinb57 <sinb57@naver.com>
2022-02-18 02:41:31 +0900

    refactor: OutputMessage 클래스, enum 클래스로 변경

--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -3,7 +3,6 @@ package racingcar.view.output;
 import java.util.List;
 
 import racingcar.utils.Delimiter;
-import racingcar.view.output.message.OutputMessage;
 
 public class OutputView {
 
@@ -11,17 +10,21 @@ public class OutputView {
         System.out.println(message);
     }
 
+    private void printMessage(final OutputMessage outputMessage) {
+        this.printMessage(outputMessage.getMessage());
+    }
+
     public void printMessageOfRequestCarNames() {
-        printMessage(OutputMessage.MESSAGE_OF_REQUEST_CAR_NAMES);
+        printMessage(OutputMessage.REQUEST_CAR_NAMES_MESSAGE);
     }
 
     public void printMessageOfRequestRound() {
-        printMessage(OutputMessage.MESSAGE_OF_REQUEST_ROUND);
+        printMessage(OutputMessage.REQUEST_ROUND_MESSAGE);
     }
 
     public void printMessageOfStatusTitle() {
         printEmptyLine();
-        printMessage(OutputMessage.MESSAGE_OF_STATUS_TITLE);
+        printMessage(OutputMessage.PRINT_TITLE_OF_STATUS);
     }
 
     public void printCarStatuses(final List<String> carStatuses) {
@@ -31,7 +34,7 @@ public class OutputView {
 
     public void printMessageOfWinners(final List<String> winnerNames) {
         final String message = String.format(
-                OutputMessage.MESSAGE_FORMAT_OF_PRINT_WINNERS, Delimiter.joinWithComma(winnerNames));
+                OutputMessage.PRINT_WINNERS.getMessage(), Delimiter.joinWithComma(winnerNames));
         printMessage(message);
     }
 
