5ad84427546588b71309c26561859809103c1a1b
meatsby <meatsby@gmail.com>
2022-02-18 18:10:14 +0900

    test: Cars 테스트 추가
    - 3명 중 한 명의 우승자
    - 전체공동우승
    - 1명의 참가자, 1명의 우승자

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -7,6 +7,18 @@ import org.junit.jupiter.api.Test;
 class CarsTest {
 
     @Test
+    void getSingleChampion() {
+        Cars cars = new Cars(new String[] {"car1", "car2", "car3"});
+
+        cars.getCars().get(0).move(() -> true);
+        cars.getCars().get(1).move(() -> false);
+        cars.getCars().get(2).move(() -> false);
+
+        assertThat(cars.getChampions())
+            .containsOnly("car1");
+    }
+
+    @Test
     void getChampions() {
         Cars cars = new Cars(new String[] {"car1", "car2", "car3"});
 
@@ -18,5 +30,29 @@ class CarsTest {
             .contains("car1")
             .contains("car2")
             .doesNotContain("car3");
+    }
+
+    @Test
+    void getChampionsWithAllHavingHighScore() {
+        Cars cars = new Cars(new String[] {"car1", "car2", "car3"});
+
+        cars.getCars().get(0).move(() -> true);
+        cars.getCars().get(1).move(() -> true);
+        cars.getCars().get(2).move(() -> true);
+
+        assertThat(cars.getChampions())
+            .contains("car1")
+            .contains("car2")
+            .contains("car3");
+    }
+
+    @Test
+    void getChampionWithSingleParticipant() {
+        Cars cars = new Cars(new String[] {"car1"});
+
+        cars.getCars().get(0).move(() -> true);
+
+        assertThat(cars.getChampions())
+            .containsOnly("car1");
     }
 }
