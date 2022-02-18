ee5422d5b39f3db71df1c65949f53e8e5bcbfb83
sure-why-not <pkse1213@gmail.com>
2022-02-18 17:37:32 +0900

    refactor : RacingResult.results 자료형을 Map 인터페이스로 수정

--- src/main/java/racingcar/domain/RacingResult.java
+++ src/main/java/racingcar/domain/RacingResult.java
@@ -3,12 +3,13 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RacingResult {
 
 	private static final int ADDITIONAL_NUMBER = 1;
 
-	private final HashMap<Round, List<Car>> results = new HashMap<>();
+	private final Map<Round, List<Car>> results = new HashMap<>();
 	private AttemptNumber attemptNumber = AttemptNumber.fromInitValue();
 
 	public void add(List<Car> cars)  {
