f39faa35dd6f7ff1d68f36192493a917779f6aae
Wishoon <heonga2@gmail.com>
2022-02-18 18:53:59 +0900

    fix: Name 객체에 포함된 상수들을 영문법을 명확하게, 너무 구체적인 이름은 추상화하여 수정

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -3,9 +3,9 @@ package racingcar.domain;
 import java.util.Objects;
 
 public class Name {
-    private static final int MINIMUM_NUMBER_LENGTH = 1;
-    private static final int MAXIMUM_NUMBER_LENGTH = 5;
-    private static final String NAME_CONTAINS_BLANK = " ";
+    private static final int MINIMUM_LENGTH = 1;
+    private static final int MAXIMUM_LENGTH = 5;
+    private static final String BLANK = " ";
 
     private final String name;
 
@@ -16,13 +16,13 @@ public class Name {
     }
 
     private void isValidateLength(final String name) {
-        if (MINIMUM_NUMBER_LENGTH > name.length() || name.length() > MAXIMUM_NUMBER_LENGTH) {
+        if (MINIMUM_LENGTH > name.length() || name.length() > MAXIMUM_LENGTH) {
             throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
         }
     }
 
     private void isValidateContainBlank(final String name) {
-        if (name.contains(NAME_CONTAINS_BLANK)) {
+        if (name.contains(BLANK)) {
             throw new IllegalArgumentException("이름에는 공백이 포함될 수 없습니다.");
         }
     }
