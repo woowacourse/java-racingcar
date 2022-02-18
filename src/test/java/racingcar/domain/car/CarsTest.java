6b3ffa9e86a744540d648e86e52d7a9d6787b857
sinb57 <sinb57@naver.com>
2022-02-18 13:21:12 +0900

    test: CarsTest 클래스, 자동차 이름 NULL 예외 테스트 제거

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -27,13 +27,6 @@ class CarsTest {
         assertThrows(WrongArgumentException.class, () -> new Cars(carNames, TRUE_MOVE_STRATEGY));
     }
 
-    @DisplayName("자동차 이름은 NULL이 될 수 없다")
-    @ParameterizedTest
-    @MethodSource(PROVIDER_PATH + "provideForNullExceptionTest")
-    void carNameNullExceptionTest(final List<String> names) {
-        exceptionTest(names);
-    }
-
     @DisplayName("자동차 이름은 공백이 될 수 없다")
     @ParameterizedTest
     @MethodSource(PROVIDER_PATH + "provideForEmptyExceptionTest")
