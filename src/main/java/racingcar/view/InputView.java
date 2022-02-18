ac85116ab981b3b919d056e507f9da2cc7c0d2b4
yxxnghwan <younghwan960@gmail.com>
2022-02-18 20:32:28 +0900

    style(all): 공백 정리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,12 +2,11 @@ package racingcar.view;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import racingcar.utils.validator.CarNameValidator;
 import racingcar.utils.validator.RoundNumberValidator;
 
-public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+public class CarInputView implements CarInputView<T, R> {
+
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String ROUND_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String NAME_DELIMITER = ",";
