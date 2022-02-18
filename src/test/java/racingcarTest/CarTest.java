e719f0cc7c4a065467f761f20057e75eec4339ac
hyewoncc <hyewoncc@gmail.com>
2022-02-18 18:51:15 +0900

    refactor: CsvSource를 이용하도록 테스트 변경

--- src/test/java/racingcarTest/CarTest.java
+++ src/test/java/racingcarTest/CarTest.java
@@ -2,36 +2,19 @@ package racingcarTest;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.domain.Car;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-
-    private static final int INT_OVER_RANDOM_CONDITION = 5;
-    private static final int INT_RANDOM_CONDITION = 4;
-    private static final int INT_UNDER_RANDOM_CONDITION = 3;
-    private static final int CAR_MOVED = 1;
-    private static final int CAR_DIDNT_MOVE = 0;
-
-    private static Stream<Arguments> randomConditionArguments() {
-        return Stream.of(
-            Arguments.of(INT_OVER_RANDOM_CONDITION, CAR_MOVED),
-            Arguments.of(INT_RANDOM_CONDITION, CAR_MOVED),
-            Arguments.of(INT_UNDER_RANDOM_CONDITION, CAR_DIDNT_MOVE));
-    };
-
     @ParameterizedTest
-    @MethodSource("randomConditionArguments")
-    @DisplayName("랜덤값에_따라_전진_실행")
-    void randomNum_is4OrOver_carGoForward(int random, int expectedCarPosition) {
+    @CsvSource(value = {"5,1", "4,1", "3,0"}, delimiter = ',')
+    @DisplayName("4를_넘는_값에_전진_미만은_정지")
+    void num_Over4_carGoForward(int random, int expectedPosition) {
         Car testCar = new Car("test");
 
         testCar.goForward(random);
-        assertThat(testCar.getPosition()).isEqualTo(expectedCarPosition);
+        assertThat(testCar.getPosition()).isEqualTo(expectedPosition);
     }
 }
