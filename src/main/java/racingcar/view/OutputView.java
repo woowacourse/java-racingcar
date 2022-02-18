1a0257ffff6935cfc1edac0050d474d99903f9df
hyunrrr <whgusrms96@gmail.com>
2022-02-19 00:23:47 +0900

    refactor: 도메인에 있었던 출력 로직 뷰에 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,23 +1,34 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
     private static final String WINNER_NAME_DELIMITER = ", ";
+    private static final String NAME_POSITION_CONNECTOR = " : ";
+    private static final String POSITION_MARKER = "-";
     private static final String TRIAL_RESULT = "실행 결과:";
     private static final String WINNER_ANNOUNCEMENT = "가 최종 우승했습니다.";
     public static void printGameStartMessage(){
         System.out.println(TRIAL_RESULT);
     }
 
-    public static void printCurrentRaceState(List<String> currentRaceState){
-        for (String oneCarState : currentRaceState) {
-            System.out.println(oneCarState);
-        }
+    public void printCurrentRaceState(Map<String, Integer> carNamesAndPositions){
+        carNamesAndPositions.keySet()
+            .stream()
+            .forEach(s -> raceStateFormat(s, carNamesAndPositions.get(s)));
+    }
+    private void raceStateFormat(String name, int position) {
+        System.out.println(name + NAME_POSITION_CONNECTOR + visualizePosition(position));
     }
 
-    public static void printWinners(List<String> winnerNames){
+    private String visualizePosition(int position) {
+        return NAME_POSITION_CONNECTOR.repeat(position);
+    }
+
+
+    public void printWinners(List<String> winnerNames){
         System.out.println(String.join(WINNER_NAME_DELIMITER, winnerNames) + WINNER_ANNOUNCEMENT);
     }
 }
