08a60c1ed76669855878955c05667ebb66925de1
= <ulim130@naver.com>
2022-02-18 19:27:44 +0900

    refactor : 누락된 접근제어자 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -13,13 +13,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingGameTest {
 
-    RacingGame racingGame;
+    private RacingGame racingGame;
 
     @BeforeEach
     void initRacingGame() {
         racingGame = new RacingGame();
     }
-
 
     @Test
     @DisplayName("시도 횟수에 정수를 입력하지 않는 경우 예외 발생")
