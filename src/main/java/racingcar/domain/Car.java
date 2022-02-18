315a3f8a3e683b49644e192a50958bdb907df6ad
soominsohn <soominsohn@gmail.com>
2022-02-18 16:40:36 +0900

    style(car): change position of method and variable

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,22 +1,20 @@
 package racingcar.domain;
 
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import racingcar.utils.Constant;
 
 public class Car {
 
-    private String name;
-    private int position;
-
     private static final int START_POSITION = 0;
-    private static final int MINIMUM_MOVE = 4;
     private static final int MAXIMUM_NAME_LENGTH = 5;
 
     private static final String CAR_NAME_PATTERN_REGEX = "^[a-zA-Z]*$";
 
-    private static final Pattern pattern = Pattern.compile(CAR_NAME_PATTERN_REGEX);
+    private static final Pattern CAR_NAME_PATTERN = Pattern.compile(CAR_NAME_PATTERN_REGEX);
+
+    private String name;
+    private int position;
 
     public Car(String name) {
         checkName(name);
@@ -24,11 +22,16 @@ public class Car {
         this.position = START_POSITION;
     }
 
-
     public void movePosition(MoveCondition moveCondition) {
+
         if (moveCondition.isMovable()) {
             position++;
         }
+
+    }
+
+    public boolean isSamePosition(int number) {
+        return position == number;
     }
 
     public int getPosition() {
@@ -39,31 +42,35 @@ public class Car {
         return name;
     }
 
-    public boolean isSamePosition(int number) {
-        return position == number;
-    }
-
     public void setPosition(int number) {
         position = number;
     }
 
     private void checkName(String name) {
+
         checkLength(name);
         checkValidPattern(name);
+
     }
 
     private void checkLength(String name) {
+
         if (name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(Constant.NOT_ALLOW_LENGTH_ERROR);
         }
+
     }
 
     private void checkValidPattern(String name) {
-        Matcher matcher = pattern.matcher(name);
+
+        Matcher matcher = CAR_NAME_PATTERN.matcher(name);
+
         if (!matcher.find()) {
             throw new IllegalArgumentException(Constant.NOT_ALPHA_ERROR);
         }
+
     }
+
 }
 
 
