104014fb962df52b7d410ef9fcfb0fa1ae89dba5
cjlee38 <cjlee38@github.com>
2022-02-19 01:00:07 +0900

    test: 전진전략 기반 자동차 위치 움직임 테스트

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -3,8 +3,12 @@ package racingcar.domain.car;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import racingcar.domain.movement.FixedMovementStrategy;
+import racingcar.domain.movement.MovementStrategy;
+import racingcar.domain.movement.RandomMovementStrategy;
 import racingcar.exception.InvalidNameInputException;
 
 public class CarsTest {
@@ -26,5 +30,20 @@ public class CarsTest {
         Assertions.assertThatThrownBy(() -> {
                 new Cars(input);
             }).isInstanceOf(InvalidNameInputException.class);
+    }
+
+    @Test
+    @DisplayName("전략에 따라 자동차들이 움직인다.")
+    public void testCarsMovement() {
+        // given
+        Cars cars = new Cars("pobi,seung,cha");
+        MovementStrategy strategy = new FixedMovementStrategy();
+        // when
+        cars.move(strategy);
+        // then
+        List<Car> carList = cars.getCarList();
+        Assertions.assertThat(carList.get(0).getPosition()).isEqualTo(1);
+        Assertions.assertThat(carList.get(1).getPosition()).isEqualTo(1);
+        Assertions.assertThat(carList.get(2).getPosition()).isEqualTo(1);
     }
 }
--- src/test/java/racingcar/domain/car/PositionTest.java
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -2,8 +2,12 @@ package racingcar.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import racingcar.domain.movement.FixedMovementStrategy;
+import racingcar.domain.movement.MovementStrategy;
 
 public class PositionTest {
 
@@ -11,6 +15,18 @@ public class PositionTest {
     void create() {
         Position position = new Position();
         assertThat(position.isSame(0)).isTrue();
+    }
+
+    @Test
+    @DisplayName("포지션은 전진전략으로 이동할 수 있다.")
+    public void testPositionMovement() {
+        // given
+        Position position = new Position();
+        MovementStrategy strategy = new FixedMovementStrategy();
+        // when
+        position.move(strategy);
+        // then
+        assertThat(position.isSame(1)).isTrue();
     }
 
 }
