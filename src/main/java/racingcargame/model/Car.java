b1386df7e012cadc9f74efe5adb91fe3dd344c8e
jaejae-yoo <wotj102@gmail.com>
2022-02-18 16:04:07 +0900

    refactor: 자동차 이동 기능 구조 변경

--- src/main/java/racingcargame/model/Car.java
+++ src/main/java/racingcargame/model/Car.java
@@ -2,8 +2,6 @@ package racingcargame.model;
 
 public class Car implements Comparable<Car> {
     private static final int MINIMUM_MOVE_ACCESSIBLE = 4;
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_NUMBER = 0;
 
     private final String name;
     private int position;
@@ -17,15 +15,10 @@ public class Car implements Comparable<Car> {
         return position == winner.getPosition();
     }
 
-    void moveCar() {
-        if (isMoveCar()) {
+    void move(int number) {
+        if (number >= MINIMUM_MOVE_ACCESSIBLE) {
             position ++;
         }
-    }
-
-    private boolean isMoveCar() {
-        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(MIN_NUMBER, MAX_NUMBER + 1);
-        return randomNumberGenerator.pickRandomNumber() >= MINIMUM_MOVE_ACCESSIBLE;
     }
 
     @Override
--- src/main/java/racingcargame/model/Cars.java
+++ src/main/java/racingcargame/model/Cars.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Cars {
     private static final int START_POSITION = 0;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     private final List<Car> cars = new ArrayList<>();
 
     public Cars(List<String> carNames) {
@@ -12,7 +15,10 @@ public class Cars {
     }
 
     void moveCars() {
-        cars.forEach(Car::moveCar);
+        for (Car car : cars) {
+            RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(MIN_NUMBER, MAX_NUMBER + 1);
+            car.move(randomNumberGenerator.pickRandomNumber());
+        }
     }
 
     public List<Car> getCars() {
