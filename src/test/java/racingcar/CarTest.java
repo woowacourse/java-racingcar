75527857bb993c42c87d7db9562a0fb0e6f06564
seong-wooo <seongwoo0513@uos.ac.kr>
2022-02-19 11:16:04 +0900

    refactor: CarTest 내의 의존 관계 수정
    각 테스트에서 객체를 생성하도록 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,7 +3,6 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 import racingcar.model.MovingCarStrategy;
@@ -11,42 +10,39 @@ import racingcar.model.RandomMovingStrategy;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class CarTest {
-    private MovingCarStrategy movingCarStrategy;
-
-    @BeforeEach
-    public void beforeEach() {
-        movingCarStrategy = new RandomMovingStrategy();
-    }
 
     @Test
     public void 자동차_이름_공백() {
+        MovingCarStrategy movingCarStrategy = new RandomMovingStrategy();
         assertThatThrownBy(() -> new Car(null, movingCarStrategy))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 자동차_이름_null() {
+        MovingCarStrategy movingCarStrategy = new RandomMovingStrategy();
         assertThatThrownBy(() -> new Car("", movingCarStrategy))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 자동차_이름_길이_제한_예외처리() {
+        MovingCarStrategy movingCarStrategy = new RandomMovingStrategy();
         assertThatThrownBy(() -> new Car("배카라쿠네당", movingCarStrategy))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 전진_성공() {
-        movingCarStrategy = () -> true;
+        MovingCarStrategy movingCarStrategy = () -> true;
         Car testCar = new Car("아스피", movingCarStrategy);
         testCar.move();
         assertThat(testCar.isSamePosition(new Car("배카라", 1))).isEqualTo(true);
     }
 
     @Test
-    public void 전진_살패() {
-        movingCarStrategy = () -> false;
+    public void 전진_실패() {
+        MovingCarStrategy movingCarStrategy = () -> false;
         Car testCar = new Car("아스피", movingCarStrategy);
         testCar.move();
         assertThat(testCar.isSamePosition(new Car("배카라", 1))).isEqualTo(false);
