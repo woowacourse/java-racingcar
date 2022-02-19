84adafc2b122357a7339989555df054e53767589
meatsby <meatsby@gmail.com>
2022-02-19 15:19:14 +0900

    refactor: Random 값 반환 클래스 RandomMovingStrategy 내부로 재배치

--- src/main/java/racingcar/domain/RandomMovingStrategy.java
+++ src/main/java/racingcar/domain/RandomMovingStrategy.java
@@ -1,11 +1,13 @@
 package racingcar.domain;
 
-import racingcar.util.RandomGenerator;
+import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy {
+    private static final Random random = new Random();
+    private static final int RANGE = 10;
     private static final int THRESHOLD = 4;
 
     public boolean isMovable() {
-        return RandomGenerator.generateRandomNumber() >= THRESHOLD;
+        return random.nextInt(RANGE) >= THRESHOLD;
     }
 }
