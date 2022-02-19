4c4741d7223bc70ba6cff0f063c494e7faffba5d
bomi <bonita9931@gmail.com>
2022-02-20 02:27:06 +0900

    test: WinnersTest가 우승자 확인이 아닌 차의 이름을 올바르게 반환하는지를 확인.

--- src/test/java/racingcar/WinnersTest.java
+++ src/test/java/racingcar/WinnersTest.java
@@ -3,19 +3,16 @@ package racingcar;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Cars;
 import racingcar.model.Winners;
-import racingcar.util.FixedNumberGenerator;
-import racingcar.util.NumberGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WinnersTest {
     @Test
-    void 우승자_확인() {
+    void 우승자_차의_이름을_반환하는지_확인() {
         Cars cars = Cars.initCars("bom,sun");
-        NumberGenerator numberGenerator = new FixedNumberGenerator(4);
-        cars.race(numberGenerator);
         Winners winners = new Winners(cars);
+        String expected = "bom, sun";
 
-        assertThat(winners.makeWinnerName(",")).contains("bom").contains("sun");
+        assertThat(winners.makeWinnerName(", ")).isEqualTo(expected);
     }
 }
