7d20052063e75aeb084498053efa94d5efcbfd2c
bomi <bonita9931@gmail.com>
2022-02-19 19:41:03 +0900

    refactor: 사용하지 않는 GameControllerTest 삭제

--- src/test/java/racingcar/GameControllerTest.java
+++
@@ -1,9 +0,0 @@
-package racingcar;
-
-import racingcar.controller.GameController;
-
-@SuppressWarnings("NonAsciiCharacters")
-class GameControllerTest {
-    private final GameController gameController = new GameController();
-
-}
