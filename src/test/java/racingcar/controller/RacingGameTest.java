3bca42f7ba2a53efa87402571e4ce2f0bee0b53d
yh20studio(tiki) <yh20studio@gmail.com>
2022-02-19 16:37:26 +0900

    test(racingGameTest): play() 통합테스트 추가

--- src/test/java/racingcar/controller/RacingGameTest.java
+++ src/test/java/racingcar/controller/RacingGameTest.java
@@ -1,16 +1,69 @@
 package racingcar.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.numberGenerator.MovableNumberGenerator;
 import racingcar.numberGenerator.NumberGenerator;
 import racingcar.numberGenerator.RandomNumberGenerator;
+import racingcar.numberGenerator.UnMovableNumberGenerator;
 import racingcar.view.ErrorMessage;
 
 class RacingGameTest {
+
+    @DisplayName("play() 움직임이 있는 경우 테스트")
+    @Test()
+    void play_movable_test() throws Exception {
+        String[] input = {"name1,name2", "1"};
+        byte[] buf = String.join("\n", input).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+        OutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        RacingGame racingGame = new RacingGame();
+        NumberGenerator numberGenerator = new MovableNumberGenerator();
+        racingGame.play(numberGenerator);
+        assertThat(output.toString()).isEqualTo(
+                "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n" +
+                        "시도할 회수는 몇회인가요?\n" +
+                        "\n" +
+                        "실행 결과\n" +
+                        "name1 : -\n" +
+                        "name2 : -\n" +
+                        "\n" +
+                        "name1, name2가 최종 우승했습니다.");
+    }
+
+    @DisplayName("play() 움직임이 없는 경우 테스트")
+    @Test()
+    void play_unMovable_test() throws Exception {
+        String[] input = {"name1,name2", "1"};
+        byte[] buf = String.join("\n", input).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+        OutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        
+        RacingGame racingGame = new RacingGame();
+        NumberGenerator numberGenerator = new UnMovableNumberGenerator();
+        racingGame.play(numberGenerator);
+        assertThat(output.toString()).isEqualTo(
+                "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n" +
+                        "시도할 회수는 몇회인가요?\n" +
+                        "\n" +
+                        "실행 결과\n" +
+                        "name1 : \n" +
+                        "name2 : \n" +
+                        "\n" +
+                        "name1, name2가 최종 우승했습니다.");
+    }
 
     @DisplayName("play() 입력 값에 공백이 있는 경우 예외 테스트")
     @ParameterizedTest()
