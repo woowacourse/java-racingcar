d72478c0c160f945d9db0c4a8510c8af710ff5d1
RunaNam <nmj5012@naver.com>
2022-02-20 00:52:26 +0900

    test: add more test in CarTest

--- src/test/java/racingCar/domain/CarTest.java
+++ src/test/java/racingCar/domain/CarTest.java
@@ -17,14 +17,30 @@ public class CarTest {
 
 	@Test
 	@DisplayName("Car 이동 확인")
-	void moveCarTest() {
+	void moveCarMoveTest() {
 		car.moveCar(true);
 		assertThat(car.getPosition()).isEqualTo(1);
 	}
 
 	@Test
+	@DisplayName("Car 정지 확인")
+	void moveCarStopTest() {
+		car.moveCar(false);
+		assertThat(car.getPosition()).isEqualTo(0);
+	}
+
+	@Test
 	@DisplayName("승자 확인 테스트")
 	void isWinnerTest() {
+		car.moveCar(true);
+		car.moveCar(true);
+		car.moveCar(true);
+		assertThat(car.isWinner(3)).isEqualTo(true);
+	}
+
+	@Test
+	@DisplayName("승자가 아닌 경우 테스트")
+	void isNotWinnerTest() {
 		car.moveCar(true);
 		car.moveCar(true);
 		assertThat(car.isWinner(3)).isEqualTo(false);
