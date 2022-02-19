40c4d48f928f082162d29485fe8508a1850ee0a5
skullKim <kyk990328@gmail.com>
2022-02-19 21:56:13 +0900

    refactor(CarRepositoryTest.java): CarRepository 테스트 추가
    CarRepository의 로직을 검사하기 위한 테스트 추가

---
+++ src/test/java/racingcar/models/CarRepositoryTest.java
@@ -0,0 +1,32 @@
+package racingcar.models;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarRepositoryTest {
+
+	final CarRepository carRepository = new CarRepository("a, b, c");
+
+	@Test
+	@DisplayName("승자 리스트가 정상적으로 가져와 지는지 확인한다.")
+	void testGetWinnersName() {
+		assertThat(carRepository.getWinnersName().size()).isGreaterThan(0);
+	}
+
+	@Test
+	@DisplayName("한 턴을 실행했을 때 자동차들이 앞으로 전진 또는 제자리인지 확인")
+	void testGoForward() {
+		carRepository.getThisTurnResult()
+			.forEach((car) -> {
+				assertThat(car.getPosition()).isLessThanOrEqualTo(1);
+			});
+	}
+
+	@BeforeEach
+	void startThisTurn() {
+		carRepository.startThisTurn();
+	}
+}
