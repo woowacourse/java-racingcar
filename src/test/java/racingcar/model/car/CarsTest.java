d588ce60c79792abf275046d7b24025933a0418a
jochungbeom <cndqjacndqja@gmail.com>
2022-02-19 15:31:19 +0900

    refactor(CarsTest): BeforeEach 사용으로 중복되는 코드 제거

--- src/test/java/racingcar/model/car/CarsTest.java
+++ src/test/java/racingcar/model/car/CarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.model.car;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,13 +11,19 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
+    Cars cars;
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        cars = new Cars();
+        car = new Car("범고래");
+        cars.save(car);
+    }
+
     @Test
     @DisplayName("자동차를 CarRepository에 저장할 때는 Car로 하지만 읽어올 때는 CarDtos로 읽어온다.")
     void getCars() {
-        Cars cars = new Cars();
-        Car car = new Car("범고래");
-        cars.save(car);
-
         List<CarDto> carDtos = cars.getCarDtos();
         CarDto carDto = new CarDto(car);
         assertThat(carDtos.contains(carDto)).isTrue();
@@ -26,9 +33,6 @@ public class CarsTest {
     @CsvSource(value = {"1,0", "3,0", "4,1", "5,1"}, delimiter = ',')
     @DisplayName("자동차들이 입력 받은 랜덤 숫자 값에 따라 4이상이면 전진, 3이하면 정지한다.")
     void move(int input, int actual) {
-        Cars cars = new Cars();
-        Car car = new Car("범고래");
-        cars.save(car);
         cars.move(List.of(input));
         assertThat(car.getPosition()).isEqualTo(actual);
     }
