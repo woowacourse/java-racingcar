9e4f06baef265e119b6c539169772e63f4f672d3
nailseong <nnis9857@gmail.com>
2022-02-19 11:43:09 +0900

    refactor(RacingController) : movingStrategy를 인스턴스 변수로 변경하고 생성자 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,11 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.domain.movestrategy.RandomMovingStrategy;
 
 public class RacingCar {
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController();
+        RacingController racingController = new RacingController(new RandomMovingStrategy());
         racingController.play();
     }
 }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -11,23 +11,28 @@ import racingcar.domain.Cars;
 import racingcar.domain.TrialCount;
 import racingcar.domain.Winners;
 import racingcar.domain.movestrategy.MovingStrategy;
-import racingcar.domain.movestrategy.RandomMovingStrategy;
 
 public class RacingController {
+
+    private final MovingStrategy movingStrategy;
+
+    public RacingController(MovingStrategy movingStrategy) {
+        this.movingStrategy = movingStrategy;
+    }
 
     public void play() {
         try {
             Cars cars = Cars.fromNames(insertName());
             TrialCount trialCount = new TrialCount(insertNumber());
 
-            progressTurns(cars, trialCount, new RandomMovingStrategy());
+            progressTurns(cars, trialCount);
             printWinners(new Winners(cars));
         } catch (IllegalArgumentException e) {
             printErrorMessage(e.getMessage());
         }
     }
 
-    private void progressTurns(Cars cars, TrialCount trialCount, MovingStrategy movingStrategy) {
+    private void progressTurns(Cars cars, TrialCount trialCount) {
         printResultMessage();
 
         for (int i = 0; trialCount.canContinue(i); i++) {
