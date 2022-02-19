d450599a7ddd78da2eae9e0d53a704fabe4aba13
awesomeo184 <awesomeo184@gmail.com>
2022-02-19 23:14:08 +0900

    refactor(InputView): 범용성이 떨어지는 상수를 인터페이스에서 제거한다

--- src/main/java/racingcar/view/ConsoleInputView.java
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleInputView implements InputView {
+
+
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String DELIMITER = ",";
+
     private final Scanner scanner = new Scanner(System.in);
 
     @Override
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public interface InputView {
 
-    String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    String COUNT_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
-    String DELIMITER = ",";
-
     List<String> getCarNames();
 
     int getCount();
