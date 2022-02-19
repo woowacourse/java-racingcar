f2b82ed616a1afc547ea7130a3ae0e7cda956643
Donggyu Kim <a29661498@gmail.com>
2022-02-19 14:48:15 +0900

    refactor: RandomUtil 로직을 RandomMovingStrategy 내부로
    RnadomMovingStrategy가 굳이 랜덤 값을 생성해주는 부분에 대한 로직을 가지고 있던 RandomUtil에 대한 의존성을 가지지 않고, 내부로 가지고와서 내부에서만 사용해도 된다고 생각하여 리팩터링 하였습니다.

--- src/main/java/racingcar/domain/movestrategy/RandomMovingStrategy.java
+++ src/main/java/racingcar/domain/movestrategy/RandomMovingStrategy.java
@@ -1,15 +1,18 @@
 package racingcar.domain.movestrategy;
 
-import racingcar.util.RandomUtil;
-
 public class RandomMovingStrategy implements MovingStrategy {
 
     private static final int PROGRESS_CONDITION_VALUE = 4;
+    private static final int RANDOM_RANGE = 10;
 
     @Override
     public boolean isMovable() {
-        int randomNumber = RandomUtil.getRandomNumber();
+        int randomNumber = getRandomNumber();
 
         return randomNumber >= PROGRESS_CONDITION_VALUE;
+    }
+
+    private static int getRandomNumber() {
+        return (int) Math.floor(Math.random() * RANDOM_RANGE);
     }
 }
