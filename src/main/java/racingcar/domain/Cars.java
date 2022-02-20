6470bda1e73fcc4dcd0b799b2c9be8772229d3df
asebn1 <asebn1@khu.ac.kr>
2022-02-20 21:34:32 +0900

    refactor: 상수 적용

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Cars {
     private List<Car> cars;
 
-    public Cars(List<Car> cars) {
+    public Cars(final List<Car> cars) {
         this.cars = cars;
     }
 
@@ -28,7 +28,7 @@ public class Cars {
 
     public List<String> getWinners() {
         List<String> winners = new ArrayList<>();
-                cars.stream()
+        cars.stream()
                 .filter(car -> car.checkIfPositionSame(getMaxPosition()))
                 .forEach(car -> winners.add(car.getName()));
         return winners;
