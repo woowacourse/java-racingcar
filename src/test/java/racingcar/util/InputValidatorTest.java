e16edf827604eda024a7f23330974dfbe8879929
brorae <jhr3239@gmail.com>
2022-02-20 21:14:17 +0900

    test : 이동할 횟수 정상 읿력 테스트 추가

--- src/test/java/racingcar/util/InputValidatorTest.java
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -3,6 +3,7 @@ package racingcar.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -10,10 +11,17 @@ public class InputValidatorTest {
 
     @DisplayName("정수가 아닌 값 검증 테스트")
     @Test
-    void validateTryCountTest() {
+    void validateTryCountTest1() {
         assertThatThrownBy(() -> {
             InputValidator.validateTryCount("일");
         }).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("정수 값 검증 테스트")
+    @Test
+    void validateTryCountTest2() {
+        int actual = InputValidator.validateTryCount("1");
+        assertThat(actual).isEqualTo(1);
     }
 
     @DisplayName("이름 공백 검증 테스트")
