9edc19c2dc0e76cf130e5aa9fc58df0494e13bd4
Hyunta <mohai2618@gmail.com>
2022-02-20 14:44:53 +0900

    refactor : CarNameValidatorTest 검증하는 테스트 수정

--- src/test/java/racingcar/utils/CarNameValidatorTest.java
+++ src/test/java/racingcar/utils/CarNameValidatorTest.java
@@ -17,15 +17,6 @@ public class CarNameValidatorTest {
     }
 
     @Test
-    @DisplayName("이름이 5자 초과일 경우")
-    void longName() {
-        Assertions.assertThatThrownBy(() -> {
-                    validateCarNames(new String[]{"summer", "crong"});
-                }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_LONG_NAME);
-    }
-
-    @Test
     @DisplayName("중복된 이름이 있을 경우")
     void duplicateName() {
         Assertions.assertThatThrownBy(() -> {
@@ -35,12 +26,20 @@ public class CarNameValidatorTest {
     }
 
     @Test
+    @DisplayName("이름이 5자 초과일 경우")
+    void longName() {
+        Assertions.assertThatThrownBy(() -> {
+                    validateCarName("summer");
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_LONG_NAME);
+    }
+
+    @Test
     @DisplayName("빈 이름이 주어지거나 입력값이 없을 경우")
     void emptyName() {
         Assertions.assertThatThrownBy(() -> {
-                    validateCarNames(new String[]{""});
+                    validateCarName("");
                 }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_EMPTY_NAME);
     }
-
 }
