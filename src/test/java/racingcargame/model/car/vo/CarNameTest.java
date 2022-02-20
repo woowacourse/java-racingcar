5d764d71252ff3cffb757fa62fcebd0efac0bdf4
koo <gudonghee2000@gmail.com>
2022-02-20 17:29:17 +0900

    refactor: CarNameTest 클래스의 테스트 함수명세 구체화, 공통필드 제거

--- src/test/java/racingcargame/model/car/vo/CarNameTest.java
+++ src/test/java/racingcargame/model/car/vo/CarNameTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class CarNameTest {
 
-    @DisplayName("입력된 자동차 이름이 없는 경우 테스트")
+    @DisplayName("자동차 이름이 없으면 오류를 발생한다.")
     @Test
     void checkValidateName_EmptyName() {
         final String emptyName = "";
@@ -18,7 +18,7 @@ class CarNameTest {
                 hasMessageContaining("[error] 입력된 자동차 이름중 이름이 없는 자동차가 있습니다.");
     }
 
-    @DisplayName("자동차 이름이 5자를 초과하는 경우 테스트")
+    @DisplayName("자동차 이름이 5자를 넘으면 오류를 발생한다.")
     @Test
     void checkValidateName_ValidNameLength() {
         final String carName = "토미데이비슨";
@@ -29,7 +29,7 @@ class CarNameTest {
                 hasMessageContaining("[error] 자동차 이름을 5자 이하로 입력해주세요.");
     }
 
-    @DisplayName("자동차 이름이 올바른 입력일 경우 테스트")
+    @DisplayName("자동차 이름이 저장 됬는지 확인한다.")
     @Test
     void checkValidateName() {
         final String carName = "하디베어";
