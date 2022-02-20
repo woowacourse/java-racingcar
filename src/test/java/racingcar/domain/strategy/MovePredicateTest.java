acdc19827d31953eef695875174197448bd08d0a
YJGwon <yj970125@gmail.com>
2022-02-20 15:49:52 +0900

    refactor(MovePredicateTest): 불필요한 Car 이동 삭제, false인 case 추가

--- src/test/java/racingcar/domain/strategy/MovePredicateTest.java
+++ src/test/java/racingcar/domain/strategy/MovePredicateTest.java
@@ -5,15 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import racingcar.domain.Car;
-
 public class MovePredicateTest {
     @Test
-    @DisplayName("4 이상의 값일 때 자동차 이동")
-    void test_5_car_move() {
-        Car car = new Car("forky");
+    @DisplayName("4 이상의 값일 때 true 반환")
+    void test_5() {
         MovePredicate movePredicate = new MovePredicate();
-        car.move(movePredicate.test(5));
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(movePredicate.test(5)).isTrue();
+    }
+
+    @Test
+    @DisplayName("4 미만의 값일 때 false 반환")
+    void test_3() {
+        MovePredicate movePredicate = new MovePredicate();
+        assertThat(movePredicate.test(3)).isFalse();
     }
 }
