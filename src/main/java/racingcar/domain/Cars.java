fd667253f3b4d953c992d981caff31acc0d1f619
summerlunaa <summerlunaa9@gmail.com>
2022-02-20 17:35:38 +0900

    refactor: 자동차 경주 검증 로직을 racingGameValidator에서 Cars로 이동

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,18 +3,21 @@ package racingcar.domain;
 import static racingcar.Util.Utils.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import racingcar.validator.RacingGameValidator;
-
 public class Cars {
+	public static final String ERROR_ONLY_NAME = "[ERROR] 이름을 2개 이상 입력해주세요.";
+	public static final String ERROR_DUPLICATE_NAME = "[ERROR] 중복된 이름이 있습니다.";
 	public static final int MOVE_CONDITION = 4;
 
 	private final List<Car> cars = new ArrayList<>();
 
 	public Cars(String[] carNames) {
-		RacingGameValidator.validateCarNames(carNames);
+		validateCarNames(carNames);
 		for (String carName : carNames) {
 			cars.add(new Car(carName));
 		}
@@ -22,6 +25,24 @@ public class Cars {
 
 	public Cars(List<Car> cars) {
 		this.cars.addAll(cars);
+	}
+
+	private void validateCarNames(String[] names) {
+		checkDuplicateName(names);
+		checkOnlyName(names);
+	}
+
+	private static void checkOnlyName(String[] names) {
+		if (names.length == 1) {
+			throw new IllegalArgumentException(ERROR_ONLY_NAME);
+		}
+	}
+
+	private static void checkDuplicateName(String[] names) {
+		Set<String> hashNames = new HashSet<>(Arrays.asList(names));
+		if (hashNames.size() != names.length) {
+			throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
+		}
 	}
 
 	public List<Car> getCars() {
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,13 +1,16 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.domain.Cars.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import racingcar.Util.Utils;
 
 class CarsTest {
 	@Test
@@ -16,6 +19,22 @@ class CarsTest {
 		String[] carNames = {"pobi", "crong", "huan"};
 		Cars cars = new Cars(carNames);
 		assertThat(cars.getCars().size()).isEqualTo(carNames.length);
+	}
+
+	@Test
+	@DisplayName("입력한 이름값이 한개인 경우")
+	void onlyName() {
+		Assertions.assertThatThrownBy(() -> new Cars(Utils.splitter("pobi")))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ERROR_ONLY_NAME);
+	}
+
+	@Test
+	@DisplayName("중복된 이름이 있을 경우")
+	void duplicateName() {
+		Assertions.assertThatThrownBy(() -> new Cars(Utils.splitter("pobi,pobi")))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ERROR_DUPLICATE_NAME);
 	}
 
 	@Test
