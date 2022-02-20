dbad8ac9a465c677d01b23a17b5fafea66a03e90
Hyunta <mohai2618@gmail.com>
2022-02-20 14:41:19 +0900

    refactor : CarDto 변수명 수정

--- src/main/java/racingcar/domain/CarDto.java
+++ src/main/java/racingcar/domain/CarDto.java
@@ -1,23 +1,23 @@
 package racingcar.domain;
 
 public class CarDto {
-    private String carName;
-    private int carPosition;
+    private final String name;
+    private final int position;
 
-    public CarDto(String carName, int carPosition) {
-        this.carName = carName;
-        this.carPosition = carPosition;
+    public CarDto(final String name, final int position) {
+        this.name = name;
+        this.position = position;
     }
 
     public static CarDto from(Car car) {
         return new CarDto(car.getName(), car.getPosition());
     }
 
-    public String getCarName() {
-        return carName;
+    public String getName() {
+        return name;
     }
 
-    public int getCarPosition() {
-        return carPosition;
+    public int getPosition() {
+        return position;
     }
 }
