5b6a01dcb150798301f9fade05c69d9b23cc3242
YJGwon <yj970125@gmail.com>
2022-02-20 14:11:18 +0900

    refactor(Random): 불필요한 Random class 삭제 후 java.util.Random으로 대체

--- src/main/java/racingcar/domain/strategy/MovePredicate.java
+++ src/main/java/racingcar/domain/strategy/MovePredicate.java
@@ -1,15 +1,15 @@
 package racingcar.domain.strategy;
 
+import java.util.Random;
 import java.util.function.IntPredicate;
-
-import racingcar.domain.util.Random;
 
 public class MovePredicate implements IntPredicate {
     private static final int MOVE_CRITERIA = 4;
     private static final int RANDOM_VALUE_BOUND = 9;
+    private static final Random RANDOM = new java.util.Random();
 
     public boolean testRandom() {
-        return test(Random.makeRandomValue(RANDOM_VALUE_BOUND));
+        return test(RANDOM.nextInt(RANDOM_VALUE_BOUND + 1));
     }
 
     @Override
--- src/main/java/racingcar/domain/util/Random.java
+++
@@ -1,9 +0,0 @@
-package racingcar.domain.util;
-
-public class Random {
-    private static final java.util.Random RANDOM = new java.util.Random();
-
-    public static int makeRandomValue(int bound) {
-        return RANDOM.nextInt(bound + 1);
-    }
-}
