7e03f72d07680996ab6cc3625870a79dee656f7e
Kim Seunglae <obey1342@gmail.com>
2022-02-21 00:40:59 +0900

    refactor(CarsTest): 메소드 네이밍 및 테스트 방식 수정

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,14 +23,16 @@ class CarsTest {
     }
 
     @Test
-    void createCarsBySplitComma() {
+    void createCarsBySplitDelimiter() {
         String[] input = {"pobi", "seung", "char"};
         Cars cars = Cars.create(input);
         List<Car> carList = cars.getCars();
-        assertThat(carList.size()).isEqualTo(3);
-        assertThat(carList.get(0)).isEqualTo(new Car("pobi"));
-        assertThat(carList.get(1)).isEqualTo(new Car("seung"));
-        assertThat(carList.get(2)).isEqualTo(new Car("char"));
+        Assertions.assertAll(
+            () -> assertThat(carList.size()).isEqualTo(3),
+            () -> assertThat(carList.get(0)).isEqualTo(new Car("pobi")),
+            () -> assertThat(carList.get(1)).isEqualTo(new Car("seung")),
+            () -> assertThat(carList.get(2)).isEqualTo(new Car("char"))
+        );
     }
 
     @DisplayName("우승자가 1명일 경우 정상적인지 확인")
