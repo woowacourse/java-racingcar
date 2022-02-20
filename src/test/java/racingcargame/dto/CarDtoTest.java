a2ec19986be6581e7fe17468ff2180ac886dd267
koo <gudonghee2000@gmail.com>
2022-02-20 17:18:36 +0900

    refactor: CarDtoTest 클래스의 테스트 함수명세 구체화

--- src/test/java/racingcargame/dto/CarDtoTest.java
+++ src/test/java/racingcargame/dto/CarDtoTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 class CarDtoTest {
     private final CarDto carDto = new CarDto("애니", 3);
 
-    @DisplayName("이름 반환이 정상적으로 되는지 테스트")
+    @DisplayName("자동차 이름을 반환해준다.")
     @Test
     void getName() {
         assertThat(carDto.getName()).isEqualTo("애니");
     }
 
-    @DisplayName("자동차 위치값 반환이 정상적으로 되는지 테스트")
+    @DisplayName("자동차 위치 값을 반환해준다.")
     @Test
     void getPosition() {
         assertThat(carDto.getPosition()).isEqualTo(3);
