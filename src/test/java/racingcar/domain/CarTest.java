7eb188c8b0dfad88fd7b4deafa38ccb45bff07c0
summerlunaa <summerlunaa9@gmail.com>
2022-02-20 17:35:02 +0900

    refactor: 자동차 이름 검증 로직을 carNameValidator에서 Car로 이동

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,9 +1,10 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.domain.Car.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 	@Test
@@ -12,6 +13,22 @@ class CarTest {
 		String name = "pobi";
 		Car car = new Car(name);
 		assertThat(car.getName()).isEqualTo(name);
+	}
+
+	@Test
+	@DisplayName("이름이 5자 초과일 경우")
+	void longName() {
+		assertThatThrownBy(() -> new Car("summer"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ERROR_LONG_NAME);
+	}
+
+	@Test
+	@DisplayName("빈 이름이 주어지거나 입력값이 없을 경우")
+	void emptyName() {
+		assertThatThrownBy(() -> new Car(""))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(ERROR_EMPTY_NAME);
 	}
 
 	@Test
