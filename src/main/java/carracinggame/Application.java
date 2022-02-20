8ecf69f340cdbad215f41fc4a020d360220b48b2
LEO <76774809+DWL21@users.noreply.github.com>
2022-02-20 23:52:39 +0900

    Delete Application.java

--- src/main/java/carracinggame/Application.java
+++
@@ -1,9 +0,0 @@
-package carracinggame;
-
-public class Application {
-
-    public static void main(String[] args) {
-        CarRacingGame racingCar = new CarRacingGame();
-        racingCar.start();
-    }
-}
