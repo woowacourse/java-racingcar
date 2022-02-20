3cfba57dbe318004aef865da99915cede914a816
SuyeonChoi <tndus970120@gmail.com>
2022-02-20 21:31:48 +0900

    feat(Car): compareTo, equals, hashCode 재정의

--- src/main/java/carracing/model/Car.java
+++ src/main/java/carracing/model/Car.java
@@ -2,10 +2,11 @@ package carracing.model;
 
 import carracing.dto.CarDto;
 import carracing.utils.MoveStrategy;
+import java.util.Objects;
 
 import static carracing.view.messages.ExceptionMessage.*;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
 
@@ -32,5 +33,27 @@ public class Car {
 
     public boolean isEqualPositionTo(Car car) {
         return this.position == car.position;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return o.position - this.position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
