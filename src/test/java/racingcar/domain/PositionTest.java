3b4aff2b5e02b821e021894ccfb74e48c3ae658a
summerlunaa <summerlunaa9@gmail.com>
2022-02-21 00:30:43 +0900

    fix: moveCount 인자 삭제

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class PositionTest {
 	@Test
 	@DisplayName("자동차 위치가 음수인 경우")
-	void notPositive(int moveCount) {
+	void notPositive() {
 		assertThatThrownBy(() -> new Position(-1))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(ERROR_NEGATIVE);
