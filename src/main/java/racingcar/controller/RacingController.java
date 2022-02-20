8ac3b78567a98030498c13d0bc8ceeab6e7ce6a7
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-02-21 03:55:17 +0900

    refactor: CarController, 메인 -> RacingController에 통합

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,37 +1,62 @@
 package racingcar.controller;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import racingcar.model.CarDto;
+import racingcar.model.CarToCarDtoMapper;
+import racingcar.model.Cars;
+import racingcar.model.Random;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
 public class RacingController {
-	private final CarController carController;
+	private static final Cars cars = new Cars();
 
-	public RacingController() {
-		this.carController = new CarController();
+	public static void main(String[] args) {
+		RacingController controller = new RacingController();
+		controller.createCarsByUserInput();
+		controller.runInIteration(InputView.getIterationNumber());
+		ResultView.printWinners(controller.getWinners());
 	}
 
 	public void createCarsByUserInput() {
 		try {
-			carController.createCars(InputView.getCarNames());
+			createCars(InputView.getCarNames());
 		} catch (RuntimeException e) {
 			System.out.println(e.getMessage());
 			createCarsByUserInput();
 		}
 	}
 
-	public void run() {
-		int iteration = InputView.getIterationNumber();
-		ResultView.printBeforeGameResult();
-		runInIteration(iteration);
-		ResultView.printWinners(carController.getWinners());
+	List<String> splitCarNames(String input) {
+		return Arrays.stream(input.split(","))
+			.map(String::trim)
+			.collect(Collectors.toList());
+	}
+
+	void createCars(String carNames) {
+		cars.createCars(splitCarNames(carNames));
 	}
 
 	private void runInIteration(int iteration) {
-		IntStream.range(0, iteration).forEach(i -> {
-			carController.run();
-			ResultView.printGameResult(carController.getCars());
-		});
+		for (int i = 0; i < iteration; i++) {
+			moveAll();
+			ResultView.printGameResult(getCars());
+		}
+	}
+
+	void moveAll() {
+		int numberOfCars = cars.getSize();
+		cars.moveAll(new Random().createNumbers(numberOfCars));
+	}
+
+	List<CarDto> getCars() {
+		return CarToCarDtoMapper.carsToCarDtos(cars.getCars());
+	}
+
+	List<CarDto> getWinners() {
+		return CarToCarDtoMapper.carsToCarDtos(cars.getWinners());
 	}
 }
--- src/test/java/racingcar/controller/RacingControllerTest.java
+++ src/test/java/racingcar/controller/RacingControllerTest.java
@@ -8,25 +8,25 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class CarControllerTest {
+public class RacingControllerTest {
 
 	@Test
 	@DisplayName("자동차 이름 콤마 기준 분리, 공백 제거")
 	void splitCarNamesTest() {
-		CarController carController = new CarController();
+		RacingController racingController = new RacingController();
 		List<String> expected = Arrays.asList("범고래", "소주캉", "고래");
 		String userInputCarNames = "  범고래 , 소주캉,   고래";
 
-		assertThat(carController.splitCarNames(userInputCarNames)).isEqualTo(expected);
+		assertThat(racingController.splitCarNames(userInputCarNames)).isEqualTo(expected);
 	}
 
 	@Test
 	@DisplayName("3대의 자동차 목록 생성 테스트")
 	void createCarsTest() {
-		CarController carController = new CarController();
+		RacingController racingController = new RacingController();
 		String userInputCarNames = "범고래,소주캉,고래";
-		carController.createCars(userInputCarNames);
+		racingController.createCars(userInputCarNames);
 
-		assertThat(carController.getCars().size()).isEqualTo(3);
+		assertThat(racingController.getCars().size()).isEqualTo(3);
 	}
 }
