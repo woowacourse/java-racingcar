f2eec6934c7ecd5e4d0c7eb02bf14042c0420f8a
koo <gudonghee2000@gmail.com>
2022-02-20 16:06:32 +0900

    feat: CarPosition 클래스에 equals와 hashcode 함수 오버라이딩

--- src/main/java/racingcargame/model/car/vo/CarPosition.java
+++ src/main/java/racingcargame/model/car/vo/CarPosition.java
@@ -19,4 +19,21 @@ public class CarPosition implements Comparable<CarPosition> {
     public int compareTo(final CarPosition winnerCarPosition) {
         return this.position - winnerCarPosition.position;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CarPosition otherCarPosition = (CarPosition) o;
+        return position == otherCarPosition.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return position;
+    }
 }
