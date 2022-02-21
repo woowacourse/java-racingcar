e9da6ca53560e82e4c2f412c0556a4392af20b6e
yeongunheo <gjduddns@gmail.com>
2022-02-21 11:32:24 +0900

    test: 자동차가 4 이상이면 전진, 3 이하면 정지하는 테스트 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,10 +2,9 @@ package racingcar;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.controller.CarController;
 import racingcar.domain.Car;
 
@@ -18,6 +17,22 @@ public class RacingCarTest {
     public void saveCarNames() {
         final CarController controller = new CarController(new Scanner(System.in));
         final List<Car> cars = controller.saveCars("pobi,crong,honux");
-        assertThat(cars.get(0).getName().equals("pobi")).isTrue();
+        final List<String> carNames = cars.stream()
+                    .map(Car::getName)
+                    .collect(Collectors.toList());
+        assertThat(carNames)
+                .contains("pobi")
+                .contains("crong")
+                .contains("honux");
+    }
+
+    @Test
+    @DisplayName("4 이상이면 전진, 3 이하면 정지")
+    public void moveAndStop() {
+        final Car car = new Car("pobi");
+        final Car stopCar = car.race(3);
+        final Car moveCar = car.race(4);
+        assertThat(stopCar.getPosition()).isEqualTo(0);
+        assertThat(moveCar.getPosition()).isEqualTo(1);
     }
 }
