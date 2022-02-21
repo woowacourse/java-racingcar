aef7728d975791cf839ca9f4daa37066a0f0639f
SuyeonChoi <tndus970120@gmail.com>
2022-02-21 16:20:44 +0900

    test(CarTest): 자동차 위치 비교 테스트 코드 구현

--- src/test/java/carracing/model/CarTest.java
+++ src/test/java/carracing/model/CarTest.java
@@ -1,10 +1,11 @@
 package carracing.model;
 
-import static carracing.view.messages.ExceptionMessage.*;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarTest {
 
@@ -30,5 +31,16 @@ public class CarTest {
         car.move(() -> false);
         //then
         assertThat(car.compareTo(stoppedCar)).isZero();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:false", "0:true"}, delimiter = ':')
+    @DisplayName("자동차의 위치가 같으면 true, 다르면 false를 반환")
+    void 자동차_위치비교(int position, boolean expected) {
+        //given
+        Car car = new Car("pobi");
+        //when
+        //then
+        assertThat(car.isEqualPosition(position)).isEqualTo(expected);
     }
 }
