40e68a5fbd3501e6403fc4ac627c07671be7e27e
dongho <dongho108@naver.com>
2022-02-21 11:35:10 +0900

    refactor : 전진기준숫자 상수화

--- src/main/java/racingcar/utils/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/generator/RandomNumberGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomNumberGenerator implements NumberGenerator {
     private static final Random random;
     private static final int RANDOM_BOUND = 10;
+    private static final int FORWARD_NUMBER = 4;
 
     static {
         random = new Random();
@@ -13,6 +14,6 @@ public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public boolean canForward() {
-        return random.nextInt(RANDOM_BOUND) >= 4;
+        return random.nextInt(RANDOM_BOUND) >= FORWARD_NUMBER;
     }
 }
