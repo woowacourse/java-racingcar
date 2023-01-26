3685f575f4964064fd95853bb1c1f0707ca79591
Jinho <dfghcvb11@naver.com>
2023-01-27 04:13:58 +0900

    feat: null 또는 빈 문자열 입력시 0을 반환하는 기능

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,4 +2,11 @@ package calculator;
 
 public class StringCalculator {
 
+    public static int splitAndSum(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            return 0;
+        }
+
+        return Integer.parseInt(inputString);
+    }
 }
