b4ee9c551115a110e61e396cadcc3a3962e08bf3
Jinho <dfghcvb11@naver.com>
2023-01-27 04:34:51 +0900

    feat: splitAndSum 메서드에 입력한 문자열을 사용자가 임의로 설정한 구분자를 기준으로 분리한 다음 각 숫자의 합을 반환하는 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
@@ -9,7 +11,18 @@ public class StringCalculator {
             return 0;
         }
 
-        return Arrays.stream(inputString.split("[,:]"))
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputString);
+        if (matcher.find()) {
+            String numberString = matcher.group(2);
+            String customDelimiter = matcher.group(1);
+            return splitByDelimiterAndSum(numberString, customDelimiter);
+        }
+
+        return splitByDelimiterAndSum(inputString, "[,:]");
+    }
+
+    public static int splitByDelimiterAndSum(String numberString, String delimiter) {
+        return Arrays.stream(numberString.split(delimiter))
             .mapToInt(Integer::parseInt)
             .sum();
     }
