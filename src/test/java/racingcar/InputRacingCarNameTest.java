59c964af7dcdce747d90e4e023e16bc4cb4312ae
Jinho <dfghcvb11@naver.com>
2023-01-27 16:25:33 +0900

    test: 자동차 이릅을 입력할 때 공란을 입력하면 예외가 발생하고 입력을 다시 받는 테스트 코드 작성

--- src/test/java/racingcar/InputRacingCarNameTest.java
+++ src/test/java/racingcar/InputRacingCarNameTest.java
@@ -1,19 +1,23 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import calculator.StringCalculator;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class InputRacingCarNameTest {
 
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private InputStream inputStream;
 
     @AfterEach
@@ -24,13 +28,13 @@ class InputRacingCarNameTest {
     @Test
     void 자동차_이름_한개_입력_테스트() {
         //given
-        Input input = new Input();
-
-        // when
         String inputString = "jinho";
         inputStream = new ByteArrayInputStream(inputString.getBytes());
         System.setIn(inputStream);
 
+        Input input = new Input();
+
+        // when
         List<String> carNamesList = input.getCarNamesList();
 
         // then
@@ -40,13 +44,13 @@ class InputRacingCarNameTest {
     @Test
     void 자동차_이름_여러개_입력_테스트() {
         //given
-        Input input = new Input();
-
-        // when
         String inputString = "jinho, dongho, skull";
         inputStream = new ByteArrayInputStream(inputString.getBytes());
         System.setIn(inputStream);
 
+        Input input = new Input();
+
+        // when
         List<String> carNamesList = input.getCarNamesList();
 
         // then
@@ -55,5 +59,43 @@ class InputRacingCarNameTest {
             () -> assertThat(carNamesList.get(1)).isEqualTo("dongho"),
             () -> assertThat(carNamesList.get(2)).isEqualTo("skull")
         );
+    }
+
+    @Test
+    void 자동차_이름에_공란_입력시_입력_다시_받기() {
+        //given
+        setOutPrintStream();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("[ERROR] 아무 이름도 입력하지 않으셨습니다. 다시 입력해주세요");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String expectedOutput = output.toString();
+        resetOutputStream();
+
+        Input input = new Input();
+
+        try {
+            // when
+            String inputString = "\n";
+            inputStream = new ByteArrayInputStream(inputString.getBytes());
+            System.setIn(inputStream);
+
+            input.getCarNamesList();
+            failBecauseExceptionWasNotThrown(NoSuchElementException.class);
+        } catch (NoSuchElementException e) {
+            // then
+            String actualOutput = output.toString();
+            assertThat(actualOutput).isEqualTo(expectedOutput);
+        } finally {
+            resetOutputStream();
+        }
+    }
+
+    void setOutPrintStream() {
+        System.setOut(new PrintStream(output));
+    }
+
+    void resetOutputStream() {
+        System.setOut(System.out);
+        output.reset();
     }
 }
