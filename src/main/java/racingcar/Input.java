71ca50bb95735e9e03fb204efe05e983d65e05b3
Jinho <dfghcvb11@naver.com>
2023-01-27 14:41:42 +0900

    feat: 게임 횟수 입력받기 기능 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,5 +1,17 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Input {
 
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int getRacingGameCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return inputGameCountString();
+    }
+
+    private int inputGameCountString() {
+        return scanner.nextInt();
+    }
 }
--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,27 +1,31 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 class InputTest {
-    private InputStream input;
+
+    private InputStream inputStream;
 
     @Test
     void 게임_횟수_입력_테스트() {
+        //given
         String inputString = "10";
-        input = new ByteArrayInputStream(inputString.getBytes());
-        System.setIn(input);
-        int expectedInput = Integer.parseInt(new Scanner(System.in).nextLine());
+        inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+
+        int expectedInput = 10;
 
         Input input = new Input();
+
+        // when
         int actualInput = input.getRacingGameCount();
 
+        // then
         assertThat(expectedInput).isEqualTo(actualInput);
     }
 }
