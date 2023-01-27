4d1dd1f4457537bdd12ce2d06dfdea30d6ee65a3
Jinho <dfghcvb11@naver.com>
2023-01-27 17:42:32 +0900

    test: 통합 테스트 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static racingcar.CarRacingController.initCarRacingGame;
+
 public class Application {
 
     public static void main(String[] args) {
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,13 +1,16 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
+import java.io.SequenceInputStream;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +19,6 @@ import org.junit.jupiter.api.Test;
 public class ApplicationTest {
 
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    private InputStream inputStream;
 
     @BeforeEach
     void setOutPrintStream() {
@@ -24,51 +26,29 @@ public class ApplicationTest {
     }
 
     @AfterEach
-    void resetOutputStream() throws IOException {
+    void resetOutputStream() {
         System.setOut(System.out);
         output.reset();
-        inputStream.reset();
     }
 
     @Test
-    void 어플리케이션_실행_통합_테스트() throws IOException {
+    void 어플리케이션_실행_통합_테스트() {
         // given
-        setOutPrintStream();
-        어플리케이션_실행_통합_테스트_아웃풋();
-        String expectedOutput = output.toString();
-        resetOutputStream();
-
-        String inputString = "jinho, dongh\n 3";
-        inputStream = new ByteArrayInputStream(inputString.getBytes());
-        System.setIn(inputStream);
+        List<InputStream> streams = Arrays.asList(
+            new ByteArrayInputStream("jinho, dong\n".getBytes()),
+            new ByteArrayInputStream("1".getBytes())
+        );
+        System.setIn(new SequenceInputStream(Collections.enumeration(streams)));
 
         // when
         Application.main(new String[]{});
         String actualOutputString = output.toString();
 
         // then
-        assertThat(actualOutputString).isEqualTo(expectedOutput);
-    }
-
-    void 어플리케이션_실행_통합_테스트_아웃풋() {
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-        System.out.println("시도할 횟수는 몇회인가요?");
-
-        System.out.println("실행 결과");
-        System.out.println("jinho : -");
-        System.out.println("dongh : -");
-        System.out.println();
-
-        System.out.println("jinho : --");
-        System.out.println("dongh : --");
-        System.out.println();
-
-        System.out.println("jinho : ---");
-        System.out.println("dongh : ---");
-        System.out.println();
-
-        System.out.println("최종 우승자 : jinho, dongh");
+        assertAll(
+            () -> assertThat(actualOutputString.contains("jinho : ")),
+            () -> assertThat(actualOutputString.contains("dong : ")),
+            () -> assertThat(actualOutputString.contains("최종 우승자 : "))
+        );
     }
 }
