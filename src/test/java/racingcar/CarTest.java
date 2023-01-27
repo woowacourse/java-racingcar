fdff50ec0e922e5255d34c675790e76f75f0829a
Jinho <dfghcvb11@naver.com>
2023-01-27 12:45:06 +0900

    test: 자동차가 진행 가능 범위 내의 무작위 숫자를 뽑는 경우 진행 가능 여부 확인 테스트 코드 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
@@ -17,5 +18,23 @@ public class CarTest {
 
         // then
         assertThat(result).isEqualTo(carName);
+    }
+
+    @RepeatedTest(100)
+    void 자동차는_진행_가능_범위에서_진행할_수_있다() {
+        // given
+        final int FROM = 4;
+        final int TO = 9;
+
+        RangedRandomNumberPicker randomNumberPicker = new RangedRandomNumberPicker(FROM, TO);
+        Car car = new RandomMovingCar("jinho", randomNumberPicker);
+
+        // when
+        car.race();
+        car.race();
+        car.race();
+
+        // then
+        assertThat(car.getPosition()).isEquals(3);
     }
 }
