99fd180215622476188b1de3f00185ca45404182
amaran-th <songsy405@gmail.com>
2023-02-07 18:09:04 +0900

    refactor : String 학습 테스트 리팩토링
    - 일부 공통된 주제를 가진 메서드에 BDD 구조 적용

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,27 +2,36 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("String 클래스")
 public class StringTest {
 
-  @Test
-  @DisplayName("[1,2]의 경우")
-  public void splitOneTwo() {
-    String given = "1,2";
-    String[] result = given.split(",");
+  @Nested
+  @DisplayName("split 메소드는 ','를 구분자로 설정했을 때")
+  class Describe_split {
+    @Test
+    @DisplayName("대상 문자열이 '1,2'인 경우 '1', '2'를 반환한다")
+    public void splitOneTwo() {
+      String given = "1,2";
+      String[] result = given.split(",");
 
-    Assertions.assertThat(result).contains("1", "2");
+      Assertions.assertThat(result).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("대상 문자열이 '1'인 경우 '1'를 반환한다")
+    public void splitOnlyOne() {
+      String given = "1";
+      String[] result = given.split(",");
+
+      Assertions.assertThat(result).containsExactly("1");
+    }
   }
 
-  @Test
-  @DisplayName("[1]의 경우")
-  public void splitOnlyOne() {
-    String given = "1";
-    String[] result = given.split(",");
 
-    Assertions.assertThat(result).containsExactly("1");
-  }
+
 
   @Test
   @DisplayName("문자열 양 끝 문자 제거")
@@ -32,26 +41,29 @@ public class StringTest {
 
     Assertions.assertThat(result).isEqualTo("1,2");
   }
+  @Nested
+  @DisplayName("charAt 메소드는")
+  public class Describe_charAt{
+    @Test
+    @DisplayName("접근하려는 인덱스가 범위 내에 있을 때 해당 인덱스의 값을 반환한다.")
+    public void getInOfIndexChar() {
+      String given = "abc";
+      int givenIndex = 1;
+      char result = given.charAt(givenIndex);
 
-  @Test
-  @DisplayName("범위 내 index의 문자 접근")
-  public void getInOfIndexChar() {
-    String given = "abc";
-    int givenIndex = 1;
-    char result = given.charAt(givenIndex);
+      Assertions.assertThat(result).isEqualTo('b');
+    }
 
-    Assertions.assertThat(result).isEqualTo('b');
+    @Test
+    @DisplayName("접근하려는 인덱스가 범위를 벗어나면 StringIndexOutOfBoundsException을 반환한다.")
+    public void getOutOfIndexChar() {
+      String given = "abc";
+      int givenIndex = 3;
+
+      Assertions.assertThatThrownBy(() -> {
+        given.charAt(givenIndex);
+      }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
   }
 
-  @Test
-  @DisplayName("범위 밖 index의 문자 접근")
-  public void getOutOfIndexChar() {
-    String given = "abc";
-    int givenIndex = 3;
-    //char result = ;
-
-    Assertions.assertThatThrownBy(() -> {
-      given.charAt(givenIndex);
-    }).isInstanceOf(StringIndexOutOfBoundsException.class);
-  }
 }
