5732c1c65121c0d1475940cf9ad0fd6af47f9e12
hum02 <ehdgma02@gmail.com>
2023-02-07 20:43:27 +0900

    refactor: junit 학습테스트 메소드명,변수명 변경

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -27,20 +28,23 @@ public class SetCollectionTest {
     }
 
     @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 테스트")
     void testSize() {
         int size = numbers.size();
         assertThat(size).isEqualTo(3);
     }
 
     @ParameterizedTest
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 테스트")
     @ValueSource(ints = {1, 2, 3})
     void testContains(int data) {
         assertTrue(numbers.contains(data));
     }
 
     @ParameterizedTest
+    @DisplayName("Set의 contains 메소드를 활용해 입력 값에 따라 결과 값이 다른 경우에 대한 테스트")
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void toUpperCase_ShouldGenerateTheExpectedUppercaseValue(Integer input, Boolean expected) {
+    void testContains_WithFalse(Integer input, Boolean expected) {
         Boolean actualValue = numbers.contains(input);
         assertEquals(expected, actualValue);
     }
