46f09d4126d180415a4172c993704080d9e4fd9f
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-07 16:32:04 +0900

    test: 값이 2개 이상인 문자열 분리 테스트 작성

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,15 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void 값이_2개_이상인_문자열_분리() {
+        String input = "1,2";
+        String result[] = input.split(",");
+
+        assertThat(result).containsExactly("1", "2");
+    }
+}
