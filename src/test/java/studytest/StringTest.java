f819c2d85be8b6c710486f52777817f739fdab5f
0chil <0@chll.it>
2023-02-07 16:27:47 +0900

    test: 범위 외에 접근시 예외가 발생한다

--- src/test/java/studytest/StringTest.java
+++ src/test/java/studytest/StringTest.java
@@ -1,9 +1,13 @@
 package studytest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringTest {
 
@@ -15,12 +19,13 @@ public class StringTest {
     }
 
     @Test
-    void 숫자_한개_입력시_한개반환(){
+    void 숫자_한개_입력시_한개반환() {
         String input = "1";
         String[] splitResult = input.split(",");
 
         assertThat(splitResult).containsExactly("1");
     }
+
     @Test
     void 괄호를_포함한_문자열에서_괄호제거() {
         String input = "(1,2)";
@@ -30,10 +35,19 @@ public class StringTest {
 
     @Test
     @DisplayName("특정위치_문자를_반환_테스트")
-    void 특정위치_문자를_반환(){
+    void 특정위치_문자를_반환() {
         String input = "abc";
         Integer index = 1;
         Character result = input.charAt(index);
         assertThat(result).isEqualTo('b');
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 3})
+    @DisplayName("범위 외에 접근시 예외가 발생한다")
+    void 범위_외에_접근시_예외_발생(int provided) {
+        String input = "abc";
+        assertThatThrownBy(()->input.charAt(provided))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
