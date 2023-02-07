ac242dad96c261c97de82da57e0f2dad38a248af
kevstevie <jianna442@gmail.com>
2023-02-07 16:13:40 +0900

    feat: Car 클래스 구현 및 테스트코드 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        this.position += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
