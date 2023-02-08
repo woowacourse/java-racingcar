8732e4df22d4359a3828cf771c13ddaf7e2025e7
java-saeng <gusdn85554@naver.com>
2023-02-08 17:37:46 +0900

    refactor : 매직넘버 상수화

--- src/main/java/validation/TryCountValidation.java
+++ src/main/java/validation/TryCountValidation.java
@@ -2,8 +2,10 @@ package validation;
 
 public class TryCountValidation {
 
+    private static final int BOUNDARY_NEGATIVE_VALUE = 0;
+
     public static void validateTryCount(int input) {
-        if(input < 0) {
+        if(input < BOUNDARY_NEGATIVE_VALUE) {
             throw new IllegalArgumentException("양수를 입력하세요.");
         }
     }
