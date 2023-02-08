9a3d77398218a820334963017797e55512cbf643
donghae <donghae1999@gmail.com>
2023-02-08 17:51:24 +0900

    feat: 시도 횟수 만큼 반복 및 출력 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,13 +3,17 @@ package controller;
 import model.Cars;
 import view.InputView;
 import view.MessageView;
+import view.OutputView;
 
 public class Controller {
     private final InputView inputView = new InputView();
     private final MessageView messageView = new MessageView();
+    private final OutputView outputView = new OutputView();
+
     public void run(){
         Cars cars = setCars();
         int tryCount = setTryCount();
+        repeatMoving(cars, tryCount);
     }
 
     private Cars setCars(){
@@ -20,5 +24,12 @@ public class Controller {
     private int setTryCount(){
         messageView.printTryCountMessage();
         return inputView.inputTryCount();
+    }
+
+    private void repeatMoving(Cars cars, int tryCount) {
+        for (int count = 0; count < tryCount; count++) {
+            cars.moveResult();
+            outputView.printResult(cars);
+        }
     }
 }
