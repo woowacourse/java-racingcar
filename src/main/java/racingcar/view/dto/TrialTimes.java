ae8a58a61b9917a7a5e94faed1463e18c33d655f
70825 <hello70825@gmail.com>
2023-02-08 15:49:24 +0900

    refactor: Dto 클래스 이름 수정 TryTimes -> TrialTimes

---
+++ src/main/java/racingcar/view/dto/TrialTimes.java
@@ -0,0 +1,9 @@
+package racingcar.view.dto;
+
+public class TrialTimes {
+    private final Integer trialTimes;
+
+    public TrialTimes(String trialTimes) {
+        this.trialTimes = Integer.parseInt(trialTimes);
+    }
+}
