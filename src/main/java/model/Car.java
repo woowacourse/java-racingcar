350e26ab24338016b933e68889addad9a627fe19
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-08 17:53:35 +0900

    feat(Car): equals, hashcode 메소드 작성

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,6 +1,7 @@
 package model;
 
 import exception.ErrorCode;
+import java.util.Objects;
 import type.CarInformation;
 
 public class Car {
@@ -9,6 +10,27 @@ public class Car {
     public Car(final String name) {
         validate(name);
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public String getName() {
+        return name;
     }
 
     private void validate(final String name) {
