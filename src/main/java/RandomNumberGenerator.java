11ce257a25fccfadbb000f3eb2812ee8505f213f
Hyeonjae-K <href.hjk@gmail.com>
2023-02-08 18:50:09 +0900

    feat: RandomNumberGenerator 객체 구현

---
+++ src/main/java/RandomNumberGenerator.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    static Random rand = new Random();
+    static final int upperBound = 10;
+
+    public static int randomNumberGenerate() {
+        return rand.nextInt(upperBound);
+    }
+}
