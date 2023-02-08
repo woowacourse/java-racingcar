1984bd7e7abbf3459f8db9a94e8f0f026d39f32a
0chil <0@chll.it>
2023-02-08 18:09:38 +0900

    test(Game): 공동 우승자가 있을 수 있다

--- src/test/java/car/GameTest.java
+++ src/test/java/car/GameTest.java
@@ -17,4 +17,14 @@ public class GameTest {
 
         assertThat(game.getWinners()).containsOnly(car2);
     }
+
+    @Test
+    void 공동_우승자가_있을_수_있다() {
+        Car car1 = new Car("땡칠", 2);
+        Car car2 = new Car("물떡", 2);
+        Car car3 = new Car("루쿠", 1);
+        Game game = new Game(List.of(car1, car2, car3));
+
+        assertThat(game.getWinners()).containsOnly(car1, car2);
+    }
 }
