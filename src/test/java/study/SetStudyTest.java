a4bb7d33eb1d9ce0de1cfdc29ae86428da8eb780
sh111-coder <ohk9134@naver.com>
2023-02-08 15:09:42 +0900

    test(SetStydtTest) : Set 입력 값에 따라 결과 값이 다른 경우 테스트

--- src/test/java/study/SetStudyTest.java
+++ src/test/java/study/SetStudyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -36,4 +37,13 @@ public class SetStudyTest {
     void confirmContainsInSet(int value) {
         Assertions.assertThat(numbers.contains(value)).isTrue();
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set 입력 값에 따라 결과 값이 다른 경우 테스트")
+    void confirmExpectedValueByInputSet(int value, boolean expected) {
+        Assertions.assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+
+
 }
