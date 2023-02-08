ddcdaefc19ca5b2021071964fd25ac6a29ad28bf
sh111-coder <ohk9134@naver.com>
2023-02-08 14:22:59 +0900

    test(StringStudyTest) : 특정 문자 위치 찾기 문자열로 특정 문자 찾기 테스트

--- src/test/java/study/StringStudyTest.java
+++ src/test/java/study/StringStudyTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringStudyTest {
@@ -13,7 +15,7 @@ public class StringStudyTest {
     class split_테스트 {
 
         @Test
-        void String_1과_2로_분리할_경우_테스트() {
+        void 1과_2로_분리할_경우_테스트() {
             String input = "1,2";
             String[] splitedInput = input.split(",");
 
@@ -21,7 +23,7 @@ public class StringStudyTest {
         }
 
         @Test
-        void String_1로만_분리할_경우_테스트() {
+        void 1로만_분리할_경우_테스트() {
             String input = "1";
             String[] splitedInput = input.split(",");
 
@@ -29,11 +31,20 @@ public class StringStudyTest {
         }
 
         @Test
-        void String_특정_문자_제거_테스트() {
+        void 특정_문자_제거_테스트() {
             String input = "(1,2)";
             String replacedInput = input.replaceAll("[()]", "");
 
             assertThat(replacedInput).isEqualTo("1,2");
+        }
+
+        @Test
+        void 특정_문자_위치_찾기_문자열로_특정_문자_찾기_테스트() {
+            String input = "abc";
+
+            assertThat(input.indexOf("a")).isEqualTo(0);
+            assertThat(input.indexOf("b")).isEqualTo(1);
+            assertThat(input.indexOf("c")).isEqualTo(2);
         }
     }
 }
