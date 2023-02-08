b2218a7b4065f5a165709feea627eadc1e1874be
hyunseo <goatsfish@gmail.com>
2023-02-08 20:38:28 +0900

    feat: CarDTO 추가

---
+++ src/main/java/racingcar/dto/CarDTO.java
@@ -0,0 +1,21 @@
+package racingcar.dto;
+
+import racingcar.domain.Car;
+
+public class CarDTO {
+	private final String carName;
+	private final int carPosition;
+
+	public CarDTO(Car car) {
+		this.carName = car.getName();
+		this.carPosition = car.getPosition();
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public int getCarPosition() {
+		return carPosition;
+	}
+}
