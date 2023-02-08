73466cd94969d4314eb494eb39396d3aae81dd90
Hyeonjae-K <href.hjk@gmail.com>
2023-02-08 19:24:21 +0900

    test :  Car 테스트 작성중

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,35 +1,33 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import domain.Car;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
-    private Car car;
+    Car car1;
+    Car car2;
+    Car car3;
+    @BeforeAll
+    static void setup(){
+        car1 = new Car("jerry");
+        car2 = new Car("jena");
+        car3 = new Car("poby");
 
-    @BeforeEach
-    void setup() {
-        car = new Car("jerry");
+        for (int i = 0; i < 5; i++){
+            car1.move();
+            car2.move();
+            car3.move();
+        }
     }
 
     @Test
     void testToString() {
-        for (int i = 0; i < 5; i++) {
-            car.move(4);
-        }
-        assertThat(car.toString().split(": ")[1].length())
-                .isEqualTo(car.getPosition());
+        int distance = car1.getDistance();
+        assertThat(car1.toString().split(": ").length).isEqualTo(distance);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
-    void moveTest(int number) {
-        for (int i = 0; i < number; i++) {
-            car.move(4);
-        }
-        Assertions.assertEquals(car.getPosition(), number);
+    @Test
+    void move() {
     }
 }
