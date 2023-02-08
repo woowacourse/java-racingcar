40bc8040e343865a69ae0029d5437d2de87ca2df
hyunseo <goatsfish@gmail.com>
2023-02-08 19:25:47 +0900

    feat: 자동차 이름들을 입력받는 기능 추가

--- src/main/java/racingcar/exception/ErrorMessages.java
+++ src/main/java/racingcar/exception/ErrorMessages.java
@@ -2,7 +2,8 @@ package racingcar.exception;
 
 public enum ErrorMessages {
 	CAR_NAME_BLANK_EXCEPTION("[ERROR] 자동차 이름은 공백이거나 빌 수 없습니다."),
-	CAR_NAME_LENGTH_EXCEPTION("[ERROR] 자동차 이름 길이는 5를 넘을 수 없습니다.");
+	CAR_NAME_LENGTH_EXCEPTION("[ERROR] 자동차 이름 길이는 5를 넘을 수 없습니다."),
+	CAR_NAME_INPUT_EXCEPTION("[ERROR] 자동차 이름들을 다시 입력해주세요.");
 
 	private final String message;
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,28 @@
 package racingcar.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import racingcar.dto.CarNamesDTO;
+import racingcar.exception.ErrorMessages;
+
 public class InputView {
+	private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+
+	private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+	public CarNamesDTO inputCarName() {
+		System.out.println(CAR_NAMES_INPUT_MESSAGE);
+		String carNames = null;
+
+		try {
+			carNames = bufferedReader.readLine();
+		} catch (IOException e) {
+			throw new RuntimeException(ErrorMessages.CAR_NAME_INPUT_EXCEPTION.getMessage());
+		}
+		return new CarNamesDTO(carNames);
+	}
+
+
 }
