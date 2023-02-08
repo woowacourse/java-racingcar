755f29deaa97e31cf013d07e6d873a7bab4f9995
70825 <hello70825@gmail.com>
2023-02-08 21:08:01 +0900

    test: 자동차 위치 출력 형식 테스트 추가

---
+++ src/test/java/racingcar/model/car/CarTest.java
@@ -0,0 +1,24 @@
+package racingcar.model.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @DisplayName("자동차의 현재 위치 형식 테스트")
+    @Test
+    void currentStateFormatTest(){
+        // given
+        Car car = new Car("pobi");
+        String expected = "pobi : -";
+
+        // when
+        String actual = car.getCurrentStateFormat();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    };
+
+}
