da1e3556e0e189c7b73365c9ba88bee7fa2451e9
seokhwan-an <asok369258@naver.com>
2023-02-08 15:03:35 +0900

    feat(domain) 자동차 이름의 길이를 검증하는 기능 추가

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Car {
+
+    private static final int NAME_MIN_LENGTH = 1;
+    private static final int NAME_MAX_LENGTH = 5;
+    private static final String NAME_LENGTH_ERROR = "[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.";
+
+    private String name;
+    private int position;
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+
+    // 자동차 이름의 길이를 검증하는 메소드
+    private void validateName(String name) {
+        if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
+    }
+}
