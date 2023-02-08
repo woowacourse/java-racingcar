48c788644e3c7ed7adab04a65cd13e451401779b
0chil <0@chll.it>
2023-02-08 17:22:01 +0900

    feat: 랜덤값이 0이상 3이하일때 정지하고, 4이상 9이하일때 전진한다

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -3,6 +3,9 @@ package car;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
@@ -19,4 +22,13 @@ public class CarTest {
 
         assertThat(car.getPosition()).isEqualTo(2);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0","3:0","4:1","9:1"}, delimiter = ':')
+    void 랜덤값이_0이상_3이하일때_정지하고_4이상_9이하일때_전진한다(int provided, int position){
+        Car car = new Car("물떡");
+        car.move(provided);
+        assertThat(car.getPosition()).isEqualTo(position);
+    }
+
 }
