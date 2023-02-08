344f0f78ddf31112845b425d146c4acd203214e5
yujamint <woawoa0513@gmail.com>
2023-02-08 15:28:39 +0900

    feat: 이동횟수 범위 유효성 검사 구현

---
+++ src/main/java/domain/MovingCount.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class MovingCount {
+    private final Integer count;
+
+    public MovingCount(Integer count) {
+        validateCountRange(count);
+        this.count = count;
+    }
+
+    private void validateCountRange(Integer count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("이동 횟수는 1이상의 수자만 가능합니다.");
+        }
+    }
+}
