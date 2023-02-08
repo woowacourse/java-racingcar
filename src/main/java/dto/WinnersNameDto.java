68dcf79037635a895254b6c80283fdb3227dc19e
kwonyj1022 <kwonyj1022@naver.com>
2023-02-08 18:01:29 +0900

    <feat>(dto/WinnersNameDto.java): 최종 우승한 Car들의 name을 가지고 있는 dto 클래스

---
+++ src/main/java/dto/WinnersNameDto.java
@@ -0,0 +1,25 @@
+package dto;
+
+import domain.Car;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinnersNameDto {
+    private final List<String> winnersNames;
+
+    public static WinnersNameDto of(List<Car> winningCars) {
+        List<String> winnersName = winningCars.stream()
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+        return new WinnersNameDto(winnersName);
+    }
+
+    private WinnersNameDto(List<String> winnersNames) {
+        this.winnersNames = winnersNames;
+    }
+    public List<String> getWinnersNames() {
+        return Collections.unmodifiableList(winnersNames);
+    }
+}
