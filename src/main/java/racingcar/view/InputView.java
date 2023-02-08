d4582f51f1421e613f7f0fddbdce8b1a230d485a
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-08 15:55:23 +0900

    feat: View에 게임 시도 횟수를 입력받는 기능을 구현한다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,18 +7,24 @@ import java.util.Scanner;
 public class InputView {
 
     private final Scanner scanner;
+    private final OutputView outputView;
 
-    public InputView(Scanner scanner) {
+    public InputView(Scanner scanner, OutputView outputView) {
         this.scanner = scanner;
+        this.outputView = outputView;
     }
 
     public List<String> readCars() {
-        OutputView outputView = new OutputView();
-        outputView.printNames();
+        OutputView.printNames();
         String line = scanner.nextLine();
         String[] splitLine = line.split(",");
         return Arrays.asList(splitLine);
     }
 
+    public int readGameRound() {
+        outputView.printGameRoundGuide();
+        String line = scanner.nextLine();
+        return Integer.parseInt(line);
+    }
 
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,7 +2,11 @@ package racingcar.view;
 
 public class OutputView {
 
-    public void printNames() {
+    public static void printNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public void printGameRoundGuide() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 }
