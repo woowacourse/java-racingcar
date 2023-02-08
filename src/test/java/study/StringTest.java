3af831d37e206cec1014df6f0e2542ca115ee934
bjk1649 <qhwns1649@naver.com>
2023-02-08 16:03:42 +0900

    test: 메서드 명 오타 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
-    @DisplayName("split 메소드₩ - 숫자 두 개 입력")
+    @DisplayName("split 메소드 - 숫자 두 개 입력")
     void splitTestWithTwoNumbers() {
         String target = "1,2";
         assertThat(target.split(",")).containsExactly("1", "2");
