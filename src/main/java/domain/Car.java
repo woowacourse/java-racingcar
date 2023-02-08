119ce378ea4d4f04c4ffa08587822162a12056f9
kokodak <kokodakadokok@gmail.com>
2023-02-08 15:13:45 +0900

    feat(car): 자동차가 가질 수 있는 속성 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
