596d9df0958e21a75802e7bc196c5b82363fd4a2
apptie <appti@naver.com>
2023-02-08 17:32:40 +0900

    test: Name 공백 테스트

--- src/test/java/model/wrapper/NameTest.java
+++ src/test/java/model/wrapper/NameTest.java
@@ -1,5 +1,6 @@
 package model.wrapper;
 
+import exception.BlankNameException;
 import exception.WrongNameLengthException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +10,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 class NameTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "abcdef"})
+    @ValueSource(strings = {"aaaaaa", "bbbbbbbbb"})
     @DisplayName("자동차 이름 길이가 유효하지 않다면 WrongNameLengthException 예외가 발생한다.")
     void wrongNameLengthTest(String wrongInput) {
         Assertions.assertThatThrownBy(() -> new Name(wrongInput))
                 .isInstanceOf(WrongNameLengthException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "             "})
+    @DisplayName("자동차 이름에 공백만이 입력된다면 BlankNameException 예외가 발생한다.")
+    void blankNameTest(String blankInput) {
+        Assertions.assertThatThrownBy(() -> new Name(blankInput))
+                .isInstanceOf(BlankNameException.class);
     }
 }
