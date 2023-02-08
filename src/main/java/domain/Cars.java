7fecde82841a3bbf07612cf093a6d4fc1ae51c4a
seokhwan-an <asok369258@naver.com>
2023-02-08 16:41:02 +0900

    feat(domain) : 생성된 자동차들의 정보를 저장하고 관리하는 클래스 생성
    세부사항
    자동차 대수는 1대 이상이어야 한다.

---
+++ src/main/java/domain/Cars.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private static final int CARS_MIN_SIZE = 1;
+    private static final String CARS_SIZE_ERROR = "[ERROR] 자동차 대수는 1이상이어야 합니다.";
+
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+
+    private void validateCarsSize(List<Car> cars) {
+        if (cars.size() < CARS_MIN_SIZE) {
+            throw new IllegalArgumentException(CARS_SIZE_ERROR);
+        }
+    }
+
+}
