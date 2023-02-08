124e9bd7612020ad06b4735f5887c130d536aa73
ingpyo <juninpyo726@gmail.com>
2023-02-08 16:54:56 +0900

    feat:자동차의 상태 출력 기능

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -10,4 +10,11 @@ public class OutputView {
     private static final String START_INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String END_MESSAGE = "가 최종 우승했습니다.";
 
+    public void printStatus(String carName, int position) {
+        System.out.print(carName + CAR_NAME_FORMAT);
+        for(int index = 0; index < position; index++) {
+            System.out.print(MOVEMENT);
+        }
+        System.out.println();
+    }
 }
