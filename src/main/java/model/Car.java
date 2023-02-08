17478f785b8dac43c7cd35d8c5d847d83d51870b
donghae <donghae1999@gmail.com>
2023-02-08 16:19:28 +0900

    feat: Car 생성자 추가
    생성자 추가 및 자동차 유효성 검사

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Car {
+    private final String EXCEPTION_MESSAGE = "자동차 이름은 1~5자 이내의 이름으로 입력하여야 합니다.";
+    public String name;
+    public int location;
+
+    public Car(String name) {
+        validCarName(name);
+        this.name = name;
+        this.location=0;
+    }
+
+    private void validCarName(String name) {
+        if (name.isEmpty() || name.length() > 5)
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+    }
+}
