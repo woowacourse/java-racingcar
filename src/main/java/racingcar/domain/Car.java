3b176e51310f613b8893fd1c35f347ebb3d33838
hyunseo <goatsfish@gmail.com>
2023-02-08 17:15:14 +0900

    feat: Car 이름을 검증하는 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,8 +5,22 @@ public class Car {
 	int position;
 
 	public Car(String name) {
+		validateNameBlank(name);
+		validateNameLength(name);
 		this.name = name;
 		position = 0;
 	}
 
+	private void validateNameBlank(String name) {
+		if (name.isBlank()) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateNameLength(String name) {
+		int nameLength = name.length();
+		if (nameLength > 5) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
