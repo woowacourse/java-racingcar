a6de204a40cd6122c2930c5c2acc3252866a10af
seokjin8678 <rnrn8678@naver.com>
2023-02-08 17:53:21 +0900

    test: RacingCarNamesRequest 테스트 코드 작성

---
+++ src/test/java/racingcar/dto/RacingCarNamesRequestTest.java
@@ -0,0 +1,21 @@
+package racingcar.dto;
+
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.exception.ExceptionMessage.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingCarNamesRequestTest {
+
+    @ParameterizedTest
+    @DisplayName("자동차의 이름이 6글자를 초과하면 예외가 발생한다.")
+    @ValueSource(strings = {"carcar", "car,carcar", "car,car2,carcar"})
+    void car_nameLengthOverThanSix(String input) {
+        // expect
+        assertThatThrownBy(() -> RacingCarNamesRequest.of(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(OUT_OF_CAR_NAME_LENGTH.getMessage());
+    }
+}
