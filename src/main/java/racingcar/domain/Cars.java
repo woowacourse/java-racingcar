a68b0f4043ff4ec65f1c33b704d6f617d664053e
hyunseo <goatsfish@gmail.com>
2023-02-08 17:33:16 +0900

    feat: Cars 클래스 기능 추가
    - Car 등록 기능 추가
    
    - Cars 중복 이름 검증 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,20 @@
 package racingcar.domain;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Cars {
+	private final Set<Car> cars = new LinkedHashSet<>();
+
+	public void generateCar(String name) {
+		Car newCar = new Car(name);
+		validateDuplicatedCarName(newCar);
+		cars.add(newCar);
+	}
+
+	private void validateDuplicatedCarName(Car newCar) {
+		if (cars.contains(newCar)) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
