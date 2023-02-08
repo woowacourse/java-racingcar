f3c3ad97f06a49d53e57d2c28e276e34e3ba5ae0
70825 <hello70825@gmail.com>
2023-02-08 15:57:31 +0900

    feat: 시도할 횟수가 정수인지 검증하는 기능 추가

--- src/main/java/racingcar/view/dto/TrialTimes.java
+++ src/main/java/racingcar/view/dto/TrialTimes.java
@@ -4,6 +4,20 @@ public class TrialTimes {
     private final Integer trialTimes;
 
     public TrialTimes(String trialTimes) {
+        validate(trialTimes);
         this.trialTimes = Integer.parseInt(trialTimes);
     }
+
+    private void validate(String trialTimes) {
+        validateNumber(trialTimes);
+    }
+
+    private void validateNumber(String trialTimes) {
+        try {
+            int number = Integer.parseInt(trialTimes);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 정수만 입력 가능합니다.");
+        }
+    }
+
 }
---
+++ src/test/java/racingcar/view/dto/TrialTimesTest.java
@@ -0,0 +1,20 @@
+package racingcar.view.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TrialTimesTest {
+
+    @DisplayName("시도 횟수는 정수값만 입력 가능합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.3", "한글", "?>"})
+    void validateNumberTest(String input) {
+        assertThatThrownBy(() -> {
+            new TrialTimes(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
