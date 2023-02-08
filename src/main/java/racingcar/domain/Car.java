494c92190756a3c5594d1c71ca81fd2005839197
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-08 20:20:34 +0900

    feat: Car 생성자 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,15 @@
 package racingcar.domain;
 
+import racingcar.util.Validator;
+
 public class Car {
+
+    private final String name;
+
+    public Car(String name) {
+        Validator.validateNameLength(name);
+        Validator.validateNotEmptyInput(name);
+
+        this.name = name;
+    }
 }
