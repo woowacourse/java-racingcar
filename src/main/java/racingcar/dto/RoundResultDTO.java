29c89164d0c03b8b242a13cea30b039ab2fec0cf
hyunseo <goatsfish@gmail.com>
2023-02-08 20:39:15 +0900

    feat: RoundResultDTO 추가
    - 매 라운드마다 경기 결과를 전달

--- src/main/java/racingcar/dto/RoundResultDTO.java
+++ src/main/java/racingcar/dto/RoundResultDTO.java
@@ -1,4 +1,21 @@
 package racingcar.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import racingcar.domain.Car;
+
 public class RoundResultDTO {
+	private final List<CarDTO> roundResult = new ArrayList<>();
+
+	public RoundResultDTO(Set<Car> cars) {
+		for (Car car : cars) {
+			roundResult.add(new CarDTO(car));
+		}
+	}
+
+	public List<CarDTO> getRoundResult() {
+		return roundResult;
+	}
 }
