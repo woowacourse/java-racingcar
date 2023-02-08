68b97a2cb6a27db9d719ee98c9d103fc39335e7c
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-08 15:57:21 +0900

    Set 학습 테스트 요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,8 +32,14 @@ public class SetTest {
     }
 
     @ParameterizedTest(name = "contains 메소드 테스트 - {0} 포함 테스트")
-    @ValueSource(ints = {1,2,3})
+    @ValueSource(ints = {1, 2, 3})
     void setContainsTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest(name = "contains 메소드 테스트 - {0} 포함, 결과 : {1}")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void setContainsTest2(int number, boolean expectedValue) {
+        assertThat(numbers.contains(number)).isEqualTo(expectedValue);
     }
 }
