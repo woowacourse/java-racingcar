6d94943fd44ae66e573d827164490eff798596f0
donghae <donghae1999@gmail.com>
2023-02-08 15:58:33 +0900

    feat: 자동차 이름 입력 요청 메시지 출력 추가

---
+++ src/main/java/view/MessageView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class MessageView {
+    private final static String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+
+    public void printCarNameMessage() {
+        System.out.println(CAR_NAME_MESSAGE);
+    }
+}
