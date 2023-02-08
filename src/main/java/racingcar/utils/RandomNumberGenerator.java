11f26f73a904888d3861f9716536aec1fdd4a73d
hyunseo <goatsfish@gmail.com>
2023-02-08 20:39:47 +0900

    feat: 0~9 사이의 랜덤 정수를 생성하는 기능 추가

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,4 +1,12 @@
 package racingcar.utils;
 
+import java.util.Random;
+
 public class RandomNumberGenerator {
+
+	private static final Random RANDOM = new Random();
+
+	public static int generateRandomNumber() {
+		return RANDOM.nextInt(10);
+	}
 }
