b98f908044791651198d649d1a81d4bee2e6df63
eunbii0213 <eunbii0213@naver.com>
2023-02-08 15:26:50 +0900

    feat: 사용자 입력 가이드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,5 +2,14 @@ package view;
 
 public class InputView {
 
-    private Input input;
+    private static final String inputCarNameMessage = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String inputTryCountMessage = "시도할 회수는 몇회인가요?";
+
+    public void printInputCarNameGuide() {
+        System.out.println(inputCarNameMessage);
+    }
+
+    public void printInputTryCountGuide() {
+        System.out.println(inputTryCountMessage);
+    }
 }
