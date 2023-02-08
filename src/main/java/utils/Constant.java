3b2785abcd5f30b3cc0e3c398d014928d43e06ac
yeonkkk <tjddus0302@gmail.com>
2023-02-08 18:27:57 +0900

    feat: Constant.java

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Constant {
+
+    public static final int MIN_NAME_SIZE = 0;
+    public static final int MAX_NAME_SIZE = 5;
+    public static final int MOVING_STANDARD_NUM = 4;
+    public static final int MAX_MOVING_NUM = 9;
+    public static final int MIN_CARS_SIZE = 1;
+    public static final int MIN_TRYTIMES = 1;
+
+}
