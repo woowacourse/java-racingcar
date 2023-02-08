e20b52b8934d315e0f7d96653c3393d1f968b6a1
hyunseo <goatsfish@gmail.com>
2023-02-08 20:42:26 +0900

    feat: 라운드 결과 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,34 @@
 package racingcar.view;
 
+import java.util.List;
+
+import racingcar.dto.CarDTO;
+import racingcar.dto.RoundResultDTO;
+
 public class OutputView {
+	private static final String RESULT_MESSAGE = "실행결과";
+	private static final String PATH = "-";
+	public void printResultMessage() {
+		System.out.println(RESULT_MESSAGE);
+	}
+	public void printRoundResult(RoundResultDTO roundResultDTO) {
+		List<CarDTO> carDTOs = roundResultDTO.getRoundResult();
+		for (CarDTO carDTO : carDTOs) {
+			String carName = carDTO.getCarName();
+			int carPosition = carDTO.getCarPosition();
+			String carPath = createCarPath(carPosition);
+			System.out.printf("%s : %s %s", carName, carPath, System.lineSeparator());
+
+		}
+	}
+
+	private String createCarPath(int carPosition) {
+		StringBuilder carPath = new StringBuilder();
+		for (int length = 0; length < carPosition; length++) {
+			carPath.append(PATH);
+		}
+
+		return carPath.toString();
+	}
+
 }
