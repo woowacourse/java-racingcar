5b1efb93a052fce5d8832067d377e7afa3f38c58
seokhwan-an <asok369258@naver.com>
2023-02-08 20:19:17 +0900

    feat(view) : 자동차 레이싱 라운드 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,12 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final String READ_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
-    public List<String> readCarNames(){
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final int ROUND_MIN_NUM = 1;
+    private static final String READ_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String READ_RACING_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ROUND_NUMBER_ERROR = "[ERROR] 시도 횟수는 1이상이어야 합니다.";
+
+    public List<String> readCarNames() {
         System.out.println(READ_CARS_NAME_MESSAGE);
         return Arrays.asList(scanner.nextLine().trim().split(","));
+    }
+
+    public int readRacingRound() {
+        System.out.println(READ_RACING_ROUND_MESSAGE);
+        int round = scanner.nextInt();
+        validateRoundInput(round);
+        return round;
+    }
+
+    private void validateRoundInput(int round) {
+        if (round < ROUND_MIN_NUM) {
+            throw new IllegalArgumentException(ROUND_NUMBER_ERROR);
+        }
     }
 
 }
