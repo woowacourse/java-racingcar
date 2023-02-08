c0e9300315ffa1304aa2bf23eabf727ef33e33a0
seokjin8678 <rnrn8678@naver.com>
2023-02-08 17:48:47 +0900

    test: RacingCarService 테스트 코드 추가 작성
    - 자동차 생성
    - 자동차 정보 반환

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.dto.RacingCarNamesRequest;
@@ -33,5 +34,33 @@ class RacingCarServiceTest {
 
         // then
         assertThat(carStatuses.get(0).getPosition()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"car:1", "car1,car2:2", "car1,car2,car3:3"}, delimiter = ':')
+    @DisplayName("자동차가 생성되어야 한다.")
+    void car_create(String input, int expected) {
+        // given
+        service.createCars(RacingCarNamesRequest.of(input));
+
+        // when
+        List<RacingCarStatusResponse> carStatuses = service.getCarStatuses();
+
+        // then
+        assertThat(carStatuses.size()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("자동차의 정보가 정상적으로 반환되어야 한다.")
+    void car_getStatues() {
+        // given
+        service.createCars(RacingCarNamesRequest.of("car1"));
+
+        // when
+        List<RacingCarStatusResponse> carStatuses = service.getCarStatuses();
+
+        // then
+        assertThat(carStatuses.get(0).getPosition()).isEqualTo(0);
+        assertThat(carStatuses.get(0).getName()).isEqualTo("car1");
     }
 }
