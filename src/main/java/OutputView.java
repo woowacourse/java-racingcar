28ff5a0066dc409c9b4aad79a06da286e7c8ba5d
cpot5620 <=cpot5620@naver.com>
2023-02-08 15:12:18 +0900

    feat(OutputView): 최종 우승자 출력

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -1,5 +1,21 @@
+import java.util.StringJoiner;
+
 public class OutputView {
+    private static final String DELIMITER = ", ";
+    private static final String PREFIX = "";
+    private static final String SUFFIX = "";
+    private static final String WINNER_MSG = "%s가 최종 우승했습니다.";
+
     public static void printState(Cars cars) {
         System.out.println(cars);
+    }
+
+    public static void printWinners(Cars cars) {
+        StringJoiner stringJoiner = new StringJoiner(DELIMITER, PREFIX, SUFFIX);
+        for (Car car : cars) {
+            stringJoiner.add(car.getName());
+        }
+
+        System.out.println(String.format(WINNER_MSG, stringJoiner));
     }
 }
