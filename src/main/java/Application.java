3aa82d968082de8c2086d296d3ce9736e7df5f1a
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-02-08 17:08:08 +0900

    feat(application): 클라이언트 단 메서드 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,22 +9,29 @@ import view.input.InputView;
 import java.util.List;
 
 public class Application {
+    static GameService gameService = new GameService(new CarRepository());
+    static GameController gameController = new GameController(gameService);
+
     public static void main(String[] args) {
+        String[] names = getNames();
+        List<Car> cars = makeCars(names);
+        int count = getCount();
+    }
+
+    private static String[] getNames() {
         String inputNames = InputView.inputCarName();
         String[] names = Converter.splitInput(inputNames);
         InputVerifier.validateNameLength(names);
+        return names;
+    }
 
-        GameService gameService = new GameService(new CarRepository());
-        GameController gameController = new GameController(gameService);
+    private static List<Car> makeCars(String[] names) {
+        return gameController.getCars(names);
+    }
 
-        List<Car> cars = gameController.getCars(names);
-
-
+    private static int getCount() {
         String inputCount = InputView.inputCount();
         InputVerifier.checkInputTypeIsNumber(inputCount);
-        int count = Converter.convertType(inputCount);
-
-
-        System.out.println("count = " + count);
+        return Converter.convertType(inputCount);
     }
 }
