fae01f822f924f0de11b86293ffdce027ddd5bba
kevstevie <jianna442@gmail.com>
2023-02-08 14:47:05 +0900

    feat: 차 이름 입력 validation

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,31 @@
+package racingcar.util;
+
+import java.util.Arrays;
+
+public class Validator {
+
+    private static final String DELIMITER = ",";
+    private Validator() {
+    }
+
+    static void validateCarNames(String carNames) {
+        String[] split = carNames.split(DELIMITER);
+        validateCarNameLength(split);
+        validateNoInput(split);
+    }
+
+    private static void validateNoInput(String[] split) {
+        long noInputCount = Arrays.stream(split).filter(s -> s.trim().equals("")).count();
+        if (noInputCount >= 1) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private static void validateCarNameLength(String[] split) {
+        long count = Arrays.stream(split).filter(s -> s.length() >= 5)
+                .count();
+        if (count >= 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
