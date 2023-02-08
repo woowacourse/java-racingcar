4a6d4c50f46c8f282e1929acd3585d07e5023041
70825 <hello70825@gmail.com>
2023-02-08 14:42:41 +0900

    feat: 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,17 @@
 package racingcar.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final String GET_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getCarNames() {
+        System.out.println(GET_CAR_NAME_MESSAGE);
+        return scanner.nextLine();
+    }
 }
