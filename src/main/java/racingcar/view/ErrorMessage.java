aec3a3cf316883e1d23d942fb5c5d7aed68d1b7a
kevstevie <jianna442@gmail.com>
2023-02-08 16:18:21 +0900

    fix: 인터페이스 내 불필요한 접근제어자 제거

--- src/main/java/racingcar/view/ErrorMessage.java
+++ src/main/java/racingcar/view/ErrorMessage.java
@@ -1,10 +1,10 @@
 package racingcar.view;
 
 public interface ErrorMessage {
-    public static final String ERROR_PREFIX = "[ERROR]";
-    public static final String CAR_NAME_LENGTH = "차 이름은 5글자 이하로 입력해야합니다.";
-    public static final String NO_INPUT = "공백 입력은 허용되지 않습니다.";
-    public static final String NOT_NUMBER = "숫자만 입력 가능합니다.";
-    public static final String NOT_POSITIVE_NUMBER = "양수만 입력 가능합니다.";
+    String ERROR_PREFIX = "[ERROR]";
+    String CAR_NAME_LENGTH = "차 이름은 5글자 이하로 입력해야합니다.";
+    String NO_INPUT = "공백 입력은 허용되지 않습니다.";
+    String NOT_NUMBER = "숫자만 입력 가능합니다.";
+    String NOT_POSITIVE_NUMBER = "양수만 입력 가능합니다.";
 
 }
