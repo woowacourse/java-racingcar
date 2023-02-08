3c41596fc1abffeedd173a9c79372872b65b571b
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-08 19:42:46 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,11 +1,9 @@
 package racingcar.util;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
     @ParameterizedTest(name = "입력이 공백이면 예외를 발생시킨다.")
