ec00f680ed152826c95662f1a4e673187e271e35
Miseong Kim <miseongk@naver.com>
2023-02-08 17:18:28 +0900

    feat: 자동차 이름 입력 시 구분자 없는 경우 예외처리

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -8,6 +8,9 @@ public class CarFactory {
     public List<Car> makeCars(String inputNames) {
         String[] names = inputNames.split(",");
 
+        if (!inputNames.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 구분자(,)가 필요해요.");
+        }
         if (names.length < 2) {
             throw new IllegalArgumentException("[ERROR] 경주는 최소 두명이 필요해요.");
         }
