17145864075567e86ee3688cb408c1605cdbd6f2
dev_kong <ahdjd5@gmail.com>
2023-02-08 20:09:15 +0900

    test: service 테스트 코드 작성

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,46 @@
+package service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import utils.exception.AlreadyDefinedFieldException;
+
+class ServiceTest {
+    Service service;
+
+    @BeforeEach
+    void setUp() {
+        service = new Service();
+    }
+
+    @Test
+    @DisplayName("중복된 이름이 입력되면 예외발생")
+    void setCarsFailTest() {
+        assertThatThrownBy(() -> service.setCars(List.of("fox", "fox")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("시도횟수가 null 이면 rule에 시도횟수 저장")
+    void setTrial() {
+        service.setTrial(4L);
+        Long result = service.getTrial();
+
+        assertThat(4L).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("시도횟수가 null이 아니면, 예외발생")
+    void setTrialFailTest() {
+        service.setTrial(4L);
+
+        assertThatThrownBy(() -> service.setTrial(4L))
+                .isInstanceOf(AlreadyDefinedFieldException.class);
+    }
+}
