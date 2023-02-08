b6514d0e695d1c537dc6445c27201264fe085cd8
yujamint <woawoa0513@gmail.com>
2023-02-08 16:04:54 +0900

    test: 자동차 이동 성공/실패 테스트 작성

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void moveSuccessTest() {
+        Car car = new Car("name");
+        Integer nowDistance = car.getDistance();
+
+        car.move(true);
+
+        Assertions.assertThat(car.getDistance()).isEqualTo(nowDistance + 1);
+    }
+
+    @Test
+    void moveFailTest() {
+        Car car = new Car("name");
+        Integer nowDistance = car.getDistance();
+
+        car.move(false);
+
+        Assertions.assertThat(car.getDistance()).isEqualTo(nowDistance);
+    }
+}
