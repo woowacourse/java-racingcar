fb93e3d523f8b93fe0a0f5ceeb2d7037fbebb203
xxeol2 <jhj01177@naver.com>
2023-02-08 16:47:16 +0900

    feat: 자동차 이름 입력값 검증 기능 구현

--- src/main/java/view/input/ErrorMessages.java
+++ src/main/java/view/input/ErrorMessages.java
@@ -2,6 +2,9 @@ package view.input;
 
 
 public enum ErrorMessages {
+    EMPTY_INPUT("입력값이 없습니다."),
+    NOT_PROPER_CAR_NAME_LENGTH(
+        "자동차 이름 길이는 " + Validator.MIN_CAR_NAME_LENGTH + "자 이상, " + Validator.MAX_CAR_NAME_LENGTH + "자 이하여야합니다.");
     private final String content;
 
     ErrorMessages(String content) {
--- src/main/java/view/input/Validator.java
+++ src/main/java/view/input/Validator.java
@@ -1,10 +1,39 @@
 package view.input;
 
+import java.util.List;
+
 public class Validator {
 
+    static final int MIN_CAR_NAME_LENGTH = 1;
+    static final int MAX_CAR_NAME_LENGTH = 5;
     private final static String ERROR_HEADER = "[ERROR] ";
 
     private static void throwError(ErrorMessages errorMessage) throws IllegalArgumentException {
+
         throw new IllegalArgumentException(ERROR_HEADER + errorMessage.getContent());
+    }
+
+    public static void isProperCarNames(final List<String> carNames) throws IllegalArgumentException {
+        isEmpty(carNames);
+        isAllProperLength(carNames);
+    }
+
+    private static void isEmpty(final List<String> carNames) throws IllegalArgumentException {
+        if (carNames.size() == 0) {
+            throwError(ErrorMessages.EMPTY_INPUT);
+        }
+    }
+
+    private static void isProperLength(final String carName) {
+        final int MIN = 1;
+        final int MAX = 5;
+        if (carName.length() < MIN || carName.length() > MAX) {
+            throwError(ErrorMessages.NOT_PROPER_CAR_NAME_LENGTH);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isAllProperLength(final List<String> carNames) throws IllegalArgumentException {
+        carNames.forEach(Validator::isProperLength);
     }
 }
