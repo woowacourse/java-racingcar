26641d8477ab02cb478b054c642e204296153eca
swonny <wonny921@gmail.com>
2023-02-08 19:18:39 +0900

    feat: 자동차 위치 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,5 @@
 package view;
 
-<<<<<<< HEAD
 import racingcar.Car;
 
 import java.util.List;
@@ -40,27 +39,6 @@ public class OutputView {
     }
 
     private static void println(String value) {
-=======
-import java.io.IOException;
-
-import static constant.Constants.READ_CAR_NAMES;
-import static constant.Constants.READ_NUMBER_OF_TRY;
-
-public class OutputView {
-    public void readCarNames() {
-        print(READ_CAR_NAMES);
-    }
-
-    public void readNumberOfTry() {
-        print(READ_NUMBER_OF_TRY);
-    }
-
-    public void printException(IOException ioException) {
-        print(ioException.getMessage());
-    }
-
-    private void print(String value) {
->>>>>>> 984262c (feat: 자동차 이름 입력)
         System.out.println(value);
     }
 }
