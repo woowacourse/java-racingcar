9397b09d55ceaa8e415576b3475c2d684b1fafae
donghae <donghae1999@gmail.com>
2023-02-08 17:13:02 +0900

    feat: 자동차 위치 이동 하기 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public class Car {
     private final String EXCEPTION_MESSAGE = "자동차 이름은 1~5자 이내의 이름으로 입력하여야 합니다.";
     public String name;
@@ -8,11 +10,15 @@ public class Car {
     public Car(String name) {
         validCarName(name);
         this.name = name;
-        this.location=0;
+        this.location = 0;
     }
 
     private void validCarName(String name) {
         if (name.isEmpty() || name.length() > 5)
             throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+    }
+
+    public void moveCar() {
+        this.location += 1;
     }
 }
