4fafcf69db2c1e5cc5477e6e7951f98104a5f47c
jeonghoon <shb03323@naver.com>
2023-02-08 16:02:15 +0900

    feat: 게임 시도 횟수 입력 UI 컨트롤러에 연결
    숫자가 아닌 문자 입력 시 예외 처리

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,21 +2,35 @@ package racingcar.controller;
 
 import racingcar.view.InputView;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class RacingCarController {
 
-    private void run() {
+    public void run() {
+        initCarNames();
+        initTries();
     }
     //,를 제거했을 때 빈 리스트가 되는 경우 예외 처리한다.
     //플레이어가 1명 이하인 경우 예외 처리한다.
-    private List<String> initCarNames() {
+    private void initCarNames() {
         String input = InputView.inputCarNames();
-        return splitCarNames(input);
+
+        List<String> carNames = splitCarNames(input);
+        //레포지토리에서 차 객체를 생성한다.
+        //car 레포에 전달.
     }
 
     private List<String> splitCarNames(String input) {
         return List.of(input.split(","));
     }
 
+    private void initTries() {
+        try{
+            int input = InputView.inputTries();
+            // 게임 도메인에 값 전달하기
+        } catch (InputMismatchException e) {
+            initTries();
+        }
+    }
 }
