377f9753232bcc46ad8513f5646dded2a91ae48d
bjk1649 <qhwns1649@naver.com>
2023-02-08 16:04:31 +0900

    test: size 메소드- 올바른 크기를 반환하는지 확인하는 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,9 @@ package study;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -14,5 +17,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 메소드 - 올바른 크기 반환")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
