49e570e10091ac07c671aecd321518ff87c27a05
ingpyo <juninpyo726@gmail.com>
2023-02-08 18:35:57 +0900

    refactor:하드코딩 제거

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -9,6 +9,7 @@ public class OutputView {
     private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String START_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String END_MESSAGE = "가 최종 우승했습니다.";
+    private static final String DIVISION_CHAR = ",";
 
     public void printStatus(String carName, int position) {
         System.out.print(carName + CAR_NAME_FORMAT);
@@ -37,6 +38,6 @@ public class OutputView {
         for (Car car : winners) {
             winnerNames.add(car.getName());
         }
-        return String.join(", ", winnerNames);
+        return String.join(DIVISION_CHAR, winnerNames);
     }
 }
