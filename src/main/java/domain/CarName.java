974eec57d3b3e7e72d304cd8631f8a3e92cb25eb
yujamint <woawoa0513@gmail.com>
2023-02-08 14:53:09 +0900

    feat: 자동차이름 5글자 이하 유효성 검사 구현

---
+++ src/main/java/domain/CarName.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class CarName {
+    private String name;
+
+    public CarName(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하입니다.");
+        }
+        if (name.length() < 1) {
+            throw new IllegalArgumentException("자동차 이름은 한 글자 이상입니다.");
+        }
+    }
+}
+
