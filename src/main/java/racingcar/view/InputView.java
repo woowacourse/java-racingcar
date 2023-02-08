1e3c4fc15169b32b30a55a8aa09931fa41fc7004
kevstevie <jianna442@gmail.com>
2023-02-08 15:27:43 +0900

    feat: 차 위치 출력 메서드

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String CAR_POSITION_SIGN = "-";
+    private static final String COLON = " : ";
     private final Scanner sc = new Scanner(System.in);
     public String[] inputCarName() {
         System.out.println(OutputView.INPUT_CAR_NAME);
@@ -19,5 +21,9 @@ public class InputView {
         String input = sc.nextLine();
         int inputNumber = Validator.validateGameTime(input);
         return inputNumber;
+    }
+
+    public void printPosition(String name, int position) {
+        System.out.print(name + COLON + CAR_POSITION_SIGN.repeat(position));
     }
 }
