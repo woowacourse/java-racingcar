692e5faa6cf055fd245c21fca949a544fcb3f60f
sh111-coder <ohk9134@naver.com>
2023-02-08 21:21:48 +0900

    feat(OutputView) : 실행 결과 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,6 +9,7 @@ public class OutputView {
 
     private static final String PRINT_READ_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String PRINT_READ_TRY_NUM_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String PRINT_RACING_RESULT_MESSAGE = "\n실행 결과";
 
     private OutputView() {}
 
@@ -29,7 +30,7 @@ public class OutputView {
     }
 
     public void printRacingResultMessage(){
-
+        printMessage(PRINT_RACING_RESULT_MESSAGE);
     }
 
     public void printCurrentRacingCarsPosition(Map<String, Integer> carPositonMap) {
