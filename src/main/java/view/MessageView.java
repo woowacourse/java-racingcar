1a3adb6278cd5e679e0e8162fb1cd00de4c884c7
donghae <donghae1999@gmail.com>
2023-02-08 16:37:43 +0900

    feat: 시도할 횟수 입력 요청 메시지 출력 추가

--- src/main/java/view/MessageView.java
+++ src/main/java/view/MessageView.java
@@ -2,8 +2,13 @@ package view;
 
 public class MessageView {
     private final static String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final static String TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?\n";
 
     public void printCarNameMessage() {
         System.out.println(CAR_NAME_MESSAGE);
+    }
+
+    public void printTryCountMessage(){
+        System.out.println(TRY_COUNT_MESSAGE);
     }
 }
