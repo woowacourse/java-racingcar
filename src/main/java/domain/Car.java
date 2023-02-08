c3e0beaa330ef0aef6515a6d59508ee3db89206d
jeomxon <jeomxon@gmail.com>
2023-02-08 17:27:10 +0900

    feat: 자동차 이름 길이에 대한 예외처리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -10,7 +10,14 @@ public class Car {
     private final List<String> position;
 
     public Car(String name) {
+        validateLengthOfName(name);
         this.name = name;
         this.position = new ArrayList<>();
+    }
+
+    private void validateLengthOfName(String name){
+        if(name.length() < 1 || name.length() > 5 ){
+            throw new IllegalArgumentException();
+        }
     }
 }
