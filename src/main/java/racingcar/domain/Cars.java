6ada752cfc6578a435894a6d9cce775d8edf031c
jiwon <ljwon77@daum.net>
2023-02-08 17:19:41 +0900

    feat: 자동차 리스트를 관리하는 일급 컬렉션 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(String carNames) {
+        this.cars = splitCarNames(carNames);
+    }
+
+    private List<Car> splitCarNames(String carNames) {
+        String[] names = carNames.split(",");
+        return Arrays.stream(names)
+                .map(Car::of)
+                .collect(Collectors.toList());
+    }
+
+    public static Cars of(String carNames) {
+        return new Cars(carNames);
+    }
+}
