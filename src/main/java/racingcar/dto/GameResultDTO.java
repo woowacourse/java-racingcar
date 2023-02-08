baca9eb7cc5ba9b25db5d60604a5610f5824ec67
hyunseo <goatsfish@gmail.com>
2023-02-08 20:57:34 +0900

    feat: GameResultDTO 클래스 추가

--- src/main/java/racingcar/dto/GameResultDTO.java
+++ src/main/java/racingcar/dto/GameResultDTO.java
@@ -1,4 +1,16 @@
 package racingcar.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameResultDTO {
+	private final List<String> winnerNames = new ArrayList<>();
+
+	public GameResultDTO(List<String> winnerNames) {
+		this.winnerNames.addAll(winnerNames);
+	}
+
+	public List<String> getWinnerNames() {
+		return winnerNames;
+	}
 }
