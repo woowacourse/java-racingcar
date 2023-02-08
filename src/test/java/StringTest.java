4ce3e1352919f9e7a0eca3011684dcdbbf2256e5
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 13:37:23 +0900

    feat(StringTest): 요구사항 3 구현

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,6 +27,17 @@ class StringTest {
     void replaceBracketTest() {
         String input = "(1,2)";
         assertThat(input.replaceAll("[()]", "")).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져오고, 위치 값을 벗어나면 예외 처리한다.")
+    void stringIndexTest() {
+        String input = "abc";
+        try{
+            System.out.println(input.charAt(5));
+        }catch(StringIndexOutOfBoundsException exception){
+            assertThatExceptionOfType(StringIndexOutOfBoundsException.class).equals(exception);
+        }
     }
 
 }
