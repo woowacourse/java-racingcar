92fe91d3953a061b835af950a4079f1c0955122c
yenawee <yaena0319@naver.com>
2023-02-08 19:56:19 +0900

    test : Winner 객체 테스트

--- src/test/java/WinnerTest.java
+++ src/test/java/WinnerTest.java
@@ -1,31 +1,29 @@
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import domain.Car;
-import domain.Winner;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WinnerTest {
     static Car car1;
     static Car car2;
     static Car car3;
     static Winner winner;
-
     @BeforeAll
-    static void setup() {
+    static void setup(){
         car1 = new Car("jerry");
         car2 = new Car("jena");
         car3 = new Car("poby");
         List<Car> cars = new ArrayList<>();
 
         winner = new Winner();
-        for (int i = 0; i < 5; i++) {
-            car1.move(4);
-            car3.move(4);
-        }
-        car2.move(4);
+
+
+        while (car1.getDistance() != 5) car1.move();
+        while (car2.getDistance() != 4) car2.move();
+        while (car3.getDistance() != 5) car3.move();
 
         cars.add(car1);
         cars.add(car2);
@@ -36,12 +34,14 @@ class WinnerTest {
 
     @Test
     void findWinnerNamesTest() {
-        assertThat(winner.getWinnerNames().toArray())
-                .containsExactly(car1.getName(), car3.getName());
+        assertThat(winner.getWinnerNames().toArray()).containsExactly(car1.getName());
+
+
     }
 
     @Test
     void testToString() {
-        assertThat(winner.toString()).contains(car3.getName());
+       assertThat(winner.toString()).contains(car1.getName(), car3.getName());
+
     }
 }
