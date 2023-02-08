74660336d913ceec69c161d3701b16f3712f64b8
70825 <hello70825@gmail.com>
2023-02-08 17:50:35 +0900

    feat: 트랙 생성자 추가

--- src/main/java/racingcar/model/track/Track.java
+++ src/main/java/racingcar/model/track/Track.java
@@ -1,4 +1,15 @@
 package racingcar.model.track;
 
+import racingcar.model.car.Cars;
+
 public class Track {
+    private final Cars cars;
+    private final int trialTime;
+
+    public Track(Cars cars, int trialTime) {
+        this.cars = cars;
+        this.trialTime = trialTime;
+    }
+
+
 }
