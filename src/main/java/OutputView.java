2e03cb41d834507f3f43e7c80b66bf1edc6223be
ingpyo <juninpyo726@gmail.com>
2023-02-08 17:28:56 +0900

    refactor:상수명 수정

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
     private static final String CAR_NAME_FORMAT = " : ";
     private static final String MOVEMENT = "-";
     private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String START_INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String START_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String END_MESSAGE = "가 최종 우승했습니다.";
 
     public void printStatus(String carName, int position) {
