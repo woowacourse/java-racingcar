d75e511b6dce536018ef1f33a340fc45dc81f453
swonny <wonny921@gmail.com>
2023-02-08 15:44:22 +0900

    test: 매개변수 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,25 +1,15 @@
 package study;
 
-<<<<<<< HEAD
-import org.assertj.core.internal.Numbers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-=======
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
->>>>>>> 0eeabe8 (test: HashSet 사이즈 테스트)
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-<<<<<<< HEAD
-import static org.junit.jupiter.api.Assertions.assertTrue;
-=======
->>>>>>> 0eeabe8 (test: HashSet 사이즈 테스트)
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -33,12 +23,10 @@ public class SetTest {
         numbers.add(3);
     }
 
-    // Test Case 구현
     @Test
     void getSize() {
         assertThat(numbers.size()).isEqualTo(3);
     }
-<<<<<<< HEAD
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
@@ -51,6 +39,5 @@ public class SetTest {
     void findNumber(int sameNumber, boolean result) {
         assertThat(numbers.contains(sameNumber)).isEqualTo(result);
     }
-=======
->>>>>>> 0eeabe8 (test: HashSet 사이즈 테스트)
+
 }
