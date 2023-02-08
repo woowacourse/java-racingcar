ff0595e71955350f86e4c51548b80592c245a771
70825 <hello70825@gmail.com>
2023-02-08 22:02:48 +0900

    test: 시도한 횟수만큼 게임이 진행되는지 확인하는 테스트 추가

--- src/test/java/racingcar/model/track/TrackTest.java
+++ src/test/java/racingcar/model/track/TrackTest.java
@@ -2,16 +2,28 @@ package racingcar.model.track;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.TestDataManager;
 import racingcar.model.car.Cars;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TrackTest {
 
     @DisplayName("시도한 횟수만큼 진행하는지 확인하는 테스트")
     @Test
     void raceCarTest() {
-        Cars cars =
+        // given
+        Cars cars = TestDataManager.getCarsByStrategies();
+        int trialTime = 3;
+        Track track = new Track(cars, trialTime);
+
+        // when
+        for (int i = 0; i <= trialTime; i++) {
+            track.race();
+        }
+
+        // then
+        assertThat(track.runnable()).isFalse();
     }
 
 }
