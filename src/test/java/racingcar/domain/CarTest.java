10ad18f624e544f9597a98c6ec2141eaecf26509
seokjin8678 <rnrn8678@naver.com>
2023-02-08 16:11:22 +0900

    test: Car 클래스 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.dto.RacingCarStatusResponse;
+
+class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("mycar");
+    }
+
+    @Test
+    @DisplayName("차를 움직였을 때 위치가 변해야 한다.")
+    void car_move() {
+        // given
+        RacingCarStatusResponse status = car.getStatus();
+        car.move();
+        RacingCarStatusResponse postStatus = car.getStatus();
+
+        // expect
+        assertThat(postStatus.getPosition()).isGreaterThan(status.getPosition());
+    }
+}
