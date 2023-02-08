5cf5d89f9d2da3cd74a07c226b93db63e6000396
sh111-coder <ohk9134@naver.com>
2023-02-08 20:07:09 +0900

    feat(RacingCars) : RacingCars 클래스, 메서드 선언

---
+++ src/main/java/model/RacingCars.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCars {
+
+    private List<Car> cars;
+
+    public RacingCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getWinners() {
+        return new ArrayList<>();
+    }
+    
+    private Car getMaxPositionCar() {
+        return new Car("", 0);
+    }
+
+    private List<Car> getMaxPositionCars(Car maxPositionCar) {
+        return new ArrayList<>();
+    }
+}
