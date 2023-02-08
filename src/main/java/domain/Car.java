cd51d4dc15c246664c5d9c9ed858eb3ebb4200a1
java-saeng <gusdn85554@naver.com>
2023-02-08 17:33:23 +0900

    refactor : 거리 default value 상수 처리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,13 +2,15 @@ package domain;
 
 public class Car {
 
+    private static final int DEFAULT_DISTANCE_VALUE = 0;
+
     private String name;
 
     private int distance;
 
     public Car(final String name) {
         this.name = name;
-        this.distance = 0;
+        this.distance = DEFAULT_DISTANCE_VALUE;
     }
 
     public void move() {
