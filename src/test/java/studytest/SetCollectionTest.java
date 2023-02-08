1b1d67ac2ab2b09a42cd30990184cb7950360071
0chil <0@chll.it>
2023-02-08 15:43:52 +0900

    test: Set에 값이 존재하는지 알 수 있다

--- src/test/java/studytest/SetCollectionTest.java
+++ src/test/java/studytest/SetCollectionTest.java
@@ -3,9 +3,13 @@ package studytest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
     @Test
@@ -17,5 +21,14 @@ public class SetCollectionTest {
         input.add(3);
 
         assertThat(input).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set에 값이 존재하는지 알 수 있다")
+    @ValueSource(ints = {1,2,3})
+    void Set에_값이_존재하는지_알_수_있다(int provided){
+        Set<Integer> set = Set.of(1, 2, 3);
+
+        assertThat(set).contains(provided);
     }
 }
