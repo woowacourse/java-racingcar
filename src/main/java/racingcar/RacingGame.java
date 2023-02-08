16de3f959e81a66da8de4ea3063832d56474e662
kevstevie <jianna442@gmail.com>
2023-02-08 15:21:40 +0900

    feat: 랜덤한 값으로 차 위치를 업데이트한다

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -5,21 +5,39 @@ import racingcar.view.InputView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class RacingGame {
 
+    private static final int RANGE_MAX = 9;
+    private static final int RANGE_MIN = 0;
+
     private final InputView inputView = new InputView();
+    private final int gameCount;
+
     private List<Car> cars = new ArrayList<>();
     private List<Car> winners = new ArrayList<>();
 
 
     public RacingGame() {
         initRacing();
+        this.gameCount = inputView.inputGameTime();
     }
 
     private void initRacing() {
         String[] input = inputView.inputCarName();
         Arrays.stream(input).forEach(s -> cars.add(new Car(s)));
+    }
+
+    private int generateRandomNumber() {
+        return (int) ((Math.random() * (RANGE_MAX - RANGE_MIN)) + RANGE_MIN);
+    }
+
+    private void updateCarPosition() {
+        for (int i = 0; i < cars.size(); i++) {
+            int randomNumber = generateRandomNumber();
+            cars.get(i).move(randomNumber);
+        }
     }
 
 }
