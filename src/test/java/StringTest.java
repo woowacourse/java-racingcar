acce68e3c635ff9c092960995e08b4f8c34525f5
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 09:49:19 +0900

    refactor(StringTest): 메서드 분리

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -6,15 +6,16 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
     @Test
-    @DisplayName(",를 기준으로 split한다.")
+    @DisplayName("요구사항 1: ,를 기준으로 split한다.")
     void splitByCommaTest() {
-        String[] arr = splitByComma("1,2");
-        assertThat(arr).contains("1", "2");
-        assertThat(arr).containsExactly("1", "2");
+        containsTest("1,2", "1", "2");
+        containsTest("1", "1");
+    }
 
-        String[] arr2 = splitByComma("1");
-        assertThat(arr2).contains("1");
-        assertThat(arr2).containsExactly("1");
+    private static void containsTest(String input, String... output) {
+        String[] arr = splitByComma(input);
+        assertThat(arr).contains(output);
+        assertThat(arr).containsExactly(output);
     }
 
     private static String[] splitByComma(String input) {
