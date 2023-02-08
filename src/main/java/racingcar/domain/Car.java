30675a5d7d5baefae4649997712d7085388f677a
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-08 20:48:46 +0900

    feat: 자동차의 이동 조건 value 기록 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,11 +5,18 @@ import racingcar.util.Validator;
 public class Car {
 
     private final String name;
-
+    private ValueLog valuelog;
     public Car(String name) {
         Validator.validateNameLength(name);
         Validator.validateNotEmptyInput(name);
 
         this.name = name;
+        this.valuelog = new ValueLog();
     }
+
+    public void addValue(int value) {
+        valuelog.add(value);
+    }
+
+
 }
---
+++ src/main/java/racingcar/domain/ValueLog.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValueLog {
+    private List<Integer> log = new ArrayList<>();
+
+    public void add(int value) {
+        log.add(value);
+    }
+}
