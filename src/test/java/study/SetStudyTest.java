f85dc4118b339104c7ab7e3a92fe657cbf09f1e0
sh111-coder <ohk9134@naver.com>
2023-02-08 14:56:26 +0900

    test(SetStudyTest) : Set 크기 확인 테스트

---
+++ src/test/java/study/SetStudyTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetStudyTest {
+
+    private Set<Integer> numbers;
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+
+    @Test
+    @DisplayName("Set 크기 확인 테스트")
+    void confirmSizeOfSet(){
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
