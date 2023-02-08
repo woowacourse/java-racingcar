ebc58a6b8ebe94984b0d299edb8f771b3c064d59
jeonghoon <shb03323@naver.com>
2023-02-08 16:23:27 +0900

    feat: 자동차 이름 목록으로 자동차 객체 생성

--- src/main/java/racingcar/domain/CarRepository.java
+++ src/main/java/racingcar/domain/CarRepository.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarRepository {
+
+    private static final int START_POINT = 0;
     private final List<Car> cars;
 
-    public CarRepository(List<Car> cars) {
-        this.cars = cars;
+    public CarRepository(List<String> carNames) {
+        this.cars = createCarsByNames(carNames);
+    }
+
+    private List<Car> createCarsByNames(List<String> carNames) {
+        return carNames.stream()
+                .map(carName -> new Car(carName, START_POINT))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
