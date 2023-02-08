27fccd54e295a5543d3dfe780833197cfbfb0757
70825 <hello70825@gmail.com>
2023-02-08 14:45:32 +0900

    feat: 시도 횟수 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String GET_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String GET_TRY_TIME_MESSAGE = "시도할 횟수는 몇회인가요?";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -12,6 +13,11 @@ public class InputView {
 
     public String getCarNames() {
         System.out.println(GET_CAR_NAME_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public String getTryTimes() {
+        System.out.println(GET_TRY_TIME_MESSAGE);
         return scanner.nextLine();
     }
 }
