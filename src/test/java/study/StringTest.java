8b78c16e8b9d98b9060d7c4fa6198d455c7e2b80
bjk1649 <qhwns1649@naver.com>
2023-02-08 15:31:28 +0900

    test: split 메소드 - 숫자 한 개를 ','으로 구분하는 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,10 +7,17 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
-    @DisplayName("split 메소드 - 숫자 두 개 입력")
+    @DisplayName("split 메소드₩ - 숫자 두 개 입력")
     void splitTestWithTwoNumbers() {
         String target = "1,2";
         assertThat(target.split(",")).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("split 메소드 - 숫자 하나 입력")
+    void splitTestOneNumber() {
+        String target = "1";
+        assertThat(target.split(",")).containsExactly("1");
     }
 
 }
