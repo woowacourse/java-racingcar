6f8e64752248232b855506b86b6402747539c81a
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 15:05:06 +0900

    feat(CarNameValidator): 6자 이상의 이름이 포함된 경우 예외처리

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -7,5 +7,10 @@ public class CarNameValidator {
         if (names.size() < 2) {
             throw new IllegalArgumentException("2개 이상의 자동차를 입력해 주세요.");
         }
+        for(String name : names){
+            if(name.length() > 5){
+                throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
+            }
+        }
     }
 }
