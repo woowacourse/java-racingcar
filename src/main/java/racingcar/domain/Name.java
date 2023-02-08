11dc23ece7c88ba5f13b3fcd7ea9665a7d9aeaf2
chaewon121 <cw121@naver.com>
2023-02-08 16:14:57 +0900

    feat: 이름에 대한 equals & hashcode 구현

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,11 +1,14 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private static final int MAX_LENGTH = 5;
     private static final int MIN_LENGTH = 0;
 
     private final String name;
+
     public Name(final String name) {
         validate(name);
         this.name = name;
@@ -22,5 +25,18 @@ public class Name {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return name.equals(name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NameTest {
@@ -31,5 +32,16 @@ public class NameTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Name(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("equals 시 같은 이름인 경우 true를 반환한다.")
+    void test_4() {
+        // given
+        Name name1 = new Name("123");
+        Name name2 = new Name("123");
+
+        // when & then
+        assertThat(name1).isEqualTo(name2);
     }
 }
