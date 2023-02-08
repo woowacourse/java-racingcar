880f030f8861da760919c3bef05141c675722171
kokodak <kokodakadokok@gmail.com>
2023-02-08 15:25:02 +0900

    feat(car): 움직일 수 있는 기능 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,6 +1,7 @@
 package domain;
 
 public class Car {
+    private static final int MOVED_LOWER_BOUND = 4;
 
     private String name;
     private int position;
@@ -8,5 +9,11 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVED_LOWER_BOUND) {
+            position++;
+        }
     }
 }
