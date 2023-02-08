96433cb5de747f1a7065d7c28e86a35e406ee5e3
yujamint <woawoa0513@gmail.com>
2023-02-08 16:31:18 +0900

    feat: Output View 구현

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,22 @@
+package view;
+
+import domain.Car;
+
+import java.util.List;
+
+public class OutputView {
+    public void printCarNameMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public void printTryCountMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printCurrentResult(List<Car> carsInfo) {
+        for (Car car : carsInfo) {
+            car.printCarInfo();
+        }
+        System.out.println();
+    }
+}
