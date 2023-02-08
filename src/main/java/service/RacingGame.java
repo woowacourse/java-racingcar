57e2fc16b3345b70af6c6efd9796681aacf3df72
SproutMJ <mjk54565@gmail.com>
2023-02-08 17:05:39 +0900

    feat: 랜덤값에 따라 모든 자동차에게 움직이도록 하는 기능

--- src/main/java/service/RacingGame.java
+++ src/main/java/service/RacingGame.java
@@ -7,16 +7,41 @@ import java.util.Collections;
 import java.util.List;
 
 public class RacingGame {
+    private static final int RANDOM_START_INCLUSIVE = 0;
+    private static final int RANDOM_END_INCLUSIVE = 9;
+    private static final int PROCESS_MOVING_MIN_NUMBER = 4;
+
     private List<Car> cars;
+    private NumberGenerator numberGenerator;
 
-
-    public RacingGame() {
+    public RacingGame(NumberGenerator numberGenerator) {
         this.cars = new ArrayList<>();
+        this.numberGenerator = numberGenerator;
     }
 
     public void generateCars(List<String> carNames) {
         for (String carName : carNames) {
             cars.add(new Car(carName));
+        }
+    }
+
+    public void processAllCars() {
+        for (Car car : cars) {
+            int random = numberGenerator.generate(0, 9);
+            process(car, random);
+        }
+    }
+
+    private void process(Car car, int value) {
+        validateProcessValue(value);
+        if (value >= PROCESS_MOVING_MIN_NUMBER) {
+            car.move();
+        }
+    }
+
+    private void validateProcessValue(int value) {
+        if (value < RANDOM_START_INCLUSIVE || value > RANDOM_END_INCLUSIVE) {
+            throw new IllegalArgumentException("자동차 전진을 위한 값은 지정된 범위 내여야 합니다.");
         }
     }
 
--- src/test/java/service/RacingGameTest.java
+++ src/test/java/service/RacingGameTest.java
@@ -1,19 +1,42 @@
 package service;
 
+import domain.Car;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameTest {
     @Test
     void 이름들로_자동차들_생성_테스트() {
-        RacingGame racingGame = new RacingGame();
+        RacingGame racingGame = new RacingGame(new RandomNumberGenerator());
         List<String> carNames = Arrays.asList("aaa", "bbb", "ccc");
         racingGame.generateCars(carNames);
 
         assertThat(racingGame.getCars().size()).isEqualTo(carNames.size());
+    }
+
+    @Test
+    void 값에_따른_자동차_진행_테스트() {
+        RacingGame racingGame = new RacingGame(new TestNumberGenerator());
+        List<String> carNames = new ArrayList<>(List.of("aaa", "bbb", "ccc"));
+        racingGame.generateCars(carNames);
+
+        racingGame.processAllCars();
+        List<Car> resultCars = racingGame.getCars();
+
+        assertThat(resultCars.get(0).getPosition()).isEqualTo(0);
+        assertThat(resultCars.get(1).getPosition()).isEqualTo(0);
+        assertThat(resultCars.get(2).getPosition()).isEqualTo(1);
+    }
+
+    private static class TestNumberGenerator implements NumberGenerator {
+        Queue<Integer> numbers = new LinkedList<>(List.of(0, 3, 4));
+
+        @Override
+        public int generate(int startInclusive, int endInclusive) {
+            return numbers.poll();
+        }
     }
 }
