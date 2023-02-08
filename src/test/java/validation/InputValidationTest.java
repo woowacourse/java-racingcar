29aa2c9294d7818f1f5294237e6d8b00633b2900
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-08 20:50:15 +0900

    test(InputValidationTest): 양수만 입력 유효성 검사 기능 테스트 작성

--- src/test/java/validation/InputValidationTest.java
+++ src/test/java/validation/InputValidationTest.java
@@ -1,11 +1,13 @@
 package validation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import dto.InputValidationRequest;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import type.ValidationType;
 import validation.impl.EmptyValueValidationChain;
+import validation.impl.PositiveNumberValidationChain;
 
 public class InputValidationTest {
 
@@ -21,7 +23,21 @@ public class InputValidationTest {
 
         //when
         //then
-        Assertions.assertThatThrownBy(() -> validator.validate(request))
+        assertThatThrownBy(() -> validator.validate(request))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void positiveNumberValidationTest() {
+        //given
+        validator = new PositiveNumberValidationChain();
+        String target = "0";
+        InputValidationRequest request = new InputValidationRequest(
+            List.of(ValidationType.POSITIVE_NUMBER), target);
+
+        //when
+        //then
+        assertThatThrownBy(() -> validator.validate(request))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
