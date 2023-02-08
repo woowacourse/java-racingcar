4c6c21b04693db012e1f3b2160d07fc927f4ad90
SproutMJ <mjk54565@gmail.com>
2023-02-08 13:50:11 +0900

    test: Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void 초기화() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void Set_크기_확인_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void Set_값_존재_참_테스트(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void Set_값_존재_참_또는_거짓_테스트(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+
+
+}
