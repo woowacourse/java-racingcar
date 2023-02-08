b9a9411249ef29bc35a90ed3b20d35b790c12e84
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 09:33:39 +0900

    refactor(StringTest): 메서드 분리

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -8,15 +8,17 @@ class StringTest {
     @Test
     @DisplayName(",를 기준으로 split한다.")
     void splitByCommaTest() {
-        String input = "1,2";
-        String[] arr = input.split(",");
+        String[] arr = splitByComma("1,2");
         assertThat(arr).contains("1", "2");
         assertThat(arr).containsExactly("1", "2");
 
-        String input2 = "1";
-        String[] arr2 = input2.split(",");
+        String[] arr2 = splitByComma("1");
         assertThat(arr2).contains("1");
         assertThat(arr2).containsExactly("1");
+    }
+
+    private static String[] splitByComma(String input) {
+        return input.split(",");
     }
 
 }
