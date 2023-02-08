9c699fa3166c29768ecc29e78b356da51b6ade50
ingpyo <juninpyo726@gmail.com>
2023-02-08 16:54:05 +0900

    feat:출력에 필요한 문자열 상수화

---
+++ src/main/java/OutputView.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAGE = "실행결과";
+    private static final String CAR_NAME_FORMAT = " : ";
+    private static final String MOVEMENT = "-";
+    private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String START_INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String END_MESSAGE = "가 최종 우승했습니다.";
+
+}
