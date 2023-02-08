8e95ba21949e3f65f24f91c3f56850ce66ed7030
xxeol2 <jhj01177@naver.com>
2023-02-08 18:35:23 +0900

    fix: 빈문자열 입력 검증 로직 변경, throw 중복 제거
    세부 변경 사항 : ERROR_HEADER 접근제어자 변경, 상수 재사용

--- src/main/java/view/input/Validator.java
+++ src/main/java/view/input/Validator.java
@@ -6,30 +6,25 @@ public class Validator {
 
     static final int MIN_CAR_NAME_LENGTH = 1;
     static final int MAX_CAR_NAME_LENGTH = 5;
-    private final static String ERROR_HEADER = "[ERROR] ";
+    static final String ERROR_HEADER = "[ERROR] ";
 
-    private static void throwError(ErrorMessages errorMessage) throws IllegalArgumentException {
-
+    private static void throwError(ErrorMessages errorMessage) {
         throw new IllegalArgumentException(ERROR_HEADER + errorMessage.getContent());
     }
 
     public static void isProperCarNames(final List<String> carNames) throws IllegalArgumentException {
-        isEmpty(carNames);
         isAllProperLength(carNames);
     }
 
-    private static void isEmpty(final List<String> carNames) throws IllegalArgumentException {
-        if (carNames.size() == 0) {
+    public static void isNotEmpty(final String input) throws IllegalArgumentException {
+        if (input.equals("")) {
             throwError(ErrorMessages.EMPTY_INPUT);
         }
     }
 
-    private static void isProperLength(final String carName) {
-        final int MIN = 1;
-        final int MAX = 5;
-        if (carName.length() < MIN || carName.length() > MAX) {
+    private static void isProperLength(final String carName) throws IllegalArgumentException {
+        if (carName.length() < MIN_CAR_NAME_LENGTH || carName.length() > MAX_CAR_NAME_LENGTH) {
             throwError(ErrorMessages.NOT_PROPER_CAR_NAME_LENGTH);
-            throw new IllegalArgumentException();
         }
     }
 
