4269d7e8daa192f6bad1b5a18f6698a519213a80
chaewon121 <cw121@naver.com>
2023-02-08 15:42:56 +0900

    feat: 랜덤숫자 생성 기능 구현

---
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class NumberGeneratorTest {
+
+    @Disabled("많은 반복, 따로 테스트")
+    @Test
+    @DisplayName("generate() 시 최솟값과 최댓값 사이의 임의의 값을 생성한다.")
+    @RepeatedTest(2000)
+    void test_1() {
+        // given
+        NumberGenerator generator = new NumberGenerator();
+
+        // when
+        int generatedNumber = generator.generate(0, 9);
+        // Math.random() 같은 함수는 어떻게 테스트하면 좋을지?
+
+        // then
+        Assertions.assertThat(generatedNumber).isGreaterThanOrEqualTo(0);
+        Assertions.assertThat(generatedNumber).isLessThanOrEqualTo(9);
+    }
+}
