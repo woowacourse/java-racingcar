23aebe71ca9427edeac106f2df6c671fa80c0032
woo-chang <clllickme@naver.com>
2023-02-08 15:13:07 +0900

    feat: 차 이동 기능 구현
    Co-authored-by: greeng00se <thegreengoose7@gmail.com>

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,18 +1,33 @@
 package domain;
 
-class Car {
+public class Car {
 
-    private static final int CAR_NAME_LOWER_BOUND = 0;
-    private static final int CAR_NAME_UPPER_BOUND = 5;
+    private static final int NAME_LOWER_BOUND = 0;
+    private static final int NAME_UPPER_BOUND = 5;
+    private static final int MOVE_LOWER_BOUND = 4;
     private static final String INVALID_NAME_LENGTH_MESSAGE = "차의 이름은 1자 이상 ~ 5자 이하여야 합니다.";
+
+    private String name;
+    private int position = 0;
 
     public Car(String name) {
         validate(name);
+        this.name = name;
     }
 
     private void validate(String name) {
-        if (CAR_NAME_LOWER_BOUND == name.length() || CAR_NAME_UPPER_BOUND < name.length()) {
+        if (NAME_LOWER_BOUND == name.length() || NAME_UPPER_BOUND < name.length()) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH_MESSAGE);
         }
+    }
+
+    public void move(final int value) {
+        if (value >= MOVE_LOWER_BOUND) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,8 +1,11 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
@@ -11,9 +14,19 @@ public class CarTest {
     @ValueSource(strings = {"dazzle", ""})
     @DisplayName("이름의 길이가 없거나 5보다 크다면 예외를 던진다.")
     void should_throwException_when_invalidNameLength(String name) {
-        Assertions.assertThatThrownBy(() -> new Car(name))
+        assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("차의 이름은 1자 이상 ~ 5자 이하여야 합니다.");
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"4,1", "3,0"})
+    @DisplayName("값을 입력받고 4 이상인 경우 전진하고, 3 이하인 경우 멈춘다.")
+    void should_move_when_valueIsMoreThanFour(int value, int position) {
+        Car car = new Car("Herb");
+
+        car.move(value);
+
+        assertThat(car.getPosition()).isEqualTo(position);
+    }
 }
