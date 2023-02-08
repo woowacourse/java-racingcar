4d6e5afb8fde918bf33b3b83d322a48ab010aebd
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-08 20:04:13 +0900

    feat: "실행 결과"를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 public class OutputView {
     private static final String ASK_INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String ASK_INPUT_NUMBER_OF_ROUNDS = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_STARTING_MESSAGE = "실행 결과";
 
     public void printAskingInputCarNames() {
         System.out.println(ASK_INPUT_CAR_NAMES_MESSAGE);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public void printAskingInputNumberOfRounds() {
         System.out.println(ASK_INPUT_NUMBER_OF_ROUNDS);
+    }
+
+    public void printResultStartingMessage() {
+        System.out.println(RESULT_STARTING_MESSAGE);
     }
 }
