0db2d56499da4bbc9d4bfcfc40ab730ad795a052
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 13:48:04 +0900

    feat(SetTest): 요구사항 3 구현

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,10 +1,12 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -22,12 +24,18 @@ public class SetTest {
     // Test Case 구현
     @Test
     void sizeTest() {
-        assertThat(numbers.size()).isEqualTo(3);
+        assertEquals(numbers.size(), 3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void contains(int input) {
+    void containsValueSourceTest(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsCsvSourceTest(String input, String expected) {
+        assertEquals(Boolean.parseBoolean(expected), numbers.contains(Integer.parseInt(input)));
     }
 }
