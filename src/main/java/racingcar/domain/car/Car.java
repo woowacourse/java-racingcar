2bd7663572f41c3f1fb080bc4cb1c9c4861a063e
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-08 17:25:23 +0900

    feat: 자동차의 이름을 검증하는 기능을 구현한다

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -3,8 +3,33 @@ package racingcar.domain.car;
 public class Car {
 
     private final String name;
+    private int position;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
+        this.position = 0;
     }
+
+    private void validate(String name) {
+        if (!isValidateName(name)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 영어로만 구성되어야 합니다.");
+        }
+        if (isOutOfRange(name)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 한 글자 이상 다섯 글자 이하여야 합니다.");
+        }
+    }
+
+    private boolean isOutOfRange(String name) {
+        return name.length() > 5 || name.length() < 1;
+    }
+
+    private boolean isValidateName(String name) {
+        return name.matches("[a-zA-Z]]");
+    }
+
+    public void updatePosition() {
+        this.position++;
+    }
+
 }
