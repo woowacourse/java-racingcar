0a82766a4a44389cfc9127c77b8aad883ebef904
hyunseo <goatsfish@gmail.com>
2023-02-08 21:53:33 +0900

    feat: 에러 메시지 추가

--- src/main/java/racingcar/exception/ErrorMessages.java
+++ src/main/java/racingcar/exception/ErrorMessages.java
@@ -3,6 +3,8 @@ package racingcar.exception;
 public enum ErrorMessages {
 	CAR_NAME_BLANK_EXCEPTION("[ERROR] 자동차 이름은 공백이거나 빌 수 없습니다."),
 	CAR_NAME_LENGTH_EXCEPTION("[ERROR] 자동차 이름 길이는 5를 넘을 수 없습니다."),
+	CARS_SIZE_EXCEPTION("[ERROR] 2개 이상의 자동차 이름을 입력해 주세요."),
+	CAR_NAME_DUPLICATE_EXCEPTION("[ERROR] 자동차 이름은 중복될 수 없습니다."),
 	CAR_NAME_INPUT_EXCEPTION("[ERROR] 자동차 이름들을 다시 입력해주세요."),
 	GAME_ROUND_INPUT_EXCEPTION("[ERROR] 시도할 게임 회수를 다시 입력해주세요."),
 	GAME_ROUND_INPUT_FORMAT_DIGIT_EXCEPTION("[ERROR] 시도할 게임 회수는 정수형이어야 합니다."),
