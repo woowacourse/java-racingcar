1384d319beccdd19259d1e045a41cdbdb40851be
0chil <0@chll.it>
2023-02-08 19:38:55 +0900

    feat(RandomMoveChance): 0~9의 랜덤값으로 이동을 결정한다

---
+++ src/main/java/car/RandomMoveChance.java
@@ -0,0 +1,17 @@
+package car;
+
+import java.util.Random;
+
+public class RandomMoveChance implements MoveChance {
+    private static final int MIN_MOVABLE_NUMBER = 4;
+
+    @Override
+    public boolean isMovable() {
+        return makeRandomNumber() < MIN_MOVABLE_NUMBER;
+    }
+
+    private int makeRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+}
