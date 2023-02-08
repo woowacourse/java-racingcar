f79358cc4114cf00b0e876a0df0911bbbc73b23a
swonny <wonny921@gmail.com>
2023-02-08 15:49:32 +0900

    test: 매개변수 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.internal.Numbers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,8 +33,13 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void hasNumber1(int sameNumber) {
+    void hasNumber(int sameNumber) {
         assertThat(numbers.contains(sameNumber)).isTrue();
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void findNumber(int sameNumber, boolean result) {
+        assertThat(numbers.contains(sameNumber)).isEqualTo(result);
+    }
 }
