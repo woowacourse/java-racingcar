e53032e578987bf466d532b4b54c36e74f79cf6d
BGuga <tmdwns9913@gmail.com>
2023-02-08 18:36:35 +0900

    feat(Position): add Position

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.Comparator;
+
+public class Position implements Comparable<Position> {
+    private final Integer INITIAL_DEFAULT_VALUE = 0;
+    private final Integer MOVE_STEP = 1;
+    private Integer value;
+
+    public Position() {
+        this.value = INITIAL_DEFAULT_VALUE;
+    }
+
+    public void increasePosition() {
+        this.value += MOVE_STEP;
+    }
+
+    @Override
+    public int compareTo(Position o) {
+        if(this.value > o.value){
+            return 1;
+        }
+        if(this.value == o.value){
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Position position = (Position)o;
+        return this.value == position.value;
+    }
+}
