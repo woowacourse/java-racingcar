2a8b230a772753453582e25eb71febe2fef84c66
woosung1223 <dntjd991223@naver.com>
2023-02-08 23:10:12 +0900

    feat: 경주 흐름을 제어하는 컨트롤러 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import controller.RacingController;
+import view.input.InputView;
+import view.output.OutputView;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        new RacingController(new InputView(scanner), new OutputView()).start();
+    }
+}
---
+++ src/main/java/controller/RacingController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import domain.RacingGame;
+import view.input.InputView;
+import view.output.OutputView;
+
+public class RacingController {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private RacingGame racingGame;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        makeRacingGame();
+        startRacingGame();
+        makeRacingGameResult();
+    }
+
+    private void makeRacingGame() {
+        try {
+            this.racingGame = new RacingGame(inputView.readCarName());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            makeRacingGame();
+        }
+    }
+
+    private void startRacingGame() {
+        try {
+            int gameTry = inputView.readGameTry();
+            outputView.printGameResultMessage();
+            for (int i = 0; i < gameTry; i++) {
+                racingGame.start();
+                outputView.printRacingStatus(racingGame.getCars());
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            startRacingGame();
+        }
+    }
+
+    private void makeRacingGameResult() {
+        outputView.printRacingStatus(racingGame.getCars());
+        outputView.printRacingWinners(racingGame.getWinners());
+    }
+}
