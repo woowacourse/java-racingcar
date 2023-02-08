6c76208c91ac147e2ad35e7b8b5804b680b88c74
kevstevie <jianna442@gmail.com>
2023-02-08 14:01:38 +0900

    feat: Junit study String 요구사항 1

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StringTest {
+
+    @Test
+    @DisplayName("split 작동 테스트")
+    void splitTest() {
+        String test = "1,2";
+        String[] split = test.split(",");
+
+        assertThat(split).contains("1")
+                .contains("2");
+    }
+
+    @Test
+    @DisplayName("\"1\"이 케이스로 주어졌을 때 ,로 split")
+    void splitTest2() {
+        String test = "1";
+        String[] split = test.split(",");
+
+        assertThat(split).containsExactly("1");
+    }
+}
