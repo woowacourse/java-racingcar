b982ff632f23cd191cddc08d21c228cb17c08119
kevstevie <jianna442@gmail.com>
2023-02-08 15:59:59 +0900

    feat: 우승자 출력 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,23 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String CAR_POSITION_SIGN = "-";
     private static final String COLON = " : ";
+    private static final String RESULT_POSTFIX = "가 최종 우승했습니다.";
+
+
     public void printPosition(String name, int position) {
-        System.out.print(name + COLON + CAR_POSITION_SIGN.repeat(position));
+        System.out.println(name + COLON + CAR_POSITION_SIGN.repeat(position));
+    }
+
+    public void printWinners(List<String> winners) {
+        int beginIndex = 1;
+        int endIndex = winners.toString().length() - 1;
+
+        System.out.print(winners.toString().substring(beginIndex, endIndex));
+        System.out.println(RESULT_POSTFIX);
     }
 }
