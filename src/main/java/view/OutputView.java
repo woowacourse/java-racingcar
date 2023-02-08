d68693e29ff44afd08c695fc5b0b9882c33f85ff
sh111-coder <ohk9134@naver.com>
2023-02-08 21:17:41 +0900

    feat(OutputView) : 자동차 이름 입력 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
 
     private static final OutputView INSTANCE = new OutputView();
 
+    private static final String PRINT_READ_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+
     private OutputView() {}
 
     public static OutputView getInstance() {
@@ -14,11 +16,11 @@ public class OutputView {
     }
 
     private void printMessage(String message) {
-
+        System.out.println(message);
     }
 
     public void printReadCarNamesMessage() {
-
+        printMessage(PRINT_READ_CAR_NAMES_MESSAGE);
     }
 
     public void printReadTryNumMessage() {
