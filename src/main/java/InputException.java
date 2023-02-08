0bdd24c12b1d0926ebbd178d3d73d7bfdfc47303
ingpyo <juninpyo726@gmail.com>
2023-02-08 18:31:04 +0900

    feat:시도 횟수 예외처리 기능

--- src/main/java/InputException.java
+++ src/main/java/InputException.java
@@ -4,4 +4,9 @@ public class InputException {
             throw new IllegalArgumentException("[ERROR] 자동차 이름의 길이가 1이상 4자 이하여야 합니다.");
         }
     }
+    public void checkInputCountLength(int count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("[ERROR] 0보다 큰 숫자를 입력해야 합니다.");
+        }
+    }
 }
