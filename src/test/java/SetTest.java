e9fb75ddcf3ba4c9f1599c04e92923c2fcdbfcc9
Eunkeee <eunkeeeee@gmail.com>
2023-02-08 13:41:14 +0900

    feat(SetTest): 요구사항 1 구현

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,25 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void sizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
