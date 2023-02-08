4bbcd6d5ea1be66184ce3e8a377f63f4d15a3194
eunbii0213 <eunbii0213@naver.com>
2023-02-08 15:27:54 +0900

    refactor: 사용자 입력 가이드 변수명 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,14 +2,14 @@ package view;
 
 public class InputView {
 
-    private static final String inputCarNameMessage = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static final String inputTryCountMessage = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printInputCarNameGuide() {
-        System.out.println(inputCarNameMessage);
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
     }
 
     public void printInputTryCountGuide() {
-        System.out.println(inputTryCountMessage);
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
     }
 }
