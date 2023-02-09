d272171fdeffab12a46022672f1af4e45b3acce4
0chil <0@chll.it>
2023-02-09 10:39:49 +0900

    style: 포맷팅

--- src/test/java/studytest/SetCollectionTest.java
+++ src/test/java/studytest/SetCollectionTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class SetCollectionTest {
     @Test
     @DisplayName("Set의 사이즈를 반환한다")
-    void Set의_사이즈를_반환한다(){
+    void Set의_사이즈를_반환한다() {
         HashSet<Integer> input = new HashSet<>();
         input.add(1);
         input.add(2);
@@ -28,8 +28,8 @@ public class SetCollectionTest {
 
     @ParameterizedTest
     @DisplayName("Set에 값이 존재하는지 알 수 있다")
-    @ValueSource(ints = {1,2,3})
-    void Set에_값이_존재하는지_알_수_있다(int provided){
+    @ValueSource(ints = {1, 2, 3})
+    void Set에_값이_존재하는지_알_수_있다(int provided) {
         Set<Integer> set = Set.of(1, 2, 3);
 
         assertThat(set).contains(provided);
@@ -37,9 +37,9 @@ public class SetCollectionTest {
 
     @ParameterizedTest
     @DisplayName("Set에 값의 유무를 알 수 있다")
-    @CsvSource(value = {"1,2,3=true","4,5=false"}, delimiter = '=')
-    void Set에_값의_유무를_알_수_있다(String provided, boolean expected){
-        Set<Integer> set = Set.of(1,2,3);
+    @CsvSource(value = {"1,2,3=true", "4,5=false"}, delimiter = '=')
+    void Set에_값의_유무를_알_수_있다(String provided, boolean expected) {
+        Set<Integer> set = Set.of(1, 2, 3);
         List<Integer> integers = parseIntegers(provided);
         assertThat(set.containsAll(integers)).isEqualTo(expected);
     }
