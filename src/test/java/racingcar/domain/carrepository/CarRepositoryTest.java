692906824ad69aee24bfaf09fc48d9fbbfb8b64d
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 12:11:16 +0900

    test: CarRepository 의 생성 및 검증 기능을 테스트한다

--- src/test/java/racingcar/domain/carrepository/CarRepositoryTest.java
+++ src/test/java/racingcar/domain/carrepository/CarRepositoryTest.java
@@ -11,11 +11,30 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarRepositoryTest {
 
     @ParameterizedTest
-    @MethodSource("getCarRepository")
+    @MethodSource("getCars")
+    @DisplayName("두 개 이상이고 중복이 없는 자동차 객체의 배열이 입력되었을 때, CarRepository 객체가 생성되는지 확인")
+    void validate_test(List<Car> cars) {
+        CarRepository carRepository = new CarRepository(cars);
+
+        assertThat(carRepository).isInstanceOf(CarRepository.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getWrongCars")
+    @DisplayName("이름이 중복되거나 하나의 자동차만 입력되었을 때, 오류를 발생시키는지 확인")
+    void validate_error_test(List<Car> cars) {
+        assertThatThrownBy(() -> new CarRepository(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCarRepositoryAndRandomNumber")
     @DisplayName("자동차 이동 조건에 대한 서로 다른 숫자가 입력되었을 때, 자동차 이동 여부를 확인")
     void movePosition(List<Car> cars, int randomNumber, int expectedPosition) {
         CarRepository carRepository = new CarRepository(cars);
@@ -31,7 +50,21 @@ class CarRepositoryTest {
     void findWinner() {
     }
 
-    static Stream<Arguments> getCarRepository() {
+    static Stream<Arguments> getCars() {
+        return Stream.of(
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("poby"))),
+                Arguments.arguments(List.of(new Car("poy"), new Car("oioy")))
+        );
+    }
+
+    static Stream<Arguments> getWrongCars() {
+        return Stream.of(
+                Arguments.arguments(List.of(new Car("poy"))),
+                Arguments.arguments(List.of(new Car("poy"), new Car("poy")))
+                );
+    }
+
+    static Stream<Arguments> getCarRepositoryAndRandomNumber() {
         return Stream.of(
                 Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 4, 1),
                 Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 5, 1),
