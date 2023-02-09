bec34fc38fcced87da3dbe33d500319928c6224a
Eunkeee <eunkeeeee@gmail.com>
2023-02-09 10:56:04 +0900

    refactor(MainController): 작동 방식 개선

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 import model.Car;
 import model.CarRepository;
 import view.InputView;
@@ -10,37 +13,68 @@ public class MainController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final Map<GameStatus, Supplier<GameStatus>> gameGuide;
 
     public MainController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.gameGuide = initializeGameGuide();
+    }
+
+    private Map<GameStatus, Supplier<GameStatus>> initializeGameGuide() {
+        Map<GameStatus, Supplier<GameStatus>> gameGuide = new EnumMap<>(GameStatus.class);
+        gameGuide.put(GameStatus.SET_CARS, this::setCars);
+        gameGuide.put(GameStatus.MOVE_CARS, this::moveCars);
+        return gameGuide;
     }
 
     public void play() {
-        try {
-            setCars();
-            moveCars();
-        } catch (IllegalArgumentException exception) {
-            outputView.printExceptionMessage(exception);
+        GameStatus gameStatus = progress(GameStatus.SET_CARS);
+        while (gameStatus.isContinue(gameStatus)) {
+            gameStatus = progress(gameStatus);
         }
     }
 
-    private void moveCars() {
+    private GameStatus progress(GameStatus gameStatus) {
+        try {
+            return gameGuide.get(gameStatus).get();
+        } catch (IllegalArgumentException exception) {
+            outputView.printExceptionMessage(exception);
+            return GameStatus.APPLICATION_EXIT;
+        } catch (NullPointerException exception) {
+            return GameStatus.APPLICATION_EXIT;
+        }
+    }
+
+    private GameStatus moveCars() {
         int moveCount = inputView.readMoveCount();
         outputView.printResultMessage();
         moveAllCars(moveCount);
         outputView.printWinners(CarRepository.getWinners());
+        return GameStatus.APPLICATION_EXIT;
     }
 
-    private void setCars() {
+    private GameStatus setCars() {
         List<String> carNames = inputView.readCarNames();
         carNames.stream().map(Car::new).forEach(CarRepository::addCars);
+        return GameStatus.MOVE_CARS;
     }
 
     private void moveAllCars(int moveCount) {
         for (int i = 0; i < moveCount; i++) {
             CarRepository.moveAllCars();
             outputView.printResult(CarRepository.cars());
+        }
+    }
+
+    private enum GameStatus {
+        SET_CARS, MOVE_CARS, APPLICATION_EXIT;
+
+        GameStatus() {
+        }
+
+        boolean isContinue(GameStatus gameStatus) {
+            return !gameStatus.equals(APPLICATION_EXIT);
         }
     }
 }
