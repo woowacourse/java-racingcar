73ddb246b44b0c4ac2567925eb4fa7b60c92b928
seokhwan-an <asok369258@naver.com>
2023-02-09 11:04:06 +0900

    feat(view) : 최종 결과 문구를 출력하는 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,11 +2,14 @@ package view;
 
 import domain.Car;
 import domain.Cars;
+import java.util.List;
 
 public class OutputView {
 
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String CAR_RESULT_FORMAT = "%s : %s";
+    private static final String WINNER_MESSAGE_FORMAT = "가 최종 우승했습니다.";
+    private static final String NAME_DELIMITER = ", ";
 
     public void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
@@ -18,5 +21,15 @@ public class OutputView {
                 String.format(CAR_RESULT_FORMAT, car.getName(), car.getMovePosition()));
         }
         System.out.println();
+    }
+
+    public void printFinalResult(Cars cars) {
+        List<Car> winners = cars.decideWinner();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : winners) {
+            stringBuilder.append(car.getName() + NAME_DELIMITER);
+        }
+        String winnerNames = stringBuilder.toString();
+        System.out.print(winnerNames.substring(0, winnerNames.length()-1) + WINNER_MESSAGE_FORMAT);
     }
 }
