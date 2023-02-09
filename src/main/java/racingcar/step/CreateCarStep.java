f1f2edb14a361b420ff329adc7b2c354493874f7
chaewon121 <cw121@naver.com>
2023-02-09 10:45:40 +0900

    feat: 자동차를 입력받는 단계 구현

---
+++ src/main/java/racingcar/step/CreateCarStep.java
@@ -0,0 +1,24 @@
+package racingcar.step;
+
+import racingcar.controller.RacingCarController;
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+
+public class CreateCarStep extends RacingCarApplicationStep {
+
+    public CreateCarStep(final RacingCarController controller) {
+        super(controller);
+    }
+
+    @Override
+    public boolean executable() {
+        return true;
+    }
+
+    @Override
+    public Step execute() {
+        String carNames = InputView.inputCarNames();
+        Cars cars = controller.createCars(carNames);
+        return new InputLapStep(controller, cars);
+    }
+}
