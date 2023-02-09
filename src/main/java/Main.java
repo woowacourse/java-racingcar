1c7a5611bbb9c277eb31f1b9ff4c331da24d070f
apptie <appti@naver.com>
2023-02-09 13:43:30 +0900

    feat: Main 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import controller.CarController;
+import java.util.Scanner;
+import service.CarService;
+import utils.RacingRandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView(new Scanner(System.in));
+        OutputView outputView = new OutputView();
+        CarService carService = new CarService();
+        CarController carController = new CarController(inputView, outputView, carService);
+
+        carController.run(new RacingRandomNumberGenerator());
+    }
+}
