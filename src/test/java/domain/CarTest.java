d0bfa0d1e7272cd5fff3954878bff76321c10afc
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 14:11:30 +0900

    <test>(domain/CarTest.java): 공백이 포함된 이름에 대한 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -9,7 +9,8 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
-    @DisplayName("Car 객체 생성 시 이름이 5자 이하이면 정상적으로 생성된다.")
+
+    @DisplayName("Car 객체 생성 시 검증을 통과하면 정상적으로 생성한다.")
     @Test
     void test1() {
         assertThatNoException().isThrownBy(() -> new Car("자동차"));
@@ -21,9 +22,15 @@ class CarTest {
         assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"));
     }
 
-    @DisplayName("move를 위한 최솟값을 만족시키면 position을 1 증가 시킨다.")
+    @DisplayName("Car 객체 생성 시 이름에 공백이 포함되면 예외를 발생시킨다.")
     @Test
     void test3() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"));
+    }
+
+    @DisplayName("move를 위한 최솟값을 만족시키면 position을 1 증가 시킨다.")
+    @Test
+    void test4() {
         Car car = new Car("any");
 
         int beforePosition = car.getPosition();
@@ -35,7 +42,7 @@ class CarTest {
 
     @DisplayName("move를 위한 최솟값을 만족시키지 못하면 position은 변화하지 않는다.")
     @Test
-    void test4() {
+    void test5() {
         Car car = new Car("any");
 
         int beforePosition = car.getPosition();
