9aa0685cf5abd29567d86cc0fffd77b65aba6ba9
bjk1649 <qhwns1649@naver.com>
2023-02-09 16:59:50 +0900

    feat: 실행 결과를 출력하기 전 안내 메시지 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String BEFORE_PRINT_RESULT_ANNOUNCEMENT = "실행 결과";
+
+    public void announceBeforePrintResult() {
+        System.out.println(BEFORE_PRINT_RESULT_ANNOUNCEMENT);
+    }
+
+}
