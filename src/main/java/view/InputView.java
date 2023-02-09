403643e8e9a841a2845aa68e0a782d0b6472fddd
sh111-coder <ohk9134@naver.com>
2023-02-09 11:10:16 +0900

    feat(InputView) : 자동차 이름이 중복되는 경우 예외 처리 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -8,6 +11,8 @@ public class InputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String CAR_NAMES_BLANK_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 입력되지 않았습니다.";
+    private static final String CAR_NAMES_DELIMITER = ",";
+    private static final String CAR_NAMES_DUPLICATE_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 중복되었습니다.";
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -19,9 +24,9 @@ public class InputView {
 
     public String[] readCarNames() {
         String carNames = scanner.nextLine();
-
         validateCarNames(carNames);
-
+        String[] splitCarNames = getSplitCarNames(carNames);
+        validateSplitCarNames(splitCarNames);
 
         return new String[]{""};
     }
@@ -36,12 +41,14 @@ public class InputView {
         return inputValue.equals("");
     }
 
-    private String[] splitCarNames(String carNames) {
-        return new String[]{""};
+    private String[] getSplitCarNames(String carNames) {
+        return carNames.split(CAR_NAMES_DELIMITER);
     }
 
     private void validateSplitCarNames(String[] splitCarNames) {
-
+        if (isDuplicated(splitCarNames)) {
+            throw new IllegalArgumentException(CAR_NAMES_DUPLICATE_ERROR);
+        }
     }
 
     private void validateCarName(String carName) {
@@ -49,7 +56,9 @@ public class InputView {
     }
 
     private boolean isDuplicated(String[] splitCarNames) {
-        return true;
+        Set<String> removedDuplicatedSplitCarNames = Arrays.stream(splitCarNames)
+                .collect(Collectors.toSet());
+        return splitCarNames.length != removedDuplicatedSplitCarNames.size();
     }
 
     private boolean isCorrectCarNameLength(String carName) {
