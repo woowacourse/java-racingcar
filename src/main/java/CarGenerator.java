bfa9cc8d9258807dbcff2adbaaf728c3c3190722
yenawee <yaena0319@naver.com>
2023-02-09 10:58:37 +0900

    feat: CarGenerator 객체 구현

---
+++ src/main/java/CarGenerator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarGenerator {
+    public List<Car> generateCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+}
