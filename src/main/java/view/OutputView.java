5fdcfe16fdb9521fdc54b060b0557f0141e987bd
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-09 14:26:14 +0900

    refactor(OutputView): joining delimiter 상수화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     private static final String INFO_SHOW_MESSAGE = "실행결과";
     private static final String WINNER_SHOW_POSTFIX_MESSAGE = "가 최종 우승했습니다.";
     private static final String LINE_CHANGE_MESSAGE = "";
+    private static final String JOINING_DELIMITER = ", ";
 
     public void showInfoMessage() {
         printMessage(INFO_SHOW_MESSAGE);
@@ -27,7 +28,7 @@ public class OutputView {
     private static String getWinnerNames(List<Car> cars) {
         return cars.stream()
                 .map(car -> car.getName())
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(JOINING_DELIMITER));
     }
 
     private void printMessage(String message) {
