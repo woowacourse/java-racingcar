18b8761716581255eb3ac6e749afbc4f0c93355b
nuyh <hjaehyun25@gmail.com>
2023-02-09 11:01:21 +0900

    refactor: OutputView에서 출력문 만드는 부분 메서드로 추출

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,15 +11,23 @@ public class OutputView {
     }
 
     public void printRaceResult(Map<String, Integer> result) {
-        result.forEach((key, value) -> System.out.println(key + " : " + "-".repeat(value)));
+        result.forEach((name, position) -> System.out.println(formatAsResult(name, position)));
         System.out.println();
     }
 
     public void printWinners(List<String> winners) {
-        System.out.println(winners.stream().collect(Collectors.joining(", ", "", "가 최종 우승했습니다.")));
+        System.out.println(formatAsWinners(winners));
     }
 
     public void printError(String errMessage) {
         System.out.println("[ERROR] " + errMessage);
+    }
+
+    private String formatAsResult(String name, Integer position) {
+        return name + " : " + "-".repeat(position);
+    }
+
+    private String formatAsWinners(List<String> winners) {
+        return winners.stream().collect(Collectors.joining(", ", "", "가 최종 우승했습니다."));
     }
 }
