8d8293433ae0e76c5c831792a04463494579fd7c
hum02 <ehdgma02@gmail.com>
2023-02-09 10:15:18 +0900

    feat: 시도횟수 입력받기 구현

--- src/main/java/racingCar/RacingCarController.java
+++ src/main/java/racingCar/RacingCarController.java
@@ -1,4 +1,31 @@
 package racingCar;
 
+import racingCar.domain.CarGroup;
+import racingCar.view.InputView;
+import racingCar.view.OutputView;
+
+import java.util.List;
+import java.util.function.Supplier;
+
 public class RacingCarController {
+
+    public void run() {
+        CarGroup carGroup = repeat(this::makeCars);
+        Integer tryCount = repeat(InputView::readTryCount);
+    }
+
+    private CarGroup makeCars() {
+        List<String> carNames = repeat(InputView::readCarNames);
+        CarGroup carGroup = new CarGroup(carNames);
+        return carGroup;
+    }
+
+    private static <T> T repeat(Supplier<T> reader) {
+        try {
+            return reader.get();
+        } catch (Exception e) {
+            OutputView.printError(e.getMessage());
+            return repeat(reader);
+        }
+    }
 }
--- src/main/java/racingCar/view/InputView.java
+++ src/main/java/racingCar/view/InputView.java
@@ -1,29 +1,43 @@
 package racingCar.view;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private Scanner scanner = new Scanner(System.in);
-    public List<String> readCarNames() throws IOException {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.nextLine();
         String[] carNames = input.split(",");
-
-        if (isEmpty(carNames)) {
-            return Arrays.stream(carNames)
-                    .collect(Collectors.toList());
-        }
-        throw new IOException();
+        validateEmpty(carNames);
+        return Arrays.stream(carNames)
+                .collect(Collectors.toList());
     }
 
-    private boolean isEmpty(String[] carNames) {
+    public static Integer readTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String input = scanner.nextLine();
+        validateEmpty(input);
+        return validateIsDigit(input);
+    }
+
+    private static void validateEmpty(String[] carNames) throws IllegalArgumentException {
         if (carNames.length == 0) {
-            return false;
+            throw new IllegalArgumentException("[ERROR] 자동차 이름을 다시 입력해주세요.");
         }
-        return true;
+    }
+
+    private static void validateEmpty(String tryCount) throws IllegalArgumentException {
+        if (tryCount.length() == 0) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수를 다시 입력해주세요.");
+        }
+    }
+
+    private static Integer validateIsDigit(String input) throws IllegalArgumentException {
+        return Integer.parseInt(input);
     }
 }
