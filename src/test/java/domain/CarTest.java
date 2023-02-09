e57172b0bbe02c844e076107da26450133630ffb
woosung1223 <dntjd991223@naver.com>
2023-02-09 14:54:20 +0900

    test: Car 객체에 대한 테스트 생성

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,10 +1,19 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CarTest {
 
     @ParameterizedTest
