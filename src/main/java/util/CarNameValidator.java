013d8650769abb089b8ebe8fda691ab45484a3cc
Eunkeee <eunkeeeee@gmail.com>
2023-02-09 11:13:05 +0900

    refactor(CarNameValidator): 하드코딩 제거

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -3,14 +3,34 @@ package util;
 import java.util.List;
 
 public class CarNameValidator {
+    public static final int MIN_CAR_NUMBER = 2;
+    public static final int MAX_NAME_LENGTH = 5;
+
     public void validate(List<String> names) {
-        if (names.size() < 2) {
-            throw new IllegalArgumentException("2개 이상의 자동차를 입력해 주세요.");
-        }
-        for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요.");
-            }
+        validateCarNumber(names);
+        names.forEach(this::validateCarNameLength);
+    }
+
+    private static void validateCarNumber(List<String> names) {
+        if (names.size() < MIN_CAR_NUMBER) {
+            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NUMBER.message);
         }
     }
+
+    private void validateCarNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NAME_LENGTH.message);
+        }
+    }
+
+    private enum Message {
+        EXCEPTION_CAR_NUMBER("2개 이상의 자동차를 입력해 주세요."),
+        EXCEPTION_CAR_NAME_LENGTH("5자 이하의 이름을 입력해주세요.");
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+    }
+
 }
