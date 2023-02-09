1c51abc5d8fca841ebf659d2be021a2b960864a0
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 11:55:38 +0900

    test: CarRepository의 movePosition 메소드를 테스트 한다

--- src/test/java/racingcar/domain/carrepository/CarRepositoryTest.java
+++ src/test/java/racingcar/domain/carrepository/CarRepositoryTest.java
@@ -1,15 +1,44 @@
 package racingcar.domain.carrepository;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.domain.car.Car;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRepositoryTest {
 
-    @Test
-    void movePosition() {
+    @ParameterizedTest
+    @MethodSource("getCarRepository")
+    @DisplayName("자동차 이동 조건에 대한 서로 다른 숫자가 입력되었을 때, 자동차 이동 여부를 확인")
+    void movePosition(List<Car> cars, int randomNumber, int expectedPosition) {
+        CarRepository carRepository = new CarRepository(cars);
 
+        carRepository.movePosition(randomNumber);
+
+        for (Car car : cars) {
+            assertThat(car.getPosition()).isEqualTo(expectedPosition);
+        }
     }
 
     @Test
     void findWinner() {
+    }
+
+    static Stream<Arguments> getCarRepository() {
+        return Stream.of(
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 4, 1),
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 5, 1),
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 9, 1),
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 0, 0),
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 2, 0),
+                Arguments.arguments(List.of(new Car("poy"), new Car("joy"), new Car("kane")), 3, 0)
+                );
     }
 }
