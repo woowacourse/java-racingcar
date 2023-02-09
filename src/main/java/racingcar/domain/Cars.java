b8a673ca5a723424844e67719d71d15ac9e5ccd5
bjk1649 <qhwns1649@naver.com>
2023-02-09 15:35:49 +0900

    feat: 각 자동차들에 움직임을 시도하도록 명령

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import racingcar.dto.CarDto;
+import racingcar.util.IntGenerator;
+import racingcar.util.RandomIntGenerator;
+
+public class Cars {
+    List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        this.cars = new LinkedList<>();
+        carNames.forEach(carName -> cars.add(new Car(carName)));
+    }
+
+    public Cars(List<String> carNames, IntGenerator intGenerator) {
+        this.cars = new LinkedList<>();
+        carNames.forEach(carName -> cars.add(new Car(carName, intGenerator)));
+    }
+
+    public void tryMoving() {
+        cars.forEach(Car::tryMove);
+    }
+
+}
