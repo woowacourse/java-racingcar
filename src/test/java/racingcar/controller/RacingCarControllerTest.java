b79fecb00119c8ad24b4ebf0c45e9407f24f29db
seokjin8678 <rnrn8678@naver.com>
2023-02-09 14:11:24 +0900

    test: RacingCarController 작동 확인 테스트 코드 추가

---
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -0,0 +1,49 @@
+package racingcar.controller;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.dto.RacingCarNamesRequest;
+import racingcar.dto.RacingCarStatusResponse;
+import racingcar.dto.RacingCarWinnerResponse;
+import racingcar.dto.TryCountRequest;
+import racingcar.service.RacingCarService;
+import racingcar.view.RacingCarView;
+
+class RacingCarControllerTest {
+    private RacingCarController racingCarController;
+
+    private static class MockRacingCarView implements RacingCarView {
+        @Override
+        public RacingCarNamesRequest receiveCarNames() {
+            return RacingCarNamesRequest.of("car1,car2,car3");
+        }
+
+        @Override
+        public TryCountRequest receiveTryCount() {
+            return TryCountRequest.of("5");
+        }
+
+        @Override
+        public void printRacingResult(List<RacingCarStatusResponse> responses) {
+
+        }
+
+        @Override
+        public void printWinners(RacingCarWinnerResponse response) {
+
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        racingCarController = new RacingCarController(new RacingCarService(), new MockRacingCarView());
+    }
+
+    @Test
+    @DisplayName("Controller가 정상적으로 작동해야한다.")
+    void controller_start() {
+        racingCarController.start();
+    }
+}
