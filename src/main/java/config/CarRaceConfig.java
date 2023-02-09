ec7fb9d01e7e41bc0ee2136d306f7fed5fb95fcc
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-09 12:14:28 +0900

    feat(CarRaceConfig): 자동차 경주 게임 객체 생명주기 관리 코드 구현

---
+++ src/main/java/config/CarRaceConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+import controller.CarRaceController;
+import repository.CarRaceResultRepositoryImpl;
+import service.CarRaceService;
+import service.CarRaceServiceImpl;
+import service.RandomNumberGeneratorImpl;
+import validation.InputValidationChain;
+import validation.impl.EmptyValueValidationChain;
+import validation.impl.InputValidateSuccessChain;
+import validation.impl.NumberRangeValidationChain;
+import validation.impl.PositiveNumberValidationChain;
+import view.InputView;
+import view.OutputView;
+
+public class CarRaceConfig {
+
+    public static CarRaceController config() {
+        return new CarRaceController(new InputView(), new OutputView(),
+            makeValidator(), makeService());
+    }
+
+    private static CarRaceService makeService() {
+        return new CarRaceServiceImpl(new CarRaceResultRepositoryImpl(),
+            new RandomNumberGeneratorImpl());
+    }
+
+    private static InputValidationChain makeValidator() {
+        InputValidationChain emptyValueValidationChain = new EmptyValueValidationChain();
+        InputValidationChain numberRangeValidationChain = new NumberRangeValidationChain();
+        InputValidationChain positiveNumberValidationChain = new PositiveNumberValidationChain();
+        InputValidationChain inputValidationChain = new InputValidateSuccessChain();
+        emptyValueValidationChain.setNext(numberRangeValidationChain);
+        numberRangeValidationChain.setNext(positiveNumberValidationChain);
+        positiveNumberValidationChain.setNext(inputValidationChain);
+        return emptyValueValidationChain;
+    }
+}
