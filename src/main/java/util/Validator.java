a88aeae93f7a0f17b431e061ca02e940a972182e
yujamint <woawoa0513@gmail.com>
2023-02-09 10:46:26 +0900

    refactor: MovingCount객체 삭제에 의한 코드 수정

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -4,12 +4,19 @@ public class Validator {
     private static final String CHARACTER_REG = "^[a-zA-Z]*$";
 
     public static void validateMovingCountInput(String input) {
-        if (hasCharacter(input)) {
+        validateIsDigit(input);
+        validateCountRange(Integer.parseInt(input));
+    }
+
+    private static void validateIsDigit(String input) {
+        if (input.matches(CHARACTER_REG)) {
             throw new IllegalArgumentException("이동할 횟수는 숫자만 입력 가능합니다.");
         }
     }
 
-    private static boolean hasCharacter(String input) {
-        return input.matches(CHARACTER_REG);
+    private static void validateCountRange(Integer count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("이동 횟수는 1이상의 수자만 가능합니다.");
+        }
     }
 }
