68b9cf878d0e1efc1942631c1bd3727ed20480f7
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-02-09 13:38:39 +0900

    test(finder): 자동차 스텝 중 최대값 찾기 테스트

--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -10,10 +10,10 @@ public class OutputView {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_COUNT = "시도할 회수는 몇회인가요?";
     private static final String FINAL_WINNER = "가 최종 우승했습니다.";
-    public static final String EXECUTE_RESULT = "실행 결과";
-    public static final String COLON = " : ";
-    public static final String HYPHEN = "-";
-    public static final String COMMA = ", ";
+    private static final String EXECUTE_RESULT = "실행 결과";
+    private static final String COLON = " : ";
+    private static final String HYPHEN = "-";
+    private static final String COMMA = ", ";
 
     public static void printInputCarName() {
         System.out.println(INPUT_CAR_NAME);
---
+++ src/test/java/util/FinderTest.java
@@ -0,0 +1,42 @@
+package util;
+
+import domain.Car;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinderTest {
+    @DisplayName("자동차 객체들 중 가장 높은 step값 찾기")
+    @Test
+    void 가장_높은_스텝값_찾기() {
+        Car carA = new Car("carA");
+        carA.increaseStep();
+
+        Car carB = new Car("carB");
+        carB.increaseStep();
+        carB.increaseStep();
+
+        Car carC = new Car("carC");
+        carC.increaseStep();
+        carC.increaseStep();
+        carC.increaseStep();
+
+        Assertions.assertEquals(Finder.findMaxStep(Arrays.asList(carA, carB, carC)), 3);
+    }
+
+    @DisplayName("자동차들이 모두 이동하지 않았을 때 0을 반환해야 한다.")
+    @Test
+    void 스텝이_모두_0인경우() {
+        Car carA = new Car("carA");
+        Car carB = new Car("carB");
+        Car carC = new Car("carC");
+
+        Assertions.assertEquals(Finder.findMaxStep(Arrays.asList(carA, carB, carC)), 0);
+    }
+
+}
