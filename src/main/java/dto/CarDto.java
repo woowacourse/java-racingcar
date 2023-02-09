d5a08890d0c3fbffdfef6fb44db2b2295b91e287
kokodak <kokodakadokok@gmail.com>
2023-02-09 13:28:42 +0900

    feat(carDto): Car에 대한 DTO 구현

---
+++ src/main/java/dto/CarDto.java
@@ -0,0 +1,25 @@
+package dto;
+
+import domain.Car;
+
+public class CarDto {
+    private String name;
+    private int position;
+
+    private CarDto(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public static CarDto from(Car car) {
+        return new CarDto(car.getName(), car.getPosition());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
