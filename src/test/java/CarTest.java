83bf1628cb14f41d45d607bd8ddfc695347376cd
yenawee <yaena0319@naver.com>
2023-02-09 15:09:04 +0900

    refactor : 요구사항 확인

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -6,17 +6,13 @@ import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-
     @Test
     void testToString() {
         Car car = new Car("jena");
         for (int i = 0; i < 5; i++) {
             car.move();
         }
-        assertThat(car.toString().split(": ")[1].length()).isEqualTo(car.getDistance());
-    }
-
-    @Test
-    void move() {
+        assertThat(car.toString().split(": ")[1].length())
+                .isEqualTo(car.getDistance());
     }
 }
--- src/test/java/WinnerTest.java
+++ src/test/java/WinnerTest.java
@@ -11,16 +11,15 @@ class WinnerTest {
     static Car car2;
     static Car car3;
     static Winner winner;
+
     @BeforeAll
-    static void setup(){
+    static void setup() {
         car1 = new Car("jerry");
         car2 = new Car("jena");
         car3 = new Car("poby");
         List<Car> cars = new ArrayList<>();
 
         winner = new Winner();
-
-
         while (car1.getDistance() != 5) car1.move();
         while (car2.getDistance() != 4) car2.move();
         while (car3.getDistance() != 5) car3.move();
@@ -34,14 +33,12 @@ class WinnerTest {
 
     @Test
     void findWinnerNamesTest() {
-        assertThat(winner.getWinnerNames().toArray()).containsExactly(car1.getName());
-
-
+        assertThat(winner.getWinnerNames().toArray())
+                .containsExactly(car1.getName(),car3.getName());
     }
 
     @Test
     void testToString() {
-       assertThat(winner.toString()).contains(car1.getName(), car3.getName());
-
+        assertThat(winner.toString()).contains(car3.getName());
     }
 }
