8c540d49765c7f4f49763a8cb1b65bd2fff05a88
amaran-th <songsy405@gmail.com>
2023-02-09 15:42:47 +0900

    fix(InputView) : readRoundNum()의 에러 처리가 제대로 동작하지 않던 문제 해결
    - nextInt()의 특성으로 인해 repeat 메서드가 무한 호출되던 문제를 readLine()을 호출하는 것으로 변경하여 해결

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,39 +1,42 @@
 package view;
 
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
 
   private final Scanner scanner = new Scanner(System.in);
 
+  private final Pattern DIGIT = Pattern.compile("[0-9]+");
+
   public List<String> readCarNames() {
     System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     String[] split = readLine().split(",");
+
     return Arrays.stream(split)
         .peek(this::validateCarName)
         .collect(Collectors.toList());
   }
 
   private String readLine() {
-    return scanner.next();
+    return scanner.nextLine();
   }
 
-  private void validateCarName(String inputCarName) {
+  private void validateCarName(String inputCarName) throws IllegalArgumentException {
     if (inputCarName.length() < 1 || inputCarName.length() > 5) {
       throw new IllegalArgumentException("자동차의 이름의 길이는 1 이상 5 이하여아 한다");
     }
   }
 
   public int readRoundNum() {
-    try {
-      System.out.println("시도할 횟수는 몇회인가요?");
-      return scanner.nextInt();
-    } catch (InputMismatchException e) {
-      throw new InputMismatchException("이동 횟수는 숫자여야 합니다.");
+    System.out.println("시도할 횟수는 몇회인가요?");
+    String input = readLine();
+    if (DIGIT.matcher(input).matches()) {
+      return Integer.parseInt(input);
     }
+    throw new IllegalArgumentException("이동 횟수는 숫자여야 합니다.");
   }
 }
