105e590c61c752cc65fc4f1db7f9c72377f0f49b
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 11:33:05 +0900

    <feat>(application/RacingGameApplication.java): 게임 로직을 실행하는 application 구현

---
+++ src/main/java/application/RacingGameApplication.java
@@ -0,0 +1,59 @@
+package application;
+
+import domain.Car;
+import domain.Cars;
+import domain.NumberGenerator;
+import domain.TryCount;
+import dto.request.CarNameDto;
+import dto.response.CarStatusDto;
+import dto.response.WinnersNameDto;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGameApplication {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final NumberGenerator numberGenerator;
+
+    public RacingGameApplication(InputView inputView, OutputView outputView, NumberGenerator numberGenerator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void run() {
+        List<CarNameDto> carNameDtos = inputView.sendCarsName();
+        TryCount tryCount = inputView.sendTryCount();
+
+        Cars cars = getCars(carNameDtos);
+
+        playGame(tryCount, cars);
+        printResult(cars);
+    }
+
+    private Cars getCars(List<CarNameDto> carNameDtos) {
+        return carNameDtos.stream()
+                .map(carNameDto -> new Car(carNameDto.getName()))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Cars::new));
+    }
+
+    private void playGame(TryCount tryCount, Cars cars) {
+        for (int i = 0; i < tryCount.getTryCount(); i++) {
+            cars.moveCars(numberGenerator);
+            List<CarStatusDto> carStatusDtos = cars.getCars()
+                    .stream()
+                    .map(CarStatusDto::of)
+                    .collect(Collectors.toList());
+            outputView.printEachRound(carStatusDtos);
+        }
+    }
+
+    private void printResult(Cars cars) {
+        List<Car> winners = cars.findWinners();
+        outputView.printWinners(WinnersNameDto.of(winners));
+    }
+}
