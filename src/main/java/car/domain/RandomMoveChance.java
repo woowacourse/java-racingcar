35bfb5d8616d4e0eff203dd2cd09cfe737f5416c
0chil <0@chll.it>
2023-02-09 16:10:52 +0900

    refactor(RandomMoveChange): 상수 이동

--- src/main/java/car/domain/RandomMoveChance.java
+++ src/main/java/car/domain/RandomMoveChance.java
@@ -1,9 +1,10 @@
 package car.domain;
 
+import static car.option.Option.MIN_MOVABLE_NUMBER;
+
 import java.util.Random;
 
 public class RandomMoveChance implements MoveChance {
-    private static final int MIN_MOVABLE_NUMBER = 4;
 
     @Override
     public boolean isMovable() {
--- src/main/java/car/option/Option.java
+++ src/main/java/car/option/Option.java
@@ -5,4 +5,5 @@ public class Option {
     public static final int MAX_NAME_LENGTH = 5;
     public static final int MIN_NAME_LENGTH = 1;
     public static final int MIN_TRIAL_COUNT = 0;
+    public static final int MIN_MOVABLE_NUMBER = 4;
 }
