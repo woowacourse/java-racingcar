4691dbb6b87eab583c1a3a0566936d4222c2439f
woo-chang <clllickme@naver.com>
2023-02-09 10:46:57 +0900

    feat: 시도 횟수 입력 기능 구현
    Co-authored-by: greeng00se <thegreengoose7@gmail.com>

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,25 +1,62 @@
 package view;
 
-import java.text.MessageFormat;
+import static java.text.MessageFormat.format;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputValidator {
 
     private static final String INVALID_NAME_MESSAGE = "차의 이름은 {0}자 이상 ~ {1}자 이하여야 합니다.";
     private static final int NAME_LENGTH_LOWER_BOUND = 1;
     private static final int NAME_LENGTH_UPPER_BOUND = 5;
+    private static final String DELIMITER = ",";
+    private static final int SPLIT_LIMIT = -1;
+    private static final String INVALID_COUNT_MESSAGE = "횟수는 {0}이상의 정수만 가능합니다.";
+    private static final int COUNT_LOWER_BOUND = 1;
 
-    public void validateNames(final List<String> names) {
+    public List<String> validateNames(final String input) {
+        List<String> names = generateNames(input);
         for (String name : names) {
             validateName(name);
         }
+        return names;
+    }
+
+    private List<String> generateNames(final String input) {
+        return Arrays.stream(input.split(DELIMITER, SPLIT_LIMIT))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     private void validateName(final String name) {
         if (name.length() < NAME_LENGTH_LOWER_BOUND || name.length() > NAME_LENGTH_UPPER_BOUND) {
-            throw new IllegalArgumentException(
-                    MessageFormat.format(INVALID_NAME_MESSAGE, NAME_LENGTH_LOWER_BOUND, NAME_LENGTH_UPPER_BOUND)
-            );
+            throw new IllegalArgumentException(format(
+                    INVALID_NAME_MESSAGE,
+                    NAME_LENGTH_LOWER_BOUND,
+                    NAME_LENGTH_UPPER_BOUND
+            ));
+        }
+    }
+
+    public int validateCount(final String input) {
+        int number = parseNumber(input);
+        validatePositiveNumber(number);
+        return number;
+    }
+
+    private int parseNumber(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(format(INVALID_COUNT_MESSAGE, COUNT_LOWER_BOUND));
+        }
+    }
+
+    private void validatePositiveNumber(final int number) {
+        if (number < COUNT_LOWER_BOUND) {
+            throw new IllegalArgumentException(format(INVALID_COUNT_MESSAGE, COUNT_LOWER_BOUND));
         }
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,15 +2,12 @@ package view;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final String READ_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static final String DELIMITER = ",";
-    private static final int SPLIT_LIMIT = -1;
+    private static final String READ_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private final InputValidator inputValidator;
     private final BufferedReader reader;
@@ -23,14 +20,12 @@ public class InputView {
     public List<String> readCarNames() throws IOException {
         System.out.println(READ_NAMES_MESSAGE);
         String input = reader.readLine();
-        List<String> names = generateNames(input);
-        inputValidator.validateNames(names);
-        return names;
+        return inputValidator.validateNames(input);
     }
 
-    private List<String> generateNames(final String input) {
-        return Arrays.stream(input.split(DELIMITER, SPLIT_LIMIT))
-                .map(String::trim)
-                .collect(Collectors.toList());
+    public int readCount() throws IOException {
+        System.out.println(READ_COUNT_MESSAGE);
+        String input = reader.readLine();
+        return inputValidator.validateCount(input);
     }
 }
