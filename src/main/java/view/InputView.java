8986c1ab2bdbb94fb9d751e791239b1a9c4dfdb2
sh111-coder <ohk9134@naver.com>
2023-02-09 10:59:00 +0900

    feat(InputView) : 자동차 이름이 빈 값인 경우 예외 처리 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,15 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
 
     private static final InputView INSTANCE = new InputView();
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String CAR_NAMES_BLANK_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 입력되지 않았습니다.";
+
+    private final Scanner scanner = new Scanner(System.in);
 
     private InputView() {}
 
@@ -11,11 +18,22 @@ public class InputView {
     }
 
     public String[] readCarNames() {
+        String carNames = scanner.nextLine();
+
+        validateCarNames(carNames);
+
+
         return new String[]{""};
     }
 
+    private void validateCarNames(String carNames) {
+        if (isBlank(carNames)) {
+            throw new IllegalArgumentException(CAR_NAMES_BLANK_ERROR);
+        }
+    }
+
     private boolean isBlank(String inputValue) {
-        return true;
+        return inputValue.equals("");
     }
 
     private String[] splitCarNames(String carNames) {
