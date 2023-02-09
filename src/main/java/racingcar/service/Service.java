9ae73d8afcfe68cfb101deaa750db90bcfb82896
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-09 15:13:18 +0900

    feat: service 자동차 추가 기능 구현

--- src/main/java/racingcar/service/Service.java
+++ src/main/java/racingcar/service/Service.java
@@ -1,4 +1,21 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
 public class Service {
+
+    private final Cars cars;
+
+    private final String CAR_NAME_DELIMITER = ",";
+
+    public Service() {
+        this.cars = new Cars();
+    }
+
+    public void makeCars(String carNames) {
+        for (String name : carNames.split(CAR_NAME_DELIMITER)) {
+            cars.add(new Car(name));
+        }
+    }
 }
