8c0f54697e69e26621723aab56e463b39cb8da44
apptie <appti@naver.com>
2023-02-09 14:48:34 +0900

    refactor: 리터럴 상수 처리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,17 +11,19 @@ public class OutputView {
     private static final String POSITION_STATE = "-";
     private static final String ROUND_FORMAT = "%s : %s";
     private static final String WINNER_FORMAT = "%s가 최종 우승했습니다.";
+    private static final String LINE_FEED = "";
+    private static final String RACE_START_MESSAGE = "실행 결과";
+    private static final String DELIMITER = ", ";
 
     public void printRoundStartMessage() {
-        //TODO 상수 리팩터링
-        print("");
-        print("실행 결과");
+        print(LINE_FEED);
+        print(RACE_START_MESSAGE);
     }
 
     public void printRound(List<CarDto> carsDto) {
         carsDto.forEach(dto -> print(String.format(ROUND_FORMAT, dto.getName(),
                 getCarPositionState(dto.getPosition()))));
-        print("");
+        print(LINE_FEED);
     }
 
     private String getCarPositionState(int position) {
@@ -31,7 +33,7 @@ public class OutputView {
     public void printWinner(List<WinnerCarDto> winnersDto) {
         String winners = winnersDto.stream()
                 .map(WinnerCarDto::getName)
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(DELIMITER));
 
         print(String.format(WINNER_FORMAT, winners));
     }
