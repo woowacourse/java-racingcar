61b6bc1056add3aa8275d64ba0672f068e6583b8
seokhwan-an <asok369258@naver.com>
2023-02-09 12:02:19 +0900

    feat(controller) : 레이싱 게임 controller 구현
    세부사항
    - 자동차 이름에 대한 사용자 입력을 받는다.
    - 총 시도 횟수에 대한 사용자 입력을 받는다.
    - 자동차 경주를 진행한다.
    - 경주 결과를 출력하고 프로그램을 종료한다.

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,48 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.RacingGame;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class Controller {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final NumberGenerator numberGenerator;
+
+    public Controller(InputView inputView, OutputView outputView, NumberGenerator numberGenerator){
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void run() {
+        List<String> carNames = inputView.readCarNames();
+        Cars cars = generateCars(carNames);
+        int round = inputView.readRacingRound();
+        RacingGame racingGame = new RacingGame(numberGenerator, cars);
+        showGameResult(racingGame, round);
+    }
+
+    private Cars generateCars(List<String> carNames) {
+        List<Car> carInstances = new ArrayList<>();
+        for(String name : carNames) {
+            carInstances.add(new Car(name));
+        }
+        return new Cars(carInstances);
+    }
+
+    private void showGameResult(RacingGame racingGame, int round) {
+        outputView.printResultMessage();
+        for(int i = 0; i < round; i++) {
+            racingGame.playRound();
+            outputView.printRoundResult(racingGame.getCars());
+        }
+        outputView.printFinalResult(racingGame.getCars());
+    }
+
+}
