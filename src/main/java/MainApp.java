17ddf877b730fda817c2a46fc45bd435286870e3
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 11:58:59 +0900

    <feat>(view/InputViewProxy.java): exception 발생시 로직을 재호출하도록 하는 Proxy 클래스 구현

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,12 +1,17 @@
 import application.RacingGameApplication;
 import domain.RandomNumberGenerator;
 import view.InputView;
+import view.InputViewProxy;
 import view.OutputView;
 
 public class MainApp {
     public static void main(String[] args) {
         RacingGameApplication racingGameApplication =
-                new RacingGameApplication(new InputView(), new OutputView(), new RandomNumberGenerator());
+                new RacingGameApplication(initInputView(), new OutputView(), new RandomNumberGenerator());
         racingGameApplication.run();
+    }
+
+    private static InputView initInputView() {
+        return new InputViewProxy(new InputView());
     }
 }
