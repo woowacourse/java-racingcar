25847abaef28b4536f10450ee260744dcd2ebf39
chaewon121 <cw121@naver.com>
2023-02-09 11:54:05 +0900

    refactor: 하드코딩한 문자열대신 상수로 사용

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,7 +2,9 @@ package racingcar.view;
 
 public class OutputView {
 
+    private static final String RACE_RESULT_MESSAGE = "실행 결과";
+
     public static void printResult() {
-        System.out.println("실행 결과");
+        System.out.println(RACE_RESULT_MESSAGE);
     }
 }
