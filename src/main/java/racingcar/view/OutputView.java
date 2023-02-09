57e3cfb23624f419280aa60c76fbb0b37e53a812
bjk1649 <qhwns1649@naver.com>
2023-02-09 17:10:29 +0900

    feat: 최종 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import racingcar.dto.CarDto;
 
@@ -7,6 +9,7 @@ public class OutputView {
     private static final String BEFORE_PRINT_RESULT_ANNOUNCEMENT = "실행 결과";
     private static final String NAME_AND_COUNT_PREFIX = " : ";
     private static final String DISTANCE_PREFIX = "-";
+    private static final String WINNER_ANNOUNCEMENT = "가 최종 우승했습니다.";
 
     public void announceBeforePrintResult() {
         System.out.println(BEFORE_PRINT_RESULT_ANNOUNCEMENT);
@@ -17,8 +20,8 @@ public class OutputView {
             System.out.print(carDto.getName());
             System.out.print(NAME_AND_COUNT_PREFIX);
             printMovedDistance(carDto.getMovedCount());
-            System.out.println();
         });
+        System.out.println();
     }
 
     private void printMovedDistance(int movedCount) {
@@ -27,5 +30,16 @@ public class OutputView {
         }
         System.out.println();
     }
+
+    public void printWinner(List<CarDto> winnerDtos) {
+        List<String> winnerNames = new LinkedList<>();
+        winnerDtos.forEach(winnerDto -> {
+            winnerNames.add(winnerDto.getName());
+        });
+        System.out.println(String.join(",", winnerNames));
+        System.out.println(WINNER_ANNOUNCEMENT);
+    }
+
+
 
 }
