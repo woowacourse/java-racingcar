a13a04c0f5ac1c96bd3bdeae28de679fc230adf1
apptie <appti@naver.com>
2023-02-09 14:54:35 +0900

    refactor: Generator 테스트 매직 넘버 상수 처리

--- src/test/java/utils/RacingRandomNumberGeneratorTest.java
+++ src/test/java/utils/RacingRandomNumberGeneratorTest.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.RepeatedTest;
 
 class RacingRandomNumberGeneratorTest {
 
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+
     @RepeatedTest(10)
     @DisplayName("0부터 9까지의 랜덤값이 생성된다.")
     void generate() {
         RacingRandomNumberGenerator generator = new RacingRandomNumberGenerator();
 
         Assertions.assertThat(generator.generate())
-                .isGreaterThanOrEqualTo(0)
-                .isLessThanOrEqualTo(9);
+                .isGreaterThanOrEqualTo(MIN_VALUE)
+                .isLessThanOrEqualTo(MAX_VALUE);
     }
 }
