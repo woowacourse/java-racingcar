ceac359e22430824f9cf6e44532786bd996f75b0
jeomxon <jeomxon@gmail.com>
2023-02-09 11:32:16 +0900

    feat: 최종 우승 자동차를 확인하는 기능 추가

---
+++ src/main/java/domain/RacingResult.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RacingResult {
+
+    private final Map<String, Integer> history;
+
+    public RacingResult(Map<String, Integer> history) {
+        this.history = history;
+    }
+
+    public List<String> getWinner() {
+        Integer maxValue = Collections.max(history.values());
+
+        return history.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(maxValue))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+}
