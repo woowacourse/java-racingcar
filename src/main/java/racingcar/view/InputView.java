01dd2937668bb421afa839e04368788a8c58ab4f
iamjooon2 <iamjooon2@gmail.com>
2023-02-09 14:54:38 +0900

    refactor: Scanner 클래스 선언 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,19 +5,18 @@ import racingcar.util.Validator;
 import java.util.Scanner;
 
 public class InputView {
-
-    private final Scanner sc = new Scanner(System.in);
+    private final Scanner scan = new Scanner(System.in);
 
     public String[] inputCarName() {
         System.out.println(OutputConstant.INPUT_CAR_NAME);
-        String input = sc.nextLine();
+        String input = scan.nextLine();
         String[] carNames = Validator.validateCarNames(input);
         return carNames;
     }
 
     public int inputGameTime() {
         System.out.println(OutputConstant.INPUT_TRY_COUNT);
-        String input = sc.nextLine();
+        String input = scan.nextLine();
         int inputNumber = Validator.validateGameTime(input);
         return inputNumber;
     }
