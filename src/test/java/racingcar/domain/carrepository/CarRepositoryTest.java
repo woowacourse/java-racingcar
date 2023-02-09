91cf96ceb46e7cf92302b72959ef31363028ae2a
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 11:36:02 +0900

    refactor: CarRepository의 movePosition 메소드를 리팩토링한다
    stream 연산으로 수정한다.

---
+++ src/test/java/racingcar/domain/carrepository/CarRepositoryTest.java
@@ -0,0 +1,15 @@
+package racingcar.domain.carrepository;
+
+import org.junit.jupiter.api.Test;
+
+class CarRepositoryTest {
+
+    @Test
+    void movePosition() {
+
+    }
+
+    @Test
+    void findWinner() {
+    }
+}
