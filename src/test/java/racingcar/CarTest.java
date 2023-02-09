63117e1a789eff9e8948e2f73f22203e0ef74a1c
swonny <wonny921@gmail.com>
2023-02-09 14:58:14 +0900

    test: 자동차 생성시 이름 길이 예외 처리 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
