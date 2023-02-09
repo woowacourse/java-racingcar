85b662c1ef6a7697ec75fdb75b5d7229d4200adb
cpot5620 <=cpot5620@naver.com>
2023-02-09 13:02:51 +0900

    refactor(RandomUtil): 사용하지 않는 랜덤 클래스 삭제

--- src/test/java/uilts/RandomUtilTest.java
+++ src/test/java/uilts/RandomUtilTest.java
@@ -1,11 +1,9 @@
 package uilts;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.NormalRandomGenerator;
 import utils.RandomGenerator;
-import utils.RandomUtil;
 
 import static org.assertj.core.api.Assertions.*;
 
