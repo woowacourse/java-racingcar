bc090edd99d8775f7f055c2bca5284bf1d5bc590
sh111-coder <ohk9134@naver.com>
2023-02-09 09:53:28 +0900

    feat(OutputView) : 최종 우승자 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     private static final String PRINT_READ_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String PRINT_READ_TRY_NUM_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String PRINT_RACING_RESULT_MESSAGE = "\n실행 결과";
+    private static final String PRINT_WINNERS_MESSAGE = "가 최종 우승했습니다.";
 
     private OutputView() {}
 
@@ -43,8 +44,6 @@ public class OutputView {
     }
 
     public void printWinners(List<String> winners) {
-
+        printMessage(String.join(", ", winners + PRINT_WINNERS_MESSAGE));
     }
-
-
 }
