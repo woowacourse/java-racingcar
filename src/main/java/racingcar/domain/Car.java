b86ce7c88f85da5c228c0e139d29f3b37868973b
bjk1649 <qhwns1649@naver.com>
2023-02-09 14:09:43 +0900

    feat: 유효성 검사 - 이름

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -9,12 +9,16 @@ public class Car {
     private static final int MINIMUM_BOUNDARY_NUMBER_DETERMINING_MOVE = 0;
     private static final int MAXIMUM_BOUNDARY_NUMBER_DETERMINING_MOVE = 9;
     private static final int MINIMUM_NUMBER_TO_MOVE = 4;
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String WRONG_LENGTH_OF_NAME_ANNOUNCEMENT = "[ERROR] 잘못된 이름 길이입니다.";
 
     private String name;
     private int movedCount;
 
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.movedCount = 0;
     }
@@ -25,6 +29,13 @@ public class Car {
 
     public CarDto getStatus() {
         return new CarDto(this.name, this.movedCount);
+    }
+
+    private void validateName(String name) {
+        if (MINIMUM_NAME_LENGTH <= name.length() && name.length() <= MAXIMUM_NAME_LENGTH) {
+            return;
+        }
+        throw new IllegalArgumentException(WRONG_LENGTH_OF_NAME_ANNOUNCEMENT);
     }
 
 }
