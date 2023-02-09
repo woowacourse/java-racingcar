afff4de546dac387a54094b9f3fb829f3965d934
donghae <donghae1999@gmail.com>
2023-02-09 16:16:15 +0900

    fix: 시도 횟수 입력 값 예외 처리 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,33 @@
 package view;
 
+import constant.ExceptionMessage;
+
 import java.util.Scanner;
 
 public class InputView {
     private Scanner scanner = new Scanner(System.in);
+    private ExceptionMessage exceptionMessage;
 
     public String inputCarName() {
         return scanner.nextLine();
     }
 
     public int inputTryCount() {
-        return Integer.parseInt(scanner.nextLine());
+        try {
+            int tryCount = Integer.parseInt(scanner.nextLine());
+            return checkPositiveNumber(tryCount);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(exceptionMessage.getExceptionMessage(
+                    ExceptionMessage.EXCEPTION_NOT_NUMBER_MESSAGE));
+        }
+    }
+
+    private int checkPositiveNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException(exceptionMessage.getExceptionMessage(
+                    ExceptionMessage.EXCEPTION_TRY_COUNT_MESSAGE));
+        }
+
+        return number;
     }
 }
