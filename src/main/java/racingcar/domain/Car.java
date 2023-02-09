f65811a9b98ca7bffd22e95692480adfb155bcf6
jeonghoon <shb03323@naver.com>
2023-02-09 13:22:00 +0900

    feat: 자동차 이름 길이 예외 처리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,17 @@
 package racingcar.domain;
 
+import racingcar.constant.ErrorConstant;
 import racingcar.dto.CarStatus;
 
 public class Car {
     private static final int MINIMUM_NUMBER_TO_MOVE = 4;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int MINIMUM_NAME_LENGTH = 1;
     private final String name;
     private final Position currentPosition;
 
     public Car(String name, int startPoint) {
+        validateName(name);
         this.name = name;
         this.currentPosition = new Position(startPoint);
     }
@@ -28,12 +32,6 @@ public class Car {
         return new CarStatus(name, currentPosition);
     }
 
-    public void validateName(String input) {
-        if (input.length() > 5 || input.length() <1) {
-            throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
-        }
-    }
-
     public int compareTo(Car other) {
         return this.currentPosition.getPosition() - other.currentPosition.getPosition();
     }
@@ -44,5 +42,16 @@ public class Car {
 
     private boolean isMovable(int number) {
         return number >= MINIMUM_NUMBER_TO_MOVE;
+    }
+
+    private void validateName(String input) {
+        String name = removeBlank(input);
+        if (name.length() > MAXIMUM_NAME_LENGTH || name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorConstant.ERROR_PREFIX + "이름의 길이는 1이상 5이하여야 합니다.");
+        }
+    }
+
+    private String removeBlank(String input) {
+        return input.trim();
     }
 }
