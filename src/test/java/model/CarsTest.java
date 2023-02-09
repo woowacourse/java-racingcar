716e98f582c433be512afd4357516e43e5a2b501
donghae <donghae1999@gmail.com>
2023-02-09 14:34:29 +0900

    test: 우승자 결과 테스트 추가
    단일우승자, 다중우승자

--- src/test/java/model/CarsTest.java
+++ src/test/java/model/CarsTest.java
@@ -2,6 +2,12 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +20,50 @@ class CarsTest {
         Cars cars = new Cars("pobi,crong,honux");
 
         assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("단일 우승자인 경우 결과 테스트")
+    void getWinnerTest() {
+
+        String names = "ocean,jamie";
+        Cars cars = new Cars(names);
+        List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(9,0,6,7));
+
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumbers);
+
+        for(int i=0; i<2; i++)
+            cars.moveResult(testNumberGenerator);
+
+        assertThat(cars.getWinners()).isEqualTo("ocean");
+    }
+
+    @Test
+    @DisplayName("다중 우승자인 경우 결과 테스트")
+    void getWinnersTest() {
+
+        String names = "ocean,jamie";
+        Cars cars = new Cars(names);
+        List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(9,9,6,6));
+
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(randomNumbers);
+
+        for(int i=0; i<2; i++)
+            cars.moveResult(testNumberGenerator);
+
+        assertThat(cars.getWinners()).isEqualTo("ocean, jamie");
+    }
+
+    static class TestNumberGenerator implements NumberGenerator {
+        private final List<Integer> numbers;
+
+        public TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generateNumber() {
+            return numbers.remove(0);
+        }
     }
 }
