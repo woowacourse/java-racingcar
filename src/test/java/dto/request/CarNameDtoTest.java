31dca05e1546641cd7d9fe058952cea7f669b71a
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 14:29:27 +0900

    <test>(dto/request/CarNameDtoTest.java): CarNameDto test

---
+++ src/test/java/dto/request/CarNameDtoTest.java
@@ -0,0 +1,29 @@
+package dto.request;
+
+import domain.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+class CarNameDtoTest {
+
+    @DisplayName("Car의 이름이 검증을 통과하면 예외를 발생시키지 않는다.")
+    @Test
+    void test1() {
+        assertThatNoException().isThrownBy(() -> new CarNameDto("자동차"));
+    }
+
+    @DisplayName("Car의 이름이 5자 초과이면 예외를 발생시킨다.")
+    @Test
+    void test2() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto("잘못된자동차이름"));
+    }
+
+    @DisplayName("Car의 이름에 공백이 포함되면 예외를 발생시킨다.")
+    @Test
+    void test3() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto(" bad"));
+    }
+}
