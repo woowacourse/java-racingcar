442bf324dd2ae59ea38a1365397d40372c60dfb9
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-09 15:08:00 +0900

    test: 우승자를 확인하는 기능 테스트 코드 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
@@ -16,6 +17,7 @@ class CarsTest {
         cars = new Cars();
         cars.add(new Car("test1"));
         cars.add(new Car("test2"));
+        cars.add(new Car("test3"));
     }
 
 
@@ -24,9 +26,11 @@ class CarsTest {
     void getTurnCountTest() {
         cars.addNextCarValue(1);
         cars.addNextCarValue(1);
+        cars.addNextCarValue(1);
 
         int firstTurnCount = cars.getTurnCount();
 
+        cars.addNextCarValue(5);
         cars.addNextCarValue(5);
         cars.addNextCarValue(5);
 
@@ -41,10 +45,22 @@ class CarsTest {
     @Test
     @DisplayName("자동차들의 라운드 턴수가 다른 예외 상황 테스트")
     void turnCountExceptionTest() {
-        for(int i=0; i<3; i++){
-            cars.addNextCarValue(3);
+        for (int i = 0; i < 4; i++) {
+            cars.addNextCarValue(4);
         }
         assertThatThrownBy(() -> cars.getTurnCount()).isInstanceOf(IllegalStateException.class);
     }
 
+    @Test
+    @DisplayName("우승자를 확인하는 기능 테스트")
+    void getWinnersTest() {
+        cars.addNextCarValue(4);
+        cars.addNextCarValue(7);
+        cars.addNextCarValue(1);
+        cars.addNextCarValue(9);
+        cars.addNextCarValue(8);
+        cars.addNextCarValue(2);
+
+        assertThat(cars.getWinners()).containsExactly("test1", "test2");
+    }
 }
