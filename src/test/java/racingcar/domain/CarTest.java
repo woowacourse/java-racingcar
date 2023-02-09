2938ff2706890238a6fac3360585871084c7a169
bjk1649 <qhwns1649@naver.com>
2023-02-09 14:21:16 +0900

    test: 유효성 검사 - 이름
    case 1 - 올바른 이름 입력
    case 2 - 잘못된 이름 입력

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.dto.CarDto;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,4 +25,27 @@ class CarTest {
         assertThat(carDtoAfterMove.getName()).isEqualTo(carDtoBeforeMove.getName()).as("이름이 같아야 한다.");
         assertThat(carDtoAfterMove.getMovedCount()).isEqualTo(carDtoBeforeMove.getMovedCount() + 1).as("이동 횟수는 1이 더해진다.");
     }
+
+    @Test
+    @DisplayName("올바른 이름으로 생성")
+    void shouldCreateCarCorrectlyWhenUseAppropriateName() {
+        // given
+        String appropriateName = "테스트이름";
+        // when
+        Car car = new Car(appropriateName);
+        // then
+        assertThat(car.getStatus().getName()).isEqualTo(appropriateName);
+    }
+
+    @ParameterizedTest
+    @DisplayName("예외 발생 - 잘못된 이름 길이")
+    @ValueSource(strings = {"아주 긴 이름입니다", " "})
+    void shouldThrowIllegalArgumentExceptionWhenUseLongName(String inputName) {
+        // given
+        String wrongName = inputName;
+        // when
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    new Car(wrongName);
+                });}
 }
