6743f2d9b2c0eae62b88d676160fe3ea587b6789
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 15:55:41 +0900

    <refactor>(domain/CarsTest.java): test 로직을 직관적이게 리팩터링

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -2,39 +2,43 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import utils.NumberGenerator;
 
 import java.util.List;
 
 import static domain.Car.MINIMUM_NUMBER_TO_MOVE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarsTest {
 
-    @DisplayName("findWinners()를 통해 position이 가장 높은 Car들을 가져온다")
-    @Test
-    void test1() {
+    private final NumberGenerator mockNumberGenerator = new MockNumberGenerator();
+
+    @DisplayName("moveCars를 통해 조건을 충족시킨 Car를 move 시킨다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:1", "1:0", "2:1"}, delimiter = ':')
+    void test1(int index, int expectedPosition) {
         Car carA = new Car("carA");
         Car carB = new Car("carB");
         Car carC = new Car("carC");
 
         Cars cars = new Cars(List.of(carA, carB, carC));
 
-        carA.move(MINIMUM_NUMBER_TO_MOVE);
-        carB.move(MINIMUM_NUMBER_TO_MOVE);
-        List<Car> winners = cars.findWinners();
+        cars.moveCars(mockNumberGenerator);
 
-        assertThat(winners).containsExactly(carA, carB);
+        Car carAfterMove = cars.getCars().get(index);
+        assertEquals(carAfterMove.getPosition(), expectedPosition);
     }
 
-    @DisplayName("moveCars를 통해 조건을 충족시킨 Car를 move 시킨다.")
+    @DisplayName("findWinners()를 통해 position이 가장 높은 Car들을 가져온다")
     @Test
     void test2() {
         Car carA = new Car("carA");
         Car carB = new Car("carB");
         Car carC = new Car("carC");
 
-        NumberGenerator mockNumberGenerator = new MockNumberGenerator();
         Cars cars = new Cars(List.of(carA, carB, carC));
 
         cars.moveCars(mockNumberGenerator);
