5748f092fff3c36080878af1063ee8e4ca279a0a
0chil <0@chll.it>
2023-02-09 15:34:28 +0900

    refactor(option): 상수 클래스 생성

---
+++ src/main/java/car/option/Option.java
@@ -0,0 +1,8 @@
+package car.option;
+
+public class Option {
+    public static final int INITIAL_POSITION = 0;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MIN_TRIAL_COUNT = 0;
+}
