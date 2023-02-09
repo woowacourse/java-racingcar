90f99bc0a400a46688c36fbff71f468fed380924
kokodak <kokodakadokok@gmail.com>
2023-02-09 11:03:30 +0900

    feat(stringParser): parse 메서드 구현

--- src/main/java/utils/StringParser.java
+++ src/main/java/utils/StringParser.java
@@ -1,9 +1,12 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
     public static List<String> parse(String input) {
-        return null;
+        String[] split = input.split(",");
+        return Arrays.asList(split);
     }
 }
