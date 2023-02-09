736e5ad041a8c1fb5087f6c311193d98e5dc9829
0chil <0@chll.it>
2023-02-09 10:44:01 +0900

    refactor(Game): 중복 코드 제거, 메서드명 변경

--- src/test/java/car/GameTest.java
+++ src/test/java/car/GameTest.java
@@ -19,7 +19,7 @@ public class GameTest {
         Car car3 = new Car("루쿠", 1);
         Game game = new Game(List.of(car1, car2, car3), MOVABLE_CHANCE);
 
-        assertThat(game.getWinners()).containsOnly(car2);
+        assertThat(game.findWinners()).containsOnly(car2);
     }
 
     @Test
@@ -30,7 +30,7 @@ public class GameTest {
         Car car3 = new Car("루쿠", 1);
         Game game = new Game(List.of(car1, car2, car3), MOVABLE_CHANCE);
 
-        assertThat(game.getWinners()).containsOnly(car1, car2);
+        assertThat(game.findWinners()).containsOnly(car1, car2);
     }
 
     @Test
