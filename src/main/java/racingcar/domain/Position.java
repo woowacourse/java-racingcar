fbbb259d796ab798e607841ae40743256b791f07
jeonghoon <shb03323@naver.com>
2023-02-09 12:19:07 +0900

    feat: Position 도메인 생성

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Position {
+    private int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public void move() {
+        position++;
+    }
+}
