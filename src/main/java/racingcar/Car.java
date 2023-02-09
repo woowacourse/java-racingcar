67b0c18b838ca17fa1a0c541c95b383b067d9b2a
kevstevie <jianna442@gmail.com>
2023-02-09 11:44:38 +0900

    refactor: Car 생성자에서 이름 유효성 검증

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.util.Validator;
+
 public class Car {
 
+    public static final int MOVE_RANGE = 1;
     private static final int INIT_POSITION = 0;
     private static final int BOUNDARY = 4;
 
@@ -9,13 +12,14 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        Validator.validateCarNames(name);
         this.name = name;
         this.position = INIT_POSITION;
     }
 
     public void move(int number) {
         if (number >= BOUNDARY) {
-            this.position += 1;
+            this.position += MOVE_RANGE;
         }
     }
 
