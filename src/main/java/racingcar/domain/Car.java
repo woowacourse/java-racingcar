799da65983ecde69d8207ee1ca661cbe917c9ec1
jeonghoon <shb03323@naver.com>
2023-02-09 12:24:46 +0900

    feat: 최중 우승자 선정 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -12,17 +12,36 @@ public class Car {
         this.currentPosition = new Position(startPoint);
     }
 
-    public void move(NumberGenerator numberGenerator) {
+    public String getName() {
+        return name;
+    }
+
+    public CarStatus move(NumberGenerator numberGenerator) {
         int randomNumber = numberGenerator.generate();
         if (isMovable(randomNumber)) {
             currentPosition.move();
         }
+        return new CarStatus(name, currentPosition);
     }
+
+    public CarStatus getCarStatus() {
+        return new CarStatus(name, currentPosition);
+    }
+
     public void validateName(String input) {
         if (input.length() > 5 || input.length() <1) {
             throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
         }
     }
+
+    public int compareTo(Car other) {
+        return this.currentPosition.getPosition() - other.currentPosition.getPosition();
+    }
+
+    public boolean isSamePosition(Car target) {
+        return currentPosition.equals(target.currentPosition);
+    }
+
     private boolean isMovable(int number) {
         return number >= MINIMUM_NUMBER_TO_MOVE;
     }
--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Position {
     private int position;
 
@@ -9,5 +11,26 @@ public class Position {
 
     public void move() {
         position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position p = (Position) o;
+        return Objects.equals(position, p.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
