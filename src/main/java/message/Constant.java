707f409a164949676a1de843839d1feee8d698f1
jaeyeon kim <jakind@naver.com>
2023-02-09 13:57:55 +0900

    refactor(Constant, Error) : 상수 클래스 생성

---
+++ src/main/java/message/Constant.java
@@ -0,0 +1,18 @@
+package message;
+
+public class Constant {
+
+    public static final String RESULT = "실행 결과";
+    public static final String WIN_MENTION = "%s가 최종 우승했습니다.%n";
+    public static final String SEPARATOR = ",";
+    public static final String WINNER_CONNECTOR = ", ";
+    public static final String STICK = "-";
+
+    public static final int UPPER_BOUND = 10;
+    public static final int START_DISTANCE = 0;
+    public static final int LOWER_BOUND = 1;
+//    public static final int UPPER_BOUND = 5;
+    public static final int MOVE_BOUNDARY = 4;
+    public static final String CAR_NAMES_MENTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String NUMBER_OF_TIME_MENTION = "시도할 횟수는 몇회인가요?";
+}
---
+++ src/main/java/message/Error.java
@@ -0,0 +1,8 @@
+package message;
+
+public class Error {
+
+    public static final String NON_NUMBER_ERROR = "숫자만 입력 가능합니다.";
+    public static final String CAR_NAME_LENGTH_ERROR = "자동차명은 1 ~ 5 글자로 입력해야합니다.";
+    public static final String NUMBER_OF_TIME_RANGE_ERROR = "1 이상만 가능합니다.";
+}
