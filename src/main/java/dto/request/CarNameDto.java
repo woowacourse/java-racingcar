4774a0789925e36947f48382289c7ea9c0c1767d
kwonyj1022 <kwonyj1022@naver.com>
2023-02-09 14:07:06 +0900

    <feat>(domain/Car.java): 이름에 공백이 포함되면 예외를 반환

--- src/main/java/dto/request/CarNameDto.java
+++ src/main/java/dto/request/CarNameDto.java
@@ -1,5 +1,6 @@
 package dto.request;
 
+import static domain.Car.BLANK;
 import static domain.Car.MAX_NAME_LENGTH;
 
 public class CarNameDto {
@@ -16,9 +17,21 @@ public class CarNameDto {
     }
 
     private void validate(String name) {
+        validateToContainBlank(name);
+        validateLength(name);
+    }
+
+    private void validateLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("[Error] 이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. " +
+            throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. " +
                     "입력값 : "  + name);
+        }
+    }
+
+    private void validateToContainBlank(String name) {
+        if (name.contains(BLANK)) {
+            throw new IllegalArgumentException("이름에 공백을 포함할 수 없습니다." +
+                    "입력값 : " + name);
         }
     }
 }
