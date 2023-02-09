101dd342da3037497a328c5bf0afebb009dd84fc
seokhwan-an <asok369258@naver.com>
2023-02-09 16:43:23 +0900

    test(view) : 사용자 시도 횟수 입력값이 숫자인지 검증하는 기능 테스트

---
+++ src/test/java/view/InputViewTest.java
@@ -0,0 +1,32 @@
+package view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.view.InputView;
+
+class InputViewTest {
+
+    InputView inputView = new InputView();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"r", "ㄱ", ",", "0"})
+    @DisplayName("시도 횟수 사용자 입력 값이 옳바른 지 검증하는 테스트")
+    void validateRoundTest(String input){
+        //Given
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        //When
+        Throwable result = catchThrowable(()->{inputView.readRacingRound();});
+
+
+        //Then
+        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    }
+}
