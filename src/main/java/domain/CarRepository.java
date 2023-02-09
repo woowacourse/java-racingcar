33f6659a04e0f03918b2f2689d44a814afcc0e93
yeonkkk <tjddus0302@gmail.com>
2023-02-09 16:11:38 +0900

    feat: CarRepository 인터페이스 선언

--- src/main/java/domain/CarRepository.java
+++ src/main/java/domain/CarRepository.java
@@ -1,50 +1,17 @@
 package domain;
 
-import static utils.Constant.ADD_POINT;
-import static utils.Constant.MOVING_STANDARD_NUM;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import strategy.MovingStrategy;
 
-public class CarRepository {
+public interface CarRepository {
 
-    private Map<Car, Integer> carBoard;
+    void cycleCars(MovingStrategy movingStrategy);
+    void move(Car car,int number);
+    boolean isAllowedToMove(int number);
+    List<Car> findWinners();
+    Map<Car, Integer> getCarBoard();
 
-    public CarRepository(Map<Car, Integer> carBoard) {
-        this.carBoard = carBoard;
-    }
+    void insertCarBoard(Map<Car, Integer> cars);
 
-    public void cycleCars(MovingStrategy movingStrategy) {
-        for (Car car : carBoard.keySet()) {
-            move(car, movingStrategy.getNum());
-        }
-    }
-
-    public void move(Car car, int number) {
-        if (isAllowedToMove(number)) {
-            carBoard.put(car, carBoard.get(car) + ADD_POINT);
-        }
-    }
-
-    public boolean isAllowedToMove(int number) {
-        if (number >= MOVING_STANDARD_NUM) {
-            return true;
-        }
-        return false;
-    }
-
-
-    public List<Car> findWinners() {
-        int maxValue = Collections.max(carBoard.values());
-        return carBoard.keySet().stream()
-            .filter(x -> carBoard.get(x) == maxValue)
-            .collect(Collectors.toList());
-    }
-
-    public Map<Car, Integer> getCarBoard() {
-        return carBoard;
-    }
 }
