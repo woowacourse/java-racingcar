44e0c41605845759a335b21a9e12663303185770
SproutMJ <mjk54565@gmail.com>
2023-02-09 11:35:20 +0900

    feat: 자동차의 정보를 전달받아 이름과 위치를 뷰에 넘겨주는 기능

---
+++ src/main/java/RacingGameController.java
@@ -0,0 +1,42 @@
+import domain.Car;
+import service.RacingGame;
+import service.RandomNumberGenerator;
+import ui.InputView;
+import ui.OutputView;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class RacingGameController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final RacingGame racingGame = new RacingGame(new RandomNumberGenerator());
+
+//    public void run() {
+//        List<String> carNames = inputView.requestCarNames();
+//        racingGame.generateCars(carNames);
+//        int trialCount = inputView.requestTrialCount();
+//        race(trialCount);
+//    }
+//
+//    private void race(int trialCount) {
+//        showCurrentGameResult();
+//        for (int i = 0; i < trialCount; i++) {
+//            racingGame.processAllCars();
+//            showCurrentGameResult();
+//        }
+//    }
+
+    private void showCurrentGameResult() {
+        List<Car> gameResult = racingGame.getCars();
+        outputView.printGameResult(convertGameResult(gameResult));
+
+    }
+
+    private TreeMap<String, Integer> convertGameResult(List<Car> gameResult) {
+        TreeMap<String, Integer> converted = new TreeMap<>();
+        gameResult.forEach(car -> converted.put(car.getName(), car.getPosition()));
+        return converted;
+    }
+}
