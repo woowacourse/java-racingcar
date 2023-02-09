1296deb26b7a6b0bb6be120f06b2a29a7b4b9280
xxeol2 <jhj01177@naver.com>
2023-02-09 13:04:27 +0900

    feat: RaceController 생성

---
+++ src/main/java/controller/RaceController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import domain.Car;
+import domain.Race;
+import java.util.List;
+import repository.CarRepository;
+import utils.Judge;
+
+public class RaceController {
+
+    private final CarRepository carRepository;
+    private Race race;
+    private final int DRIVING_DISTANCE = 1;
+
+    public RaceController() {
+        carRepository = new CarRepository();
+    }
+
+    public void playGame(int totalCount) {
+        race = new Race(totalCount);
+        while (!race.isFinished()) {
+            playRound();
+        }
+    }
+
+    public void addAllParticipants(List<String> carNames) {
+        carNames.forEach(carRepository::add);
+    }
+
+    private void playRound() {
+        List<Car> participants = carRepository.findAll();
+        participants.forEach(this::driveOrNot);
+        race.addCount();
+    }
+
+    private void driveOrNot(Car car) {
+        int number = car.chooseNumber();
+        if (Judge.isAble(number)) {
+            car.drive(DRIVING_DISTANCE);
+        }
+    }
+}
