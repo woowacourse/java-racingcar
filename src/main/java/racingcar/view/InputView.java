446f4e53d811bbbebaacc495683026a63adf4f66
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 15:29:15 +0900

    chore: InputView 메소드들의 세부 내용을 정리한다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -16,25 +16,22 @@ public class InputView {
 
     public List<String> readCars() {
         outputView.printNames();
+
         String line = scanner.nextLine();
         validateLine(line);
-        String[] splitLine = line.split(",");
-        return Arrays.asList(splitLine);
-    }
 
+        return Arrays.asList(line.split(","));
+    }
 
     public int readGameRound() {
         outputView.printGameRoundGuide();
+
         String line = scanner.nextLine();
+
         return Integer.parseInt(line);
     }
 
     private void validateLine(String line) {
-        isBlank(line);
-        String format = "[a-zA-z][2]+";
-    }
-
-    private void isBlank(String line) {
         if (line.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
         }
