0562b2f48cd636df32eab3f5bf74f5f1c48d2e8c
BGuga <tmdwns9913@gmail.com>
2023-02-09 14:20:54 +0900

    test(Car): test Car Comparable

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,11 +2,14 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.domain.Car;
 import racingcar.domain.CarMovement;
 import racingcar.domain.CarName;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,5 +23,17 @@ public class CarTest {
         Car car = new Car("푸우");
         car.move(carMovement);
         assertThat(car).extracting("pos").extracting("value").isEqualTo(position);
+    }
+
+    @Test
+    @DisplayName("자동차 비교 테스트")
+    void carCompareTest() {
+        Car 에코 = new Car("에코");
+        Car 푸우 = new Car("푸우");
+
+        List<Car> cars = List.of(에코, 푸우);
+        에코.move(new CarMovement(4));
+
+        assertThat(에코.compareTo(푸우)).isEqualTo(1);
     }
 }
