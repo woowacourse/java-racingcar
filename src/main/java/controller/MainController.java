23a6aeb32e298c7ec48d4e9d1c3a9141e0fc91ef
Eunkeee <eunkeeeee@gmail.com>
2023-02-09 10:42:13 +0900

    refactor(MainController): 메서드 분리

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -18,19 +18,29 @@ public class MainController {
 
     public void play() {
         try {
-            List<String> carNames = inputView.readCarNames();
-            for (String carName : carNames) {
-                CarRepository.addCars(new Car(carName));
-            }
-            int moveCount = inputView.readMoveCount();
-            outputView.printResultMessage();
-            for (int i = 0; i < moveCount; i++) {
-                CarRepository.moveAllCars();
-                outputView.printResult(CarRepository.cars());
-            }
-            outputView.printWinners(CarRepository.getWinners());
+            setCars();
+            moveCars();
         } catch (IllegalArgumentException exception) {
             outputView.printExceptionMessage(exception);
+        }
+    }
+
+    private void moveCars() {
+        int moveCount = inputView.readMoveCount();
+        outputView.printResultMessage();
+        moveAllCars(moveCount);
+        outputView.printWinners(CarRepository.getWinners());
+    }
+
+    private void setCars() {
+        List<String> carNames = inputView.readCarNames();
+        carNames.stream().map(Car::new).forEach(CarRepository::addCars);
+    }
+
+    private void moveAllCars(int moveCount) {
+        for (int i = 0; i < moveCount; i++) {
+            CarRepository.moveAllCars();
+            outputView.printResult(CarRepository.cars());
         }
     }
 }
