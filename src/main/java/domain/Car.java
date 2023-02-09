1abd57e8e2470f454a839b0ee56c3580da02f2e2
yeonkkk <tjddus0302@gmail.com>
2023-02-09 15:32:44 +0900

    refactor(Car.java): 불필요 의존 관계 제거

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,28 +1,12 @@
 package domain;
 
-import static utils.Constant.MOVING_STANDARD_NUM;
 
 public class Car {
 
     private final Name name;
-    private StringBuffer position = new StringBuffer();
 
     public Car(Name name) {
         this.name = name;
-    }
-
-    public void move(int number) {
-        if (number >= MOVING_STANDARD_NUM) {
-            position.append("-");
-        }
-    }
-
-    public String getStatus() {
-        return name.getName() + " : " + position;
-    }
-
-    public int getPosition() {
-        return position.length();
     }
 
     public String getName() {
