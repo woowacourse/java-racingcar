536061a44c2bb21b0d521b5f6c61b40fbb51244f
hyunseo <goatsfish@gmail.com>
2023-02-09 12:23:15 +0900

    feat: Car 움직임 관리 기능 추가

---
+++ src/main/java/racingcar/domain/CarMovement.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+public class CarMovement {
+	private static final int STOP_MIN_NUMBER = 0;
+	private static final int STOP_MAX_NUMBER = 3;
+	private static final int MOVE_FORWARD_MIN_NUMBER = 4;
+	private static final int MOVE_FORWARD_MAX_NUMBER = 9;
+	NumberGenerator numberGenerator;
+
+	public CarMovement(NumberGenerator numberGenerator) {
+		this.numberGenerator = numberGenerator;
+	}
+
+	public boolean isCarMoveForward() {
+		int number = numberGenerator.generateNumber();
+		if (isStop(number)) {
+			return false;
+		}
+		if (isForward(number)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isForward(int number) {
+		return (number >= MOVE_FORWARD_MIN_NUMBER && number <= MOVE_FORWARD_MAX_NUMBER);
+	}
+
+	private boolean isStop(int number) {
+		return (number >= STOP_MIN_NUMBER && number <= STOP_MAX_NUMBER);
+	}
+}
