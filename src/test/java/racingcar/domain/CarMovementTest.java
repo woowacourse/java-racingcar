8ff791a0a0b9c73cac8627346fa72e3e675fa8ce
hyunseo <goatsfish@gmail.com>
2023-02-09 14:11:49 +0900

    test: CarMovement 테스트 추가

---
+++ src/test/java/racingcar/domain/CarMovementTest.java
@@ -0,0 +1,45 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static racingcar.config.CarMovementConfig.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CarMovementTest {
+	NumberGenerator numberGenerator;
+	CarMovement carMovement;
+
+	@Test
+	void createRandomCarMovementTest() {
+		carMovement = new CarMovement(() -> 0);
+		carMovement.isCarMoveForward();
+		assertThat(carMovement).isNotNull();
+	}
+
+	@DisplayName("자동차 이동 테스트")
+	@ParameterizedTest(name = "number = {0}, isMoveForward = {1}")
+	@MethodSource("moveForwardDummy")
+	void isRandomCarMoveForwardTest(int number, boolean expected) {
+		numberGenerator = new RandomNumberGenerator();
+		carMovement = new CarMovement(() -> number);
+		boolean isCarMoveForward = carMovement.isCarMoveForward();
+		assertThat(isCarMoveForward).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> moveForwardDummy() {
+		return Stream.of(
+			Arguments.arguments(MOVE_FORWARD_MIN_NUMBER.getNumber(), true),
+			Arguments.arguments(MOVE_FORWARD_MAX_NUMBER.getNumber(), true),
+			Arguments.arguments((MOVE_FORWARD_MIN_NUMBER.getNumber()+MOVE_FORWARD_MAX_NUMBER.getNumber())/2, true),
+			Arguments.arguments(STOP_MIN_NUMBER.getNumber(), false),
+			Arguments.arguments(STOP_MAX_NUMBER.getNumber(), false),
+			Arguments.arguments((STOP_MIN_NUMBER.getNumber()+STOP_MAX_NUMBER.getNumber())/2, false)
+		);
+	}
+}
