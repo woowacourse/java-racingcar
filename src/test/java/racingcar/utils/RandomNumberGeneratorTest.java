a61bc94d800ed2acbc88f9b684443dd5ceb2bcdb
hyunseo <goatsfish@gmail.com>
2023-02-09 13:46:52 +0900

    test: 랜덤 정수 생성 테스트 수정

--- src/test/java/racingcar/utils/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/utils/RandomNumberGeneratorTest.java
@@ -1,15 +1,20 @@
 package racingcar.utils;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.RandomNumberGenerator;
+
 class RandomNumberGeneratorTest {
+	final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
 	@DisplayName("랜덤 넘버 생성 기능 테스트")
 	@RepeatedTest(10)
 	void generateRandomNumberTest() {
-		int randomNumber = RandomNumberGenerator.generateRandomNumber();
-		Assertions.assertThat(randomNumber).isBetween(0, 9);
+		int generatedRandomNumber = numberGenerator.generateNumber();
+		assertThat(generatedRandomNumber).isBetween(0, 9);
 	}
 }
