4603d575997edf50122b037f5e6f516cc12b4201
BGuga <tmdwns9913@gmail.com>
2023-02-09 15:17:12 +0900

    feat(InputView): add InputView

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,31 @@
+package racingcar.view;
+
+import racingcar.domain.TrialCount;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    Scanner input = new Scanner(System.in);
+    private final String NAME_LIST_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final String TRIAL_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
+
+    private String readLineWithMessage(String message) {
+        System.out.println(message);
+        return input.nextLine();
+    }
+
+    public List<String> getCarNames() {
+        String line = readLineWithMessage(NAME_LIST_PROMPT);
+        String[] words = line.split(",");
+        return Arrays.asList(words);
+    }
+
+    public TrialCount trialCount() {
+        String input_count = readLineWithMessage(TRIAL_COUNT_PROMPT);
+        Integer number = Integer.parseInt(input_count);
+        return new TrialCount(number);
+    }
+}
