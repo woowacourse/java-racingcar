85b42fa5f3a5c7f536ea25a82e8615181becb3c4
apptie <appti@naver.com>
2023-02-09 13:40:52 +0900

    feat: CarService 구현

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,28 @@
+package service;
+
+import dto.CarDto;
+import dto.WinnerCarDto;
+import java.util.List;
+import model.Cars;
+import utils.RacingNumberGenerator;
+
+public class CarService {
+
+    private Cars cars;
+
+    public void generateCars(String carNames) {
+        cars = new Cars(carNames);
+    }
+
+    public void race(RacingNumberGenerator generator) {
+        cars.race(generator);
+    }
+
+    public List<CarDto> getCarsDto() {
+        return cars.getCarsDto();
+    }
+
+    public List<WinnerCarDto> getWinner() {
+        return cars.getWinner();
+    }
+}
