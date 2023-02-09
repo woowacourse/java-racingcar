aaa5f32b3e2b93179c2d605a1feacd7487bf1017
chaewon121 <cw121@naver.com>
2023-02-09 11:54:57 +0900

    feat: 현재 상태를 출력하는 View 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,33 @@
 package racingcar.view;
 
+import racingcar.controller.response.MovedResultResponse;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OutputView {
 
+    private static final String CARS_STATE_FORMAT = "%s : %s\n"; // ex) name : ---
     private static final String RACE_RESULT_MESSAGE = "실행 결과";
+    private static final String BAR = "-";
+    private static final int ZERO = 0;
+
+    public static void printState(final MovedResultResponse result) {
+        Map<String, Integer> positionMap = result.getPositionMap();
+
+        String results = positionMap.keySet().stream()
+                .map(name -> String.format(CARS_STATE_FORMAT, name, makeBar(positionMap.get(name)))
+                ).collect(Collectors.joining());
+
+        System.out.println(results);
+    }
+
+    private static String makeBar(final int size) {
+        return IntStream.range(ZERO, size)
+                .mapToObj(it -> BAR)
+                .collect(Collectors.joining());
+    }
 
     public static void printResult() {
         System.out.println(RACE_RESULT_MESSAGE);
