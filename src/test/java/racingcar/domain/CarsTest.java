7787018738be42c3db64f4b0dc4ab362e7256b49
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-09 11:24:33 +0900

    test: 자동차들의 라운드 턴수가 다른 예외 상황 테스트 코드 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,22 +1,27 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class CarsTest {
 
+    private Cars cars;
+
+    @BeforeEach
+    void setup() {
+        cars = new Cars();
+        cars.add(new Car("test1"));
+        cars.add(new Car("test2"));
+    }
+
+
     @Test
     @DisplayName("자동차들의 라운드 진행 테스트")
     void getTurnCountTest() {
-        Car car1 = new Car("test1");
-        Car car2 = new Car("test2");
-
-        Cars cars = new Cars();
-        cars.add(car1);
-        cars.add(car2);
-
         cars.addNextCarValue(1);
         cars.addNextCarValue(1);
 
@@ -31,7 +36,15 @@ class CarsTest {
             softly.assertThat(firstTurnCount).isEqualTo(1);
             softly.assertThat(secondTurnCount).isEqualTo(2);
         });
+    }
 
+    @Test
+    @DisplayName("자동차들의 라운드 턴수가 다른 예외 상황 테스트")
+    void turnCountExceptionTest() {
+        for(int i=0; i<3; i++){
+            cars.addNextCarValue(3);
+        }
+        assertThatThrownBy(() -> cars.getTurnCount()).isInstanceOf(IllegalStateException.class);
     }
 
 }
