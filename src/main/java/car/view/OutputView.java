6ea25775da0e1768a37af55d61225ed0566f396a
0chil <0@chll.it>
2023-02-09 13:35:17 +0900

    feat(view): 입출력 구현

---
+++ src/main/java/car/view/OutputView.java
@@ -0,0 +1,31 @@
+package car.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import car.domain.Car;
+
+public class OutputView {
+
+    public void noticeResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printCar(Car car) {
+        System.out.print(car.getName() + " : ");
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<Car> winners) {
+        List<String> winnerNames = new ArrayList<>();
+        for (Car winner : winners) {
+            winnerNames.add(winner.getName());
+        }
+        String joinedNames = String.join(", ", winnerNames);
+        System.out.println(joinedNames + "가 최종 우승했습니다.");
+    }
+
+}
