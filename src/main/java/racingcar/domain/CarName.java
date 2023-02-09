9f6326afdda05bc39639aa766ff71567fa5e8422
eunbii0213 <eunbii0213@naver.com>
2023-02-09 13:39:41 +0900

    fix: 5글자 이상 이름 입력시 에러 메세지 출력

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,12 +1,13 @@
-package domain;
+package racingcar.domain;
 
 public final class CarName {
     public static final int MAX_LENGTH = 5;
+    private static final String NAME_ERROR_MESSAGE = "올바른 이름을 입력해주세요.";
     private final String name;
 
     public CarName(String name) {
         if (name == null || name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NAME_ERROR_MESSAGE);
         }
         this.name = name;
     }
