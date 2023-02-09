81e9b33a764e66bddeb56faf36cb241caf5fbfe7
ingpyo <juninpyo726@gmail.com>
2023-02-09 17:11:14 +0900

    test:자동차 이름 입력 예외 처리 테스트

---
+++ src/test/java/InputExceptionTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputExceptionTest {
+    InputException inputException;
+
+    @BeforeEach
+    void setUp() {
+        inputException = new InputException();
+    }
+
+    @Test
+    void checkCarNameLength() {
+        assertThatThrownBy(() -> {
+            inputException.checkCarNameLength("우아한테크코스");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
