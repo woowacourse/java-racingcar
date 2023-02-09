62de2ef3b4daecbbf80169862ca07e2d1c820e4c
hum02 <ehdgma02@gmail.com>
2023-02-09 16:47:33 +0900

    refactor: 자바 컨벤션을 위한 개행 추가

--- src/main/java/racingCar/view/OutputView.java
+++ src/main/java/racingCar/view/OutputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+
     public static void printError(String errorMsg) {
         System.out.println(errorMsg);
     }
