ed6dd8489b1153806fffe405475f975350130e19
song-wooseok <sws981202@gmail.com>
2023-02-09 11:32:59 +0900

    feat: 사용자에게 시도횟수와 자동차 이름을 입력받는 기능 구현

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,2 +1,43 @@
-package controller;public class RacingGameController {
+package controller;
+
+import domain.RacingCar;
+import domain.RacingCars;
+import domain.TryCount;
+import java.util.List;
+import java.util.stream.Collectors;
+import util.CommaSeparator;
+import view.InputView;
+
+public class RacingGameController {
+
+    private RacingCars racingCars;
+    private TryCount tryCount;
+
+    void start(){
+       setUpGame();
+    }
+
+    void setUpGame(){
+        racingCars = createRacingCar();
+        tryCount = getTryCount();
+    }
+
+    private RacingCars createRacingCar(){
+        List<String> names = CommaSeparator.sliceNameByComma(getNames());
+        return new RacingCars(createRacingCar(names));
+    }
+
+    private String getNames() {
+        return InputView.requestCarName();
+    }
+
+    private List<RacingCar> createRacingCar(List<String> names) {
+        return names.stream()
+                .map(RacingCar::new)
+                .collect(Collectors.toList());
+    }
+
+    private TryCount getTryCount(){
+        return new TryCount(InputView.requestTryCount());
+    }
 }
