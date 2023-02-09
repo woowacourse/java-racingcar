5b2c07dd4d09a66048e23a1a343bbdb378831cdd
LJW25 <dbjwl@g.skku.edu>
2023-02-09 16:34:10 +0900

    test : 사용하지 않는 패키지 제거

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.view.InputView;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
