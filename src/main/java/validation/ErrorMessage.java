54033c77641045b424f2244a8e352c025902108b
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-09 14:36:56 +0900

    refactor(ErrorMessage): 사용하지 않는 객체 제거

--- src/main/java/validation/ErrorMessage.java
+++ src/main/java/validation/ErrorMessage.java
@@ -3,7 +3,6 @@ package validation;
 public enum ErrorMessage {
 
     WRONG_NAME_SIZE("자동차 이름은 1~5 글자입니다."),
-    EMPTY_INPUT_CAR("1개 이상의 자동차를 입력해주세요."),
     NOT_DIGIT_INPUT("숫자를 입력해주세요."),
     WRONG_NUMBER_SIZE("1 이상의 시도 횟수를 입력해주세요.");
 
