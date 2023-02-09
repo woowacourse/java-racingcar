6104b32eb26a84014d9aeaaa52ec892cbd3d069f
jeonghoon <shb03323@naver.com>
2023-02-09 12:21:17 +0900

    feat: Car에 Position 도메인 연결

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,28 +1,28 @@
 package racingcar.domain;
 
+import racingcar.dto.CarStatus;
+
 public class Car {
     private static final int MINIMUM_NUMBER_TO_MOVE = 4;
     private final String name;
-    private int currentPoint;
+    private final Position currentPosition;
 
     public Car(String name, int startPoint) {
         this.name = name;
-        this.currentPoint = startPoint;
+        this.currentPosition = new Position(startPoint);
     }
 
     public void move(NumberGenerator numberGenerator) {
         int randomNumber = numberGenerator.generate();
         if (isMovable(randomNumber)) {
-            currentPoint++;
+            currentPosition.move();
         }
     }
-
     public void validateName(String input) {
         if (input.length() > 5 || input.length() <1) {
             throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
         }
     }
-
     private boolean isMovable(int number) {
         return number >= MINIMUM_NUMBER_TO_MOVE;
     }
