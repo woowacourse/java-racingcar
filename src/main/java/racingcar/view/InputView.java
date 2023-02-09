fe36bf3e4fd151be635da91978b449ee34f5c228
LJW25 <dbjwl@g.skku.edu>
2023-02-09 16:29:52 +0900

    refactor(InputView) : 상수 순서 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,11 +9,11 @@ public class InputView {
 
     private static final InputView INSTANCE = new InputView();
 
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String CAR_NAMES_DELIMITER = ",";
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String CAR_NAMES_BLANK_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 입력되지 않았습니다.";
-    private static final String CAR_NAMES_DELIMITER = ",";
     private static final String CAR_NAMES_DUPLICATE_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 중복되었습니다.";
-    private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String CAR_NAME_LENGTH_ERROR = ERROR_PREFIX + "경주할 자동차 이름이 5글자 초과했습니다.";
     private static final String CAR_NAME_BLANK_ERROR = ERROR_PREFIX + "각 자동차 이름은 빈 값일 수 없습니다.";
     private static final String TRY_NUM_BLANK_ERROR = ERROR_PREFIX + "시도할 횟수가 입력되지 않았습니다.";
