c3e483b440f976e68717422687857f32c45017d1
hanueleee <ezzeezz@naver.com>
2023-02-09 15:05:51 +0900

    test: TryCountValidator 테스트 작성

--- src/main/java/racingcar/validator/TryCountValidator.java
+++ src/main/java/racingcar/validator/TryCountValidator.java
@@ -6,7 +6,7 @@ public class TryCountValidator {
     }
 
     private void validateNumeric(String tryCount) {
-        if (tryCount != null && tryCount.matches("[0-9]+")) {
+        if (tryCount != null && tryCount.matches("[0-9]+") && !tryCount.equals("0")) {
             return;
         }
         throw new IllegalArgumentException("시도할 횟수는 자연수만 가능합니다.");
