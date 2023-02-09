1b27db5958b88f61e572a29ded90d7bfc14c653d
seokhwan-an <asok369258@naver.com>
2023-02-09 12:03:59 +0900

    feat(application) : 프로젝트 구현 완료

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import racingcar.controller.Controller;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Controller controller = new Controller(new InputView(), new OutputView(), new RandomNumberGenerator());
+        controller.run();
+    }
+
+}
