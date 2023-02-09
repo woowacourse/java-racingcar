1ba4e70b33979c07ce1a3d944b57a23f5b1585f9
bjk1649 <qhwns1649@naver.com>
2023-02-09 17:50:23 +0900

    fix: 시도횟수가 숫자가 아닐때 버퍼 비워주도록 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String INPUT_CAR_NAMES_ANNOUNCEMENT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_ANNOUNCEMENT = "시도할 회수는 몇회인가요?";
-    private static final int ILLEGAL_TRY_COUNT_INPUT = -1;
     private static final int MINIMUM_TRY_COUNT = 1;
     private static final int MAXIMUM_TRY_COUNT = 9;
     private static final String WRONG_TRY_COUNT_ANNOUNCEMENT = "[ERROR] 잘못된 시도 횟수입니다.";
@@ -39,6 +38,7 @@ public class InputView {
             System.out.println(INPUT_TRY_COUNT_ANNOUNCEMENT);
             return scanner.nextInt();
         } catch (InputMismatchException e) {
+            scanner.nextLine();
             throw new IllegalArgumentException(WRONG_TRY_COUNT_ANNOUNCEMENT);
         }
     }
