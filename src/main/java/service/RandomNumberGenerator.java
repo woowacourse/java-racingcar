411c5b5daf6ef4fafc0110127f78eabb8340587e
SproutMJ <mjk54565@gmail.com>
2023-02-09 23:03:09 +0900

    refactor: 랜던 생성 로직 오류 수정 및 범위 검증 로직 추가

--- src/main/java/service/RandomNumberGenerator.java
+++ src/main/java/service/RandomNumberGenerator.java
@@ -4,6 +4,9 @@ public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public int generate(int startInclusive, int endInclusive) {
-        return startInclusive + (int) (Math.random() * endInclusive);
+        if (!(startInclusive <= endInclusive)) {
+            throw new IllegalArgumentException("시작범위는 마지막 범위 이하여야 합니다.");
+        }
+        return startInclusive + (int) (Math.random() * (endInclusive - startInclusive));
     }
 }
