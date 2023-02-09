9381ba50ea3405a94274e0f02129a16434362053
seokhwan-an <asok369258@naver.com>
2023-02-09 16:01:11 +0900

    test(domain) : 자동자 전진 여부를 판단하는 기능 테스트

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -3,9 +3,11 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
-import org.assertj.core.condition.Negative;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 import racingcar.domain.NumberGenerator;
@@ -28,4 +30,33 @@ public class CarTest {
         assertThat(result).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"3:0", "4:1"}, delimiter = ':')
+    @DisplayName("자동차의 전진 여부를 판단한다.")
+    void goForwardTest(int randomNumber, int expectedResult) {
+        //Given
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(
+            new ArrayList<>(List.of(randomNumber)));
+        Car car = new Car("pobi", testNumberGenerator);
+
+        //When
+        car.goForward();
+
+        //Then
+        assertThat(car.getMovePosition().length()).isEqualTo(expectedResult);
+    }
+
+    static class TestNumberGenerator implements NumberGenerator {
+
+        private final List<Integer> numbers;
+
+        public TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
 }
