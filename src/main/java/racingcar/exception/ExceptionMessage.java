2ba1b6d48393cf3a86a1b0e3fed77f377e386372
seokjin8678 <rnrn8678@naver.com>
2023-02-09 11:28:52 +0900

    feat: 우승한 자동차들의 이름을 구하는 메서드 추가

--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -3,7 +3,8 @@ package racingcar.exception;
 public enum ExceptionMessage {
     OUT_OF_CAR_NAME_LENGTH("자동차의 이름은 6글자를 넘길 수 없습니다."),
     BLANK_CAR_NAME("자동차의 이름이 공백이 될 수 없습니다."),
-    DUPLICATE_CAR_NAME("자동차의 이름이 중복됩니다.");
+    DUPLICATE_CAR_NAME("자동차의 이름이 중복됩니다."),
+    EMPTY_CARS("자동차들이 존재하지 않습니다.");
 
     private final static String EXCEPTION_HEADER = "[ERROR] ";
     private final String message;
--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,12 +1,16 @@
 package racingcar.service;
 
+import static java.util.Comparator.*;
 import static java.util.stream.Collectors.*;
+import static racingcar.exception.ExceptionMessage.EMPTY_CARS;
 
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.domain.Position;
 import racingcar.dto.RacingCarNamesRequest;
 import racingcar.dto.RacingCarStatusResponse;
+import racingcar.dto.RacingCarWinnerResponse;
 
 public class RacingCarService {
     private final List<Car> cars =  new ArrayList<>();
@@ -30,5 +34,20 @@ public class RacingCarService {
         return cars.stream()
                 .map(Car::getStatus)
                 .collect(toList());
+    }
+
+    public RacingCarWinnerResponse findWinners() {
+        Position maxPosition = getMaxPosition();
+        List<Car> winners = cars.stream()
+                .filter(car -> car.isSamePosition(maxPosition))
+                .collect(toList());
+        return RacingCarWinnerResponse.of(winners);
+    }
+
+    private Position getMaxPosition() {
+        return cars.stream()
+                .map(Car::getMovedLength)
+                .max(comparing(Position::getValue))
+                .orElseThrow(() -> new IllegalStateException(EMPTY_CARS.getMessage()));
     }
 }
