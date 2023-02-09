3476ad1888881a3e35b7a6095dcb3899302aeaa3
swonny <wonny921@gmail.com>
2023-02-09 14:44:30 +0900

    fix: 테스트 시, 레포지토리의 클래스 필드에 자동차가 누적되어 발생하는 오류 해결

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
