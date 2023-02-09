965fb4e11dce37bf0a843f361df5868e22e58cb9
bjk1649 <qhwns1649@naver.com>
2023-02-09 17:05:02 +0900

    feat: 한 번의 이동이 일어날 때 마다 이동한 결과를 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,31 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.dto.CarDto;
+
 public class OutputView {
     private static final String BEFORE_PRINT_RESULT_ANNOUNCEMENT = "실행 결과";
+    private static final String NAME_AND_COUNT_PREFIX = " : ";
+    private static final String DISTANCE_PREFIX = "-";
 
     public void announceBeforePrintResult() {
         System.out.println(BEFORE_PRINT_RESULT_ANNOUNCEMENT);
+    }
+
+    public void printResult(List<CarDto> carDtos) {
+        carDtos.forEach(carDto -> {
+            System.out.print(carDto.getName());
+            System.out.print(NAME_AND_COUNT_PREFIX);
+            printMovedDistance(carDto.getMovedCount());
+            System.out.println();
+        });
+    }
+
+    private void printMovedDistance(int movedCount) {
+        for (int index = 0; index < movedCount; index++) {
+            System.out.print(DISTANCE_PREFIX);
+        }
+        System.out.println();
     }
 
 }
