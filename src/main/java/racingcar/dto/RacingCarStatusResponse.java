c79949605f3e9be0ab27a9cb67b4b7ed211829c0
seokjin8678 <rnrn8678@naver.com>
2023-02-09 10:41:38 +0900

    refactor: DTO 클래스 불변 객체로 리팩터링

--- src/main/java/racingcar/dto/RacingCarStatusResponse.java
+++ src/main/java/racingcar/dto/RacingCarStatusResponse.java
@@ -1,8 +1,8 @@
 package racingcar.dto;
 
 public class RacingCarStatusResponse {
-    private String name;
-    private int position;
+    private final String name;
+    private final int position;
 
     public RacingCarStatusResponse(String name, int position) {
         this.name = name;
@@ -13,15 +13,7 @@ public class RacingCarStatusResponse {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPosition() {
         return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 }
