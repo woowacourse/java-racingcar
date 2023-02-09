cb60b34dbf7c15d7d0fcec1fd86c9ff0c0277fdc
xxeol2 <jhj01177@naver.com>
2023-02-09 12:26:25 +0900

    feat: CarRepository 생성

---
+++ src/main/java/repository/CarRepository.java
@@ -0,0 +1,35 @@
+package repository;
+
+import domain.Car;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CarRepository {
+
+    private List<Car> cars;
+    private Map<String, Integer> nameCount;
+
+    public CarRepository() {
+        cars = new ArrayList<>();
+        nameCount = new HashMap<>();
+    }
+
+    public List<Car> findAll() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    private int addName(String name) {
+        if (!nameCount.containsKey(name)) {
+            return nameCount.put(name, 0);
+        }
+        return nameCount.merge(name, 1, Integer::sum);
+    }
+
+    public void add(String name) {
+        Car car = new Car(name, addName(name));
+        cars.add(car);
+    }
+}
