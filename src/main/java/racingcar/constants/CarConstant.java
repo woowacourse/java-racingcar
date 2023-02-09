91a2020ea5c521073839946ec539b3c655d00b95
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-09 11:27:39 +0900

    feat: 각 자동차의 이름의 길이가 1부터 5사이가 아닐 경우 예외 처리하는 기능 구현

---
+++ src/main/java/racingcar/constants/CarConstant.java
@@ -0,0 +1,16 @@
+package racingcar.constants;
+
+public enum CarConstant {
+    NAME_MAX_LENGTH(5),
+    NAME_MIN_LENGTH(0);
+
+    private final int number;
+
+    CarConstant(int number) {
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,21 @@
 package racingcar.domain;
 
 import java.util.Map;
+import racingcar.constants.CarConstant;
 
 public class Car {
     private String name;
     private int distance;
 
     public Car(String name, int distance) {
+        validateNameLength(name);
         this.name = name;
         this.distance = distance;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > CarConstant.NAME_MAX_LENGTH.getNumber() || name.length() <= CarConstant.NAME_MIN_LENGTH.getNumber()){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름의 길이는 1부터 5 사이여야 합니다.");
+        }
     }
 }
