eb0224c5b2205ec4b342b56f958add19ea797fd5
swonny <wonny921@gmail.com>
2023-02-09 14:32:28 +0900

    test: CarRepository의 findAll 메서드 테스트 추가
    - findAll() : 레포지토리에 저장된 모든 자동차 반환하는 메서드

--- src/test/java/racingcar/CarRepositoryTest.java
+++ src/test/java/racingcar/CarRepositoryTest.java
@@ -1,14 +1,34 @@
 package racingcar;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-//class CarRepositoryTest {
-//    @BeforeEach
-//    @MethodSource
-//    void setUp() {
-//        CarRepository.updateCars()
-//    }
-//}
+class CarRepositoryTest {
+    @BeforeEach
+    @MethodSource
+    void setUp() {
+        Car pobi = new Car("pobi", 2);
+        Car mery = new Car("mery", 3);
+        Car abel = new Car("abel", 2);
+        List<Car> cars = List.of(pobi, mery, abel);
+        CarRepository.updateCars(cars);
+    }
+
+    @Test
+    void findAll() {
+        List<String> carNames = CarRepository.findAll()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.toUnmodifiableList());
+
+        assertThat(carNames).isEqualTo(List.of("pobi", "mery", "abel"));
+    }
+}
