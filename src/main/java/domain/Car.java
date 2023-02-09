8740e7cc6d57f72008c1ecfa63786472784b0042
kokodak <kokodakadokok@gmail.com>
2023-02-09 11:11:15 +0900

    feat(car): 자동차 이름 검증 기능 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,13 +2,21 @@ package domain;
 
 public class Car {
     private static final int MOVED_LOWER_BOUND = 4;
+    private static final String INVALID_LENGTH_MESSAGE = "자동차의 이름은 5자 이하여야 합니다.";
 
     private String name;
     private int position;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validate(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
+        }
     }
 
     public void move(int randomNumber) {
