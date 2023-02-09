faa22cd03fcdfd2932ade04dc589c307430d99c4
apptie <appti@naver.com>
2023-02-09 13:42:25 +0900

    feat: CarController 구현

---
+++ src/main/java/controller/CarController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import service.CarService;
+import service.wrapper.Round;
+import utils.RacingNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
+public class CarController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CarService carService;
+
+    public CarController(InputView inputView, OutputView outputView, CarService carService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.carService = carService;
+    }
+
+    public void run(RacingNumberGenerator generator) {
+        carService.generateCars(inputView.inputCarsName());
+        Round round = new Round(inputView.inputRound());
+
+        outputView.printRoundStartMessage();
+        for (int count = 0; count < round.getRound(); count++) {
+            carService.race(generator);
+            outputView.printRound(carService.getCarsDto());
+        }
+        outputView.printWinner(carService.getWinner());
+    }
+}
