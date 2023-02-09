7fa474fc6af970701edb772b01103fcfa74c4218
chaewon121 <cw121@naver.com>
2023-02-09 12:27:28 +0900

    feat: 자동차들 중 우승한 자동자를 반환하는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,10 +1,14 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
 
+    private static final int FIRST_INDEX = 0;
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
     private static final int MIN_SIZE = 2;
@@ -25,5 +29,21 @@ public class Cars {
 
     public List<Car> getCars() {
         return this.cars;
+    }
+
+    public List<Car> winners() {
+        List<Car> cars = sortedCars();
+        Car winner = cars.get(FIRST_INDEX);
+
+        return cars.stream()
+                .filter(it -> it.getPosition().equals(winner.getPosition()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Car> sortedCars() {
+        List<Car> cars = new ArrayList<>(this.cars);
+        cars.sort(Comparator.comparing(Car::getPosition));
+        Collections.reverse(cars);
+        return cars;
     }
 }
