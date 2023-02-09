3d9dd616e75912a74d56230282e2a3192f768a2e
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 15:31:45 +0900

    refactor: Car 객체에 equals, hashCode 메소드를 구현한다

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain.car;
 
+import java.util.Objects;
+
 public class Car {
 
     private final String name;
@@ -38,5 +40,22 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return position == car.position && name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
