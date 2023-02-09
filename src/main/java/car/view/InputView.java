5226b2a2a0130bddbf7804b158d5829e75cbb5bb
0chil <0@chll.it>
2023-02-09 16:23:06 +0900

    refactor(InputView): 패턴 상수화

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final Pattern NOT_INTEGER_PATTERN = Pattern.compile("^\\d+");
 
     public String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -19,15 +20,15 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    private void validateTrialCountUnderMaxInteger(String input) {
-        if (input.length() >= 10) {
-            throw new IllegalArgumentException("시도 횟수는 999999999이하여야 합니다.");
+    private void validateInteger(String input) {
+        if (NOT_INTEGER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다");
         }
     }
 
-    private void validateInteger(String input) {
-        if (!Pattern.matches("\\d+", input)) {
-            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다");
+    private void validateTrialCountUnderMaxInteger(String input) {
+        if (input.length() >= 10) {
+            throw new IllegalArgumentException("시도 횟수는 999999999이하여야 합니다.");
         }
     }
 }
