106cf17bd9ac5369c8595b1a955fbd72cdb2d285
Eunkeee <eunkeeeee@gmail.com>
2023-02-09 14:53:40 +0900

    test(MoveCountValidator): 테스트 코드 작성

--- src/main/java/util/MoveCountValidator.java
+++ src/main/java/util/MoveCountValidator.java
@@ -33,7 +33,7 @@ public class MoveCountValidator {
         }
     }
 
-    private enum Message {
+    public enum Message {
         EXCEPTION_NUMBER_RANGE("1회 이상 이동해야 합니다."),
         EXCEPTION_NUMERIC("자연수만 입력 가능합니다."),
         EXCEPTION_INT_RANGE("int 범위를 초과하는 입력은 불가능합니다.");
@@ -41,6 +41,10 @@ public class MoveCountValidator {
 
         Message(String message) {
             this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
 
