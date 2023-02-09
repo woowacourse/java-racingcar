3618705a30cfe82e38437c68d1cea10e9bffac2c
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-09 10:27:20 +0900

    feat: 자동차 이름 입력받는 기능 구현

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,10 +1,11 @@
 package racingcar.service;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class CarService {
     public void initializeService() {
         OutputView.printNameInput();
-
+        String carNames = InputView.readCarNames();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String readCarNames(){
+        String carNames = scanner.nextLine();
+        return carNames;
+    }
 }
