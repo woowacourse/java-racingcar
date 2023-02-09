aada0246617284f6f6e691ec877f637e5d69de0f
jiwon <ljwon77@daum.net>
2023-02-09 12:13:59 +0900

    test: 랜덤 숫자에 따른 자동차 이동 여부에 대한 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.provider.TestProvider;
 
+import java.lang.reflect.Method;
+
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -29,5 +31,42 @@ class CarsTest {
     void givenNullCarNames_thenFail(String carNames) {
         assertThatThrownBy(() -> Cars.of(carNames))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource (ints = {4, 9})
+    @DisplayName("랜덤값이 4 이상이면 자동차가 전진한다.")
+    void givenFourMoreNumber_thenCarMove(int randomNumber) throws Exception {
+        // given
+        Cars cars = TestProvider.createTestCars();
+        Car car = TestProvider.createTestCar();
+
+        // when
+        Method method = cars.getClass().getDeclaredMethod("checkNumberAndMove", Car.class, int.class);
+        method.setAccessible(true);
+        method.invoke(cars, car, randomNumber);
+
+        // then
+        assertThat(car)
+                .isEqualTo(TestProvider.createTestMoveCar());
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {0, 3})
+    @DisplayName("랜덤값이 3 이하면 자동차가 정지한다.")
+    void givenThreeLessNumber_thenCarStop(int randomNumber) throws Exception {
+        // given
+        Cars cars = TestProvider.createTestCars();
+        Car car = TestProvider.createTestCar();
+
+        // when
+        Method method = cars.getClass().getDeclaredMethod("checkNumberAndMove", Car.class, int.class);
+        method.setAccessible(true);
+        method.invoke(cars, car, randomNumber);
+
+        // then
+        assertThat(car)
+                .isEqualTo(TestProvider.createTestCar());
     }
 }
--- src/test/java/racingcar/provider/TestProvider.java
+++ src/test/java/racingcar/provider/TestProvider.java
@@ -1,10 +1,21 @@
 package racingcar.provider;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class TestProvider {
     public static Cars createTestCars() {
         String carNames = "pobi,crong,honux";
         return Cars.of(carNames);
+    }
+
+    public static Car createTestCar() {
+        return Car.of("pobi");
+    }
+
+    public static Car createTestMoveCar() {
+        Car car = Car.of("pobi");
+        car.move();
+        return car;
     }
 }
