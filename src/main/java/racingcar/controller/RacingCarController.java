ea59bb6cac38dd1e4ef8d9d71ade880d5527c7f4
jeonghoon <shb03323@naver.com>
2023-02-09 12:25:46 +0900

    feat: 최중 결과 출력 기능 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,16 +4,24 @@ import racingcar.domain.CarRepository;
 import racingcar.domain.NumberGenerator;
 import racingcar.domain.RacingGame;
 import racingcar.domain.RandomNumberGenerator;
+import racingcar.dto.CarStatus;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.InputMismatchException;
 import java.util.List;
 
 public class RacingCarController {
+    private final RacingGame racingGame;
+
+    public RacingCarController() {
+        this.racingGame = initGame();
+    }
 
     public void run() {
-        RacingGame racingGame = initGame();
-        racingGame.race();
+        OutputView.printResultMessage();
+        race();
+        showFinalResult();
     }
 
     private RacingGame initGame() {
@@ -41,5 +49,23 @@ public class RacingCarController {
         } catch (InputMismatchException e) {
             return getTries();
         }
+    }
+
+    private void race() {
+        while (!racingGame.isFinish()) {
+            List<CarStatus> turnResult = racingGame.takeOneTurn();
+            OutputView.printCarStatus(turnResult);
+        }
+    }
+
+    private void showFinalResult() {
+        List<CarStatus> finalPosition = racingGame.getFinalPosition();
+        OutputView.printCarStatus(finalPosition);
+        prizeWinner();
+    }
+
+    private void prizeWinner() {
+        List<String> winnersName = racingGame.getWinnersName();
+        OutputView.printFinalResult(winnersName);
     }
 }
