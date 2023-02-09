d46ed6d5ade4165beadfdce2fa96304a11b13d51
kokodak <kokodakadokok@gmail.com>
2023-02-09 16:03:53 +0900

    feat(numberValidator): 검증 기능 강화

--- src/main/java/utils/NumberValidator.java
+++ src/main/java/utils/NumberValidator.java
@@ -4,12 +4,22 @@ public class NumberValidator {
 
 
     private static final String NOT_NUMBER_MESSAGE = "[ERROR] 반복횟수는 숫자여야 합니다.";
+    private static final String INVALID_NUMBER_MESSAGE = "[ERROR] 반복횟수는 0보다 커야 합니다.";
 
     public static int validate(String input) {
         try {
-            return Integer.parseInt(input);
+            int number = Integer.parseInt(input);
+            validateRange(number);
+            return number;
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMBER_MESSAGE, e);
         }
+    }
+
+    public static void validateRange(int number) {
+        if (number > 0) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
     }
 }
