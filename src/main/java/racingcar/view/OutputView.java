a42233e61bf1c5c879b27bc338c7b320b5ed82aa
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-09 15:30:05 +0900

    refactor: printResult 메소드의 파라미터가 변경됨에 따라 함수 내용을 리팩토링한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,9 @@
 package racingcar.view;
 
-import racingcar.domain.car.Car;
-import racingcar.domain.carrepository.CarRepository;
+import racingcar.domain.result.Result;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -15,23 +15,23 @@ public class OutputView {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-
     public void printResultGuide() {
         System.out.println("실행 결과");
     }
 
-    public void printResult(CarRepository carRepository) {
-        List<Car> cars = carRepository.getRepository();
-        for (Car car : cars) {
-            String position = "-".repeat(car.getPosition());
-            System.out.println(String.format("%s : %s", car.getName(), position));
+    public void printResult(Result result) {
+        List<Map<String, Integer>> results = result.getResults();
+        for (Map<String, Integer> roundResult : results) {
+            for (String key : roundResult.keySet()) {
+                String position = "-".repeat(roundResult.get(key));
+                System.out.println(String.format("%s : %s", key, position));
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 
-    public void printWinners(CarRepository carRepository) {
-        List<String> winners = carRepository.findWinner();
-        String winner = String.join(", ", winners);
+    public void printWinners(Result results) {
+        String winner = String.join(", ", results.findWinners());
         System.out.println(String.format("%s가 최종 우승했습니다.", winner));
     }
 }
