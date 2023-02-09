d153a93974df7219f508309c887033b5c5878fc4
seokhwan-an <asok369258@naver.com>
2023-02-09 16:42:48 +0900

    feat(view) : 사용자 시도 횟수 입력값이 숫자인지 검증하는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,29 +3,38 @@ package racingcar.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
     private static final int ROUND_MIN_NUM = 1;
     private static final String READ_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String READ_RACING_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ROUND_INPUT_ERROR = "[ERROR] 라운드는 숫자여야 합니다.";
     private static final String ROUND_NUMBER_ERROR = "[ERROR] 시도 횟수는 1이상이어야 합니다.";
+    private static final Pattern pattern = Pattern.compile("^[0-9]*$");
 
     public List<String> readCarNames() {
         System.out.println(READ_CARS_NAME_MESSAGE);
+        Scanner scanner = new Scanner(System.in);
         return Arrays.asList(scanner.nextLine().trim().split(","));
     }
 
     public int readRacingRound() {
         System.out.println(READ_RACING_ROUND_MESSAGE);
-        int round = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        String round = scanner.nextLine().trim();
         validateRoundInput(round);
-        return round;
+        return Integer.parseInt(round);
     }
 
-    private void validateRoundInput(int round) {
-        if (round < ROUND_MIN_NUM) {
+    private void validateRoundInput(String round) {
+        Matcher matcher = pattern.matcher(round);
+        if(!matcher.matches()) {
+            throw new IllegalArgumentException(ROUND_INPUT_ERROR);
+        }
+        if (Integer.parseInt(round) < ROUND_MIN_NUM) {
             throw new IllegalArgumentException(ROUND_NUMBER_ERROR);
         }
     }
