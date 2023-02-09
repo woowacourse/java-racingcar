b6537b527fa3965ed2eb7757d04134fbe2d4433d
hum02 <ehdgma02@gmail.com>
2023-02-09 16:20:25 +0900

    refactor(CarGroupTest): 사용되지 않는 import문 제거

--- src/test/java/racingCar/domain/CarGroupTest.java
+++ src/test/java/racingCar/domain/CarGroupTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import racingCar.util.FixedNumberGenerator;
 import racingCar.util.RandomNumberGenerator;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
