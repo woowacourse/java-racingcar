01b692c44adee25b21e7b0f614941ff337e1f7b0
chaewon121 <cw121@naver.com>
2023-02-09 11:42:56 +0900

    feat: 자동차들을 움직이는 컨트롤러 기능 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,7 +1,9 @@
 package racingcar.controller;
 
+import racingcar.controller.response.MovedResultResponse;
 import racingcar.domain.Cars;
 import racingcar.domain.Lap;
+import racingcar.domain.NumberGenerator;
 
 import java.util.Arrays;
 
@@ -10,6 +12,11 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 public class RacingCarController {
 
     private static final String DELIMITER = ",";
+    private final NumberGenerator generator;
+
+    public RacingCarController(final NumberGenerator generator) {
+        this.generator = generator;
+    }
 
     public Cars createCars(final String carNames) {
         return new Cars(Arrays.stream(carNames.split(DELIMITER))
@@ -18,5 +25,11 @@ public class RacingCarController {
 
     public Lap confirmTotalLap(final int totalLap) {
         return new Lap(totalLap);
+    }
+
+    public MovedResultResponse moveCars(final Cars cars, final Lap lap) {
+        cars.move(generator);
+        lap.next();
+        return new MovedResultResponse(cars);
     }
 }
--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -3,13 +3,15 @@ package racingcar.controller;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.Cars;
-import racingcar.domain.Lap;
+import racingcar.domain.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("RacingCarController 는 ")
 class RacingCarControllerTest {
 
-    private RacingCarController controller = new RacingCarController();
+    private RacingCarController controller = new RacingCarController(new NumberGenerator());
 
     @Nested
     @DisplayName("성공 테스트")
@@ -41,6 +43,65 @@ class RacingCarControllerTest {
 
             // then
             assertThat(lap.getTotalLap()).isEqualTo(totalLapInput);
+        }
+
+        @Test
+        @DisplayName(
+                "moveCars() 시 Cars와 Lap을 받으면 자동차를 랜덤값에 따라 움직이고 현재 바퀴수를 1만큼 증가시킨다."
+        )
+        void success_test_3_version_1() {
+            // given
+            int totalLapInput = 3;
+            List<String> carNames = List.of("말랑", "채채");
+            MoveMethodCalledCountStoreCars cars = new MoveMethodCalledCountStoreCars(carNames);
+            Lap lap = new Lap(totalLapInput);
+            int beforeCurrentLap = lap.getCurrentLap();
+
+            // when
+            controller.moveCars(cars, lap);
+
+            // then
+            assertThat(cars.getCallCount()).isEqualTo(1);
+            assertThat(lap.getCurrentLap()).isEqualTo(beforeCurrentLap + 1);
+        }
+
+        /**
+         * success_test_3_version_1 과
+         * (cars.moveCars()의 호출 여부만을 검증,
+         * cars.moveCars()는 Cars 테스트를 통해 정상동작함을 확인할 수 있기 때문에 이곳에서는 호출만을 검증한다.)
+         * <p>
+         * success_test_3_version_2(전체 동작이 잘 이루어지는지 검증)
+         * <p>
+         * 의 방법 중 어떤 것이 더 좋다고 생각하시나요?
+         */
+        @Test
+        @DisplayName(
+                "moveCars() 시 Cars와 Lap을 받으면 자동차를 랜덤값에 따라 움직이고 현재 바퀴수를 1만큼 증가시킨다."
+        )
+        void success_test_3_version_2() {
+            // given
+            int totalLapInput = 3;
+            List<String> carNames = List.of("말랑", "채채");
+            Cars cars = new Cars(carNames);
+            Lap lap = new Lap(totalLapInput);
+            RacingCarController controller = new RacingCarController(
+                    new MockFixedNumberGenerator(4));
+            List<Position> beforeMovePositions =
+                    cars.getCars().stream().map(Car::getPosition).collect(Collectors.toList());
+            int beforeCurrentLap = lap.getCurrentLap();
+
+            // when
+            controller.moveCars(cars, lap);
+
+            // then
+            List<Position> afterMovePositions =
+                    cars.getCars().stream().map(Car::getPosition).collect(Collectors.toList());
+
+            for (int i = 0; i < beforeMovePositions.size(); i++) {
+                assertThat(afterMovePositions.get(i).getValue())
+                        .isEqualTo(beforeMovePositions.get(i).getValue() + 1);
+            }
+            assertThat(lap.getCurrentLap()).isEqualTo(beforeCurrentLap + 1);
         }
     }
 
@@ -72,5 +133,24 @@ class RacingCarControllerTest {
                 Arguments.of(""),
                 Arguments.of(" ")
         );
+    }
+
+    static class MoveMethodCalledCountStoreCars extends Cars {
+
+        private int count = 0;
+
+        public MoveMethodCalledCountStoreCars(List<String> cars) {
+            super(cars);
+        }
+
+        @Override
+        public void move(NumberGenerator generator) {
+            super.move(generator);
+            count++;
+        }
+
+        public int getCallCount() {
+            return count;
+        }
     }
 }
