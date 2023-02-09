70e1f157b59371a0b634c4145981e43fe285ff9f
Eunkeee <eunkeeeee@gmail.com>
2023-02-09 14:15:11 +0900

    test(RandomCarMoveManager): 테스트 코드 작성

---
+++ src/test/java/model/manager/RandomCarMoveManagerTest.java
@@ -0,0 +1,18 @@
+package model.manager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RandomCarMoveManagerTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false", "1:false", "2:false", "3:false", "4:true", "5:true", "7:true", "8:true",
+            "9:true"}, delimiter = ':')
+    void boundaryTest(String input, String expected) {
+        assertThat(new RandomCarMoveManager().isMove(Integer.parseInt(input))).isEqualTo(
+                Boolean.parseBoolean(expected));
+    }
+
+}
