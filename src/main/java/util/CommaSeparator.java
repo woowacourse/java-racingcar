0d956c01f8e4678d08070ba5c5dd3474eff0935f
song-wooseok <sws981202@gmail.com>
2023-02-09 14:29:06 +0900

    refactor:for문을 Collection API로 변경

--- src/main/java/util/CommaSeparator.java
+++ src/main/java/util/CommaSeparator.java
@@ -1,5 +1,7 @@
 package util;
 
+import static java.util.Collections.addAll;
+
 import exception.CommaNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,24 +10,22 @@ public class CommaSeparator {
 
     private static final String COMMA = ",";
 
-    public static List<String> sliceNameByComma(final String names){
+    public static List<String> sliceNameByComma(final String names) {
         validateComma(names);
 
         return getSplitName(names);
     }
 
-    private static List<String> getSplitName(final String names){
+    private static List<String> getSplitName(final String names) {
         List<String> splitNames = new ArrayList<>();
 
-        for(String name: names.split(COMMA)){
-            splitNames.add(name);
-        }
+        addAll(splitNames, names.split(COMMA));
 
         return splitNames;
     }
 
-    private static void validateComma(final String names){
-        if(!names.contains(COMMA)){
+    private static void validateComma(final String names) {
+        if (!names.contains(COMMA)) {
             throw new CommaNotFoundException("[ERROR] 쉼표로 이름을 구분해주세요.");
         }
     }
