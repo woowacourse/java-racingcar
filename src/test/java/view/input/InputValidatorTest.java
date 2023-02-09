2ebee8bc8bec40fc79b128389b50d5385ed90d52
woosung1223 <dntjd991223@naver.com>
2023-02-09 13:27:21 +0900

    test: 입력 예외처리 테스트 생성

---
+++ src/test/java/view/input/InputValidatorTest.java
@@ -0,0 +1,62 @@
+package view.input;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class InputValidatorTest {
+
+    private final InputValidator inputValidator = new InputValidator();
+
+    @ParameterizedTest
+    @MethodSource("parameterProviderWhenFail")
+    void validateCarNameShouldBeFail(List<String> splitCarName) {
+        assertThatThrownBy(() -> inputValidator.validateCarName(splitCarName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private Stream<Arguments> parameterProviderWhenFail() {
+        return Stream.of(
+                Arguments.of(List.of("pobi", " ")),
+                Arguments.of(List.of(" ", "\n")),
+                Arguments.of(List.of("abcdef", "qwert"))
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterProviderWhenSuccess")
+    void validateCarNameShouldBeSuccess(List<String> splitCarName) {
+        assertThatCode(() -> inputValidator.validateCarName(splitCarName))
+                .doesNotThrowAnyException();
+    }
+
+    private Stream<Arguments> parameterProviderWhenSuccess() {
+        return Stream.of(
+                Arguments.of(List.of("pobi", "qwe")),
+                Arguments.of(List.of("q", "abcde"))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "abc", " ", "10001", "3.5"})
+    void validateGameTryShouldBeFail(String gameTry) {
+        assertThatThrownBy(() -> inputValidator.validateGameTry(gameTry))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10000", "5000"})
+    void validateGameTryShouldBeSuccess(String gameTry) {
+        assertThatCode(() -> inputValidator.validateGameTry(gameTry))
+                .doesNotThrowAnyException();
+    }
+}
