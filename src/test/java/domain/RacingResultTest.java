c77b222f0ea67eacaa96e708a9d8c80f58f59ef4
GyeongHwanBaek <cvc100326@naver.com>
2023-02-10 16:50:41 +0900

    test: 우승한 자동차를 불러올때 기록이 없는 경우 예외가 발생하는 지에 대한 테스트

--- src/test/java/domain/RacingResultTest.java
+++ src/test/java/domain/RacingResultTest.java
@@ -15,28 +15,39 @@ class RacingResultTest {
     @Test
     void checkWinner() {
         Map<String, Integer> history = new HashMap<>();
-        history.put("a",4);
-        history.put("b",2);
-        history.put("c",3);
-        history.put("d",4);
+        history.put("a", 4);
+        history.put("b", 2);
+        history.put("c", 3);
+        history.put("d", 4);
         RacingResult racingResult = new RacingResult(history);
 
         List<String> winners = racingResult.getWinner();
 
-        assertThat(winners).isEqualTo(List.of("a","d"));
+        assertThat(winners).isEqualTo(List.of("a", "d"));
     }
 
     @DisplayName("최종 우승 자동차 대수가 1대인 경우를 확인한다.")
     @Test
     void checkWinnerWhenWinnerIsOnlyOne() {
         Map<String, Integer> history = new HashMap<>();
-        history.put("a",4);
-        history.put("b",2);
-        history.put("c",3);
+        history.put("a", 4);
+        history.put("b", 2);
+        history.put("c", 3);
         RacingResult racingResult = new RacingResult(history);
 
         List<String> winners = racingResult.getWinner();
 
         assertThat(winners).isEqualTo(List.of("a"));
+    }
+
+    @DisplayName("우승한 자동차를 불러올때 기록이 없는 경우 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenResultIsEmpty() {
+        Map<String, Integer> history = new HashMap<>();
+
+        RacingResult racingResult = new RacingResult(history);
+
+        assertThatThrownBy(() -> racingResult.getWinner())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
