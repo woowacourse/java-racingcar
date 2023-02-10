6f729f01e4eb45858a55fa307c7444537a52fd31
swonny <wonny921@gmail.com>
2023-02-10 18:05:43 +0900

    refactor: 랜덤 값을 구현하는 전략패턴의 구현체를 라운드를 반복하는 메서드 안에서 변수로 따로 분리
    - 불필요한 객체 생성을 피하기 위해 변수로 분리합니다.

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -9,8 +9,10 @@ public class RacingCarGame {
     public void repeatRounds() {
 //        TODO : 유효성 검사 validator 객체로 추출 => NumberOfTryValidator.validate(numberOfTry)
         // NumberOfTryValidator.validate(); => "숫자만 입력할 수 있습니다."
+        MoveStrategy moveStrategy = new RandomBasedStrategy();
+
         for (Car car : CarRepository.findAll()) {
-            car.move(new RandomBasedStrategy());
+            car.move(moveStrategy);
         }
     }
 
