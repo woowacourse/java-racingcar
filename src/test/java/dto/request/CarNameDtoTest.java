f8e57b92197960d1c28f3d39edd539903ab2798f
kwonyj1022 <kwonyj1022@naver.com>
2023-02-10 21:24:01 +0900

    test: Car 이름 검증 테스트에서 실패 이유가 달라도 테스트가 성공하는 오류 수정

--- src/test/java/dto/request/CarNameDtoTest.java
+++ src/test/java/dto/request/CarNameDtoTest.java
@@ -3,6 +3,7 @@ package dto.request;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static domain.Car.MAX_NAME_LENGTH;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
@@ -17,12 +18,14 @@ class CarNameDtoTest {
     @DisplayName("Car의 이름이 5자 초과이면 예외를 발생시킨다.")
     @Test
     void Car_이름_검증_실패_길이초과() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto("잘못된자동차이름"));
+        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto("잘못된자동차이름"))
+                .withMessageContaining("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. ");
     }
 
     @DisplayName("Car의 이름에 공백이 포함되면 예외를 발생시킨다.")
     @Test
     void Car_이름_검증_실패_공백포함() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto(" bad"));
+        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto(" bad"))
+                .withMessageContaining("이름에 공백을 포함할 수 없습니다." );
     }
 }
