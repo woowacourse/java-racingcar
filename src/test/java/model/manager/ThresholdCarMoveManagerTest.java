9bd7431e3fed985f70cba6b453f46e5532d5317a
hectick <chyeon1223@cau.ac.kr>
2023-02-10 14:59:39 +0900

    test(ThresholdCarMoveManager): 테스트 클래스 이름 변경

--- src/test/java/model/manager/ThresholdCarMoveManagerTest.java
+++ src/test/java/model/manager/ThresholdCarMoveManagerTest.java
@@ -5,13 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class RandomCarMoveManagerTest {
+class ThresholdCarMoveManagerTest {
 
     @ParameterizedTest
     @CsvSource(value = {"0:false", "1:false", "2:false", "3:false", "4:true", "5:true", "7:true", "8:true",
             "9:true"}, delimiter = ':')
     void boundaryTest(String input, String expected) {
-        assertThat(new RandomCarMoveManager().isMove(Integer.parseInt(input))).isEqualTo(
+        assertThat(new ThresholdCarMoveManager().isMove(Integer.parseInt(input))).isEqualTo(
                 Boolean.parseBoolean(expected));
     }
 
