a6654ec0a884b10e3a94b3f6f6eab86627f5915a
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-10 20:56:02 +0900

    fix: 자동차 이름의 양옆 공백 제거 구현

--- src/main/java/racingcar/domain/CarNames.java
+++ src/main/java/racingcar/domain/CarNames.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarNames {
 
@@ -10,7 +11,8 @@ public class CarNames {
     private final String CAR_NAME_DELIMITER = ",";
 
     public CarNames(String carNames) {
-        names = List.copyOf(Arrays.asList(carNames.split(CAR_NAME_DELIMITER)));
+        List<String> splitNames = Arrays.asList(carNames.split(CAR_NAME_DELIMITER));
+        names = splitNames.stream().map(String::trim).collect(Collectors.toUnmodifiableList());
     }
 
     public List<String> getNames() {
