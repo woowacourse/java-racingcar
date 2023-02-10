30c79e76af459b3bd34eca0788c438542d3b4fa0
sosow0212 <sosow0212@naver.com>
2023-02-10 15:26:12 +0900

    remove : (CarsTest) 불필요한 메서드 제거

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,13 +1,12 @@
 package domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 import java.util.List;
 import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class CarsTest {
 
@@ -15,11 +14,11 @@ class CarsTest {
     @DisplayName("getWinner() : 가장 많이 움직인 자동차를 반환해준다.")
     void test_getWinner() {
         // given
-        final Car pobi = createCar("pobi");
+        final Car pobi = new Car("pobi");
         pobi.move();
         pobi.move();
 
-        final Car crong = createCar("crong");
+        final Car crong = new Car("crong");
         crong.move();
 
         Cars cars = new Cars(List.of(pobi, crong));
@@ -31,20 +30,15 @@ class CarsTest {
         assertThat(result).contains(pobi.getName());
     }
 
-    private Car createCar(String name) {
-        return new Car(name);
-    }
-
-
     @Test
     @DisplayName("getCurrentStatus() : 현재 움직인 거리를 보여준다.")
     void test_getCurrentStatus() {
         // given
-        final Car pobi = createCar("pobi");
+        final Car pobi = new Car("pobi");
         pobi.move();
         pobi.move();
 
-        final Car crong = createCar("crong");
+        final Car crong = new Car("crong");
         crong.move();
 
         Cars cars = new Cars(List.of(pobi, crong));
@@ -54,6 +48,6 @@ class CarsTest {
 
         // then
         assertThat(carCurrentStatus).hasSize(2)
-                                    .contains(entry("pobi", 2), entry("crong", 1));
+                .contains(entry("pobi", 2), entry("crong", 1));
     }
 }
