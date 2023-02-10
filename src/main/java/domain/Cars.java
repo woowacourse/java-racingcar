7e7d7935fdcb3f6ee15aa5f6438e9a98720e78cb
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-10 23:07:37 +0900

    refactor: 테스트 코드에만 쓰이던 메서드 제거

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -5,19 +5,10 @@ import java.util.List;
 
 public class Cars {
 
-    private static final int CAN_NOT_FIND_MAXIMUM = -1;
-
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
-    }
-
-    public Car findByName(String name) {
-        return cars.stream()
-                .filter(car -> car.getName().equals(name))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
     }
 
     public List<Car> getCars() {
