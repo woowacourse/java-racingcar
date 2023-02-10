37219038aba7d284ac537ef03f098973bb5e51b2
Youngho <cyh6099@gmail.com>
2023-02-10 17:54:56 +0900

    refactor : Validator -> MovingCountValidator로 이름 수정으로 인한 변화

--- src/test/java/util/MovingCountValidatorTest.java
+++ src/test/java/util/MovingCountValidatorTest.java
@@ -1,19 +1,19 @@
-package domain;
+package util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import util.Validator;
 
-@DisplayName("시도 횟수는")
+@DisplayName("시도횟수")
 class MovingCountValidatorTest {
+
     @DisplayName("1~100사이의 숫자가 아니라면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"0","101"})
     void validateMovingCountRangeTest(String input) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> Validator.validateMovingCountInput(input));
+                () -> MovingCountValidator.validateMovingCountInput(input));
     }
 
     @DisplayName("숫자가 아니라면 예외가 발생한다.")
@@ -21,6 +21,6 @@ class MovingCountValidatorTest {
     @ValueSource(strings = {"a",""})
     void validateMovingCountIsDigitTest(String input) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> Validator.validateMovingCountInput(input));
+                () -> MovingCountValidator.validateMovingCountInput(input));
     }
 }
