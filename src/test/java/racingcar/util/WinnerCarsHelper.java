6a9abdcb071ec9fb371e8bc72e321b352ea44fbb
shin-mallang <huipulci1@naver.com>
2023-02-10 17:42:44 +0900

    feat: 테스트를 위한 Reflection 편의 기능 구현

--- src/test/java/racingcar/util/WinnerCarsHelper.java
+++ src/test/java/racingcar/util/WinnerCarsHelper.java
@@ -3,7 +3,6 @@ package racingcar.util;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 public class WinnerCarsHelper {
@@ -11,10 +10,7 @@ public class WinnerCarsHelper {
     public static Cars withWinnerCars(final List<Car> cars) {
         try {
             Cars result = new Cars(List.of("쓰레기값1", "쓰레기값2"));
-            Field privateCarsField = result.getClass().getDeclaredField("cars");
-            privateCarsField.setAccessible(true);
-            privateCarsField.set(result, cars);
-
+            ReflectionTestUtils.setDeclaredField(result, "cars", cars);
             return result;
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException("Reflection Error");
