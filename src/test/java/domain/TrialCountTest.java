00a6df8239978ee3c07961c2e6764d1861b6b6d1
ECHO <eunchan1001@gmail.com>
2023-02-10 18:29:22 +0900

    test(TrialCountTest): add test case for valid constructor

--- src/test/java/domain/TrialCountTest.java
+++ src/test/java/domain/TrialCountTest.java
@@ -6,13 +6,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.TrialCount;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class TrialCountTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    @DisplayName("생성자 정상 동작 테스트")
+    void validTrialCountTest(int value) {
+        assertThat(new TrialCount(value)).isInstanceOf(TrialCount.class);
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, -2, 0})
     @DisplayName("생성자 예외 발생 테스트")
     void invalidTrialCountTest(int value) {
-        Assertions.assertThatThrownBy(() -> {
+        assertThatThrownBy(() -> {
             new TrialCount(value);
         }).isInstanceOf(IllegalArgumentException.class);
     }
