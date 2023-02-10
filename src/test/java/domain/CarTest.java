0dc647e48a95c718a6565b2e5e01e9ef8eeb4e07
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-10 16:09:59 +0900

    refactor: move 메서드 Parameterized 테스트 적용

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,11 +1,11 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
@@ -17,32 +17,28 @@ class CarTest {
     }
 
     @DisplayName("0 ~ 3의 값일때는 움직이지 않는다.")
-    @Test
-    void moveTest1() {
+    @ParameterizedTest
+    @CsvSource({"0", "1", "2", "3"})
+    void moveTest1(int number) {
         //given
-        List<Integer> numbers = List.of(0, 1, 2, 3);
 
         //when
-        for (int number : numbers) {
-            car.move(number);
-        }
+        car.move(number);
 
         //then
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @DisplayName("4 ~ 9의 값일때는 움직인다.")
-    @Test
-    void moveTest2() {
+    @ParameterizedTest
+    @CsvSource({"4", "5", "6", "7", "8", "9"})
+    void moveTest2(int number) {
         //given
-        List<Integer> numbers = List.of(4, 5, 6, 7, 8, 9);
 
         //when
-        for (int number : numbers) {
-            car.move(number);
-        }
+        car.move(number);
 
         //then
-        assertThat(car.getPosition()).isEqualTo(6);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 }
