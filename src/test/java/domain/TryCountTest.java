fe4cf71388bf718b04c6f04c3566e52fd4de68aa
HubCreator <chocomilk8604@gmail.com>
2023-02-10 14:48:49 +0900

    refactor(TryCountTest): 실패하는 테스트 작성 및 ParameterizedTest 사용

--- src/test/java/domain/TryCountTest.java
+++ src/test/java/domain/TryCountTest.java
@@ -1,20 +1,29 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import error.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("시도 횟수는")
 class TryCountTest {
-    @DisplayName("시도 횟수는 1회 이상이여야 합니다.")
-    @Test
-    public void test1() {
-        Assertions.assertThatThrownBy(() -> new TryCount(0))
-                .isInstanceOf(IllegalArgumentException.class);
 
-        assertDoesNotThrow(() -> new TryCount(50));
-        assertDoesNotThrow(() -> new TryCount(1));
+    @DisplayName("0회 이하이면 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -100})
+    void test_fail(int value) {
+        assertThatThrownBy(() -> new TryCount(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_COUNT.getValue());
     }
 
+    @DisplayName("1회 이상이면 통과한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 100})
+    void test_success(int value) {
+        assertDoesNotThrow(() -> new TryCount(value));
+    }
 }
