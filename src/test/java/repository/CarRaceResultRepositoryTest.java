6423ea97366cbce5aab504e5e8a540c3b07b2d4a
somsom13 <somin1113@naver.com>
2023-02-11 00:14:45 +0900

    test(CarRaceResultRepositoryTest): 모든 자동차 이동 결과 테스트

--- src/test/java/repository/CarRaceResultRepositoryTest.java
+++ src/test/java/repository/CarRaceResultRepositoryTest.java
@@ -1,13 +1,17 @@
 package repository;
 
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import model.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarRaceResultRepositoryTest {
 
@@ -37,7 +41,7 @@ class CarRaceResultRepositoryTest {
     }
 
     @Test
-    @DisplayName("차 레이싱 결과 저장")
+    @DisplayName("새로운 차 저장")
     void save() {
         //given
         Car newCar = new Car("new");
@@ -46,7 +50,7 @@ class CarRaceResultRepositoryTest {
         carRaceResultRepository.save(newCar);
 
         //then
-        assertThat(carRaceResultRepository.findMoveCountByName(newCar.getName())).isEqualTo(1);
+        assertThat(carRaceResultRepository.findAllNameAndPosition().size()).isEqualTo(3);
     }
 
     @Test
@@ -73,25 +77,43 @@ class CarRaceResultRepositoryTest {
     }
 
     @Test
-    @DisplayName("모든 차 이름을 반환한다")
+    @DisplayName("모든 차 객체를 반환한다")
     void checkCarNameReturn() {
         //given
         //when
-        List<String> carNames = carRaceResultRepository.findAllCars();
+        List<Car> cars = carRaceResultRepository.findAll();
 
         //then
-        assertThat(carNames.size()).isEqualTo(2);
-        assertThat(carNames).containsAll(List.of(car1.getName(), car2.getName()));
+        assertThat(cars.size()).isEqualTo(2);
+        assertThat(cars.stream()
+            .map(Car::getName)
+            .collect(Collectors.toList()))
+            .containsOnly(car1.getName(), car2.getName());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:2", "3:1"}, delimiter = ':')
+    @DisplayName("멈춘 차 / 전진한 차의 업데이트를 테스트한다")
+    void checkCarRaceUpdate(int power, int movePosition) {
+        //given
+        //when
+        car1.move(power);
+
+        //then
+        assertThat(carRaceResultRepository.findMoveCountByName(car1.getName())).isEqualTo(
+            movePosition);
     }
 
     @Test
-    @DisplayName("차 이름에 따른 이동을 테스트한다")
-    void checkCarRaceByName() {
+    @DisplayName("모든 차의 이동 결과 반환을 테스트한다")
+    void checkAllCarNameAndPosition() {
         //given
         //when
-        carRaceResultRepository.moveByName(car1.getName());
+        car1.move(4);
+        car2.move(3);
+
         //then
-        assertThat(carRaceResultRepository.getRaceResult().get(car1.getName())).isEqualTo(2);
-        assertThat(carRaceResultRepository.getRaceResult().get(car2.getName())).isEqualTo(1);
+        assertThat(carRaceResultRepository.findAllNameAndPosition()).containsOnly(
+            entry(car1.getName(), 2), entry(car2.getName(), 1));
     }
 }
