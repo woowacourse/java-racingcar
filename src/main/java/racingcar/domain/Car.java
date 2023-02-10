8eb019c37ad8acff10c65ddae32a9c4feed9af7e
BGuga <tmdwns9913@gmail.com>
2023-02-10 20:32:37 +0900

    refactor(Car): delete unexpected inner class

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -37,32 +37,4 @@ public class Car implements Comparable<Car> {
                 .compareTo(o.position
                 );
     }
-
-    public static class CarManager {
-        private List<Car> cars;
-
-        public CarManager(List<Car> cars) {
-            this.cars = cars;
-        }
-
-        public void moveCarsRandomly() {
-            this.cars.forEach(car -> car.move(makeRandomCarMove()));
-        }
-
-        public List<Car> getCars() {
-            return this.cars;
-        }
-
-        public List<Car> getWinners() {
-            Car maxCar = Collections.max(cars);
-            return cars.stream()
-                    .filter(car -> car.compareTo(maxCar) == 0)
-                    .collect(Collectors.toList());
-        }
-
-        private CarMovement makeRandomCarMove() {
-            int moveCount = (int) (Math.random() * 10);
-            return new CarMovement(moveCount);
-        }
-    }
 }
