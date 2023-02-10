1b6025e6f80c89e5f212facbd64eb4e57d5d356b
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-11 07:52:29 +0900

    fix: 우승자 이름을 담는 자료구조 유형 변경 후 테스트에 해당 내용 반영

--- src/test/java/racingcar/domain/result/ResultTest.java
+++ src/test/java/racingcar/domain/result/ResultTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,22 +15,23 @@ class ResultTest {
 
     @ParameterizedTest
     @MethodSource("getResult")
-    void findWinners(List<Map<String, Integer>> sampleResult, List<String> expectedWinners) {
+    void findWinners(List<Map<String, Integer>> sampleResult, Set<String> expectedWinners) {
         Result result = new Result(sampleResult);
 
-        List<String> actualWinners = result.findWinners();
+        Set<String> actualWinners = result.findWinners();
 
         assertThat(actualWinners).isEqualTo(expectedWinners);
     }
+
 
     static Stream<Arguments> getResult() {
         return Stream.of(
                 Arguments.arguments(List.of(Map.of("poy", 5,
                         "joy", 3,
-                        "poby", 0)), List.of("poy")),
+                        "poby", 0)), Set.of("poy")),
                 Arguments.arguments(List.of(Map.of("poy", 5,
                         "joy", 5,
-                        "poby", 5)), List.of("poy", "joy", "poby"))
+                        "poby", 5)), Set.of("poy", "joy", "poby"))
         );
     }
 }
