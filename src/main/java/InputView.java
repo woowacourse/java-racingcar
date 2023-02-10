f9aeebf0ddb5d17a61d55058fb1123cad84549d4
ingpyo <juninpyo726@gmail.com>
2023-02-10 14:25:11 +0900

    refactor:입력 질문 문자열 InputView로 옮김

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -4,14 +4,18 @@ public class InputView {
     InputException inputException = new InputException();
     private static final Scanner scanner = new Scanner(System.in);
     private static final String DIVISION_CHAR = ",";
+    private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String START_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
     public String[] inputCarName() {
+        System.out.println(START_INPUT_CAR_NAME_MESSAGE);
         String[] inputCars = scanner.nextLine().split(DIVISION_CHAR);
         validateInputCarName(inputCars);
         return inputCars;
     }
 
     public int inputCount() {
+        System.out.println(COUNT_MESSAGE);
         return validateInputCount(scanner.nextInt());
     }
 
--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -5,8 +5,6 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "실행결과";
     private static final String CAR_NAME_FORMAT = " : ";
     private static final String MOVEMENT = "-";
-    private static final String COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String START_INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String END_MESSAGE = "가 최종 우승했습니다.";
     private static final String DIVISION_CHAR = ",";
 
@@ -22,13 +20,6 @@ public class OutputView {
         System.out.println(RESULT_MESSAGE);
     }
 
-    public void printStartMessage() {
-        System.out.println(START_INPUT_CAR_NAME_MESSAGE);
-    }
-
-    public void printCountMessage() {
-        System.out.println(COUNT_MESSAGE);
-    }
 
     public void printWinners(List<String> winners) {
         System.out.println(winnerFormat(winners) + END_MESSAGE);
