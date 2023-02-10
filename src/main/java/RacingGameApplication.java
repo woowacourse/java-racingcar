d7b6ecb1a97863fcbce3e1662bfc159fd98e7d8d
sosow0212 <sosow0212@naver.com>
2023-02-10 14:22:26 +0900

    refactor : (RacingGameEngine) 동사부터 시작 돼서 행위를 알 수 있게끔 메서드 리네이밍

--- src/main/java/RacingGameApplication.java
+++ src/main/java/RacingGameApplication.java
@@ -3,6 +3,6 @@ import engine.RacingGameEngine;
 public class RacingGameApplication {
     public static void main(String[] args) {
         RacingGameEngine racingGameEngine = new RacingGameEngine();
-        racingGameEngine.gameStart();
+        racingGameEngine.startGame();
     }
 }
