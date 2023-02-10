fc0b954afbb10fdd734863d91f16ac06f3da949b
hyunseo <ohseo8813@gmail.com>
2023-02-10 17:40:11 +0900

    <refactor>(domain/CarTest.java): 테스트 코드의 메소드명을 변경하고 계층형으로 수정

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,74 +1,88 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static domain.Car.MINIMUM_NUMBER_TO_MOVE;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
-    @DisplayName("Car 객체 생성 시 검증을 통과하면 정상적으로 생성한다.")
-    @Test
-    void test1() {
-        assertThatNoException().isThrownBy(() -> new Car("자동차"));
+    @DisplayName("Car 객체 생성시")
+    @Nested
+    class create_car {
+
+        @DisplayName("검증을 통과하면 정상적으로 생성한다.")
+        @Test
+        void success() {
+            assertThatNoException().isThrownBy(() -> new Car("자동차"));
+        }
+
+        @DisplayName("이름이 5자 초과이면 예외를 발생시킨다.")
+        @Test
+        void fail_by_name_length_exception() {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"));
+        }
+
+        @DisplayName("이름에 공백이 포함되면 예외를 발생시킨다.")
+        @Test
+        void fail_by_contain_blank() {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"));
+        }
     }
 
-    @DisplayName("Car 객체 생성 시 이름이 5자 초과이면 예외를 발생시킨다.")
-    @Test
-    void test2() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"));
+    @DisplayName("move 메소드는 ")
+    @Nested
+    class move {
+
+        @DisplayName("최솟값을 만족시키면 position을 1 증가 시킨다.")
+        @Test
+        void success() {
+            Car car = new Car("any");
+
+            int beforePosition = car.getPosition();
+            car.move(MINIMUM_NUMBER_TO_MOVE);
+            int afterPosition = car.getPosition();
+
+            assertThat(afterPosition).isEqualTo(beforePosition+1);
+        }
+
+        @DisplayName("최솟값을 만족시키지 못하면 position은 변화하지 않는다.")
+        @Test
+        void fail() {
+            Car car = new Car("any");
+
+            int beforePosition = car.getPosition();
+            car.move(MINIMUM_NUMBER_TO_MOVE - 1);
+            int afterPosition = car.getPosition();
+
+            assertThat(afterPosition).isEqualTo(beforePosition);
+        }
     }
 
-    @DisplayName("Car 객체 생성 시 이름에 공백이 포함되면 예외를 발생시킨다.")
-    @Test
-    void test3() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"));
-    }
+    @DisplayName("isSamePosition 메소드는 ")
+    @Nested
+    class isSamePosition {
 
-    @DisplayName("move를 위한 최솟값을 만족시키면 position을 1 증가 시킨다.")
-    @Test
-    void test4() {
-        Car car = new Car("any");
+        @DisplayName("position이 같다면 true를 반환한다.")
+        @Test
+        void return_true() {
+            Car carA = new Car("carA");
+            Car carB = new Car("carB");
 
-        int beforePosition = car.getPosition();
-        car.move(MINIMUM_NUMBER_TO_MOVE);
-        int afterPosition = car.getPosition();
+            assertThat(carA.isSamePosition(carB)).isTrue();
+        }
 
-        assertEquals(beforePosition + 1, afterPosition);
-    }
+        @DisplayName("position이 다르면 false를 반환한다.")
+        @Test
+        void return_false() {
+            Car carA = new Car("carA");
+            Car carB = new Car("carB");
 
-    @DisplayName("move를 위한 최솟값을 만족시키지 못하면 position은 변화하지 않는다.")
-    @Test
-    void test5() {
-        Car car = new Car("any");
+            carA.move(MINIMUM_NUMBER_TO_MOVE);
 
-        int beforePosition = car.getPosition();
-        car.move(MINIMUM_NUMBER_TO_MOVE - 1);
-        int afterPosition = car.getPosition();
-
-        assertEquals(beforePosition, afterPosition);
-    }
-
-    @DisplayName("isSamePosition()은 position이 같다면 true를 반환한다.")
-    @Test
-    void test6() {
-        Car carA = new Car("carA");
-        Car carB = new Car("carB");
-
-        assertTrue(carA.isSamePosition(carB));
-    }
-
-    @DisplayName("isSamePosition()은 position이 다르면 false를 반환한다.")
-    @Test
-    void test7() {
-        Car carA = new Car("carA");
-        Car carB = new Car("carB");
-
-        carA.move(MINIMUM_NUMBER_TO_MOVE);
-
-        assertFalse(carA.isSamePosition(carB));
+            assertThat(carA.isSamePosition(carB)).isFalse();
+        }
     }
 }
