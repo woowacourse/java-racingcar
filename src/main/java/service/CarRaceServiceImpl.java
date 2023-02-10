91656b229399a042995054db58e869fe451012dc
somsom13 <somin1113@naver.com>
2023-02-11 00:19:55 +0900

    refactor(CarRaceService): 자동차 이동 메소드 로직 변경

--- src/main/java/service/CarRaceServiceImpl.java
+++ src/main/java/service/CarRaceServiceImpl.java
@@ -12,7 +12,6 @@ import repository.CarRaceResultRepository;
 public class CarRaceServiceImpl implements CarRaceService {
 
     private static final int MAX_RANDOM_NUMBER = 10;
-    private static final int MOVE_LIMIT = 4;
     private static final int MAX_CAR_COUNT = 100;
     private final CarRaceResultRepository carRaceResultRepository;
     private final NumberGenerator numberGenerator;
@@ -27,19 +26,21 @@ public class CarRaceServiceImpl implements CarRaceService {
     public Map<String, Integer> saveCars(final List<String> names) {
         validateCarCount(names);
         names.forEach(name -> carRaceResultRepository.save(new Car(name)));
-        return carRaceResultRepository.getRaceResult();
+        return carRaceResultRepository.findAllNameAndPosition();
     }
 
     @Override
     public Map<String, Integer> move() {
-        List<String> cars = carRaceResultRepository.findAllCars();
-        cars.forEach(this::moveCar);
-        return carRaceResultRepository.getRaceResult();
+        for (Car car : carRaceResultRepository.findAll()) {
+            car.move(numberGenerator.generateNumber(MAX_RANDOM_NUMBER));
+            carRaceResultRepository.updatePosition(car);
+        }
+        return carRaceResultRepository.findAllNameAndPosition();
     }
 
     @Override
     public List<String> getResult() {
-        Map<String, Integer> resultBoard = carRaceResultRepository.getRaceResult();
+        Map<String, Integer> resultBoard = carRaceResultRepository.findAllNameAndPosition();
         Integer max = Collections.max(resultBoard.values());
         return resultBoard.entrySet()
             .stream()
@@ -48,16 +49,11 @@ public class CarRaceServiceImpl implements CarRaceService {
             .collect(Collectors.toList());
     }
 
-    private void moveCar(final String car) {
-        if (numberGenerator.generateNumber(MAX_RANDOM_NUMBER) >= MOVE_LIMIT) {
-            carRaceResultRepository.moveByName(car);
-        }
-    }
-
     private void validateCarCount(final List<String> names) {
         if (names.size() > MAX_CAR_COUNT) {
             throw new IllegalArgumentException(
                 String.format(ErrorCode.TOO_MANY_CAR.getMessage(), MAX_CAR_COUNT));
         }
     }
+
 }
