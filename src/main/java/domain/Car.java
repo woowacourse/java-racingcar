27ffc4dbb23f993a185ef77a07075569466ee046
hyunseo <ohseo8813@gmail.com>
2023-02-10 17:46:59 +0900

    <refactor>(domain/CarTest.java): 예외 메시지를 상수화하여 테스트에서 메시지도 검증

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,6 +5,8 @@ public class Car implements Comparable<Car> {
     public static final String BLANK = " ";
     public static final int MAX_NAME_LENGTH = 5;
     public static final int MINIMUM_NUMBER_TO_MOVE = 4;
+    public static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. ";
+    public static final String BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE = "이름에 공백을 포함할 수 없습니다.";
 
     private final String name;
     private int position = 0;
@@ -44,15 +46,13 @@ public class Car implements Comparable<Car> {
 
     private void validateLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. " +
-                    "입력값 : "  + name);
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE + "입력값 : "  + name);
         }
     }
 
     private void validateToContainBlank(String name) {
         if (name.contains(BLANK)) {
-            throw new IllegalArgumentException("이름에 공백을 포함할 수 없습니다." +
-                    "입력값 : " + name);
+            throw new IllegalArgumentException(BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE + "입력값 : " + name);
         }
     }
 }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static domain.Car.MINIMUM_NUMBER_TO_MOVE;
+import static domain.Car.*;
 import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
@@ -22,13 +22,15 @@ class CarTest {
         @DisplayName("이름이 5자 초과이면 예외를 발생시킨다.")
         @Test
         void fail_by_name_length_exception() {
-            assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"));
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"))
+                    .withMessageContaining(NAME_LENGTH_EXCEPTION_MESSAGE);
         }
 
         @DisplayName("이름에 공백이 포함되면 예외를 발생시킨다.")
         @Test
         void fail_by_contain_blank() {
-            assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"));
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"))
+                    .withMessageContaining(BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE);
         }
     }
 
