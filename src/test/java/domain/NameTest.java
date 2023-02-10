fd1c3d1a8e0f305829543b0d2bc2deb397cc6865
song-wooseok <sws981202@gmail.com>
2023-02-10 17:39:46 +0900

    refactor: 자동차 이름을 정상적으로 검증 후 객체를 생성하는 테스트 추가

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import exception.NameIsOutOfBoundException;
 import org.junit.jupiter.api.DisplayName;
@@ -12,10 +13,15 @@ public class NameTest {
     @ParameterizedTest
     @DisplayName("이름의 길이가 1보다 작거나 5보다 크면 Exception이 발생")
     @ValueSource(strings = {"", "abcdef"})
-    void nameLengthUnderOneOrOverFive(final String name) {
-        //when
-        //then
+    void incorrectNameLengthTest(final String name) {
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(NameIsOutOfBoundException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름의 길이가 1과 5사이 이면 정상적으로 Name 객체가 생성")
+    @ValueSource(strings = {"a", "abcde"})
+    void correctNameLengthTest(final String name) {
+        assertDoesNotThrow(() -> new Name(name));
     }
 }
