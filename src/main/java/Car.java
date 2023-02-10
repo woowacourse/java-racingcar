5ef48619bec480b557bcf765c3a1c780c48327be
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-02-10 14:28:40 +0900

    refactor : 자동차가 움직이는 책임을 Car에서 담당하도록 수정

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,12 +1,23 @@
 public class Car {
+    private static final int INIT_POSITION = 0;
+    private static final int MIN_MOVABLE_POWER = 4;
     private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
-        position = 0;
+        position = INIT_POSITION;
     }
 
+    public void move(int power) {
+        if (movable(power)) {
+            position = position + 1;
+        }
+    }
+
+    private boolean movable(int power) {
+        return power >= MIN_MOVABLE_POWER;
+    }
 
     public void setPosition(int position) {
         this.position = position;
