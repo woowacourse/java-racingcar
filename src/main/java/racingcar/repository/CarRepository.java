c2a3c2ed122762e305c5a4331b3a243c04f57707
seokjin8678 <rnrn8678@naver.com>
2023-02-10 14:27:54 +0900

    style: 코드 서식 정리

--- src/main/java/racingcar/repository/CarRepository.java
+++ src/main/java/racingcar/repository/CarRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class CarRepository {
-    private final List<Car> cars =  new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
 
     public void saveAll(List<Car> cars) {
         this.cars.addAll(cars);
     }
 
+    // List.copyOf와 Collections.unmodifiableList의 차이?
     public List<Car> findAll() {
         return List.copyOf(cars);
     }
