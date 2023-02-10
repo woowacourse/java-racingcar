0cfb898db2285dd289cf21e32556db353a44a3aa
seokho <kim76744dev@gmail.com>
2023-02-11 00:58:30 +0900

    test: NameTest 정상적으로 작동하는 코드 추가

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,11 +1,12 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import exception.NameIsOutOfBoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NameTest {
 
@@ -17,5 +18,15 @@ public class NameTest {
         //then
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(NameIsOutOfBoundException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름의 길이가 1이상 5이하 이면 정상동작한다.")
+    @ValueSource(strings = {"abcde", "abcd", "abc","ab", "a"})
+    void nameLengthOneToFive(final String name) {
+        //when
+        //then
+        assertThatCode(() -> new Name(name))
+                .doesNotThrowAnyException();
     }
 }
