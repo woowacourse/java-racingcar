899995c884d0e0b02cc7c9d5f5616760b95f5e12
watterricecake <fruturum@nate.com>
2023-02-10 13:52:24 +0900

    refactor(inputView): 인스턴스 분리

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -1,36 +1,37 @@
 package car.view;
 
-import static car.option.Option.MAX_TRIAL_COUNT_LENGTH;
-
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final Pattern NOT_INTEGER_PATTERN = Pattern.compile("\\D+");
-
+//    private static final Scanner scanner = new Scanner(System.in);
+//    private static final Pattern NOT_INTEGER_PATTERN = Pattern.compile("\\D+");
+    private final Input input;
+    public InputView(Input input){
+        this.input = input;
+    }
     public String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return scanner.nextLine().split(",");
+        return input.inputCarNames().split(",");
     }
 
     public int inputTrialCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        String input = scanner.nextLine();
-        validateInteger(input);
-        validateTrialCountUnderMaxInteger(input);
-        return Integer.parseInt(input);
+//        String input = scanner.nextLine();
+//        validateInteger(input);
+//        validateTrialCountUnderMaxInteger(input);
+        return Integer.parseInt(input.inputTrialCount());
     }
 
-    private void validateInteger(String input) {
-        if (NOT_INTEGER_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다");
-        }
-    }
-
-    private void validateTrialCountUnderMaxInteger(String input) {
-        if (input.length() >= MAX_TRIAL_COUNT_LENGTH) {
-            throw new IllegalArgumentException("시도 횟수는 999999999이하여야 합니다.");
-        }
-    }
+//    private void validateInteger(String input) {
+//        if (NOT_INTEGER_PATTERN.matcher(input).matches()) {
+//            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다");
+//        }
+//    }
+//
+//    private void validateTrialCountUnderMaxInteger(String input) {
+//        if (input.length() >= MAX_TRIAL_COUNT_LENGTH) {
+//            throw new IllegalArgumentException("시도 횟수는 999999999이하여야 합니다.");
+//        }
+//    }
 }
---
+++ src/test/java/car/view/testInputConsole.java
@@ -0,0 +1,13 @@
+package car.view;
+
+public class testInputConsole implements Input{
+    @Override
+    public String inputCarNames() {
+        return "땡칠,호이,달리,루쿠,저문,아마란스";
+    }
+
+    @Override
+    public String inputTrialCount() {
+        return "10";
+    }
+}
