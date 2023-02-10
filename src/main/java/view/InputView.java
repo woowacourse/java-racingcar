aaa40a5067a423399248947f0ddf7e42b5e6bc08
hyunseo <ohseo8813@gmail.com>
2023-02-10 23:49:22 +0900

    <refactor>(view/InputView.java): sendCarsName의 scanner.next를 nextLine으로 변경(공백이 무시되어 입력되던 문제가 발생)

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
         Scanner scanner = new Scanner(System.in);
-        return Arrays.stream(scanner.next().split(","))
+        return Arrays.stream(scanner.nextLine().split(","))
                 .map(CarNameDto::new)
                 .collect(Collectors.toList());
     }
