d6771207851ae220fdd771e5ebea881a4afb1fba
Eunkeee <eunkeeeee@gmail.com>
2023-02-10 14:16:13 +0900

    style(MainController): 상수 이름 변경

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import model.car.Car;
-import model.manager.CarMoveManager;
 import model.car.CarRepository;
-import model.number.RandomNumber;
+import model.manager.CarMoveManager;
+import util.number.RandomNumberGenerator;
 import view.InputView;
 import view.OutputView;
 
@@ -27,13 +27,13 @@ public class MainController {
 
     private Map<GameStatus, Supplier<GameStatus>> initializeGameGuide() {
         Map<GameStatus, Supplier<GameStatus>> gameGuide = new EnumMap<>(GameStatus.class);
-        gameGuide.put(GameStatus.SET_CARS, this::setCars);
-        gameGuide.put(GameStatus.MOVE_CARS, this::moveCars);
+        gameGuide.put(GameStatus.SET_GAME, this::setCars);
+        gameGuide.put(GameStatus.PLAY_GAME, this::moveCars);
         return gameGuide;
     }
 
     public void play() {
-        GameStatus gameStatus = progress(GameStatus.SET_CARS);
+        GameStatus gameStatus = progress(GameStatus.SET_GAME);
         while (gameStatus.isContinue(gameStatus)) {
             gameStatus = progress(gameStatus);
         }
@@ -45,7 +45,7 @@ public class MainController {
         } catch (IllegalArgumentException exception) {
             outputView.printExceptionMessage(exception);
             return GameStatus.APPLICATION_EXIT;
-        } catch (NullPointerException exception) {
+        } catch (Exception exception) {
             return GameStatus.APPLICATION_EXIT;
         }
     }
@@ -61,18 +61,18 @@ public class MainController {
     private GameStatus setCars() {
         List<String> carNames = inputView.readCarNames();
         carNames.stream().map(Car::new).forEach(CarRepository::addCars);
-        return GameStatus.MOVE_CARS;
+        return GameStatus.PLAY_GAME;
     }
 
     private void moveAllCars(int moveCount) {
         for (int i = 0; i < moveCount; i++) {
-            CarRepository.cars().forEach(car -> car.move(carMoveManager.isMove(RandomNumber.getRandomNumber())));
+            CarRepository.cars().forEach(car -> car.move(carMoveManager.isMove(RandomNumberGenerator.getRandomNumber())));
             outputView.printResult(CarRepository.cars());
         }
     }
 
     private enum GameStatus {
-        SET_CARS, MOVE_CARS, APPLICATION_EXIT;
+        SET_GAME, PLAY_GAME, APPLICATION_EXIT;
 
         GameStatus() {
         }
