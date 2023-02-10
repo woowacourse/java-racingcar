328a5113d3511aa57a6c00fa41db11af7c3e64e6
BGuga <tmdwns9913@gmail.com>
2023-02-10 19:37:06 +0900

    refactor(CarMovement): remove magic number

--- src/main/java/racingcar/domain/CarMovement.java
+++ src/main/java/racingcar/domain/CarMovement.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 public class CarMovement {
 
     private final static String OUT_OF_MOVEMENT_RANGE_EXCEPTION = "범위 밖의 움직임 값입니다.";
+    private final static int MAX_VALUE = 9;
+    public final static int MIN_VALUE = 0;
     private final int value;
 
     public CarMovement(int value) {
@@ -11,7 +13,7 @@ public class CarMovement {
     }
 
     private void validate(int value) {
-        if (value >= 10 || value < 0) {
+        if (value > MAX_VALUE || value < MIN_VALUE) {
             throw new IllegalArgumentException(OUT_OF_MOVEMENT_RANGE_EXCEPTION);
         }
     }
