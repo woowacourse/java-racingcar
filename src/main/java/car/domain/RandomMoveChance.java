8f89ec472ead32c14908cb2920d038cfaa3371d3
0chil <0@chll.it>
2023-02-10 18:33:14 +0900

    refactor(MoveChance): 상수화

--- src/main/java/car/domain/RandomMoveChance.java
+++ src/main/java/car/domain/RandomMoveChance.java
@@ -3,8 +3,10 @@ package car.domain;
 import java.util.Random;
 
 public class RandomMoveChance implements MoveChance {
-    
+
+    private static final Random RANDOM = new Random();
     private static final int MIN_MOVABLE_NUMBER = 4;
+    private static final int RANDOM_NUMBER_BOUNDARY = 10;
 
     @Override
     public boolean isMovable() {
@@ -12,7 +14,6 @@ public class RandomMoveChance implements MoveChance {
     }
 
     private int makeRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(10);
+        return RANDOM.nextInt(RANDOM_NUMBER_BOUNDARY);
     }
 }
