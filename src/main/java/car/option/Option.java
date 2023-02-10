d015b0b9b7861dc4e754e53ff739df5753ea5427
0chil <0@chll.it>
2023-02-10 14:46:13 +0900

    refactor(OutputView): 상수명 수정

--- src/main/java/car/option/Option.java
+++ src/main/java/car/option/Option.java
@@ -8,6 +8,6 @@ public class Option {
     public static final int MIN_MOVABLE_NUMBER = 4;
     public static final String CAR_INFIX = " : ";
     public static final String POSITION_CHARACTER = "-";
-    public static final String WINNER_DELIMITER = ", ";
+    public static final String CAR_DELIMITER = ", ";
     public static final String CAR_NAME_SEPARATOR = ",";
 }
