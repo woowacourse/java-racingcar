3cf66601b7413d544530f79235a98bf4b42d62e0
Wugawuga <dnr3655@naver.com>
2023-02-10 16:30:34 +0900

    test: 우승 자동차 위치 테스트

--- src/test/java/racingcar/RacingcarServiceTest.java
+++ src/test/java/racingcar/RacingcarServiceTest.java
@@ -12,6 +12,23 @@ import racingcar.service.RacingcarService;
 class RacingcarServiceTest {
 
     @Test
+    @DisplayName("우승 자동차 위치 확인하기")
+    void findWinnerPosition() {
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+
+        car2.move(5);
+        car3.move(5);
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+
+        int winnerPosition = findWinnerPosition(cars, new RacingcarService());
+
+        assertThat(winnerPosition).isEqualTo(1);
+    }
+
+    @Test
     @DisplayName("우승자 확인하기")
     void findWinner() {
         Car car1 = new Car("car1");
@@ -23,9 +40,13 @@ class RacingcarServiceTest {
 
         List<Car> cars = Arrays.asList(car1, car2, car3);
         RacingcarService racingcarService = new RacingcarService();
-        int winnerPosition = racingcarService.findPosition(cars);
+        int winnerPosition = findWinnerPosition(cars, racingcarService);
         List<Car> winners = racingcarService.findWinner(cars, winnerPosition);
 
         assertThat(winners).containsExactly(car2, car3);
+    }
+
+    private static int findWinnerPosition(List<Car> cars, RacingcarService racingcarService) {
+        return racingcarService.findPosition(cars);
     }
 }
