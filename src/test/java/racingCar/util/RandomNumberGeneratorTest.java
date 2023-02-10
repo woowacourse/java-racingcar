e1c53550d86beeb70ddc6e4a270eea835d534c42
hum02 <ehdgma02@gmail.com>
2023-02-10 18:33:13 +0900

    refactor: 잘못된 displayname설명 수정, repeatedtest로 RandomNumberGenerator테스트

--- src/test/java/racingCar/util/RandomNumberGeneratorTest.java
+++ src/test/java/racingCar/util/RandomNumberGeneratorTest.java
@@ -1,7 +1,7 @@
 package racingCar.util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,8 +9,8 @@ class RandomNumberGeneratorTest {
 
     private final RandomNumberGenerator randomNumberPicker = new RandomNumberGenerator();
 
-    @Test
-    @DisplayName("범위를 0부터 3으로 설정했을 때 범위 내의 값이 나오는지 테스트")
+    @RepeatedTest(20)
+    @DisplayName("범위를 0부터 9범위 내의 값이 나오는지 테스트")
     void pickOneNumberTest() {
         int pickOneNumber = randomNumberPicker.generateNumber();
         assertTrue(0 <= pickOneNumber && pickOneNumber <= 9);
