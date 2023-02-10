889a89156294ea2b839eb05e75b917669951df11
kokodak <kokodakadokok@gmail.com>
2023-02-10 15:13:29 +0900

    refactor: 변수명 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,12 +13,12 @@ public class InputView {
     private static final String READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String READ_ATTEMPT_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static List<String> readCarNames() throws IOException {
         try {
             System.out.println(READ_CAR_NAME_MESSAGE);
-            String input = br.readLine();
+            String input = bufferedReader.readLine();
             List<String> carNames = StringParser.splitByComma(input);
             validate(carNames);
             return carNames;
@@ -37,7 +37,7 @@ public class InputView {
     public static int readAttemptNumber() throws IOException {
         try {
             System.out.println(READ_ATTEMPT_NUMBER_MESSAGE);
-            String input = br.readLine();
+            String input = bufferedReader.readLine();
             return NumberValidator.validate(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
