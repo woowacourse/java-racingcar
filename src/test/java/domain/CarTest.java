6a5c1a48664578d850b9d08caac929f5c1927223
kokodak <kokodakadokok@gmail.com>
2023-02-10 15:54:51 +0900

    test: 테스트 메서드명 수정, 논리 수정, 접근제어자 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarTest {
 
-    Car car;
+    private Car car;
 
     @BeforeEach
     void init() {
@@ -17,32 +19,28 @@ class CarTest {
     }
 
     @DisplayName("0 ~ 3의 값일때는 움직이지 않는다.")
-    @Test
-    void moveTest1() {
+    @ParameterizedTest
+    @CsvSource({"0, 0", "1, 0", "2, 0", "3, 0"})
+    void should_Stay_When_NumberIsZeroToThree(int number, int position) {
         //given
-        List<Integer> numbers = List.of(0, 1, 2, 3);
 
         //when
-        for (int number : numbers) {
-            car.move(number);
-        }
+        car.move(number);
 
         //then
-        assertThat(car.getPosition()).isEqualTo(0);
+        assertThat(car.getPosition()).isEqualTo(position);
     }
 
     @DisplayName("4 ~ 9의 값일때는 움직인다.")
-    @Test
-    void moveTest2() {
+    @ParameterizedTest
+    @CsvSource({"4, 1", "5, 1", "6, 1", "7, 1", "8, 1", "9, 1"})
+    void should_Move_When_NumberIsFourToNine(int number, int position) {
         //given
-        List<Integer> numbers = List.of(4, 5, 6, 7, 8, 9);
 
         //when
-        for (int number : numbers) {
-            car.move(number);
-        }
+        car.move(number);
 
         //then
-        assertThat(car.getPosition()).isEqualTo(6);
+        assertThat(car.getPosition()).isEqualTo(position);
     }
 }
