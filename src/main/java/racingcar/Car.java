b2adb6823fa99c44577a947e374302d06934e9ab
kevstevie <jianna442@gmail.com>
2023-02-10 13:28:17 +0900

    refactor: Validator 삭제, Car 객체에서 메서드로 직접 유효성 검증

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,22 +1,36 @@
 package racingcar;
 
-import racingcar.util.Validator;
-
 public class Car {
+
 
     public static final int MOVE_RANGE = 1;
     private static final int INIT_POSITION = 0;
     private static final int BOUNDARY = 4;
+    public static final int MAX_LENGTH = 5;
 
     private final String name;
     private int position;
 
     public Car(String name) {
         String carName = name.trim();
-        Validator.validateCarNames(carName);
+        validateBlank(name);
+        validateLength(carName);
         this.name = carName;
         this.position = INIT_POSITION;
     }
+
+    private static void validateBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLength(String carName) {
+        if (carName.length() >= MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     public void move(int number) {
         if (number >= BOUNDARY) {
