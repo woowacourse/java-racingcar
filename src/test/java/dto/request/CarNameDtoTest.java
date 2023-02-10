8b0c93a036c6f7be95b4cdf51345e250055501c8
hyunseo <ohseo8813@gmail.com>
2023-02-10 18:26:54 +0900

    <refactor>(dto/request/CarNameDtoTest.java): 예외 메시지까지 포함하여 테스트 하도록 리팩토링

--- src/test/java/dto/request/CarNameDtoTest.java
+++ src/test/java/dto/request/CarNameDtoTest.java
@@ -3,26 +3,32 @@ package dto.request;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static domain.Car.BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE;
+import static domain.Car.NAME_LENGTH_EXCEPTION_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class CarNameDtoTest {
 
-    @DisplayName("Car의 이름이 검증을 통과하면 예외를 발생시키지 않는다.")
+    @DisplayName("CarNameDto의 이름이 검증을 통과하면 예외를 발생시키지 않는다.")
     @Test
-    void test1() {
+    void create_success() {
         assertThatNoException().isThrownBy(() -> new CarNameDto("자동차"));
     }
 
-    @DisplayName("Car의 이름이 5자 초과이면 예외를 발생시킨다.")
+    @DisplayName("CarNameDto의 이름이 5자 초과이면 예외를 발생시킨다.")
     @Test
-    void test2() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto("잘못된자동차이름"));
+    void fail_by_name_length_exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new CarNameDto("잘못된자동차이름"))
+                .withMessageContaining(NAME_LENGTH_EXCEPTION_MESSAGE);
     }
 
-    @DisplayName("Car의 이름에 공백이 포함되면 예외를 발생시킨다.")
+    @DisplayName("CarNameDto의 이름에 공백이 포함되면 예외를 발생시킨다.")
     @Test
-    void test3() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new CarNameDto(" bad"));
+    void fail_by_contain_blank_exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new CarNameDto(" bad"))
+                .withMessageContaining(BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE);
     }
 }
