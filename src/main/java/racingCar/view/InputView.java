dc0c32ce979ed163226268a6c6e0e64cbfddc331
kdkdhoho <hkim4410@naver.com>
2023-02-10 18:30:15 +0900

    refactor(InputView): 불필요한 함수 제거 및 함수명 변경

--- src/main/java/racingCar/view/InputView.java
+++ src/main/java/racingCar/view/InputView.java
@@ -3,7 +3,6 @@ package racingCar.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -12,22 +11,14 @@ public class InputView {
     public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.nextLine();
-        String[] carNames = input.split(",");
-        validateEmpty(carNames);
-        return Arrays.stream(carNames)
-                .collect(Collectors.toList());
+        String[] names = input.split(",");
+        validateNames(names);
+        return Arrays.asList(names);
     }
 
-    public static Integer readTryCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String input = scanner.nextLine();
-        validateEmpty(input);
-        return validateIsDigit(input);
-    }
-
-    private static void validateEmpty(String[] carNames) throws IllegalArgumentException {
-        if (isBlank(carNames)) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름을 다시 입력해주세요.");
+    private static void validateNames(String[] input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException("자동차는 1개 이상 입력해주세요.");
         }
     }
 
@@ -35,7 +26,14 @@ public class InputView {
         return carNames.length == 0;
     }
 
-    private static void validateEmpty(String tryCount) throws IllegalArgumentException {
+    public static int readTryCount() {
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String input = scanner.nextLine();
+        validateTryCount(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateTryCount(String tryCount) {
         if (isBlank(tryCount)) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수를 다시 입력해주세요.");
         }
@@ -43,9 +41,5 @@ public class InputView {
 
     private static boolean isBlank(String tryCount) {
         return tryCount.length() == 0;
-    }
-
-    private static Integer validateIsDigit(String input) throws IllegalArgumentException {
-        return Integer.parseInt(input);
     }
 }
