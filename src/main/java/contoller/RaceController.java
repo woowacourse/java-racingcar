161f1d0f439a242441dea4a8fd52685acf0035b6
This2sho <zx00018@naver.com>
2023-02-11 04:48:04 +0900

    refactor(RaceController): final 키워드 적용

--- src/main/java/contoller/RaceController.java
+++ src/main/java/contoller/RaceController.java
@@ -4,14 +4,22 @@ import domain.Car;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import util.RandomUtil;
+import util.RandomGenerator;
 import view.InputView;
 import view.OutputView;
 
 public class RaceController {
 
-    private InputView inputView = new InputView();
-    private OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        final List<Car> cars = makeCars();
+        final int roundNum = repeat(inputView::readTrialNum);
+
+        startRace(cars, roundNum);
+        pickWinner(cars);
+    }
 
     private <T> T repeat(Supplier<T> inputReader) {
         try {
@@ -22,16 +30,8 @@ public class RaceController {
         }
     }
 
-    public void run() {
-        List<Car> cars = makeCars();
-        int roundNum = repeat(inputView::readTrialNum);
-
-        startRace(cars, roundNum);
-        pickWinner(cars);
-    }
-
     private List<Car> makeCars() {
-        List<String> carNames = repeat(inputView::readCarNames);
+        final List<String> carNames = repeat(inputView::readCarNames);
         return carNames.stream()
             .map(Car::new)
             .collect(Collectors.toList());
@@ -46,13 +46,13 @@ public class RaceController {
 
     private void runRound(List<Car> cars) {
         for (Car car : cars) {
-            car.move(RandomUtil.getDigit());
+            car.move(RandomGenerator.getDigit());
         }
         outputView.printCarsStatus(cars);
     }
 
     private void pickWinner(List<Car> cars) {
-        int maxPosition = getMaxPosition(cars);
+        final int maxPosition = getMaxPosition(cars);
 
         List<String> winnerNames = cars.stream().sorted()
             .filter(c -> c.getPosition() == maxPosition)
