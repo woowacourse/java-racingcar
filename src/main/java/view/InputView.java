696df0de03ea00d75a99a664cc071d2210e6659b
song-wooseok <sws981202@gmail.com>
2023-02-10 16:57:59 +0900

    refactor: input() 메서드 호출시 새로운 Scanner 인스턴스를 생성하지 않도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
     private static final String requestCarNameMessage = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String requestTryCountMessage = "시도할 회수는 몇회인가요?";
 
-    private static Scanner scanner;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String requestCarName() {
         System.out.println(requestCarNameMessage);
@@ -20,9 +20,6 @@ public class InputView {
     }
 
     private static String input() {
-        if (scanner == null) {
-            scanner = new Scanner(System.in);
-        }
         return scanner.nextLine();
     }
 }
