5250fef07dec3ef85ea95b95684dfccc59945a5a
somsom13 <somin1113@naver.com>
2023-02-10 17:20:59 +0900

    test(CarTest): 차 이동, 정차 테스트

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,9 +1,11 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
@@ -26,5 +28,19 @@ class CarTest {
         //when
         new Car(name);
         //then
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:2", "9:2", "0:1", "3:1"}, delimiter = ':')
+    @DisplayName("차가 가지는 힘에 따른 이동, 정차를 테스트한다")
+    void checkCarPositionByPower(int power, int position) {
+        //given
+        Car car1 = new Car("car1");
+
+        //when
+        car1.move(power);
+
+        //then
+        assertThat(car1.getCurrentPosition()).isEqualTo(position);
     }
 }
