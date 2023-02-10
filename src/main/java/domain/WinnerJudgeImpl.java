532bc4d1221b7a827a54b29385b7fcf81a02e6e2
Kayoung Yoon <dev.kayoung@gmail.com>
2023-02-10 16:40:01 +0900

    refactor: 우승자를 판단하는 기능 구현

---
+++ src/main/java/domain/WinnerJudgeImpl.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinnerJudgeImpl implements WinnerJudge{
+    @Override
+    public List<Car> getWinner(List<Car> cars) {
+        int winPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        return cars.stream()
+                .filter(car -> car.getPosition() == winPosition)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/domain/WinnerJudgeTest.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinnerJudgeTest {
+    WinnerJudgeImpl winnerJudge = new WinnerJudgeImpl();
+    List<Car> cars;
+    List<Car> winners;
+
+    @BeforeEach
+    void setup() {
+        Car winner = new Car("dani");
+        // TODO: 사용하는 입장에서 앞으로 움직이는 조건을 확인하기 어려움. Car 개선하기
+        winner.moveDependingOn(9);
+
+        Car notWinner = new Car("hihi");
+        notWinner.moveDependingOn(0);
+
+        cars = List.of(winner, notWinner);
+    }
+
+    @Test
+    @DisplayName("반환값에 우승자가 포함되어 있는지 테스트")
+    void shouldContainsWinners() {
+        winners = winnerJudge.getWinner(cars);
+        Assertions.assertThat(winners)
+                .containsExactly(new Car("dani"));
+    }
+
+    @Test
+    @DisplayName("반환값에 우승자가 아닌 사람이 포함되어 있지 않은지 테스트")
+    void shouldNotContainNonWinners() {
+        winners = winnerJudge.getWinner(cars);
+        Assertions.assertThat(winners)
+                .doesNotContain(new Car("hihi"));
+    }
+}
