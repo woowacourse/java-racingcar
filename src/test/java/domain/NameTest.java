6bb8167f7d601a6e1f796955e335625ffd58001b
HubCreator <chocomilk8604@gmail.com>
2023-02-10 14:53:18 +0900

    refactor(NameTest): 실패하는 테스트 케이스 분리 및 ParameterizedTest 적용

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,24 +1,28 @@
 package domain;
 
+import error.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class NameTest {
 
-    @DisplayName("자동차 이름은 1자 이상, 5자 이하여야 한다.")
-    @Test
-    void test() {
-        assertDoesNotThrow(() -> new Name("쥬"));
-        assertDoesNotThrow(() -> new Name("쥬니"));
-        assertDoesNotThrow(() -> new Name("쥬니헙크1"));
+    @DisplayName("자동차 이름은 1자 이상, 5자 이하이면 통과한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "123", "12345"})
+    void test1(String value) {
+        assertDoesNotThrow(() -> new Name(value));
+    }
 
-
-        assertThatThrownBy(() -> new Name(""))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Name("123456"))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("자동차 이름은 0자, 혹은 5자 초과이면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    void test2(String value) {
+        assertThatThrownBy(() -> new Name(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_NAME_LENGTH.getValue());
     }
 }
