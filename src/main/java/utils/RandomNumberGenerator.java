84f6393b84334c713043b28b2f35f1bcf8ee9834
hyunseo <ohseo8813@gmail.com>
2023-02-10 18:28:07 +0900

    <refactor>(utils/RandomNumberGenerator.java): 멤버변수 Random을 final로 변경

--- src/main/java/utils/RandomNumberGenerator.java
+++ src/main/java/utils/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator{
 
-    private Random random = new Random();
+    private final Random random = new Random();
 
     @Override
     public int generate() {
