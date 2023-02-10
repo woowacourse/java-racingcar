972b9e91058a25605d2ffd9c05062f67a59abfbb
cpot5620 <=cpot5620@naver.com>
2023-02-10 14:26:48 +0900

    test: @ParameterizedTest 적용 및 성공/실패 케이스 분리

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,24 +1,27 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("자동차 이름은")
 class NameTest {
 
-    @DisplayName("자동차 이름은 1자 이상, 5자 이하여야 한다.")
-    @Test
-    void test() {
-        assertDoesNotThrow(() -> new Name("쥬"));
-        assertDoesNotThrow(() -> new Name("쥬니"));
-        assertDoesNotThrow(() -> new Name("쥬니헙크1"));
+    @DisplayName("1자 이상, 5자 이하로 생성할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"쥬", "쥬니", "쥬니헙크1"})
+    void test1(String name) {
+        assertDoesNotThrow(() -> new Name(name));
+    }
 
-
-        assertThatThrownBy(() -> new Name(""))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Name("123456"))
+    @DisplayName("1자 이상, 5자 이하가 아닌 경우, 생성할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    void test2(String name) {
+        assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/domain/TryCountTest.java
+++ src/test/java/domain/TryCountTest.java
@@ -3,18 +3,27 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("시도 횟수가")
 class TryCountTest {
-    @DisplayName("시도 횟수는 1회 이상이여야 합니다.")
-    @Test
-    public void test1() {
-        Assertions.assertThatThrownBy(() -> new TryCount(0))
-                .isInstanceOf(IllegalArgumentException.class);
 
-        assertDoesNotThrow(() -> new TryCount(50));
-        assertDoesNotThrow(() -> new TryCount(1));
+    @DisplayName("1회 이상인 경우 정상 동작한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,10,100})
+    public void test1(int count) {
+        assertDoesNotThrow(() -> new TryCount(count));
+    }
+
+    @DisplayName("1회 미만인 경우 오류가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-100,-30,0})
+    public void test2(int count) {
+        Assertions.assertThatThrownBy(() -> new TryCount(count))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
