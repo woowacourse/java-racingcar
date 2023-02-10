f689406c3a46d9ade585248c9fab07facfc0a7de
splitube <voicesplit@daum.net>
2023-02-11 00:42:27 +0900

    test: Car 객체 생성 테스트 구현

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,43 +1,72 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.fail;
+
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import validation.ErrorMessages;
 
 class CarTest {
 
-    private Car car;
-
-    @BeforeEach
-    void before() {
-        car = new Car("test", 0);
+    @DisplayName("자동차 이름이 null일 경우 오류를 던진다.")
+    @Test
+    void create_fail_name_null() {
+        //given
+        //when then
+        assertThatThrownBy(() -> {
+            Car car = new Car(null, 0);
+        }).isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ErrorMessages.INVALID_CAR_NAME_LENGTH.getMessage());
     }
 
+    @DisplayName("자동차 이름이 빈문자열일 경우 오류를 던진다.")
+    @Test
+    void create_fail_name_empty() {
+        //given
+        //when then
+        assertThatThrownBy(() -> {
+            Car car = new Car("", 0);
+        }).isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ErrorMessages.INVALID_CAR_NAME_LENGTH.getMessage());
+    }
+
+    @DisplayName("자동차 이름이 5자 이상일 경우 오류를 던진다.")
+    @Test
+    void create_fail_name_too_long() {
+        //given
+        //when then
+        assertThatThrownBy(() -> {
+            Car car = new Car("123456", 0);
+        }).isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ErrorMessages.INVALID_CAR_NAME_LENGTH.getMessage());
+    }
+
+    @DisplayName("자동차 이름이 적절한 경우")
+    @Test
+    void create_success() {
+        try {
+            Car car = new Car("valid", 0);
+        } catch (Exception e) {
+            fail("정상적으로 자동차를 생성해야 합니다.");
+        }
+    }
+
+    @DisplayName("자동차를 전진 시킨다.")
     @ValueSource(ints = {1, 2, 3, 4, 5})
     @ParameterizedTest
     void 차량_전진_테스트(int driveCount) {
         //given
         final int DRIVING_DISTANCE = 1;
+        Car car = new Car("valid", 0);
         //when
-        for (int count = 0; count < driveCount; count++) {
-            car.drive(DRIVING_DISTANCE);
-        }
+        IntStream.range(0, driveCount).map(count -> DRIVING_DISTANCE).forEach(car::drive);
         //then
         int drivenDistance = car.getDrivenDistance();
-        org.assertj.core.api.Assertions.assertThat(drivenDistance).isEqualTo(driveCount);
-    }
-
-    @Test
-    void 차량_이름_toString_테스트() {
-        //given
-        String name = "test";
-        //when
-        Car car1 = new Car(name, 0);
-        Car car2 = new Car(name, 1);
-        //then
-        Assertions.assertThat(car1.toString()).isEqualTo("test");
-        Assertions.assertThat(car2.toString()).isEqualTo("test-1");
+        assertThat(drivenDistance).isEqualTo(driveCount);
     }
 }
