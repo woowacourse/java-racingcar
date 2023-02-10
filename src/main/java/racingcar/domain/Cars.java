295f6b7b3630346fcca97ee78b191e83bdf1d05f
Devtist <yt930310@gmail.com>
2023-02-10 19:14:31 +0900

    refactor: Cars의 필드 컬렉션 final로 선언

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 public class Cars {
 
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
 
     private final int FIRST_INDEX = 0;
 
