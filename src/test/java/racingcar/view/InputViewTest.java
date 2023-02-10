1a4e6b225b49a4411d1151069deae14802a4e041
sh111-coder <ohk9134@naver.com>
2023-02-10 20:22:20 +0900

    test(InputViewValidatorTest) : 입력 검증 기능 테스트 InputViewValidatorTest로 이동

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
@@ -25,92 +23,5 @@ class InputViewTest {
         System.setIn(in);
 
         Assertions.assertThat(inputView.readCarNames()).containsExactly("성하", "이오", "제이");
-    }
-
-    @Test
-    @DisplayName("자동차 이름이 빈 값인 경우 예외 처리")
-    void carNamesBlankTest() {
-        String carNames = "\n";
-        InputStream in = generateUserInput(carNames);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"이오,이오,성하\n", "이오,성하,성하\n"})
-    @DisplayName("구분한 자동차 이름이 중복된 경우")
-    void carNamesDuplicateTest(String carNames) {
-        InputStream in = generateUserInput(carNames);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"이오이오이오, 성하\n", "재이재이재이,이오이오이오이\n"})
-    @DisplayName("구분한 자동차 이름이 5글자 초과인 경우")
-    void eachCarNameLengthTest(String carNames) {
-        InputStream in = generateUserInput(carNames);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"이오이오,,\n", ",,성하\n"})
-    @DisplayName("구분한 자동차 이름이 빈 값인 경우")
-    void eachCarNameBlankTest(String carNames) {
-        InputStream in = generateUserInput(carNames);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("시도할 횟수 입력 테스트")
-    void readTryNumTest() {
-        String tryNum = "5";
-        InputStream in = generateUserInput(tryNum);
-        System.setIn(in);
-
-        Assertions.assertThat(inputView.readTryNum()).isEqualTo(5);
-    }
-
-    @Test
-    @DisplayName("시도할 횟수가 빈 값인 경우 예외 테스트")
-    void readTryNumBlankTest() {
-        String tryNum = "\n";
-        InputStream in = generateUserInput(tryNum);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readTryNum())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"0.5\n", "-0.5\n"})
-    @DisplayName("시도할 횟수가 정수가 아닌 경우 예외 테스트")
-    void readTryNumIntegerTest(String tryNum) {
-        InputStream in = generateUserInput(tryNum);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readTryNum())
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"-1\n", "0\n"})
-    @DisplayName("시도할 횟수가 0 이하인 경우 예외 테스트")
-    void readTryNumNotPositiveTest(String tryNum) {
-        InputStream in = generateUserInput(tryNum);
-        System.setIn(in);
-
-        Assertions.assertThatThrownBy(() -> inputView.readTryNum())
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
---
+++ src/test/java/racingcar/view/InputViewValidatorTest.java
@@ -0,0 +1,91 @@
+package racingcar.view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewValidatorTest {
+
+    private InputViewValidator inputViewValidator = InputViewValidator.getInstance();
+
+    @Nested
+    @DisplayName("전체 자동차 이름 검증 테스트")
+    class carNamesTest {
+
+        @Test
+        @DisplayName("전체 입력받은 자동차 이름이 빈 값인 경우 예외 처리")
+        void carNamesBlankTest() {
+            String carNames = "";
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateCarNames(carNames))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("분리한 자동차 이름 검증 테스트")
+    class splitCarNameTest {
+
+        @Test
+        @DisplayName("분리한 자동차 이름이 중복된 경우")
+        void splitCarNamesDuplicateTest() {
+            String[] carNames = new String[]{"성하", "성하", "이오"};
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateSplitCarNames(carNames))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"이오이오이오", "재이재이재이재이재이"})
+        @DisplayName("분리한 자동차 이름이 5글자 초과인 경우")
+        void splitCarNameLengthTest(String splitCarName) {
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateSplitCarName(splitCarName))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("분리한 자동차 이름이 빈 값인 경우")
+        void splitCarNameBlankTest() {
+            String splitCarName = "";
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateSplitCarName(splitCarName))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("시도할 횟수 검증 테스트")
+    class tryNumTest {
+
+        @Test
+        @DisplayName("시도할 횟수가 빈 값인 경우 예외 처리")
+        void tryNumBlankTest() {
+            String tryNum = "";
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateTryNum(tryNum))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"0.5", "-0.5"})
+        @DisplayName("시도할 횟수가 정수가 아닌 경우 예외 처리")
+        void tryNumIntegerTest(String tryNum) {
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateTryNum(tryNum))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"-1", "0"})
+        @DisplayName("시도할 횟수가 0 이하인 경우 예외 테스트")
+        void readTryNumNotPositiveTest(String tryNum) {
+
+            Assertions.assertThatThrownBy(() -> inputViewValidator.validateTryNum(tryNum))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
