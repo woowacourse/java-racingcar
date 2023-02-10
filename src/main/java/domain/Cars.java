fdbc783f9b55bde3334f22973febc6ce47b88e80
Youngho <cyh6099@gmail.com>
2023-02-10 17:14:59 +0900

    refactor : 클래스 네이밍 수정, .연산자 개행, cars복사 후 반환하도록 수정

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -3,17 +3,13 @@ package domain;
 import util.RandomNumberGenerator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//TODO : 클래스 네이밍 수정
-//TODO : .연산자 개행
-//TODO : getCarsInfo() 반환 코드 불변으로 수정
-public class CarsInfo {
-    private final List<Car> carsInfo = new ArrayList<>();
+public class Cars {
+    private final List<Car> cars = new ArrayList<>();
 
-    public CarsInfo(List<String> carNames) {
+    public Cars(List<String> carNames) {
         checkNameDuplication(carNames);
         addCar(carNames);
     }
@@ -29,34 +25,34 @@ public class CarsInfo {
 
     private void addCar(List<String> carNames) {
         for (String carName : carNames) {
-            carsInfo.add(new Car(carName));
+            cars.add(new Car(carName));
         }
     }
 
     public List<String> findWinners() {
         Integer maxDistance = findMaxDistance();
-        return carsInfo.stream()
+        return cars.stream()
                 .filter(car -> car.getDistance() == maxDistance)
                 .map(Car::getCarName)
                 .collect(Collectors.toList());
     }
 
     private Integer findMaxDistance() {
-        return carsInfo.stream()
+        return cars.stream()
                 .mapToInt(Car::getDistance)
-                .max().
-                orElseThrow(NoSuchFieldError::new);
+                .max()
+                .orElse(0);
     }
 
     public void progressRound(RandomNumberGenerator randomNumberGenerator, RoundResult roundResult) {
-        for (Car car : carsInfo) {
+        for (Car car : cars) {
             Integer randomNumber = randomNumberGenerator.generateRandomNumber();
             boolean movingResult = roundResult.isGo(randomNumber);
             car.move(movingResult);
         }
     }
 
-    public List<Car> getCarsInfo() {
-        return Collections.unmodifiableList(carsInfo);
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
     }
 }
