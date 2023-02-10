4fdd66b97d76b00d55006691f9f9776f88fdb537
yujamint <woawoa0513@gmail.com>
2023-02-10 17:46:15 +0900

    refactor: static 키워드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,16 +7,16 @@ import java.util.List;
 import java.util.StringJoiner;
 
 public class OutputView {
-    private final char newLine = '\n';
-    private final String INPUT_CAR_NAME_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private final String INPUT_TRY_COUNT_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final char newLine = '\n';
+    private static final String INPUT_CAR_NAME_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_TRY_COUNT_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
 
-    private final String CAR_INFO_FORMAT_DELIMITER = " : ";
-    private final char CAR_POSITION_MESSAGE = '-';
+    private static final String CAR_INFO_FORMAT_DELIMITER = " : ";
+    private static final char CAR_POSITION_MESSAGE = '-';
 
-    private final String RESULT_GUIDE_MESSAGE = "실행결과";
-    private final String RESULT_FORMAT_DELIMITER = ", ";
-    private final String RESULT_WINNER_GUIDE_MESSAGE = "가 최종 우승했습니다.";
+    private static final String RESULT_GUIDE_MESSAGE = "실행결과";
+    private static final String RESULT_FORMAT_DELIMITER = ", ";
+    private static final String RESULT_WINNER_GUIDE_MESSAGE = "가 최종 우승했습니다.";
 
     public void printCarNameMessage() {
         System.out.println(INPUT_CAR_NAME_GUIDE_MESSAGE);
