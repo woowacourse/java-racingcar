c5d0ae5c04761bab4253cd6367fc66e382fd67f4
dooboocookie <bkh3030@gmail.com>
2023-02-10 18:30:09 +0900

    test: 숫자 생성기 추가한 것을 토대로 전진 정지 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -13,6 +13,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class CarTest {
 
@@ -43,7 +44,27 @@ class CarTest {
 	@Test
 	void moveTest() {
 		Car car = new Car("이름입니다");
-		Assertions.assertDoesNotThrow(car::move);
+		assertDoesNotThrow(car::move);
+	}
+
+	@DisplayName("자동차 전진 테스트")
+	@ParameterizedTest(name = "generatedNumber = {0}")
+	@ValueSource(ints = {4,5,6,7,8,9})
+	void moveForwardTest(int generatedNumber) {
+		Car car = new Car("이름입니다", new CarMovement(() -> generatedNumber));
+		int initPosition = car.getPosition();
+		car.move();
+		assertThat(car.getPosition()).isEqualTo(initPosition + 1);
+	}
+
+	@DisplayName("자동차 정지 테스트")
+	@ParameterizedTest(name = "generatedNumber = {0}")
+	@ValueSource(ints = {1,2,3})
+	void moveStopTest(int generatedNumber) {
+		Car car = new Car("이름입니다", new CarMovement(() -> generatedNumber));
+		int initPosition = car.getPosition();
+		car.move();
+		assertThat(car.getPosition()).isEqualTo(initPosition);
 	}
 
 	static Stream<Arguments> carNameDummy() {
