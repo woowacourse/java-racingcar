b4e4676086983318c26cdfbb6471acd17f652f29
Devtist <yt930310@gmail.com>
2023-02-10 19:27:33 +0900

    test: 정상적인 입력에 대한 테스트 코드 작성

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,10 +1,11 @@
 package racingcar.util;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
     @ParameterizedTest(name = "입력이 공백이면 예외를 발생시킨다.")
@@ -13,6 +14,11 @@ class ValidatorTest {
         assertThatThrownBy(() -> Validator.validateNotEmptyInput(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest(name = "입력이 자연수가 아니면 예외를 발생시킨다. - 입력값 : {0}")
+    @ValueSource(strings = {"2", "1000"})
+    void validateNaturalNumberNormalCaseTest(String input) {
+        assertDoesNotThrow(() -> Validator.validateNaturalNumber(input));
+    }
 
     @ParameterizedTest(name = "입력이 null이거나 공백이면 예외를 발생시킨다.")
     @NullAndEmptySource
