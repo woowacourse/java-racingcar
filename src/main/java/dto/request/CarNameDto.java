61c5fc79a93f14a7d00b1363914cbff69de25acb
hyunseo <ohseo8813@gmail.com>
2023-02-10 13:52:41 +0900

    <style>(dto/reqeust/CarNameDto.java): code convention에 맞게 메소드 순서 변경

--- src/main/java/dto/request/CarNameDto.java
+++ src/main/java/dto/request/CarNameDto.java
@@ -21,17 +21,17 @@ public class CarNameDto {
         validateLength(name);
     }
 
-    private void validateLength(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. " +
-                    "입력값 : "  + name);
-        }
-    }
-
     private void validateToContainBlank(String name) {
         if (name.contains(BLANK)) {
             throw new IllegalArgumentException("이름에 공백을 포함할 수 없습니다." +
                     "입력값 : " + name);
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. " +
+                    "입력값 : "  + name);
         }
     }
 }
