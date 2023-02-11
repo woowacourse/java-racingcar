a119e88142ac4b7a13543f3de0105d01b7b3a4b6
seokjin8678 <rnrn8678@naver.com>
2023-02-11 23:33:23 +0900

    test: TryCount 테스트 코드 추가

---
+++ src/test/java/racingcar/domain/TryCountTest.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TryCountTest {
+
+    @Test
+    @DisplayName("시도 횟수가 다되면 거짓이 반환되어야 한다.")
+    void tryCount_countdownCompleteReturnFalse() {
+        // given
+        TryCount tryCount = new TryCount(5);
+
+        // when
+        while (tryCount.countdown()){
+            // do something
+        }
+
+        // then
+        assertThat(tryCount.countdown())
+                .isFalse();
+    }
+
+    @Test
+    @DisplayName("시도 횟구가 남으면 참이 반환되어야 한다.")
+    void tryCount_countdownNotCompleteReturnTrue() {
+        // given
+        TryCount tryCount = new TryCount(5);
+
+        // when
+        tryCount.countdown();
+
+        // then
+        assertThat(tryCount.countdown())
+                .isTrue();
+    }
+}
