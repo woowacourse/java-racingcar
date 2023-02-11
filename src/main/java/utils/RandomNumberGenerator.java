336a84cf4af1200e8f5d008d7803e8bc6c242db1
yenawee <yaena0319@naver.com>
2023-02-11 14:55:39 +0900

    refactor : random 객체 final 키워드 추가

--- src/main/java/utils/RandomNumberGenerator.java
+++ src/main/java/utils/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
     static final int UPPER_BOUND = 10;
-    static Random rand = new Random();
+    static final Random rand = new Random();
 
     public static int randomNumberGenerate() {
         return rand.nextInt(UPPER_BOUND);
