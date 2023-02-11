baee5fb66ad21ffa9ac1960deb4d70ffc748bea5
iamjooon2 <iamjooon2@gmail.com>
2023-02-11 18:21:24 +0900

    fix: 사용하지 않는 함수 제거

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,11 +1,9 @@
 package racingcar.util;
 
-import java.util.Arrays;
 import racingcar.view.ErrorMessage;
 
 public class Validator {
-    private static final String DELIMITER = ",";
-    private static final String NO_INPUT = "";
+
     private static final int MIN_GAME_TIME = 1;
     private static final int CAR_NAME_MAX_LENGTH = 5;
 
@@ -18,21 +16,6 @@ public class Validator {
         }
         if (carName.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_PREFIX + ErrorMessage.NO_INPUT);
-        }
-    }
-
-    public static String[] validateCarNames(String carNamesString) {
-        String[] carNames = carNamesString.split(DELIMITER);
-        validateCarNameLengthByArray(carNames);
-        validateCarNameNoInputByArray(carNames);
-        return carNames;
-    }
-
-    public static void validateCarNameLengthByArray(String[] split) {
-        long count = Arrays.stream(split).filter(s -> s.length() >= CAR_NAME_MAX_LENGTH)
-                .count();
-        if (count >= 1) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_PREFIX + ErrorMessage.CAR_NAME_LENGTH);
         }
     }
 
@@ -56,13 +39,6 @@ public class Validator {
             throw new IllegalArgumentException(ErrorMessage.ERROR_PREFIX + ErrorMessage.NOT_NUMBER);
         }
         return integer;
-    }
-
-    public static void validateCarNameNoInputByArray(String[] split) {
-        long noInputCount = Arrays.stream(split).filter(s -> s.trim().equals(NO_INPUT)).count();
-        if (noInputCount >= 1) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_PREFIX + ErrorMessage.NO_INPUT);
-        }
     }
 
 }
