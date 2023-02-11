53e6570da8bbfe479a45d747af9154f42e2b8345
hong-sile <gurwns9325@naver.com>
2023-02-11 21:29:48 +0900

    refactor: 요구사항에 맞게 repeatUntilReadValidInput 메서드의 depth를 1로 변경

--- docs/README.md
+++ docs/README.md
@@ -37,7 +37,7 @@
 6. 최종 우승자를 출력하기(OutputView)
 
 # 리팩토링 시 해야될 일들
-- [ ] ExceptionHandlingTemplate의 repeat...메소드의 depth를 1로 줄이기
+- [x] ExceptionHandlingTemplate의 repeat...메소드의 depth를 1로 줄이기
 - [ ] controller에서 tryTime을 기준으로 while을 돌리는데 이를 service레이어에서 실행하게 변경
 - [ ] List<Car>를 감싸는 일급컬렉션인 Cars를 구현하기
 - [x] RandomNumberPicker에서 Random을 필드변수로 두기.
--- src/main/java/domain/ExceptionHandlingTemplate.java
+++ src/main/java/domain/ExceptionHandlingTemplate.java
@@ -8,12 +8,11 @@ public interface ExceptionHandlingTemplate {
     OutputView outputView = new OutputView();
 
     static <T, R> R repeatUntilReadValidInput(Function<T, R> object, Supplier<T> input) {
-        while (true) {
-            try {
-                return object.apply(input.get());
-            } catch (IllegalArgumentException e) {
-                outputView.printErrorMessage(e.getMessage());
-            }
+        try {
+            return object.apply(input.get());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return repeatUntilReadValidInput(object, input);
         }
     }
 }
