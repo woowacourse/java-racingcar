8aadd665ea73fb946188b24c8f836ecac3f32d71
hyunseo <ohseo8813@gmail.com>
2023-02-11 12:23:02 +0900

    <refactor>(domain/CarsTest.java): Car의 position의 기본값을 상수화하여 테스트

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,13 +3,14 @@ package domain;
 public class Car implements Comparable<Car> {
 
     public static final String BLANK = " ";
+    public static final int DEFAULT_POSITION_VALUE = 0;
     public static final int MAX_NAME_LENGTH = 5;
     public static final int MINIMUM_NUMBER_TO_MOVE = 4;
     public static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다. ";
     public static final String BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE = "이름에 공백을 포함할 수 없습니다.";
 
     private final String name;
-    private int position = 0;
+    private int position = DEFAULT_POSITION_VALUE;
 
     public Car(String name) {
         validate(name);
