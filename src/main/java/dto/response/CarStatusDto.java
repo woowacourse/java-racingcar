6e603af946a250173c5d99cddd5d79ada6187ddc
kwonyj1022 <kwonyj1022@naver.com>
2023-02-11 19:28:09 +0900

    refactor: 메서드가 생성자 다음에 오도록 순서 변경

--- src/main/java/dto/response/CarStatusDto.java
+++ src/main/java/dto/response/CarStatusDto.java
@@ -7,13 +7,13 @@ public class CarStatusDto {
     private final String name;
     private final int position;
 
-    public static CarStatusDto of(Car car) {
-        return new CarStatusDto(car.getName(), car.getPosition());
-    }
-
     private CarStatusDto(String name, int position) {
         this.name = name;
         this.position = position;
+    }
+
+    public static CarStatusDto of(Car car) {
+        return new CarStatusDto(car.getName(), car.getPosition());
     }
 
     public String getName() {
