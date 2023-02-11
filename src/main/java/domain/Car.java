417b6457fe1d6811c68f53c0ad061d95c102723a
kiarakim <kiarakim413@gmail.com>
2023-02-11 19:01:01 +0900

    refactor : static과 final 제어자 순서 변경, 매직넘버 제거, 캐스팅 오류 해결

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,47 +1,55 @@
 package domain;
 
+import java.util.Objects;
+
 public class Car implements Comparable<Car> {
-    private final static String ONE_TO_FIVE = "이름은 1이상 5이하입니다.";
-    private final String carName;
-    private int distance = 0;
+	private static final String ONE_TO_FIVE = "이름은 1이상 5이하입니다.";
+	public static final int MIN_BOUND = 1;
+	public static final int MAX_BOUND = 5;
+	private final String carName;
+	private int distance = 0;
 
-    public Car(String inputCar) {
-        String trimInput = eliminateBlank(inputCar);
-        validateNameLength(trimInput);
-        this.carName = trimInput;
-    }
+	public Car(String inputCar) {
+		String trimInput = eliminateBlank(inputCar);
+		validateNameLength(trimInput);
+		this.carName = trimInput;
+	}
 
-    private String eliminateBlank(String inputCar) {
-        return inputCar.trim();
-    }
+	private String eliminateBlank(String inputCar) {
+		return inputCar.trim();
+	}
 
-    private void validateNameLength(String inputCar) {
-        if (inputCar.length() < 1 || inputCar.length() > 5) {
-            throw new IllegalArgumentException(ONE_TO_FIVE);
-        }
-    }
+	private void validateNameLength(String inputCar) {
+		if (inputCar.length() < MIN_BOUND || inputCar.length() > MAX_BOUND) {
+			throw new IllegalArgumentException(ONE_TO_FIVE);
+		}
+	}
 
-    public void addDistance(int makeNumber) {
-        if (makeNumber >= 4) distance++;
-    }
+	public void addDistance(int makeNumber) {
+		if (makeNumber >= 4) {
+			distance++;
+		}
+	}
 
-    public String getCarName() {
-        return carName;
-    }
+	public String getCarName() {
+		return carName;
+	}
 
-    public int getDistance() {
-        return distance;
-    }
+	public int getDistance() {
+		return distance;
+	}
 
-    @Override
-    public int compareTo(Car car) {
-        return (this.distance > car.distance) ? 1 : -1;
-    }
+	@Override
+	public int compareTo(Car car) {
+		return Integer.compare(this.distance, car.distance);
+	}
 
-    @Override
-    public boolean equals(Object ob){
-        String carName = (String) ob;
-        return this.carName.equals(carName);
-    }
-
+	@Override
+	public boolean equals(Object ob) {
+		String carName = "";
+		if (ob instanceof String) {
+			carName = (String)ob;
+		}
+		return Objects.equals(this.carName, carName);
+	}
 }
