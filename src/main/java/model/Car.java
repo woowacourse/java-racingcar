b872821cbddb258d91d84b9142000a88c83d6b24
thdwoqor <celebarte01@gmail.com>
2023-02-12 04:17:21 +0900

    refactor : `Car` 클래스가 스스로 움직일지 여부를 결정

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,16 +1,22 @@
 package model;
 
-public class Car {
+import java.util.Random;
+
+public class Car implements MovableStrategy{
     public static final int RANDOM_MOVE_BOUNDARY = 4;
+    public static final int RANDOM_NUMBER_GENERATE_UPPER_BOUND = 10;
 
     private final String name;
+    private final Random random;
 
     public Car(String name) {
         this.name = name;
+        this.random = new Random();
     }
 
-    public boolean move(int randomNumber) {
-        return randomNumber >= RANDOM_MOVE_BOUNDARY;
+    @Override
+    public boolean isMove() {
+        return random.nextInt(RANDOM_NUMBER_GENERATE_UPPER_BOUND) >= RANDOM_MOVE_BOUNDARY;
     }
 
     @Override
