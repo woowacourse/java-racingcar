c49b7294dbe7d573ef20c1987b067866b527f925
kiarakim <kiarakim413@gmail.com>
2023-02-12 02:01:04 +0900

    test : Trial 테스트 코드 작성

--- src/test/java/domain/TrialTest.java
+++ src/test/java/domain/TrialTest.java
@@ -1,31 +1,25 @@
 package domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
+@DisplayName("시도 횟수는")
 class TrialTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"3ge", "~45", "2!9", "-98", "78.4"})
-    void checkChar(String input) {
-        assertThatThrownBy(() ->
-                new Car(input)
-        ).isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @Test
-    void checkZero() {
-        assertThatThrownBy(() ->
-                new Trial("0")
-        ).isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("정수값만 입력 가능하다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"문자", "char", "@#"})
+	void checkOnlyInteger(String input) {
+		assertThatThrownBy(() -> new Trial(Integer.parseInt(input))).isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @Test
-    void checkExtreme() {
-        assertThatThrownBy(() ->
-                new Trial("356574687456352313457643244567")
-        ).isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("시도 횟수가 0일 경우 테스트")
+	@Test
+	void checkZero() {
+		assertThatThrownBy(() -> new Trial(0)).isInstanceOf(IllegalArgumentException.class);
+	}
 }
