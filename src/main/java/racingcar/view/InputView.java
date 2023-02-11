5b3263d70a71e13e7e9bb6ca3115256a5ed6831d
Be-student <presentsong@naver.com>
2023-02-11 23:33:25 +0900

    질문 : scanner를 생성하게 되면 제대로 통과하는 것을 확인할 수 있었습니다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final Scanner scanner = new Scanner(System.in);
+//    private static final Scanner scanner = new Scanner(System.in);
 
 
     private InputView() {
@@ -19,11 +19,13 @@ public class InputView {
     }
 
     private static int nextInt() {
+        Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         return Integer.parseInt(line);
     }
 
     public static List<String> inputCarName() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(INPUT_CAR_NAME_MESSAGE);
         return List.of(scanner.nextLine().split(","));
     }
