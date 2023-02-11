3c50259c538146cd9efb7c60851b283c462eee96
chanminmaxscaler <jcm04@naver.com>
2023-02-11 20:30:05 +0900

    refactor: GameSystem 클래스를 리팩토링한다
    변경 내용 :
    - 매직넘버를 상수로 선언하여 사용한다.
    - 사용하지 않는 필드를 삭제한다.
    - 생성자에서 GameRecord 객체를 받아 클래스 변수로 사용할 수 있도록 한다.
    - executeRace 메소드에서 반환값을 없앤다.
    - executeRace 메소드에 게임 결과를 기록하는 기능을 추가한다.
    - getGameResult 메소드를 추가한다.
    - 우승자를 찾는 메소드를 추가한다.

--- src/main/java/racingcar/domain/system/GameSystem.java
+++ src/main/java/racingcar/domain/system/GameSystem.java
@@ -1,35 +1,68 @@
 package racingcar.domain.system;
 
 import racingcar.domain.cars.Cars;
-import racingcar.domain.numbergenerator.NumberGenerator;
+import racingcar.domain.dto.CarDto;
+import racingcar.domain.numbergenerator.RandomSingleDigitGenerator;
+import racingcar.domain.record.GameRecord;
 import racingcar.domain.result.Result;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GameSystem {
 
-    private final int gameRound;
-    private final NumberGenerator numberGenerator;
+    private static final int DEFAULT_POSITION = 0;
+    private static final int MINIMUM_GAME_ROUND = 0;
+    private final int finalRound;
+    private final GameRecord gameRecord;
 
-    public GameSystem(int gameRound, NumberGenerator numberGenerator) {
-        validate(gameRound);
-        this.gameRound = gameRound;
-        this.numberGenerator = numberGenerator;
+    public GameSystem(int finalRound, GameRecord gameRecord) {
+        validate(finalRound);
+        this.finalRound = finalRound;
+        this.gameRecord = gameRecord;
     }
 
     private void validate(int gameRound) {
-        if (gameRound <= 0) {
+        if (gameRound <= MINIMUM_GAME_ROUND) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 0 이하 일 수 없습니다.");
         }
     }
 
-    public Result executeRace(Cars cars) {
-        List<Map<String, Integer>> results = new ArrayList<>();
-        for (int i = 0; i < gameRound; i++) {
-            results.add(cars.movePosition());
+    public void executeRace(Cars cars) {
+        for (int gameRound = 0; gameRound < finalRound; gameRound++) {
+            cars.moveBy(new RandomSingleDigitGenerator());
+            recordGameResult(gameRound, cars);
         }
-        return new Result(results);
+    }
+
+    private void recordGameResult(int gameRound, Cars cars) {
+        gameRecord.record(gameRound, cars);
+    }
+
+    public Result getGameResult() {
+        return new Result(gameRecord.getRecords());
+    }
+
+    public List<String> findWinners() {
+        Set<CarDto> carDtos = getFinalRoundCarDtos();
+
+        Integer position = findTopPosition(carDtos);
+
+        return carDtos.stream()
+                .filter(carDto -> carDto.getPosition() == position)
+                .map(CarDto::getCarName)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Integer findTopPosition(Set<CarDto> carDtos) {
+        return carDtos.stream()
+                .max(Comparator.comparing(CarDto::getPosition))
+                .map(CarDto::getPosition)
+                .orElse(DEFAULT_POSITION);
+    }
+
+    private Set<CarDto> getFinalRoundCarDtos() {
+        Map<Integer, Set<CarDto>> records = gameRecord.getRecords();
+        return records.get(finalRound);
     }
 }
