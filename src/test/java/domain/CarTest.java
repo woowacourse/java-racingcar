e1235191523b324c1ce6840cbb1b47d2b472a7d0
hyunseo <ohseo8813@gmail.com>
2023-02-11 13:55:30 +0900

    <refactor>(domain/CarTest.java): Car 객체의 validation에 관한 테스트를 ParameterizedTest를 활용하도록 리팩토링

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -3,6 +3,8 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static domain.Car.*;
 import static org.assertj.core.api.Assertions.*;
@@ -14,22 +16,25 @@ class CarTest {
     class create_car {
 
         @DisplayName("검증을 통과하면 정상적으로 생성한다.")
-        @Test
-        void success() {
-            assertThatNoException().isThrownBy(() -> new Car("자동차"));
+        @ParameterizedTest
+        @ValueSource(strings = {"car", "name", "me"})
+        void success(String rightName) {
+            assertThatNoException().isThrownBy(() -> new Car(rightName));
         }
 
         @DisplayName("이름이 5자 초과이면 예외를 발생시킨다.")
-        @Test
-        void fail_by_name_length_exception() {
-            assertThatIllegalArgumentException().isThrownBy(() -> new Car("잘못된자동차이름"))
+        @ParameterizedTest
+        @ValueSource(strings = {"lenSix", "reallyLongName"})
+        void fail_by_name_length_exception(String wrongLengthName) {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car(wrongLengthName))
                     .withMessageContaining(NAME_LENGTH_EXCEPTION_MESSAGE);
         }
 
         @DisplayName("이름에 공백이 포함되면 예외를 발생시킨다.")
-        @Test
-        void fail_by_contain_blank() {
-            assertThatIllegalArgumentException().isThrownBy(() -> new Car(" bad"))
+        @ParameterizedTest
+        @ValueSource(strings = {" ", " car", "car "})
+        void fail_by_contain_blank(String nameContainingBlank) {
+            assertThatIllegalArgumentException().isThrownBy(() -> new Car(nameContainingBlank))
                     .withMessageContaining(BLANK_CONTAIN_NAME_EXCEPTION_MESSAGE);
         }
     }
