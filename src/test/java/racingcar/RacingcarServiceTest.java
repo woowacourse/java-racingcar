685e492af5b81c129b689951583f3dad1aaad8b7
Wugawuga <dnr3655@naver.com>
2023-02-11 17:57:56 +0900

    refactor: 서비스에서 자동차 움직임 확인 삭제, 우승자 확인

--- src/test/java/racingcar/RacingcarServiceTest.java
+++ src/test/java/racingcar/RacingcarServiceTest.java
@@ -2,9 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
@@ -12,45 +10,13 @@ import racingcar.service.RacingcarService;
 
 class RacingcarServiceTest {
 
-    private final RacingcarService racingcarService = new RacingcarService();
-    private Car car1;
-    private Car car2;
-    private Car car3;
-
-    @BeforeEach
-    void initCars() {
-        car1 = new Car("car1");
-        car2 = new Car("car2");
-        car3 = new Car("car3");
-    }
-
-    @Test
-    @DisplayName("우승 자동차 위치 확인하기")
-    void findWinnerPosition() {
-        car2.move(5);
-        car3.move(5);
-
-        List<Car> cars = Arrays.asList(car1, car2, car3);
-
-        int winnerPosition = findWinnerPosition(cars, racingcarService);
-
-        assertThat(winnerPosition).isEqualTo(1);
-    }
-
     @Test
     @DisplayName("우승자 확인하기")
     void findWinner() {
-        car2.move(5);
-        car3.move(5);
+        RacingcarService racingcarService = new RacingcarService("car1,car2,car3");
 
-        List<Car> cars = Arrays.asList(car1, car2, car3);
-        int winnerPosition = findWinnerPosition(cars, racingcarService);
-        List<Car> winners = racingcarService.findWinner(cars, winnerPosition);
+        List<Car> winners = racingcarService.findWinners();
 
-        assertThat(winners).containsExactly(car2, car3);
-    }
-
-    private int findWinnerPosition(List<Car> cars, RacingcarService racingcarService) {
-        return racingcarService.findPosition(cars);
+        assertThat(winners.toString()).contains("car1", "car2", "car3");
     }
 }
