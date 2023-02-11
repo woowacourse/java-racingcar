cb8b30f9b0dbbbc187eb0ad66d47762595547e9f
splitube <voicesplit@daum.net>
2023-02-11 15:54:45 +0900

    refactor: main에서 객체들 사이의 관계를 맺어줌

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,15 @@
+import controller.RaceController;
+import util.RandomNumberGenerator;
+import view.input.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        Game game = new Game();
-        game.play();
+        RaceController raceController = new RaceController();
+        InputView inputView = new InputView();
+        Game game = new Game(raceController);
+        game.ready(inputView);
+        game.playGame(new RandomNumberGenerator());
+        game.showResult();
     }
 }
