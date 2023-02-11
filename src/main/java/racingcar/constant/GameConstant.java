42118f5a56995e058051a236696ebc91775305db
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-11 23:38:35 +0900

    refactor : 랜덤 숫자 생성 기능 수정

--- src/main/java/racingcar/constant/GameConstant.java
+++ src/main/java/racingcar/constant/GameConstant.java
@@ -1,5 +1,8 @@
 package racingcar.constant;
 
 public class GameConstant {
-    public static final int START_POINT = 0;
+    public static final int RACE_START_POINT = 0;
+    public static final int MINIMUM_POWER_OF_ENGINE = 0;
+    public static final int MAXIMUM_POWER_OF_ENGINE = 9;
+    public static final int MINIMUM_POWER_TO_MOVE = 4;
 }
