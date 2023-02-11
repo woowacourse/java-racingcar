13daffcd4e0d184b00897e054c5bea1d81ff1f9e
jyeost <pig9619@naver.com>
2023-02-11 15:19:36 +0900

    style : import 순서를 변경하였다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,9 @@
 package view;
 
+import java.util.Scanner;
+
 import domain.Cars;
 import domain.Trial;
-
-import java.util.Scanner;
 
 public class InputView {
     private final Scanner scanner = new Scanner(System.in);
