d805cb102b0b31b1bb571e4698c251c10aa0f5ae
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-11 20:25:57 +0900

    refactor: DTO 클래스의 책임 분리 DTO클래스(CarNamesRequest.class) 에서 이루어지는 도메인 객체의 논리적인 특징에 대한 검증 로직 분리

--- src/main/java/racingcar/model/car/Car.java
+++ src/main/java/racingcar/model/car/Car.java
@@ -1,8 +1,13 @@
 package racingcar.model.car;
 
+import java.util.regex.Pattern;
 import racingcar.model.car.strategy.MovingStrategy;
+import racingcar.util.ErrorMessage;
 
 public class Car {
+    private static final String VALID_WORD_REGEX = "(\\w)+";
+    private static final Pattern CAR_NAME_PATTERN = Pattern.compile(VALID_WORD_REGEX);
+    private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final int DEFAULT_POSITION = 1;
 
     private final String carName;
@@ -10,6 +15,7 @@ public class Car {
     private final MovingStrategy movingStrategy;
 
     public Car(String carName, MovingStrategy movingStrategy) {
+        validate(carName);
         this.carName = carName;
         this.movingStrategy = movingStrategy;
     }
@@ -24,6 +30,23 @@ public class Car {
 
     public boolean movable() {
         return movingStrategy.movable();
+    }
+
+    private void validate(String carName) {
+        validateValidWord(carName);
+        validateOverMaxLength(carName);
+    }
+
+    private void validateValidWord(String splitCarName) {
+        if (!CAR_NAME_PATTERN.matcher(splitCarName).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_FORMAT.message());
+        }
+    }
+
+    private void validateOverMaxLength(String splitCarName) {
+        if (splitCarName.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_OVER_RANGE.message());
+        }
     }
 
     public String getCarName() {
--- src/main/java/racingcar/view/dto/CarNamesRequest.java
+++ src/main/java/racingcar/view/dto/CarNamesRequest.java
@@ -1,66 +1,36 @@
 package racingcar.view.dto;
 
-import racingcar.util.ErrorMessage;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import racingcar.util.ErrorMessage;
 
 public class CarNamesRequest {
-    private static final String VALID_WORD_REGEX = "(\\w)+";
     private static final String SEPARATOR = ",";
-    private static final int CAR_NAME_MAX_LENGTH = 5;
-    private static final Pattern CAR_NAME_PATTERN = Pattern.compile(VALID_WORD_REGEX);
 
     private final String carNames;
 
-    public CarNamesRequest(String carNames) {
-        validate(carNames);
-        this.carNames = carNames;
+    public CarNamesRequest(String userInput) {
+        validate(userInput);
+        this.carNames = userInput;
     }
 
-    private void validate(String carNames) {
-        String[] splitCarNames = carNames.split(SEPARATOR);
-        validateEmptyCarNames(splitCarNames);
-        for (String splitCarName : splitCarNames) {
-            validateWord(splitCarName);
-            validateBlank(splitCarName);
-            validateOverMaxLength(splitCarName);
-        }
-
-        validateDuplicateCarNames(splitCarNames);
+    private void validate(String userInput) {
+            validateBlank(userInput);
     }
 
-    private void validateEmptyCarNames(String[] carNames) {
-        if (carNames.length == 0) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_NULL.message());
-        }
-    }
-    private void validateDuplicateCarNames(String[] splitCarNames) {
-        int carNamesCount = splitCarNames.length;
-        long distinctCount = Arrays.stream(splitCarNames).distinct().count();
-
-        if (carNamesCount != distinctCount) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATED.message());
-        }
-    }
+//    private void validateDuplicateCarNames(String[] splitCarNames) {
+//        int carNamesCount = splitCarNames.length;
+//        long distinctCount = Arrays.stream(splitCarNames).distinct().count();
+//
+//        if (carNamesCount != distinctCount) {
+//            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATED.message());
+//        }
+//    }
 
     private void validateBlank(String splitCarName) {
         if (splitCarName.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_NULL.message());
-        }
-    }
-
-    private void validateWord(String splitCarName) {
-        if (!CAR_NAME_PATTERN.matcher(splitCarName).matches()) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_FORMAT.message());
-        }
-    }
-
-    private void validateOverMaxLength(String splitCarName) {
-        if (splitCarName.length() > CAR_NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_OVER_RANGE.message());
         }
     }
 
