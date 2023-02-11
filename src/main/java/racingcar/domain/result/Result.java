58540eea9bab4ab81138e75db51c03ef00d4e8d0
chanminmaxscaler <jcm04@naver.com>
2023-02-11 20:36:44 +0900

    refactor: Result 객체에 우승자를 찾는 기능을 삭제한다.

--- src/main/java/racingcar/domain/result/Result.java
+++ src/main/java/racingcar/domain/result/Result.java
@@ -1,38 +1,19 @@
 package racingcar.domain.result;
 
-import java.util.Comparator;
-import java.util.List;
+import racingcar.domain.dto.CarDto;
+
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class Result {
 
-    private final List<Map<String, Integer>> results;
+    private final Map<Integer, Set<CarDto>> results;
 
-    public Result(List<Map<String, Integer>> results) {
+    public Result(Map<Integer, Set<CarDto>> results) {
         this.results = results;
     }
 
-    public List<Map<String, Integer>> getResults() {
+    public Map<Integer, Set<CarDto>> getResults() {
         return results;
-    }
-
-    public List<String> findWinners() {
-        Map<String, Integer> lastResult = getLastResult();
-        return lastResult.keySet()
-                .stream()
-                .filter(key -> lastResult.get(key) == findMaxPosition(lastResult))
-                .collect(Collectors.toList());
-    }
-
-    private Map<String, Integer> getLastResult() {
-        return results.get(results.size() - 1);
-    }
-
-    private int findMaxPosition(Map<String, Integer> lastResult) {
-        return lastResult.values()
-                .stream()
-                .max(Comparator.comparing(x -> x))
-                .orElseThrow(IllegalArgumentException::new);
     }
 }
