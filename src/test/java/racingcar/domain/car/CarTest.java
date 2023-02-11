4609b9a48c2b9878bfc9e9f72aced36027600422
chanminmaxscaler <jcm04@naver.com>
2023-02-11 20:25:02 +0900

    refactor: CarTest 테스트코드를 리팩토링한다
    변경 내용 :
    - 메소드의 이름을 수정한다.
    - DisplayName 내용을 수정한다.
    - Car 객체의 move 기능을 테스트 하는 내용을 수정한다.
    power에 따라 움직였는지 여부를 확인할 수 있도록 한다.

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -2,39 +2,42 @@ package racingcar.domain.car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("자동차 객체의")
 class CarTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"roy", "hoocu", "sun", "ROO", "Ruca"})
-    @DisplayName("영어로 구성된 한 글자 이상 다섯 글자 이하의 문자열이 입력되었을 때, Car 객체가 생성되는지 확인")
-    void validate_test(String input) {
-        Car car = new Car(input);
-
-        assertThat(car).isInstanceOf(Car.class);
+    @DisplayName("생성자에 영어로 구성된 한 글자 이상 다섯 글자 이하의 이름을 입력할 때, 오류가 발생하지 않는지 확인")
+    void create_test(String input) {
+        assertDoesNotThrow(() -> new Car(input));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"코이", "koy1", "royroy", "----", "ㅁdfsㅇ"})
-    @DisplayName("영어가 아니거나 범위를 벗어난 문자열이 입력되었을 때, 오류가 발생하는지 확인")
-    void validate_error_test(String input) {
+    @ValueSource(strings = {"코이", "koy1", "royroy", "----", "ㅁdfsㅇ", "현구막"})
+    @DisplayName("생성자에 영어가 아니거나 다섯 글자를 초과한 이름을 입력할 때, 오류가 발생하는지 확인")
+    void create_error_test(String input) {
         assertThatThrownBy(() -> new Car(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"poby"})
-    @DisplayName("Car 객체의 position을 업데이트 했을 때, position이 1이 되었는지 확인")
-    void updatePosition_test(String name) {
-        Car car = new Car(name);
+    @CsvSource(value = {"0:0", "2:0", "3:0", "4:1", "9:1"}, delimiter = ':')
+    @DisplayName("move 메소드에 power를 입력했을 때, 4를 기준으로 position이 변화하도록 움직이는지 확인")
+    void move_test(int power, int expectedPosition) {
+        String exampleCarName = "hoocu";
+        Car car = new Car(exampleCarName);
 
-        car.updatePosition();
+        car.move(power);
+        int actualPosition = car.getPosition();
 
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(actualPosition).isEqualTo(expectedPosition);
     }
 }
