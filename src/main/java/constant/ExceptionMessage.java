1ec3786ec863aa6952a9e405f48b683834ad704d
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-11 21:04:54 +0900

    feat(ExceptionMessage): 예외 메시지 추가
    자동차 이름을 아무 것도 입력하지 않은 경우에 대한 예외 메시지 추가

--- src/main/java/constant/ExceptionMessage.java
+++ src/main/java/constant/ExceptionMessage.java
@@ -1,6 +1,7 @@
 package constant;
 
 public enum ExceptionMessage {
+    CAR_NAME_NOT_INPUT_MESSAGE("자동차 이름을 입력해주세요."),
     CAR_NAME_WRONG_LENGTH_MESSAGE("자동차 이름은 1~5자 이내의 이름으로 입력하여야 합니다."),
     CAR_NAME_BLANK_MESSAGE("자동차 이름은 공백만 입력하는 것이 불가능합니다."),
     TRY_COUNT_NOT_NUMBER_MESSAGE("시도 횟수는 숫자로만 입력하여야 합니다."),
