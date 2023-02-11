770d4336f0bc1d8a084f97aaee5b13e6d3bad5ac
donghae <donghae1999@gmail.com>
2023-02-11 23:24:27 +0900

    refactor: Random 객체 생성 위치 변경

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
     private static final int RANDOM_NUMBER_STANDARD = 10;
+    private static Random random = new Random();
 
     @Override
     public int generateNumber() {
-        Random random = new Random();
         return random.nextInt(RANDOM_NUMBER_STANDARD);
     }
 }
