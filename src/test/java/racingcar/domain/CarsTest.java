9445d0b6fcbdb2fb488d00362ce8a43efcd752f7
jiwon <ljwon77@daum.net>
2023-02-11 17:00:09 +0900

    test: 경주 진행 결과 개수 및 초기 자동차 위치 테스트 추가, private method에 대한 테스트 제거

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -5,121 +5,133 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.dto.CarRaceDto;
+import racingcar.exception.DuplicateException;
+import racingcar.exception.GlobalException;
+import racingcar.mock.MockNumberGenerator;
 import racingcar.provider.TestProvider;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static racingcar.enumType.ExceptionMessage.DUPLICATE_MESSAGE;
 
 class CarsTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,honux"})
-    @DisplayName("정상적인 차량 이름이 들어오면 예외가 발생하지 않는다.")
+    @DisplayName("정상적인 자동차 이름이 들어오면 예외가 발생하지 않는다.")
     void givenNormalCarNames_thenSuccess(String carNames) {
-        assertThatCode(() -> Cars.of(carNames))
+        // given
+        String testCarNames = "pobi,crong,honux";
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
+
+        // when & then
+        assertThatCode(() -> Cars.of(carNames, numberGenerator))
                 .doesNotThrowAnyException();
 
-        assertThat(Cars.of(carNames))
-                .isEqualTo(TestProvider.createTestCars());
+        assertThat(Cars.of(carNames, numberGenerator))
+                .isEqualTo(TestProvider.createTestCars(testCarNames, numberGenerator));
     }
 
     @ParameterizedTest
     @NullSource
-    @DisplayName("차량 이름에 null값이 들어오면 split 시 에외가 발생한다.")
+    @DisplayName("자동차 이름에 null 값이 들어오면 split 시 에외가 발생한다.")
     void givenNullCarNames_thenFail(String carNames) {
-        assertThatThrownBy(() -> Cars.of(carNames))
+        // given
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
+
+        // when & then
+        assertThatThrownBy(() -> Cars.of(carNames, numberGenerator))
                 .isInstanceOf(NullPointerException.class);
     }
 
-
     @ParameterizedTest
-    @ValueSource(ints = {4, 9})
-    @DisplayName("랜덤값이 4 이상이면 자동차가 전진한다.")
-    void givenFourMoreNumber_thenCarMove(int randomNumber) throws Exception {
+    @ValueSource(strings = {"pobi,pobi"})
+    @DisplayName("자동차 이름에 중복값이 들어오면 예외가 발생한다.")
+    void givenDuplicateCarNames_thenFail(String carNames) {
         // given
-        Cars cars = TestProvider.createTestCars();
-        Car car = TestProvider.createTestCar("pobi");
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
 
-        // when
-        Method method = cars.getClass().getDeclaredMethod("checkNumberAndMove", Car.class, int.class);
-        method.setAccessible(true);
-        method.invoke(cars, car, randomNumber);
-
-        // then
-        assertThat(car)
-                .isEqualTo(TestProvider.createTestMoveCar(car));
+        // when & then
+        assertThatThrownBy(() -> Cars.of(carNames, numberGenerator))
+                .isInstanceOf(GlobalException.class)
+                .isExactlyInstanceOf(DuplicateException.class)
+                .hasMessageContaining(DUPLICATE_MESSAGE.getValue());
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 3})
-    @DisplayName("랜덤값이 3 이하면 자동차가 정지한다.")
-    void givenThreeLessNumber_thenCarStop(int randomNumber) throws Exception {
+    @Test
+    @DisplayName("모든 자동차의 초기 위치는 1이어야 한다.")
+    void givenCars_whenInit_thenPositionIsOne() {
         // given
-        Cars cars = TestProvider.createTestCars();
-        Car car = TestProvider.createTestCar("pobi");
+        String carNames = "pobi,crong,honux";
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
+        Cars testCars = TestProvider.createTestCars(carNames, numberGenerator);
 
         // when
-        Method method = cars.getClass().getDeclaredMethod("checkNumberAndMove", Car.class, int.class);
-        method.setAccessible(true);
-        method.invoke(cars, car, randomNumber);
+        List<CarRaceDto> initResult = testCars.initStatus();
 
         // then
-        assertThat(car)
-                .isEqualTo(TestProvider.createTestCar("pobi"));
+        int normalPositionCarCount = (int) initResult.stream()
+                .filter(carRaceDto -> carRaceDto.getCarPosition() == 1)
+                .count();
+
+        assertThat(normalPositionCarCount)
+                .isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("경주를 진행하면 자동차의 개수만큼 결과가 나와야 한다.")
+    void givenCars_whenRace_thenReturnResultAboutCarCount() {
+        // given
+        String carNames = "pobi,crong,honux";
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
+        Cars testCars = TestProvider.createTestCars(carNames, numberGenerator);
+
+        // when
+        List<CarRaceDto> carRaceResult = testCars.race();
+
+        // then
+        assertThat(carRaceResult.size())
+                .isEqualTo(3);
     }
 
     @Test
     @DisplayName("우승한 자동차를 뽑는다.")
-    void givenCarInfo_thenPickWinner() throws Exception {
+    void givenCarInfo_thenPickWinner() {
         // given
-        Car pobi = createCarAndMove("pobi", 5);
-        Car crong = createCarAndMove("crong", 3);
-        Car honux = createCarAndMove("honux", 1);
-
-        Cars cars = TestProvider.createTestCars();
-        Field field = cars.getClass().getDeclaredField("cars");
-        field.setAccessible(true);
-        field.set(cars, List.of(pobi, crong, honux));
+        String carNames = "pobi,crong,honux";
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(true);
+        Cars testCars = TestProvider.createTestCars(carNames, numberGenerator);
 
         // when
-        String winners = cars.pickWinners();
+        testCars.race();
+        List<String> winners = testCars.pickWinners();
 
         // then
+        assertThat(winners.size())
+                .isEqualTo(1);
+
         assertThat(winners)
-                .isEqualTo("pobi");
+                .isEqualTo(List.of("honux"));
     }
 
     @Test
     @DisplayName("우승한 자동차 리스트를 뽑는다.")
-    void givenCarInfo_thenPickWinners() throws Exception {
+    void givenCarInfo_thenPickWinners() {
         // given
-        Car pobi = createCarAndMove("pobi", 3);
-        Car crong = createCarAndMove("crong", 3);
-        Car honux = createCarAndMove("honux", 3);
-
-        Cars cars = TestProvider.createTestCars();
-        Field field = cars.getClass().getDeclaredField("cars");
-        field.setAccessible(true);
-        field.set(cars, List.of(pobi, crong, honux));
+        String carNames = "pobi,crong,honux";
+        MockNumberGenerator numberGenerator = TestProvider.createMockNumberGenerator(false);
+        Cars testCars = TestProvider.createTestCars(carNames, numberGenerator);
 
         // when
-        String winners = cars.pickWinners();
+        testCars.race();
+        List<String> winners = testCars.pickWinners();
 
         // then
         assertThat(winners)
-                .isEqualTo("pobi, crong, honux");
-    }
-
-    private Car createCarAndMove(String carName, int moveCount) {
-        Car car = TestProvider.createTestCar(carName);
-        for (int i = 0; i < moveCount; i++) {
-            TestProvider.createTestMoveCar(car);
-        }
-        return car;
+                .isEqualTo(List.of("pobi", "crong", "honux"));
     }
 }
--- src/test/java/racingcar/provider/TestProvider.java
+++ src/test/java/racingcar/provider/TestProvider.java
@@ -2,19 +2,24 @@ package racingcar.provider;
 
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.Race;
+import racingcar.mock.MockNumberGenerator;
 
 public class TestProvider {
-    public static Cars createTestCars() {
-        String carNames = "pobi,crong,honux";
-        return Cars.of(carNames);
-    }
 
-    public static Car createTestCar(String carName) {
+    public static Car createTestCar(final String carName) {
         return Car.of(carName);
     }
 
-    public static Car createTestMoveCar(Car car) {
-        car.move();
-        return car;
+    public static MockNumberGenerator createMockNumberGenerator(final boolean isIncrease) {
+        return new MockNumberGenerator(isIncrease);
+    }
+
+    public static Cars createTestCars(final String carNames, final MockNumberGenerator numberGenerator) {
+        return Cars.of(carNames, numberGenerator);
+    }
+
+    public static Race createTestRace(final String raceCount) {
+        return Race.of(raceCount);
     }
 }
