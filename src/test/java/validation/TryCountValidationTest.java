e8816f106ce52bcbaec6c9c796805e7d6944999f
sosow0212 <sosow0212@naver.com>
2023-02-11 23:30:29 +0900

    test : validateTryCount() 메서드 실행시 실패 경우와 성공 경우의 케이스를 나눔

--- src/test/java/validation/TryCountValidationTest.java
+++ src/test/java/validation/TryCountValidationTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TryCountValidationTest {
 
@@ -20,13 +22,24 @@ class TryCountValidationTest {
                 .hasMessageContaining(expectedMessage);
     }
 
-    @Test
-    @DisplayName("validateTryCount() 성공 경우")
-    void test_validateTryCount_success() {
+    @ParameterizedTest
+    @ValueSource(ints = {-3, -2, -1})
+    @DisplayName("validateTryCount() 실패 경우")
+    void test_validateTryCount_fail(int input) {
         // given
-        final int input = 3;
+        String expectedMessage = "0 이상의 수를 입력해주세요.";
 
         // when, then
+        assertThatThrownBy(() -> TryCountValidation.validateTryCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessage);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    @DisplayName("validateTryCount() 성공 경우")
+    void test_validateTryCount_success(int input) {
+        // when & then
         TryCountValidation.validateTryCount(input);
     }
 }
