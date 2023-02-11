52d1a1544121ae030b52f393e626fc3132d5c58c
yeonkkk <tjddus0302@gmail.com>
2023-02-11 20:07:44 +0900

    refactor: Car 클래스에서 위치 상태를 관리하도록 수정
    - 기존에 CarRepository 에서 관리했던 차의 위치 상태를 Car 클래스에서 position 으로 관리하도록 수정
    - 자동차 이름 검증 외에 기능이 없는 Name 클래스를 Car 클래스의 내부 클래스로 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,68 @@
 package domain;
 
 
+import static utils.ErrorMessage.WRONG_NAME_LENGTH;
+
 public class Car {
 
-    private final Name name;
+    private final int MOVING_STANDARD_NUM = 4;
+    private final int ADD_POINT = 1;
+    private final String POSITION_BAR = "-";
 
-    public Car(Name name) {
-        this.name = name;
+
+    private final Name name;
+    private int position;
+
+    public Car(String name) {
+        this.name = new Name(name);
+        this.position = 0;
+    }
+
+    public void move(final int number) {
+        if (isAllowedToMove(number)) {
+            this.position += ADD_POINT;
+        }
+    }
+
+    private boolean isAllowedToMove(final int number) {
+        if (number >= MOVING_STANDARD_NUM) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getCarStatus() {
+        return name.getName() + " : " + POSITION_BAR.repeat(position);
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    private class Name {
+
+        private final int MIN_NAME_SIZE = 0;
+        private final int MAX_NAME_SIZE = 5;
+
+        private final String name;
+
+        private Name(String name) {
+            validateCarNameLength(name.length());
+            this.name = name;
+        }
+
+        private void validateCarNameLength(int length) {
+            if (length <= MIN_NAME_SIZE || length > MAX_NAME_SIZE) {
+                throw new IllegalArgumentException(WRONG_NAME_LENGTH.of());
+            }
+        }
+
+        private String getName() {
+            return name;
+        }
     }
 }
