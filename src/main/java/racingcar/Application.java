a57aa8c4167bd278dfba5fa5e6f0da5830fa66eb
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-11 09:18:25 +0900

    fix: 메서드 간 위치 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
 
 public class Application {
 
+    public static void main(String[] args) {
+        GameController gameController = new GameController(createInputView(), createOutputView());
+        gameController.run();
+    }
+
     private static InputView createInputView() {
         return new InputView(new Scanner(System.in), new OutputView());
     }
 
     private static OutputView createOutputView() {
         return new OutputView();
-    }
-
-    public static void main(String[] args) {
-        GameController gameController = new GameController(createInputView(), createOutputView());
-        gameController.run();
     }
 }
