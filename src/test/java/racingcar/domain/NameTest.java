86fd020c89c07bd12b771fd439a5c9c61856717d
iamjooon2 <iamjooon2@gmail.com>
2023-02-11 21:04:55 +0900

    test: Name 도메인 테스트 작성

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "aaaaaa", ""})
+    @DisplayName("Name 생성시 에러 발생 테스트")
+    void nameFailTest(String carName) {
+        assertThatThrownBy(() -> new Name(carName)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"gavi", "jude", "pobi"})
+    @DisplayName("Name 생성 성공 테스트")
+    void carNameSuccessTest(String carName) {
+        new Name(carName);
+    }
+
+}
