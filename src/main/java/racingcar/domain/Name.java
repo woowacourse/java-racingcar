b896203229768d5927128e17f416e511cd811b3c
seokjin8678 <rnrn8678@naver.com>
2023-02-11 11:53:47 +0900

    feat: Name 클래스 추가

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Name {
+    private final String value;
+
+    public Name(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
