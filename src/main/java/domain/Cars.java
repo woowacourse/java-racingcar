250a022221e632646d019bedb27242121b14269e
kiarakim <kiarakim413@gmail.com>
2023-02-11 19:12:01 +0900

    refactor : 코드 컨벤션에 따른 제어자 순서 변경

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -4,62 +4,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    List<Car> cars = new ArrayList<>();
-    private final static String ERROR_NAME = "두 대 이상의 자동차 이름을 입력해주세요";
-    private final static String ERROR_DUPLICATION = "중복되지 않은 자동차를 입력해주세요";
-    private final static String COMMA = ",";
+	List<Car> cars = new ArrayList<>();
+	private static final String ERROR_NAME = "두 대 이상의 자동차 이름을 입력해주세요";
+	private static final String ERROR_DUPLICATION = "중복되지 않은 자동차를 입력해주세요";
+	private static final String COMMA = ",";
 
-    public Cars(String userInput) {
-        validateNotNull(userInput);
-        validateHasComma(userInput);
-        addCar(userInput);
-    }
+	public Cars(String userInput) {
+		validateNotNull(userInput);
+		validateHasComma(userInput);
+		addCar(userInput);
+	}
 
-    private void validateNotNull(String userInput) {
-        if (userInput == null || "".equals(userInput)) {
-            throw new IllegalArgumentException(ERROR_NAME);
-        }
-    }
+	private void validateNotNull(String userInput) {
+		if (userInput == null || "".equals(userInput)) {
+			throw new IllegalArgumentException(ERROR_NAME);
+		}
+	}
 
-    private void validateHasComma(String userInput) {
-        if (!userInput.contains(COMMA)) {
-            throw new IllegalArgumentException(ERROR_NAME);
-        }
-    }
+	private void validateHasComma(String userInput) {
+		if (!userInput.contains(COMMA)) {
+			throw new IllegalArgumentException(ERROR_NAME);
+		}
+	}
 
-    private void addCar(String userInput) {
-        String[] userInputSplit = userInput.split(COMMA);
-        for (String inputSplit : userInputSplit) {
-            validateDuplication(inputSplit);
-            cars.add(new Car(inputSplit));
-        }
-    }
+	private void addCar(String userInput) {
+		String[] userInputSplit = userInput.split(COMMA);
+		for (String inputSplit : userInputSplit) {
+			validateDuplication(inputSplit);
+			cars.add(new Car(inputSplit));
+		}
+	}
 
-    private void validateDuplication(String inputSplit) {
-        if (cars.contains(inputSplit)) {
-            throw new IllegalArgumentException(ERROR_DUPLICATION);
-        }
-    }
+	private void validateDuplication(String inputSplit) {
+		if (cars.contains(inputSplit)) {
+			throw new IllegalArgumentException(ERROR_DUPLICATION);
+		}
+	}
 
-    public int getCarsSize() {
-        return cars.size();
-    }
+	public int getCarsSize() {
+		return cars.size();
+	}
 
-    public void addDistance(List<Integer> randomNumbers) {
-        for (int i = 0; i < cars.size(); i++) {
-            cars.get(i).addDistance(randomNumbers.get(i));
-        }
+	public void addDistance(List<Integer> randomNumbers) {
+		for (int i = 0; i < cars.size(); i++) {
+			cars.get(i).addDistance(randomNumbers.get(i));
+		}
         /*for (Car car : cars) {
             // car.addDistance(new RandomNumberMaker().makeNumber());
 
         }*/
-    }
+	}
 
-    public Car getCar(int i) {
-        return this.cars.get(i);
-    }
+	public Car getCar(int i) {
+		return this.cars.get(i);
+	}
 
-    public List<Car> getCars() {
-        return cars;
-    }
+	public List<Car> getCars() {
+		return cars;
+	}
 }
