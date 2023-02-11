45e9f44f7378c0aa4f12decc9eb1829b1f885e5a
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-11 20:47:30 +0900

    refactor: Random 객체 반복 호출하지 않도록 수정

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class RandomNumberGenerator implements NumberGenerator {
     private final int MAX_RANDOM_NUMBER = 10;
 
+    private final Random random = new Random();
+
     @Override
     public int generateNumber() {
-        Random random = new Random();
         return random.nextInt(MAX_RANDOM_NUMBER);
     }
 }
