2ba9914eb5e2e8a39b6570fcde5798442e8e70f3
thdwoqor <celebarte01@gmail.com>
2023-02-12 00:45:57 +0900

    test : 검증 로직 테스트 코드 추가

---
+++ src/test/java/util/CarNameValidatorTest.java
@@ -0,0 +1,26 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarNameValidatorTest {
+
+    private Validator carNameValidator;
+
+    @DisplayName("이름 입력 실패")
+    @ParameterizedTest
+    @ValueSource(strings = {"\n", "포비는너무멋져남자가봐도반하겠어", ",,,,,", "파워, ,메튜"})
+    void validate(String input) {
+        carNameValidator = new CarNameValidator();
+        List<String> carNames = Arrays.asList(input.split(","));
+        assertThatThrownBy(() -> carNameValidator.validate(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차명은 1 ~ 5 글자로 입력해야합니다.");
+    }
+}
---
+++ src/test/java/util/RepeatCountValidatorTest.java
@@ -0,0 +1,34 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RepeatCountValidatorTest {
+
+    private Validator repeatCountValidator;
+
+    @DisplayName("반복 횟수 입력 실패 1미만 100초과")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "101", "-1"})
+    void validateRange(String input) {
+        repeatCountValidator = new RepeatCountValidator();
+
+        assertThatThrownBy(() -> repeatCountValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상만 가능합니다.");
+    }
+
+    @DisplayName("반복 횟수 입력 실패 숫자 이외의값")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " ", "가", "@",""})
+    void validateNumber(String input) {
+        repeatCountValidator = new RepeatCountValidator();
+
+        assertThatThrownBy(() -> repeatCountValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력 가능합니다.");
+    }
+}
