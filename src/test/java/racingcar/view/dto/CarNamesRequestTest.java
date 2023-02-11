cde38e001dff96b47e48938b2e587ed27bec799b
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-11 21:37:25 +0900

    test: 여러 자동차 이름 입력을 분리하여 반환하는 로직에 대한 테스트 추가

--- src/test/java/racingcar/view/dto/CarNamesRequestTest.java
+++ src/test/java/racingcar/view/dto/CarNamesRequestTest.java
@@ -1,8 +1,11 @@
 package racingcar.view.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.util.ErrorMessage;
@@ -16,5 +19,21 @@ class CarNamesRequestTest {
             new CarNamesRequest(input);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.CAR_NAME_NULL.message());
+    }
+
+    @DisplayName("\",\"로 구분된 문자는 정상 구분되어야 한다.")
+    @Test
+    void separateValueTest() {
+        //given
+        String values = "pobi,crong,royce";
+        CarNamesRequest carNames = new CarNamesRequest(values);
+
+        //when
+        List<String> splitCarNames = carNames.toSplitCarNames();
+
+        //then
+        assertThat(splitCarNames).hasSize(3);
+        assertThat(splitCarNames)
+                .containsExactly("pobi", "crong", "royce");
     }
 }
