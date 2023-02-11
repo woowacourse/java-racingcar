fc943943dc3620558f7098ba8de72bc6787249d9
splitube <voicesplit@daum.net>
2023-02-11 15:14:52 +0900

    test: 입력값이 있을때까지 입력받는 기능 테스트

--- src/test/java/view/input/InputViewTest.java
+++ src/test/java/view/input/InputViewTest.java
@@ -1,43 +1,49 @@
 package view.input;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import validation.ErrorMessages;
 
 class InputViewTest {
 
     private InputView inputView;
+    private ByteArrayOutputStream outputMessage;
 
-    void setInput(String input) {
+    void setInputStream(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         inputView = new InputView();
     }
 
-    @DisplayName("입력이 빈 문자열일 때 오류를 던진다")
+    void setOutput() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @DisplayName("입력이 빈 문자열이 아닐때까지 입력을 받는다")
     @Test
     void input_empty() {
-        setInput("\nb");
-        assertThatThrownBy(inputView::getCarNames).isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(ErrorMessages.EMPTY_INPUT.getMessage());
+        setInputStream("\n\nb");
+        setOutput();
+        String inputExist = inputView.getInputUntilExist();
+        assertThat(
+            "입력값이 없습니다. 다시 입력해주세요\n입력값이 없습니다. 다시 입력해주세요\n")
+            .isEqualTo(outputMessage.toString());
+        assertThat(inputExist).isEqualTo("b");
     }
 
     @DisplayName("공백으로만 이루어져 있는 문자열은 모든 공백이 무시된다.")
     @Test
     void input_strip_all_blank() {
-        setInput("              \nb");
-        assertThatThrownBy(inputView::getCarNames).isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(ErrorMessages.EMPTY_INPUT.getMessage());
-    }
-
-    @DisplayName("입력 앞뒤에 존재하는 공백은 무시한다.")
-    @Test
-    void input_strip_word_exist() {
-        setInput("       a      \nb");
-        String input = inputView.getCarNames();
-        assertThat(input).isEqualTo("a");
+        setInputStream("             \n\n\nb");
+        setOutput();
+        String inputExist = inputView.getInputUntilExist();
+        assertThat(
+            "입력값이 없습니다. 다시 입력해주세요\n입력값이 없습니다. 다시 입력해주세요\n입력값이 없습니다. 다시 입력해주세요\n")
+            .isEqualTo(outputMessage.toString());
+        assertThat(inputExist).isEqualTo("b");
     }
 }
