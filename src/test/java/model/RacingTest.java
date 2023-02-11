ca698e3e3d557608d7b7992384e683b3f1f72716
thdwoqor <celebarte01@gmail.com>
2023-02-12 04:30:42 +0900

    refactor : 성공만하는 `TestCarSuccess` 클래스와 실패만하는 `TestCarFail` 클래스를 생성하여 `Racing` 클래스 테스트

--- src/test/java/model/RacingTest.java
+++ src/test/java/model/RacingTest.java
@@ -3,43 +3,76 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Random;
+import java.util.LinkedHashMap;
 
-import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class RacingTest {
 
     @Test
     @DisplayName("자동차 이동 결과 확인")
     void moveAll() {
-        Racing racing = new Racing(List.of("pobi", "crong", "honux"), createRandom(4));
-        racing.moveAllCars();
 
-        assertThat(racing.toString()).isEqualTo("pobi : -\n"
-                + "crong : -\n"
-                + "honux : -\n");
+        LinkedHashMap<MovableStrategy, Integer> scoreBoard = new LinkedHashMap<>();
+        scoreBoard.put(new TestCarSuccess("TestTrue"), 0);
+        scoreBoard.put(new TestCarFail("TestFail"), 0);
+
+        Racing racing = new Racing(scoreBoard);
+        racing.moveAll();
+        racing.moveAll();
+
+        assertThat(racing.toString()).isEqualTo("TestTrue : --\n"
+                + "TestFail : \n");
     }
 
     @Test
     @DisplayName("경주 우승자 확인")
     void winner() {
-        Racing racing = new Racing(List.of("pobi", "crong", "honux"), createRandom(4));
-        racing.moveAllCars();
 
-        assertThat(racing.winner()
-                .stream()
-                .map(Car::toString)
-                .collect(toList()))
-                .contains("pobi", "crong", "honux");
+        LinkedHashMap<MovableStrategy, Integer> scoreBoard = new LinkedHashMap<>();
+        scoreBoard.put(new TestCarSuccess("TestTrue"), 0);
+        scoreBoard.put(new TestCarFail("TestFail"), 0);
+
+        Racing racing = new Racing(scoreBoard);
+        racing.moveAll();
+        racing.moveAll();
+
+        assertThat(racing.getWinner().toString()).isEqualTo("[TestTrue]");
     }
 
-    public RandomNumberGenerator createRandom(int returnValue) {
-        return new RandomNumberGenerator(new Random() {
-            public int nextInt(int bound) {
-                return returnValue;
-            }
-        });
+
+    public class TestCarSuccess implements MovableStrategy{
+        private final String name;
+
+        public TestCarSuccess(String name) {
+            this.name = name;
+        }
+        @Override
+        public boolean isMove() {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public class TestCarFail implements MovableStrategy{
+        private final String name;
+
+        public TestCarFail(String name) {
+            this.name = name;
+        }
+        @Override
+        public boolean isMove() {
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
