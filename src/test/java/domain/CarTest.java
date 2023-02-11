908e395d2b2c5905b3f3b2e6e60af3273dbda7c7
JunDonghyuk <ehdgur4814@naver.com>
2023-02-12 00:55:09 +0900

    test: 자동차 길이 유효성 검증 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,10 +1,13 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CarTest {
@@ -19,5 +22,14 @@ class CarTest {
         }
 
         assertThat(car.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hadddy", "edannnn", "abcdef"})
+    @DisplayName("자동차 이름은 5자가 넘으면 안된다.")
+    void carConstructTest(final String carName) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름의 길이가 5글자를 넘습니다.");
     }
 }
