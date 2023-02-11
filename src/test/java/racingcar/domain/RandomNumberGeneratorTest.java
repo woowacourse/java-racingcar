f61601fc02e772ccaf95a3476d2605cae05970fb
greeng00se <thegreengoose7@gmail.com>
2023-02-11 19:11:16 +0900

    test: RandomNumberGenerator 테스트 출력 메시지 가독성 개선

--- src/test/java/racingcar/domain/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/domain/RandomNumberGeneratorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.RepeatedTest;
 @DisplayName("RandomNumberGenerator 클래스")
 class RandomNumberGeneratorTest {
 
-    @RepeatedTest(10)
+    @RepeatedTest(value = 10, name = "{displayName} repetition: {currentRepetition} of {totalRepetitions}")
     @DisplayName("generate 메서드는 0 부터 9 사이의 숫자를 무작위로 생성한다.")
     void should_returnNumber_betweenZeroToNine_when_generate() {
         NumberGenerator numberGenerator = new RandomNumberGenerator();
