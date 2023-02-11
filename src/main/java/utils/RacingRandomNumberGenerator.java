3d94a9dc798f877f5c4ec7a41e44418c9a71ed4a
apptie <appti@naver.com>
2023-02-11 16:49:38 +0900

    refactor: RacingRandomNumberGenerator 상수 이름 변경

--- src/main/java/utils/RacingRandomNumberGenerator.java
+++ src/main/java/utils/RacingRandomNumberGenerator.java
@@ -2,11 +2,11 @@ package utils;
 
 public class RacingRandomNumberGenerator implements RacingNumberGenerator{
 
-    private static final int MIN_VALUE = 0;
-    private static final int MAX_VALUE = 9;
+    private static final int MIN_INCLUSIVE_VALUE = 0;
+    private static final int MAX_INCLUSIVE_VALUE = 9;
 
     @Override
     public int generate() {
-        return (int) (Math.random() * (MAX_VALUE - MIN_VALUE + 1));
+        return (int) (Math.random() * (MAX_INCLUSIVE_VALUE - MIN_INCLUSIVE_VALUE + 1));
     }
 }
