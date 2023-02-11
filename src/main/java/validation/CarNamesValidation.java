6b0f0c5eb2f49f802f49412b56d2ae9dc830dbeb
sosow0212 <sosow0212@naver.com>
2023-02-11 18:02:45 +0900

    refactor : 메서드 분리 및 리네이밍 진행

--- src/main/java/validation/CarNamesValidation.java
+++ src/main/java/validation/CarNamesValidation.java
@@ -7,30 +7,33 @@ import java.util.Set;
 public class CarNamesValidation {
 
     private static final int CAR_NAME_LENGTH_MAX = 5;
-    private static final int CAR_COUNT_MIN = 0;
     private static final String SPLIT_DELIMITER = ",";
 
     public static void validateInputCarNames(String input) {
         validateCountOfCars(input);
         validateNameOfCar(input);
-        checkDuplicatedCarName(input);
+        validateDuplicatedCarName(input);
     }
 
     private static void validateCountOfCars(String input) {
-        if (splitCarNames(input).length == CAR_COUNT_MIN || input.isEmpty()) {
+        if (input.isEmpty()) {
             throw new IllegalArgumentException("자동차를 한 대 이상 작성해주세요.");
         }
     }
 
     private static void validateNameOfCar(final String input) {
         for (final String carName : splitCarNames(input)) {
-            if (carName.length() > CAR_NAME_LENGTH_MAX) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하로 작성해주세요.");
-            }
+            checkLengthOfName(carName);
         }
     }
 
-    private static void checkDuplicatedCarName(final String input) {
+    private static void checkLengthOfName(String carName) {
+        if (carName.length() > CAR_NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 작성해주세요.");
+        }
+    }
+
+    private static void validateDuplicatedCarName(final String input) {
         Set<String> checkDuplicated = new HashSet<>(List.of(splitCarNames(input)));
 
         if (checkDuplicated.size() != splitCarNames(input).length) {
