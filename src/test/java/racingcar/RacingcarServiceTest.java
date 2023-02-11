64ac56a666232520aec4be3b088f5efd6ead25fc
Wugawuga <dnr3655@naver.com>
2023-02-12 01:49:23 +0900

    refactor: 서비스 테스트 수정

--- src/test/java/racingcar/RacingcarServiceTest.java
+++ src/test/java/racingcar/RacingcarServiceTest.java
@@ -4,11 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.service.RacingcarService;
-import racingcar.util.TextSplit;
 
 class RacingcarServiceTest {
 
@@ -19,10 +17,6 @@ class RacingcarServiceTest {
 
         List<String> winnersCarText = racingcarService.findWinners();
 
-        List<String> winners = winnersCarText.stream()
-                .map(text -> TextSplit.split(text, ",").get(0))
-                .collect(Collectors.toList());
-
-        assertThat(winners).containsExactly("car1", "car2", "car3");
+        assertThat(winnersCarText).containsExactly("car1,0", "car2,0", "car3,0");
     }
 }
