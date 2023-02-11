62c570a64e41678afb2ad2ef9813e72f32a0c149
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-11 23:03:56 +0900

    refactor : 자동차 이름의 유효성 검사 기능 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,20 +1,16 @@
 package racingcar.domain;
 
-import racingcar.constant.ErrorConstant;
 import racingcar.dto.CarStatus;
 
-public class Car {
-    private static final int MINIMUM_NUMBER_TO_MOVE = 4;
-    private static final int MAXIMUM_NAME_LENGTH = 5;
-    private static final int MINIMUM_NAME_LENGTH = 1;
+import static racingcar.constant.GameConstant.MINIMUM_NUMBER_TO_MOVE;
 
+public class Car {
     private final String name;
     private final Position currentPosition;
 
-    public Car(String name, int startPoint) {
-        validateName(name);
+    public Car(String name) {
         this.name = name;
-        this.currentPosition = new Position(startPoint);
+        this.currentPosition = new Position();
     }
 
     public String getName() {
@@ -43,16 +39,5 @@ public class Car {
 
     private boolean isMovable(int number) {
         return number >= MINIMUM_NUMBER_TO_MOVE;
-    }
-
-    private void validateName(String input) {
-        String name = removeBlank(input);
-        if (name.length() > MAXIMUM_NAME_LENGTH || name.length() < MINIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException(ErrorConstant.ERROR_PREFIX + "이름의 길이는 1이상 5이하여야 합니다.");
-        }
-    }
-
-    private String removeBlank(String input) {
-        return input.trim();
     }
 }
