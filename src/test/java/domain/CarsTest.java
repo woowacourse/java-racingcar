f3f759574140e6d89039369f746dc7da2df90941
dhkim <akdlzmf1123@gmail.com>
2023-02-11 13:15:17 +0900

    test: Cars의 핵심 기능 테스트 추가

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("자동차들의 ")
+@DisplayName("자동차들이 ")
 class CarsTest {
 
     @DisplayName("이름 길이가 1000만자 이상이면 예외 발생")
@@ -30,5 +33,44 @@ class CarsTest {
     @ValueSource(strings = {"abc,bd", "차이름,%!"})
     void lengthTest2(String input) {
         assertThatNoException().isThrownBy(()->Cars.from(input));
+    }
+
+    @DisplayName("현재의 위치를 반환한다.")
+    @Test
+    void getResultTest() {
+        Cars cars = Cars.from("abc,def,ghi");
+        Map<String, Integer> expected = new LinkedHashMap<>();
+        expected.put("abc", 0);
+        expected.put("def", 0);
+        expected.put("ghi", 0);
+        assertThat(cars.getResult()).isEqualTo(expected);
+    }
+
+    @DisplayName("4 ~ 9 사이의 값을 입력받으면 전진한다")
+    @Test
+    void moveTest() {
+        Cars cars = Cars.from("abc,def,ghi");
+        cars.move(new TestMovingPolicy());
+        Map<String, Integer> expected = new LinkedHashMap<>();
+        expected.put("abc", 1);
+        expected.put("def", 1);
+        expected.put("ghi", 0);
+        assertThat(cars.getResult()).isEqualTo(expected);
+    }
+
+    @DisplayName("가장 많이 전진하면 승자가 된다.")
+    @Test
+    void getWinnersTest() {
+        Cars cars = Cars.from("abc,def,ghi");
+        cars.move(new TestMovingPolicy());
+        assertThat(cars.getWinners()).containsExactly("abc", "def");
+    }
+
+    private class TestMovingPolicy implements MovingPolicy {
+        private int num = 5;
+        @Override
+        public int decide() {
+            return num--;
+        }
     }
 }
--- src/test/java/domain/MoveCountTest.java
+++ src/test/java/domain/MoveCountTest.java
@@ -8,22 +8,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
+@DisplayName("이동 횟수가 ")
 class MoveCountTest {
 
-    @ParameterizedTest(name = "횟수가 1 미만이거나 10억 초과면 예외 발생")
+    @ParameterizedTest(name = "1 미만이거나 10억 초과면 예외 발생")
     @ValueSource(strings = {"", "applej", "0", "1000000001"})
     void fail(String input) {
         assertThatThrownBy(() -> new MoveCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest(name = "횟수가 1 ~ 10억이면 정상 동작")
+    @ParameterizedTest(name = "1 ~ 10억이면 정상 동작")
     @ValueSource(strings = {"1", "1000000000"})
     void success(String input) {
         assertThatNoException().isThrownBy(() -> new MoveCount(input));
     }
 
-    @DisplayName("decrease는 count를 1 감소시킨다.")
+    @DisplayName("decrease하면 count를 1 감소시킨다.")
     @Test
     void decreaseTest() {
         MoveCount moveCount = new MoveCount("1");
@@ -31,7 +32,7 @@ class MoveCountTest {
         assertThat(moveCount.getCount()).isEqualTo(0);
     }
 
-    @ParameterizedTest(name = "moveCount는 값이 0일 때만 isOver이다.")
+    @ParameterizedTest(name = "값이 0일 때만 isOver이 true이다.")
     @CsvSource({"1,true", "2,false"})
     void isOverTest(String count, Boolean expected) {
         MoveCount moveCount = new MoveCount(count);
