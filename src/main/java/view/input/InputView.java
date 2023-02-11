0e03eb66344b13f5692dafd3b2fd53f6d65783b8
splitube <voicesplit@daum.net>
2023-02-11 15:14:24 +0900

    feat: InputView에 입력값이 있을 때까지 입력 받는 기능 구현

--- src/main/java/view/input/InputView.java
+++ src/main/java/view/input/InputView.java
@@ -7,8 +7,8 @@ import validation.ValidateResult;
 
 public class InputView {
 
-    private static String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static String ENTER_COUNT = "시도할 회수는 몇회인가요?";
+    private static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String ENTER_COUNT = "시도할 회수는 몇회인가요?";
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -16,17 +16,24 @@ public class InputView {
         return scanner.nextLine().strip();
     }
 
-    public String getCarNames() {
+    public void showEnterCarNameMessage() {
         System.out.println(ENTER_CAR_NAMES);
-        return getInput();
     }
 
-    public String getCount() {
+    public void showEnterCountMessage() {
         System.out.println(ENTER_COUNT);
-        return getInput();
     }
 
-    private String getInput() {
+    public String getInputUntilExist() {
+        try {
+            return getInput();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getInputUntilExist();
+        }
+    }
+
+    private String getInput() throws IllegalArgumentException {
         String input = readLine();
         validateEmpty(input);
         return input;
