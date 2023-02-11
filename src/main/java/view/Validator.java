b7252fd740067d52ac5097d430b23a6c8f57a034
yenawee <yaena0319@naver.com>
2023-02-11 21:27:56 +0900

    refactor(Validator) : 에러 메세지 상수화

--- src/main/java/view/Validator.java
+++ src/main/java/view/Validator.java
@@ -6,6 +6,11 @@ import java.util.Set;
 
 public class Validator {
     private static final String DIGIT = "\\d+";
+    private static final String TRYCOUNT_NONDIGIT_ERROR_MESSAGE = "[ERROR] : 숫자만 입력이 가능합니다.";
+    private static final String TRYCOUNT_RANGE_ERROR_MESSAGE = "[ERROR] : 1이상의 정수만 가능합니다.";
+    private static final String CARNAMES_BLANK_ERROR_MESSAGE = "[ERROR] : 빈 이름은 들어올 수 없습니다";
+    private static final String CARNAMES_LENGTH_ERROR_MESSAGE = "[ERROR] : 이름 길이는 1~5자 이하여야 합니다";
+    private static final String CARNAMES_DUPLICATE_ERROR_MESSAGE = "[ERROR] : 자동차 이름이 중복되었습니다";
 
     public void checkValidCarNames(String[] carNames) {
         checkBlank(carNames);
@@ -15,19 +20,19 @@ public class Validator {
 
     public void checkDigits(String input) {
         if (!input.matches(DIGIT)) {
-            throw new IllegalArgumentException("[ERROR] : 숫자만 입력이 가능합니다.");
+            throw new IllegalArgumentException(TRYCOUNT_NONDIGIT_ERROR_MESSAGE);
         }
     }
 
     public void checkRange(int tryCount) {
         if (tryCount < 1) {
-            throw new IllegalArgumentException("[ERROR] : 1이상의 정수만 가능합니다.");
+            throw new IllegalArgumentException(TRYCOUNT_RANGE_ERROR_MESSAGE);
         }
     }
 
     private void checkBlank(String[] carNames) {
         if (carNames.length == 0) {
-            throw new IllegalArgumentException("[ERROR] : 빈 이름은 들어올 수 없습니다");
+            throw new IllegalArgumentException(CARNAMES_BLANK_ERROR_MESSAGE);
         }
     }
 
@@ -36,14 +41,14 @@ public class Validator {
                 .filter(name -> name.isEmpty() || name.length() > 5)
                 .count();
         if (invalidCount != 0) {
-            throw new IllegalArgumentException("[ERROR] : 이름 길이는 1~5자 이하여야 합니다");
+            throw new IllegalArgumentException(CARNAMES_LENGTH_ERROR_MESSAGE);
         }
     }
 
     private void checkDouble(String[] carNames) {
         Set<String> carNamesSet = new HashSet<>(Arrays.asList(carNames));
         if (carNamesSet.size() != carNames.length) {
-            throw new IllegalArgumentException("[ERROR] : 자동차 이름이 중복되었습니다");
+            throw new IllegalArgumentException(CARNAMES_DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
