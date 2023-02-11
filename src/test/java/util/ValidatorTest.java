761e4c1f86c60cfbf571970049b26c7903c8afc3
yujamint <woawoa0513@gmail.com>
2023-02-12 00:47:30 +0900

    test: Validator 테스트 케이스 분류
    각각 다른 케이스를 한 메소드에서 테스트했는데, 이를 각각의 메소드로 분류

--- src/test/java/util/ValidatorTest.java
+++ src/test/java/util/ValidatorTest.java
@@ -1,17 +1,24 @@
 package util;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
+    @Test
+    @DisplayName("1보다 작은 숫자를 입력하면 예외가 발생한다..")
+    void movingCountInputLessThanMinValue() {
+        String input = "0";
 
-    @ParameterizedTest
-    @ValueSource(strings = {"a","0"})
-    void validateMovingCountInput(String input) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Validator.validateMovingCountInput(input));
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 입력이 들어오면 예외가 발생한다.")
+    void movingCountInputNonDigit() {
+        String input = "a";
+
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> Validator.validateMovingCountInput(input));
     }
