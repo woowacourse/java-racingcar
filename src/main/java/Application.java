98f262bdddde28b29d30762fb5417db047b239d2
yenawee <yaena0319@naver.com>
2023-02-12 23:41:25 +0900

    refactor(RacingGame, Application) : inputview 를 RacingGame 클래스에서 분리하여 main에서 입력을 받는다

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,14 @@
 import domain.RacingGame;
+import view.Input;
+import view.Output;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
-        racingGame.initGame();
+        Output.printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        final String[] carNames = Input.getCarNames(Input.getInput());
+        Output.printMessage("시도할 회수는 몇회인가요?");
+        final int tryCount = Input.getTryCount(Input.getInput());
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
         racingGame.start();
         racingGame.printResult();
     }
--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,9 +1,7 @@
 package domain;
 
-import view.Input;
 import view.Output;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
@@ -12,18 +10,11 @@ public class RacingGame {
     private List<Car> cars;
     private int tryCount;
 
-    public RacingGame() {
+    public RacingGame(String[] carNames, int tryCount) {
         carGenerator = new CarGenerator();
         winner = new Winner();
-        cars = new ArrayList<>();
-    }
-
-    public void initGame() {
-        Output.printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] carNames = Input.getCarNames(Input.getInput());
         cars = carGenerator.generateCars(carNames);
-        Output.printMessage("시도할 회수는 몇회인가요?");
-        this.tryCount = Input.getTryCount(Input.getInput());
+        this.tryCount = tryCount;
     }
 
     public void start() {
