1b2d3c5846a6530142d3f45b073829a6c895cec4
hyunseo <goatsfish@gmail.com>
2023-02-12 23:02:22 +0900

    test : 랜덤 번호 생성 테스트

--- src/test/java/racingcar/domain/number/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/domain/number/RandomNumberGeneratorTest.java
@@ -1,14 +1,15 @@
-package racingcar.domain;
+package racingcar.domain.number;
 
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
+@DisplayName("랜덤 넘버 ")
 class RandomNumberGeneratorTest {
 	final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
-	@DisplayName("랜덤 넘버 생성 성공 테스트")
+	@DisplayName("숫자 생성 성공 테스트")
 	@RepeatedTest(10)
 	void generateRandomNumberTest() {
 		int generatedRandomNumber = numberGenerator.generateNumber();
