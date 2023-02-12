98b2cca7ea70fcf2b4d1ac4effb57de98ff39763
Choi-JJunho <junho5336@gmail.com>
2023-02-12 10:53:50 +0900

    refactor: rename method

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static List<String> inputNames() {
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = SCANNER.nextLine();
         return Arrays.stream(input.split(","))
