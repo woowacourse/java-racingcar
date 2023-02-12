ff2d25cf35e9d1b764a7fd183a3b8bc6e1f6ee39
echo <echo.backend@gmail.com>
2023-02-12 21:21:36 +0900

    fix(InputView): make input private

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner input = new Scanner(System.in);
+    private Scanner input = new Scanner(System.in);
     private final String NAME_LIST_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private final String TRIAL_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
 
