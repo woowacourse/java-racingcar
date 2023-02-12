94d984421871a1464d7904cfe19e1eef201b48e8
chaewon121 <cw121@naver.com>
2023-02-13 02:18:02 +0900

    refactor: 랜덤숫자 다시 구현

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,10 +1,10 @@
 package racingcar.domain;
 
-public class NumberGenerator {
+public class RandomNumberGenerator {
 
     private static final int ONE = 1;
 
-    public int generate(final int minNumber, final int maxNumber) {
+    public static int generateNumber(final int minNumber, final int maxNumber) {
         return (int) ((Math.random() * (maxNumber - minNumber + ONE)) + minNumber);
     }
 }
