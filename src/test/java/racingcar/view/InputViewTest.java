ae6f42f262069caf279b7cdb019494b306f97a72
MoonJeWoong <munjin0201@naver.com>
2023-02-13 00:05:43 +0900

    refactor(test) : 테스트 함수 목적에 맞지 않는 케이스 제거
    세부사항
    - 0은 입력 값 형식 검증이 아니라 도메인 로직에 해당하는 범위에 대한 검증
      테스트로 이루어져야 한다.

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"r", "ㄱ", ",", "0", "\n"})
+    @ValueSource(strings = {"r", "ㄱ", ",", "\n"})
     @DisplayName("시도 횟수 사용자 입력 값이 올바른 지 검증하는 테스트")
     void validateRoundTest(String input) {
         //Given
@@ -28,5 +29,4 @@ class InputViewTest {
         //Then
         assertThat(result).isInstanceOf(IllegalArgumentException.class);
     }
-    
 }
