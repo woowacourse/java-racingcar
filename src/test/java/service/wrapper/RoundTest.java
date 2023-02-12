633152254ec143177b66b073890caaa5ff477b32
apptie <appti@naver.com>
2023-02-12 13:12:21 +0900

    test: Round 로직 변경으로 인한 테스트 수정

--- src/test/java/service/wrapper/RoundTest.java
+++ src/test/java/service/wrapper/RoundTest.java
@@ -1,5 +1,31 @@
 package service.wrapper;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class RoundTest {
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("1 이상이면 true를 반환한다.")
+    void continueRace(int input) {
+        Round round = new Round(input);
+
+        boolean result = round.isRacing();
+
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("0 이라면 false를 반환한다.")
+    void exitRace() {
+        Round round = new Round(0);
+
+        boolean result = round.isRacing();
+
+        Assertions.assertThat(result).isFalse();
+    }
 }
