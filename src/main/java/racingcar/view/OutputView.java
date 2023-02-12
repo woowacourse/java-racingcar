51f16282c5a9c09785e027b975b932e3843ea38b
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-12 17:39:02 +0900

    refactor: 생성자 접근 제어자 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,11 @@
 package racingcar.view;
 
 public class OutputView {
-
     private static OutputView outputView;
+
+    private OutputView() {
+    }
+
     public static OutputView getInstance() {
         if (outputView == null) {
             outputView = new OutputView();
