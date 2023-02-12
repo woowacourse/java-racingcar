22b4a1cad2f50575fb22f7d401ccb3c90216776d
watterricecake <fruturum@nate.com>
2023-02-12 11:48:11 +0900

    refactor(outputView): 출력 테스트 assertThat 적용

--- src/test/java/car/view/OutputViewTest.java
+++ src/test/java/car/view/OutputViewTest.java
@@ -6,12 +6,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class outputViewTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class OutputViewTest  extends OutputTestStream {
     private final OutputView outputView = new OutputView();
+
     @Test
     @DisplayName("")
     void 자동차를_출력하는_테스트(){
         outputView.printCars(List.of(new Car("포비", 5)));
+        assertThat(output()).contains("포비 : -----");
     }
 
     @Test
@@ -19,5 +23,6 @@ public class outputViewTest {
     void 우승자를_출력하는_테스트(){
         List<Car> winners = List.of(new Car("포비"), new Car("네오"));
         outputView.printWinners(winners);
+        assertThat(output()).contains(String.format("포비, 네오가 최종 우승했습니다."));
     }
 }
