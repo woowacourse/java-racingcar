da0d9437c9a678e85fca66023d83468e06756449
seokhwan-an <asok369258@naver.com>
2023-02-13 01:24:53 +0900

    refactor(domain) : 라운드 클래스 추가
    세부새항
    - 라운드는 게임의 시도횟수를 저장하고 관리한다.
    - 시도 횟수는 숫자여야 하며 1보다 같거나 커야한다.

---
+++ src/main/java/racingcar/domain/Round.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Round {
+
+    private static final Pattern pattern = Pattern.compile("^[0-9]*$");
+    private static final int TRIAL_COUNT_MIN_NUMBER = 1;
+    private static final String TRIAL_COUNT_INPUT_ERROR = "[ERROR] 시도 횟수는 숫자이어야 합니다.";
+    private static final String TRIAL_COUNT_NUMBER_ERROR = "[ERROR] 시도 횟수는 1이상이어야 합니다.";
+
+    private int trialCount;
+
+    public Round(String trialCount) {
+        validateTrialCount(trialCount);
+        this.trialCount = Integer.parseInt(trialCount);
+    }
+
+    public boolean isRacing() {
+        //TODO : 게임시도가 가능한지 체크한다.
+    }
+
+    public void minusTrialCount() {
+        //TODO : 시도횟수를 하나씩 감소시킨다.
+    }
+
+    private void validateTrialCount(String trialCount) {
+        Matcher matcher = pattern.matcher(trialCount);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(TRIAL_COUNT_INPUT_ERROR);
+        }
+        if (Integer.parseInt(trialCount) < TRIAL_COUNT_MIN_NUMBER) {
+            throw new IllegalArgumentException(TRIAL_COUNT_NUMBER_ERROR);
+        }
+    }
+
+}
