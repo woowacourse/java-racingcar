bfd2eade1013874b9211f563707b267f2d295646
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-13 06:21:58 +0900

    feat : OutputView 에러 메시지 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,13 +10,14 @@ public class OutputView {
     private static final String MOVING_SYMBOL = "-";
     private static final String WINNER_DELIMITER = ", ";
     private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
+    private static final String ERROR_PREFIX = "[ERROR] ";
 
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
     }
 
-    public static void printCarStatus(List<CarStatus> carStatuses) {
-        carStatuses.forEach(carStatus -> {
+    public static void printRaceResult(List<CarStatus> raceResult) {
+        raceResult.forEach(carStatus -> {
             int moveCount = carStatus.getCurrentPosition();
             System.out.println(carStatus.getName() + DELIMITER + MOVING_SYMBOL.repeat(moveCount));
         });
@@ -26,5 +27,9 @@ public class OutputView {
     public static void printFinalResult(List<String> winnersName) {
         String winnersResult = String.join(WINNER_DELIMITER, winnersName);
         System.out.println(winnersResult + WINNER_MESSAGE);
+    }
+
+    public static void printError(String errorMessage) {
+        System.out.println(ERROR_PREFIX + errorMessage);
     }
 }
