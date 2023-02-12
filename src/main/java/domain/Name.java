fe1ee1f0db71b9c7dbc56e458094f3ef2fa844c4
kokodak <kokodakadokok@gmail.com>
2023-02-12 15:17:46 +0900

    refactor: 이름 검증에 대한 책임을 Car에서 Name으로 위임

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,10 +2,23 @@ package domain;
 
 public class Name {
 
+    private static final String NO_NAME_EXISTS_MESSAGE = "[ERROR] 자동차의 이름은 반드시 있어야 합니다.";
+    private static final String INVALID_LENGTH_MESSAGE = "[ERROR] 자동차의 이름은 5글자까지 가능합니다.";
+
     private final String name;
 
     public Name(final String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(final String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(NO_NAME_EXISTS_MESSAGE);
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
+        }
     }
 
     public String getName() {
