7745e187de906d99325da01b64bfa63af5e92e58
junpakPark <junpak.park@gmail.com>
2023-02-12 22:13:09 +0900

    refactor : Race 객체 생성자 인자값 Cars 객체로 변경 및 numberGenerator 위치 변경

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import racingcar.domain.vo.CarStatus;
 import racingcar.util.NumberGenerator;
-import racingcar.util.RaceNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,39 +16,37 @@ public class Race {
 
     private int tryCount;
     private final Cars cars;
-    private final NumberGenerator numberGenerator;
 
-    private Race(List<String> carNames, int tryCount) {
+    private Race(Cars cars, int tryCount) {
         validateRange(tryCount);
         this.tryCount = tryCount;
-        this.cars = Cars.of(carNames);
-        this.numberGenerator = new RaceNumberGenerator();
+        this.cars = cars;
     }
 
-    public static Race of(List<String> carNames, int tryCount) {
-        return new Race(carNames, tryCount);
+    public static Race of(Cars cars, int tryCount) {
+        return new Race(cars, tryCount);
     }
 
-    public List<CarStatus> setUpGame() {
-        return cars.getRoundResults();
-    }
-
-    public List<CarStatus> playRound() {
-        cars.move(setUpRound());
+    public List<CarStatus> playRound(NumberGenerator numberGenerator) {
+        cars.move(numberGenerator);
         return cars.getRoundResults();
     }
 
     public List<String> findWinnersName() {
         return cars.pickWinners().stream()
-                .map(car -> car.getCarStatus().getName())
+                .map(Car::getName)
                 .collect(Collectors.toList());
+    }
+
+    public List<CarStatus> getRoundResults() {
+        return cars.getRoundResults();
     }
 
     public int getTryCount() {
         return tryCount;
     }
 
-    private List<Integer> setUpRound() {
+    private List<Integer> setUpRound(NumberGenerator numberGenerator) {
         List<Integer> moveConditionValues = new ArrayList<>();
         for (int i = 0; i < cars.getNumberOfCars(); i++) {
             moveConditionValues.add(numberGenerator.generate());
