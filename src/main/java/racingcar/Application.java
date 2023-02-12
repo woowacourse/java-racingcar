5a6949fd6286a75cb196e47f5d26cafc6f452989
Woojin Lee <dhkep03@gmail.com>
2023-02-13 02:03:02 +0900

    chore: 사용하지 않는 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,6 @@
 package racingcar;
 
-import java.util.InputMismatchException;
-import java.util.List;
 import racingcar.controller.RacingController;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
 
