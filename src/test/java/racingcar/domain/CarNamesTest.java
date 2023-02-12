60809da108f877a6f9edbf40f9b9bacbd393fc46
Devtist <yt930310@gmail.com>
2023-02-13 00:48:19 +0900

    test: 자동차 이름 입력이 공백인 경우에 대한 테스트코드 작성

--- src/test/java/racingcar/domain/CarNamesTest.java
+++ src/test/java/racingcar/domain/CarNamesTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,5 +14,11 @@ class CarNamesTest {
     @ValueSource(strings = {"test1,test2,test3", "a,a,a"})
     void carNamesTest(String string) {
         assertThat(new CarNames(new CarNamesRequestDTO(string)).getNames()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 입력이 공백인 경우 테스트")
+    void carNamesEmptyInputTest() {
+        assertThatThrownBy(() -> new CarNames(new CarNamesRequestDTO(""))).isInstanceOf(IllegalArgumentException.class);
     }
 }
