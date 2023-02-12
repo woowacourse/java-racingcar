d2b57d620cc05a6fc4c7ccf9c9350f96036024c3
chaewon121 <cw121@naver.com>
2023-02-13 02:45:12 +0900

    refactor: 자동차 리스트 생성하는 기능 구현 및 예외처리 기능 재구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,49 +1,52 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static racingcar.domain.RandomNumberGenerator.generateNumber;
 
 public class Cars {
+    private List<Car> cars = new ArrayList<>();
 
-    private static final int FIRST_INDEX = 0;
-    private static final int MIN_NUMBER = 0;
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_SIZE = 2;
-    private final List<Car> cars;
-
-    public Cars(final List<String> cars) {
-        if (cars.size() < MIN_SIZE) {
-            throw new IllegalArgumentException();
+    public Cars(String carNames) {
+        String[] splitCarNames = stripStringArray(carNames);
+        validation(splitCarNames);
+        // TODO: 스트림으로 바꾸기
+        for (String name : splitCarNames) {
+            cars.add(new Car(name));
         }
-        this.cars = cars.stream()
-                .map(Car::new)
-                .collect(Collectors.toUnmodifiableList());
     }
 
-    public void move(final NumberGenerator generator) {
-        cars.forEach(car -> car.tryMove(generator.generate(MIN_NUMBER, MAX_NUMBER)));
+    public String[] stripStringArray(String names) {
+        return names.split(",");
+    }
+
+    public void moveCars() {
+        for (Car car : this.cars) {
+            car.tryMove(generateNumber(0, 9));
+        }
     }
 
     public List<Car> getCars() {
         return this.cars;
     }
 
-    public List<Car> winners() {
-        List<Car> cars = sortedCars();
-        Car winner = cars.get(FIRST_INDEX);
-
-        return cars.stream()
-                .filter(it -> it.getPosition().equals(winner.getPosition()))
-                .collect(Collectors.toList());
+    private void validation(String[] names) {
+        if (isDuplication(names)) {
+            throw new IllegalArgumentException("자동차 이름은 중복이 불가능 합니다.");
+        }
+        if (isNotInRange(names)) {
+            throw new IllegalArgumentException("자동차는 두개 이상 입력해야 합니다.");
+        }
     }
 
-    private List<Car> sortedCars() {
-        List<Car> cars = new ArrayList<>(this.cars);
-        cars.sort(Comparator.comparing(Car::getPosition));
-        Collections.reverse(cars);
-        return cars;
+    private boolean isDuplication(String[] names) {
+        return Arrays.stream(names).distinct().count() != names.length;
     }
+
+    private boolean isNotInRange(String[] names) {
+        return names.length < 2;
+    }
+
 }
