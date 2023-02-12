920dd0c05a9ab81835c1a7bd99417db477784b88
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-12 15:55:59 +0900

    feat(GameTrial): RacingGame 내에서 호출될 메서드 정의
    - isGreaterThan 메서드 생성: 게임 종료 판단을 위한 메서드
    - reduce 메서드 생성: 게임 진행 시 시도 횟수 차감을 위한 메서드

--- src/main/java/domain/GameTrial.java
+++ src/main/java/domain/GameTrial.java
@@ -7,4 +7,12 @@ public class GameTrial {
     public GameTrial(int gameTrial) {
         this.gameTrial = gameTrial;
     }
+
+    public boolean isGreaterThan(int otherCount) {
+        return gameTrial > otherCount;
+    }
+
+    public void reduce() {
+        gameTrial--;
+    }
 }
