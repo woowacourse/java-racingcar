f90b488643c5f34c48d89164b23e7591ac6da57f
kim0914 <kosa0914@naver.com>
2023-02-12 16:16:40 +0900

    feat(Car): 자동차 이름 검증 메서드 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,34 @@
 package domain;
 
+import validation.ErrorMessage;
+
 public class Car {
 
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
     private final static int MIN_MOVE_NUMBER = 4;
 
     private final String name;
     private int moveCount;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
         this.moveCount = 0;
+    }
+
+    private void validate(String name) {
+        if (isEmptyOrBlank(name) || isWrongSizeName(name)) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_CAR_NAME.getMessage());
+        }
+    }
+
+    private boolean isEmptyOrBlank(String name) {
+        return name.isEmpty() || name.isBlank();
+    }
+
+    private boolean isWrongSizeName(String name) {
+        return name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH;
     }
 
     public void move() {
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,12 +1,24 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import validation.ErrorMessage;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "gray1234"})
+    @DisplayName("1 ~ 5 크기의 이름이 아닌 경우 예외가 발생한다.")
+    public void carCreateFail(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.WRONG_CAR_NAME.getMessage());
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 6, 8, 9})
