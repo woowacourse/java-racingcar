188b708521f9091ca95551c461c44be1468e1fa4
BGuga <tmdwns9913@gmail.com>
2023-02-13 00:42:33 +0900

    refactor: make to handle error situation when user insert non-numberic tryCount Value

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class InputView {
     private Scanner input = new Scanner(System.in);
     private OutputView output = new OutputView();
+    private static final String NON_NUMBER_TRIAL_COUNT_INPUT_ERROR_MESSAGE = "숫자가 아닌 값은 시도 횟수로 입력할 수 없습니다";
     private static final String NAME_LIST_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRIAL_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
 
@@ -23,8 +24,13 @@ public class InputView {
     }
 
     public int getTrialCount() {
-        String inputCount = readLineWithMessage(TRIAL_COUNT_PROMPT);
-        output.printBlankLine();
-        return Integer.parseInt(inputCount);
+        try{
+            String inputCount = readLineWithMessage(TRIAL_COUNT_PROMPT);
+            output.printBlankLine();
+            return Integer.parseInt(inputCount);
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException(NON_NUMBER_TRIAL_COUNT_INPUT_ERROR_MESSAGE);
+        }
     }
 }
