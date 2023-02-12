af88199986d58687bb0566a98d4204b4ce2102bb
amaran-th <songsy405@gmail.com>
2023-02-12 18:20:08 +0900

    refactor : 컨벤션 맞추기
    - 파일 끝 개행문자 추가
    
    (cherry picked from commit ed145cc10315e3985f95e8b14f0a932330543dd2)
    (cherry picked from commit a9283ec296cd61d5f7c69fced2a6dc6e5db69aad)

--- src/main/java/strategy/RandomPowerSupplier.java
+++ src/main/java/strategy/RandomPowerSupplier.java
@@ -6,7 +6,7 @@ public class RandomPowerSupplier implements PowerSupplier {
 
   private static final Random random = new Random();
   private static final int MAX_POWER = 10;
-  
+
   @Override
   public int supply() {
     return random.nextInt(MAX_POWER);
