4fdbe4afb895e345938abf04d358139b0ea4ff5d
hgo641 <hgo641@gmail.com>
2023-02-12 15:06:28 +0900

    feat: Range 유효성 체크 로직 추가

--- src/main/java/racingcar/domain/Range.java
+++ src/main/java/racingcar/domain/Range.java
@@ -5,8 +5,15 @@ public class Range {
     private final int max;
 
     public Range(int min, int max) {
+        validateRange(min, max);
         this.min = min;
         this.max = max;
+    }
+
+    private void validateRange(int min, int max){
+        if(min >= max){
+            throw new IllegalArgumentException("Range의 max값은 min값보다 커야합니다.");
+        }
     }
 
     public Boolean isInRange(int number) {
