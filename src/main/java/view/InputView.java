40b2a3b3afdbc1ae9a8012940275635ce88b5d4a
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-12 20:47:56 +0900

    refactor: 메서드 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,17 +7,28 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String[] getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = scanner.nextLine();
-        String[] carNames = input.split(",");
-        Validator.checkDuplication(carNames);
-        Validator.checkEmpty(carNames);
-        return carNames;
+        String input = getInput("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return splitNames(input);
     }
 
     public static int getTryCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        String input = scanner.nextLine();
+        String input = getInput("시도할 횟수는 몇회인가요?");
+        return parseTryCount(input);
+    }
+
+    private static String getInput(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    private static String[] splitNames(String input) {
+        String[] names = input.split(",");
+        Validator.checkDuplication(names);
+        Validator.checkEmpty(names);
+        return names;
+    }
+
+    private static int parseTryCount(String input) {
         Validator.checkDigits(input);
         int tryCount = Integer.parseInt(input);
         Validator.checkRange(tryCount);
