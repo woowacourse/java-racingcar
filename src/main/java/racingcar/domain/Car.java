e1596194ba11410cc5e1b1776877ec179c45086c
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-13 07:30:30 +0900

    refactor : rebase

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -33,18 +33,6 @@ public class Car {
         return currentPosition.equals(target.currentPosition);
     }
 
-    public void validateName(String input) {
-        if (input.length() > 5 || input.length() <1) {
-            throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
-        }
-    }
-
-    public void validateName(String input) {
-        if (input.length() > 5 || input.length() <1) {
-            throw new IllegalArgumentException("이름의 길이는 1이상 5이하여야 합니다.");
-        }
-    }
-
     private boolean isMovable(int number) {
         return number >= MINIMUM_NUMBER_TO_MOVE;
     }
