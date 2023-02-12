b69804730007fba61b5d0700ffc53acede1315e6
jyeost <pig9619@naver.com>
2023-02-12 21:35:28 +0900

    refactor : inputView가 trial의 책임을 대신하면서 TrialTest에 있던 입력값 검증 테스트들을 InputViewTest로 이동시켰다.

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -11,11 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("사용자의 입력값은")
 class InputViewTest extends IOTest {
 
     @Test
-    @DisplayName("정상적일때 반복문을 돌지않고, 오류도 발생하지 않는다")
+    @DisplayName("차의 이름들이 정상적으로 입력되었을때 반복문을 돌지않고, 오류도 발생하지 않는다")
     void checkSuccess() {
         assertThatCode(() -> {
             systemIn("aa,bb,cc");
@@ -25,7 +24,7 @@ class InputViewTest extends IOTest {
     }
 
     @ParameterizedTest
-    @DisplayName("빈 값이나 공백이 들어왔을때 반복문을 돈다")
+    @DisplayName("차의 이름들에 대해 빈 값이나 공백이 들어왔을때 반복문을 돈다")
     @ValueSource(strings = {"", " "})
     void checkBlank(String input) {
         assertThatThrownBy(() -> {
@@ -36,7 +35,7 @@ class InputViewTest extends IOTest {
     }
 
     @ParameterizedTest
-    @DisplayName("이름 규칙에 맞지않는 값이 들어왔을때 반복문을 돈다")
+    @DisplayName("차의 이름들이 이름 규칙에 맞지않는 값이 들어왔을때 반복문을 돈다")
     @ValueSource(strings = {"aa,bb,ccdddd", "aa, ,bb", "aa,,bb"})
     void carNameLength(String input) {
         assertThatThrownBy(() -> {
@@ -47,7 +46,7 @@ class InputViewTest extends IOTest {
     }
 
     @Test
-    @DisplayName("한 대의 차만 입력되었을때 반복문을 돈다")
+    @DisplayName("한 대의 차의 이름만 입력되었을때 반복문을 돈다")
     void checkCommaExist() {
         assertThatThrownBy(() -> {
             systemIn("aa");
@@ -57,7 +56,7 @@ class InputViewTest extends IOTest {
     }
 
     @Test
-    @DisplayName("중복된 차 이름 일때 반복문을 돈다")
+    @DisplayName("중복된 차 이름 입력 됐을때 반복문을 돈다")
     void checkDuplication() {
         assertThatThrownBy(() -> {
             systemIn("aa,bb,bb");
@@ -67,7 +66,7 @@ class InputViewTest extends IOTest {
     }
 
     @Test
-    @DisplayName(" 차 이름 앞 뒤의 공백이 있을 때 지워진다")
+    @DisplayName("차 이름 앞 뒤의 공백이 있을 때 지워진다")
     void checkTrim() {
         systemIn(" aa ,bb ");
         InputView inputView = new InputView(new Scanner(System.in));
@@ -75,12 +74,43 @@ class InputViewTest extends IOTest {
         assertThat(list.containsAll(List.of("aa", "bb"))).isTrue();
     }
 
-    @DisplayName(" 차 이름이 길이에 맞지 않거나, 공백일때 반복문을 돈다")
+    @DisplayName("차 이름이 길이에 맞지 않거나, 공백일때 반복문을 돈다")
     @ParameterizedTest
     @ValueSource(strings = {"asdafg", "qwerfds", "", "    "})
     void checkLength(String input) {
         assertThatThrownBy(() -> {
             systemIn(input);
+            InputView inputView = new InputView(new Scanner(System.in));
+            inputView.askCarNames();
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("시도 횟수에, 숫자가 아닌 값들이 들어오면 오류가 발생하는지 확인한다")
+    @ValueSource(strings = {"3ge", "~45", "2!9", "-98", "78.4"})
+    void checkChar(String input) {
+        assertThatThrownBy(() -> {
+            systemIn(input);
+            InputView inputView = new InputView(new Scanner(System.in));
+            inputView.askCarNames();
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 0이 들어왔을때 오류가 발생하는지 알아본다")
+    void checkZero() {
+        assertThatThrownBy(() -> {
+            systemIn("0");
+            InputView inputView = new InputView(new Scanner(System.in));
+            inputView.askCarNames();
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    @DisplayName("시도횟수의 입력값이 int 범위를 넘어가면 오류가 발생하는지 알아본다")
+    void checkExtreme() {
+        assertThatThrownBy(() -> {
+            systemIn("496574567843567");
             InputView inputView = new InputView(new Scanner(System.in));
             inputView.askCarNames();
         }).isInstanceOf(NoSuchElementException.class);
