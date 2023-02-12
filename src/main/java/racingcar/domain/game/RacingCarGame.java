57914909c02eb97dc9a0ff569a447f7656cbfb06
shin-mallang <huipulci1@naver.com>
2023-02-12 20:58:36 +0900

    chore: 필요없는 주석 제거

--- src/main/java/racingcar/domain/game/RacingCarGame.java
+++ src/main/java/racingcar/domain/game/RacingCarGame.java
@@ -33,11 +33,6 @@ public class RacingCarGame {
         return cars;
     }
 
-    /**
-     * 조금 더 고민해 본 결과,Cars의 내부 상태를 숨기기 위해(getter 를 public 으로 공개하지 않도록)
-     * Cars에서 반환하는 것이 더 좋겠다 판단하였어요!
-     * 이에 대한 카일의 의견이 궁금해요!
-     */
     public void race() {
         if (!hasMoreLap()) {
             throw new IllegalStateException("이미 모든 바퀴를 진행했습니다.");
