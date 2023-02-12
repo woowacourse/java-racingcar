2ae1514f87ba35f42248b67d4fb984a478d6be8c
yujamint <woawoa0513@gmail.com>
2023-02-13 02:46:38 +0900

    test: move() 매개변수 수정에 따른 테스트 코드 수정
    @DisplayName 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,28 +1,30 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("자동차는 ")
 class CarTest {
 
     @Test
+    @DisplayName("4 이상의 숫자가 나오면 전진한다.")
     void moveSuccessTest() {
         Car car = new Car("name");
         Integer nowDistance = car.getDistance();
 
-        car.move(true);
+        car.move(new MovementManager(), 4);
 
         Assertions.assertThat(car.getDistance()).isEqualTo(nowDistance + 1);
     }
 
     @Test
+    @DisplayName("3 이하의 숫자가 나오면 정지한다.")
     void moveFailTest() {
         Car car = new Car("name");
         Integer nowDistance = car.getDistance();
 
-        car.move(false);
+        car.move(new MovementManager(), 3);
 
         Assertions.assertThat(car.getDistance()).isEqualTo(nowDistance);
     }
