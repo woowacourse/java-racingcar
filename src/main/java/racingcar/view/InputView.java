718fd24691ff5dc005039c7410f69c9fbbab84b0
hgo641 <hgo641@gmail.com>
2023-02-12 14:56:46 +0900

    refactor: InputView 인스턴스 변수 scanner 생성 및 입력방식 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class InputView {
     private final static String READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private final static String READ_TRY_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
+    private final Scanner scanner = new Scanner(System.in);
 
     public String readCarName() {
         System.out.println(READ_CAR_NAME_MESSAGE);
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public String readTryCount() {
         System.out.println(READ_TRY_COUNT_MESSAGE);
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 }
