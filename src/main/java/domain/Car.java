adee93c44ac057bb223803a89c038ea99e8c709a
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-12 20:36:29 +0900

    style: toString 제거

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -14,11 +14,6 @@ public class Car {
         this.position = 0;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s  : %s", name, "-".repeat(position));
-    }
-
     public void move(int power) {
         if (power >= MIN_MOVE_POWER) {
             this.position++;
