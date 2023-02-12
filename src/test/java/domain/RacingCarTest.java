c878cf3f9fb98d9503baa601b8b0df0ddb03c8f4
seokho <kim76744dev@gmail.com>
2023-02-13 08:13:40 +0900

    test: TestCarMaker를 class로 변경

--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static domain.TestCarMaker.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class RacingCarTest extends TestCarMaker {
+class RacingCarTest {
 
     @ParameterizedTest
     @DisplayName("4이상이면 움직이고 3 이하이면 움직이지 않는다.")
--- src/test/java/domain/TestCarMaker.java
+++ src/test/java/domain/TestCarMaker.java
@@ -1,11 +1,11 @@
 package domain;
 
-public abstract class TestCarMaker {
+public class TestCarMaker {
 
     private static final int MOVE_LOWER_LIMIT = 4;
     private static final int NOT_MOVE_HIGHER_LIMIT = 3;
 
-    public RacingCar makeCarByValue(final String carName, final int value) {
+    public static RacingCar makeCarByValue(final String carName, final int value) {
         if (value >= MOVE_LOWER_LIMIT) {
             return createMoveRacingCar(carName);
         }
@@ -13,22 +13,22 @@ public abstract class TestCarMaker {
         return createNotMoveRacingCar(carName);
     }
 
-    public RacingCar createMoveRacingCar(final String carName) {
+    public static RacingCar createMoveRacingCar(final String carName) {
         return new RacingCar(carName, new MoveCarPicker());
     }
 
-    public RacingCar createNotMoveRacingCar(final String carName) {
+    public static RacingCar createNotMoveRacingCar(final String carName) {
         return new RacingCar(carName, new NotMoveCarPicker());
     }
 
-    private class MoveCarPicker implements Pickable {
+    private static class MoveCarPicker implements Pickable {
         @Override
         public int pickNumber() {
             return MOVE_LOWER_LIMIT;
         }
     }
 
-    private class NotMoveCarPicker implements Pickable {
+    private static class NotMoveCarPicker implements Pickable {
         @Override
         public int pickNumber() {
             return NOT_MOVE_HIGHER_LIMIT;
