28e9cd006042557725f0eea8139d271fbf660ff0
kokodak <kokodakadokok@gmail.com>
2023-02-12 15:16:23 +0900

    feat: 이름에 대한 원시값을 포장할 Name 클래스 생성

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Name {
+
+    private final String name;
+
+    public Name(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
