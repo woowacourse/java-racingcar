8018aee37c11a2d26451b5b65ca4913c1d814783
Devtist <yt930310@gmail.com>
2023-02-13 01:01:20 +0900

    style: JAVA 컨벤션에 맞춰서 공백 정리

--- src/test/java/racingcar/view/ViewRendererTest.java
+++ src/test/java/racingcar/view/ViewRendererTest.java
@@ -3,7 +3,6 @@ package racingcar.view;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
