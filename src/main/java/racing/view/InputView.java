607eecc4b986ae6a8598afc91d41a0a65e3733c1
drunkenhw <drunkenhw@gmail.com>
2023-02-12 20:43:40 +0900

    refactor: Modify to constant

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -8,13 +8,14 @@ import java.util.stream.Collectors;
 public class InputView {
     public static final String INPUT_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String SPLIT_DELIMITER = ",";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static List<String> inputNames() {
         System.out.println(INPUT_NAMES);
         String input = SCANNER.next();
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(SPLIT_DELIMITER))
                 .collect(Collectors.toList());
     }
 
--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -7,18 +7,20 @@ import racing.domain.Cars;
 public class OutputView {
     public static final String GAME_RESULT_FORMAT = "%s : %s\n";
     public static final String WINNER_FORMAT = "%s가 최종 우승했습니다.\n";
+    private static final String POSITION_VIEW = "-";
+    public static final String WINNER_DELIMITER = ", ";
 
     public static void printRacing(Cars cars) {
         for (Car car : cars.getCars()) {
             int position = car.getPosition();
-            String positionView = "-".repeat(position);
+            String positionView = POSITION_VIEW.repeat(position);
             System.out.printf(GAME_RESULT_FORMAT, car.getName(), positionView);
         }
         System.out.println();
     }
 
     public static void printWinners(List<String> names) {
-        String winners = String.join(", ", names);
+        String winners = String.join(WINNER_DELIMITER, names);
         System.out.printf(WINNER_FORMAT, winners);
     }
 }
