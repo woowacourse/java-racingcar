7c866aff01aa4ae4cc422e6b9f44ab52a7af7059
chaewon121 <cw121@naver.com>
2023-02-13 03:11:12 +0900

    feat: 입력기능 및 결과 출력 구현

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,22 +1,42 @@
 package racingcar;
 
-import racingcar.controller.RacingCarController;
-import racingcar.domain.NumberGenerator;
-import racingcar.step.CreateCarStep;
-import racingcar.step.Step;
+import racingcar.controller.RacingController;
+import racingcar.domain.Cars;
+import racingcar.domain.Lap;
+import racingcar.domain.Winners;
+import racingcar.view.InputView;
+
+import static racingcar.view.OutputView.printWinners;
 
 public class RacingCarApplication {
 
     public static void main(String[] args) {
-        NumberGenerator generator = new NumberGenerator();
-        RacingCarController racingCarController = new RacingCarController(generator);
-        Step step = new CreateCarStep(racingCarController);
-        run(step);
+        //TODO: 컨트롤러와 메인을 역할을 나눴는데 뭐가더 나은 방법인지..?
+        Cars cars = inputCarNames();
+        Lap lap = inputLap();
+
+        final var racingController = new RacingController();
+        racingController.start(cars, lap);
+        Winners winners = new Winners(cars.getCars());
+
+        printWinners(winners);
     }
 
-    private static void run(Step step) {
-        while (step.executable()) {
-            step = step.execute();
+    private static Lap inputLap() {
+        try {
+            return new Lap(InputView.inputTotalLap());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputLap();
+        }
+    }
+
+    private static Cars inputCarNames() {
+        try {
+            return new Cars(InputView.inputCarNames());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputCarNames();
         }
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,32 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
+import racingcar.domain.Winners;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     private static final String CARS_STATE_FORMAT = "%s : %s"; // ex) name : ---
     private static final String BAR = "-";
 
+    public static void printWinners(Winners winners) {
+        System.out.println(String.join(", ", getCarNames(winners.getWinners())) + "가 최종 우승했습니다.");
+    }
+
     public static void printCarsStatus(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(getCarStatus(car));
         }
         System.out.println();
+    }
+
+    //TODO: 위너의 이름을 리스트로 반환하는 기능을 위너 클래스에 넣어야할지..?
+    private static List<String> getCarNames(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 
     private static String getCarStatus(Car car) {
