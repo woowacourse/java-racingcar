69fa8bec8092422b2063df4f9fe1a04ede496e15
MoonJeWoong <munjin0201@naver.com>
2023-02-12 21:02:33 +0900

    refactor(test) : InputView 리팩토링
    세부사항
    - 사용자가 아무 입력도 하지 않은채로 엔터를 눌렀을 시 예외처리 테스트 추가

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,21 +13,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
-    InputView inputView = new InputView();
+
 
     @ParameterizedTest
-    @ValueSource(strings = {"r", "ㄱ", ",", "0"})
+    @ValueSource(strings = {"r", "ㄱ", ",", "0", "\n"})
     @DisplayName("시도 횟수 사용자 입력 값이 올바른 지 검증하는 테스트")
     void validateRoundTest(String input) {
         //Given
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+        InputView inputView = new InputView(new Scanner(System.in));
 
         //When
-        Throwable result = catchThrowable(() -> {
-            inputView.readRacingRound();
-        });
-
+        Throwable result = catchThrowable(inputView::readRacingRound);
 
         //Then
         assertThat(result).isInstanceOf(IllegalArgumentException.class);
