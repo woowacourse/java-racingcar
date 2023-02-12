c882da2905ccaa34f848b1e78f5c75528e26069e
donghae <donghae1999@gmail.com>
2023-02-13 00:02:09 +0900

    refactor: 운영체제별로 개행문자 차이에 따른 개행 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class OutputView {
     private final static String CAR_LOCATION_INDICATOR = "-";
-    private final static String PRINT_CAR_LOCATION = "%s : %s\n";
+    private final static String PRINT_CAR_LOCATION = "%s : %s" + System.lineSeparator();
     private final static String CAR_WINNER_INDICATOR = ",";
-    private final static String PRINT_WINNER = "%s가 최종 우승했습니다.\n";
-    private final static String PRINT_RESULT = "\n실행 결과";
+    private final static String PRINT_WINNER = "%s가 최종 우승했습니다." + System.lineSeparator();
+    private final static String PRINT_RESULT = System.lineSeparator() + "실행 결과";
     private final static String PRINT_REQUEST_CAR_NAME
             = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private final static String PRINT_REQUEST_TRY_COUNT = "시도할 회수는 몇회인가요?";
