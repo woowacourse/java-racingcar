138ac64372a0ad0984e58ac15be023a24d6886a5
ingpyo <juninpyo726@gmail.com>
2023-02-13 01:28:22 +0900

    refactor:Car클래스 안에 프린트 기능 제거,Car클래스 안에 move함수 사용

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,7 +1,9 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> carInformation = new ArrayList<>();
@@ -13,4 +15,27 @@ public class Cars {
             carInformation.add(new Car(carName));
         }
     }
+
+    public List<Car> powerInjectionAllCars() {
+        for (Car car : carInformation) {
+            car.move(powerNumberGenerator.generate());
+        }
+        return carInformation;
+    }
+
+    public List<String> selectWinners() {
+        return carInformation.stream()
+                .filter(car -> Collections.max(positions()) == car.getPosition())
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> positions() {
+        List<Integer> positions = new ArrayList<>();
+        for (Car car : carInformation) {
+            positions.add(car.getPosition());
+        }
+        return positions;
+    }
+
 }
