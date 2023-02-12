1eec43b3bb56db6dd7b798489c342e4982b42209
jiwon <ljwon77@daum.net>
2023-02-13 02:09:29 +0900

    feat: 객체가 존재하지 않을 경우 발생하는 커스텀 exception 추가

--- src/main/java/racingcar/enumType/ExceptionMessage.java
+++ src/main/java/racingcar/enumType/ExceptionMessage.java
@@ -10,7 +10,8 @@ public enum ExceptionMessage implements EnumUtil<String, String> {
     , INPUT_MESSAGE("입력 중에 예기치 못한 오류가 발생하였습니다. 예외 메시지: %s")
     , TYPE_MESSAGE("%s은(는) 숫자만 입력할 수 있습니다.")
     , RANGE_MESSAGE("%d 이상의 값을 입력해 주세요.")
-    , DUPLICATE_MESSAGE("중복된 값을 입력할 수 없습니다.");
+    , DUPLICATE_MESSAGE("중복된 값을 입력할 수 없습니다.")
+    , NO_RESOURCE_MESSAGE("%s(이)가 존재하지 않습니다.");
 
     private final String message;
 
