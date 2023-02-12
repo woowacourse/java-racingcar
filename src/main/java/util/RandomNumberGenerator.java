ccdb4e19b023e6ebf9d886569a65c09f16380481
hectick <chyeon1223@cau.ac.kr>
2023-02-12 12:38:13 +0900

    refactor(RandomNumberGenerator): 상수 추가

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -4,11 +4,12 @@ import java.security.SecureRandom;
 
 public class RandomNumberGenerator {
     private static final int MAX_NUMBER = 9;
+    private static final int BOUNDARY_NUMBER = MAX_NUMBER + 1;
 
     private RandomNumberGenerator() {
     }
 
     public static int getRandomNumber() {
-        return new SecureRandom().nextInt(MAX_NUMBER+1);
+        return new SecureRandom().nextInt(BOUNDARY_NUMBER);
     }
 }
