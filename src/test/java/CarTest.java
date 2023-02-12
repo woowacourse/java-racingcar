ef7a737a70f597aa0b88046e3d080001dd66c0d1
Hyeonjae-K <href.hjk@gmail.com>
2023-02-12 16:06:34 +0900

    test: move 메서드 테스트 코드 작성

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,16 +1,34 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setup() {
+        car = new Car("jerry");
+    }
 
     @Test
     void testToString() {
-        Car car = new Car("jena");
         for (int i = 0; i < 5; i++) {
             car.move(4);
         }
         assertThat(car.toString().split(": ")[1].length())
                 .isEqualTo(car.getPosition());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void moveTest(int number) {
+        for (int i = 0; i < number; i++) {
+            car.move(4);
+        }
+        Assertions.assertEquals(car.getPosition(), number);
     }
 }
