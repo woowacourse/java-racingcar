47dfdcf62cc5230e7fba8a5552c76b9f817f99e4
watterricecake <fruturum@nate.com>
2023-02-12 21:05:55 +0900

    refactor(RandomMoveChance): Application 테스트를 위한 Controller 생성자

--- src/main/java/car/Application.java
+++ src/main/java/car/Application.java
@@ -1,11 +1,12 @@
 package car;
 
 import car.controller.GameController;
+import car.view.InputConsole;
 
 public class Application {
 
     public static void main(String[] args) {
-        GameController gameController = new GameController();
+        GameController gameController = new GameController(new InputConsole());
         gameController.startGame();
         gameController.play();
         gameController.showResult();
