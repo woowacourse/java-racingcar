6c4228e3e9a8faeeee5cc2509fc23e499280ff49
kim0914 <kosa0914@naver.com>
2023-02-12 16:02:29 +0900

    fix(RandomNumberGenerator): 난수 발생 범위 변경
    - 1~9 -> 0~9

--- src/test/java/domain/RandomNumberGeneratorTest.java
+++ src/test/java/domain/RandomNumberGeneratorTest.java
@@ -8,13 +8,13 @@ import static org.assertj.core.api.Assertions.*;
 class RandomNumberGeneratorTest {
 
     @Test
-    @DisplayName("난수 발생기로 생성되는 숫자는 1~9사이의 정수이다.")
+    @DisplayName("난수 발생기로 생성되는 숫자는 0~9사이의 정수이다.")
     public void checkRandomNumberRange() {
         NumberGenerator numberGenerator = new RandomNumberGenerator();
 
         for (int i = 0; i < 10000; i++) {
             assertThat(numberGenerator.generate())
-                    .isBetween(1, 9);
+                    .isBetween(0, 9);
         }
     }
 }
