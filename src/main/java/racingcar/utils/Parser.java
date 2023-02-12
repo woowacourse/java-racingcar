ed628269148c10afbf4407923a4d11bfd609d4ff
hanueleee <ezzeezz@naver.com>
2023-02-12 17:27:17 +0900

    refactor: Parser에서 파싱 전 유효성 검증 로직 추가

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+    private static final String PARSING_EXCEPTION_MESSAGE = "빈 입력입니다.";
+
     public static List<String> parsing(String text, String delimiter) {
+        if (text == null || text.equals("")) {
+            throw new IllegalArgumentException(PARSING_EXCEPTION_MESSAGE);
+        }
         return Arrays.asList(text.split(delimiter));
     }
 }
