aeda99a1abb99348415cd222fa8a81a0b2dcf04e
yeonkkk <tjddus0302@gmail.com>
2023-02-12 14:39:24 +0900

    refactor: 사용자 입력 값 validation 추가

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -3,7 +3,9 @@ package utils;
 public enum ErrorMessage {
     COMMON("[ERROR] "),
     WRONG_NAME_LENGTH("1에서 5사이의 이름 길이만 입력 가능합니다."),
-    WRONG_TRY_TIMES("시도 횟수는 최소 1회 이상입니다.");
+    WRONG_TRY_TIMES_RANGE("시도 횟수는 최소 1회 이상입니다."),
+    WRONG_TRY_TIMES_TYPE("시도 횟수는 숫자만 입력해야합니다."),
+    WRONG_NAME_CONTAINS_BLANK("자동차 이름은 공백을 포함할 수 없습니다.");
 
     private final String message;
 
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,8 @@
 package view;
 
-import static utils.ErrorMessage.WRONG_TRY_TIMES;
+import static utils.ErrorMessage.WRONG_NAME_CONTAINS_BLANK;
+import static utils.ErrorMessage.WRONG_TRY_TIMES_RANGE;
+import static utils.ErrorMessage.WRONG_TRY_TIMES_TYPE;
 
 import java.util.List;
 import java.util.Scanner;
@@ -14,20 +16,38 @@ public class InputView {
 
     public List<String> inputCarNames() {
         String carNames = scanner.nextLine();
-        // TODO: carName 공백 validation
+        validateContainsBlank(carNames);
         return List.of(carNames.split(CAR_NAME_DELIMITER));
     }
 
     public int inputTryTimes() {
-        // TODO: tryTimes 숫자 validation
-        int tryTimes = scanner.nextInt();
-        validateTryTimes(tryTimes);
-        return tryTimes;
+        String tryTimes = scanner.nextLine();
+        validateTryTimesType(tryTimes);
+        validateTryTimesRange(Integer.parseInt(tryTimes));
+        return Integer.parseInt(tryTimes);
     }
 
-    private void validateTryTimes(int tryTimes) {
+    private void validateTryTimesRange(int tryTimes) {
         if (tryTimes < MIN_TRY_TIMES) {
-            throw new IllegalArgumentException(WRONG_TRY_TIMES.of());
+            throw new IllegalArgumentException(WRONG_TRY_TIMES_RANGE.of());
+        }
+    }
+
+    private void validateContainsBlank(String carNames) {
+        if (carNames.contains(" ")) {
+            throw new IllegalArgumentException(WRONG_NAME_CONTAINS_BLANK.of());
+        }
+    }
+
+    private void validateTryTimesType(String tryTimes) {
+        for (char element : tryTimes.toCharArray()) {
+            checkDigit(element);
+        }
+    }
+
+    private void checkDigit(char element) {
+        if (!Character.isDigit(element)) {
+            throw new IllegalArgumentException(WRONG_TRY_TIMES_TYPE.of());
         }
     }
 }
