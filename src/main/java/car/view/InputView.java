4ccb982529a982da37ce0d9b45ef5cdedc5914cc
watterricecake <fruturum@nate.com>
2023-02-12 22:13:25 +0900

    feat(inputView): 이름사이 공백 입력시 공백 제거

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     }
     public String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return input.inputCarNames().split(",");
+        return input.inputCarNames().replace(" ","").split(",");
     }
 
     public int inputTrialCount() {
