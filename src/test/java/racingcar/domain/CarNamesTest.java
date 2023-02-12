68b75552af4616469a7844e6828ff5d7e99e3d90
Devtist <yt930310@gmail.com>
2023-02-12 12:41:38 +0900

    test: 자동차들의 이름 입력에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarNamesTest.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import racingcar.dto.CarNamesRequestDTO;
+
+class CarNamesTest {
+    @ParameterizedTest(name = "자동차 이름이 delimiter로 분리되는지 테스트")
+    @ValueSource(strings = {"test1,test2,test3", ",,"})
+    void carNamesTest(String string) {
+        assertThat(new CarNames(new CarNamesRequestDTO(string)).getNames()).hasSize(3);
+    }
+}
