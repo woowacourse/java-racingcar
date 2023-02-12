6425dcfeba74b1acd8e0e411fa9d7a4842a777aa
jeomxon <jeomxon@gmail.com>
2023-02-13 00:05:23 +0900

    refactor: Car의 name에 대한 검증을 Name으로 이동

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -3,15 +3,11 @@ package racing.domain;
 public class Car {
 
     private static final int INITIAL_VALUE = 0;
-    private static final int MINIMUM_LENGTH_OF_CAR_NAME = 1;
-    private static final int MAXIMUM_LENGTH_OF_CAR_NAME = 5;
-    private static final String LENGTH_OF_CAR_NAME_ERROR = "[ERROR] 자동차이름의 길이는 1-5자까지 가능합니다.";
 
     private final Name name;
     private Position position;
 
     public Car(String name) {
-        validateLengthOfName(name);
         this.name = new Name(name);
         this.position = new Position(INITIAL_VALUE);
     }
@@ -28,11 +24,5 @@ public class Car {
 
     public Position getPosition() {
         return position;
-    }
-
-    private void validateLengthOfName(String name) {
-        if (name.length() < MINIMUM_LENGTH_OF_CAR_NAME || name.length() > MAXIMUM_LENGTH_OF_CAR_NAME) {
-            throw new IllegalArgumentException(LENGTH_OF_CAR_NAME_ERROR);
-        }
     }
 }
--- src/main/java/racing/domain/Name.java
+++ src/main/java/racing/domain/Name.java
@@ -4,9 +4,14 @@ import java.util.Objects;
 
 public class Name {
 
+    private static final int MINIMUM_LENGTH_OF_CAR_NAME = 1;
+    private static final int MAXIMUM_LENGTH_OF_CAR_NAME = 5;
+    private static final String LENGTH_OF_CAR_NAME_ERROR = "[ERROR] 자동차이름의 길이는 1-5자까지 가능합니다.";
+
     private final String name;
 
     public Name(String name) {
+        validateLengthOfName(name);
         this.name = name;
     }
 
@@ -25,5 +30,11 @@ public class Name {
     @Override
     public int hashCode() {
         return Objects.hash(getName());
+    }
+
+    private void validateLengthOfName(String name) {
+        if (name.length() < MINIMUM_LENGTH_OF_CAR_NAME || name.length() > MAXIMUM_LENGTH_OF_CAR_NAME) {
+            throw new IllegalArgumentException(LENGTH_OF_CAR_NAME_ERROR);
+        }
     }
 }
