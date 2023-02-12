92c4787c2fb8fcdfe7d8f759842d12bc3b105033
yoondgu <doyou2ing@naver.com>
2023-02-12 19:23:31 +0900

    refactor: 자동차 상태 출력 포맷 상수 처리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,7 +6,8 @@ import java.util.Map;
 public class OutputView {
 
     private static final String POSITION_MARK = "-";
-    
+
+    private static final String CAR_STATUS_MESSAGE_FORMAT = "%s : %s\n";
     private static final String WINNERS_MESSAGE_DELIMITER = ", ";
     private static final String WINNERS_MESSAGE_FORMAT = "%s가 최종 우승했습니다.\n";
     private static final String RESULT_MESSAGE = "실행결과";
@@ -14,8 +15,12 @@ public class OutputView {
     private static final int POSITION_MARK_DEFAULT_VALUE = 1;
 
     public void printCarsStatus(final Map<String, Integer> carsStatus) {
-        carsStatus.forEach((name, position) -> System.out.println(name + " : " + convertPosition(position)));
+        carsStatus.forEach((name, position) -> System.out.print(convertCarStatusDisplay(name, position)));
         System.out.println();
+    }
+
+    private String convertCarStatusDisplay(final String name, final int position) {
+        return String.format(CAR_STATUS_MESSAGE_FORMAT, name, convertPosition(position));
     }
 
     private String convertPosition(final int position) {
