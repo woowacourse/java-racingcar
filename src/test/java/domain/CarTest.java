1b6c82c6e01695308ddf9050a4ccb48bd64ca3f7
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-12 21:05:32 +0900

    style: DisplayName 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,6 +9,7 @@ class CarTest {
 
     @ParameterizedTest
     @CsvSource(value = {"0,0", "1,0", "2,0", "3,0", "4,1", "5,1", "6,1", "7,1", "8,1", "9,1"}, delimiter = ',')
+    @DisplayName("power 값에 따른 이동 여부 확인 테스트")
     void moveTest(int power, int expectPosition) {
         Car car = new Car("jerry");
         car.move(power);
