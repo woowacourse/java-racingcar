153784fd81efd3d7de6919aea504d92c399ba1b2
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-13 06:21:26 +0900

    refactor : 랜덤 숫자 관련 상수명 변경

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,12 +1,13 @@
 package racingcar.domain;
 
-import static racingcar.constant.GameConstant.MAXIMUM_POWER_OF_ENGINE;
-import static racingcar.constant.GameConstant.MINIMUM_POWER_OF_ENGINE;
+import static racingcar.constant.GameConstant.MAXIMUM_NUMBER_OF_ENGINE;
+import static racingcar.constant.GameConstant.MINIMUM_NUMBER_OF_ENGINE;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public int generate() {
-        return (int) (Math.random() * (MAXIMUM_POWER_OF_ENGINE - MINIMUM_POWER_OF_ENGINE+1)) + MINIMUM_POWER_OF_ENGINE;
+        return (int) (Math.random() * (MAXIMUM_NUMBER_OF_ENGINE - MINIMUM_NUMBER_OF_ENGINE + 1))
+                + MINIMUM_NUMBER_OF_ENGINE;
     }
 }
