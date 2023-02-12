229c268032a80a111e4a37c198bd945a86ac3adc
Choi-JJunho <junho5336@gmail.com>
2023-02-12 15:48:20 +0900

    test: add RacingGameTest

---
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -0,0 +1,61 @@
+package racing.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racing.RandomNumberGenerator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class RacingGameTest {
+
+    private final List<String> carNames = List.of("박스터", "포르쉐", "소나타");
+
+    @Test
+    @DisplayName("play 메소드를 호출하면 모든 자동차의 position이 1 증가한다.")
+    void play_with_always_move() {
+        RacingGame game = new RacingGame(carNames, new AlwaysMoveGenerator());
+        game.play();
+
+        List<Car> result = game.getResult();
+
+        List<Integer> positions = result.stream()
+                .map(Car::getPosition)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(positions).containsOnly(1);
+    }
+
+    @Test
+    @DisplayName("play 메소드를 호출하면 모든 자동차의 position이 1 증가하지 않는다.")
+    void play_with_always_stop() {
+        RacingGame game = new RacingGame(carNames, new AlwaysStopGenerator());
+        game.play();
+
+        List<Car> result = game.getResult();
+
+        List<Integer> positions = result.stream()
+                .map(Car::getPosition)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(positions).containsOnly(0);
+    }
+
+    private static class AlwaysMoveGenerator implements RandomNumberGenerator {
+
+        @Override
+        public int generate() {
+            return 4;
+        }
+    }
+
+    private static class AlwaysStopGenerator implements RandomNumberGenerator {
+
+        @Override
+        public int generate() {
+            return 3;
+        }
+    }
+
+}
