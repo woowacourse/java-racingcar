2a06f361c6eb3f86bdcd7901b5499798341e79a7
sosow0212 <sosow0212@naver.com>
2023-02-12 14:40:16 +0900

    test : move() 메서드의 성공 경우와 실패 경우의 테스트를 분리

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -3,22 +3,42 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
-    @Test
-    @DisplayName("move() : 자동차의 이동 테스트")
-    void test_move() {
+    @ParameterizedTest
+    @ValueSource(strings = {"4", "5", "6"})
+    @DisplayName("move() : 자동차의 이동 성공 테스트")
+    void test_move_success(int givenPower) {
         // given
-        int givenDistance = 10;
-        int expectedDistanceAfterMove = givenDistance + 1;
+        int power = givenPower;
+        int givenDistance = 0;
+        int expectedDistanceAfterMoveSuccess = givenDistance + 1;
         Car car = new Car("pobi", givenDistance);
 
         // when
-        car.move();
+        car.move(power);
 
         // then
-        assertThat(car.getDistance()).isEqualTo(expectedDistanceAfterMove);
+        assertThat(car.getDistance()).isEqualTo(expectedDistanceAfterMoveSuccess);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "2", "3"})
+    @DisplayName("move() : 자동차 이동 실패 테스트")
+    void test_move_fail(int givenPower) {
+        // given
+        int power = givenPower;
+        int givenDistance = 0;
+        int expectedDistanceAfterMoveSuccess = givenDistance + 1;
+        Car car = new Car("pobi", givenDistance);
+
+        // when
+        car.move(power);
+
+        // then
+        assertThat(car.getDistance()).isNotEqualTo(expectedDistanceAfterMoveSuccess);
     }
 }
