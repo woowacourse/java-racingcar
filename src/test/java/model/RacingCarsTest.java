06cdb0eee6cfbcc8bf1628ae18fcab92e5884f4c
somsom13 <somin1113@naver.com>
2023-02-12 15:15:05 +0900

    test: RacingCars 차 이동 확인 테스트

--- src/test/java/model/RacingCarsTest.java
+++ src/test/java/model/RacingCarsTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import exception.ErrorCode;
@@ -7,8 +8,25 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import service.NumberGenerator;
 
 class RacingCarsTest {
+
+    static class TestMoveNumberGenerator implements NumberGenerator {
+
+        @Override
+        public int generateNumber(int maxNumber) {
+            return 4;
+        }
+    }
+
+    static class TestNotMoveNumberGenerator implements NumberGenerator {
+
+        @Override
+        public int generateNumber(int maxNumber) {
+            return 3;
+        }
+    }
 
     @Test
     @DisplayName("차 이름이 중복되면 예외가 발생한다")
@@ -42,6 +60,7 @@ class RacingCarsTest {
         //given
         int carLimit = 100;
         List<Car> cars = new ArrayList<>();
+
         for (int i = 0; i <= carLimit; i++) {
             cars.add(new Car(String.valueOf(i)));
         }
@@ -52,4 +71,39 @@ class RacingCarsTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(String.format(ErrorCode.TOO_MANY_CAR.getMessage(), carLimit));
     }
+
+    @Test
+    @DisplayName("모든 차를 이동시킨다")
+    void moveCars() {
+        //given
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        RacingCars racingCars = new RacingCars(List.of(car1, car2));
+
+        //when
+        racingCars.move(new TestMoveNumberGenerator(), 10);
+
+        //then
+        for (Integer raceResult : racingCars.getCurrentPositions().values()) {
+            assertThat(raceResult).isEqualTo(2);
+        }
+    }
+
+    @Test
+    @DisplayName("모든 차를 이동시키지 않는다.")
+    void notMoveCars() {
+        //given
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        RacingCars racingCars = new RacingCars(List.of(car1, car2));
+
+        //when
+        racingCars.move(new TestNotMoveNumberGenerator(), 10);
+
+        //then
+        for (Integer raceResult : racingCars.getCurrentPositions().values()) {
+            assertThat(raceResult).isEqualTo(1);
+        }
+    }
+
 }
