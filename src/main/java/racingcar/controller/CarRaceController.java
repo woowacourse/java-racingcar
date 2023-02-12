d6908df1783f74ebe38f1050572dd9da23602c2a
jiwon <ljwon77@daum.net>
2023-02-13 01:25:26 +0900

    refactor: 경기 결과 출력 및 승자 출력 메서드 분리

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -3,8 +3,10 @@ package racingcar.controller;
 import racingcar.domain.Cars;
 import racingcar.domain.Race;
 import racingcar.domain.RaceNumberGenerator;
-import racingcar.domain.dto.CarRaceDto;
+import racingcar.domain.dto.CarStatusDto;
 import racingcar.domain.dto.RaceResultDto;
+import racingcar.service.CarsService;
+import racingcar.service.RaceService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -19,31 +21,32 @@ public class CarRaceController {
 
     private final OutputView outputView;
 
+    private final CarsService carsService;
+
+    private final RaceService raceService;
+
     private final RaceNumberGenerator numberGenerator;
 
     public CarRaceController() {
         inputView = new InputView();
         outputView = new OutputView();
+        carsService = new CarsService();
+        raceService = new RaceService(carsService);
         numberGenerator = new RaceNumberGenerator();
     }
 
     public void race() {
         Cars cars = getUserInputAndCreateCars();
         Race race = getUserInputAndCreateRace();
-
-        List<CarRaceDto> initCarResult = cars.initStatus();
-        List<RaceResultDto> totalRaceResult = race.getRaceResults(cars);
-        outputView.printRaceResult(initCarResult, totalRaceResult);
-
-        List<String> winners = cars.pickWinners();
-        outputView.printWinnersResult(winners);
+        getRaceResult(cars, race);
+        getWinnerResult(cars);
     }
 
     private Cars getUserInputAndCreateCars() {
         return inputView.getUserInput(() -> {
             OutputView.printMessage(GET_CAR_NAMES_MESSAGE.getValue());
             String carNames = inputView.readConsole();
-            return Cars.createCars(carNames, numberGenerator);
+            return carsService.createCars(carNames, numberGenerator);
         });
     }
 
@@ -51,7 +54,18 @@ public class CarRaceController {
         return inputView.getUserInput(() -> {
             OutputView.printMessage(GET_TRY_COUNT_MESSAGE.getValue());
             String tryCount = inputView.readConsole();
-            return Race.createRace(tryCount);
+            return raceService.createRace(tryCount);
         });
+    }
+
+    private void getRaceResult(final Cars cars, final Race race) {
+        List<CarStatusDto> initCarStatus = carsService.getCarStatus(cars);
+        List<RaceResultDto> totalRaceResult = raceService.getRaceResults(cars, race);
+        outputView.printRaceResult(initCarStatus, totalRaceResult);
+    }
+
+    private void getWinnerResult(final Cars cars) {
+        List<String> winners = carsService.pickWinners(cars);
+        outputView.printWinnersResult(winners);
     }
 }
