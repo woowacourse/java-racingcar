b11a9374045da5e1c04e5da7cafe47b40a49ba16
MoonJeWoong <munjin0201@naver.com>
2023-02-12 21:18:39 +0900

    refactor(test) : cars 객체를 생성하는 중복 코드 beforeEach로 분리 리팩토링

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -6,10 +6,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
+
+    private static final int TEST_GAME_ROUND = 3;
+
+    private Car pobi;
+    private Car neo;
+    private Car hiiro;
+    private Cars cars;
+
+    @BeforeEach
+    void beforeTest() {
+        pobi = new Car("pobi", new TestNumberGenerator(new ArrayList<>(List.of(3, 4, 5))));
+        neo = new Car("neo", new TestNumberGenerator(new ArrayList<>(List.of(4, 3, 5))));
+        hiiro = new Car("hiiro", new TestNumberGenerator(new ArrayList<>(List.of(1, 4, 2))));
+
+        cars = new Cars(new ArrayList<>(List.of(pobi, neo, hiiro)));
+    }
 
     @Test
     @DisplayName("자동차 이름이 없어서 빈 리스트가 넘어오면 오류가 발생한다.")
@@ -29,18 +46,12 @@ class CarsTest {
     @Test
     @DisplayName("우승한 자동차를 제공한다.")
     void decideWinnerTest() {
+        //todo : cars 객체를 생성하는 코드를 beforeEach로 분리하기 (공통된 car 객체 삽입 부분)
+
         //Given
-        Car pobi = new Car("pobi", new TestNumberGenerator(new ArrayList<>(List.of(3, 4, 5))));
-        Car neo = new Car("neo", new TestNumberGenerator(new ArrayList<>(List.of(3, 4, 5))));
-        Car hiiro = new Car("hiiro", new TestNumberGenerator(new ArrayList<>(List.of(1, 4, 2))));
-
-        for (int i = 0; i < 3; i++) {
-            pobi.goForward();
-            neo.goForward();
-            hiiro.goForward();
+        for (int i = 0; i < TEST_GAME_ROUND; i++) {
+            cars.moveEachCar();
         }
-
-        Cars cars = new Cars(new ArrayList<>(List.of(pobi, neo, hiiro)));
 
         //When
         List<Car> result = cars.findAllWinner();
@@ -52,10 +63,6 @@ class CarsTest {
     @Test
     @DisplayName("모든 자동차들이 전진을 1회 시도한다.")
     void eachCarMoveTest() {
-        //Given
-        Car pobi = new Car("pobi", new TestNumberGenerator(new ArrayList<>(List.of(3))));
-        Car neo = new Car("neo", new TestNumberGenerator(new ArrayList<>(List.of(4))));
-        Cars cars = new Cars(new ArrayList<>(List.of(pobi, neo)));
 
         //When
         List<Car> result = cars.moveEachCar();
@@ -63,6 +70,7 @@ class CarsTest {
         //Then
         assertThat(result.get(0).getPosition()).isEqualTo(0);
         assertThat(result.get(1).getPosition()).isEqualTo(1);
+        assertThat(result.get(2).getPosition()).isEqualTo(0);
     }
 
 
