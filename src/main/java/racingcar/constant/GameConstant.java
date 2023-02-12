47030fe19e04628b8bcd820185624498babcbe1e
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-13 06:21:26 +0900

    refactor : 랜덤 숫자 관련 상수명 변경

--- src/main/java/racingcar/constant/GameConstant.java
+++ src/main/java/racingcar/constant/GameConstant.java
@@ -2,7 +2,7 @@ package racingcar.constant;
 
 public class GameConstant {
     public static final int RACE_START_POINT = 0;
-    public static final int MINIMUM_POWER_OF_ENGINE = 0;
-    public static final int MAXIMUM_POWER_OF_ENGINE = 9;
-    public static final int MINIMUM_POWER_TO_MOVE = 4;
+    public static final int MINIMUM_NUMBER_OF_ENGINE = 0;
+    public static final int MAXIMUM_NUMBER_OF_ENGINE = 9;
+    public static final int MINIMUM_NUMBER_TO_MOVE = 4;
 }
