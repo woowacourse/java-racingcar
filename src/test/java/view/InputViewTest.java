c775e0edadd6069b1e420be582c22b3d8a691664
kwonyj1022 <kwonyj1022@naver.com>
2023-02-13 01:33:36 +0900

    test: tryCount 검증 테스트 추가

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static domain.Car.MAX_NAME_LENGTH;
+import static domain.TryCount.TRY_COUNT_LIMIT;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
@@ -38,5 +39,36 @@ class InputViewTest {
         System.setIn(in);
         assertThatIllegalArgumentException().isThrownBy(() -> new InputView().sendCarsName())
                 .withMessageContaining("이름에 공백을 포함할 수 없습니다.");
+    }
+
+    @DisplayName("tryCount가 게임을 실행하기에 무리가 없는 크기의 수인지 검증한다.")
+    @Test
+    void tryCount_크기_검증_성공() {
+        String input = "10";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatNoException().isThrownBy(() -> new InputView().sendTryCount());
+
+    }
+
+    @DisplayName("tryCount가 1보다 작으면 예외를 발생시킨다.")
+    @Test
+    void tryCount_크기_검증_실패_너무작은수() {
+        String input = "0";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatIllegalArgumentException().isThrownBy(() -> new InputView().sendTryCount())
+                .withMessageContaining("시도 횟수는 양의 정수여야 합니다.");
+
+    }
+
+    @DisplayName("tryCount가 100보다 크면 예외를 발생시킨다.")
+    @Test
+    void tryCount_크기_검증_실패_너무큰수() {
+        String input = "101";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatIllegalArgumentException().isThrownBy(() -> new InputView().sendTryCount())
+                .withMessageContaining("시도할 회수는 " + TRY_COUNT_LIMIT +"회 이하여야 합니다.");
     }
 }
