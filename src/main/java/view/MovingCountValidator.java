07ee44562ec191a06bd9f70cb5b708ae5d760b09
Youngho <cyh6099@gmail.com>
2023-02-12 15:28:28 +0900

    refactor : 시도횟수 정수 여부 검증 로직 수정

--- src/main/java/view/MovingCountValidator.java
+++ src/main/java/view/MovingCountValidator.java
@@ -1,24 +1,27 @@
 package view;
 
 public class MovingCountValidator {
-    private static final String CHARACTER_REG = "^[a-zA-Z]*$";
-    private static final int MOVING_COUNT_MIN_RANGE = 1;
-    private static final int MOVING_COUNT_MAX_RANGE = 100;
+    private static final int TRY_COUNT_MIN_RANGE = 1;
+    private static final int TRY_COUNT_MAX_RANGE = 100;
+    private static final String INPUT_INVALIDATE_MESSAGE = String.format("시도횟수는 %d~%d 범위의 숫자만 가능합니다.", TRY_COUNT_MIN_RANGE, TRY_COUNT_MAX_RANGE);
 
-    public static void validateMovingCountInput(String input) {
-        validateIsDigit(input);
-        validateCountRange(Integer.parseInt(input));
+    public static void validateTryCountInput(String tryCount) {
+        validateIsDigit(tryCount);
+        validateTryCountRange(Integer.parseInt(tryCount));
     }
 
-    private static void validateIsDigit(String input) {
-        if (input.matches(CHARACTER_REG)) {
-            throw new IllegalArgumentException("이동할 횟수는 숫자만 입력 가능합니다.");
+    private static void validateIsDigit(String tryCount) {
+        try {
+            Integer.parseInt(tryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_INVALIDATE_MESSAGE);
         }
+
     }
 
-    private static void validateCountRange(Integer count) {
-        if (count < MOVING_COUNT_MIN_RANGE || count > MOVING_COUNT_MAX_RANGE) {
-            throw new IllegalArgumentException("이동 횟수는 1이상, 100이하의 숫자만 가능합니다.");
+    private static void validateTryCountRange(int tryCount) {
+        if (tryCount < TRY_COUNT_MIN_RANGE || tryCount > TRY_COUNT_MAX_RANGE) {
+            throw new IllegalArgumentException(INPUT_INVALIDATE_MESSAGE);
         }
     }
 }
