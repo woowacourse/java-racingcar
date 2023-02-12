bca78657cf5ac344a61e6cac8e6f11914cf542c0
amaran-th <songsy405@gmail.com>
2023-02-12 18:18:06 +0900

    refactor(Test) : 자동차 이동/멈춤을 strategy 구현체로 구현
    (cherry picked from commit 399a0bf587ef05ba868feb99476e9a3b6be0272c)
    (cherry picked from commit 16ac8b9fedcb60529b4b6fcb2adbd624cffb4ce7)

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,11 +2,25 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import strategy.HighPowerSupplier;
+import strategy.LowPowerSupplier;
+import strategy.PowerSupplier;
 
 class CarTest {
 
-  private Car car1 = new Car("car1");
+  private static final PowerSupplier highPowerSupplier = new HighPowerSupplier();
+  private static final PowerSupplier lowPowerSupplier = new LowPowerSupplier();
+
+  private Car car1;
+  private Car car2;
+
+  @BeforeEach
+  void init() {
+    car1 = new Car("car1");
+    car2 = new Car("car2");
+  }
 
   @Test
   void 자동차_생성() {
@@ -15,23 +29,22 @@ class CarTest {
 
   @Test
   void 자동차_이동() {
-    car1.move(7);
+    car1.move(highPowerSupplier.supply());
     int currentPosition = car1.getPosition();
     assertThat(currentPosition).isEqualTo(1);
   }
 
   @Test
   void 자동차_멈춤() {
-    car1.move(3);
+    car1.move(lowPowerSupplier.supply());
     int currentPosition = car1.getPosition();
     assertThat(currentPosition).isEqualTo(0);
   }
 
   @Test
   void 자동차_위치_비교() {
-    Car car2 = new Car("car2");
-    int go = 4;
-    car2.move(go);
+    car1.move(lowPowerSupplier.supply());
+    car2.move(highPowerSupplier.supply());
     assertThat(car1.compareTo(car2) < 0).isTrue();
   }
 }
