8b503bf6ce00e2e4458091993d3a0c24fbe8a861
watterricecake <fruturum@nate.com>
2023-02-12 22:59:59 +0900

    refactor(TestInputView): 불필요한 코드 제거

--- src/test/java/car/view/TestInputConsole.java
+++ src/test/java/car/view/TestInputConsole.java
@@ -12,12 +12,10 @@ public class TestInputConsole implements Input{
     @Override
     public String inputCarNames() {
         return this.testCarName;
-        //return "땡칠,호이,달리,루쿠,저문,아마란스";
     }
 
     @Override
     public String inputTrialCount() {
         return this.testTrialCount;
-        //return "10";
     }
 }
