bd39779f541ebcf93771fb2951beee28034a3517
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-02-12 23:05:27 +0900

    test : 자동차 이름의 길이에 대한 경계값 테스트

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 class CarTest {
     Car car1;
@@ -37,9 +36,21 @@ class CarTest {
     }
 
 
-    @Test
-    @DisplayName("자동차의 이름이 길면 예외를 던짐")
-    void validateCarName() {
-        assertThatThrownBy(() -> new Car("long_car_name")).isInstanceOf(CarNameLengthException.class);
+    @Nested
+    @DisplayName("자동차 이름의 길이에 대한 테스트")
+    class ValidateCarName {
+        @Test
+        @DisplayName("길이가 6이상이면 예외를 던짐")
+        void case1() {
+            assertThatThrownBy(() -> new Car("666666")).isInstanceOf(CarNameLengthException.class);
+        }
+
+        @Test
+        @DisplayName("길이가 5이하이면 정상 실행")
+        void case2() {
+            assertThatCode(() -> new Car("55555")).doesNotThrowAnyException();
+            assertThatNoException().isThrownBy(() -> new Car("55555"));
+        }
     }
+
 }
