5f3faf55c92ba8dec6e45f8c9703cba171ad2fcf
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-13 06:04:24 +0900

    refactor : CarFactory 도메인의 자동차 리스트 생성 기능 수정

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -1,11 +1,11 @@
 package racingcar.domain;
 
-import racingcar.constant.ErrorConstant;
-import racingcar.constant.GameConstant;
-
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static racingcar.constant.GameConstant.RACE_START_POINT;
 
 public class CarFactory {
 
@@ -14,7 +14,10 @@ public class CarFactory {
     private static final int CAR_NAME_MAXIMUM_LENGTH = 5;
 
     public static List<Car> generate(String input) {
-        final List<String> carNames = List.of(input.split(CAR_NAME_DELIMITER));
+        final List<String> carNames = Arrays.stream(input.split(CAR_NAME_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toUnmodifiableList());
+
         validateDuplicatedCarNames(carNames);
 
         return createCarsByNames(carNames);
@@ -24,21 +27,20 @@ public class CarFactory {
         return carNames.stream()
                 .map(carName -> {
                     validateLengthOfCarName(carName);
-                    return new Car(carName);})
+                    return new Car(carName, RACE_START_POINT);
+                })
                 .collect(Collectors.toUnmodifiableList());
     }
 
     private static void validateDuplicatedCarNames(List<String> carNames) {
         if (Set.copyOf(carNames).size() != carNames.size()) {
-            throw new IllegalArgumentException(ErrorConstant.ERROR_PREFIX + "자동차 이름은 중복되지 않아야 합니다.");
+            throw new IllegalArgumentException("자동차 이름은 중복되지 않아야 합니다.");
         }
     }
 
-    private static void validateLengthOfCarName(String input) {
-        String name = input.trim();
-
+    private static void validateLengthOfCarName(String name) {
         if (name.length() > CAR_NAME_MAXIMUM_LENGTH || name.length() < CAR_NAME_MINIMUM_LENGTH) {
-            throw new IllegalArgumentException(ErrorConstant.ERROR_PREFIX + "자동차 이름의 길이는 1이상 5이하여야 합니다.");
+            throw new IllegalArgumentException("자동차 이름의 길이는 1이상 5이하여야 합니다.");
         }
     }
 
