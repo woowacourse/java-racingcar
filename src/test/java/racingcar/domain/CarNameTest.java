d9b758086c11fcdfe768d068d517fc11ad56f70e
Be-student <presentsong@naver.com>
2023-02-12 22:52:30 +0900

    fix: IEA 를 IllegalArgumentException 으로 축약하지 않음

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -11,14 +11,14 @@ import org.junit.jupiter.api.Test;
 class CarNameTest {
 
     @Test
-    void 생성시_길이가_5를_넘는_데이터가_들어오면_IEA_예외가_발생한다() {
+    void 생성시_길이가_5를_넘는_데이터가_들어오면_IllegalArgumentException_예외가_발생한다() {
         assertThatThrownBy(() -> new CarName("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름의 길이가 초과되었습니다");
     }
 
     @Test
-    void 생성시_null_인_이름이_들어오면_IEA_예외가_발생한다() {
+    void 생성시_null_인_이름이_들어오면_IllegalArgumentException_예외가_발생한다() {
         assertThatThrownBy(() -> new CarName(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름이 null입니다.");
