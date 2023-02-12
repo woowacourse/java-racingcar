6c028f7c14442c0c8bfab81dddd3d5b8e6866397
yoondgu <doyou2ing@naver.com>
2023-02-12 19:17:59 +0900

    refactor: 입력요청 메시지 상수 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,16 +8,19 @@ public class InputView {
 
     private static final String requestDelimiter = ",";
 
+    private static final String ASK_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String ASK_RACING_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> askCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        System.out.println(ASK_CAR_NAMES_MESSAGE);
         final String carNamesLine = scanner.nextLine();
         return Arrays.asList(carNamesLine.split(requestDelimiter));
     }
 
-    public int askTrialCount() {
-        System.out.println("시도할 회수는 몇회인가요?");
+    public int askRacingCount() {
+        System.out.println(ASK_RACING_COUNT_MESSAGE);
         final int trialCount = readInt();
         if (trialCount <= 0) {
             throw new IllegalArgumentException("시도할 회수는 자연수여야 합니다.");
