3e86afc90176f000d79f562283d704a08c82523f
hum02 <ehdgma02@gmail.com>
2023-02-12 23:20:05 +0900

    test: tryCount가 0일 때 decrease할 경우에 대한 테스트 추가

--- src/test/java/racingCar/domain/RacingGameTest.java
+++ src/test/java/racingCar/domain/RacingGameTest.java
@@ -1,10 +1,8 @@
 package racingCar.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingCar.FixedNumberGenerator;
 import racingCar.dto.CarDto;
@@ -12,7 +10,8 @@ import racingCar.dto.CarDto;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingGameTest {
 
@@ -23,8 +22,9 @@ class RacingGameTest {
         RacingGame racingGame = new RacingGame(List.of("aa", "bb", "cc"), 3);
 
         List<CarDto> carStatuses = racingGame.play(new FixedNumberGenerator(fixedNumber));
+        List<Integer> positions = carStatuses.stream().map(CarDto::getPosition).collect(Collectors.toList());
 
-        Assertions.assertThat(carStatuses.stream().map(CarDto::getPosition).collect(Collectors.toList())).containsOnly(0);
+        assertThat(positions).containsOnly(0);
     }
 
     @ParameterizedTest
@@ -34,7 +34,19 @@ class RacingGameTest {
         RacingGame racingGame = new RacingGame(List.of("aa", "bb", "cc"), 6);
 
         List<CarDto> carStatuses = racingGame.play(new FixedNumberGenerator(fixedNumber));
+        List<Integer> positions = carStatuses.stream().map(CarDto::getPosition).collect(Collectors.toList());
 
-        Assertions.assertThat(carStatuses.stream().map(CarDto::getPosition).collect(Collectors.toList())).containsOnly(1);
+        assertThat(positions).containsOnly(1);
+    }
+
+    @Test
+    @DisplayName("racingGame의 시도횟수가 0인 상태에서 또 play 하면 예외가 발생한다")
+    void play_fail_if_tryCount_is_0() {
+        RacingGame racingGame = new RacingGame(List.of("aa", "bb", "cc"), 0);
+
+        assertThatThrownBy(() -> {
+            racingGame.play(new FixedNumberGenerator(3));
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("게임의 시도횟수를 모두 소진했습니다.");
     }
 }
