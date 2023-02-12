c945d019592e19b9df322250cc5a4bd6a6327f32
somsom13 <somin1113@naver.com>
2023-02-12 12:01:15 +0900

    refactor: 난수 생성 방식 변경

--- src/main/java/service/RandomNumberGenerator.java
+++ src/main/java/service/RandomNumberGenerator.java
@@ -1,9 +1,13 @@
 package service;
 
+import java.util.Random;
+
 public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final Random random = new Random();
 
     @Override
     public int generateNumber(final int maxNumber) {
-        return (int) (Math.random() * maxNumber);
+        return random.nextInt(maxNumber);
     }
 }
