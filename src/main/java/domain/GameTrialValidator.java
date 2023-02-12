6a72128b05e83cb0d654c4cdcc2155b06953b5aa
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-12 15:57:59 +0900

    feat(GameTrialValidator): 게임 시도 횟수를 검증하기 위한 클래스 생성

---
+++ src/main/java/domain/GameTrialValidator.java
@@ -0,0 +1,28 @@
+package domain;
+
+import validation.ErrorMessage;
+
+import java.util.regex.Pattern;
+
+public class GameTrialValidator {
+
+    private static final int MIN_TRIAL_COUNT = 1;
+    private static final String DIGIT_REGEX = "^[0-9]+$";
+
+    public static void validateTrialCount(String trialCount) {
+        checkDigit(trialCount);
+        checkTrialCount(trialCount);
+    }
+
+    private static void checkDigit(String input) {
+        if (!Pattern.matches(DIGIT_REGEX, input)) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT_NUMBER.getErrorMessage());
+        }
+    }
+
+    private static void checkTrialCount(String trailCount) {
+        if (Integer.parseInt(trailCount) < MIN_TRIAL_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT_NUMBER.getErrorMessage());
+        }
+    }
+}
