97be82febe2f5e25dffbe77b3186d7617b414700
kokodak <kokodakadokok@gmail.com>
2023-02-13 16:20:40 +0900

    feat: 시도 횟수 검증 기능 추가 및 Controller단에 적용

--- src/main/java/controller/RacingCarController.java
+++ src/main/java/controller/RacingCarController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.AttemptNumber;
 import domain.Cars;
 import dto.CarDto;
 import utils.NumberGenerator;
@@ -12,7 +13,11 @@ import java.util.List;
 
 public class RacingCarController {
 
-    private final NumberGenerator numberGenerator = new RandomNumberGenerator();
+    private final NumberGenerator numberGenerator;
+
+    public RacingCarController() {
+        this.numberGenerator = new RandomNumberGenerator();
+    }
 
     public void run() throws IOException {
         Cars cars = getCars();
@@ -31,9 +36,10 @@ public class RacingCarController {
     }
 
     private void race(final Cars cars) throws IOException {
-        int attemptNumber = InputView.readAttemptNumber();
+        AttemptNumber attemptNumber = new AttemptNumber(InputView.readAttemptNumber());
         OutputView.printResult();
-        while ((attemptNumber--) > 0) {
+        while (attemptNumber.isRemain()) {
+            attemptNumber.decrease();
             cars.moveAll(numberGenerator);
             printStatus(cars);
         }
--- src/main/java/domain/AttemptNumber.java
+++ src/main/java/domain/AttemptNumber.java
@@ -2,17 +2,29 @@ package domain;
 
 public class AttemptNumber {
 
+    private static final String NOT_POSITIVE_INTEGER_MESSAGE = "[ERROR] 시도 횟수는 양의 정수여야 합니다.";
+    private static final String INVALID_NUMBER_MESSAGE = "[ERROR] 시도 횟수는 100회 이하여야 합니다.";
+
     private int attemptNumber;
 
     public AttemptNumber(final int attemptNumber) {
         this.attemptNumber = attemptNumber;
     }
 
+    private void validate(final int attemptNumber) {
+        if (attemptNumber <= 0) {
+            throw new IllegalArgumentException(NOT_POSITIVE_INTEGER_MESSAGE);
+        }
+        if (attemptNumber > 100) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
+    }
+
     public void decrease() {
         attemptNumber--;
     }
 
-    public boolean isAttemptRemain() {
+    public boolean isRemain() {
         return attemptNumber != 0;
     }
 }
