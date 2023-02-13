e9888be7fd76ff1ecb418a7f40b078fbfd54c74e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-13 10:27:06 +0900

    style: 구분을 위한 공백 추가

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 
 public class RandomNumberGenerator {
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
     public static int pickNumberInRange(int min, int max) {
         return SECURE_RANDOM.nextInt(max - 2) + min;
     }
