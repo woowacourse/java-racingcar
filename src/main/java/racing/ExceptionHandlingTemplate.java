281eddfa84ee85f2fdc74c0556477d8f46c238e2
Kayoung Yoon <dev.kayoung@gmail.com>
2023-02-13 10:58:16 +0900

    refactor: 예외 메시지 출력 메서드를 정적 메서드로 변경

--- src/main/java/racing/ExceptionHandlingTemplate.java
+++ src/main/java/racing/ExceptionHandlingTemplate.java
@@ -5,13 +5,12 @@ import java.util.function.Supplier;
 import racing.view.OutputView;
 
 public interface ExceptionHandlingTemplate {
-    OutputView outputView  = new OutputView();
-    static  <T, R> R repeatUntilSucceed(Function<T, R> object, Supplier<T> input) {
+    static <T, R> R repeatUntilSucceed(Function<T, R> object, Supplier<T> input) {
         while (true) {
             try {
                 return object.apply(input.get());
             } catch (IllegalArgumentException e) {
-                outputView.printErrorMessage(e.getMessage());
+                OutputView.printErrorMessage(e.getMessage());
             }
         }
     }
