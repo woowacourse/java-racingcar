f887250a46b53f8f7d7ac5b59f9938e4bd0e7265
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-02-13 11:43:14 +0900

    test : moveAll에 대한 테스트 추가

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -2,11 +2,18 @@ package domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import util.NumberGenerator;
+import util.RandomNumberGenerator;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CarsTest {
     Cars cars;
@@ -44,4 +51,36 @@ class CarsTest {
     void findMaxPositionCar() {
         assertThat(cars.findMaxPositionCar()).isEqualTo(car2);
     }
+
+    @Nested
+    @DisplayName("모든 차를 전진, 혹은 멈추는 동작에 대한 테스트")
+    class MoveAll{
+        @ParameterizedTest(name = "랜덤 숫자가 4로 전진하여 {0}번째 차의 위치는 {1}")
+        @CsvSource(value = {"0:2", "1:3", "2:2"}, delimiter = ':')
+        void case1(int index, int expectedPosition) {
+            NumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
+
+            when(randomNumberGenerator.generate()).thenReturn(4);
+
+            cars.moveAll(randomNumberGenerator);
+
+            List<Car> moved = cars.getAll();
+            assertThat(moved.get(index).getPosition()).isEqualTo(expectedPosition);
+        }
+
+        @ParameterizedTest(name = "랜덤 숫자가3으로 정지하여 {0}번째 차의 위치는 {1}")
+        @CsvSource(value = {"0:1", "1:2", "2:1"}, delimiter = ':')
+        void case2(int index, int expectedPosition) {
+            NumberGenerator randomNumberGenerator = mock(RandomNumberGenerator.class);
+
+            when(randomNumberGenerator.generate()).thenReturn(3);
+
+            cars.moveAll(randomNumberGenerator);
+
+            List<Car> moved = cars.getAll();
+            assertThat(moved.get(index).getPosition()).isEqualTo(expectedPosition);
+        }
+    }
+
+
 }
