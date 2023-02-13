77792e1f515fb8a9cea54858f8c4bb375632231d
Choi-JJunho <junho5336@gmail.com>
2023-02-13 17:08:16 +0900

    refactor: wrapping TryCount

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -9,25 +9,19 @@ public class RacingGame {
 
     private final RandomNumberGenerator numberGenerator;
     private final Cars cars;
-    private int tryCount;
+    private final TryCount tryCount;
 
     public RacingGame(final RandomNumberGenerator numberGenerator, final Cars cars, final int tryCount) {
         this.cars = cars;
-        this.tryCount = tryCount;
+        this.tryCount = new TryCount(tryCount);
         this.numberGenerator = numberGenerator;
     }
 
     public void play() {
         cars.moveAll(numberGenerator);
-        validateTryCount(tryCount);
-        tryCount--;
+        tryCount.decreaseCount();
     }
 
-    private void validateTryCount(final int tryCount) {
-        if (tryCount <= 0) {
-            throw new IllegalArgumentException("실행 횟수 보다 많이 실행할 수 없습니다.");
-        }
-    }
 
     public List<Car> decideResult() {
         return cars.getUnmodifiableCars();
@@ -40,6 +34,6 @@ public class RacingGame {
     }
 
     public boolean isEnd() {
-        return tryCount <= 0;
+        return tryCount.isEnd();
     }
 }
