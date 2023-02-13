9c2dfbef14f8200073e768f2cc44add539d8abdd
hanueleee <ezzeezz@naver.com>
2023-02-13 17:16:16 +0900

    refactor: 전진테스트 수정

--- src/test/java/racingcar/domain/RoundManagerTest.java
+++ src/test/java/racingcar/domain/RoundManagerTest.java
@@ -5,28 +5,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.dto.RacingCarDto;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 class RoundManagerTest {
     @ParameterizedTest
     @MethodSource("전진_결과_데이터")
     @DisplayName("range범위에 따른 전진 결과 테스트")
-    public void 전진_결과_테스트(int num, List<String> expected) {
+    public void 전진_결과_테스트(int num, int expectedPosition) {
         Range range = new Range(4, 9);
         NumberGenerator numberGenerator = new DefaultNumberGenerator(num);
         AdvanceJudgement advanceJudgement = new AdvanceJudgement(range, numberGenerator);
 
         RoundManager roundManager = new RoundManager(advanceJudgement);
         roundManager.addRacingCar(new RacingCar("car"));
-        Assertions.assertEquals(expected, roundManager.runRound());
+
+        RacingCarDto racingCarDto = roundManager.runRound().get(0);
+        Integer position = racingCarDto.getPosition();
+
+        Assertions.assertEquals(expectedPosition, position);
     }
 
     static Stream<Arguments> 전진_결과_데이터() {
         return Stream.of(
-                Arguments.of(1, List.of("car : -")),
-                Arguments.of(9, List.of("car : --"))
+                Arguments.of(1, 0),
+                Arguments.of(9, 1)
         );
     }
 }
