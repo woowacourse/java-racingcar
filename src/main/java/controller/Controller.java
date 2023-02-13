9e79c66e9dff23c6473f94cf717af307733edca7
kiarakim <kiarakim413@gmail.com>
2023-02-13 16:02:15 +0900

    refactor : 경주 게임 메서드 분리

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,7 +3,6 @@ package controller;
 import domain.Cars;
 import domain.Trial;
 import domain.Winner;
-import util.RandomNumberMaker;
 import view.InputView;
 import view.OutputView;
 
@@ -14,16 +13,14 @@ public class Controller {
 	public void run() {
 		Cars cars = inputView.askCars();
 		Trial trial = inputView.askTrial();
-		for (int i = 0; i < trial.getTrial(); i++) {
-			printRound(cars);
-		}
+		startRacing(cars, trial);
 		outputView.printWinner(new Winner(cars));
 	}
 
-	private void printRound(Cars cars) {
-		for (int j = 0; j < cars.getCarsSize(); j++) {
-			cars.addDistance(RandomNumberMaker.makeNumber(), j);
+	private void startRacing(Cars cars, Trial trial) {
+		for (int i = 0; i < trial.getTrial(); i++) {
+			cars.move();
+			outputView.printCarsDistance(cars);
 		}
-		outputView.printCarsDistance(cars);
 	}
 }
