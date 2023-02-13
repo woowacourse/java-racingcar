55cbbb7f44a0029b1ee7f303157a08fdf11be9df
jyeost <pig9619@naver.com>
2023-02-13 17:14:20 +0900

    refactor : 테스트가 통과하게 고치고, BeforEach와 DiplayName 어노테이션을 추가하였다

--- src/test/java/domain/WinnerTest.java
+++ src/test/java/domain/WinnerTest.java
@@ -4,26 +4,34 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinnerTest {
+    private final List<String> names = List.of("name1", "name2", "name3");
+    private Cars cars;
+
+    @BeforeEach()
+    void init() {
+        cars = new Cars(names);
+    }
+
+    @DisplayName("name1, name2, name3 각 0, 1, 2의 distance 가 예상될때 name3만 우승하는 지 알아본다")
     @Test
-    void checkWinner() {
-        List<String> names = List.of("aa", "bb", "cc");
-        Cars cars = new Cars(names);
+    void checkOneWinner() {
         cars.addDistance(List.of(2, 5, 7));
         cars.addDistance(List.of(1, 2, 7));
         Winner winner = new Winner(cars);
-        assertThat(String.join("", winner.getWinners())).isEqualTo("ff");
+        assertThat(winner.getWinners().containsAll(List.of("name3"))).isTrue();
     }
 
+    @DisplayName("name1, name2, name3 각 0, 1, 2의 distance 가 예상될때 name2와 name3가 공동우승하는 지 알아본다")
     @Test
     void checkTwoWinners() {
-        List<String> names = List.of("aa", "bb", "cc");
-        Cars cars = new Cars(names);
         cars.addDistance(List.of(2, 5, 7));
         cars.addDistance(List.of(1, 8, 7));
         Winner winner = new Winner(cars);
-        assertThat(String.join(",", winner.getWinners())).isEqualTo("bb,ff");
+        assertThat(winner.getWinners().containsAll(List.of("name2", "name3"))).isTrue();
     }
 }
