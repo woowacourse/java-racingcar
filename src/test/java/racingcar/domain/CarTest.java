24c39b55c1138f7d0e1041f3e28448b2b893c000
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-13 13:40:16 +0900

    test: 메서드 이름 네이밍 컨벤션 적용

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -17,7 +17,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> name : ''{0}''")
         @ValueSource(strings = {"", "abcdef", "pobiiiiii"})
         @DisplayName("자동차 이름의 길이가 0 이하거나 5보다 클 경우 예외 발생")
-        void Should_ThrowException_길이가_0이하거나_5보다_큰_경우(String name) {
+        void Should_ThrowException_When_NameMoreThan6(String name) {
             assertThatThrownBy(() -> new Car(name, 0))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("[ERROR] 자동차 이름의 길이는 1부터 5 사이여야 합니다.");
@@ -26,7 +26,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> name : ''{0}''")
         @ValueSource(strings = {"a", "abc", "abcde"})
         @DisplayName("자동차 이름의 길이가 1 이상 5 이하인 경우 성공")
-        void Should_Success_길이가_1이상_5이하인_경우(String name) {
+        void Should_Success_When_NameLessThan5(String name) {
             assertDoesNotThrow(() -> new Car(name, 0));
         }
     }
@@ -37,7 +37,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> name : ''{0}''")
         @ValueSource(strings = {" ", "abc!", "123* "})
         @DisplayName("자동차 이름이 영숫자가 아닌 경우 예외 발생")
-        void Should_ThrowException_이름이_영숫자가_아닌_경우(String name) {
+        void Should_ThrowException_When_NameIsNotEnglishAndNumber(String name) {
             assertThatThrownBy(() -> new Car(name, 0))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("[ERROR] 자동차 이름은 영숫자로 이루어져야 합니다.");
@@ -46,7 +46,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> name : ''{0}''")
         @ValueSource(strings = {"a1", "a2cd", "ab34e"})
         @DisplayName("자동차 이름이 영숫자인 경우 성공")
-        void Should_Success_이름이_영숫자인_경우(String name) {
+        void Should_Success_When_NameIsEnglishAndNumber(String name) {
             assertDoesNotThrow(() -> new Car(name, 0));
         }
     }
@@ -60,7 +60,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> engine : ''{0}''")
         @ValueSource(ints = {0, 2, 3})
         @DisplayName("랜덤 값이 4보다 작은 경우 멈춤")
-        void Should_Success_랜덤값이_4보다_작은_경우(int number) {
+        void Should_Success_When_EngineLessThan4(int engine) {
             carService.runForward(car, number);
             assertThat(car.getDistance()).isEqualTo(0);
         }
@@ -68,7 +68,7 @@ class CarTest {
         @ParameterizedTest(name = "{displayName} {index} ==> engine : ''{0}''")
         @ValueSource(ints = {4, 7})
         @DisplayName("랜덤 값이 4 이상인 경우 전진")
-        void Should_Success_랜덤값이_4이상인_경우(int number) {
+        void Should_Success_When_EngineMoreThan4(int number) {
             carService.runForward(car, number);
             assertThat(car.getDistance()).isEqualTo(1);
         }
