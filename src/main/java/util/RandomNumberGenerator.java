8e75b13b45741782d00d6013451318b0b804cfcf
hectick <chyeon1223@cau.ac.kr>
2023-02-13 22:23:50 +0900

    refactor(RandomNumberGenerator): 필드 변수 추가

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -5,11 +5,12 @@ import java.security.SecureRandom;
 public class RandomNumberGenerator {
     private static final int MAX_NUMBER = 9;
     private static final int BOUNDARY_NUMBER = MAX_NUMBER + 1;
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     private RandomNumberGenerator() {
     }
 
     public static int getRandomNumber() {
-        return new SecureRandom().nextInt(BOUNDARY_NUMBER);
+        return secureRandom.nextInt(BOUNDARY_NUMBER);
     }
 }
