89971a7fcf50958bb10882ba349fa4aa89cb30b9
seokjin8678 <rnrn8678@naver.com>
2023-02-13 16:35:34 +0900

    test: TryCount 테스트 코드 추가

--- src/test/java/racingcar/controller/TryCountTest.java
+++ src/test/java/racingcar/controller/TryCountTest.java
@@ -1,9 +1,12 @@
 package racingcar.controller;
 
 import static org.assertj.core.api.Assertions.*;
+import static racingcar.exception.ExceptionMessage.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TryCountTest {
 
@@ -14,12 +17,12 @@ class TryCountTest {
         TryCount tryCount = new TryCount(5);
 
         // when
-        while (tryCount.countdown()){
-            // do something
+        while (tryCount.isRemaining()) {
+            tryCount.countdown();
         }
 
         // then
-        assertThat(tryCount.countdown())
+        assertThat(tryCount.isRemaining())
                 .isFalse();
     }
 
@@ -29,11 +32,26 @@ class TryCountTest {
         // given
         TryCount tryCount = new TryCount(5);
 
-        // when
-        tryCount.countdown();
-
-        // then
-        assertThat(tryCount.countdown())
+        // expect
+        assertThat(tryCount.isRemaining())
                 .isTrue();
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 음수가 주어지면 예외가 발생해야 한다.")
+    void create_minusInput() {
+        // expect
+        assertThatThrownBy(() -> new TryCount(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ILLEGAL_TRY_COUNT.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("시도 횟수가 정상적으로 생성되어야 한다.")
+    @ValueSource(ints = {0, 1, 2, Integer.MAX_VALUE})
+    void create_success(int input) {
+        // expect
+        assertThatCode(() -> new TryCount(input))
+                .doesNotThrowAnyException();
     }
 }
