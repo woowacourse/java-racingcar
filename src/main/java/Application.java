6dd2f31c23698faceae655af55559a492d7f578e
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-13 10:07:30 +0900

    feat: config 객체 삭제 및 시작 포인트에 설정 메소드 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,40 @@
-import config.CarRaceConfig;
+import controller.CarRaceController;
+import repository.CarRaceResultRepositoryImpl;
+import service.CarRaceService;
+import service.CarRaceServiceImpl;
+import service.RandomNumberGenerator;
+import validation.InputValidationChain;
+import validation.impl.EmptyValueValidationChain;
+import validation.impl.InputValidateSuccessChain;
+import validation.impl.NumberRangeValidationChain;
+import validation.impl.PositiveNumberValidationChain;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        CarRaceConfig.config().start();
+        config().start();
+    }
+
+    private static CarRaceController config() {
+        return new CarRaceController(new InputView(), new OutputView(),
+            makeValidator(), makeService());
+    }
+
+    private static CarRaceService makeService() {
+        return new CarRaceServiceImpl(new CarRaceResultRepositoryImpl(),
+            new RandomNumberGenerator());
+    }
+
+    private static InputValidationChain makeValidator() {
+        final InputValidationChain emptyValueValidationChain = new EmptyValueValidationChain();
+        final InputValidationChain numberRangeValidationChain = new NumberRangeValidationChain();
+        final InputValidationChain positiveNumberValidationChain = new PositiveNumberValidationChain();
+        final InputValidationChain inputValidationChain = new InputValidateSuccessChain();
+        emptyValueValidationChain.setNext(numberRangeValidationChain);
+        numberRangeValidationChain.setNext(positiveNumberValidationChain);
+        positiveNumberValidationChain.setNext(inputValidationChain);
+        return emptyValueValidationChain;
     }
 }
