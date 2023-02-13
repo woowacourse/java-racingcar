2dc212bc08eaebec53c4425a7bbc66279e450443
Jae-Baek Song <83541246+thdwoqor@users.noreply.github.com>
2023-02-13 21:24:19 +0900

    [2단계 - 자동차 경주 리팩터링] 파워(송재백) 미션 제출합니다. (#571)
    * refactor : 사용 하지 않는 상수 제거
    
    * refactor : `InputView` 결과값을 `RacingController`에 주입하는 방식으로 변경
    
    * refactor : 자바 API를 테스트 할 필요가 없음으로 삭제
    
    * refactor : 사용하지 않는 테스트코드 제거
    
    * refactor : `InputView` 와 `OutputView`를 주입하는 방식으로 변경
    
    * refactor : `Car` 클래스가 스스로 이동을 결정할수 있게  랜덤 객체를 `Car` 로 이동
    
    * refactor : `Car` 클래스 변경에 따른 테스트 클래스 수정
    
    * refactor : 	자바 코드 컨벤션 공백 추가
    
    * refactor : 사용하지 않는 클래스 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,18 @@
 import controller.RacingController;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         try {
-            new RacingController().start();
+            final RacingController racingGame = new RacingController(
+                    new InputView(),
+                    new OutputView()
+            );
+
+            racingGame.race();
+
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -2,54 +2,42 @@ package controller;
 
 import model.Car;
 import model.Cars;
-import model.RandomGenerator;
 import view.InputView;
 import view.OutputView;
 
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RacingController {
-    public static final int START_POSITION = 0;
 
     private final InputView inputView;
     private final OutputView outputView;
     private Cars cars;
 
-    public RacingController() {
-        inputView = new InputView(System.in);
-        outputView = new OutputView();
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        initializeCars();
     }
 
-    public void start() {
-        prepareRacing();
-        process();
-        end();
-    }
-
-    private void prepareRacing() {
-        cars = new Cars(inputView.inputCarNames()
+    private void initializeCars() {
+        this.cars = new Cars(inputView.getCarNames()
                 .stream()
-                .map(name -> new Car(name, new RandomGenerator(new Random())))
+                .map(name -> new Car(name))
                 .collect(Collectors.toUnmodifiableList())
         );
     }
 
-    private void process() {
-        int numberOfTimes = inputView.inputRepeatCount();
+    public void race() {
         outputView.resultHeader();
-        IntStream.range(0, numberOfTimes)
+        IntStream.range(0, inputView.getTryCount())
                 .forEach(this::repeat);
+        outputView.result(cars);
+        outputView.winner(cars.getWinner());
     }
 
     private void repeat(int count) {
         cars.moveAll();
         outputView.result(cars);
-    }
-
-    private void end() {
-        outputView.result(cars);
-        outputView.winner(cars.getWinner());
     }
 }
