fe23527aa32654505369ae5991326ad3c847b39d
hectick <chyeon1223@cau.ac.kr>
2023-02-13 23:16:23 +0900

    test(RandomNumberValidator): 테스트 코드 작성

--- src/test/java/util/RandomNumberGeneratorTest.java
+++ src/test/java/util/RandomNumberGeneratorTest.java
@@ -1,11 +1,13 @@
 package util;
 
+import model.manager.ThresholdCarMoveManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class RandomNumberGeneratorTest {
 
@@ -13,7 +15,8 @@ class RandomNumberGeneratorTest {
     @DisplayName("랜덤 값이 0 이상 9 이하로 생성되는지 테스트한다.")
     void randomNumberTest() {
         int number = RandomNumberGenerator.getRandomNumber();
-        assertThat(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).contains(number)).isTrue();
+        assertThatCode(() -> RandomNumberValidator.validate(number))
+                .doesNotThrowAnyException();
     }
 
 }
