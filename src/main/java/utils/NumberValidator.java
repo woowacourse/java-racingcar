a623db3922f5b70c235f1384addbfc14c8d2108d
kokodak <kokodakadokok@gmail.com>
2023-02-13 17:06:41 +0900

    fix: NumberValidator가 숫자가 아닌 정수를 검증하도록 수정

--- src/main/java/utils/NumberValidator.java
+++ src/main/java/utils/NumberValidator.java
@@ -2,13 +2,13 @@ package utils;
 
 public class NumberValidator {
 
-    private static final String NOT_NUMBER_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+    private static final String NOT_INTEGER_MESSAGE = "[ERROR] 시도 횟수는 정수여야 합니다.";
 
     public static int parseInt(final String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NUMBER_MESSAGE, e);
+            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE, e);
         }
     }
 }
--- src/test/java/utils/NumberValidatorTest.java
+++ src/test/java/utils/NumberValidatorTest.java
@@ -9,19 +9,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberValidatorTest {
 
-    @DisplayName("양의 정수가 아닌 값이 들어오면 예외를 던진다")
+    @DisplayName("정수가 아닌 값이 들어오면 예외를 던진다")
     @ParameterizedTest
-    @CsvSource({"''", "abc", "-1", "0", "3.3"})
-    void should_ThrowIllegalArgumentException_When_InputIsNotPositiveInteger(String input) {
+    @CsvSource({"''", "abc", ".//.//", "3.3"})
+    void should_ThrowIllegalArgumentException_When_InputIsNotInteger(String input) {
         assertThatThrownBy(() -> NumberValidator.parseInt(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
 
-    @DisplayName("양의 정수가 들어오면 예외를 던지지 않는다")
+    @DisplayName("정수가 들어오면 예외를 던지지 않는다")
     @ParameterizedTest
-    @CsvSource({"1", "5", "1000"})
-    void should_DoesNotThrowException_When_InputIsPositiveInteger(String input) {
+    @CsvSource({"1", "-3", "0"})
+    void should_DoesNotThrowException_When_InputIsInteger(String input) {
         assertThatCode(() -> NumberValidator.parseInt(input))
                 .doesNotThrowAnyException();
     }
