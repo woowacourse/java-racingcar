b45b92356ca9aa578c17e6713ce04aa380fd195d
kiarakim <kiarakim413@gmail.com>
2023-02-13 16:08:51 +0900

    refactor : 우승자 확인 및 중복 가능 확인

--- src/test/java/domain/WinnerTest.java
+++ src/test/java/domain/WinnerTest.java
@@ -10,40 +10,28 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("우승자")
 class WinnerTest {
-	Cars cars = new Cars(new ArrayList<>(List.of(
-		new Car("benz"),
-		new Car("honda"),
-		new Car("audi")
-	)));
+	static Cars cars;
 
 	@DisplayName("를 확인합니다.")
 	@Test
 	void checkWinner() {
-
-		cars.addDistance(8, 0);
-		cars.addDistance(3, 1);
-		cars.addDistance(5, 2);
-
-		cars.addDistance(7, 0);
-		cars.addDistance(2, 1);
-		cars.addDistance(0, 2);
-
+		cars = new Cars(new ArrayList<>(List.of(
+			new Car("benz", 3)
+		)));
 		Winner winner = new Winner(cars);
-		assertThat(String.join("", winner.getWinners())).isEqualTo("benz");
+		assertThat(winner.getWinners()).isEqualTo(List.of("benz"));
 	}
 
 	@DisplayName("는 여러명일 수 있습니다.")
 	@Test
 	void checkManyWinners() {
-		cars.addDistance(8, 0);
-		cars.addDistance(3, 1);
-		cars.addDistance(5, 2);
-
-		cars.addDistance(7, 0);
-		cars.addDistance(2, 1);
-		cars.addDistance(9, 2);
+		cars = new Cars(new ArrayList<>(List.of(
+			new Car("benz", 3),
+			new Car("honda", 3),
+			new Car("audi", 1)
+		)));
 
 		Winner winner = new Winner(cars);
-		assertThat(String.join(",", winner.getWinners())).isEqualTo("benz,audi");
+		assertThat(winner.getWinners()).isEqualTo(List.of("benz", "honda"));
 	}
 }
