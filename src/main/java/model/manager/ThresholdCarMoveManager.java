00b0862755beeada4a91d20feb32bd720f099594
hectick <chyeon1223@cau.ac.kr>
2023-02-13 23:09:14 +0900

    feat(RandomNumberValidator): 생성된 랜덤 숫자가 0이상 9이하가 아니면 예외처리

--- src/main/java/model/manager/ThresholdCarMoveManager.java
+++ src/main/java/model/manager/ThresholdCarMoveManager.java
@@ -1,10 +1,14 @@
 package model.manager;
 
+import util.RandomNumberValidator;
+
 public class ThresholdCarMoveManager implements CarMoveManager {
     private static final int BOUNDARY_VALUE = 4;
 
     @Override
     public boolean isMove(int number) {
+        RandomNumberValidator.validate(number);
         return number >= BOUNDARY_VALUE;
     }
+
 }
