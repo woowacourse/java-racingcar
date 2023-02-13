43a0595e208185f9021f33a7bec10041db251acf
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-13 17:47:13 +0900

    refactor: 도메인 영역에서 출력 역할 분리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,8 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.RacingCarContext;
+import racingcar.model.car.Car;
 import racingcar.model.car.CarFactory;
 import racingcar.model.car.Cars;
 import racingcar.model.track.Track;
@@ -9,8 +12,10 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 import racingcar.view.dto.CarNamesRequest;
 import racingcar.view.dto.TrialTimesRequest;
+import racingcar.view.formatter.track.TrackStateFormatter;
 
 public class RacingController {
+    private static final TrackStateFormatter TRACK_FORMATTER = RacingCarContext.getTrackStateFormatter();
     private final InputView inputView;
     private final OutputView outputView;
     private final CarFactory carFactory;
@@ -41,15 +46,22 @@ public class RacingController {
     }
 
     public void startRace(Track track) {
-        outputView.printOutputFormat(track.carsPositionFormat());
+        Cars carsOnTrack = track.getCars();
+        printInitialCarsPositions(carsOnTrack);
         while (track.runnable()) {
             track.race();
-            outputView.printOutputFormat(track.carsPositionFormat());
+            String carsPositionFormat = TRACK_FORMATTER.formatCarsPosition(carsOnTrack);
+            outputView.printOutputFormat(carsPositionFormat);
         }
     }
 
+    private void printInitialCarsPositions(Cars cars) {
+        outputView.printOutputFormat(TRACK_FORMATTER.formatCarsPosition(cars));
+    }
+
     public void concludeWinner(Track track) {
-        outputView.printOutputFormat(track.formatWinningCars());
+        List<Car> winningCars = track.getCars().getCars();
+        outputView.printOutputFormat(TRACK_FORMATTER.formatWinnerCars(winningCars));
     }
 
     public void terminatedByException(String errorMessage) {
--- src/main/java/racingcar/model/track/Track.java
+++ src/main/java/racingcar/model/track/Track.java
@@ -1,13 +1,10 @@
 package racingcar.model.track;
 
-import racingcar.RacingCarContext;
 import racingcar.model.car.Cars;
-import racingcar.view.formatter.track.TrackStateFormatter;
 import racingcar.model.trialtimes.TrialTimes;
 
 public class Track {
     private static final int END_CONDITION = 0;
-    private static final TrackStateFormatter FORMATTER = RacingCarContext.getTrackStateFormatter();
 
     private final Cars cars;
     private final TrialTimes trialTimes;
@@ -26,11 +23,7 @@ public class Track {
         return trialTimes.isGreaterThan(END_CONDITION);
     }
 
-    public String carsPositionFormat() {
-        return FORMATTER.formatCarsPosition(cars);
-    }
-
-    public String formatWinningCars() {
-        return FORMATTER.formatWinnerCars(cars.getWinnerCars());
+    public Cars getCars() {
+        return cars;
     }
 }
