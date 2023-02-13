b0a4953cdacea0653cc6e54ed23f000ce85ea0bc
seokjin8678 <rnrn8678@naver.com>
2023-02-13 16:55:08 +0900

    test: Position 테스트 코드 추가

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -1,9 +1,12 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static racingcar.exception.ExceptionMessage.ILLEGAL_POSITION;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PositionTest {
 
@@ -21,7 +24,7 @@ class PositionTest {
         assertThat(position1)
                 .isEqualTo(position2);
     }
-    
+
     @Test
     @DisplayName("값이 다르면 거짓이 반환되어야 한다.")
     void position_differentValueNotEquals() {
@@ -46,5 +49,23 @@ class PositionTest {
         // then
         assertThat(position.getValue())
                 .isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("위치에 음수가 주어지면 예외가 발생해야 한다.")
+    void create_minusInput() {
+        // expect
+        assertThatThrownBy(() -> new Position(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ILLEGAL_POSITION.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("위치가 정상적으로 생성되어야 한다.")
+    @ValueSource(ints = {0, 1, 2, Integer.MAX_VALUE})
+    void create_success(int input) {
+        // expect
+        assertThatCode(() -> new Position(input))
+                .doesNotThrowAnyException();
     }
 }
