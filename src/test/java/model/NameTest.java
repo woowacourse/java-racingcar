42753712883264f7d3bcc94d2bb1a5a5ea8c2fb9
somsom13 <somin1113@naver.com>
2023-02-13 23:39:07 +0900

    test: Name 생성 테스트

---
+++ src/test/java/model/NameTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "가나다라마바"})
+    @DisplayName("차 이름 길이가 범위 밖인 경우 예외가 발생한다")
+    void createCarWithNameOutOfRange(String name) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Name(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "가나다라마"})
+    @DisplayName("차 이름 길이가 범위 내인 경우 통과한다")
+    void createCarWithCorrectNameRange(String name) {
+        //given
+        //when
+        new Name(name);
+        //then
+    }
+
+}
