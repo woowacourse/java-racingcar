94fbd6d01620479c72727b736fcb3f497064c32c
swonny <wonny921@gmail.com>
2023-02-13 11:24:44 +0900

    test: CarNameValidator, CarRepositoryValidator에 정상적으로 실행되는 테스트 추가

--- src/test/java/validator/CarNameValidatorTest.java
+++ src/test/java/validator/CarNameValidatorTest.java
@@ -1,5 +1,6 @@
 package validator;
 
+import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CarNameValidatorTest {
 
     @ParameterizedTest(name = "carName : {0}")
+    @ValueSource(strings = {"a", "aa" , "aaa", "aaaa", "aaaaa"})
+    @DisplayName("자동차 이름이 1글자 이상 5글자 이하인 경우 예외처리 없이 정상적으로 실행된다.")
+    void validate(String carName) {
+        Assertions.assertThatNoException()
+                .isThrownBy(() ->CarNameValidator.validate(carName));
+    }
+
+    @ParameterizedTest(name = "carName : {0}")
     @NullAndEmptySource
-    @DisplayName("자동차 이름 입력이 비어있는 경우 예외처리")
+    @DisplayName("자동차 이름 입력이 비어있는 경우 예외처리한다.")
     void validateNullAndEmpty(String carName) {
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> CarNameValidator.validate(carName));
     }
 
     @Test
-    @DisplayName("자동차 이름의 길이 범위 초과 시 예외처리")
+    @DisplayName("자동차 이름의 길이 범위 초과 시 예외처리한다.")
     void validateOutOfLength() {
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> CarNameValidator.validate("dddddd"));
@@ -26,7 +35,7 @@ class CarNameValidatorTest {
 
     @ParameterizedTest(name = "carName : {0}")
     @ValueSource(strings = {"aa, b,cc", "aa,b ,cc"})
-    @DisplayName("이름에는 공백을 입력 시 예외처리")
+    @DisplayName("이름에는 공백을 입력 시 예외처리한다.")
     void validateOutOfRange(String carName) {
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> CarNameValidator.validate(carName));
--- src/test/java/validator/CarRepositoryValidatorTest.java
+++ src/test/java/validator/CarRepositoryValidatorTest.java
@@ -9,6 +9,14 @@ import java.util.List;
 
 public class CarRepositoryValidatorTest {
     @Test
+    @DisplayName("입력한 차가 한 대 이상이며 이름이 중복되지 않은 경우 정상적으로 실행한다.")
+    void addToCarRepository() {
+        List<Car> cars = List.of(new Car("aa"), new Car("bb"), new Car("cc"));
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> CarRepositoryValidator.validate(cars));
+    }
+
+    @Test
     @DisplayName("중복되는 차 이름이 있을 때 예외를 발생시킨다.")
     void validateDuplicatedCarNames() {
         List<Car> cars = List.of(new Car("aa"), new Car("aa"));
