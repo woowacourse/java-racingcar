9a92b51ea97006bc052dd3780c3e101806bd8891
70825 <hello70825@gmail.com>
2023-02-13 16:30:30 +0900

    fix: 시도한 횟수만큼 진행하는지 확인하는 테스트가 통과하지 못하는 것을 수정

--- src/test/java/racingcar/TestDataManager.java
+++ src/test/java/racingcar/TestDataManager.java
@@ -12,9 +12,9 @@ public class TestDataManager {
         position = 1 인 crong, 을 포함한 Cars를 반환
      */
     public static Cars getCarsTestData() {
-        TestMovingStrategy pobiTestMovingStrategy = new TestMovingStrategy(new int[]{5, 8, 7});
-        TestMovingStrategy crongTestMovingStrategy = new TestMovingStrategy(new int[]{1, 2, 4});
-        TestMovingStrategy eddyTestMovingStrategy = new TestMovingStrategy(new int[]{6, 4, 8});
+        TestMovingStrategy pobiTestMovingStrategy = new TestMovingStrategy(new int[]{5, 8, 7, 3});
+        TestMovingStrategy crongTestMovingStrategy = new TestMovingStrategy(new int[]{1, 2, 4, 5});
+        TestMovingStrategy eddyTestMovingStrategy = new TestMovingStrategy(new int[]{6, 4, 8, 1});
 
         Car pobi = new Car("pobi", pobiTestMovingStrategy);
         Car crong = new Car("crong", crongTestMovingStrategy);
