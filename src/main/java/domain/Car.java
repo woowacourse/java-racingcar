e5d919c7b40c60c110160934d62e7882844e8bc7
xxeol2 <jhj01177@naver.com>
2023-02-15 00:01:09 +0900

    feat: 전진 판별 숫자 유효성 검사 로직 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -6,6 +6,8 @@ public class Car {
 
     private static final int DRIVING_DISTANCE = 1;
     private static final int MOVING_STANDARD = 4;
+    private static final int NUMBER_MIN_INCLUSIVE = 0;
+    private static final int NUMBER_MAX_INCLUSIVE = 9;
 
     private final CarName name;
     private int drivenDistance = 0;
@@ -24,7 +26,17 @@ public class Car {
     }
 
     private int chooseNumber() {
-        return numberGenerator.generate();
+        int number = numberGenerator.generate();
+        validateNumber(number);
+        return number;
+    }
+
+    private void validateNumber(final int number) {
+        final String NOT_PROPER_NUMBER_RANGE = "차량 전진 판별 숫자는 0이상 9이하여야합니다.";
+
+        if (number < NUMBER_MIN_INCLUSIVE || number > NUMBER_MAX_INCLUSIVE) {
+            throw new IllegalArgumentException(NOT_PROPER_NUMBER_RANGE);
+        }
     }
 
     public int getDrivenDistance() {
