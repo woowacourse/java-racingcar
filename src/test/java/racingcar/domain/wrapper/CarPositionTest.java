6a627528bc368047bf4a469b1e389684419c7960
jiwon <ljwon77@daum.net>
2023-02-14 15:45:42 +0900

    test: CarPosition에 대한 테스트 추가
    - 위치값 증가 메서드에 대해서 테스트를 추가하였습니다.

--- src/test/java/racingcar/domain/wrapper/CarPositionTest.java
+++ src/test/java/racingcar/domain/wrapper/CarPositionTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain.wrapper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,18 +9,38 @@ import static racingcar.domain.constant.CarConstant.INIT_POSITION;
 
 class CarPositionTest {
 
+    private CarPosition carPosition;
+
+    @BeforeEach
+    void init() {
+        carPosition = CarPosition.create();
+    }
+
     @Test
     @DisplayName("초기 CarPosition 객체 생성 시 position은 1이어야 한다.")
     void givenNormalPosition_thenSuccess() {
         // given, when
-        CarPosition carPosition = CarPosition.create();
         int position = carPosition.getPosition();
 
         // then
         assertThat(carPosition)
-                .isIn(CarPosition.class);
+                .isInstanceOf(CarPosition.class);
 
         assertThat(position)
                 .isEqualTo(INIT_POSITION.getValue());
+    }
+
+    @Test
+    @DisplayName("위치 값을 1 증가시킨다.")
+    void whenAddPosition_thenPositionPlus() {
+        // given
+        int beforePosition = carPosition.getPosition();
+
+        // when
+        carPosition.addPosition();
+
+        // then
+        assertThat(carPosition.getPosition())
+                .isEqualTo(beforePosition + 1);
     }
 }
