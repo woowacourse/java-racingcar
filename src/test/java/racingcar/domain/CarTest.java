e1a3f745e167aec036c4b100b9154c94b7ea1ebd
Woojin Lee <dhkep03@gmail.com>
2023-02-14 09:51:03 +0900

    test: 자동차 이동 테스트 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.dto.CarDto;
 import racingcar.util.DeterminedIntGenerator;
 
 import static org.assertj.core.api.Assertions.*;
@@ -15,16 +14,13 @@ class CarTest {
     @ParameterizedTest
     @DisplayName("이동 시도 시, 정수 값에 따라 이동 횟수가 달라진다.")
     @CsvSource(value = {"1:0", "3:0", "4:1", "9:1"}, delimiter = ':')
-    void shouldPlusOneAtMovedCountWhenMove(int number, int addedMovedCount) {
+    void shouldPlusOneAtMovedCountWhenMove(int number, int movedCount) {
         // given
         Car targetCar = new Car("test", new DeterminedIntGenerator(number));
-        CarDto carDtoBeforeMove = new CarDto(targetCar);
         // when
         targetCar.tryMove();
-        CarDto carDtoAfterMove = new CarDto(targetCar);
         // then
-        assertThat(carDtoAfterMove.getName()).isEqualTo(carDtoBeforeMove.getName());
-        assertThat(carDtoAfterMove.getMovedCount()).isEqualTo(carDtoBeforeMove.getMovedCount() + addedMovedCount);
+        assertThat(targetCar.getMovedCount()).isEqualTo(movedCount);
     }
 
     @Test
