8626ee9464b438932ccdf92f46f8ca4afd6481d2
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-14 11:26:29 +0900

    feat(ExceptionMessage): 예외 메시지 추가
    우승자 선정에 있어 문제가 생겼을 때에 대한 예외 추가

--- src/main/java/constant/ExceptionMessage.java
+++ src/main/java/constant/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     CAR_NAME_BLANK_MESSAGE("자동차 이름은 공백만 입력하는 것이 불가능합니다."),
     TRY_COUNT_NOT_NUMBER_MESSAGE("시도 횟수는 숫자로만 입력하여야 합니다."),
     TRY_COUNT_NOT_POSITIVE_MESSAGE("시도 횟수는 1이상 입력하여야 합니다."),
-    DUPLICATION_CAR_NAME_MESSAGE("자동차 이름 중 중복되는 값이 없어야 합니다.");
+    DUPLICATION_CAR_NAME_MESSAGE("자동차 이름 중 중복되는 값이 없어야 합니다."),
+    GET_MAX_LOCATION_MESSAGE("우승자 선정 중 문제가 발생했습니다.");
 
     private static final String TAG = "[ERROR] ";
     private final String exceptionMessage;
