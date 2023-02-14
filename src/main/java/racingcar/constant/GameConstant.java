b91a195bbc39267947f025a0724db8799cdd198d
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-14 11:22:39 +0900

    refactor : 게임 관련 상수 제거

--- src/main/java/racingcar/constant/GameConstant.java
+++
@@ -1,8 +0,0 @@
-package racingcar.constant;
-
-public class GameConstant {
-    public static final int RACE_START_POINT = 0;
-    public static final int MINIMUM_NUMBER_OF_ENGINE = 0;
-    public static final int MAXIMUM_NUMBER_OF_ENGINE = 9;
-    public static final int MINIMUM_NUMBER_TO_MOVE = 4;
-}
--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
-import static racingcar.constant.GameConstant.MAXIMUM_NUMBER_OF_ENGINE;
-import static racingcar.constant.GameConstant.MINIMUM_NUMBER_OF_ENGINE;
-
 public class RandomNumberGenerator implements NumberGenerator {
+    private final int MINIMUM_NUMBER_OF_ENGINE;
+    private final int MAXIMUM_NUMBER_OF_ENGINE;
+
+    public RandomNumberGenerator(int MINIMUM_NUMBER_OF_ENGINE, int MAXIMUM_NUMBER_OF_ENGINE) {
+        this.MINIMUM_NUMBER_OF_ENGINE = MINIMUM_NUMBER_OF_ENGINE;
+        this.MAXIMUM_NUMBER_OF_ENGINE = MAXIMUM_NUMBER_OF_ENGINE;
+    }
 
     @Override
     public int generate() {
