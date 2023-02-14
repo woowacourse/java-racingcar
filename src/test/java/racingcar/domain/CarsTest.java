bc7ed5ce595e46589f1644356f6d777b301d4511
Miseong Kim <miseongk@naver.com>
2023-02-15 01:19:58 +0900

    test: Cars 객체 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,6 +1,7 @@
-package racingcar;
+package racingcar.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,23 +9,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
+import racingcar.strategy.MovableStrategy;
+import racingcar.strategy.MovingStrategy;
 
 class CarsTest {
 
     private Car car1;
     private Car car2;
     private Cars cars;
+    private MovingStrategy movableStrategy = new MovableStrategy();
 
     @BeforeEach
     void init() {
-        car1 = new Car("밀리");
-        car2 = new Car("밀리밀리");
+        car1 = new Car("밀리", 0);
+        car2 = new Car("밀리밀리", 0);
 
-        car1.move(5);
-        car1.move(5);
-        car2.move(5);
+        car1.move(movableStrategy);
+        car1.move(movableStrategy);
+        car2.move(movableStrategy);
         cars = new Cars(Arrays.asList(car1, car2));
 
     }
@@ -32,7 +34,7 @@ class CarsTest {
     @Test
     @DisplayName("게임 결과 확인")
     void getResult() {
-        List<String> carsResult = cars.getResult();
+        List<String> carsResult = cars.getResult("-");
 
         assertAll(
                 () -> assertEquals("밀리 : --", carsResult.get(0)),
@@ -41,17 +43,9 @@ class CarsTest {
     }
 
     @Test
-    @DisplayName("최대 위치 찾기")
-    void findMaxPosition() {
-        int maxPosition = cars.findMaxPosition();
-
-        Assertions.assertThat(maxPosition).isEqualTo(2);
-    }
-
-    @Test
     @DisplayName("우승자 찾기")
     void findWinners() {
-        List<Car> winners = cars.findWinners(2);
+        List<Car> winners = cars.findWinners();
 
         Assertions.assertThat(winners).containsExactly(car1);
     }
