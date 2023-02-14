79c50b872183e6f64fc6cfd4253bf1ef9c5f4012
Miseong Kim <miseongk@naver.com>
2023-02-15 01:19:37 +0900

    test: Car 객체 테스트

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -25,4 +25,21 @@ public class Position {
     public int getPosition() {
         return position;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
 }
