d383b71231ad05d6e850a9a825261b0f2ac79d2a
0chil <0@chll.it>
2023-02-14 21:20:22 +0900

    fix(InputView): 입력 오류

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
 
     public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = SCANNER.next();
+        String input = SCANNER.nextLine();
         String[] carNames = input.split(CAR_NAME_SEPARATOR);
         return trim(List.of(carNames));
     }
@@ -23,6 +23,8 @@ public class InputView {
 
     public int inputTrialCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return SCANNER.nextInt();
+        int input = SCANNER.nextInt();
+        SCANNER.nextLine();
+        return input;
     }
 }
