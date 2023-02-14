b03bd5d2140d57020013f8542ef3ec9feeb14e6f
Miseong Kim <miseongk@naver.com>
2023-02-15 01:23:57 +0900

    refactor: 인스턴스 변수 final 지정

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -5,7 +5,7 @@ public class Name {
     private static final int MINIMUM_NAME_LENGTH = 1;
     private static final int MAXIMUM_NAME_LENGTH = 5;
 
-    private String name;
+    private final String name;
 
     public Name(String name) {
         if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
