c2ecfe8dc8c11587837fcd5f3109d8799c65877d
moonchaewon <codnjs8989@gmail.com>
2023-02-14 09:53:36 +0900

    refactor: modify test value with boundary value

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -13,22 +13,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class RacingGameTest {
-	class ZeroGenerator implements NumberGenerator {
+	class ThreeGenerator implements NumberGenerator {
 		@Override
 		public int generateNumber() {
-			return 0;
+			return 3;
 		}
 	}
 
-	class NineGenerator implements NumberGenerator {
+	class FourGenerator implements NumberGenerator {
 		@Override
 		public int generateNumber() {
-			return 9;
+			return 4;
 		}
 	}
 
-	ZeroGenerator zeroGenerator = new ZeroGenerator();
-	NineGenerator nineGenerator = new NineGenerator();
+	ThreeGenerator threeGenerator = new ThreeGenerator();
+	FourGenerator fourGenerator = new FourGenerator();
 	RacingGame racingGame;
 
 	@BeforeEach
@@ -44,7 +44,7 @@ class RacingGameTest {
 		@DisplayName("모든 차의 위치가 이동해야 한다.")
 		void moveAllCarsTest() {
 			RacingCars.add(new Car("code"));
-			racingGame = new RacingGame(nineGenerator);
+			racingGame = new RacingGame(fourGenerator);
 			racingGame.moveCars();
 
 			List<Car> cars = RacingCars.getCars();
@@ -58,7 +58,7 @@ class RacingGameTest {
 		@DisplayName("모든 차의 위치가 변하지 않아야 한다.")
 		void moveNoneCarsTest() {
 			RacingCars.add(new Car("code"));
-			racingGame = new RacingGame(zeroGenerator);
+			racingGame = new RacingGame(threeGenerator);
 			racingGame.moveCars();
 
 			List<Car> cars = RacingCars.getCars();
@@ -75,7 +75,7 @@ class RacingGameTest {
 		@DisplayName("가장 많이 전진한 자동차가 우승자여야 한다.")
 		void getWinnersTest() {
 			assertThat(RacingCars.getCars().size()).isEqualTo(2);
-			racingGame = new RacingGame(nineGenerator);
+			racingGame = new RacingGame(fourGenerator);
 			racingGame.moveCars();
 
 			RacingCars.add(new Car("code"));
