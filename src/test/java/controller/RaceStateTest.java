e7aa2bba179b840e27ace5be7e58abb2b9ff5b77
apptie <appti@naver.com>
2023-02-14 18:09:44 +0900

    test: 새로운 항목 추가로 인한 RaceState 테스트 케이스 추가

--- src/test/java/controller/RaceStateTest.java
+++ src/test/java/controller/RaceStateTest.java
@@ -2,16 +2,16 @@ package controller;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class RaceStateTest {
 
-    @Test
-    @DisplayName("RaceState의 값이 APPLICATION_EXIT라면 false를 반환한다.")
-    void exitRace() {
-        boolean result = RaceState.APPLICATION_EXIT.isRacing();
+    @ParameterizedTest
+    @EnumSource(names = {"APPLICATION_EXIT", "APPLICATION_EXCEPTION"})
+    @DisplayName("RaceState의 값이 APPLICATION_EXIT, APPLICATION_EXCEPTION이라면 false를 반환한다.")
+    void exitRace(RaceState raceState) {
+        boolean result = raceState.isRacing();
 
         Assertions.assertThat(result).isFalse();
     }
