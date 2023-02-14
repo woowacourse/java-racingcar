5ef12018bd3415c806f20adf2c1b521275abcb73
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-14 11:20:44 +0900

    refactor : CarFactory 자동차 이름 유효성 검사 기능 삭제 및 생성자 방어적 복사 추가

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -1,46 +1,31 @@
 package racingcar.domain;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static racingcar.constant.GameConstant.RACE_START_POINT;
-
 public class CarFactory {
 
-    private static final String CAR_NAME_DELIMITER = ",";
-    private static final int CAR_NAME_MINIMUM_LENGTH = 1;
-    private static final int CAR_NAME_MAXIMUM_LENGTH = 5;
-
-    public static List<Car> generate(String input) {
-        final List<String> carNames = Arrays.stream(input.split(CAR_NAME_DELIMITER))
-                .map(String::trim)
-                .collect(Collectors.toUnmodifiableList());
-
+    public static List<Car> generate(List<String> input, int startPoint) {
+        final List<String> carNames = new ArrayList<>(input);
         validateDuplicatedCarNames(carNames);
 
-        return createCarsByNames(carNames);
+        return createCarsByNames(carNames, startPoint);
     }
 
-    private static List<Car> createCarsByNames(List<String> carNames) {
+    /**
+     * TODO: createCarsByNames() 메서드의 경우에도 List반환 시 방어적 복사를 해야하나요?
+     */
+    private static List<Car> createCarsByNames(List<String> carNames, int startPoint) {
         return carNames.stream()
-                .map(carName -> {
-                    validateLengthOfCarName(carName);
-                    return new Car(carName, RACE_START_POINT);
-                })
+                .map(carName -> new Car(carName, startPoint))
                 .collect(Collectors.toUnmodifiableList());
     }
 
     private static void validateDuplicatedCarNames(List<String> carNames) {
         if (Set.copyOf(carNames).size() != carNames.size()) {
             throw new IllegalArgumentException("자동차 이름은 중복되지 않아야 합니다.");
-        }
-    }
-
-    private static void validateLengthOfCarName(String name) {
-        if (name.length() > CAR_NAME_MAXIMUM_LENGTH || name.length() < CAR_NAME_MINIMUM_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름의 길이는 1이상 5이하여야 합니다.");
         }
     }
 
