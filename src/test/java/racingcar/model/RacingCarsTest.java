b5454d097c6bcea5663bbc8a3e1a2b523f14c872
LJW25 <dbjwl@g.skku.edu>
2023-02-16 00:27:46 +0900

    test(RacingCarsTest): 한 회 진행 테스트 추가

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -3,12 +3,14 @@ package racingcar.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.CarNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class RacingCarsTest {
+import static org.assertj.core.util.Lists.newArrayList;
 
+class RacingCarsTest {
 
     @Test
     @DisplayName("우승자 선정 기능 - 우승자가 1명일 경우 테스트")
@@ -38,11 +40,9 @@ class RacingCarsTest {
         Assertions.assertThat(racingCars.getWinners()).containsOnly(test1, test2, test3);
     }
 
-
     @Test
     @DisplayName("우승자 선정 기능 - 모두 동점일 경우 테스트")
     void getAllWinnersTest() {
-        List<Car> cars = new ArrayList<>();
         Car test1 = new Car("test1", 1);
         Car test2 = new Car("test2", 1);
         Car test3 = new Car("test3", 1);
@@ -52,5 +52,39 @@ class RacingCarsTest {
 
         Assertions.assertThat(racingCars.getWinners().size()).isEqualTo(5);
         Assertions.assertThat(racingCars.getWinners()).containsOnly(test1, test2, test3, test4, test5);
+    }
+
+    @Test
+    @DisplayName("한 회 시도 테스트")
+    void tryOneTimeTest() {
+        ArrayList<Integer> testNumber = newArrayList(0, 3, 4, 9);
+
+        Car test1 = new Car("test1", 0);
+        Car test2 = new Car("test2", 0);
+        Car test3 = new Car("test3", 0);
+        Car test4 = new Car("test4", 0);
+        RacingCars racingCars = new RacingCars(List.of(test1, test2, test3, test4));
+
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(testNumber);
+        racingCars.tryOneTime(testNumberGenerator);
+
+        Assertions.assertThat(test1.getPosition()).isEqualTo(0);
+        Assertions.assertThat(test2.getPosition()).isEqualTo(0);
+        Assertions.assertThat(test3.getPosition()).isEqualTo(1);
+        Assertions.assertThat(test4.getPosition()).isEqualTo(1);
+    }
+
+    static class TestNumberGenerator implements CarNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
     }
 }
