fcbcbf1059c1981171a23dd0327c70b0385294a1
eunbii0213 <eunbii0213@naver.com>
2023-02-16 02:34:05 +0900

    refactor(InputView): Output 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,17 +4,11 @@ public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
 
-    private final Output output;
-
-    public InputView(Output output) {
-        this.output = output;
+    public static void printInputTryCountGuide() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
     }
 
-    public void printInputTryCountGuide() {
-        output.println(INPUT_TRY_COUNT_MESSAGE);
-    }
-
-    public void printInputCarNameGuide() {
-        output.println(INPUT_CAR_NAME_MESSAGE);
+    public static void printInputCarNameGuide() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
     }
 }
