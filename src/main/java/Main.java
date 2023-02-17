0384803fe91fc2ed49b3e221b1cc694a77abca85
이건회 <rg970604@naver.com>
2023-02-17 14:59:21 +0900

    refactor : 타 클래스 변경으로 인한 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import controller.GamePlay;
+import domain.Cars;
+import genertor.NumberGenerator;
+import genertor.RandomNumberGenerator;
+import service.CarFactory;
+import view.InputView;
 
 public class Main {
 
     public static void main(String[] args) {
+        Cars cars = new Cars(CarFactory.buildCars(InputView.inputCarNames()));
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
 
-        GamePlay gamePlay = new GamePlay();
+        GamePlay gamePlay = new GamePlay(cars, numberGenerator);
         gamePlay.gameStart();
     }
 }
