19873d0f6993f94d78ecd48d839facbe74cc08de
이건회 <rg970604@naver.com>
2023-02-17 14:59:05 +0900

    refactor : 구분자, 위치 표현 바 제외 상수 제거

--- src/main/java/genertor/RandomNumberGenerator.java
+++ src/main/java/genertor/RandomNumberGenerator.java
@@ -4,11 +4,9 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
-    private static final int MAX_MOVING_NUM = 9;
-
     @Override
     public int getNum() {
         Random random = new Random();
-        return random.nextInt(MAX_MOVING_NUM);
+        return random.nextInt(9);
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,19 +7,15 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static final String ENTER_TRY_TIMES = "시도할 회수는 몇회인가요?";
-    private static final String WINNER_NAME_DELIMITER = ", ";
+    private static final String DELIMITER = ", ";
     private static final String POSITION_BAR = "-";
-    private static final String RESULT = "실행 결과";
-    private static final String FINAL_WINNER = "가 최종 우승했습니다.";
 
     public static void printInputCarNamesNotice() {
-        System.out.println(ENTER_CAR_NAME);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
     public static void printInputTryTimesNotice() {
-        System.out.println(ENTER_TRY_TIMES);
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 
     public static void printCarNameAndPosition(Cars cars) {
@@ -33,11 +29,11 @@ public class OutputView {
     }
 
     public static void printResultNotice() {
-        System.out.println(RESULT);
+        System.out.println("실행 결과");
     }
 
     public static void printWinner(List<Car> winnerCars) {
         List<String> winnerCarNames = winnerCars.stream().map(Car::getName).collect(Collectors.toList());
-        System.out.println(String.join(WINNER_NAME_DELIMITER, winnerCarNames) + FINAL_WINNER);
+        System.out.println(String.join(DELIMITER, winnerCarNames) + "가 최종 우승했습니다.");
     }
 }
