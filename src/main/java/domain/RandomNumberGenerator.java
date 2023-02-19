177ab51cdff07f7d9d9d668080ded13b14a0388d
kiarakim <kiarakim413@gmail.com>
2023-02-19 15:42:43 +0900

    refactor(RandomNumberGenerator) : 키워드 순서 변경

--- src/main/java/domain/RandomNumberGenerator.java
+++ src/main/java/domain/RandomNumberGenerator.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
 	private static final int RANDOM_RULE_NUMBER = 10;
-
-	private final static Random random = new Random();
+	private static final Random random = new Random();
 
 	public int generate() {
 		return random.nextInt(RANDOM_RULE_NUMBER);
