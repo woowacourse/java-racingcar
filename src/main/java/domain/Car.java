61ecf63ed43b75aa827ca672398d50f9bf0ba533
kiarakim <kiarakim413@gmail.com>
2023-02-19 15:23:55 +0900

    refactor(Car) : 중복 생성자 삭제

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,46 +1,26 @@
 package domain;
 
+import domain.strategy.MovingStrategy;
+
 public class Car {
-	public static final int MIN_BOUND = 1;
-	public static final int MAX_BOUND = 5;
-	private static final String ONE_TO_FIVE = "이름은 1이상 5이하입니다.";
+	private final CarName carName;
+	private int position;
 
-	private final String carName;
-	private int distance;
-
-	public Car(String input) {
-		String trimInput = trimAllBlank(input);
-		validateNameLength(trimInput);
-		this.carName = trimInput;
-		this.distance = 0;
+	public Car(String name) {
+		this.carName = new CarName(name);
 	}
 
-	public Car(String input, int distance) {
-		this.carName = input;
-		this.distance = distance;
-	}
-
-	private String trimAllBlank(String input) {
-		return input.replaceAll(" ", "");
-	}
-
-	private void validateNameLength(String input) {
-		if (input.length() < MIN_BOUND || input.length() > MAX_BOUND) {
-			throw new IllegalArgumentException(ONE_TO_FIVE);
+	public void move(MovingStrategy movingStrategy) {
+		if (movingStrategy.isMove()) {
+			position++;
 		}
 	}
 
-	public void move(boolean isMovable) {
-		if (isMovable) {
-			distance++;
-		}
+	public int getPosition() {
+		return position;
 	}
 
-	public String getCarName() {
-		return carName;
-	}
-
-	public int getDistance() {
-		return distance;
+	public String getName() {
+		return carName.getName();
 	}
 }
