fdfdad3dab2825c4ff9d0dcdbdaa87dc97d082f3
piedra-de-flor <xxspdlqjxx@naver.com>
2023-06-16 21:21:07 +0900

    Refactor(InputView) : inline method getters

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -4,15 +4,11 @@ public class InputView {
     static Scanner scanner = new Scanner(System.in);
     public int getCarNumber() {
         System.out.println("자동차 대수는 몇 대인가요?");
-        int carNumber = scanner.nextInt();
-
-        return carNumber;
+        return scanner.nextInt();
     }
 
     public int getTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int tryCount = scanner.nextInt();
-
-        return tryCount;
+        return scanner.nextInt();
     }
 }
