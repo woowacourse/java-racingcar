62d588715c403ffe860e62cea3f95d8ded9cc36f
piedra-de-flor <xxspdlqjxx@naver.com>
2023-06-17 20:09:06 +0900

    Feat(GameStarter) : add function to move cars

--- src/main/java/GameStarter.java
+++ src/main/java/GameStarter.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //어플리케이션을 시작 및 제어하는 main 클래스
 public class GameStarter {
-
+    private final InputView inputView = new InputView();
+    private final Filter filter = new Filter();
+    private final CarFactory carFactory = new CarFactory();
     public static List<Car> carList = new ArrayList<>();
-    int carNum;
-    int tryCount;
+    private int carNum;
+    private int tryCount;
 
     //어플리케이션 시작
     public static void main(String[] args) {
@@ -16,13 +19,20 @@ public class GameStarter {
 
     //어플리케이션 흐름 제어
     private void run() {
-        InputView inputView = new InputView();
-        Filter filter = new Filter();
-        CarFactory carFactory = new CarFactory();
+        initGameProperty();
+    }
 
+    private void initGameProperty() {
         checkInput(inputView, filter);
         carFactory.makeCar(carNum);
+    }
+
+    private void startGame() {
         carList.forEach(car -> car.setRandomNumber(carFactory.insertRandomNumber()));
+        carList.stream()
+                .filter(car -> filter.CheckRandomNumber(car.getRandomNumber()))
+                .collect(Collectors.toList())
+                .forEach(Car::forward);
     }
 
     private void checkInput(InputView inputView, Filter filter) {
