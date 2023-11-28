208ba9a2df9812f5bda971892aec018279e59d54
kdkdhoho <hkim4410@naver.com>
2023-11-28 14:12:24 +0900

    feat: Cars 도메인 구현

---
+++ src/main/java/domain/Cars.java
@@ -0,0 +1,50 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        validateCarNameDuplicate(cars);
+        this.cars = new ArrayList<>(cars);
+    }
+
+    private void validateCarNameDuplicate(List<Car> cars) {
+        List<String> filtered = cars.stream()
+                .map(Car::name)
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (cars.size() != filtered.size()) {
+            throw new IllegalArgumentException("자동차 이름에 중복이 존재합니다.");
+        }
+    }
+
+    public List<Car> playOnce(NumberGenerator numberGenerator) {
+        for (Car car : cars) {
+            int randomNumber = numberGenerator.generate();
+            car.move(randomNumber);
+        }
+        return cars;
+    }
+
+    public List<Car> winners() {
+        int maximumDistance = cars.stream()
+                .mapToInt(Car::distance)
+                .max().getAsInt();
+
+        Distance criteria = new Distance(maximumDistance);
+
+        return cars.stream()
+                .filter(car -> car.isSameDistance(criteria))
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> cars() {
+        return List.copyOf(cars);
+    }
+}
---
+++ src/test/java/domain/CarsTest.java
@@ -0,0 +1,72 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+
+    @Test
+    void 자동차_이름은_중복될_수_없다() {
+        // given
+        List<Car> cars = List.of(new Car(
+                new Name("doggy")), new Car(new Name("doggy"))
+        );
+
+        // when
+        assertThatIllegalArgumentException().isThrownBy(() -> new Cars(cars));
+    }
+
+    @Test
+    void 자동차_이름이_중복되지_않으면_객체_생성에_성공한다() {
+        // given
+        List<Car> cars = List.of(
+                new Car(new Name("a")), new Car(new Name("b"))
+        );
+
+        // when
+        Cars result = new Cars(cars);
+
+        // then
+        assertThat(result.cars()).hasSize(2);
+    }
+
+    @Test
+    void 경주를_한_번_진행한다() {
+        // given
+        Cars cars = new Cars(List.of(
+                new Car(new Name("A")), new Car(new Name("B")))
+        );
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        // when
+        List<Car> result = cars.playOnce(randomNumberGenerator);
+
+        // then
+        List<Integer> distances = result.stream()
+                .map(Car::distance)
+                .collect(Collectors.toList());
+        assertThat(distances).allMatch(it -> it == 0 || it == 1);
+    }
+
+    @Test
+    void 우승자를_반환한다() {
+        // given
+        Car a = new Car(new Name("A"));
+        Car b = new Car(new Name("B"));
+        a.move(4);
+
+        Cars cars = new Cars(List.of(a, b));
+
+        // when
+        List<Car> result = cars.winners();
+
+        // then
+        assertThat(result)
+                .containsOnly(a)
+                .doesNotContain(b);
+    }
+}
