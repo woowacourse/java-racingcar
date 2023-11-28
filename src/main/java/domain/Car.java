64d6e4e3500fd7b14b3b5e7234d698b908b7d433
kdkdhoho <hkim4410@naver.com>
2023-11-28 14:12:20 +0900

    feat: Car 도메인 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Car {
+
+    private static final int MOVABLE_CONDITION_VALUE = 4;
+
+    private static final int MOVE_DISTANCE_VALUE = 1;
+
+    private final Name name;
+    private Distance distance;
+
+    public Car(Name name) {
+        this.name = name;
+        this.distance = Distance.initial();
+    }
+
+    public Distance move(int number) {
+        if (number >= MOVABLE_CONDITION_VALUE) {
+            distance = distance.plus(MOVE_DISTANCE_VALUE);
+        }
+        return this.distance;
+    }
+
+    public boolean isSameDistance(Distance distance) {
+        return this.distance.equals(distance);
+    }
+
+    public String name() {
+        return name.value();
+    }
+
+    public int distance() {
+        return distance.value();
+    }
+}
---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,49 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(value = ReplaceUnderscores.class)
+class CarTest {
+
+    @Test
+    void 이름을_가진다() {
+        // given
+        Name name = new Name("doggy");
+
+        // when
+        Car car = new Car(name);
+
+        // then
+        assertThat(car.name()).isEqualTo("doggy");
+    }
+
+    @Test
+    void 숫자가_4_이상이면_한_칸_이동한다() {
+        // given
+        Car car = new Car(new Name("doggy"));
+        int number = 4;
+
+        // when
+        Distance result = car.move(number);
+
+        // then
+        assertThat(result.value()).isEqualTo(1);
+    }
+
+    @Test
+    void 숫자가_3_이하이면_이동하지_않는다() {
+        // given
+        Car car = new Car(new Name("doggy"));
+        int number = 3;
+
+        // when
+        Distance result = car.move(number);
+
+        // then
+        assertThat(result.value()).isEqualTo(0);
+    }
+}
