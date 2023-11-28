d91306c58b9338c9ed34a156062c5bc533f71ee2
kdkdhoho <hkim4410@naver.com>
2023-11-28 18:07:58 +0900

    test: NameFixture 생성

---
+++ src/test/java/fixture/NameFixture.java
@@ -0,0 +1,10 @@
+package fixture;
+
+import domain.vo.Name;
+
+public class NameFixture {
+
+    public static Name doggy = new Name("doggy");
+    public static Name _0chil = new Name("0chil");
+    public static Name _1bean = new Name("1bean");
+}
