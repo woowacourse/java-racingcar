8770488f21144c4c2291eb1b6774d74cd1f9e786
jongmee <ra123221@gmail.com>
2024-02-14 18:00:00 +0900

    feat: 중복된 자동차 이름 검증 메서드 생성

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -1,9 +1,19 @@
 package racingcar.util;
 
+import java.util.List;
+import java.util.Set;
+
 public class Validation {
     public static void validateCarNameLength(final String carName) {
         if (carName.isEmpty() || carName.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+        }
+    }
+
+    public static void validateDuplicatedNames(final List<String> carNames) {
+        final Set<String> uniqueNames = Set.copyOf(carNames);
+        if(uniqueNames.size() != carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
         }
     }
 }
