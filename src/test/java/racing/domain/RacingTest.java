2efc19e88a971cbc465b01e99d4abd59059a4209
robinjoon <robin980108@naver.com>
2024-02-14 16:29:44 +0900

    test: 우승자 판별 기능 테스트 추가

--- src/test/java/racing/domain/RacingTest.java
+++ src/test/java/racing/domain/RacingTest.java
@@ -1,11 +1,24 @@
 package racing.domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RacingTest {
+    static Stream<Arguments> getMultipleWinnersParameters() {
+        return Stream.of(
+                Arguments.of(List.of(new Car(0, "test1"), new Car(1, "test2")),
+                        List.of(new Car(1, "test2"))),
+                Arguments.of(List.of(new Car(1, "test1"), new Car(1, "test2")),
+                        List.of(new Car(1, "test1"), new Car(1, "test2")))
+        );
+    }
+
     @Test
     @DisplayName("레이스 테스트")
     void doRace() {
@@ -19,12 +32,13 @@ class RacingTest {
                 .isEqualTo(expectedRacing);
     }
 
-    @Test
-    void getWinners() {
-        List<Car> testCars = List.of(new Car(0, "test1"), new Car(1, "test2"));
+    @ParameterizedTest
+    @MethodSource("getMultipleWinnersParameters")
+    void getMultipleWinners(List<Car> testCars, List<Car> expectedCars) {
         Racing racing = new Racing(testCars, 1);
         racing.doRace(List.of(1, 1));
         List<Car> result = racing.getWinners();
-        Assertions.assertThat(result).isEqualTo(List.of(new Car(1, "test2")));
+        Assertions.assertThat(result)
+                .containsExactlyElementsOf(expectedCars);
     }
 }
