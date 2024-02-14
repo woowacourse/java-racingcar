3d5d2901ad5ea3fd21870adeb05975f58f211ab4
HyungUk Ryu <hw0603@naver.com>
2024-02-14 19:57:15 +0900

    feat(validator): 자동차 이름 검증 메소드 구현

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -3,13 +3,20 @@ package racingcar.utils;
 import java.util.Arrays;
 
 public class Validator {
-    public static void validateNameLength(String name) {
+    public static void validateCarName(String[] names) {
+        validateDuplicateNames(names);
+        for (String name : names) {
+            validateNameLength(name);
+            validateNameCharacters(name);
+        }
+    }
+    private static void validateNameLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름은 5글자를 초과할 수 없습니다.");
         }
     }
 
-    public static void validateDuplicateNames(String[] names) {
+    private static void validateDuplicateNames(String[] names) {
         int distinctedNamesCount = (int) Arrays.stream(names)
                 .distinct()
                 .count();
@@ -19,7 +26,7 @@ public class Validator {
         }
     }
 
-    public static void validateNameCharacters(String name) {
+    private static void validateNameCharacters(String name) {
         if (!name.matches("^[a-zA-Z]*$")) {
             throw new IllegalArgumentException("자동차의 이름은 영어로만 이루어져야 합니다.");
         }
