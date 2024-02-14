71589867281280aa81b54c9c9515ce3277b5bc0b
yoonjuho <dbswn990@naver.com>
2024-02-14 16:26:03 +0900

    refactor(RandomGenerator): 랜덤 실행 횟수를 주입받는다.

--- src/main/java/model/RandomGenerator.java
+++ src/main/java/model/RandomGenerator.java
@@ -1,10 +1,16 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomGenerator {
 
-    public static int run(){
-        return new Random().nextInt(9);
+    public static List<Integer> run(int carSize){
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < carSize; i++) {
+            result.add(new Random().nextInt(9));
+        }
+        return result;
     }
 }
--- src/test/java/model/RandomGeneratorTest.java
+++ src/test/java/model/RandomGeneratorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 class RandomGeneratorTest {
@@ -11,6 +13,6 @@ class RandomGeneratorTest {
     @DisplayName("0에서 9 사이에서 random한 값을 구할 수 있다.")
     @RepeatedTest(100)
     void run(){
-        assertThat(RandomGenerator.run()).isBetween(0,9);
+        assertThat(RandomGenerator.run(1)).containsAnyOf(0,1,2,3,4,5,6,7,8,9);
     }
 }
