75521643f18dda3a400dd36d6437bd20d2597577
mzeong <mjeong@sookmyung.ac.kr>
2024-02-14 19:53:05 +0900

    feat: 자동차 이름을 입력

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,4 +1,11 @@
 package racing.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readNames(){
+        return scanner.nextLine();
+    }
 }
