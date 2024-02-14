3050a0b2c645d480f38096f9fb7a843e0a138ecd
zangsu <zangsu_@naver.com>
2024-02-14 18:46:27 +0900

    feat : 최ì종 우승자 출력

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -1,5 +1,11 @@
+import java.util.List;
+
 public class OutputView {
     public static void showCarLocation(String name, int location) {
         System.out.println(name + " : " + "-".repeat(location));
+    }
+
+    public static void showWinners(List<String> winnersName) {
+        System.out.println(String.join(", ", winnersName) + "가 최종 우승했습니다.");
     }
 }
