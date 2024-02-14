556a5bacbe0f193649f633f5c5b77e47fa50d0c8
nayonsoso <panda0329@naver.com>
2024-02-14 16:13:28 +0900

    refactor(CarName): 자동차 이름이 비어 있을 경우 예외를 처리한다.

---
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -0,0 +1,24 @@
+package racingcar.domain.car;
+
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.InvalidInputException;
+
+public class CarName {
+    private final static int MAX_NAME_LENGTH = 4;
+    private final String name;
+
+    public CarName(final String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() >= MAX_NAME_LENGTH) {
+            throw new InvalidInputException(ErrorMessage.INVALID_CAR_NAME_LENGTH);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
