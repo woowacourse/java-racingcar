39fac7897ccecb4922123470a2913992b906a7d9
takoyakimchi <c4nyou@gmail.com>
2024-02-14 18:26:01 +0900

    feat(view): 사용자로부터 문자열 입력받는 함수 생성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static String read() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
