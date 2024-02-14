159ee79ff3729846ec01e1f10e2de852e065f98a
HyungHoKim <hkim1109@naver.com>
2024-02-14 14:46:07 +0900

    feat(InputValidator): 시도 횟수 예외 처리
    시도 횟수가 음수면 예외를 던진다.

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -8,9 +8,28 @@ public class InputValidator {
         }
     }
 
+    public static void tryNumberValidator(String number){
+        int validateNumber = validateNumber(number);
+        validateNegativeNumber(validateNumber);
+    }
+
     private static void validateCarNameLength(String carName) {
         if(carName.length()> CAR_NAME_MAX_LENGTH){
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_TOO_LONG);
+        }
+    }
+
+    private static int validateNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE_NUMBER);
+        }
+    }
+
+    private static void validateNegativeNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE_NUMBER);
         }
     }
 }
