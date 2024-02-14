3d1c4a217f3931048e31ee7e8be6b8ff0ca83b9d
SungKyum Kim <sungkyum1@naver.com>
2024-02-14 16:28:43 +0900

    feat: 자동차 이름 분리 기능 구현

--- src/main/java/RacingCarGame/RacingCarGame.java
+++ src/main/java/RacingCarGame/RacingCarGame.java
@@ -6,5 +6,7 @@ public class RacingCarGame {
     public void run() {
         final Scanner scanner = new Scanner(System.in);
         String rawCarNames = scanner.nextLine();
+
+        String[] carNames = rawCarNames.split(",");
     }
 }
