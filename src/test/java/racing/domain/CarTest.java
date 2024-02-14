9888f244ba60a13fdd07e407710b70f72845e550
mzeong <mjeong@sookmyung.ac.kr>
2024-02-14 18:06:09 +0900

    test: 자동차 이름이 공백인 경우 예외 처리

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,13 @@ class CarTest {
     @DisplayName("자동차 이름이 5자 초과이면 예외")
     void createCarWithOverSize() {
         assertThatThrownBy(()->new Car("pobipobi"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 공백이면 예외")
+    void createCarWithEmpty() {
+        assertThatThrownBy(()->new Car(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
