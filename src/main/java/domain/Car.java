f69315c796abcbe75fbc28327b77bf42c25979b9
jhon3242 <rldotnr2@gmail.com>
2024-02-14 16:14:26 +0900

    feat (Car) : 자동차 이름에 대한 검증 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,24 @@
 package domain;
 
 public class Car {
+
     private final String name;
     private int position;
 
     public Car(String name) {
+        validate(name);
+
         this.name = name;
         this.position = 0;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차의 이름은 빈칸이 아니어야 합니다");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자 이하이어야 합니다");
+        }
     }
 
     public void move(boolean isMove) {
