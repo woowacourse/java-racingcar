efbeff38d320350c472d7c8ccbfc1ab190ae2a84
robinjoon <robin980108@naver.com>
2024-02-14 18:06:01 +0900

    feat: 자동차 이름 입력 기능 구현

---
+++ src/main/java/racing/input/CarNameInputManager.java
@@ -0,0 +1,31 @@
+package racing.input;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class CarNameInputManager {
+    public static List<String> getNameFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        return getName(input);
+    }
+
+    static List<String> getName(String input) {
+        if (input.startsWith(",") || input.endsWith(",")) {
+            throw new RuntimeException("이름의 길이는 1 이상 5 이하여야 합니다.");
+        }
+        boolean isWrongNameLength = Arrays.stream(input.split(","))
+                .anyMatch(s -> s.length() < 1 || s.length() > 5);
+        if (isWrongNameLength) {
+            throw new RuntimeException("이름의 길이는 1 이상 5 이하여야 합니다.");
+        }
+        boolean hasWrongCharacterInName = Arrays.stream(input.split(","))
+                .anyMatch(s -> !Pattern.matches("^[a-zA-Z]+$", s));
+        if (hasWrongCharacterInName) {
+            throw new RuntimeException("이름은 알파벳 대소문자로만 이루어져야 합니다.");
+        }
+        return Arrays.stream(input.split(",")).toList();
+    }
+}
