b996fbd9546a20c6549ea8a5295720d03f917331
HyungUk Ryu <hw0603@naver.com>
2024-02-14 20:27:36 +0900

    fix(validatorTest): 접근제어자 수정에 따른 테스트 코드 수정

--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -11,7 +11,8 @@ public class ValidatorTest {
     @DisplayName("자동차의 이름이 5글자를 초과하는 경우 예외가 발생한다.")
     void overNameLength() {
         final var overName = "아아아아아아";
-        assertThatThrownBy(() -> Validator.validateNameLength(overName))
+        final String[] duplicatedNames = {"abc", "def", "ref", overName};
+        assertThatThrownBy(() -> Validator.validateCarName(duplicatedNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차의 이름은 5글자를 초과할 수 없습니다.");
     }
@@ -19,8 +20,9 @@ public class ValidatorTest {
     @Test
     @DisplayName("자동차의 이름들 중 중복된 이름이 있는 경우 예외가 발생한다.")
     void duplicatedNamesTest() {
-        final String[] duplicatedNames = {"abc", "def", "abc", "jei"};
-        assertThatThrownBy(() -> Validator.validateDuplicateNames(duplicatedNames))
+        final var duplicatedName = "abd";
+        final String[] duplicatedNames = {duplicatedName, "def", "ref", duplicatedName};
+        assertThatThrownBy(() -> Validator.validateCarName(duplicatedNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차의 이름은 중복될 수 없습니다.");
     }
@@ -28,8 +30,8 @@ public class ValidatorTest {
     @Test
     @DisplayName("자동차의 이름 중 영어가 아닌 다른 문자가 있는 경우 예외가 발생한다.")
     void isAlphabetic() {
-        final String wrongName = "rar1";
-        assertThatThrownBy(() -> Validator.validateNameCharacters(wrongName))
+        final String[] wrongName = {"rar1"};
+        assertThatThrownBy(() -> Validator.validateCarName(wrongName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차의 이름은 영어로만 이루어져야 합니다.");
     }
