7419fe03131a5a302a8b5dfec50c01f04790f525
BurningFalls <sherlocky31415@gmail.com>
2024-02-14 17:06:58 +0900

    feat+test: trycount 기능 구현 및 테스트 완료

---
+++ src/main/java/racingcar/model/TryCount.java
@@ -0,0 +1,37 @@
+package racingcar.model;
+
+public class TryCount {
+    private int value;
+
+    public TryCount(String input) {
+        this.value = validatedInput(input);
+    }
+
+    public int validatedInput(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            isOverZero(number);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isOverZero(int number) {
+        if(number < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void consume() {
+        value--;
+    }
+
+    public boolean untilZero() {
+        return value > 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/racingcar/TryCountTest.java
@@ -0,0 +1,55 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.model.TryCount;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TryCountTest {
+
+    @Nested
+    class ValidateInput {
+        @Test
+        @DisplayName("입력값이 문자이면 예외를 발생한다.")
+        void testIsNotNumeric(){
+            String given = "문자열";
+            assertThatThrownBy(() -> new TryCount(given))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("입력값이 음수이면 예외를 발생한다.")
+        void testIsMinusValue(){
+            String given = "-1";
+            assertThatThrownBy(() -> new TryCount(given))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    class Function {
+        @Test
+        @DisplayName("trycount 소비")
+        void testConsume() {
+            TryCount count = new TryCount("5");
+            count.consume();
+            int actualValue = count.getValue();
+            int expectedValue = 4;
+            assertThat(actualValue).isEqualTo(expectedValue);
+        }
+
+        @ParameterizedTest
+        @DisplayName("1 이상인지 확인")
+        @CsvSource({"0,false",
+        "1,true"})
+        void testUntilZero(String given, boolean expected) {
+            TryCount count = new TryCount(given);
+            boolean actual = count.untilZero();
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+}
