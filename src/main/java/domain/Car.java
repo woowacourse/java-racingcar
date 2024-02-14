f204bd32ac5b4fb1c9c722093aa26c5f46e15f6c
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-14 15:36:19 +0900

    FEAT(domain) : 자동차 기능 추가
    위치 정보, 전진 이동하는 기능, 현재 상태 로그 기능 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,14 +1,26 @@
 package domain;
 
 public class Car {
-    private String name;
+    private final String name;
+    private int position;
 
     public Car(String name) {
         validate(name);
+        position = 0;
         this.name = name;
     }
 
     private static void validate(String name) {
         // TODO: 자동차 이름 검증
     }
+
+    public void move() {
+        position++;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
+
 }
