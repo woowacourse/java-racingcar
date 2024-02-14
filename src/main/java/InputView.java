0ab8f8d83bcd5b24005bb9b8b74fca1524668963
tsulocalize <tsulocalize@gmail.com>
2024-02-14 17:34:45 +0900

    feat : 자동차 이름 입력 기능 구현

---
+++ src/main/java/InputView.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> getCarName() {
+        String input = scanner.nextLine();
+        List<String> splitCarNames = new ArrayList<>();
+
+        try {
+            if (input.contains(" ")) {
+                throw new IllegalArgumentException("입력에 공백이 포함될 수 없습니다.");
+            }
+
+            splitCarNames = List.of(input.split(","));
+
+            for (String carName : splitCarNames) {
+                if (carName.isEmpty() || carName.length() > 5) {
+                    throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+                }
+            }
+            long distinctCarCount = splitCarNames.stream().distinct().count();
+            if (distinctCarCount != splitCarNames.size()) {
+                throw new IllegalArgumentException("중복된 자동차 이름은 사용할 수 없습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getCarName();
+        }
+
+        return splitCarNames;
+    }
+}
