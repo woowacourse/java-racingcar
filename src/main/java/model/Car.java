40b2e67509c301d6784333b5b2ea78704548eab8
Nam Gi Beom <rlqja0523@naver.com>
2024-02-14 16:12:49 +0900

    feat: 자동차 클래스 추가

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Car {
+    private final Name name;
+    private int position;
+
+    public Car(Name name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        position++;
+    }
+}
