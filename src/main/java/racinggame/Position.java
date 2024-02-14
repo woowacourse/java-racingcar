ef0039faf59782902c1c620ecead2f604eb62d74
le2sky <leehaneul990623@gmail.com>
2024-02-14 15:34:04 +0900

    feat : 위치 생성 기능 구현

---
+++ src/main/java/racinggame/Position.java
@@ -0,0 +1,34 @@
+package racinggame;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final int position;
+
+    private Position(int position) {
+        this.position = position;
+    }
+
+    public static Position from(int position) {
+        // 검증 필요
+        if (position < 0) {
+            throw new RuntimeException();
+        }
+
+        return new Position(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
---
+++ src/test/java/racinggame/PositionTest.java
@@ -0,0 +1,51 @@
+package racinggame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @DisplayName("자동차 위치를 생성한다.")
+    @Test
+    void create() {
+        Assertions.assertThatCode(() -> Position.from(1))
+            .doesNotThrowAnyException();
+    }
+
+    @DisplayName("자동차 위치가 같으면 동등하다.")
+    @Test
+    void equalsAndHashSuccess() {
+        Position one = Position.from(1);
+        Position other = Position.from(1);
+
+        assertThat(one).isEqualTo(other);
+    }
+
+    @DisplayName("자동차 위치가 다르면 동등하지 않다")
+    @Test
+    void equalsAndHashFail() {
+        Position one = Position.from(1);
+        Position other = Position.from(2);
+
+        assertThat(one).isNotEqualTo(other);
+    }
+
+    @DisplayName("자동차 위치는 음수가 될 수 없다.")
+    @Test
+    void createNonNegative() {
+        assertThatCode(() -> Position.from(-1))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("자동차 위치가 양수라면 에러가 반환되지 않는다.")
+    @Test
+    void createPositive() {
+        assertThatCode(() -> Position.from(1))
+            .doesNotThrowAnyException();
+    }
+
+}
