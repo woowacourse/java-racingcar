3cd2aa27d78e644e862e91ce12fd736e910772fd
mzeong <mjeong@sookmyung.ac.kr>
2024-02-14 19:29:59 +0900

    feat: 0에서 9 사이에서 random 값을 생성

--- src/main/java/racing/util/RandomGenerator.java
+++ src/main/java/racing/util/RandomGenerator.java
@@ -1,4 +1,10 @@
 package racing.util;
 
+import java.util.Random;
+
 public class RandomGenerator {
+    static public int generate() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
 }
