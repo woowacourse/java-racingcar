b5de4a807e2c881c0fbac046750344e8ec01147d
HyungUk Ryu <hw0603@naver.com>
2024-02-14 19:58:36 +0900

    feat(car): Car 클래스 생성자 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,6 +4,11 @@ public class Car {
     private String name;
     private int position;
 
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
     public void move() {
         position++;
     }
