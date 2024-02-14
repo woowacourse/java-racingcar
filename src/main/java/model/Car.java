4bcf59d15f8285552d1b161c040b49b1f980cb3b
yoonjuho <dbswn990@naver.com>
2024-02-14 15:56:16 +0900

    feat(Car): equals & hashcode 재정의

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -34,5 +34,16 @@ public class Car {
         statuses.add(CarStatus.STOP);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(statuses, car.statuses);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, statuses);
+    }
 }
