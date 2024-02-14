f2316ecf45c572c4a649bac3c895dcbf559281f9
Nam Gi Beom <rlqja0523@naver.com>
2024-02-14 18:06:08 +0900

    feat: 자동차 이름 입력 및 시도 횟수 입력받는 클래스 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분).");
+        String carNames = scanner.nextLine();
+        return carNames.split(",");
+    }
+
+    public static int inputTryCount() {
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String tryCount = scanner.nextLine();
+        return Integer.parseInt(tryCount);
+    }
+}
