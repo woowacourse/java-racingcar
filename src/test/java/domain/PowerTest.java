fa13ead744f52cf719a11f06ddf5311316e6e4aa
SCY <helenason@naver.com>
2024-02-14 20:59:39 +0900

    test: Power 도메인 테스트 작성

---
+++ src/test/java/domain/PowerTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("자동차 파워 테스트")
+public class PowerTest {
+    @DisplayName("파워가 자동차를 움직이기에 충분한 경우를 파악할 수 있다")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 9})
+    void testSufficientPowerDetection(int powerValue) {
+        Power power = Power.from(powerValue);
+        assertThat(power.isSufficientPower()).isTrue();
+    }
+
+    @DisplayName("파워가 자동차를 움직이기에 충분하지 않은 경우를 파악할 수 있다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void testInsufficientPowerDetection(int powerValue) {
+        Power power = Power.from(powerValue);
+        assertThat(power.isSufficientPower()).isFalse();
+    }
+}
