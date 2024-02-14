739566b133dfcf7900e1db156dbe9089f6aeb422
takoyakimchi <c4nyou@gmail.com>
2024-02-14 20:07:56 +0900

    feat(service): 자동차 전진 로직 추가

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,10 +1,17 @@
 package service;
 
+import domain.Car;
+
 public class Service {
 
-    public static boolean willMove() {
+    public static void moveForward(Car car) {
+        if (willMove()) {
+            car.move();
+        }
+    }
+
+    private static boolean willMove() {
         int randomNumber = (int) (Math.random() * 10);
         return randomNumber >= 4;
     }
-
 }
