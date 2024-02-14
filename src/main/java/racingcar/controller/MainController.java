487978f083380061e33f28597bb6af42cf483f04
Minjoo <m0522inju@gmail.com>
2024-02-14 22:01:23 +0900

    feat: 게임을 진행하는 controller 구현

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -1,11 +1,36 @@
 package racingcar.controller;
 
 import java.util.List;
+import java.util.stream.IntStream;
+import racingcar.domain.Cars;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 import racingcar.view.model.ConsoleReader;
 
 public class MainController {
     public void run() {
-        final List<String> input = InputView.readCarNames(new ConsoleReader());
+        final Cars cars = getCars();
+        final int tryCount = getTryCount();
+        proceedRounds(cars, tryCount);
+    }
+
+    private Cars getCars() {
+        final List<String> carNames = InputView.readCarNames(new ConsoleReader());
+        return new Cars(carNames);
+    }
+
+    private int getTryCount() {
+        return InputView.readTryCount(new ConsoleReader());
+    }
+
+    public void proceedRounds(final Cars cars, final int tryCount){
+        OutputView.printResultSubject();
+        IntStream.rangeClosed(1, tryCount)
+                .forEach(i -> proceedOneRound(cars));
+    }
+
+    private void proceedOneRound(final Cars cars) {
+        cars.move();
+        OutputView.printResult(cars.result());
     }
 }
