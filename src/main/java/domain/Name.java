5bc61a73a9c785ee043c573947d9f1213ee48d57
tackyu <tack970913@gmail.com>
2024-02-14 17:17:33 +0900

    feat : Name Domain 생성
    이에 따른 테스트 코드 추가

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Name {
+    private String value;
+    private static final Integer LENGTH_LIMIT = 5;
+
+    public Name(String value) {
+        validate(value);
+        this.value = value;
+    }
+    private void validate(String value){
+        if(value.isBlank()){
+            throw new IllegalArgumentException("blank");
+        }
+        if(value.length()>LENGTH_LIMIT){
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameTest {
+
+    @Test
+    @DisplayName("문자열을 통해 이름을 생성한다")
+    public void createName(){
+
+        //Given
+        String value = "pond";
+
+        //When
+        Name name = new Name(value);
+
+        //Then
+        assertInstanceOf(Name.class,name);
+    }
+    @ParameterizedTest
+    @DisplayName("공백 이름은 에러를 발생시킨다")
+    @ValueSource(strings = {""," "})
+    public void throwExceptionWhenValueIsBlank(String value){
+        assertThrows(IllegalArgumentException.class,()->{
+            new Name(value);
+        });
+    }
+    @Test
+    @DisplayName("다섯 글자 이상의 이름은 에러를 발생시킨다.")
+    public void throwExceptionWhenValueIsExceed5(){
+        String value = "joyson";
+
+        assertThrows(IllegalArgumentException.class,()-> {
+                    new Name(value);
+                }
+        );
+    };
+}
