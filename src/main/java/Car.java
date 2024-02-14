94f80d57c1a0369c5294f6e867734e7bc4c02e3c
eunjungL <dldmswjd322@gmail.com>
2024-02-14 16:46:43 +0900

    feat : 자동차 도메인 생성

---
+++ src/main/java/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private final String name;
+    private final int forward;
+
+    public Car(String name) {
+        this.name = name;
+        this.forward = 0;
+    }
+
+
+}
