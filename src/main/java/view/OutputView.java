e2dfa756306edf11fdbdf1640234efb4a180f1f1
takoyakimchi <c4nyou@gmail.com>
2024-02-14 19:47:54 +0900

    feat(view): 라운드 별 실행 결과 출력 로직 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,13 @@
 package view;
 
+import domain.Car;
+import java.util.List;
+
 public class OutputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_ROUND = "시도할 회수는 몇회인가요?";
-    private static final String ROUND_RESULT = "실행 결과";
+    private static final String GAME_RESULT = "실행 결과";
     private static final String WINNING_MESSAGE = "가 최종 우승했습니다.";
 
     public static void printCarNames() {
@@ -16,6 +19,12 @@ public class OutputView {
     }
 
     public static void printRoundResult() {
-        System.out.println(ROUND_RESULT);
+        System.out.println(GAME_RESULT);
+    }
+
+    public static void printScore(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : " + new String("-").repeat(car.getScore()));
+        }
     }
 }
