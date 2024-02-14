e594dfcec7f58a6cb4866824ad62a6094019679d
Arachneee <jhg2819@naver.com>
2024-02-14 16:00:41 +0900

    feat: 전진 여부 결정하기 기능 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+
+    private static final int MIN_NUMBER = 4;
+    private int position;
+
+    public void go(final int number) {
+        if (number >= MIN_NUMBER) {
+            position++;
+        }
+    }
+}
