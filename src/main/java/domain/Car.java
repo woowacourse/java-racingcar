778ead3eda37cfcd59a6a1971afc6c06ede6c0a6
SCY <helenason@naver.com>
2024-02-14 17:27:01 +0900

    feat: Car 도메인 속성 정의 및 검증 구현

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class Car {
+    private final String carName;
+
+    private Car(String carName) {
+        validate(carName);
+        this.carName = carName;
+    }
+
+    public static Car of(String name) {
+        return new Car(name);
+    }
+
+    private void validate(String carName) {
+        validateCarNameLength(carName);
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
