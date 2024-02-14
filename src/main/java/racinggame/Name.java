dfd96267ed689f40fca62ccd4eea527644007ed8
le2sky <leehaneul990623@gmail.com>
2024-02-14 15:12:50 +0900

    feat : 이름 생성 기능 구현

---
+++ src/main/java/racinggame/Name.java
@@ -0,0 +1,37 @@
+package racinggame;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String name;
+
+    private Name(String name) {
+        this.name = name;
+    }
+
+    public static Name from(String name) {
+        if (name.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        if (name.length() > 5) {
+            throw new RuntimeException();
+        }
+
+        return new Name(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
