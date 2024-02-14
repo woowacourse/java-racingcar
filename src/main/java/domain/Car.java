4c6cfcbdbc49a57f7a20741273904a3456d5d023
yoonseo choi <choiyoonseo02@gmail.com>
2024-02-14 15:06:15 +0900

    feat: 자동차 객체

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Car {
+    public final String name;
+    public int location;
+
+    public Car(String name) {
+        this.name = name;
+        location = 0;
+    }
+}
