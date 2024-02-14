95492c12c791675b5cf1c50211ddb1faac33f707
unifolio0 <boho82ha@gmail.com>
2024-02-14 16:27:48 +0900

    feat : 쉼표 기준 분리 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String inputCarNames() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.next();
+    }
+}
