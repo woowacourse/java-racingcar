3f00005e5d0891654cac0e35b5808ef45811b3cf
SCY <helenason@naver.com>
2024-02-14 17:29:23 +0900

    feat: 자동차 일급 컬렉션 속성 정의

---
+++ src/main/java/domain/Cars.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<Car> cars) {
+        return new Cars(cars);
+    }
+
+}
