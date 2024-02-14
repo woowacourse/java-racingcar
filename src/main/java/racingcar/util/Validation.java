02fd68a57f9b2df9b1e9778bc56614d257e86b4c
jongmee <ra123221@gmail.com>
2024-02-14 18:13:11 +0900

    fix: 자동차 이름 목록 검증 메서드 수정

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -4,8 +4,14 @@ import java.util.List;
 import java.util.Set;
 
 public class Validation {
-    public static void validateCarNameLength(final String carName) {
-        if (carName.isEmpty() || carName.length() > 5) {
+    public static void validateNullName(final String carNames) {
+        if(carNames == null) {
+            throw new IllegalArgumentException("자동차 이름 목록은 null일 수 없습니다.");
+        }
+    }
+
+    public static void validateCarNamesFormat(final String carName) {
+        if (carName.isBlank() || carName.isEmpty() || carName.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
         }
     }
