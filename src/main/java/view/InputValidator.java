645395b5228663b743ca330f7c78dced833852d4
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-14 15:28:43 +0900

    feat(InputView): 반복 횟수 입력 기능 구현

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,9 +1,9 @@
 package view;
 
 public class InputValidator {
-    private static final String BLANK_ERROR_MESSAGE = "경주할 자동차 이름에 대한 입력은 공백일 수 없습니다.";
+    private static final String BLANK_ERROR_MESSAGE = "입력은 공백일 수 없습니다.";
 
-    public static void validateCarNames(String input) {
+    public static void validate(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
         }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String input = scanner.nextLine();
 
-        InputValidator.validateCarNames(input);
+        InputValidator.validate(input);
         return input;
+    }
+
+    public static int inputRound() {
+        System.out.println(INPUT_ROUND_MESSAGE);
+        String input = scanner.nextLine();
+
+        InputValidator.validate(input);
+        return Integer.parseInt(input);
     }
 }
