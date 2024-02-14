a180b016cdd4431dfed5086cea64bac908f4338d
tsulocalize <tsulocalize@gmail.com>
2024-02-14 18:14:12 +0900

    feat : 랜덤 조건에 따라 자동차를 움직이는 기능 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Car {
+
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void moveCar() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int randomNumber = random.nextInt(10);
+        if (randomNumber >= 4) {
+            distance++;
+        }
+        if (distance < 0) {
+            throw new IllegalArgumentException("이동 거리는 음수가 될 수 없습니다.");
+        }
+    }
+}
