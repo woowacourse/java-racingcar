ba40023ddb9fc8acd61f94fa4d7800fa47ce6398
nayonsoso <panda0329@naver.com>
2024-02-14 18:00:49 +0900

    feat(RaceCountRequest): 시도 횟수 입력을 dto를 통해 숫자로 변환한다.

--- src/main/java/racingcar/dto/request/RaceCountRequest.java
+++ src/main/java/racingcar/dto/request/RaceCountRequest.java
@@ -1,5 +1,22 @@
 package racingcar.dto.request;
 
-public record RaceCountRequest(String input) {
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.InvalidInputException;
+import racingcar.view.utils.InputUtils;
 
+public record RaceCountRequest(String input) {
+    private static final int MAX_RACE_COUNT = 100;
+    private static final int MIN_RACE_COUNT = 1;
+
+    public int toInt() {
+        int count = InputUtils.parseToInt(input);
+        validateRaceCountRange(count);
+        return count;
+    }
+
+    private void validateRaceCountRange(int count) {
+        if (count > MAX_RACE_COUNT || MIN_RACE_COUNT > count) {
+            throw new InvalidInputException(ErrorMessage.INVALID_RACE_COUNT_RANGE);
+        }
+    }
 }
--- src/main/java/racingcar/view/utils/InputUtils.java
+++ src/main/java/racingcar/view/utils/InputUtils.java
@@ -1,5 +1,8 @@
 package racingcar.view.utils;
 
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.InvalidInputException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
@@ -8,6 +11,14 @@ public class InputUtils {
     private static final String DELIMITER = ",";
 
     private InputUtils() {
+    }
+
+    public static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(ErrorMessage.INPUT_NOT_A_NUMBER);
+        }
     }
 
     public static List<String> splitByComma(String input) {
