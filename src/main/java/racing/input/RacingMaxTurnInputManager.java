e103fa5935809238d7b79440176a6f7e974943c4
robinjoon <robin980108@naver.com>
2024-02-14 19:11:49 +0900

    feat: 자동차 이동 횟수 입력 기능 구현

---
+++ src/main/java/racing/input/RacingMaxTurnInputManager.java
@@ -0,0 +1,24 @@
+package racing.input;
+
+import java.util.Scanner;
+
+public class RacingMaxTurnInputManager {
+
+    public static int getMaxTurnFromConsole(Scanner scanner) {
+        String input = scanner.next();
+        return getMaxTurn(input);
+    }
+
+    static int getMaxTurn(String input) {
+        int parsedNumber;
+        try {
+            parsedNumber = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("총 이동 횟수는 1 이상 100이하의 정수만 가능합니다.");
+        }
+        if (parsedNumber < 1 || parsedNumber > 100) {
+            throw new RuntimeException("총 이동 횟수는 1 이상 100이하의 정수만 가능합니다.");
+        }
+        return parsedNumber;
+    }
+}
