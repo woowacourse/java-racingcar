35bc5e93c99e5e14f5b38f40eafa157f352b09a6
HyungHoKim <hkim1109@naver.com>
2024-02-14 14:55:44 +0900

    test(InputValidatorTest): 입력값 예외 처리 테스트 코드 작성
    자동차 이름 및 시도 횟수 예외 처리에 관한 테스트 코드 작성

---
+++ src/test/java/utils/InputValidatorTest.java
@@ -0,0 +1,37 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.ErrorMessage.CAR_NAME_TOO_LONG;
+import static utils.ErrorMessage.NOT_POSITIVE_NUMBER;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputValidatorTest {
+
+    @Test
+    @DisplayName("자동차 이름이 5자를 초과하면 예외가 발생한다.")
+    public void tooLongCarNamesThrowException() {
+        assertThatThrownBy(() -> InputValidator.carNamesValidator("명오배키켬미"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_NAME_TOO_LONG);
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자가 아니면 예외가 발생한다.")
+    public void notNumberTryNumberThrowException() {
+        assertThatThrownBy(() -> InputValidator.tryNumberValidator("배키"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_POSITIVE_NUMBER);
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 양수가 아니면 예외가 발생한다.")
+    public void notPositiveTryNumberThrowException() {
+        assertThatThrownBy(() -> InputValidator.tryNumberValidator("-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_POSITIVE_NUMBER);
+    }
+}
