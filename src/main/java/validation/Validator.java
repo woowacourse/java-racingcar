25640febaf3cb6529fbcb95a608236f7a52f92cf
takoyakimchi <c4nyou@gmail.com>
2024-02-14 18:45:14 +0900

    feat(validation): 자동차 이름 규칙 중복 판별 로직 추가

--- src/main/java/validation/Validator.java
+++ src/main/java/validation/Validator.java
@@ -1,6 +1,8 @@
 package validation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
@@ -10,12 +12,19 @@ public class Validator {
         for (String name : names) {
             validateNameRules(name);
         }
-//        validateNameDuplication();
+        validateNameDuplication(names);
     }
 
     private static void validateNameRules(String name) {
         if (!name.matches(NAME_RULE)) {
             throw new IllegalArgumentException("1~5자의 알파벳만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateNameDuplication(List<String> names) {
+        Set<String> nonDuplicatedNames = new HashSet<>(names);
+        if (nonDuplicatedNames.size() != names.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
         }
     }
 }
