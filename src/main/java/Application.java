704127e79677ec06543a7c0f2dccf832ce6a3f98
hyun <osh123chm@gmail.com>
2024-02-14 16:30:13 +0900

    feat: 자동차 이름은 쉼표로 구분해야 한다

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,7 @@
+import view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-
+        new InputView().askCarNames();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,16 @@
 package view;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    public List<String> askCarNames() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return parseCarNames(input);
+    }
+
+    private List<String> parseCarNames(String input) {
+        return List.of(input.split(","));
+    }
 }
