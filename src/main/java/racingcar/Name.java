8e70fac51499f616d3506f4e947f94546d755f34
donghoony <aru0504@naver.com>
2024-02-14 15:24:26 +0900

    feat(Name): 자동차에 부여될 이름 클래스 작성, 검증 로직 구현

---
+++ src/main/java/racingcar/Name.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+public class Name {
+    private static final int MAX_LENGTH = 5;
+
+    private final String value;
+
+    public Name(String value) {
+        validateName(value);
+        this.value = value;
+    }
+
+    private void validateName(String value) {
+        validateNameLength(value);
+        validateNameNotBlank(value);
+    }
+
+    private void validateNameLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void validateNameNotBlank(String value) {
+        if (value.isBlank()) {
+            throw new RuntimeException();
+        }
+    }
+}
