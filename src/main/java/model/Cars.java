6b53437a11d5eab8d7e7dc07740b9774cd89532d
hyun <osh123chm@gmail.com>
2024-02-14 17:36:05 +0900

    feat: 자동차 이름은 중복될 수 없다

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars fromNames(List<String> names) {
+        validateDuplicateName(names);
+        List<Car> cars = names.stream()
+                .map(name -> new Car(name))
+                .toList();
+        return new Cars(cars);
+    }
+
+    private static void validateDuplicateName(List<String> names) {
+        Set<String> nonDuplicateNames = new HashSet<>(names);
+        if(nonDuplicateNames.size() != names.size()){
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없다");
+        }
+    }
+
 }
---
+++ src/test/java/model/CarsTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+    @Test
+    void 자동차_이름_리스트로_자동차_리스트를_생성한다() {
+        Cars cars = Cars.fromNames(List.of("pobi", "dora"));
+        assertThat(cars.getClass()).isEqualTo(Cars.class);
+    }
+
+    @Test
+    void 중복된_자동차_이름이_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> Cars.fromNames(List.of("pobi", "pobi")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
