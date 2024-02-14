090c5cd7c9b72f5dd1736a5b1cf95da4cb9076b0
yoonseo choi <choiyoonseo02@gmail.com>
2024-02-14 15:05:08 +0900

    feat: 자동차 이름, 시도할 횟수 입력 안내 메시지

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,7 @@
 package view;
 
 public class InputView {
-
-    public static void requestCarName() {
+    public static void requestCarName () {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
