6df5ac20ba41dc3b6fff2a1f8562fecf3c3a95a8
mzeong <mjeong@sookmyung.ac.kr>
2024-02-14 18:35:56 +0900

    feat: 겹치는 자동차 이름이 존재하면 예외 처리

--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -8,9 +8,22 @@ import java.util.stream.Stream;
 public class Cars {
     private final List<Car> cars;
 
-    public Cars(String cars) {
-        List<String> names = stringToList(cars);
-        this.cars = names.stream().map(Car::new).toList();
+    public Cars(String rawNames) {
+        List<String> carNames = stringToList(rawNames);
+        validate(carNames);
+        List<Car> cars = carNames.stream().map(Car::new).toList();
+        this.cars = cars;
+    }
+
+    private void validate(List<String> cars) {
+        validateDuplicateName(cars);
+    }
+
+    private static void validateDuplicateName(List<String> cars) {
+        int count = (int) cars.stream().distinct().count();
+        if (count != cars.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private List<String> stringToList(String string) {
--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -3,6 +3,7 @@ package racing.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarsTest {
@@ -11,5 +12,12 @@ class CarsTest {
     @DisplayName("정상적인 Cars 객체 생성")
     void createCars(){
         assertDoesNotThrow(()->new Cars("pobi,crong,honux"));
+    }
+
+    @Test
+    @DisplayName("겹치는 자동차 이름이 존재하면 예외")
+    void createCarsWithDuplicateName(){
+        assertThatThrownBy(()->new Cars("pobi, pobi"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
