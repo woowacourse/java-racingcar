ef2bac968039d0ce40faa9532dd2bc43dae943b7
SungKyum Kim <sungkyum1@naver.com>
2024-02-14 16:38:36 +0900

    feat: 빈 문자열 입력 예외 처리 기능 구현

--- src/main/java/RacingCarGame/RacingCarGame.java
+++ src/main/java/RacingCarGame/RacingCarGame.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class RacingCarGame {
     public void run() {
-        final Scanner scanner = new Scanner(System.in);
-        String rawCarNames = scanner.nextLine();
+        boolean flag = true;
+        while (flag) {
+            try {
+                final Scanner scanner = new Scanner(System.in);
+                String rawCarNames = scanner.nextLine();
+                if (rawCarNames == null || rawCarNames.isEmpty()) {
+                    throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다. 다시 입력해주세요.");
+                }
 
-        String[] carNames = rawCarNames.split(",");
+                String[] carNames = rawCarNames.split(",");
+                flag = false;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+
+        // "포비,,브라운"
     }
 }
