1fa2de0fd2cb0d519a21ed390d709a5aeaefcf9d
eun-byeol <joeunbyeol98@gmail.com>
2024-02-14 15:09:09 +0900

    feat: main 클래스 생성, 입력 기능 구현

---
+++ src/main/java/racingcar/constant/ViewMessage.java
@@ -0,0 +1,17 @@
+package racingcar.constant;
+
+public enum ViewMessage {
+	INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."),
+	INPUT_TRY_COUNT("시도할 회수는 몇회인가요?");
+
+	private final String message;
+
+	ViewMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
+// INPUT_CAR_NAMES
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import static racingcar.constant.ViewMessage.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	private static final String SPLIT_DELIMITER = ",";
+	public List<String> readCarNames() {
+		System.out.println(INPUT_CAR_NAMES.getMessage());
+		return Arrays.stream(Console.readLine().strip().split(SPLIT_DELIMITER))
+				.toList();
+	}
+
+	public String readTryCount() {
+		System.out.println(INPUT_TRY_COUNT.getMessage());
+		return Console.readLine().strip();
+	}
+}
