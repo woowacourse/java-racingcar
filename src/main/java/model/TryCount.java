00c9f90d3a3c7ac367ee297efbed1aaf7dd6d89c
Nam Gi Beom <rlqja0523@naver.com>
2024-02-14 14:58:18 +0900

    feat: 시도 횟수 클래스 추가

---
+++ src/main/java/model/TryCount.java
@@ -0,0 +1,16 @@
+package model;
+
+public class TryCount {
+    private int tryCount;
+
+    public TryCount(int tryCount) {
+        verifyTryCount(tryCount);
+        this.tryCount = tryCount;
+    }
+
+    private void verifyTryCount(int tryCount) {
+        if (tryCount < 1 || tryCount > 120) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상 120 이하여야 합니다.");
+        }
+    }
+}
