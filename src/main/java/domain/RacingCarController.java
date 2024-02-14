dec5ba22d5cc69202b0cc5ee605a1514630514c2
jhon3242 <rldotnr2@gmail.com>
2024-02-14 16:44:00 +0900

    feat (InputView) : 시도 횟수 입력 기능 구현

--- src/main/java/domain/RacingCarController.java
+++ src/main/java/domain/RacingCarController.java
@@ -1,5 +1,19 @@
 package domain;
 
+import java.util.List;
+import view.InputView;
+
 public class RacingCarController {
 
+    public static void run() {
+        List<String> carNames = InputView.inputNames();
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .toList();
+
+        int count = InputView.inputTryCount();
+        if (count <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 회 이상이어야 합니다.");
+        }
+    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,19 +8,29 @@ import java.util.List;
 
 public class InputView {
 
-    public List<String> inputNames() {
+    public static List<String> inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = input();
         return Arrays.stream(input.split(","))
                 .toList();
     }
 
-    private String input() {
+    private static String input() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             return br.readLine();
         } catch (IOException e) {
             return input();
+        }
+    }
+
+    public static int inputTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        try {
+            return Integer.parseInt(input());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자이어야 합니다.");
         }
     }
 }
