25463f6bad7c82ec3b713372ec1d66ac23241b2b
takoyakimchi <c4nyou@gmail.com>
2024-02-14 20:36:47 +0900

    feat(service): 우승자 판단 로직 추가

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,6 +1,8 @@
 package service;
 
 import domain.Car;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Service {
 
@@ -11,6 +13,20 @@ public class Service {
         if (willMove()) {
             car.move();
         }
+    }
+
+    public static List<String> getWinnerNames(List<Car> cars) {
+        int maxScore = cars.stream()
+            .mapToInt(Car::getScore)
+            .max()
+            .orElseThrow(RuntimeException::new);
+
+        List<String> winnerNames = cars.stream()
+            .filter(car -> car.getScore() == maxScore)
+            .map(Car::getName)
+            .toList();
+
+        return winnerNames;
     }
 
     private static boolean willMove() {
