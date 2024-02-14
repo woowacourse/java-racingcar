76fac8c149f10b8221610be17037ecdada0478aa
HyungHoKim <hkim1109@naver.com>
2024-02-14 15:03:27 +0900

    feat(Car): 자동차 객체 생성
    자동차 이름을 받으면 객체를 생성한다.

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Car {
+    private final String name;
+    private int movement;
+
+    public Car(String name) {
+        this.name = name;
+        this.movement = 0;
+    }
+}
