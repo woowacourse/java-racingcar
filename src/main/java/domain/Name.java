8fb338b06a21ea752599adb8f96cab6f08b67ae3
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-14 16:33:06 +0900

    feat(Name): 자동차 이름 검증 기능 구현

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Name {
+    private static final String NAME_EXCEPTION_MESSAGE = "자동차의 이름은 공백이거나 %d자를 초과할 수 없습니다.";
+    private static final int MAX_LENGTH_OF_NAME = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank() || name.length() > MAX_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException(
+                    String.format(NAME_EXCEPTION_MESSAGE, MAX_LENGTH_OF_NAME));
+        }
+    }
+}
--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,11 +1,11 @@
 package view;
 
 public class InputValidator {
-    private static final String BLANK_ERROR_MESSAGE = "입력은 공백일 수 없습니다.";
+    private static final String BLANK_EXCEPTION_MESSAGE = "입력은 공백일 수 없습니다.";
 
     public static void validate(String input) {
         if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
+            throw new IllegalArgumentException(BLANK_EXCEPTION_MESSAGE);
         }
     }
 }
