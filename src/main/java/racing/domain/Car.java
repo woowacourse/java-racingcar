8c27746a95b9bb93e502780014824fde649a8d94
mzeong <mjeong@sookmyung.ac.kr>
2024-02-14 18:02:21 +0900

    feat: 자동차 이름이 5자 초과이면 예외 처리

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -4,6 +4,17 @@ public class Car {
     private final String name;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    public void validate(String name){
+        validateSize(name);
+    }
+
+    private void validateSize(String name) {
+        if(name.length() > 5 || name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +14,12 @@ class CarTest {
     @DisplayName("정상적인 자동차 객체 생성")
     void createCar() {
         assertDoesNotThrow(()->new Car("pobi"));
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자 초과이면 예외")
+    void createCarWithOverSize() {
+        assertThatThrownBy(()->new Car("pobipobi"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
