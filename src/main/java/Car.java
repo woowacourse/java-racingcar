5b5d981886dfa31175d55b16f2df38ae3f94a179
dwax1324 <dwax1324@gmail.com>
2024-02-14 20:12:17 +0900

    feat : 차 이름 검증

---
+++ src/main/java/Car.java
@@ -0,0 +1,42 @@
+public class Car {
+
+    private int distance;
+    private String name;
+
+    public Car(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (!isValidSize(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isValidSize(String name) {
+        return name.length() >= 1 && name.length() <= 5;
+    }
+
+
+    public static Car fromName(String name) {
+        return new Car(name);
+    }
+
+    public static Car fromEmpty() {
+        return new Car("");
+    }
+
+
+    public void move() {
+        this.distance++;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,38 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+
+    @Test
+    @DisplayName("차 객체를 생성한다.")
+    void car() {
+        assertThatCode(Car::fromEmpty).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("차를 움직이면 거리가 늘어난다.")
+    void carMove() {
+        Car car = Car.fromEmpty();
+        car.move();
+        assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("차 이름의 길이가 1 이상 5이하면 예외를 발생시키지 않는다")
+    void validCarName() {
+        assertThatCode(() -> Car.fromName("qwey")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("차 이름의 길이가 1 이상 5이하면 예외를 발생시킨다")
+    void invalidCarName() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Car.fromName("abcdefg"));
+    }
+}
