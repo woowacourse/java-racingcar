fd320f46227c281a455d26442de3a2109f42a69a
tackyu <tack970913@gmail.com>
2024-02-14 17:18:52 +0900

    feat : RaceCount Domain 생성
    이에 따른 테스트 코드 추가

---
+++ src/main/java/domain/RaceCount.java
@@ -0,0 +1,17 @@
+package domain;
+public class RaceCount {
+    private final Integer value;
+    public RaceCount(final Integer value){
+        validate(value);
+        this.value = value;
+    }
+    private void validate(Integer value){
+        if(value<=0){
+            throw new IllegalArgumentException("횟수가 0보다 커야합니다.");
+        }
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
