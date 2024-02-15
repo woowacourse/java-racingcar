87f8613fd7dc96dfd0b6280f78ee59489a4bd571
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-15 14:20:48 +0900

    refactor: 팩토리 메서드 추가 및 상수화
    - 이름만 파라미터로 사용하여 위치를 0으로 지정하여 Car 객체를 생성하는 팩토리 메서드 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,7 +1,9 @@
 public class Car {
 
+    private static final int MIN_NUMBER_FOR_MOVE = 4;
     private final CarName carName;
     private final CarLocation carLocation;
+
 
     private Car(CarName carName, CarLocation carLocation) {
         this.carName = carName;
@@ -15,8 +17,15 @@ public class Car {
         );
     }
 
+    public static Car from(String name) {
+        return new Car(
+                new CarName(name),
+                new CarLocation(0)
+        );
+    }
+
     public void move(int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= MIN_NUMBER_FOR_MOVE) {
             carLocation.increaseLocation();
         }
     }
