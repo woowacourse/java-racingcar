cd7862d3aa31288cd42cb59397b7d6a618d432c5
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-15 14:21:20 +0900

    feat: CarName 유효성 검증 추가

--- src/main/java/CarName.java
+++ src/main/java/CarName.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+
 public class CarName {
     private final String name;
-    
+
     public CarName(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (isInvalidNameLength(name)) {
+            throw new IllegalArgumentException("[ERROR] 이름 길이는 1~5글자");
+        }
+    }
+
+    private static boolean isInvalidNameLength(String name) {
+        return name == null || name.isBlank() || name.length() > 5;
     }
 
     public String getName() {
