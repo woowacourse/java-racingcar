90c1823f1b22720c5dabd18b947f7045f25d0e68
tackyu <tack970913@gmail.com>
2024-02-15 14:17:48 +0900

    feat : Splitter Util 생성
    이에 따른 테스트 코드 추가

---
+++ src/main/java/util/Splitter.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Splitter {
+    public static List<String> splitWithComma(String originalString){
+        return Arrays.stream(originalString.split(",")).toList();
+    }
+
+}
