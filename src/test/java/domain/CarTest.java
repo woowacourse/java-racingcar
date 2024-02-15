dbccf49dafcf18dcfb0301f4e79e92b15edbfa95
yoonseo choi <choiyoonseo02@gmail.com>
2024-02-15 14:39:02 +0900

    test: 자동차 이름 길이 검사

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,16 @@
+package domain;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+    @DisplayName("자동차 이름이 5자 초과하면 예외가 발생한다")
+    @Test
+    void validatorTest() {
+        Assertions.assertThatThrownBy(() -> new Car("애쉬릴리엘라"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자");
+    }
+}
