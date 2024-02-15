87f3e8d5aa5ae4847f87ae5cfabc9b22284541ba
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 13:54:34 +0900

    test: 테스트 데이터 추가 및 테스트명 구체화

--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class RoundTest {
 
-    @DisplayName("시도 횟수 입력")
+    @DisplayName("시도 횟수 입력 값을 검증한다.")
     @ParameterizedTest
+    @NullSource
     @ValueSource(strings = {"+4", "ss", "", "   ", "-1", "3.2", "0"})
     void roundValidationTest(String input) {
         assertThatThrownBy(() -> Round.from(input)).
