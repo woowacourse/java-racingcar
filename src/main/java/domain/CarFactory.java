ddf7ebf83c8746543efe415c8782cd4fbed9278f
kelly6bf <kelly6bf@gmail.com>
2024-02-15 14:04:29 +0900

    refactor: CarFactory 중복된 자동차 이름 검증 로직 추가

--- src/main/java/domain/CarFactory.java
+++ src/main/java/domain/CarFactory.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.Set;
 
 public class CarFactory {
 
@@ -8,6 +9,7 @@ public class CarFactory {
 
     public static Cars generateCars(List<String> carNames) {
         validateCarNamesCount(carNames);
+        validateDuplicatedCarNames(carNames);
 
         List<Car> cars = carNames.stream()
                 .map(name -> new Car(randomNumberGenerator, name))
@@ -19,6 +21,14 @@ public class CarFactory {
     private static void validateCarNamesCount(List<String> carNames) {
         if (carNames.size() < 2) {
             throw new RuntimeException("자동차 이름의 개수는 2이상이여야 합니다.");
+        }
+    }
+
+    private static void validateDuplicatedCarNames(List<String> carNames) {
+        int distinctSize = Set.of(carNames).size();
+
+        if (carNames.size() != distinctSize) {
+            throw new RuntimeException("중복된 자동차 이름을 허용하지 않습니다.");
         }
     }
 }
--- src/test/java/domain/CarFactoryTest.java
+++ src/test/java/domain/CarFactoryTest.java
@@ -19,4 +19,16 @@ class CarFactoryTest {
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("자동차 이름의 개수는 2이상이여야 합니다.");
     }
+    
+    @DisplayName("중복된 이름이 있는 문자열 리스트가 입력되면 예외가 발생한다.")
+    @Test
+    void initCarsThrowExceptionWhenDuplicatedCarNames() throws Exception {
+        // Given
+        List<String> invalidInput = List.of("test", "test");
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> CarFactory.generateCars(invalidInput))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("중복된 자동차 이름을 허용하지 않습니다.");
+    }
 }
