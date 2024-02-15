43d9a45fcab23512faee515a6f6debd236c0562d
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-15 14:34:23 +0900

    feat : 문자열 변환 기능을 담당하는 클래스 추가

---
+++ src/main/java/util/StringConvertor.java
@@ -0,0 +1,18 @@
+package util;
+
+import domain.Car;
+import domain.Cars;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringConvertor {
+    private static final String COMMA = ",";
+
+    private StringConvertor() {
+    }
+
+    public static List<Car> convertListSplitByComma(String value) {
+        return Arrays.stream(value.split(COMMA))
+                .map(Car::from).toList();
+    }
+}
