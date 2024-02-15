fccb4fc7a469ac8440d7e7161261b45620553819
tsulocalize <tsulocalize@gmail.com>
2024-02-15 13:50:47 +0900

    refactor : InputView 클래스의 getCarName() 메서드 분리 및 에러 메시지 상수 처리

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,44 +1,64 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    private final Scanner scanner;
+    private final static String MESSAGE_NO_DUPLICATE_CAR_NAMES = "중복된 자동차 이름은 사용할 수 없습니다.";
+    private final static String MESSAGE_LENGTH_OF_CAR_NAME = "자동차 이름은 1자 이상 5자 이하여야 합니다.";
+    private final static String MESSAGE_NO_SPACE = "중복된 자동차 이름은 사용할 수 없습니다.";
+    private final Scanner scanner = new Scanner(System.in);
     private final OutputView outputView;
 
-    public InputView() {
-        this.outputView = new OutputView();
-        this.scanner = new Scanner(System.in);
+
+    public InputView(OutputView outputView) {
+        this.outputView = outputView;
     }
 
     public List<String> getCarName() {
         outputView.printCarNameInputMessage();
         String input = scanner.nextLine();
-        List<String> splitCarNames = new ArrayList<>();
-
+        List<String> splitCarNames;
         try {
-            if (input.contains(" ")) {
-                throw new IllegalArgumentException("입력에 공백이 포함될 수 없습니다.");
-            }
-
-            splitCarNames = List.of(input.split(","));
-
-            for (String carName : splitCarNames) {
-                if (carName.isEmpty() || carName.length() > 5) {
-                    throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
-                }
-            }
-            long distinctCarCount = splitCarNames.stream().distinct().count();
-            if (distinctCarCount != splitCarNames.size()) {
-                throw new IllegalArgumentException("중복된 자동차 이름은 사용할 수 없습니다.");
-            }
+            validateNoSpace(input);
+            splitCarNames = getSplitCarNames(input);
+            validateLengthOfCarNames(splitCarNames);
+            validateNoDuplicatedCarNames(splitCarNames);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return getCarName();
         }
-
         return splitCarNames;
+    }
+
+    private void validateNoDuplicatedCarNames(List<String> splitCarNames) {
+        long distinctCarCount = splitCarNames.stream()
+                .distinct()
+                .count();
+        if (distinctCarCount != splitCarNames.size()) {
+            throw new IllegalArgumentException(MESSAGE_NO_DUPLICATE_CAR_NAMES);
+        }
+    }
+
+    private void validateLengthOfCarNames(List<String> splitCarNames) {
+        for (String carName : splitCarNames) {
+            validateLengthOfCarName(carName);
+        }
+    }
+
+    private void validateLengthOfCarName(String carName) {
+        if (carName.isEmpty() || carName.length() > 5) {
+            throw new IllegalArgumentException(MESSAGE_LENGTH_OF_CAR_NAME);
+        }
+    }
+
+    private List<String> getSplitCarNames(String input) {
+        return List.of(input.split(","));
+    }
+
+    private void validateNoSpace(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException(MESSAGE_NO_SPACE);
+        }
     }
 
     public int getTryCount() {
--- src/main/java/RacingcarApplication.java
+++ src/main/java/RacingcarApplication.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class RacingcarApplication {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
-        InputView inputView = new InputView();
+        InputView inputView = new InputView(outputView);
         List<Car> cars = new ArrayList<>();
 
         List<String> carNames = inputView.getCarName();
