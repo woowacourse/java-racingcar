97118f9282c6488717eab0afb26627c879be7d23
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 15:32:58 +0900

    test: 테스트명 구체화

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,6 +12,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
+    @DisplayName("랜덤 숫자가 4 이상일 때만 자동차가 전진한다.")
     @ParameterizedTest
     @CsvSource({"5, 1", "3, 0", "4, 1"})
     void moveForwardTest(int randomNumber, int expectedPosition) {
@@ -20,6 +22,7 @@ public class CarTest {
         assertThat(car.getPosition()).isEqualTo(expectedPosition);
     }
 
+    @DisplayName("자동차 이름이 공백 포함 5자 이하가 아닌 경우 에러가 발생한다.")
     @Test
     void carNameLengthTest() {
         String wrongName = "abcdef";
@@ -28,6 +31,7 @@ public class CarTest {
                 .isInstanceOf(IllegalStateException.class);
     }
 
+    @DisplayName("자동차 이름이 공백(Blank)이거나 null인 경우 경우 에러가 발생한다.")
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"", "    "})
