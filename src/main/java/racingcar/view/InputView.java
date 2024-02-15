5888454dbe273b1bcabd100fdb3e2a6d485aedf7
Zeus6768 <akaps6768@gmail.com>
2024-02-15 14:55:54 +0900

    fix: 입력 시 공백 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
 
     public Cars askCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carNames = Console.readLine();
+        String carNames = readLine();
         String[] splittedInput = carNames.split(",");
         List<Car> cars = Arrays.stream(splittedInput).map(Car::new).toList();
         return new Cars(cars);
@@ -18,6 +18,10 @@ public class InputView {
 
     public int askMoveCount() {
         System.out.println("\n시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
+        return Integer.parseInt(readLine());
+    }
+
+    private String readLine() {
+        return Console.readLine().trim();
     }
 }
