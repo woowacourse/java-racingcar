433544a88db9254211764b89d3105d4436b4c14f
haiseong <wjdgotjd9908@gmail.com>
2024-02-15 16:24:36 +0900

    refactor: Scanner 변수명 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,11 +8,11 @@ import java.util.stream.Stream;
 
 public class InputView {
 
-    private final Scanner sc = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = sc.nextLine();
+        String input = scanner.nextLine();
 
         return Stream.of(input.split(","))
                 .map(String::trim)
@@ -21,7 +21,7 @@ public class InputView {
 
     public RacingCount inputRacingCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int input = parseNumber(sc.nextLine());
+        int input = parseNumber(scanner.nextLine());
 
         return new RacingCount(input);
     }
