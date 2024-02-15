6e85a60f9f23e85b8fc5dbc9ef4b5d9a2752265c
le2sky <leehaneul990623@gmail.com>
2024-02-15 16:10:09 +0900

    refactor : 입출력 기능 가독성 개선

--- src/main/java/racinggame/Main.java
+++ src/main/java/racinggame/Main.java
@@ -1,43 +1,22 @@
 package racinggame;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 import racinggame.domain.Car;
 import racinggame.domain.RacingGame;
 import racinggame.domain.Round;
-import racinggame.domain.RoundResult;
 import racinggame.domain.condition.RandomMoveCondition;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String carName = br.readLine();
-        List<String> carNames = Arrays.asList(carName.split(","));
-        List<Car> cars = carNames.stream()
-            .map(Car::from)
-            .toList();
-
+        List<Car> cars = InputView.readNames();
+        Round round = InputView.readRound();
         RacingGame game = RacingGame.of(cars, new RandomMoveCondition());
 
-        System.out.println("시도할 회수는 몇회인가요?");
-        Round round = Round.from(Integer.parseInt(br.readLine()));
-
         game.race(round);
-        List<RoundResult> results = game.getResult();
 
-        results.forEach(result -> {
-            result.carInfos().forEach(carInfo -> {
-                System.out.println(carInfo.name() + " : " + "-".repeat(carInfo.position()));
-            });
-
-            System.out.println();
-        });
-
-        System.out.format("%s가 최종 우승했습니다.", String.join(", ", game.findWinnerName()));
+        OutputView.printResult(game.getResult());
+        OutputView.printWinner(game.findWinnerName());
     }
 }
