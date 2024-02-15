eff0843d839ffcb1562dc6ed960144e14c6f6e6f
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-15 15:01:34 +0900

    feat(OutputView): 라운드 별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,33 @@
 package racingcar.view;
 
+import racingcar.dto.CarDto;
+
+import java.util.List;
+
 public class OutputView {
     private static final String NEWLINE = System.lineSeparator();
     private static final String RESULT_MESSAGE = NEWLINE + "실행 결과";
+    private static final String COLON = " : ";
+    private static final String POSITION_EXPRESSION_SYMBOL = "-";
 
     public void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printRoundResult(List<CarDto> cars) {
+        for (CarDto car : cars) {
+            printCarInformation(car);
+        }
+        System.out.println();
+    }
+
+    private void printCarInformation(CarDto car) {
+        StringBuilder informationBuilder = new StringBuilder();
+
+        informationBuilder.append(car.getName())
+                .append(COLON)
+                .append(POSITION_EXPRESSION_SYMBOL.repeat(car.getPosition()));
+
+        System.out.println(informationBuilder);
     }
 }
