26895a90ae32da9ef7a7e7a5f4acbe62fae31384
SungKyum Kim <sungkyum1@naver.com>
2024-02-15 11:07:13 +0900

    test: 반복 횟수 도메인 테스트 코드 생성

--- src/test/java/domain/CountTest.java
+++ src/test/java/domain/CountTest.java
@@ -1,4 +1,14 @@
+package domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CountTest {
-  
+    @ParameterizedTest
+    @ValueSource(ints = {0,-1})
+    void countFailTest(int value) {
+        assertThrows(IllegalArgumentException.class, () -> new Count(value));
+    }
 }
