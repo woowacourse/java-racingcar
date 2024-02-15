ff58cf0bcdee46f3675f526d874c4e8318fcc7f6
tsulocalize <tsulocalize@gmail.com>
2024-02-15 15:41:57 +0900

    refactor : 랜덤 숫자 생성 기능 외부 클래스로 분리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,5 @@
 package racingcar.domain;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Car {
 
     private String name;
@@ -22,8 +20,7 @@ public class Car {
 
     // 0 ~ 9 까지의 랜덤한 수를 생성해 4이상일 경우 전진, 4미만일 경우 정지한다.
     public void moveCar() {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        int randomNumber = random.nextInt(10);
+        int randomNumber = RandomGenerator.getRandomNumberUnderTen();
         if (randomNumber >= 4) {
             distance++;
         }
---
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomGenerator {
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    public static int getRandomNumberUnderTen() {
+        return random.nextInt(10);
+    }
+}
