d787e8d0d63b08423cc74bc93e03273df444da55
PgmJun <chltmdwns96@gmail.com>
2024-02-15 15:57:22 +0900

    test: Cars 객체 생성 테스트 작성
    - 입력받은 자동차의 개수가 2대 이상이면 객체 생성에 성공한다
    - 입력받은 자동차의 개수가 1대 이하이면 객체 생성에 실패한다

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,12 +1,44 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class CarsTest {
+    private static CarAccelerator accelerator;
+
+    @BeforeAll
+    static void init() {
+        accelerator = new CarAccelerator();
+    }
+
+
+    @Test
+    @DisplayName("입력받은 자동차의 개수가 2대 이상이면 객체 생성에 성공한다")
+    void createCarsSuccess() {
+        //given
+        Car car1 = new Car("pobi", accelerator);
+        Car car2 = new Car("pobi", accelerator);
+        List<Car> carInfos = List.of(car1, car2);
+        //when
+        //then
+        Assertions.assertThatCode(() -> new Cars(carInfos, accelerator));
+    }
+
+    @Test
+    @DisplayName("입력받은 자동차의 개수가 1대 이하이면 객체 생성에 실패한다")
+    void createCarsFail() {
+        //given
+        Car car = new Car("pobi", accelerator);
+        List<Car> carInfos = List.of(car);
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> new Cars(carInfos, accelerator))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("자동차들 중 가장 많이 이동한 차량의 Position을 반환한다")
