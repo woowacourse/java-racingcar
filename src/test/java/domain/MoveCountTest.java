e360fdf2d6dc226b0d6480321c557265be8f2b2a
SCY <helenason@naver.com>
2024-02-15 13:52:17 +0900

    fix: 이동 횟수의 범위 변경에 따른 부수 효과 정리

--- src/test/java/domain/MoveCountTest.java
+++ src/test/java/domain/MoveCountTest.java
@@ -10,17 +10,17 @@ import static org.assertj.core.api.Assertions.*;
 @DisplayName("자동차 이동 횟수 도메인 테스트")
 class MoveCountTest {
 
-    @DisplayName("자동차 이동 횟수가 음수일 경우 도메인이 생성되지 않는다.")
+    @DisplayName("자동차 이동 횟수가 음수이거나 0일 경우 도메인이 생성되지 않는다.")
     @ParameterizedTest
-    @ValueSource(ints = {-1, -2, -333})
+    @ValueSource(ints = {0, -1, -2, -333})
     void testCreateMoveCountWithInvalidValue(int moveCount) {
         assertThatThrownBy(() -> MoveCount.from(moveCount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("자동차 이동 횟수가 음수가 아닐일 경우 도메인이 생성된다.")
+    @DisplayName("자동차 이동 횟수가 양의 정수일 경우 도메인이 생성된다.")
     @ParameterizedTest
-    @ValueSource(ints = {0, 3, 5})
+    @ValueSource(ints = {1, 3, 5})
     void testCreateMoveCountWithValidValue(int moveCount) {
         assertThatCode(() -> MoveCount.from(moveCount)).doesNotThrowAnyException();
     }
@@ -36,7 +36,8 @@ class MoveCountTest {
     @DisplayName("움직일 수 있는 횟수가 0인지 체크할 수 있다")
     @Test
     void testDetectCountZero() {
-        MoveCount moveCount = MoveCount.from(0);
+        MoveCount moveCount = MoveCount.from(1);
+        moveCount.consume();
         assertThat(moveCount.isCountZero()).isTrue();
     }
 
