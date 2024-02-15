bdf228b3e364e7bd50d8468a7a23078b9a9656dc
HyungUk Ryu <hw0603@naver.com>
2024-02-15 16:27:01 +0900

    refactor(randomNumberGenerator): 랜덤 범위 상수 포장

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -3,8 +3,10 @@ package racingcar.utils;
 import java.util.Random;
 
 public class RandomNumberGenerator {
+    private static final int RANDOM_BOUND = 10;
+
     public static int generate() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(RANDOM_BOUND);
     }
 }
