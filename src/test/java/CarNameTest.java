1b8d80b23cf51f2d272eea734238ad708fabd9f4
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-15 14:23:04 +0900

    test: CarName의 테스트 class

---
+++ src/test/java/CarNameTest.java
@@ -0,0 +1,17 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CarNameTest {
+
+    @DisplayName("유효하지 않은 이름 길이에 대한 예외 발생 확인")
+    @ParameterizedTest(name = "{0}")
+    @CsvSource(value = {"빈 입력 :  ", "5글자 초과 : abcdef"}, delimiter = ':')
+    void constructor(String caseName, String carName) {
+        assertThatThrownBy(() -> new CarName(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
