af260a72b55868c03ad6aa36ffa6c841334d32d5
eun-byeol <joeunbyeol98@gmail.com>
2024-02-15 15:25:58 +0900

    test: 자동차 이름 중복 입력 테스트 케이스 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -25,19 +25,40 @@ class CarTest {
         car = new Car(VALID_CAR_NAME);
     }
 
-    @DisplayName("이름이 5자 초과이면 예외를 던진다")
+    @DisplayName("이름이 1자 미만 5자 초과이면 예외를 던진다")
     @ParameterizedTest
-    @ValueSource(strings = {"점심은순두부", "1234567"})
+    @ValueSource(strings = {"점심은순두부", "1234567", ""})
     void testInvalidNameLength(String carName) {
         assertThatThrownBy(() -> new Car(carName))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("이름이 5자 이하이면 예외를 던지지 않는다")
+    @DisplayName("이름이 1자 이상 5자 이내이면 예외를 던지지 않는다")
     @ParameterizedTest
     @ValueSource(strings = {"점심순두부", "1"})
     void testValidNameLength(String carName) {
         assertDoesNotThrow(() -> new Car(carName));
+    }
+
+    @DisplayName("중복된 자동차 이름이 존재하면 예외를 던진다")
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedCarNames")
+    void testUniqueCarNames(List<Car> cars) {
+        assertThatThrownBy(() -> Car.validateUniqueCarNames(cars))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideDuplicatedCarNames() {
+        return Stream.of(
+                Arguments.of(createCarsWithName(List.of("조조네조", "감쟈감쟈", "조조네조"))),
+                Arguments.of(createCarsWithName(List.of("123", "123", "1 2 3")))
+        );
+    }
+
+    private static List<Car> createCarsWithName(List<String> carNames) {
+        return carNames.stream()
+                .map(Car::new)
+                .toList();
     }
 
     @DisplayName("오일이 4 미만이면 이동하지 않는다")
