eb09c086547aeb6f51167866501e27bcb3c2f521
kelly6bf <kelly6bf@gmail.com>
2024-02-15 14:09:42 +0900

    reafactor: 입력된 자동차 이름 문자열에 앞뒤 공백을 제거하는 로직 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,20 +2,21 @@ package view;
 
 import domain.RacingCount;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class InputView {
 
     Scanner sc = new Scanner(System.in);
 
-    // TODO : 입력값 검증 및 재입력 기능 추가
     public List<String> inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = sc.nextLine();
 
-        return Collections.unmodifiableList(List.of(input.split(",")));
+        return Stream.of(input.split(","))
+                .map(String::trim)
+                .toList();
     }
 
     public RacingCount inputRacingCount() {
