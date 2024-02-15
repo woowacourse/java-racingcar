9b1eea8f72e72e5c348052777dfea246cf4a4c37
jongmee <ra123221@gmail.com>
2024-02-15 09:56:51 +0900

    feat: controller에서 입력 및 자동차 이동 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,12 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+import racingcar.util.Parser;
+import racingcar.util.Validator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RacingController {
     private final InputView inputView;
@@ -14,5 +19,30 @@ public class RacingController {
         this.outputView = outputView;
     }
 
+    public void run() {
+        String carNames = inputView.readCarNames();
+        Validator.validateNullName(carNames);
+        List<String> parsedCarNames = Parser.parseCarNames(carNames);
+        Validator.validateDuplicatedNames(parsedCarNames);
 
+        Cars cars = new Cars();
+        for(String name: parsedCarNames) {
+            Validator.validateCarNamesFormat(name);
+            cars.addCar(name);
+        }
+
+        String tryCount = inputView.readTryCount();
+        int parsedTryCount = Validator.validateInteger(tryCount);
+        Validator.validateTryCount(parsedTryCount);
+
+        outputView.printResultMsg();
+        for(int i = 0 ; i < parsedTryCount; i++) {
+            moveCars(cars);
+        }
+    }
+
+    private void moveCars(final Cars cars) {
+        cars.moveAllCars();
+        outputView.printCarPosition(cars);
+    }
 }
