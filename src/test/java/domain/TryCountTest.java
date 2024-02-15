0f1d97bce6e5a71b8927ca47d6f66f030f29d22d
PgmJun <chltmdwns96@gmail.com>
2024-02-15 16:01:04 +0900

    test: TryCount 객체 생성 테스트 작성
    - 시도 횟수가 1 이상이면 객체 생성에 성공한다
    - 시도 횟수가 1 미만이면 객체 생성에 실패한다

---
+++ src/test/java/domain/TryCountTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TryCountTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    @DisplayName("시도 횟수가 1 이상이면 객체 생성에 성공한다")
+    void createTryCountSuccess(int value) {
+        Assertions.assertThatCode(() -> new TryCount(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -1, 0})
+    @DisplayName("시도 횟수가 1 미만이면 객체 생성에 실패한다")
+    void createTryCountFail(int value) {
+        Assertions.assertThatThrownBy(() -> new TryCount(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
