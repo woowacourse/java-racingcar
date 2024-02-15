c1bae854f1d07da7f61cf9dd23d81a156cdc6154
yechop <channetattoo@gmail.com>
2024-02-15 13:56:15 +0900

    feat(Race): 자동차 경주 생성

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -4,18 +4,11 @@ public class Controller {
 
     private final View view = new View();
 
-    void start() {
+    public void start() {
         List<Car> cars = view.readCars();
         int count = view.readCount();
 
-        for (Car car : cars) {
-            int random = (int) (Math.random() * 10) + 1;
-            if (random > 4) {
-                car.go();
-            }
-        }
-
-
+        Race race = new Race(count, cars);
     }
 
 }
---
+++ src/main/java/Race.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Race {
+
+    private final int count;
+    private final List<Car> cars;
+
+    public Race(int count, List<Car> cars) {
+        this.count = count;
+        this.cars = cars;
+    }
+}
