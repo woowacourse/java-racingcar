4a7a250a4b1ef45379587a4b15f5a968ddb2d08d
donghoony <aru0504@naver.com>
2024-02-15 13:07:43 +0900

    style(NotANumberException): 컨벤션에 맞게 수정

--- src/main/java/racingcar/exception/NotANumberException.java
+++ src/main/java/racingcar/exception/NotANumberException.java
@@ -1,6 +1,6 @@
 package racingcar.exception;
 
-public class NotANumberException extends RacingCarException{
+public class NotANumberException extends RacingCarException {
 
     public NotANumberException() {
         super("입력한 문자를 정수로 변환할 수 없습니다.");
