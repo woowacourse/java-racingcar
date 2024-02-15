d3e1b57f952cc9e2c41bbb2b56cb64baefb56b08
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 13:43:52 +0900

    test: 이름이 공백이거나 null인 경우 예외 검증 테스트

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -6,13 +6,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
     @ParameterizedTest
     @CsvSource({"5, 1", "3, 0", "4, 1"})
     void moveForwardTest(int randomNumber, int expectedPosition) {
-        Car car = new Car("lemone");
+        Car car = new Car("lemon");
         car.moveForward(randomNumber);
 
         assertThat(car.getPosition()).isEqualTo(expectedPosition);
@@ -22,6 +24,14 @@ public class CarTest {
     void carNameLengthTest() {
         String wrongName = "abcdef";
 
+        assertThatThrownBy(() -> new Car(wrongName))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "    "})
+    void carNameBlankOrNullTest(String wrongName) {
         assertThatThrownBy(() -> new Car(wrongName))
                 .isInstanceOf(IllegalStateException.class);
     }
