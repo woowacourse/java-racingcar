db6036eac476143b3d32cd25c3b84bd62f91286d
hangillee <skfcb10@naver.com>
2024-02-15 11:44:46 +0900

    chore: 매직 넘버 상수화

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -1,12 +1,15 @@
 package domain;
 
 public class Racing {
+    private static final int RANDOM_NUMBER_UPPER_BOUND = 10;
+    private static final int MOVE_FORWARD_LOWER_BOUND = 3;
+
     private static Integer generateRandomNumber() {
-        return (int) (Math.random() * 10);
+        return (int) (Math.random() * RANDOM_NUMBER_UPPER_BOUND);
     }
 
     public static boolean isMoveForward() {
         int number = generateRandomNumber();
-        return number > 3;
+        return number > MOVE_FORWARD_LOWER_BOUND;
     }
 }
