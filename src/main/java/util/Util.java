b7dbdc9c052fbf490b73ee8e47f39bfbf7130b5f
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 14:24:35 +0900

    refactor: 랜덤 숫자 추출 계산식 상수화

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -2,7 +2,10 @@ package util;
 
 public class Util {
 
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     public static int generateRandomNumber() {
-        return (int) (Math.random() * 10);
+        return (int) (Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
     }
 }
