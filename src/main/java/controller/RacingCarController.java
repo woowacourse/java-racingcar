24d8f709cf90ea32544a1156f04a37f03b36566a
eunjungL <dldmswjd322@gmail.com>
2024-02-15 14:32:22 +0900

    feat : 자동차 이름 입력 기능 추가

---
+++ src/main/java/controller/RacingCarController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import domain.Cars;
+import service.RacingCarService;
+import view.InputView;
+
+public class RacingCarController {
+
+    private final InputView inputView;
+    private final RacingCarService racingCarService;
+
+    public RacingCarController(RacingCarService racingCarService, InputView inputView) {
+        this.racingCarService = racingCarService;
+        this.inputView = inputView;
+    }
+
+    public void run() {
+
+    }
+
+    private Cars getCars() {
+        String carsName = inputView.inputCarsName();
+        return null;
+    }
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String inputCarsName() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine().trim();
+    }
+}
