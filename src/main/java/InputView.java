0725d763b7a677ec2530e12abc3fbbf026b0ec37
tsulocalize <tsulocalize@gmail.com>
2024-02-15 14:02:54 +0900

    refactor : InputView 클래스의 getTryCount() 메서드 분리 및 에러 메시지 상수 처리

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -6,6 +6,8 @@ public class InputView {
     private final static String MESSAGE_NO_DUPLICATE_CAR_NAMES = "중복된 자동차 이름은 사용할 수 없습니다.";
     private final static String MESSAGE_LENGTH_OF_CAR_NAME = "자동차 이름은 1자 이상 5자 이하여야 합니다.";
     private final static String MESSAGE_NO_SPACE = "중복된 자동차 이름은 사용할 수 없습니다.";
+    private final static String MESSAGE_ONLY_NUMBER = "시도할 회수는 숫자여야만 가능합니다.";
+    private final static String MESSAGE_ONLY_NATURAL_NUMBER = "시도할 회수는 자연수를 입력해 주세요.";
     private final Scanner scanner = new Scanner(System.in);
     private final OutputView outputView;
 
@@ -28,6 +30,29 @@ public class InputView {
             return getCarName();
         }
         return splitCarNames;
+    }
+
+    public int getTryCount() {
+        outputView.printTryCountInputMessage();
+        String input = scanner.nextLine();
+        int tryCount;
+        try {
+            tryCount = Integer.parseInt(input);
+            validateNaturalNumber(tryCount);
+        } catch (NumberFormatException e) {
+            System.out.println(MESSAGE_ONLY_NUMBER);
+            return getTryCount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getTryCount();
+        }
+        return tryCount;
+    }
+
+    private void validateNaturalNumber(int tryCount) {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException(MESSAGE_ONLY_NATURAL_NUMBER);
+        }
     }
 
     private void validateNoDuplicatedCarNames(List<String> splitCarNames) {
@@ -59,24 +84,5 @@ public class InputView {
         if (input.contains(" ")) {
             throw new IllegalArgumentException(MESSAGE_NO_SPACE);
         }
-    }
-
-    public int getTryCount() {
-        outputView.printTryCountInputMessage();
-        String input = scanner.nextLine();
-        int tryCount;
-        try {
-            tryCount = Integer.parseInt(input);
-            if (tryCount <= 0) {
-                throw new IllegalArgumentException("시도할 회수는 자연수를 입력해 주세요.");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("시도할 회수는 숫자여야만 가능합니다.");
-            return getTryCount();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getTryCount();
-        }
-        return tryCount;
     }
 }
