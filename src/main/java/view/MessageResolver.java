4731899504823af296eb8a1283b35de0dc7f08e6
SCY <helenason@naver.com>
2024-02-15 12:47:30 +0900

    feat: 출력 메시지 리졸버 구현

---
+++ src/main/java/view/MessageResolver.java
@@ -0,0 +1,29 @@
+package view;
+
+import domain.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MessageResolver {
+
+    public String resolveMoveResultMessage(List<Car> cars) {
+        return cars.stream()
+                .map(car -> car.getCarName().toString() + " : " + resolveCarPositionMessage(car.getPosition()))
+                .collect(Collectors.joining("\n")) + "\n";
+    }
+
+    private String resolveCarPositionMessage(Integer carPosition) {
+        String message = "";
+        for (int i = 0; i < carPosition; i++) {
+            message += "-";
+        }
+        return message;
+    }
+
+    public String resolveWinnerMessage(List<Car> winners) {
+        return winners.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", ")) + "가 최종 우승했습니다.";
+    }
+}
