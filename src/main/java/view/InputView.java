2340cecd20c2106cb56a1b76f837ee670e0f83af
kelly6bf <kelly6bf@gmail.com>
2024-02-15 13:18:04 +0900

    refactor: InputView 입력 안내 메시지 추가 및 패키지 경로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,6 @@
-package input;
+package view;
+
+import domain.RacingCount;
 
 import java.util.Collections;
 import java.util.List;
@@ -10,8 +12,16 @@ public class InputView {
 
     // TODO : 입력값 검증 및 재입력 기능 추가
     public List<String> inputCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = sc.nextLine();
 
         return Collections.unmodifiableList(List.of(input.split(",")));
+    }
+
+    public RacingCount inputRacingCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int input = sc.nextInt();
+
+        return new RacingCount(input);
     }
 }
