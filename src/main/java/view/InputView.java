7393359b271e7922feb6c463f987b29bffbacf44
hoyeonyy <highoption@naver.com>
2024-02-15 14:16:56 +0900

    style(InputView) : 출력문 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분.)");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.next();
     }
 
