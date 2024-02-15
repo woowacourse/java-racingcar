3eb4d45a50b67ca96a6acf190cc29529ed83c287
Nam Gi Beom <rlqja0523@naver.com>
2024-02-15 18:08:31 +0900

    test: 우승자를 찾는 기능 테스트

--- src/test/java/application/RaceServiceTest.java
+++ src/test/java/application/RaceServiceTest.java
@@ -1,10 +1,12 @@
 package application;
 
+import static fixture.CarFixture.자동차;
 import static fixture.CarFixture.자동차들;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import model.Car;
 import model.Cars;
 import org.junit.jupiter.api.Test;
 import util.NumberGenerator;
@@ -43,6 +45,46 @@ class RaceServiceTest {
                 () -> assertThat(cars.getCars().get(2).getPosition()).isEqualTo(1)
         );
     }
+
+    @Test
+    void 단독_우승자를_찾는다() {
+        // given
+        Car car1 = 자동차("prin");
+        Car car2 = 자동차("mark");
+        Car car3 = 자동차("pobi");
+        Cars cars = 자동차들(car1, car2, car3);
+        NumberGenerator numberGenerator = new TestNumberGenerator(List.of(5, 3, 2));
+        RaceService raceService = new RaceService(numberGenerator);
+        raceService.moveCars(cars);
+
+        // when
+        List<String> winners = raceService.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(1)
+                .contains(car1.getName());
+
+    }
+
+    @Test
+    void 공동_우승자를_찾는다() {
+        // given
+        Car car1 = 자동차("prin");
+        Car car2 = 자동차("mark");
+        Car car3 = 자동차("pobi");
+        Cars cars = 자동차들(car1, car2, car3);
+        NumberGenerator numberGenerator = new TestNumberGenerator(List.of(3, 4, 5));
+        RaceService raceService = new RaceService(numberGenerator);
+        raceService.moveCars(cars);
+
+        // when
+        List<String> winners = raceService.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(2)
+                .contains(car2.getName(), car3.getName());
+    }
+
 
     class TestNumberGenerator implements NumberGenerator {
         private final List<Integer> numbers;
