c0da4f839c9a9a9425ad05b9427d09c3ddcd0a23
hoyeonyy <highoption@naver.com>
2024-02-15 14:22:50 +0900

    feat(OutputView) : showRoundResult 메서드 생성
    - 매 라운드 마다 결과를 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,24 @@
 package view;
 
+import model.Car;
+import model.Race;
+
 public class OutputView {
     public static void showRaceResult() {
         System.out.println("\n실행 결과");
+    }
+
+    public static void showRoundResult(Race race) {
+        StringBuilder roundResult = new StringBuilder();
+
+        for (Car car : race.getCars()) {
+            roundResult.append(car.getName())
+                    .append(" : ")
+                    .append(car.showDistance())
+                    .append("\n");
+        }
+
+        System.out.println(roundResult);
     }
 
 }
