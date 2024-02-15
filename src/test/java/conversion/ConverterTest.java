26ac36ecb710a1ba50278b732ad78866500d4256
takoyakimchi <c4nyou@gmail.com>
2024-02-15 16:44:03 +0900

    refactor(test): 자동차 이름 입력 시 구분자 전후 공백 제거 테스트

--- src/test/java/conversion/ConverterTest.java
+++ src/test/java/conversion/ConverterTest.java
@@ -1,9 +1,9 @@
 package conversion;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import domain.Car;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +11,25 @@ import org.junit.jupiter.api.Test;
 class ConverterTest {
 
     @Test
-    @DisplayName("자동차 이름 변환 작업 테스트: String -> List<Car>")
+    @DisplayName("자동차 이름 변환 작업 테스트 (1): String -> List<Car>")
     void toCars() {
         // given
         String rawCarNames = "aa,bb,cc";
+
+        // when
+        List<Car> carsActual = Converter.toCars(rawCarNames);
+
+        // then
+        assertEquals("aa", carsActual.get(0).getName());
+        assertEquals("bb", carsActual.get(1).getName());
+        assertEquals("cc", carsActual.get(2).getName());
+    }
+
+    @Test
+    @DisplayName("자동차 이름 변환 작업 테스트 (2): 공백 제거 테스트")
+    void toCars_trimBlank() {
+        // given
+        String rawCarNames = "  aa  ,    bb , cc  ";
 
         // when
         List<Car> carsActual = Converter.toCars(rawCarNames);
