d2ac1a9098dc6c56a05c8974c304de3422159d8e
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 11:09:38 +0900

    test: 자동차 이름 입력 예외 검증 테스트

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,7 +1,9 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,10 +12,17 @@ public class CarTest {
     @ParameterizedTest
     @CsvSource({"5, 1", "3, 0", "4, 1"})
     void moveForwardTest(int randomNumber, int expectedPosition) {
-        Car car = new Car();
+        Car car = new Car("lemone");
         car.moveForward(randomNumber);
 
         assertThat(car.getPosition()).isEqualTo(expectedPosition);
     }
 
+    @Test
+    void carNameLengthTest() {
+        String wrongName = "abcdef";
+
+        assertThatThrownBy(() -> new Car(wrongName))
+                .isInstanceOf(IllegalStateException.class);
+    }
 }
