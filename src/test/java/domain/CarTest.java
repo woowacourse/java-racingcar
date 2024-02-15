db17891d4068712aa343214b63df7f78c1c40de5
PgmJun <chltmdwns96@gmail.com>
2024-02-15 15:33:42 +0900

    test: Car 객체 테스트 로직 추가 구현
    - 자동차가 이동하지 않는 경우 테스트
    - 엑셀 push 기능 테스트

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -28,12 +29,26 @@ class CarTest {
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
-//    @Test
-//    @DisplayName("4 미만의 값을 받으면 자동차가 이동하지 않는다")
-//    void doNotMoveCar() {
-//        //given
-//        Car car = new Car("pobi");
-//        //then
-//        assertThat(car.getPosition()).isEqualTo(0);
-//    }
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1, 2, 3})
+    @DisplayName("4 미만의 값을 받으면 자동차가 이동하지 않는다")
+    void doNotMoveCar(int power) {
+        //given
+        Car car = new Car("pobi", accelerator);
+        //when
+        car.moveForward(power);
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("엑셀을 push하면 0~9 사이의 값만 리턴한다")
+    void return0To9WhenPushAccelerator() {
+        //given
+        Car car = new Car("pobi", accelerator);
+        //when
+        int actual = car.pushAccelerator();
+        //then
+        assertThat(actual).isBetween(CarAccelerator.MIN_ACCEL_POWER, CarAccelerator.MAX_ACCEL_POWER);
+    }
 }
