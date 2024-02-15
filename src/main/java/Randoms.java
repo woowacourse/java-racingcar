75f6e09f5315d07f2fe21d421e78aa3da8a72f8e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-15 14:22:09 +0900

    comment: 주석 추가

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -6,8 +6,7 @@ public class Randoms {
     private static final Random random = new Random();
 
     /**
-     *
-     * @param count 뽑을 숫자 개수
+     * @param count        뽑을 숫자 개수
      * @param endInclusive 숫자 범위
      * @return 0부터 endInclusive까지의 count만큼의 랜덤 숫자 리스트
      */
@@ -20,6 +19,10 @@ public class Randoms {
         return randomNumbers;
     }
 
+    /**
+     * @param endInclusive 최댓값
+     * @return 0 이상 endInclusive 이하의 랜덤 정수
+     */
     public static int getRandomNumber(int endInclusive) {
         return random.nextInt(endInclusive + 1);
     }
