3bb6c6cdb358b6c7c8d46e0a24402f328b739c6e
jhon3242 <rldotnr2@gmail.com>
2024-02-15 17:46:06 +0900

    refactor : 매직 상수 처리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,20 +2,23 @@ package domain;
 
 public class Car {
 
+    public static final int DEFAULT_POSITION = 0;
+    public static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position;
 
     public Car(String name) {
         validate(name);
         this.name = name;
-        this.position = 0;
+        this.position = DEFAULT_POSITION;
     }
 
     private void validate(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차의 이름은 빈칸이 아니어야 합니다. : " + name);
         }
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하이어야 합니다. : " + name);
         }
     }
