6b0c88a8f5d6629c06b89310cdc14c7cf31d7a30
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 13:30:06 +0900

    test: 자동차 이름 중복 예외 검증 테스트

---
+++ src/test/java/model/CarsTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarsTest {
+
+    @DisplayName("자동차 이름이 중복되면 에러가 발생한다.")
+    @Test
+    void nameDuplicatedTest() {
+        List<String> carNames = List.of("lemon","lemon","a","nyang");
+        assertThatThrownBy(() -> Cars.from(carNames)).isInstanceOf( IllegalArgumentException.class);
+    }
+}
