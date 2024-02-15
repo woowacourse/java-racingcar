d0f656ba9599f889b5b2209828c589c22bd7f23b
takoyakimchi <c4nyou@gmail.com>
2024-02-15 13:49:00 +0900

    style(view): 해결된 TODO 삭제

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class InputView {
 
-    // TODO: 실패 시 에러처리 및 재입력 받는 부분 작성하기
     public static String read() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
