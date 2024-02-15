5d89ea10f07d237469e5bbe1abaac0e081b508d2
hoyeonyy <highoption@naver.com>
2024-02-15 15:07:48 +0900

    feat(Name) : validateNameLength 메서드 생성
    - 이름이 공백이거나 5글자 보다 큰 경우 예외 발생

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -4,7 +4,14 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("이름의 길이는 1~5글자 사이만 가능합니다.");
+        }
     }
 
     public String getName() {
