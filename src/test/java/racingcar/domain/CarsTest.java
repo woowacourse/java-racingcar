4627defe970a745ccde5003ae4c57bb66e490a74
Kyungwon Kim <masonkimseoul@gmail.com>
2024-02-15 14:36:18 +0900

    test: Cars에서 다수의 우승자 선정 테스트 작성

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -57,4 +57,25 @@ class CarsTest {
                 .extracting(Car::getName)
                 .isEqualTo(List.of("메이슨"));
     }
+
+    @Test
+    @DisplayName("다수의 우승자가 발생하는 지 확인한다.")
+    void determineMultipleWinnerTest() {
+        //given
+        Cars cars = new Cars(List.of("미아", "메이슨", "조조", "감자", "도라"));
+        List<Car> createdCars = cars.getCars();
+        createdCars.get(0).move(9);
+        createdCars.get(1).move(8);
+        createdCars.get(2).move(7);
+        createdCars.get(3).move(3);
+        createdCars.get(4).move(2);
+
+        //when
+        List<Car> winners = cars.determineWinner();
+
+        //then
+        assertThat(winners)
+                .extracting(Car::getName)
+                .isEqualTo(List.of("미아", "메이슨", "조조"));
+    }
 }
