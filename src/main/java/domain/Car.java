7ffbe402f1c1c3927b2eca20cffbe63adb4cdf38
SungKyum Kim <sungkyum1@naver.com>
2024-02-15 11:22:40 +0900

    feat: 자동차 도메인 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,2 +1,20 @@
-package domain;public class Car {
+package domain;
+
+import utils.RandomNumberGenerator;
+
+public class Car {
+    private CarName carName;
+    private int position;
+
+    public Car(CarName carName) {
+        this.carName = carName;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomNumber = RandomNumberGenerator.generate();
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
 }
