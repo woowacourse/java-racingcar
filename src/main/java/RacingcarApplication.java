d593791790532a281f5e9877bac443f89a7d8109
tsulocalize <tsulocalize@gmail.com>
2024-02-15 13:05:29 +0900

    feat: 자동차 게임 실행 기능 구현

---
+++ src/main/java/RacingcarApplication.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingcarApplication {
+    public static void main(String[] args) {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        List<Car> cars = new ArrayList<>();
+
+        List<String> carNames = inputView.getCarName();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        int tryCount = inputView.getTryCount();
+        for(int i = 0 ;i<tryCount;i++) {
+            for (Car car : cars) {
+                car.moveCar();
+            }
+            outputView.printTryResult(cars);
+        }
+        outputView.printWinners(cars);
+    }
+}
