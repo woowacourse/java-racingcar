4ce49080546b1ce5b1a7a4700877b3b58c7aada1
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-15 15:36:25 +0900

    REFACTOR : InputView의 입력 스트림을 외부에서 주입 받도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
+import domain.RaceManager;
+import view.InputView;
+
 public class Application {
+    private static final InputView inputView = new InputView(System.in);
+
     public static void main(String[] args) {
-        RaceManager raceManager = new RaceManager();
+        RaceManager raceManager = new RaceManager(inputView);
         raceManager.run();
     }
 }
