bf3a5579de5ee1b15e51c114d983e607a8d977bd
Minjoo <m0522inju@gmail.com>
2024-02-15 14:10:52 +0900

    refactor: 사용하지 않는 임포트 코드 제거

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,12 +1,10 @@
 package racingcar.domain;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static racingcar.exception.ExceptionMessage.*;
 
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
