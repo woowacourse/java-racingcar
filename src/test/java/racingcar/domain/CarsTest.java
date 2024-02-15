3afe80f01d4d90a2df13dba49067f8390712772c
jongmee <ra123221@gmail.com>
2024-02-15 14:32:59 +0900

    test: Cars 클래스 테스트 작성
    - 모든 자동차들이 조건에 따라 움직이는지, 올바르게 단일 우승자를 선정하는지 테스트함

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,60 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+    @Test
+    @DisplayName("모든 자동차들이 조건에 의해 전진한다.")
+    void moveAllCarsTest() {
+        //given
+        Cars cars = new Cars(List.of("미아", "메이슨"));
+
+        //when
+        cars.moveAllCars(new RandomNumberForward());
+
+        //then
+        List<Car> movedCars = cars.getCars();
+        assertThat(movedCars)
+                .extracting(Car::getCount)
+                .containsExactly(1, 1);
+    }
+
+    @Test
+    @DisplayName("모든 자동차들이 조건에 의해 정지한다.")
+    void dontMoveAllCarsTest() {
+        //given
+        Cars cars = new Cars(List.of("미아", "메이슨"));
+
+        //when
+        cars.moveAllCars(new RandomNumberStop());
+
+        //then
+        List<Car> movedCars = cars.getCars();
+        assertThat(movedCars)
+                .extracting(Car::getCount)
+                .containsExactly(0, 0);
+    }
+
+    @Test
+    @DisplayName("단일의 우승자가 발생하는 지 확인한다.")
+    void determineUniqueWinnerTest() {
+        //given
+        Cars cars = new Cars(List.of("미아", "메이슨"));
+        List<Car> createdCars = cars.getCars();
+        createdCars.get(0).move(1);
+        createdCars.get(1).move(8);
+
+        //when
+        List<Car> winners = cars.determineWinner();
+
+        //then
+        assertThat(winners)
+                .extracting(Car::getName)
+                .isEqualTo(List.of("메이슨"));
+    }
+}
