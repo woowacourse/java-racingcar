4952648ac62ed4046f50604e818fef39b15443f3
yechop <channetattoo@gmail.com>
2024-02-15 14:49:19 +0900

    test(Car): Car 테스트

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    private final Car car = new Car("abc");
+
+    @Test
+    void getName() {
+        assertThat(car.getName()).isEqualTo("abc");
+    }
+
+    @Test
+    void getResult() {
+        assertThat(car.getResult()).isEqualTo("abc : \n");
+    }
+
+    @Test
+    void move() {
+        //Todo
+    }
+
+    @Test
+    void getLocation() {
+        assertThat(car.getLocation()).isEqualTo(0);
+    }
+}
