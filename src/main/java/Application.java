e9f4f91446bf0e9ec0248faae1688ac4f8cf4091
hoyeonyy <highoption@naver.com>
2024-02-15 16:28:04 +0900

    style(Application) : 가독성 향상을 위해 메서드 명 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,6 @@ import controller.RaceController;
 public class Application {
 
     public static void main(String[] args) {
-        new RaceController().process();
+        new RaceController().startRace();
     }
 }
