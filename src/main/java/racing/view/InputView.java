423ccf10645645e73d09e75e047695ad8569566c
mzeong <mjeong@sookmyung.ac.kr>
2024-02-15 13:47:55 +0900

    feat: 입력 안내 문구 추가

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -3,7 +3,19 @@ package racing.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String ENTER_TRYCOUNT = "시도할 횟수는 몇회인가요?";
+    private static final String ENTER_NAMES= "경주할 자동차 이름을 입력하세요(이름을 쉼표(,)를 기준으로 구분).";
     private final Scanner scanner = new Scanner(System.in);
+
+    public String readNames(){
+        System.out.println(ENTER_NAMES);
+        return readLine();
+    }
+
+    public int readTryCount(){
+        System.out.println(ENTER_TRYCOUNT);
+        return readInt();
+    }
 
     private String readLine() {
         String input = scanner.nextLine();
@@ -13,7 +25,7 @@ public class InputView {
         return input;
     }
 
-    public int readInt(){
+    private int readInt(){
         return Integer.parseInt(readLine());
     }
 }
