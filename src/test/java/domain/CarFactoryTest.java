7e2e19365a60f02fde301610a3665281c603cd42
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-15 13:58:19 +0900

    TEST : Carfactory 기능 테스트 추가

---
+++ src/test/java/domain/CarFactoryTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarFactoryTest {
+
+    @Test
+    @DisplayName("자동차 이름이 정상적으로 생성되는가")
+    void cars_generation_test() {
+        String carNames = "mangcho,weve,pobi,neo";
+
+        CarFactory carFactory = new CarFactory(carNames);
+        List<Car> expected = List.of(new Car("mangcho"), new Car("weve"), new Car("pobi"),
+                new Car("neo"));
+
+        assertThat(carFactory.getAllCars()).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    @DisplayName("우승자가 정상적으로 반환되는가")
+    void get_winners_test() {
+        String carNames = "mangcho,weve,pobi,neo";
+        CarFactory carFactory = new CarFactory(carNames);
+
+        List<Car> cars = carFactory.getAllCars();
+        Car winner = cars.get(0);
+        winner.execute(9);
+
+        assertThat(carFactory.getWinners()).containsExactly(winner);
+    }
+}
