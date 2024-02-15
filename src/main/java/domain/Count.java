23a25c7fb0b255cc6a96521534cc7d06cad8384c
dwax1324 <dwax1324@gmail.com>
2024-02-15 15:29:17 +0900

    feat : 시도 횟수 유효성 검증

--- src/main/java/domain/Count.java
+++ src/main/java/domain/Count.java
@@ -4,7 +4,14 @@ public class Count {
     private final int count;
 
     public Count(int count) {
+        validate(count);
         this.count = count;
+    }
+
+    private void validate(int count) {
+        if (!(count >= 1 && count <= 100)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static Count from(int count) {
