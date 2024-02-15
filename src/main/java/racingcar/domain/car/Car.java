f050903052e22a4252598f2a6eafa1d41f9e5769
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-15 09:51:10 +0900

    feat(Car): 자동차 객체에 Position을 추가하고 Comparable를 상속받아 compareTo를 구현한다.

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,13 +1,24 @@
 package racingcar.domain.car;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private final CarName name;
+    private final Position position;
 
     public Car(final String name) {
         this.name = new CarName(name);
+        this.position = new Position(0);
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    public int getPosition() {
+        return position.getValue();
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.position.compareTo(o.position);
     }
 }
