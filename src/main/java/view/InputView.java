7e4d691999c6c0c1ce87cd195966d4ca7d3a80f6
hoyeonyy <highoption@naver.com>
2024-02-15 16:00:41 +0900

    fix(InputView) : Scanner 메서드 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,12 +7,12 @@ public class InputView {
 
     public static String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return scanner.next();
+        return scanner.nextLine();
     }
 
     // 메서드 이름 생각
     public static String inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return scanner.next();
+        return scanner.nextLine();
     }
 }
