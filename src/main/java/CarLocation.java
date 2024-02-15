5ce1f1eb5cbf28d511335700e424a0c163facf5e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-15 11:02:04 +0900

    refactor: 테스트를 위한 CarLocation class에 location 필드 및 해당 값 검증 추가

--- src/main/java/CarLocation.java
+++ src/main/java/CarLocation.java
@@ -1,8 +1,19 @@
 public class CarLocation {
     private int location;
 
+    public CarLocation(int location) {
+        validate(location);
+        this.location = location;
+    }
+
+    private void validate(int location) {
+        if (location < 0) {
+            throw new IllegalArgumentException("[ERROR] 음수 위치");
+        }
+    }
+
     public CarLocation() {
-        location = 0;
+        this(0);
     }
 
     public void increaseLocation() {
