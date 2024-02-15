38f2d7e7267c51a11bb9d9d47de93b6591b76cc2
Minjoo <m0522inju@gmail.com>
2024-02-15 13:45:37 +0900

    refactor: OutputView 출력 메시지 및 delimiter 상수로 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,13 +3,16 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
+    private static final String RESULT_SUBJECT = "실행 결과";
+    private static final String WINNERS_SUFFIX = "%s가 최종 우승했습니다.";
+    private static final String WINNERS_DELIMITER = ", ";
 
     private OutputView() {
     }
 
     public static void printResultSubject() {
         System.out.println();
-        System.out.println("실행 결과");
+        System.out.println(RESULT_SUBJECT);
     }
 
     public static void printResult(final List<String> result) {
@@ -18,7 +21,7 @@ public class OutputView {
     }
 
     public static void printWinners(final List<String> winners) {
-        System.out.printf("%s가 최종 우승했습니다.", String.join(", ", winners));
+        System.out.printf(WINNERS_SUFFIX, String.join(WINNERS_DELIMITER, winners));
     }
 
     public static void printErrorMessage(String message) {
