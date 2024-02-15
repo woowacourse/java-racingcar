9eafdb7e9858cd04c26bfc27db440dc52cb2f0c1
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-15 11:16:29 +0900

    feat: 시도 횟수 입력 예외 검증

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -1,6 +1,11 @@
 package model;
 
+import java.util.regex.Pattern;
+
 public class Round {
+
+    private static final String NATURAL_FORMAT_REGEX = "^[\\d]*$";
+    private static final String ZERO = "0";
 
     private int round;
 
@@ -9,7 +14,24 @@ public class Round {
     }
 
     public static Round from(String round) {
+        validate(round);
         return new Round(Integer.parseInt(round));
     }
 
+    private static void validate(String input) {
+        checkIsNumber(input);
+        checkIsZero(input);
+    }
+
+    private static void checkIsNumber(String input) {
+        if (!Pattern.matches(NATURAL_FORMAT_REGEX, input)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 자연수여야 합니다.");
+        }
+    }
+
+    private static void checkIsZero(String input) {
+        if (input.equals(ZERO)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 자연수여야 합니다.");
+        }
+    }
 }
