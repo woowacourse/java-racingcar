de96ca10d1ff6aad800b0d240b4170fd85bc3816
tsulocalize <tsulocalize@gmail.com>
2024-02-16 17:45:17 +0900

    refactor : RandomGenerator 인스턴스화 불가능하도록 수정

--- src/main/java/racingcar/domain/RandomGenerator.java
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -5,6 +5,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomGenerator {
     private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
+    private RandomGenerator() {
+    }
+
     public static int getRandomNumberUnder(int upperBound) {
         return random.nextInt(upperBound);
     }
