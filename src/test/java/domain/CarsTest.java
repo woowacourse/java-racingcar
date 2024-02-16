71ae4de5324ddecbf48c34109c9b9c9abe1d941e
hangillee <skfcb10@naver.com>
2024-02-16 17:40:43 +0900

    test(domain): 리팩토링에 따른 프로덕션 코드 수정으로 인한 테스트 변경

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,8 +1,6 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class RacingCarsTest {
+class CarsTest {
     private static List<Car> cars;
 
     @BeforeEach
     void createCars() {
         cars = new ArrayList<>();
-        
+
         Car pobiCar = new Car("pobi");
         pobiCar.moveForward(4);
 
@@ -31,32 +29,16 @@ class RacingCarsTest {
     }
 
     @Test
-    @DisplayName("시도 횟수는 숫자여야 한다.")
-    void isRoundCountsNumber() {
-        RacingCars racingCars = new RacingCars(cars);
-        String counts = "5번";
-        assertThrows(IllegalArgumentException.class, () -> racingCars.startRounds(counts));
-    }
-
-    @Test
-    @DisplayName("각 라운드 별 결과를 반환해야 한다.")
-    void isValidRoundResult() {
-        RacingCars racingCars = new RacingCars(cars);
-        String roundResult = racingCars.startRounds("1");
-        assertThat(roundResult).contains("pobi : -");
+    @DisplayName("가장 멀리간 자동차의 거리를 반환해야 한다.")
+    void getMostFarDistance() {
+        Cars cars = new Cars(CarsTest.cars);
+        assertEquals(1, cars.getMaxDistance());
     }
 
     @Test
     @DisplayName("최종 자동차 경주 우승자 정보를 반환해야 한다.")
     void isValidWinnerResult() {
-        RacingCars racingCars = new RacingCars(cars);
-        assertEquals("pobi, gugu", racingCars.getWinners());
-    }
-
-    @Test
-    @DisplayName("자동차 이름은 중복될 수 없다.")
-    void isDuplicatedCarName() {
-        List<Car> cars = new ArrayList<>(List.of(new Car("pobi"), new Car("pobi")));
-        assertThrows(IllegalArgumentException.class, () -> new RacingCars(cars));
+        Cars cars = new Cars(CarsTest.cars);
+        assertEquals(List.of(new Car("pobi"), new Car("gugu")), cars.getWinners(1));
     }
 }
