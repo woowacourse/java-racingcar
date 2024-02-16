057e98d77dec36195dab092a3e738d05836bd28d
SCY <helenason@naver.com>
2024-02-16 17:48:12 +0900

    test: 이동 횟수 에러 메시지 테스트 코드 추가

--- src/test/java/domain/MoveCountTest.java
+++ src/test/java/domain/MoveCountTest.java
@@ -12,21 +12,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 @DisplayName("자동차 이동 횟수 도메인 테스트")
 class MoveCountTest {
 
-    @DisplayName("자동차 이동 횟수가 음수이거나 0일 경우 도메인이 생성되지 않는다.")
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1, -2, -333})
-    void testCreateMoveCountWithInvalidValue(int moveCount) {
-        assertThatThrownBy(() -> MoveCount.from(moveCount))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("자동차 이동 횟수가 양의 정수일 경우 도메인이 생성된다.")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 3, 5})
-    void testCreateMoveCountWithValidValue(int moveCount) {
-        assertThatCode(() -> MoveCount.from(moveCount)).doesNotThrowAnyException();
-    }
-
     @DisplayName("움직일 수 있는 횟수를 차감할 수 있다")
     @Test
     void testConsumeCount() {
@@ -48,5 +33,22 @@ class MoveCountTest {
     void testDetectCountNonZero() {
         MoveCount moveCount = MoveCount.from(1);
         assertThat(moveCount.isCountZero()).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -99999})
+    @DisplayName("자동차 이동 횟수가 0 또는 음수인 경우 에러가 발생한다.")
+    void createByInvalidValue(int value) {
+        assertThatThrownBy(() -> MoveCount.from(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(MoveCount.ERROR_OF_MOVE_COUNT_RANGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 99999})
+    @DisplayName("자동차 이동 횟수가 양의 정수인 경우 에러가 발생하지 않는다.")
+    void createByValidValue(int value) {
+        assertThatCode(() -> MoveCount.from(value))
+                .doesNotThrowAnyException();
     }
 }
