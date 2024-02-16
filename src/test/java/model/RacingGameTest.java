cc60db00515c18937fe7b826107ee751821e1eec
Nam Gi Beom <rlqja0523@naver.com>
2024-02-17 00:15:51 +0900

    refactor: 자동차 리스트 fixture 반영, final 키워드 추가

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import static fixture.CarFixture.자동차;
+import static fixture.CarFixture.자동차들;
 import static fixture.RacingGameFixture.레이싱게임;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,39 +18,37 @@ class RacingGameTest {
     @Test
     void 중복된_자동차가_존재하지_않으면_예외가_발생하지_않는다() {
         // given
-        Car carPobi = 자동차("pobi");
-        Car carCrong = 자동차("crong");
-        Car carHonux = 자동차("honux");
-        List<Car> cars = List.of(carPobi, carCrong, carHonux);
+        final String uniqueCarName1 = "pobi";
+        final String uniqueCarName2 = "crong";
+        final String uniqueCarName3 = "honux";
 
         // when & then
-        assertDoesNotThrow(() -> new RacingGame(cars));
+        assertDoesNotThrow(() -> new RacingGame(자동차들(uniqueCarName1, uniqueCarName2, uniqueCarName3)));
     }
 
     @Test
     void 중복된_자동차가_존재하면_예외가_발생한다() {
         // given
-        Car carPobi1 = 자동차("pobi");
-        Car carPobi2 = 자동차("pobi");
-        Car carHonux = 자동차("honux");
-        List<Car> cars = List.of(carPobi1, carPobi2, carHonux);
+        final String duplicateCarName1 = "pobi";
+        final String duplicateCarName2 = "pobi";
+        final String uniqueCarName = "crong";
 
         // when & then
-        assertThatThrownBy(() -> new RacingGame(cars))
+        assertThatThrownBy(() -> new RacingGame(자동차들(duplicateCarName1, duplicateCarName2, uniqueCarName)))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 단독_우승자를_찾는다() {
         // given
-        Car winner = 자동차("prin");
-        Car loser1 = 자동차("mark");
-        Car loser2 = 자동차("pobi");
-        RacingGame racingGame = 레이싱게임(winner, loser1, loser2);
+        final Car winner = 자동차("prin");
+        final Car loser1 = 자동차("mark");
+        final Car loser2 = 자동차("pobi");
+        final RacingGame racingGame = 레이싱게임(winner, loser1, loser2);
         winner.moveForward(movableNumberGenerator);
 
         // when
-        List<String> winners = racingGame.findWinners();
+        final List<String> winners = racingGame.findWinners();
 
         // then
         assertThat(winners).hasSize(1)
@@ -59,15 +58,15 @@ class RacingGameTest {
     @Test
     void 공동_우승자를_찾는다() {
         // given
-        Car loser = 자동차("prin");
-        Car winner1 = 자동차("mark");
-        Car winner2 = 자동차("pobi");
-        RacingGame racingGame = 레이싱게임(loser, winner1, winner2);
+        final Car loser = 자동차("prin");
+        final Car winner1 = 자동차("mark");
+        final Car winner2 = 자동차("pobi");
+        final RacingGame racingGame = 레이싱게임(loser, winner1, winner2);
         winner1.moveForward(movableNumberGenerator);
         winner2.moveForward(movableNumberGenerator);
 
         // when
-        List<String> winners = racingGame.findWinners();
+        final List<String> winners = racingGame.findWinners();
 
         // then
         assertThat(winners).hasSize(2)
