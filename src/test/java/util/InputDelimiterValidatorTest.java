f3a797291b4d7ffe8b87aafa81ecd378d015193e
Nam Gi Beom <rlqja0523@naver.com>
2024-02-17 00:56:06 +0900

    refactor: default delimiter 지정

--- src/test/java/util/InputDelimiterValidatorTest.java
+++ src/test/java/util/InputDelimiterValidatorTest.java
@@ -3,11 +3,14 @@ package util;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import enums.Delimiter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputDelimiterValidatorTest {
+    private static final Delimiter DEFAULT_DELIMITER = Delimiter.COMMA;
+
     private final InputValidator inputDelimiterValidator = new InputDelimiterValidator();
 
     @ParameterizedTest
@@ -15,13 +18,14 @@ class InputDelimiterValidatorTest {
     void 지정한_구분자로_구분되어_있지_않을_경우_예외가_발생한다(String input) {
         // when & then
         assertThatThrownBy(() -> inputDelimiterValidator.validate(input))
-                .isExactlyInstanceOf(IllegalArgumentException.class);
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DEFAULT_DELIMITER.getErrorMessage());
     }
 
     @Test
     void 지정한_구분자로_구분되어_있을경우_예외가_발생하지_않는다() {
         // given
-        String input = "prin,mark";
+        String input = "prin" + DEFAULT_DELIMITER.getValue() + "mark";
 
         // when & then
         assertDoesNotThrow(() -> inputDelimiterValidator.validate(input));
@@ -30,7 +34,7 @@ class InputDelimiterValidatorTest {
     @Test
     void 지정한_구분자로_구분되어_있지만_마지막이_구분자로_끝날_경우_예외가_발생한다() {
         // given
-        String input = "prin,mark,";
+        String input = "prin" + DEFAULT_DELIMITER.getValue() + "mark" + DEFAULT_DELIMITER.getValue();
 
         // when & then
         assertThatThrownBy(() -> inputDelimiterValidator.validate(input))
