5347f79f0aca93b8f3fbddd79f16c07e571da8c8
yoonjuho <dbswn990@naver.com>
2024-02-16 17:39:21 +0900

    test(CarTest): 불필요한 테스트 삭제

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -12,34 +12,26 @@ class CarTest {
 
   @ParameterizedTest
   @NullAndEmptySource
-  @DisplayName("이름은 존재해야 한다.")
+  @DisplayName("이름이 없을 시 예외가 발생한다.")
   void validateNullAndEmpty(String input) {
     assertThatThrownBy(() -> new Car(input)).isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
-  @DisplayName("이름은 5자 이하만 가능하다.")
+  @DisplayName("이름이 6자 이상일 시 예외가 발생한다.")
   void validateNameLength() {
     String name = "abcdefg";
     assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
-  @DisplayName("경주 현황을 기록할 수 있다.")
-  void getStatues() {
-    Car car = create();
-    assertThat(car.getStatuses())
-        .containsExactly(MoveStatus.FORWARD, MoveStatus.STOP, MoveStatus.FORWARD);
-  }
-
-  @Test
   @DisplayName("전진한 횟수를 알 수 있다.")
   void getForwardCount() {
-    Car car = create();
+    Car car = movedCar();
     assertThat(car.getForwardCount()).isEqualTo(2);
   }
 
-  private Car create() {
+  private Car movedCar() {
     Car car = new Car("pobi");
     car.forward();
     car.stop();
