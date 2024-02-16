7eff299ab790633326392a60198d1f39ee48cc19
seokmyungham <hellotjraud@gmail.com>
2024-02-17 03:04:13 +0900

    test(InputStringParserTest): 테스트 추가

---
+++ src/test/java/utils/InputStringParserTest.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputStringParserTest {
+
+    @Test
+    @DisplayName("입력 문자열 split메서드 파싱 테스트")
+    void parseInputToListTest() {
+        List<String> names = InputStringParser.parseInputToList("재즈,호기,재즈다,호기다");
+
+        Assertions.assertThat(names).isEqualTo(List.of("재즈", "호기", "재즈다", "호기다"));
+    }
+
+}
