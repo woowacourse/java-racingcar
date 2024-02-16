7e01295e53691621f6266bde4d50212ac858a6e5
unifolio0 <boho82ha@gmail.com>
2024-02-16 19:52:56 +0900

    test : 정지에 관한 테스트 추가

--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -6,12 +6,20 @@ import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
 
-    @DisplayName("전진 여부를 정확히 구하는 지 테스트.")
+    @DisplayName("랜덤값이 4이상일 시 전진하는 지 테스트")
     @Test
-    public void test(){
+    public void moveTest(){
         RacingCar racingCar = new RacingCar("bito");
         racingCar.race(4);
         Assertions.assertThat(racingCar.getMoveNumber()).isEqualTo(1);
+    }
+
+    @DisplayName("랜덤값이 4미만일 시 멈추는 지 테스트")
+    @Test
+    public void notMoveTest(){
+        RacingCar racingCar = new RacingCar("bito");
+        racingCar.race(3);
+        Assertions.assertThat(racingCar.getMoveNumber()).isEqualTo(0);
     }
 
 }
