b9d339cf31856d024372423c5df62e8e3279a64d
tsulocalize <tsulocalize@gmail.com>
2024-02-16 16:36:44 +0900

    refactor : 특정 Condition에 따른 자동차 이동을 확인하도록 test 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,25 +1,15 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.condition.FalseCondition;
 import racingcar.condition.TrueCondition;
 
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
-
 class CarTest {
-    @BeforeAll
-    static void init() {
-        mockStatic(RandomGenerator.class);
-    }
-
     @Test
-    @DisplayName("랜덤_숫자가_4_이상인_경우_전진")
+    @DisplayName("조건이_참일_경우_전진")
     void moveCarTest() {
-        when(RandomGenerator.getRandomNumberUnderTen()).thenReturn(4);
         Car car = new Car("apple");
 
         int beforeDistance = car.getDistance();
@@ -30,9 +20,8 @@ class CarTest {
     }
 
     @Test
-    @DisplayName("랜덤_숫자가_3_이하인_경우_정지")
+    @DisplayName("조건이_거짓일_경우_정지")
     void stopCarTest() {
-        when(RandomGenerator.getRandomNumberUnderTen()).thenReturn(3);
         Car car = new Car("apple");
 
         int beforeDistance = car.getDistance();
