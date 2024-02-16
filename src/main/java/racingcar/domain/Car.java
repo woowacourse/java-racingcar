2a003f89e911d8121b4f89ad4c150837d3cd7383
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-16 20:31:37 +0900

    refactor: 유지보수를 위한 움직임 증가량 상수화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 public class Car {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final int DEFAULT_FORWARD = 0;
+    private static final int MOVE_STEP = 1;
     private final String name;
     private int forward;
 
@@ -24,7 +25,7 @@ public class Car {
 
     public void move(final CarMoveStrategy carMoveStrategy, final int value) {
         if (carMoveStrategy.isMovable(value)) {
-            forward++;
+            forward += MOVE_STEP;
         }
     }
 
