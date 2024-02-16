a9fa86edea347544f0d0bc1436d481b8dc57136a
seokmyungham <hellotjraud@gmail.com>
2024-02-17 01:35:09 +0900

    style(TryCountValidator): 상수명을 대문자로 수정

--- src/main/java/utils/TryCountValidator.java
+++ src/main/java/utils/TryCountValidator.java
@@ -4,10 +4,10 @@ import java.util.regex.Pattern;
 
 public class TryCountValidator {
     private static final String TRY_COUNT_RANGE_ERROR_MESSAGE = "시도 횟수는 양의 정수여야만 합니다.";
-    private static final Pattern format = Pattern.compile("[1-9]\\d*");
+    private static final Pattern FORMAT = Pattern.compile("[1-9]\\d*");
 
     public static void validateTryCountFormat(String invalidTryCount) {
-        if (!format.matcher(invalidTryCount).matches()) {
+        if (!FORMAT.matcher(invalidTryCount).matches()) {
             throw new IllegalArgumentException(TRY_COUNT_RANGE_ERROR_MESSAGE);
         }
     }
