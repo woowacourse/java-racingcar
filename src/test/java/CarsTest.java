7f6914065c93d13b25419bcc2a20c8d46c4344db
이상진 <pricelees@gmail.com>
2024-02-16 16:36:23 +0900

    test(CarsTest) : Cars의 유효성 검증 추가에 따른 테스트 추가

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,10 +1,14 @@
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
 
+    @DisplayName("모든 차량의 정상 이동 확인")
     @Test
     void moveAll() {
 
@@ -27,6 +31,24 @@ class CarsTest {
         assertThat(actual).isEqualTo(expectedLocations);
     }
 
+    @DisplayName("입력된 숫자 리스트의 갯수와 차량 갯수가 다를때 예외 처리 확인")
+    @Test
+    void moveAll_WithInvalidCount() {
+        List<Car> carList = List.of(
+            Car.from("a", 0),
+            Car.from("b", 0),
+            Car.from("c", 0),
+            Car.from("d", 0)
+        );
+        Cars cars = new Cars(carList);
+
+        List<Integer> randomNumbers = List.of(0, 3, 4, 5, 9);
+        assertThatThrownBy(() -> cars.moveAll(randomNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("가장 많이 이동한 차량의 위치 확인")
     @Test
     void getMaxLocation() {
 
