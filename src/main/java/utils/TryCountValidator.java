8a3d8eabaee85f6b5e44c7f19be35856881bdc5c
seokmyungham <hellotjraud@gmail.com>
2024-02-17 00:58:47 +0900

    refactor(TryCountValidator): 코드 리팩토링
    정규식 문자열에서 Patten 상수를 사용하도록 코드 수정
    에러 메시지 상수 이름을 TRY_COUNT_RANGE_ERROR_MESSAGE로 수정

--- src/main/java/utils/TryCountValidator.java
+++ src/main/java/utils/TryCountValidator.java
@@ -1,12 +1,14 @@
 package utils;
 
+import java.util.regex.Pattern;
+
 public class TryCountValidator {
-    private static final String INVALID_FORMAT_ERROR_MESSAGE = "시도 횟수는 양의 정수여야만 합니다.";
-    private static final String format = "[1-9]\\d*";
+    private static final String TRY_COUNT_RANGE_ERROR_MESSAGE = "시도 횟수는 양의 정수여야만 합니다.";
+    private static final Pattern format = Pattern.compile("[1-9]\\d*");
 
     public static void validateTryCountFormat(String invalidTryCount) {
-        if (!invalidTryCount.matches(format)) {
-            throw new IllegalArgumentException(INVALID_FORMAT_ERROR_MESSAGE);
+        if (!format.matcher(invalidTryCount).matches()) {
+            throw new IllegalArgumentException(TRY_COUNT_RANGE_ERROR_MESSAGE);
         }
     }
 }
