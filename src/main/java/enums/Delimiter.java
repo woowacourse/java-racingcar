ef11e335a8af845dba851d88d299e69f422de635
Nam Gi Beom <rlqja0523@naver.com>
2024-02-17 00:42:10 +0900

    refactor: Delimiter에 에러메시지 추가

--- src/main/java/enums/Delimiter.java
+++ src/main/java/enums/Delimiter.java
@@ -6,11 +6,13 @@ public enum Delimiter {
     private final String value;
     private final String regex;
     private final String korName;
+    private final String errorMessage;
 
     Delimiter(String value, String regex, String korName) {
         this.value = value;
         this.regex = regex;
         this.korName = korName;
+        this.errorMessage = String.format("%s(%s)를 기준으로 구분해야 합니다.", this.korName, this.value);
     }
 
     public String getValue() {
@@ -23,5 +25,9 @@ public enum Delimiter {
 
     public String getKorName() {
         return this.korName;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
     }
 }
--- src/main/java/util/CarNamesValidator.java
+++ src/main/java/util/CarNamesValidator.java
@@ -1,17 +1,16 @@
 package util;
 
-import static enums.Delimiter.COMMA;
-
+import enums.Delimiter;
 import java.util.regex.Pattern;
 
 public class CarNamesValidator extends InputValidator {
-    private static final Pattern CAR_NAMES_PATTERN = Pattern.compile(COMMA.getRegex());
+    private static final Delimiter DEFAULT_DELIMITER = Delimiter.COMMA;
+    private static final Pattern CAR_NAMES_PATTERN = Pattern.compile(DEFAULT_DELIMITER.getRegex());
 
     @Override
     void validateCustom(String input) {
         if (!CAR_NAMES_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException(
-                    String.format("자동차 이름은 %s(%s)를 기준으로 구분해야 합니다.", COMMA.getKorName(), COMMA.getValue()));
+            throw new IllegalArgumentException(DEFAULT_DELIMITER.getErrorMessage());
         }
     }
 }
