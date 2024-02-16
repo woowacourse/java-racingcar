91a418ef903061f6a481d4b6e450345a3960972d
devhoya97 <diajbh123@gmail.com>
2024-02-16 18:31:27 +0900

    refactor: Util 이름을 사용하지 않도록 클래스명 변경

--- src/main/java/racingcar/domain/CarAccelerator.java
+++ src/main/java/racingcar/domain/CarAccelerator.java
@@ -1,13 +1,11 @@
 package racingcar.domain;
 
-import racingcar.util.RandomNumberUtils;
-
 public class CarAccelerator implements Accelerator{
     public static final int MIN_ACCEL_POWER = 0;
     public static final int MAX_ACCEL_POWER = 9;
 
     @Override
     public int push() {
-        return RandomNumberUtils.generate(MIN_ACCEL_POWER, MAX_ACCEL_POWER);
+        return RandomNumberGenerator.generate(MIN_ACCEL_POWER, MAX_ACCEL_POWER);
     }
 }
--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -1,9 +1,9 @@
-package racingcar.util;
+package racingcar.domain;
 
 import java.util.Random;
 
 
-public class RandomNumberUtils {
+public class RandomNumberGenerator {
     private static final Random random = new Random();
 
     public static int generate(int min, int max) {
