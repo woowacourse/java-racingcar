df611b8236906d808b5e36a3ee626ea702f9707d
ikjo39 <wer04129@gmail.com>
2024-02-16 21:41:36 +0900

    style: 불필요한 줄바꿈 제거 및 EOL 추가

--- src/main/java/racingcar/model/Position.java
+++ src/main/java/racingcar/model/Position.java
@@ -15,8 +15,12 @@ public class Position {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
         Position position = (Position) obj;
         return this.position == position.position;
     }
