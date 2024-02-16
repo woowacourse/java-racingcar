906c65b20df56c8bba608fe19c69496c3b0e272b
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-16 16:18:13 +0900

    test: CarTest 경계값만 테스트 하는 것으로 변경

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,37 +1,28 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-    @DisplayName("0부터 9까지의 숫자를 넣고 차량의 전진 여부를 확인")
-    @ParameterizedTest(name = "{0}을 입력 : {1}칸 이동")
-    @MethodSource("randomNumberProvider")
-    void move(int randomNumber, int expectedLocation) {
+    @DisplayName("4 이상의 숫자가 주어졌을 경우 전진하는지 확인")
+    @Test
+    void checkMove() {
         Car car = Car.from("a");
 
-        car.move(randomNumber);
+        car.move(4);
 
-        assertThat(car.getCarLocation()).isEqualTo(expectedLocation);
+        assertThat(car.getCarLocation()).isEqualTo(1);
     }
 
-    static Stream<Arguments> randomNumberProvider() {
-        return Stream.of(
-                arguments(0, 0),
-                arguments(1, 0),
-                arguments(2, 0),
-                arguments(3, 0),
-                arguments(4, 1),
-                arguments(5, 1),
-                arguments(6, 1),
-                arguments(7, 1),
-                arguments(8, 1),
-                arguments(9, 1)
-        );
+    @DisplayName("4 이상의 숫자가 주어졌을 경우 전진하는지 확인")
+    @Test
+    void checkStop() {
+        Car car = Car.from("a");
+
+        car.move(3);
+
+        assertThat(car.getCarLocation()).isEqualTo(0);
     }
+
 }
