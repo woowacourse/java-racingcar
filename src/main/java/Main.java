956a2e0ee6d61ac8d620f9a9974f24866b03f889
takoyakimchi <c4nyou@gmail.com>
2024-02-16 17:56:49 +0900

    refactor: controller 삭제하고 해당 부분 main으로 이동

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,37 @@
-import controller.Controller;
+import conversion.Converter;
+import domain.Car;
+import java.util.List;
+import service.Service;
+import view.InputView;
+import view.IterativeReader;
+import view.OutputView;
 
 public class Main {
 
     public static void main(String[] args) {
-        Controller.start();
+        List<Car> cars = IterativeReader.readUntilNoError(Main::findCars);
+        int round = IterativeReader.readUntilNoError(Main::findRound);
+
+        OutputView.printRoundResult();
+
+        for (int i = 0; i < round; i++) {
+            Service.playOneRound(cars);
+            OutputView.printScore(cars);
+        }
+
+        List<String> winnerNames = Service.getWinnerNames(cars);
+        OutputView.printWinners(winnerNames);
+    }
+
+    private static List<Car> findCars() {
+        OutputView.printCarNames();
+        String rawCarNames = InputView.read();
+        return Converter.toCars(rawCarNames);
+    }
+
+    private static int findRound() {
+        OutputView.printRound();
+        String rawRound = InputView.read();
+        return Converter.toRound(rawRound);
     }
 }
