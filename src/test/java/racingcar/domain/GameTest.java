323b51a48eeec1139920ac92aba426f5d0be78cd
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-16 20:12:41 +0900

    test: Null 테스트 검증

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("게임 생성에서")
@@ -23,6 +24,7 @@ class GameTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"0", "-1", "1.5", "", " ", "1 2"})
+    @NullSource
     @DisplayName("시도할 횟수가 자연수가 아닐 경우 예외가 발생한다")
     public void nonNaturalNumberTest(String userTryCount) {
         //given
