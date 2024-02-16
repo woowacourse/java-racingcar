adfddb7cd208b7bf204eff463bcff4bce7ac1412
Gyeongho Yang <chchch1213@naver.com>
2024-02-16 16:17:39 +0900

    refactor: 불필요한 랜덤 숫자 주석 제거

--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -5,22 +5,15 @@ import java.util.Random;
 public class RandomNumber implements RandomUtil {
     private static final Random random = new Random();
 
-    /**
-     * minLimit와 maxLimit을 포함하여 랜담값을 뽑는 함수
-     *
-     * @param minLimit
-     * @param maxLimit
-     * @return int
-     */
-    @Override
-    public int generate(int minLimit, int maxLimit) {
-        validate(minLimit, maxLimit);
-        return random.nextInt(maxLimit - minLimit) + minLimit;
-    }
-
     private static void validate(int minLimit, int maxLimit) {
         if (minLimit > maxLimit) {
             throw new IllegalStateException();
         }
+    }
+
+    @Override
+    public int generate(int minLimit, int maxLimit) {
+        validate(minLimit, maxLimit);
+        return random.nextInt(maxLimit - minLimit) + minLimit;
     }
 }
