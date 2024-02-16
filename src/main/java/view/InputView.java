7be8f91d7e5d184adabaae46077f9136d011c66c
seokmyungham <hellotjraud@gmail.com>
2024-02-17 00:54:06 +0900

    style(InputView): 상수 명 수정
    INPUT_CARS_NAME_MESSAGE -> MESSAGE_PROMPT_CAR_NAMES
    INPUT_TRY_COUNT_MESSAGE -> MESSAGE_PROMPT_TRY_COUNT

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,18 +3,18 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String INPUT_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String MESSAGE_PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String MESSAGE_PROMPT_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputCarName() {
-        System.out.println(INPUT_CARS_NAME_MESSAGE);
+        System.out.println(MESSAGE_PROMPT_CAR_NAMES);
 
         return scanner.nextLine();
     }
 
     public static String inputTryCount() {
-        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+        System.out.println(MESSAGE_PROMPT_TRY_COUNT);
 
         return scanner.nextLine();
     }
