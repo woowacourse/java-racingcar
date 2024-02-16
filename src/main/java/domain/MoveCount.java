5518afc3946b1e1aef14cecc4783bf06ead8b2ac
libienz <leekh10195@gmail.com>
2024-02-16 16:36:38 +0900

    refactor: 유저가 이동횟수를 잘못 입력했을 경우 에러 메시지 첨부

--- src/main/java/domain/MoveCount.java
+++ src/main/java/domain/MoveCount.java
@@ -27,7 +27,7 @@ public class MoveCount {
 
     private void validateRange(int moveCount) {
         if (moveCount <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 이동 횟수는 1이상의 정수로 입력해주세요");
         }
     }
 }
