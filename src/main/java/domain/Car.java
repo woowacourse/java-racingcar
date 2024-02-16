21b6dca1de004a9ef6e4192d357d941fa7061e57
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-16 15:16:29 +0900

    style (Car) : 의미를 부여하기 위해 MAX_NAME_LENGTH 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,6 +2,8 @@ package domain;
 
 public class Car {
 
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position;
 
@@ -15,7 +17,7 @@ public class Car {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차의 이름은 빈칸이 아니어야 합니다. : " + name);
         }
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하이어야 합니다. : " + name);
         }
     }
