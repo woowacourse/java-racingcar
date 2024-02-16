be799afa8ef99b1979715ad39ab906fe8389790a
seunghye218 <seunghye@student.42seoul.kr>
2024-02-16 19:11:32 +0900

    refactor: 정적 상수 변수명 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,7 +3,7 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String CAR_NAME_DELIMITER = ",";
 
     private InputView() {
@@ -11,13 +11,13 @@ public class InputView {
 
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분).");
-        String carNames = scanner.nextLine();
+        String carNames = SCANNER.nextLine();
         return carNames.split(CAR_NAME_DELIMITER);
     }
 
     public static int inputTryCount() {
         System.out.println("시도할 회수는 몇 회인가요?");
-        String tryCount = scanner.nextLine();
+        String tryCount = SCANNER.nextLine();
         return Integer.parseInt(tryCount);
     }
 }
