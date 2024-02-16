18714de423918c40a4d8188356db3851fa5f55ed
Minjoo <m0522inju@gmail.com>
2024-02-17 01:03:33 +0900

    refactor: 상수 컨벤션에 맞게 수정

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -3,13 +3,13 @@ package racingcar.util;
 import java.util.Random;
 
 public class RandomNumberGenerator {
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     private RandomNumberGenerator() {
         // 인스턴스 생성 방지용
     }
 
     public static int generate(final int max) {
-        return random.nextInt(max + 1); // bound 미만의 숫자를 return하기 때문에 +1
+        return RANDOM.nextInt(max + 1); // bound 미만의 숫자를 return하기 때문에 +1
     }
 }
