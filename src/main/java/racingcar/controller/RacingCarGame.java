d1f8ea6c654fcdb5abc3aaf3e63ceb516ab47d7c
Gyeongho Yang <chchch1213@naver.com>
2024-02-16 16:38:28 +0900

    refactor: 혼동이 생길 수 있는 불필요한 함수 분리 제거

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,25 +1,23 @@
 package racingcar.controller;
 
-import java.util.function.Supplier;
 import racingcar.domain.Game;
 import racingcar.domain.Vehicles;
 import racingcar.ui.InputView;
 import racingcar.ui.OutputView;
 
+import java.util.function.Supplier;
+
 public class RacingCarGame {
+
     public void start() {
-        Game game = getGame();
-        printResultLine();
+        Game game = createGame();
+        OutputView.printNewLine();
+        OutputView.printResultTitle();
         game.proceed();
         OutputView.printWinner(game.getWinner());
     }
 
-    private static void printResultLine() {
-        OutputView.printNewLine();
-        OutputView.printResultTitle();
-    }
-
-    private Game getGame() {
+    private Game createGame() {
         Vehicles vehicles = requestUntilValidated(() -> Vehicles.from(InputView.readCarNames()));
         return requestUntilValidated(() -> Game.from(InputView.readTryCount(), vehicles));
     }
