a2af457c73fe682349dbe8a606f1444d0467dbe3
이상진 <pricelees@gmail.com>
2024-02-16 16:27:49 +0900

    style(Car) : 메서드 의미 중복 제거
    < description >
    - carLocation.increaseLocation()은 의미가 겹친다고 생각했고, carLocation.increase()로 해당 부분 수정
    - getter에서도 역시 의미가 겹친다고 생각해서, 이 부분에 대한 고민이 필요할 것 같음

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,40 +1,40 @@
 public class Car {
 
-    private static final int MIN_NUMBER_FOR_MOVE = 4;
-    private final CarName carName;
-    private final CarLocation carLocation;
+	private static final int MIN_NUMBER_FOR_MOVE = 4;
 
+	private final CarName carName;
+	private final CarLocation carLocation;
 
-    private Car(CarName carName, CarLocation carLocation) {
-        this.carName = carName;
-        this.carLocation = carLocation;
-    }
+	private Car(CarName carName, CarLocation carLocation) {
+		this.carName = carName;
+		this.carLocation = carLocation;
+	}
 
-    public static Car from(String name, int location) {
-        return new Car(
-                new CarName(name),
-                new CarLocation(location)
-        );
-    }
+	public static Car from(String name, int location) {
+		return new Car(
+			new CarName(name),
+			new CarLocation(location)
+		);
+	}
 
-    public static Car from(String name) {
-        return new Car(
-                new CarName(name),
-                new CarLocation(0)
-        );
-    }
+	public static Car from(String name) {
+		return new Car(
+			new CarName(name),
+			new CarLocation(0)
+		);
+	}
 
-    public void move(int randomNumber) {
-        if (randomNumber >= MIN_NUMBER_FOR_MOVE) {
-            carLocation.increaseLocation();
-        }
-    }
+	public void move(int randomNumber) {
+		if (randomNumber >= MIN_NUMBER_FOR_MOVE) {
+			carLocation.increase();
+		}
+	}
 
-    public String getCarName() {
-        return carName.getName();
-    }
+	public String getCarName() {
+		return carName.getName();
+	}
 
-    public int getCarLocation() {
-        return carLocation.getLocation();
-    }
+	public int getCarLocation() {
+		return carLocation.getLocation();
+	}
 }
