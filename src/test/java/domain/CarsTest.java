2c5e59b89a5abe1d35651a59faa7f628389d30d5
SCY <helenason@naver.com>
2024-02-16 17:56:38 +0900

    style: 변수 생성 코드 함축

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -12,16 +12,12 @@ class CarsTest {
     @DisplayName("참여 중인 자동차 중 전진 거리가 가장 먼 자동차를 뽑는다.")
     @Test
     void testChooseWinnerCarByPosition() {
-
         Car winner = Car.of("car4", 4);
         Cars cars = Cars.from(
                 List.of(Car.of("car1", 1),
                         Car.of("car2", 2),
                         Car.of("car3", 3),
                         winner));
-
-        List<Car> winners = cars.chooseWinners();
-
-        assertThat(winners).containsExactly(winner);
+        assertThat(cars.chooseWinners()).containsExactly(winner);
     }
 }
