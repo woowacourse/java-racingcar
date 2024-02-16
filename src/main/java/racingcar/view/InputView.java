9ba878d274e71805c72d10656493455e2bace18a
gyungchan Jo <namejgc@naver.com>
2024-02-16 18:22:07 +0900

    refactor: 입력 검증 구조 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String BLANK_EXCEPTION_MESSAGE = "입력은 공백일 수 없습니다.";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputCarNames() {
@@ -19,7 +20,13 @@ public class InputView {
         System.out.println(inputMessage);
         String input = scanner.nextLine();
 
-        InputValidator.validate(input);
+        validateInput(input);
         return input;
+    }
+
+    private static void validateInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_EXCEPTION_MESSAGE);
+        }
     }
 }
