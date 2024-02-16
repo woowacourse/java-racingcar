269fa0bfadc1c170b4cf4e767a10b7aa29c084b2
takoyakimchi <c4nyou@gmail.com>
2024-02-16 22:28:14 +0900

    docs: TODO 일부 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             // TODO: 아래 중복되는 부분을 메서드로 통합하기
+            // TODO: 메인 메서드 15줄 이내로 변경
             OutputView.printCarNames();
             String rawCarNames = InputView.read();
             RacingGame racingGame = new RacingGame(rawCarNames); // TODO: 예외 발생 시 재입력받기 구현
@@ -20,6 +21,7 @@ public class Main {
 
             OutputView.printRoundResult();
 
+            // TODO: for문을 없애는 방법을 생각해보기
             for (int i = 0; i < round.getRound(); i++) {
                 racingGame.play();
                 OutputView.printScore(racingGame.getCars());
--- src/main/java/view/IterativeReader.java
+++ src/main/java/view/IterativeReader.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 public class IterativeReader {
 
+    // TODO: ErrorHandler로 대체하는 작업 진행하기
     public static <T> T readUntilNoError(Supplier<T> supplier) {
         while (true) {
             try {
