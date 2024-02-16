606240edf424e27b560edde080c44c37c88ae841
seokmyungham <hellotjraud@gmail.com>
2024-02-17 02:28:25 +0900

    feat(InputStringParser): InputStringParser 클래스 및 메서드 이름 변경

--- src/main/java/utils/InputStringParser.java
+++ src/main/java/utils/InputStringParser.java
@@ -3,11 +3,9 @@ package utils;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Convertor {
-
-    public static List<String> convertStringToList(String input) {
+public class InputStringParser {
+    public static List<String> parseInputToList(String input) {
         String[] names = input.split(",");
-
         return new ArrayList<>(List.of(names));
     }
 }
