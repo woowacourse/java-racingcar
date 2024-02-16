ea855b142703290bd4580ca15083e5d834eeee1e
seokmyungham <hellotjraud@gmail.com>
2024-02-17 04:27:26 +0900

    refactor(Name): 가독성 향상을 위한 상수 변수 추가

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -2,6 +2,8 @@ package model;
 
 public class Name {
     private static final String NAME_LENGTH_RANGE_ERROR_MESSAGE = "이름의 길이는 1~5글자 사이만 가능합니다.";
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
 
     public Name(String name) {
@@ -10,7 +12,7 @@ public class Name {
     }
 
     private void validateNameLength(String name) {
-        if (name.isBlank() || name.length() > 5) {
+        if (name.isBlank() || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(NAME_LENGTH_RANGE_ERROR_MESSAGE);
         }
     }
