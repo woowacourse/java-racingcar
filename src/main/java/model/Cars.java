09fa8ae5ae580056f7d07a5284d63d732aca59d8
eunhee <dmsgml9903@gmail.com>
2024-02-16 17:10:33 +0900

    fix(Cars): max값이 존재하지 않는 경우 예외 처리 추가
    maxMovement가 존재하지 않는 경우 예외를 던지는 코드 작성

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.OptionalInt;
 
 public class Cars {
     private final MovesGenerator movesGenerator;
@@ -17,7 +19,8 @@ public class Cars {
     }
 
     public List<Car> findWinners() {
-        int maxMovement = cars.stream().mapToInt(Car::getMovement).max().getAsInt();
+        int maxMovement = cars.stream().mapToInt(Car::getMovement).max()
+                .orElseThrow(() -> new NoSuchElementException("자동차의 최대 이동거리가 존재하지 않습니다."));
         return cars.stream()
                 .filter(car -> car.getMovement() == maxMovement)
                 .toList();
