978ab06f8057cfacfe2aff39179702b0cb51cc71
seokmyungham <hellotjraud@gmail.com>
2024-02-17 04:20:23 +0900

    refactor(InputStringParser): parseInputToList 반환 타입 수정

--- src/main/java/utils/InputStringParser.java
+++ src/main/java/utils/InputStringParser.java
@@ -1,11 +1,10 @@
 package utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InputStringParser {
     public static List<String> parseInputToList(String input) {
         String[] names = input.split(",");
-        return new ArrayList<>(List.of(names));
+        return List.of(names);
     }
 }
