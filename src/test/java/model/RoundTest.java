323bb89ae563bb43390a010d48619acf4c64b1c9
JiHyeonL <lejinhy@gmail.com>
2024-02-16 15:47:54 +0900

    test: 테스트 DisplayName 자세하게 변경

--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class RoundTest {
 
-    @DisplayName("시도 횟수 입력 값을 검증한다.")
+    @DisplayName("시도 횟수 입력 값이 자연수가 아닌 경우 예외가 발생하는지 검증한다.")
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"+4", "ss", "", "   ", "-1", "3.2", "0"})
     void roundValidationTest(String input) {
-        assertThatThrownBy(() -> Round.from(input)).
-                isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Round.from(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
