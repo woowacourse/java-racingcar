2cf8b8221b33ec195ff24cc42a8ed5cdcc34680c
PgmJun <chltmdwns96@gmail.com>
2024-02-16 19:06:37 +0900

    refactor: 시도 횟수 제한 로직 변경
    - 1 미만 시 예외 발생 -> 1개 미만 5 초과 시 예외 발생

--- src/main/java/domain/TryCount.java
+++ src/main/java/domain/TryCount.java
@@ -3,13 +3,16 @@ package domain;
 public class TryCount {
     private int value;
 
+    private static final int MIN_TRY_COUNT = 1;
+    private static final int MAX_TRY_COUNT = 5;
+
     public TryCount(int value) {
         validate(value);
         this.value = value;
     }
 
     private void validate(int value) {
-        if (value < 1) {
+        if (value < MIN_TRY_COUNT || value > MAX_TRY_COUNT) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/domain/TryCountTest.java
+++ src/test/java/domain/TryCountTest.java
@@ -15,8 +15,8 @@ class TryCountTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-2, -1, 0})
-    @DisplayName("시도 횟수가 1 미만이면 객체 생성에 실패한다")
+    @ValueSource(ints = {0, 6})
+    @DisplayName("시도 횟수가 1 미만, 5 초과이면 객체 생성에 실패한다")
     void createTryCountFail(int value) {
         Assertions.assertThatThrownBy(() -> new TryCount(value))
                 .isInstanceOf(IllegalArgumentException.class);
