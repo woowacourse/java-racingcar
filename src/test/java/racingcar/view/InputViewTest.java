cd32efcaef5df3fe9a241f4e49118471a493e30b
donghoony <aru0504@naver.com>
2024-02-16 16:53:59 +0900

    refactor(Reader): 함수형 인터페이스 어노테이션 추가, 테스트에서 이를 활용하도록 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -3,7 +3,6 @@ package racingcar.view;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,21 +10,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.exception.NotANumberException;
 import racingcar.exception.TryNumberOutOfRangeException;
-import racingcar.view.reader.ConsoleReader;
 
 class InputViewTest {
-
-    InputView setupInputStream(String inputString) {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(inputString.getBytes());
-        System.setIn(byteArrayInputStream);
-        return new InputView(new ConsoleReader(System.in));
-    }
 
     @Test
     @DisplayName("이름을 입력받고, 쉼표를 기준으로 올바르게 나눈다.")
     void nameSplitTest() {
         // given
-        InputView view = setupInputStream("aru,polla,hogi");
+        InputView view = new InputView(() -> "aru,polla,hogi");
         // when
         List<String> names = view.getNames();
         // then
@@ -37,7 +29,7 @@ class InputViewTest {
     @DisplayName("시도한 횟수가 범위를 벗어나면, 예외를 발생한다")
     void tryCountOutOfRangeTest(String input) {
         // given
-        InputView view = setupInputStream(input);
+        InputView view = new InputView(() -> input);
         // when, then
         assertThatThrownBy(view::getTryNumber)
                 .isInstanceOf(TryNumberOutOfRangeException.class);
@@ -48,7 +40,7 @@ class InputViewTest {
     @DisplayName("시도한 범위가 올바른 범위라면, 양의 정수로 변환한다")
     void tryCountConvertTest(String input) {
         // given
-        InputView view = setupInputStream(input);
+        InputView view = new InputView(() -> input);
         // when
         int actual = view.getTryNumber();
         int expected = Integer.parseInt(input);
@@ -61,7 +53,7 @@ class InputViewTest {
     @DisplayName("횟수로 숫자가 아닌 문자열이 입력되면 예외를 발생한다.")
     void invalidTryCountTest(String input) {
         // given
-        InputView view = setupInputStream(input);
+        InputView view = new InputView(() -> input);
         // when, then
         assertThatThrownBy(view::getTryNumber)
                 .isInstanceOf(NotANumberException.class);
