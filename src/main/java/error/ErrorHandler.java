0dba1b5a22c106b454f7fd2f49ad7b4d3a8543f6
takoyakimchi <c4nyou@gmail.com>
2024-02-17 17:16:39 +0900

    refactor(error): 메서드 이름 변경 및 Runnable -> Supplier 변경
    리턴 값 필요에 따른 함수형 인터페이스 변경

--- src/main/java/error/ErrorHandler.java
+++ src/main/java/error/ErrorHandler.java
@@ -7,11 +7,10 @@ public class ErrorHandler {
     public static final String ERROR_PREFIX = "[ERROR] ";
 
     // TODO: 예외 발생 시 재입력받는 부분에 사용
-    public static void handle(Runnable runnable) {
+    public static <T> T executeErrorOccurable(Supplier<T> supplier) {
         while (true) {
             try {
-                runnable.run();
-                break;
+                return supplier.get();
             } catch (IllegalArgumentException e) {
                 System.out.println(ERROR_PREFIX + e.getMessage());
             }
