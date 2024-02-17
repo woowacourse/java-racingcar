1f1954ee38fd91ffabc8b1634bcebea0d0238c4b
HoeSeong123 <ghltjd3@gmail.com>
2024-02-17 23:29:02 +0900

    refactor(Controller) : 불필요한 멤버변수 제거

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,7 +1,6 @@
 package racingcar.controller;
 
 import racingcar.model.RandomNumberGenerator;
-import racingcar.view.validator.InputValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 import racingcar.model.Car;
@@ -13,33 +12,41 @@ import java.util.List;
 public class Controller {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private List<Car> cars;
-    private final Referee referee = new Referee();
 
     public void run() {
+        Referee referee = new Referee();
+
         List<String> carNames = inputView.getCarNames();
         int tryNumber = inputView.getTryNumber();
-        generateCarList(carNames);
+
+        List<Car> cars = generateCarList(carNames);
+
         outputView.printResultHeaderPrompt();
+
         while(tryNumber-- > 0){
-            runRace();
+            moveCars(cars);
         }
+
         outputView.printWinners(referee.generateWinners(cars));
     }
 
-    private void runRace() {
+    private void moveCars(List<Car> cars) {
         for(Car car : cars) {
             car.move();
             outputView.printResult(car.getName(), car.getPosition());
         }
+
         OutputView.parseLine();
     }
 
-    private void generateCarList(List<String> carNames) {
-        cars = new ArrayList<>();
+    private List<Car> generateCarList(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+
         for(String carName : carNames){
             Car car = new Car(carName, new RandomNumberGenerator());
             cars.add(car);
         }
+
+        return cars;
     }
 }
