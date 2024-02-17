8b4d95b189b994e982f17826aa0a5a6bff40ace0
fromitive <fromitive@gmail.com>
2024-02-17 15:48:07 +0900

    refactor(RandomNumericGenerator): 접근제어자 변경

--- src/main/java/racingcar/controller/numericgenerator/RandomNumericGenerator.java
+++ src/main/java/racingcar/controller/numericgenerator/RandomNumericGenerator.java
@@ -5,8 +5,8 @@ import racingcar.controller.NumericGenerator;
 import java.util.Random;
 
 public class RandomNumericGenerator implements NumericGenerator {
+    private final Random random = new Random();
 
-    public Random random = new Random();
     @Override
     public int generate() {
         return random.nextInt(10);
