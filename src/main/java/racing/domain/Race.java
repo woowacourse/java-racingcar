f2e371c44d87448601133588f1aad576a7428b11
mzeong <mjeong@sookmyung.ac.kr>
2024-02-17 23:59:54 +0900

    refactor: randomNumberGenerator 변경에 따라 Race 클래스 수정

--- src/main/java/racing/domain/Race.java
+++ src/main/java/racing/domain/Race.java
@@ -2,31 +2,34 @@ package racing.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import racing.util.NumberGenerator;
+import racing.util.RandomNumberGenerator;
 
 public class Race {
 
+    private static final int NUMBER_GENERATOR_BOUND = 10;
+    private static final int NUMBER_GENERATOR_START = 0;
+
     private final Cars cars;
     private final TryCount tryCount;
-    private final NumberGenerator numberGenerator;
+    private final RandomNumberGenerator randomNumberGenerator;
 
-    public Race(Cars cars, TryCount tryCount, NumberGenerator numberGenerator) {
+    public Race(Cars cars, TryCount tryCount) {
         this.cars = cars;
         this.tryCount = tryCount;
-        this.numberGenerator = numberGenerator;
+        this.randomNumberGenerator = new RandomNumberGenerator(NUMBER_GENERATOR_BOUND, NUMBER_GENERATOR_START);
     }
 
-    public List<String> proceed() {
-        List<String> result = new ArrayList<>();
+    public List<Cars> proceed() {
+        List<Cars> result = new ArrayList<>();
         while (tryCount.isRemain()) {
             tryCount.consume();
-            cars.moveAll(numberGenerator);
-            result.add(cars.toString());
+            cars.moveAll(randomNumberGenerator);
+            result.add(cars);
         }
         return result;
     }
 
-    public List<Car> getWinners() {
+    public List<Car> findWinners() {
         return cars.findFurthestCars();
     }
 }
