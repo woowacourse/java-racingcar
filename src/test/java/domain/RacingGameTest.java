d70eec7448a617e6cd78831dee233f3f4f8f8b70
takoyakimchi <c4nyou@gmail.com>
2024-02-17 19:17:49 +0900

    feat(test): RacingGame 도메인 단위 테스트 추가

--- src/test/java/domain/RacingGameTest.java
+++ src/test/java/domain/RacingGameTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,21 +8,84 @@ import org.junit.jupiter.api.Test;
 class RacingGameTest {
 
     @Test
-    @DisplayName("")
-    void findWinners() {
-        // given
-        Car carA = new Car("carA");
-        Car carB = new Car("carB");
-        Car carC = new Car("carC");
+    @DisplayName("자동차 이름 정상 입력(1)")
+    void carNames_ok() {
+        new RacingGame("a");
+        new RacingGame("a,bb,cCc,dDdD,eeeee");
+        new RacingGame("a  , bb  , cCc ,    dDdD ,eeeee");
+    }
 
-        // when
-        carA.move();
-        carB.move();
-        carB.move();
-        carC.move();
-        carC.move();
+    @Test
+    @DisplayName("자동차 이름 정상 입력(2): 경계값 테스트")
+    void carNames_ok_boundaryTest_hundredCases() {
+        new RacingGame(
+            "a    ,b    ,c    ,d    ,e    ,f    ,g    ,h    ,i    ,j    ,"
+                + "k    ,l    ,m    ,n    ,o    ,p    ,q    ,r    ,s    ,t    ,"
+                + "u    ,v    ,w    ,x    ,y    ,z    ,aa   ,bb   ,cc   ,dd   ,"
+                + "ee   ,ff   ,gg   ,hh   ,ii   ,jj   ,kk   ,ll   ,mm   ,nn   ,"
+                + "oo   ,pp   ,qq   ,rr   ,ss   ,tt   ,uu   ,vv   ,ww   ,xx   ,"
+                + "yy   ,zz   ,aaa  ,bbb  ,ccc  ,ddd  ,eee  ,fff  ,ggg  ,hhh  ,"
+                + "iii  ,jjj  ,kkk  ,lll  ,mmm  ,nnn  ,ooo  ,ppp  ,qqq  ,rrr  ,"
+                + "sss  ,ttt  ,uuu  ,vvv  ,www  ,xxx  ,yyy  ,zzz  ,aaaa ,bbbb ,"
+                + "cccc ,dddd ,eeee ,ffff ,gggg ,hhhh ,iiii ,jjjj ,kkkk ,llll ,"
+                + "mmmm ,nnnn ,oooo ,pppp ,qqqq ,rrrr ,ssss ,tttt ,uuuu ,vvvv"
+        );
+    }
 
-        // then
+    @Test
+    @DisplayName("자동차 이름 예외 입력(1): 자동차 이름에 중복이 있어서는 안 된다.")
+    void carNames_exception_noDuplication() {
+        assertThatThrownBy(() -> new RacingGame("aa,aa,bb"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    @DisplayName("자동차 이름 예외 입력(2): 입력이 비어 있으면 안 된다.")
+    void carNames_exception_noEmptyInput() {
+        assertThatThrownBy(() -> new RacingGame(""))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new RacingGame(" "))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 예외 입력(3): 쉼표만 입력하면 안 된다.")
+    void carNames_exception_onlyComma() {
+        assertThatThrownBy(() -> new RacingGame(",,"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 예외 입력(4): 이름은 하나의 쉼표로 구분되어야 한다.")
+    void carNames_exception_delimiterViolation() {
+        assertThatThrownBy(() -> new RacingGame("a,b.c"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingGame("a,b,c,,d"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingGame("a,,b,c,d"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingGame(",,,a,b"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingGame("a,b,,,"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 예외 입력(5): 1~100개의 이름만 입력 가능하다.")
+    void carNames_exception_rangeViolation() {
+        assertThatThrownBy(() -> new RacingGame(
+            "a    ,b    ,c    ,d    ,e    ,f    ,g    ,h    ,i    ,j    ,"
+                + "k    ,l    ,m    ,n    ,o    ,p    ,q    ,r    ,s    ,t    ,"
+                + "u    ,v    ,w    ,x    ,y    ,z    ,aa   ,bb   ,cc   ,dd   ,"
+                + "ee   ,ff   ,gg   ,hh   ,ii   ,jj   ,kk   ,ll   ,mm   ,nn   ,"
+                + "oo   ,pp   ,qq   ,rr   ,ss   ,tt   ,uu   ,vv   ,ww   ,xx   ,"
+                + "yy   ,zz   ,aaa  ,bbb  ,ccc  ,ddd  ,eee  ,fff  ,ggg  ,hhh  ,"
+                + "iii  ,jjj  ,kkk  ,lll  ,mmm  ,nnn  ,ooo  ,ppp  ,qqq  ,rrr  ,"
+                + "sss  ,ttt  ,uuu  ,vvv  ,www  ,xxx  ,yyy  ,zzz  ,aaaa ,bbbb ,"
+                + "cccc ,dddd ,eeee ,ffff ,gggg ,hhhh ,iiii ,jjjj ,kkkk ,llll ,"
+                + "mmmm ,nnnn ,oooo ,pppp ,qqqq ,rrrr ,ssss ,tttt ,uuuu ,vvvv ,"
+                + "aaaaa"
+        )).isInstanceOf(IllegalArgumentException.class);
     }
 }
