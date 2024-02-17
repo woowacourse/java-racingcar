cbbab12f60880ffd51dd0cf4d37425a604d5a76f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-17 23:16:12 +0900

    refactor : RaceResult 삭제 , Cars 에서 로직 담당
    - RaceResult 를 오버 엔지니어링으로 판단하여 삭제
    - Cars 에서 해당 도메인이 가진 기능 수행
    
    - 이에 따른 테스트 코드 역시 반영 ( 삭제 / 코드 추가 )

--- src/main/java/domain/car/Cars.java
+++ src/main/java/domain/car/Cars.java
@@ -1,25 +1,47 @@
 package domain.car;
 
-import domain.name.NameCatalog;
+import domain.name.Names;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Cars {
     private final List<Car> value;
 
-    private Cars(List<Car> cars) {
+    public Cars(List<Car> cars) {
         this.value = cars;
     }
 
-    public static Cars from(NameCatalog nameCatalog) {
+    public static Cars from(Names names) {
 
-        List<Car> cars = nameCatalog.getNames()
-                .stream()
-                .map(Car::new)
-                .toList();
+        List<Car> cars = names.getNames()
+                              .stream()
+                              .map(Car::new)
+                              .toList();
 
         return new Cars(cars);
+    }
+
+    public List<Car> getMaxPositionCars() {
+        Integer maxPosition = getMaxPosition();
+        return selectCarsWithPosition(maxPosition);
+    }
+
+    private Integer getMaxPosition() {
+        return value.
+                stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    private List<Car> selectCarsWithPosition(Integer position) {
+        return
+                value.stream()
+                     .filter(car -> car.getPosition()
+                                       .equals(position))
+                     .toList();
     }
 
     public List<Car> getValue() {
