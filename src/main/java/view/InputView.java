d213a945a946c7ae25b70057427f4bca7279e514
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-17 23:15:44 +0900

    refactor: 출력 문구 변수화 해제, 이름 구분자 상수명 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,24 +7,22 @@ import java.util.List;
 
 public class InputView {
 
-    private static final String NAME_DELIMITER = ",";
-    private static final String READ_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(%s)를 기준으로 구분).";
-    private static final String READ_ROUND = "시도할 회수는 몇회인가요?";
+    private static final String CAR_NAME_DELIMITER = ",";
 
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public List<String> readCarNames() throws IOException {
-        System.out.println(String.format(READ_CAR_NAMES, NAME_DELIMITER));
+        System.out.println(String.format("경주할 자동차 이름을 입력하세요(이름은 쉼표(%s)를 기준으로 구분).", CAR_NAME_DELIMITER));
         return splitNames(bufferedReader.readLine());
     }
 
     private List<String> splitNames(String names) {
-        String[] splitNames = names.split(NAME_DELIMITER);
+        String[] splitNames = names.split(CAR_NAME_DELIMITER);
         return List.of(splitNames);
     }
 
     public String readRound() throws IOException {
-        System.out.println(READ_ROUND);
+        System.out.println("시도할 회수는 몇회인가요?");
         return bufferedReader.readLine();
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,16 +2,13 @@ package view;
 
 import model.Cars;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String WINNER_DELIMITER = ", ";
-    private static final String WRITE_RESULT = "\n실행 결과";
-    private static final String WRITE_WINNERS = "가 최종 우승했습니다.";
+    private static final String WINNER_NAME_DELIMITER = ", ";
 
     public void writeResultMessage() {
-        System.out.println(WRITE_RESULT);
+        System.out.println("\n실행 결과");
     }
 
     public void writeRoundResult(Cars cars) {
@@ -19,7 +16,7 @@ public class OutputView {
     }
 
     public void writeWinners(List<String> names) {
-        String winners = String.join(WINNER_DELIMITER, names);
-        System.out.println(winners + WRITE_WINNERS);
+        String winners = String.join(WINNER_NAME_DELIMITER, names);
+        System.out.println(winners + "가 최종 우승했습니다.");
     }
 }
