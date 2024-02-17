0464c3e195b284dd47cdeffba242353ca578b05d
le2sky <leehaneul990623@gmail.com>
2024-02-18 05:03:28 +0900

    refactor : 역할이 보이는 이름으로 개선

--- src/main/java/racinggame/domain/condition/RandomMoveCondition.java
+++ src/main/java/racinggame/domain/condition/RandomMoveCondition.java
@@ -7,10 +7,10 @@ public class RandomMoveCondition implements MoveCondition {
 
     private static final int MOVABLE_STANDARD = 4;
     private static final int RANDOM_BOUND = 10;
-    private static final Random RANDOM = new Random();
+    private static final Random RANDOM_GENERATOR = new Random();
 
     @Override
     public boolean isMovable() {
-        return RANDOM.nextInt(RANDOM_BOUND) >= MOVABLE_STANDARD;
+        return RANDOM_GENERATOR.nextInt(RANDOM_BOUND) >= MOVABLE_STANDARD;
     }
 }
