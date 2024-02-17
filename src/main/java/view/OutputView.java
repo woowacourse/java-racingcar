f553291a16d8cdc7bf55af117e38f1d7584d3d80
hoyeonyy <highoption@naver.com>
2024-02-17 21:36:01 +0900

    refactor(OutputView) : 모델을 직접 넘기지 않고 필요한 값만 넘김

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,8 +2,6 @@ package view;
 
 import java.util.List;
 import java.util.StringJoiner;
-import model.Car;
-import model.Race;
 
 public class OutputView {
     private static final String OUTPUT_RESULT_MESSAGE = "\n실행 결과";
@@ -12,21 +10,20 @@ public class OutputView {
         System.out.println(OUTPUT_RESULT_MESSAGE);
     }
 
-    public static void showRoundResult(Race race) {
+    public static void showRoundResult(List<String> carNames, List<String> carDistances) {
         StringBuilder roundResult = new StringBuilder();
 
-        for (Car car : race.getCars()) {
-            roundResult.append(car.getName())
+        for (int i = 0; i < carNames.size(); i++) {
+            roundResult.append(carNames.get(i))
                     .append(" : ")
-                    .append(car.showDistance())
+                    .append(carDistances.get(i))
                     .append("\n");
         }
 
         System.out.println(roundResult);
     }
 
-    public static void showWinners(Race race) {
-        List<String> winners = race.selectWinners();
+    public static void showWinners(List<String> winners) {
         StringJoiner result = new StringJoiner(", ", "", "가 최종 우승했습니다.");
         for (String winner : winners) {
             result.add(winner);
