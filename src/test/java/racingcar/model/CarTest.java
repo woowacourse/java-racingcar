bf0be33cc74e374196384349830d5f345eeb6830
HoeSeong123 <ghltjd3@gmail.com>
2024-02-17 21:23:27 +0900

    test(CarTest) : Car 클래스에 대한 추가적인 테스트 진행

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -37,4 +37,68 @@ class CarTest {
 
         assertThat(car.getPosition()).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("두 포지션 값 중 큰 값을 리턴하는지 확인(본인의 값이 더 큰 경우)")
+    void testOtherPositionIsSmaller() {
+        Car car = new Car("test", new NumberGenerator() {
+            @Override
+            public int generateNumber(int bound) {
+                return 4;
+            }
+        });
+
+        car.move();
+        car.move();
+
+        assertThat(car.comparePosition(1)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("두 포지션 값 중 큰 값을 리턴하는지 확인(전달받은 값이 더 큰 경우)")
+    void testOtherPositionIsBigger() {
+        Car car = new Car("test", new NumberGenerator() {
+            @Override
+            public int generateNumber(int bound) {
+                return 4;
+            }
+        });
+
+        car.move();
+        car.move();
+
+        assertThat(car.comparePosition(3)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("전달받은 값과 포지션이 같은 경우 true를 반환하는지 확인")
+    void testMatchesPosition() {
+        Car car = new Car("test", new NumberGenerator() {
+            @Override
+            public int generateNumber(int bound) {
+                return 4;
+            }
+        });
+
+        car.move();
+        car.move();
+
+        assertThat(car.matchesPosition(2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("전달받은 값과 포지션이 다른 경우 false를 반환하는지 확인")
+    void testDoesNotMatchPosition() {
+        Car car = new Car("test", new NumberGenerator() {
+            @Override
+            public int generateNumber(int bound) {
+                return 4;
+            }
+        });
+
+        car.move();
+        car.move();
+
+        assertThat(car.matchesPosition(3)).isFalse();
+    }
 }
