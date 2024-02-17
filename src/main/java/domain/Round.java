5adb329aa3d7a81f2de6a3bdf49b77a6661244bb
takoyakimchi <c4nyou@gmail.com>
2024-02-17 18:27:14 +0900

    refactor: Round의 validation 로직 정리

--- src/main/java/domain/Round.java
+++ src/main/java/domain/Round.java
@@ -8,24 +8,28 @@ public class Round {
     private final int round;
 
     public Round(String rawRound) {
-        this.round = makeRoundFrom(rawRound);
+        this.round = validate(rawRound);
     }
 
     public int getRound() {
         return round;
     }
 
-    private int makeRoundFrom(String rawRound) {
+    private int validate(String rawRound) {
+        int number = validateNonNumber(rawRound);
+        validateRange(number);
+        return number;
+    }
+
+    private int validateNonNumber(String rawRound) {
         try {
-            int round = Integer.parseInt(rawRound);
-            validate(round);
-            return round;
+            return Integer.parseInt(rawRound);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
     }
 
-    private void validate(int round) {
+    private void validateRange(int round) {
         if (round < MIN_ROUND || round > MAX_ROUND) {
             throw new IllegalArgumentException(
                 "시도 횟수는 " + MIN_ROUND + " ~ " + MAX_ROUND + "만 입력 가능합니다.");
