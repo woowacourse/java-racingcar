a3cb10cfe003715f40c90ed7da2d02981db0c2f5
mzeong <mjeong@sookmyung.ac.kr>
2024-02-18 00:00:57 +0900

    refactor: proceed 메서드 반환 타입 변경에 따라 proceed 테스트 메서드에서 타입 수정

--- src/test/java/racing/domain/RaceTest.java
+++ src/test/java/racing/domain/RaceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racing.util.MovableNumberGenerator;
 
 class RaceTest {
 
@@ -13,8 +12,8 @@ class RaceTest {
     @DisplayName("주어진 횟수만큼 진행")
     void proceed() {
         int tryCount = 10;
-        Race race = new Race(new Cars("pobi,crong,honux"), new TryCount(tryCount), new MovableNumberGenerator());
-        List<String> result = race.proceed();
+        Race race = new Race(new Cars("pobi,crong,honux"), new TryCount(tryCount));
+        List<Cars> result = race.proceed();
         assertThat(result).hasSize(tryCount);
     }
 }
