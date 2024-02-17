b6e28076a7d880982873c9d3e94c556343b12515
takoyakimchi <c4nyou@gmail.com>
2024-02-17 17:31:52 +0900

    style: 해결된 TODO 주석 삭제

--- src/main/java/error/ErrorHandler.java
+++ src/main/java/error/ErrorHandler.java
@@ -6,7 +6,6 @@ public class ErrorHandler {
 
     public static final String ERROR_PREFIX = "[ERROR] ";
 
-    // TODO: 예외 발생 시 재입력받는 부분에 사용
     public static <T> T executeErrorOccurable(Supplier<T> supplier) {
         while (true) {
             try {
