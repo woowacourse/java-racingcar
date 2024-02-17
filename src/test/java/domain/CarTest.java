19b1ada0d6d998deb5e86173ca066f05745d5c84
takoyakimchi <c4nyou@gmail.com>
2024-02-17 18:53:34 +0900

    feat(test): Car 도메인 단위 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,22 +1,99 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     @Test
-    void compare() {
+    @DisplayName("이름 정상 테스트 (1): 자동차의 이름 정상 입력")
+    void carName_ok() {
+        new Car("a");
+        new Car("aBcDe");
+    }
+
+    @Test
+    @DisplayName("이름 예외 테스트 (1): 자동차의 이름은 공백일 수 없다.")
+    void carName_exception_noBlank_noEmpty() {
+        assertThatThrownBy(() -> new Car(""))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Car(" "))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름 예외 테스트 (2): 자동차의 이름은 1~5자만 허용된다.")
+    void carName_exception_cantViolateLengthRegulations() {
+        assertThatThrownBy(() -> new Car(""))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Car("abcdef"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름 예외 테스트 (3): 자동차의 이름은 알파벳만 가능하다.")
+    void carName_exception_() {
+        assertThatThrownBy(() -> new Car("a123"))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Car("A!"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차는 정상적으로 움직인다.")
+    void move() {
+        // given
         Car carA = new Car("CarA");
-        Car carB = new Car("CarB");
-        List<Car> cars = new ArrayList<>(List.of(carA, carB));
+
+        // when
         carA.move();
 
-        Assertions.assertThat(Collections.max(cars)).isEqualTo(carA);
+        // then
+        assertThat(carA.getScore()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Car 객체는 score를 기준으로 최댓값을 구할 수 있다.")
+    void compare() {
+        // given
+        Car carA = new Car("CarA");
+        Car carB = new Car("CarB");
+        Car carC = new Car("CarC");
+        List<Car> cars = new ArrayList<>(List.of(carA, carB, carC));
+
+        // when
+        carA.move();
+        carB.move();
+        carC.move();
+        carC.move();
+
+        // then
+        assertThat(Collections.max(cars)).isEqualTo(carC);
+    }
+
+    @Test
+    @DisplayName("두 Car 객체가 score를 기준으로 같으면 같다고 판정한다.")
+    void equals() {
+        // given
+        Car carA = new Car("CarA");
+        Car carB = new Car("CarB");
+
+        // when
+        carA.move();
+        carA.move();
+        carB.move();
+        carB.move();
+
+        // then
+        assertThat(carA).isEqualTo(carB);
     }
 }
