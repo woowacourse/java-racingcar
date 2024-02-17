6bcc0d3a2a1ec58c9516c3ef2440bd966c60f567
Minjoo <m0522inju@gmail.com>
2024-02-17 20:17:04 +0900

    refactor: TryCount 생성 시 0이나 음수가 들어온 케이스 각각 테스트 할 수 있도록 개선

--- src/test/java/racingcar/domain/TryCountTest.java
+++ src/test/java/racingcar/domain/TryCountTest.java
@@ -4,14 +4,17 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TryCountTest {
 
-    @Test
-    @DisplayName("[Exception] 시도 횟수가 양의 정수가 아니면 예외를 던진다")
-    void createTryCountByNotPositiveNumber() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    @DisplayName("[Exception] 시도 횟수가 0이나 음수이면 예외를 던진다")
+    void createTryCountByNotPositiveNumber(int tryCount) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new TryCount(-1))
+                .isThrownBy(() -> new TryCount(tryCount))
                 .withMessage("시도 횟수는 양의 정수를 입력해야 합니다.");
     }
 
