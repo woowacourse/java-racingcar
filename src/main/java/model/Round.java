0db8980d8da136702a7c6cf8721f2362a4d51d41
JiHyeonL <lejinhy@gmail.com>
2024-02-17 23:07:06 +0900

    fix: rebase 에러 충돌 수정

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -1,5 +1,7 @@
 package model;
 
+import static util.ErrorMessage.*;
+
 import java.util.regex.Pattern;
 
 import static view.OutputView.ERROR_PREFIX;
@@ -27,19 +29,32 @@ public class Round {
 
     private static void checkIsNull(String input) {
         if (input == null) {
+<<<<<<< HEAD
             throw new IllegalArgumentException(ERROR_PREFIX + "시도 횟수를 입력해 주십시오.");
+=======
+            throw new IllegalArgumentException(ERROR_ROUND_IS_NULL.getMessage());
+>>>>>>> bf2e5d4 (refactor: 에러 메시지 Enum 클래스로 상수화)
         }
     }
 
     private static void checkIsNumber(String input) {
         if (!Pattern.matches(NATURAL_FORMAT_REGEX, input)) {
+<<<<<<< HEAD
             throw new IllegalArgumentException(ERROR_PREFIX + "시도 횟수는 자연수여야 합니다.");
+=======
+            throw new IllegalArgumentException(ERROR_ROUND_IS_NATURAL_NUMBER.getMessage());
+>>>>>>> bf2e5d4 (refactor: 에러 메시지 Enum 클래스로 상수화)
         }
     }
 
     private static void checkIsZero(String input) {
+<<<<<<< HEAD
         if (input.equals(0)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "시도 횟수를 입력해 주세요.");
+=======
+        if (input.equals(ZERO)) {
+            throw new IllegalArgumentException(ERROR_ROUND_IS_NATURAL_NUMBER.getMessage());
+>>>>>>> bf2e5d4 (refactor: 에러 메시지 Enum 클래스로 상수화)
         }
     }
 
