d7e54cbb048136547bb053461cbc82392e5a9164
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-18 01:16:24 +0900

    refactor : NumberGenerator 제거 -> GameController 에서 담당
    - NumberGenerator 제거
    - GameController createPower 에서 담당
    
    [관련 피드백 링크](https://github.com/woowacourse/java-racingcar/pull/713#discussion_r1491154329)

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,52 +1,64 @@
 package controller;
+
 import domain.car.Car;
 import domain.car.Cars;
-import domain.name.NameCatalog;
+import domain.car.constant.CarConstant;
+import domain.name.Names;
 import domain.race.RaceCount;
 import domain.race.RaceProgress;
-import domain.race.RaceResult;
-import util.NumberGenerator;
 import view.InputView;
 import view.OutputView;
+
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
+
 public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
+
     public GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
     }
+
     public void play() {
         Cars cars = register();
 
         RaceCount raceCount = inputView.inputRaceCount();
 
-        racing(cars.getValue(),raceCount.getValue());
+        racing(cars.getValue(), raceCount.getValue());
 
-        finish(cars.getValue());
-    }
-    private Cars register(){
-        NameCatalog nameCatalog = inputView.inputCarNameCatalog();
-        return Cars.from(nameCatalog);
+        finish(cars);
     }
 
-    private void racing(final List<Car> cars, Integer raceCount){
+    private Cars register() {
+        Names names = inputView.inputCarNameCatalog();
+        return Cars.from(names);
+    }
+
+    private void racing(final List<Car> cars, Integer raceCount) {
         List<RaceProgress> raceProgresses = IntStream.range(0, raceCount)
-                .mapToObj(i -> {
-                    race(cars);
-                    return RaceProgress.from(cars);
-                })
-                .toList();
-        outputView.printRaceProgresses(raceProgresses);
+                                                     .mapToObj(i -> {
+                                                         race(cars);
+                                                         return RaceProgress.from(cars);
+                                                     })
+                                                     .toList();
+        outputView.printAllRaceProgress(raceProgresses);
     }
+
     private void race(final List<Car> cars) {
         cars.forEach(car -> {
-            car.race(NumberGenerator.generate(Car.FORWARD_MAX_NUMBER));
+            Integer power = createPower();
+            car.race(power);
         });
     }
-    private void finish(final List<Car> cars){
-        RaceResult raceResult = RaceResult.of(cars);
-        outputView.printRaceResult(raceResult);
+
+    private void finish(Cars cars) {
+        outputView.printRaceResult(cars);
+    }
+
+    private Integer createPower() {
+        return new Random().nextInt(CarConstant.MAXIMUM_POWER);
     }
 }
--- src/main/java/util/NumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.util.Random;
-
-public class NumberGenerator {
-    public static Integer generate(final Integer value){
-        return new Random().nextInt(value);
-    }
-}
