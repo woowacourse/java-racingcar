800cba2fb9d0e19218febb3fdfbd9fc4963ca5ab
mzeong <mjeong@sookmyung.ac.kr>
2024-02-18 04:03:55 +0900

    refactor: 파서 생성

--- src/main/java/racing/controller/Controller.java
+++ src/main/java/racing/controller/Controller.java
@@ -1,6 +1,8 @@
 package racing.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import racing.domain.Car;
 import racing.domain.Cars;
 import racing.domain.Race;
 import racing.domain.TryCount;
@@ -11,14 +13,17 @@ public class Controller {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final Parser parser;
 
-    public Controller(InputView inputView, OutputView outputView) {
+    public Controller(InputView inputView, OutputView outputView, Parser parser) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.parser = parser;
     }
 
     public void startGame() {
-        Cars cars = new Cars(inputView.readNames());
+        List<String> names = parser.parse(inputView.readNames());
+        Cars cars = new Cars(names.stream().map(Car::new).collect(Collectors.toList()));
         TryCount tryCount = new TryCount(inputView.readTryCount());
 
         Race race = new Race(cars, tryCount);
