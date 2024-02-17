b7f459fc91fca9f031c9c71dc71cb4d148fa0ac4
mzeong <mjeong@sookmyung.ac.kr>
2024-02-18 00:16:49 +0900

    refactor: 데이터 출력을 위해 toString 대신 getter 사용

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -3,15 +3,22 @@ package racing.view;
 import java.util.List;
 import java.util.stream.Collectors;
 import racing.domain.Car;
+import racing.domain.Cars;
 
 public class OutputView {
 
     private static final String RESULT = "\n실행 결과";
+    private static final String POSITION_MARKER = "-";
     private static final String WINNERS = "%s가 최종 우승했습니다.";
 
-    public void printResult(List<String> result) {
+    public void printResult(List<Cars> result) {
         System.out.println(RESULT);
-        System.out.println(String.join("\n", result));
+        for (Cars cars : result) {
+            List<String> roundResult = cars.getCars().stream()
+                    .map(car -> String.format("%s : %s", car.getName(), POSITION_MARKER.repeat(car.getPosition())))
+                    .toList();
+            System.out.println(String.join("\n", roundResult) + "\n");
+        }
     }
 
     public void printWinners(List<Car> winners) {
