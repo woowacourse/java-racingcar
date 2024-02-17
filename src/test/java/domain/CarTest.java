1b00af780ac71862c48e822780fe2cd6178d0430
3juhwan <13selfesteem91@naver.com>
2024-02-17 10:46:29 +0900

    Test: 자동차 이름 경계값 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,25 +1,38 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarTest {
 
     @Test
+    @DisplayName("1글자 글자 이름은 정상인가")
+    void car_created_by_1_name_test() {
+        assertThatCode(() -> new Car("A"))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("5글자 글자 이름은 정상인가")
+    void car_created_by_5_name_test() {
+        assertThatCode(() -> new Car("ABCDE"))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
     @DisplayName("이름이 5글자 초과면 예외가 발생하는가")
     void car_created_by_more_than_5_name_test() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Car("abccccc");
-        });
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("ABCDEF"));
     }
 
     @Test
     @DisplayName("이름이 1글자 미만이면 예외가 발생하는가")
     void car_created_by_less_than_1_name_test() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Car("");
-        });
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car(""));
     }
 }
