977046bb54c458b3643d0c1a95a65a02a175dd53
kunsanglee <gosmdochee2@gmail.com>
2024-02-17 17:07:50 +0900

    test : Cars 테스트코드 작성 및 RacingGameTest에 작성된 테스트 삭제

--- src/test/java/racinggame/domain/RacingGameTest.java
+++ src/test/java/racinggame/domain/RacingGameTest.java
@@ -2,7 +2,6 @@ package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,32 +15,6 @@ class RacingGameTest {
     void create() {
         assertThatCode(() -> RacingGame.of(createCars("아톰", "이상"), moveCondition()))
                 .doesNotThrowAnyException();
-    }
-
-    @DisplayName("모든 자동차를 움직일 수 있다.")
-    @Test
-    void moveAll() {
-        RacingGame game = RacingGame.of(createCars("아톰", "이상"), moveCondition());
-
-        game.race(Round.from(1));
-
-        assertThat(game.getCars()).isEqualTo(List.of(Car.of("아톰", 1), Car.of("이상", 1)));
-    }
-
-    @DisplayName("경주에 참여하는 자동차의 수는 최소 2대이다.")
-    @Test
-    void checkCarsSize() {
-        assertThatThrownBy(() -> RacingGame.of(createCars("이상"), moveCondition()))
-                .isInstanceOf(RuntimeException.class);
-    }
-
-    @DisplayName("중복을 확인한다.")
-    @Test
-    void checkDistinct() {
-        assertThatThrownBy(
-                () -> RacingGame.of(createCars("이상", "이상"),
-                        moveCondition()))
-                .isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("라운드 수 만큼 경주를 진행할 수 있다.")
