2c0315d6e9e257bb80aaa7eb8ef21922050ea23b
JiHyeonL <lejinhy@gmail.com>
2024-02-17 19:48:45 +0900

    fix: rebase 도중 충돌 오류로 인해 수정

---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,37 @@
+package model;
+
+import static util.Util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return new Cars(cars);
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            car.moveForward(generateRandomNumber());
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder allTrace = new StringBuilder();
+        for (Car car : cars) {
+            allTrace.append(car).append("\n");
+        }
+        return allTrace.toString();
+    }
+}
