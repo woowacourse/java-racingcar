ad955936624bbf0c11e27647e6431e1862d83219
kunsanglee <gosmdochee2@gmail.com>
2024-02-17 17:50:07 +0900

    refactor : InputView에서 자동차 이름을 입력받을 때 공백을 제거하도록 수정

--- src/main/java/racinggame/InputView.java
+++ src/main/java/racinggame/InputView.java
@@ -18,7 +18,9 @@ class InputView {
 
     public static Cars readCars() {
         String inputNames = readLine("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        List<String> carNames = Arrays.asList(inputNames.split(DELIMITER));
+        List<String> carNames = Arrays.stream(inputNames.split(DELIMITER))
+                .map(String::trim)
+                .toList();
 
         return Cars.of(Objects.requireNonNull(carNames));
     }
