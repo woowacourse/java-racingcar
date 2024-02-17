c0746e394ec81439502804c1e9b574d8da0bf10d
Eugene Jang <kyunellroll@gmail.com>
2024-02-17 22:12:25 +0900

    refactor(CarFactory.java): Car 마다 Engine을 부여하도록 구현
    - Car마다 Engine을 부혀하도록 stream 수정
    - Engine을 만드는 Private method 생성

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -5,15 +5,19 @@ import racingcar.random.NumberGenerator;
 
 public class CarFactory {
 
-    private final Engine engine;
+    private final NumberGenerator numberGenerator;
 
     public CarFactory(NumberGenerator numberGenerator) {
-        this.engine = new Engine(numberGenerator);
+        this.numberGenerator = numberGenerator;
     }
 
     public List<Car> createCars(List<String> carNames) {
         return carNames.stream()
-                .map(name -> new Car(name, engine))
+                .map(name -> new Car(name, buildEngine()))
                 .toList();
+    }
+
+    private Engine buildEngine() {
+        return new Engine(numberGenerator);
     }
 }
