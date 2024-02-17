044eea592392ffb36c90b005f3ad15ecab141d7d
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-18 03:24:41 +0900

    refactor: RacingGame 클래스에서 비즈니스 로직 수행하도록 변경
    - 사용되지 않는 클래스 삭제

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,32 +1,63 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class RacingGame {
 
     private static final int MINIMUM_TRY_COUNT = 1;
 
-    private final int tryCount;
-    private final Cars cars;
+    private final List<Car> cars;
+    private final int numberOfRaces;
+    private List<RaceResult> raceResults;
 
-    public RacingGame(int tryCount, Cars cars) {
-        validateTryCount(tryCount);
-        this.tryCount = tryCount;
+    public RacingGame(List<Car> cars, int numberOfRaces) {
+        validateNumberOfRaces(numberOfRaces);
         this.cars = cars;
+        this.numberOfRaces = numberOfRaces;
+        this.raceResults = new ArrayList<>();
     }
 
-    public TotalResult run() {
-        List<RoundResult> gameResult = new ArrayList<>();
-        for (int i = 0; i < tryCount; i++) {
-            gameResult.add(cars.makeCarsMove());
+    public List<RaceResult> startRace() {
+        for (int i = 0; i < numberOfRaces; i++) {
+            raceResults.add(moveCars());
         }
-        return new TotalResult(gameResult);
+        return raceResults;
     }
 
-    private void validateTryCount(int tryCount) {
-        if (tryCount < MINIMUM_TRY_COUNT) {
-            throw new IllegalArgumentException();
+    public List<Car> selectWinner() {
+        int maxPosition = getMaxPosition();
+        return cars.stream()
+                .filter(car -> maxPosition == car.getPosition())
+                .toList();
+    }
+
+    private RaceResult moveCars() {
+        Map<String, Integer> raceResult = new LinkedHashMap<>();
+        for (Car car : cars) {
+            car.move(generateRandomNumber());
+            raceResult.put(car.getName(), car.getPosition());
+        }
+        return new RaceResult(raceResult);
+    }
+
+    private int generateRandomNumber() {
+        return new Random().nextInt(10);
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
+
+    private void validateNumberOfRaces(int numberOfRaces) {
+        if (numberOfRaces < MINIMUM_TRY_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 경주 횟수는 최소 1회 이상 입력해주세요.");
         }
     }
 }
