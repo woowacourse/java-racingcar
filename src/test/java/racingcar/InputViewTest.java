14b40064d2a7f5f4270b48a6792a1d1784dd6c52
BurningFalls <sherlocky31415@gmail.com>
2024-02-17 17:26:48 +0900

    test: 콤마를 기준으로 문자열 분리하는 테스트를 작성

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -2,10 +2,14 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.view.InputView;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
@@ -18,6 +22,16 @@ public class InputViewTest {
         void testIfValidInput(String given) {
             assertThatThrownBy(() -> InputView.checkIfValidInput(given))
                     .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("콤마를 기준으로 문자열 분리")
+        void testParseWithComma() {
+            String given = "차1, 차2";
+            List<String> actual = List.of("차1", "차2");
+            List<String> expected = InputView.parseWithComma(given);
+
+            assertThat(actual).isEqualTo(expected);
         }
     }
 }
