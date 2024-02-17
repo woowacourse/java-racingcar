2e4bdb9a9fba37eec1ff3ef412efd800b5eeb882
hangillee <skfcb10@naver.com>
2024-02-18 03:33:55 +0900

    chore: 커스텀 예외가 어떤 종류인지 더 명확하게 하기 위해 상속받는 예외 변경

--- src/main/java/exception/DuplicateCarNameException.java
+++ src/main/java/exception/DuplicateCarNameException.java
@@ -2,7 +2,7 @@ package exception;
 
 import java.util.List;
 
-public class DuplicateCarNameException extends RuntimeException {
+public class DuplicateCarNameException extends IllegalArgumentException {
     private static final String DUPLICATE_CAR_NAME_EXCEPTION = "[ERROR] 자동차 이름은 중복될 수 없습니다.";
 
     public DuplicateCarNameException(List<String> names) {
--- src/main/java/exception/EmptyCarNameException.java
+++ src/main/java/exception/EmptyCarNameException.java
@@ -1,6 +1,6 @@
 package exception;
 
-public class EmptyCarNameException extends RuntimeException {
+public class EmptyCarNameException extends IllegalArgumentException {
     private static final String EMPTY_CAR_NAME_EXCEPTION = "[ERROR] 자동차 이름은 비어있을 수 없습니다.\n";
 
     public EmptyCarNameException() {
