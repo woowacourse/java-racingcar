32ec414c36d16db3ebc461f4c6966928ae8c4f63
JiHyeonL <lejinhy@gmail.com>
2024-02-17 19:32:40 +0900

    fix: rebase 도중 충돌 오류로 인해 수정

--- src/main/java/controller/RacingCarController.java
+++ src/main/java/controller/RacingCarController.java
@@ -4,6 +4,7 @@ import model.RacingGame;
 import model.Round;
 import view.InputView;
 import view.OutputView;
+
 import java.io.IOException;
 import java.util.List;
 
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-
     private static final String NAME_DELIMITER = ",";
     private static final String READ_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(%s)를 기준으로 구분).";
     private static final String READ_ROUND = "시도할 회수는 몇회인가요?";
@@ -16,6 +15,8 @@ public class InputView {
 
     public List<String> readCarNames() throws IOException {
         System.out.println(String.format(READ_CAR_NAMES, NAME_DELIMITER));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         return splitNames(bufferedReader.readLine());
     }
 
