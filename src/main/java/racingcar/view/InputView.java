c16a23a217cae97f21d01fa94d25c8c75c2ce90b
Arachneee <jhg2819@naver.com>
2024-02-17 23:50:38 +0900

    refactor: 불필요 메소드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,16 +8,12 @@ public class InputView {
 
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return readLine();
+        return scanner.nextLine();
     }
 
 
     public String readTryRound() {
         System.out.println("시도할 횟수는 몇회인가요?");
-        return readLine();
-    }
-
-    protected String readLine() {
         return scanner.nextLine();
     }
 }
