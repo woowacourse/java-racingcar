0a1ac29fae796602def2a8889c2e61a592c24c08
takoyakimchi <c4nyou@gmail.com>
2024-02-17 17:51:17 +0900

    refactor: 불필요한 try-catch 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,8 @@ import view.InputView;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            RacingGame racingGame = ErrorHandler.executeErrorOccurable(InputView::readCarNames);
-            Round round = ErrorHandler.executeErrorOccurable(InputView::readRound);
-            racingGame.play(round);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        RacingGame racingGame = ErrorHandler.executeErrorOccurable(InputView::readCarNames);
+        Round round = ErrorHandler.executeErrorOccurable(InputView::readRound);
+        racingGame.play(round);
     }
 }
