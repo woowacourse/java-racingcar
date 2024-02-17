3d6fa4b9fa7d0b9a294a4c9f808feb8e8a3f5044
KimSangHyeok <tkdgur0906@naver.com>
2024-02-17 17:35:20 +0900

    refactor : mock을 사용하지 않고 Car가 움직이는지 검증하는 테스트 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,42 +1,30 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
-
 class CarTest {
-    @BeforeAll
-    static void init() {
-        mockStatic(RandomGenerator.class);
-    }
 
     @Test
     @DisplayName("랜덤_숫자가_4_이상인_경우_전진")
     void moveCarTest() {
-        when(RandomGenerator.getRandomNumberUnderTen()).thenReturn(4);
-        Car car = new Car("apple");
+        Car car = new Car("capy");
+        int initDistance = car.getDistance();
+        car.moveCar(5);
+        int movedDistance = car.getDistance();
 
-        int beforeDistance = car.getDistance();
-        car.moveCar(RandomGenerator.getRandomNumberUnderTen());
-        int afterDistance = car.getDistance();
-
-        Assertions.assertThat(afterDistance).isEqualTo(beforeDistance + 1);
+        Assertions.assertThat(movedDistance).isEqualTo(initDistance + 1);
     }
 
     @Test
     @DisplayName("랜덤_숫자가_3_이하인_경우_정지")
     void stopCarTest() {
-        when(RandomGenerator.getRandomNumberUnderTen()).thenReturn(3);
-        Car car = new Car("apple");
+        Car car = new Car("capy");
+        int initDistance = car.getDistance();
+        car.moveCar(3);
+        int movedDistance = car.getDistance();
 
-        int beforeDistance = car.getDistance();
-        car.moveCar(RandomGenerator.getRandomNumberUnderTen());
-        int afterDistance = car.getDistance();
-
-        Assertions.assertThat(afterDistance).isEqualTo(beforeDistance);
+        Assertions.assertThat(movedDistance).isEqualTo(initDistance);
     }
 }
