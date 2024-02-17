8669af9994544ae949973a54262178873aa68939
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-18 03:19:49 +0900

    test: 도메인 수정에 의한 테스트 코드 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -16,15 +16,16 @@ class CarTest {
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"", "123456"})
-    void exceptionInvalidInput(String given) {
+    void exceptionCreateCar(String carName) {
         //given, when, then
-        assertThatThrownBy(() -> new Car(given))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 자동차 이름은 1~5자로 입력해주세요.");
     }
 
     @DisplayName("자동차를 생성하면 위치 초기값은 0이다.")
     @Test
-    void createCarByCarName() {
+    void confirmPosition() {
         //given
         String carName = "ted";
         int expectedPosition = 0;
@@ -39,14 +40,15 @@ class CarTest {
     @DisplayName("4보다 큰 숫자일 경우 전진, 작을 경우 전진하지 않는다.")
     @ParameterizedTest
     @CsvSource(value = {"4,1", "3,0"})
-    void carMovingTest(int given, int expected) {
+    void confirmMove(int given, int expected) {
         //given
         String name = "daon";
 
         //when
         Car car = new Car(name);
+        car.move(given);
 
         //then
-        assertThat(car.move(given)).isEqualTo(expected);
+        assertThat(car.getPosition()).isEqualTo(expected);
     }
 }
