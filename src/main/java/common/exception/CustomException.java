53f9c557bb032b3dbe8b662ebe824aa56ba2e851
PgmJun <chltmdwns96@gmail.com>
2024-02-17 12:35:16 +0900

    remove: 미사용 constructor 제거

--- src/main/java/common/exception/CustomException.java
+++ src/main/java/common/exception/CustomException.java
@@ -1,10 +1,6 @@
 package common.exception;
 
 public class CustomException extends RuntimeException {
-    public CustomException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public CustomException(String message) {
         super(message);
     }
--- src/main/java/common/exception/model/ValidateException.java
+++ src/main/java/common/exception/model/ValidateException.java
@@ -3,10 +3,6 @@ package common.exception.model;
 import common.exception.CustomException;
 
 public class ValidateException extends CustomException {
-    public ValidateException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public ValidateException(String message) {
         super(message);
     }
