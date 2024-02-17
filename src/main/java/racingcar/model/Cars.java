b95439ec0d902e7e653415c3b168a8ce0f36eaa8
BurningFalls <sherlocky31415@gmail.com>
2024-02-17 16:52:01 +0900

    style: Cars 클래스의 역할과 책임에 맞지 않는 메서드를 Inputview 클래스로 이동
    checkIfValidInput, parseWithComma 메서드를 이동

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,42 +2,18 @@ package racingcar.model;
 
 import racingcar.controller.NumericGenerator;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Cars {
     private NumericGenerator generator;
     private List<Car> cars;
 
-    public Cars(NumericGenerator generator, String input) {
+    public Cars(NumericGenerator generator, List<String> carNames) {
         this.generator = generator;
-
-        checkIfValidInput(input);
-        List<String> carNames = parseWithComma(input);
 
         this.cars = carNames.stream()
                 .map(Car::new)
-                .collect(Collectors.toList());
-    }
-
-    public void checkIfValidInput(String input) {
-        String regex = "^[^,]+(,[^,]+)*$";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("[ERROR] 문자열의 형태가 유효하지 않습니다.");
-        }
-    }
-
-    public List<String> parseWithComma(String input) {
-        return Arrays.stream(input.split(","))
-                .map(String::trim)
-                .filter(str -> !str.isEmpty())
                 .collect(Collectors.toList());
     }
 
