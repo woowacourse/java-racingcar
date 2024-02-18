0992badfb12fae3ccc5ce4794216076ab0656870
Nam Gi Beom <rlqja0523@naver.com>
2024-02-18 19:31:58 +0900

    refactor: 잘못된 변수 이름 변경

--- src/test/java/support/ImmovableNumberGenerator.java
+++ src/test/java/support/ImmovableNumberGenerator.java
@@ -3,10 +3,10 @@ package support;
 import util.NumberGenerator;
 
 public class ImmovableNumberGenerator implements NumberGenerator {
-    private static final int IMMUTABLE_NUMBER = 3;
+    private static final int IMMOVABLE_NUMBER = 3;
 
     @Override
     public int generateNumber() {
-        return IMMUTABLE_NUMBER;
+        return IMMOVABLE_NUMBER;
     }
 }
