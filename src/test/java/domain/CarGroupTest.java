4c9d83cdffa8e41e5f2a3ae415cb2e1c09e14915
3juhwan <13selfesteem91@naver.com>
2024-02-18 15:27:22 +0900

    Test: CarGroup 테스트 추가

--- src/test/java/domain/CarGroupTest.java
+++ src/test/java/domain/CarGroupTest.java
@@ -1,8 +1,10 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,46 +12,70 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarGroupTest {
 
     @Test
-    @DisplayName("자동차 이름이 정상적으로 생성되는가")
-    void cars_generation_test() {
-        String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
-
-        CarGroup carGroup = new CarGroup(carNames);
-        List<Car> expected = List.of(new Car("mang"), new Car("weve"), new Car("pobi"),
-                new Car("neo"));
-
-        assertThat(carGroup.getAllCars()).containsExactlyElementsOf(expected);
-    }
-
-    @Test
-    @DisplayName("우승자가 정상적으로 반환되는가")
-    void get_winners_test() {
+    @DisplayName("이름에 따라 자동차들이 정상적으로 생성되는가")
+    void is_cars_correctly_made_by_name() {
         // given
         String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
-        CarGroup carGroup = new CarGroup(carNames);
 
         // when
-        List<Car> cars = carGroup.getAllCars();
-        Car winner = cars.get(0);
-        winner.move(9);
-
-        // then
-        assertThat(carGroup.getWinners()).containsExactly(winner);
-    }
-
-    @Test
-    @DisplayName("우승자가 없는가")
-    void no_winners_test() {
-        // given
-        String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
         CarGroup carGroup = new CarGroup(carNames);
 
-        // when
-        List<Car> cars = carGroup.getAllCars();
-        Car winner = cars.get(0);
-        winner.move(0);
-
         // then
-        assertThat(carGroup.getWinners()).isEmpty();
+        assertThat(carGroup.getAllCars()).extracting("name")
+                .containsExactlyElementsOf(Arrays.stream(carNames).toList());
     }
+
+    @Nested
+    @DisplayName("우승자 반환 테스트")
+    class WinnersReturnTest {
+
+        @Test
+        @DisplayName("단독 우승인 경우를 정상적으로 반환되는가")
+        void return_single_winner_correctly_test() {
+            // given
+            String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
+            CarGroup carGroup = new CarGroup(carNames);
+
+            // when
+            List<Car> cars = carGroup.getAllCars();
+            Car winner = cars.get(0);
+            winner.move(9);
+
+            // then
+            List<Car> winners = carGroup.getWinners();
+            assertThat(winners).containsExactly(winner);
+        }
+
+        @Test
+        @DisplayName("복수 우승인 경우를 정상적으로 반환되는가")
+        void return_multiple_winners_correctly_test() {
+            // given
+            String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
+            CarGroup carGroup = new CarGroup(carNames);
+
+            // when
+            List<Car> cars = carGroup.getAllCars();
+            Car winnerA = cars.get(0);
+            winnerA.move(9);
+            Car winnerB = cars.get(1);
+            winnerB.move(6);
+
+            // then
+            List<Car> winners = carGroup.getWinners();
+            assertThat(winners).containsExactlyElementsOf(List.of(winnerA, winnerB));
+        }
+
+        @Test
+        @DisplayName("우승자가 없는가")
+        void no_winners_test() {
+            // given
+            String[] carNames = new String[]{"mang", "weve", "pobi", "neo"};
+            CarGroup carGroup = new CarGroup(carNames);
+
+            // then
+            List<Car> winners = carGroup.getWinners();
+            assertThat(winners).isEmpty();
+        }
+    }
+
 }
