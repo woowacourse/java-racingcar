b4336678ae8c6011e87e3dd60e2fc9c2fe32441e
slimsha2dy <chy2581@naver.com>
2024-02-18 22:35:14 +0900

    rename: RacingController 클래스의 이름을 RacingGame으로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,9 @@
 package racingcar;
 
-import racingcar.controller.RacingController;
+import racingcar.controller.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        new RacingController().run();
+        new RacingGame().run();
     }
 }
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -8,7 +8,7 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 import racingcar.view.model.ConsoleReader;
 
-public class RacingController {
+public class RacingGame {
     public void run() {
         final Cars cars = createCars(InputView.readCarNames(new ConsoleReader()));
         final int tryCount = promptUserForTryCount();
