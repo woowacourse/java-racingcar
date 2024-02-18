571888f5371ee7ffefce9365e4836e29e6233e3c
yoonjuho <dbswn990@naver.com>
2024-02-18 22:20:44 +0900

    refactor(Car): 자동차는 이름과 자신의 이동 기록만을 저장한다.

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,53 +1,34 @@
 package model;
 
-import common.Exception;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Car {
-  private static final int MAX_NAME_LENGTH = 5;
-  private final String name;
-  private final List<MoveStatus> statuses;
+  private static final int POWER_THRESHOLD = 4;
+  private final CarName carName;
+  private final List<MoveStatus> movingHistory;
 
   public Car(String name) {
-    validateNullAndEmpty(name);
-    validateNameLength(name);
-    this.name = name;
-    this.statuses = new ArrayList<>();
+    this.carName = new CarName(name);
+    this.movingHistory = new ArrayList<>();
   }
 
-  private void validateNullAndEmpty(String name) {
-    if (Objects.isNull(name) || name.isBlank()) {
-      throw new IllegalArgumentException(Exception.CAR_NAME_NULL_BLANK.getMessage());
+  public void move(int power) {
+    if (power >= POWER_THRESHOLD) {
+      movingHistory.add(MoveStatus.FORWARD);
+      return;
     }
+    movingHistory.add(MoveStatus.STOP);
   }
 
-  private void validateNameLength(String name) {
-    if (name.length() > MAX_NAME_LENGTH) {
-      throw new IllegalArgumentException(Exception.CAR_NAME_LENGTH.getMessage());
-    }
-  }
-
-  public void forward() {
-    statuses.add(MoveStatus.FORWARD);
-  }
-
-  public void stop() {
-    statuses.add(MoveStatus.STOP);
-  }
-
-  public List<MoveStatus> getStatuses() {
-    return new ArrayList<>(statuses);
+  public int countOfForward() {
+    return (int)
+        movingHistory.stream().filter(carStatus -> carStatus.equals(MoveStatus.FORWARD)).count();
   }
 
   public String getName() {
-    return name;
-  }
-
-  public int getForwardCount() {
-    return (int)
-        statuses.stream().filter(carStatus -> carStatus.equals(MoveStatus.FORWARD)).count();
+    return carName.getName();
   }
 
   @Override
@@ -55,11 +36,11 @@ public class Car {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Car car = (Car) o;
-    return Objects.equals(name, car.name) && Objects.equals(statuses, car.statuses);
+    return Objects.equals(carName, car.carName) && Objects.equals(movingHistory, car.movingHistory);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name, statuses);
+    return Objects.hash(carName, movingHistory);
   }
 }
--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,36 +1,41 @@
 package model;
 
+import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import response.Exception;
 
 class CarTest {
-
-  @ParameterizedTest
-  @NullAndEmptySource
-  @DisplayName("이름이 없을 시 예외가 발생한다.")
-  void validateNullAndEmpty(String input) {
-    assertThatThrownBy(() -> new Car(input)).isInstanceOf(IllegalArgumentException.class);
-  }
-
   @Test
-  @DisplayName("이름이 6자 이상일 시 예외가 발생한다.")
-  void validateNameLength() {
-    String name = "abcdefg";
-    assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
+  @DisplayName("자동차들이 중복된 이름을 가질 시 예외가 발생한다.")
+  void validateDuplicate() {
+    assertThatThrownBy(() -> new Cars(of(new Car("포비"), new Car( "포비"))))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(Exception.DUPLICATE.getMessage());
+  }
+  @Test
+  @DisplayName("자동차는 전진하거나 정지 할 수 있다.")
+  void move(){
+    Car movedCar = new Car("pobi");
+    Car stoppedCar = new Car("left");
+    movedCar.move(4);
+    stoppedCar.move(3);
+    assertAll(
+            () ->assertThat(movedCar.countOfForward()).isEqualTo(1),
+            () ->assertThat(stoppedCar.countOfForward()).isEqualTo(0)
+    );
+  }
+  @Test
+  @DisplayName("자동차는 전진한 횟수를 알 수 있다.")
+  void countOfForward(){
+    Car movedCar = new Car("pobi");
+    movedCar.move(4);
+    movedCar.move(4);
+    assertThat(movedCar.countOfForward()).isEqualTo(2);
   }
 
-  @Test
-  @DisplayName("전진한 횟수를 알 수 있다.")
-  void getForwardCount() {
-    Car car = new Car("pobi");
-    car.forward();
-    car.stop();
-    car.forward();
-    assertThat(car.getForwardCount()).isEqualTo(2);
-  }
 }
