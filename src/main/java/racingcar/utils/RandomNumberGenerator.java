00ea577f720063e301044239a652c16b087a7c9b
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-18 23:31:01 +0900

    feat(randomNumberGenerator): 입력한 크기만큼의 난수를 생성하는 메서드 추가
    - 입력한 크기만큼의 난수를 생성하는지 테스트하는 코드 추가

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,8 +1,17 @@
 package racingcar.utils;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class RandomNumberGenerator {
+    public static List<Integer> generate(int size) {
+        Random random = new Random();
+        return IntStream.range(0, size)
+                .mapToObj(i -> random.nextInt(10))
+                .toList();
+    }
+
     public static int generate() {
         Random random = new Random();
         return random.nextInt(10);
--- src/test/java/racingcar/utils/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/utils/RandomNumberGeneratorTest.java
@@ -11,4 +11,11 @@ public class RandomNumberGeneratorTest {
         int randomNumber = RandomNumberGenerator.generate();
         assertThat(randomNumber).isBetween(0, 9);
     }
+
+    @Test
+    @DisplayName("입력한 크기만큼의 난수를 생성한다.")
+    void randomListSizeTest() {
+        int size = 5;
+        assertThat(RandomNumberGenerator.generate(size)).hasSize(size);
+    }
 }
