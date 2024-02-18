d616e06732c8bd9c6db964d6bf66b101057e72c3
yoonjuho <dbswn990@naver.com>
2024-02-18 22:23:23 +0900

    refactor(Main): try-with-resource 재적용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
 import controller.RacingCarController;
-import java.io.IOException;
 import view.InputView;
 import view.OutputView;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     try (InputView inputView = new InputView()) {
       new RacingCarController(inputView, new OutputView()).run();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
     }
   }
 }
