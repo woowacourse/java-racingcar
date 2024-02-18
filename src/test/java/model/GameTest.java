a5f90f1207475fdea213c0e13c9eabbfdd96ad4d
yoonjuho <dbswn990@naver.com>
2024-02-18 22:25:01 +0900

    refactor(Game): 일급 컬렉션 사용으로 인한 메서드 시그니처 변경

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -2,47 +2,18 @@ package model;
 
 import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import common.Exception;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-  private Car pobi;
-  private Car left;
-
-  @BeforeEach
-  void setUp() {
-    pobi = new Car("포비");
-    left = new Car("왼손");
-  }
-
-  @Test
-  @DisplayName("자동차들이 중복된 이름을 가질 시 예외가 발생한다.")
-  void validateDuplicate() {
-    assertThatThrownBy(() -> new Game(of("포비", "포비")))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining(Exception.DUPLICATE.getMessage());
-  }
 
   @Test
   @DisplayName("우승자를 알아 낼 수 있다.")
-  void findWinner() {
-    Game game = new Game(of(pobi.getName(), left.getName()));
-    pobi.forward();
-    left.stop();
-    assertThat(game.findWinners(List.of(pobi, left))).isEqualTo(List.of(pobi));
-  }
-
-  @Test
-  @DisplayName("공동 우승자를 알아 낼 수 있다")
-  void findWinners() {
-    Game game = new Game(of(pobi.getName(), left.getName()));
-    pobi.forward();
-    left.forward();
-    assertThat(game.findWinners(List.of(pobi, left))).isEqualTo(List.of(pobi, left));
+  void winners() {
+    Car pobi = new Car("pobi");
+    Car left = new Car("left");
+    pobi.move(4);
+    assertThat(new Game().winners(new Cars(of(pobi, left)))).isEqualTo(new Cars(of(pobi)));
   }
 }
