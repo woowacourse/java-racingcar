228f77dd0c1456247b4328a72dd60c44595c8cd0
JiHyeonL <lejinhy@gmail.com>
2024-02-18 20:53:11 +0900

    refactor: 출력 포맷 OutputView로 보냄에 따라 수정된 사항 적용

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -14,12 +14,12 @@ public class CarTest {
 
     @DisplayName("랜덤 숫자가 4 이상일 때만 자동차가 전진한다.")
     @ParameterizedTest
-    @CsvSource({"5, '-'", "3, ''", "4, '-'"})
-    void moveForwardTest(int randomNumber, String expectedPosition) {
+    @CsvSource({"5, 1", "3, 0", "4, 1"})
+    void moveForwardTest(int randomNumber, int expectedPosition) {
         final Car car = Car.from("lemon");
         car.moveForward(randomNumber);
 
-        assertThat(car.showPositionTraceByFormat("-")).isEqualTo(expectedPosition);
+        assertThat(car.getPosition()).isEqualTo(expectedPosition);
     }
 
     @DisplayName("자동차 이름이 공백 포함 5자 이하가 아닌 경우 에러가 발생한다.")
--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -4,21 +4,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.List;
 
 public class RacingGameTest {
 
-    @DisplayName("자동차 이름이 중복되면 에러가 발생한다.")
-    @Test
-    void nameDuplicatedTest() {
-        final List<String> carNames = List.of("lemon","lemon","a","nyang");
-        assertThatThrownBy(() -> RacingGame.from(carNames));
-    }
+    @DisplayName("시도 횟수 입력 값이 자연수가 아닌 경우 예외가 발생하는지 검증한다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"+4", "ss", "", "   ", "-1", "3.2", "0"})
+    void roundValidationTest(String tryCount) {
+        final List<String> correctNames = List.of("lemon" ,"berry");
+        final Cars correctCars = Cars.from(correctNames);
 
-    @DisplayName("자동차 이름 개수가 2개 미만이면 에러가 발생한다.")
-    @Test
-    void carNameCountTest() {
-        final List<String> carNames = List.of("lemon");
-        assertThatThrownBy(() -> RacingGame.from(carNames));
+        System.out.println(correctCars.getCars());
+        assertThatThrownBy(() -> RacingGame.from(correctCars, tryCount))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
