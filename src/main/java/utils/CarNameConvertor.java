67565fed7205016109b40ba4713d856946f63acb
hoyeonyy <highoption@naver.com>
2024-02-18 14:20:38 +0900

    refactor(CarNameConvertor) : 기본 생성자의 접근을 막음

--- src/main/java/utils/CarNameConvertor.java
+++ src/main/java/utils/CarNameConvertor.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarNameConvertor {
+    private CarNameConvertor() {
+    }
 
     public static List<String> splitCarNames(String input) {
         String[] names = input.split(",");
