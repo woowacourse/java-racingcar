e5392d2465f11d14f8d0b0cc0e432f517a8095fb
이상진 <pricelees@gmail.com>
2024-02-18 17:21:25 +0900

    refactor(Application) : RacingGame의 생성 방법 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,17 @@
+import controller.RacingGame;
+import domain.strategy.MovingStrategy;
+import domain.strategy.RandomNumberMovingStrategy;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
 
-    public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
-        racingGame.run();
-    }
+	public static void main(String[] args) {
+		InputView inputView = new InputView();
+		OutputView outputView = new OutputView();
+		MovingStrategy movingStrategy = new RandomNumberMovingStrategy();
+
+		RacingGame racingGame = new RacingGame(inputView, outputView, movingStrategy);
+		racingGame.run();
+	}
 }
