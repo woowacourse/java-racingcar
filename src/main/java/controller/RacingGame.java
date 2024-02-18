4625bd3a09719bf1f58145d84a0c8a1c8a448afa
이상진 <pricelees@gmail.com>
2024-02-18 17:13:22 +0900

    refactor(RacingGame): MovingStrategy로 인한 수정사항 반영

--- src/main/java/controller/RacingGame.java
+++ src/main/java/controller/RacingGame.java
@@ -7,59 +7,55 @@ import java.util.function.Supplier;
 import constants.ErrorMessages;
 import domain.Car;
 import domain.Cars;
-import utils.Randoms;
+import domain.strategy.MovingStrategy;
 import view.InputView;
 import view.OutputView;
 
-
 public class RacingGame {
-    private static final int MAX_RANDOM_NUMBER_RANGE = 9;
+	private static final String CAR_NAMES_DELIMITER = ",";
+	private static final int INITIAL_LOCATION = 0;
 
-    private final InputView inputView;
-    private final OutputView outputView;
+	private final InputView inputView;
+	private final OutputView outputView;
+	private final MovingStrategy movingStrategy;
 
-    public RacingGame(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
+	public RacingGame(InputView inputView, OutputView outputView, MovingStrategy movingStrategy) {
+		this.inputView = inputView;
+		this.outputView = outputView;
+		this.movingStrategy = movingStrategy;
+	}
 
-    public void run() {
-        String names = retryInputOnException(inputView::receiveNames);
-        int trialCount = retryInputOnException(inputView::receiveTrialCount);
+	public void run() {
+		String names = retryInputOnException(inputView::receiveNames);
+		int trialCount = retryInputOnException(inputView::receiveTrialCount);
 
-        Cars cars = loadCars(names);
-        race(trialCount, cars);
-        outputView.printWinners(cars.getLargestLocationCarsName());
-    }
+		Cars cars = loadCars(names);
+		race(trialCount, cars);
+		outputView.printWinners(cars.getLargestLocationCarsName());
+	}
 
-    private <T> T retryInputOnException(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            System.out.println(ErrorMessages.INVALID_INPUT.getMessage());
-            return retryInputOnException(supplier);
-        }
-    }
+	private <T> T retryInputOnException(Supplier<T> supplier) {
+		try {
+			return supplier.get();
+		} catch (IllegalArgumentException e) {
+			System.out.println(ErrorMessages.INVALID_INPUT.getMessage());
+			return retryInputOnException(supplier);
+		}
+	}
 
-    private Cars loadCars(String carNames) {
-        List<Car> carList = Arrays.stream(carNames.split(","))
-                .map(Car::from)
-                .toList();
+	private Cars loadCars(String carNames) {
+		List<Car> carList = Arrays.stream(carNames.split(CAR_NAMES_DELIMITER))
+			.map(name -> new Car(name, INITIAL_LOCATION, movingStrategy))
+			.toList();
 
-        return new Cars(carList);
-    }
+		return new Cars(carList);
+	}
 
-    private void race(int trialCount, Cars cars) {
-        outputView.printResultPrefix();
-        for (int i = 0; i < trialCount; i++) {
-            List<Integer> randomNumbers = createRandomNumber(cars.getCount());
-            cars.moveAll(randomNumbers);
-            outputView.printCurrentLocations(cars.getCurrentLocations());
-        }
-    }
-
-
-    protected List<Integer> createRandomNumber(int carCount) {
-        return Randoms.getRandomNumbers(carCount, MAX_RANDOM_NUMBER_RANGE);
-    }
+	private void race(int trialCount, Cars cars) {
+		outputView.printResultPrefix();
+		for (int i = 0; i < trialCount; i++) {
+			cars.moveAll();
+			outputView.printCurrentLocations(cars.getCurrentLocations());
+		}
+	}
 }
