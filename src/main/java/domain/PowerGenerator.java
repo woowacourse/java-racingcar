b464be812d91d68b985ef7e01c24448a6628ce14
SCY <helenason@naver.com>
2024-02-19 00:37:29 +0900

    refactor: 랜덤 숫자 생성 방법 변경

--- src/main/java/domain/PowerGenerator.java
+++ src/main/java/domain/PowerGenerator.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.util.Random;
+
 public class PowerGenerator {
 
     private static final Integer MIN = 0;
     private static final Integer MAX = 9;
 
     public Integer makeRandomNumber() {
-        return (int) (Math.random() * (MAX - MIN + 1) + MIN);
+        Random random = new Random();
+        return random.nextInt(MAX - MIN + 1);
     }
 }
