b31f5766888832f4b0151bc3ad2d191829416612
hoyeonyy <highoption@naver.com>
2024-02-18 14:24:21 +0900

    refactor(RaceController) : 값을 주입 받는 메서드 생성

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -1,30 +1,60 @@
 package controller;
 
 
+import model.Cars;
 import model.Race;
+import model.Round;
+import view.InputView;
 import view.OutputView;
 
 public class RaceController {
 
-    private final InputController inputController;
+    private final InputView inputView;
+    private final OutputView outputView;
 
-    public RaceController() {
-        this.inputController = new InputController();
+    public RaceController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void startRace() {
-        Race race = inputController.makeRace();
-        int tryCount = inputController.makeTryCount();
+        Race race = makeRace();
+        Round round = makeTryCount();
 
-        OutputView.showRaceResult();
-        runOneRound(tryCount, race);
-        OutputView.showWinners(race.selectWinners());
+        outputView.showRaceResult();
+        startRound(round, race);
+        outputView.showWinners(race.selectWinners());
     }
 
-    private void runOneRound(int tryCount, Race race) {
-        for (int round = 0; round < tryCount; round++) {
-            race.oneGame();
-            OutputView.showRoundResult(race.getCarsName(), race.getDistances());
+    private Race makeRace() {
+        try {
+            String carNames = inputView.inputCarName();
+            Cars cars = new Cars(carNames);
+
+            return new Race(cars);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+            return makeRace();
+        }
+    }
+
+    private Round makeTryCount() {
+        try {
+            String inputRound = inputView.inputRound();
+
+            return new Round(inputRound);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+            return makeTryCount();
+        }
+    }
+
+    private void startRound(Round round, Race race) {
+        for (int i = 0; i < round.getRound(); i++) {
+            race.startRound();
+            outputView.showRoundResult(race.getCarsName(), race.getDistances());
         }
     }
 }
