a66966f7a3e7fc6022f2cc2fdb8fcc7cbfe5df2d
nhlee98 <e5slnh9876@gmail.com>
2024-02-18 20:07:34 +0900

    refactor : 안쓰는 메소드 제거

--- src/main/java/domain/car/CarPosition.java
+++ src/main/java/domain/car/CarPosition.java
@@ -13,23 +13,6 @@ public final class CarPosition {
         this.position += distance;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (!(object instanceof CarPosition carPosition)) {
-            return false;
-        }
-
-        return this.position == carPosition.position;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(position);
-    }
-
     public int getPosition() {
         return position;
     }
