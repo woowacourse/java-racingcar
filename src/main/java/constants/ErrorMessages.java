3f384579683ddc9b53d0cbf0df211d65778d70a9
이상진 <pricelees@gmail.com>
2024-02-18 15:04:41 +0900

    refactor(ErrorMessages) : 차량 이름과 위치에 대한 예외 메시지 추가

--- src/main/java/constants/ErrorMessages.java
+++ src/main/java/constants/ErrorMessages.java
@@ -1,7 +1,9 @@
 package constants;
 
 public enum ErrorMessages {
-	INVALID_INPUT("입력 형식이 올바르지 않습니다.");
+	INVALID_INPUT("입력 형식이 올바르지 않습니다."),
+	INVALID_CAR_NAME("이름의 길이는 1~5글자여야 합니다."),
+	INVALID_CAR_LOCATION("위치값은 음수가 될 수 없습니다");
 
 	private static final String prefix = "[ERROR]";
 
