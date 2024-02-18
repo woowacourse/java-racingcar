d12b7770a8e2b8775de9f77d07c79e75a893de8b
unifolio0 <boho82ha@gmail.com>
2024-02-18 12:43:08 +0900

    test : 테스트 추가

--- src/test/java/domain/RacingCarNamesTest.java
+++ src/test/java/domain/RacingCarNamesTest.java
@@ -4,24 +4,33 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 class RacingCarNamesTest {
     @DisplayName("레이싱카 이름이 5글자 이하인지 확인한다.")
     @Test
-    public void testCarNameLength() {
-        List<String> carNameOverThan5Length = Collections.singletonList("racingcar");
+    public void testCarNameOverThan5Length() {
+        List<String> carNameContainOverThan5Length = List.of("bito", "zeze", "baekho", "ready", "kirby");
 
-        Assertions.assertThatThrownBy(() -> new RacingCarNames(carNameOverThan5Length))
+        Assertions.assertThatThrownBy(() -> new RacingCarNames(carNameContainOverThan5Length))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름은 5글자 이하만 가능합니다.");
+                .hasMessage("이름은 1글자 이상 5글자 이하만 가능합니다.");
+    }
+
+    @DisplayName("레이싱카 이름이 1글자 이상인지 확인한다.")
+    @Test
+    public void testCarNameBlank() {
+        List<String> carNameContainBlank = List.of("bito", "zeze", "baekho","","ready", "kirby");
+
+        Assertions.assertThatThrownBy(() -> new RacingCarNames(carNameContainBlank))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1글자 이상 5글자 이하만 가능합니다.");
     }
 
     @DisplayName("레이싱카 이름이 중복되는 지 확인한다.")
     @Test
     public void testCarNameDuplication() {
-        List<String> duplicatedNames = Collections.singletonList("rac,rac,bito");
+        List<String> duplicatedNames = List.of("bito", "zeze", "bito");
 
         Assertions.assertThatThrownBy(() -> new RacingCarNames(duplicatedNames))
                 .isInstanceOf(IllegalArgumentException.class)
