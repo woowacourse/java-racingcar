394f1aa829f76f059004f22bef47f80070402965
tackyu <tack970913@gmail.com>
2024-02-18 16:18:29 +0900

    refactor: 접근 제어자 변경

--- src/main/java/util/NumberGenerator.java
+++ src/main/java/util/NumberGenerator.java
@@ -1,7 +1,7 @@
 package util;
 
 import java.util.Random;
-
+//TODO 도메인으로 설계해보기
 public class NumberGenerator {
     public static Integer generate(final Integer value) {
         return new Random().nextInt(value);
