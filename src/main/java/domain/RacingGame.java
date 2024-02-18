d9cccad9015983fb0a161821e69da896ea79eef1
3juhwan <13selfesteem91@naver.com>
2024-02-18 16:56:17 +0900

    Fix: controller에 도메인 조립

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -2,25 +2,16 @@ package domain;
 
 import utils.PowerGenerator;
 
-import java.util.stream.IntStream;
-
 public class RacingGame {
-    private final PowerGenerator powerGenerator;
     private final CarGroup carGroup;
-    private final Attempts attempts;
+    private final PowerGenerator powerGenerator;
 
-    public RacingGame(final CarGroup carGroup, final Attempts attempts, final PowerGenerator powerGenerator) {
+    public RacingGame(final CarGroup carGroup, final PowerGenerator powerGenerator) {
         this.carGroup = carGroup;
-        this.attempts = attempts;
         this.powerGenerator = powerGenerator;
     }
 
-    public void race() {
-        final int numberOfAttempts = attempts.numberOfAttempts();
-        IntStream.range(0, numberOfAttempts).forEach(i -> moveCars());
-    }
-
-    private void moveCars() {
+    public void moveCars() {
         carGroup.getAllCars().forEach(car -> {
             final int power = powerGenerator.getNumber();
             car.move(power);
