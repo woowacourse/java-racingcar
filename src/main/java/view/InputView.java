6bb751011ee837b0ccc1bcbc01f478b9b0880158
unifolio0 <boho82ha@gmail.com>
2024-02-18 11:57:17 +0900

    feat : view가 약간의 데이터 가공이 가능하도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,26 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner sc = new Scanner(System.in);
-    public static String inputCarNames() {
+    private static final Scanner sc = new Scanner(System.in);
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return sc.next();
+        String input = sc.nextLine().trim();
+        validateLastComma(input);
+        return List.of(input.split(","));
     }
 
-    public static String inputMoveCount() {
+    private static void validateLastComma(String input) {
+        if (input.endsWith(",")) {
+            throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하만 가능합니다.");
+        }
+    }
+
+    public static int inputMoveCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return sc.next().trim();
+        String input = sc.nextLine();
+        return Integer.parseInt(input);
     }
 }
