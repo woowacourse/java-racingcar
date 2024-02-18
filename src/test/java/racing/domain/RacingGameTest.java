3b2899ccfd29005beb5301119bfa28c6d6f14bd8
robinjoon <robin980108@naver.com>
2024-02-18 11:29:29 +0900

    test: 우승자 테스트 추가

--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -7,21 +7,45 @@ import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
 import racing.dto.RacingResult;
 import racing.dto.RacingResults;
 
 class RacingGameTest {
 
+
   @Test
   @DisplayName("게임 실행 테스트")
   void run() {
+    //given
     List<String> carNames = List.of("testA", "testB", "testC");
+    List<Integer> racingConditions = List.of(CAR_MOVE_THRESHOLD, CAR_MOVE_THRESHOLD - 1,
+        CAR_MOVE_THRESHOLD);
     RacingGame racingGame = new RacingGame(carNames, 1);
-    RacingResults racingResults = racingGame.run(
-        size -> List.of(CAR_MOVE_THRESHOLD, CAR_MOVE_THRESHOLD - 1, CAR_MOVE_THRESHOLD));
+    //when
+    RacingResults racingResults = racingGame.run(size -> racingConditions);
     RacingResults expected = new RacingResults(List.of(
         new RacingResult(Map.of("testA", 1, "testB", 0, "testC", 1))));
+    //then
     Assertions.assertThat(racingResults.isSameResult(expected))
         .isTrue();
+  }
+
+  @Test
+  @DisplayName("우승자 테스트")
+  @MethodSource("getWinnerNamesParameters")
+  void getWinnerNames() {
+    //given
+    List<String> carNames = List.of("testA", "testB", "testC");
+    List<Integer> racingConditions = List.of(CAR_MOVE_THRESHOLD, CAR_MOVE_THRESHOLD - 1,
+        CAR_MOVE_THRESHOLD);
+    List<String> expectedWinnerNames = List.of("testA", "testC");
+    RacingGame racingGame = new RacingGame(carNames, 1);
+    //when
+    RacingResults racingResults = racingGame.run(size -> racingConditions);
+    List<String> winnerNames = racingResults.getWinnerNames();
+    //then
+    Assertions.assertThat(winnerNames)
+        .containsExactlyInAnyOrderElementsOf(expectedWinnerNames);
   }
 }
