de3f9b96a5b4dcc4678108db76f88f8eb51c9911
ay-eonii <naayen@naver.com>
2024-02-18 22:45:15 +0900

    feat(RacingGame): 경주게임 도메인

---
+++ src/main/java/domain/RacingGame.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.List;
+
+public class RacingGame {
+    private final RaceCars raceCars;
+    private final Count count;
+
+    public RacingGame(RaceCars raceCars, Count count) {
+        this.raceCars = raceCars;
+        this.count = count;
+    }
+
+    public void race() {
+        while (count.hasRemain()) {
+            raceCars.play();
+            count.down();
+        }
+    }
+
+    public Count getCount() {
+        return count;
+    }
+
+    public List<Car> judgeWinners() {
+        raceCars.sortCarsDes();
+        int maxLocation = raceCars.getCar(0).getLastLocation();
+        return raceCars.getWinners(maxLocation);
+    }
+
+    public RaceCars getRaceCars() {
+        return raceCars;
+    }
+}
