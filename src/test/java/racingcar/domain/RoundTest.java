a4e2ed0ab5a055a6167c2bfb4978eab6acca5138
gyungchan Jo <namejgc@naver.com>
2024-02-19 02:37:53 +0900

    test(Round): 라운드를 줄이는 기능 테스트

--- src/test/java/racingcar/domain/RoundTest.java
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -3,13 +3,25 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RoundTest {
 
     @Test
     @DisplayName("음수인 경우 예외가 발생한다.")
     void invalidRoundTest() {
-        assertThrows(IllegalArgumentException.class, () -> new Round(-1));
+        assertThatThrownBy(() -> new Round(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("라운드를 줄일 수 있다.")
+    void decrease() {
+        Round round = new Round(1);
+        assertThat(round.isLast()).isFalse();
+
+        round.decrease();
+        assertThat(round.isLast()).isTrue();
     }
 }
