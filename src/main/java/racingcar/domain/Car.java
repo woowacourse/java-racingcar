b7904f3fc1da80b74d9795869192e557d1c16a02
eun-byeol <joeunbyeol98@gmail.com>
2024-02-18 12:12:10 +0900

    refactor(Car): validateNumberLength 메서드 내 매직넘버 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int MAX_LENGTH_OF_NAME = 5;
     private static final int REQUIRED_NUMBER_OF_POWER = 4;
     private static final String INVALID_CAR_NAME_LENGTH = "이름은 1자 이상 5자 이내로 입력해 주세요.";
 
@@ -14,7 +15,7 @@ public class Car {
     }
 
     private void validateNameLength(String name) {
-        if (name.length() > 5 || name.isEmpty()) {
+        if (name.length() > MAX_LENGTH_OF_NAME || name.isEmpty()) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
         }
     }
