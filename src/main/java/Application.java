aa38d8b676a6ea0fbe60ad0c8592be2d7ed758d8
JiHyeonL <lejinhy@gmail.com>
2024-02-18 17:12:49 +0900

    refactor: Application에서 Controller 객체 사용하지 않기 때문에 new Controller로 축약

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        RacingCarController controller = new RacingCarController();
-        controller.run();
+        new RacingCarController().run();
     }
 }
