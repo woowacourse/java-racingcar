daf2868f6665031b19c9b4d8457b390077286638
MingyeomKim <mingyum119@naver.com>
2024-02-18 21:36:22 +0900

    test(CarTest): 자동차의 position을 비교하는 메서드 테스트

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,5 +1,8 @@
 package domain;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -25,6 +28,26 @@ class CarTest {
             int power = 1;
             car.move(power);
             Assertions.assertThat(car.getPosition()).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("자동차의 position 비교 테스트")
+    @Nested
+    class GetNameIfSamePositionTest {
+        @DisplayName("position이 일치하지 않아 Optional.empty()를 반환한다.")
+        @Test
+        void isNotEqualPositionTest() {
+            Car car = new Car(new CarName("toby"));
+            Optional<String> result = car.getNameIfSamePosition(3);
+            assertTrue(result.isEmpty());
+        }
+
+        @DisplayName("position이 일치하여 자동차의 이름을 반환한다.")
+        @Test
+        void isEqualPositionTest() {
+            Car car = new Car(new CarName("toby"));
+            Optional<String> result = car.getNameIfSamePosition(0);
+            assertTrue(result.isPresent());
         }
     }
 }
