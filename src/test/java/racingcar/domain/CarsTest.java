b7032e1cca376dadabac0b2b01ac2ff88eb765cd
KimSangHyeok <tkdgur0906@naver.com>
2024-02-19 01:00:03 +0900

    test : 자동차들의 최대 길이 계산 및 우승자 찾는 테스트 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    private static Cars cars;
+
+    @BeforeAll
+    static void init() {
+        Car car1 = new Car("car1");
+        car1.moveCar();
+        Car car2 = new Car("car2");
+        car2.moveCar();
+        car2.moveCar();
+        Car car3 = new Car("car3");
+        car3.moveCar();
+        car3.moveCar();
+        car3.moveCar();
+        cars = new Cars(List.of(car1, car2, car3));
+    }
+
+    @DisplayName("여러대의_자동차중_최대_길이_계산")
+    @Test
+    void calculateLongestDistance() {
+        assertThat(cars.calculateLongestDistance()).isEqualTo(3);
+    }
+
+    @DisplayName("여러대의_자동차중_우승자_찾기")
+    @Test
+    void findWinners() {
+        assertThat(cars.findWinners(3).get(0)).isEqualTo("car3");
+    }
+
+
+}
