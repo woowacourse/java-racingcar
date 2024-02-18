6d80218385966757ae517af41b9c30deae1f5ccc
eunjungL <dldmswjd322@gmail.com>
2024-02-18 16:35:54 +0900

    refactor: Cars의 불필요한 validation 묶음 제거 및 checkForward->checkCanDrive로 더 명시적이게 변경

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,22 +3,17 @@ package domain;
 import util.Constants;
 import util.Exceptions;
 
-import static util.Constants.MIN_FORWARD_NUMBER;
-
 public class Car {
+
     private final String name;
     private int forward;
 
     public Car(String name) {
-        validate(name);
+        validateCarNameLength(name);
+        validateNameFormat(name);
 
         this.name = name;
         this.forward = 0;
-    }
-
-    private void validate(String name){
-        validateCarNameLength(name);
-        validateNameFormat(name);
     }
 
     private void validateCarNameLength(String name) {
@@ -28,19 +23,19 @@ public class Car {
     }
 
     private void validateNameFormat(String name) {
-        if(name.contains(" ")) {
+        if (name.contains(" ")) {
             throw new IllegalArgumentException(Exceptions.NAME_FORMAT_EXCEPTION.getMessage());
         }
     }
 
     public void drive(int randomNumber) {
-        if (checkForward(randomNumber)) {
+        if (checkCanDrive(randomNumber)) {
             forward += 1;
         }
     }
 
-    private boolean checkForward(int randomNumber) {
-        return randomNumber >= MIN_FORWARD_NUMBER;
+    private boolean checkCanDrive(int randomNumber) {
+        return randomNumber >= Constants.MIN_FORWARD_NUMBER;
     }
 
     public String getName() {
