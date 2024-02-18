be49fa9e34c81ba0df60c456f3944b5c5cab1b73
nhlee98 <e5slnh9876@gmail.com>
2024-02-18 15:07:39 +0900

    test : 시도 횟수에 대한 테스트 케이스 추가

--- src/test/java/domain/AttemptCountTest.java
+++ src/test/java/domain/AttemptCountTest.java
@@ -4,18 +4,22 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttemptCountTest {
 
     @DisplayName("시도 횟수가 1보다 작다면 예외가 발생한다.")
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2, -3})
     void attemptCountExceptionTest() {
         assertThatThrownBy(() -> new AttemptCount(0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("시도 횟수가 1이상이라면 예외가 발생하지 않는다.")
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
     void attemptCountSuccessTest() {
         assertThatCode(() -> new AttemptCount(1))
                 .doesNotThrowAnyException();
