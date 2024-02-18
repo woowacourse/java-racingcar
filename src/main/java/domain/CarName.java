97eafb98615cbd0b78791c20401b431a534121dc
이상진 <pricelees@gmail.com>
2024-02-18 14:58:41 +0900

    style(CarName) : 최대 이름 길이, 예외 메시지 상수화

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -1,6 +1,13 @@
 package domain;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import constants.ErrorMessages;
+
 public class CarName {
+	private static final int MAX_NAME_LENGTH = 5;
+
 	private final String name;
 
 	public CarName(String name) {
@@ -10,12 +17,12 @@ public class CarName {
 
 	private void validate(String name) {
 		if (isInvalidNameLength(name)) {
-			throw new IllegalArgumentException("[ERROR] 이름 길이는 1 이상 5 이하여야 합니다.");
+			throw new IllegalArgumentException(ErrorMessages.INVALID_CAR_NAME.getMessage());
 		}
 	}
 
 	private static boolean isInvalidNameLength(String name) {
-		return name == null || name.isBlank() || name.length() > 5;
+		return name == null || name.isBlank() || name.length() > MAX_NAME_LENGTH;
 	}
 
 	public String getName() {
