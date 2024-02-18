97ddb52e5d1219af9c9404a285cf970be3d8db19
unifolio0 <boho82ha@gmail.com>
2024-02-18 13:58:31 +0900

    refactor : static메소드로만 구성되어 있어 객체 생성 제한

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private InputView() {}
+
     private static final Scanner sc = new Scanner(System.in);
     public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
