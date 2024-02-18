926e31d3b2d76d5df88548d5743d3fb1f2b72565
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-19 00:32:03 +0900

    refactor : Controller 에서 담당한 기능 Cars Domain 으로 분리
    - 경기 진행하는 performEachRace , performRace GameController -> Cars

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -9,9 +9,7 @@ import util.RandomNumberGenerator;
 import view.InputView;
 import view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class GameController {
     private final InputView inputView;
@@ -31,7 +29,7 @@ public class GameController {
 
         RaceCount raceCount = inputView.inputRaceCount();
 
-        performRace(cars.getValue(), raceCount.getValue());
+        performRace(cars, raceCount.getValue());
 
         announceWinners(cars);
     }
@@ -41,28 +39,13 @@ public class GameController {
         return Cars.from(names);
     }
 
-    private void performRace(final List<Car> cars, Integer raceCount) {
-        List<RaceProgress> raceProgresses = new ArrayList<>();
-        for (int i = 0; i < raceCount; i++) {
-            performEachRace(cars);
-            raceProgresses.add(RaceProgress.from(cars));
-        }
-
+    private void performRace(final Cars cars, Integer raceCount) {
+        List<RaceProgress> raceProgresses = cars.performRace(raceCount, randomNumberGenerator);
         outputView.printAllRaceProgress(raceProgresses);
-    }
-
-    private void performEachRace(final List<Car> cars) {
-        cars.forEach(car -> {
-            int power = createPower();
-            car.race(power);
-        });
     }
 
     private void announceWinners(Cars cars) {
         outputView.printRaceResult(cars);
     }
 
-    private int createPower() {
-        return randomNumberGenerator.generate();
-    }
 }
