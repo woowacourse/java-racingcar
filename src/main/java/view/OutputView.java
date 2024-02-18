463fa18271dbf4e3a8016984c6db56ee6fd312e8
이상진 <pricelees@gmail.com>
2024-02-18 13:34:47 +0900

    feat(OutputView): 출력과 관련된 기능 분리

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.util.stream.Collectors;
+
+import domain.Car;
+import domain.Cars;
+
+public class OutputView {
+
+	public void printResultPrefix() {
+		System.out.println();
+		System.out.println("실행 결과");
+	}
+
+
+	public void printCurrentRace(Cars cars) {
+		cars.getCarList().forEach(car -> {
+			System.out.printf("%s : %s\n", car.getCarName(), "-".repeat(car.getCarLocation()));
+		});
+		System.out.println();
+	}
+
+	public void printWinners(Cars cars) {
+		String winners = cars.getCarList().stream()
+			.filter(car -> car.getCarLocation() == cars.getMaxLocation())
+			.map(Car::getCarName)
+			.collect(Collectors.joining(", ")) + "가 최종 우승했습니다.";
+		;
+		System.out.println(winners);
+	}
+}
