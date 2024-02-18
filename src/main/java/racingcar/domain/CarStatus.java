9b0746af206c45e8df188db00feccaadd99f6658
Eugene Jang <kyunellroll@gmail.com>
2024-02-18 21:11:38 +0900

    refactor(Circuit.java): CarStatus가 스스로 움직이도록 수정
    - CarStatus가 스스로 움직이도록 변경

--- src/main/java/racingcar/domain/CarStatus.java
+++ src/main/java/racingcar/domain/CarStatus.java
@@ -1,4 +1,13 @@
 package racingcar.domain;
 
-public record CarStatus(String name, int position) {
+public record CarStatus(String name, int position) implements Comparable<CarStatus> {
+
+    public boolean isPositionAt(int targetPosition) {
+        return position == targetPosition;
+    }
+
+    @Override
+    public int compareTo(CarStatus status) {
+        return position - status.position;
+    }
 }
