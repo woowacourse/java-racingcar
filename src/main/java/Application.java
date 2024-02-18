d9a47981d8f77e2a5c01d4c2db2b8c2cf2e529d0
3juhwan <13selfesteem91@naver.com>
2024-02-18 16:56:17 +0900

    Fix: controller에 도메인 조립

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,54 @@
-import domain.RaceManager;
-import utils.NumberGenerator;
-import utils.RandomGenerator;
+import domain.Attempts;
+import domain.CarGroup;
+import domain.RacingGame;
+import utils.PowerGenerator;
+import utils.RandomPowerGenerator;
 import view.InputView;
+import view.OutputView;
+
+import java.io.IOException;
+
+import static java.util.stream.IntStream.range;
 
 public class Application {
     private static final InputView inputView = new InputView(System.in);
-    private static final NumberGenerator numberGenerator = new RandomGenerator();
+    private static final PowerGenerator powerGenerator = new RandomPowerGenerator();
 
     public static void main(String[] args) {
-        RaceManager raceManager = new RaceManager(inputView, numberGenerator);
-        raceManager.run();
+        final CarGroup carGroup = repeatUntilGetValidCarNames();
+        final Attempts attempts = repeatUntilGetValidNumberOfAttempts();
+
+        OutputView.printRaceResultHeader();
+        race(carGroup, attempts);
+        OutputView.printResult(carGroup);
+    }
+
+    private static void race(CarGroup carGroup, Attempts attempts) {
+        final RacingGame racingGame = new RacingGame(carGroup, powerGenerator);
+        int numberOfAttempts = attempts.numberOfAttempts();
+
+        range(0, numberOfAttempts).forEach(i -> {
+            racingGame.moveCars();
+            OutputView.printCarsPosition(carGroup);
+        });
+    }
+
+    private static CarGroup repeatUntilGetValidCarNames() {
+        try {
+            String[] carNames = inputView.readCarNames();
+            return new CarGroup(carNames);
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return repeatUntilGetValidCarNames();
+        }
+    }
+
+    private static Attempts repeatUntilGetValidNumberOfAttempts() {
+        try {
+            Integer numberOfAttempts = inputView.readNumberOfAttempts();
+            return new Attempts(numberOfAttempts);
+        } catch (IOException | IllegalArgumentException e) {
+            return repeatUntilGetValidNumberOfAttempts();
+        }
     }
 }
