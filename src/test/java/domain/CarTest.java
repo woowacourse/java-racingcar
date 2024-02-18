510945e07448b60209c589f79458b6572a1eb13f
SungKyum Kim <sungkyum1@naver.com>
2024-02-19 00:36:07 +0900

    test: Car move 리팩토링에 맞추어 테스트 코드 수정

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,43 +1,22 @@
 package domain;
 
 
-import static org.mockito.Mockito.mockStatic;
-
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import utils.RandomNumberGenerator;
-import static org.mockito.BDDMockito.given;
 
 
 class CarTest {
-    private static MockedStatic<RandomNumberGenerator> generator;
-
-    @BeforeAll
-    public static void setUp() {
-        generator = mockStatic(RandomNumberGenerator.class);
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        generator.close();
-    }
-
     @Test
     void carMoveTest() {
-        given(RandomNumberGenerator.generate()).willReturn(5);
         Car car = new Car(new CarName("toby"));
-        car.move();
+        car.move(4);
         Assertions.assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
     void carDoesNotMoveTest() {
-        given(RandomNumberGenerator.generate()).willReturn(2);
         Car car = new Car(new CarName("toby"));
-        car.move();
+        car.move(3);
         Assertions.assertThat(car.getPosition()).isEqualTo(0);
     }
 }
