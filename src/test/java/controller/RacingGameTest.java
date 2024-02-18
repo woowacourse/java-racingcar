ef7a7041ac73265eeb1b076090a44d3a9242ae0e
이상진 <pricelees@gmail.com>
2024-02-18 17:17:31 +0900

    test(RacingGame): 메서드의 변경 및 추가를 반영하여 테스트 코드 수정

--- src/test/java/controller/RacingGameTest.java
+++ src/test/java/controller/RacingGameTest.java
@@ -1,109 +1,105 @@
 package controller;
 
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.time.Duration;
-import java.util.List;
 import java.util.NoSuchElementException;
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import controller.RacingGame;
+import domain.strategy.MovingStrategy;
 import view.InputView;
 import view.OutputView;
 
 class RacingGameTest {
-    private PrintStream standardOut;
-    private OutputStream captor;
+	private PrintStream standardOut;
+	private OutputStream captor;
 
-    @BeforeEach
-    void setUp() {
-        standardOut = System.out;
-        captor = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(captor));
-    }
+	@BeforeEach
+	void setUp() {
+		standardOut = System.out;
+		captor = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(captor));
+	}
 
-    @AfterEach
-    void tearDown() {
-        System.setOut(standardOut);
-        System.out.println(captor.toString().trim());
-    }
+	@AfterEach
+	void tearDown() {
+		System.setOut(standardOut);
+		System.out.println(captor.toString().trim());
+	}
 
+	@DisplayName("게임 정상 진행 확인")
+	@Test
+	void run() {
+		command("a,b", "3");
+		start();
 
-    @Test
-    void run() {
-        /*
-         * 차량 3대를 3번 전진시키는데, 모든 차량을 3번 다 전진시키도록 설정
-         */
-        command("a,b,c", "3");
-        RacingGame racingGame = new RacingGame(new InputView(), new OutputView()) {
-            @Override
-            protected List<Integer> createRandomNumber(int carCount) {
-                return List.of(4, 5, 6);
-            }
-        };
+		assertThat(captor.toString()).contains(
+			"실행 결과",
+			"a : -",
+			"b : -",
+			"a : --",
+			"b : --",
+			"a : ---",
+			"b : ---",
+			"a, b가 최종 우승했습니다."
+		);
+	}
 
-        racingGame.run();
+	@DisplayName("차량 이름에 대한 예외 발생시 반복 입력 확인")
+	@Test
+	void run_WithInvalidLocation() {
+		command(
+			"", // 빈 입력
+			"abcdef,g,h", // 글자수 초과
+			"a,,b" // 이름에 쉼표가 들어가는 경우
+		);
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::start);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
 
-        Assertions.assertThat(captor.toString()).contains(
-                "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).",
-                "시도할 회수는 몇회인가요?",
-                "실행 결과",
-                "a : -",
-                "b : -",
-                "c : -",
-                "a : --",
-                "b : --",
-                "c : --",
-                "a : ---",
-                "b : ---",
-                "c : ---",
-                "a, b, c가 최종 우승했습니다."
-        );
-    }
+	@DisplayName("전진 횟수에 대한 예외 발생시 반복 입력 확인")
+	@Test
+	void run_WithInvalidTrialCount() {
+		command(
+			"", // 빈 입력
+			"abc", // 문자 입력
+			"-1" // 음수 입력
+		);
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::start);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
 
-    @DisplayName("차량 이름에 대한 예외 발생시 반복 입력 확인")
-    @Test
-    void run_WithInvalidLocation() {
-        command(
-                "", // 빈 입력
-                "abcdef,g,h", // 글자수 초과
-                "a,,b" // 이름에 쉼표가 들어가는 경우
-        );
-        try {
-            assertTimeoutPreemptively(Duration.ofSeconds(1L), this::start);
-        } catch (NoSuchElementException ignored) {
-        }
-    }
+	private void start() {
+		InputView inputView = new InputView();
+		OutputView outputView = new OutputView();
 
-    @DisplayName("전진 횟수에 대한 예외 발생시 반복 입력 확인")
-    @Test
-    void run_WithInvalidTrialCount() {
-        command(
-                "", // 빈 입력
-                "abc", // 문자 입력
-                "-1" // 음수 입력
-        );
-        try {
-            assertTimeoutPreemptively(Duration.ofSeconds(1L), this::start);
-        } catch (NoSuchElementException ignored) {
-        }
-    }
+		// 차량이 항상 이동하도록 설정
+		MovingStrategy movingStrategy = new MovingStrategy() {
+			@Override
+			public boolean canMove() {
+				return true;
+			}
+		};
+		RacingGame racingGame = new RacingGame(inputView, outputView, movingStrategy);
 
-    private void start() {
-        RacingGame racingGame = new RacingGame(new InputView(), new OutputView() `);
-        racingGame.run();
-    }
+		racingGame.run();
+	}
 
-    private void command(final String... args) {
-        final byte[] buf = String.join("\n", args).getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
-    }
+	private void command(final String... args) {
+		final byte[] buf = String.join("\n", args).getBytes();
+		System.setIn(new ByteArrayInputStream(buf));
+	}
 }
