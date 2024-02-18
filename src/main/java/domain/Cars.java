991a49a515eb1675bd59e7a373bba29883c2ed59
hangillee <skfcb10@naver.com>
2024-02-18 22:39:54 +0900

    refactor: NumberGenerator에 대한 의존성 주입 적용

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -5,13 +5,15 @@ import java.util.List;
 public class Cars {
 
     private final List<Car> racingCars;
+    private final NumberGenerator generator;
 
-    public Cars(final List<Car> racingCars) {
+    public Cars(final List<Car> racingCars, final NumberGenerator generator) {
         this.racingCars = racingCars;
+        this.generator = generator;
     }
 
     public List<Car> startRounds() {
-        racingCars.forEach(car -> car.moveForward(NumberGenerator.generateRandomNumber()));
+        racingCars.forEach(car -> car.moveForward(generator.generateNumber()));
         return racingCars;
     }
 
