24e23b9875759a207b363f6332a4c13eb74daa0f
SungKyum Kim <sungkyum1@naver.com>
2024-02-19 01:40:12 +0900

    test: 경주 당 자동차 상태 반환 기능 테스트 구현

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,9 +1,11 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarsTest {
     @Test
@@ -12,7 +14,16 @@ class CarsTest {
                 "toby", "tebah", "anna", "brown", "brown"
         );
         assertThrows(IllegalArgumentException.class, () ->
-            Cars.from(carNames)
+                Cars.from(carNames)
         );
+    }
+
+    @Test
+    void carsRaceReturnTest() {
+        List<String> carNames = List.of(
+                "toby", "tebah", "anna", "brown", "bro"
+        );
+        Cars cars = Cars.from(carNames);
+        assertEquals(cars.race().carStates().size(), 5);
     }
 }
