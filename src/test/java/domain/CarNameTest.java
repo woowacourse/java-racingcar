157222b5928b8399b1a926207b7738c147b11578
MingyeomKim <mingyum119@naver.com>
2024-02-18 21:42:47 +0900

    test(CarNameTest): 자동차가 성공적으로 생성되는 테스트 코드 구현

--- src/test/java/domain/CarNameTest.java
+++ src/test/java/domain/CarNameTest.java
@@ -1,14 +1,22 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarNameTest {
+    @DisplayName("자동차의 이름이 5자 이하여서 자동차가 성공적으로 생성된다.")
+    @Test
+    void carNameSuccessTest() {
+        final String testCarName = "test";
+        assertDoesNotThrow(() -> new CarName(testCarName));
+    }
+
+    @DisplayName("자동차의 이름이 5자 초과되어 자동차의 생성이 실패한다.")
     @Test
     void carNameFailTest() {
         final String testCarName = "포포포포포비";
         assertThrows(IllegalArgumentException.class, () -> new CarName(testCarName));
     }
-
 }
