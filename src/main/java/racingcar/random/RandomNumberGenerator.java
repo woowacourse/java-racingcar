5263079a67d0ba6a0e3581ceb44671cf0a91ebaa
Eugene Jang <kyunellroll@gmail.com>
2024-02-18 21:58:52 +0900

    refactor(NumberGenerator.java): RandomNumber을 만드는 책임을 Engine에서 제외
    - Random으로 돌리는 책임을 Engine에서 제외
    - 그에 따른 test 변경

--- src/main/java/racingcar/random/RandomNumberGenerator.java
+++ src/main/java/racingcar/random/RandomNumberGenerator.java
@@ -6,16 +6,11 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomNumberGenerator implements NumberGenerator {
 
     private static final Random random = ThreadLocalRandom.current();
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
 
     @Override
-    public int generateNumberInRange(final int startInclusive, final int endInclusive) {
-        validateRange(startInclusive, endInclusive);
-        return random.nextInt(startInclusive, endInclusive + 1);
-    }
-
-    private void validateRange(int startInclusive, int endInclusive) {
-        if (startInclusive > endInclusive) {
-            throw new IllegalArgumentException("startInclusive should be less than or equal to endInclusive");
-        }
+    public int generateNumberInRange() {
+        return random.nextInt(MIN_RANGE, MAX_RANGE + 1);
     }
 }
