b22593f32309fed655471bf74e4395708a859701
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-19 00:10:39 +0900

    test: 불필요 예외 검증 테스트 제거
    `Game`에서 예외 처리 로직을 삭제했기에 예외 검증 테스트 제거

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -2,18 +2,14 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("게임 생성에서")
+@DisplayName("게임")
 class GameTest {
     @Test
-    @DisplayName("시도할 횟수를 변환하여 게임을 생성한다")
+    @DisplayName("생성한다")
     public void createGameTest() {
         //given
         String userCarNames = "choco,seyan";
@@ -23,21 +19,8 @@ class GameTest {
         assertDoesNotThrow(() -> new Game(TryCount.from(userTryCount), Cars.from(userCarNames)));
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"0", "-1", "1.5", "", " ", "1 2"})
-    @NullSource
-    @DisplayName("시도할 횟수가 자연수가 아닐 경우 예외가 발생한다")
-    public void nonNaturalNumberTest(String userTryCount) {
-        //given
-        String userCarNames = "choco,seyan";
-
-        //when & then
-        assertThrows(NumberFormatException.class,
-                () -> new Game(TryCount.from(userTryCount), Cars.from(userCarNames)));
-    }
-
     @Test
-    @DisplayName("게임의 진행은 오류 없이 진행된다.")
+    @DisplayName("진행은 오류 없이 진행된다.")
     public void runProceed() {
         //given
         String userCarNames = "choco,seyan";
