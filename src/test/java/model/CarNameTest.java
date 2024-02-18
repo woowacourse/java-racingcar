443cb5b3631b5b6f7456ff07c175e7458acf5a86
yoonjuho <dbswn990@naver.com>
2024-02-18 17:40:44 +0900

    refactor(CarName): 클래스 분리

---
+++ src/test/java/model/CarNameTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarNameTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이름이 없을 시 예외가 발생한다.")
+    void validateNullAndEmpty(String input) {
+        assertThatThrownBy(() -> new CarName(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름이 6자 이상일 시 예외가 발생한다.")
+    void validateNameLength() {
+        String name = "abcdefg";
+        assertThatThrownBy(() -> new CarName(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
