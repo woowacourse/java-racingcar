0926f06945452c208c629a515c74da38b8f73854
3juhwan <13selfesteem91@naver.com>
2024-02-18 14:58:12 +0900

    Refactor: Car 클래스 final 예약어 추가, 적절한 변수명으로 수정

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -4,31 +4,35 @@ import java.util.Objects;
 
 public class Car {
     private final String name;
-    private int position;
+    private int position = 0;
 
-    public Car(String name) {
-        validate(name);
-        position = 0;
+    public Car(final String name) {
+        validateName(name);
         this.name = name;
     }
 
-    private void validate(String name) {
+    Car(final String name, final int position) {
+        this(name);
+        this.position = position;
+    }
+
+    private void validateName(final String name) {
         if (isNameLengthOutOfRange(name)) {
             throw new IllegalArgumentException("1이상 5이하의 이름을 입력하세요.");
         }
     }
 
-    private boolean isNameLengthOutOfRange(String name) {
+    private boolean isNameLengthOutOfRange(final String name) {
         return name.length() < 1 || name.length() > 5;
     }
 
-    public void move(int number) {
-        if (number >= 4) {
+    public void move(final int power) {
+        if (power >= 4) {
             position++;
         }
     }
 
-    public boolean isSamePosition(int position) {
+    public boolean isPositionedAt(final int position) {
         return this.position == position;
     }
 
