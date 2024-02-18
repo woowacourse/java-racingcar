d94c155b4b6f4e5dd89ff1bb93cd9f51eaf1d9ad
이상진 <pricelees@gmail.com>
2024-02-18 17:43:44 +0900

    refactor(Randoms) : 미사용 메서드 삭제

--- src/main/java/utils/Randoms.java
+++ src/main/java/utils/Randoms.java
@@ -1,27 +1,11 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms {
 	private static final Random random = new Random();
 
 	private Randoms() {
-	}
-
-	/**
-	 * @param count        뽑을 숫자 개수
-	 * @param endInclusive 숫자 범위
-	 * @return 0부터 endInclusive까지의 count만큼의 랜덤 숫자 리스트
-	 */
-	public static List<Integer> getRandomNumbers(int count, int endInclusive) {
-		List<Integer> randomNumbers = new ArrayList<>();
-		for (int i = 0; i < count; i++) {
-			randomNumbers.add(getRandomNumber(endInclusive));
-		}
-
-		return randomNumbers;
 	}
 
 	/**
