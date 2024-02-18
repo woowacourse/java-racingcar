359855f6f8c8e6120761fff9c3c30d51b60fc9d2
mina <qkralsdk991@naver.com>
2024-02-18 19:52:12 +0900

    refactor: 입력 부분 controller->view단으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,40 @@
 package view;
 
-public class InputView {
+import domain.Car;
+import domain.Game;
 
-    public static void requestCarName () {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static List<Car> requestCarName (Game game) {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return setCarName(game);
     }
 
-    public static void requestAttemptLimit() {
+    public static int requestAttemptLimit(Game game) {
         System.out.println("시도할 회수는 몇회인가요?");
+        return setInputAttemptLimit(game);
+    }
+
+    private static List<Car> setCarName(Game game) {
+        try {
+            return game.setCars(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return requestCarName(game);
+        }
+    }
+
+    private static int setInputAttemptLimit(Game game) {
+        try {
+            return game.setAttemptLimit(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return requestAttemptLimit(game);
+        }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,8 +3,6 @@ package view;
 import domain.Car;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class OutputView {
     public static void printErrorMessage(String errorMessage) {
