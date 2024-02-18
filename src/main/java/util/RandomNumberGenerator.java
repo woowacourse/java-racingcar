27c49c4140a8a375bcdea82e8187992513c38a36
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-19 00:09:41 +0900

    refactor : RandomNumberGenerator 가 NumberGenerator 구현하게 분리
    - 테스트에 용이하게 하기 위해 NumberGenerator interface 사용

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package util;
 
 import java.util.Random;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator{
     private static final int RANDOM_NUMBER_UPPER_BOUND = 10;
 
     public int generate() {
