ec3430f5888aa26b2b4a428ad27287fbffb33744
이상진 <pricelees@gmail.com>
2024-02-18 17:13:45 +0900

    refactor(ErrorMessages): 랜덤 숫자에 대한 예외 메시지 추가

--- src/main/java/constants/ErrorMessages.java
+++ src/main/java/constants/ErrorMessages.java
@@ -3,7 +3,8 @@ package constants;
 public enum ErrorMessages {
 	INVALID_INPUT("입력 형식이 올바르지 않습니다."),
 	INVALID_CAR_NAME("이름의 길이는 1~5글자여야 합니다."),
-	INVALID_CAR_LOCATION("위치값은 음수가 될 수 없습니다");
+	INVALID_CAR_LOCATION("위치값은 음수가 될 수 없습니다"),
+	INVALID_RANDOM_NUMBER("랜덤 숫자는 9를 초과할 수 없습니다.");
 
 	private static final String prefix = "[ERROR]";
 
