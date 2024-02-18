06d6c35508da94fbdec78f6670caf828a66f0647
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-18 23:48:15 +0900

    test: CarNameTest에 0글자이거나 null에 대한 예외 발생 확인 추가

--- src/test/java/CarNameTest.java
+++ src/test/java/CarNameTest.java
@@ -4,13 +4,24 @@ import model.CarName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class CarNameTest {
 
-    @DisplayName("유효하지 않은 이름 길이에 대한 예외 발생 확인")
+    @DisplayName("이름 길이 초과에 대한 예외 발생 확인")
     @ParameterizedTest(name = "{0}")
     @CsvSource(value = {"빈 입력 :  ", "5글자 초과 : abcdef"}, delimiter = ':')
-    void constructor(String caseName, String carName) {
+    void constructorOver(String caseName, String carName) {
+        assertThatThrownBy(() -> new CarName(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+
+    @DisplayName("0글자이거나 null 대한 예외 발생 확인")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void constructorNull(String carName) {
         assertThatThrownBy(() -> new CarName(carName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
