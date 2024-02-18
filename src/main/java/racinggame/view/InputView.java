cd8d9455d28b47dc6039ea787eed27aca77fd9cc
le2sky <leehaneul990623@gmail.com>
2024-02-18 14:26:00 +0900

    refactor : 변수 선언 인라인화

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -12,9 +12,8 @@ public class InputView {
 
     public List<Car> readCars() throws IOException {
         String inputNames = readLine("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        List<String> carNames = Arrays.asList(inputNames.split(","));
 
-        return carNames.stream()
+        return Arrays.stream(inputNames.split(","))
             .map(Car::new)
             .toList();
     }
