82684f4a6c707fb6a3f2aa6279cf536406483c32
JiHyeonL <lejinhy@gmail.com>
2024-02-18 22:39:56 +0900

    test: 사용한하는 import 삭제

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -3,7 +3,6 @@ package model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
