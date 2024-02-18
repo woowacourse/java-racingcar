32cb68b8d8b354ddf72d96c21ccfaf2392ecee65
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-18 20:48:06 +0900

    refactor(Main): 컨트롤러와 view를 분리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,20 @@ import controller.RacingCarController;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         try (InputView inputView = new InputView(); OutputView outputView = new OutputView()) {
-            new RacingCarController(inputView, outputView).run();
+            List<String> carsName = inputView.inputCarsName();
+            int trialCount = inputView.inputTrialCount();
+
+            RacingCarController racingCarController = new RacingCarController(carsName, trialCount);
+            racingCarController.run();
+
+            outputView.printResultComment();
+            outputView.printAllRacingResult(racingCarController.getRacingCars(), trialCount);
+            outputView.printWinner(racingCarController.findWinners());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
