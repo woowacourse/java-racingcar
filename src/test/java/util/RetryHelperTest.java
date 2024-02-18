09fcc80080def922fdb4f29c7c194dd5a898c482
robinjoon <robin980108@naver.com>
2024-02-18 11:30:59 +0900

    test: RetryHelperTest에 DisplayName 추가

--- src/test/java/util/RetryHelperTest.java
+++ src/test/java/util/RetryHelperTest.java
@@ -1,11 +1,13 @@
 package util;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RetryHelperTest {
 
   @Test
+  @DisplayName("재시도 횟수가 최대값을 넘지 않는 경우")
   void retry() {
     final int[] retryCount = {0};
     RetryHelper retryHelper = new RetryHelper(2);
@@ -19,6 +21,7 @@ class RetryHelperTest {
   }
 
   @Test
+  @DisplayName("재시도 횟수가 최댓값을 넘는 경우 예외 발생")
   void retryTimeOver() {
     final int[] retryCount = {0};
     RetryHelper retryHelper = new RetryHelper(2);
