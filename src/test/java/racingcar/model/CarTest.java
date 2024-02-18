e36266aee096fb1ebb07e0f01e35cb9ff0998a79
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-18 23:23:59 +0900

    fix(carTest): car의 이름은 영어만 가능하기에 이름 수정
    - 생성된 난수가 이동 기준을 만족하지 않는 경우, 난수룰 경계값으로 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -5,11 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CarTest {
+class CarTest {
     @Test
     @DisplayName("생성된 난수가 이동 기준을 만족할 때 위치가 1 증가한다.")
     void moveTest() {
-        final Car car = new Car("페드로");
+        final Car car = new Car("pedro");
         final int randomNumber = 4;
 
         car.move(randomNumber);
@@ -20,8 +20,8 @@ public class CarTest {
     @Test
     @DisplayName("생성된 난수가 이동 기준을 만족하지 않을 때 위치는 변하지 않는다.")
     void notMoveTest() {
-        final Car car = new Car("몰리");
-        final int randomNumber = 2;
+        final Car car = new Car("moly");
+        final int randomNumber = 3;
 
         car.move(randomNumber);
 
