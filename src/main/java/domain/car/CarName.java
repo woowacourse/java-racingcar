c77d6444ef6982a8730712acf6df57d44d3dfec5
nhlee98 <e5slnh9876@gmail.com>
2024-02-18 18:51:00 +0900

    refactor : 앞뒤 공백 검증을 strip 메소드로 검증하도록 변경

--- src/main/java/domain/car/CarName.java
+++ src/main/java/domain/car/CarName.java
@@ -13,8 +13,7 @@ public class CarName {
     private void validate(String name) {
         validateLength(name);
         validateBlank(name);
-        validateStartsWithBlank(name);
-        validateEndsWithBlank(name);
+        validateStartsOrEndsWithBlank(name);
     }
 
     private void validateLength(String name) {
@@ -29,15 +28,9 @@ public class CarName {
         }
     }
 
-    private void validateStartsWithBlank(String name) {
-        if (name.startsWith(" ")) {
-            throw new IllegalArgumentException("자동차 이름은 공백으로 시작할 수 없습니다.");
-        }
-    }
-
-    private void validateEndsWithBlank(String name) {
-        if (name.endsWith(" ")) {
-            throw new IllegalArgumentException("자동차 이름은 공백으로 끝날 수 없습니다.");
+    private void validateStartsOrEndsWithBlank(String name) {
+        if (!name.strip().equals(name)) {
+            throw new IllegalArgumentException("자동차 이름은 공백으로 시작하거나 끝날 수 없습니다.");
         }
     }
 
