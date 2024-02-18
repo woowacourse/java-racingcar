af9ae112f7c1758e67f8a23298d5ceb08cfd19fc
gyungchan Jo <namejgc@naver.com>
2024-02-18 13:27:45 +0900

    refactor: 이름에 대한 검증 기능 수정
    이름에 대한 검증을 Car 객체 안에서 수행하도록 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,14 +1,23 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MAXIMUM_LENGTH_OF_NAME = 5;
     private static final int MINIMUM_NUMBER_TO_MOVE = 4;
 
-    private final Name name;
+    private final String name;
     private final Position position;
 
     public Car(String name) {
-        this.name = new Name(name);
+        validateName(name);
+        this.name = name;
         this.position = new Position();
+    }
+
+    private void validateName(final String name) {
+        if (name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException(
+                    String.format("자동차의 이름은 공백이거나 %d자를 초과할 수 없습니다.", MAXIMUM_LENGTH_OF_NAME));
+        }
     }
 
     public void move(int number) {
@@ -22,7 +31,7 @@ public class Car {
     }
 
     public String getName() {
-        return name.getName();
+        return name;
     }
 
     public int getPosition() {
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,12 +1,32 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
+
+    @Test
+    @DisplayName("자동차는 이름을 가진다.")
+    void createCar() {
+        Car car = new Car("pobi");
+
+        assertThat(car).extracting("name")
+                .isEqualTo("pobi");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "banana"})
+    @DisplayName("자동차의 이름이 공백이거나 5글자를 초과하면 예외가 발생한다.")
+    void invalidName(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"4, 1", "3, 0"})
