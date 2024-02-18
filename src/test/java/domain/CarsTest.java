4ed904bfa61df737d979242ba31abc04b2a7e945
MingyeomKim <mingyum119@naver.com>
2024-02-18 21:49:29 +0900

    test(CarTest): 자동차들을 성공적으로 생성하는 테스트 코드 구현

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -3,16 +3,32 @@ package domain;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
-    @Test
-    void carsNameDuplicatedTest() {
-        List<String> carNames = List.of(
-                "toby", "tebah", "anna", "brown", "brown"
-        );
-        assertThrows(IllegalArgumentException.class, () ->
-            Cars.from(carNames)
-        );
+    @Nested
+    @DisplayName("자동차 생성 테스트")
+    class CarGenerateTest {
+        @DisplayName("자동차들이 성공적으로 생성된다.")
+        @Test
+        void success() {
+            List<String> carNames = List.of(
+                    "toby", "tebah", "anna"
+            );
+            assertDoesNotThrow(() -> Cars.from(carNames));
+        }
+
+        @DisplayName("자동차들의 이름이 중복되어 자동차 생성에 실패한다.")
+        @Test
+        void fail() {
+            List<String> carNames = List.of(
+                    "toby", "tebah", "anna", "brown", "brown"
+            );
+            assertThrows(IllegalArgumentException.class, () ->
+                    Cars.from(carNames)
+            );
+        }
     }
 }
