edd86a83341c58d18904ba495749353567ed133e
SungKyum Kim <sungkyum1@naver.com>
2024-02-19 00:35:22 +0900

    refactor: 자동차 전진 조건인 power를 외부에서 주입받도록 리팩터링

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,10 @@
 package domain;
 
 import java.util.Optional;
-import utils.RandomNumberGenerator;
 
 public class Car {
     private static final int MOVE_LOWER_BOUND = 4;
-    private CarName carName;
+    private final CarName carName;
     private int position;
 
     public Car(CarName carName) {
@@ -13,15 +12,14 @@ public class Car {
         this.position = 0;
     }
 
-    public void move() {
-        int randomNumber = RandomNumberGenerator.generate();
-        if (randomNumber >= MOVE_LOWER_BOUND) {
+    public void move(int power) {
+        if (power >= MOVE_LOWER_BOUND) {
             position++;
         }
     }
 
     public Optional<String> getNameIfMax(int max) {
-        if(position == max) {
+        if (position == max) {
             return Optional.of(carName.getName());
         }
         return Optional.empty();
