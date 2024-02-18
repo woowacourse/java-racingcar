67f056a749e4e5112b862b73b6b2abf1853941ba
nhlee98 <e5slnh9876@gmail.com>
2024-02-18 10:16:22 +0900

    refactor : 입력에 콤마가 포함되는지 여부를 InputView에서 검증하지 않도록 변경
    콤마 없이 하나의 이름만 입력하는 경우에 예외를 발생시킬지에 대한 로직은 비즈니스 로직이기 때문에 도메인 객체가 이를 검증하도록 변경한다.

--- src/main/java/util/SplitValidator.java
+++ src/main/java/util/SplitValidator.java
@@ -5,19 +5,12 @@ public class SplitValidator {
     private SplitValidator() {}
 
     public static void validate(String input, String delimiter) {
-        validateDelimiter(input, delimiter);
-        validateEndsWithDelimiter(input, delimiter);
+        validateStartsOrEndsWithDelimiter(input, delimiter);
     }
 
-    private static void validateDelimiter(String input, String delimiter) {
-        if (!input.contains(delimiter)) {
-            throw new IllegalArgumentException(String.format("입력은 구분자 %s로 구분하여 입력해야 합니다.", delimiter));
-        }
-    }
-
-    private static void validateEndsWithDelimiter(String input, String delimiter) {
-        if (input.endsWith(delimiter)) {
-            throw new IllegalArgumentException(String.format("입력은 구분자 %s로 끝날 수 없습니다.", delimiter));
+    private static void validateStartsOrEndsWithDelimiter(String input, String delimiter) {
+        if (input.startsWith(delimiter) || input.endsWith(delimiter)) {
+            throw new IllegalArgumentException(String.format("입력은 구분자 %s로 시작하거나 끝날 수 없습니다.", delimiter));
         }
     }
 }
