11af054ab9cb30d6f49e6a398149037b794d6fdf
ay-eonii <naayen@naver.com>
2024-02-18 23:43:23 +0900

    test(RaceCars): 랜덤 수 생성 정책 생성자에 추가

--- src/test/java/domain/RaceCarsTest.java
+++ src/test/java/domain/RaceCarsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +27,7 @@ class RaceCarsTest {
                     new Car("hi"),
                     new Car("hi")
             );
-            assertThatThrownBy(() -> new RaceCars(cars))
+            assertThatThrownBy(() -> new RaceCars(cars, new RandomNumberGenerator()))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
@@ -36,7 +37,7 @@ class RaceCarsTest {
             List<Car> cars = List.of(
                     new Car("hi")
             );
-            assertThatThrownBy(() -> new RaceCars(cars))
+            assertThatThrownBy(() -> new RaceCars(cars, new RandomNumberGenerator()))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
@@ -49,7 +50,7 @@ class RaceCarsTest {
         Car two = new Car("two");
         Car three = new Car("three");
         List<Car> cars = new ArrayList<>(List.of(one, two, three));
-        RaceCars raceCars = new RaceCars(cars);
+        RaceCars raceCars = new RaceCars(cars, new RandomNumberGenerator());
 
         one.move(4);
         two.move(0);
@@ -63,4 +64,47 @@ class RaceCarsTest {
         //then
         assertThat(winners).contains(one, three);
     }
+
+
+    @Nested
+    @DisplayName("경주 테스트")
+    class PlayTest {
+
+        @ParameterizedTest
+        @ValueSource(ints = {4, 9})
+        @DisplayName("랜덤값이 4이상이면 한 칸 전진한다.")
+        void playWithMove(int random) {
+            NumberGenerator numberGenerator = () -> random;
+
+            Car one = new Car("one");
+            Car two = new Car("two");
+            List<Car> cars = new ArrayList<>(List.of(one, two));
+            RaceCars raceCars = new RaceCars(cars, numberGenerator);
+
+            raceCars.play();
+
+            Car car = raceCars.getCar(0);
+
+            assertThat(car.getLocation(1)).isEqualTo(1);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, 3})
+        @DisplayName("랜덤값이 4미만이면 정지한다.")
+        void playWithStop(int random) {
+            NumberGenerator numberGenerator = () -> random;
+
+            Car one = new Car("one");
+            Car two = new Car("two");
+            List<Car> cars = new ArrayList<>(List.of(one, two));
+            RaceCars raceCars = new RaceCars(cars, numberGenerator);
+
+            raceCars.play();
+
+            Car car = raceCars.getCar(0);
+
+            assertThat(car.getLocation(1)).isEqualTo(0);
+        }
+    }
+
 }
