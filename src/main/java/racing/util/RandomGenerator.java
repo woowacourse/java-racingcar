958deb164930f8498b79927b8d67a4dd3eb4f882
ehtjsv2 <ehtjsv2@naver.com>
2024-02-19 17:27:44 +0900

    refactor: 10을 상수로 변경

--- src/main/java/racing/util/RandomGenerator.java
+++ src/main/java/racing/util/RandomGenerator.java
@@ -3,8 +3,10 @@ package racing.util;
 import java.util.Random;
 
 public class RandomGenerator {
+    private static final int END_BOUNDARY_NUMBER = 10;
+
     static public int generate() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(END_BOUNDARY_NUMBER);
     }
 }
