6f0865b1715ce0247ef16a7c420f1ca93a4f82a6
hyun <osh123chm@gmail.com>
2024-02-19 13:00:09 +0900

    refactor: power(랜덤숫자) 생성 로직 변경

--- src/main/java/model/powergenerator/RandomPowerGenerator.java
+++ src/main/java/model/powergenerator/RandomPowerGenerator.java
@@ -1,13 +1,10 @@
 package model.powergenerator;
 
-import java.util.Random;
-
 public class RandomPowerGenerator implements PowerGenerator {
     private static final int NUMBER_BOUND = 10;
 
     @Override
     public int generatePower() {
-        Random random = new Random();
-        return random.nextInt(NUMBER_BOUND);
+        return (int) (Math.random() * 10);
     }
 }
