b2ae1a174b1007136ad398dde51aedcfb2a80b1f
ehtjsv2 <ehtjsv2@naver.com>
2024-02-19 20:05:33 +0900

    test: 우승자 찾는 함수 테스트 추가

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -1,8 +1,12 @@
 package racing.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +23,31 @@ class CarsTest {
     void createCarsWithDuplicateName() {
         assertThatThrownBy(() -> new Cars("pobi, pobi"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("우승자를 찾습니다.")
+    void findWinner() {
+        Cars cars = new Cars("dodo, jery, coli");
+        List<Car> expectedWinner = List.of(new Car("dodo"));
+
+        cars.getCars().get(0).moveByNumber(5);
+        List<Car> actualWinners = cars.findWinners();
+
+        assertEquals(expectedWinner.get(0).getName(),actualWinners.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("공동 우승자를 찾습니다.")
+    void findWinners() {
+        Cars cars = new Cars("dodo, jery, coli");
+
+        Car dodo = cars.getCars().get(0);
+        Car coli = cars.getCars().get(2);
+        dodo.moveByNumber(5);
+        coli.moveByNumber(5);
+        List<Car> actualWinners = cars.findWinners();
+
+        assertThat(actualWinners).containsExactly(dodo,coli);
     }
 }
