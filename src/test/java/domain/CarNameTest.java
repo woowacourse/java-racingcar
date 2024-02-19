7a8b2d181fa3089b8aee81c8010464db2cf7e994
SCY <helenason@naver.com>
2024-02-19 21:51:56 +0900

    feat: 자동차 이름의 길이가 0인 경우 예외 처리 추가

--- src/test/java/domain/CarNameTest.java
+++ src/test/java/domain/CarNameTest.java
@@ -11,15 +11,15 @@ public class CarNameTest {
 
     @DisplayName("자동차 이름이 5자가 넘지 않을 경우 정상적으로 도메인이 생성된다.")
     @ParameterizedTest
-    @ValueSource(strings = {"에", "에버", "에버에", "에버에버", "에버에버1"})
+    @ValueSource(strings = {"1", "4444", "55555"})
     void createByValidNameLength(String name) {
         assertThatCode(() -> CarName.from(name))
                 .doesNotThrowAnyException();
     }
 
-    @DisplayName("자동차 이름이 5자가 넘을 경우 에러가 발생한다.")
+    @DisplayName("자동차 이름이 5자 초과 또는 0자 이하일 경우 에러가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"에버에버에버", "에버에버에버에", "에버에버에버에버"})
+    @ValueSource(strings = {"", "666666"})
     void createByInvalidNameLength(String name) {
         assertThatThrownBy(() -> CarName.from(name))
                 .isInstanceOf(IllegalArgumentException.class);
