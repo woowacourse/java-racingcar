a60cbc26700593f118123b54fb19df2586183b48
Kyungwon Kim <masonkimseoul@gmail.com>
2024-02-19 12:12:21 +0900

    refactor: validateCarNamesFormat에서 예외 처리 추가
    자동차 이름은 알파벳 대소문자만 허용

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -2,12 +2,15 @@ package racingcar.util;
 
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Validator {
     private static final int MIN_TRY_COUNT = 1;
     private static final int MAX_TRY_COUNT = 50;
+    private static final String ALPHABET_FORMAT = "^[a-zA-Z]+$";
     private static final String EMPTY_CAR_NAMES_ERROR_MSG = "자동차 이름 목록을 입력해주세요.";
     private static final String CAR_NAME_LENGTH_ERROR_MSG = "자동차 이름은 1자 이상 5자 이하여야 합니다.";
+    private static final String CAR_NAME_ALPHABET_ERROR_MSG = "자동차 이름은 알파벳 대소문자로만 구성되어야 합니다.";
     private static final String NAME_DUPLICATED_ERROR_MSG = "자동차 이름은 중복될 수 없습니다.";
     private static final String TRY_COUNT_INTEGER_FORMAT_ERROR_MSG = "시도 회수는 정수여야 합니다.";
     private static final String TRY_COUNT_NEGATIVE_INTEGER_ERROR_MSG = "시도할 회수는 양의 정수여야 합니다.";
@@ -29,6 +32,9 @@ public class Validator {
     private static void validateCarNamesFormat(final String carName) {
         if (carName.isBlank() || carName.isEmpty() || carName.length() > 5) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MSG);
+        }
+        if (!Pattern.matches(ALPHABET_FORMAT, carName)) {
+            throw new IllegalArgumentException(CAR_NAME_ALPHABET_ERROR_MSG);
         }
     }
 
