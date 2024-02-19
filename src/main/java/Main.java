93a2b98b01c471936d1b5d0d5494e8c202ad814f
takoyakimchi <c4nyou@gmail.com>
2024-02-19 17:10:27 +0900

    refactor: main 내 출력 로직 OutputView로 이동

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,10 +14,6 @@ public class Main {
         RacingGame racingGame = errorHandler.readUntilNoExceptionOccurs(inputView::readCarNames);
         Round round = errorHandler.readUntilNoExceptionOccurs(inputView::readRound);
 
-        System.out.println("실행 결과");
-        for (int i = 0; i < round.getRound(); i++) {
-            outputView.printScore(racingGame.playOneRound());
-        }
-        outputView.printCars(racingGame.findWinners());
+        outputView.printGameResult(racingGame, round);
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,21 +1,32 @@
 package view;
 
 import domain.Car;
+import domain.RacingGame;
+import domain.Round;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
 
+    private static final String GAME_RESULT = "실행 결과";
     private static final String WIN_THE_GAME = "가 최종 우승했습니다.";
 
-    public void printScore(List<Car> cars) {
+    public void printGameResult(RacingGame racingGame, Round round) {
+        System.out.println(GAME_RESULT);
+        for (int i = 0; i < round.getRound(); i++) {
+            printScore(racingGame.playOneRound());
+        }
+        printCars(racingGame.findWinners());
+    }
+
+    private void printScore(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getScore()));
         }
         System.out.println();
     }
 
-    public void printCars(List<Car> cars) {
+    private void printCars(List<Car> cars) {
         List<String> carNames = new ArrayList<>();
         for (Car car : cars) {
             carNames.add(car.getName());
