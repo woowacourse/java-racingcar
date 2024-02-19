16cd7eef03ce30e11e60b363121592509326d6b4
JiHyeonL <lejinhy@gmail.com>
2024-02-19 11:07:34 +0900

    test: 랜덤 숫자 - 전진 테스트 소스 순서대로 작성

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -14,7 +14,7 @@ public class CarTest {
 
     @DisplayName("랜덤 숫자가 4 이상일 때만 자동차가 전진한다.")
     @ParameterizedTest
-    @CsvSource({"5, 1", "3, 0", "4, 1"})
+    @CsvSource({"3, 0", "4, 1", "5, 1" })
     void moveForwardTest(int randomNumber, int expectedPosition) {
         final Car car = Car.from("lemon");
         car.moveForward(randomNumber);
