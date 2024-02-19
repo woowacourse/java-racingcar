96d33d5549953cf357cacac21cc7113e1fd5e0ba
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-19 14:57:26 +0900

    style(src): 코드 컨벤션 적용

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,50 +1,50 @@
 package model;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarTest {
 
-  private Car car;
+    private Car car;
 
-  @BeforeEach
-  void setUp() {
-    car = new Car("pobi");
-    car.forward();
-    car.stop();
-    car.forward();
-  }
+    @BeforeEach
+    void setUp() {
+        car = new Car("pobi");
+        car.forward();
+        car.stop();
+        car.forward();
+    }
 
-  @ParameterizedTest
-  @NullAndEmptySource
-  @DisplayName("이름이 없을 시 예외가 발생한다.")
-  void validateNullAndEmpty(String input) {
-    assertThatThrownBy(() -> new Car(input)).isInstanceOf(IllegalArgumentException.class);
-  }
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이름이 없을 시 예외가 발생한다.")
+    void validateNullAndEmpty(String input) {
+        assertThatThrownBy(() -> new Car(input)).isInstanceOf(IllegalArgumentException.class);
+    }
 
-  @Test
-  @DisplayName("이름이 6자 이상일 시 예외가 발생한다.")
-  void validateNameLength() {
-    String name = "abcdefg";
-    assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
-  }
+    @Test
+    @DisplayName("이름이 6자 이상일 시 예외가 발생한다.")
+    void validateNameLength() {
+        String name = "abcdefg";
+        assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
+    }
 
-  @Test
-  @DisplayName("경주 현황을 알 수 있다.")
-  void getStatues() {
-    assertThat(car.getStatuses())
-            .containsExactly(MoveStatus.FORWARD, MoveStatus.STOP, MoveStatus.FORWARD);
-  }
+    @Test
+    @DisplayName("경주 현황을 알 수 있다.")
+    void getStatues() {
+        assertThat(car.getStatuses())
+                .containsExactly(MoveStatus.FORWARD, MoveStatus.STOP, MoveStatus.FORWARD);
+    }
 
-  @Test
-  @DisplayName("전진한 횟수를 알 수 있다.")
-  void getForwardCount() {
-    assertThat(car.getForwardCount()).isEqualTo(2);
-  }
+    @Test
+    @DisplayName("전진한 횟수를 알 수 있다.")
+    void getForwardCount() {
+        assertThat(car.getForwardCount()).isEqualTo(2);
+    }
 }
