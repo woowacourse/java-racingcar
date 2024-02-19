f64eafe7c559ffcc06f39ac39ec6da9831bb0e3a
nayonsoso <panda0329@naver.com>
2024-02-19 09:02:03 +0900

    refactor(RaceParticipantsRequest): 자동차 이름을 콤마로 구분할 때 utils를 사용하지 않는다.

--- src/main/java/racingcar/dto/request/RaceParticipantsRequest.java
+++ src/main/java/racingcar/dto/request/RaceParticipantsRequest.java
@@ -1,17 +1,25 @@
 package racingcar.dto.request;
 
-import java.util.List;
 import racingcar.domain.RaceParticipants;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.move.MovingStrategy;
-import racingcar.view.utils.InputUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 public record RaceParticipantsRequest(String input) {
     public RaceParticipants toRaceParticipants(final MovingStrategy movingStrategy) {
-        List<Car> cars = InputUtils.splitByComma(input).stream()
+        List<Car> cars = splitByComma().stream()
                 .map(carName -> new Car(carName, movingStrategy))
                 .toList();
 
         return new RaceParticipants(cars);
+    }
+
+    private List<String> splitByComma() {
+        if (input == null) {
+            return List.of();
+        }
+        return Arrays.stream(input.split(",", -1)).toList();
     }
 }
