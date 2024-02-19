6b35c9b43bad5fc19ca84f68949c4aa1104ae5f5
KimSangHyeok <tkdgur0906@naver.com>
2024-02-19 10:07:40 +0900

    refactor : 에러 메시지의 값을 변경하지 못하게 final 추가

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     MESSAGE_ONLY_NATURAL_NUMBER("시도할 회수는 자연수를 입력해 주세요."),
     MESSAGE_ONLY_NUMBER("시도할 회수는 숫자여야만 가능합니다.");
 
-    private String message;
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = message;
