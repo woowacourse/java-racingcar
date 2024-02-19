e4f2b966dc47b647968b8306815b4f55cd4ba05a
MingyeomKim <mingyum119@naver.com>
2024-02-19 12:51:39 +0900

    test: 우승자를 결정하는 로직 테스트

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -5,8 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import dto.CarStatus;
 import dto.CarsStatus;
+import dto.Winners;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -70,6 +71,61 @@ class CarsTest {
             for (CarStatus status : carsStatus.status()) {
                 assertThat(status.position()).isEqualTo(0);
             }
+        }
+    }
+
+    @DisplayName("우승자 결정 테스트")
+    @Nested
+    class JudgeTest {
+        @DisplayName("한 명의 우승자가 결정된다.")
+        @Test
+        void oneWinnerJudgeTest() {
+            // given
+            List<String> carNames = List.of("toby", "pobi", "anna");
+            Cars cars = Cars.from(carNames);
+
+            // when
+            cars.move(new TestOneWinnerGenerator());
+            Winners winners = cars.judge();
+
+            // then
+            Assertions.assertThat(winners.winners().size()).isEqualTo(1);
+            Assertions.assertThat(winners.winners().get(0)).isEqualTo("anna");
+        }
+
+        @DisplayName("두 명의 우승자가 결정된다.")
+        @Test
+        void bothWinnerJudgeTest() {
+            // given
+            List<String> carNames = List.of("toby", "pobi", "anna");
+            Cars cars = Cars.from(carNames);
+
+            // when
+            cars.move(new TestBothWinnerGenerator());
+            Winners winners = cars.judge();
+
+            // then
+            Assertions.assertThat(winners.winners().size()).isEqualTo(2);
+            Assertions.assertThat(winners.winners().get(0)).isEqualTo("pobi");
+            Assertions.assertThat(winners.winners().get(1)).isEqualTo("anna");
+        }
+    }
+
+    static class TestOneWinnerGenerator implements NumberGenerator {
+        private static List<Integer> numbers = List.of(3, 2, 9);
+        private static int index = 0;
+        @Override
+        public int generate() {
+            return numbers.get(index++);
+        }
+    }
+
+    static class TestBothWinnerGenerator implements NumberGenerator {
+        private static List<Integer> numbers = List.of(3, 6, 9);
+        private static int index = 0;
+        @Override
+        public int generate() {
+            return numbers.get(index++);
         }
     }
 }
