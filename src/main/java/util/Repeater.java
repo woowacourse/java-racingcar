7191971ed478a762f753bf80fc2d52f1cf052cf3
libienz <leekh10195@gmail.com>
2024-02-20 06:08:40 +0900

    refactor: 반복 유틸 클래스 인덴트 개선

--- src/main/java/util/Repeater.java
+++ src/main/java/util/Repeater.java
@@ -4,12 +4,11 @@ import java.util.function.Supplier;
 
 public class Repeater {
     public static <T> T repeatUntilNoException(Supplier<T> inputSupplier) {
-        while (true) {
-            try {
-                return inputSupplier.get();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            return inputSupplier.get();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputSupplier.get();
         }
     }
 }
