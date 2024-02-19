a1f33519b738e6807f138dd9f674bb1c3922203b
jongmee <ra123221@gmail.com>
2024-02-19 22:41:07 +0900

    refactor: 상수명 변경, 메서드 체이닝 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
 import racingcar.manager.RacingManager;
 
 public class Application {
     public static void main(String[] args) {
         RacingManager manager = new RacingManager();
-        manager.createRacingController().start();
+        RacingController racingController = manager.createRacingController();
+        racingController.start();
     }
 }
--- src/main/java/racingcar/view/ConsoleOutputView.java
+++ src/main/java/racingcar/view/ConsoleOutputView.java
@@ -8,7 +8,7 @@ public class ConsoleOutputView implements OutputView {
     private static final String RESULT_MSG = "실행 결과";
     private static final String POSITION_FORM = "%s : %s";
     private static final String TRACE = "-";
-    private static final String WINNER_MSG = "%s가 최종 우승했습니다.";
+    private static final String WINNER_FORM = "%s가 최종 우승했습니다.";
 
     public void printCarPosition(final Cars cars) {
         for(Car car : cars.getCars()) {
@@ -26,14 +26,10 @@ public class ConsoleOutputView implements OutputView {
     public void printWinners(final List<Car> winners) {
         List<String> winnerNames = winners.stream().map(Car::getName).toList();
         final String names = String.join(", ", winnerNames);
-        System.out.println(String.format(WINNER_MSG, names));
+        System.out.println(String.format(WINNER_FORM, names));
     }
 
     public void printResultMsg() {
         System.out.println(RESULT_MSG);
     }
-
-//    public void printErrorMsg(String errorMsg) {
-//        System.out.println(errorMsg);
-//    }
 }
