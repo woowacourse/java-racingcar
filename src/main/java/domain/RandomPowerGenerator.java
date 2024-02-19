49fe7ab282468e115b2b64b39f7e7947e152c65a
libienz <leekh10195@gmail.com>
2024-02-19 13:50:36 +0900

    refactor: PowerGenerator 인터페이스로 추출 다형성 개선

--- src/main/java/domain/RandomPowerGenerator.java
+++ src/main/java/domain/RandomPowerGenerator.java
@@ -1,10 +1,11 @@
 package domain;
 
-public class RandomPowerGenerator {
+public class RandomPowerGenerator implements PowerGenerator {
 
     private static final Integer MIN = 0;
     private static final Integer MAX = 9;
 
+    @Override
     public Power generate() {
         Integer randomNumber = makeRandomNumber();
         return Power.from(randomNumber);
