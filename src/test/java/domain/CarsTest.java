80209aba92fe13487545bdde5e990f040827996b
MingyeomKim <mingyum119@naver.com>
2024-02-19 12:42:59 +0900

    test(CarsTest): 자동차가 전진거나 전진하지 않는 테스트 코드 구현

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,8 +1,12 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import dto.CarStatus;
+import dto.CarsStatus;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -29,6 +33,43 @@ class CarsTest {
             assertThrows(IllegalArgumentException.class, () ->
                     Cars.from(carNames)
             );
+        }
+    }
+
+
+    @DisplayName("이동 테스트")
+    @Nested
+    class MoveTest {
+        @DisplayName("랜덤 값이 4 이상이어서 자동차들이 전진한다.")
+        @Test
+        void MoveTest() {
+            // given
+            List<String> carNames = List.of("toby", "pobi", "anna", "brown");
+            Cars cars = Cars.from(carNames);
+
+            // when
+            CarsStatus carsStatus = cars.move(() -> 7);
+
+            // then
+            for (CarStatus status : carsStatus.status()) {
+                assertThat(status.position()).isEqualTo(1);
+            }
+        }
+
+        @DisplayName("랜덤 값이 4 미만이어서 자동차들이 전진하지 않는다.")
+        @Test
+        void doesNotMoveTest() {
+            // given
+            List<String> carNames = List.of("toby", "pobi", "anna", "brown");
+            Cars cars = Cars.from(carNames);
+
+            // when
+            CarsStatus carsStatus = cars.move(() -> 3);
+
+            // then
+            for (CarStatus status : carsStatus.status()) {
+                assertThat(status.position()).isEqualTo(0);
+            }
         }
     }
 }
