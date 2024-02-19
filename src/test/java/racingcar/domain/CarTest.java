b682f62ffe29e665e7a161721301415c774b212e
Gyeongho Yang <chchch1213@naver.com>
2024-02-19 17:28:59 +0900

    refactor: 차 이름 길이 제한 임계값을 기준으로 하는 테스트로 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,13 +1,13 @@
 package racingcar.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("자동차")
 class CarTest {
@@ -29,7 +29,7 @@ class CarTest {
 
     @DisplayName("적절하지 않은 이름은 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"chocochip", "aaaaaaaaa"})
+    @ValueSource(strings = {"chocochip", "aaaaaa"})
     public void testInvalidName(String name) {
         //given & when & then
         assertThrows(IllegalArgumentException.class, () -> new Car(name));
