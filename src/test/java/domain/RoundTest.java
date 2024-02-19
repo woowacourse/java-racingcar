400d7428e4963f84dfdea42a675bffd944b1eec0
linirini <2001yerin@naver.com>
2024-02-19 13:52:53 +0900

    test : 1회 이상 시도횟수 생성 성공 테스트

--- src/test/java/domain/RoundTest.java
+++ src/test/java/domain/RoundTest.java
@@ -3,9 +3,18 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class RoundTest {
+
+    @DisplayName("시도횟수는 1회 이상이다.")
+    @Test
+    void validRoundCount(){
+        int count = 1;
+        Round round = new Round(1);
+        assertThat(round.getCount()).isEqualTo(count);
+    }
 
     @DisplayName("0 이하의 값을 입력하면 예외가 발생한다.")
     @Test
