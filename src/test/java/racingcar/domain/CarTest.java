d947870209c70d361a7ca653cc37c9edf0b5277c
tsulocalize <tsulocalize@gmail.com>
2024-02-19 10:17:09 +0900

    refactor : 자동차 이름 검증 Test Case 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,7 +6,27 @@ import org.junit.jupiter.api.Test;
 import racingcar.testCondition.FalseCondition;
 import racingcar.testCondition.TrueCondition;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarTest {
+    @Test
+    @DisplayName("이름이_5자_이상인_자동차는_생성할_수_없다")
+    void carNameLengthTest() {
+        assertThatThrownBy(() -> {
+                Car car = new Car("123456");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("이름에_공백을_포함할_수_없다")
+    void carNameNoSpaceTest() {
+        assertThatThrownBy(() -> {
+            Car car = new Car("12 34");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름에 공백을 포함할 수 없습니다.");
+    }
+
     @Test
     @DisplayName("조건이_참일_경우_전진")
     void moveCarTest() {
