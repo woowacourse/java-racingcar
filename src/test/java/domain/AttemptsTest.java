a12fa6eded07921ec82e9c0e17704c91c872956d
3juhwan <13selfesteem91@naver.com>
2024-02-20 22:19:41 +0900

    Test: Attempts 클래스 새로 추가된 동작 테스트
    - isEnd()
    - decrease()

--- src/test/java/domain/AttemptsTest.java
+++ src/test/java/domain/AttemptsTest.java
@@ -1,11 +1,11 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("시도 횟수 테스트")
 public class AttemptsTest {
@@ -25,4 +25,46 @@ public class AttemptsTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Attempts(numberOfAttempts));
     }
+
+    @Test
+    @DisplayName("시도 횟수가 정상적으로 감소하는가")
+    void valid_attempts_decrease_test() {
+        // given
+        Attempts attempts = new Attempts(9);
+
+        // when
+        attempts.decrease();
+
+        // then
+        assertThat(attempts).isEqualTo(new Attempts(8));
+    }
+
+    @Test
+    @DisplayName("시도가 종료됐는가")
+    void is_attempts_ended_test() {
+        // given
+        Attempts attempts = new Attempts(9);
+
+        // when
+        for (int i = 0; i < 9; i++) {
+            attempts.decrease();
+        }
+
+        // then
+        assertThat(attempts.isEnd()).isTrue();
+    }
+
+    @Test
+    @DisplayName("시도가 종료되지 않았는가")
+    void is_attempts_not_ended_test() {
+        // given
+        Attempts attempts = new Attempts(9);
+
+        // when
+        attempts.decrease();
+
+        // then
+        assertThat(attempts.isEnd()).isFalse();
+    }
+
 }
