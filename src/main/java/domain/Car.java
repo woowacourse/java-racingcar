93d84c93840d4526629d6993b5b2faf738e4504c
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-20 10:43:35 +0900

    feat : Car의 equals, hashCode Override

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Car {
     private static final int MINIMUM_CAR_NAME_LENGTH = 0;
     private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
@@ -43,6 +45,24 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Car) obj;
+        return Objects.equals(this.name, that.name) &&
+                this.position == that.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 
 }
