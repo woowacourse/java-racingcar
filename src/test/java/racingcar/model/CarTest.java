7ada3104496bb061529841626cb2ec9d5f7ee196
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-22 21:24:42 +0900

    test(car): 자동차의 현재 위치를 우승자의 위치와 비교하여 우승자를 판정하는 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +27,28 @@ class CarTest {
         car.move(randomNumber);
 
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차의 현재 위치를 우승자의 위치와 비교하여 우승자를 판정한다.")
+    void isWinnerTest() {
+        final Car car = new Car("moly");
+        final int movePosition = 4;
+        final int winnerPosition = 3;
+
+        car.move(movePosition);
+        car.move(movePosition);
+        car.move(movePosition);
+
+        assertThat(car.isWinner(winnerPosition)).isTrue();
+    }
+
+    @Test
+    @DisplayName("자동차의 현재 위치를 우승자의 위치와 비교하여 우승자를 판정한다.")
+    void isNotWinnerTest() {
+        final Car car = new Car("moly");
+        final int winnerPosition = 3;
+
+        assertThat(car.isWinner(winnerPosition)).isFalse();
     }
 }
