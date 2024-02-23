015937c390739764e465ac58c7aa56a18f1386dc
haiseong <wjdgotjd9908@gmail.com>
2024-02-23 10:54:25 +0900

    fix: record에 equals, hashcode 메서드 삭제

--- src/main/java/domain/car/CarDistance.java
+++ src/main/java/domain/car/CarDistance.java
@@ -1,7 +1,5 @@
 package domain.car;
 
-import java.util.Objects;
-
 public record CarDistance(int distance) {
     private static final CarDistance INITIAL_DISTANCE = new CarDistance(0);
 
@@ -11,22 +9,5 @@ public record CarDistance(int distance) {
 
     public static CarDistance getInitialDistance() {
         return INITIAL_DISTANCE;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        CarDistance that = (CarDistance) o;
-        return distance == that.distance;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(distance);
     }
 }
