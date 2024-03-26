f7dbd6c7e9bcc35647cccb49dc752c2329962edb
hidu java lotto <aoyouwe@gamil.com>
2024-03-26 18:06:42 +0900

    a

---
+++ src/main/java/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
+
+
